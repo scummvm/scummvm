68345930b5f0cc40a6412082850a18f2177b5b3b
Ludvig Strigeus <strigeus@users.sourceforge.net>
2001-11-08 19:59:05 +0000

    _useOrDecompress bug fix
    svn-id: r3475

--- gfx.cpp
+++ gfx.cpp
@@ -671,7 +671,7 @@ void Gdi::decompressBitmap() {
 		0x0, 0x1, 0x3, 0x7, 0xF, 0x1F, 0x3F, 0x7F, 0xFF, 0x0,
 	};
 
-	_useOrDecompress = true;
+	_useOrDecompress = false;
 
 	byte code = *_smap_ptr++;
 
