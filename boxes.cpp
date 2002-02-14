6cb652c26c64d9da42c116f38c53759af02c6a96
James Brown <ender@users.sourceforge.net>
2002-02-14 18:20:22 +0000

    More zak goodness!
    svn-id: r3581

--- boxes.cpp
+++ boxes.cpp
@@ -45,9 +45,12 @@ byte Scumm::getNumBoxes() {
 Box *Scumm::getBoxBaseAddr(int box) {
         byte *ptr = getResourceAddress(rtMatrix, 2);
 	checkRange(ptr[0]-1, 0, box, "Illegal box %d");
-	if(_features & GF_SMALL_HEADER)
-		return (Box*)(ptr + box*SIZEOF_BOX + 1);
-	else
+        if(_features & GF_SMALL_HEADER) {
+		if (_gameId == GID_ZAK256) 
+			return (Box*)(ptr + box*(SIZEOF_BOX-2) + 1);		
+		else
+                        return (Box*)(ptr + box*SIZEOF_BOX + 1);
+        } else
 		return (Box*)(ptr + box*SIZEOF_BOX + 2);
 }
 
