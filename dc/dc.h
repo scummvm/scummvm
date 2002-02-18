834c5efee960f7d989ba3232217dc0ca29db5985
Marcus Comstedt <marcus_c@users.sourceforge.net>
2002-02-18 19:56:50 +0000

    Prototype for initSound()
    svn-id: r3607

--- dc/dc.h
+++ dc/dc.h
@@ -1,4 +1,5 @@
 
+extern void initSound();
 extern void checkSound(Scumm *s);
 extern void handleInput(struct mapledev *pad, int16 &mouse_x, int16 &mouse_y,
 			byte &leftBtnPressed, byte &rightBtnPressed,
