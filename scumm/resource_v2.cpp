b17e5075bf2239b141c370571f18a950d0bdba5c
Max Horn <max@quendi.de>
2002-09-13 23:29:38 +0000

    fixed file header
    svn-id: r4937

--- scumm/resource_v2.cpp
+++ scumm/resource_v2.cpp
@@ -1,6 +1,5 @@
 /* ScummVM - Scumm Interpreter
- * Copyright (C) 2001  Ludvig Strigeus
- * Copyright (C) 2001/2002 The ScummVM project
+ * Copyright (C) 2002 The ScummVM project
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -15,6 +14,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Header$
  *
  */
 
