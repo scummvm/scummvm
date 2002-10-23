ae8d845472c90f129f973ec9898eede2ddef4603
Nicolas Bacca <arisme@users.sourceforge.net>
2002-10-23 00:01:46 +0000

    Changes to WinCE project
    svn-id: r5266

--- backends/wince/resource.h
+++ backends/wince/resource.h
@@ -2,57 +2,15 @@
 // Microsoft Developer Studio generated include file.
 // Used by PocketSCUMM.rc
 //
-#define IDD_GAMESELECT                  105
-#define IDD_SOUND_DIALOG                106
-#define IDB_SKIP                        113
-#define IDB_DISK                        115
-#define IDB_EXIT                        116
-#define IDB_SOUND_ON                    117
-#define IDB_SOUND_OFF                   118
-#define IDB_PANEL                       119
-#define IDI_POCKETSCUMM                 127
-#define IDB_KEYBOARD                    129
-#define IDL_POCKETSCUMM                 130
-#define IDB_MENUTITLE                   133
-#define IDD_DIALOG1                     134
-#define IDM_MENU                        1000
-#define IDC_ABOUT                       1000
-#define IDC_MONKEY                      1000
-#define IDC_OPTIONS                     1000
-#define IDC_EXIT                        1001
-#define IDC_MONKEY2                     1001
-#define IDD_SLIDERMUSIC                 1001
-#define IDC_ATLANTIS                    1002
-#define IDD_SLIDERSFX                   1002
-#define IDC_PLAYFATE                    1003
-#define IDC_TENTACLE                    1004
-#define IDC_DOTTDEMO                    1005
-#define IDC_SAMNMAX                     1006
-#define IDC_SNMDEMO                     1007
-#define IDC_DISABLE_SOUND               1008
-#define IDC_SLOW_SOUND                  1009
-#define IDC_FT                          1010
-#define IDC_LISTAVAILABLE               1011
-#define IDC_GAMEDESC                    1014
-#define IDC_PLAY                        1015
-#define IDC_FILEPATH                    1016
-#define IDC_SCAN                        1018
-#define ID_POCKETSCUMM                  40005
-#define IDS_CAP_POCKETSCUMM             40006
-#define IDC_LANDSCAPE                   40007
-#define IDC_SKIP                        40008
-#define IDC_LOADSAVE                    40009
-#define IDC_SOUND                       40011
-#define IDC_SOUNDVOL                    40012
-#define IDM_POCKETSCUMM                 40013
+#define IDI_ICON1                       101
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        135
-#define _APS_NEXT_COMMAND_VALUE         40014
-#define _APS_NEXT_CONTROL_VALUE         1016
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
