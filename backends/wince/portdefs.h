f2cff6cc10a7072c450323eaafe9226807f0f109
Nicolas Bacca <arisme@users.sourceforge.net>
2002-10-31 01:05:50 +0000

    doh
    svn-id: r5358

--- backends/wince/portdefs.h
+++ backends/wince/portdefs.h
@@ -1,3 +1,26 @@
+/* ScummVM - Scumm Interpreter
+ * Copyright (C) 2001/2002 The ScummVM project
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Header$
+ *
+ */
+
+// Missing string/stdlib/assert declarations for WinCE 2.xx
+
 #if _WIN32_WCE < 300
 
 void *calloc(size_t n, size_t s);
@@ -10,4 +33,5 @@ void assert( void* expression );
 void assert( int expression );
 long int strtol(const char *nptr, char **endptr, int base);
 char *strdup( const char *s);
+
 #endif
