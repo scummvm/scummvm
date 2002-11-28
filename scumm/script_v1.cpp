1cacb9badb6a9f5b4fc3d2597163b3c586e58f66
Max Horn <max@quendi.de>
2002-11-28 22:11:43 +0000

    fix for bug #600287 (ZAK256: Palette messed up)
    svn-id: r5736

--- scumm/script_v1.cpp
+++ scumm/script_v1.cpp
@@ -1653,7 +1653,7 @@ void Scumm::o5_roomOps()
 				b = getVarOrDirectWord(0x40);
 			}
 			checkRange(256, 0, a, "o5_roomOps: 2: Illegal room color slot (%d)");
-			_currentPalette[a] = b;
+			copyPalColor(a, b);
 			_fullRedraw = 1;
 		} else {
 			error("room-color is no longer a valid command");
