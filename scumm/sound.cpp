c44512ff718e8e20b535892f8de806b1394cd4b7
Max Horn <max@quendi.de>
2002-12-26 22:02:58 +0000

    added FIXME comment
    svn-id: r6178

--- scumm/sound.cpp
+++ scumm/sound.cpp
@@ -954,11 +954,13 @@ static void music_handler (void * engine) {
 #define OUTPUT_SIZE 66150 // ((22050 * 2 * 2) / 4) * 3
 
 void Sound::playBundleMusic(char * song) {
+
 	if (_scumm->_silentDigitalImuse == true) {
 		return;
 	}
 
 	if (_nameBundleMusic == NULL) {
+		// FIXME: we have MUSDISK1.BUN and MUSDISK2.BUN in COMI.
 		if (_scumm->_bundle->openMusicFile("digmusic.bun", _scumm->getGameDataPath()) == false) {
 			return;
 		}
