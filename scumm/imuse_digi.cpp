f63cb964685392d4954f23ce25abc7132ec4b375
Paweł Kołodziejski <aquadran@scummvm.org>
2002-12-28 21:29:46 +0000

    added CMI music imuse tables
    svn-id: r6240

--- scumm/imuse_digi.cpp
+++ scumm/imuse_digi.cpp
@@ -256,6 +256,196 @@ static const imuse_music_table _digStateMusicTable[] = {
 	{-1,	"",											"",												"",							0},
 };
 
+static const imuse_music_table _comiStateMusicTable[] = {
+	{1000,	"STATE_NULL",					"",												"",							0},
+	{1001,	"stateNoChange",			"",												"",							0},
+	{1098,	"stateCredits1",			"1098-Credits1",					"1098-C~1.IMX",	0},
+	{1099,	"stateMenu",					"1099-Menu",							"1099-M~1.IMX",	0},
+	{1100,	"stateHold1",					"1100-Hold1",							"1100-H~1.IMX",	0},
+	{1101,	"stateWaterline1",		"1101-Waterline1",				"1101-W~1.IMX",	0},
+	{1102,	"stateHold2",					"1102-Hold2",							"1102-H~1.IMX",	0},
+	{1103,	"stateWaterline2",		"1103-Waterline2",				"1103-W~1.IMX",	0},
+	{1104,	"stateCannon",				"1104-Cannon",						"1104-C~1.IMX",	0},
+	{1105,	"stateTreasure",			"1105-Treasure",					"1105-T~1.IMX",	0},
+	{1200,	"stateFortBase",			"1200-Fort Base",					"1200-F~1.IMX",	0},
+	{1201,	"statePreFort",				"1201-Pre-Fort",					"1201-P~1.IMX",	0},
+	{1202,	"statePreVooOut",			"1202-PreVoo Out",				"1202-P~1.IMX",	0},
+	{1203,	"statePreVooIn",			"1203-PreVoo In",					"1203-P~1.IMX",	0},
+	{1204,	"statePreVooLad",			"1204-PreVoo Lady",				"1204-P~1.IMX",	0},
+	{1205,	"stateVoodooOut",			"1205-Voodoo Out",				"1205-V~1.IMX",	0},
+	{1210,	"stateVoodooIn",			"1210-Voodoo In",					"1210-V~1.IMX",	0},
+	{1212,	"stateVoodooInAlt",		"1210-Voodoo In",					"1210-V~1.IMX",	0},
+	{1215,	"stateVoodooLady",		"1215-Voodoo Lady",				"1215-V~1.IMX",	0},
+	{1219,	"statePrePlundermap",	"1219-Pre-Map",						"1219-P~1.IMX",	0},
+	{1220,	"statePlundermap",		"1220-Plunder Map",				"1220-P~1.IMX",	0},
+	{1222,	"statePreCabana",			"1222-Pre-Cabana",				"1222-P~1.IMX",	0},
+	{1223,	"stateCabana",				"1223-Cabana",						"1223-C~1.IMX",	0},
+	{1224,	"statePostCabana",		"1224-Post-Cabana",				"1224-P~1.IMX",	0},
+	{1225,	"stateBeachClub",			"1225-Beach Club",				"1225-B~1.IMX",	0},
+	{1230,	"stateCliff",					"1230-Cliff",							"1230-C~1.IMX",	0},
+	{1232,	"stateBelly",					"1232-Belly",							"1232-B~1.IMX",	0},
+	{1235,	"stateQuicksand",			"1235-Quicksand",					"1235-Q~1.IMX",	0},
+	{1240,	"stateDangerBeach",		"1240-Danger Beach",			"1240-D~1.IMX",	0},
+	{1241,	"stateDangerBeachAlt","1240-Danger Beach",			"1240-D~1.IMX",	0},
+	{1245,	"stateRowBoat",				"1245-Row Boat",					"1245-R~1.IMX",	0},
+	{1247,	"stateAlongside",			"1247-Alongside",					"1247-A~1.IMX",	0},
+	{1277,	"stateAlongsideAlt",	"1247-Alongside",					"1247-A~1.IMX",	0},
+	{1250,	"stateChimpBoat",			"1250-Chimp Boat",				"1250-C~1.IMX",	0},
+	{1255,	"stateMrFossey",			"1255-Mr Fossey",					"1255-M~1.IMX",	0},
+	{1259,	"statePreTown",				"1259-Pre-Town",					"1259-P~1.IMX",	0},
+	{1260,	"stateTown",					"1260-Town",							"1260-T~1.IMX",	0},
+	{1264,	"statePreMeadow",			"1264-Pre-Meadow",				"1264-P~1.IMX",	0},
+	{1265,	"stateMeadow",				"1265-Meadow",						"1265-M~1.IMX",	0},
+	{1266,	"stateMeadowAmb",			"1266-Meadow Amb",				"1266-M~1.IMX",	0},
+	{1270,	"stateWardrobePre",		"1270-Wardrobe-Pre",			"1270-W~1.IMX",	0},
+	{1272,	"statePreShow",				"1272-Pre-Show",					"1272-P~1.IMX",	0},
+	{1274,	"stateWardrobeShow",	"1274-Wardrobe-Show",			"1274-W~1.IMX",	0},
+	{1276,	"stateShow",					"1276-Show",							"1276-S~1.IMX",	0},
+	{1277,	"stateWardrobeJug",		"1277-Wardrobe-Jug",			"1277-W~1.IMX",	0},
+	{1278,	"stateJuggling",			"1278-Juggling",					"1278-J~1.IMX",	0},
+	{1279,	"statePostShow",			"1279-Post-Show",					"1279-P~1.IMX",	0},
+	{1280,	"stateChickenShop",		"1280-Chicken Shop",			"1280-C~1.IMX",	0},
+	{1285,	"stateBarberShop",		"1285-Barber Shop",				"1285-B~1.IMX",	0},
+	{1286,	"stateVanHelgen",			"1286-Van Helgen",				"1286-V~1.IMX",	0},
+	{1287,	"stateBill",					"1287-Bill",							"1287-B~1.IMX",	0},
+	{1288,	"stateHaggis",				"1288-Haggis",						"1288-H~1.IMX",	0},
+	{1289,	"stateRottingham",		"1289-Rottingham",				"1289-R~1.IMX",	0},
+	{1305,	"stateDeck",					"1305-Deck",							"1305-D~1.IMX",	0},
+	{1310,	"stateCombatMap",			"1310-Combat Map",				"1310-C~1.IMX",	0},
+	{1320,	"stateShipCombat",		"1320-Ship Combat",				"1320-S~1.IMX",	0},
+	{1325,	"stateSwordfight",		"1325-Swordfight",				"1325-S~1.IMX",	0},
+	{1327,	"stateSwordRott",			"1327-Sword Rott",				"1327-S~1.IMX",	0},
+	{1330,	"stateTownEdge",			"1330-Town Edge",					"1330-T~1.IMX",	0},
+	{1335,	"stateSwordLose",			"1335-Sword Lose",				"1335-S~1.IMX",	0},
+	{1340,	"stateSwordWin",			"1340-Sword Win",					"1340-S~1.IMX",	0},
+	{1345,	"stateGetMap",				"1345-Get Map",						"1345-G~1.IMX",	0},
+	{1400,	"stateWreckBeach",		"1400-Wreck Beach",				"1400-W~1.IMX",	0},
+	{1405,	"stateBloodMap",			"1405-Blood Map",					"1405-B~1.IMX",	0},
+	{1410,	"stateClearing",			"1410-Clearing",					"1410-C~1.IMX",	0},
+	{1415,	"stateLighthouse",		"1415-Lighthouse",				"1415-L~1.IMX",	0},
+	{1420,	"stateVillage",				"1420-Village",						"1420-V~1.IMX",	0},
+	{1423,	"stateVolcano",				"1423-Volcano",						"1423-V~1.IMX",	0},
+	{1425,	"stateAltar"					"1425-Altar",							"1425-A~1.IMX",	0},
+	{1430,	"stateHotelOut",			"1430-Hotel Out",					"1430-H~1.IMX",	0},
+	{1435,	"stateHotelBar",			"1435-Hotel Bar",					"1435-H~1.IMX",	0},
+	{1440,	"stateHotelIn",				"1440-Hotel In",					"1440-H~1.IMX",	0},
+	{1445,	"stateTarotLady",			"1445-Tarot Lady",				"1445-T~1.IMX",	0},
+	{1447,	"stateGoodsoup",			"1447-Goodsoup",					"1447-G~1.IMX",	0},
+	{1448,	"stateGuestRoom",			"1448-Guest Room",				"1448-G~1.IMX",	0},
+	{1450,	"stateWindmill",			"1450-Windmill",					"1450-W~1.IMX",	0},
+	{1455,	"stateCemetary",			"1455-Cemetary",					"1455-C~1.IMX",	0},
+	{1460,	"stateCrypt",					"1460-Crypt",							"1460-C~1.IMX",	0},
+	{1463,	"stateGraveDigger",		"1463-Gravedigger",				"1463-G~1.IMX",	0},
+	{1465,	"stateMonkey1",				"1465-Monkey1",						"1465-M~1.IMX",	0},
+	{1475,	"stateStanDark",			"1475-Stan Dark",					"1475-S~1.IMX",	0},
+	{1477,	"stateStanLight",			"1477-Stan",							"1477-S~1.IMX",	0},
+	{1480,	"stateEggBeach",			"1480-Egg Beach",					"1480-E~1.IMX",	0},
+	{1485,	"stateSkullIsland",		"1485-Skull Island",			"1485-S~1.IMX",	0},
+	{1490,	"stateSmugglersCave",	"1490-Smugglers",					"1490-S~1.IMX",	0},
+	{1500,	"stateLeChuckTalk",		"1500-Le Talk",						"1500-L~1.IMX",	0},
+	{1505,	"stateCarnival",			"1505-Carnival",					"1505-C~1.IMX",	0},
+	{1511,	"stateHang",					"1511-Hang",							"1511-H~1.IMX",	0},
+	{1512,	"stateRum",						"1512-Rum",								"1512-RUM.IMX",	0},
+	{1513,	"stateTorture",				"1513-Torture",						"1513-T~1.IMX",	0},
+	{1514,	"stateSnow",					"1514-Snow",							"1514-S~1.IMX", 0},
+	{1515,	"stateCredits",				"1515-Credits (end)",			"1515-C~1.IMX",	0},
+	{1520,	"stateCarnAmb",				"1520-Carn Amb",					"1520-C~1.IMX",	0},
+	{-1,	"",											"",												"",							0},
+};
+
+static const imuse_music_table _comiSeqMusicTable[] = {
+	{2000,	"SEQ_NULL",						"",												"",							0},
+	{2100,	"seqINTRO",						"",												"",							0},
+	{2105,	"seqInterlude1",			"2105-Interlude1",				"2105-I~1.IMX",	0},
+	{2110,	"seqLastBoat",				"",												"",							0},
+	{2115,	"seqSINK_SHIP",				"",												"",							0},
+	{2120,	"seqCURSED_RING",			"",												"",							0},
+	{2200,	"seqInterlude2",			"2200-Interlude2",				"2200-I~1.IMX",	0},
+	{2210,	"seqKidnapped",				"2210-Kidnapped",					"2210-K~1.IMX",	0},
+	{2220,	"seqSnakeVomits",			"",												"",							0},
+	{2222,	"seqPopBalloon",			"",												"",							0},
+	{2225,	"seqDropBalls",				"2225-Drop Balls",				"2225-D~1.IMX",	0},
+	{2232,	"seqArriveBarber",		"2232-Arrive Barber",			"2232-A~1.IMX",	0},
+	{2233,	"seqAtonal",					"2233-Atonal",						"2233-A~1.IMX",	0},
+	{2235,	"seqShaveHead1",			"2235-Shave Head",				"2235-S~1.IMX",	0},
+	{2236,	"seqShaveHead2",			"2235-Shave Head",				"2235-S~1.IMX",	0},
+	{2245,	"seqCaberLose",				"2245-Caber Lose",				"2245-C~1.IMX",	0},
+	{2250,	"seqCaberWin",				"2250-Caber Win",					"2250-C~1.IMX",	0},
+	{2255,	"seqDuel1",						"2255-Duel",							"2255-D~1.IMX",	0},
+	{2256,	"seqDuel2",						"2255-Duel",							"2255-D~1.IMX",	0},
+	{2257,	"seqDuel3",						"2255-Duel",							"2255-D~1.IMX",	0},
+	{2260,	"seqBlowUpTree1",			"2260-Blow Up Tree",			"2260-B~1.IMX",	0},
+	{2261,	"seqBlowUpTree2",			"2260-Blow Up Tree",			"2260-B~1.IMX",	0},
+	{2275,	"seqMonkeys",					"2275-Monkeys",						"2275-M~1.IMX",	0},
+	{2277,	"seqAttack",					"",												"",							0},
+	{2285,	"seqSharks",					"2285-Sharks",						"2285-S~1.IMX",	0},
+	{2287,	"seqTowelWalk",				"2287-Towel Walk",				"2287-T~1.IMX",	0},
+	{2293,	"seqNICE_BOOTS",			"",												"",							0},
+	{2295,	"seqBIG_BONED",				"",												"",							0},
+	{2300,	"seqToBlood",					"2300-To Blood",					"2300-T~1.IMX",	0},
+	{2301,	"seqInterlude3",			"2301-Interlude3",				"2301-I~1.IMX",	0},
+	{2302,	"seqRott1",						"2302-Rott Attack",				"2302-R~1.IMX",	0},
+	{2304,	"seqRott2",						"2302-Rott Attack",				"2302-R~1.IMX",	0},
+	{2305,	"seqRott2b",					"2302-Rott Attack",				"2302-R~1.IMX",	0},
+	{2306,	"seqRott3",						"2302-Rott Attack",				"2302-R~1.IMX",	0},
+	{2308,	"seqRott4",						"2302-Rott Attack",				"2302-R~1.IMX",	0},
+	{2309,	"seqRott5",						"2302-Rott Attack",				"2302-R~1.IMX",	0},
+	{2311,	"seqVerse1",					"2311-Song",							"2311-S~1.IMX",	0},
+	{2312,	"seqVerse2",					"2311-Song",							"2311-S~1.IMX",	0},
+	{2313,	"seqVerse3",					"2311-Song",							"2311-S~1.IMX",	0},
+	{2314,	"seqVerse4",					"2311-Song",							"2311-S~1.IMX",	0},
+	{2315,	"seqVerse5",					"2311-Song",							"2311-S~1.IMX",	0},
+	{2316,	"seqVerse6",					"2311-Song",							"2311-S~1.IMX",	0},
+	{2317,	"seqVerse7",					"2311-Song",							"2311-S~1.IMX",	0},
+	{2318,	"seqVerse8",					"2311-Song",							"2311-S~1.IMX",	0},
+	{2319,	"seqSongEnd",					"2311-Song",							"2311-S~1.IMX",	0},
+	{2336,	"seqRiposteLose",			"2336-Riposte Lose",			"2336-R~1.IMX",	0},
+	{2337,	"seqRiposteWin",			"2337-Riposte Win",				"2337-R~1.IMX",	0},
+	{2338,	"seqInsultLose",			"2338-Insult Lose",				"2338-I~1.IMX",	0},
+	{2339,	"seqInsultWin",				"2339-Insult Win",				"2339-I~1.IMX",	0},
+	{2340,	"seqSwordLose",				"1335-Sword Lose",				"1335-S~1.IMX",	0},
+	{2345,	"seqSwordWin",				"1340-Sword Win",					"1340-S~1.IMX",	0},
+	{2347,	"seqGetMap",					"1345-Get Map",						"1345-G~1.IMX",	0},
+	{2400,	"seqInterlude4",			"2400-Interlude4",				"2400-I~1.IMX",	0},
+	{2405,	"seqSHIPWRECK",				"",												"2408-F~1.IMX",	0},
+	{2408,	"seqFakeCredits",			"2408-Fake Credits",			"2408-F~1.IMX",	0},
+	{2410,	"seqPassOut",					"2410-Pass Out",					"2410-P~1.IMX",	0},
+	{2414,	"seqGhostTalk",				"2414-Ghost Talk",				"2414-G~1.IMX",	0},
+	{2415,	"seqGhostWedding",		"2414-Ghost Talk",				"2414-G~1.IMX",	0},
+	{2420,	"seqEruption",				"2420-Eruption",					"2420-E~1.IMX",	0},
+	{2425,	"seqSacrifice",				"2425-Sacrifice",					"2425-S~1.IMX",	0},
+	{2426,	"seqSacrificeEnd",		"2425-Sacrifice",					"2425-S~1.IMX",	0},
+	{2430,	"seqScareDigger",			"2430-Scare Digger",			"2430-S~1.IMX",	0},
+	{2445,	"seqSkullArrive",			"2445-Skull Arrive",			"2445-S~1.IMX",	0},
+	{2450,	"seqFloat",						"2450-Cliff Fall",				"2450-C~1.IMX",	0},
+	{2451,	"seqFall",						"2450-Cliff Fall",				"2450-C~1.IMX",	0},
+	{2452,	"seqUmbrella",				"2450-Cliff Fall",				"2450-C~1.IMX",	0},
+	{2460,	"seqFight",						"2460-Fight",							"2460-F~1.IMX",	0},
+	{2465,	"seqLAVE_RIDE",				"",												"",							0},
+	{2470,	"seqMORE_SLAW",				"",												"",							0},
+	{2480,	"seqLIFT_CURSE",			"",												"",							0},
+	{2500,	"seqInterlude5",			"2500-Interlude5",				"2500-I~1.IMX",	0},
+	{2502,	"seqExitSkycar",			"2502-Exit Skycar",				"2502-E~1.IMX",	0},
+	{2504,	"seqGrow1",						"2504-Grow",							"2504-G~1.IMX",	0},
+	{2505,	"seqGrow2",						"2504-Grow",							"2504-G~1.IMX",	0},
+	{2508,	"seqInterlude6",			"2508-Interlude6",				"2508-I~1.IMX",	0},
+	{2515,	"seqFINALE",					"",												"",							0},
+	{2520,	"seqOut",							"2520-Out",								"2520-OUT.IMX",	0},
+	{2530,	"seqZap1a",						"2530-Zap1",							"2530-Z~1.IMX",	0},
+	{2531,	"seqZap1b",						"2530-Zap1",							"2530-Z~1.IMX",	0},
+	{2532,	"seqZap1c",						"2530-Zap1",							"2530-Z~1.IMX",	0},
+	{2540,	"seqZap2a",						"2540-Zap2",							"2540-Z~1.IMX",	0},
+	{2541,	"seqZap2b",						"2540-Zap2",							"2540-Z~1.IMX",	0},
+	{2542,	"seqZap2c",						"2540-Zap2",							"2540-Z~1.IMX",	0},
+	{2550,	"seqZap3a",						"2550-Zap3",							"2550-Z~1.IMX",	0},
+	{2551,	"seqZap3b",						"2550-Zap3",							"2550-Z~1.IMX",	0},
+	{2552,	"seqZap3c",						"2550-Zap3",							"2550-Z~1.IMX",	0},
+	{2560,	"seqZap4a",						"2560-Zap4",							"2560-Z~1.IMX",	0},
+	{2561,	"seqZap4b",						"2560-Zap4",							"2560-Z~1.IMX",	0},
+	{2562,	"seqZap4c",						"2560-Zap4",							"2560-Z~1.IMX",	0},
+	{-1,		"",										"",												"",							0},
+};
+
 static const imuse_music_table _digSeqMusicTable[] = {
 	{2000,	"SEQ_NULL",							"",												"",							0},
 	{2005,	"seqLogo",							"",												"",							0},
@@ -868,6 +1058,19 @@ int32 IMuseDigital::doCommand(int a, int b, int c, int d, int e, int f, int g, i
 						return 0;
 					}
 				}
+			} else if (_scumm->_gameId == GID_CMI) {
+				for(l = 0;; l++) {
+					if (_comiStateMusicTable[l].index == -1) {
+						return 1;
+					}
+					if ((_comiStateMusicTable[l].index == b)) {
+						debug(2, "Play imuse music: %s, %s, %s", _comiStateMusicTable[l].name, _comiStateMusicTable[l].title, _comiStateMusicTable[l].filename);
+						if (_comiStateMusicTable[l].filename[0] != 0) {
+//							_scumm->_sound->playBundleMusic((char*)&_comiStateMusicTable[l].filename);
+						}
+						return 0;
+					}
+				}
 			} else if (_scumm->_gameId == GID_FT) {
 				for(l = 0;; l++) {
 					if (_ftStateMusicTable[l].index == -1) {
@@ -898,6 +1101,19 @@ int32 IMuseDigital::doCommand(int a, int b, int c, int d, int e, int f, int g, i
 						debug(2, "Play imuse music: %s, %s, %s", _digSeqMusicTable[l].name, _digSeqMusicTable[l].title, _digSeqMusicTable[l].filename);
 						if (_digSeqMusicTable[l].filename[0] != 0) {
 							_scumm->_sound->playBundleMusic((char*)&_digSeqMusicTable[l].filename);
+						}
+						return 0;
+					}
+				}
+			} else if (_scumm->_gameId == GID_CMI) {
+				for(l = 0;; l++) {
+					if (_comiSeqMusicTable[l].index == -1) {
+						return 1;
+					}
+					if ((_comiSeqMusicTable[l].index == b)) {
+						debug(2, "Play imuse music: %s, %s, %s", _comiSeqMusicTable[l].name, _comiSeqMusicTable[l].title, _comiSeqMusicTable[l].filename);
+						if (_comiSeqMusicTable[l].filename[0] != 0) {
+//							_scumm->_sound->playBundleMusic((char*)&_comiSeqMusicTable[l].filename);
 						}
 						return 0;
 					}
