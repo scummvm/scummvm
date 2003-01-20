8d3e8505a18efd6271eeab5139c3bf54914c893d
Nicolas Bacca <arisme@users.sourceforge.net>
2003-01-20 07:59:43 +0000

    Missing exported functions for HPCs
    svn-id: r6526

--- backends/wince/portdefs.h
+++ backends/wince/portdefs.h
@@ -25,6 +25,8 @@
 
 void *calloc(size_t n, size_t s);
 int isdigit(int c);
+int isprint(int c);
+int isspace(int c);
 char *strrchr(const char *s, int c);
 char *strdup(const char *s);
 int _stricmp( const char *string1, const char *string2 );
@@ -33,5 +35,4 @@ void assert( void* expression );
 void assert( int expression );
 long int strtol(const char *nptr, char **endptr, int base);
 char *strdup( const char *s);
-
 #endif
