5c94c996edc9de19588d408e9b959d18910b23af
Jonathan Gray <khalek@users.sourceforge.net>
2003-01-29 12:22:24 +0000

    remove outdated comment
    svn-id: r6571

--- scumm/vars.cpp
+++ scumm/vars.cpp
@@ -241,7 +241,7 @@ void Scumm_v8::setupScummVars()
 
 	//VAR_VOICE_MODE = 39; // 0 is voice, 1 is voice+text, 2 is text only
 	VAR_GAME_LOADED = 40;
-	VAR_LANGUAGE = 41; // see Languages enum in scumm.h for possible values	
+	VAR_LANGUAGE = 41;
 	
 	VAR_CURRENTDISK = 42;
 
