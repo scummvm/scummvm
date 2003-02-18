5a60d022cac590bc7cb3ca2ee0a3654cfcf8fb6b
Jonathan Gray <khalek@users.sourceforge.net>
2003-02-18 01:09:49 +0000

    fix typo
    svn-id: r6620

--- common/gameDetector.cpp
+++ common/gameDetector.cpp
@@ -126,9 +126,9 @@ const VersionSettings version_settings[] = {
 
 	/* Scumm Version 6 */
 	{"puttputt", "Putt-Putt Joins The Parade (DOS)", GID_SAMNMAX, 6, 1, 1,
-	 GF_NEW_OPCODES | GF_AFTER_V6 | GF_USE_KEY | GF_ADLIB_DEFAULT | GF_HUMONGOUS, 0},
-	{"puttdemo", "Putt-Putt Joins The Parade (DOS Demo)", GID_SAMNMAX, 6, 1, 1,
 	 GF_NEW_OPCODES | GF_AFTER_V6 | GF_USE_KEY | GF_ADLIB_DEFAULT | GF_HUMONGOUS | GF_NEW_COSTUMES, 0},
+	{"puttdemo", "Putt-Putt Joins The Parade (DOS Demo)", GID_SAMNMAX, 6, 1, 1,
+	 GF_NEW_OPCODES | GF_AFTER_V6 | GF_USE_KEY | GF_ADLIB_DEFAULT | GF_HUMONGOUS, 0},
 	{"moondemo", "Putt-Putt Goes To The Moon (DOS Demo)", GID_SAMNMAX, 6, 1, 1,
 	 GF_NEW_OPCODES | GF_AFTER_V6 | GF_USE_KEY | GF_ADLIB_DEFAULT | GF_HUMONGOUS, 0},
 	{"puttmoon", "Putt-Putt Goes To The Moon (DOS)", GID_SAMNMAX, 6, 1, 1,
