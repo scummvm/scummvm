2d1e313018994f9ac5feab903f205e2bc4db183d
Jonathan Gray <khalek@users.sourceforge.net>
2003-04-26 05:29:25 +0000

    stop assertion from triggering to make ft finishable again
    svn-id: r7123

--- scumm/smush/smush_player.cpp
+++ scumm/smush/smush_player.cpp
@@ -384,7 +384,9 @@ void SmushPlayer::handleImuseAction(Chunk &b) {
 	int flags = b.getWord();
 	int unknown = b.getShort();
 	int track_flags = b.getWord();
-
+	
+	if (flags != 46)
+		return;
 	assert(flags == 46 && unknown == 0);
 	int track_id = b.getWord();
 	int index = b.getWord();
