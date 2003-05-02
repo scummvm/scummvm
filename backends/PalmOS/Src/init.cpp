4e5f8d1ed8dc1e8679a82c67c6b24165f555e9ff
Chris Apers <chrilith@users.sourceforge.net>
2003-05-02 14:39:29 +0000

    Not needed
    svn-id: r7267

--- backends/PalmOS/Src/init.cpp
+++
@@ -1,9 +0,0 @@
-//#include <PalmOS.h>
-//#include "palm.h"
-//#include "starterrsc.h"
-
-//UInt16 gHRrefNum = 0;
-//Boolean gVibrator = false;
-//Boolean gFlipping = false;
-//Boolean gScreenLocked = false;
-
