3703ed51dca1450eb7c6c70ffdaffb34728d8ba5
Jamieson Christian <jamieson630@users.sourceforge.net>
2003-05-19 20:34:41 +0000

    Oops....
    svn-id: r7700

--- common/gameDetector.cpp
+++ common/gameDetector.cpp
@@ -635,7 +635,7 @@ int GameDetector::detectMain() {
 	 * default, OR if the game is an older game that doesn't
 	 * support anything else anyway. */
 #ifndef __PALM_OS__ // currently adlib is not supported, is this really needed ?
-	if ((_midi_driver == MD_AUTO && _features & GF_ADLIB_DEFAULT) || GF_SMALL_HEADER) {
+	if ((_midi_driver == MD_AUTO && _features & GF_ADLIB_DEFAULT) || _features & GF_SMALL_HEADER) {
 		_midi_driver = MD_ADLIB;
 		_use_adlib = true;
 	}
