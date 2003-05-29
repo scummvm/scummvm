a8d6ac05da168ab69d2cf02faf403e26dfe2419b
Robert Göffringmann <lavosspawn@users.sourceforge.net>
2003-05-29 14:06:01 +0000

    whoops
    svn-id: r8101

--- sky/sky.cpp
+++ sky/sky.cpp
@@ -143,13 +143,12 @@ void SkyState::go() {
 	loadBase0();
 
 	while (1) {
-		//delay(100);
+		delay(100);
 		_skyLogic->engine();
 		_skyScreen->recreate();
 		_skyScreen->spriteEngine();
 		_skyScreen->flip();
 		_skyScreen->showScreen(_skyScreen->giveCurrent());
-		//getchar();
 	}
 }
 
