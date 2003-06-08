2600628339853a6734c5ebfd44c849c31e3f321c
Chris Apers <chrilith@users.sourceforge.net>
2003-06-08 09:28:18 +0000

    Cleanup, add qsort function
    svn-id: r8393

--- backends/PalmOS/Src/missing/stdlib.h
+++ backends/PalmOS/Src/missing/stdlib.h
@@ -24,30 +24,18 @@
 #define STDLIB_H
 
 #include <PalmOS.h>
+#include "MemGlue.h"
 #include "mathlib.h"
-
-//#define memNewChunkFlagNonMovable	0x0200 
-#define memNewChunkFlagAllowLarge	0x1000  // this is not in the sdk *g* 
-#define memHeapFlagReadOnly			0x0001
-
-#define	NO_HEAP_FOUND				-1
-
-SysAppInfoPtr SysGetAppInfo(SysAppInfoPtr *uiAppPP, SysAppInfoPtr *actionCodeAppPP) 
-							SYS_TRAP(sysTrapSysGetAppInfo); 
-
 
 #define atoi				StrAToI
 #define atol				StrAToI
 #define abs(a)				((a) < 0 ? -(a) : (a))
 //#define abs					fabs
-#define	malloc(a)			calloc(a,1)
+#define	malloc(a)			MemGluePtrNew(a)
 //#define free				MemPtrFree
 #define strtol(a,b,c)		StrAToI(a)
-#define qsort(a,b,c,d)
+#define qsort(a,b,c,d)		SysQSort((a), (b), (c), (CmpFuncPtr)(&d), 0);
 #define rand()				SysRandom(0)
-
-void MemExtInit();
-void MemExtCleanup();
 
 MemPtr realloc(MemPtr oldP, UInt32 size);
 MemPtr calloc(UInt32 nelem, UInt32 elsize);
