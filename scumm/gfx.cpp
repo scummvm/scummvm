33791b7929ad1db53e09498cff0fa5026c6f55fc
Jonathan Gray <khalek@users.sourceforge.net>
2003-06-17 07:14:39 +0000

    ega palette changes, correct colour used for sentence line and use amiga colour for colour normally used for skin colours, sarien does something similiar...
    svn-id: r8524

--- scumm/gfx.cpp
+++ scumm/gfx.cpp
@@ -2519,14 +2519,16 @@ void Scumm::setupEGAPalette() {
 	setPalColor( 2,   0, 168,   0);
 	setPalColor( 3,   0, 168, 168);
 	setPalColor( 4, 168,   0,   0);
-	setPalColor( 5, 168,   0, 168);
+//	setPalColor( 5, 168,   0, 168); // prev value
+	setPalColor( 5, 252,  84, 252);
 	setPalColor( 6, 168,  84,   0);
 	setPalColor( 7, 168, 168, 168);
 	setPalColor( 8,  84,  84,  84);
 	setPalColor( 9,  84,  84, 252);
 	setPalColor(10,   0, 252,   0);
 	setPalColor(11,   0, 252, 252);
-	setPalColor(12, 252,  84,  84);
+//	setPalColor(12, 252,  84,  84); // PC
+	setPalColor(12, 252, 144, 144); // Amiga value gives nicer skin colour
 	setPalColor(13, 252,   0, 252);
 	setPalColor(14, 252, 252,   0);
 	setPalColor(15, 252, 252, 252);
