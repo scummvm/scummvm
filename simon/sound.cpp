ece6f28fd7a0678af09984239e39ced2fa265875
Max Horn <max@quendi.de>
2003-06-21 15:32:25 +0000

    fix (?) for an invalid read report in valgrin
    svn-id: r8578

--- simon/sound.cpp
+++ simon/sound.cpp
@@ -214,7 +214,7 @@ int MP3Sound::playSound(uint sound, PlayingSoundHandle *handle, byte flags)
 
 	uint32 size = _offsets[sound+1] - _offsets[sound];
 
-	byte *buffer = (byte *)malloc(size);
+	byte *buffer = (byte *)malloc(size + 1);
 	_file->read(buffer, size);
 
 	return _mixer->playMP3(handle, buffer, size, flags);
