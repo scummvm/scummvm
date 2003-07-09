6d84c679069ea1d56f3eb84406bd5a51d05556f9
Robert Göffringmann <lavosspawn@users.sourceforge.net>
2003-07-09 00:35:41 +0000

    /me slaps himself
    svn-id: r8875

--- sky/control.cpp
+++ sky/control.cpp
@@ -1325,7 +1325,7 @@ uint16 SkyControl::parseSaveData(uint8 *srcBuf) {
 	if (saveRev >= 3) {
 		LODSD(srcPos, gameVersion);
 		if (gameVersion != SkyState::_systemVars.gameVersion) {
-			if (SkyState::isCDVersion() && (gameVersion >= 365)) { // cd versions are compatible
+			if ((!SkyState::isCDVersion()) || (gameVersion < 365)) { // cd versions are compatible
 				printf("This savegame was created by Beneath a Steel Sky v0.0%03d\n", gameVersion);
 				printf("It cannot be loaded by this version (v0.0%3d)\n", SkyState::_systemVars.gameVersion);
 				return RESTORE_FAILED;
