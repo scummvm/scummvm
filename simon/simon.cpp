2980a78dffc968188de2633b2bd01c97b2fdd236
James Brown <ender@users.sourceforge.net>
2003-07-14 08:48:38 +0000

    We ARE in a cvs freeze, PLEASE at least test if your code compiles before commiting... Jamieson, this isn't even a valid line of C! @toad Jamieson630
    svn-id: r9004

--- simon/simon.cpp
+++ simon/simon.cpp
@@ -434,7 +434,8 @@ SimonEngine::SimonEngine(GameDetector *detector, OSystem *syst)
 	// Setup midi driver
 	if (!driver)
 		driver = MidiDriver_ADLIB_create();
-	midi.mapMT32toGM (!(_game & GF_SIMON2) && !detector->_native_mt32)
+
+//	midi.mapMT32toGM (!(_game & GF_SIMON2) && !detector->_native_mt32)
 	midi.set_driver(driver);
 	int ret = midi.open();
 	if (ret)
