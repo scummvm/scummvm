660ca0f52657a633f7852019b6242cfabe319c70
Max Horn <max@quendi.de>
2003-07-22 16:30:00 +0000

    fixed button hotkeys in dialogs with a focused widget (e.g. the F5 save/load dialog)
    svn-id: r9127

--- gui/dialog.cpp
+++ gui/dialog.cpp
@@ -181,20 +181,20 @@ void Dialog::handleKeyDown(uint16 ascii, int keycode, int modifiers) {
 	if (_focusedWidget) {
 		if (_focusedWidget->handleKeyDown(ascii, keycode, modifiers))
 			return;
-	} else {
-		// Hotkey handling
-		Widget *w = _firstWidget;
-		ascii = toupper(ascii);
-		while (w) {
-			if (w->_type == kButtonWidget && ascii == toupper(((ButtonWidget *)w)->_hotkey)) {
-				// We first send a mouseDown then a mouseUp.
-				// FIXME: insert a brief delay between both.
-				w->handleMouseDown(0, 0, 1, 1);
-				w->handleMouseUp(0, 0, 1, 1);
-				return;
-			}
-			w = w->_next;
+	}
+
+	// Hotkey handling
+	Widget *w = _firstWidget;
+	ascii = toupper(ascii);
+	while (w) {
+		if (w->_type == kButtonWidget && ascii == toupper(((ButtonWidget *)w)->_hotkey)) {
+			// The hotkey for widget w was pressed. We fake a mouse click into the
+			// button by invoking the appropriate methods.
+			w->handleMouseDown(0, 0, 1, 1);
+			w->handleMouseUp(0, 0, 1, 1);
+			return;
 		}
+		w = w->_next;
 	}
 
 	// ESC closes all dialogs by default
