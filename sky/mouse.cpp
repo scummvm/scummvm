7e3d4b34f2e338f2f3fb8019a820d16547130179
Robert Göffringmann <lavosspawn@users.sourceforge.net>
2003-08-04 01:16:11 +0000

    fixed dialog skipping
    svn-id: r9447

--- sky/mouse.cpp
+++ sky/mouse.cpp
@@ -313,3 +313,12 @@ void SkyMouse::fnOpenCloseHand(bool open) {
     spriteMouse(0, 5, 5);
 }
 
+bool SkyMouse::wasClicked(void) {
+
+	if (_logicClick) {
+		_logicClick = false;
+		return true;
+	} else
+		return false;
+}
+
