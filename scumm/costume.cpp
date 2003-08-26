b91c182571c3f699e1b387ba94eaa1a16e55c60a
Travis Howell <kirben@scummvm.org>
2003-08-26 16:09:09 +0000

    Revert v1 zak face costume color nack to 8, since palette is fixed.
    svn-id: r9870

--- scumm/costume.cpp
+++ scumm/costume.cpp
@@ -369,7 +369,7 @@ void CostumeRenderer::procC64(int actor) {
 		palette[1] = v1_mm_actor_palatte_1[actor];
 		palette[2] = v1_mm_actor_palatte_2[actor];
 	} else {
-		palette[1] = 10;
+		palette[1] = 8;
 		palette[2] = _palette[actor];
 	}
 	mask = v1.mask_ptr;
