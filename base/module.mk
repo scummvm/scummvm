dbb20eaee5c7837d10e02661b83f5360ea7db395
James Brown <ender@users.sourceforge.net>
2003-09-18 12:07:56 +0000

    This'll also break Makefile.noconf
    svn-id: r10291

--- base/module.mk
+++ base/module.mk
@@ -11,7 +11,9 @@ MODULE_DIRS += \
 
 # Some of the base files depend on the values of the DISABLE_* flags defined
 # in config.mak. Hence we add an explicit make dependency on that file.
-# Temporarily disabled since it causes troubles for MinGW
+#
+# Temporarily disabled since it causes troubles for MinGW - and
+# will break Makefile.noconf
 #base/gameDetector.o base/plugins.o: config.mak
 
 # Include common rules 
