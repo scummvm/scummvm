f7d50f7c68f4c48edacf8d1c6fd60bd3e17f5a6d
Max Horn <max@quendi.de>
2003-09-18 19:56:38 +0000

    unify legal headers a bit here
    svn-id: r10305

--- scumm/player_v3a.h
+++ scumm/player_v3a.h
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Header: /cvsroot/scummvm/scummvm/scumm/player_v3a.h
+ * $Header$
  *
  */
 
