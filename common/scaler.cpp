b2b9a7cb07a24124e4ea3877a72d44cff7bcbff7
Max Horn <max@quendi.de>
2003-09-27 23:11:26 +0000

    cheak doxygen comments
    svn-id: r10448

--- common/scaler.cpp
+++ common/scaler.cpp
@@ -666,14 +666,14 @@ void DotMatrix(const uint8 *srcPtr, uint32 srcPitch, uint8 *dstPtr, uint32 dstPi
 
 static int   RGBtoYUV[65536];
 
-// Interpolate two 16 bit pixels with the given weights.
+/** Interpolate two 16 bit pixels with the weights specified in the template parameters. */
 template<int w1, int w2>
 static inline uint16 interpolate16_2(uint16 p1, uint16 p2) {
 	return ((((p1 & redblueMask) * w1 + (p2 & redblueMask) * w2) / (w1 + w2)) & redblueMask) |
 	       ((((p1 & greenMask) * w1 + (p2 & greenMask) * w2) / (w1 + w2)) & greenMask);
 }
 
-// Interpolate three 16 bit pixels with the given weights.
+/** Interpolate three 16 bit pixels with the weights specified in the template parameters. */
 template<int w1, int w2, int w3>
 static inline uint16 interpolate16_3(uint16 p1, uint16 p2, uint16 p3) {
 	return ((((p1 & redblueMask) * w1 + (p2 & redblueMask) * w2 + (p3 & redblueMask) * w3) / (w1 + w2 + w3)) & redblueMask) |
