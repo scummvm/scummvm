d9557d9dbbfe653c9ced47b17e45a427142184bb
Joost Peters <joostp@scummvm.org>
2003-09-29 22:30:02 +0000

    small cleanup
    svn-id: r10492

--- queen/logic.cpp
+++ queen/logic.cpp
@@ -21,8 +21,7 @@
 
 #include "queen/logic.h"
 
-QueenLogic::QueenLogic(QueenResource *resource) {
-	_resource = resource;  
+QueenLogic::QueenLogic(QueenResource *resource) : _resource(resource) {
 	_jas = _resource->loadFile("QUEEN.JAS", 20);
 	
 	initialise();
