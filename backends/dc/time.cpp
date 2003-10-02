f26d3d1784756fccaaf306f6b13a28cb709ad5bc
Max Horn <max@quendi.de>
2003-10-02 17:28:02 +0000

    updated Marcus' copyright
    svn-id: r10543

--- backends/dc/time.cpp
+++ backends/dc/time.cpp
@@ -1,6 +1,6 @@
 /* ScummVM - Scumm Interpreter
  * Dreamcast port
- * Copyright (C) 2002  Marcus Comstedt
+ * Copyright (C) 2002-2003  Marcus Comstedt
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
