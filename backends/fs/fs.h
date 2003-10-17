b86ae6641bea6d4d7a4e3f8e90cf976b5a63c7b1
Max Horn <max@quendi.de>
2003-10-17 10:22:40 +0000

    make FSList inherit 'public' from List (so that we can use List::Iterator)
    svn-id: r10849

--- backends/fs/fs.h
+++ backends/fs/fs.h
@@ -146,7 +146,7 @@ public:
 /*
  * A sorted list of multiple file system nodes. E.g. the contents of a given directory.
  */
-class FSList : Common::List<FilesystemNode *> {
+class FSList : public Common::List<FilesystemNode *> {
 public:
 	~FSList() {
 		for (int i = 0; i < _size; i++)
