f4ad708ef8ee5725f88c983636915033e2b285f6
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2003-10-28 18:39:24 +0000

    fixed compilation

--- main.cpp
+++ main.cpp
@@ -130,7 +130,6 @@ int main(int argc, char *argv[]) {
   Engine::instance()->font = glGenLists(256);
   #ifdef WIN32
   {
-	#warning entering w32 code
 	HDC   hDC;
 	HFONT font;
 	SDL_SysWMinfo wmi;
