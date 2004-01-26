5802ceb9754b7e779b6a5bfad6bde4ac68b0d78b
Nicolas Bacca <arisme@users.sourceforge.net>
2004-01-26 08:15:50 +0000

    New resources files
    svn-id: r12616

--- backends/wince/resource.h
+++ backends/wince/resource.h
@@ -1,12 +1,21 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
+// Microsoft eMbedded Visual C++ generated include file.
 // Used by PocketSCUMM.rc
 //
-#define IDI_ICON1                       101
 #define IDI_POCKETSCUMM                 101
 #define IDR_SMARTFON_MENUBAR_GAMES      102
 #define IDR_SMARTFON_MENUBAR_OPTIONS    103
 #define IDB_LOGO                        106
+#define IMAGE_PANEL                     109
+#define PANEL_GENERIC                   109
+#define IMAGE_KEYBOARD                  110
+#define PANEL_KEYBOARD                  110
+#define ITEM_SKIP                       114
+#define ITEM_OPTIONS                    115
+#define ITEM_SOUND_ON                   118
+#define ITEM_SOUND_OFF                  119
+#define ITEM_VIEW_PORTRAIT              120
+#define ITEM_VIEW_LANDSCAPE             122
 #define IDS_CAP_MENUITEM40002           40003
 #define ID_MENU_GAMES                   40006
 #define IDM_SMARTFON_SCAN               40007
@@ -21,7 +30,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_RESOURCE_VALUE        123
 #define _APS_NEXT_COMMAND_VALUE         40022
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
