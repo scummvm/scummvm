0e24cabec85c797d2e2d2edf7d3102457bac1b54
James Brown <ender@users.sourceforge.net>
2004-02-13 10:26:40 +0000

    Remove debug printf()
    svn-id: r12843

--- scumm/debugger.cpp
+++ scumm/debugger.cpp
@@ -70,7 +70,7 @@ ScummDebugger::ScummDebugger(ScummEngine *s)
 	DVar_Register("scumm_room", &_vm->_currentRoom, DVAR_INT, 0);
 	DVar_Register("scumm_roomresource", &_vm->_roomResource, DVAR_INT, 0);
 	DVar_Register("scumm_vars", &_vm->_scummVars, DVAR_INTARRAY, _vm->_numVariables);
-	printf("Registered debugger\n");
+
 	DVar_Register("scumm_gamename", &_vm->_targetName, DVAR_STRING, 0);
 	DVar_Register("scumm_exename", &_vm->_gameName, DVAR_STRING, 0);
 	DVar_Register("scumm_gameid", &_vm->_gameId, DVAR_INT, 0);
