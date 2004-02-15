955a96ada55f6ba1a5974664eae18591e3f40748
Travis Howell <kirben@scummvm.org>
2004-02-15 03:47:26 +0000

    Add fix for akos colorKey issue in fbear
    svn-id: r12898

--- scumm/akos.cpp
+++ scumm/akos.cpp
@@ -948,7 +948,12 @@ byte AkosRenderer::codec16(int xmoveCur, int ymoveCur) {
 	Common::Rect clip;
 	int32 maxw, maxh;
 	int32 skip_x, skip_y, cur_x, cur_y;
-	const byte transparency = (_vm->_features & GF_HUMONGOUS) ? 0 : 255;
+	byte transparency;
+	//FIXME There should be better method to determine transparency in HE games
+	if (_vm->_features & GF_HUMONGOUS) 
+		transparency = (_vm->_gameId == GID_FBEAR) ? 5 : 0;
+	else
+		transparency = 255;
 
 	if (_actorHitMode) {
 		warning("codec16: _actorHitMode not yet implemented");
