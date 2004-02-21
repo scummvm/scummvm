1831d71ed357bc582625365f7e3164550669a2e3
Travis Howell <kirben@scummvm.org>
2004-02-21 15:12:46 +0000

    Ooops, didn't meant to revert
    svn-id: r12968

--- scumm/script.cpp
+++ scumm/script.cpp
@@ -1109,7 +1109,7 @@ void ScummEngine::beginOverride() {
 	fetchScriptByte();
 	fetchScriptWord();
 	
-	// FIXME: why is this here? it doesn't seem to belong here?
+	// This is based on disassembly
 	VAR(VAR_OVERRIDE) = 0;
 }
 
