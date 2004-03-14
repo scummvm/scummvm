2c16ced3edb771a4d2894cae603e8857532a226a
Paweł Kołodziejski <aquadran@scummvm.org>
2004-03-14 13:45:12 +0000

    changed versions for release
    svn-id: r13273

--- base/main.cpp
+++ base/main.cpp
@@ -83,9 +83,9 @@
  * to setup, but I have no idea if/how one can also change files which are not
  * currently being commit'ed.
  */
-const char *gScummVMVersion = "0.6.0pre";
+const char *gScummVMVersion = "0.6.0";
 const char *gScummVMBuildDate = __DATE__ " " __TIME__;
-const char *gScummVMFullVersion = "ScummVM 0.6.0pre (" __DATE__ " " __TIME__ ")";
+const char *gScummVMFullVersion = "ScummVM 0.6.0 (" __DATE__ " " __TIME__ ")";
 const char *gScummVMFeatures = ""
 #ifdef USE_VORBIS
 	"Vorbis "
