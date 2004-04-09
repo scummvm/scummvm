5f89f5b2b756891d5f7a032b84a6bd89333056aa
Paweł Kołodziejski <aquadran@scummvm.org>
2004-04-09 04:20:17 +0000

    correction for stoping sequence
    svn-id: r13511

--- scumm/imuse_digi/dimuse_music.cpp
+++ scumm/imuse_digi/dimuse_music.cpp
@@ -31,18 +31,16 @@ namespace Scumm {
 #define COMI_SEQ_OFFSET (COMI_STATE_OFFSET + 94)
 
 void IMuseDigital::refreshScripts() {
+	Common::StackLock lock(_mutex, "IMuseDigital::refreshScripts()");
 	bool found = false;
-	{
-		Common::StackLock lock(_mutex, "IMuseDigital::refreshScripts()");
-		for (int l = 0; l < MAX_DIGITAL_TRACKS; l++) {
-			if ((_track[l].used) && (_track[l].volGroupId == IMUSE_VOLGRP_MUSIC) && (!_track[l].volFadeUsed)) {
-				found = true;
-			}
+	for (int l = 0; l < MAX_DIGITAL_TRACKS; l++) {
+		if ((_track[l].used) && (_track[l].volGroupId == IMUSE_VOLGRP_MUSIC) && (!_track[l].volFadeUsed)) {
+			found = true;
 		}
 	}
 
 	if ((!found) && (_curMusicSeq != 0)) {
-		parseScriptCmds(0x1001, 0, 0, 0, 0, 0, 0, 0);
+		parseScriptCmds(0x2000, 0, 0, 0, 0, 0, 0, 0);
 	}
 }
 
