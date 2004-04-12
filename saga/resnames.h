6055328d27f73f27c016eac414531b9c1081a41a
Eugene Sandulenko <sev@scummvm.org>
2004-04-12 22:26:48 +0000

    oops. Forgot the file
    svn-id: r13567

---
+++ saga/resnames.h
@@ -0,0 +1,141 @@
+/* ScummVM - Scumm Interpreter
+ * Copyright (C) 2004 The ScummVM project
+ *
+ * The ReInherit Engine is (C)2000-2003 by Daniel Balsom.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Header$
+ *
+ */
+/*
+
+ Description:   
+ 
+    Descriptive names for game resource numbers
+
+ Notes: 
+*/
+
+#ifndef SAGA_RESOURCENAMES_H_
+#define SAGA_RESOURCENAMES_H_
+
+namespace Saga {
+
+/* Lookup tables */
+#define ITE_SCENE_LUT  1806
+#define ITE_SCRIPT_LUT 216
+
+#define IHNM_SCENE_LUT  1272
+#define IHNM_SCRIPT_LUT 0
+
+/* SCENES */
+#define ITE_DEFAULT_SCENE 32
+
+/* FONTS */
+#define RN_MEDIUM_FONT 0
+#define RN_BIG_FONT 1
+#define RN_SMALL_FONT 2
+
+/* INTERFACE IMAGES */
+#define ITE_COMMAND_PANEL 3
+#define ITE_DIALOGUE_PANEL 4
+
+#define IHNM_COMMAND_PANEL  9
+#define IHNM_DIALOGUE_PANEL 10
+
+#define ITE_SETUP_PANEL 5
+#define ITE_COMMAND_BUTTONSPRITES 7
+#define ITE_DEFAULT_PORTRAITS 125
+
+/* ITE Scene resource numbers */
+#define ITE_INTRO_ANIM_SCENE 1538
+#define ITE_CAVE_SCENE_1 1542
+#define ITE_CAVE_SCENE_2 1545
+#define ITE_CAVE_SCENE_3 1548
+#define ITE_CAVE_SCENE_4 1551
+
+#define ITE_VALLEY_SCENE 1556
+#define ITE_TREEHOUSE_SCENE 1560
+#define ITE_FAIREPATH_SCENE 1564
+#define ITE_FAIRETENT_SCENE 1567
+
+#define ITE_INTRO_ANIM_STARTFRAME 1529
+
+#define INTRO_ANIM_1 1530
+#define INTRO_ANIM_2 1531
+#define INTRO_ANIM_3 1532
+#define INTRO_ANIM_4 1533
+#define INTRO_ANIM_5 1534
+#define INTRO_ANIM_6 1535
+#define INTRO_ANIM_7 1536
+
+#define CAVE_IMG_1 1540
+#define CAVE_IMG_2 1543
+#define CAVE_IMG_3 1546
+#define CAVE_IMG_4 1549
+
+#define INTRO_IMG_1 1552
+#define INTRO_IMG_2 1557
+#define INTRO_IMG_3 1561
+#define INTRO_IMG_4 1565
+
+/* ITE_VOICES */
+#define CAVE_VOICE_0 0
+#define CAVE_VOICE_1 1
+#define CAVE_VOICE_2 2
+#define CAVE_VOICE_3 3
+#define CAVE_VOICE_4 4
+#define CAVE_VOICE_5 5
+#define CAVE_VOICE_6 6
+#define CAVE_VOICE_7 7
+#define CAVE_VOICE_8 8
+#define CAVE_VOICE_9 9
+#define CAVE_VOICE_10 10
+#define CAVE_VOICE_11 11
+#define CAVE_VOICE_12 12
+#define CAVE_VOICE_13 13
+
+/* MUSIC */
+#define MUSIC_1 9
+#define MUSIC_2 10
+#define MUSIC_3 11
+#define MUSIC_4 12
+#define MUSIC_5 13
+#define MUSIC_6 14
+#define MUSIC_7 15
+#define MUSIC_8 16
+#define MUSIC_9 17
+#define MUSIC_10 18
+#define MUSIC_11 19
+#define MUSIC_12 20
+#define MUSIC_13 21
+#define MUSIC_14 22
+#define MUSIC_15 23
+#define MUSIC_16 24
+#define MUSIC_17 25
+#define MUSIC_18 26
+#define MUSIC_19 27
+#define MUSIC_20 28
+#define MUSIC_21 29
+#define MUSIC_22 30
+#define MUSIC_23 31
+#define MUSIC_24 32
+#define MUSIC_25 33
+#define MUSIC_26 34
+
+} // End of namespace Saga
+
+#endif				/* SAGA_RESOURCENAMES_H_ */
