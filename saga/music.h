e344d4e1b06b6639b04c7648ed4acefa27fce8ab
Eugene Sandulenko <sev@scummvm.org>
2004-04-29 03:54:03 +0000

    Forgot to commit header file
    svn-id: r13671

---
+++ saga/music.h
@@ -0,0 +1,118 @@
+/* ScummVM - Scumm Interpreter
+ * Copyright (C) 2004 The ScummVM project
+ *
+ * The ReInherit Engine is (C)2000-2003 by Daniel Balsom.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Header$
+ *
+ */
+/*
+ Description:   
+ 
+    Music class
+
+ Notes: 
+*/
+
+#ifndef SAGA_MUSIC_H_
+#define SAGA_MUSIC_H_
+
+#include "rscfile_mod.h"
+#include "game_mod.h"
+#include "sound/mixer.h"
+#include "sound/mididrv.h"
+#include "sound/midiparser.h"
+
+namespace Saga {
+enum MUSIC_FLAGS {
+
+	R_MUSIC_LOOP = 0x01
+};
+
+class MusicPlayer : public MidiDriver {
+public:
+	MusicPlayer(MidiDriver *driver);
+	~MusicPlayer();
+	void setVolume(int volume);
+	int getVolume()			{ return _masterVolume; }
+	
+	void hasNativeMT32(bool b)	{ _nativeMT32 = b; }
+	void playMusic();
+	void stopMusic();
+	void setLoop(bool loop)		{ _looping = loop; }
+	
+	//MidiDriver interface implementation
+	int open();
+	void close();
+	void send(uint32 b);
+	
+	void metaEvent(byte type, byte *data, uint16 length);
+	
+	void setTimerCallback(void *timerParam, void (*timerProc)(void *)) { }
+	uint32 getBaseTempo(void)	{ return _driver ? _driver->getBaseTempo() : 0; }
+	
+	//Channel allocation functions
+	MidiChannel *allocateChannel()		{ return 0; }
+	MidiChannel *getPercussionChannel()	{ return 0; }
+
+	MidiParser *_parser;
+
+protected:
+
+	static void onTimer(void *data);
+
+	MidiChannel *_channel[16];
+	MidiDriver *_driver;
+	byte _channelVolume[16];
+	bool _nativeMT32;
+	
+	bool _isPlaying;
+	bool _looping;
+	bool _randomLoop;
+	byte _masterVolume;
+	
+	byte *_musicData;
+	uint16 *_buf;
+	uint32 _musicDataSize;
+};
+
+class Music {
+ public:
+
+	Music(MidiDriver *driver, int enabled);
+	~Music(void);
+
+	int play(ulong music_rn, uint flags);
+	int pause(void);
+	int resume(void);
+	int stop(void);
+
+ private:
+
+	SagaEngine *_vm;
+	SoundMixer *_mixer;
+
+	MusicPlayer *_player;
+
+	int _musicInitialized;
+	int _enabled;
+
+ };
+
+} // End of namespace Saga
+
+#endif				/* SAGA_MUSIC_H_ */
