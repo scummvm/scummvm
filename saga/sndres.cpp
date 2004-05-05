6c38484c1a4b0932b331e6f83d067bfa9f1cc556
Paweł Kołodziejski <aquadran@scummvm.org>
2004-05-05 06:56:01 +0000

    workaround warning - fixme
    svn-id: r13779

--- saga/sndres.cpp
+++ saga/sndres.cpp
@@ -142,7 +142,7 @@ int SndRes::loadVocSound(byte *snd_res, size_t snd_res_len, R_SOUNDBUFFER *snd_b
 	voc_hb.voc_version = readS->readUint16LE();
 	voc_hb.voc_fileid = readS->readUint16LE();
 
-	if (snd_res_len - readS->tell() < voc_hb.db_offset + R_VOC_GENBLOCK_LEN) {
+	if ((int32)(snd_res_len - readS->tell()) < (int32)(voc_hb.db_offset + R_VOC_GENBLOCK_LEN)) {
 		return R_FAILURE;
 	}
 
