abda73a874edf55c8774f00cd8a0091931c2c625
Nicolas Bacca <arisme@users.sourceforge.net>
2004-05-09 14:34:47 +0000

    Goodbye KeysBuffer, hello EventsBuffer
    svn-id: r13819

---
+++ backends/wince/CEkeys/EventsBuffer.cpp
@@ -0,0 +1,82 @@
+/* ScummVM - Scumm Interpreter
+ * Copyright (C) 2001-2004 The ScummVM project
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Header$
+ *
+ */
+
+#include "stdafx.h"
+#include "EventsBuffer.h"
+
+namespace CEKEYS {	
+
+	bool EventsBuffer::simulateKey(Key *key) {
+		SDL_Event ev = {0};
+
+		if (!key->keycode())
+			key->setKeycode(key->ascii());
+
+		if (!key->ascii())
+			key->setAscii(key->keycode());
+
+		ev.type = SDL_KEYDOWN;
+		ev.key.keysym.mod = (SDLMod)key->flags();
+		ev.key.keysym.sym = (SDLKey)key->keycode();
+		ev.key.keysym.unicode = key->keycode();
+		if (SDL_PushEvent(&ev))
+			return false;
+		ev.type = SDL_KEYUP;
+		return (SDL_PushEvent(&ev) == 0);
+	}
+
+	bool EventsBuffer::simulateMouseMove(int x, int y) {
+		SDL_Event ev = {0};
+
+		ev.type = SDL_MOUSEMOTION;
+		ev.motion.x = x;
+		ev.motion.y = y;
+		return (SDL_PushEvent(&ev) == 0);
+	}
+
+	bool EventsBuffer::simulateMouseLeftClick(int x, int y) {
+		SDL_Event ev = {0};
+
+		ev.type = SDL_MOUSEBUTTONDOWN;
+		ev.button.button = SDL_BUTTON_LEFT;
+		ev.button.x = x;
+		ev.button.y = y;
+		if (SDL_PushEvent(&ev))
+			return false;
+		ev.type = SDL_MOUSEBUTTONUP;
+		return (SDL_PushEvent(&ev) == 0);
+	}
+
+	bool EventsBuffer::simulateMouseRightClick(int x, int y) {
+		SDL_Event ev = {0};
+
+		ev.type = SDL_MOUSEBUTTONDOWN;
+		ev.button.button = SDL_BUTTON_RIGHT;
+		ev.button.x = x;
+		ev.button.y = y;
+		if (SDL_PushEvent(&ev))
+			return false;
+		ev.type = SDL_MOUSEBUTTONUP;
+		return (SDL_PushEvent(&ev) == 0);
+	}
+}
+
+
