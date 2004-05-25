b06da5a81fddcb0fbf2842f6ffe93b6e8ef066ab
Chris Apers <chrilith@users.sourceforge.net>
2004-05-25 14:58:33 +0000

    ARM resources def
    svn-id: r13889

--- backends/PalmOS/Rsc/armcode.r
+++ backends/PalmOS/Rsc/armcode.r
@@ -1,1 +1,8 @@
-read 'ARMC' (1000, "ScummVM ARM") "PNO/ARMC0001.bin";
+// ARM optimized functions
+
+read 'ARMC' (1, "ARM")	"PNO/Pa1Snd.bin";
+read 'ARMC' (2, "ARM")	"PNO/StreamSnd.bin";
+read 'ARMC' (3, "ARM")	"PNO/WideLS.bin";
+read 'ARMC' (4, "ARM")	"PNO/WidePT.bin";
+read 'ARMC' (5, "ARM")	"PNO/CopyRect.bin";
+
