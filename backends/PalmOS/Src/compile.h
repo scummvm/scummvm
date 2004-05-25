aecff97a43fed276afe25ff5e8251a8cff5513ab
Chris Apers <chrilith@users.sourceforge.net>
2004-05-25 13:10:12 +0000

    Moved to their own file
    svn-id: r13859

---
+++ backends/PalmOS/Src/compile.h
@@ -0,0 +1,17 @@
+#ifndef __COMPILE_H__
+#define __COMPILE_H__
+
+//#define DISABLE_SCUMM
+//#define DISABLE_SIMON
+#define DISABLE_SKY
+#define DISABLE_SWORD1
+#define DISABLE_SWORD2
+//#define DISABLE_QUEEN
+#define DISABLE_SAGA
+#define DISABLE_KYRA
+#define DISABLE_AWE
+
+// PalmOS
+//#define DISABLE_TAPWAVE
+
+#endif
