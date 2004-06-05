9bfd778b54510f82f2447ee133ac9f782e01482c
Travis Howell <kirben@scummvm.org>
2004-06-05 01:02:33 +0000

    Needed for DOS HE games
    svn-id: r13930

--- scumm/scumm.cpp
+++ scumm/scumm.cpp
@@ -1175,7 +1175,8 @@ void ScummEngine::initScummVars() {
 		VAR(VAR_LANGUAGE) = _language;
 	} else if (_version >= 7) {
 		VAR(VAR_V6_EMSSPACE) = 10000;
-	} else if (_heversion >= 72) {
+	} else if (_heversion == 60) {
+		// For DOS games, not sure about others.
 		VAR(VAR_SOUNDPARAM) = 1; // soundblaster for music
 		VAR(VAR_SOUNDPARAM2) = 1; // soundblaster for sfx
 	} else {
