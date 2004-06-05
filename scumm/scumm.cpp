e356a4ccc37db1371620f98ec87248dc978c5795
Jonathan Gray <khalek@users.sourceforge.net>
2004-06-05 13:41:08 +0000

    Allow dialogue options in the SegaCD version of Monkey Island to be cycled through with mousewheel. Closes RFE #953111
    svn-id: r13936

--- scumm/scumm.cpp
+++ scumm/scumm.cpp
@@ -1711,6 +1711,19 @@ void ScummEngine::parseEvents() {
 		case OSystem::EVENT_RBUTTONUP:
 			_rightBtnPressed &= ~msDown;
 			break;
+		
+		// The following two cases enable dialog choices to be
+		// scrolled through in the SegaCD version of MI
+		// as nothing else uses the wheel don't bother
+		// checking the gameid
+			
+		case OSystem::EVENT_WHEELDOWN:
+			_keyPressed = 55;
+			break;
+
+		case OSystem::EVENT_WHEELUP:
+			_keyPressed = 54;
+			break;
 	
 		case OSystem::EVENT_QUIT:
 			if (_confirmExit)
