d5618cfdeb0a8a8aba83d9e8da6dd545e1540fe6
Travis Howell <kirben@scummvm.org>
2004-06-28 09:28:58 +0000

    Delay only required when using subtitles
    svn-id: r14103

--- scumm/sound.cpp
+++ scumm/sound.cpp
@@ -450,7 +450,7 @@ void Sound::processSfxQueues() {
 			}
 		}
 
-		if (finished && _vm->_talkDelay == 0) {
+		if ((!ConfMan.getBool("subtitles") && finished) || (finished && _vm->_talkDelay == 0)) {
 			_vm->stopTalk();
 		}
 	}
