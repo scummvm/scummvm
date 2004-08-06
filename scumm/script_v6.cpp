a7b610b25b5035962e5ee27610e49c1a5d2e5172
Max Horn <max@quendi.de>
2004-08-06 15:55:12 +0000

    WORKAROUND bug #903223 (COMI: Guybrush reads church clock incorrectly (German))
    svn-id: r14487

--- scumm/script_v6.cpp
+++ scumm/script_v6.cpp
@@ -815,6 +815,25 @@ void ScummEngine_v6::o6_startScript() {
 	getStackList(args, ARRAYSIZE(args));
 	script = pop();
 	flags = pop();
+	
+	// WORKAROUND bug #903223: In Puerto Pollo, if you have Guybrush examine
+	// the church clock, he'll read out the current time. Nice touch, only that
+	// it sounds crap in the german version (and maybe others, too). It seems
+	// the original engine of the german version played just a simple fixed
+	// text in this spot, for the above reason. Since the data files are
+	// unchanged, it must have been an engine hack job. No idea how they did
+	// it exactly, but this here is how we do it :-)
+	if (_gameId == GID_CMI && script == 204 &&
+		_currentRoom == 15 && vm.slot[_currentScript].number == 421 &&
+		_language == Common::DE_DEU) {
+
+		_actorToPrintStrFor = 1;
+		_string[0].loadDefault();
+		actorTalk((const byte *)"/VDSO325/Whoa! Look at the time. Gotta scoot.");
+
+		return;
+	}
+	
 	runScript(script, (flags & 1) != 0, (flags & 2) != 0, args);
 }
 
