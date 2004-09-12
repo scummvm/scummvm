230ea60d674ff2fb50441f47e509f96747edaffb
Chris Apers <chrilith@users.sourceforge.net>
2004-09-12 15:16:04 +0000

    New ARM code
    svn-id: r15074

--- backends/PalmOS/Rsc/armcode.r
+++ backends/PalmOS/Rsc/armcode.r
@@ -1,8 +1,9 @@
 // ARM optimized functions
 
-read 'ARMC' (1, "ARM")	"PNO/Pa1Snd.bin";
-read 'ARMC' (2, "ARM")	"PNO/StreamSnd.bin";
-read 'ARMC' (3, "ARM")	"PNO/WideLS.bin";
-read 'ARMC' (4, "ARM")	"PNO/WidePT.bin";
-read 'ARMC' (5, "ARM")	"PNO/CopyRect.bin";
+read 'ARMC' (1, "ARM")	"PNO/widelandscape.bin";
+read 'ARMC' (2, "ARM")	"PNO/wideportrait.bin";
+read 'ARMC' (3, "ARM")	"PNO/copyrectangle.bin";
+
+read 'ARMC' (4, "ARM")	"PNO/proc3.bin";
+read 'ARMC' (5, "ARM")	"PNO/drawstrip.bin";
 
