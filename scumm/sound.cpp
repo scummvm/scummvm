d7085acea18e9134da230e8223f529f7978fb3f9
Travis Howell <kirben@scummvm.org>
2004-09-21 01:07:29 +0000

    Switch order to prevent warning
    svn-id: r15213

--- scumm/sound.cpp
+++ scumm/sound.cpp
@@ -773,14 +773,14 @@ int Sound::isSoundRunning(int sound) const {
 		}
 	}
 
+	if (_vm->_mixer->isSoundIDActive(sound))
+		return 1;
+
 	if (isSoundInQueue(sound))
 		return 1;
 
 	if (!_vm->isResourceLoaded(rtSound, sound))
 		return 0;
-
-	if (_vm->_mixer->isSoundIDActive(sound))
-		return 1;
 
 	if (_vm->_musicEngine)
 		return _vm->_musicEngine->getSoundStatus(sound);
