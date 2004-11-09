e92da967606bdae79911ba32ca6fc56faf297586
Chris Apers <chrilith@users.sourceforge.net>
2004-11-09 11:56:45 +0000

    Updated prefix file
    svn-id: r15771

--- backends/PalmOS/Src/compile.h
+++ backends/PalmOS/Src/compile.h
@@ -1,17 +1,23 @@
 #ifndef __COMPILE_H__
 #define __COMPILE_H__
 
-//#define DISABLE_SCUMM
-//#define DISABLE_SIMON
+// Engines
+#define DISABLE_SCUMM
+#define DISABLE_SIMON
 #define DISABLE_SKY
-#define DISABLE_SWORD1
+//#define DISABLE_SWORD1
 #define DISABLE_SWORD2
-//#define DISABLE_QUEEN
+#define DISABLE_QUEEN
 #define DISABLE_SAGA
 #define DISABLE_KYRA
 #define DISABLE_AWE
 
 // PalmOS
+//#define DISABLE_LIGHTSPEED
 //#define DISABLE_TAPWAVE
+//#define REDUCE_MEMORY_USAGE
+
+//#define _DEBUG
+//#define _DEBUG_STDIO
 
 #endif
