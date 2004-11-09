5fc9d34542f9df18bea1a75ad74f488808128ac7
Chris Apers <chrilith@users.sourceforge.net>
2004-11-09 11:35:17 +0000

    Moved to init_arm.h
    svn-id: r15763

--- backends/PalmOS/Src/arm/native.h
+++ backends/PalmOS/Src/arm/native.h
@@ -142,13 +142,4 @@ typedef struct {
 	byte masked;
 } BlitType;
 
-// calls
-MemPtr	_PceInit(DmResID resID);
-UInt32	_PceCall(void *armP, void *userDataP);
-void	_PceFree(void *armP);
-
-MemPtr	_PnoInit(DmResID resID, PnoDescriptor *pnoP);
-UInt32	_PnoCall(PnoDescriptor *pnoP, void *userDataP);
-void	_PnoFree(PnoDescriptor *pnoP, MemPtr armP);
-
 #endif
