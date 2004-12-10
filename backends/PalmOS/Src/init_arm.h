7a194532209ce00480bcc11d09fa231e3d322bd3
Chris Apers <chrilith@users.sourceforge.net>
2004-12-10 15:29:34 +0000

    New ARM call
    svn-id: r16020

--- backends/PalmOS/Src/init_arm.h
+++ backends/PalmOS/Src/init_arm.h
@@ -4,13 +4,4 @@
 void ARMInit();
 void ARMRelease();
 
-// calls
-MemPtr	_PceInit(DmResID resID);
-UInt32	_PceCall(void *armP, void *userDataP);
-void	_PceFree(void *armP);
-
-MemPtr	_PnoInit(DmResID resID, PnoDescriptor *pnoP);
-UInt32	_PnoCall(PnoDescriptor *pnoP, void *userDataP);
-void	_PnoFree(PnoDescriptor *pnoP, MemPtr armP);
-
 #endif
