08cdd912cebbca4f07e869ed8cfd544bc15f0d08
Chris Apers <chrilith@users.sourceforge.net>
2004-12-20 19:24:19 +0000

    Updated resource files
    svn-id: r16190

--- backends/PalmOS/Rsc/armcode.r
+++ backends/PalmOS/Rsc/armcode.r
@@ -1,11 +1,7 @@
 // ARM optimized functions
 
-read 'ARMC' (1, "ARM")	"PNO/widelandscape.bin";
-read 'ARMC' (2, "ARM")	"PNO/wideportrait.bin";
-read 'ARMC' (3, "ARM")	"PNO/copyrectangle.bin";
+read 'ARMC' (100, "ARM")	"PNO/common.arm";
 
-read 'ARMC' (4, "ARM")	"PNO/proc3.bin";
-read 'ARMC' (5, "ARM")	"PNO/drawstrip.bin";
-
-read 'ARMC' (6, "ARM")	"PNO/blit.bin";
-
+// Comment this line if there is no ARM optimization
+// for the compiled engine
+//read 'ARMC' (101, "ARM")	"PNO/engine.arm";
