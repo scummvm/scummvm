1184e19657ce740a22cf85c242d249580679f4ce
Chris Apers <chrilith@users.sourceforge.net>
2004-12-20 16:59:43 +0000

    Use a draw function instead of a preinit one
    svn-id: r16166

--- backends/PalmOS/Src/forms/formtabs.h
+++ backends/PalmOS/Src/forms/formtabs.h
@@ -1,10 +1,13 @@
 #ifndef __FORMTABS_H__
 #define __FORMTABS_H__
 
+typedef void (TabProc)();
+
 typedef struct {
 	FormPtr srcP;
 	UInt16 first;
 	UInt16 last;
+	TabProc *drawFunc;
 } TabDataType, *TabDataPtr;
 
 typedef struct {
@@ -13,11 +16,9 @@ typedef struct {
 	TabDataPtr tabs;
 } TabType, *TabPtr;
 
-typedef void (TabProc)(FormType *);
-
 TabType	*TabNewTabs		(UInt16 cnt);
 void 	 TabDeleteTabs	(TabType *tabP);
-Err 	 TabAddContent	(FormType **frmP, TabType *tabP, const Char *nameP, UInt16 rscID, TabProc *preInit = 0);
+Err 	 TabAddContent	(FormType **frmP, TabType *tabP, const Char *nameP, UInt16 rscID, TabProc *drawFunc = 0);
 void 	 TabSetActive	(FormType *frmP, TabType *tabP, UInt16 num);
 
 #endif
