f5381592efc043f70426a29efe7e5275eb26fda5
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2005-01-01 15:23:13 +0000

    removed never used lua opcodes and add new monkey4 stubs opcodes

--- lua.cpp
+++ lua.cpp
@@ -1357,13 +1357,6 @@ static void BlastText() {
 }
 
 #define STUB_FUNC(name) static void name() { stubWarning(#name); }
-STUB_FUNC(EnginePause)
-STUB_FUNC(EngineResume)
-STUB_FUNC(GetActorScale)
-STUB_FUNC(GetActorTurnRate)
-STUB_FUNC(SetActorOffsetYaw)
-STUB_FUNC(PutActorAtOrigin)
-STUB_FUNC(GetClippedPos)
 STUB_FUNC(SetActorInvClipNode)
 STUB_FUNC(NukeResources)
 STUB_FUNC(UnShrinkBoxes)
@@ -1477,67 +1470,17 @@ STUB_FUNC(DrawRectangle)
 STUB_FUNC(DrawPolygon)
 STUB_FUNC(DrawLine)
 STUB_FUNC(WalkActorToAvoiding)
-STUB_FUNC(WalkActorToInterest)
-STUB_FUNC(IsActorLooking)
-STUB_FUNC(CanActorSee)
 STUB_FUNC(GetActorChores)
-STUB_FUNC(GetActorLoopingChores)
-STUB_FUNC(ScreenToWorld)
 STUB_FUNC(Exit)
-STUB_FUNC(UnLockFont)
-STUB_FUNC(LockSound)
-STUB_FUNC(UnlockSound)
-STUB_FUNC(LockCostume)
-STUB_FUNC(UnlockCostume)
-STUB_FUNC(PrintMessage)
 STUB_FUNC(PrintError)
-STUB_FUNC(QueryDialog)
-STUB_FUNC(GetSectorVertices)
-STUB_FUNC(IsSectorActive)
-STUB_FUNC(SpewShutdown)
-STUB_FUNC(GetPhysicalBytes)
-STUB_FUNC(SetMemoryUsage)
 STUB_FUNC(SetCameraPosition)
-STUB_FUNC(GetCameraInterest)
 STUB_FUNC(GetCameraFOV)
 STUB_FUNC(SetCameraFOV)
 STUB_FUNC(GetCameraRoll)
-STUB_FUNC(SnapToPlane)
-STUB_FUNC(FindLight)
-STUB_FUNC(IsLightOn)
-STUB_FUNC(SetLightType)
-STUB_FUNC(GetLightType)
-STUB_FUNC(GetLightPosition)
-STUB_FUNC(SetLightRotation)
-STUB_FUNC(GetLightRotation)
-STUB_FUNC(SetLightAngles)
-STUB_FUNC(GetLightAngles)
-STUB_FUNC(GetLightIntensity)
-STUB_FUNC(PointLightAt)
 STUB_FUNC(LoadBundle)
-STUB_FUNC(UnloadBundle)
 STUB_FUNC(ActorShadow)
-STUB_FUNC(ActorDistToShadowPlane)
-STUB_FUNC(MarkDirtyRect)
-STUB_FUNC(CacheRect)
-STUB_FUNC(RestoreCacheRect)
-STUB_FUNC(ResReport)
-STUB_FUNC(IrisComplete)
-STUB_FUNC(IrisClear)
-STUB_FUNC(SaveScreen)
-STUB_FUNC(FindFileOnAnyCD)
-STUB_FUNC(Test)
 STUB_FUNC(ActorPuckOrient)
-STUB_FUNC(ActorVoiceIs3D)
-STUB_FUNC(DumpMemoryLog)
-STUB_FUNC(BuildCache)
-STUB_FUNC(RestoreCache)
-STUB_FUNC(SetWalkSystemFadeTime)
 STUB_FUNC(GetMemoryUsage)
-STUB_FUNC(Concatfallback)
-STUB_FUNC(TypeOverride)
-STUB_FUNC(DfltCamera)
-STUB_FUNC(DfltControl)
 STUB_FUNC(GetFontDimensions)
 STUB_FUNC(PurgeText)
 STUB_FUNC(GetVideoDevices)
@@ -1546,9 +1489,100 @@ STUB_FUNC(SetHardwareState)
 STUB_FUNC(Enumerate3DDevices)
 STUB_FUNC(EnumerateVideoDevices)
 
-static void LuaGetTickCount() {
-	stubWarning("GetTickCount");
-}
+	// new Monkey 4 opcodes:
+
+STUB_FUNC(SetActiveCD)
+STUB_FUNC(GetActiveCD)
+STUB_FUNC(AreWeInternational)
+STUB_FUNC(MakeScreenTextures)
+STUB_FUNC(ThumbnailFromFile)
+STUB_FUNC(ClearSpecialtyTexture)
+STUB_FUNC(UnloadActor)
+STUB_FUNC(PutActorInOverworld)
+STUB_FUNC(RemoveActorFromOverworld)
+STUB_FUNC(ClearOverworld)
+STUB_FUNC(ToggleOverworld)
+STUB_FUNC(ActorStopMoving)
+STUB_FUNC(SetActorFOV)
+STUB_FUNC(SetActorLighting)
+STUB_FUNC(SetActorHeadLimits)
+STUB_FUNC(ActorActivateShadow)
+STUB_FUNC(EnableActorPuck)
+STUB_FUNC(SetActorGlobalAlpha)
+STUB_FUNC(SetActorLocalAlpha)
+STUB_FUNC(SetActorSortOrder)
+STUB_FUNC(GetActorSortOrder)
+STUB_FUNC(AttachActor)
+STUB_FUNC(DetachActor)
+STUB_FUNC(IsChoreValid)
+STUB_FUNC(IsChorePlaying)
+STUB_FUNC(IsChoreLooping)
+STUB_FUNC(StopActorChores)
+STUB_FUNC(PlayChore)
+STUB_FUNC(StopChore)
+STUB_FUNC(PauseChore)
+STUB_FUNC(AdvanceChore)
+STUB_FUNC(CompleteChore)
+STUB_FUNC(LockChore)
+STUB_FUNC(UnlockChore)
+STUB_FUNC(LockChoreSet)
+STUB_FUNC(UnlockChoreSet)
+STUB_FUNC(LockBackground)
+STUB_FUNC(UnLockBackground)
+STUB_FUNC(EscapeMovie)
+STUB_FUNC(StopAllSounds)
+STUB_FUNC(LoadSound)
+STUB_FUNC(FreeSound)
+STUB_FUNC(PlayLoadedSound)
+STUB_FUNC(SetGroupVolume)
+STUB_FUNC(GetSoundVolume)
+STUB_FUNC(SetSoundVolume)
+STUB_FUNC(EnableAudioGroup)
+STUB_FUNC(EnableVoiceFX)
+STUB_FUNC(PlaySoundFrom)
+STUB_FUNC(PlayLoadedSoundFrom)
+STUB_FUNC(SetReverb)
+STUB_FUNC(UpdateSoundPosition)
+STUB_FUNC(ImSelectSet)
+STUB_FUNC(ImStateHasLooped)
+STUB_FUNC(ImStateHasEnded)
+STUB_FUNC(ImPushState)
+STUB_FUNC(ImPopState)
+STUB_FUNC(ImFlushStack)
+STUB_FUNC(ImGetMillisecondPosition)
+STUB_FUNC(GetSectorName)
+STUB_FUNC(GetCameraYaw)
+STUB_FUNC(YawCamera)
+STUB_FUNC(GetCameraPitch)
+STUB_FUNC(PitchCamera)
+STUB_FUNC(RollCamera)
+STUB_FUNC(UndimAll)
+STUB_FUNC(UndimRegion)
+STUB_FUNC(GetCPUSpeed)
+STUB_FUNC(NewLayer)
+STUB_FUNC(FreeLayer)
+STUB_FUNC(SetLayerSortOrder)
+STUB_FUNC(SetLayerFrame)
+STUB_FUNC(AdvanceLayerFrame)
+STUB_FUNC(PushText)
+STUB_FUNC(PopText)
+STUB_FUNC(NukeAllScriptLocks)
+STUB_FUNC(ToggleDebugDraw)
+STUB_FUNC(ToggleDrawCameras)
+STUB_FUNC(ToggleDrawLights)
+STUB_FUNC(ToggleDrawSectors)
+STUB_FUNC(ToggleDrawBBoxes)
+STUB_FUNC(ToggleDrawFPS)
+STUB_FUNC(ToggleDrawPerformance)
+STUB_FUNC(ToggleDrawActorStats)
+STUB_FUNC(SectEditSelect)
+STUB_FUNC(SectEditPlace)
+STUB_FUNC(SectEditDelete)
+STUB_FUNC(SectEditInsert)
+STUB_FUNC(SectEditSortAdd)
+STUB_FUNC(SectEditForgetIt)
+STUB_FUNC(FRUTEY_Begin)
+STUB_FUNC(FRUTEY_End)
 
 static void dummyHandler() {
 }
@@ -1725,17 +1759,13 @@ static struct {
 };
 
 struct luaL_reg mainOpcodes[] = {
-	{ "EnginePause", EnginePause },
-	{ "EngineResume", EngineResume },
 	{ "EngineDisplay", EngineDisplay },
-	{ "SearchForFileOrSwapCDs", SearchForFileOrSwapCDs },
 	{ "CheckForFile", CheckForFile },
 	{ "Load", Load },
 	{ "Save", Save },
 	{ "SetActorColormap", SetActorColormap },
 	{ "GetActorCostume", GetActorCostume },
 	{ "SetActorCostume", SetActorCostume },
-	{ "GetActorScale", GetActorScale },
 	{ "SetActorScale", SetActorScale },
 	{ "GetActorTimeScale", GetActorTimeScale },
 	{ "SetActorTimeScale", SetActorTimeScale },
@@ -1748,7 +1778,6 @@ struct luaL_reg mainOpcodes[] = {
 	{ "SetActorWalkRate", SetActorWalkRate },
 	{ "GetActorWalkRate", GetActorWalkRate },
 	{ "SetActorTurnRate", SetActorTurnRate },
-	{ "GetActorTurnRate", GetActorTurnRate },
 	{ "SetSelectedActor", SetSelectedActor },
 	{ "LoadActor", LoadActor },
 	{ "GetActorPos", GetActorPos },
@@ -1756,23 +1785,17 @@ struct luaL_reg mainOpcodes[] = {
 	{ "GetActorPuckVector", GetActorPuckVector },
 	{ "GetActorYawToPoint", GetActorYawToPoint },
 	{ "SetActorReflection", SetActorReflection },
-	{ "SetActorOffsetYaw", SetActorOffsetYaw },
 	{ "PutActorAtInterest", PutActorAtInterest },
-	{ "PutActorAtOrigin", PutActorAtOrigin },
 	{ "PutActorAt", PutActorAt },
 	{ "PutActorInSet", PutActorInSet },
-	{ "GetClippedPos", GetClippedPos },
 	{ "WalkActorVector", WalkActorVector },
 	{ "WalkActorForward", WalkActorForward },
 	{ "DriveActorTo", DriveActorTo },
 	{ "WalkActorTo", WalkActorTo },
 	{ "WalkActorToAvoiding", WalkActorToAvoiding },
-	{ "WalkActorToInterest", WalkActorToInterest },
 	{ "ActorLookAt", ActorLookAt },
-	{ "IsActorLooking", IsActorLooking },
 	{ "SetActorLookRate", SetActorLookRate },
 	{ "GetActorLookRate", GetActorLookRate },
-	{ "CanActorSee", CanActorSee },
 	{ "GetVisibleThings", GetVisibleThings },
 	{ "GetCameraActor", GetCameraActor },
 	{ "SetActorHead", SetActorHead },
@@ -1799,24 +1822,15 @@ struct luaL_reg mainOpcodes[] = {
 	{ "IsActorResting", IsActorResting },
 	{ "SetActorChoreLooping", SetActorChoreLooping },
 	{ "GetActorChores", GetActorChores },
-	{ "GetActorLoopingChores", GetActorLoopingChores },
 	{ "GetActorCostumeDepth", GetActorCostumeDepth },
 	{ "WorldToScreen", WorldToScreen },
-	{ "ScreenToWorld", ScreenToWorld },
 	{ "exit", Exit },
 	{ "FunctionName", FunctionName },
 	{ "EnableDebugKeys", EnableDebugKeys },
 	{ "LockFont", LockFont },
-	{ "UnLockFont", UnLockFont },
-	{ "LockSound", LockSound },
-	{ "UnlockSound", UnlockSound },
-	{ "LockCostume", LockCostume },
-	{ "UnlockCostume", UnlockCostume },
 	{ "EnableControl", EnableControl },
 	{ "DisableControl", DisableControl },
 	{ "GetControlState", GetControlState },
-	{ "GetControlState", GetControlState },
-	{ "PrintMessage", PrintMessage },
 	{ "PrintError", PrintError },
 	{ "PrintWarning", PrintWarning },
 	{ "PrintDebug", PrintDebug },
@@ -1841,27 +1855,20 @@ struct luaL_reg mainOpcodes[] = {
 	{ "FileFindNext", FileFindNext },
 	{ "FileFindDispose", FileFindDispose },
 	{ "InputDialog", InputDialog },
-	{ "QueryDialog", QueryDialog },
 	{ "WriteRegistryValue", WriteRegistryValue },
 	{ "ReadRegistryValue", ReadRegistryValue },
-	{ "GetSectorVertices", GetSectorVertices },
 	{ "GetSectorOppositeEdge", GetSectorOppositeEdge },
-	{ "IsSectorActive", IsSectorActive },
 	{ "MakeSectorActive", MakeSectorActive },
 	{ "PreRender", PreRender },
 	{ "SpewStartup", SpewStartup },
-	{ "SpewShutdown", SpewShutdown },
 	{ "GetCurrentScript", GetCurrentScript },
 	{ "PrintActorCostumes", PrintActorCostumes },
 	{ "PushActorCostume", PushActorCostume },
 	{ "PopActorCostume", PopActorCostume },
 	{ "LoadCostume", LoadCostume },
-	{ "GetPhysicalBytes", GetPhysicalBytes },
-	{ "SetMemoryUsage", SetMemoryUsage },
 	{ "RotateVector", RotateVector },
 	{ "GetCameraPosition", GetCameraPosition },
 	{ "SetCameraPosition", SetCameraPosition },
-	{ "GetCameraInterest", GetCameraInterest },
 	{ "SetCameraInterest", SetCameraInterest },
 	{ "GetCameraFOV", GetCameraFOV },
 	{ "SetCameraFOV", SetCameraFOV },
@@ -1871,24 +1878,12 @@ struct luaL_reg mainOpcodes[] = {
 	{ "PointActorAt", PointActorAt },
 	{ "TurnActorTo", TurnActorTo },
 	{ "PerSecond", PerSecond },
-	{ "SnapToPlane", SnapToPlane },
 	{ "GetAngleBetweenVectors", GetAngleBetweenVectors },
 	{ "GetAngleBetweenActors", GetAngleBetweenActors },
 	{ "SetAmbientLight", SetAmbientLight },
-	{ "FindLight", FindLight },
 	{ "TurnLightOn", TurnLightOn },
-	{ "IsLightOn", IsLightOn },
-	{ "SetLightType", SetLightType },
-	{ "GetLightType", GetLightType },
 	{ "SetLightPosition", SetLightPosition },
-	{ "GetLightPosition", GetLightPosition },
-	{ "SetLightRotation", SetLightRotation },
-	{ "GetLightRotation", GetLightRotation },
-	{ "SetLightAngles", SetLightAngles },
-	{ "GetLightAngles", GetLightAngles },
 	{ "SetLightIntensity", SetLightIntensity },
-	{ "GetLightIntensity", GetLightIntensity },
-	{ "PointLightAt", PointLightAt },
 	{ "LightMgrSetChange", LightMgrSetChange },
 	{ "LightMgrStartup", LightMgrStartup },
 	{ "ImStartSound", ImStartSound },
@@ -1909,7 +1904,6 @@ struct luaL_reg mainOpcodes[] = {
 	{ "ImResume", ImResume },
 	{ "ImSetVoiceEffect", ImSetVoiceEffect },
 	{ "LoadBundle", LoadBundle },
-	{ "UnloadBundle", UnloadBundle },
 	{ "SetGamma", SetGamma },
 	{ "SetActorWalkDominate", SetActorWalkDominate },
 	{ "SetActorConstrain", SetActorConstrain },
@@ -1927,7 +1921,6 @@ struct luaL_reg mainOpcodes[] = {
 	{ "SetActorShadowPlane", SetActorShadowPlane },
 	{ "SetActorShadowPoint", SetActorShadowPoint },
 	{ "SetActiveShadow", SetActiveShadow },
-	{ "ActorDistToShadowPlane", ActorDistToShadowPlane },
 	{ "KillActorShadows", KillActorShadows },
 	{ "AddShadowPlane", AddShadowPlane },
 	{ "SetActorShadowValid", SetActorShadowValid },
@@ -1940,23 +1933,15 @@ struct luaL_reg mainOpcodes[] = {
 	{ "FlushControls", FlushControls },
 	{ "SetActorCollisionMode", SetActorCollisionMode },
 	{ "SetActorCollisionScale", SetActorCollisionScale },
-	{ "MarkDirtyRect", MarkDirtyRect },
-	{ "CacheRect", CacheRect },
-	{ "RestoreCacheRect", RestoreCacheRect },
-	{ "ResReport", ResReport },
-	{ "HardwareAccelerated", HardwareAccelerated },
 	{ "SetActorClipActive", SetActorClipActive },
 	{ "SetActorClipPlane", SetActorClipPlane },
 	{ "FadeOutChore", FadeOutChore },
 	{ "FadeInChore", FadeInChore },
 	{ "IrisDown", IrisDown },
 	{ "IrisUp", IrisUp },
-	{ "IrisComplete", IrisComplete },
-	{ "IrisClear", IrisClear },
 	{ "TextFileGetLineCount", TextFileGetLineCount },
 	{ "TextFileGetLine", TextFileGetLine },
 	{ "ScreenShot", ScreenShot },
-	{ "SaveScreen", SaveScreen },
 	{ "GetSaveGameImage", GetSaveGameImage },
 	{ "GetImage", GetImage },
 	{ "FreeImage", FreeImage },
@@ -1966,36 +1951,116 @@ struct luaL_reg mainOpcodes[] = {
 	{ "GetSaveGameData", GetSaveGameData },
 	{ "SetTextSpeed", SetTextSpeed },
 	{ "GetTextSpeed", GetTextSpeed },
-	{ "FindFileOnAnyCD", FindFileOnAnyCD },
 	{ "DetachFromResources", DetachFromResources },
 	{ "AttachToResources", AttachToResources },
-	{ "Test", Test },
 	{ "ActorPuckOrient", ActorPuckOrient },
-	{ "GetTickCount", LuaGetTickCount },
-	{ "ActorVoiceIs3D", ActorVoiceIs3D },
 	{ "JustLoaded", JustLoaded },
-	{ "DumpMemoryLog", DumpMemoryLog },
 	{ "ResetTextures", ResetTextures },
-	{ "BuildCache", BuildCache },
-	{ "RestoreCache", RestoreCache },
 	{ "ShrinkBoxes", ShrinkBoxes },
 	{ "UnShrinkBoxes", UnShrinkBoxes },
 	{ "GetShrinkPos", GetShrinkPos },
 	{ "NukeResources", NukeResources },
 	{ "SetActorInvClipNode", SetActorInvClipNode },
 	{ "GetDiskFreeSpace", GetDiskFreeSpace },
-	{ "SetWalkSystemFadeTime", SetWalkSystemFadeTime },
 	{ "SaveIMuse", SaveIMuse },
 	{ "RestoreIMuse", RestoreIMuse },
 	{ "GetMemoryUsage", GetMemoryUsage },
-	{ "dofile", new_dofile }
-};
+	{ "dofile", new_dofile },
 
-struct luaL_reg additionalOpcodes[] = {
-	{ "  concatfallback", Concatfallback },
-	{ "  typeoverride", TypeOverride },
-	{ "  dfltcamera", DfltCamera },
-	{ "  dfltcontrol", DfltControl }
+	// new Monkey 4 opcodes:
+
+	{ "SetActiveCD", SetActiveCD },
+	{ "GetActiveCD", GetActiveCD },
+	{ "AreWeInternational", AreWeInternational },
+	{ "MakeScreenTextures", MakeScreenTextures },
+	{ "ThumbnailFromFile", ThumbnailFromFile },
+	{ "ClearSpecialtyTexture", ClearSpecialtyTexture },
+	{ "UnloadActor", UnloadActor },
+	{ "PutActorInOverworld", PutActorInOverworld },
+	{ "RemoveActorFromOverworld", RemoveActorFromOverworld },
+	{ "ClearOverworld", ClearOverworld },
+	{ "ToggleOverworld", ToggleOverworld },
+	{ "ActorStopMoving", ActorStopMoving },
+	{ "SetActorFOV", SetActorFOV },
+	{ "SetActorLighting", SetActorLighting },
+	{ "SetActorHeadLimits", SetActorHeadLimits },
+	{ "ActorActivateShadow", ActorActivateShadow },
+	{ "EnableActorPuck", EnableActorPuck },
+	{ "SetActorGlobalAlpha", SetActorGlobalAlpha },
+	{ "SetActorLocalAlpha", SetActorLocalAlpha },
+	{ "SetActorSortOrder", SetActorSortOrder },
+	{ "GetActorSortOrder", GetActorSortOrder },
+	{ "AttachActor", AttachActor },
+	{ "DetachActor", DetachActor },
+	{ "IsChoreValid", IsChoreValid },
+	{ "IsChorePlaying", IsChorePlaying },
+	{ "IsChoreLooping", IsChoreLooping },
+	{ "StopActorChores", StopActorChores },
+	{ "PlayChore", PlayChore },
+	{ "StopChore", StopChore },
+	{ "PauseChore", PauseChore },
+	{ "AdvanceChore", AdvanceChore },
+	{ "CompleteChore", CompleteChore },
+	{ "LockChore", LockChore },
+	{ "UnlockChore", UnlockChore },
+	{ "LockChoreSet", LockChoreSet },
+	{ "UnlockChoreSet", UnlockChoreSet },
+	{ "LockBackground", LockBackground },
+	{ "UnLockBackground", UnLockBackground },
+	{ "EscapeMovie", EscapeMovie },
+	{ "StopAllSounds", StopAllSounds },
+	{ "LoadSound", LoadSound },
+	{ "FreeSound", FreeSound },
+	{ "PlayLoadedSound", PlayLoadedSound },
+	{ "SetGroupVolume", SetGroupVolume },
+	{ "GetSoundVolume", GetSoundVolume },
+	{ "SetSoundVolume", SetSoundVolume },
+	{ "EnableAudioGroup", EnableAudioGroup },
+	{ "EnableVoiceFX", EnableVoiceFX },
+	{ "PlaySoundFrom", PlaySoundFrom },
+	{ "PlayLoadedSoundFrom", PlayLoadedSoundFrom },
+	{ "SetReverb", SetReverb },
+	{ "UpdateSoundPosition", UpdateSoundPosition },
+	{ "ImSelectSet", ImSelectSet },
+	{ "ImStateHasLooped", ImStateHasLooped },
+	{ "ImStateHasEnded", ImStateHasEnded },
+	{ "ImPushState", ImPushState },
+	{ "ImPopState", ImPopState },
+	{ "ImFlushStack", ImFlushStack },
+	{ "ImGetMillisecondPosition", ImGetMillisecondPosition },
+	{ "GetSectorName", GetSectorName },
+	{ "GetCameraYaw", GetCameraYaw },
+	{ "YawCamera", YawCamera },
+	{ "GetCameraPitch", GetCameraPitch },
+	{ "PitchCamera", PitchCamera },
+	{ "RollCamera", RollCamera },
+	{ "UndimAll", UndimAll },
+	{ "UndimRegion", UndimRegion },
+	{ "GetCPUSpeed", GetCPUSpeed },
+	{ "NewLayer", NewLayer },
+	{ "FreeLayer", FreeLayer },
+	{ "SetLayerSortOrder", SetLayerSortOrder },
+	{ "SetLayerFrame", SetLayerFrame },
+	{ "AdvanceLayerFrame", AdvanceLayerFrame },
+	{ "PushText", PushText },
+	{ "PopText", PopText },
+	{ "NukeAllScriptLocks", NukeAllScriptLocks },
+	{ "ToggleDebugDraw", ToggleDebugDraw },
+	{ "ToggleDrawCameras", ToggleDrawCameras },
+	{ "ToggleDrawLights", ToggleDrawLights },
+	{ "ToggleDrawSectors", ToggleDrawSectors },
+	{ "ToggleDrawBBoxes", ToggleDrawBBoxes },
+	{ "ToggleDrawFPS", ToggleDrawFPS },
+	{ "ToggleDrawPerformance", ToggleDrawPerformance },
+	{ "ToggleDrawActorStats", ToggleDrawActorStats },
+	{ "SectEditSelect", SectEditSelect },
+	{ "SectEditPlace", SectEditPlace },
+	{ "SectEditDelete", SectEditDelete },
+	{ "SectEditInsert", SectEditInsert },
+	{ "SectEditSortAdd", SectEditSortAdd },
+	{ "SectEditForgetIt", SectEditForgetIt },
+	{ "FRUTEY_Begin", FRUTEY_Begin },
+	{ "FRUTEY_End", FRUTEY_End }
 };
 
 struct luaL_reg textOpcodes[] = {
@@ -2051,9 +2116,6 @@ void register_lua() {
 
 	// Register main opcodes functions
 	luaL_openlib(mainOpcodes, sizeof(mainOpcodes) / sizeof(mainOpcodes[0]));
-
-	// Register additional opcodes functions
-	luaL_openlib(additionalOpcodes, sizeof(additionalOpcodes) / sizeof(additionalOpcodes[0]));
 
 	// Register text opcodes functions
 	luaL_openlib(textOpcodes, sizeof(textOpcodes) / sizeof(textOpcodes[0]));
