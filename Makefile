2f77e27ca3b8022e3ef4149cd0ce433c975d6398
Max Horn <max@quendi.de>
2005-01-11 22:02:05 +0000

    Move 'tools' related build rules into tools/module.mk; fixed remainder of bug #1100125 (I hope...)
    svn-id: r16547

--- Makefile
+++ Makefile
@@ -84,41 +84,6 @@ deb:
 	fakeroot debian/rules binary
 
 #######################################################################
-# Tools directory
-#######################################################################
-
-TOOLS := tools/convbdf$(EXEEXT) tools/md5table$(EXEEXT)
-
-tools: $(TOOLS)
-
-tools/convbdf$(EXEEXT): $(srcdir)/tools/convbdf.c
-	$(MKDIR) tools/$(DEPDIR)
-	$(CC) -Wall -o $@ $<
-
-tools/md5table$(EXEEXT): $(srcdir)/tools/md5table.c
-	$(MKDIR) tools/$(DEPDIR)
-	$(CC) -Wall -o $@ $<
-
-#simon/simon-md5.h: $(srcdir)/tools/simon-md5.txt
-#	tools/md5table$(EXEEXT) --c++ < $< > $@
-
-#scumm/scumm-md5.h: $(srcdir)/tools/scumm-md5.txt
-#	tools/md5table$(EXEEXT) --c++ < $< > $@
-
-credits:
-	$(srcdir)/tools/credits.pl --text > AUTHORS
-	$(srcdir)/tools/credits.pl --html > ../web/credits.inc
-	$(srcdir)/tools/credits.pl --cpp > gui/credits.h
-	$(srcdir)/tools/credits.pl --xml > ../docs/docbook/credits.xml
-
-md5scumm: tools/md5table$(EXEEXT)
-	tools/md5table$(EXEEXT) --c++ < $(srcdir)/tools/scumm-md5.txt > scumm/scumm-md5.h
-	tools/md5table$(EXEEXT) --php < $(srcdir)/tools/scumm-md5.txt > ../web/docs/md5.inc
-
-md5simon: tools/md5table$(EXEEXT)
-	tools/md5table$(EXEEXT) --c++ < $(srcdir)/tools/simon-md5.txt > simon/simon-md5.h
-
-#######################################################################
 # Unit/regression tests                                               #
 # In order to use 'make test' you have to install cxxtest inside the  #
 # test/cxxtest dir. Get it from http://cxxtest.sourceforge.net.       #
@@ -187,4 +152,4 @@ win32dist: scummvm$(EXEEXT)
 	u2d $(WIN32PATH)/*.txt
 
 
-.PHONY: deb bundle test osxsnap win32dist dist install uninstall credits tools
+.PHONY: deb bundle test osxsnap win32dist dist install uninstall
---
+++ tools/module.mk
@@ -0,0 +1,77 @@
+MODULE := tools
+
+MODULE_DIRS += \
+	tools
+
+
+#######################################################################
+# Tools directory
+#######################################################################
+
+TOOLS := tools/convbdf$(EXEEXT) tools/md5table$(EXEEXT)
+
+
+# Make sure the 'all' / 'clean' targets build/clean the tools, too
+all: tools
+clean: clean-tools
+
+# Main target
+tools: $(TOOLS)
+
+clean-tools:
+	-$(RM) $(TOOLS)
+
+#
+# Build rules for the tools
+#
+
+tools/convbdf$(EXEEXT): $(srcdir)/tools/convbdf.c
+	$(MKDIR) tools/$(DEPDIR)
+	$(CC) -Wall -o $@ $<
+
+tools/md5table$(EXEEXT): $(srcdir)/tools/md5table.c
+	$(MKDIR) tools/$(DEPDIR)
+	$(CC) -Wall -o $@ $<
+
+#
+# Rules to explicitly rebuild the credits / MD5 tables
+#
+
+credits:
+	$(srcdir)/tools/credits.pl --text > AUTHORS
+	$(srcdir)/tools/credits.pl --html > ../web/credits.inc
+	$(srcdir)/tools/credits.pl --cpp > gui/credits.h
+	$(srcdir)/tools/credits.pl --xml > ../docs/docbook/credits.xml
+
+md5scumm: tools/md5table$(EXEEXT)
+	tools/md5table$(EXEEXT) --c++ < $(srcdir)/tools/scumm-md5.txt > scumm/scumm-md5.h
+	tools/md5table$(EXEEXT) --php < $(srcdir)/tools/scumm-md5.txt > ../web/docs/md5.inc
+
+md5simon: tools/md5table$(EXEEXT)
+	tools/md5table$(EXEEXT) --c++ < $(srcdir)/tools/simon-md5.txt > simon/simon-md5.h
+
+
+#
+# Rules which automatically and implicitly rebuild the credits and 
+# MD5 tables when needed.
+# These are currently disabled, because if the input data changes, then
+# the generated files should be checked in, too. Otherwise, we'd reduce
+# portability to system on which our tools can't (automatically) be
+# run for some reason.
+#
+
+#simon/simon-md5.h: $(srcdir)/tools/simon-md5.txt tools/md5table$(EXEEXT)
+#	tools/md5table$(EXEEXT) --c++ < $< > $@
+
+#scumm/scumm-md5.h: $(srcdir)/tools/scumm-md5.txt tools/md5table$(EXEEXT)
+#	tools/md5table$(EXEEXT) --c++ < $< > $@
+
+#AUTHORS: $(srcdir)/tools/credits.pl
+#	$(srcdir)/tools/credits.pl --text > $@
+
+#gui/credits.h: $(srcdir)/tools/credits.pl
+#	$(srcdir)/tools/credits.pl --cpp > $@
+
+
+
+.PHONY: clean-tools tools credits md5scumm md5simon
