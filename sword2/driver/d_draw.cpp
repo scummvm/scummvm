0b461c85062a42682b1075b9ed93572b588f4656
Torbjörn Andersson <eriktorbjorn@users.sourceforge.net>
2005-01-17 10:57:15 +0000

    Updated copyright header, as discussed on the mailing list.
    svn-id: r16580

--- sword2/driver/d_draw.cpp
+++ sword2/driver/d_draw.cpp
@@ -1,4 +1,5 @@
-/* Copyright (C) 1994-2005 Revolution Software Ltd
+/* Copyright (C) 1994-1998 Revolution Software Ltd.
+ * Copyright (C) 2003-2005 The ScummVM project
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
