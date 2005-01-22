3b637456aad81702d8a61b7488e3d43c5d9532ea
Travis Howell <kirben@scummvm.org>
2005-01-22 01:47:07 +0000

    Set SFX volume correctly.
    svn-id: r16611

--- simon/simon.cpp
+++ simon/simon.cpp
@@ -665,7 +665,7 @@ int SimonEngine::init(GameDetector &detector) {
 	if (!_mixer->isReady())
 		warning("Sound initialization failed. "
 						"Features of the game that depend on sound synchronization will most likely break");
-	set_volume(ConfMan.getInt("sfx_volume"));
+	_mixer->setVolumeForSoundType(SoundMixer::kSFXAudioDataType, ConfMan.getInt("sfx_volume"));
 	_mixer->setVolumeForSoundType(SoundMixer::kMusicAudioDataType, ConfMan.getInt("music_volume"));
 
 	_system->beginGFXTransaction();
