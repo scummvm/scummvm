862e03da66facda0d19eb9c01cc03d0e49f7878a
Eugene Sandulenko <sev@scummvm.org>
2005-04-05 15:53:16 +0000

    Updated part of headers.
    svn-id: r17390

--- gob/driver_vga.cpp
+++ gob/driver_vga.cpp
@@ -1,4 +1,25 @@
-#include "driver_vga.h"
+/* ScummVM - Scumm Interpreter
+ * Copyright (C) 2004 Ivan Dubrov
+ * Copyright (C) 2004-2005 The ScummVM project
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * aint32 with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Header$
+ *
+ */
+#include "gob/driver_vga.h"
 
 #define STUB_FUNC	printf("STUB: %s\n", __PRETTY_FUNCTION__)
 
