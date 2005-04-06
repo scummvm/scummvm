f7ae1b822d353f44772a2fe6826bda83c6bd8682
Travis Howell <kirben@scummvm.org>
2005-04-06 02:26:47 +0000

    Correction
    svn-id: r17407

--- scumm/script_v5.cpp
+++ scumm/script_v5.cpp
@@ -1517,9 +1517,7 @@ void ScummEngine_v5::o5_loadRoomWithEgo() {
 	}
 
 	// This is based on disassembly
-	camera._cur.x = a->_pos.x;
-
-	setCameraAt(a->_pos.x, a->_pos.y);
+	camera._cur.x = camera._dest.x = a->_pos.x;
 	setCameraFollows(a);
 
 	_fullRedraw = 1;
