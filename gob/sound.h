9aaff636aad78681650909f5a08decba2cd40283
Max Horn <max@quendi.de>
2005-04-09 19:19:54 +0000

    Fix legal header. Be careful when doing global search and replace! Better use full-word regex, to avoid problems like this
    svn-id: r17485

--- gob/sound.h
+++ gob/sound.h
@@ -13,7 +13,7 @@
  * GNU General Public License for more details.
 
  * You should have received a copy of the GNU General Public License
- * aint32 with this program; if not, write to the Free Software
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  * $Header$
