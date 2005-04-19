9a758a9479767c97a0361959cae26fb82f7102df
Travis Howell <kirben@scummvm.org>
2005-04-19 07:28:43 +0000

    Simpler check.
    svn-id: r17680

--- scumm/palette.cpp
+++ scumm/palette.cpp
@@ -239,11 +239,7 @@ void ScummEngine::setPaletteFromPtr(const byte *ptr, int numcolor) {
 		// if we remove this patch?
 		// Since it also causes problems in Zak256, I am turning it off for all V4 games and older.
 		if (_version >= 5 && _version <= 6) {
-			if ((_heversion >= 80) && (i == 15 || r < 252 || g < 252 || b < 252)) {
-				*dest++ = r;
-				*dest++ = g;
-				*dest++ = b;
-			} else if (i <= 15 || r < 252 || g < 252 || b < 252) {
+			if ((_heversion <= 72 && i < 15) || i == 15 || r < 252 || g < 252 || b < 252) {
 				*dest++ = r;
 				*dest++ = g;
 				*dest++ = b;
