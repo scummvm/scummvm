3890d18e0cba28e4a1c1daf1ddf2a0e5c5a3ddf0
Eugene Sandulenko <sev@scummvm.org>
2005-05-10 00:39:59 +0000

    Fix compilation
    svn-id: r18016

--- common/singleton.h
+++ common/singleton.h
@@ -45,7 +45,7 @@ private:
 	 * and you specialise makeInstance to return an instance of a subclass.
 	 */
 	//template <class T>
-#if defined (_WIN32_WCE) || (_MSC_VER)
+#if defined (_WIN32_WCE) || defined (_MSC_VER)
 //FIXME evc4 and msvc7 doesn't like it as private member
 public:
 #endif
