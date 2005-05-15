d91c687619fc9a79c9ce0b701fe23851b47648ee
Torbjörn Andersson <eriktorbjorn@users.sourceforge.net>
2005-05-15 13:57:58 +0000

    Indentation and whitespace.
    svn-id: r18107

--- common/md5.h
+++ common/md5.h
@@ -25,19 +25,17 @@
 
 namespace Common {
 
-typedef struct
-{
-    uint32 total[2];
-    uint32 state[4];
-    uint8 buffer[64];
-}
-md5_context;
+typedef struct {
+	uint32 total[2];
+	uint32 state[4];
+	uint8 buffer[64];
+} md5_context;
 
-void md5_starts( md5_context *ctx );
-void md5_update( md5_context *ctx, const uint8 *input, uint32 length );
-void md5_finish( md5_context *ctx, uint8 digest[16] );
+void md5_starts(md5_context *ctx);
+void md5_update(md5_context *ctx, const uint8 *input, uint32 length);
+void md5_finish(md5_context *ctx, uint8 digest[16]);
 
-bool md5_file( const char *name, uint8 digest[16], const char *directory = NULL, uint32 length = 0 );
+bool md5_file(const char *name, uint8 digest[16], const char *directory = NULL, uint32 length = 0);
 
 } // End of namespace Common
 
