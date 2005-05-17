8d782007c19c50240bf3101013ea57e8ca35bc76
Max Horn <max@quendi.de>
2005-05-17 11:46:07 +0000

    Added my Woodruff version to the MD5 list
    svn-id: r18137

--- gob/gob.cpp
+++ gob/gob.cpp
@@ -91,6 +91,9 @@ static const GobGameSettings gob_games[] = {
 
 	// CD 1.0
 	{"woodruff", "The Bizarre Adventures of Woodruff and the Schnibble", GF_WOODRUFF, "c27402cee260d2ff1c4cecb2006a630a"},
+
+	// CD 1.00, German release (INTRO.STRK seems to be multilingual, though?)
+	{"woodruff", "The Bizarre Adventures of Woodruff and the Schnibble", GF_WOODRUFF, "17581e3219d18898c35d616d1b823943"},
 #endif
 	{0, 0, 0, 0}
 };
