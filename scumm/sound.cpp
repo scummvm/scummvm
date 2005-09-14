2b9bf456de24195e2c5431bcba8ee1cc992e57c2
Jonathan Gray <khalek@users.sourceforge.net>
2005-09-14 10:38:07 +0000

    Only try opening SfxFile for >= v5 games.
    svn-id: r18822

--- scumm/sound.cpp
+++ scumm/sound.cpp
@@ -1074,7 +1074,9 @@ void Sound::talkSound(uint32 a, uint32 b, int mode, int channel) {
 
 void Sound::setupSound() {
 	delete _sfxFile;
-	_sfxFile = openSfxFile();
+
+	if (_vm->_version >= 5)
+		_sfxFile = openSfxFile();
 
 	if (_vm->_heversion >= 70) {
 		setupHEMusicFile();
