016620838a417689065ba8d7d71d7a2ae49eebb0
Chris Apers <chrilith@users.sourceforge.net>
2005-10-12 20:46:05 +0000

    Little change
    svn-id: r19050

--- backends/PalmOS/Src/init_arm.cpp
+++ backends/PalmOS/Src/init_arm.cpp
@@ -2,16 +2,16 @@
 #include "globals.h"
 #include "init_arm.h"
 
-static void PnoInit(DmResID resID,PNOInitType *pnoP) {
+static void PnoInit(DmResID resID, PNOInitType *pnoP) {
 	// Load and allocate PNO
 	MemHandle armH = DmGetResource('ARMC', resID);
-
+	
 	if (armH) {
 		MemPtr armP = MemHandleLock(armH);
 		PnoLoad(&pnoP->pnoDesc, armP);
 		MemPtrUnlock(armP);
 		DmReleaseResource(armH);
-
+		
 		// Init PNO
 		PnoEntryHeader *header = (PnoEntryHeader *)ALIGN_4BYTE(pnoP->headerBuffer);
 		pnoP->alignedHeader = header;
@@ -31,5 +31,5 @@ void ARMInit() {
 void ARMRelease() {
 	PnoUnload(&ARM(ARM_ENGINE).pnoDesc);
 	PnoUnload(&ARM(ARM_COMMON).pnoDesc);
+	MemSet(gVars->arm, sizeof(gVars->arm), 0);
 }
-
