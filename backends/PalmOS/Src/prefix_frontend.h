1b96ad1a91ff0117892a0bd5d6f37b576b4038cf
Chris Apers <chrilith@users.sourceforge.net>
2005-10-12 21:11:49 +0000

    68k modules and frontend prefix files
    svn-id: r19055

---
+++ backends/PalmOS/Src/prefix_frontend.h
@@ -0,0 +1,7 @@
+#ifndef PREFIX_H
+#define PREFIX_H
+
+#define PALMOS_68K
+//#define _DEBUG_ENGINE
+
+#endif
---
+++ backends/PalmOS/Src/prefix_simon.h
@@ -0,0 +1,9 @@
+#ifndef PREFIX_H
+#define PREFIX_H
+
+#include "compile.h"
+#undef DISABLE_SIMON
+
+#define PALMOS_68K
+
+#endif
