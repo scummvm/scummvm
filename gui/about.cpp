b36677af7101bc356c81dedf36302555515eb306
Eugene Sandulenko <sev@scummvm.org>
2005-10-18 01:30:26 +0000

    Update FSF address. Eek. Actually that took place on May 1, 2005
    svn-id: r19142

--- gui/about.cpp
+++ gui/about.cpp
@@ -13,7 +13,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  * $Header$
  */
@@ -63,7 +63,7 @@ static const char *credits_intro[] = {
 "\\C""",
 "\\C""This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.",
 "\\C""",
-"\\C""You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.",
+"\\C""You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.",
 "\\C"""
 };
 
