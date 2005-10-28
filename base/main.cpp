0b0c3fda6e578ea43241f8c47b5b685d027d961f
Eugene Sandulenko <sev@scummvm.org>
2005-10-28 19:03:55 +0000

    0.8.0pre -> 0.8.0 and put release date. Tag is 2 hours away.
    svn-id: r19345

--- base/main.cpp
+++ base/main.cpp
@@ -93,9 +93,9 @@
  * to setup, but I have no idea if/how one can also change files which are not
  * currently being commit'ed.
  */
-const char *gScummVMVersion = "0.8.0pre";
+const char *gScummVMVersion = "0.8.0";
 const char *gScummVMBuildDate = __DATE__ " " __TIME__;
-const char *gScummVMFullVersion = "ScummVM 0.8.0pre (" __DATE__ " " __TIME__ ")";
+const char *gScummVMFullVersion = "ScummVM 0.8.0 (" __DATE__ " " __TIME__ ")";
 const char *gScummVMFeatures = ""
 #ifdef USE_TREMOR
 	"Tremor "
