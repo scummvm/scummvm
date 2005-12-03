4e05925676036dd06a962cfa214d1d6da8e1e209
Chris Apers <chrilith@users.sourceforge.net>
2005-12-03 19:18:03 +0000

    Not needed anymore
    svn-id: r19736

--- backends/PalmOS/Src/arm/pno_queen.h
+++
@@ -1,8 +0,0 @@
-#ifndef PNOQUEEN_H
-#define PNOQUEEN_H
-
-#define COMPILE_QUEEN
-
-unsigned long Display_blit(void *userData68KP);
-
-#endif
--- backends/PalmOS/Src/arm/pno_scumm.h
+++
@@ -1,9 +0,0 @@
-#ifndef PNOSCUMM_H
-#define PNOSCUMM_H
-
-#define COMPILE_SCUMM
-
-unsigned long Gdi_drawStripToScreen(void *userData68KP);
-unsigned long CostumeRenderer_proc3(void *userData68KP);
-
-#endif
