775386459972b43308de3f82edf75660ad5e2161
Max Horn <max@quendi.de>
2005-12-09 22:23:55 +0000

    Fix warning
    svn-id: r19768

--- sky/disk.cpp
+++ sky/disk.cpp
@@ -176,7 +176,7 @@ uint8 *Disk::loadFile(uint16 fileNr) {
 uint16 *Disk::loadScriptFile(uint16 fileNr) {
 	uint16 *buf = (uint16*)loadFile(fileNr);
 #ifdef SCUMM_BIG_ENDIAN
-	for (int i = 0; i < _lastLoadedFileSize / 2; i++)
+	for (uint i = 0; i < _lastLoadedFileSize / 2; i++)
 		buf[i] = FROM_LE_16(buf[i]);
 #endif
 	return buf;
