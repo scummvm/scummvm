2d844c0140a6ace7e9c0f428de1f99271e4b789a
Johannes Schickel <lordhoto@scummvm.org>
2006-01-19 21:50:54 +0000

    Changed the default names of the game detector.
    svn-id: r20092

--- kyra/kyra.cpp
+++ kyra/kyra.cpp
@@ -71,23 +71,23 @@ struct KyraGameSettings {
 };
 
 static const KyraGameSettings kyra_games[] = {
-	{ "kyra1", "Legend of Kyrandia (Floppy, English)",	GI_KYRA1, GF_ENGLISH | GF_FLOPPY, // english floppy 1.0 from Malice
+	{ "kyra1", "Legend of Kyrandia (English/Floppy)",	GI_KYRA1, GF_ENGLISH | GF_FLOPPY, // english floppy 1.0 from Malice
 										"3c244298395520bb62b5edfe41688879", "GEMCUT.EMC" },
-	{ "kyra1", "Legend of Kyrandia (Floppy, English)",	GI_KYRA1, GF_ENGLISH | GF_FLOPPY, 
+	{ "kyra1", "Legend of Kyrandia (English/Floppy)",	GI_KYRA1, GF_ENGLISH | GF_FLOPPY, 
 										"796e44863dd22fa635b042df1bf16673", "GEMCUT.EMC" },
-	{ "kyra1", "Legend of Kyrandia (Floppy, French)",	GI_KYRA1, GF_FRENCH | GF_FLOPPY,
+	{ "kyra1", "Legend of Kyrandia (French/Floppy)",	GI_KYRA1, GF_FRENCH | GF_FLOPPY,
 										"abf8eb360e79a6c2a837751fbd4d3d24", "GEMCUT.EMC" },
-	{ "kyra1", "Legend of Kyrandia (Floppy, German)",	GI_KYRA1, GF_GERMAN | GF_FLOPPY, 
+	{ "kyra1", "Legend of Kyrandia (German/Floppy)",	GI_KYRA1, GF_GERMAN | GF_FLOPPY, 
 										"6018e1dfeaca7fe83f8d0b00eb0dd049", "GEMCUT.EMC"},
-	{ "kyra1", "Legend of Kyrandia (Floppy, Spanish)",	GI_KYRA1, GF_SPANISH | GF_FLOPPY, // from VooD
+	{ "kyra1", "Legend of Kyrandia (Spanish/Floppy)",	GI_KYRA1, GF_SPANISH | GF_FLOPPY, // from VooD
 										"8909b41596913b3f5deaf3c9f1017b01", "GEMCUT.EMC"},
-	{ "kyra1", "Legend of Kyrandia (Floppy, Spanish)",	GI_KYRA1, GF_SPANISH | GF_FLOPPY, // floppy 1.8 from clemmy
+	{ "kyra1", "Legend of Kyrandia (Spanish/Floppy)",	GI_KYRA1, GF_SPANISH | GF_FLOPPY, // floppy 1.8 from clemmy
 										"747861d2a9c643c59fdab570df5b9093", "GEMCUT.EMC"},
-	{ "kyra1", "Legend of Kyrandia (CD, English)",		GI_KYRA1, GF_ENGLISH | GF_TALKIE, 
+	{ "kyra1", "Legend of Kyrandia (English/CD)",		GI_KYRA1, GF_ENGLISH | GF_TALKIE, 
 										"fac399fe62f98671e56a005c5e94e39f", "GEMCUT.PAK" },
-	{ "kyra1", "Legend of Kyrandia (CD, German)",		GI_KYRA1, GF_GERMAN | GF_TALKIE, 
+	{ "kyra1", "Legend of Kyrandia (German/CD)",		GI_KYRA1, GF_GERMAN | GF_TALKIE, 
 										"230f54e6afc007ab4117159181a1c722", "GEMCUT.PAK" },
-	{ "kyra1", "Legend of Kyrandia (CD, French)",		GI_KYRA1, GF_FRENCH | GF_TALKIE, 
+	{ "kyra1", "Legend of Kyrandia (French/CD)",		GI_KYRA1, GF_FRENCH | GF_TALKIE, 
 										"b037c41768b652a040360ffa3556fd2a", "GEMCUT.PAK" },
 	{ "kyra1", "Legend of Kyrandia (Demo)",			GI_KYRA1, GF_DEMO | GF_ENGLISH,
 										"fb722947d94897512b13b50cc84fd648", "DEMO1.WSA" },
