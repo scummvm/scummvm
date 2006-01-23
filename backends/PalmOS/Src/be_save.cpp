d183a714462aadaee248c107ab26d1334f22398a
Chris Apers <chrilith@users.sourceforge.net>
2006-01-23 19:46:27 +0000

    Update copyright notice
    svn-id: r20151

--- backends/PalmOS/Src/be_save.cpp
+++ backends/PalmOS/Src/be_save.cpp
@@ -1,7 +1,7 @@
 /* ScummVM - Scumm Interpreter
  * Copyright (C) 2001  Ludvig Strigeus
  * Copyright (C) 2001-2006 The ScummVM project
- * Copyright (C) 2002-2005 Chris Apers - PalmOS Backend
+ * Copyright (C) 2002-2006 Chris Apers - PalmOS Backend
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
