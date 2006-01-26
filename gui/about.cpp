b5dcb4e874dceceddd87b21f2e5f04ac911aeca7
Oystein Eftevaag <vinterstum@users.sourceforge.net>
2006-01-26 23:18:36 +0000

    Adding Kyrandia to the About dialog copyright section.
    svn-id: r20197

--- gui/about.cpp
+++ gui/about.cpp
@@ -61,6 +61,7 @@ static const char *credits_intro[] = {
 "\\C""and Steve Stamatiadis",
 "\\C""Inherit the Earth (C) Wyrmkeep Entertainment",
 "\\C""Goblins Games (C) Coktel Vision",
+"\\C""The Legend of Kyrandia (C) Electronic Arts",
 "\\C""",
 "\\C""This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.",
 "\\C""",
