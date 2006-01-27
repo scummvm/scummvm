fef533e93e554fd1cb1cb44dac410632d5051562
Eugene Sandulenko <sev@scummvm.org>
2006-01-27 19:05:24 +0000

    Proper RCS IDs
    svn-id: r20236

--- common/unzip.h
+++ common/unzip.h
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
- * $Header $
+ * $Header$
  */
 
 /* unzip.h -- IO for uncompress .zip files using zlib
