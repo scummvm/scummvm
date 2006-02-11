9e3ee81579d34cca3c76e2d64e82d1d90329ab9a
Chris Apers <chrilith@users.sourceforge.net>
2006-02-11 18:34:49 +0000

    Updated header
    svn-id: r20564

--- backends/PalmOS/Src/missing/time.h
+++ backends/PalmOS/Src/missing/time.h
@@ -1,6 +1,7 @@
 /* ScummVM - Scumm Interpreter
  * Copyright (C) 2001  Ludvig Strigeus
  * Copyright (C) 2001-2006 The ScummVM project
+ * Copyright (C) 2002-2006 Chris Apers - PalmOS Backend
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -20,6 +21,7 @@
  * $Id$
  *
  */
+
 #ifndef __TIME_H__
 #define __TIME_H__
 
