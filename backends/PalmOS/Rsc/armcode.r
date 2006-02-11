fde4fd87e7b19888778a5926fefe5a2a220990f0
Chris Apers <chrilith@users.sourceforge.net>
2006-02-11 19:08:25 +0000

    Bye bye
    svn-id: r20576

--- backends/PalmOS/Rsc/armcode.r
+++
@@ -1,7 +0,0 @@
-// ARM optimized functions
-
-read 'ARMC' (100, "ARM")	"PNO/common.arm";
-
-// Comment this line if there is no ARM optimization
-// for the compiled engine
-//read 'ARMC' (101, "ARM")	"PNO/engine.arm";
