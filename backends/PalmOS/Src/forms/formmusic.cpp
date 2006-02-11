9b1d8dc1149ba344e88da06f70f945ee35562e23
Chris Apers <chrilith@users.sourceforge.net>
2006-02-11 18:24:42 +0000

    Fixed driver test
    svn-id: r20561

--- backends/PalmOS/Src/forms/formmusic.cpp
+++ backends/PalmOS/Src/forms/formmusic.cpp
@@ -1,3 +1,27 @@
+/* ScummVM - Scumm Interpreter
+ * Copyright (C) 2001  Ludvig Strigeus
+ * Copyright (C) 2001-2006 The ScummVM project
+ * Copyright (C) 2002-2006 Chris Apers - PalmOS Backend
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * $URL$
+ * $Id$
+ *
+ */
+
 #include <PalmOS.h>
 
 #include "start.h"
@@ -295,7 +319,7 @@ Boolean MusicFormHandleEvent(EventPtr eventP) {
 					if (!OPTIONS_TST(kOptDeviceZodiac) && !OPTIONS_TST(kOptSonyPa1LibAPI)) {
 						ListType *list1P = (ListType *)GetObjectPtr(TabMusicDriverList);
 
-						if (LstGetSelection(list1P) == 1) {
+						if (LstGetSelection(list1P) == 4) {
 							FrmCustomAlert(FrmInfoAlert, "There is no built-in MIDI support on your device.", 0, 0);
 							LstSetSelection(list1P, 0);
 							CtlSetLabel((ControlType *)GetObjectPtr(TabMusicDriverPopTrigger), LstGetSelectionText(list1P, LstGetSelection(list1P)));
