3dc99262a2460ab2c6d24e476673a2489c24b982
Chris Apers <chrilith@users.sourceforge.net>
2006-02-12 18:39:43 +0000

    - Removed obsolete DEBUG defines - Added LURE and OS5 defines
    svn-id: r20637

--- backends/PalmOS/Src/compile.h
+++ backends/PalmOS/Src/compile.h
@@ -1,3 +1,27 @@
+/* ScummVM - Scumm Interpreter
+ * Copyright (C) 2001  Ludvig Strigeus
+ * Copyright (C) 2001-2006 The ScummVM project
+ * Copyright (C) 2002-2006 Chris Apers - PalmOS Backend
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * $URL$
+ * $Id$
+ *
+ */
+
 #ifndef __COMPILE_H__
 #define __COMPILE_H__
 
@@ -14,23 +38,20 @@
 #define DISABLE_KYRA
 #define DISABLE_AWE
 #define DISABLE_GOB 
+#define DISABLE_LURE
 
 #define DISABLE_HQ_SCALERS
 
 // PalmOS
 #define PALMOS_MODE
 #define COMPILE_ZODIAC
+//#define COMPILE_OS5
 
 //#define DISABLE_ADLIB
-
 //#define DISABLE_LIGHTSPEED
 //#define DISABLE_TAPWAVE
-//#define DISABLE_ARM
 //#define REDUCE_MEMORY_USAGE
 
-//#define DEBUG_ARM
 //#define _DEBUG
-//#define _DEBUG_STDIO
-//#define _DEBUG_ENGINE
 
 #endif
