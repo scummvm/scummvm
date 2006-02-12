353ea3881f7d651ded9be6b3f034987fcb7adddc
Chris Apers <chrilith@users.sourceforge.net>
2006-02-12 18:30:15 +0000

    New definitions for frontend <-> module communication
    svn-id: r20635

--- backends/PalmOS/Src/palmdefs.h
+++ backends/PalmOS/Src/palmdefs.h
@@ -2,7 +2,17 @@
 #define PALMDEFS_H
 
 #define appFileCreator						'ScVM'
-#define	sysAppLaunchCustomDeleteEngine		(sysAppLaunchCmdCustomBase + 0)
+#define	sysAppLaunchCustomEngineDelete		(sysAppLaunchCmdCustomBase + 0)
+#define	sysAppLaunchCustomEngineGetInfo		(sysAppLaunchCmdCustomBase + 1)
+
+enum {
+	GET_NONE		= 0,
+	GET_DATACOMMON	= 1 << 1,
+	GET_DATAENGINE	= 1 << 2,
+
+	GET_MODE68K		= 1 << 14,
+	GET_MODEARM		= 1 << 15,
+};
 
 SysAppInfoPtr SysGetAppInfo(SysAppInfoPtr *uiAppPP, SysAppInfoPtr *actionCodeAppPP)
 	SYS_TRAP(sysTrapSysGetAppInfo);
