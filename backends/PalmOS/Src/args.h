08efabc14d9b54827b279c8fc6acd752f5a5057f
Chris Apers <chrilith@users.sourceforge.net>
2006-02-12 19:27:48 +0000

    New feature to pass selected stack size to modules
    svn-id: r20648

--- backends/PalmOS/Src/args.h
+++ backends/PalmOS/Src/args.h
@@ -1,3 +1,27 @@
+/* ScummVM - Scumm Interpreter
+ * Copyright (C) 2001  Ludvig Strigeus
+ * Copyright (C) 2001-2006 The ScummVM project
+ * Copyright (C) 2002-2006 Chris Apers - PalmOS Backend
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * $URL$
+ * $Id$
+ *
+ */
+
 #ifndef ARGS_H
 #define ARGS_H
 
@@ -6,6 +30,7 @@
 #define ftrArgsData		1300
 #define ftrArgsCount	1301
 #define ftrVars			1302
+#define ftrStack		1303
 
 Char **ArgsInit();
 void ArgsAdd(Char **argvP, const Char *argP, const Char *parmP, UInt8 *countArgP);
