b568df4de6b7995f5778905250c73528fa43b489
Chris Apers <chrilith@users.sourceforge.net>
2006-02-26 13:02:34 +0000

    Missing prefix files for 68k engines
    svn-id: r20913

---
+++ backends/PalmOS/Src/prefix_cine.h
@@ -0,0 +1,9 @@
+#ifndef PREFIX_H
+#define PREFIX_H
+
+#include "compile.h"
+#undef DISABLE_CINE
+
+#define PALMOS_68K
+
+#endif
---
+++ backends/PalmOS/Src/prefix_kyra.h
@@ -0,0 +1,9 @@
+#ifndef PREFIX_H
+#define PREFIX_H
+
+#include "compile.h"
+#undef DISABLE_KYRA
+
+#define PALMOS_68K
+
+#endif
