2ef4f1fea2c04092e512fabc80aff2eced31c69d
Chris Apers <chrilith@users.sourceforge.net>
2006-02-26 16:36:51 +0000

    Move launcher files to a new folder
    svn-id: r20931

---
+++ backends/PalmOS/Src/launcher/forms/formCards.h
@@ -0,0 +1,32 @@
+/* ScummVM - Scumm Interpreter
+ * Copyright (C) 2001  Ludvig Strigeus
+ * Copyright (C) 2001-2006 The ScummVM project
+ * Copyright (C) 2002-2006 Chris Apers - PalmOS Backend
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * $URL$
+ * $Id$
+ *
+ */
+
+#ifndef __FORMCARDS_H__
+#define __FORMCARDS_H__
+
+UInt16 parseCards();
+void CardSlotFormUpdate();
+void CardSlotCreateDirs();
+
+#endif
