4d4c29787eaee56904661df0fc183bd7b614609d
Max Horn <max@quendi.de>
2006-03-29 11:00:39 +0000

    Improved the 'test' build rule
    svn-id: r21492

--- test/module.mk
+++ test/module.mk
@@ -1,17 +1,29 @@
 ######################################################################
 # Unit/regression tests, based on CxxTest.
 # Use the 'test' target to run them.
-# Edit 'TESTS' to add more tests.
+# Edit TESTS and TESTLIBS to add more tests.
+#
 ######################################################################
 
-CXXTEST := test/cxxtest
-TESTS := test/common/*.h
-CPPFLAGS += -I$(CXXTEST)
-test: runner
-	./runner
-runner: runner.o common/libcommon.a
-	$(CXX) -o $@ $+
-runner.cpp: $(TESTS)
-	$(CXXTEST)/cxxtestgen.py --error-printer -o  $@ $+
+TESTS        := test/common/*.h
+TEST_LIBS    := common/libcommon.a
+
+#
+TEST_FLAGS   := --runner=StdioPrinter
+TEST_CFLAGS  := -Itest/cxxtest
+TEST_LDFLAGS := 
+
+
+# Enable this to get an X11 GUI for the error reporter.
+#TEST_FLAGS   += --gui=X11Gui
+#TEST_LDFLAGS += -L/usr/X11R6/lib -lX11
+
+
+test: test/runner
+	./test/runner
+test/runner: test/runner.cpp $(TEST_LIBS)
+	$(CXX) $(CXXFLAGS) $(CPPFLAGS) $(TEST_LDFLAGS) $(TEST_CFLAGS) -o $@ $+
+test/runner.cpp: $(TESTS)
+	test/cxxtest/cxxtestgen.py $(TEST_FLAGS) -o $@ $+
 
 .PHONY: test
