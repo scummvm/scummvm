071884589a090472c7a1e85dd98ea1a3d9c27d41
Travis Howell <kirben@scummvm.org>
2006-04-09 22:44:27 +0000

    Adjust line breaks, to prevent regression.
    svn-id: r21738

--- engines/simon/charset.cpp
+++ engines/simon/charset.cpp
@@ -317,7 +317,7 @@ void SimonEngine::showmessage_print_char(byte chr) {
 		print_char_helper_1(&chr, 1);
 		print_char_helper_5(_textWindow);
 	} else if (chr == 0 || chr == ' ' || chr == 10) {
-		uint count = (getGameType() == GType_FF) ? _printCharPixelCount + 1: _numLettersToPrint;
+		uint count = (getGameType() == GType_FF) ? _printCharPixelCount + 1: _printCharPixelCount;
 		if (_printCharMaxPos - _printCharCurPos >= count) {
 			_printCharCurPos += _printCharPixelCount;
 			print_char_helper_1(_lettersToPrintBuf, _numLettersToPrint);
