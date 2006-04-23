b9fbf9091d46d1ecd6927c11f8cd3a4ccfe49abf
Travis Howell <kirben@scummvm.org>
2006-04-23 08:34:19 +0000

    Fix video sequences shown by arcade machines
    svn-id: r22092

--- engines/simon/animation.cpp
+++ engines/simon/animation.cpp
@@ -244,6 +244,7 @@ void MoviePlayer::processFrame() {
 	uint x = (_vm->_screenWidth - _width) / 2;
 	uint y = (_vm->_screenHeight - _height) / 2;
 
+	memcpy(_vm->_frontBuf, _frameBuffer1, _frameSize);
 	_vm->_system->copyRectToScreen(_vm->_frontBuf, _width, x, y, _width, _height);
 
 	if ((_bgSoundStream == NULL) || ((_mixer->getSoundElapsedTime(_bgSound) * _frameRate) / 1000 < _frameNum + 1) ||
