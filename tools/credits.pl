12fd917fedbb295289390da3f2a9096238fb3fff
Eugene Sandulenko <sev@scummvm.org>
2006-06-13 18:36:10 +0000

    Sync credits changes.
    svn-id: r23095

--- tools/credits.pl
+++ tools/credits.pl
@@ -536,7 +536,7 @@ begin_credits("Credits");
 		  add_person("Kostas Nakos", "knakos", "");
 	  end_section();
 
-	  begin_section("Playstation 2");
+	  begin_section("PlayStation 2");
 		  add_person("Robert G&ouml;ffringmann", "lavosspawn", "");
 	  end_section();
 
@@ -572,9 +572,9 @@ begin_credits("Credits");
     end_section();
 
     begin_section("Documentation");
-	begin_section("Documentation Manager");
-		add_person("Joachim Eberhard", "joachimeberhard", "");
-    	end_section();
+	begin_persons();
+		add_person("Joachim Eberhard", "joachimeberhard", "Documentation manager");
+    	end_persons();
     end_section();
 
     begin_section("Retired Team Members");
