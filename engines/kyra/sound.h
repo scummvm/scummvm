7d0435f23ce7cc19e007ff77b8dfb4bd0554c386
Johannes Schickel <lordhoto@scummvm.org>
2006-08-15 18:04:01 +0000

    - adding LPGL headers to adlib sound specific files - stating about dual license of adlib sound specific files
    svn-id: r23708

--- engines/kyra/sound.h
+++ engines/kyra/sound.h
@@ -1,5 +1,10 @@
 /* ScummVM - Scumm Interpreter
+ *
+ * This file is licensed under both GPL and LGPL
  * Copyright (C) 2004-2006 The ScummVM project
+ * Copyright (C) 2006 Torbjorn Andersson and Johannes Schickel
+ *
+ * GPL License
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -14,6 +19,22 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * LPGL License
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  * $URL$
  * $Id$
