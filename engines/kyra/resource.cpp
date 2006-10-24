0d357aab4d7add6011ee1fbc99c502820daf0278
Johannes Schickel <lordhoto@scummvm.org>
2006-10-24 00:30:48 +0000

    Fix for (reopened) bug #1582149 ("KYRA1: Crash when meeting Malcolm").
    svn-id: r24480

--- engines/kyra/resource.cpp
+++ engines/kyra/resource.cpp
@@ -72,9 +72,9 @@ Resource::Resource(KyraEngine *engine) {
 
 	if (_engine->game() == GI_KYRA1 && _engine->gameFlags().isTalkie) {
 		static const char *list[] = {
-			"ADL.PAK", "CHAPTER1.VRM", "COL.PAK", "DRAGON1.APK", "DRAGON2.APK", "FINALE.PAK",
-			"INTRO1.PAK", "INTRO2.PAK", "INTRO3.PAK", "INTRO4.PAK", "MISC.PAK",	"SND.PAK",
-			"STARTUP.PAK", "XMI.PAK", 0
+			"ADL.PAK", "CHAPTER1.VRM", "COL.PAK", "FINALE.PAK", "INTRO1.PAK", "INTRO2.PAK",
+			"INTRO3.PAK", "INTRO4.PAK", "MISC.PAK",	"SND.PAK", "STARTUP.PAK", "XMI.PAK",
+			"CAVE.APK", "DRAGON1.APK", "DRAGON2.APK", "LAGOON.APK", 0
 		};
 
 		for (int i = 0; list[i]; ++i) {
