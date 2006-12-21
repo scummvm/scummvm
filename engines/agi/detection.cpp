e6f95af215f226280ff7dca27c52181e604ce09a
Eugene Sandulenko <sev@scummvm.org>
2006-12-21 02:08:53 +0000

    Fix copyright and svn properties.
    svn-id: r24909

--- engines/agi/detection.cpp
+++ engines/agi/detection.cpp
@@ -1,8 +1,6 @@
 /* ScummVM - Scumm Interpreter
  * Copyright (C) 2006 The ScummVM project
  *
- * cinE Engine is (C) 2004-2005 by CinE Team
- *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
@@ -17,8 +15,8 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
- * $URL:$
- * $Id:$
+ * $URL$
+ * $Id$
  *
  */
 
