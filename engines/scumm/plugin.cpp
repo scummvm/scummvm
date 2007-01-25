d3b4e89e8003351aafdf6202c00a5db5d407ae09
Travis Howell <kirben@scummvm.org>
2007-01-25 09:54:47 +0000

    Add Dutch version of Freddi Fish and Luther's Maze Madness for Mac.
    svn-id: r25180

--- engines/scumm/plugin.cpp
+++ engines/scumm/plugin.cpp
@@ -625,6 +625,7 @@ static const GameFilenamePattern gameFilenamesTable[] = {
 	{ "lost", "Verloren", kGenHEMac, Common::NL_NLD, Common::kPlatformMacintosh, 0 },
 
 	{ "maze", "maze", kGenHEPC, UNK_LANG, UNK, 0 },
+	{ "maze", "Doolhof", kGenHEPC, Common::NL_NLD, UNK, 0 },
 	{ "maze", "Doolhof", kGenHEMac, Common::NL_NLD, Common::kPlatformMacintosh, 0 },
 	{ "maze", "Maze Madness", kGenHEMac, UNK_LANG, Common::kPlatformMacintosh, 0 },
 
