5e93000a752d1be20cb2f10d87c0d388c91e717c
Willem Jan Palenstijn <wjp@usecode.org>
2007-02-15 20:24:03 +0000

    fix compile error
    svn-id: r25612

--- engines/scumm/imuse_digi/dimuse_script.cpp
+++ engines/scumm/imuse_digi/dimuse_script.cpp
@@ -165,7 +165,7 @@ void IMuseDigital::flushTracks() {
 	debug(5, "flushTracks()");
 	for (int l = 0; l < MAX_DIGITAL_TRACKS + MAX_DIGITAL_FADETRACKS; l++) {
 		Track *track = _track[l];
-		if (track->used && (track->readyToRemove) {
+		if (track->used && (track->readyToRemove)) {
 			if (track->stream) {
 				if (!track->stream->endOfStream()) {
 	 				track->stream->finish();
