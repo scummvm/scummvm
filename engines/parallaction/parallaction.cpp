0f3ebbd3b371a900ce8d2d48d4f4b5eb68672933
Nicola Mettifogo <peres@scummvm.org>
2007-02-19 13:07:18 +0000

    table routines merged with common engine code
    svn-id: r25721

--- engines/parallaction/parallaction.cpp
+++ engines/parallaction/parallaction.cpp
@@ -22,6 +22,7 @@
 
 #include "parallaction/parallaction.h"
 #include "parallaction/menu.h"
+#include "parallaction/parser.h"
 #include "parallaction/disk.h"
 #include "parallaction/music.h"
 #include "parallaction/inventory.h"
@@ -1003,6 +1004,66 @@ void jobWaitRemoveJob(void *parm, Job *j) {
 	}
 
 	return;
+}
+
+
+void Parallaction::initTable(const char *path, char** table) {
+//	printf("initTable(%s)\n", path);
+
+	Common::File	stream;
+
+	if (!stream.open(path))
+		errorFileNotFound(path);
+
+	uint16 count = 0;
+
+	tableFillBuffers(stream);
+
+	while (scumm_stricmp(_tokens[0], "ENDTABLE")) {
+
+		table[count] = (char*)memAlloc(strlen(_tokens[0])+1);
+		strcpy(table[count], _tokens[0]);
+
+		count++;
+		tableFillBuffers(stream);
+	}
+
+	table[count] = NULL;
+
+	stream.close();
+
+	return;
+
+}
+
+void Parallaction::freeTable(char** table) {
+
+	uint16 count = 0;
+
+	while (table[count]) {
+
+		memFree(table[count]);
+		table[count] = NULL;
+
+		count++;
+	}
+
+	return;
+
+}
+
+int16 Parallaction::searchTable(const char *s, const char **table) {
+
+	int16 count = 0;
+
+	if (!s) return 0;
+
+	while (table[count]) {
+		if (!scumm_stricmp(table[count], s)) return count + 1;
+		count++;
+	}
+
+	return -1;
 }
 
 
