08d82864109807b5d5a2896bc261564b698731e7
Neil Millstone <agent-q@users.sourceforge.net>
2007-03-18 01:45:14 +0000

    adding key scanning functions compatible with ScummVM's IPC
    svn-id: r26188

---
+++ backends/platform/ds/arm9/source/keys.h
@@ -0,0 +1,13 @@
+
+
+
+namespace DS {
+
+void scanKeys(void);
+uint32 keysHeld(void);
+uint32 keysDown(void);
+uint32 keysDownRepeat(void);
+void keysSetRepeat( u8 setDelay, u8 setRepeat );
+uint32 keysUp(void);
+
+}
