24972223484ad8d5cec5ee16a5611b57f95d1477
Chris Apers <chrilith@users.sourceforge.net>
2007-05-01 10:23:11 +0000

    Test Cruise engine
    svn-id: r26706

--- backends/platform/PalmOS/Src/launcher/games.h
+++ backends/platform/PalmOS/Src/launcher/games.h
@@ -170,6 +170,7 @@ enum {
 	ENGINE_SCUMM,
 	ENGINE_AGI,
 	ENGINE_TOUCHE,
+	ENGINE_CRUISE,
 	ENGINE_COUNT
 };
 
@@ -184,13 +185,14 @@ static const struct {
 	{ "cine",			"Delphine Cinematique v1.0" },
 	{ "queen",			"Flight of the Amazon Queen" },
 	{ "lure",			"Lure of the Tempress" },
-	{ "gob",			"Gobliiins" },
+	{ "gob",			"Gobliiins, Bargon Attack and more ..." },
 	{ "kyra",			"Kyrandia" },
 	{ "parallaction",	"Parallaction" },
 	{ "saga",			"SAGA Engine" },
 	{ "scumm",			"Scumm Games" },
 	{ "agi",			"Sierra AGI" },
 	{ "touche",			"Touche: The Adventures of the Fifth Musketeer" },
+	{ "cruise",			"Cruise for a Corpse" },
 };
 
 
