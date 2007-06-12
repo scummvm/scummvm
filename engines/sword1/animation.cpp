12c9fb03820c383d5d10fa3c3ba8e2cffad8db98
Travis Howell <kirben@scummvm.org>
2007-06-12 06:08:47 +0000

    Add support for pausing/resume cutscenes shown on the OmniTV in The Feeble Files.
    svn-id: r27371

--- engines/sword1/animation.cpp
+++ engines/sword1/animation.cpp
@@ -410,7 +410,7 @@ bool MoviePlayerDXA::load(uint32 id) {
 	snprintf(filename, sizeof(filename), "%s.dxa", sequenceList[id]);
 	if (loadFile(filename)) {
 		// The Broken Sword games always use external audio tracks.
-		if (_fd.readUint32BE() != MKID_BE('NULL'))
+		if (_fd->readUint32BE() != MKID_BE('NULL'))
 			return false;
 		_frameWidth = getWidth();
 		_frameHeight = getHeight();
