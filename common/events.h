8a658e01878150f0163c91f88ab0747294a740be
Max Horn <max@quendi.de>
2007-06-22 20:33:51 +0000

    Added 'ASCII_' enum values for certain standard keys, and update comment
    svn-id: r27619

--- common/events.h
+++ common/events.h
@@ -217,25 +217,31 @@ enum KeyCode {
 };
 
 /**
- * List of fake 'ascii' values used in keyboard events.
- * The values here are based on what certain SCUMM games require
- * in their scripts.
- * @todo Get rid of this, and instead enforce that engines use the
- * keycode instead to handle these.
+ * List of certan special and some fake 'ascii' values used in keyboard events.
+ * The values for the function keys listed here are based on what certain SCUMM
+ * games expect in their scripts.
+ * @todo Get rid of the function key values, and instead enforce that engines use
+ * the keycode value to handle these.
  */
 enum {
-	ASCII_F1 = 315,
-	ASCII_F2 = 316,
-	ASCII_F3 = 317,
-	ASCII_F4 = 318,
-	ASCII_F5 = 319,
-	ASCII_F6 = 320,
-	ASCII_F7 = 321,
-	ASCII_F8 = 322,
-	ASCII_F9 = 323,
-	ASCII_F10 = 324,
-	ASCII_F11 = 325,
-	ASCII_F12 = 326
+	ASCII_BACKSPACE     = 8,
+	ASCII_TAB           = 9,
+	ASCII_RETURN        = 13,
+	ASCII_ESCAPE        = 27,
+	ASCII_SPACE         = 32,
+
+	ASCII_F1            = 315,
+	ASCII_F2            = 316,
+	ASCII_F3            = 317,
+	ASCII_F4            = 318,
+	ASCII_F5            = 319,
+	ASCII_F6            = 320,
+	ASCII_F7            = 321,
+	ASCII_F8            = 322,
+	ASCII_F9            = 323,
+	ASCII_F10           = 324,
+	ASCII_F11           = 325,
+	ASCII_F12           = 326
 };
 
 /**
