4f8f14d14e54362015c219a1c23ebaf7ffb37ae4
Travis Howell <kirben@scummvm.org>
2007-07-01 12:52:46 +0000

    Correct md5 checksum for restart state file in the French DOS version of Elvira 2.
    svn-id: r27821

--- engines/agos/intern.h
+++ engines/agos/intern.h
@@ -32,7 +32,7 @@ enum ChildType {
 	kRoomType = 1,
 	kObjectType = 2,
 	kPlayerType = 3,
-	kGenExitType = 4,   // Elira 1 specifc
+	kGenExitType = 4,   // Elvira 1 specifc
 	kSuperRoomType = 4, // Elvira 2 specic
 
 	kContainerType = 7,
