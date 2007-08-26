c51fb71f05db52735e6133915a875de8d2f6e1b2
Filippos Karapetis <bluegr@gmail.com>
2007-08-26 03:24:39 +0000

    The music table loaded for chapter 6 (final chapter) in IHNM is problematic, so don't load any music for that chapter until the cause is found
    svn-id: r28743

--- engines/saga/sfuncs.cpp
+++ engines/saga/sfuncs.cpp
@@ -1676,6 +1676,12 @@ void Script::sfPlayMusic(SCRIPTFUNC_PARAMS) {
 			warning("sfPlayMusic: Wrong song number (%d > %d)", param1, _vm->_music->_songTableLen - 1);
 		} else {
 			_vm->_music->setVolume(_vm->_musicVolume == 10 ? -1 : _vm->_musicVolume * 25, 1);
+			// HACK for chapter 6 (last chapter) in IHNM. For some reason, the songtable loaded is
+			// incorrect, and the game crashes here when trying to load a music track there. For now,
+			// just don't change the music track for chapter 6
+			// FIXME: Figure out what's wrong with the loaded music track and remove this hack
+			if (_vm->getGameType() == GType_IHNM && _vm->_scene->currentChapterNumber() == 6)
+				return;
 			_vm->_music->play(_vm->_music->_songTable[param1], param2 ? MUSIC_LOOP : MUSIC_NORMAL);
 			if (!_vm->_scene->haveChapterPointsChanged()) {
 				_vm->_scene->setCurrentMusicTrack(param1);
