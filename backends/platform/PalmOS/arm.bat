19078098ebf932f16ec96e136b8dde9cb0572b1b
Chris Apers <chrilith@users.sourceforge.net>
2007-09-01 19:40:35 +0000

    CW doesn't support ASM source file, this bat is required to execute an external compiler and add object code to CW project
    svn-id: r28806

---
+++ backends/platform/PalmOS/arm.bat
@@ -0,0 +1,3 @@
+as.exe ../../../sound/rate_arm_asm.s -o Obj/rate_arm_asm.o
+as.exe ../../../engines/scumm/gfxARM.s -o Obj/gfxARM.o
+as.exe ../../../engines/scumm/smush/codec47ARM.s -o Obj/codec47ARM.o
