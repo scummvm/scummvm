924a9074fbedadd9b1d402d71899c6bc9d45bcb1
Torbjörn Andersson <eriktorbjorn@users.sourceforge.net>
2007-09-22 07:49:51 +0000

    Set svn:keywords
    svn-id: r29013

--- tools/create_kyradat/util.c
+++ tools/create_kyradat/util.c
@@ -15,8 +15,8 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
- * $URL: https://scummvm.svn.sourceforge.net/svnroot/scummvm/tools/trunk/util.c $
- * $Id: util.c 23136 2006-06-15 14:34:46Z h00ligan $
+ * $URL$
+ * $Id$
  *
  */
 
