94cd7dd04a061f0c256ae3ade78bfe3cb5507dcf
Johannes Schickel <lordhoto@scummvm.org>
2007-09-29 11:40:31 +0000

    Added a hack for the tools to allow building on MinGW with SDL backend. (see bug #1800764 "TOOLS: MinGW tools building broken")
    svn-id: r29131

--- tools/create_kyradat/create_kyradat.cpp
+++ tools/create_kyradat/create_kyradat.cpp
@@ -20,6 +20,12 @@
  *
  */
 
+// HACK to allow building with the SDL backend on MinGW
+// see bug #1800764 "TOOLS: MinGW tools building broken"
+#ifdef main
+#undef main
+#endif // main
+
 #include "create_kyradat.h"
 
 #include "md5.h"
--- tools/create_lure/create_lure_dat.cpp
+++ tools/create_lure/create_lure_dat.cpp
@@ -27,6 +27,12 @@
  * to work properly
  */
 
+// HACK to allow building with the SDL backend on MinGW
+// see bug #1800764 "TOOLS: MinGW tools building broken"
+#ifdef main
+#undef main
+#endif // main
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
