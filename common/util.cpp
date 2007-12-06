d3a9cfc2cbcbdc8e8676bffb64df1ac9b6f2f16e
Filippos Karapetis <bluegr@gmail.com>
2007-12-06 18:48:44 +0000

    Reverted commit #29733, as RandomSource() is called before g_system is initialized (e.g. from sound/fmopl.cpp)
    svn-id: r29736

--- common/util.cpp
+++ common/util.cpp
@@ -154,8 +154,11 @@ void hexdump(const byte * data, int len, int bytesPerLine) {
 RandomSource::RandomSource() {
 	// Use system time as RNG seed. Normally not a good idea, if you are using
 	// a RNG for security purposes, but good enough for our purposes.
-	assert(g_system);
-	uint32 seed = g_system->getMillis();
+#if defined (__SYMBIAN32__) && defined (__WINS__)
+	uint32 seed = 0; // Symbian produces RT crash on time(0)
+#else
+	uint32 seed = time(0);
+#endif
 	setSeed(seed);
 }
 
