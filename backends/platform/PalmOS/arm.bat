9b77034daf75a32d2162c0ab1d7e97a286fd7ced
Chris Apers <chrilith@users.sourceforge.net>
2007-12-15 12:01:01 +0000

    Added missing symbol definition
    svn-id: r29861

--- backends/platform/PalmOS/arm.bat
+++ backends/platform/PalmOS/arm.bat
@@ -1,3 +1,3 @@
-as.exe ../../../sound/rate_arm_asm.s -o Obj/rate_arm_asm.o
-as.exe ../../../engines/scumm/gfxARM.s -o Obj/gfxARM.o
-as.exe ../../../engines/scumm/smush/codec47ARM.s -o Obj/codec47ARM.o
+as.exe --defsym PALMOS_MODE=1 ../../../sound/rate_arm_asm.s -o Obj/rate_arm_asm.o
+as.exe --defsym PALMOS_MODE=1 ../../../engines/scumm/gfxARM.s -o Obj/gfxARM.o
+as.exe --defsym PALMOS_MODE=1 ../../../engines/scumm/smush/codec47ARM.s -o Obj/codec47ARM.o
