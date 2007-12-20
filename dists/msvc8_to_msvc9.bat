cd230a4ae88b877485242ef0dd9ba17e89a0552e
Filippos Karapetis <bluegr@gmail.com>
2007-12-20 13:23:53 +0000

    Added batch files to convert MSVC9 project files to MSVC8 and vice versa
    svn-id: r29914

---
+++ dists/msvc8_to_msvc9.bat
@@ -0,0 +1,22 @@
+@echo off
+if not exist rpl.exe goto no_rpl
+
+echo Creating MSVC9 project files from the MSVC8 ones
+copy /y msvc8\*.vcproj msvc9\
+copy /y msvc8\*.sln msvc9\
+rpl -e -q "Version=\"8,00\"" "Version=\"9,00\"" msvc9\*.vcproj
+rpl -e -q "Keyword=\"Win32Proj\"" "Keyword=\"Win32Proj\"\n\tTargetFrameworkVersion=\"131072\"" msvc9\*.vcproj
+rpl -e -q "EntryPointSymbol=\"WinMainCRTStartup\"" "EntryPointSymbol=\"WinMainCRTStartup\"\n\t\t\t\tRandomizedBaseAddress=\"1\"\n\t\t\t\tDataExecutionPrevention=\"0\"" msvc9\scummvm.vcproj
+rpl -e -q "Format Version 9.00" "Format Version 10.00" msvc9\scummvm.sln
+rpl -e -q "# Visual C++ Express 2005" "# Visual C++ Express 2008" msvc9\scummvm.sln
+rpl -e -q "# Visual Studio 2005" "# Visual Studio 2008" msvc9\scummvm.sln
+goto the_end
+
+:no_rpl
+echo You need the Windows version of GNU rpl
+echo Get it here:
+echo http://gnuwin32.sourceforge.net/packages/rpl.htm
+echo Place rpl.exe from the bin folder inside the archive in the folder where
+echo this batch file resides
+
+:the_end
---
+++ dists/msvc9_to_msvc8.bat
@@ -0,0 +1,23 @@
+@echo off
+if not exist rpl.exe goto no_rpl
+
+echo Creating MSVC8 project files from the MSVC9 ones
+copy /y msvc9\*.vcproj msvc8\
+copy /y msvc9\*.sln msvc8\
+rpl -e -q "Version=\"9,00\"" "Version=\"8,00\"" msvc8\*.vcproj
+rpl -e -q "\tTargetFrameworkVersion=\"131072\"\n" "" msvc8\*.vcproj
+rpl -e -q "\t\t\t\tRandomizedBaseAddress=\"1\"\n" "" msvc8\scummvm.vcproj
+rpl -e -q "\t\t\t\tDataExecutionPrevention=\"0\"\n" "" msvc8\scummvm.vcproj
+rpl -e -q "Format Version 10.00" "Format Version 9.00" msvc8\scummvm.sln
+rpl -e -q "# Visual C++ Express 2008" "# Visual C++ Express 2005" msvc8\scummvm.sln
+rpl -e -q "# Visual Studio 2008" "# Visual Studio 2005" msvc8\scummvm.sln
+goto the_end
+
+:no_rpl
+echo You need the Windows version of GNU rpl
+echo Get it here:
+echo http://gnuwin32.sourceforge.net/packages/rpl.htm
+echo Place rpl.exe from the bin folder inside the archive in the folder where
+echo this batch file resides
+
+:the_end
