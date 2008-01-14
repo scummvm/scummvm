6992238026e7459207b83b280e1420a7eea4ab0c
Paweł Kołodziejski <aquadran@scummvm.org>
2008-01-14 20:35:58 +0000

    experimental force flush fade track if it's in use
    svn-id: r30487

--- engines/scumm/imuse_digi/dimuse_track.cpp
+++ engines/scumm/imuse_digi/dimuse_track.cpp
@@ -328,8 +328,8 @@ IMuseDigital::Track *IMuseDigital::cloneToFadeOutTrack(Track *track, int fadeDel
 	}
 
 	if (_track[track->trackId + MAX_DIGITAL_TRACKS]->used) {
-		warning("IMuseDigital::cloneToFadeOutTrack: No free fade track");
-		return NULL;
+		warning("IMuseDigital::cloneToFadeOutTrack: No free fade track, force flush");
+		flushTrack(_track[track->trackId + MAX_DIGITAL_TRACKS]);
 	}
 
 	fadeTrack = _track[track->trackId + MAX_DIGITAL_TRACKS];
