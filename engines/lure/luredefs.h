3a0f7b4ea89280c32f30dbec33d4bd6ae72f99b6
Paul Gilbert <dreammaster@scummvm.org>
2008-01-17 09:50:42 +0000

    Updated required lure.dat version number
    svn-id: r30526

--- engines/lure/luredefs.h
+++ engines/lure/luredefs.h
@@ -34,7 +34,7 @@ namespace Lure {
 
 #define SUPPORT_FILENAME "lure.dat"
 #define LURE_DAT_MAJOR 1
-#define LURE_DAT_MINOR 28
+#define LURE_DAT_MINOR 29
 #define LURE_MIN_SAVEGAME_MINOR 25
 #define LURE_SAVEGAME_MINOR 31
 
