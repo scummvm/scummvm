213ac50740779cae168fa0fd31087bb36466bf29
Johannes Schickel <lordhoto@scummvm.org>
2008-04-06 15:05:12 +0000

    Oops. that shouldn't have been reverted by last committ.
    svn-id: r31433

--- engines/kyra/kyra_v2.cpp
+++ engines/kyra/kyra_v2.cpp
@@ -183,8 +183,6 @@ Movie *KyraEngine_v2::createWSAMovie() {
 int KyraEngine_v2::init() {
 	_screen = new Screen_v2(this, _system);
 	assert(_screen);
-	if (!_screen->init())
-		error("_screen->init() failed");
 
 	KyraEngine::init();
 	initStaticResource();
