9e4115a3bf5c2d2004f5876a5c9b6b62c2706630
Christopher Page <cpage88@users.sourceforge.net>
2008-05-22 23:40:36 +0000

    AGOS: Fixed a memory leak from earlier in a better way
    svn-id: r32219

--- engines/agos/icons.cpp
+++ engines/agos/icons.cpp
@@ -534,8 +534,6 @@ void AGOSEngine::drawIconArray(uint num, Item *itemRef, int line, int classMask)
 		window->iconPtr->upArrow = _scrollUpHitArea;
 		window->iconPtr->downArrow = _scrollDownHitArea;
 	}
-
-	free (window->iconPtr);
 }
 
 uint AGOSEngine_Feeble::setupIconHitArea(WindowBlock *window, uint num, uint x, uint y, Item *item_ptr) {
