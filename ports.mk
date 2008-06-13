c78cc2c936d4ae6113ce0c0203fc0219cde5a69f
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2008-06-13 12:29:57 +0000

    adopted copyright stuff

--- ports.mk
+++ ports.mk
@@ -13,19 +13,16 @@ install: all
 	$(INSTALL) -c -s -m 755 "$(srcdir)/residual$(EXEEXT)" "$(DESTDIR)$(BINDIR)/residual$(EXEEXT)"
 	#$(INSTALL) -d "$(DESTDIR)$(MANDIR)/man6/"
 	#$(INSTALL) -c -m 644 "$(srcdir)/dists/residual.6" "$(DESTDIR)$(MANDIR)/man6/residual.6"
-	#$(INSTALL) -d "$(DESTDIR)$(PREFIX)/share/pixmaps/"
+	$(INSTALL) -d "$(DESTDIR)$(PREFIX)/share/pixmaps/"
 	#$(INSTALL) -c -m 644 "$(srcdir)/icons/residual.xpm" "$(DESTDIR)$(PREFIX)/share/pixmaps/residual.xpm"
-	#$(INSTALL) -d "$(DESTDIR)$(PREFIX)/share/doc/residual/"
-	$(INSTALL) -c -m 644 "$(srcdir)/AUTHORS" "$(srcdir)/COPYING" "$(srcdir)/COPYRIGHT" "$(srcdir)/NEWS" "$(srcdir)/README" "$(DESTDIR)$(PREFIX)/share/doc/residual/"
-	$(INSTALL) -d "$(DESTDIR)$(DATADIR)/residual/"
-	$(INSTALL) -c -m 644 $(DIST_FILES_THEMES) $(DIST_FILES_ENGINEDATA) "$(DESTDIR)$(DATADIR)/residual/"
+	$(INSTALL) -d "$(DESTDIR)$(PREFIX)/share/doc/residual/"
+	$(INSTALL) -c -m 644 "$(srcdir)/AUTHORS" "$(srcdir)/COPYING.LGPL" "$(srcdir)/COPYING.GPL" "$(srcdir)/NEWS" "$(srcdir)/README" "$(srcdir)/TODO""$(DESTDIR)$(PREFIX)/share/doc/residual/"
 
 uninstall:
 	rm -f "$(DESTDIR)$(BINDIR)/residual$(EXEEXT)"
 	#rm -f "$(DESTDIR)$(MANDIR)/man6/residual.6"
 	#rm -f "$(DESTDIR)$(PREFIX)/share/pixmaps/residual.xpm"
-	#rm -rf "$(DESTDIR)$(PREFIX)/share/doc/residual/"
-	#rm -rf "$(DESTDIR)$(DATADIR)/residual/"
+	rm -rf "$(DESTDIR)$(PREFIX)/share/doc/residual/"
 
 deb:
 	ln -sf dists/debian;
@@ -41,9 +38,6 @@ bundle: scummvm-static $(srcdir)/dists/macosx/Info.plist
 	echo "APPL????" > $(bundle_name)/Contents/PkgInfo
 	cp $(srcdir)/dists/macosx/Info.plist $(bundle_name)/Contents/
 	cp $(srcdir)/icons/residual.icns $(bundle_name)/Contents/Resources/
-	cp $(srcdir)/dists/pred.dic $(bundle_name)/Contents/Resources/
-	cp $(DIST_FILES_THEMES) $(bundle_name)/Contents/Resources/
-	cp $(DIST_FILES_ENGINEDATA) $(bundle_name)/Contents/Resources/
 	$(srcdir)/tools/credits.pl --rtf > $(bundle_name)/Contents/Resources/Credits.rtf
 	chmod 644 $(bundle_name)/Contents/Resources/*
 	cp residual-static $(bundle_name)/Contents/MacOS/residual
@@ -53,9 +47,6 @@ bundle: scummvm-static $(srcdir)/dists/macosx/Info.plist
 iphonebundle: $(srcdir)/dists/iphone/Info.plist
 	mkdir -p $(bundle_name)
 	cp $(srcdir)/dists/iphone/Info.plist $(bundle_name)/
-	cp $(srcdir)/dists/pred.dic $(bundle_name)/
-	cp $(DIST_FILES_THEMES) $(bundle_name)/
-	cp $(DIST_FILES_ENGINEDATA) $(bundle_name)/
 	cp residual $(bundle_name)/Residual
 	cp $(srcdir)/dists/iphone/icon.png $(bundle_name)/icon.png
 	cp $(srcdir)/dists/iphone/Default.png $(bundle_name)/Default.png
@@ -120,9 +111,10 @@ iphone: $(OBJS)
 osxsnap: bundle credits
 	mkdir Residual-snapshot
 	cp $(srcdir)/AUTHORS ./Residual-snapshot/Authors
-	cp $(srcdir)/COPYING ./Residual-snapshot/License
-	cp $(srcdir)/COPYRIGHT ./Residual-snapshot/Copyright\ Holders
+	cp $(srcdir)/COPYING.LGPL ./Residual-snapshot/License.LGPL
+	cp $(srcdir)/COPYING.GPL ./Residual-snapshot/License.GPL
 	cp $(srcdir)/NEWS ./Residual-snapshot/News
+	cp $(srcdir)/TODO ./Residual-snapshot/Todo
 	cp $(srcdir)/README ./Residual-snapshot/Residual\ ReadMe
 	/Developer/Tools/SetFile -t ttro -c ttxt ./Residual-snapshot/*
 	/Developer/Tools/CpMac -r $(bundle_name) ./Residual-snapshot/
@@ -147,11 +139,12 @@ residualico.o: $(srcdir)/icons/residual.ico
 win32dist: residual$(EXEEXT)
 	mkdir -p $(WIN32PATH)
 	strip residual.exe -o $(WIN32PATH)/residual$(EXEEXT)
-	#cp $(srcdir)/AUTHORS $(WIN32PATH)/AUTHORS.txt
-	cp $(srcdir)/COPYING $(WIN32PATH)/COPYING.txt
-	#cp $(srcdir)/COPYRIGHT $(WIN32PATH)/COPYRIGHT.txt
-	#cp $(srcdir)/NEWS $(WIN32PATH)/NEWS.txt
+	cp $(srcdir)/AUTHORS $(WIN32PATH)/AUTHORS.txt
+	cp $(srcdir)/COPYING.LGPL $(WIN32PATH)/COPYING_LGPL.txt
+	cp $(srcdir)/COPYING.GPL $(WIN32PATH)/COPYING_GPL.txt
+	cp $(srcdir)/NEWS $(WIN32PATH)/NEWS.txt
 	cp $(srcdir)/README $(WIN32PATH)/README.txt
+	cp $(srcdir)/TODO $(WIN32PATH)/TODO.txt
 	cp /usr/local/README-SDL.txt $(WIN32PATH)
 	cp /usr/local/bin/SDL.dll $(WIN32PATH)
 	u2d $(WIN32PATH)/*.txt
@@ -167,11 +160,12 @@ aos4dist: residual
 	cp icons/residual.info $(AOS4PATH)/$<_SVN.info
 	cp $(DIST_FILES_THEMES) $(AOS4PATH)/themes/
 	cp $(DIST_FILES_ENGINEDATA) $(AOS4PATH)/extras/
-	#cp $(srcdir)/AUTHORS $(AOS4PATH)/AUTHORS.txt
-	cp $(srcdir)/COPYING $(AOS4PATH)/COPYING.txt
-	#cp $(srcdir)/COPYRIGHT $(AOS4PATH)/COPYRIGHT.txt
-	#cp $(srcdir)/NEWS $(AOS4PATH)/NEWS.txt
+	cp $(srcdir)/AUTHORS $(AOS4PATH)/AUTHORS.txt
+	cp $(srcdir)/COPYING.LGPL $(AOS4PATH)/COPYING_LGPL.txt
+	cp $(srcdir)/COPYING.GPL $(AOS4PATH)/COPYING_GPL.txt
+	cp $(srcdir)/NEWS $(AOS4PATH)/NEWS.txt
 	cp $(srcdir)/README $(AOS4PATH)/README.txt
+	cp $(srcdir)/TODO $(AOS4PATH)/TODO.txt
 	cp /sdk/local/documentation/SDL-1.2.9/README-SDL.txt $(AOS4PATH)
 
 .PHONY: deb bundle osxsnap win32dist install uninstall
