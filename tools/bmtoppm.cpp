1ffbab7d3c3e5e7d1737969d51d6a48b3550333f
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2008-06-15 13:00:53 +0000

    updated copyright headers

--- tools/bmtoppm.cpp
+++ tools/bmtoppm.cpp
@@ -1,10 +1,8 @@
 /* Residual - Virtual machine to run LucasArts' 3D adventure games
- * Copyright (C) 2003-2006 The ScummVM-Residual Team (www.scummvm.org)
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * Residual is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the AUTHORS
+ * file distributed with this source distribution.
 
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
