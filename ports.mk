a324441df5454fe7db3c270c1061dbcf329c5e3a
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2008-06-16 06:41:23 +0000

    enabled support for icons

--- ports.mk
+++ ports.mk
@@ -132,7 +132,7 @@ osxsnap: bundle credits
 # Windows specific
 #
 
-residualico.o: 
+residualico.o: $(srcdir)/icons/residual.ico
 	$(WINDRES) -I$(srcdir) $(srcdir)/dists/residual.rc residualico.o
 
 # Special target to create a win32 snapshot binary
