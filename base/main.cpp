c67b91437673e070119c782a33b070083c3d7f27
Max Horn <max@quendi.de>
2008-06-22 10:26:18 +0000

    Disabled return to launcher on trunk again
    svn-id: r32745

--- base/main.cpp
+++ base/main.cpp
@@ -228,9 +228,7 @@ static int runGame(const EnginePlugin *plugin, OSystem &system, const Common::St
 	// Reset the file/directory mappings
 	Common::File::resetDefaultDirectories();
 
-
-
-    return 1;   // Changing from return 0, in order to return to launcher
+	return 0;
 }
 
 
@@ -278,6 +276,7 @@ extern "C" int scummvm_main(int argc, char *argv[]) {
 
 	// Load the plugins.
 	PluginManager::instance().loadPlugins();
+	EngineMan.getPlugins();
 
 	// Process the remaining command line settings. Must be done after the
 	// config file and the plugins have been loaded.
