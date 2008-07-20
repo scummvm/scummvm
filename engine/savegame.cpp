a7d3c58fe47acb6813a0add4b8eb116deccd6726
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2008-07-20 16:06:42 +0000

    set to 1 version

--- engine/savegame.cpp
+++ engine/savegame.cpp
@@ -32,7 +32,7 @@ Common::SaveFileManager *g_saveFileMan;
 
 #define SAVEGAME_HEADERTAG	'RSAV'
 #define SAVEGAME_FOOTERTAG	'ESAV'
-#define SAVEGAME_VERSION		2
+#define SAVEGAME_VERSION		1
 
 // Constructor. Should create/open a saved game
 SaveGame::SaveGame(const char *filename, bool saving) :
