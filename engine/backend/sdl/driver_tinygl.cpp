0c1da3ed3a7e1c04cb200fa43550ae149240b664
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2008-07-20 10:02:13 +0000

    fixed warning

--- engine/backend/sdl/driver_tinygl.cpp
+++ engine/backend/sdl/driver_tinygl.cpp
@@ -586,7 +586,7 @@ void DriverTinyGL::drawEmergString(int x, int y, const char *text, const Color &
 	uint16 color = ((fgColor.red() & 0xF8) << 8) | ((fgColor.green() & 0xFC) << 3) | (fgColor.blue() >> 3);
 
 	for (int l = 0; l < (int)strlen(text); l++) {
-		char c = text[l];
+		int c = text[l];
 		assert(c >= 32 && c <= 127);
 		const uint8 *ptr = Font::emerFont[c - 32];
 		for (int py = 0; py < 13; py++) {
