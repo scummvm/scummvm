1d689497d73efeed9eebcee51fc09b4ca0f92e24
Max Horn <max@quendi.de>
2008-08-05 21:27:19 +0000

    Backport of patch #2014663: Do not mark stack as executable in HQx scaler
    svn-id: r33646

--- graphics/scaler/hq2x_i386.asm
+++ graphics/scaler/hq2x_i386.asm
@@ -1840,3 +1840,8 @@ FuncTable2:
     dd ..@cross8, ..@flag0,  ..@flag0,  ..@flag0,
     dd ..@flag0,  ..@flag0,  ..@flag0,  ..@flag0
 
+
+%ifidn __OUTPUT_FORMAT__,elf
+section .note.GNU-stack noalloc noexec nowrite progbits
+%endif
+
