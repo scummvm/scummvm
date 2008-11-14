bfc9b972f53f92fde5b803fe24e27bcaa6e3fb26
Torbjörn Andersson <eriktorbjorn@users.sourceforge.net>
2008-11-14 22:48:09 +0000

    Added license header, and guard against multiple inclusion.
    (Sorry about the confusing commit message earlier. I thought I had committed a
    fix for bug #2284199 ("IGOR: Strange code in drawInventory()") without checking
    for warnings first.)
    
    svn-id: r35065

--- engines/igor/resource_ids.h
+++ engines/igor/resource_ids.h
@@ -1,3 +1,31 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * $URL$
+ * $Id$
+ *
+ */
+
+#ifndef IGOR_RESOURCE_IDS
+#define IGOR_RESOURCE_IDS
+
 #define FNT_Dialogue 1
 #define ANM_PhilipVodka 2
 #define AOF_PhilipVodka 3
@@ -446,3 +474,5 @@
 #define STR_SharewarePenduloAddress4 446
 #define STR_SharewarePenduloAddress5 447
 #define STR_SharewarePenduloAddress6 448
+
+#endif
