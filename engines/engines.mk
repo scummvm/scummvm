8467dc73612a6809ab4ee21fb2f2cd3d5eef4271
Eugene Sandulenko <sev@scummvm.org>
2009-01-02 18:01:19 +0000

    More configure code for additional SAGA engine features.
    svn-id: r35676

--- engines/engines.mk
+++ engines/engines.mk
@@ -85,6 +85,15 @@ endif
 ifdef ENABLE_SAGA
 DEFINES += -DENABLE_SAGA=$(ENABLE_SAGA)
 MODULES += engines/saga
+
+ifdef ENABLE_IHNM
+DEFINES += -DENABLE_IHNM
+endif
+
+ifdef ENABLE_SAGA2
+DEFINES += -DENABLE_SAGA2
+endif
+
 endif
 
 ifdef ENABLE_SKY
