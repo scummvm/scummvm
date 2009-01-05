d104e6fe3358124f4517c6fc5512bc966182b949
Max Horn <max@quendi.de>
2009-01-05 20:12:25 +0000

    Workaround issue in Graphics::Surface code on Haiku
    svn-id: r35745

--- graphics/surface.h
+++ graphics/surface.h
@@ -47,7 +47,7 @@ struct Surface {
 	Surface() : w(0), h(0), pitch(0), pixels(0), bytesPerPixel(0) {}
 
 	inline const void *getBasePtr(int x, int y) const {
-		return static_cast<const byte *>(pixels) + y * pitch + x * bytesPerPixel;
+		return (const byte *)(pixels) + y * pitch + x * bytesPerPixel;
 	}
 
 	inline void *getBasePtr(int x, int y) {
