21c0632b94f1418b24bd052003b46799e63e71a9
Travis Howell <kirben@scummvm.org>
2009-03-01 10:16:28 +0000

    Add DOS demo of Elvira 1.
    svn-id: r39027

--- engines/agos/agos.cpp
+++ engines/agos/agos.cpp
@@ -1004,7 +1004,8 @@ Common::Error AGOSEngine::go() {
 
 	}
 
-	if (getGameType() == GType_ELVIRA1 && getFeatures() & GF_DEMO) {
+	if (getGameType() == GType_ELVIRA1 && getPlatform() == Common::kPlatformAmiga &&
+		(getFeatures() & GF_DEMO)) {
 		playMusic(0, 0);
 	}
 
