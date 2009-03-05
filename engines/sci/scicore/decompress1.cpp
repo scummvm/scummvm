2017d1c9ea12968e80a7aaeed1d9289f39c3a96e
Sven Hesse <drmccoy@users.sourceforge.net>
2009-03-05 19:43:12 +0000

    Added a missing NULLing
    svn-id: r39134

--- engines/sci/scicore/decompress1.cpp
+++ engines/sci/scicore/decompress1.cpp
@@ -306,6 +306,7 @@ int decompress1(Resource *result, Common::ReadStream &stream, int sci_version) {
 	if (stream.read(buffer, compressedLength) != compressedLength) {
 		free(result->data);
 		free(buffer);
+		result->data = 0;
 		return SCI_ERROR_IO_ERROR;
 	};
 
