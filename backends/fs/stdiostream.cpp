344a4f5199d50127bb7e66dd1a6a69b4c205d30e
Max Lingua <sunmax@users.sourceforge.net>
2009-03-06 00:26:48 +0000

    Added more ps2_* pseudo-posix entries for PS2 backend in fs/stdiostream.cpp to properly support eof / clearerr.
    svn-id: r39143

--- backends/fs/stdiostream.cpp
+++ backends/fs/stdiostream.cpp
@@ -40,19 +40,21 @@
 
 	#define fopen(a, b)			ps2_fopen(a, b)
 	#define fclose(a)			ps2_fclose(a)
-	#define fseek(a, b, c)			ps2_fseek(a, b, c)
+	#define fseek(a, b, c)		ps2_fseek(a, b, c)
 	#define ftell(a)			ps2_ftell(a)
 	#define feof(a)				ps2_feof(a)
-	#define fread(a, b, c, d)		ps2_fread(a, b, c, d)
-	#define fwrite(a, b, c, d)		ps2_fwrite(a, b, c, d)
+	#define fread(a, b, c, d)	ps2_fread(a, b, c, d)
+	#define fwrite(a, b, c, d)	ps2_fwrite(a, b, c, d)
 
-	//#define fprintf				ps2_fprintf	// used in common/util.cpp
-	//#define fflush(a)			ps2_fflush(a)	// used in common/util.cpp
+	#define fprintf				ps2_fprintf  // used in common/util.cpp
+	#define fflush(a)			ps2_fflush(a)  // used in common/util.cpp
+	#define ferror(a)			ps2_ferror(a)
+	#define clearerr(a)			ps2_clearerr(a)
 
 	//#define fgetc(a)			ps2_fgetc(a)	// not used
-	//#define fgets(a, b, c)			ps2_fgets(a, b, c)	// not used
-	//#define fputc(a, b)			ps2_fputc(a, b)	// not used
-	//#define fputs(a, b)			ps2_fputs(a, b)	// not used
+	//#define fgets(a, b, c)	ps2_fgets(a, b, c)	// not used
+	//#define fputc(a, b)		ps2_fputc(a, b)	// not used
+	//#define fputs(a, b)		ps2_fputs(a, b)	// not used
 
 	//#define fsize(a)			ps2_fsize(a)	// not used -- and it is not a standard function either
 #endif
