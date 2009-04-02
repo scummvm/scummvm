8ae0ed17061a98233a26c4efe70d4bf2f27d89d8
Filippos Karapetis <bluegr@gmail.com>
2009-04-02 19:37:56 +0000

    Applied fingolfin's suggestion to remove the roundf() implementation and make the code in that bit simpler and more readable (since points are always positive)
    svn-id: r39802

--- engines/sci/engine/kpathing.cpp
+++ engines/sci/engine/kpathing.cpp
@@ -77,15 +77,13 @@ enum {
 	PF_FATAL = -2
 };	
 
-#define scummvm_roundf(a) ((fmod(a, 1) < 0.5) ? floor(a) : ceil(a))
-
 // Floating point struct
 struct FloatPoint {
 	FloatPoint() : x(0), y(0) {}
 	FloatPoint(float x_, float y_) : x(x_), y(y_) {}
 
 	Common::Point toPoint() {
-		return Common::Point((int16)scummvm_roundf(x), (int16)scummvm_roundf(y));
+		return Common::Point(x + 0.5, y + 0.5);
 	}
 
 	float x, y;
