f7dfd85663f55b2a1be694f4728c127b99f01cd6
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2009-04-04 15:16:42 +0000

    applied patch #2728035

--- engine/model.cpp
+++ engine/model.cpp
@@ -34,6 +34,7 @@
 #include "engine/backend/platform/driver.h"
 
 #include <cstring>
+#include <cstdio>
 
 Model::Model(const char *filename, const char *data, int len, const CMap &cmap) :
 		Resource(filename), _numMaterials(0), _numGeosets(0) {
