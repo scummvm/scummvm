01cf7c412b7560f6e0981c3865892ba1ad79dccc
Travis Howell <kirben@scummvm.org>
2009-04-04 06:12:36 +0000

    Fix regression in HE games re-releases.
    svn-id: r39831

--- engines/scumm/akos.cpp
+++ engines/scumm/akos.cpp
@@ -303,7 +303,7 @@ void AkosRenderer::setPalette(byte *new_palette) {
 	if (_vm->_game.heversion >= 99 && _paletteNum) {
 		for (i = 0; i < size; i++)
 			_palette[i] = (byte)_vm->_hePalettes[_paletteNum * 1024 + 768 + akpl[i]];
-	} else if (_vm->_game.heversion >= 99 && rgbs) {
+	} else if ((_vm->_game.features & GF_16BIT_COLOR) && rgbs) {
 		for (i = 0; i < size; i++) {
 			if (new_palette[i] == 0xFF) {
 				uint8 col = akpl[i];
