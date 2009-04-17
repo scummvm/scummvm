3723b117911178b933f458a67b22efd98ae65b6f
Max Lingua <sunmax@users.sourceforge.net>
2009-04-17 19:57:06 +0000

    Patched 0.13.1 to force RTL when compiled with -DFORCE_RTL, mostly useful (and on poular request!) on PlayStation2 and maybe other consoles.
    We will later find a cleaner solution.
    
    svn-id: r39971

--- base/main.cpp
+++ base/main.cpp
@@ -322,11 +322,15 @@ extern "C" int scummvm_main(int argc, char *argv[]) {
 			}
 
 			// Quit unless an error occurred, or Return to launcher was requested
+			#ifndef FORCE_RTL
 			if (result == 0 && !g_system->getEventManager()->shouldRTL())
 				break;
-
+			#endif
 			// Reset RTL flag in case we want to load another engine
 			g_system->getEventManager()->resetRTL();
+			#ifdef FORCE_RTL
+			g_system->getEventManager()->resetQuit();
+			#endif
 
 			// Discard any command line options. It's unlikely that the user
 			// wanted to apply them to *all* games ever launched.
