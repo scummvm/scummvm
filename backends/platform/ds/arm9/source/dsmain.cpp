6f3582ba2d1f9166e8efa6e0790f689b6397c563
Travis Howell <kirben@scummvm.org>
2009-04-22 11:57:11 +0000

    Correct version number.
    svn-id: r40073

--- backends/platform/ds/arm9/source/dsmain.cpp
+++ backends/platform/ds/arm9/source/dsmain.cpp
@@ -3066,7 +3066,7 @@ int main(void) {
 	consolePrintf("-------------------------------\n");
 	consolePrintf("ScummVM DS\n");
 	consolePrintf("Ported by Neil Millstone\n");
-	consolePrintf("Version 0.13.1 beta3 ");
+	consolePrintf("Version 0.13.1 ");
 #if defined(DS_BUILD_A)
 	consolePrintf("build A\n");
 	consolePrintf("Lucasarts SCUMM games (SCUMM)\n");
