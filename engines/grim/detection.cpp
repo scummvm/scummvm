ea9779fb1bea1f708b8fa52195f09fca86de6d26
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2009-05-27 18:07:10 +0000

    cleanup comments

--- engines/grim/detection.cpp
+++ engines/grim/detection.cpp
@@ -72,7 +72,7 @@ static const GrimGameDescription gameDescriptions[] = {
 		{
 			"grim",
 			0,
-			AD_ENTRY1s("grim.tab", "464138caf47e580cbb237dee10674b16", 362212), // by garrythefish
+			AD_ENTRY1s("grim.tab", "464138caf47e580cbb237dee10674b16", 362212), // TODO crrect to proper size
 			Common::DE_DEU,
 			Common::kPlatformPC,
 			ADGF_NO_FLAGS
@@ -83,7 +83,7 @@ static const GrimGameDescription gameDescriptions[] = {
 		{
 			"grim",
 			0,
-			AD_ENTRY1s("grim.tab", "b1460cd029f13718f7f62c2403e047ec", 372709), // by jvprat
+			AD_ENTRY1s("grim.tab", "b1460cd029f13718f7f62c2403e047ec", 372709),
 			Common::ES_ESP,
 			Common::kPlatformPC,
 			ADGF_NO_FLAGS
