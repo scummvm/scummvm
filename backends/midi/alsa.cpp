ca330071bea8cae5d019be7b8ca2e59c5ab720b3
Filippos Karapetis <bluegr@gmail.com>
2009-05-30 17:06:30 +0000

    Fixed compilation error
    svn-id: r41037

--- backends/midi/alsa.cpp
+++ backends/midi/alsa.cpp
@@ -231,7 +231,7 @@ int MidiDriver_ALSA::parse_addr(const char *arg, int *client, int *port) {
 	char *p;
 
 	if (isdigit(*arg)) {
-		if ((p = strpbrk(arg, ADDR_DELIM)) == NULL)
+		if ((p = (char *)strpbrk(arg, ADDR_DELIM)) == NULL)
 			return -1;
 		*client = atoi(arg);
 		*port = atoi(p + 1);
