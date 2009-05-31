010fbadf012324fb4f82ea2acdc77917f83fa4bf
Filippos Karapetis <bluegr@gmail.com>
2009-05-31 16:02:10 +0000

    Document that debug level 11 turns on all special debug level messages (refer to rev. #28858)
    svn-id: r41076

--- common/debug.cpp
+++ common/debug.cpp
@@ -122,7 +122,7 @@ DebugChannelList listDebugChannels() {
 }
 
 bool isDebugChannelEnabled(uint32 level) {
-	// FIXME: Seems gDebugLevel 11 has a special meaning? Document that!
+	// Debug level 11 turns on all special debug level messages
 	if (gDebugLevel == 11)
 		return true;
 //	return gDebugLevelsEnabled & (1 << level);
@@ -130,7 +130,7 @@ bool isDebugChannelEnabled(uint32 level) {
 }
 
 bool isDebugChannelEnabled(const String &name) {
-	// FIXME: Seems gDebugLevel 11 has a special meaning? Document that!
+	// Debug level 11 turns on all special debug level messages
 	if (gDebugLevel == 11)
 		return true;
 
@@ -217,7 +217,7 @@ void debugN(int level, const char *s, ...) {
 void debugC(int level, uint32 debugChannels, const char *s, ...) {
 	va_list va;
 
-	// FIXME: Seems gDebugLevel 11 has a special meaning? Document that!
+	// Debug level 11 turns on all special debug level messages
 	if (gDebugLevel != 11)
 		if (level > gDebugLevel || !(Common::gDebugLevelsEnabled & debugChannels))
 			return;
@@ -230,7 +230,7 @@ void debugC(int level, uint32 debugChannels, const char *s, ...) {
 void debugC(uint32 debugChannels, const char *s, ...) {
 	va_list va;
 
-	// FIXME: Seems gDebugLevel 11 has a special meaning? Document that!
+	// Debug level 11 turns on all special debug level messages
 	if (gDebugLevel != 11)
 		if (!(Common::gDebugLevelsEnabled & debugChannels))
 			return;
