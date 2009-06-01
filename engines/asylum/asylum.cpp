8461e778db51e9cd93693664d64254a5a311b435
Alex Bevilacqua <alexbevi@gmail.com>
2009-06-01 18:05:46 +0000

    ASYLUM: updated detection routines so that the engine loads properly
    git-svn-id: http://asylumengine.googlecode.com/svn/trunk@3 0bfb4aae-4ea4-11de-8d8d-752d95cf3e3c

---
+++ engines/asylum/asylum.cpp
@@ -0,0 +1,57 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "common/config-manager.h"
+#include "common/events.h"
+#include "common/system.h"
+#include "common/file.h"
+
+#include "asylum/asylum.h"
+#include "asylum/graphics.cpp"
+
+namespace Asylum {
+
+AsylumEngine::AsylumEngine(OSystem *system, Common::Language language)
+	: Engine(system) {
+
+	Common::File::addDefaultDirectory(_gameDataDir.getChild("Data"));
+	Common::File::addDefaultDirectory(_gameDataDir.getChild("Vids"));	
+
+	_eventMan->registerRandomSource(_rnd, "asylum");
+}
+
+AsylumEngine::~AsylumEngine() {
+	//Common::clearAllDebugChannels();	
+}
+
+Common::Error AsylumEngine::run() {
+	initGraphics(640, 480, true);
+	
+	GraphicResource* g = new GraphicResource;
+	
+	g->loadResource("res.001");
+	
+	delete g;
+	
+	return Common::kNoError;
+}
+
+} // namespace Asylum
