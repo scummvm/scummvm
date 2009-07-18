d362ca86f6fbb4c4f12485ed3ef013713709bd9a
Gregory Montoir <cyx@scummvm.org>
2009-07-18 21:26:44 +0000

    add "-IQ" suffix test to SO_LOAD_STRING (same as SO_SAVE_STRING) ; fixes french version of atlantis
    svn-id: r42596

--- engines/scumm/script_v5.cpp
+++ engines/scumm/script_v5.cpp
@@ -1785,7 +1785,7 @@ void ScummEngine_v5::o5_roomOps() {
 			while ((chr = fetchScriptByte()))
 				filename += chr;
 
-			if (filename.hasPrefix("iq-") || filename.hasPrefix("IQ-") || filename.hasSuffix("-iq")) {
+			if (filename.hasPrefix("iq-") || filename.hasPrefix("IQ-") || filename.hasSuffix("-iq") || filename.hasSuffix("-IQ")) {
 				filename = _targetName + ".iq";
 			} else {
 				error("SO_LOAD_STRING: Unsupported filename %s", filename.c_str());
