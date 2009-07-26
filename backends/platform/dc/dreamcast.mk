0c396ae1fe84617ed6eb79fea66f3d5e94acac91
Marcus Comstedt <marcus_c@users.sourceforge.net>
2009-07-26 23:16:51 +0000

    Shorten version field.
    svn-id: r42829

--- backends/platform/dc/dreamcast.mk
+++ backends/platform/dc/dreamcast.mk
@@ -29,6 +29,6 @@ IP.BIN : ip.txt
 ip.txt : $(srcdir)/backends/platform/dc/ip.txt.in
 	if [ x"$(VER_EXTRA)" = xsvn ]; then \
 	  if [ -z "$(VER_SVNREV)" ]; then ver="SVN"; else ver="r$(VER_SVNREV)"; fi; \
-	else ver="V$(VERSION)"; fi; \
+	else ver="V$(VER_MAJOR).$(VER_MINOR).$(VER_PATCH)"; fi; \
 	sed -e 's/[@]VERSION[@]/'"$$ver"/ -e 's/[@]DATE[@]/$(shell date '+%Y%m%d')/' < $< > $@
 
