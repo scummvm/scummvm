e377059aeee205fb84ec42c6e5339ae5170baaa7
Alexandre Fontoura <alexandrefontoura@gmail.com>
2009-08-01 19:49:16 +0000

    ASYLUM: Some work on grabbing Black Jack in VCR BlowUpPuzzle. It still buggy. See comments for more detail.
    git-svn-id: http://asylumengine.googlecode.com/svn/trunk@270 0bfb4aae-4ea4-11de-8d8d-752d95cf3e3c

--- engines/asylum/blowuppuzzle.h
+++ engines/asylum/blowuppuzzle.h
@@ -60,7 +60,8 @@ protected:
 	uint32	_mouseY;
 	uint32	_curMouseCursor;
 	int32	_cursorStep;
-	bool    _leftClick;
+	bool    _leftClickUp;
+    bool    _leftClickDown;
 	bool    _active;
 
     GraphicResource *_bgResource;
@@ -78,16 +79,16 @@ protected:
 
 
 const Common::Rect BlowUpPuzzleVCRPolies[10] = {
-    Common::Rect(0x0F7, 0x157, 0x13A, 0x183),
-    Common::Rect(0x14B, 0x15C, 0x17B, 0x18B),
-    Common::Rect(0x18C, 0x161, 0x1D2, 0x18F),
-    Common::Rect(0x1FB, 0x156, 0x233, 0x185),
-    Common::Rect(0x154, 0x196, 0x173, 0x1AF),
-    Common::Rect(0x19A, 0x19B, 0x1B9, 0x1B3),
-    Common::Rect(0x1E3, 0x1A0, 0x202, 0x1BC),
-    Common::Rect(0x0,   0x19B, 0x3C,  0x1E0),
-    Common::Rect(0x4C,  0x1AC, 0x0A0, 0x1E0),
-    Common::Rect(0x0BB, 0x1B7, 0x0F0, 0x1E0)
+    Common::Rect(0x0F7, 0x157, 0x13A, 0x183), // rewind button region
+    Common::Rect(0x14B, 0x15C, 0x17B, 0x18B), // stop button region
+    Common::Rect(0x18C, 0x161, 0x1D2, 0x18F), // play button region
+    Common::Rect(0x1FB, 0x156, 0x233, 0x185), // rec button region
+    Common::Rect(0x154, 0x196, 0x173, 0x1AF), // red jack hole region
+    Common::Rect(0x19A, 0x19B, 0x1B9, 0x1B3), // yellow jack hole region
+    Common::Rect(0x1E3, 0x1A0, 0x202, 0x1BC), // black jack hole region
+    Common::Rect(0x0,   0x19B, 0x3C,  0x1E0), // black jack on table region
+    Common::Rect(0x4C,  0x1AC, 0x0A0, 0x1E0), // red jack on table region
+    Common::Rect(0x0BB, 0x1B7, 0x0F0, 0x1E0)  // yellow jack on table region
 };
 
 class BlowUpPuzzleVCR : public BlowUpPuzzle {
@@ -110,7 +111,20 @@ private:
 		kPluggedOnRed       = 1,
 		kPluggedOnYellow    = 2,
 		kPluggedOnBlack     = 3,
-        kShowShadows        = 4
+        kOnHand             = 4
+	};
+
+    enum VCRRegions {
+		kRewindButton   = 0,
+		kStopButton     = 1,
+		kPlayButton     = 2,
+		kRecButton      = 3,
+        kRedHole        = 4,
+        kYellowHole     = 5,
+        kBlackHole      = 6,
+        kBlackJack      = 7,
+        kRedJack        = 8,
+        kYellowJack     = 9
 	};
 
     int _jacksState[3];
@@ -131,7 +145,10 @@ private:
     void updateRedJack();
     void updateYellowJack();
 
-    void isCursorInPolyRegion();
+    void updateCursorInPolyRegion();
+
+    void handleMouseDown();
+    void handleMouseUp();
 }; // end of class BlowUpPuzzleVCR
 
 
