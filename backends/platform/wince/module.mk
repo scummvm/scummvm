73572158a8d32ab1202756b5511f4aec82c181b8
Kostas Nakos <knakos@users.sourceforge.net>
2009-08-04 13:17:33 +0000

    commit slightly modified patch #2828646: WINCE: Integrate Windows CE with the master build system
    svn-id: r43041

---
+++ backends/platform/wince/module.mk
@@ -0,0 +1,21 @@
+MODULE := backends/platform/wince
+
+MODULE_OBJS := \
+	CEActionsPocket.o CEDevice.o CEScaler.o \
+	CEActionsSmartphone.o CELauncherDialog.o wince-sdl.o \
+	CEgui/GUIElement.o CEgui/Panel.o CEgui/SDL_ImageResource.o \
+	CEgui/ItemAction.o CEgui/PanelItem.o CEgui/Toolbar.o \
+	CEgui/ItemSwitch.o CEgui/PanelKeyboard.o CEgui/ToolbarHandler.o \
+	CEkeys/EventsBuffer.o \
+	../../../gui/Actions.o ../../../gui/Key.o ../../../gui/KeysDialog.o \
+	../sdl/sdl.o ../sdl/graphics.o ../sdl/events.o ../sdl/hardwarekeys.o \
+	missing/missing.o \
+	PocketSCUMM.o \
+	ARMscaler.o \
+	smartLandScale.o
+
+MODULE_DIRS += \
+	backends/platform/wince/
+
+# We don't use the rules.mk here on purpose
+OBJS := $(addprefix $(MODULE)/, $(MODULE_OBJS)) $(OBJS)
