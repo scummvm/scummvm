76e8bb5eea5eb988a119e2011b3b3b85e5986562
Joost Peters <joostp@scummvm.org>
2009-08-14 16:13:34 +0000

    PSP: create an EBOOT.PBP when building via configure
    svn-id: r43370

--- backends/platform/psp/psp.mk
+++ backends/platform/psp/psp.mk
@@ -1,6 +1,24 @@
-all: psp_fixup_elf
+all: pack_pbp
 
-psp_fixup_elf: scummvm.elf 
+PSP_EXE := scummvm$(EXEEXT)
+PSP_EXE_STRIPPED := scummvm_stripped$(EXEEXT)
+PACK_PBP = pack-pbp
+
+$(PSP_EXE_STRIPPED): $(PSP_EXE)
+	$(STRIP) $< -o $@
+
+psp_fixup_elf: $(PSP_EXE_STRIPPED)
 	$(PSPDEV)/bin/psp-fixup-imports $<
 
-.PHONY: psp_fixup_elf
+pack_pbp: psp_fixup_elf
+	$(PACK_PBP) EBOOT.PBP \
+	$(srcdir)/backends/platform/psp/param.sfo \
+	$(srcdir)/backends/platform/psp/icon0.png \
+	NULL \
+	$(srcdir)/backends/platform/psp/pic0.png \
+	$(srcdir)/backends/platform/psp/pic1.png \
+	NULL \
+	$(PSP_EXE_STRIPPED) \
+	NULL
+
+.PHONY: psp_fixup_elf pack_pbp
