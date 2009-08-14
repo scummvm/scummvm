4552f5151014fb9b21eaf7da415951fb69c74a06
Joost Peters <joostp@scummvm.org>
2009-08-14 16:55:54 +0000

    add psp_clean build rule to clean up EBOOT.PBP and scummvm_stripped.elf
    svn-id: r43378

--- backends/platform/psp/psp.mk
+++ backends/platform/psp/psp.mk
@@ -1,17 +1,22 @@
 all: pack_pbp
+clean: psp_clean
 
 PSP_EXE := scummvm$(EXEEXT)
 PSP_EXE_STRIPPED := scummvm_stripped$(EXEEXT)
+EBOOT = EBOOT.PBP
 PACK_PBP = pack-pbp
 
 $(PSP_EXE_STRIPPED): $(PSP_EXE)
 	$(STRIP) $< -o $@
 
+psp_clean:
+	$(RM) $(PSP_EXE_STRIPPED) $(EBOOT)
+
 psp_fixup_elf: $(PSP_EXE_STRIPPED)
 	$(PSPDEV)/bin/psp-fixup-imports $<
 
 pack_pbp: psp_fixup_elf
-	$(PACK_PBP) EBOOT.PBP \
+	$(PACK_PBP) $(EBOOT) \
 	$(srcdir)/backends/platform/psp/param.sfo \
 	$(srcdir)/backends/platform/psp/icon0.png \
 	NULL \
