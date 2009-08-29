19920fece2e164f1685c1f98faf1d3cff604f3ea
Travis Howell <kirben@scummvm.org>
2009-08-29 01:13:47 +0000

    Fix the default save game path setting in Macintosh versions of HE72+ games.
    svn-id: r43784

--- engines/scumm/he/script_v60he.cpp
+++ engines/scumm/he/script_v60he.cpp
@@ -131,8 +131,11 @@ int ScummEngine_v60he::convertFilePath(byte *dst, int dstSize) {
 		r = 2;
 	} else if (dst[2] == 'b' && dst[5] == 'k') { // Backyard Basketball INI
 		r = 13;
-	} else if (dst[0] == '*' && dst[1] == '/') { // Save Game Path (HE72 - HE100)
+	} else if (dst[0] == '*' && dst[1] == '/') { // Save Game Path (Windows HE72 - HE100)
 		// The default save game path is set to '*/' by ScummVM
+		r = 2;
+	} else if (dst[0] == '*' && dst[1] == ':') { // Save Game Path (Macintosh HE72 - HE100)
+		// The default save game path is set to ':/' by ScummVM
 		r = 2;
 	} else if (dst[0] == 'c' && dst[1] == ':') { // Save Game Path (HE60 - HE71)
 		// The default save path is game path (DOS) or 'c:/hegames/' (Windows)
