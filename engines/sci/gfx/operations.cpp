bd5ba266b405736f886c0c953b387b9c19febc0f
Filippos Karapetis <bluegr@gmail.com>
2009-09-04 15:47:09 +0000

    Oops
    svn-id: r43943

--- engines/sci/gfx/operations.cpp
+++ engines/sci/gfx/operations.cpp
@@ -1109,7 +1109,7 @@ void gfxop_set_pointer_position(GfxState *state, Common::Point pos) {
 	state->pointer_pos = pos;
 
 	if (pos.x > 320 || pos.y > 200) {
-		debugC("[GFX] Attempt to place pointer at invalid coordinates (%d, %d)\n", pos.x, pos.y);
+		debug("[GFX] Attempt to place pointer at invalid coordinates (%d, %d)\n", pos.x, pos.y);
 		return; // Not fatal
 	}
 
