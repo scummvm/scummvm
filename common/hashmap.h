537c886aeba5412896afa423263dd301e42e9931
Max Horn <max@quendi.de>
2009-09-21 21:37:12 +0000

    Stop confusing doxygen :)
    svn-id: r44237

--- common/hashmap.h
+++ common/hashmap.h
@@ -39,10 +39,9 @@
 #endif
 
 namespace Common {
-/**
-  * The sgi IRIX MIPSpro Compiler has difficulties with nested templates. 
-  * This and the other __sgi conditionals below work around these problems.
-  */
+
+// The sgi IRIX MIPSpro Compiler has difficulties with nested templates. 
+// This and the other __sgi conditionals below work around these problems.
 #ifdef __sgi
 template<class T> class IteratorImpl;
 #endif
