eb04f8567348da085283a7e260fb55ad9331e7e5
Benjamin Haisch <john_doe@techie.com>
2009-09-21 10:53:25 +0000

    TOLTECS: - Added detection entry for the German demo version

--- engines/toltecs/detection.cpp
+++ engines/toltecs/detection.cpp
@@ -91,6 +91,18 @@ static const ToltecsGameDescription gameDescriptions[] = {
 		},
 	},
 
+	{
+		// 3 Skulls of the Toltecs German Demo version
+		{
+			"toltecs",
+			0,
+			AD_ENTRY1s("WESTERN", "1c85e82712d24f1d5c1ea2a66ddd75c2", 47730038),
+			Common::DE_DEU,
+			Common::kPlatformPC,
+			ADGF_NO_FLAGS
+		},
+	},
+
 	{ AD_TABLE_END_MARKER }
 };
 
