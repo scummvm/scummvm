957f9c0b551282d94693ac4762033e6b1630af0d
Max Horn <max@quendi.de>
2009-09-25 11:27:12 +0000

    More of patch #2850021: disable hq-scaler&mt32emu for motoezx&motomagx-platform
    svn-id: r44344

--- dists/motomagx/pep/app/scummvm.sh
+++ dists/motomagx/pep/app/scummvm.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 GAMES=/mmc/mmca1/.Games
-export DATA_DIR=$GAMES/.data/ScummVM
 export LD_LIBRARY_PATH=$GAMES/.lib:$LD_LIBRARY_PATH
 # uncomment next line if you wanna 270-degrees clockwise rotated screen 
 #export SDL_QT_INVERT_ROTATION=1
-
 GAME_PATH=`basename $0`
 GAME_PATH=`echo $0 | sed -e 's/'$GAME_PATH'//g'`
 cd "$GAME_PATH"
-
-export HOME="$GAME_PATH"
-
-export LD_PREADLOAD=/usr/lib/libtaskman.so
-
-exec ./scummvm -g 1x
+export HOME="$GAMES"
+exec ./scummvm --gfx-mode=1x > $GAME_PATH/scummvm.log
