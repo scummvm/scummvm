1a4257f46ffd9179ebb06c4b2cb7617f4acceb6e
Max Horn <max@quendi.de>
2009-09-25 11:22:14 +0000

    Move linuxmoto specific stuff from ports.mk to a new file backends/platform/linuxmoto/linuxmoto.mk
    svn-id: r44342

---
+++ backends/platform/linuxmoto/linuxmoto.mk
@@ -0,0 +1,35 @@
+# Special target to create a motoezx snapshot
+motoezx: $(EXECUTABLE)
+	$(MKDIR) motoezx/scummvm
+	$(STRIP) $(EXECUTABLE) -o motoezx/scummvm/$(EXECUTABLE)
+	$(INSTALL) -c -m 644 $(DIST_FILES_THEMES) $(DIST_FILES_ENGINEDATA) motoezx/scummvm/
+	$(CP) $(srcdir)/backends/vkeybd/packs/vkeybd_default.zip motoezx/scummvm/
+	$(CP) $(srcdir)/dists/motoezx/* motoezx/scummvm/
+	tar -C motoezx -cvzf motoezx/ScummVM.pkg scummvm
+
+# Special target to create a motomagx snapshot
+motomagx-mpkg: $(EXECUTABLE)
+	$(MKDIR) motomagx/scummvm
+	$(STRIP) $(EXECUTABLE) -o motomagx/scummvm/$(EXECUTABLE)
+	$(INSTALL) -c -m 644 $(DIST_FILES_THEMES) $(DIST_FILES_ENGINEDATA) motomagx/scummvm/
+	$(CP) $(srcdir)/backends/vkeybd/packs/vkeybd_default.zip motomagx/scummvm/
+	$(CP) $(srcdir)/dists/motomagx/mpkg/* motomagx/scummvm/
+	tar -C motomagx -cvzf motomagx/ScummVM.mpkg scummvm
+
+motomagx-mgx: $(EXECUTABLE)
+	$(MKDIR) motomagx/scummvm
+	$(STRIP) $(EXECUTABLE) -o motomagx/scummvm/$(EXECUTABLE)
+	$(INSTALL) -c -m 644 $(DIST_FILES_THEMES) $(DIST_FILES_ENGINEDATA) motomagx/scummvm/
+	$(CP) $(srcdir)/backends/vkeybd/packs/vkeybd_default.zip motomagx/scummvm/
+	$(CP) $(srcdir)/dists/motomagx/mgx/* motomagx/scummvm/
+	tar -C motomagx -cvzf motomagx/ScummVM.mgx scummvm
+
+motomagx-pep: $(EXECUTABLE)
+	$(MKDIR) motomagx/pep
+	$(CP) -r $(srcdir)/dists/motomagx/pep/* motomagx/pep
+	$(STRIP) $(EXECUTABLE) -o motomagx/pep/app/$(EXECUTABLE)
+	$(INSTALL) -c -m 644 $(DIST_FILES_THEMES) $(DIST_FILES_ENGINEDATA) motomagx/pep/app
+	$(CP) $(srcdir)/backends/vkeybd/packs/vkeybd_default.zip motomagx/pep/app
+	tar -C motomagx/pep -czvf motomagx/ScummVM.pep app description.ini  scummvm_big_usr.png  scummvm_small_usr.png
+
+.PHONY: motoezx motomagx-mpkg motomagx-mgx motomagx-pep
