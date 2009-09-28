9c4f4e677534a75652b85454997f9d9ec05aee14
Andre Heider <dhewg@wiibrew.org>
2009-09-28 09:05:29 +0000

    Fixed whitespace fail
    svn-id: r44442

--- backends/platform/wince/wince.mk
+++ backends/platform/wince/wince.mk
@@ -1,3 +1,4 @@
 backends/platform/wince/PocketSCUMM.o: $(srcdir)/backends/platform/wince/PocketSCUMM.rc
-    $(QUIET)$(MKDIR) $(*D)
-    $(WINDRES) $(WINDRESFLAGS) -I$(srcdir)/backends/platform/wince $< $@
+	$(QUIET)$(MKDIR) $(*D)
+	$(WINDRES) $(WINDRESFLAGS) -I$(srcdir)/backends/platform/wince $< $@
+
