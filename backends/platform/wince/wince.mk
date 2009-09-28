0dcc75671e5fb0c60b00b9905451dcfddced89aa
Andre Heider <dhewg@wiibrew.org>
2009-09-28 08:57:44 +0000

    Fix WinCE build error
    svn-id: r44441

--- backends/platform/wince/wince.mk
+++ backends/platform/wince/wince.mk
@@ -1,5 +1,3 @@
-all: PocketSCUMM.o
-
-PocketSCUMM.o: $(srcdir)/backends/platform/wince/PocketSCUMM.rc
-	mkdir -p backends/platform/wince
-	$(WINDRES) -I$(srcdir)/backends/platform/wince $(srcdir)/backends/platform/wince/PocketSCUMM.rc backends/platform/wince/PocketSCUMM.o
+backends/platform/wince/PocketSCUMM.o: $(srcdir)/backends/platform/wince/PocketSCUMM.rc
+    $(QUIET)$(MKDIR) $(*D)
+    $(WINDRES) $(WINDRESFLAGS) -I$(srcdir)/backends/platform/wince $< $@
