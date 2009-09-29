9cb418aab00942bd0fd9553016ee9fadb2b8bcbd
Johannes Schickel <lordhoto@scummvm.org>
2009-09-29 14:38:44 +0000

    Backport of r44463.
    svn-id: r44465

--- engines/scumm/debugger.cpp
+++ engines/scumm/debugger.cpp
@@ -506,7 +506,7 @@ bool ScummDebugger::Cmd_Debug(int argc, const char **argv) {
 
 	// No parameters given: Print out a list of all channels and their status
 	if (argc <= 1) {
-		DebugPrintf("Available debug channels: ");
+		DebugPrintf("Available debug channels:\n");
 		for (Common::DebugChannelList::const_iterator i = lvls.begin(); i != lvls.end(); ++i) {
 			DebugPrintf("%c%s - %s (%s)\n", i->enabled ? '+' : ' ',
 					i->name.c_str(), i->description.c_str(),
