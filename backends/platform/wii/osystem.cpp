a6b2e9255fff6bf079115ba8aa7fa6dd9069cfed
Andre Heider <dhewg@wiibrew.org>
2009-10-01 16:52:05 +0000

    Fixed mismatching malloc/free, and plugged a memleak.
    svn-id: r44526

--- backends/platform/wii/osystem.cpp
+++ backends/platform/wii/osystem.cpp
@@ -211,7 +211,7 @@ OSystem::MutexRef OSystem_Wii::createMutex() {
 
 	if (res) {
 		printf("ERROR creating mutex\n");
-		delete mutex;
+		free(mutex);
 		return NULL;
 	}
 
@@ -237,6 +237,8 @@ void OSystem_Wii::deleteMutex(MutexRef mutex) {
 
 	if (res)
 		printf("ERROR destroying mutex %p (%d)\n", mutex, res);
+
+	free(mutex);
 }
 
 void OSystem_Wii::setWindowCaption(const char *caption) {
