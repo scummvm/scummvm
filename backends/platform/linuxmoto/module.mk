126450f3bc272570d19d0dc8a7473287d1b72ff0
Max Horn <max@quendi.de>
2009-10-04 23:21:01 +0000

    Revert nonsense I did in backends/platform/linuxmoto/module.mk and modify ASM build rules instead, enforcing the creation of output dirs for compiled files
    svn-id: r44647

--- backends/platform/linuxmoto/module.mk
+++ backends/platform/linuxmoto/module.mk
@@ -6,18 +6,14 @@ MODULE_OBJS := \
 	linuxmoto-sdl.o \
 	linuxmoto-scaler.o \
 	linuxmoto-graphics.o \
-	hardwarekeys.o
+	hardwarekeys.o \
+	../wince/ARMscaler.o
 
 MODULE_DIRS += \
 	backends/platform/linuxmoto/
 
 # We don't use the rules.mk here on purpose
 OBJS := $(addprefix $(MODULE)/, $(MODULE_OBJS)) $(OBJS)
-
-# HACK: We import one of the WinCE scalers
-OBJS += backends/platform/wince/ARMscaler.o
-MODULE_DIRS += \
-	backends/platform/wii/
 
 # HACK: The linuxmoto backend is based on the SDL one, so we load that, too.
 include $(srcdir)/backends/platform/sdl/module.mk
