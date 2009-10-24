ff78169680249e3a8f48b8198f08b2b8aadac496
Travis Howell <kirben@scummvm.org>
2009-10-24 12:33:18 +0000

    Correct error in CD music table for PCE version of Loom.
    svn-id: r45359

--- engines/scumm/sound.cpp
+++ engines/scumm/sound.cpp
@@ -166,7 +166,7 @@ void Sound::playSound(int soundID) {
 
 	if (_vm->_game.id == GID_LOOM && _vm->_game.platform == Common::kPlatformPCEngine) {
 		if (soundID >= 13 && soundID <= 32) {
-			static const char tracks[20] = {3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 21, 19, 20, 21};
+			static const char tracks[20] = {3, 4, 5, 7, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 21, 19, 20, 21};
 
 			_currentCDSound = soundID;
 			playCDTrack(tracks[soundID - 13], 1, 0, 0);
