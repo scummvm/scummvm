939d8d1a373a9c4dc0feb780f57894296acf4148
Torbjörn Andersson <eriktorbjorn@users.sourceforge.net>
2009-10-26 20:03:07 +0000

    Silence GCC warning.
    svn-id: r45413

--- engines/tinsel/heapmem.cpp
+++ engines/tinsel/heapmem.cpp
@@ -454,7 +454,7 @@ void MemoryReAlloc(MEM_NODE *pMemNode, long size) {
 	if (size != pMemNode->size) {
 
 		// make sure memory object is not locked and discarded
-		assert(pMemNode->flags == DWM_LOCKED | DWM_DISCARDED);
+		assert(pMemNode->flags == (DWM_LOCKED | DWM_DISCARDED));
 		assert(pMemNode->size == 0);
 
 		// unlink the mnode from the current heap
