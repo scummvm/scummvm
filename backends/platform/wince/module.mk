501dfa24895b15f3be8b50406e62fe557f3d0fb1
Max Horn <max@quendi.de>
2009-11-10 00:10:30 +0000

    WINCE: List each object in MODULE_OBJS on its own line, for readability
    svn-id: r45798

--- backends/platform/wince/module.mk
+++ backends/platform/wince/module.mk
@@ -1,14 +1,29 @@
 MODULE := backends/platform/wince
 
 MODULE_OBJS := \
-	CEActionsPocket.o CEDevice.o CEScaler.o \
-	CEActionsSmartphone.o CELauncherDialog.o wince-sdl.o \
-	CEgui/GUIElement.o CEgui/Panel.o CEgui/SDL_ImageResource.o \
-	CEgui/ItemAction.o CEgui/PanelItem.o CEgui/Toolbar.o \
-	CEgui/ItemSwitch.o CEgui/PanelKeyboard.o CEgui/ToolbarHandler.o \
+	CEActionsPocket.o \
+	CEDevice.o \
+	CEScaler.o \
+	CEActionsSmartphone.o \
+	CELauncherDialog.o \
+	wince-sdl.o \
+	CEgui/GUIElement.o \
+	CEgui/Panel.o \
+	CEgui/SDL_ImageResource.o \
+	CEgui/ItemAction.o \
+	CEgui/PanelItem.o \
+	CEgui/Toolbar.o \
+	CEgui/ItemSwitch.o \
+	CEgui/PanelKeyboard.o \
+	CEgui/ToolbarHandler.o \
 	CEkeys/EventsBuffer.o \
-	../../../gui/Actions.o ../../../gui/Key.o ../../../gui/KeysDialog.o \
-	../sdl/sdl.o ../sdl/graphics.o ../sdl/events.o ../sdl/hardwarekeys.o \
+	../../../gui/Actions.o \
+	../../../gui/Key.o \
+	../../../gui/KeysDialog.o \
+	../sdl/sdl.o \
+	../sdl/graphics.o \
+	../sdl/events.o \
+	../sdl/hardwarekeys.o \
 	missing/missing.o \
 	PocketSCUMM.o \
 	ARMscaler.o \
