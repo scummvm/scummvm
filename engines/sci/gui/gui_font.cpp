83c7b6b6b6f0b30876384d9d6e4b4fd27a604ba9
Martin Kiewitz <m_kiewitz@users.sourceforge.net>
2009-11-12 19:11:50 +0000

    SCI/newgui: SciGuiFont fixing delete
    svn-id: r45867

--- engines/sci/gui/gui_font.cpp
+++ engines/sci/gui/gui_font.cpp
@@ -57,7 +57,7 @@ SciGuiFont::SciGuiFont(ResourceManager *resMan, GuiResourceId resourceId)
 }
 
 SciGuiFont::~SciGuiFont() {
-	delete _chars;
+	delete []_chars;
 	_resMan->unlockResource(_resource);
 }
 
