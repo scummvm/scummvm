ebc206afdea7c531af80beff049971f4536ca5de
Alex Bevilacqua <alexbevi@gmail.com>
2009-12-04 21:55:30 +0000

    ASYLUM: minor cleanup
    git-svn-id: http://asylumengine.googlecode.com/svn/trunk@412 0bfb4aae-4ea4-11de-8d8d-752d95cf3e3c

--- engines/asylum/speech.cpp
+++ engines/asylum/speech.cpp
@@ -31,9 +31,9 @@
 namespace Asylum {
 
 Speech::Speech(Scene *scene): _scene(scene) {
-    _tick = _scene->vm()->getTick();
-    _soundResIdx = 0;
-    _textResIdx = 0;
+	_tick        = _scene->vm()->getTick();
+	_soundResIdx = 0;
+	_textResIdx  = 0;
 }
 
 Speech::~Speech() {
@@ -41,116 +41,116 @@ Speech::~Speech() {
 }
 
 int32 Speech::play(int32 speechIdx) {
-    int32 soundResIdx = 0;
+	int32 soundResIdx = 0;
 
-    switch (_scene->worldstats()->actorType) {
-    case kMax: {
-        int32 soundResIdx2 = speechIdx;
-        int32 textResIdx = speechIdx;
+	switch (_scene->worldstats()->actorType) {
+	case kMax: {
+		int32 soundResIdx2 = speechIdx;
+		int32 textResIdx = speechIdx;
 
-        if (speechIdx >= 259) {
-            soundResIdx2 -= 9;
-            textResIdx -= 9;
-        }
+		if (speechIdx >= 259) {
+			soundResIdx2 -= 9;
+			textResIdx -= 9;
+		}
 
-        soundResIdx = soundResIdx2 - 0x7FFD0000;
-        // setup the right index for sound and text
-        setPlayerSpeech(soundResIdx, textResIdx - 0x7FFFFFAD);
-    }
-        break;
-    // TODO: check if actor type is right for the following cases
-    case kSarah: {
-        soundResIdx = speechIdx - 0x7FFBF879;
-        setPlayerSpeech(soundResIdx, speechIdx - 0x7FFFFDB6);
-    }
-        break;
-    case kCyclops: {
-        soundResIdx = speechIdx - 0x7FFBF7DC;
-        setPlayerSpeech(soundResIdx, speechIdx - 0x7FFFFD19);
-    }
-        break;
-    case kAztec: {
-        soundResIdx = speechIdx - 0x7FFBF746;
-        setPlayerSpeech(soundResIdx, speechIdx - 0x7FFFFC83);
-    }
-        break;
-    default:
-        soundResIdx = 0;
-        break;
-    }
+		soundResIdx = soundResIdx2 - 0x7FFD0000;
+		// setup the right index for sound and text
+		setPlayerSpeech(soundResIdx, textResIdx - 0x7FFFFFAD);
+	}
+		break;
+	// TODO: check if actor type is right for the following cases
+	case kSarah: {
+		soundResIdx = speechIdx - 0x7FFBF879;
+		setPlayerSpeech(soundResIdx, speechIdx - 0x7FFFFDB6);
+	}
+		break;
+	case kCyclops: {
+		soundResIdx = speechIdx - 0x7FFBF7DC;
+		setPlayerSpeech(soundResIdx, speechIdx - 0x7FFFFD19);
+	}
+		break;
+	case kAztec: {
+		soundResIdx = speechIdx - 0x7FFBF746;
+		setPlayerSpeech(soundResIdx, speechIdx - 0x7FFFFC83);
+	}
+		break;
+	default:
+		soundResIdx = 0;
+		break;
+	}
 
-    return soundResIdx;
+	return soundResIdx;
 }
 
 void Speech::setPlayerSpeech(int32 soundResIdx, int32 textResIdx) {
-    if (soundResIdx) {
-        if (_scene->vm()->sound()->isPlaying(soundResIdx)) {
-            _scene->vm()->sound()->stopSound(soundResIdx);
-        }
-    }
+	if (soundResIdx) {
+		if (_scene->vm()->sound()->isPlaying(soundResIdx)) {
+			_scene->vm()->sound()->stopSound(soundResIdx);
+		}
+	}
 
-    _soundResIdx = soundResIdx;
-    _textResIdx = textResIdx;
+	_soundResIdx = soundResIdx;
+	_textResIdx = textResIdx;
 
-    prepareSpeech();
+	prepareSpeech();
 }
 
 void Speech::prepareSpeech() {
-    //int32 startTick = _scene->vm()->getTick();
+	//int32 startTick = _scene->vm()->getTick();
 
-    if (_soundResIdx) {
-        if (!_scene->vm()->sound()->isPlaying(_soundResIdx)/* || _tick && startTick >= _tick*/) {
-            processSpeech();
-        }
+	if (_soundResIdx) {
+		if (!_scene->vm()->sound()->isPlaying(_soundResIdx)/* || _tick && startTick >= _tick*/) {
+			processSpeech();
+		}
 
-        if (Config.showEncounterSubtitles) {
-            int16 check = 0;
-            /*Common::Point *pt;
-            _scene->getActorPosition(_scene->getActor(), pt);
-            
-            check = pt->y < 240;
-            check = pt->y >= 240;*/
-            int32 posY = ((check - 1) & 0x118) + 40;
+		if (Config.showEncounterSubtitles) {
+			int16 check = 0;
+			/*Common::Point *pt;
+			_scene->getActorPosition(_scene->getActor(), pt);
 
-            _scene->vm()->text()->loadFont(_scene->getResourcePack(), _scene->worldstats()->commonRes.font3);
-            _scene->vm()->text()->drawText(20, posY, _textDataPos);
+			check = pt->y < 240;
+			check = pt->y >= 240;*/
+			int32 posY = ((check - 1) & 0x118) + 40;
 
-            _scene->vm()->text()->loadFont(_scene->getResourcePack(), _scene->worldstats()->commonRes.font1);
-            _scene->vm()->text()->drawText(20, posY, _textData);
-        }
-    }
+			_scene->vm()->text()->loadFont(_scene->getResourcePack(), _scene->worldstats()->commonRes.font3);
+			_scene->vm()->text()->drawText(20, posY, _textDataPos);
+
+			_scene->vm()->text()->loadFont(_scene->getResourcePack(), _scene->worldstats()->commonRes.font1);
+			_scene->vm()->text()->drawText(20, posY, _textData);
+		}
+	}
 }
 
 void Speech::processSpeech() {
-    char * txt;
+	char * txt;
 
-    _tick = 0;
+	_tick = 0;
 
-    txt = _scene->vm()->text()->getResText(_textResIdx);
+	txt = _scene->vm()->text()->getResText(_textResIdx);
 
-    if (*(txt + strlen((const char *)txt) - 2) == 1) {
-        _textResIdx = 0;
-        _textData = 0;
-        _textDataPos = 0;
-    } else {
-        if (*txt == '{') {
-            _textData = txt + 3;
-            _textDataPos = 0;
-            
-            _scene->vm()->text()->loadFont(_scene->getResourcePack(), _scene->worldstats()->commonRes.font1);
-            _scene->vm()->sound()->playSpeech(_soundResIdx);
-        } else {
-            _textData = 0;
-            _textDataPos = txt;
-            
-            if (*txt == '/') {
-                _textDataPos = txt + 2;
-            }
-            
-            _scene->vm()->text()->loadFont(_scene->getResourcePack(), _scene->worldstats()->commonRes.font3);
-            _scene->vm()->sound()->playSpeech(_soundResIdx);
-        }
-    }
+	if (*(txt + strlen((const char *)txt) - 2) == 1) {
+		_textResIdx = 0;
+		_textData = 0;
+		_textDataPos = 0;
+	} else {
+		if (*txt == '{') {
+			_textData = txt + 3;
+			_textDataPos = 0;
+
+			_scene->vm()->text()->loadFont(_scene->getResourcePack(), _scene->worldstats()->commonRes.font1);
+			_scene->vm()->sound()->playSpeech(_soundResIdx);
+		} else {
+			_textData = 0;
+			_textDataPos = txt;
+
+			if (*txt == '/') {
+				_textDataPos = txt + 2;
+			}
+
+			_scene->vm()->text()->loadFont(_scene->getResourcePack(), _scene->worldstats()->commonRes.font3);
+			_scene->vm()->sound()->playSpeech(_soundResIdx);
+		}
+	}
 }
 
 } // end of namespace Asylum
