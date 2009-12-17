3c5473f832694e69133fec656329e5660902f298
Arnaud Boutonné <strangerke@users.sourceforge.net>
2009-12-17 22:50:34 +0000

    gob - Add MD5 for LiT floppy part 1 "full", part 1 "light" and part 2. Part 2 still has to be properly separated between "full" and "light"
    svn-id: r46411

--- engines/gob/detection.cpp
+++ engines/gob/detection.cpp
@@ -56,7 +56,9 @@ static const PlainGameDescriptor gobGames[] = {
 	{"ajworld", "A.J's World of Discovery"},
 	{"gob3", "Goblins Quest 3"},
 	{"gob3cd", "Goblins Quest 3 CD"},
-	{"lostintime", "Lost in Time"},
+	{"lit1", "Lost in Time Part 1"},
+	{"lit2", "Lost in Time Part 2"},
+	{"lit", "Lost in Time"},
 	{"inca2", "Inca II: Wiracocha"},
 	{"woodruff", "The Bizarre Adventures of Woodruff and the Schnibble"},
 	{"dynasty", "The Last Dynasty"},
@@ -1876,7 +1878,7 @@ static const GOBGameDescription gameDescriptions[] = {
 	},
 	{
 		{
-			"lostintime",
+			"lit",
 			"",
 			AD_ENTRY1s("intro.stk", "7b7f48490dedc8a7cb999388e2fadbe3", 3930674),
 			EN_USA,
@@ -1890,7 +1892,7 @@ static const GOBGameDescription gameDescriptions[] = {
 	},
 	{
 		{
-			"lostintime",
+			"lit",
 			"",
 			AD_ENTRY1s("intro.stk", "e0767783ff662ed93665446665693aef", 4371238),
 			HB_ISR,
@@ -1904,7 +1906,7 @@ static const GOBGameDescription gameDescriptions[] = {
 	},
 	{ // Supplied by Arshlan in the forums
 		{
-			"lostintime",
+			"lit",
 			"",
 			AD_ENTRY1s("intro.stk", "3712e7527ba8ce5637d2aadf62783005", 72318),
 			FR_FRA,
@@ -1918,7 +1920,7 @@ static const GOBGameDescription gameDescriptions[] = {
 	},
 	{ // Supplied by cartman_ on #scummvm
 		{
-			"lostintime",
+			"lit",
 			"",
 			AD_ENTRY1s("intro.stk", "f1f78b663893b58887add182a77df151", 3944090),
 			DE_DEU,
@@ -1932,7 +1934,7 @@ static const GOBGameDescription gameDescriptions[] = {
 	},
 	{ // Supplied by goodoldgeorg in bug report #2105220
 		{
-			"lostintime",
+			"lit",
 			"",
 			AD_ENTRY1s("intro.stk", "cd322cb3c64ef2ba2f2134aa2122cfe9", 3936700),
 			ES_ESP,
@@ -1946,7 +1948,7 @@ static const GOBGameDescription gameDescriptions[] = {
 	},
 	{ // Supplied by koalet in bug report #2479034
 		{
-			"lostintime",
+			"lit",
 			"",
 			{
 				{"intro.stk", 0, "af98bcdc70e1f1c1635577fd726fe7f1", 3937310},
@@ -1964,7 +1966,7 @@ static const GOBGameDescription gameDescriptions[] = {
 	},
 	{
 		{
-			"lostintime",
+			"lit",
 			"",
 			AD_ENTRY1s("intro.stk", "6263d09e996c1b4e84ef2d650b820e57", 4831170),
 			EN_USA,
@@ -1978,7 +1980,7 @@ static const GOBGameDescription gameDescriptions[] = {
 	},
 	{
 		{
-			"lostintime",
+			"lit",
 			"",
 			AD_ENTRY1s("intro.stk", "6263d09e996c1b4e84ef2d650b820e57", 4831170),
 			FR_FRA,
@@ -1992,7 +1994,7 @@ static const GOBGameDescription gameDescriptions[] = {
 	},
 	{
 		{
-			"lostintime",
+			"lit",
 			"",
 			AD_ENTRY1s("intro.stk", "6263d09e996c1b4e84ef2d650b820e57", 4831170),
 			IT_ITA,
@@ -2006,7 +2008,7 @@ static const GOBGameDescription gameDescriptions[] = {
 	},
 	{
 		{
-			"lostintime",
+			"lit",
 			"",
 			AD_ENTRY1s("intro.stk", "6263d09e996c1b4e84ef2d650b820e57", 4831170),
 			DE_DEU,
@@ -2020,7 +2022,7 @@ static const GOBGameDescription gameDescriptions[] = {
 	},
 	{
 		{
-			"lostintime",
+			"lit",
 			"",
 			AD_ENTRY1s("intro.stk", "6263d09e996c1b4e84ef2d650b820e57", 4831170),
 			ES_ESP,
@@ -2034,7 +2036,7 @@ static const GOBGameDescription gameDescriptions[] = {
 	},
 	{
 		{
-			"lostintime",
+			"lit",
 			"",
 			AD_ENTRY1s("intro.stk", "6263d09e996c1b4e84ef2d650b820e57", 4831170),
 			EN_GRB,
@@ -2048,7 +2050,7 @@ static const GOBGameDescription gameDescriptions[] = {
 	},
 	{ // Supplied by SiRoCs in bug report #2093672
 		{
-			"lostintime",
+			"lit",
 			"",
 			AD_ENTRY1s("intro.stk", "795be7011ec31bf5bb8ce4efdb9ee5d3", 4838904),
 			EN_USA,
@@ -2062,7 +2064,7 @@ static const GOBGameDescription gameDescriptions[] = {
 	},
 	{ // Supplied by SiRoCs in bug report #2093672
 		{
-			"lostintime",
+			"lit",
 			"",
 			AD_ENTRY1s("intro.stk", "795be7011ec31bf5bb8ce4efdb9ee5d3", 4838904),
 			FR_FRA,
@@ -2076,7 +2078,7 @@ static const GOBGameDescription gameDescriptions[] = {
 	},
 	{ // Supplied by SiRoCs in bug report #2093672
 		{
-			"lostintime",
+			"lit",
 			"",
 			AD_ENTRY1s("intro.stk", "795be7011ec31bf5bb8ce4efdb9ee5d3", 4838904),
 			IT_ITA,
@@ -2090,7 +2092,7 @@ static const GOBGameDescription gameDescriptions[] = {
 	},
 	{ // Supplied by SiRoCs in bug report #2093672
 		{
-			"lostintime",
+			"lit",
 			"",
 			AD_ENTRY1s("intro.stk", "795be7011ec31bf5bb8ce4efdb9ee5d3", 4838904),
 			DE_DEU,
@@ -2104,7 +2106,7 @@ static const GOBGameDescription gameDescriptions[] = {
 	},
 	{ // Supplied by SiRoCs in bug report #2093672
 		{
-			"lostintime",
+			"lit",
 			"",
 			AD_ENTRY1s("intro.stk", "795be7011ec31bf5bb8ce4efdb9ee5d3", 4838904),
 			ES_ESP,
@@ -2118,7 +2120,7 @@ static const GOBGameDescription gameDescriptions[] = {
 	},
 	{ // Supplied by SiRoCs in bug report #2093672
 		{
-			"lostintime",
+			"lit",
 			"",
 			AD_ENTRY1s("intro.stk", "795be7011ec31bf5bb8ce4efdb9ee5d3", 4838904),
 			EN_GRB,
@@ -2132,7 +2134,7 @@ static const GOBGameDescription gameDescriptions[] = {
 	},
 	{
 		{
-			"lostintime",
+			"lit",
 			"",
 			AD_ENTRY1s("intro.stk", "0ddf39cea1ec30ecc8bfe444ebd7b845", 4207330),
 			EN_GRB,
@@ -2146,7 +2148,7 @@ static const GOBGameDescription gameDescriptions[] = {
 	},
 	{
 		{
-			"lostintime",
+			"lit",
 			"",
 			AD_ENTRY1s("intro.stk", "0ddf39cea1ec30ecc8bfe444ebd7b845", 4207330),
 			FR_FRA,
@@ -2160,7 +2162,7 @@ static const GOBGameDescription gameDescriptions[] = {
 	},
 	{
 		{
-			"lostintime",
+			"lit",
 			"",
 			AD_ENTRY1s("intro.stk", "0ddf39cea1ec30ecc8bfe444ebd7b845", 4207330),
 			ES_ESP,
@@ -2174,7 +2176,7 @@ static const GOBGameDescription gameDescriptions[] = {
 	},
 	{
 		{
-			"lostintime",
+			"lit",
 			"",
 			AD_ENTRY1s("intro.stk", "0ddf39cea1ec30ecc8bfe444ebd7b845", 4219382),
 			DE_DEU,
@@ -2188,7 +2190,7 @@ static const GOBGameDescription gameDescriptions[] = {
 	},
 	{
 		{
-			"lostintime",
+			"lit",
 			"",
 			AD_ENTRY1s("intro.stk", "0ddf39cea1ec30ecc8bfe444ebd7b845", 4219382),
 			FR_FRA,
@@ -2202,9 +2204,59 @@ static const GOBGameDescription gameDescriptions[] = {
 	},
 	{ // Found in french ADI 2.6 Francais-Maths 4e
 		{
-			"lostintime",
+			"lit",
 			"",
 			AD_ENTRY1s("intro.stk", "58ee9583a4fb837f02d9a58e5f442656", 3937120),
+			FR_FRA,
+			kPlatformWindows,
+			ADGF_NO_FLAGS,
+			GUIO_NOSUBTITLES | GUIO_NOSPEECH
+		},
+		kGameTypeLostInTime,
+		kFeaturesAdlib,
+		0, 0, 0
+	},
+	{
+		{
+			"lit1",
+			"Full install",
+			{
+				{"intro.stk", 0, "93c91bc9e783d00033042ae83144d7dd", 72318},
+				{"partie2.itk", 0, "78f00bd8eb9e680e6289bba0130b1b33", 4396644},
+				{0, 0, 0, 0}
+			},
+			FR_FRA,
+			kPlatformWindows,
+			ADGF_NO_FLAGS,
+			GUIO_NOSUBTITLES | GUIO_NOSPEECH
+		},
+		kGameTypeLostInTime,
+		kFeaturesAdlib,
+		0, 0, 0
+	},
+	{
+		{
+			"lit1",
+			"Light install",
+			{
+				{"intro.stk", 0, "93c91bc9e783d00033042ae83144d7dd", 72318},
+				{"partie2.itk", 0, "78f00bd8eb9e680e6289bba0130b1b33", 664064},
+				{0, 0, 0, 0}
+			},
+			FR_FRA,
+			kPlatformWindows,
+			ADGF_NO_FLAGS,
+			GUIO_NOSUBTITLES | GUIO_NOSPEECH
+		},
+		kGameTypeLostInTime,
+		kFeaturesAdlib,
+		0, 0, 0
+	},
+	{
+		{
+			"lit2",
+			"",
+			AD_ENTRY1s("intro.stk", "17acbb212e62addbe48dc8f2282c98cb", 72318),
 			FR_FRA,
 			kPlatformWindows,
 			ADGF_NO_FLAGS,
