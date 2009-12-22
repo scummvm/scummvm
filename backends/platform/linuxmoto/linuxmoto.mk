bac7961b3f8f5692fcdd5683866d6e9f935a7ca6
Eugene Sandulenko <sev@scummvm.org>
2009-12-22 23:37:31 +0000

    Patch #2896875: "dist update for linuxmoto-platform"
    svn-id: r46491

--- backends/platform/linuxmoto/linuxmoto.mk
+++ backends/platform/linuxmoto/linuxmoto.mk
@@ -1,40 +1,44 @@
 # Special target to create a motoezx snapshot
 motoezx: $(EXECUTABLE)
-	$(MKDIR) motoezx/scummvm
-	$(STRIP) $(EXECUTABLE) -o motoezx/scummvm/$(EXECUTABLE)
-	$(INSTALL) -c -m 644 $(DIST_FILES_THEMES) $(DIST_FILES_ENGINEDATA) motoezx/scummvm/
-	$(CP) $(srcdir)/backends/vkeybd/packs/vkeybd_default.zip motoezx/scummvm/
-	$(CP) $(srcdir)/dists/motoezx/* motoezx/scummvm/
-	tar -C motoezx -cvzf motoezx/ScummVM.pkg scummvm
-	$(RM) -r motoezx/scummvm
+	$(MKDIR) release/scummvm
+	$(STRIP) $(EXECUTABLE) -o release/scummvm/$(EXECUTABLE)
+	$(INSTALL) -c -m 644 $(DIST_FILES_THEMES) $(DIST_FILES_ENGINEDATA) release/scummvm/
+	$(INSTALL) -c -m 644 $(DIST_FILES_DOCS)  release/scummvm/
+	$(CP) $(srcdir)/backends/vkeybd/packs/vkeybd_default.zip release/scummvm/
+	$(CP) $(srcdir)/dists/motoezx/* release/scummvm/
+	tar -C release -cvzf release/ScummVM-motoezx.pkg scummvm
+	$(RM) -r release/scummvm
 
 # Special target to create a motomagx snapshot
 motomagx-mpkg: $(EXECUTABLE)
-	$(MKDIR) motomagx/scummvm
-	$(STRIP) $(EXECUTABLE) -o motomagx/scummvm/$(EXECUTABLE)
-	$(INSTALL) -c -m 644 $(DIST_FILES_THEMES) $(DIST_FILES_ENGINEDATA) motomagx/scummvm/
-	$(CP) $(srcdir)/backends/vkeybd/packs/vkeybd_default.zip motomagx/scummvm/
-	$(CP) $(srcdir)/dists/motomagx/mpkg/* motomagx/scummvm/
-	tar -C motomagx -cvzf motomagx/ScummVM.mpkg scummvm
-	$(RM) -r motomagx/scummvm
+	$(MKDIR) release/scummvm
+	$(STRIP) $(EXECUTABLE) -o release/scummvm/$(EXECUTABLE)
+	$(INSTALL) -c -m 644 $(DIST_FILES_THEMES) $(DIST_FILES_ENGINEDATA) release/scummvm/
+	$(INSTALL) -c -m 644 $(DIST_FILES_DOCS)  release/scummvm/
+	$(CP) $(srcdir)/backends/vkeybd/packs/vkeybd_default.zip release/scummvm/
+	$(CP) $(srcdir)/dists/motomagx/mpkg/* release/scummvm/
+	tar -C release -cvzf release/ScummVM-motomagx.mpkg scummvm
+	$(RM) -r release/scummvm
 
 motomagx-mgx: $(EXECUTABLE)
-	$(MKDIR) motomagx/scummvm
-	$(STRIP) $(EXECUTABLE) -o motomagx/scummvm/$(EXECUTABLE)
-	$(INSTALL) -c -m 644 $(DIST_FILES_THEMES) $(DIST_FILES_ENGINEDATA) motomagx/scummvm/
-	$(CP) $(srcdir)/backends/vkeybd/packs/vkeybd_default.zip motomagx/scummvm/
-	$(CP) $(srcdir)/dists/motomagx/mgx/* motomagx/scummvm/
-	tar -C motomagx -cvf motomagx/ScummVM.mgx scummvm
-	$(RM) -r motomagx/scummvm
+	$(MKDIR) release/scummvm
+	$(STRIP) $(EXECUTABLE) -o release/scummvm/$(EXECUTABLE)
+	$(INSTALL) -c -m 644 $(DIST_FILES_THEMES) $(DIST_FILES_ENGINEDATA) release/scummvm/
+	$(INSTALL) -c -m 644 $(DIST_FILES_DOCS)  release/scummvm/
+	$(CP) $(srcdir)/backends/vkeybd/packs/vkeybd_default.zip release/scummvm/
+	$(CP) $(srcdir)/dists/motomagx/mgx/* release/scummvm/
+	tar -C release -cvf release/ScummVM-motomagx.mgx scummvm
+	$(RM) -r release/scummvm
 
 motomagx-pep: $(EXECUTABLE)
-	$(MKDIR) motomagx/pep
-	$(CP) -r $(srcdir)/dists/motomagx/pep/* motomagx/pep
-	$(RM) -r motomagx/pep/app/.svn
-	$(STRIP) $(EXECUTABLE) -o motomagx/pep/app/$(EXECUTABLE)
-	$(INSTALL) -c -m 644 $(DIST_FILES_THEMES) $(DIST_FILES_ENGINEDATA) motomagx/pep/app
-	$(CP) $(srcdir)/backends/vkeybd/packs/vkeybd_default.zip motomagx/pep/app
-	tar -C motomagx/pep -czvf motomagx/ScummVM.pep app description.ini  scummvm_big_usr.png  scummvm_small_usr.png
-	$(RM) -r motomagx/pep
+	$(MKDIR) release/pep
+	$(CP) -r $(srcdir)/dists/motomagx/pep/* release/pep
+	$(RM) -r release/pep/app/.svn
+	$(STRIP) $(EXECUTABLE) -o release/pep/app/$(EXECUTABLE)
+	$(INSTALL) -c -m 644 $(DIST_FILES_THEMES) $(DIST_FILES_ENGINEDATA) release/pep/app
+	$(INSTALL) -c -m 644 $(DIST_FILES_DOCS)  release/pep/app
+	$(CP) $(srcdir)/backends/vkeybd/packs/vkeybd_default.zip release/pep/app
+	tar -C release/pep -czvf release/ScummVM-motomagx.pep app description.ini  scummvm_big_usr.png  scummvm_small_usr.png
+	$(RM) -r release/pep
 
 .PHONY: motoezx motomagx-mpkg motomagx-mgx motomagx-pep
