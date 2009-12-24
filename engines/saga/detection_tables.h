49c41a0c6c860a57e4797d4f95ed3c0a6ad0eb7f
Eugene Sandulenko <sev@scummvm.org>
2009-12-24 21:36:57 +0000

    Fix detection for ITE Floppy Italian
    svn-id: r46537

--- engines/saga/detection_tables.h
+++ engines/saga/detection_tables.h
@@ -583,7 +583,7 @@ static const SAGAGameDescription gameDescriptions[] = {
 			"ite",
 			"Floppy",
 			{
-	{"ite.rsc",		GAME_RESOURCEFILE,					"8f4315a9bb10ec839253108a032c8b54", 8905108},
+	{"ite.rsc",		GAME_RESOURCEFILE,					"8f4315a9bb10ec839253108a032c8b54", 8905106},
 	{"scripts.rsc",	GAME_SCRIPTFILE,					"516f7330f8410057b834424ea719d1ef", 340726},
 	{ NULL, 0, NULL, 0}
 			},
