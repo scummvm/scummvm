ddce48ed03a0bb5c28c5f13e9e3c5016e3bed2a1
Max Horn <max@quendi.de>
2009-12-26 01:38:57 +0000

    Remove x86 ASM version of sykcpt code, just use the generic code everywhere
    svn-id: r46574

--- tools/skycpt/KmpSearch.h
+++ tools/skycpt/KmpSearch.h
@@ -23,17 +23,12 @@
 #ifndef __KmpSearch__
 #define __KmpSearch__
 
-#ifndef _MSC_VER
-#define __fastcall
-#endif
-
 class KmpSearch {
 public:
-	void __fastcall init(const char *subStr);
-	const char * __fastcall search(const char *str);
-//private:
-	char _retarget[256], _subStr[256];
-	unsigned long _strLen;
+	void init(const char *subStr);
+	const char *search(const char *str);
+private:
+	char _subStr[256];
 };
 
 #endif //__KmpSearch__
