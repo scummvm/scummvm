16966727e988773eecfa2d0bdb8d15eb8ca5c6ef
Johannes Schickel <lordhoto@scummvm.org>
2009-12-30 16:29:29 +0000

    Commit of patch 6 "Add MSVC10 dists folder (batch file and readme)" from fr #2891357 "create_msvc: Add support for MSVC10".
    svn-id: r46762

---
+++ dists/msvc10/create_msvc10.bat
@@ -0,0 +1,34 @@
+@echo off
+echo.
+echo Automatic creation of the MSVC10 project files
+echo.
+
+if not exist create_msvc.exe goto no_tool
+
+:question
+echo.
+set /p batchanswer="Enable (S)table engines only, or (A)ll engines? (S/A)"
+if "%batchanswer%"=="s" goto stable
+if "%batchanswer%"=="S" goto stable
+if "%batchanswer%"=="a" goto all
+if "%batchanswer%"=="A" goto all
+goto question
+
+:no_tool
+echo create_msvc.exe not found in the current folder.
+echo You need to build it first and copy it in this
+echo folder
+goto done
+
+:all
+echo Creating project files with all engines enabled (stable and unstable)
+create_msvc ..\.. --enable-all-engines --msvc-version 10
+goto done
+
+:stable
+echo Creating normal project files, with only the stable engines enabled
+create_msvc ..\.. --msvc-version 10
+goto done
+
+:done
+pause
