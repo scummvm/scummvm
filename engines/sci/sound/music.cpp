54fc5121568eda38ec21897cb295a32471ce6d3a
Walter van Niftrik <waltervn@users.sourceforge.net>
2010-01-08 14:02:08 +0000

    SCI: Use adlib music by default
    svn-id: r47155

--- engines/sci/sound/music.cpp
+++ engines/sci/sound/music.cpp
@@ -67,6 +67,9 @@ void SciMusic::init() {
 	const MidiDriverDescription *md = MidiDriver::findMusicDriver(ConfMan.get("music_driver"));
 	_midiType = md ? md->id : MD_AUTO;
 
+	if (_midiType == MD_AUTO)
+		_midiType = MD_ADLIB;
+
 	switch (_midiType) {
 	case MD_ADLIB:
 		// FIXME: There's no Amiga sound option, so we hook it up to Adlib
