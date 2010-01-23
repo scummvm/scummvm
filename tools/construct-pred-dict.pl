a469913c60657461541c6cb37442a92514440ed3
Arnaud Boutonné <strangerke@users.sourceforge.net>
2010-01-23 15:27:47 +0000

    Tools - Replace some more headers by standard headers
    svn-id: r47478

--- tools/construct-pred-dict.pl
+++ tools/construct-pred-dict.pl
@@ -1,7 +1,10 @@
 #!perl
 #
 # ScummVM - Scumm Interpreter
-# Copyright (C) 2006 The ScummVM project
+#
+# ScummVM is the legal property of its developers, whose names
+# are too numerous to list here. Please refer to the COPYRIGHT
+# file distributed with this source distribution.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
--- tools/md5table.c
+++ tools/md5table.c
@@ -1,5 +1,8 @@
 /* md5table - Convert a MD5 table to either PHP or C++ code
- * Copyright (C) 2003-2006 The ScummVM Team
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
