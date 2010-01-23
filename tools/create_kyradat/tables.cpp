e488e0f7d5da43263ca373c60222e66d66852767
Arnaud Boutonné <strangerke@users.sourceforge.net>
2010-01-23 15:00:11 +0000

    Tools - Use standard header
    svn-id: r47476

--- tools/create_kyradat/tables.cpp
+++ tools/create_kyradat/tables.cpp
@@ -1,5 +1,8 @@
-/* ScummVM Tools
- * Copyright (C) 2009 The ScummVM project
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
--- tools/create_kyradat/util.cpp
+++ tools/create_kyradat/util.cpp
@@ -1,5 +1,8 @@
-/* Scumm Tools
- * Copyright (C) 2003-2006  The ScummVM Team
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
