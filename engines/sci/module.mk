d6611ec7dcca72c52021dcbb5c37e107b16361de
Travis Howell <kirben@scummvm.org>
2010-01-31 12:54:48 +0000

    Sort order.
    svn-id: r47748

--- engines/sci/module.mk
+++ engines/sci/module.mk
@@ -30,22 +30,22 @@ MODULE_OBJS := \
 	engine/selector.o \
 	engine/seg_manager.o \
 	engine/segment.o \
-	engine/static_selectors.o \
 	engine/state.o \
+	engine/static_selectors.o \
 	engine/vm.o \
-	graphics/gui.o \
-	graphics/ports.o \
-	graphics/paint16.o \
-	graphics/cache.o \
 	graphics/animate.o \
+	graphics/cache.o \
 	graphics/compare.o \
 	graphics/controls.o \
 	graphics/cursor.o \
 	graphics/font.o \
+	graphics/gui.o \
 	graphics/menu.o \
+	graphics/paint16.o \
 	graphics/palette.o \
 	graphics/picture.o \
 	graphics/portrait.o \
+	graphics/ports.o \
 	graphics/screen.o \
 	graphics/text.o \
 	graphics/transitions.o \
@@ -57,14 +57,14 @@ MODULE_OBJS := \
 	sound/midiparser_sci.o \
 	sound/music.o \
 	sound/soundcmd.o \
-	sound/iterator/core.o \
-	sound/iterator/iterator.o \
-	sound/iterator/songlib.o \
 	sound/drivers/adlib.o \
 	sound/drivers/amiga.o \
 	sound/drivers/fb01.o \
 	sound/drivers/midi.o \
 	sound/drivers/pcjr.o \
+	sound/iterator/core.o \
+	sound/iterator/iterator.o \
+	sound/iterator/songlib.o \
 	video/seq_decoder.o
 	
 	
