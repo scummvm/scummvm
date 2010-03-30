dda14f32cce546393e90571b5e7f1385291ef69a
Nicola Mettifogo <peres@scummvm.org>
2010-03-30 06:01:26 +0000

    (Backport) Let Parallaction return game-specific gameids. This completes the fix for bug 2969234 after LordHoto's patch.
    svn-id: r48423

--- engines/parallaction/detection.cpp
+++ engines/parallaction/detection.cpp
@@ -234,7 +234,7 @@ static const ADParams detectionParams = {
 	// Structure for autoupgrading obsolete targets
 	0,
 	// Name of single gameid (optional)
-	"parallaction",
+	0,
 	// List of files for file-based fallback detection (optional)
 	0,
 	// Flags
