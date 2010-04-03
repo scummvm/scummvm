eb1fc08122929211a94e081c8dd6183d65c338b5
Travis Howell <kirben@scummvm.org>
2010-04-03 14:14:48 +0000

    Disable the load from launcher button, in all games of Parallaction game engine.
    svn-id: r48487

--- engines/parallaction/detection.cpp
+++ engines/parallaction/detection.cpp
@@ -240,7 +240,7 @@ static const ADParams detectionParams = {
 	// Flags
 	0,
 	// Additional GUI options (for every game}
-	Common::GUIO_NONE
+	Common::GUIO_NOLAUNCHLOAD
 };
 
 class ParallactionMetaEngine : public AdvancedMetaEngine {
