5a17a7be5f4cdd4bc0b805c1dc5c47e2190d5490
Max Horn <max@quendi.de>
2010-04-13 09:54:21 +0000

    Patch #2984292: "AmigaOS4: Remove _SVN from unstable builds"
    svn-id: r48652

--- ports.mk
+++ ports.mk
@@ -173,8 +173,8 @@ win32dist: $(EXECUTABLE)
 # Special target to create an AmigaOS snapshot installation
 aos4dist: $(EXECUTABLE)
 	mkdir -p $(AOS4PATH)
-	$(STRIP) $(EXECUTABLE) -o $(AOS4PATH)/$(EXECUTABLE)_SVN
-	cp icons/scummvm.info $(AOS4PATH)/$(EXECUTABLE)_SVN.info
+	$(STRIP) $(EXECUTABLE) -o $(AOS4PATH)/$(EXECUTABLE)
+	cp icons/scummvm.info $(AOS4PATH)/$(EXECUTABLE).info
 	cp $(DIST_FILES_THEMES) $(AOS4PATH)/themes/
 	cp $(DIST_FILES_ENGINEDATA) $(AOS4PATH)/extras/
 	cp $(srcdir)/AUTHORS $(AOS4PATH)/AUTHORS.txt
