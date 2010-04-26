249df2c0285b82ef0f3bcee568362c6d44db5116
Neil Millstone <agent-q@users.sourceforge.net>
2010-04-26 17:57:03 +0000

    DS: Updating version number before the 1.1.1 tagging
    svn-id: r48808

--- backends/platform/ds/arm9/source/dsmain.cpp
+++ backends/platform/ds/arm9/source/dsmain.cpp
@@ -3162,7 +3162,7 @@ int main(void) {
 	consolePrintf("-------------------------------\n");
 	consolePrintf("ScummVM DS\n");
 	consolePrintf("Ported by Neil Millstone\n");
-	consolePrintf("Version 1.1.0 ");
+	consolePrintf("Version 1.1.1 ");
 #if defined(DS_BUILD_A)
 	consolePrintf("build A\n");
 	consolePrintf("Lucasarts SCUMM games (SCUMM)\n");
