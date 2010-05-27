0fdfd5d47b5a50b6125bfeca92d83d49b09a6ee1
Tony Puccinelli <toneman1138@users.sourceforge.net>
2010-05-27 05:03:09 +0000

    added files for ps2 loadable modules (mainly transplanted from psp code with VERY minor tweaks)
    svn-id: r49256

--- backends/plugins/ps2/ps2-provider.cpp
+++ backends/plugins/ps2/ps2-provider.cpp
@@ -1,3 +1,28 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * $URL: https://scummvm.svn.sourceforge.net/svnroot/scummvm/scummvm/branches/gsoc2010-plugins/backends/plugins/ps2/ps2-provider.cpp $
+ * $Id: ps2-provider.cpp 49253 2010-05-26 5:17:18Z Toneman $
+ *
+ */
+
 #if defined(DYNAMIC_MODULES) && defined(__PS2__)
 
 #include "backends/plugins/ps2/ps2-provider.h"
