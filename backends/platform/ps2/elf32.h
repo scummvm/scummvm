df2ee91e45b75f7982f19bccb10a0600f5e051b7
Tony Puccinelli <toneman1138@users.sourceforge.net>
2010-05-28 00:53:18 +0000

    added svn keywords 'Id' and 'URL' to new files
    svn-id: r49282

--- backends/platform/ps2/elf32.h
+++ backends/platform/ps2/elf32.h
@@ -18,8 +18,8 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
- * $URL: https://scummvm.svn.sourceforge.net/svnroot/scummvm/scummvm/branches/gsoc2010-plugins/backends/platform/ps2/elf32.h $
- * $Id: elf32.h 49253 2010-05-12 05:26:54Z Toneman $
+ * $URL$
+ * $Id$
  *
  */
 
--- backends/platform/ps2/ps2loader.cpp
+++ backends/platform/ps2/ps2loader.cpp
@@ -18,8 +18,8 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
- * $URL: https://scummvm.svn.sourceforge.net/svnroot/scummvm/scummvm/branches/gsoc2010-plugins/backends/platform/ps2/ps2loader.cpp $
- * TODO: $ID
+ * $URL$
+ * $Id$
  *
  */
 
