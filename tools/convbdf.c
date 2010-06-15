a8598767c627d8be616f31855b1ff5d6e0f752f9
Johannes Schickel <lordhoto@scummvm.org>
2010-06-15 19:25:45 +0000

    Fix some warnings inside convbdf.c
    svn-id: r49887

--- tools/convbdf.c
+++ tools/convbdf.c
@@ -38,12 +38,12 @@
 #include <time.h>
 
 int READ_UINT16(void *addr) {
-	unsigned char *buf = addr;
+	unsigned char *buf = (unsigned char *)addr;
 	return (buf[0] << 8) | buf[1];
 }
 
 void WRITE_UINT16(void *addr, int value) {
-	unsigned char *buf = addr;
+	unsigned char *buf = (unsigned char *)addr;
 	buf[0] = (value >> 8) & 0xFF;
 	buf[1] = value & 0xFF;
 }
@@ -611,7 +611,7 @@ int bdf_read_bitmaps(FILE *fp, struct font* pf) {
 	/* determine whether font doesn't require encode table*/
 	l = 0;
 	for (i = 0; i < pf->size; ++i) {
-		if (pf->offset[i] != l) {
+		if (pf->offset[i] != (unsigned long)l) {
 			encodetable = 1;
 			break;
 		}
@@ -648,7 +648,7 @@ int bdf_read_bitmaps(FILE *fp, struct font* pf) {
 
 	/* reallocate bits array to actual bits used*/
 	if (ofs < pf->bits_size) {
-		pf->bits = realloc(pf->bits, ofs * sizeof(bitmap_t));
+		pf->bits = (bitmap_t *)realloc(pf->bits, ofs * sizeof(bitmap_t));
 		pf->bits_size = ofs;
 	}
 	else {
