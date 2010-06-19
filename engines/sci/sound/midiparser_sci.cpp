a845e8b859e215a24e68bfbc5990153faf733548
Martin Kiewitz <m_kiewitz@users.sourceforge.net>
2010-06-19 19:39:36 +0000

    SCI: move setVolume down inside loadMusic so that it actually affects the used channels
    svn-id: r50058

--- engines/sci/sound/midiparser_sci.cpp
+++ engines/sci/sound/midiparser_sci.cpp
@@ -81,15 +81,15 @@ bool MidiParser_SCI::loadMusic(SoundResource::Track *track, MusicEntry *psnd, in
 	_channelRemap[9] = 9; // never map channel 9, because that's used for percussion
 	_channelRemap[15] = 15; // never map channel 15, because thats used by sierra internally
 
-	if (_pSnd)
-		setVolume(psnd->volume);
-
 	if (channelFilterMask) {
 		// SCI0 only has 1 data stream, but we need to filter out channels depending on music hardware selection
 		midiFilterChannels(channelFilterMask);
 	} else {
 		midiMixChannels();
 	}
+
+	if (_pSnd)
+		setVolume(_pSnd->volume);
 
 	_num_tracks = 1;
 	_tracks[0] = _mixedData;
