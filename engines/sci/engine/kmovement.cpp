6240e5353e1b26e78893bf2b7bca182b00fcd379
Martin Kiewitz <m_kiewitz@users.sourceforge.net>
2010-07-19 17:27:56 +0000

    SCI: kDoBresen - check xLast/yLast selectors
    fixed mixed-up fairy tales demo
    
    svn-id: r51035

--- engines/sci/engine/kmovement.cpp
+++ engines/sci/engine/kmovement.cpp
@@ -268,9 +268,12 @@ reg_t kDoBresen(EngineState *s, int argc, reg_t *argv) {
 	axis = (int16)readSelectorValue(segMan, mover, SELECTOR(b_xAxis));
 
 	if ((getSciVersion() >= SCI_VERSION_1_LATE)) {
-		// save last position into mover
-		writeSelectorValue(segMan, mover, SELECTOR(xLast), x);
-		writeSelectorValue(segMan, mover, SELECTOR(yLast), y);
+		// Mixed-Up Fairy Tales has no xLast/yLast selectors
+		if (SELECTOR(xLast) != -1) {
+			// save last position into mover
+			writeSelectorValue(segMan, mover, SELECTOR(xLast), x);
+			writeSelectorValue(segMan, mover, SELECTOR(yLast), y);
+		}
 	}
 
 	//printf("movecnt %d, move speed %d\n", movcnt, max_movcnt);
