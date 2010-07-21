781aee909bd029f51fe8348dc1158f6835fc7d5b
Martin Kiewitz <m_kiewitz@users.sourceforge.net>
2010-07-21 20:28:53 +0000

    SCI: r51104 adding #ifdef and check for hires
    pq4cd is probably low-res, so adding check for hires and adding #ifdef as well
    
    svn-id: r51106

--- engines/sci/engine/kgraphics.cpp
+++ engines/sci/engine/kgraphics.cpp
@@ -427,11 +427,15 @@ reg_t kCelWide(EngineState *s, int argc, reg_t *argv) {
 
 	celWidth = g_sci->_gfxCache->kernelViewGetCelWidth(viewId, loopNo, celNo);
 
-	if (getSciVersion() == SCI_VERSION_2_1)
-		celWidth = celWidth / 2; // half the width returned here, fixes lsl6 action icon placements
+#ifdef ENABLE_SCI32
+	if (getSciVersion() == SCI_VERSION_2_1) {
+		if (g_sci->_gfxScreen->getWidth() > 320)
+			celWidth = celWidth / 2; // half the width returned here, fixes lsl6 action icon placements
+	}
 	// the scripts work low-res and add the returned value from here to the coordinate
 	// TODO: check, if this is actually right. I'm slightly confused by this, but even GK1CD has some idivs in this
 	//  code, so it seems plausible.
+#endif
 	return make_reg(0, celWidth);
 }
 
