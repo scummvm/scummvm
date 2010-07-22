ba47a427b2d8c511a2e9ebbddce28dce0c8d19d8
Martin Kiewitz <m_kiewitz@users.sourceforge.net>
2010-07-22 21:40:10 +0000

    SCI: sci2+ has a different kSetCursor order
    fixes gk1 mouse cursor issue
    thx to wjp
    
    svn-id: r51168

--- engines/sci/engine/kgraphics.cpp
+++ engines/sci/engine/kgraphics.cpp
@@ -144,14 +144,19 @@ static reg_t kSetCursorSci11(EngineState *s, int argc, reg_t *argv) {
 		g_sci->_gfxCursor->kernelSetPos(pos);
 		break;
 	case 4: {
-		int16 top = argv[0].toSint16();
-		int16 left = argv[1].toSint16();
-		int16 bottom = argv[2].toSint16();
-		int16 right = argv[3].toSint16();
+		int16 top, left, bottom, right;
 
-		// In SCI32, the right parameter seems to be divided by 2
-		if (getSciVersion() >= SCI_VERSION_2)
-			right *= 2;
+		if (getSciVersion() >= SCI_VERSION_2) {
+			top = argv[1].toSint16();
+			left = argv[0].toSint16();
+			bottom = argv[3].toSint16();
+			right = argv[2].toSint16();
+		} else {
+			top = argv[0].toSint16();
+			left = argv[1].toSint16();
+			bottom = argv[2].toSint16();
+			right = argv[3].toSint16();
+		}
 
 		if ((right >= left) && (bottom >= top)) {
 			Common::Rect rect = Common::Rect(left, top, right, bottom);
