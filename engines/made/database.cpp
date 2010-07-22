68783fa01cb144e676137444126e3c9d6caf0fa9
Yotam Barnoy <yotambarnoy@gmail.com>
2010-07-22 11:20:16 +0000

    MADE: fixed bug 3029750 caused by caching from commit 46102.
    Cache needed to be cleared every time we load a game. Otherwise, those platforms that happen to use the exact same addresses (PC) were ok, but those platforms that had different addresses (PSP for example) would get messed up.
    
    svn-id: r51138

--- engines/made/database.cpp
+++ engines/made/database.cpp
@@ -515,6 +515,8 @@ int16 GameDatabaseV2::loadgame(const char *filename, int16 version) {
 		_objects[i]->load(*in);
 	}
 	delete in;
+	
+	_objectPropertyCache.clear();	// make sure to clear cache
 	return result;
 }
 
@@ -644,6 +646,8 @@ void GameDatabaseV3::load(Common::SeekableReadStream &sourceS) {
 void GameDatabaseV3::reloadFromStream(Common::SeekableReadStream &sourceS) {
 	sourceS.seek(_gameStateOffs);
 	sourceS.read(_gameState, _gameStateSize);
+	
+	_objectPropertyCache.clear();	// make sure to clear cache
 }
 
 bool GameDatabaseV3::getSavegameDescription(const char *filename, Common::String &description, int16 version) {
@@ -734,6 +738,9 @@ int16 GameDatabaseV3::loadgame(const char *filename, int16 version) {
 	in->skip(64); // skip savegame description
 	in->read(_gameState, _gameStateSize);
 	delete in;
+	
+	_objectPropertyCache.clear();	// make sure to clear cache
+	
 	return 0;
 }
 
