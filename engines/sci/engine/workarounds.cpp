bc04a8377be36480b4a4b737e7f3ca14cde62e6e
Martin Kiewitz <m_kiewitz@users.sourceforge.net>
2010-07-24 07:45:47 +0000

    SCI: adding workaround for room 150 sq4
    svn-id: r51237

--- engines/sci/engine/workarounds.cpp
+++ engines/sci/engine/workarounds.cpp
@@ -113,6 +113,7 @@ const SciWorkaroundEntry kDisplay_workarounds[] = {
 //    gameID,           room,script,lvl,          object-name, method-name,    call,index,                workaround
 const SciWorkaroundEntry kDisposeScript_workarounds[] = {
     { GID_QFG1,           64,    64,  0,               "rm64", "dispose",        -1,    0, { WORKAROUND_IGNORE,    0 } }, // when leaving graveyard, parameter 0 is an object
+    { GID_SQ4,           150,   151,  0,        "fightScript", "dispose",        -1,    0, { WORKAROUND_IGNORE,    0 } }, // during fight with vohaul, parameter 0 is an object
     SCI_WORKAROUNDENTRY_TERMINATOR
 };
 
