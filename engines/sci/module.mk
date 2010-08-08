f5d1482c64d1969bf77f5107a792bff84dc7e903
Sven Hesse <drmccoy@users.sourceforge.net>
2010-08-08 01:09:20 +0000

    SCI: Remove the now useless Sci::VMDDecoder wrapper
    svn-id: r51923

--- engines/sci/module.mk
+++ engines/sci/module.mk
@@ -77,8 +77,7 @@ ifdef ENABLE_SCI32
 MODULE_OBJS += \
 	graphics/frameout.o \
 	graphics/paint32.o \
-	graphics/robot.o \
-	video/vmd_decoder.o
+	graphics/robot.o
 endif
 
 # This module can be built as a plugin
