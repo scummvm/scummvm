98a8c01dbb4498edfb4f310b15334d6139f614c3
Filippos Karapetis <bluegr@gmail.com>
2010-08-18 07:14:17 +0000

    SCI: Fixed bug #3038433 - "HOYLE1: Crashes"
    svn-id: r52182

--- engines/sci/engine/workarounds.cpp
+++ engines/sci/engine/workarounds.cpp
@@ -48,6 +48,7 @@ const SciWorkaroundEntry opcodeDptoaWorkarounds[] = {
 
 //    gameID,           room,script,lvl,          object-name, method-name,    call,index,             workaround
 const SciWorkaroundEntry opcodeGeWorkarounds[] = {
+	{ GID_HOYLE1,           5,  213,  0,                   "", "export 0",       -1,    0, { WORKAROUND_FAKE,   1 } }, // happens sometimes during cribbage - bug #3038433
 	{ GID_PQ3,             31,   31,  0,              "rm031", "init",           -1,    0, { WORKAROUND_FAKE,   1 } }, // pq3 english: when exiting the car, while morales is making phonecalls - bug #3037565
 	SCI_WORKAROUNDENTRY_TERMINATOR
 };
