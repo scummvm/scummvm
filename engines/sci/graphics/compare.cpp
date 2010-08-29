fc3dfd1bf7fe319c8ab29b9b1991b798c6b83109
Martin Kiewitz <m_kiewitz@users.sourceforge.net>
2010-08-29 18:19:11 +0000

    SCI: adding comment about invalid rect return val
    svn-id: r52444

--- engines/sci/graphics/compare.cpp
+++ engines/sci/graphics/compare.cpp
@@ -187,7 +187,7 @@ reg_t GfxCompare::kernelCanBeHere(reg_t curObject, reg_t listReference) {
 
 	if (!checkRect.isValidRect()) {	// can occur in Iceman and Mother Goose - HACK? TODO: is this really occuring in sierra sci? check this
 		warning("kCan(t)BeHere - invalid rect %d, %d -> %d, %d", checkRect.left, checkRect.top, checkRect.right, checkRect.bottom);
-		return NULL_REG;
+		return NULL_REG; // this means "can be here"
 	}
 
 	adjustedRect = _coordAdjuster->onControl(checkRect);
