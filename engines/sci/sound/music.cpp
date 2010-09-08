06afbd7fbbdb0c581103591dd130d5847966395c
Martin Kiewitz <m_kiewitz@users.sourceforge.net>
2010-09-08 10:49:12 +0000

    SCI: dont remap to channel 9 (fix real mt32)
    svn-id: r52634

--- engines/sci/sound/music.cpp
+++ engines/sci/sound/music.cpp
@@ -291,6 +291,8 @@ int16 SciMusic::tryToOwnChannel(MusicEntry *caller, int16 bestChannel) {
 	}
 	// otherwise look for unused channel
 	for (int channelNr = _driverFirstChannel; channelNr < 15; channelNr++) {
+		if (channelNr == 9) // never map to channel 9 (precussion)
+			continue;
 		if (!_usedChannel[channelNr]) {
 			_usedChannel[channelNr] = caller;
 			return channelNr;
