ca767d5913416d3fcfcec74c15e2b3e30042c22b
Florian Kagerer <athrxx@users.sourceforge.net>
2010-09-21 16:05:33 +0000

    KYRA2/LOL PC-98: fix sfx rate
    svn-id: r52842

--- engines/kyra/sound_towns.cpp
+++ engines/kyra/sound_towns.cpp
@@ -587,7 +587,7 @@ void SoundTownsPC98_v2::beginFadeOut() {
 }
 
 int32 SoundTownsPC98_v2::voicePlay(const char *file, Audio::SoundHandle *handle, uint8, bool) {
-	//static const uint16 rates[] =	{ 0x10E1, 0x0CA9, 0x0870, 0x0654, 0x0438, 0x032A, 0x021C, 0x0194 };
+	static const uint16 rates[] = { 0x10E1, 0x0CA9, 0x0870, 0x0654, 0x0438, 0x032A, 0x021C, 0x0194 };
 	static const char patternHOF[] = "%s.PCM";
 	static const char patternLOL[] = "%s.VOC";
 
@@ -608,7 +608,7 @@ int32 SoundTownsPC98_v2::voicePlay(const char *file, Audio::SoundHandle *handle,
 	if (!src)
 		return 0;
 
-	//uint16 sfxRate = rates[READ_LE_UINT16(src)];
+	uint16 sfxRate = rates[READ_LE_UINT16(src)];
 	src += 2;
 	bool compressed = (READ_LE_UINT16(src) & 1) ? true : false;
 	src += 2;
@@ -648,8 +648,7 @@ int32 SoundTownsPC98_v2::voicePlay(const char *file, Audio::SoundHandle *handle,
 		sfx[i] = cmd;
 	}
 
-	_currentSFX = Audio::makeRawStream(sfx, outsize, 11025,
-							Audio::FLAG_UNSIGNED | Audio::FLAG_LITTLE_ENDIAN);
+	_currentSFX = Audio::makeRawStream(sfx, outsize, sfxRate * 10, Audio::FLAG_UNSIGNED | Audio::FLAG_LITTLE_ENDIAN);
 	_mixer->playStream(Audio::Mixer::kSFXSoundType, &_soundChannels[h], _currentSFX);
 	if (handle)
 		*handle = _soundChannels[h];
