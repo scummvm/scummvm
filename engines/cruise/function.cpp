d221176c47d33618be8ab8f3a81da6384037d751
Johannes Schickel <lordhoto@scummvm.org>
2010-10-15 14:14:33 +0000

    CRUISE: Replace some strncpy with strlcpy.
    svn-id: r53495

--- engines/cruise/function.cpp
+++ engines/cruise/function.cpp
@@ -58,7 +58,7 @@ int16 Op_LoadOverlay() {
 
 	updateAllScriptsImports();
 
-	strncpy(nextOverlay, overlayName, 38);
+	Common::strlcpy(nextOverlay, overlayName, sizeof(nextOverlay));
 
 	return overlayLoadResult;
 }
