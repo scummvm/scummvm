34ff8269185dded206173796383eb3bd4732c769
Travis Howell <kirben@scummvm.org>
2010-10-18 04:23:30 +0000

    SCUMM: Check the sound channel is valid, before checking the sound channel.
    svn-id: r53561

--- engines/scumm/he/sound_he.cpp
+++ engines/scumm/he/sound_he.cpp
@@ -241,7 +241,7 @@ int SoundHE::isSoundCodeUsed(int sound) {
 			chan = i;
 	}
 
-	if (_mixer->isSoundHandleActive(_heSoundChannels[chan]) && chan != -1) {
+	if (chan != -1 && _mixer->isSoundHandleActive(_heSoundChannels[chan])) {
 		return _heChannel[chan].sbngBlock;
 	} else {
 		return 0;
@@ -255,7 +255,7 @@ int SoundHE::getSoundPos(int sound) {
 			chan = i;
 	}
 
-	if (_mixer->isSoundHandleActive(_heSoundChannels[chan]) && chan != -1) {
+	if (chan != -1 && _mixer->isSoundHandleActive(_heSoundChannels[chan])) {
 		int time =  _vm->getHETimer(chan + 4) * _heChannel[chan].rate / 1000;
 		return time;
 	} else {
