6dc5fccfe3ad475f1e079ed9915a62943aedf588
Max Horn <max@quendi.de>
2010-10-31 17:23:31 +0000

    WINCE: Fix assert() implementation
    svn-id: r53980

--- backends/platform/wince/missing/assert.h
+++ backends/platform/wince/missing/assert.h
@@ -3,7 +3,7 @@
 // defined in common/util.h
 void CDECL _declspec(noreturn) error(const char *s, ...);
 
-#define assert(e) ((e) ? 0 : (::error("Assertion failed " #e " (%s, %d)", __FILE__, __LINE__)))
+#define assert(e) ((e) ? 0 : (::error("Assertion failed %s (%s, %d)", #e, __FILE__, __LINE__)))
 
 #define abort() ::error("Abort (%s, %d)", __FILE__, __LINE__)
 
