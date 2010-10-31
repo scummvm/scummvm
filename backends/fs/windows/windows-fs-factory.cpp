917c4b00cb95f390ab75f77620c202866edb5254
Max Horn <max@quendi.de>
2010-10-31 00:14:59 +0000

    WINDOWS: Enable use of forbidden symbols
    svn-id: r53972

--- backends/fs/windows/windows-fs-factory.cpp
+++ backends/fs/windows/windows-fs-factory.cpp
@@ -23,6 +23,10 @@
  */
 
 #if defined(WIN32)
+
+// Disable symbol overrides so that we can use system headers.
+#define FORBIDDEN_SYMBOL_ALLOW_ALL
+
 #include "backends/fs/windows/windows-fs-factory.h"
 #include "backends/fs/windows/windows-fs.cpp"
 
