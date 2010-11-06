b105b5e659805b10fa149bc051fea8e4acc4fcfe
Arnaud Boutonné <strangerke@users.sourceforge.net>
2010-11-06 10:11:46 +0000

    TOOL: Add original palette in tool, disabled
    This may be required by purists, but is awful (thus 
    currently not used)
    
    svn-id: r54101

--- tools/create_hugo/staticdisplay.h
+++ tools/create_hugo/staticdisplay.h
@@ -35,6 +35,7 @@
 
 #define SIZE_PAL_ARRAY 64
 
+#if 1
 // Color table of standard 16 VGA colors
 // Values from "Programmers guide to EGA/VGA cards" Ferraro, p303
 #define V1 168                                      // Low intensity value
@@ -44,22 +45,49 @@
 
 
 byte _palette[SIZE_PAL_ARRAY] = {
-	0,   0,   0, 0,                               // BLACK
-	0,   0,  V1, 0,                               // BLUE
-	0,  V1,   0, 0,                               // GREEN
-	0,  V1,  V1, 0,                               // CYAN
-	V1,   0,   0, 0,                               // RED
-	V1,   0,  V1, 0,                               // MAGENTA
-	V1,  V3,   0, 0,                               // BROWN
-	V1,  V1,  V1, 0,                               // WHITE (LIGHT GRAY)
-	V3,  V3,  V3, 0,                               // GRAY  (DARK GRAY)
-	V4,  V4,  V2, 0,                               // LIGHTBLUE
-	V4,  V2,  V4, 0,                               // LIGHTGREEN
-	V4,  V2,  V2, 0,                               // LIGHTCYAN
-	V2,  V4,  V4, 0,                               // LIGHTRED
-	V2,  V4,  V2, 0,                               // LIGHTMAGENTA
-	V2,  V2,  V4, 0,                               // YELLOW
-	V2,  V2,  V2, 0                                // BRIGHTWHITE
+	0,   0,   0,  0,                                // BLACK
+	0,   0,   V1, 0,                                // BLUE
+	0,   V1,  0,  0,                                // GREEN
+	0,   V1,  V1, 0,                                // CYAN
+	V1,  0,   0,  0,                                // RED
+	V1,  0,   V1, 0,                                // MAGENTA
+	V1,  V3,  0,  0,                                // BROWN
+	V1,  V1,  V1, 0,                                // WHITE (LIGHT GRAY)
+	V3,  V3,  V3, 0,                                // GRAY  (DARK GRAY)
+	V4,  V4,  V2, 0,                                // LIGHTBLUE
+	V4,  V2,  V4, 0,                                // LIGHTGREEN
+	V4,  V2,  V2, 0,                                // LIGHTCYAN
+	V2,  V4,  V4, 0,                                // LIGHTRED
+	V2,  V4,  V2, 0,                                // LIGHTMAGENTA
+	V2,  V2,  V4, 0,                                // YELLOW
+	V2,  V2,  V2, 0                                 // BRIGHTWHITE
 };
+#else
+// Original paletter found in original exe. 
+// Currently disabled, because the result is quite ugly!
+// Color table of nearest standard 16 colors in system static palette
+#define C1 191                                      // Low intensity value
+#define C2 255                                      // High intensity value
+#define C3 127                                      // Special for Brown/Gray
+byte _palette[SIZE_PAL_ARRAY] = {
+	0,   0,   0,  0,                                // BLACK
+	0,   0,   C1, 0,                                // BLUE
+	0,   C1,  0,  0,                                // GREEN
+	0,   C1,  C1, 0,                                // CYAN
+	C1,  0,   0,  0,                                // RED
+	C1,  0,   C1, 0,                                // MAGENTA
+	C3,  C3,  0,  0,                                // BROWN
+	C1,  C1,  C1, 0,                                // WHITE (LIGHT GRAY)
+	C3,  C3,  C3, 0,                                // GRAY  (DARK GRAY)
+	0,   0,   C2, 0,                                // LIGHTBLUE
+	0,   C2,  0,  0,                                // LIGHTGREEN
+	0,   C2,  C2, 0,                                // LIGHTCYAN
+	C2,  0,   0,  0,                                // LIGHTRED
+	C2,  0,   C2, 0,                                // LIGHTMAGENTA
+	C2,  C2,  0,  0,                                // YELLOW
+	C2,  C2,  C2, 0                                 // BRIGHTWHITE
+};
+#endif
+
 
 #endif
