ce733ab75a9af19fe203930b37f9fc7fb8712cae
Johannes Schickel <lordhoto@scummvm.org>
2010-11-07 19:13:30 +0000

    TUCKER: Add GUIO_NOLAUNCHLOAD to the detection flags.
    svn-id: r54126

--- engines/tucker/detection.cpp
+++ engines/tucker/detection.cpp
@@ -123,7 +123,7 @@ static const ADParams detectionParams = {
 	// Flags
 	0,
 	// Additional GUI options (for every game}
-	Common::GUIO_NONE,
+	Common::GUIO_NOLAUNCHLOAD,
 	// Maximum directory depth
 	1,
 	// List of directory globs
