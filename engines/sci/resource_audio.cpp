8a788d16d779a651876ed3d4caf7fda0b3fa342c
Matthew Hoops <clone2727@gmail.com>
2010-11-09 16:06:33 +0000

    SCI: Fix compile when SCI32 is not enabled
    svn-id: r54160

--- engines/sci/resource_audio.cpp
+++ engines/sci/resource_audio.cpp
@@ -276,7 +276,7 @@ int ResourceManager::readAudioMapSCI11(ResourceSource *map) {
 #ifndef ENABLE_SCI32
 	// SCI32 support is not built in. Check if this is a SCI32 game
 	// and if it is abort here.
-	if (_volVersion == kResVersionSci32)
+	if (_volVersion >= kResVersionSci2)
 		return SCI_ERROR_RESMAP_NOT_FOUND;
 #endif
 
