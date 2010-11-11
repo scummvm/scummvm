2fcf5167ac11c3039527e8df508b52a1237d15d3
Arthur Blot <arthur.blot78@gmail.com>
2010-11-11 20:26:07 +0000

    ASYLUM: Small fix for object and actor coordinates
    git-svn-id: http://asylumengine.googlecode.com/svn/trunk@539 0bfb4aae-4ea4-11de-8d8d-752d95cf3e3c

--- engines/asylum/resources/object.cpp
+++ engines/asylum/resources/object.cpp
@@ -178,10 +178,12 @@ void Object::draw() {
 
 	// Draw the object
 	Common::Point point;
-	getScene()->adjustCoordinates(x, y, &point);
+	GraphicResource *gra = new GraphicResource(_vm, _resourceId);
+	GraphicFrame *fra = gra->getFrame(_frameIndex);
+	getScene()->adjustCoordinates(x + fra->x, y + fra->y, &point);
 
 	if (_field_67C <= 0 || _field_67C >= 4 || Config.performance <= 1) {
-		getScreen()->addGraphicToQueue(_resourceId, _frameIndex, x, y, (flags >> 11) & kObjectFlag2, _field_67C - 3, _priority);
+		getScreen()->addGraphicToQueue(_resourceId, _frameIndex, point.x, point.y, (flags >> 11) & kObjectFlag2, _field_67C - 3, _priority);
 	} else {
 		// TODO: Do Cross Fade
 		//getScreen()->addGraphicToQueue(_resourceId, _frameIndex, x, y, getWorld()->backgroundImage, getWorld()->xLeft, getWorld()->yTop, 0, 0, _field_67C - 1);
