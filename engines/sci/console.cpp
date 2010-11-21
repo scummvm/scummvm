788598a6899d7252164b1727ccaf1f7eb2d2b1c4
Filippos Karapetis <bluegr@gmail.com>
2010-11-21 10:31:02 +0000

    SCI: Fixed the "version" console command in SCI1.1 and earlier games
    svn-id: r54406

--- engines/sci/console.cpp
+++ engines/sci/console.cpp
@@ -453,7 +453,8 @@ bool Console::cmdGetVersion(int argc, const char **argv) {
 	DebugPrintf("Move count type: %s\n", (_engine->_features->handleMoveCount()) ? "increment" : "ignore");
 	DebugPrintf("SetCursor type: %s\n", getSciVersionDesc(_engine->_features->detectSetCursorType()));
 #ifdef ENABLE_SCI32
-	DebugPrintf("kString type: %s\n", (_engine->_features->detectSci2StringFunctionType() == kSci2StringFunctionOld) ? "SCI2 (old)" : "SCI2.1 (new)");
+	if (getSciVersion() >= SCI_VERSION_2)
+		DebugPrintf("kString type: %s\n", (_engine->_features->detectSci2StringFunctionType() == kSci2StringFunctionOld) ? "SCI2 (old)" : "SCI2.1 (new)");
 #endif
 	DebugPrintf("View type: %s\n", viewTypeDesc[g_sci->getResMan()->getViewType()]);
 	DebugPrintf("Uses palette merging: %s\n", g_sci->_gfxPalette->isMerging() ? "yes" : "no");
