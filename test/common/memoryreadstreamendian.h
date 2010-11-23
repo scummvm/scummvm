5d791cb090a6ead8594b956316f3735382703e8d
Max Horn <max@quendi.de>
2010-11-23 22:27:00 +0000

    TEST: Add/extend MemoryReadStream(Endian) test cases
    svn-id: r54440

--- test/common/memoryreadstreamendian.h
+++ test/common/memoryreadstreamendian.h
@@ -2,11 +2,11 @@
 
 #include "common/memstream.h"
 
-class MemoryReadStreamTestSuite : public CxxTest::TestSuite {
+class MemoryReadStreamEndianTestSuite : public CxxTest::TestSuite {
 	public:
 	void test_seek_set() {
 		byte contents[] = { 'a', 'b', '\n', '\n', 'c', '\n' };
-		Common::MemoryReadStream ms(contents, sizeof(contents));
+		Common::MemoryReadStreamEndian ms(contents, sizeof(contents), false);
 
 		ms.seek(0, SEEK_SET);
 		TS_ASSERT_EQUALS(ms.pos(), 0);
@@ -23,7 +23,7 @@ class MemoryReadStreamTestSuite : public CxxTest::TestSuite {
 
 	void test_seek_cur() {
 		byte contents[] = { 'a', 'b', '\n', '\n', 'c' };
-		Common::MemoryReadStream ms(contents, sizeof(contents));
+		Common::MemoryReadStreamEndian ms(contents, sizeof(contents), false);
 
 		ms.seek(3, SEEK_CUR);
 		TS_ASSERT_EQUALS(ms.pos(), 3);
@@ -44,7 +44,7 @@ class MemoryReadStreamTestSuite : public CxxTest::TestSuite {
 
 	void test_seek_end() {
 		byte contents[] = { 'a', 'b', '\n', '\n', 'c' };
-		Common::MemoryReadStream ms(contents, sizeof(contents));
+		Common::MemoryReadStreamEndian ms(contents, sizeof(contents), false);
 
 		ms.seek(0, SEEK_END);
 		TS_ASSERT_EQUALS(ms.pos(), 5);
@@ -61,7 +61,7 @@ class MemoryReadStreamTestSuite : public CxxTest::TestSuite {
 
 	void test_seek_read_le() {
 		byte contents[] = { 1, 2, 3, 4, 5, 6, 7 };
-		Common::MemoryReadStream ms(contents, sizeof(contents));
+		Common::MemoryReadStreamEndian ms(contents, sizeof(contents), false);
 
 		TS_ASSERT_EQUALS(ms.readUint16LE(), 0x0201UL);
 		TS_ASSERT_EQUALS(ms.pos(), 2);
@@ -74,11 +74,37 @@ class MemoryReadStreamTestSuite : public CxxTest::TestSuite {
 
 	void test_seek_read_be() {
 		byte contents[] = { 1, 2, 3, 4, 5, 6, 7 };
-		Common::MemoryReadStream ms(contents, sizeof(contents));
+		Common::MemoryReadStreamEndian ms(contents, sizeof(contents), false);
 
 		TS_ASSERT_EQUALS(ms.readUint16BE(), 0x0102UL);
 		TS_ASSERT_EQUALS(ms.pos(), 2);
 		TS_ASSERT_EQUALS(ms.readUint32BE(), 0x03040506UL);
+		TS_ASSERT_EQUALS(ms.pos(), 6);
+		TS_ASSERT_EQUALS(ms.readByte(), 0x07);
+		TS_ASSERT_EQUALS(ms.pos(), 7);
+		TS_ASSERT(!ms.eos());
+	}
+
+	void test_seek_read_le2() {
+		byte contents[] = { 1, 2, 3, 4, 5, 6, 7 };
+		Common::MemoryReadStreamEndian ms(contents, sizeof(contents), false);
+
+		TS_ASSERT_EQUALS(ms.readUint16(), 0x0201UL);
+		TS_ASSERT_EQUALS(ms.pos(), 2);
+		TS_ASSERT_EQUALS(ms.readUint32(), 0x06050403UL);
+		TS_ASSERT_EQUALS(ms.pos(), 6);
+		TS_ASSERT_EQUALS(ms.readByte(), 0x07);
+		TS_ASSERT_EQUALS(ms.pos(), 7);
+		TS_ASSERT(!ms.eos());
+	}
+
+	void test_seek_read_be2() {
+		byte contents[] = { 1, 2, 3, 4, 5, 6, 7 };
+		Common::MemoryReadStreamEndian ms(contents, sizeof(contents), true);
+
+		TS_ASSERT_EQUALS(ms.readUint16(), 0x0102UL);
+		TS_ASSERT_EQUALS(ms.pos(), 2);
+		TS_ASSERT_EQUALS(ms.readUint32(), 0x03040506UL);
 		TS_ASSERT_EQUALS(ms.pos(), 6);
 		TS_ASSERT_EQUALS(ms.readByte(), 0x07);
 		TS_ASSERT_EQUALS(ms.pos(), 7);
