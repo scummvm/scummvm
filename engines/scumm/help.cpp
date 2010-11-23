f20da5f98c8a27917e425109659ae2a685d4ec08
Thierry Crozat <criezy@scummvm.org>
2010-11-23 22:47:20 +0000

    I18N: Add translation of the SCUMM help dialog.
    svn-id: r54449

--- engines/scumm/help.cpp
+++ engines/scumm/help.cpp
@@ -30,6 +30,7 @@
 
 #include "scumm/help.h"
 #include "scumm/scumm.h"
+#include "common/translation.h"
 
 namespace Scumm {
 
@@ -72,216 +73,216 @@ void ScummHelp::updateStrings(byte gameId, byte version, Common::Platform platfo
 	int i = 0;
 	switch (page) {
 	case 1:
-		title = "Common keyboard commands:";
-		ADD_BIND("F5", "Save / Load dialog");
+		title = _("Common keyboard commands:");
+		ADD_BIND("F5", _("Save / Load dialog"));
 		if (version >= 5)
-			ADD_BIND(".", "Skip line of text");
-		ADD_BIND("Esc", "Skip cutscene");
-		ADD_BIND("Space", "Pause game");
-		ADD_BIND("Ctrl 0-9", "Load game state 1-10");
-		ADD_BIND("Alt 0-9", "Save game state 1-10");
+			ADD_BIND(".", _("Skip line of text"));
+		ADD_BIND(_("Esc"), _("Skip cutscene"));
+		ADD_BIND(_("Space"), _("Pause game"));
+		ADD_BIND("Ctrl 0-9", _("Load game state 1-10"));
+		ADD_BIND("Alt 0-9", _("Save game state 1-10"));
 #ifdef MACOSX
-		ADD_BIND("Cmd q", "Quit");
+		ADD_BIND("Cmd q", _("Quit"));
 #else
-		ADD_BIND("Alt x, Ctrl z", "Quit");
+		ADD_BIND("Alt x, Ctrl z", _("Quit"));
 #endif
-		ADD_BIND("Alt Enter", "Toggle fullscreen");
-		ADD_BIND("[, ]", "Music volume up / down");
-		ADD_BIND("-, +", "Text speed slower / faster");
-		ADD_BIND("Enter", "Simulate left mouse button");
-		ADD_BIND("Tab", "Simulate right mouse button");
+		ADD_BIND(_("Alt Enter"), _("Toggle fullscreen"));
+		ADD_BIND("[, ]", _("Music volume up / down"));
+		ADD_BIND("-, +", _("Text speed slower / faster"));
+		ADD_BIND(_("Enter"), _("Simulate left mouse button"));
+		ADD_BIND(_("Tab"), _("Simulate right mouse button"));
 		break;
 	case 2:
-		title = "Special keyboard commands:";
-		ADD_BIND("~, #", "Show / Hide console");
-		ADD_BIND("Ctrl d", "Start the debugger");
-		ADD_BIND("Ctrl s", "Show memory consumption");
-		ADD_BIND("Ctrl f", "Run in fast mode (*)");
-		ADD_BIND("Ctrl g", "Run in really fast mode (*)");
-		ADD_BIND("Ctrl m", "Toggle mouse capture");
-		ADD_BIND("Ctrl Alt 1-8", "Switch between graphics filters");
-		ADD_BIND("Ctrl Alt +, -", "Increase / Decrease scale factor");
-		ADD_BIND("Ctrl Alt a", "Toggle aspect-ratio correction");
+		title = _("Special keyboard commands:");
+		ADD_BIND("~, #", _("Show / Hide console"));
+		ADD_BIND("Ctrl d", _("Start the debugger"));
+		ADD_BIND("Ctrl s", _("Show memory consumption"));
+		ADD_BIND("Ctrl f", _("Run in fast mode (*)"));
+		ADD_BIND("Ctrl g", _("Run in really fast mode (*)"));
+		ADD_BIND("Ctrl m", _("Toggle mouse capture"));
+		ADD_BIND("Ctrl Alt 1-8", _("Switch between graphics filters"));
+		ADD_BIND("Ctrl Alt +, -", _("Increase / Decrease scale factor"));
+		ADD_BIND("Ctrl Alt a", _("Toggle aspect-ratio correction"));
 		ADD_LINE;
 		ADD_LINE;
 		// FIXME: This should use word-wrapping, and should not assume
 		//        that the font is mono-spaced.
-		ADD_TEXT("* Note that using ctrl-f and");
-		ADD_TEXT("  ctrl-g are not recommended");
-		ADD_TEXT("  since they may cause crashes");
-		ADD_TEXT("  or incorrect game behaviour.");
+		ADD_TEXT(_("* Note that using ctrl-f and"));
+		ADD_TEXT(_("  ctrl-g are not recommended"));
+		ADD_TEXT(_("  since they may cause crashes"));
+		ADD_TEXT(_("  or incorrect game behaviour."));
 		break;
 	case 3:
 		if (gameId == GID_LOOM)
-			title = "Spinning drafts on the keyboard:";
+			title = _("Spinning drafts on the keyboard:");
 		else
-			title = "Main game controls:";
+			title = _("Main game controls:");
 		switch (gameId) {
 		case GID_ZAK:
 		case GID_MANIAC:
 			if (platform == Common::kPlatformNES) {
-				ADD_BIND("q", "Push");
-				ADD_BIND("a", "Pull");
-				ADD_BIND("z", "Give");
-				ADD_BIND("w", "Open");
-				ADD_BIND("s", "Close");
-				ADD_BIND("x", "Go to");
-				ADD_BIND("e", "Get");
-				ADD_BIND("d", "Use");
-				ADD_BIND("c", "Read");
-				ADD_BIND("r", "New kid");
-				ADD_BIND("f", "Turn on");
-				ADD_BIND("v", "Turn off");
+				ADD_BIND("q", _("Push"));
+				ADD_BIND("a", _("Pull"));
+				ADD_BIND("z", _("Give"));
+				ADD_BIND("w", _("Open"));
+				ADD_BIND("s", _("Close"));
+				ADD_BIND("x", _("Go to"));
+				ADD_BIND("e", _("Get"));
+				ADD_BIND("d", _("Use"));
+				ADD_BIND("c", _("Read"));
+				ADD_BIND("r", _("New kid"));
+				ADD_BIND("f", _("Turn on"));
+				ADD_BIND("v", _("Turn off"));
 				break;
 			}
 
-			ADD_BIND("q", "Push");
-			ADD_BIND("a", "Pull");
-			ADD_BIND("z", "Give");
-			ADD_BIND("w", "Open");
-			ADD_BIND("s", "Close");
-			ADD_BIND("x", "Read");
-			ADD_BIND("e", "Walk to");
-			ADD_BIND("d", "Pick up");
-			ADD_BIND("c", "What is");
+			ADD_BIND("q", _("Push"));
+			ADD_BIND("a", _("Pull"));
+			ADD_BIND("z", _("Give"));
+			ADD_BIND("w", _("Open"));
+			ADD_BIND("s", _("Close"));
+			ADD_BIND("x", _("Read"));
+			ADD_BIND("e", _("Walk to"));
+			ADD_BIND("d", _("Pick up"));
+			ADD_BIND("c", _("What is"));
 			if (gameId == GID_MANIAC) {
-				ADD_BIND("r", "Unlock");
-				ADD_BIND("f", "New kid");
+				ADD_BIND("r", _("Unlock"));
+				ADD_BIND("f", _("New kid"));
 			} else {
-				ADD_BIND("r", "Put on");
-				ADD_BIND("f", "Take off");
+				ADD_BIND("r", _("Put on"));
+				ADD_BIND("f", _("Take off"));
 			}
-			ADD_BIND("v", "Use");
-			ADD_BIND("t", "Turn on");
-			ADD_BIND("g", "Turn off");
+			ADD_BIND("v", _("Use"));
+			ADD_BIND("t", _("Turn on"));
+			ADD_BIND("g", _("Turn off"));
 			if (gameId == GID_MANIAC)
-				ADD_BIND("b", "Fix");
+				ADD_BIND("b", _("Fix"));
 			else
-				ADD_BIND("b", "Switch");
+				ADD_BIND("b", _("Switch"));
 			break;
 		case GID_INDY3:
-			ADD_BIND("q", "Push");
-			ADD_BIND("a", "Pull");
-			ADD_BIND("z", "Give");
-			ADD_BIND("w", "Open");
-			ADD_BIND("s", "Close");
-			ADD_BIND("x", "Look");
-			ADD_BIND("e", "Walk to");
-			ADD_BIND("d", "Pick up");
-			ADD_BIND("c", "What is");
-			ADD_BIND("r", "Use");
-			ADD_BIND("f", "Turn on");
-			ADD_BIND("v", "Turn off");
-			ADD_BIND("t", "Talk");
-			ADD_BIND("g", "Travel");
-			ADD_BIND("b", "To Henry / To Indy");
+			ADD_BIND("q", _("Push"));
+			ADD_BIND("a", _("Pull"));
+			ADD_BIND("z", _("Give"));
+			ADD_BIND("w", _("Open"));
+			ADD_BIND("s", _("Close"));
+			ADD_BIND("x", _("Look"));
+			ADD_BIND("e", _("Walk to"));
+			ADD_BIND("d", _("Pick up"));
+			ADD_BIND("c", _("What is"));
+			ADD_BIND("r", _("Use"));
+			ADD_BIND("f", _("Turn on"));
+			ADD_BIND("v", _("Turn off"));
+			ADD_BIND("t", _("Talk"));
+			ADD_BIND("g", _("Travel"));
+			ADD_BIND("b", _("To Henry / To Indy"));
 			break;
 		case GID_LOOM:
-			ADD_BIND("q, c", "play C minor on distaff");
-			ADD_BIND("w, d", "play D on distaff");
-			ADD_BIND("e, e", "play E on distaff");
-			ADD_BIND("r, f", "play F on distaff");
-			ADD_BIND("t, g", "play G on distaff");
-			ADD_BIND("y, a", "play A on distaff");
-			ADD_BIND("u, b", "play B on distaff");
-			ADD_BIND("i, C", "play C major on distaff");
+			ADD_BIND("q, c", _("play C minor on distaff"));
+			ADD_BIND("w, d", _("play D on distaff"));
+			ADD_BIND("e, e", _("play E on distaff"));
+			ADD_BIND("r, f", _("play F on distaff"));
+			ADD_BIND("t, g", _("play G on distaff"));
+			ADD_BIND("y, a", _("play A on distaff"));
+			ADD_BIND("u, b", _("play B on distaff"));
+			ADD_BIND("i, C", _("play C major on distaff"));
 			break;
 		case GID_MONKEY_EGA:
 		case GID_MONKEY_VGA:
-			ADD_BIND("o", "Open");
-			ADD_BIND("c", "Close");
-			ADD_BIND("s", "puSh");
-			ADD_BIND("y", "pull (Yank)");
-			ADD_BIND("w", "Walk to");
-			ADD_BIND("p", "Pick up");
-			ADD_BIND("t", "Talk to");
-			ADD_BIND("g", "Give");
-			ADD_BIND("u", "Use");
-			ADD_BIND("l", "Look at");
-			ADD_BIND("n", "turn oN");
-			ADD_BIND("f", "turn oFf");
+			ADD_BIND("o", _("Open"));
+			ADD_BIND("c", _("Close"));
+			ADD_BIND("s", _("puSh"));
+			ADD_BIND("y", _("pull (Yank)"));
+			ADD_BIND("w", _("Walk to"));
+			ADD_BIND("p", _("Pick up"));
+			ADD_BIND("t", _("Talk to"));
+			ADD_BIND("g", _("Give"));
+			ADD_BIND("u", _("Use"));
+			ADD_BIND("l", _("Look at"));
+			ADD_BIND("n", _("turn oN"));
+			ADD_BIND("f", _("turn oFf"));
 			break;
 		case GID_MONKEY:
 		case GID_MONKEY2:
 		case GID_INDY4:
 		case GID_TENTACLE:
-			ADD_BIND("g", "Give");
-			ADD_BIND("o", "Open");
-			ADD_BIND("c", "Close");
-			ADD_BIND("p", "Pick up");
-			ADD_BIND("l", "Look at");
-			ADD_BIND("t", "Talk to");
-			ADD_BIND("u", "Use");
-			ADD_BIND("s", "puSh");
-			ADD_BIND("y", "pull (Yank)");
+			ADD_BIND("g", _("Give"));
+			ADD_BIND("o", _("Open"));
+			ADD_BIND("c", _("Close"));
+			ADD_BIND("p", _("Pick up"));
+			ADD_BIND("l", _("Look at"));
+			ADD_BIND("t", _("Talk to"));
+			ADD_BIND("u", _("Use"));
+			ADD_BIND("s", _("puSh"));
+			ADD_BIND("y", _("pull (Yank)"));
 			if (platform == Common::kPlatformSegaCD) {
-				ADD_BIND("KeyUp", "Highlight prev dialogue");
-				ADD_BIND("KeyDown", "Highlight next dialogue");
+				ADD_BIND(_("KeyUp"), _("Highlight prev dialogue"));
+				ADD_BIND(_("KeyDown"), _("Highlight next dialogue"));
 			}
 			break;
 		case GID_SAMNMAX:
-			ADD_BIND("w", "Walk");
-			ADD_BIND("t", "Talk");
-			ADD_BIND("u", "Use");
-			ADD_BIND("i", "Inventory");
-			ADD_BIND("o", "Object");
-			ADD_BIND("p", "Pick up");
-			ADD_BIND("l", "Look");
-			ADD_BIND("b", "Black and White / Color");
+			ADD_BIND("w", _("Walk"));
+			ADD_BIND("t", _("Talk"));
+			ADD_BIND("u", _("Use"));
+			ADD_BIND("i", _("Inventory"));
+			ADD_BIND("o", _("Object"));
+			ADD_BIND("p", _("Pick up"));
+			ADD_BIND("l", _("Look"));
+			ADD_BIND("b", _("Black and White / Color"));
 			break;
 		case GID_FT:
-			ADD_BIND("e", "Eyes");
-			ADD_BIND("t", "Tongue");
-			ADD_BIND("i", "Inventory");
-			ADD_BIND("p", "Punch");
-			ADD_BIND("k", "Kick");
+			ADD_BIND("e", _("Eyes"));
+			ADD_BIND("t", _("Tongue"));
+			ADD_BIND("i", _("Inventory"));
+			ADD_BIND("p", _("Punch"));
+			ADD_BIND("k", _("Kick"));
 			break;
 		case GID_DIG:
-			ADD_BIND("e", "Examine");
-			ADD_BIND("t", "Regular cursor");
-			ADD_BIND("i", "Inventory");
-			ADD_BIND("c", "Comm");
+			ADD_BIND("e", _("Examine"));
+			ADD_BIND("t", _("Regular cursor"));
+			ADD_BIND("i", _("Inventory"));
+			ADD_BIND("c", _("Comm"));
 			break;
 		case GID_CMI:
-			ADD_BIND("F1", "Save / Load / Options");
-			ADD_BIND("e", "Examine");
-			ADD_BIND("t", "Talk to");
-			ADD_BIND("i", "Inventory");
-			ADD_BIND("u", "Use");
+			ADD_BIND("F1", _("Save / Load / Options"));
+			ADD_BIND("e", _("Examine"));
+			ADD_BIND("t", _("Talk to"));
+			ADD_BIND("i", _("Inventory"));
+			ADD_BIND("u", _("Use"));
 			break;
 		}
 		break;
 	case 4:
-		title = "Other game controls:";
+		title = _("Other game controls:");
 		if (version <= 2) {
-			ADD_TEXT("Inventory:");
-			ADD_BIND("u", "Scroll list up");
-			ADD_BIND("j", "Scroll list down");
-			ADD_BIND("i", "Upper left item");
-			ADD_BIND("k", "Lower left item");
-			ADD_BIND("o", "Upper right item");
-			ADD_BIND("l", "Lower right item");
+			ADD_TEXT(_("Inventory:"));
+			ADD_BIND("u", _("Scroll list up"));
+			ADD_BIND("j", _("Scroll list down"));
+			ADD_BIND("i", _("Upper left item"));
+			ADD_BIND("k", _("Lower left item"));
+			ADD_BIND("o", _("Upper right item"));
+			ADD_BIND("l", _("Lower right item"));
 			ADD_LINE;
 		} else if (gameId == GID_INDY3 || gameId == GID_ZAK) {
 			// Indy3, or FM-TOWNS Zak
-			ADD_TEXT("Inventory:");
-			ADD_BIND("y", "Upper left item");
-			ADD_BIND("h", "Middle left item");
-			ADD_BIND("n", "Lower left item");
-			ADD_BIND("u", "Upper right item");
-			ADD_BIND("j", "Middle right item");
-			ADD_BIND("m", "Lower right item");
-			ADD_BIND("o", "Scroll list up");
-			ADD_BIND("l", "Scroll list down");
+			ADD_TEXT(_("Inventory:"));
+			ADD_BIND("y", _("Upper left item"));
+			ADD_BIND("h", _("Middle left item"));
+			ADD_BIND("n", _("Lower left item"));
+			ADD_BIND("u", _("Upper right item"));
+			ADD_BIND("j", _("Middle right item"));
+			ADD_BIND("m", _("Lower right item"));
+			ADD_BIND("o", _("Scroll list up"));
+			ADD_BIND("l", _("Scroll list down"));
 			ADD_LINE;
 		}
 		if (gameId == GID_MANIAC) {
-			ADD_TEXT("Switching characters:");
+			ADD_TEXT(_("Switching characters:"));
 			ADD_BIND("F1", "Dave");
-			ADD_BIND("F2", "Second kid");
-			ADD_BIND("F3", "Third kid");
+			ADD_BIND("F2", _("Second kid"));
+			ADD_BIND("F3", _("Third kid"));
 		} else if (gameId == GID_ZAK) {
-			ADD_TEXT("Switching characters:");
+			ADD_TEXT(_("Switching characters:"));
 			ADD_BIND("F1", "Zak");
 			ADD_BIND("F2", "Annie");
 			ADD_BIND("F3", "Melissa");
@@ -291,38 +292,38 @@ void ScummHelp::updateStrings(byte gameId, byte version, Common::Platform platfo
 	case 5:
 		switch (gameId) {
 		case GID_INDY3:
-			title = "Fighting controls (numpad):";
-			ADD_BIND("7", "Step back");
-			ADD_BIND("4", "Step back");
-			ADD_BIND("1", "Step back");
-			ADD_BIND("8", "Block high");
-			ADD_BIND("5", "Block middle");
-			ADD_BIND("2", "Block low");
-			ADD_BIND("9", "Punch high");
-			ADD_BIND("6", "Punch middle");
-			ADD_BIND("3", "Punch low");
+			title = _("Fighting controls (numpad):");
+			ADD_BIND("7", _("Step back"));
+			ADD_BIND("4", _("Step back"));
+			ADD_BIND("1", _("Step back"));
+			ADD_BIND("8", _("Block high"));
+			ADD_BIND("5", _("Block middle"));
+			ADD_BIND("2", _("Block low"));
+			ADD_BIND("9", _("Punch high"));
+			ADD_BIND("6", _("Punch middle"));
+			ADD_BIND("3", _("Punch low"));
 			ADD_LINE;
 			ADD_LINE;
-			ADD_TEXT("These are for Indy on left.");
-			ADD_TEXT("When Indy is on the right,");
-			ADD_TEXT("7, 4, and 1 are switched with");
-			ADD_TEXT("9, 6, and 3, respectively.");
+			ADD_TEXT(_("These are for Indy on left."));
+			ADD_TEXT(_("When Indy is on the right,"));
+			ADD_TEXT(_("7, 4, and 1 are switched with"));
+			ADD_TEXT(_("9, 6, and 3, respectively."));
 			break;
 		}
 		break;
 	case 6:
 		switch (gameId) {
 		case GID_INDY3:
-			title = "Biplane controls (numpad):";
-			ADD_BIND("7", "Fly to upper left");
-			ADD_BIND("4", "Fly to left");
-			ADD_BIND("1", "Fly to lower left");
-			ADD_BIND("8", "Fly upwards");
-			ADD_BIND("5", "Fly straight");
-			ADD_BIND("2", "Fly down");
-			ADD_BIND("9", "Fly to upper right");
-			ADD_BIND("6", "Fly to right");
-			ADD_BIND("3", "Fly to lower right");
+			title = _("Biplane controls (numpad):");
+			ADD_BIND("7", _("Fly to upper left"));
+			ADD_BIND("4", _("Fly to left"));
+			ADD_BIND("1", _("Fly to lower left"));
+			ADD_BIND("8", _("Fly upwards"));
+			ADD_BIND("5", _("Fly straight"));
+			ADD_BIND("2", _("Fly down"));
+			ADD_BIND("9", _("Fly to upper right"));
+			ADD_BIND("6", _("Fly to right"));
+			ADD_BIND("3", _("Fly to lower right"));
 			break;
 		}
 		break;
