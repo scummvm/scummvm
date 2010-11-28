5e0a441add1dfb1cff9e9868c6a4a85ea631c1ea
Filippos Karapetis <bluegr@gmail.com>
2010-11-28 15:54:20 +0000

    CREATE_PROJECT: Updated project file generation for the gsoc2010-opengl branch (thanks to LordHoto)
    svn-id: r54528

--- tools/create_project/create_project.cpp
+++ tools/create_project/create_project.cpp
@@ -274,6 +274,7 @@ int main(int argc, char *argv[]) {
 	setup.defines.splice(setup.defines.begin(), featureDefines);
 
 	// Windows only has support for the SDL backend, so we hardcode it here (along with winmm)
+	setup.defines.push_back("WIN32");
 	setup.defines.push_back("SDL_BACKEND");
 	setup.libraries.push_back("sdl");
 	setup.libraries.push_back("winmm");
