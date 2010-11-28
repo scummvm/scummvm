5a825eaae249d1a92f6252ce340d5801d84e15ca
Max Horn <max@quendi.de>
2010-11-28 21:33:52 +0000

    GP2X: Compilation fix
    svn-id: r54552

--- backends/platform/gp2x/gp2x-main.cpp
+++ backends/platform/gp2x/gp2x-main.cpp
@@ -23,7 +23,7 @@
  *
  */
 
-#include "backends/platform/gp2x/gp2x-sdl.h"
+#include "backends/platform/gp2x/gp2x-common.h.h"
 #include "backends/plugins/posix/posix-provider.h"
 #include "base/main.h"
 
