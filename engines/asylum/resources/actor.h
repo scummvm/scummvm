314b41cbbf257d983c9dfb3a6218b6f3c3853786
Julien Templier <littleboy@scummvm.org>
2010-11-28 01:27:02 +0000

    ASYLUM: Implement two more Actor process methods
    git-svn-id: http://asylumengine.googlecode.com/svn/trunk@588 0bfb4aae-4ea4-11de-8d8d-752d95cf3e3c

--- engines/asylum/resources/actor.h
+++ engines/asylum/resources/actor.h
@@ -235,11 +235,12 @@ public:
 	// Unknown methods
 	bool process(int32 actorX, int32 actorY);
 	void processStatus(int32 actorX, int32 actorY, bool doSpeech);
-	void process_401830(int32 field980, int32 actionAreaId, int32 field978, int field98C, int32 field990, int32 field974, int32 param8, int32 param9);
+	void process_401830(int32 field980, int32 actionAreaId, int32 field978, int field98C, int32 field990, int32 field974, int32 field984, int32 field988);
 	bool process_408B20(Common::Point *point, ActorDirection direction, int count, bool hasDelta);
 	void process_41BC00(int32 reactionIndex, int32 numberValue01Add);
 	void process_41BCC0(int32 reactionIndex, int32 numberValue01Substract);
 	bool process_41BDB0(int32 reactionIndex, int32 testNumberValue01);
+	bool process_4103B0(Common::Point *point, ActorDirection direction);
 	void updateAndDraw();
 	void update_409230();
 
