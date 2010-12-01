e5f2c235d307f14081812608345b963f62ccbe5d
Filippos Karapetis <bluegr@gmail.com>
2010-12-01 15:15:42 +0000

    BACKENDS: Simplify the inclusion of gl.h. SDL has already simplified it for us :)
    svn-id: r54704

--- backends/graphics/opengl/gltexture.h
+++ backends/graphics/opengl/gltexture.h
@@ -34,18 +34,12 @@
 
 #if defined(USE_GLES)
 #include <GLES/gl.h>
-#elif defined(MACOSX)
-#include <OpenGL/gl.h>
-#else
-
-#include <GL/gl.h>
-#if defined(SDL_BACKEND) && !defined(GL_VERSION_1_2)
-// The OpenGL libraries included in Windows are for OpenGL 1.1. Use the SDL
-// OpenGL header file to enable the additional needed defines for OpenGL 1.2
-// here.
-#include <SDL_opengl.h>
 #endif
 
+#if defined(SDL_BACKEND)
+#include <SDL_opengl.h>
+#else
+#include <GL/gl.h>
 #endif
 
 #include "graphics/surface.h"
