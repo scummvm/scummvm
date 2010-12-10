2a785f0b52b62782f7be64ed598a47d286c5ac8c
Julien Templier <littleboy@scummvm.org>
2010-12-10 14:25:59 +0000

    ASYLUM: Implement Menu::clickSaveGame()
    - Handle delete key as backspace
    - Handle keypad period as delete
    - Correct invalid width test when typing save name
    - Fix problems with Menu::clickDeleteGame()
    
    git-svn-id: http://asylumengine.googlecode.com/svn/trunk@695 0bfb4aae-4ea4-11de-8d8d-752d95cf3e3c

--- engines/asylum/staticres.h
+++ engines/asylum/staticres.h
@@ -50,6 +50,8 @@ const int moviesCd[49] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
                           2, 2, 2, 2, 2, 2, 2, 2, 3, 3,
                           3, 3, 3, 3, 3, 3, 3, 3, 2};
 
+const int chapterIndexes[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 4, 4, 8, 9, 0, 0};
+
 /** Default Actor Indices by Scene */
 const int actorType[16] = {0, 0, 0, 0, 1, 0, 2, 0, 3, 0, 1, 1, 3, 0, 0, 0};
 
