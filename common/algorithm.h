2184326dbc428d7b6ef1eda510ae299838d5a2d2
Max Horn <max@quendi.de>
2011-02-09 00:11:39 +0000

    COMMON: Add comment that explains strange <= checks in gcd()
    svn-id: r55838

--- common/algorithm.h
+++ common/algorithm.h
@@ -242,6 +242,9 @@ void sort(T first, T last) {
  */
 template<class T>
 T gcd(T a, T b) {
+	// Note: We check for <= instead of < to avoid spurious compiler
+	// warnings if T is an unsigned type, i.e. warnings like "comparison
+	// of unsigned expression < 0 is always false".
 	if (a <= 0)
 		a = -a;
 	if (b <= 0)
