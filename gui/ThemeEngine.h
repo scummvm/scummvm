2d46c9b6a212edb6243da63dc1f721c50406fc2a
Johannes Schickel <lordhoto@scummvm.org>
2011-03-01 04:18:13 +0100

    GUI: Increment theme version.
    With 50a682e7e1c69082735a07b2261d1c0c61ac12c9 we changed the resolution string
    in a way which is incompatible with old resolution strings, thus all old theme
    files are not usable anymore and will cause problems. To avoid that we just
    increase the version string and thus it is not possible to use them anymore.

--- gui/ThemeEngine.h
+++ gui/ThemeEngine.h
@@ -32,7 +32,7 @@
 #include "graphics/surface.h"
 #include "graphics/font.h"
 
-#define SCUMMVM_THEME_VERSION_STR "SCUMMVM_STX0.8.2"
+#define SCUMMVM_THEME_VERSION_STR "SCUMMVM_STX0.8.3"
 
 namespace Graphics {
 	struct DrawStep;
