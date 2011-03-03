08af1d9d3fcd8c288f04f0175f6cc6b1ead10dfa
md5 <md5@scummvm.org>
2011-03-04 01:42:53 +0200

    SCI: Don't patch save/restore dialogs in Phantasmagoria
    That game has its own custom made save/load system

--- engines/sci/sci.cpp
+++ engines/sci/sci.cpp
@@ -477,10 +477,11 @@ void SciEngine::patchGameSaveRestore() {
 	byte kernelIdSave = 0;
 
 	switch (_gameId) {
-	case GID_MOTHERGOOSE256: // mother goose saves/restores directly and has no save/restore dialogs
-	case GID_JONES: // gets confused, when we patch us in, the game is only able to save to 1 slot, so hooking is not required
 	case GID_HOYLE1: // gets confused, although the game doesnt support saving/restoring at all
 	case GID_HOYLE2: // gets confused, see hoyle1
+	case GID_JONES: // gets confused, when we patch us in, the game is only able to save to 1 slot, so hooking is not required
+	case GID_MOTHERGOOSE256: // mother goose saves/restores directly and has no save/restore dialogs
+	case GID_PHANTASMAGORIA: // has custom save/load code
 		return;
 	default:
 		break;
