40333617ee4a0fede7c8cdec81a025ccab06e9e3
dhewg <dhewg@wiibrew.org>
2011-03-12 12:24:14 +0100

    SWORD1: Don't set the same cursor on every frame
    Removes unnecessary overhead on the backend when the cursor has just
    one frame.

--- engines/sword1/mouse.h
+++ engines/sword1/mouse.h
@@ -99,8 +99,9 @@ private:
 	ObjectMan *_objMan;
 	Common::Point _mouse;
 
-	uint32 _currentPtrId, _currentLuggageId, _frame;
+	uint32 _currentPtrId, _currentLuggageId;
 	MousePtr *_currentPtr;
+	int _frame, _activeFrame;
 	uint16 _numObjs;
 	uint16 _lastState, _state;
 	uint32 _getOff;
