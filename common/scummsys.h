3924fc0bada989332b66292204e328341f6ab72e
md5 <md5@scummvm.org>
2011-03-19 01:56:10 +0200

    COMMON: Enable math constants under Visual Studio

--- common/scummsys.h
+++ common/scummsys.h
@@ -94,6 +94,11 @@
 	#include <stdarg.h>
 	#include <assert.h>
 	#include <ctype.h>
+	// MSVC does not define M_PI, M_SQRT2 and other math defines by default.
+	// _USE_MATH_DEFINES must be defined in order to have these defined, thus
+	// we enable it here. For more information, check:
+	// http://msdn.microsoft.com/en-us/library/4hwaceh6(v=VS.100).aspx
+	#define _USE_MATH_DEFINES
 	#include <math.h>
 
 #endif
