c144bbf9570e6d4498072374ee24edb33b6c9126
dhewg <dhewg@wiibrew.org>
2011-03-25 00:38:25 +0100

    SCI: Add two TODOs

--- engines/sci/graphics/screen.cpp
+++ engines/sci/graphics/screen.cpp
@@ -156,12 +156,14 @@ void GfxScreen::copyToScreen() {
 }
 
 void GfxScreen::copyFromScreen(byte *buffer) {
+	// TODO this ignores the pitch
 	Graphics::Surface *screen = g_system->lockScreen();
 	memcpy(buffer, screen->pixels, _displayPixels);
 	g_system->unlockScreen();
 }
 
 void GfxScreen::kernelSyncWithFramebuffer() {
+	// TODO this ignores the pitch
 	Graphics::Surface *screen = g_system->lockScreen();
 	memcpy(_displayScreen, screen->pixels, _displayPixels);
 	g_system->unlockScreen();
