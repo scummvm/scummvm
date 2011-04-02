a3d0914bbeb7f01c920a98759aeee2b8b3e3630c
Klaus Reimer <k@ailis.de>
2011-04-03 00:01:26 +0200

    Added webos backend. IPK can be build with "make webosrelease".

---
+++ backends/platform/webos/webos.mk
@@ -0,0 +1,19 @@
+# WebOS specific build targets
+
+PATH_DIST = $(srcdir)/dists/webos
+PATH_RESOURCES = $(PATH_DIST)/mojo
+
+STAGING_DIR=STAGING/org.scummvm
+
+webosrelease:
+	rm -rf $(STAGING_DIR)
+	rm -f *.ipk
+	mkdir -p $(STAGING_DIR)
+	mkdir -p $(STAGING_DIR)/bin
+	mkdir -p $(STAGING_DIR)/lib
+	cp -f $(PATH_RESOURCES)/* $(STAGING_DIR)
+	cp -f scummvm $(STAGING_DIR)/bin
+	$(WEBOS_SDK)/bin/palm-package $(STAGING_DIR)
+	rm -rf STAGING
+
+.PHONY: webosrelease
