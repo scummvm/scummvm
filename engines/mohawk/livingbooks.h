214f93e6aa7ea698672946f9650103e96ab37109
D G Turner <digitall@scummvm.org>
2011-04-11 23:21:46 +0100

    MOHAWK: Add Default Values to LB NotifyEvent Constructor.
    This silences a number of warnings about possible uninitialized
    parameter use.

--- engines/mohawk/livingbooks.h
+++ engines/mohawk/livingbooks.h
@@ -575,7 +575,7 @@ public:
 };
 
 struct NotifyEvent {
-	NotifyEvent(uint t, uint p) : type(t), param(p) { }
+	NotifyEvent(uint t, uint p) : type(t), param(p), newUnknown(0), newMode(0), newPage(0), newSubpage(0) { }
 	uint type;
 	uint param;
 
