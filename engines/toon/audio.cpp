79f514a397c9e558e25a28fa5f92a02f42d7828a
Max Horn <max@quendi.de>
2011-04-13 11:24:25 +0200

    TOON: Make ADPCM tables const

--- engines/toon/audio.cpp
+++ engines/toon/audio.cpp
@@ -29,10 +29,10 @@
 
 namespace Toon {
 
-static int ADPCM_index[8] = {
+static const int ADPCM_index[8] = {
 	-1, -1, -1, -1, 2 , 4 , 6 , 8
 };
-static int ADPCM_table[89] = {
+static const int ADPCM_table[89] = {
 	7,     8,     9,     10,    11,    12,    13,    14,    16,    17,
 	19,    21,    23,    25,    28,    31,    34,    37,    41,    45,
 	50,    55,    60,    66,    73,    80,    88,    97,    107,   118,
