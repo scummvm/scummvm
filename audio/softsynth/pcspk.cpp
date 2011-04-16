99ee683308a57b673bd9b85a9ac24663ee0b91dd
Pawel Kolodziejski <aquadran@sourceforge.net>
2011-04-16 14:08:33 +0200

    cleanup headers

--- audio/softsynth/pcspk.cpp
+++ audio/softsynth/pcspk.cpp
@@ -1,8 +1,8 @@
-/* Residual - A 3D game interpreter
- *
- * Residual is the legal property of its developers, whose names
- * are too numerous to list here. Please refer to the AUTHORS
- * file distributed with this source distribution.
+/* ScummVM - Graphic Adventure Engine
+*
+* ScummVM is the legal property of its developers, whose names
+* are too numerous to list here. Please refer to the COPYRIGHT
+* file distributed with this source distribution.
 *
 * This program is free software; you can redistribute it and/or
 * modify it under the terms of the GNU General Public License
