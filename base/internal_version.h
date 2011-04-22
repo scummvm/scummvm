3fc5004b042e859d240d9be90f31522750484807
Littleboy <littleboy@users.sourceforge.net>
2011-04-22 16:07:44 -0400

    BASE: Include external revision file when SCUMMVM_EXTERNAL_REVISION is defined
    Add template file for external_version.h

--- base/internal_version.h
+++ base/internal_version.h
@@ -2,6 +2,11 @@
 #error This file may only be included by base/version.cpp
 #endif
 
+// Loads revision number from an external file
+#ifdef SCUMMVM_EXTERNAL_REVISION
+#include "external_version.h"
+#endif
+
 #ifdef RELEASE_BUILD
 #undef SCUMMVM_REVISION
 #endif
