8f1a26ea3e86f1278d1fce58db13f3684b5d391f
dhewg <dhewg@wiibrew.org>
2011-04-22 10:56:05 +0200

    BUILD: Move ARM_ASM flags to configure
    So they're used again if a port overwrites _port_mk

--- ports.mk
+++ ports.mk
@@ -200,30 +200,3 @@ endif
 # Mark special targets as phony
 .PHONY: deb bundle osxsnap win32dist install uninstall
 
-#
-# ARM specific
-#
-ifdef USE_TREMOLO
-DEFINES += -DUSE_TREMOR -DUSE_VORBIS -DUSE_TREMOLO
-LIBS += -ltremolo
-endif
-
-ifdef USE_ARM_SMUSH_ASM
-DEFINES += -DUSE_ARM_SMUSH_ASM
-endif
-
-ifdef USE_ARM_SOUND_ASM
-DEFINES += -DUSE_ARM_SOUND_ASM
-endif
-
-ifdef USE_ARM_GFX_ASM
-DEFINES += -DUSE_ARM_GFX_ASM
-endif
-
-ifdef USE_ARM_COSTUME_ASM
-DEFINES += -DUSE_ARM_COSTUME_ASM
-endif
-
-ifdef USE_ARM_SCALER_ASM
-DEFINES += -DUSE_ARM_SCALER_ASM
-endif
