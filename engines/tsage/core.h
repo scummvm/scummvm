98affc01a6f5cce4ce4db2a164c3972203e7a068
Johannes Schickel <lordhoto@scummvm.org>
2011-04-25 15:50:41 +0200

    TSAGE: Add virtual destructor to Game class.

--- engines/tsage/core.h
+++ engines/tsage/core.h
@@ -914,6 +914,8 @@ protected:
 	static bool notLockedFn(GameHandler *g);
 	virtual void handleSaveLoad(bool saveFlag, int &saveSlot, Common::String &saveName) {}
 public:
+	virtual ~Game() {}
+
 	void addHandler(GameHandler *entry) { _handlers.push_back(entry); }
 	void removeHandler(GameHandler *entry) { _handlers.remove(entry); }
 
