d683a228a3c3216ac262d6d026ea350c80b88520
Max Horn <max@quendi.de>
2011-05-03 23:04:58 +0200

    MOTO: Change fprintf calls to debug calls

--- backends/graphics/linuxmotosdl/linuxmotosdl-graphics.cpp
+++ backends/graphics/linuxmotosdl/linuxmotosdl-graphics.cpp
@@ -144,14 +144,14 @@ void LinuxmotoSdlGraphicsManager::initSize(uint w, uint h) {
 }
 
 bool LinuxmotoSdlGraphicsManager::loadGFXMode() {
-	printf("Game ScreenMode = %d*%d\n",_videoMode.screenWidth, _videoMode.screenHeight);
+	debug("Game ScreenMode = %d*%d",_videoMode.screenWidth, _videoMode.screenHeight);
 	if (_videoMode.screenWidth > 320 || _videoMode.screenHeight > 240) {
 		_videoMode.aspectRatioCorrection = false;
 		setGraphicsMode(GFX_HALF);
-		printf("GraphicsMode set to HALF\n");
+		debug("GraphicsMode set to HALF");
 	} else {
 		setGraphicsMode(GFX_NORMAL);
-		printf("GraphicsMode set to NORMAL\n");
+		debug("GraphicsMode set to NORMAL");
 	}
 	if (_videoMode.mode == GFX_HALF && !_overlayVisible) {
 		_videoMode.overlayWidth = 320;
