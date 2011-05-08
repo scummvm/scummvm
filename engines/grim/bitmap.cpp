442f25b4520c46ef1a857de021c9f8c596212519
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2011-05-09 01:16:14 +0200

    formatting

--- engines/grim/bitmap.cpp
+++ engines/grim/bitmap.cpp
@@ -45,7 +45,7 @@ Bitmap::Bitmap(const char *fname, const char *data, int len) :
 	printf("Trying to load %s\n", fname);
 	if (len < 8 || memcmp(data, "BM  F\0\0\0", 8) != 0) {
 		// Should probably just check the filename
-		if(loadTile(fname,data,len)){
+		if (loadTile(fname, data, len)){
 			return;
 			// Everything is fine
 		}
@@ -54,19 +54,19 @@ Bitmap::Bitmap(const char *fname, const char *data, int len) :
 	}
 
 	int codec = READ_LE_UINT32(data + 8);
-//  _paletteIncluded = READ_LE_UINT32(data + 12);
+//	_paletteIncluded = READ_LE_UINT32(data + 12);
 	_numImages = READ_LE_UINT32(data + 16);
 	_x = READ_LE_UINT32(data + 20);
 	_y = READ_LE_UINT32(data + 24);
-//  _transparentColor = READ_LE_UINT32(data + 28);
+//	_transparentColor = READ_LE_UINT32(data + 28);
 	_format = READ_LE_UINT32(data + 32);
 	_bpp = READ_LE_UINT32(data + 36);
-//  _blueBits = READ_LE_UINT32(data + 40);
-//  _greenBits = READ_LE_UINT32(data + 44);
-//  _redBits = READ_LE_UINT32(data + 48);
-//  _blueShift = READ_LE_UINT32(data + 52);
-//  _greenShift = READ_LE_UINT32(data + 56);
-//  _redShift = READ_LE_UINT32(data + 60);
+//	_blueBits = READ_LE_UINT32(data + 40);
+//	_greenBits = READ_LE_UINT32(data + 44);
+//	_redBits = READ_LE_UINT32(data + 48);
+//	_blueShift = READ_LE_UINT32(data + 52);
+//	_greenShift = READ_LE_UINT32(data + 56);
+//	_redShift = READ_LE_UINT32(data + 60);
 	_width = READ_LE_UINT32(data + 128);
 	_height = READ_LE_UINT32(data + 132);
 	_currImage = 1;
@@ -122,76 +122,76 @@ Bitmap::Bitmap() :
 }
 
 // Helper function for makeBitmapFromTile
-char* getLine(int lineNum, char* data, unsigned int width,int bpp){
-	return data + (lineNum*(width*bpp));
+char *getLine(int lineNum, char* data, unsigned int width, int bpp){
+	return data + (lineNum *(width * bpp));
 }
 
-char* makeBitmapFromTile(char** bits,int width, int height, int bpp){
-	bpp = bpp/8;
-	char* fullImage = new char[width*height*bpp];
+char *makeBitmapFromTile(char **bits, int width, int height, int bpp) {
+	bpp = bpp / 8;
+	char *fullImage = new char[width * height * bpp];
 
-	const int tWidth = 256*bpp; // All tiles so far are 256 wide
+	const int tWidth = 256 * bpp; // All tiles so far are 256 wide
 	const int tWidth2 = 256;
 
-	char* target = fullImage;
-	for(int i=0;i<256;i++){
+	char *target = fullImage;
+	for (int i = 0; i < 256; i++) {
 		/* This can be modified to actually use the last 32 lines.
 		 * We simply put the lower half on line 223 and down to line 32,
 		 * then skip the last 32.
 		 * While the upper half is put on line 479 and down to line 224.
 		 */
-		if(i<224){ // Skip blank space
-			target=getLine(223-i,fullImage,width,bpp);
+		if (i < 224) { // Skip blank space
+			target=getLine(223 - i, fullImage, width, bpp);
 
-			memcpy(target,getLine(i,bits[3],tWidth2,bpp),tWidth);
+			memcpy(target, getLine(i, bits[3], tWidth2, bpp), tWidth);
 			target += tWidth;
 
-			memcpy(target,getLine(i,bits[4],tWidth2,bpp),tWidth);
+			memcpy(target, getLine(i, bits[4], tWidth2, bpp), tWidth);
 			target += tWidth;
 
-			memcpy(target,getLine(i,bits[2],tWidth2,bpp)+128*bpp,128*bpp);
-			target += tWidth/2;
+			memcpy(target, getLine(i, bits[2], tWidth2, bpp) + 128 * bpp, 128 * bpp);
+			target += tWidth / 2;
 		}
 
 		// Top half of course
 
-		target = getLine(479-i,fullImage,width,bpp);
+		target = getLine(479 - i, fullImage, width, bpp);
 
-		memcpy(target,getLine(i,bits[0],tWidth2,bpp),tWidth);
+		memcpy(target, getLine(i, bits[0], tWidth2, bpp), tWidth);
 		target += tWidth;
 
-		memcpy(target,getLine(i,bits[1],tWidth2,bpp),tWidth);
+		memcpy(target, getLine(i, bits[1], tWidth2, bpp), tWidth);
 		target += tWidth;
 
-		memcpy(target,getLine(i,bits[2],tWidth2,bpp),128*bpp);
-		target += tWidth/2;
+		memcpy(target, getLine(i, bits[2], tWidth2, bpp), 128 * bpp);
+		target += tWidth / 2;
 
 	}
 
 	return fullImage;
 }
 
-char* upconvertto32(char* data, unsigned int width, unsigned int height, int bpp){
-	if(bpp==16){
-		bpp=2;
-		char* newData = new char[width*height*4];
-		char* to = newData;
-		char red=0,green=0,blue=0;
-		for(unsigned int i=0;i<height;i++){
-			for(unsigned int j=0;j<width;j++){
-				char byte2 = data[i*width*bpp+j*2];
-				char byte1 = data[i*width*bpp+j*2+1];
+char *upconvertto32(char *data, unsigned int width, unsigned int height, int bpp) {
+	if (bpp==16) {
+		bpp = 2;
+		char *newData = new char[width * height * 4];
+		char *to = newData;
+		char red = 0, green = 0, blue = 0;
+		for (unsigned int i = 0; i< height; i++) {
+			for(unsigned int j = 0; j < width; j++) {
+				char byte2 = data[i * width * bpp + j *2];
+				char byte1 = data[i * width * bpp + j * 2 + 1];
 			// Probably Alpha, then 555.
 			// Red
-				red = (byte1>>2)&31;
+				red = (byte1 >> 2) & 31;
 				red = red << 3 | red >> 2;
 			// Green
-				char green1 = (byte1&3);
-				char green2 = (((byte2)>>5)&7);
+				char green1 = (byte1 & 3);
+				char green2 = (((byte2) >> 5) & 7);
 				char green3 = green1 << 3 | green2;
 				green = green3 << 3 | green3 >> 2 ;
 			// Blue
-				blue = (byte2)&31;
+				blue = (byte2) & 31;
 				blue = blue << 3 | blue >> 2;
 			// Some more magic to stretch the values
 				*to = red;
@@ -210,7 +210,7 @@ char* upconvertto32(char* data, unsigned int width, unsigned int height, int bpp
 	return data;
 }
 
-bool Bitmap::loadTile(const char *filename, const char *data, int len){
+bool Bitmap::loadTile(const char *filename, const char *data, int len) {
 	_x = 0;
 	_y = 0;
 	//warning("Loading TILE: %s",filename);
@@ -224,7 +224,7 @@ bool Bitmap::loadTile(const char *filename, const char *data, int len){
 	o->seek(bmoffset + 16);
 	_numImages = o->readUint32LE();
 	if (_numImages < 5)
-	error("Can not handle a tile with less than 5 images");
+		error("Can not handle a tile with less than 5 images");
 
 	_data = new char *[_numImages];
 
@@ -243,20 +243,20 @@ bool Bitmap::loadTile(const char *filename, const char *data, int len){
 		o->seek(8, SEEK_CUR);
 		o->read(_data[i], size);
 	}
-	char* bMap = makeBitmapFromTile(_data,640,480,_bpp);
+	char* bMap = makeBitmapFromTile(_data, 640, 480, _bpp);
 	for (int i = 0; i < _numImages; ++i) {
-		delete [] _data[i];
+		delete[] _data[i];
 	}
 	_data[0] = bMap;
 	_numImages = 1;
 
-	if(_bpp==16){
-		_data[0] = upconvertto32(_data[0],640,480,_bpp);
-		_bpp=32;
+	if (_bpp == 16) {
+		_data[0] = upconvertto32(_data[0], 640, 480, _bpp);
+		_bpp = 32;
 	}
 	_width = 640;
 	_height = 480;
-	_currImage=1;
+	_currImage = 1;
 
 	g_driver->createBitmap(this);
 	return true;
