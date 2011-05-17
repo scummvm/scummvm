edab6b9dfc9348053daf1321d8691a8811384319
Einar Johan T. Sømåen <einarjohants@gmail.com>
2011-05-18 01:04:03 +0800

    ALL: Fix proper path for sdl-config during make bundle on OS X

--- ports.mk
+++ ports.mk
@@ -78,7 +78,7 @@ endif
 # Location of static libs for the iPhone
 ifneq ($(BACKEND), iphone)
 # Static libaries, used for the residual-static and iphone targets
-OSX_STATIC_LIBS := `$(STATICLIBPATH)/bin/sdl-config --static-libs`
+OSX_STATIC_LIBS := `$(SDLCONFIG) --static-libs`
 endif
 
 ifdef USE_VORBIS
