e548d852db3d0e2248c259281756c613dca550e0
Giulio Camuffo <giuliocamuffo@gmail.com>
2011-05-29 11:41:51 +0200

    GRIM: Increased savegame version to 16.

--- engines/grim/savegame.cpp
+++ engines/grim/savegame.cpp
@@ -33,7 +33,7 @@ namespace Grim {
 #define SAVEGAME_HEADERTAG	'RSAV'
 #define SAVEGAME_FOOTERTAG	'ESAV'
 
-int SaveGame::SAVEGAME_VERSION = 15;
+int SaveGame::SAVEGAME_VERSION = 16;
 
 // Constructor. Should create/open a saved game
 SaveGame::SaveGame(const Common::String &filename, bool saving) :
