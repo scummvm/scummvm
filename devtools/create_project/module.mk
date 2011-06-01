8f36c52e5866bde77a5f79ac6c1ea96152e5e22f
Max Horn <max@quendi.de>
2011-06-01 23:57:12 +0200

    DEVTOOLS: Do not use -Wglobal-constructors on create_project.o

--- devtools/create_project/module.mk
+++ devtools/create_project/module.mk
@@ -12,6 +12,11 @@ MODULE_OBJS := \
 # Set the name of the executable
 TOOL_EXECUTABLE := create_project
 
+# Set custom build flags for create_project.o: It uses C++ iostreams,
+# which make use of global constructors. So we don't want warnings for
+# that.
+$(srcdir)/devtools/create_project/create_project.o: CXXFLAGS:=$(filter-out -Wglobal-constructors,$(CXXFLAGS))
+
 # Include common rules
 include $(srcdir)/rules.mk
 
