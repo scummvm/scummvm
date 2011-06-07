8ac63a37a8a0e995b9228c90b24d4fbdffcf940d
Max Horn <max@quendi.de>
2011-06-07 10:57:24 +0200

    PS2: Use OSystem's 'slots' for timer/savefile manager

--- backends/platform/ps2/systemps2.cpp
+++ backends/platform/ps2/systemps2.cpp
@@ -60,7 +60,6 @@
 #include "backends/fs/ps2/ps2-fs-factory.h"
 #include "backends/plugins/ps2/ps2-provider.h"
 
-#include "backends/saves/default/default-saves.h"
 #include "backends/timer/default/default-timer.h"
 
 #include "audio/mixer_intern.h"
@@ -348,14 +347,14 @@ OSystem_PS2::OSystem_PS2(const char *elfPath) {
 
 void OSystem_PS2::init(void) {
 	sioprintf("Timer...\n");
-	_scummTimerManager = new DefaultTimerManager();
+	_timerManager = new DefaultTimerManager();
 	_scummMixer = new Audio::MixerImpl(this, 48000);
 	_scummMixer->setReady(true);
 
 	initTimer();
 
 	sioprintf("Starting SavefileManager\n");
-	_saveManager = new Ps2SaveFileManager(this, _screen);
+	_savefileManager = new Ps2SaveFileManager(this, _screen);
 
 	sioprintf("Initializing ps2Input\n");
 	_input = new Ps2Input(this, _useMouse, _useKbd);
@@ -430,7 +429,7 @@ void OSystem_PS2::initTimer(void) {
 void OSystem_PS2::timerThreadCallback(void) {
 	while (!_systemQuit) {
 		WaitSema(g_TimerThreadSema);
-		_scummTimerManager->handler();
+		((DefaultTimerManager *)_timerManager)->handler();
 	}
 	ExitThread();
 }
@@ -600,16 +599,8 @@ void OSystem_PS2::delayMillis(uint msecs) {
 	}
 }
 
-Common::TimerManager *OSystem_PS2::getTimerManager() {
-	return _scummTimerManager;
-}
-
 Audio::Mixer *OSystem_PS2::getMixer() {
 	return _scummMixer;
-}
-
-Common::SaveFileManager *OSystem_PS2::getSavefileManager(void) {
-	return _saveManager;
 }
 
 FilesystemFactory *OSystem_PS2::getFilesystemFactory() {
@@ -770,7 +761,7 @@ void OSystem_PS2::msgPrintf(int millis, const char *format, ...) {
 
 void OSystem_PS2::powerOffCallback(void) {
 	sioprintf("powerOffCallback\n");
-	// _saveManager->quit(); // romeo
+	// _savefileManager->quit(); // romeo
 	if (_useHdd) {
 		sioprintf("umount\n");
 		fio.umount("pfs0:");
@@ -810,7 +801,7 @@ void OSystem_PS2::quit(void) {
 		DisableIntc(INT_TIMER0);
 		RemoveIntcHandler(INT_TIMER0, _intrId);
 
-		// _saveManager->quit(); // romeo
+		// _savefileManager->quit(); // romeo
 		_screen->quit();
 
 		padEnd(); // stop pad library
