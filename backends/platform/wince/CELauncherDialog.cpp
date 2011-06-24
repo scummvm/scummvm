6bfaddd70bee191a6668dffa345bec091362781e
CeRiAl <ikhatib@gmail.com>
2011-06-25 01:00:07 +0200

    WINCE: Hide toolbar when returning to game list

--- backends/platform/wince/CELauncherDialog.cpp
+++ backends/platform/wince/CELauncherDialog.cpp
@@ -27,6 +27,7 @@
 #define FORBIDDEN_SYMBOL_ALLOW_ALL
 
 #include "backends/platform/wince/wince-sdl.h"
+#include "backends/graphics/wincesdl/wincesdl-graphics.h"
 
 #include "CELauncherDialog.h"
 
@@ -37,6 +38,7 @@
 #include "gui/browser.h"
 #include "gui/message.h"
 #include "gui/ThemeEval.h"
+#include "gui/widgets/list.h"
 
 #include "common/config-manager.h"
 
@@ -69,9 +71,13 @@ public:
 };
 
 CELauncherDialog::CELauncherDialog() : GUI::LauncherDialog() {
+	((WINCESdlGraphicsManager *)((OSystem_SDL *)g_system)->getGraphicsManager())->reset_panel();
 }
 
 void CELauncherDialog::handleCommand(CommandSender *sender, uint32 cmd, uint32 data) {
+	if ((cmd == 'STRT') || (cmd == kListItemActivatedCmd) || (cmd == kListItemDoubleClickedCmd)) {
+		((WINCESdlGraphicsManager *)((OSystem_SDL *)g_system)->getGraphicsManager())->init_panel();
+	}
 	LauncherDialog::handleCommand(sender, cmd, data);
 	if (cmd == 'ABOU') {
 		CEAboutDialog about;
