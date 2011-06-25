d79364489d2aa15503d8d163fd8c6c59bd7e517e
Willem Jan Palenstijn <wjp@usecode.org>
2011-06-25 20:41:28 +0200

    DREAMWEB: Enable 100 savegames

--- engines/dreamweb/detection.cpp
+++ engines/dreamweb/detection.cpp
@@ -113,7 +113,7 @@ SaveStateList DreamWebMetaEngine::listSaves(const char *target) const {
 	return saveList;
 }
 
-int DreamWebMetaEngine::getMaximumSaveSlot() const { return 6; }
+int DreamWebMetaEngine::getMaximumSaveSlot() const { return 99; }
 
 void DreamWebMetaEngine::removeSaveState(const char *target, int slot) const {
 }
