4d4d3329c3f60db245038d48ff36a14628a0c10f
Bertrand Augereau <bertrand_augereau@yahoo.fr>
2011-06-26 15:10:18 +0200

    DREAMWEB: The generator includes a file for custom structs and a file for custom function definitions It also generates C++ symbols for asm proc addresses

---
+++ engines/dreamweb/structs.h
@@ -0,0 +1,24 @@
+struct Sprite {
+	uint16 updateCallback;
+	uint16 w2;
+	uint16 w4;
+	uint16 w6;
+	uint16 w8;
+	uint8  x;
+	uint8  y;
+	uint16 w12;
+	uint8  b14;
+	uint8  b15;
+	uint16 w16;
+	uint8  b18;
+	uint8  b19;
+	uint16 obj_data;
+	uint8  b22;
+	uint8  priority;
+	uint16 w24;
+	uint16 w26;
+	uint16 w28;
+	uint8  type;
+	uint8  hidden;
+};
+
