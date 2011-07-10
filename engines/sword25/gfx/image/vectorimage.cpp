scummvm/scummvm

commit 5dd8f2575b0f39dea909db73afc83f16732b2a5e

JANITORIAL: Silence a couple of "variable set but not used" warnings.

engines/sword25/gfx/image/vectorimage.cpp
@@ -247,9 +247,6 @@ VectorImage::VectorImage(const byte *pFileData, uint fileSize, bool &success, co
 		return;
 	}
 
-	// readout SWF size
-	Common::Rect movieRect = flashRectToBSRect(bs);
-
 	// Get frame rate and frame count
 	/* uint32 frameRate = */
 	bs.getUInt16();

engines/cine/various.cpp
@@ -688,8 +688,6 @@ int16 makeMenuChoice(const CommandeType commandList[], uint16 height, uint16 X, 
 	int16 di;
 	uint16 j;
 	int16 mouseX, mouseY;
-	int16 var_16;
-	int16 var_14;
 	int16 currentSelection, oldSelection;
 	int16 var_4;
 	SelectionMenu *menu;
@@ -730,9 +728,6 @@ int16 makeMenuChoice(const CommandeType commandList[], uint16 height, uint16 X, 
 
 	manageEvents();
 	getMouseData(mouseUpdateStatus, &button, (uint16 *)&mouseX, (uint16 *)&mouseY);
-
-	var_16 = mouseX;
-	var_14 = mouseY;
 
 	menuVar = 0;
 

engines/cruise/cruise_main.cpp
@@ -902,18 +902,8 @@ bool createDialog(int objOvl, int objIdx, int x, int y) {
 						if (!obj2Ovl)  obj2Ovl = j;
 
 						char verbe_name[80];
-						char obj1_name[80];
-						char obj2_name[80];
-						char r_verbe_name[80];
-						char r_obj1_name[80];
-						char r_obj2_name[80];
 
 						verbe_name[0]	= 0;
-						obj1_name[0]	= 0;
-						obj2_name[0]	= 0;
-						r_verbe_name[0] = 0;
-						r_obj1_name[0]	= 0;
-						r_obj2_name[0]	= 0;
 
 						ovlDataStruct *ovl2 = NULL;
 						ovlDataStruct *ovl3 = NULL;

engines/cruise/mainDraw.cpp
@@ -440,7 +440,6 @@ void buildSegment() {
 
 		// is segment on screen ?
 		if (!((tempAX > 199) || (tempDX < 0))) {
-			int dx = Y1;
 			int cx = X2 - X1;
 			if (cx == 0) {
 				// vertical line
@@ -473,7 +472,6 @@ void buildSegment() {
 			} else {
 				if (cx < 0) {
 					cx = -cx;
-					dx = Y2;
 
 					SWAP(X1, X2);
 					SWAP(Y1, Y2);
@@ -1490,9 +1488,6 @@ void mainDraw(int16 param) {
 								if (currentObjPtr->animLoop > 0)
 									currentObjPtr->animLoop--;
 							} else {
-								int16 data2;
-								data2 = currentObjPtr->animStart;
-
 								change = false;
 								currentObjPtr->animStep = 0;
 
@@ -1512,9 +1507,6 @@ void mainDraw(int16 param) {
 								if (currentObjPtr->animLoop > 0)
 									currentObjPtr->animLoop--;
 							} else {
-								int16 data2;
-								data2 = currentObjPtr->animStart;
-
 								change = false;
 								currentObjPtr->animStep = 0;
 

engines/cruise/script.cpp
@@ -231,9 +231,7 @@ int32 opcodeType2() {
 		int type = getByteFromScript();
 		int overlay = getByteFromScript();
 
-		int firstOffset;
-		int offset;
-		firstOffset = offset = getShortFromScript();
+		int offset = getShortFromScript();
 		offset += index;
 
 		int typ7 = type & 7;

engines/lure/disk.cpp
@@ -98,7 +98,6 @@ void Disk::openFile(uint8 fileNum) {
 		error("Could not open %s", sFilename);
 
 	char buffer[7];
-	uint32 bytesRead;
 
 	// If it's the support file, then move to the correct language area
 
@@ -130,7 +129,7 @@ void Disk::openFile(uint8 fileNum) {
 
 	// Validate the header
 
-	bytesRead = _fileHandle->read(buffer, 6);
+	_fileHandle->read(buffer, 6);
 	buffer[6] = '\0';
 	if (strcmp(buffer, HEADER_IDENT_STRING) != 0)
 		error("The file %s was not a valid VGA file", sFilename);

engines/lure/hotspots.cpp
@@ -763,7 +763,7 @@ void Hotspot::showMessage(uint16 messageId, uint16 destCharacterId) {
 	MemoryBlock *data = res.messagesData();
 	Hotspot *hotspot;
 	uint8 *msgData = (uint8 *) data->data();
-	uint16 v2, idVal;
+	uint16 idVal;
 	messageId &= 0x7fff;
 
 	// Skip through header to find table for given character
@@ -781,7 +781,6 @@ void Hotspot::showMessage(uint16 messageId, uint16 destCharacterId) {
 
 	// Scan through secondary list
 	uint16 *v = (uint16 *) (msgData + READ_LE_UINT16(msgData + idx + sizeof(uint16)));
-	v2 = 0;
 	while ((idVal = READ_LE_UINT16(v)) != 0xffff) {
 		++v;
 		if (READ_LE_UINT16(v) == messageId) break;

engines/sword25/gfx/image/art.cpp
@@ -2030,7 +2030,6 @@ static void art_svp_intersect_add_seg(ArtIntersectCtx *ctx, const ArtSVPSeg *in_
 	ArtActiveSeg *seg = art_new(ArtActiveSeg, 1);
 	ArtActiveSeg *test;
 	double x0, y0;
-	ArtActiveSeg *beg_range;
 	ArtActiveSeg *last = NULL;
 	ArtActiveSeg *left, *right;
 	ArtPriPoint *pri_pt = art_new(ArtPriPoint, 1);
@@ -2058,7 +2057,6 @@ static void art_svp_intersect_add_seg(ArtIntersectCtx *ctx, const ArtSVPSeg *in_
 
 	x0 = in_seg->points[0].x;
 	y0 = in_seg->points[0].y;
-	beg_range = NULL;
 	for (test = ctx->active_head; test != NULL; test = test->right) {
 		double d;
 		int test_bneg = test->flags & ART_ACTIVE_FLAGS_BNEG;

commit 1c711da8fc2f8099e641cfbe0a726e2f5ff8e308

SWORD25: Fix regression introduced in 5dd8f2575b0f
Janitorial removed function call which had a side effect.
Thus the actor image load code crashed.

engines/sword25/gfx/image/vectorimage.cpp
@@ -247,6 +247,9 @@ VectorImage::VectorImage(const byte *pFileData, uint fileSize, bool &success, co
 		return;
 	}
 
+	// readout SWF size
+	flashRectToBSRect(bs);
+
 	// Get frame rate and frame count
 	/* uint32 frameRate = */
 	bs.getUInt16();