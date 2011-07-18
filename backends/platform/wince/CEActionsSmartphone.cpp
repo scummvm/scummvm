46d15c5203c5d1a284f7aba87631bfde927f5cf6
CeRiAl <ikhatib@gmail.com>
2011-07-18 23:24:49 +0200

    WINCE: Change keymapping for AGI and Simon 1 & 2

--- backends/platform/wince/CEActionsSmartphone.cpp
+++ backends/platform/wince/CEActionsSmartphone.cpp
@@ -184,6 +184,8 @@ void CEActionsSmartphone::initInstanceGame() {
 		_key_action[SMARTPHONE_ACTION_MULTI].setKey(Common::ASCII_F1, SDLK_F1); // bargon : F1 to start
 	else if (gameid == "atlantis")
 		_key_action[SMARTPHONE_ACTION_MULTI].setKey(0, SDLK_KP0); // fate of atlantis : Ins to sucker-punch
+	else if (is_simon)
+		_key_action[SMARTPHONE_ACTION_MULTI].setKey(Common::ASCII_F10, SDLK_F10); // F10
 	else
 		_key_action[SMARTPHONE_ACTION_MULTI].setKey('V', SDLK_v, KMOD_SHIFT); // FT cheat : shift-V
 	// Bind keys
@@ -234,6 +236,15 @@ bool CEActionsSmartphone::perform(GUI::ActionType action, bool pushed) {
 				_key_action[action].setKey(SDLK_l);
 			else
 				_key_action[action].setKey(SDLK_s);
+		}
+		if (action == SMARTPHONE_ACTION_SKIP && ConfMan.get("gameid") == "agi") {
+			// In several AGI games (for example SQ2) it is needed to press F10 to exit from
+			// a screen. But we still want be able to skip normally with the skip button.
+			// Because of this, we inject a F10 keystroke here (this works and doesn't seem
+			// to have side-effects)
+			_key_action[action].setKey(Common::ASCII_F10, SDLK_F10); // F10
+			EventsBuffer::simulateKey(&_key_action[action], true);
+			_key_action[action].setKey(KEY_ALL_SKIP);
 		}
 		EventsBuffer::simulateKey(&_key_action[action], true);
 		return true;
