b609100a563ffd7d393edf089fb2e8cb5f41a5cf
Johannes Schickel <lordhoto@scummvm.org>
2011-07-24 19:53:48 +0200

    COMMON: Let intLog2 return an int instead of uint32, since it should return -1 for 0.

--- common/math.h
+++ common/math.h
@@ -58,7 +58,7 @@ static const char LogTable256[256] = {
 	LT(7), LT(7), LT(7), LT(7), LT(7), LT(7), LT(7), LT(7)
 };
 
-inline uint32 intLog2(uint32 v) {
+inline int intLog2(uint32 v) {
 	register uint32 t, tt;
 
 	if ((tt = v >> 16))
