251f7390fbcd732fc6dbd01fd9827c3384429b4e
Einar Johan T. Sømåen <einarjohants@gmail.com>
2011-07-29 11:27:03 +0200

    GRIM: verify _currScene only once per frame

--- engines/grim/grim.cpp
+++ engines/grim/grim.cpp
@@ -904,7 +904,7 @@ void GrimEngine::luaUpdate() {
 	// Run asynchronous tasks
 	lua_runtasks();
 
-	if (_mode == ENGINE_MODE_NORMAL || _mode == ENGINE_MODE_SMUSH) {
+	if (_currScene && (_mode == ENGINE_MODE_NORMAL || _mode == ENGINE_MODE_SMUSH)) {
 		// Update the actors. Do it here so that we are sure to react asap to any change
 		// in the actors state caused by lua.
 		for (ActorListType::iterator i = _actors.begin(); i != _actors.end(); ++i) {
@@ -915,7 +915,7 @@ void GrimEngine::luaUpdate() {
 			// Note that the actor need not be visible to update chores, for example:
 			// when Manny has just brought Meche back he is offscreen several times
 			// when he needs to perform certain chores
-			if (_currScene && a->isInSet(_currScene->getName()))
+			if (a->isInSet(_currScene->getName()))
 				a->update();
 		}
 		g_currentUpdatedActor = NULL;
