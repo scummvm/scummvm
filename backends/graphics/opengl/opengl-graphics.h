18a5d79c56179dc640ff9fe8e24f7bfd0bd77874
Chris Warren-Smith <cwarrensmith@gmail.com>
2011-08-13 08:06:03 +1000

    BADA: Fix formatting and non bada host compile error

--- backends/graphics/opengl/opengl-graphics.h
+++ backends/graphics/opengl/opengl-graphics.h
@@ -319,7 +319,7 @@ protected:
   /**
    * Returns the font used for on screen display
    */
-  virtual const Graphics::Font* getFontOSD();
+  virtual const Graphics::Font *getFontOSD();
 
 	/**
 	 * Update the OSD texture / surface.
