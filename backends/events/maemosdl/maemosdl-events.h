912a04053432bd344629f0ff4da8404cfcd4e2e0
Tarek Soliman <tarek@bashasoliman.com>
2011-08-26 17:22:23 -0500

    MAEMO: Zoom- is now mode selector for clicking

--- backends/events/maemosdl/maemosdl-events.h
+++ backends/events/maemosdl/maemosdl-events.h
@@ -33,8 +33,14 @@
  * SDL events manager for Maemo
  */
 class MaemoSdlEventSource : public SdlEventSource {
+public:
+	MaemoSdlEventSource();
 protected:
 	virtual bool remapKey(SDL_Event &ev, Common::Event &event);
+	virtual bool handleMouseButtonDown(SDL_Event &ev, Common::Event &event);
+	virtual bool handleMouseButtonUp(SDL_Event &ev, Common::Event &event);
+
+	bool _clickEnabled;
 };
 
 #endif
