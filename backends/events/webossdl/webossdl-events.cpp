2399907f1c124adabbdb09e0a9cebad20b4ad4a6
TomFrost <tom@frosteddesign.com>
2011-09-10 07:22:59 -0400

    WEBOS: Temporary fix: removing the pointer distance limitations to allow its use with a different aspect ratio.

--- backends/events/webossdl/webossdl-events.cpp
+++ backends/events/webossdl/webossdl-events.cpp
@@ -255,10 +255,12 @@ bool WebOSSdlEventSource::handleMouseButtonDown(SDL_Event &ev, Common::Event &ev
 		}
 		// If we're not in touchpad mode, move the cursor to the tap
 		if (!touchpadMode) {
-			int screenX = g_system->getWidth();
-			int screenY = g_system->getHeight();
-			curX = MIN(screenX, MAX(0, 0 + ev.motion.x));
-			curY = MIN(screenY, MAX(0, 0 + ev.motion.y));
+			//int screenX = g_system->getWidth();
+			//int screenY = g_system->getHeight();
+			//curX = MIN(screenX, MAX(0, 0 + ev.motion.x));
+			//curY = MIN(screenY, MAX(0, 0 + ev.motion.y));
+			curX = ev.motion.x;
+			curY = ev.motion.y;
 			event.type = Common::EVENT_MOUSEMOVE;
 			processMouseEvent(event, curX, curY);
 		}
@@ -389,11 +391,15 @@ bool WebOSSdlEventSource::handleMouseMotion(SDL_Event &ev, Common::Event &event)
 				// the mouse pointer.
 				if (!fingerDown[1] && !fingerDown[2]) {
 					if (touchpadMode) {
-						curX = MIN(screenX, MAX(0, curX + ev.motion.xrel));
-						curY = MIN(screenY, MAX(0, curY + ev.motion.yrel));
+						//curX = MIN(screenX, MAX(0, curX + ev.motion.xrel));
+						//curY = MIN(screenY, MAX(0, curY + ev.motion.yrel));
+						curX += ev.motion.xrel;
+						curY += ev.motion.yrel;
 					} else {
-						curX = MIN(screenX, MAX(0, 0 + ev.motion.x));
-						curY = MIN(screenY, MAX(0, 0 + ev.motion.y));
+						//curX = MIN(screenX, MAX(0, 0 + ev.motion.x));
+						//curY = MIN(screenY, MAX(0, 0 + ev.motion.y));
+						curX = ev.motion.x;
+						curY = ev.motion.y;
 					}
 					event.type = Common::EVENT_MOUSEMOVE;
 					processMouseEvent(event, curX, curY);
