scummvm/scummvm

commit b1cb88ddc25da64772bbba253bd2210678189ace

SCUMM: cleanup sjis font rendering code

engines/scumm/charset.h
@@ -78,10 +78,6 @@ public:
 	void addLinebreaks(int a, byte *str, int pos, int maxwidth);
 	void translateColor();
 
-#ifndef DISABLE_TOWNS_DUAL_LAYER_MODE
-	void processTownsCharsetColors(uint8 bytesPerPixel);
-#endif
-
 	virtual void setCurID(int32 id) = 0;
 	int getCurID() { return _curId; }
 
@@ -101,31 +97,26 @@ protected:
 	int _fontHeight;
 	int _numChars;
 
-	enum ShadowMode {
-		kNoShadowMode,
-		kFMTOWNSShadowMode,
-		kNormalShadowMode
-	};
 	byte _shadowColor;
-	ShadowMode _shadowMode;
-
-	void enableShadow(bool enable);
-	virtual void drawBits1(const Graphics::Surface &s, byte *dst, const byte *src, int drawTop, int width, int height, uint8 bitDepth, bool scale2x = false);
-
+	bool _shadowMode;
 
 public:
 	CharsetRendererCommon(ScummEngine *vm);
 
 	void setCurID(int32 id);
 
-	int getFontHeight();
+	virtual int getFontHeight();
 };
 
 class CharsetRendererClassic : public CharsetRendererCommon {
 protected:
-	void drawBitsN(const Graphics::Surface &s, byte *dst, const byte *src, byte bpp, int drawTop, int width, int height, bool scale2x = false);
+	virtual void drawBitsN(const Graphics::Surface &s, byte *dst, const byte *src, byte bpp, int drawTop, int width, int height);
+	void printCharIntern(bool is2byte, const byte *charPtr, int origWidth, int origHeight, int width, int height, VirtScreen *vs, bool ignoreCharsetMask);	
+	virtual void prepareDraw(uint16 chr);
 
-	void printCharIntern(bool is2byte, const byte *charPtr, int origWidth, int origHeight, int width, int height, VirtScreen *vs, bool ignoreCharsetMask);
+	int _width, _height, _origWidth, _origHeight;
+	int _offsX, _offsY;
+	const byte *_charPtr;
 
 public:
 	CharsetRendererClassic(ScummEngine *vm) : CharsetRendererCommon(vm) {}
@@ -134,18 +125,34 @@ public:
 	void drawChar(int chr, Graphics::Surface &s, int x, int y);
 
 	int getCharWidth(uint16 chr);
-
-	// Some SCUMM 5 games contain hard coded logic to determine whether to use
-	// the SCUMM fonts or the FM-Towns font rom to draw a character. For the other
-	// games we will simply check for a character greater 127.
-	bool useTownsFontRomCharacter(uint16 chr);
 };
+
+#ifdef USE_RGB_COLOR
+#ifndef DISABLE_TOWNS_DUAL_LAYER_MODE
+class CharsetRendererTownsClassic : public CharsetRendererClassic {
+public:
+	CharsetRendererTownsClassic(ScummEngine *vm);
+
+	int getCharWidth(uint16 chr);
+	int getFontHeight();
+
+private:
+	void drawBitsN(const Graphics::Surface &s, byte *dst, const byte *src, byte bpp, int drawTop, int width, int height);
+	void prepareDraw(uint16 chr);
+	void setupShadowMode();
+	bool useFontRomCharacter(uint16 chr);
+	void processCharsetColors();
+
+	uint16 _sjisCurChar;
+};
+#endif
+#endif
 
 class CharsetRendererNES : public CharsetRendererCommon {
 protected:
 	byte *_trTable;
 
-	void drawBits1(const Graphics::Surface &s, byte *dst, const byte *src, int drawTop, int width, int height, uint8 bitDepth, bool scale2x = false);
+	void drawBits1(const Graphics::Surface &s, byte *dst, const byte *src, int drawTop, int width, int height, uint8 bitDepth);
 
 public:
 	CharsetRendererNES(ScummEngine *vm) : CharsetRendererCommon(vm) {}
@@ -160,6 +167,12 @@ public:
 
 class CharsetRendererV3 : public CharsetRendererCommon {
 protected:
+	virtual void enableShadow(bool enable);
+	virtual void drawBits1(const Graphics::Surface &s, byte *dst, const byte *src, int drawTop, int width, int height, uint8 bitDepth);
+	virtual int getDrawWidthIntern(uint16 chr);
+	virtual int getDrawHeightIntern(uint16 chr);
+	virtual void setDrawCharIntern(uint16 chr) {}
+
 	const byte *_widthTable;
 
 public:
@@ -169,13 +182,34 @@ public:
 	void drawChar(int chr, Graphics::Surface &s, int x, int y);
 	void setCurID(int32 id);
 	void setColor(byte color);
+	virtual int getCharWidth(uint16 chr);
+};
+
+class CharsetRendererTownsV3 : public CharsetRendererV3 {
+public:
+	CharsetRendererTownsV3(ScummEngine *vm);
+
 	int getCharWidth(uint16 chr);
+	int getFontHeight();
+	
+private:
+	void enableShadow(bool enable);
+	void drawBits1(const Graphics::Surface &s, byte *dst, const byte *src, int drawTop, int width, int height, uint8 bitDepth);
+#ifndef DISABLE_TOWNS_DUAL_LAYER_MODE
+	int getDrawWidthIntern(uint16 chr);
+	int getDrawHeightIntern(uint16 chr);
+	void setDrawCharIntern(uint16 chr);
+#endif
+	uint16 _sjisCurChar;
 };
 
 #ifdef USE_RGB_COLOR
 class CharsetRendererPCE : public CharsetRendererV3 {
 protected:
-	void drawBits1(const Graphics::Surface &s, byte *dst, const byte *src, int drawTop, int width, int height, uint8 bitDepth, bool scale2x = false);
+	void drawBits1(const Graphics::Surface &s, byte *dst, const byte *src, int drawTop, int width, int height, uint8 bitDepth);
+
+	int getDrawWidthIntern(uint16 chr);
+	int getDrawHeightIntern(uint16 chr);
 
 public:
 	CharsetRendererPCE(ScummEngine *vm) : CharsetRendererV3(vm) {}

engines/scumm/scumm.cpp
@@ -284,7 +284,6 @@ ScummEngine::ScummEngine(OSystem *syst, const DetectorResult &dr)
 #ifndef DISABLE_TOWNS_DUAL_LAYER_MODE
 	_townsScreen = 0;
 	_cjkFont = 0;
-	_cjkChar = 0;
 #endif
 	_shadowPalette = NULL;
 	_shadowPaletteSize = 0;
@@ -1350,13 +1349,23 @@ void ScummEngine::setupCharsetRenderer() {
 			_charset = new CharsetRendererPCE(this);
 		else
 #endif
+		if (_game.platform == Common::kPlatformFMTowns)
+			_charset = new CharsetRendererTownsV3(this);
+		else
 			_charset = new CharsetRendererV3(this);
 #ifdef ENABLE_SCUMM_7_8
 	} else if (_game.version == 8) {
 		_charset = new CharsetRendererNut(this);
 #endif
 	} else {
-		_charset = new CharsetRendererClassic(this);
+#ifdef USE_RGB_COLOR
+#ifndef DISABLE_TOWNS_DUAL_LAYER_MODE
+		if (_game.platform == Common::kPlatformFMTowns)
+			_charset = new CharsetRendererTownsClassic(this);
+		else
+#endif
+#endif
+			_charset = new CharsetRendererClassic(this);
 	}
 }
 

engines/scumm/scumm.h
@@ -46,9 +46,10 @@
 /* This disables the dual layer mode which is used in FM-Towns versions
  * of SCUMM games and which emulates the behavior of the original code.
  * The only purpose is code size reduction for certain backends.
- * SCUMM 3 (FM-Towns) games will run in normal (DOS VGA) mode, which should
- * work just fine in most situations. Some glitches might occur. SCUMM 5 games
- * will not work without dual layer (and 16 bit color) support.
+ * SCUMM 3 (FM-Towns) games will run in English in normal (DOS VGA) mode,
+ * which should work just fine in most situations. Some glitches might
+ * occur. Japanese mode and SCUMM 5 FM-Towns games will not work without
+ * dual layer (and 16 bit color) support.
  */
 #define DISABLE_TOWNS_DUAL_LAYER_MODE
 #endif
@@ -345,6 +346,7 @@ class ResourceManager;
 class ScummEngine : public Engine {
 	friend class ScummDebugger;
 	friend class CharsetRenderer;
+	friend class CharsetRendererTownsClassic;
 	friend class ResourceManager;
 
 public:
@@ -1333,7 +1335,6 @@ public:
 	byte _townsPaletteFlags;
 	byte _townsCharsetColorMap[16];
 	Graphics::FontSJIS *_cjkFont;
-	uint16 _cjkChar;
 
 protected:
 	void towns_drawStripToScreen(VirtScreen *vs, int dstX, int dstY, int srcX, int srcY, int w, int h);

commit 0d1d974e14f80dda31e306716b4f9e779c6c35fc

SCUMM FM-TOWNS: fix regression and cleanup
(LOOM mouse cursor was broken in b1cb88ddc25da64772bbba253bd2210678189ace)

engines/scumm/charset.cpp
@@ -511,14 +511,15 @@ void CharsetRendererV3::enableShadow(bool enable) {
 	_shadowMode = enable;
 }
 
-void CharsetRendererV3::drawBits1(const Graphics::Surface &s, byte *dst, const byte *src, int drawTop, int width, int height, uint8 bitDepth) {
-	int y, x;
+void CharsetRendererV3::drawBits1(Graphics::Surface &dest, int x, int y, const byte *src, int drawTop, int width, int height) {
+	byte *dst = (byte *)dest.getBasePtr(x, y);
+
 	byte bits = 0;
 	uint8 col = _color;
-	int pitch = s.pitch - width * bitDepth;
-	byte *dst2 = dst + s.pitch;
+	int pitch = dest.pitch - width * dest.format.bytesPerPixel;
+	byte *dst2 = dst + dest.pitch;
 
-	for (y = 0; y < height && y + drawTop < s.h; y++) {
+	for (y = 0; y < height && y + drawTop < dest.h; y++) {
 		for (x = 0; x < width; x++) {
 			if ((x % 8) == 0)
 				bits = *src++;
@@ -527,8 +528,8 @@ void CharsetRendererV3::drawBits1(const Graphics::Surface &s, byte *dst, const b
 					dst[1] = dst2[0] = dst2[1] = _shadowColor;
 				dst[0] = col;
 			}
-			dst += bitDepth;
-			dst2 += bitDepth;
+			dst += dest.format.bytesPerPixel;
+			dst2 += dest.format.bytesPerPixel;
 		}
 
 		dst += pitch;
@@ -591,7 +592,6 @@ void CharsetRendererV3::printChar(int chr, bool ignoreCharsetMask) {
 	int width, height, origWidth = 0, origHeight;
 	VirtScreen *vs;
 	const byte *charPtr;
-	byte *dst;
 	int is2byte = (chr >= 256 && _vm->_useCJKMode) ? 1 : 0;
 
 	assertRange(0, _curId, _vm->_numCharsets - 1, "charset");
@@ -636,13 +636,10 @@ void CharsetRendererV3::printChar(int chr, bool ignoreCharsetMask) {
 		_textScreenID = vs->number;
 	}
 
-	if ((ignoreCharsetMask || !vs->hasTwoBuffers)) {
-		dst = vs->getPixels(_left, drawTop);
-		drawBits1(*vs, dst, charPtr, drawTop, origWidth, origHeight, vs->format.bytesPerPixel);
-	} else {
-		dst = (byte *)_vm->_textSurface.getBasePtr(_left * _vm->_textSurfaceMultiplier, _top * _vm->_textSurfaceMultiplier);
-		drawBits1(_vm->_textSurface, dst, charPtr, drawTop, origWidth, origHeight, _vm->_textSurface.format.bytesPerPixel);
-	}
+	if ((ignoreCharsetMask || !vs->hasTwoBuffers) && (_vm->_game.platform != Common::kPlatformFMTowns))
+		drawBits1(*vs, _left + vs->xstart, drawTop, charPtr, drawTop, origWidth, origHeight);
+	else
+		drawBits1(_vm->_textSurface, _left * _vm->_textSurfaceMultiplier, _top * _vm->_textSurfaceMultiplier, charPtr, drawTop, origWidth, origHeight);
 	
 	if (is2byte) {
 		origWidth /= _vm->_textSurfaceMultiplier;
@@ -669,9 +666,7 @@ void CharsetRendererV3::drawChar(int chr, Graphics::Surface &s, int x, int y) {
 	int width = getDrawWidthIntern(chr);
 	int height = getDrawHeightIntern(chr);
 	setDrawCharIntern(chr);
-
-	byte *dst = (byte *)s.pixels + y * s.pitch + x;
-	drawBits1(s, dst, charPtr, y, width, height, s.format.bytesPerPixel);
+	drawBits1(s, x, y, charPtr, y, width, height);
 }
 
 void CharsetRenderer::translateColor() {
@@ -993,50 +988,43 @@ void CharsetRendererTownsV3::enableShadow(bool enable) {
 #endif
 }
 
-void CharsetRendererTownsV3::drawBits1(const Graphics::Surface &s, byte *dst, const byte *src, int drawTop, int width, int height, uint8 bitDepth) {
+void CharsetRendererTownsV3::drawBits1(Graphics::Surface &dest, int x, int y, const byte *src, int drawTop, int width, int height) {
 #ifndef DISABLE_TOWNS_DUAL_LAYER_MODE
 #ifdef USE_RGB_COLOR
 	if (_sjisCurChar) {
 		assert(_vm->_cjkFont);
-		_vm->_cjkFont->drawChar(_vm->_textSurface, _sjisCurChar, _left * _vm->_textSurfaceMultiplier, _top * _vm->_textSurfaceMultiplier, _color, _shadowColor);
+		_vm->_cjkFont->drawChar(dest, _sjisCurChar, x, y, _color, _shadowColor);
 		return;
 	}
 #endif
-
-	dst = (byte *)_vm->_textSurface.getBasePtr(_left * _vm->_textSurfaceMultiplier, _top * _vm->_textSurfaceMultiplier);
-	int sfPitch = _vm->_textSurface.pitch;
-	int sfHeight = _vm->_textSurface.h;
-	bool scale2x = (_vm->_textSurfaceMultiplier == 2 && !(_sjisCurChar >= 256 && _vm->_useCJKMode));
-#else
-	int sfPitch = s.pitch;
-	int sfHeight = s.h;
+	byte *dst = (byte *)dest.getBasePtr(x, y);
+	bool scale2x = ((&dest == &_vm->_textSurface) && (_vm->_textSurfaceMultiplier == 2) && !(_sjisCurChar >= 256 && _vm->_useCJKMode));
 #endif
 
-	int y, x;
 	byte bits = 0;
 	uint8 col = _color;
-	int pitch = sfPitch - width * bitDepth;
-	byte *dst2 = dst + sfPitch;
+	int pitch = dest.pitch - width * dest.format.bytesPerPixel;
+	byte *dst2 = dst + dest.pitch;
 
 #ifndef DISABLE_TOWNS_DUAL_LAYER_MODE
 	byte *dst3 = dst2;
 	byte *dst4 = dst2;
 	if (scale2x) {
-		dst3 = dst2 + sfPitch;
-		dst4 = dst3 + sfPitch;
+		dst3 = dst2 + dest.pitch;
+		dst4 = dst3 + dest.pitch;
 		pitch <<= 1;
 	}
 #endif
 
-	for (y = 0; y < height && y + drawTop < sfHeight; y++) {
+	for (y = 0; y < height && y + drawTop < dest.h; y++) {
 		for (x = 0; x < width; x++) {
 			if ((x % 8) == 0)
 				bits = *src++;
 			if ((bits & revBitMask(x % 8)) && y + drawTop >= 0) {
-				if (bitDepth == 2) {
+				if (dest.format.bytesPerPixel == 2) {
 					if (_shadowMode) {
 						WRITE_UINT16(dst + 2, _vm->_16BitPalette[_shadowColor]);
-						WRITE_UINT16(dst + sfPitch, _vm->_16BitPalette[_shadowColor]);
+						WRITE_UINT16(dst + dest.pitch, _vm->_16BitPalette[_shadowColor]);
 					}
 					WRITE_UINT16(dst, _vm->_16BitPalette[_color]);
 				} else {
@@ -1059,8 +1047,8 @@ void CharsetRendererTownsV3::drawBits1(const Graphics::Surface &s, byte *dst, co
 #endif
 				}
 			}
-			dst += bitDepth;
-			dst2 += bitDepth;
+			dst += dest.format.bytesPerPixel;
+			dst2 += dest.format.bytesPerPixel;
 #ifndef DISABLE_TOWNS_DUAL_LAYER_MODE
 			if (scale2x) {
 				dst++;
@@ -1106,45 +1094,45 @@ void CharsetRendererTownsV3::setDrawCharIntern(uint16 chr) {
 #endif
 
 #ifdef USE_RGB_COLOR
-void CharsetRendererPCE::drawBits1(const Graphics::Surface &s, byte *dst, const byte *src, int drawTop, int width, int height, uint8 bitDepth) {
+void CharsetRendererPCE::drawBits1(Graphics::Surface &dest, int x, int y, const byte *src, int drawTop, int width, int height) {
+	byte *dst = (byte *)dest.getBasePtr(x, y);
 	if (_sjisCurChar) {
 		assert(_vm->_cjkFont);
 		uint16 col1 = _color;
 		uint16 col2 = _shadowColor;
 
-		if (s.format.bytesPerPixel == 2) {
+		if (dest.format.bytesPerPixel == 2) {
 			col1 = _vm->_16BitPalette[col1];
 			col2 = _vm->_16BitPalette[col2];
 		}
 
-		_vm->_cjkFont->drawChar(dst, _sjisCurChar, s.pitch, s.format.bytesPerPixel, col1, col2, -1, -1);
+		_vm->_cjkFont->drawChar(dst, _sjisCurChar, dest.pitch, dest.format.bytesPerPixel, col1, col2, -1, -1);
 		return;
 	}
 
-	int y, x;
 	byte bits = 0;
 
-	for (y = 0; y < height && y + drawTop < s.h; y++) {
+	for (y = 0; y < height && y + drawTop < dest.h; y++) {
 		int bitCount = 0;
 		for (x = 0; x < width; x++) {
 			if ((bitCount % 8) == 0)
 				bits = *src++;
 			if ((bits & revBitMask(bitCount % 8)) && y + drawTop >= 0) {
-				if (bitDepth == 2) {
+				if (dest.format.bytesPerPixel == 2) {
 					if (_shadowMode)
-						WRITE_UINT16(dst + s.pitch + 2, _vm->_16BitPalette[_shadowColor]);
+						WRITE_UINT16(dst + dest.pitch + 2, _vm->_16BitPalette[_shadowColor]);
 					WRITE_UINT16(dst, _vm->_16BitPalette[_color]);
 				} else {
 					if (_shadowMode)
-						*(dst + s.pitch + 1) = _shadowColor;
+						*(dst + dest.pitch + 1) = _shadowColor;
 					*dst = _color;
 				}
 			}
-			dst += bitDepth;
+			dst += dest.format.bytesPerPixel;
 			bitCount++;
 		}
 
-		dst += s.pitch - width * bitDepth;
+		dst += dest.pitch - width * dest.format.bytesPerPixel;
 	}
 }
 
@@ -1282,7 +1270,7 @@ void CharsetRendererNut::printChar(int chr, bool ignoreCharsetMask) {
 void CharsetRendererNES::printChar(int chr, bool ignoreCharsetMask) {
 	int width, height, origWidth, origHeight;
 	VirtScreen *vs;
-	byte *charPtr, *dst;
+	byte *charPtr;
 
 	// Init it here each time since it is cheap and fixes bug with
 	// charset after game load
@@ -1322,13 +1310,10 @@ void CharsetRendererNES::printChar(int chr, bool ignoreCharsetMask) {
 		_textScreenID = vs->number;
 	}
 
-	if (ignoreCharsetMask || !vs->hasTwoBuffers) {
-		dst = vs->getPixels(_left, drawTop);
-		drawBits1(*vs, dst, charPtr, drawTop, origWidth, origHeight, vs->format.bytesPerPixel);
-	} else {
-		dst = (byte *)_vm->_textSurface.pixels + _top * _vm->_textSurface.pitch + _left;
-		drawBits1(_vm->_textSurface, dst, charPtr, drawTop, origWidth, origHeight, _vm->_textSurface.format.bytesPerPixel);
-	}
+	if (ignoreCharsetMask || !vs->hasTwoBuffers)
+		drawBits1(*vs, _left + vs->xstart, drawTop, charPtr, drawTop, origWidth, origHeight);
+	else
+		drawBits1(_vm->_textSurface, _left, _top, charPtr, drawTop, origWidth, origHeight);
 
 	if (_str.left > _left)
 		_str.left = _left;
@@ -1346,7 +1331,7 @@ void CharsetRendererNES::printChar(int chr, bool ignoreCharsetMask) {
 }
 
 void CharsetRendererNES::drawChar(int chr, Graphics::Surface &s, int x, int y) {
-	byte *charPtr, *dst;
+	byte *charPtr;
 	int width, height;
 
 	if (!_trTable)
@@ -1356,8 +1341,7 @@ void CharsetRendererNES::drawChar(int chr, Graphics::Surface &s, int x, int y) {
 	width = getCharWidth(chr);
 	height = 8;
 
-	dst = (byte *)s.pixels + y * s.pitch + x;
-	drawBits1(s, dst, charPtr, y, width, height, s.format.bytesPerPixel);
+	drawBits1(s, x, y, charPtr, y, width, height);
 }
 
 #ifdef USE_RGB_COLOR
@@ -1557,14 +1541,15 @@ void CharsetRendererTownsClassic::processCharsetColors() {
 #endif
 #endif
 
-void CharsetRendererNES::drawBits1(const Graphics::Surface &s, byte *dst, const byte *src, int drawTop, int width, int height, uint8 bitDepth) {
+void CharsetRendererNES::drawBits1(Graphics::Surface &dest, int x, int y, const byte *src, int drawTop, int width, int height) {
+	byte *dst = (byte *)dest.getBasePtr(x, y);
 	for (int i = 0; i < 8; i++) {
 		byte c0 = src[i];
 		byte c1 = src[i + 8];
 		for (int j = 0; j < 8; j++)
 			dst[j] = _vm->_NESPalette[0][((c0 >> (7 - j)) & 1) | (((c1 >> (7 - j)) & 1) << 1) |
 			(_color ? 12 : 8)];
-		dst += s.pitch;
+		dst += dest.pitch;
 	}
 }
 

engines/scumm/charset.h
@@ -156,7 +156,7 @@ class CharsetRendererNES : public CharsetRendererCommon {
 protected:
 	byte *_trTable;
 
-	void drawBits1(const Graphics::Surface &s, byte *dst, const byte *src, int drawTop, int width, int height, uint8 bitDepth);
+	void drawBits1(Graphics::Surface &dest, int x, int y, const byte *src, int drawTop, int width, int height);
 
 public:
 	CharsetRendererNES(ScummEngine *vm) : CharsetRendererCommon(vm) {}
@@ -172,7 +172,7 @@ public:
 class CharsetRendererV3 : public CharsetRendererCommon {
 protected:
 	virtual void enableShadow(bool enable);
-	virtual void drawBits1(const Graphics::Surface &s, byte *dst, const byte *src, int drawTop, int width, int height, uint8 bitDepth);
+	virtual void drawBits1(Graphics::Surface &dest, int x, int y, const byte *src, int drawTop, int width, int height);
 	virtual int getDrawWidthIntern(uint16 chr);
 	virtual int getDrawHeightIntern(uint16 chr);
 	virtual void setDrawCharIntern(uint16 chr) {}
@@ -198,7 +198,7 @@ public:
 	
 private:
 	void enableShadow(bool enable);
-	void drawBits1(const Graphics::Surface &s, byte *dst, const byte *src, int drawTop, int width, int height, uint8 bitDepth);
+	void drawBits1(Graphics::Surface &dest, int x, int y, const byte *src, int drawTop, int width, int height);
 #ifndef DISABLE_TOWNS_DUAL_LAYER_MODE
 	int getDrawWidthIntern(uint16 chr);
 	int getDrawHeightIntern(uint16 chr);
@@ -210,7 +210,7 @@ private:
 #ifdef USE_RGB_COLOR
 class CharsetRendererPCE : public CharsetRendererV3 {
 private:
-	void drawBits1(const Graphics::Surface &s, byte *dst, const byte *src, int drawTop, int width, int height, uint8 bitDepth);
+	void drawBits1(Graphics::Surface &dest, int x, int y, const byte *src, int drawTop, int width, int height);
 
 	int getDrawWidthIntern(uint16 chr);
 	int getDrawHeightIntern(uint16 chr);