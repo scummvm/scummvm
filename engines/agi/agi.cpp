bc3c68c5088a39cf2befe5fba5bb2056643c4b4f
Filippos Karapetis <md5@scummvm.org>
2011-09-25 18:39:22 +0300

    AGI: Fixed bug #3292784 - "AGI: SQ2: Name forgotten after game restart"
    The string buffer should not be cleared on game restart, as game scripts
    depend on it being maintained unchanged during restart.

--- engines/agi/agi.cpp
+++ engines/agi/agi.cpp
@@ -362,9 +362,15 @@ int AgiEngine::agiInit() {
 
 	initPriTable();
 
-	// clear string buffer
-	for (i = 0; i < MAX_STRINGS; i++)
-		_game.strings[i][0] = 0;
+	// Clear the string buffer on startup, but not when the game restarts, as
+	// some scripts expect that the game strings remain unaffected after a
+	// restart. An example is script 98 in SQ2, which is not invoked on restart
+	// to ask Ego's name again. The name is supposed to be maintained in string 1.
+	// Fixes bug #3292784.
+	if (!_restartGame) {
+		for (i = 0; i < MAX_STRINGS; i++)
+			_game.strings[i][0] = 0;
+	}
 
 	// setup emulation
 
