2947390db9ae2c7d6ab38229532610c4bb494543
Tarek Soliman <tsoliman@scummvm.org>
2011-10-15 00:38:10 -0500

    KYRA: Cleanup game keymaps on rtl
    This fixes an RTL problem where the keymaps linger between lol and
    kyra1

--- engines/kyra/lol.cpp
+++ engines/kyra/lol.cpp
@@ -247,6 +247,10 @@ LoLEngine::LoLEngine(OSystem *system, const GameFlags &flags) : KyraEngine_v1(sy
 LoLEngine::~LoLEngine() {
 	setupPrologueData(false);
 
+#ifdef ENABLE_KEYMAPPER
+	_eventMan->getKeymapper()->cleanupGameKeymaps();
+#endif
+
 	delete[] _landsFile;
 	delete[] _levelLangFile;
 
