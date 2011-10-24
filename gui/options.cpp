scummvm/scummvm

commit 77c65648b1db8f8b7b245510681eafd856798d26

AD: Swtich GUI options to a char array.
This eliminates nasty limitation of caping number of flags to 31.
Current code has limitation of 255 flags, though.

Only SCUMM engine is converted, rest do not even compile.
Detection of fan talkie MI is broken as it has to be implemented
differently.

gui/options.cpp
@@ -141,7 +141,7 @@ void OptionsDialog::init() {
 	_oldTheme = g_gui.theme()->getThemeId();
 
 	// Retrieve game GUI options
-	_guioptions = 0;
+	_guioptions = "";
 	if (ConfMan.hasKey("guioptions", _domain)) {
 		_guioptionsString = ConfMan.get("guioptions", _domain);
 		_guioptions = parseGameGUIOptions(_guioptionsString);
@@ -155,7 +155,7 @@ void OptionsDialog::open() {
 	setResult(0);
 
 	// Retrieve game GUI options
-	_guioptions = 0;
+	_guioptions = "";
 	if (ConfMan.hasKey("guioptions", _domain)) {
 		_guioptionsString = ConfMan.get("guioptions", _domain);
 		_guioptions = parseGameGUIOptions(_guioptionsString);
@@ -595,11 +595,15 @@ void OptionsDialog::setAudioSettingsState(bool enabled) {
 	_midiPopUpDesc->setEnabled(enabled);
 	_midiPopUp->setEnabled(enabled);
 
-	uint32 allFlags = MidiDriver::musicType2GUIO((uint32)-1);
+	Common::String allFlags = MidiDriver::musicType2GUIO((uint32)-1);
+	char opt[256];
+
+	strncpy(opt, _guioptions.c_str(), 256);
+	bool hasMidiDefined = (strtok(opt, allFlags.c_str()) != NULL);
 
 	if (_domain != Common::ConfigManager::kApplicationDomain && // global dialog
-			(_guioptions & allFlags) && // No flags are specified
-				!(_guioptions & Common::GUIO_MIDIADLIB)) {
+		hasMidiDefined && // No flags are specified
+		!(_guioptions.contains(GUIO_MIDIADLIB))) {
 		_oplPopUpDesc->setEnabled(false);
 		_oplPopUp->setEnabled(false);
 	} else {
@@ -611,7 +615,7 @@ void OptionsDialog::setAudioSettingsState(bool enabled) {
 }
 
 void OptionsDialog::setMIDISettingsState(bool enabled) {
-	if (_guioptions & Common::GUIO_NOMIDI)
+	if (_guioptions.contains(GUIO_NOMIDI))
 		enabled = false;
 
 	_gmDevicePopUpDesc->setEnabled(_domain.equals(Common::ConfigManager::kApplicationDomain) ? enabled : false);
@@ -649,7 +653,7 @@ void OptionsDialog::setVolumeSettingsState(bool enabled) {
 	_enableVolumeSettings = enabled;
 
 	ena = enabled && !_muteCheckbox->getState();
-	if (_guioptions & Common::GUIO_NOMUSIC)
+	if (_guioptions.contains(GUIO_NOMUSIC))
 		ena = false;
 
 	_musicVolumeDesc->setEnabled(ena);
@@ -657,7 +661,7 @@ void OptionsDialog::setVolumeSettingsState(bool enabled) {
 	_musicVolumeLabel->setEnabled(ena);
 
 	ena = enabled && !_muteCheckbox->getState();
-	if (_guioptions & Common::GUIO_NOSFX)
+	if (_guioptions.contains(GUIO_NOSFX))
 		ena = false;
 
 	_sfxVolumeDesc->setEnabled(ena);
@@ -665,7 +669,7 @@ void OptionsDialog::setVolumeSettingsState(bool enabled) {
 	_sfxVolumeLabel->setEnabled(ena);
 
 	ena = enabled && !_muteCheckbox->getState();
-	if (_guioptions & Common::GUIO_NOSPEECH)
+	if (_guioptions.contains(GUIO_NOSPEECH))
 		ena = false;
 
 	_speechVolumeDesc->setEnabled(ena);
@@ -680,14 +684,14 @@ void OptionsDialog::setSubtitleSettingsState(bool enabled) {
 	_enableSubtitleSettings = enabled;
 
 	ena = enabled;
-	if ((_guioptions & Common::GUIO_NOSUBTITLES) || (_guioptions & Common::GUIO_NOSPEECH))
+	if ((_guioptions.contains(GUIO_NOSUBTITLES)) || (_guioptions.contains(GUIO_NOSPEECH)))
 		ena = false;
 
 	_subToggleGroup->setEnabled(ena);
 	_subToggleDesc->setEnabled(ena);
 
 	ena = enabled;
-	if (_guioptions & Common::GUIO_NOSUBTITLES)
+	if (_guioptions.contains(GUIO_NOSUBTITLES))
 		ena = false;
 
 	_subSpeedDesc->setEnabled(ena);
@@ -741,22 +745,26 @@ void OptionsDialog::addAudioControls(GuiObject *boss, const Common::String &pref
 	_midiPopUp = new PopUpWidget(boss, prefix + "auMidiPopup", _("Specifies output sound device or sound card emulator"));
 
 	// Populate it
-	uint32 allFlags = MidiDriver::musicType2GUIO((uint32)-1);
+	Common::String allFlags = MidiDriver::musicType2GUIO((uint32)-1);
+	char opt[256];
+
+	strncpy(opt, _guioptions.c_str(), 256);
+	bool hasMidiDefined = (strtok(opt, allFlags.c_str()) != NULL);
 
 	const MusicPlugin::List p = MusicMan.getPlugins();
 	for (MusicPlugin::List::const_iterator m = p.begin(); m != p.end(); ++m) {
 		MusicDevices i = (**m)->getDevices();
 		for (MusicDevices::iterator d = i.begin(); d != i.end(); ++d) {
-			const uint32 deviceGuiOption = MidiDriver::musicType2GUIO(d->getMusicType());
+			Common::String deviceGuiOption = MidiDriver::musicType2GUIO(d->getMusicType());
 
 			if ((_domain == Common::ConfigManager::kApplicationDomain && d->getMusicType() != MT_TOWNS  // global dialog - skip useless FM-Towns, C64, Amiga, AppleIIGS options there
 				 && d->getMusicType() != MT_C64 && d->getMusicType() != MT_AMIGA && d->getMusicType() != MT_APPLEIIGS && d->getMusicType() != MT_PC98)
-				|| (_domain != Common::ConfigManager::kApplicationDomain && !(_guioptions & allFlags)) // No flags are specified
-				|| (_guioptions & deviceGuiOption) // flag is present
+				|| (_domain != Common::ConfigManager::kApplicationDomain && !hasMidiDefined) // No flags are specified
+				|| (_guioptions.contains(deviceGuiOption)) // flag is present
 				// HACK/FIXME: For now we have to show GM devices, even when the game only has GUIO_MIDIMT32 set,
 				// else we would not show for example external devices connected via ALSA, since they are always
 				// marked as General MIDI device.
-				|| (deviceGuiOption == Common::GUIO_MIDIGM && (_guioptions & Common::GUIO_MIDIMT32))
+				|| (deviceGuiOption.contains(GUIO_MIDIGM) && (_guioptions.contains(GUIO_MIDIMT32)))
 				|| d->getMusicDriverId() == "auto" || d->getMusicDriverId() == "null") // always add default and null device
 				_midiPopUp->appendEntry(d->getCompleteName(), d->getHandle());
 		}
@@ -997,9 +1005,9 @@ void OptionsDialog::saveMusicDeviceSetting(PopUpWidget *popup, Common::String se
 }
 
 int OptionsDialog::getSubtitleMode(bool subtitles, bool speech_mute) {
-	if (_guioptions & Common::GUIO_NOSUBTITLES)
+	if (_guioptions.contains(GUIO_NOSUBTITLES))
 		return kSubtitlesSpeech; // Speech only
-	if (_guioptions & Common::GUIO_NOSPEECH)
+	if (_guioptions.contains(GUIO_NOSPEECH))
 		return kSubtitlesSubs; // Subtitles only
 
 	if (!subtitles && !speech_mute) // Speech only

audio/mididrv.cpp
@@ -55,27 +55,30 @@ const byte MidiDriver::_gmToMt32[128] = {
 	101, 103, 100, 120, 117, 113,  99, 128, 128, 128, 128, 124, 123, 128, 128, 128, // 7x
 };
 
-static const uint32 GUIOMapping[] = {
-	MT_PCSPK,		Common::GUIO_MIDIPCSPK,
-	MT_CMS,			Common::GUIO_MIDICMS,
-	MT_PCJR,		Common::GUIO_MIDIPCJR,
-	MT_ADLIB,		Common::GUIO_MIDIADLIB,
-	MT_C64,		    Common::GUIO_MIDIC64,
-	MT_AMIGA,	    Common::GUIO_MIDIAMIGA,
-	MT_APPLEIIGS,	Common::GUIO_MIDIAPPLEIIGS,
-	MT_TOWNS,		Common::GUIO_MIDITOWNS,
-	MT_PC98,		Common::GUIO_MIDIPC98,
-	MT_GM,			Common::GUIO_MIDIGM,
-	MT_MT32,		Common::GUIO_MIDIMT32,
-	0,				0
+static const struct {
+	uint32      type;
+	const char *guio;
+} GUIOMapping[] = {
+	{ MT_PCSPK,		GUIO_MIDIPCSPK, },
+	{ MT_CMS,		GUIO_MIDICMS, },
+	{ MT_PCJR,		GUIO_MIDIPCJR, },
+	{ MT_ADLIB,		GUIO_MIDIADLIB, },
+	{ MT_C64,		GUIO_MIDIC64, },
+	{ MT_AMIGA,	    GUIO_MIDIAMIGA, },
+	{ MT_APPLEIIGS,	GUIO_MIDIAPPLEIIGS, },
+	{ MT_TOWNS,		GUIO_MIDITOWNS, },
+	{ MT_PC98,		GUIO_MIDIPC98, },
+	{ MT_GM,		GUIO_MIDIGM, },
+	{ MT_MT32,		GUIO_MIDIMT32, },
+	{ 0,			0 },
 };
 
-uint32 MidiDriver::musicType2GUIO(uint32 musicType) {
-	uint32 res = 0;
+Common::String MidiDriver::musicType2GUIO(uint32 musicType) {
+	Common::String res = "";
 
-	for (int i = 0; GUIOMapping[i] || GUIOMapping[i + 1]; i += 2) {
-		if (musicType == GUIOMapping[i] || musicType == (uint32)-1)
-			res |= GUIOMapping[i + 1];
+	for (int i = 0; GUIOMapping[i].guio; i++) {
+		if (musicType == GUIOMapping[i].type || musicType == (uint32)-1)
+			res += GUIOMapping[i].guio;
 	}
 
 	return res;

audio/mididrv.h
@@ -146,7 +146,7 @@ public:
 		kDeviceId
 	};
 
-	static uint32 musicType2GUIO(uint32 musicType);
+	static Common::String musicType2GUIO(uint32 musicType);
 
 	/** Create music driver matching the given device handle, or NULL if there is no match. */
 	static MidiDriver *createMidi(DeviceHandle handle);

common/util.cpp
@@ -315,7 +315,7 @@ const char *getRenderModeDescription(RenderMode id) {
 }
 
 const struct GameOpt {
-	uint32 option;
+	const char *option;
 	const char *desc;
 } g_gameOptions[] = {
 	{ GUIO_NOSUBTITLES,  "sndNoSubs" },
@@ -341,9 +341,9 @@ const struct GameOpt {
 	{ GUIO_NONE, 0 }
 };
 
-bool checkGameGUIOption(GameGUIOption option, const String &str) {
+bool checkGameGUIOption(const String &option, const String &str) {
 	for (int i = 0; g_gameOptions[i].desc; i++) {
-		if (g_gameOptions[i].option & option) {
+		if (option.contains(g_gameOptions[i].option)) {
 			if (str.contains(g_gameOptions[i].desc))
 				return true;
 			else
@@ -370,21 +370,21 @@ const String getGameGUIOptionsDescriptionLanguage(Language lang) {
 	return String(String("lang_") + getLanguageDescription(lang));
 }
 
-uint32 parseGameGUIOptions(const String &str) {
-	uint32 res = 0;
+String parseGameGUIOptions(const String &str) {
+	Common::String res;
 
 	for (int i = 0; g_gameOptions[i].desc; i++)
 		if (str.contains(g_gameOptions[i].desc))
-			res |= g_gameOptions[i].option;
+			res += g_gameOptions[i].option;
 
 	return res;
 }
 
-const String getGameGUIOptionsDescription(uint32 options) {
+const String getGameGUIOptionsDescription(const String &options) {
 	String res = "";
 
 	for (int i = 0; g_gameOptions[i].desc; i++)
-		if (options & g_gameOptions[i].option)
+		if (options.contains(g_gameOptions[i].option[0]))
 			res += String(g_gameOptions[i].desc) + " ";
 
 	res.trim();
@@ -392,10 +392,10 @@ const String getGameGUIOptionsDescription(uint32 options) {
 	return res;
 }
 
-void updateGameGUIOptions(const uint32 options, const String &langOption) {
+void updateGameGUIOptions(const String &options, const String &langOption) {
 	const String newOptionString = getGameGUIOptionsDescription(options) + " " + langOption;
 
-	if ((options && !ConfMan.hasKey("guioptions")) ||
+	if ((!options.empty() && !ConfMan.hasKey("guioptions")) ||
 	    (ConfMan.hasKey("guioptions") && ConfMan.get("guioptions") != newOptionString)) {
 		ConfMan.set("guioptions", newOptionString);
 		ConfMan.flushToDisk();

common/util.h
@@ -78,6 +78,31 @@ template<typename T> inline void SWAP(T &a, T &b) { T tmp = a; a = b; b = tmp; }
 #  define SCUMMVM_CURRENT_FUNCTION "<unknown>"
 #endif
 
+#define GUIO_NONE			"\000"
+#define GUIO_NOSUBTITLES	"\001"
+#define GUIO_NOMUSIC		"\002"
+#define GUIO_NOSPEECH		"\003"
+#define GUIO_NOSFX			"\003"
+#define GUIO_NOMIDI			"\004"
+#define GUIO_NOLAUNCHLOAD	"\005"
+
+#define GUIO_MIDIPCSPK		"\006"
+#define GUIO_MIDICMS		"\007"
+#define GUIO_MIDIPCJR		"\010"
+#define GUIO_MIDIADLIB		"\011"
+#define GUIO_MIDIC64        "\012"
+#define GUIO_MIDIAMIGA      "\013"
+#define GUIO_MIDIAPPLEIIGS  "\014"
+#define GUIO_MIDITOWNS		"\015"
+#define GUIO_MIDIPC98		"\016"
+#define GUIO_MIDIMT32		"\017"
+#define GUIO_MIDIGM			"\020"
+
+#define GUIO1(a) (a "\0")
+#define GUIO2(a,b) (a b "\0")
+#define GUIO3(a,b,c) (a b c "\0")
+#define GUIO4(a,b,c,d) (a b c d "\0")
+
 namespace Common {
 
 /**
@@ -224,32 +249,10 @@ extern RenderMode parseRenderMode(const String &str);
 extern const char *getRenderModeCode(RenderMode id);
 extern const char *getRenderModeDescription(RenderMode id);
 
-enum GameGUIOption {
-	GUIO_NONE		= 0,
-	GUIO_NOSUBTITLES	= (1 << 0),
-	GUIO_NOMUSIC		= (1 << 1),
-	GUIO_NOSPEECH		= (1 << 2),
-	GUIO_NOSFX			= (1 << 3),
-	GUIO_NOMIDI			= (1 << 4),
-	GUIO_NOLAUNCHLOAD	= (1 << 5),
-
-	GUIO_MIDIPCSPK		= (1 << 6),
-	GUIO_MIDICMS		= (1 << 7),
-	GUIO_MIDIPCJR		= (1 << 8),
-	GUIO_MIDIADLIB		= (1 << 9),
-	GUIO_MIDIC64        = (1 << 10),
-	GUIO_MIDIAMIGA      = (1 << 11),
-	GUIO_MIDIAPPLEIIGS  = (1 << 12),
-	GUIO_MIDITOWNS		= (1 << 13),
-	GUIO_MIDIPC98		= (1 << 14),
-	GUIO_MIDIMT32		= (1 << 15),
-	GUIO_MIDIGM			= (1 << 16)
-};
-
-bool checkGameGUIOption(GameGUIOption option, const String &str);
+bool checkGameGUIOption(const String &option, const String &str);
 bool checkGameGUIOptionLanguage(Language lang, const String &str);
-uint32 parseGameGUIOptions(const String &str);
-const String getGameGUIOptionsDescription(uint32 options);
+String parseGameGUIOptions(const String &str);
+const String getGameGUIOptionsDescription(const String &options);
 const String getGameGUIOptionsDescriptionLanguage(Language lang);
 
 /**
@@ -257,7 +260,7 @@ const String getGameGUIOptionsDescriptionLanguage(Language lang);
  * domain, when they differ to the ones passed as
  * parameter.
  */
-void updateGameGUIOptions(const uint32 options, const String &langOption);
+void updateGameGUIOptions(const String &options, const String &langOption);
 
 }	// End of namespace Common
 

engines/advancedDetector.cpp
@@ -104,7 +104,7 @@ void AdvancedMetaEngine::updateGameDescriptor(GameDescriptor &desc, const ADGame
 	if (_flags & kADFlagUseExtraAsHint)
 		desc["extra"] = realDesc->extra;
 
-	desc.setGUIOptions(realDesc->guioptions | _guioptions);
+	desc.setGUIOptions(realDesc->guioptions + _guioptions);
 	desc.appendGUIOptions(getGameGUIOptionsDescriptionLanguage(realDesc->language));
 
 	if (realDesc->flags & ADGF_ADDENGLISH)
@@ -257,7 +257,7 @@ Common::Error AdvancedMetaEngine::createInstance(OSystem *syst, Engine **engine)
 	if (agdDesc->flags & ADGF_ADDENGLISH)
 		lang += " " + getGameGUIOptionsDescriptionLanguage(Common::EN_ANY);
 
-	Common::updateGameGUIOptions(agdDesc->guioptions | _guioptions, lang);
+	Common::updateGameGUIOptions(agdDesc->guioptions + _guioptions, lang);
 
 	GameDescriptor gameDescriptor = toGameDescriptor(*agdDesc, _gameids);
 
@@ -568,7 +568,7 @@ AdvancedMetaEngine::AdvancedMetaEngine(const void *descs, uint descItemSize, con
 	_md5Bytes = 5000;
 	_singleid = NULL;
 	_flags = 0;
-	_guioptions = Common::GUIO_NONE;
+	_guioptions = GUIO_NONE;
 	_maxScanDepth = 1;
 	_directoryGlobs = NULL;
 }

engines/advancedDetector.h
@@ -88,7 +88,7 @@ struct ADGameDescription {
 	 */
 	uint32 flags;
 
-	uint32 guioptions;
+	const char *guioptions;
 };
 
 /**
@@ -181,11 +181,11 @@ protected:
 	uint32 _flags;
 
 	/**
-	 * A bitmask of game GUI options which will be added to each
+	 * A list of game GUI options which will be added to each
 	 * entry in addition to per-game options. Refer to GameGUIOption
 	 * enum for the list.
 	 */
-	uint32 _guioptions;
+	Common::String _guioptions;
 
 	/**
 	 * Maximum depth of directories to look up.

engines/game.cpp
@@ -38,29 +38,29 @@ GameDescriptor::GameDescriptor() {
 	setVal("description", "");
 }
 
-GameDescriptor::GameDescriptor(const PlainGameDescriptor &pgd, uint32 guioptions) {
+GameDescriptor::GameDescriptor(const PlainGameDescriptor &pgd, Common::String guioptions) {
 	setVal("gameid", pgd.gameid);
 	setVal("description", pgd.description);
 
-	if (guioptions != 0)
+	if (!guioptions.empty())
 		setVal("guioptions", Common::getGameGUIOptionsDescription(guioptions));
 }
 
-GameDescriptor::GameDescriptor(const Common::String &g, const Common::String &d, Common::Language l, Common::Platform p, uint32 guioptions, GameSupportLevel gsl) {
+GameDescriptor::GameDescriptor(const Common::String &g, const Common::String &d, Common::Language l, Common::Platform p, Common::String guioptions, GameSupportLevel gsl) {
 	setVal("gameid", g);
 	setVal("description", d);
 	if (l != Common::UNK_LANG)
 		setVal("language", Common::getLanguageCode(l));
 	if (p != Common::kPlatformUnknown)
 		setVal("platform", Common::getPlatformCode(p));
-	if (guioptions != 0)
+	if (!guioptions.empty())
 		setVal("guioptions", Common::getGameGUIOptionsDescription(guioptions));
 
 	setSupportLevel(gsl);
 }
 
-void GameDescriptor::setGUIOptions(uint32 guioptions) {
-	if (guioptions != 0)
+void GameDescriptor::setGUIOptions(Common::String guioptions) {
+	if (!guioptions.empty())
 		setVal("guioptions", Common::getGameGUIOptionsDescription(guioptions));
 	else
 		erase("guioptions");

commit c087f917dda52ea72e200990e41b843767f0091d

GUI: Fix missing audio drivers in Edit Game dialog.
This is a regression from 77c65648b1db8f8b7b245510681eafd856798d26.

Formerly the code used strtok to check for any audio related GUIO flag to be
present in _guioptions. Since strtok tokenizes the string this won't really
work. I changed it to use strpbrk, which searches a string for any character
from a set of characters (in our case flags). The code should now have the
same semantics as of before the above mentioned commit.

This also gets rid of copying the gui options into a char array and a strncpy
call.

gui/options.cpp
@@ -616,11 +616,8 @@ void OptionsDialog::setAudioSettingsState(bool enabled) {
 	_midiPopUpDesc->setEnabled(enabled);
 	_midiPopUp->setEnabled(enabled);
 
-	Common::String allFlags = MidiDriver::musicType2GUIO((uint32)-1);
-	char opt[256];
-
-	strncpy(opt, _guioptions.c_str(), 256);
-	bool hasMidiDefined = (strtok(opt, allFlags.c_str()) != NULL);
+	const Common::String allFlags = MidiDriver::musicType2GUIO((uint32)-1);
+	bool hasMidiDefined = (strpbrk(_guioptions.c_str(), allFlags.c_str()) != NULL);
 
 	if (_domain != Common::ConfigManager::kApplicationDomain && // global dialog
 		hasMidiDefined && // No flags are specified
@@ -766,11 +763,8 @@ void OptionsDialog::addAudioControls(GuiObject *boss, const Common::String &pref
 	_midiPopUp = new PopUpWidget(boss, prefix + "auMidiPopup", _("Specifies output sound device or sound card emulator"));
 
 	// Populate it
-	Common::String allFlags = MidiDriver::musicType2GUIO((uint32)-1);
-	char opt[256];
-
-	strncpy(opt, _guioptions.c_str(), 256);
-	bool hasMidiDefined = (strtok(opt, allFlags.c_str()) != NULL);
+	const Common::String allFlags = MidiDriver::musicType2GUIO((uint32)-1);
+	bool hasMidiDefined = (strpbrk(_guioptions.c_str(), allFlags.c_str()) != NULL);
 
 	const MusicPlugin::List p = MusicMan.getPlugins();
 	for (MusicPlugin::List::const_iterator m = p.begin(); m != p.end(); ++m) {