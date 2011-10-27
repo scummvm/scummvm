627684ca73833117425f3fd9d2257efb2157cb3c
Matthew Hoops <clone2727@gmail.com>
2011-10-27 19:20:02 -0400

    GRAPHICS: Make drawThickLine take a thickness in both x and y directions

--- graphics/primitives.cpp
+++ graphics/primitives.cpp
@@ -61,59 +61,21 @@ void drawLine(int x0, int y0, int x1, int y1, int color, void (*plotProc)(int, i
 	}
 }
 
+void drawThickLine(int x0, int y0, int x1, int y1, int penX, int penY, int color, void (*plotProc)(int, int, int, void *), void *data) {
+	assert(penX > 0 && penY > 0);
 
-// FIXME: This is a limited version of thick line drawing
-// it draws striped lines at some angles. Better algorithm could
-// be found here:
-//
-//   http://homepages.enterprise.net/murphy/thickline/index.html
-//
-// Feel free to replace it with better implementation
-void drawThickLine(int x0, int y0, int x1, int y1, int thickness, int color, void (*plotProc)(int, int, int, void *), void *data) {
-	const bool steep = ABS(y1 - y0) > ABS(x1 - x0);
-
-	if (steep) {
-		SWAP(x0, y0);
-		SWAP(x1, y1);
-	}
-
-	float dx = x1 - x0;
-	float dy = y1 - y0;
-	float d = (float)sqrt(dx * dx + dy * dy);
-
-	if (!d)
+	// Shortcut
+	if (penX == 1 && penY == 1) {
+		drawLine(x0, y0, x1, y1, color, plotProc, data);
 		return;
-
-	int thickX = (int)((float)thickness * dy / d / 2);
-	int thickY = (int)((float)thickness * dx / d / 2);
-
-	const int delta_x = ABS(x1 - x0);
-	const int delta_y = ABS(y1 - y0);
-	const int delta_err = delta_y;
-	int x = x0;
-	int y = y0;
-	int err = 0;
-
-	const int x_step = (x0 < x1) ? 1 : -1;
-	const int y_step = (y0 < y1) ? 1 : -1;
-
-	if (steep)
-		drawLine(y - thickY, x + thickX, y + thickY, x - thickX, color, plotProc, data);
-	else
-		drawLine(x - thickX, y + thickY, x + thickX, y - thickY, color, plotProc, data);
-
-	while (x != x1) {
-		x += x_step;
-		err += delta_err;
-		if (2 * err > delta_x) {
-			y += y_step;
-			err -= delta_x;
-		}
-		if (steep)
-			drawLine(y - thickY, x + thickX, y + thickY, x - thickX, color, plotProc, data);
-		else
-			drawLine(x - thickX, y + thickY, x + thickX, y - thickY, color, plotProc, data);
 	}
+
+	// TODO: Optimize this. It currently is a very naive way of handling
+	// thick lines since quite often it will be drawing to the same pixel
+	// multiple times.
+	for (int x = 0; x < penX; x++)
+		for (int y = 0; y < penY; y++)
+			drawLine(x0 + x, y0 + y, x1 + x, y1 + y, color, plotProc, data);
 }
 
 }	// End of namespace Graphics
