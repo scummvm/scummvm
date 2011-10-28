scummvm/scummvm

commit 3ddb6c0c903f478b91a8965d3433a9f79ce16ba7

AUDIO: Adapt include guard names to the "new" directory name.

audio/mods/tfmx.h
@@ -25,7 +25,7 @@
 
 // normal Header Guard
 #elif !defined(SOUND_MODS_TFMX_H)
-#define SOUND_MODS_TFMX_H
+#define AUDIO_MODS_TFMX_H
 
 #include "audio/mods/paula.h"
 

audio/audiostream.h
@@ -20,8 +20,8 @@
  *
  */
 
-#ifndef SOUND_AUDIOSTREAM_H
-#define SOUND_AUDIOSTREAM_H
+#ifndef AUDIO_AUDIOSTREAM_H
+#define AUDIO_AUDIOSTREAM_H
 
 #include "common/ptr.h"
 #include "common/scummsys.h"

audio/decoders/aac.h
@@ -26,8 +26,8 @@
  *  - groovie
  */
 
-#ifndef SOUND_AAC_H
-#define SOUND_AAC_H
+#ifndef AUDIO_AAC_H
+#define AUDIO_AAC_H
 
 #include "common/scummsys.h"
 #include "common/types.h"
@@ -59,4 +59,4 @@ Codec *makeAACDecoder(
 } // End of namespace Audio
 
 #endif // #ifdef USE_FAAD
-#endif // #ifndef SOUND_AAC_H
+#endif // #ifndef AUDIO_AAC_H

audio/decoders/adpcm.h
@@ -31,8 +31,8 @@
  *  - tinsel
  */
 
-#ifndef SOUND_ADPCM_H
-#define SOUND_ADPCM_H
+#ifndef AUDIO_ADPCM_H
+#define AUDIO_ADPCM_H
 
 #include "common/scummsys.h"
 #include "common/types.h"

audio/decoders/adpcm_intern.h
@@ -28,8 +28,8 @@
  * ADPCM decoder implementations.
  */
 
-#ifndef SOUND_ADPCM_INTERN_H
-#define SOUND_ADPCM_INTERN_H
+#ifndef AUDIO_ADPCM_INTERN_H
+#define AUDIO_ADPCM_INTERN_H
 
 #include "audio/audiostream.h"
 #include "common/endian.h"

audio/decoders/aiff.h
@@ -28,8 +28,8 @@
  *  - sword1
  */
 
-#ifndef SOUND_AIFF_H
-#define SOUND_AIFF_H
+#ifndef AUDIO_AIFF_H
+#define AUDIO_AIFF_H
 
 #include "common/scummsys.h"
 #include "common/types.h"

audio/decoders/flac.h
@@ -37,8 +37,8 @@
  *  - tucker
  */
 
-#ifndef SOUND_FLAC_H
-#define SOUND_FLAC_H
+#ifndef AUDIO_FLAC_H
+#define AUDIO_FLAC_H
 
 #include "common/scummsys.h"
 #include "common/types.h"
@@ -68,4 +68,4 @@ SeekableAudioStream *makeFLACStream(
 } // End of namespace Audio
 
 #endif // #ifdef USE_FLAC
-#endif // #ifndef SOUND_FLAC_H
+#endif // #ifndef AUDIO_FLAC_H

audio/decoders/iff_sound.h
@@ -26,8 +26,8 @@
  *  - parallaction
  */
 
-#ifndef SOUND_IFF_H
-#define SOUND_IFF_H
+#ifndef AUDIO_IFF_H
+#define AUDIO_IFF_H
 
 namespace Common {
 class ReadStream;

commit 38aa82a4f502fd913a402c9183364ebfa4f85cce

AUDIO: Fix conditional compilation of maxtrax and tfmx code.
This is a regression from 3ddb6c0c903f478b91a8965d3433a9f79ce16ba7.

audio/mods/maxtrax.cpp
@@ -29,7 +29,7 @@
 #include "audio/mods/maxtrax.h"
 
 // test for engines using this class.
-#if defined(SOUND_MODS_MAXTRAX_H)
+#if defined(AUDIO_MODS_MAXTRAX_H)
 
 namespace {
 
@@ -1034,4 +1034,4 @@ void MaxTrax::outPutScore(const Score &sc, int num) {}
 
 }	// End of namespace Audio
 
-#endif // #if defined(SOUND_MODS_MAXTRAX_H)
+#endif // #if defined(AUDIO_MODS_MAXTRAX_H)

audio/mods/tfmx.cpp
@@ -29,7 +29,7 @@
 #include "audio/mods/tfmx.h"
 
 // test for engines using this class.
-#if defined(SOUND_MODS_TFMX_H)
+#if defined(AUDIO_MODS_TFMX_H)
 
 // couple debug-functions
 namespace {
@@ -1187,4 +1187,4 @@ void displayPatternstep(const void * const vptr) {
 } // End of anonymous namespace
 #endif
 
-#endif // #if defined(SOUND_MODS_TFMX_H)
+#endif // #if defined(AUDIO_MODS_TFMX_H)

audio/mods/tfmx.h
@@ -24,7 +24,7 @@
 #if !defined(ENABLE_SCUMM)
 
 // normal Header Guard
-#elif !defined(SOUND_MODS_TFMX_H)
+#elif !defined(AUDIO_MODS_TFMX_H)
 #define AUDIO_MODS_TFMX_H
 
 #include "audio/mods/paula.h"
@@ -278,4 +278,4 @@ private:
 
 }	// End of namespace Audio
 
-#endif // !defined(SOUND_MODS_TFMX_H)
+#endif // !defined(AUDIO_MODS_TFMX_H)