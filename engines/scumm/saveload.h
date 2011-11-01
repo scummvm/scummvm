8315d5d2c8939a147419f30d6bcc09afd15af9d3
athrxx <athrxx@scummvm.org>
2011-11-01 17:03:33 +0100

    SCUMM: fix save file incompatibility between DS and other ports for non FM-TOWNS games

--- engines/scumm/saveload.h
+++ engines/scumm/saveload.h
@@ -47,7 +47,7 @@ namespace Scumm {
  * only saves/loads those which are valid for the version of the savegame
  * which is being loaded/saved currently.
  */
-#define CURRENT_VER 86
+#define CURRENT_VER 87
 
 /**
  * An auxillary macro, used to specify savegame versions. We use this instead
