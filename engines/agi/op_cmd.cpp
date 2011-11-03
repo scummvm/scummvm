d3858c359fde79e443d6e61579b400a36dff13e3
Filippos Karapetis <md5@scummvm.org>
2011-11-03 22:55:22 +0200

    AGI: Commented out if statements that always evaluate to true

--- engines/agi/op_cmd.cpp
+++ engines/agi/op_cmd.cpp
@@ -1622,15 +1622,15 @@ void cmdPrintAtV(AgiGame *state, uint8 *p) {
 
 void cmdPushScript(AgiGame *state, uint8 *p) {
 	// We run AGIMOUSE always as a side effect
-	if (getFeatures() & GF_AGIMOUSE || true) {
+	//if (getFeatures() & GF_AGIMOUSE || true) {
 		state->vars[27] = state->_vm->_mouse.button;
 		state->vars[28] = state->_vm->_mouse.x / 2;
 		state->vars[29] = state->_vm->_mouse.y;
-	} else {
+	/*} else {
 		if (getVersion() >= 0x2915) {
 			debug(0, "push.script");
 		}
-	}
+	}*/
 }
 
 void cmdSetPriBase(AgiGame *state, uint8 *p) {
