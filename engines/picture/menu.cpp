0ce8beeff9991241756d7b40c597ba0e3bfe9de3
Filippos Karapetis <md5@scummvm.org>
2011-11-03 21:30:54 +0000

    PICTURE: Removed duplicate assignment

--- engines/picture/menu.cpp
+++ engines/picture/menu.cpp
@@ -55,7 +55,6 @@ int MenuSystem::run() {
 	_newMenuID = kMenuIdMain;
 	_currItemID = kItemIdNone;
 	_editingDescription = false;
-	_needRedraw = false;
 	_cfgText = true;
 	_cfgVoices = true;
 	_cfgMasterVolume = 10;
