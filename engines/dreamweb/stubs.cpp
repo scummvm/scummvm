e254e549293b8b13e8d1765b553a448e73169de9
Willem Jan Palenstijn <wjp@usecode.org>
2011-11-14 20:18:18 +0100

    DREAMWEB: Convert 'mainscreen'

--- engines/dreamweb/stubs.cpp
+++ engines/dreamweb/stubs.cpp
@@ -1919,19 +1919,17 @@ void DreamGenContext::showcity() {
 }
 
 void DreamGenContext::mainscreen() {
-	STACK_CHECK;
 	data.byte(kInmaparea) = 0;
-	bx = offset_mainlist;
-	_cmp(data.byte(kWatchon), 1);
-	if (flags.z())
-		goto checkmain;
-	bx = offset_mainlist2;
-checkmain:
-	checkcoords();
-	_cmp(data.byte(kWalkandexam), 0);
-	if (flags.z())
-		return /* (finishmain) */;
-	walkandexamine();
+	if (data.byte(kWatchon) == 1) {
+		bx = offset_mainlist;
+		checkcoords();
+	} else {
+		bx = offset_mainlist2;
+		checkcoords();
+	}
+
+	if (data.byte(kWalkandexam) != 0)
+		walkandexamine();
 }
 
 
