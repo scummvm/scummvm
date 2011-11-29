fda6e1534bc1dfad7f1f32ccf0bb51f3eb34974d
Tarek Soliman <tsoliman@scummvm.org>
2011-11-29 14:01:20 -0600

    MAEMO: Fix zoom key capture on N900 (fixes bug#3443037)
    This was semi-intentionally skipped in the intial port revival
    because the focus was on N810 at the time.
    
    This is mostly copied from the old 1.2.1 port and then cleaned up.

--- backends/graphics/maemosdl/maemosdl-graphics.h
+++ backends/graphics/maemosdl/maemosdl-graphics.h
@@ -30,6 +30,9 @@
 class MaemoSdlGraphicsManager : public SurfaceSdlGraphicsManager {
 public:
 	MaemoSdlGraphicsManager(SdlEventSource *sdlEventSource);
+
+protected:
+	virtual bool loadGFXMode();
 };
 
 #endif
