e4adfbbb8b59afce4b9b7e4707dad4c77a91be06
Strangerke <strangerke@scummvm.org>
2011-12-26 21:30:46 +0100

    MORTEVIELLE: Add namespace and standard header

--- engines/mortevielle/mor.cpp
+++ engines/mortevielle/mor.cpp
@@ -1,3 +1,32 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+/*
+ * This code is based on original Mortville Manor DOS source code
+ * Copyright (c) 1988-1989 Lankhor
+ */
+
+namespace MORTEVIELLE {
+
 /* Niveau 14 suite */
 const char m1[] = "quelqu'un entre, parait ‚tonn‚ mais ne dit rien";
 
@@ -1307,5 +1336,4 @@ void dessin(int ad) {
 	}
 }
 
-
-
+} // End of namespace MORTEVIELLE
