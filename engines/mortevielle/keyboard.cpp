1c441062a05c9ff256100179a746a84baa153c88
Strangerke <strangerke@scummvm.org>
2011-12-26 21:30:46 +0100

    MORTEVIELLE: Add namespace and standard header

--- engines/mortevielle/keyboard.cpp
+++ engines/mortevielle/keyboard.cpp
@@ -1,3 +1,32 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+/*
+ * This code is based on original Mortville Manor DOS source code
+ * Copyright (c) 1988-1989 Lankhor
+ */
+
+namespace MORTEVIELLE {
+
 char readkey1() {
 	char c;
 
@@ -58,3 +87,4 @@ void teskbd() {
 	if (keypressed())  dum = testou();
 }
 
+} // End of namespace MORTEVIELLE
