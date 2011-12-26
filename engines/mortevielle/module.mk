3e77d346281fddfd17640ab0198e067838e54113
Eugene Sandulenko <sev@scummvm.org>
2011-12-26 18:09:38 +0000

    MORTEVIELLE: Register engine at engines.mk

--- engines/mortevielle/module.mk
+++ engines/mortevielle/module.mk
@@ -1,5 +1,5 @@
 MODULE := engines/mortevielle
- 
+
 MODULE_OBJS := \
 	actions.o \
 	alert.o \
@@ -22,11 +22,11 @@ MODULE_OBJS := \
 	sprint.o \
 	taffich.o \
 	var_mor.o
- 
+
 # This module can be built as a plugin
 ifeq ($(ENABLE_MORTEVIELLE), DYNAMIC_PLUGIN)
 PLUGIN := 1
 endif
- 
+
 # Include common rules 
 include $(srcdir)/rules.mk
