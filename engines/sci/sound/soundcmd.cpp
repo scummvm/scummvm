fa5e2f750e384b1d2b7b4a2f7f5d1e00c5f13974
Filippos Karapetis <md5@scummvm.org>
2011-12-28 13:21:19 +0200

    SCI: Always prefer digital sound effects to prevent confusion to users
    This toggle will be enabled again when a more user-friendly GUI option
    is possible

--- engines/sci/sound/soundcmd.cpp
+++ engines/sci/sound/soundcmd.cpp
@@ -32,9 +32,12 @@
 
 namespace Sci {
 
+//#define ENABLE_SFX_TYPE_SELECTION
+
 SoundCommandParser::SoundCommandParser(ResourceManager *resMan, SegManager *segMan, Kernel *kernel, AudioPlayer *audio, SciVersion soundVersion) :
 	_resMan(resMan), _segMan(segMan), _kernel(kernel), _audio(audio), _soundVersion(soundVersion) {
 
+#ifdef ENABLE_SFX_TYPE_SELECTION
 	// Check if the user wants synthesized or digital sound effects in SCI1.1
 	// or later games
 	_useDigitalSFX = ConfMan.getBool("multi_midi");
@@ -45,6 +48,10 @@ SoundCommandParser::SoundCommandParser(ResourceManager *resMan, SegManager *segM
 	// resource number, but it's totally unrelated to the menu music).
 	if (getSciVersion() >= SCI_VERSION_2)
 		_useDigitalSFX = true;
+#else
+	// Always prefer digital sound effects
+	_useDigitalSFX = true;
+#endif
 
 	_music = new SciMusic(_soundVersion, _useDigitalSFX);
 	_music->init();
