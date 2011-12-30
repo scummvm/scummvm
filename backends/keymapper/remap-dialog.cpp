scummvm/scummvm

commit 1b11139dc53da2dc95f74eb19e3a903d7d786f84

KEYMAPPER: Switch to using addClearButton()

backends/keymapper/remap-dialog.cpp
@@ -172,16 +172,7 @@ void RemapDialog::reflowLayout() {
 				new GUI::StaticTextWidget(this, 0, 0, 0, 0, "", Graphics::kTextAlignRight);
 			widg.keyButton =
 				new GUI::ButtonWidget(this, 0, 0, 0, 0, "", 0, kRemapCmd + i);
-#ifndef DISABLE_FANCY_THEMES
-			if (g_gui.xmlEval()->getVar("Globals.ShowSearchPic") == 1 && g_gui.theme()->supportsImages()) {
-				widg.clearButton = new GUI::PicButtonWidget(this, 0, 0, clearButtonWidth, clearButtonHeight, _("Clear value"), kClearCmd + i);
-				((GUI::PicButtonWidget *)widg.clearButton)->useThemeTransparency(true);
-				((GUI::PicButtonWidget *)widg.clearButton)->setGfx(g_gui.theme()->getImageSurface(GUI::ThemeEngine::kImageEraser));
-			}
-			else
-#endif
-				widg.clearButton = new GUI::ButtonWidget(this, 0, 0, 0, 0, "C", _("Clear value"), kClearCmd + i);
-
+			widg.clearButton = addClearButton(this, "", kClearCmd, 0, 0, clearButtonWidth, clearButtonHeight);
 			_keymapWidgets.push_back(widg);
 		} else {
 			widg = _keymapWidgets[i];

commit 0e0eddbab333589759480941b5e01bbe77413ac8

KEYMAPPER: Fix clear button behavior in remap dialog
This is a regression from 1b11139dc53da2dc95f74eb19e3a903d7d786f84

backends/keymapper/remap-dialog.cpp
@@ -172,7 +172,7 @@ void RemapDialog::reflowLayout() {
 				new GUI::StaticTextWidget(this, 0, 0, 0, 0, "", Graphics::kTextAlignRight);
 			widg.keyButton =
 				new GUI::ButtonWidget(this, 0, 0, 0, 0, "", 0, kRemapCmd + i);
-			widg.clearButton = addClearButton(this, "", kClearCmd, 0, 0, clearButtonWidth, clearButtonHeight);
+			widg.clearButton = addClearButton(this, "", kClearCmd + i, 0, 0, clearButtonWidth, clearButtonHeight);
 			_keymapWidgets.push_back(widg);
 		} else {
 			widg = _keymapWidgets[i];