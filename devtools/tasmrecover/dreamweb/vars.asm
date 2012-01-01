e2ded73d923a2be759b272ebca7c989603ef0f31
Strangerke <strangerke@scummvm.org>
2012-01-01 11:49:41 +0100

    MISC: This is year 2012

--- devtools/tasmrecover/dreamweb/vars.asm
+++ devtools/tasmrecover/dreamweb/vars.asm
@@ -1,4 +1,4 @@
-;Copyright (c) 1990-2011 by Neil Dodwell
+;Copyright (c) 1990-2012 by Neil Dodwell
 ;Released with permission from Neil Dodwell under GPLv2
 ;See LICENSE file for full license text
 ;---------------------------------------------------Equates and definitions----
