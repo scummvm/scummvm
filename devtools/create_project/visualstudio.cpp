fb429defb2837799140d73cd54c3411f0860285f
Bastien Bouclet <bastien.bouclet@gmail.com>
2012-01-10 20:04:05 +0100

    CREATEPROJECT: Enable language extensions for Myst 3. Fixes #465.

--- devtools/create_project/visualstudio.cpp
+++ devtools/create_project/visualstudio.cpp
@@ -113,6 +113,7 @@ void VisualStudioProvider::createProjectFile(const std::string &name, const std:
 		toolConfig += (name == "tinsel" ? "DebugInformationFormat=\"3\" " : "");
 		toolConfig += (name == "sword25" ? "DisableLanguageExtensions=\"false\" " : "");
 		toolConfig += (name == "grim" ? "DisableLanguageExtensions=\"false\" " : "");
+		toolConfig += (name == "myst3" ? "DisableLanguageExtensions=\"false\" " : "");
 
 		// Win32
 		outputConfiguration(setup, project, toolConfig, "Debug", "Win32", "");
