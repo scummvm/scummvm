scummvm/scummvm

commit 0af418e7ea3a41f93fcc551a45ee5bae822d812a

Fix regression caused by OSystem::updateScreen call limitation, which caused certain animations not to display at all.
svn-id: r44063

engines/kyra/kyra_lok.cpp
@@ -489,6 +489,8 @@ void KyraEngine_LoK::delay(uint32 amount, bool update, bool isMainLoop) {
 			_animator->updateAllObjectShapes();
 			updateTextFade();
 			updateMousePointer();
+		} else {
+			_screen->updateScreen();
 		}
 
 		_isSaveAllowed = isMainLoop;

commit 6a3d0e712940bf4560e5302c40f07f5f6eb8e0aa

KYRA: Make workaround for bug #1498221 work again.
The bug in question is "KYRA1: Glitches when meeting Zanthia".

This is a regression from 0af418e7ea3a41f93fcc551a45ee5bae822d812a as far as
I can tell.

engines/kyra/kyra_lok.cpp
@@ -510,7 +510,14 @@ void KyraEngine_LoK::delay(uint32 amount, bool update, bool isMainLoop) {
 			updateTextFade();
 			updateMousePointer();
 		} else {
-			_screen->updateScreen();
+			// We call OSystem::updateScreen here and not Screen::updateScreen
+			// to avoid new graphics changes to be copied to the screen.
+			// This assures the workaround of bug #1498221
+			// "KYRA1: Glitches when meeting Zanthia" is working correctly.
+			// Since we only call updateScreen here to let systems with frame
+			// update count limitations not miss any graphics updates it
+			// should not cause any problems.
+			_system->updateScreen();
 		}
 
 		_isSaveAllowed = isMainLoop;