b93cc84604d60b1503f2c26248e82b738aa13cb6
Paul Gilbert <dreammaster@scummvm.org>
2012-01-17 09:12:27 +1100

    MORTEVIELLE: Added an optimised drawBox routine

--- engines/mortevielle/boite.cpp
+++ engines/mortevielle/boite.cpp
@@ -25,41 +25,17 @@
  * Copyright (c) 1988-1989 Lankhor
  */
 
+#include "common/textconsole.h"
 #include "mortevielle/boite.h"
-#include "mortevielle/mouse.h"
-#include "mortevielle/var_mor.h"
+#include "mortevielle/mortevielle.h"
 
 namespace Mortevielle {
 
 //Translation: box()
 void boite(int x, int y, int dx, int dy, int coul) {
-	int i;
-	int xi, yi, xo, yo;
+	warning("TODO: boite is deprecated in favour of ScreenSurface::drawBox");
 
-	/* debug('boite'); */
-	hide_mouse();
-	if (res == 1) {
-		x = (uint)x >> 1;
-		dx = (uint)dx >> 1;
-	}
-	xi = x;
-	yi = y;
-	xo = x;
-	yo = y;
-	for (i = 0; i <= dx + dy; i ++) {
-		putpix(gd, xi, yi, coul);
-		if (xi == x + dx)  {
-			if (gd != cga)  putpix(gd, pred(int, xi), yi, coul);
-			yi = succ(int, yi);
-		} else xi = succ(int, xi);
-		putpix(gd, xo, yo, coul);
-		if (yo == y + dy)  xo = succ(int, xo);
-		else {
-			if (gd != cga)  putpix(gd, succ(int, xo), yo, coul);
-			yo = succ(int, yo);
-		}
-	}
-	show_mouse();
+	g_vm->_screenSurface.drawBox(x, y, dx, dy, coul);
 }
 
 } // End of namespace Mortevielle
