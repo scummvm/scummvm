d62e1f28cb08514e7d08c2ea1318b6d5a03296a8
Paul Gilbert <dreammaster@scummvm.org>
2012-01-21 19:52:14 +1100

    TSAGE: Fix parenthesis in R2R scene 160

--- engines/tsage/ringworld2/ringworld2_scenes0.cpp
+++ engines/tsage/ringworld2/ringworld2_scenes0.cpp
@@ -1313,7 +1313,7 @@ void Scene160::Action1::signal() {
 		breakFlag = true;
 		do {
 			if (!scene->_lineNum || ((scene->_lineNum != -1) &&
-					((*scene->_creditsList.reverse_begin())->_position.y < 164) || !breakFlag)) {
+					(((*scene->_creditsList.reverse_begin())->_position.y < 164) || !breakFlag))) {
 				breakFlag = true;
 				Common::String msg = g_resourceManager->getMessage(160, scene->_lineNum++);
 
