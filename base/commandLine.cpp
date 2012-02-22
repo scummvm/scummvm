7308af64980d4bd82374968d8e478d1bdb439c78
Pawel Kolodziejski <aquadran@users.ourceforge.net>
2012-02-22 17:25:49 +0100

    COMMON: fixed compilation

--- base/commandLine.cpp
+++ base/commandLine.cpp
@@ -234,7 +234,7 @@ void registerDefaults() {
 // resp. between "--some-option" and "--no-some-option".
 #define DO_OPTION_BOOL(shortCmd, longCmd) \
 	if (isLongCmd ? (!strcmp(s+2, longCmd) || !strcmp(s+2, "no-"longCmd)) : (tolower(s[1]) == shortCmd)) { \
-		bool boolValue = (islower(static_cast<unsigned char>(s[1])) != 0); \
+		bool boolValue = (Common::isLower(s[1]) != 0); \
 		s += 2; \
 		if (isLongCmd) { \
 			boolValue = !strcmp(s, longCmd); \
