6a73da8b149fb9f8b7b42814f390e1fa72a2e212
Paweł Kołodziejski <aquadran@users.sourceforge.net>
2012-02-24 18:07:54 +0100

    SDL: trying fix compilation for amigaos4

--- backends/events/sdl/sdl-events.cpp
+++ backends/events/sdl/sdl-events.cpp
@@ -30,6 +30,16 @@
 #include "common/config-manager.h"
 #include "common/textconsole.h"
 
+//ResidualVM specific:
+#if defined(__amigaos4__)
+// KEYCODE_LESS and KEYCODE_GREATER are already defined in AmigaOS, inside
+// include/include_h/intuition/intuition.h (bug #3121350)
+#if defined(KEYCODE_LESS) && defined(KEYCODE_GREATER)
+#undef KEYCODE_LESS
+#undef KEYCODE_GREATER
+#endif
+#endif
+
 // FIXME move joystick defines out and replace with confile file options
 // we should really allow users to map any key to a joystick button
 #define JOY_DEADZONE 3200
