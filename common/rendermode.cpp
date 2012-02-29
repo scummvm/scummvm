92d219715f1fc8b3535cfce370084bd56596c2ef
Max Horn <max@quendi.de>
2012-02-29 11:43:21 +0100

    COMMON: Replace OptionsDialog::renderType2GUIO by API in rendermode.h

--- common/rendermode.cpp
+++ common/rendermode.cpp
@@ -22,6 +22,7 @@
 
 #include "common/rendermode.h"
 
+#include "common/gui_options.h"
 #include "common/str.h"
 #include "common/translation.h"
 
@@ -41,6 +42,26 @@ const RenderModeDescription g_renderModes[] = {
 	{ "pc9821", "PC-9821 (256 Colors)", kRenderPC9821 },
 	{ "pc9801", "PC-9801 (16 Colors)", kRenderPC9801 },
 	{0, 0, kRenderDefault}
+};
+
+struct RenderGUIOMapping {
+	RenderMode id;
+	const char *guio;
+};
+
+// TODO: Merge s_renderGUIOMapping into g_renderModes? the kRenderDefault
+// could be used to indicate "any" mode when passed to renderMode2GUIO (if
+// we wanted to merge allRenderModesGUIOs back into)
+static const RenderGUIOMapping s_renderGUIOMapping[] = {
+	{ kRenderHercG,		GUIO_RENDERHERCGREEN },
+	{ kRenderHercA,		GUIO_RENDERHERCAMBER },
+	{ kRenderCGA,		GUIO_RENDERCGA },
+	{ kRenderEGA,		GUIO_RENDEREGA },
+	{ kRenderVGA,		GUIO_RENDERVGA },
+	{ kRenderAmiga,		GUIO_RENDERAMIGA },
+	{ kRenderFMTowns,	GUIO_RENDERFMTOWNS },
+	{ kRenderPC9821,	GUIO_RENDERPC9821 },
+	{ kRenderPC9801,	GUIO_RENDERPC9801 }
 };
 
 DECLARE_TRANSLATION_ADDITIONAL_CONTEXT("Hercules Green", "lowres")
@@ -77,5 +98,25 @@ const char *getRenderModeDescription(RenderMode id) {
 	return 0;
 }
 
+String renderMode2GUIO(RenderMode id) {
+	Common::String res;
+
+	for (int i = 0; i < ARRAYSIZE(s_renderGUIOMapping); i++) {
+		if (id == s_renderGUIOMapping[i].id)
+			res += s_renderGUIOMapping[i].guio;
+	}
+
+	return res;
+}
+
+String allRenderModesGUIOs() {
+	Common::String res;
+
+	for (int i = 0; i < ARRAYSIZE(s_renderGUIOMapping); i++) {
+		res += s_renderGUIOMapping[i].guio;
+	}
+
+	return res;
+}
 
 } // End of namespace Common
