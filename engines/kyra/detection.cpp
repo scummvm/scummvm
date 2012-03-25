3b57dc916460537406f2ca60e1a3edb0d156bdea
Eugene Sandulenko <sev@scummvm.org>
2012-03-25 11:47:20 +0300

    KYRA: Move I18N-related commits where they belong.

--- engines/kyra/detection.cpp
+++ engines/kyra/detection.cpp
@@ -54,11 +54,11 @@ const char *const directoryGlobs[] = {
 const ADExtraGuiOptionsMap gameGuiOptions[] = {
 	// Kyrandia 3 options
 
-	// I18N: Studio audience adds an applause and cheering sounds whenever
 	// Malcolm makes a joke.
 	{
 		GAMEOPTION_KYRA3_AUDIENCE,
 		{
+			// I18N: Studio audience adds an applause and cheering sounds whenever
 			_s("Studio audience"),
 			_s("Enable studio audience"),
 			"studio_audience",
@@ -66,10 +66,10 @@ const ADExtraGuiOptionsMap gameGuiOptions[] = {
 		}
 	},
 
-	// I18N: When enabled, this option allows the user to skip text and cutscenes.
 	{
 		GAMEOPTION_KYRA3_SKIP,
 		{
+			// I18N: This option allows the user to skip text and cutscenes.
 			_s("Skip support"),
 			_s("Allow text and cutscenes to be skipped"),
 			"skip_support",
@@ -77,10 +77,10 @@ const ADExtraGuiOptionsMap gameGuiOptions[] = {
 		}
 	},
 
-	// I18N: Helium mode makes people sound like they've inhaled Helium.
 	{
 		GAMEOPTION_KYRA3_HELIUM,
 		{
+			// I18N: Helium mode makes people sound like they've inhaled Helium.
 			_s("Helium mode"),
 			_s("Enable helium mode"),
 			"helium_mode",
@@ -91,11 +91,10 @@ const ADExtraGuiOptionsMap gameGuiOptions[] = {
 #ifdef ENABLE_LOL
 	// LoL options
 
-	// I18N: When enabled, this option makes scrolling smoother when changing
-	// from one screen to another.
 	{
 		GAMEOPTION_LOL_SCROLLING,
 		{
+			// I18N: When enabled, this option makes scrolling smoother when changing from one screen to another.
 			_s("Smooth scrolling"),
 			_s("Enable smooth scrolling when walking"),
 			"smooth_scrolling",
@@ -103,12 +102,12 @@ const ADExtraGuiOptionsMap gameGuiOptions[] = {
 		}
 	},
 
-	// I18N: When enabled, this option changes the cursor when it floats to the
-	// edge of the screen to a directional arrow. The player can then click to
-	// walk towards that direction.
 	{
 		GAMEOPTION_LOL_CURSORS,
 		{
+			// I18N: When enabled, this option changes the cursor when it floats to the
+			// edge of the screen to a directional arrow. The player can then click to
+			// walk towards that direction.
 			_s("Floating cursors"),
 			_s("Enable floating cursors"),
 			"floating_cursors",
@@ -123,6 +122,7 @@ const ADExtraGuiOptionsMap gameGuiOptions[] = {
 	{
 		GAMEOPTION_EOB_HPGRAPHS,
 		{
+			// I18N: HP stands for Hit Points
 			_s("HP bar graphs"),
 			_s("Enable hit point bar graphs"),
 			"hpbargraphs",
