561b20edde3d94e63acb0b0292113a3ad434b264
Strangerke <strangerke@scummvm.org>
2012-04-01 14:29:34 +0200

    LILLIPUT: Add resource loading

--- engines/lilliput/lilliput.h
+++ engines/lilliput/lilliput.h
@@ -76,11 +76,71 @@ public:
 
 	Common::RandomSource *_rnd;
 
+	byte _buffer1[45056];
+	byte _buffer2[45056];
+	byte _buffer3[45056];
+	byte *_bufferIsoMap;
+	byte _bufferCubegfx[61440];
+	byte *_bufferMen;
+	byte *_bufferMen2;
+	byte *_bufferIsoChars;
+	byte *_bufferIdeogram;
+	byte _buffer10[4032];
+	byte _palette[768];
+	
+	int _word10800_ERULES;
+	int _word10807_ERULES;
+	byte *_rulesChunk1;
+	int _rulesBuffer2_1[40];
+	int _rulesBuffer2_2[40];
+	byte _rulesBuffer2_3[40];
+	int _rulesBuffer2_4[40];
+	byte _rulesBuffer2_5[40];
+	byte _rulesBuffer2_6[40];
+	byte _rulesBuffer2_7[40];
+	byte _rulesBuffer2_8[40];
+	byte _rulesBuffer2_9[40];
+	byte _rulesBuffer2_10[40];
+	byte _rulesBuffer2_11[40];
+	byte _rulesBuffer2_12[40];
+	byte _rulesBuffer2_13[40];
+	byte _rulesBuffer2_14[40];
+	byte _rulesBuffer2_15[40 * 32];
+	byte _rulesBuffer2_16[40 * 32];
+	int *_rulesChunk3;
+	int _rulesChunk3_size;
+	byte *_rulesChunk4;
+	int *_rulesScript;
+	int _rulesScript_size;
+	int *_rulesChunk6;
+	int _rulesChunk6_size;
+	int *_rulesChunk7;
+	int _rulesChunk7_size;
+	byte *_rulesChunk8;
+	byte _rulesChunk9[60];
+	int _rulesChunk10_size;
+	int *_rulesChunk10;
+	byte *_rulesChunk11;
+	int _rulesChunk12_size;
+	int _rulesBuffer12_1[40];
+	int _rulesBuffer12_2[40];
+	int _rulesBuffer12_3[40];
+	int _rulesBuffer12_4[40];
+	int _word12F68_ERULES;
+	byte _rulesBuffer13_1[20];
+	int _rulesBuffer13_2[20];
+	int _rulesBuffer13_3[20];
+	byte _rulesBuffer13_4[20];
+
+
 	const LilliputGameDescription *_gameDescription;
 	uint32 getFeatures() const;
 	const char *getGameId() const;
 
 	void initGame(const LilliputGameDescription *gd);
+	byte *loadVGA(Common::String filename, bool loadPal);
+	byte *loadRaw(Common::String filename);
+	void loadRules();
 
 	GameType getGameType() const;
 	Common::Platform getPlatform() const;
