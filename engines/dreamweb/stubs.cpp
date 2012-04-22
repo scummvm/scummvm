6ecf27711c886b1422f26acc87cdfd893897eb64
Filippos Karapetis <md5@scummvm.org>
2012-04-22 16:05:41 +0300

    DREAMWEB: Move all of the mouse cursor related code into a separate file

--- engines/dreamweb/stubs.cpp
+++ engines/dreamweb/stubs.cpp
@@ -876,22 +876,6 @@ void DreamWebEngine::hangOnCurs(uint16 frameCount) {
 	}
 }
 
-void DreamWebEngine::readMouse() {
-	_oldButton = _mouseButton;
-	uint16 state = readMouseState();
-	_mouseButton = state;
-}
-
-uint16 DreamWebEngine::readMouseState() {
-	_oldX = _mouseX;
-	_oldY = _mouseY;
-	uint16 x, y, state;
-	mouseCall(&x, &y, &state);
-	_mouseX = x;
-	_mouseY = y;
-	return state;
-}
-
 void DreamWebEngine::dumpTextLine() {
 	if (_newTextLine != 1)
 		return;
@@ -1513,16 +1497,6 @@ void DreamWebEngine::obName(uint8 command, uint8 commandType) {
 	}
 }
 
-void DreamWebEngine::delPointer() {
-	if (_oldPointerX == 0xffff)
-		return;
-	_delHereX = _oldPointerX;
-	_delHereY = _oldPointerY;
-	_delXS = _pointerXS;
-	_delYS = _pointerYS;
-	multiPut(_pointerBack, _delHereX, _delHereY, _pointerXS, _pointerYS);
-}
-
 void DreamWebEngine::showBlink() {
 	if (_manIsOffScreen == 1)
 		return;
@@ -1552,110 +1526,6 @@ void DreamWebEngine::dumpBlink() {
 	if (_blinkFrame >= 6)
 		return;
 	multiDump(44, 32, 16, 12);
-}
-
-void DreamWebEngine::dumpPointer() {
-	dumpBlink();
-	multiDump(_delHereX, _delHereY, _delXS, _delYS);
-	if ((_oldPointerX != _delHereX) || (_oldPointerY != _delHereY))
-		multiDump(_oldPointerX, _oldPointerY, _pointerXS, _pointerYS);
-}
-
-void DreamWebEngine::showPointer() {
-	showBlink();
-	uint16 x = _mouseX;
-	_oldPointerX = _mouseX;
-	uint16 y = _mouseY;
-	_oldPointerY = _mouseY;
-	if (_pickUp == 1) {
-		const GraphicsFile *frames;
-		if (_objectType != kExObjectType)
-			frames = &_freeFrames;
-		else
-			frames = &_exFrames;
-		const Frame *frame = &frames->_frames[(3 * _itemFrame + 1)];
-
-		uint8 width = frame->width;
-		uint8 height = frame->height;
-		if (width < 12)
-			width = 12;
-		if (height < 12)
-			height = 12;
-		_pointerXS = width;
-		_pointerYS = height;
-		uint16 xMin = (x >= width / 2) ? x - width / 2 : 0;
-		uint16 yMin = (y >= height / 2) ? y - height / 2 : 0;
-		_oldPointerX = xMin;
-		_oldPointerY = yMin;
-		multiGet(_pointerBack, xMin, yMin, width, height);
-		showFrame(*frames, x, y, 3 * _itemFrame + 1, 128);
-		showFrame(_icons1, x, y, 3, 128);
-	} else {
-		const Frame *frame = &_icons1._frames[_pointerFrame + 20];
-		uint8 width = frame->width;
-		uint8 height = frame->height;
-		if (width < 12)
-			width = 12;
-		if (height < 12)
-			height = 12;
-		_pointerXS = width;
-		_pointerYS = height;
-		multiGet(_pointerBack, x, y, width, height);
-		showFrame(_icons1, x, y, _pointerFrame + 20, 0);
-	}
-}
-
-void DreamWebEngine::animPointer() {
-
-	if (_pointerMode == 2) {
-		_pointerFrame = 0;
-		if ((_realLocation == 14) && (_commandType == 211))
-			_pointerFrame = 5;
-		return;
-	} else if (_pointerMode == 3) {
-		if (_pointerSpeed != 0) {
-			--_pointerSpeed;
-		} else {
-			_pointerSpeed = 5;
-			++_pointerCount;
-			if (_pointerCount == 16)
-				_pointerCount = 0;
-		}
-		_pointerFrame = (_pointerCount <= 8) ? 1 : 2;
-		return;
-	}
-	if (_vars._watchingTime != 0) {
-		_pointerFrame = 11;
-		return;
-	}
-	_pointerFrame = 0;
-	if (_inMapArea == 0)
-		return;
-	if (_pointerFirstPath == 0)
-		return;
-	uint8 flag, flagEx;
-	getFlagUnderP(&flag, &flagEx);
-	if (flag < 2)
-		return;
-	if (flag >= 128)
-		return;
-	if (flag & 4) {
-		_pointerFrame = 3;
-		return;
-	}
-	if (flag & 16) {
-		_pointerFrame = 4;
-		return;
-	}
-	if (flag & 2) {
-		_pointerFrame = 5;
-		return;
-	}
-	if (flag & 8) {
-		_pointerFrame = 6;
-		return;
-	}
-	_pointerFrame = 8;
 }
 
 void DreamWebEngine::printMessage(uint16 x, uint16 y, uint8 index, uint8 maxWidth, bool centered) {
@@ -3178,18 +3048,6 @@ void DreamWebEngine::purgeAnItem() {
 	for (size_t i = 0; i < kNumexobjects; ++i) {
 		if (extraObjects[i].mapad[0] && extraObjects[i].objId[0] == 255) {
 			deleteExObject(i);
-			return;
-		}
-	}
-}
-
-void DreamWebEngine::checkCoords(const RectWithCallback *rectWithCallbacks) {
-	if (_newLocation != 0xff)
-		return;
-	const RectWithCallback *r;
-	for (r = rectWithCallbacks; r->_xMin != 0xffff; ++r) {
-		if (r->contains(_mouseX, _mouseY)) {
-			(this->*(r->_callback))();
 			return;
 		}
 	}
