a3af191753e4c53cf33a730d6d10a47bcde37293
Eric Culp <eculperic@gmail.com>
2012-04-25 15:57:18 -0400

    GRAPHICS/BASE: Adds definitions for scaler plugins This includes a class for plugin implementations to inherit from and code necessary for plugin managers to use scalers

--- base/plugins.h
+++ base/plugins.h
@@ -43,7 +43,7 @@ enum PluginType {
 	PLUGIN_TYPE_ENGINE,
 	PLUGIN_TYPE_MUSIC,
 	PLUGIN_TYPE_DETECTION,
-	/* PLUGIN_TYPE_SCALER, */	// TODO: Add graphics scaler plugins
+	PLUGIN_TYPE_SCALER,
 
 	PLUGIN_TYPE_MAX
 };
@@ -54,6 +54,7 @@ enum PluginType {
 #define PLUGIN_TYPE_ENGINE_VERSION 2
 #define PLUGIN_TYPE_MUSIC_VERSION 1
 #define PLUGIN_TYPE_DETECTION_VERSION 1
+#define PLUGIN_TYPE_SCALER_VERSION 1
 
 extern int pluginTypeVersions[PLUGIN_TYPE_MAX];
 
