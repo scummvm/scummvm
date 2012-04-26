f0d4b200d7b10a1f4a32e24459c9ed97e1484d2b
Torbjörn Andersson <eriktorbjorn@users.sourceforge.net>
2012-04-27 00:35:53 +0200

    TONY: Change backslash to slash
    I assume slash works in Windows (since we use it in other engines),
    and I know backslash doesn't work in Linux because it failed for me.

--- engines/tony/module.mk
+++ engines/tony/module.mk
@@ -3,11 +3,11 @@ MODULE := engines/tony
 MODULE_OBJS := \
 	detection.o \
 	tony.o \
-	lzo\stubs.o \
-	mpal\expr.o \
-	mpal\loadmpc.o \
-	mpal\mpal.o \
-	mpal\stubs.o
+	lzo/stubs.o \
+	mpal/expr.o \
+	mpal/loadmpc.o \
+	mpal/mpal.o \
+	mpal/stubs.o
 
 # This module can be built as a plugin
 ifeq ($(ENABLE_TONY), DYNAMIC_PLUGIN)
