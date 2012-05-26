9b7b591f9050266a94ac2e337ea955d713f8481d
Eric Culp <eculperic@gmail.com>
2012-05-26 03:42:17 -0400

    BACKEND: fixes a bug related to AR correction

--- backends/graphics/surfacesdl/surfacesdl-graphics.cpp
+++ backends/graphics/surfacesdl/surfacesdl-graphics.cpp
@@ -1369,10 +1369,13 @@ void SurfaceSdlGraphicsManager::addDirtyRect(int x, int y, int w, int h, bool re
 		height = _videoMode.overlayHeight;
 	}
 
-	// Extend the dirty region by 1 pixel for scalers
+	// Extend the dirty region for scalers
 	// that "smear" the screen, e.g. 2xSAI
 	if (!realCoordinates) {
 		int adjust = (*_scalerPlugin)->extraPixels();
+		// Aspect ratio correction requires this to be at least one
+		if (adjust == 0)
+			adjust = 1;
 		x -= adjust;
 		y -= adjust;
 		w += adjust * 2;
