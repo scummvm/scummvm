0d8222fae3e242012c197090f78685969831f00b
Eric Culp <eculperic@gmail.com>
2012-05-28 16:26:31 -0400

    GRAPHICS/BASE: add maxExtraPixels() to ScalerMan

--- base/plugins.cpp
+++ base/plugins.cpp
@@ -561,3 +561,16 @@ DECLARE_SINGLETON(ScalerManager);
 const ScalerPlugin::List &ScalerManager::getPlugins() const {
 	return (const ScalerPlugin::List &)PluginManager::instance().getPlugins(PLUGIN_TYPE_SCALER);
 }
+
+int ScalerManager::getMaxExtraPixels() const {
+	int maxPixels = 0;
+	ScalerPlugin::List plugins = getPlugins();
+	ScalerPlugin::List::iterator i = plugins.begin();
+	for (; i != plugins.end(); ++i) {
+		int n = (**i)->extraPixels();
+		if (n > maxPixels) {
+			maxPixels = n;
+		}
+	}
+	return maxPixels;
+}
