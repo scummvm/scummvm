3bcb0d545b64689a78987bcebffd924e5ce51315
Eric Culp <eculperic@gmail.com>
2012-05-29 15:33:14 -0400

    GRAPHICS: fix dotmatrix inconsistencies Now it uses the x and y coordinates to produce the same pixels in the destination surface.

--- graphics/scaler/dotmatrix.cpp
+++ graphics/scaler/dotmatrix.cpp
@@ -23,7 +23,15 @@
 #include "graphics/scaler.h"
 
 void DotMatrixPlugin::initialize(Graphics::PixelFormat format) {
-	// TODO: initialize dotmatrix array
+	lookup[0] = lookup[10] = format.RGBToColor(0, 63, 0);
+	lookup[1] = lookup[11] = format.RGBToColor(0, 0, 63);
+	lookup[2] = lookup[8] = format.RGBToColor(63, 0, 0);
+	lookup[4] = lookup[6] =
+		lookup[12] = lookup[14] = format.RGBToColor(63, 63, 63);
+	lookup[5] = lookup[7] =
+		lookup[9] = lookup[13] =
+		lookup[15] = lookup[16] = format.RGBToColor(0, 0, 0);
+
 }
 
 void DotMatrixPlugin::scale(const uint8 *srcPtr, uint32 srcPitch,
@@ -37,7 +45,7 @@ void DotMatrixPlugin::scale(const uint8 *srcPtr, uint32 srcPitch,
 		Normal1x(srcPtr, srcPitch, dstPtr, dstPitch, width, height);
 		break;
 	case 2:
-		DotMatrix(srcPtr, srcPitch, dstPtr, dstPitch, width, height);
+		scaleIntern(srcPtr, srcPitch, dstPtr, dstPitch, width, height, x, y);
 		break;
 	}
 }
@@ -58,5 +66,35 @@ const char *DotMatrixPlugin::getName() const {
 	return "DotMatrix";
 }
 
+static inline uint16 DOT_16(const uint16 *dotmatrix, uint16 c, int j, int i) {
+	return c - ((c >> 2) & dotmatrix[((j & 3) << 2) + (i & 3)]);
+}
+
+void DotMatrixPlugin::scaleIntern(const uint8 *srcPtr, uint32 srcPitch, uint8 *dstPtr, uint32 dstPitch,
+					int width, int height, int x, int y) {
+
+	const uint16 *dotmatrix = lookup;
+
+	const uint32 nextlineSrc = srcPitch / sizeof(uint16);
+	const uint16 *p = (const uint16 *)srcPtr;
+
+	const uint32 nextlineDst = dstPitch / sizeof(uint16);
+	uint16 *q = (uint16 *)dstPtr;
+
+	int ja = (y * 2) & 3;
+	int ia = (x * 2) & 3;
+
+	for (int j = 0, jj = 0; j < height; ++j, jj += 2) {
+		for (int i = 0, ii = 0; i < width; ++i, ii += 2) {
+			uint16 c = *(p + i);
+			*(q + ii) = DOT_16(dotmatrix, c, jj + ja, ii + ia);
+			*(q + ii + 1) = DOT_16(dotmatrix, c, jj + ja, ii + ia + 1);
+			*(q + ii + nextlineDst) = DOT_16(dotmatrix, c, jj + ja + 1, ii + ia);
+			*(q + ii + nextlineDst + 1) = DOT_16(dotmatrix, c, jj + ja + 1, ii + ia+ 1);
+		}
+		p += nextlineSrc;
+		q += nextlineDst << 1;
+	}
+}
 
 REGISTER_PLUGIN_STATIC(DOTMATRIX, PLUGIN_TYPE_SCALER, DotMatrixPlugin);
