c17af63aacda4b18ca6e756fb710cecb3b25fc88
Strangerke <strangerke@scummvm.org>
2012-06-04 22:18:50 +0200

    TONY: Modify detection in order to use roasted.mpc too. Add French, German, Italian and Polish versions.
    This also breaks the English game detection. To be fixed!

--- engines/tony/detection_tables.h
+++ engines/tony/detection_tables.h
@@ -27,8 +27,13 @@ static const TonyGameDescription gameDescriptions[] = {
 		// Tony Tough English
 		{
 			"tony",
-			"",
-			AD_ENTRY1s("roasted.mpr", "06203dbbc85fdd1e6dc8fc211c1a6207", 135911071),
+			0,
+			{
+				{"roasted.mpr", 0, "06203dbbc85fdd1e6dc8fc211c1a6207", 135911071},
+				// Fake md5 : to be fixed!
+				{"roasted.mpc", 0, "bad97eae03a4db3e99565e39b0b3c06a", 16384},
+				AD_LISTEND
+			},
 			Common::EN_ANY,
 			Common::kPlatformPC,
 			ADGF_NO_FLAGS,
@@ -41,7 +46,11 @@ static const TonyGameDescription gameDescriptions[] = {
 		{
 			"tony",
 			"Extracted Demo",
-			AD_ENTRY1s("roasted.mpr", "06203dbbc85fdd1e6dc8fc211c1a6207", 14972409),
+			{
+				{"roasted.mpr", 0, "06203dbbc85fdd1e6dc8fc211c1a6207", 14972409},
+				{"roasted.mpc", 0, "1e247922ec869712bfd96625bc4d3c7c", 39211},
+				AD_LISTEND
+			},
 			Common::EN_ANY,
 			Common::kPlatformPC,
 			ADGF_DEMO,
@@ -54,14 +63,80 @@ static const TonyGameDescription gameDescriptions[] = {
 		{
 			"tony",
 			"Demo",
-			AD_ENTRY1s("data1.cab", "7d8b6d308f96aee3968ad7910fb11e6d", 58660608),
+			{
+				{"data1.cab", 0, "7d8b6d308f96aee3968ad7910fb11e6d", 58660608},
+				AD_LISTEND
+			},
 			Common::EN_ANY,
 			Common::kPlatformPC,
 			ADGF_DEMO | GF_COMPRESSED,
 			GUIO1(GUIO_NONE)
 		},
 	},
-
+	{
+		// Tony Tough French "Collection Aventure" provided by Strangerke
+		{
+			"tony",
+			0,
+			{
+				{"roasted.mpr", 0, "06203dbbc85fdd1e6dc8fc211c1a6207", 135911071},
+				{"roasted.mpc", 0, "e890c6a41238827bdfa9874a65618b69", 374135},
+				AD_LISTEND
+			},
+			Common::FR_FRA,
+			Common::kPlatformPC,
+			ADGF_NO_FLAGS,
+			GUIO1(GUIO_NONE)
+		},
+	},
+	{
+		// Tony Tough German "Shoe Box" provided by Strangerke
+		{
+			"tony",
+			0,
+			{
+				{"roasted.mpr", 0, "06203dbbc85fdd1e6dc8fc211c1a6207", 135911071},
+				{"roasted.mpc", 0, "ccf7ab939a34de1b13df538596431684", 389554},
+				AD_LISTEND
+			},
+			Common::DE_DEU,
+			Common::kPlatformPC,
+			ADGF_NO_FLAGS,
+			GUIO1(GUIO_NONE)
+		},
+	},
+	{
+		// Tony Tough Italian provided by Fabio Barzagli
+		{
+			"tony",
+			0,
+			{
+				{"roasted.mpr", 0, "06203dbbc85fdd1e6dc8fc211c1a6207", 135911071},
+				{"roasted.mpc", 0, "1dc896cdb945170d7408598f803411c1", 380001},
+				AD_LISTEND
+			},
+			Common::IT_ITA,
+			Common::kPlatformPC,
+			ADGF_NO_FLAGS,
+			GUIO1(GUIO_NONE)
+		},
+	},
+	{
+		// Tony Tough Polish provided by Fabio Barzagli
+		{
+			"tony",
+			0,
+			{
+				{"roasted.mpr", 0, "06203dbbc85fdd1e6dc8fc211c1a6207", 135911071},
+				{"roasted.mpc", 0, "89733ea710669acc8e7900b115f4afef", 389625},
+				AD_LISTEND
+			},
+			Common::PL_POL,
+			Common::kPlatformPC,
+			ADGF_NO_FLAGS,
+			GUIO1(GUIO_NONE)
+		},
+	},
 	{ AD_TABLE_END_MARKER }
 };
 
