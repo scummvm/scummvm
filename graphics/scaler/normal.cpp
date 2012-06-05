02902ac9a43da3bd7e9a693169158f9b36bb4f91
Eric Culp <eculperic@gmail.com>
2012-06-05 16:01:56 -0400

    GRAPHICS: added 4x scaling to normal scaler plugin

--- graphics/scaler/normal.cpp
+++ graphics/scaler/normal.cpp
@@ -27,9 +27,48 @@ NormalPlugin::NormalPlugin() {
 	_factors.push_back(1);
 	_factors.push_back(2);
 	_factors.push_back(3);
+	_factors.push_back(4);
 }
 
 void NormalPlugin::initialize(Graphics::PixelFormat format) {
+}
+
+/**
+ * Trivial nearest-neighbor 4x scaler.
+ */
+void Normal4x(const uint8 *srcPtr, uint32 srcPitch, uint8 *dstPtr, uint32 dstPitch,
+							int width, int height) {
+	uint8 *r;
+	const uint32 dstPitch2 = dstPitch * 2;
+	const uint32 dstPitch3 = dstPitch * 3;
+	const uint32 dstPitch4 = dstPitch * 4;
+
+	assert(IS_ALIGNED(dstPtr, 2));
+	while (height--) {
+		r = dstPtr;
+		for (int i = 0; i < width; ++i, r += 8) {
+			uint16 color = *(((const uint16 *)srcPtr) + i);
+
+			*(uint16 *)(r + 0) = color;
+			*(uint16 *)(r + 2) = color;
+			*(uint16 *)(r + 4) = color;
+			*(uint16 *)(r + 6) = color;
+			*(uint16 *)(r + 0 + dstPitch) = color;
+			*(uint16 *)(r + 2 + dstPitch) = color;
+			*(uint16 *)(r + 4 + dstPitch) = color;
+			*(uint16 *)(r + 6 + dstPitch) = color;
+			*(uint16 *)(r + 0 + dstPitch2) = color;
+			*(uint16 *)(r + 2 + dstPitch2) = color;
+			*(uint16 *)(r + 4 + dstPitch2) = color;
+			*(uint16 *)(r + 6 + dstPitch2) = color;
+			*(uint16 *)(r + 0 + dstPitch3) = color;
+			*(uint16 *)(r + 2 + dstPitch3) = color;
+			*(uint16 *)(r + 4 + dstPitch3) = color;
+			*(uint16 *)(r + 6 + dstPitch3) = color;
+		}
+		srcPtr += srcPitch;
+		dstPtr += dstPitch4;
+	}
 }
 
 void NormalPlugin::scale(const uint8 *srcPtr, uint32 srcPitch,
@@ -48,11 +87,14 @@ void NormalPlugin::scale(const uint8 *srcPtr, uint32 srcPitch,
 	case 3:
 		Normal3x(srcPtr, srcPitch, dstPtr, dstPitch, width, height);
 		break;
+	case 4:
+		Normal4x(srcPtr, srcPitch, dstPtr, dstPitch, width, height);
+		break;
 	}
 }
 
 uint NormalPlugin::increaseFactor() {
-	if (_factor < 3)
+	if (_factor < 4)
 		++_factor;
 	return _factor;
 }
