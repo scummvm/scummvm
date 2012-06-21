eabf4b1bf6fab970829b79e5c3b5cd43670709f8
Eric Culp <eculperic@gmail.com>
2012-06-21 11:02:00 -0400

    GRAPHICS: Add 32bpp support for TV scaler

--- graphics/scaler/tv.cpp
+++ graphics/scaler/tv.cpp
@@ -34,10 +34,17 @@ void TVPlugin::initialize(Graphics::PixelFormat format) {
 
 void TVPlugin::scale(const uint8 *srcPtr, uint32 srcPitch,
 							uint8 *dstPtr, uint32 dstPitch, int width, int height, int x, int y) {
-	if (_format.gLoss == 2)
-		scaleIntern<Graphics::ColorMasks<565> >(srcPtr, srcPitch, dstPtr, dstPitch, width, height);
-	else
-		scaleIntern<Graphics::ColorMasks<555> >(srcPtr, srcPitch, dstPtr, dstPitch, width, height);
+	if (_format.bytesPerPixel == 2) {
+		if (_format.gLoss == 2)
+			scaleIntern<Graphics::ColorMasks<565>, uint16>(srcPtr, srcPitch, dstPtr, dstPitch, width, height);
+		else
+			scaleIntern<Graphics::ColorMasks<555>, uint16>(srcPtr, srcPitch, dstPtr, dstPitch, width, height);
+	} else {
+		if (_format.aLoss == 0)
+			scaleIntern<Graphics::ColorMasks<8888>, uint32>(srcPtr, srcPitch, dstPtr, dstPitch, width, height);
+		else
+			scaleIntern<Graphics::ColorMasks<888>, uint32>(srcPtr, srcPitch, dstPtr, dstPitch, width, height);
+	}
 
 }
 
@@ -57,27 +64,32 @@ const char *TVPlugin::getPrettyName() const {
 	return "TV";
 }
 
-template<typename ColorMask>
+template<typename ColorMask, typename pixel>
 void TVPlugin::scaleIntern(const uint8 *srcPtr, uint32 srcPitch, uint8 *dstPtr, uint32 dstPitch,
 					int width, int height) {
-	const uint32 nextlineSrc = srcPitch / sizeof(uint16);
-	const uint16 *p = (const uint16 *)srcPtr;
+	const uint32 nextlineSrc = srcPitch / sizeof(pixel);
+	const pixel *p = (const pixel *)srcPtr;
 
-	const uint32 nextlineDst = dstPitch / sizeof(uint16);
-	uint16 *q = (uint16 *)dstPtr;
+	const uint32 nextlineDst = dstPitch / sizeof(pixel);
+	pixel *q = (pixel *)dstPtr;
 
 	while (height--) {
 		for (int i = 0, j = 0; i < width; ++i, j += 2) {
-			uint16 p1 = *(p + i);
-			uint32 pi;
+			pixel p1 = *(p + i);
+			pixel pi;
 
 			pi = (((p1 & ColorMask::kRedBlueMask) * 7) >> 3) & ColorMask::kRedBlueMask;
 			pi |= (((p1 & ColorMask::kGreenMask) * 7) >> 3) & ColorMask::kGreenMask;
+			pi |= p1 & ColorMask::kAlphaMask;
+
+			uint8 r, g, b;
+			_format.colorToRGB(p1,r,g,b);
+			pi = _format.RGBToColor((r*7)/8, (g*7) / 8, (b*7) / 8);
 
 			*(q + j) = p1;
 			*(q + j + 1) = p1;
-			*(q + j + nextlineDst) = (uint16)pi;
-			*(q + j + nextlineDst + 1) = (uint16)pi;
+			*(q + j + nextlineDst) = pi;
+			*(q + j + nextlineDst + 1) = pi;
 		}
 		p += nextlineSrc;
 		q += nextlineDst << 1;
