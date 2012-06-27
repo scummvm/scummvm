9ba4c24ae133ccdcdc01091812f4470c03a2adeb
Eric Culp <eculperic@gmail.com>
2012-06-27 18:39:12 -0400

    GRAPHICS: Migrate global funcs and vars to Edge Plugin class

--- graphics/scaler/edge.h
+++ graphics/scaler/edge.h
@@ -38,6 +38,44 @@ public:
 	virtual uint extraPixels() const { return 1; }
 	virtual const char *getName() const;
 	virtual const char *getPrettyName() const;
+private:
+	int16* chooseGreyscale(uint16 *pixels);
+	int32 calcPixelDiffNosqrt(uint16 pixel1, uint16 pixel2);
+	int findPrincipleAxis(uint16 *pixels, int16 *diffs, int16 *bplane,
+		int8 *sim,
+		int32 *return_angle);
+	int checkArrows(int best_dir, uint16 *pixels, int8 *sim, int half_flag);
+	int refineDirection(char edge_type, uint16 *pixels, int16 *bptr,
+		int8 *sim, double angle);
+	int fixKnights(int sub_type, uint16 *pixels, int8 *sim);
+	void initTables(const uint8 *srcPtr, uint32 srcPitch,
+		int width, int height);
+
+	template<typename ColorMask>
+	void anti_alias_grid_2x(uint8 *dptr, int dstPitch,
+		uint16 *pixels, int sub_type, int16 *bptr,
+		int8 *sim,
+		int interpolate_2x);
+	template<typename ColorMask>
+	void anti_alias_grid_clean_3x(uint8 *dptr, int dstPitch,
+		uint16 *pixels, int sub_type, int16 *bptr);
+	template<typename ColorMask>
+	void antiAliasPass2x(const uint8 *src, uint8 *dst,
+		int w, int h, int w_new, int h_new,
+		int srcPitch, int dstPitch,
+		int overlay_flag,
+		int interpolate_2x);
+	template<typename ColorMask>
+	void antiAliasPass3x(const uint8 *src, uint8 *dst,
+		int w, int h, int w_new, int h_new,
+		int srcPitch, int dstPitch,
+		int overlay_flag);
+
+	int16 _rgbTable[65536][3];       ///< table lookup for RGB
+	int16 _greyscaleTable[3][65536]; ///< greyscale tables
+	int16 *_chosenGreyscale;               ///< pointer to chosen greyscale table
+	int16 *_bptr;                          ///< too awkward to pass variables
+	int8 _simSum;                          ///< sum of similarity matrix
 };
 
 
