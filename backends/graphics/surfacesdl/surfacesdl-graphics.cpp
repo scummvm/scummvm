fbfb44029c236c3a8afe44fe189e4a052ade08a1
Eric Culp <eculperic@gmail.com>
2012-07-03 15:53:11 -0400

    SDL: Only update if a change was made when using oldSrcScale

--- backends/graphics/surfacesdl/surfacesdl-graphics.cpp
+++ backends/graphics/surfacesdl/surfacesdl-graphics.cpp
@@ -1250,7 +1250,7 @@ void SurfaceSdlGraphicsManager::internUpdateScreen() {
 
 	// Force a full redraw if requested.
 	// If _useOldSrc, the scaler will do its own partial updates.
-	if (_forceRedraw || (_useOldSrc && !_overlayVisible)) {
+	if (_forceRedraw || (_useOldSrc && !_overlayVisible && _numDirtyRects > 0)) {
 		_numDirtyRects = 1;
 		_dirtyRectList[0].x = 0;
 		_dirtyRectList[0].y = 0;
