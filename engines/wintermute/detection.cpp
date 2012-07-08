31043124d5fae105a8289ed50558e5693c5b0b6c
Einar Johan Trøan Sømåen <einarjohants@gmail.com>
2012-07-08 18:32:57 +0200

    WINTERMUTE: Add detection for Five Lethal Demons

--- engines/wintermute/detection.cpp
+++ engines/wintermute/detection.cpp
@@ -86,6 +86,16 @@ static const ADGameDescription gameDescriptions[] = {
 	},
 	{
 		"wintermute",
+		"Five Lethal Demons",
+		AD_ENTRY1s("data.dcp", "1037a77cbd001e0644898addc022322c", 15407750),
+		Common::EN_ANY,
+		Common::kPlatformWindows,
+		ADGF_UNSTABLE |
+		ADGF_USEEXTRAASTITLE,
+		GUIO0()
+	},
+	{
+		"wintermute",
 		"the white chamber",
 		AD_ENTRY1s("data.dcp", "0011d01142547c61e51ba24dc42b579e", 186451273),
 		Common::EN_ANY,
