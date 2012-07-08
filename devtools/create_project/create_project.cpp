fba7a4b12faae21d835d5fdd7c4cdff83d4cc8ac
Johannes Schickel <lordhoto@scummvm.org>
2012-07-08 22:41:44 +0200

    DEVTOOLS: Disable OpenGL support in create_project.

--- devtools/create_project/create_project.cpp
+++ devtools/create_project/create_project.cpp
@@ -854,7 +854,7 @@ const Feature s_features[] = {
 	{       "16bit",   "USE_RGB_COLOR",         "", true, "16bit color support" },
 	{     "mt32emu",     "USE_MT32EMU",         "", true, "integrated MT-32 emulator" },
 	{        "nasm",        "USE_NASM",         "", true, "IA-32 assembly support" }, // This feature is special in the regard, that it needs additional handling.
-	{      "opengl",      "USE_OPENGL", "opengl32", true, "OpenGL support" },
+	{      "opengl",      "USE_OPENGL", "opengl32", false, "OpenGL support" },
 	{     "taskbar",     "USE_TASKBAR",         "", true, "Taskbar integration support" },
 	{ "translation", "USE_TRANSLATION",         "", true, "Translation support" },
 	{      "vkeybd",   "ENABLE_VKEYBD",         "", false, "Virtual keyboard support"},
