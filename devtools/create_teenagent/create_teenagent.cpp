f85f8c806d0610197baac7edbeb78c731befa756
D G Turner <digitall@scummvm.org>
2012-07-09 13:57:41 +0100

    DEVTOOLS: Temporarily modify create_teenagent to convert eseg blob.
    This converts the eseg data to a readable string structure and outputs
    the result as C header code to stdout. Committing this for reference.

--- devtools/create_teenagent/create_teenagent.cpp
+++ devtools/create_teenagent/create_teenagent.cpp
@@ -36,6 +36,7 @@
 #include "static_tables.h"
 
 int main(int argc, char *argv[]) {
+/*
 	const char *dat_name = "teenagent.dat";
 
 	FILE *fout = fopen(dat_name, "wb");
@@ -62,6 +63,77 @@ int main(int argc, char *argv[]) {
 	fclose(fout);
 
 	fprintf(stderr, "please run \"gzip -n %s\"\n", dat_name);
+*/
+
+	printf("#define ANIM_WAIT        \"\\xff\"\n");
+	printf("#define NEW_LINE         \"\\n\"\n");
+	printf("#define DISPLAY_MESSAGE  \"\\n\\n\"\n");
+	printf("#define CHANGE_CHARACTER \"\\n\\n\\n\"\n");
+	printf("#define END_DIALOG       \"\\n\\n\\n\\n\"\n");
+	printf("\n");
+	int dialog_num = 0;
+	printf("const static char* dialog_%d[] = {\n", dialog_num);
+	int n = 0;
+	uint8 last = 0xff;
+	for (int i = 0; i < ESEG_SIZE; i++) {
+		if ((eseg[i] != 0x00 && last == 0x00) || n == 4) {
+			switch (n) {
+				case 1:
+					printf("\tNEW_LINE,\n");
+					break;
+				case 2:
+					printf("\tDISPLAY_MESSAGE,\n");
+					break;
+				case 3:
+					printf("\tCHANGE_CHARACTER,\n");
+					break;
+				case 4:
+					dialog_num++;
+					printf("\tEND_DIALOG\n};\n\n");
+					printf("const static char* dialog_%d[] = {\n", dialog_num);
+					break;
+				default:
+					fprintf(stderr, "ERROR: %d is too many nulls\n", n);
+					break;
+			}
+			n = 0;
+		}
+
+		switch (eseg[i]) {
+			case 0x00:
+				n++;
+				if (!(last == 0x00 || last == 0xff))
+					printf("\",\n");
+				break;
+			case 0xff:
+				if (!(last == 0x00 || last == 0xff))
+					printf("\",\n");
+				printf("\tANIM_WAIT,\n");
+				break;
+			default:
+				if ((last == 0x00 || last == 0xff))
+					printf("\t\"");
+
+				if (eseg[i] >= 32 && eseg[i] <= 126) {
+					if (eseg[i] == '\"')
+						printf("\\");
+					printf("%c", eseg[i]);
+				} else fprintf(stderr, "ERROR: eseg[%d]:0x%02x is outside range\n", i, eseg[i]);
+				break;
+		}
+
+		last = eseg[i];
+	}
+
+	if (n == 4) {
+		dialog_num++;
+		printf("\tEND_DIALOG\n};\n");
+	} else fprintf(stderr, "ERROR: premature end of data\n");
+
+	printf("\nconst static char** dialogs[] = {\n");
+	for (int i = 0; i < dialog_num; i++)
+		printf("\tdialog_%d%s\n", i, (i == dialog_num-1) ? "" : ",");
+	printf("};\n");
 
 	return 0;
 }
