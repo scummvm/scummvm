5886f3948778c0072d11f95c2c307f73f907f742
David-John Willis <John.Willis@Distant-earth.com>
2012-07-23 22:28:10 +0100

    OPENPANDORA: Move SDL_CreateCursor hacklet from init to loadGFXMode.
    * Fixes a bug that can occur on load.
    (cherry picked from commit 45354676ebe14df8d382681621f1d994258dbb72)

--- backends/graphics/openpandora/op-graphics.cpp
+++ backends/graphics/openpandora/op-graphics.cpp
@@ -31,17 +31,35 @@
 #include "common/mutex.h"
 #include "common/textconsole.h"
 
+static SDL_Cursor *hiddenCursor;
+
 OPGraphicsManager::OPGraphicsManager(SdlEventSource *sdlEventSource)
 	: SurfaceSdlGraphicsManager(sdlEventSource) {
 }
 
 bool OPGraphicsManager::loadGFXMode() {
+
+	uint8_t hiddenCursorData = 0;
+	hiddenCursor = SDL_CreateCursor(&hiddenCursorData, &hiddenCursorData, 8, 1, 0, 0);
+
+	/* On the OpenPandora we need to work around an SDL assumption that
+	   returns relative mouse coordinates when you get to the screen
+	   edges using the touchscreen. The workaround is to set a blank
+	   SDL cursor and not disable it (Hackish I know).
+
+	   The root issues likes in the Windows Manager GRAB code in SDL.
+	   That is why the issue is not seen on framebuffer devices like the
+	   GP2X (there is no X window manager ;)).
+	*/
+	SDL_ShowCursor(SDL_ENABLE);
+	SDL_SetCursor(hiddenCursor);
+
 	/* FIXME: For now we just cheat and set the overlay to 640*480 not 800*480 and let SDL
 	   deal with the boarders (it saves cleaning up the overlay when the game screen is
 	   smaller than the overlay ;)
 	*/
-	_videoMode.overlayWidth = 640;
-	_videoMode.overlayHeight = 480;
+	//_videoMode.overlayWidth = 640;
+	//_videoMode.overlayHeight = 480;
 	_videoMode.fullscreen = true;
 
 	if (_videoMode.screenHeight != 200 && _videoMode.screenHeight != 400)
