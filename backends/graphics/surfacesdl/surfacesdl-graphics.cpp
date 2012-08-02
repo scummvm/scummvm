f9569ea2ff5d164795bca244d149858ab5bdbcf6
Eric Culp <eculperic@gmail.com>
2012-08-02 18:02:32 -0400

    SDL: Fix formatting

--- backends/graphics/surfacesdl/surfacesdl-graphics.cpp
+++ backends/graphics/surfacesdl/surfacesdl-graphics.cpp
@@ -260,7 +260,7 @@ bool SurfaceSdlGraphicsManager::getFeatureState(OSystem::Feature f) const {
 	}
 }
 
-static void initGraphicsModes () {
+static void initGraphicsModes() {
 	s_supportedGraphicsModes = new Common::Array<OSystem::GraphicsMode>;
 	s_supportedGraphicsModesData = new Common::Array<GraphicsModeData>;
 	const ScalerPlugin::List &plugins = ScalerMan.getPlugins();
