e19ddbe2576be48d288997b137e1df8e30be6f0a
Joel Teichroeb <joel@teichroeb.net>
2012-08-19 12:16:03 -0700

    EMI: Properly initialize some class variables

--- engines/grim/emi/sound/vimatrack.cpp
+++ engines/grim/emi/sound/vimatrack.cpp
@@ -8,12 +8,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- 
+
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- 
+
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
@@ -53,7 +53,7 @@ struct SoundDesc {
 };
 	
 bool VimaTrack::isPlaying() {
-	// FIXME: Actually clean up the data better 
+	// FIXME: Actually clean up the data better
 	// (we don't currently handle the case where it isn't asked for through isPlaying, or deleted explicitly).
 	if (_stream->endOfData()) {
 		g_system->getMixer()->stopHandle(*_handle);
@@ -202,6 +202,8 @@ VimaTrack::VimaTrack(Common::String soundName) {
 	_handle = new Audio::SoundHandle();
 	_file = NULL;
 	setSoundName(soundName);
+	_mcmp = NULL;
+	_desc = NULL;
 }
 
 VimaTrack::~VimaTrack() {
