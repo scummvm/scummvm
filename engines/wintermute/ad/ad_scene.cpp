313376c32985c8ce23f148b8a70d0222e2310080
Einar Johan Trøan Sømåen <einarjohants@gmail.com>
2012-08-31 19:54:53 +0200

    WINTERMUTE: Fix a warning I missed in the last commit.

--- engines/wintermute/ad/ad_scene.cpp
+++ engines/wintermute/ad/ad_scene.cpp
@@ -1195,8 +1195,8 @@ bool AdScene::displayRegionContent(AdRegion *region, bool display3DOnly) {
 
 //////////////////////////////////////////////////////////////////////////
 int AdScene::compareObjs(const void *obj1, const void *obj2) {
-	const AdObject *object1 = *(AdObject **)obj1;
-	const AdObject *object2 = *(AdObject **)obj2;
+	const AdObject *object1 = *(const AdObject *const *)obj1;
+	const AdObject *object2 = *(const AdObject *const *)obj2;
 
 	if (object1->_posY < object2->_posY) {
 		return -1;
