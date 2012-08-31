06caf2da98f0ae7d7776dad30be3a1dbf6da22d8
Einar Johan Trøan Sømåen <einarjohants@gmail.com>
2012-08-31 17:10:01 +0200

    WINTERMUTE: Use tabs instead of spaces for indenting dctypes.h

--- engines/wintermute/dctypes.h
+++ engines/wintermute/dctypes.h
@@ -51,176 +51,175 @@ typedef Common::Array<AnsiString> AnsiStringArray;
 
 
 enum TGameState {
-    GAME_RUNNING,
-    GAME_FROZEN,
-    GAME_SEMI_FROZEN
+	GAME_RUNNING,
+	GAME_FROZEN,
+	GAME_SEMI_FROZEN
 };
 
 
 enum TImageType {
-    IMG_PALETTED8,
-    IMG_TRUECOLOR
+	IMG_PALETTED8,
+	IMG_TRUECOLOR
 };
 
 
 enum TTextAlign {
-    TAL_LEFT = 0,
-    TAL_RIGHT,
-    TAL_CENTER,
-    NUM_TEXT_ALIGN
+	TAL_LEFT = 0,
+	TAL_RIGHT,
+	TAL_CENTER,
+	NUM_TEXT_ALIGN
 };
 
 
 enum TVerticalAlign {
-    VAL_TOP = 0,
-    VAL_CENTER,
-    VAL_BOTTOM,
-    NUM_VERTICAL_ALIGN
+	VAL_TOP = 0,
+	VAL_CENTER,
+	VAL_BOTTOM,
+	NUM_VERTICAL_ALIGN
 };
 
 
 enum TDirection {
-    DI_UP           = 0,
-    DI_UPRIGHT      = 1,
-    DI_RIGHT        = 2,
-    DI_DOWNRIGHT    = 3,
-    DI_DOWN         = 4,
-    DI_DOWNLEFT     = 5,
-    DI_LEFT         = 6,
-    DI_UPLEFT       = 7,
-    NUM_DIRECTIONS  = 8,
-    DI_NONE         = 9
+	DI_UP           = 0,
+	DI_UPRIGHT      = 1,
+	DI_RIGHT        = 2,
+	DI_DOWNRIGHT    = 3,
+	DI_DOWN         = 4,
+	DI_DOWNLEFT     = 5,
+	DI_LEFT         = 6,
+	DI_UPLEFT       = 7,
+	NUM_DIRECTIONS  = 8,
+	DI_NONE         = 9
 };
-
 
 enum TEventType {
-    EVENT_NONE           = 0,
-    EVENT_INIT           = 1,
-    EVENT_SHUTDOWN       = 2,
-    EVENT_LEFT_CLICK     = 3,
-    EVENT_RIGHT_CLICK    = 4,
-    EVENT_MIDDLE_CLICK   = 5,
-    EVENT_LEFT_DBLCLICK  = 6,
-    EVENT_PRESS          = 7,
-    EVENT_IDLE           = 8,
-    EVENT_MOUSE_OVER     = 9,
-    EVENT_LEFT_RELEASE   = 10,
-    EVENT_RIGHT_RELEASE  = 11,
-    EVENT_MIDDLE_RELEASE = 12,
-    NUM_EVENTS
+	EVENT_NONE           = 0,
+	EVENT_INIT           = 1,
+	EVENT_SHUTDOWN       = 2,
+	EVENT_LEFT_CLICK     = 3,
+	EVENT_RIGHT_CLICK    = 4,
+	EVENT_MIDDLE_CLICK   = 5,
+	EVENT_LEFT_DBLCLICK  = 6,
+	EVENT_PRESS          = 7,
+	EVENT_IDLE           = 8,
+	EVENT_MOUSE_OVER     = 9,
+	EVENT_LEFT_RELEASE   = 10,
+	EVENT_RIGHT_RELEASE  = 11,
+	EVENT_MIDDLE_RELEASE = 12,
+	NUM_EVENTS
 };
 
-
 enum TUIObjectType {
-    UI_UNKNOWN,
-    UI_BUTTON,
-    UI_WINDOW,
-    UI_STATIC,
-    UI_EDIT,
-    UI_HTML,
-    UI_CUSTOM
+	UI_UNKNOWN,
+	UI_BUTTON,
+	UI_WINDOW,
+	UI_STATIC,
+	UI_EDIT,
+	UI_HTML,
+	UI_CUSTOM
 };
 
 
 enum TRendererState {
-    RSTATE_3D,
-    RSTATE_2D,
-    RSTATE_LINES,
-    RSTATE_NONE
+	RSTATE_3D,
+	RSTATE_2D,
+	RSTATE_LINES,
+	RSTATE_NONE
 };
 
 
 enum TDynamicConstructor {
-    DYNAMIC_CONSTRUCTOR
+	DYNAMIC_CONSTRUCTOR
 };
 
 
 enum TVideoMode {
-    VIDEO_WINDOW,
-    VIDEO_FULLSCREEN,
-    VIDEO_ANY
+	VIDEO_WINDOW,
+	VIDEO_FULLSCREEN,
+	VIDEO_ANY
 };
 
 
 enum TVideoPlayback {
-    VID_PLAY_POS        = 0,
-    VID_PLAY_STRETCH    = 1,
-    VID_PLAY_CENTER     = 2
+	VID_PLAY_POS        = 0,
+	VID_PLAY_STRETCH    = 1,
+	VID_PLAY_CENTER     = 2
 };
 
 
 enum TMouseEvent {
-    MOUSE_CLICK,
-    MOUSE_RELEASE,
-    MOUSE_DBLCLICK
+	MOUSE_CLICK,
+	MOUSE_RELEASE,
+	MOUSE_DBLCLICK
 };
 
 
 enum TMouseButton {
-    MOUSE_BUTTON_LEFT,
-    MOUSE_BUTTON_RIGHT,
-    MOUSE_BUTTON_MIDDLE
+	MOUSE_BUTTON_LEFT,
+	MOUSE_BUTTON_RIGHT,
+	MOUSE_BUTTON_MIDDLE
 };
 
 
 enum TTransMgrState {
-    TRANS_MGR_RUNNING, TRANS_MGR_READY
+	TRANS_MGR_RUNNING,
+	TRANS_MGR_READY
 };
 
 
 enum TTransitionType {
-    TRANSITION_NONE     = 0,
-    TRANSITION_FADE_OUT = 1,
-    TRANSITION_FADE_IN  = 2,
-    NUM_TRANSITION_TYPES
+	TRANSITION_NONE     = 0,
+	TRANSITION_FADE_OUT = 1,
+	TRANSITION_FADE_IN  = 2,
+	NUM_TRANSITION_TYPES
 };
 
 
 enum TWindowMode {
-    WINDOW_NORMAL,
-    WINDOW_EXCLUSIVE,
-    WINDOW_SYSTEM_EXCLUSIVE
+	WINDOW_NORMAL,
+	WINDOW_EXCLUSIVE,
+	WINDOW_SYSTEM_EXCLUSIVE
 };
 
 enum TSFXType {
-    SFX_NONE,
-    SFX_ECHO,
-    SFX_REVERB
+	SFX_NONE,
+	SFX_ECHO,
+	SFX_REVERB
 };
 
 
 enum TSpriteCacheType {
-    CACHE_ALL,
-    CACHE_HALF
+	CACHE_ALL,
+	CACHE_HALF
 };
 
 enum TTextEncoding {
-    TEXT_ANSI = 0,
-    TEXT_UTF8 = 1,
-    NUM_TEXT_ENCODINGS
+	TEXT_ANSI = 0,
+	TEXT_UTF8 = 1,
+	NUM_TEXT_ENCODINGS
 };
 
 enum TSpriteBlendMode {
-    BLEND_UNKNOWN       = -1,
-    BLEND_NORMAL        = 0,
-    BLEND_ADDITIVE      = 1,
-    BLEND_SUBTRACTIVE   = 2,
-    NUM_BLEND_MODES
+	BLEND_UNKNOWN       = -1,
+	BLEND_NORMAL        = 0,
+	BLEND_ADDITIVE      = 1,
+	BLEND_SUBTRACTIVE   = 2,
+	NUM_BLEND_MODES
 };
 
 enum TTTSType {
-    TTS_CAPTION = 0,
-    TTS_TALK,
-    TTS_KEYPRESS
+	TTS_CAPTION = 0,
+	TTS_TALK,
+	TTS_KEYPRESS
 };
 
 enum TShadowType {
-    SHADOW_NONE     = 0,
-    SHADOW_SIMPLE   = 1,
-    SHADOW_FLAT     = 2,
-    SHADOW_STENCIL  = 3
+	SHADOW_NONE     = 0,
+	SHADOW_SIMPLE   = 1,
+	SHADOW_FLAT     = 2,
+	SHADOW_STENCIL  = 3
 };
 
 } // end of namespace Wintermute
 
-#endif // DCTYPES_H
+#endif
