c6c4bc7566ff1ddbfd352461ec2f021eb6e1eb1f
Pawel Kolodziejski <aquadran@users.ourceforge.net>
2012-09-08 21:09:03 +0200

    ALL: sync with scummvm

--- devtools/create_project/config.h
+++ devtools/create_project/config.h
@@ -28,6 +28,9 @@
 #define LIBS_DEFINE "RESIDUALVM_LIBS"     // Name of the include environment variable
 #define REVISION_DEFINE "SCUMMVM_INTERNAL_REVISION"
 
+#define ENABLE_LANGUAGE_EXTENSIONS ""    // Comma separated list of projects that need language extensions
+#define DISABLE_EDIT_AND_CONTINUE "grim,myst3"     // Comma separated list of projects that need Edit&Continue to be disabled for co-routine support (the main project is automatically added)
+
 #define ADDITIONAL_LIBRARY "glu32"
 #define NEEDS_RTTI 0
 
