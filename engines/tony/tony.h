b1844253d4e119899e1f019a24c2e75c15830ec9
Strangerke <strangerke@scummvm.org>
2012-09-26 23:19:16 +0200

    TONY: In French version, fix 'ê' in hotspot names by replacing it by 'e', like for other accentuated characters

--- engines/tony/tony.h
+++ engines/tony/tony.h
@@ -71,7 +71,7 @@ struct TonyGameDescription;
 
 #define MAX_SFX_CHANNELS    32
 #define TONY_DAT_VER_MAJ 0
-#define TONY_DAT_VER_MIN 1
+#define TONY_DAT_VER_MIN 2
 
 struct VoiceHeader {
 	int _offset;
