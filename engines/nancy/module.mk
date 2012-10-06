c2fe5c011afc0f0dc8f413bf5ba539e61bc7ae03
Strangerke <strangerke@scummvm.org>
2012-10-06 16:26:16 +0200

    NANCY: Add skeleton engine

---
+++ engines/nancy/module.mk
@@ -0,0 +1,14 @@
+MODULE := engines/nancy
+
+MODULE_OBJS = \
+  console.o \
+  nancy.o \
+  detection.o
+
+# This module can be built as a plugin
+ifeq ($(ENABLE_NANCY), DYNAMIC_PLUGIN)
+PLUGIN := 1
+endif
+
+# Include common rules
+include $(srcdir)/rules.mk
