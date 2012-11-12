01d14a013d3972f19d341bd975ba7f6b4279e351
Dries Harnie <dries@harnie.be>
2012-11-12 23:03:54 +0100

    EMI: Move PoolSound to separate file

---
+++ engines/grim/emi/poolsound.cpp
@@ -0,0 +1,51 @@
+/* ResidualVM - A 3D game interpreter
+ *
+ * ResidualVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ *
+ */
+
+#include "common/str.h"
+
+#include "engines/grim/emi/poolsound.h"
+#include "engines/grim/resource.h"
+
+namespace Grim {
+
+	PoolSound::PoolSound() {
+		track = NULL;
+	}
+
+	PoolSound::PoolSound(const Common::String &filename) {
+		openFile(filename);
+	}
+
+	// Called when the engine restarts or Lua code calls FreeSound
+	PoolSound::~PoolSound() {
+		delete track;
+	}
+
+	void PoolSound::openFile(const Common::String &filename) {
+		track = new AIFFTrack(Audio::Mixer::kSFXSoundType, DisposeAfterUse::NO);
+		Common::SeekableReadStream *stream = g_resourceloader->openNewStreamFile(filename);
+		if (!stream)
+			return;
+		track->openSound(filename, stream);
+	}
+
+}
---
+++ engines/grim/emi/poolsound.h
@@ -0,0 +1,44 @@
+/* ResidualVM - A 3D game interpreter
+ *
+ * ResidualVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ *
+ */
+
+#ifndef GRIM_POOLSOUND_H
+#define GRIM_POOLSOUND_H
+
+#include "engines/grim/pool.h"
+#include "engines/grim/emi/sound/aifftrack.h"
+
+
+namespace Grim {
+	class PoolSound : public PoolObject<PoolSound>{
+	public:
+		PoolSound();
+		PoolSound(const Common::String &filename);
+		~PoolSound();
+
+		void openFile(const Common::String &filename);
+
+		static int32 getStaticTag() { return MKTAG('A', 'I', 'F', 'F'); }
+		AIFFTrack *track;
+	};
+}
+
+#endif
