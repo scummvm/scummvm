40afe9d03711830a209a1cc3bd3226ab0d6d2fc1
D G Turner <digitall@scummvm.org>
2012-11-18 02:42:24 +0000

    BUILD: Fix missing directory creation in aos4dist target.

--- ports.mk
+++ ports.mk
@@ -305,6 +305,8 @@ endif
 # Special target to create an AmigaOS snapshot installation
 aos4dist: $(EXECUTABLE)
 	mkdir -p $(AOS4PATH)
+	mkdir -p $(AOS4PATH)/themes
+	mkdir -p $(AOS4PATH)/extras
 	$(STRIP) $(EXECUTABLE) -o $(AOS4PATH)/$(EXECUTABLE)
 	cp ${srcdir}/icons/scummvm.info $(AOS4PATH)/$(EXECUTABLE).info
 	cp $(DIST_FILES_THEMES) $(AOS4PATH)/themes/
