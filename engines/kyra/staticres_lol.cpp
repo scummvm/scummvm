scummvm/scummvm

commit 93e69aa4da0558b05fc235684355ed38eed9863d

KYRA: clean up audio resource handling
(this was really ugly, now it's somewhat less ugly)

engines/kyra/staticres_lol.cpp
@@ -24,6 +24,7 @@
 #include "kyra/lol.h"
 #include "kyra/screen_lol.h"
 #include "kyra/gui_lol.h"
+#include "kyra/sound_intern.h"
 
 #ifdef ENABLE_LOL
 
@@ -213,35 +214,16 @@ void StaticResource::freeButtonDefs(void *&ptr, int &size) {
 }
 
 void LoLEngine::initStaticResource() {
-	// assign music resource data
-	static const char *const pcMusicFileListIntro[] = { "LOREINTR" };
-	static const char *const pcMusicFileListFinale[] = { "LOREFINL" };
-	static const char *const pcMusicFileListIngame[] = { "LORE%02d%c" };
-	static const char *const pcMusicFileListDemo[] = { "LOREDEMO" };
-
-	static const char *const pc98MusicFileListIntro[] = { 0, "lore84.86", "lore82.86", 0, 0, 0, "lore83.86", "lore81.86" };
-	static const char *const pc98MusicFileListFinale[] = { 0, 0, "lore85.86", "lore86.86", "lore87.86" };
-	static const char *const pc98MusicFileListIngame[] = { "lore%02d.86" };
-
-	memset(_soundData, 0, sizeof(_soundData));
-
+	// assign music resource data (not required for the PC version, resource loading is implemented differently there)
 	if (_flags.isDemo) {
-		_soundData[0].fileList = pcMusicFileListDemo;
-		_soundData[0].fileListLen = ARRAYSIZE(pcMusicFileListDemo);
-	} else if (_flags.platform == Common::kPlatformPC) {
-		_soundData[0].fileList = pcMusicFileListIntro;
-		_soundData[0].fileListLen = ARRAYSIZE(pcMusicFileListIntro);
-		_soundData[1].fileList = pcMusicFileListIngame;
-		_soundData[1].fileListLen = ARRAYSIZE(pcMusicFileListIngame);
-		_soundData[2].fileList = pcMusicFileListFinale;
-		_soundData[2].fileListLen = ARRAYSIZE(pcMusicFileListFinale);
+		static const char *const file[] = { "LOREDEMO" };
+		_sound->initAudioResourceInfo(kMusicIntro, &SoundResourceInfo_TownsPC98V2(file, ARRAYSIZE(file), 0, 0, 0));
 	} else if (_flags.platform == Common::kPlatformPC98) {
-		_soundData[0].fileList = pc98MusicFileListIntro;
-		_soundData[0].fileListLen = ARRAYSIZE(pc98MusicFileListIntro);
-		_soundData[1].fileList = pc98MusicFileListIngame;
-		_soundData[1].fileListLen = ARRAYSIZE(pc98MusicFileListIngame);
-		_soundData[2].fileList = pc98MusicFileListFinale;
-		_soundData[2].fileListLen = ARRAYSIZE(pc98MusicFileListFinale);
+		static const char *const fileListIntro[] = { 0, "lore84.86", "lore82.86", 0, 0, 0, "lore83.86", "lore81.86" };
+		static const char *const fileListFinale[] = { 0, 0, "lore85.86", "lore86.86", "lore87.86" };
+		_sound->initAudioResourceInfo(kMusicIntro, &SoundResourceInfo_TownsPC98V2(fileListIntro, ARRAYSIZE(fileListIntro), 0, 0, 0));
+		_sound->initAudioResourceInfo(kMusicIngame, &SoundResourceInfo_TownsPC98V2(0, 0, "lore%02d.86", 0, 0));
+		_sound->initAudioResourceInfo(kMusicFinale, &SoundResourceInfo_TownsPC98V2(fileListFinale, ARRAYSIZE(fileListFinale), 0, 0, 0));
 	}
 
 	if (_flags.isDemo)

engines/kyra/kyra_hof.cpp
@@ -274,7 +274,7 @@ Common::Error KyraEngine_HoF::go() {
 }
 
 void KyraEngine_HoF::startup() {
-	_sound->setSoundList(&_soundData[kMusicIngame]);
+	_sound->selectAudioResourceSet(kMusicIngame);
 	// The track map is exactly the same
 	// for FM-TOWNS and DOS
 	_trackMap = _dosTrackMap;

engines/kyra/kyra_hof.h
@@ -57,7 +57,6 @@ public:
 	GUI *gui() const { return _gui; }
 	virtual TextDisplayer *text() { return _text; }
 	int language() const { return _lang; }
-	const AudioDataStruct *soundData(int index) { return &_soundData[index]; }
 
 protected:
 	static const EngineDesc _hofEngineDesc;
@@ -86,8 +85,6 @@ protected:
 	static const int _gmSfxMapSize;
 	static const int8 _pcSpkSfxMap[];
 	static const int _pcSpkSfxMapSize;
-
-	AudioDataStruct _soundData[3];
 
 protected:
 	// game initialization

engines/kyra/kyra_lok.cpp
@@ -98,8 +98,6 @@ KyraEngine_LoK::KyraEngine_LoK(OSystem *system, const GameFlags &flags)
 
 	_malcolmFrame = 0;
 	_malcolmTimer1 = _malcolmTimer2 = 0;
-
-	_soundFiles = 0;
 }
 
 KyraEngine_LoK::~KyraEngine_LoK() {
@@ -122,8 +120,6 @@ KyraEngine_LoK::~KyraEngine_LoK() {
 	delete _sprites;
 	delete _animator;
 	delete _seq;
-
-	delete[] _soundFiles;
 
 	delete[] _characterList;
 
@@ -194,7 +190,7 @@ Common::Error KyraEngine_LoK::init() {
 
 	initStaticResource();
 
-	_sound->setSoundList(&_soundData[kMusicIntro]);
+	_sound->selectAudioResourceSet(kMusicIntro);
 
 	if (_flags.platform == Common::kPlatformAmiga) {
 		_trackMap = _amigaTrackMap;
@@ -349,7 +345,7 @@ void KyraEngine_LoK::startup() {
 	static const uint8 colorMap[] = { 0, 0, 0, 0, 12, 12, 12, 0, 0, 0, 0, 0 };
 	_screen->setTextColorMap(colorMap);
 
-	_sound->setSoundList(&_soundData[kMusicIngame]);
+	_sound->selectAudioResourceSet(kMusicIngame);
 	if (_flags.platform == Common::kPlatformPC98)
 		_sound->loadSoundFile("SE.DAT");
 	else

engines/kyra/kyra_lok.h
@@ -134,8 +134,6 @@ public:
 	const uint8 * const *palTable1() { return &_specialPalettes[0]; }
 	const uint8 * const *palTable2() { return &_specialPalettes[29]; }
 
-	const AudioDataStruct *soundData(int index) { return &_soundData[index]; }
-
 protected:
 	virtual Common::Error go();
 	virtual Common::Error init();
@@ -644,14 +642,6 @@ protected:
 	const uint8 *_amuleteAnim;
 
 	const uint8 * const *_specialPalettes;
-
-	const char * const *_soundFiles;
-	int _soundFilesSize;
-	const char * const *_soundFilesIntro;
-	int _soundFilesIntroSize;
-	const int32 *_cdaTrackTable;
-	int _cdaTrackTableSize;
-	AudioDataStruct _soundData[3];
 
 	// positions of the inventory
 	static const uint16 _itemPosX[];

engines/kyra/kyra_v1.h
@@ -140,14 +140,6 @@ enum {
 	GI_EOB2 = 6
 };
 
-struct AudioDataStruct {
-	const char *const *fileList;
-	int fileListLen;
-	const void *cdaTracks;
-	int cdaNumTracks;
-	int extraOffset;
-};
-
 // TODO: this is just the start of makeing the debug output of the kyra engine a bit more useable
 // in the future we maybe merge some flags  and/or create new ones
 enum DebugLevels {
@@ -164,7 +156,7 @@ enum DebugLevels {
 	kDebugLevelTimer       = 1 << 10  ///< debug level for "TimerManager" functions
 };
 
-enum MusicDataID {
+enum AudioResourceSet {
 	kMusicIntro = 0,
 	kMusicIngame,
 	kMusicFinale
@@ -242,8 +234,6 @@ public:
 	virtual void snd_playVoiceFile(int id) = 0;
 	virtual bool snd_voiceIsPlaying();
 	virtual void snd_stopVoice();
-
-	virtual const AudioDataStruct *soundData(int index) { return 0; }
 
 	// delay functionallity
 	virtual void delayUntil(uint32 timestamp, bool updateGameTimers = false, bool update = false, bool isMainLoop = false);

engines/kyra/lol.cpp
@@ -534,7 +534,7 @@ Common::Error LoLEngine::go() {
 	if (_flags.platform == Common::kPlatformPC98)
 		_sound->loadSoundFile("sound.dat");
 
-	_sound->setSoundList(&_soundData[kMusicIngame]);
+	_sound->selectAudioResourceSet(kMusicIngame);
 	if (_flags.platform != Common::kPlatformPC)
 		_sound->loadSoundFile(0);
 

engines/kyra/lol.h
@@ -275,8 +275,6 @@ public:
 	Screen *screen();
 	GUI *gui() const;
 
-	const AudioDataStruct *soundData(int index) { return &_soundData[index]; }
-
 private:
 	Screen_LoL *_screen;
 	GUI_LoL *_gui;
@@ -471,8 +469,6 @@ private:
 	int _ingameMT32SoundIndexSize;
 	const uint8 *_ingamePCSpeakerSoundIndex;
 	int _ingamePCSpeakerSoundIndexSize;
-
-	AudioDataStruct _soundData[3];
 
 	// gui
 	void gui_drawPlayField();

commit 415334fbe51b6c1332db11a113069c62c1141608

KYRA: Fix bug #3589441 "KYRA: LOL DOS is broken".
In fact it affected all DOS versions of the game.
This is a regression from 93e69aa4da0558b05fc235684355ed38eed9863d.

engines/kyra/staticres_lol.cpp
@@ -214,11 +214,19 @@ void StaticResource::freeButtonDefs(void *&ptr, int &size) {
 }
 
 void LoLEngine::initStaticResource() {
-	// assign music resource data (not required for the PC version, resource loading is implemented differently there)
+	// assign music resource data.
 	if (_flags.isDemo) {
 		static const char *const file[] = { "LOREDEMO" };
 		SoundResourceInfo_TownsPC98V2 resInfoDemo(file, ARRAYSIZE(file), 0, 0, 0);
 		_sound->initAudioResourceInfo(kMusicIntro, &resInfoDemo);
+	} else if (_flags.platform == Common::kPlatformPC) {
+		static const char *const intro[] = { "LOREINTR" };
+		static const char *const finale[] = { "LOREFINL" };
+		SoundResourceInfo_PC resInfoIntro(intro, ARRAYSIZE(intro));
+		SoundResourceInfo_PC resInfoFinale(finale, ARRAYSIZE(finale));
+		_sound->initAudioResourceInfo(kMusicIntro, &resInfoIntro);
+		// In game music file handling is different, thus does not need a file list.
+		_sound->initAudioResourceInfo(kMusicFinale, &resInfoFinale);
 	} else if (_flags.platform == Common::kPlatformPC98) {
 		static const char *const fileListIntro[] = { 0, "lore84.86", "lore82.86", 0, 0, 0, "lore83.86", "lore81.86" };
 		static const char *const fileListFinale[] = { 0, 0, "lore85.86", "lore86.86", "lore87.86" };