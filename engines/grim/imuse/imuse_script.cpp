883886d7aac5a55c62a493daaf0ce51a1d6b6e70
Giulio Camuffo <giuliocamuffo@gmail.com>
2012-12-21 16:26:12 +0100

    GRIM: Don't use int where floats are due.

--- engines/grim/imuse/imuse_script.cpp
+++ engines/grim/imuse/imuse_script.cpp
@@ -106,7 +106,7 @@ int32 Imuse::getPosIn16msTicks(const char *soundName) {
 		return false;
 	}
 
-	int32 pos = (62.5 / 60) * (5 * (getTrack->dataOffset + getTrack->regionOffset)) / (getTrack->feedSize / 12); // 16ms is 62.5 Hz
+	int32 pos = (62.5 / 60.0) * (5 * (getTrack->dataOffset + getTrack->regionOffset)) / (getTrack->feedSize / 12); // 16ms is 62.5 Hz
 	return pos;
 }
 
