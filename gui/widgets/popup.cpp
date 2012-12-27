61b25b970703a3c062846249676921fec2e165d6
Torbjörn Andersson <eriktorbjorn@users.sourceforge.net>
2012-12-27 02:18:49 +0100

    GUI: Redraw the popup widget when a the selection changes
    Otherwise, it will look like the value hasn't changed until the
    widget is redrawn for other reasons, e.g. by mouse-over.
    
    Incidentally, does anyone know why handleMouseDown() calls
    sendCommand() when the selection changes, while handleMouseWheel()
    does not?

--- gui/widgets/popup.cpp
+++ gui/widgets/popup.cpp
@@ -388,6 +388,7 @@ void PopUpWidget::handleMouseDown(int x, int y, int button, int clickCount) {
 		if (newSel != -1 && _selectedItem != newSel) {
 			_selectedItem = newSel;
 			sendCommand(kPopUpItemSelectedCmd, _entries[_selectedItem].tag);
+			draw();
 		}
 	}
 }
