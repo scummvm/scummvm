3f8e612ecb507243dacf14207f69755aca03eb0e
Tomas Jakobsson <tompsson@hotmail.com>
2012-12-29 22:52:43 +0100

    GRAPHICS: Remove unused ILBMDecoder from graphics/iff

--- graphics/iff.h
+++ graphics/iff.h
@@ -54,53 +54,6 @@ struct BMHD {
 };
 
 
-struct ILBMDecoder {
-	/**
-	 * ILBM header data, necessary for loadBitmap()
-	 */
-	Graphics::BMHD	_header;
-
-	/**
-	 * Available decoding modes for loadBitmap().
-	 */
-	enum {
-		ILBM_UNPACK_PLANES = 0xFF,		///< Decode all bitplanes, and map 1 pixel to 1 byte.
-		ILBM_PACK_PLANES   = 0x100,		///< Request unpacking, used as a mask with below options.
-
-		ILBM_1_PLANES      = 1,									///< Decode only the first bitplane, don't pack.
-		ILBM_1_PACK_PLANES = ILBM_1_PLANES | ILBM_PACK_PLANES, 	///< Decode only the first bitplane, pack 8 pixels in 1 byte.
-		ILBM_2_PLANES      = 2,									///< Decode first 2 bitplanes, don't pack.
-		ILBM_2_PACK_PLANES = ILBM_2_PLANES | ILBM_PACK_PLANES,	///< Decode first 2 bitplanes, pack 4 pixels in 1 byte.
-		ILBM_3_PLANES      = 3,									///< Decode first 3 bitplanes, don't pack.
-		ILBM_4_PLANES      = 4,									///< Decode first 4 bitplanes, don't pack.
-		ILBM_4_PACK_PLANES = ILBM_4_PLANES | ILBM_PACK_PLANES,	///< Decode first 4 bitplanes, pack 2 pixels in 1 byte.
-		ILBM_5_PLANES      = 5,									///< Decode first 5 bitplanes, don't pack.
-		ILBM_8_PLANES      = 8									///< Decode all 8 bitplanes.
-	};
-
-	/**
-	 * Fills the _header member from the given stream.
-	 */
-	void loadHeader(Common::ReadStream *stream);
-
-	/**
-	 * Loads and unpacks the ILBM bitmap data from the stream into the buffer.
-	 * The functions assumes the buffer is large enough to contain all data.
-	 * The caller controls how data should be packed by choosing mode from
-	 * the enum above.
-	 */
-	void loadBitmap(uint32 mode, byte *buffer, Common::ReadStream *stream, uint32 outPitch = 0);
-
-	/**
-	 * Converts from bitplanar to chunky representation. Intended for internal
-	 * usage, but you can be (ab)use it from client code if you know what you
-	 * are doing.
-	 */
-	void planarToChunky(byte *out, uint32 width, byte *in, uint32 planeWidth, uint32 nPlanes, bool packPlanes);
-};
-
-
-
 /**
  * Handles PBM subtype of IFF FORM files
  */
