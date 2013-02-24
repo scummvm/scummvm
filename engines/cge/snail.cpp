76bf7b7c04931c3a7304f3c004221eaf4da05b3a
Strangerke <strangerke@scummvm.org>
2013-02-24 11:21:52 +0100

    CGE: Add workaround for the dice puzzle for the devices not using a ALT key

--- engines/cge/snail.cpp
+++ engines/cge/snail.cpp
@@ -493,7 +493,15 @@ void CGEEngine::snGame(Sprite *spr, int num) {
 		_sprK2->step(newRandom(6));
 		_sprK3->step(newRandom(6));
 
+		// check the ALT key as it's the solution of the puzzle
+		// the test has been restricted to some specific OSes 
+		// in order to avoid some obvious issues (like Android, iOS, NDS, N64...)
+		// Not perfect, but at least better than nothing.
+#if defined(WIN32) || defined(UNIX) || defined(MACOSX) || defined(MOTOEZX) || defined(LINUPY) || defined(LINUXMOTO_SDL)
 		if (spr->_ref == 1 && _keyboard->_keyAlt) {
+#else
+		if (spr->_ref == 1 && _gameCase2Cpt > 1) {
+#endif
 			_sprK1->step(5);
 			_sprK2->step(5);
 			_sprK3->step(5);
