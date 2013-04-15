172aed04419c9d6a4118bbb315bd7d3fe822c00b
Sven Hesse <drmccoy@users.sourceforge.net>
2013-04-15 18:22:18 +0200

    GOB: Remove unused member variables in SaveLoad_Fascination

--- engines/gob/save/saveload.h
+++ engines/gob/save/saveload.h
@@ -247,9 +247,6 @@ protected:
 
 	static SaveFile _saveFiles[];
 
-	NotesHandler *_notesHandler;
-	TempSpriteHandler *_tempSpriteHandler;
-
 	SaveHandler *getHandler(const char *fileName) const;
 	const char *getDescription(const char *fileName) const;
 
