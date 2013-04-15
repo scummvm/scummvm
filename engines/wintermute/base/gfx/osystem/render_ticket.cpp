92dd483e9ed5f252563db26fa44a401623c7df31
Einar Johan Trøan Sømåen <einarjohants@gmail.com>
2013-04-15 18:54:43 +0200

    WINTERMUTE: Fix comparison in renderTicket.

--- engines/wintermute/base/gfx/osystem/render_ticket.cpp
+++ engines/wintermute/base/gfx/osystem/render_ticket.cpp
@@ -72,7 +72,7 @@ RenderTicket::~RenderTicket() {
 
 bool RenderTicket::operator==(RenderTicket &t) {
 	if ((t._owner != _owner) ||
-		(t._batchNum != t._batchNum) ||
+		(t._batchNum != _batchNum) ||
 		(t._hasAlpha != _hasAlpha) ||
 		(t._mirror != _mirror) ||
 		(t._colorMod != _colorMod) ||
