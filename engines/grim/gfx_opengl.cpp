960f48f0f7fb2c6e2bb6b079d443f69923079bc0
Einar Johan Trøan Sømåen <einarjohants@gmail.com>
2013-04-21 15:09:39 +0200

    GRIM: Initialize all members in GfxOpenGL.

--- engines/grim/gfx_opengl.cpp
+++ engines/grim/gfx_opengl.cpp
@@ -92,7 +92,15 @@ static char dimFragSrc[] =
 	MOV result.color.b, sum;\n\
 	END\n";
 
-GfxOpenGL::GfxOpenGL() {
+GfxOpenGL::GfxOpenGL() : _smushNumTex(0),
+						_smushTexIds(NULL),
+						_smushWidth(0),
+						_smushHeight(0),
+						_useDepthShader(false),
+						_fragmentProgram(0),
+						_useDimShader(0),
+						_dimFragProgram(0),
+						_maxLights(0) {
 	g_driver = this;
 	_storedDisplay = NULL;
 	_emergFont = 0;
