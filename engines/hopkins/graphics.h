4a85d507bbe4ae60cba708cf58544e8bea52337d
Paul Gilbert <dreammaster@scummvm.org>
2013-05-10 17:54:17 +1000

    HOPKINS: Added a zones debugger command

--- engines/hopkins/graphics.h
+++ engines/hopkins/graphics.h
@@ -118,12 +118,13 @@ public:
 	Common::Array<Common::Rect> _dirtyRects;
 	Common::Array<Common::Rect> _refreshRects;
 	bool _showDirtyRects;
+	bool _showZones;
 
 	byte *_palettePixels;
 public:
 	GraphicsManager(HopkinsEngine *vm);
 	~GraphicsManager();
-
+	
 	void clearPalette();
 	void clearScreen();
 	void clearVesaScreen();
@@ -135,6 +136,8 @@ public:
 	void addRectToArray(Common::Array<Common::Rect> &rects, const Common::Rect &newRect);
 	void displayDirtyRects();
 	void displayRefreshRects();
+	void displayZones();
+	void displayDebugRect(Graphics::Surface *surface, const Common::Rect &srcRect);
 	void copySurface(const byte *surface, int x1, int y1, int width, int height, byte *destSurface, int destX, int destY);
 	void loadImage(const Common::String &file);
 	void loadVgaImage(const Common::String &file);
