f00ee606d313ecbd03b61978361363683fb8d8b3
Matthew Hoops <clone2727@gmail.com>
2013-06-08 23:59:03 -0400

    ENGINES: Add stub Buried in Time engine

---
+++ engines/buried/buried.cpp
@@ -0,0 +1,42 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+#include "common/scummsys.h"
+#include "common/error.h"
+#include "common/system.h"
+#include "common/textconsole.h"
+
+#include "buried/buried.h"
+
+namespace Buried {
+
+BuriedEngine::BuriedEngine(OSystem *syst, const BuriedGameDescription *gameDesc) : Engine(syst), _gameDescription(gameDesc) {
+}
+
+BuriedEngine::~BuriedEngine() {
+}
+
+Common::Error BuriedEngine::run() {
+	return Common::kNoError;
+}
+
+} // End of namespace Buried
---
+++ engines/buried/module.mk
@@ -0,0 +1,14 @@
+MODULE := engines/buried
+
+MODULE_OBJS = \
+	buried.o \
+	detection.o
+
+
+# This module can be built as a plugin
+ifeq ($(ENABLE_BURIED), DYNAMIC_PLUGIN)
+PLUGIN := 1
+endif
+
+# Include common rules
+include $(srcdir)/rules.mk
