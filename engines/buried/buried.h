946857bfa49f5c32b420061fb099a4b6c9bac3d2
Matthew Hoops <clone2727@gmail.com>
2013-06-08 23:59:03 -0400

    ENGINES: Add stub Buried in Time engine

---
+++ engines/buried/buried.h
@@ -0,0 +1,54 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+#ifndef BURIED_BURIED_H
+#define BURIED_BURIED_H
+
+#include "common/scummsys.h"
+#include "common/array.h"
+
+#include "engines/engine.h"
+
+class OSystem;
+
+namespace Buried {
+
+struct BuriedGameDescription;
+
+class BuriedEngine : public ::Engine {
+protected:
+	Common::Error run();
+
+public:
+	BuriedEngine(OSystem *syst, const BuriedGameDescription *gamedesc);
+	virtual ~BuriedEngine();
+
+	// Detection related functions
+	const BuriedGameDescription *_gameDescription;
+	bool isDemo() const;
+
+	bool hasFeature(EngineFeature f) const;
+};
+
+} // End of namespace Buried
+
+#endif
