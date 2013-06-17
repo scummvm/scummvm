a183d560c1bd2c234b9bc7a8aa32af9b0f0d6182
uruk <koppirnyo@gmail.com>
2013-06-17 13:42:47 +0200

    AVALANCHE: Add standard header.

--- engines/avalanche/enhanced.h
+++ engines/avalanche/enhanced.h
@@ -1,3 +1,7 @@
+/*
+ * This code is based on the original source code of Lord Avalot d'Argent version 1.3.
+ * Copyright (c) 1994-1995 Mike, Mark and Thomas Thurman.
+ */
 #ifndef __enhanced_h__
 #define __enhanced_h__
 
--- engines/avalanche/fileunit.h
+++ engines/avalanche/fileunit.h
@@ -1,3 +1,7 @@
+/*
+ * This code is based on the original source code of Lord Avalot d'Argent version 1.3.
+ * Copyright (c) 1994-1995 Mike, Mark and Thomas Thurman.
+ */
 #ifndef __fileunit_h__
 #define __fileunit_h__
 
