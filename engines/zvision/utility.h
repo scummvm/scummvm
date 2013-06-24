8243263b33fe7aaf4e1144104f1189cd03636ff0
richiesams <adastley@gmail.com>
2013-06-24 00:46:27 -0500

    ZVISION: Add documentation to utility functions and add a glorified trim function

--- engines/zvision/utility.h
+++ engines/zvision/utility.h
@@ -29,7 +29,14 @@
 
 namespace ZVision {
 
-void WriteFileContentsToFile(Common::String sourceFile, Common::String destFile) {
+/**
+ * Opens the sourceFile utilizing Common::File (aka SearchMan) and writes the
+ * contents to destFile. destFile is created in the working directory
+ *
+ * @param sourceFile	The 'file' you want the contents of
+ * @param destFile		The name of the file where the content will be written to
+ */
+void writeFileContentsToFile(Common::String sourceFile, Common::String destFile) {
 	Common::File f;
 	f.open(sourceFile);
 	byte* buffer = new byte[f.size()];
@@ -43,6 +50,23 @@ void WriteFileContentsToFile(Common::String sourceFile, Common::String destFile)
 	dumpFile.close();
 
 	delete[] buffer;
+}
+
+/**
+ * Removes any line comments using '#' as a sequence start.
+ * Then removes any trailing and leading 'whitespace'.
+ * Uses isspace() to determine what is whitespace and what is not.
+ *
+ * @param string	The string to modify. It is modified in place
+ */
+void trimCommentsAndWhiteSpace(Common::String &string) {
+	for (int i = string.size(); i >= 0; --i) {
+		if (string[i] == '#') {
+			string.erase(i);
+		}
+	}
+
+	string.trim();
 }
 
 } // End namespace ZVision
