a5e969087b419710276ae08cac0565e144827292
Filippos Karapetis <bluegr@gmail.com>
2013-07-05 02:57:21 +0300

    CREATE_PROJECT: Add the event recorder to the feature flags

--- devtools/create_project/create_project.cpp
+++ devtools/create_project/create_project.cpp
@@ -809,19 +809,20 @@ const Feature s_features[] = {
 	{"freetype",   "USE_FREETYPE2", "freetype",         true, "FreeType support" },
 
 	// Feature flags
-	{        "bink",        "USE_BINK",         "", true, "Bink video support" },
-	{     "scalers",     "USE_SCALERS",         "", true, "Scalers" },
-	{   "hqscalers",  "USE_HQ_SCALERS",         "", true, "HQ scalers" },
-	{       "16bit",   "USE_RGB_COLOR",         "", true, "16bit color support" },
-	{     "mt32emu",     "USE_MT32EMU",         "", true, "integrated MT-32 emulator" },
-	{        "nasm",        "USE_NASM",         "", true, "IA-32 assembly support" }, // This feature is special in the regard, that it needs additional handling.
-	{      "opengl",      "USE_OPENGL", "opengl32", true, "OpenGL support" },
-	{     "taskbar",     "USE_TASKBAR",         "", true, "Taskbar integration support" },
-	{ "translation", "USE_TRANSLATION",         "", true, "Translation support" },
-	{      "vkeybd",   "ENABLE_VKEYBD",         "", false, "Virtual keyboard support"},
-	{   "keymapper","ENABLE_KEYMAPPER",         "", false, "Keymapper support"},
-	{  "langdetect",  "USE_DETECTLANG",         "", true, "System language detection support" } // This feature actually depends on "translation", there
-	                                                                                            // is just no current way of properly detecting this...
+	{            "bink",             "USE_BINK",         "", true,  "Bink video support" },
+	{         "scalers",          "USE_SCALERS",         "", true,  "Scalers" },
+	{       "hqscalers",       "USE_HQ_SCALERS",         "", true,  "HQ scalers" },
+	{           "16bit",        "USE_RGB_COLOR",         "", true,  "16bit color support" },
+	{         "mt32emu",          "USE_MT32EMU",         "", true,  "integrated MT-32 emulator" },
+	{            "nasm",             "USE_NASM",         "", true,  "IA-32 assembly support" }, // This feature is special in the regard, that it needs additional handling.
+	{          "opengl",           "USE_OPENGL", "opengl32", true,  "OpenGL support" },
+	{         "taskbar",          "USE_TASKBAR",         "", true,  "Taskbar integration support" },
+	{     "translation",      "USE_TRANSLATION",         "", true,  "Translation support" },
+	{          "vkeybd",        "ENABLE_VKEYBD",         "", false, "Virtual keyboard support"},
+	{       "keymapper",     "ENABLE_KEYMAPPER",         "", false, "Keymapper support"},
+	{   "eventrecorder", "ENABLE_EVENTRECORDER",         "", false, "Event recorder support"},
+	{      "langdetect",       "USE_DETECTLANG",         "", true,  "System language detection support" } // This feature actually depends on "translation", there
+	                                                                                                      // is just no current way of properly detecting this...
 };
 
 const Tool s_tools[] = {
