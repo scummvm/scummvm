b7b744dedf6f795909c6901af1f511f58052d91b
uruk <koppirnyo@gmail.com>
2013-07-04 11:23:20 +0200

    AVALANCHE: Update module.mk.

--- engines/avalanche/module.mk
+++ engines/avalanche/module.mk
@@ -5,7 +5,22 @@ MODULE_OBJS = \
 	avalot.o \
 	console.o \
 	detection.o \
-	gyro2.o
+	gyro2.o \
+	enhanced2.o \
+	logger2.o \
+	pingo2.o \
+	scrolls2.o \
+	visa2.o \
+	lucerna2.o \
+	enid2.o \
+	celer2.o \
+	sequence2.o \
+	timeout2.o \
+	trip6.o \
+	acci2.o \
+	basher2.o \
+	dropdown2.o \
+	closing2.o
 
 # This module can be built as a plugin
 ifeq ($(ENABLE_AVALANCHE), DYNAMIC_PLUGIN)
