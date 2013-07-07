811ea394862a35faacd2e87c68ef57a501f67f3c
richiesams <adastley@gmail.com>
2013-07-07 12:23:12 -0500

    ZVISION: Update module.mk with current objs

--- engines/zvision/module.mk
+++ engines/zvision/module.mk
@@ -1,8 +1,20 @@
 MODULE := engines/zvision
  
 MODULE_OBJS := \
+	actions.o \
+	console.o \
 	detection.o \
+	events.o \
+	graphics.o \
+	image.o \
+	lzss_read_stream.o \
+	scr_file_handling.o \
+	script_manager.o \
+	scripts.o \
+	single_value_container.o \
+	video.o \
 	zvision.o \
+	zfs_archive.o \
 	zork_avi_decoder.o \
 	zork_raw.o
  
