29061acd4ebeef2686b2ee2ff9c97d6ee4f11e91
richiesams <adastley@gmail.com>
2013-07-11 00:44:23 -0500

    ZVISION: Add operator< to the Puzzle struct
    ScriptManager does a unique-fication of a container of Puzzles using
    a sort with some other logic. The sort uses operator<

--- engines/zvision/puzzle.h
+++ engines/zvision/puzzle.h
@@ -64,6 +64,13 @@ struct Puzzle {
 	// This has to be list of pointers because ResultAction is abstract
 	Common::List<ResultAction *> resultActions;
 	byte flags;
+
+	// Used by the ScriptManager to allow unique-ification of _referenceTable
+	// The unique-ification is done by sorting, then iterating and removing duplicates
+	// The sort uses operator<
+	const bool operator<(const Puzzle &other) const {
+		return key < other.key;
+	}
 };
 
 } // End of namespace ZVision
