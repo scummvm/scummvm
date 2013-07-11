9c7203c4b922845d63b00236a78e48e4f55bd633
richiesams <adastley@gmail.com>
2013-07-11 00:25:09 -0500

    ZVISION: Create utility method to remove duplicate entries from a Common::Array

--- engines/zvision/utility.h
+++ engines/zvision/utility.h
@@ -56,7 +56,19 @@ void trimCommentsAndWhiteSpace(Common::String *string);
  */
 void dumpEveryResultAction(const Common::String &destFile);
 
+/**
+ * Removes all duplicate entries from container.
+ *
+ * @param container        
+ * @return                
+ */
+template<class T>
+void removeDuplicateEntries(Common::Array<T> *container) {
+	Common::sort(container->front(), container->back());
 
+	for (int i = 0; i < container->size(); i++) {
+		while (container[i] == container[i +1]) {
+			container->remove_at(i + 1);
 		}
 	}
 }
