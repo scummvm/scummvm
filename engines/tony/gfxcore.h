c44d6ae60cfd1d06d2431918fb74c9ba1bfeb32e
Torbjörn Andersson <eriktorbjorn@users.sourceforge.net>
2013-07-19 21:10:50 +0200

    TONY: Fix parameter to RMGfxSourceBuffer8RLEWordAA::init()
    The method it is meant to override uses "const byte *buf" so this
    one should too. I don't know if/when it's called, but fuzzie tells
    me it should be a harmless change. Hopefully fixes CID 1003892.

--- engines/tony/gfxcore.h
+++ engines/tony/gfxcore.h
@@ -408,7 +408,7 @@ public:
 
 	// Overloaded initialization methods
 	virtual void init(Common::ReadStream &ds, int dimx, int dimy, bool bLoadPalette = false);
-	virtual int init(byte *buf, int dimx, int dimy, bool bLoadPalette = false);
+	virtual int init(const byte *buf, int dimx, int dimy, bool bLoadPalette = false);
 
 	virtual ~RMGfxSourceBuffer8RLEWordAA();
 };
