8617edea04cfb2439de54d3ce4e64f92e5bd0416
Johannes Schickel <lordhoto@scummvm.org>
2013-08-03 00:33:42 +0200

    SAGA: Prefer getBasePtr over direct Surface::pixels access.

--- engines/saga/animation.cpp
+++ engines/saga/animation.cpp
@@ -501,7 +501,7 @@ void Anim::play(uint16 animId, int vectorTime, bool playing) {
 	}
 
 	anim = getAnimation(animId);
-	displayBuffer = (byte *)_vm->_render->getBackGroundSurface()->pixels;
+	displayBuffer = (byte *)_vm->_render->getBackGroundSurface()->getBasePtr(0, 0);
 
 	if (playing) {
 		anim->state = ANIM_PLAYING;
