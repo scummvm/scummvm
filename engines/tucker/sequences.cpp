431841073185d45a945a459768fccc091cc2c2fd
Johannes Schickel <lordhoto@scummvm.org>
2013-08-03 02:44:35 +0200

    TUCKER: Take advantage of Surface::getPixels.

--- engines/tucker/sequences.cpp
+++ engines/tucker/sequences.cpp
@@ -811,7 +811,7 @@ void AnimationSequencePlayer::playIntroSeq19_20() {
 	if (surface)
 		for (int i = 0; i < kScreenWidth * kScreenHeight; ++i)
 			if (_offscreenBuffer[i] == 0)
-				_offscreenBuffer[i] = *((const byte *)surface->getBasePtr(0, 0) + i);
+				_offscreenBuffer[i] = *((const byte *)surface->getPixels() + i);
 
 	if (!framesLeft)
 		_changeToNextSequence = true;
