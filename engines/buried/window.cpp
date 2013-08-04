fdfe212467e9d7d93cb7e86716bb6970667a83d0
Matthew Hoops <clone2727@gmail.com>
2013-08-04 15:26:09 -0400

    BURIED: Update the Window code a bit
    Paint events will happen separately from the queue, since they're special anyway

--- engines/buried/window.cpp
+++ engines/buried/window.cpp
@@ -22,71 +22,92 @@
 
 #include "common/textconsole.h"
 
+#include "buried/buried.h"
+#include "buried/graphics.h"
 #include "buried/message.h"
 #include "buried/window.h"
 
 namespace Buried {
 
-void Window::dispatchMessage() {
-	if (_queue.empty())
-		return;
+Window::Window(BuriedEngine *vm) : _vm(vm) {
+	_parent = 0;
+}
 
-	Message *message = _queue.pop();
+Window::~Window() {
+	// Make sure the queue is cleaned out
+	while (!_queue.empty())
+		delete _queue.pop();
+}
 
-	switch (message->getMessageType()) {
-	case kMessageTypeEraseBackground:
-		onEraseBackground();
-		break;
-	case kMessageTypeKeyUp:
-		onKeyUp(((KeyUpMessage *)message)->getKeyState(), ((KeyUpMessage *)message)->getFlags());
-		break;
-	case kMessageTypeKeyDown:
-		onKeyDown(((KeyDownMessage *)message)->getKeyState(), ((KeyDownMessage *)message)->getFlags());
-		break;
-	case kMessageTypeTimer:
-		onTimer(((TimerMessage *)message)->getTimer());
-		break;
-	case kMessageTypeSetFocus:
-		onSetFocus(((SetFocusMessage *)message)->getWindow());
-		break;
-	case kMessageTypeKillFocus:
-		onKillFocus(((KillFocusMessage *)message)->getWindow());
-		break;
-	case kMessageTypeQueryNewPalette:
-		onQueryNewPalette();
-		break;
-	case kMessageTypePaint:
-		onPaint();
-		break;
-	case kMessageTypeLButtonUp:
-		onLButtonUp(((LButtonUpMessage *)message)->getPoint(), ((LButtonUpMessage *)message)->getFlags());
-		break;
-	case kMessageTypeLButtonDown:
-		onLButtonDown(((LButtonDownMessage *)message)->getPoint(), ((LButtonDownMessage *)message)->getFlags());
-		break;
-	case kMessageTypeLButtonDoubleClick:
-		onLButtonDoubleClick(((LButtonDoubleClickMessage *)message)->getPoint(), ((LButtonDoubleClickMessage *)message)->getFlags());
-		break;
-	case kMessageTypeMButtonUp:
-		onMButtonUp(((MButtonUpMessage *)message)->getPoint(), ((MButtonUpMessage *)message)->getFlags());
-		break;
-	case kMessageTypeRButtonUp:
-		onRButtonUp(((RButtonUpMessage *)message)->getPoint(), ((RButtonUpMessage *)message)->getFlags());
-		break;
-	case kMessageTypeRButtonDown:
-		onRButtonDown(((RButtonDownMessage *)message)->getPoint(), ((RButtonDownMessage *)message)->getFlags());
-		break;
-	case kMessageTypeSetCursor:
-		onSetCursor(((SetCursorMessage *)message)->getWindow(), ((SetCursorMessage *)message)->getCursor());
-		break;
-	case kMessageTypeEnable:
-		onEnable(((EnableMessage *)message)->getEnable());
-		break;
-	default:
-		error("Unknown message type %d", message->getMessageType());
+void Window::invalidateRect(const Common::Rect &rect, bool erase) {
+	_vm->_gfx->invalidateRect(rect, erase);
+}
+
+void Window::createChild(const Common::Rect &rect, Window *parent) {
+	_rect = rect;
+	_parent = parent;
+}
+
+Common::Rect Window::getClientRect() const {
+	return Common::Rect(_rect.width(), _rect.height());
+}
+
+void Window::dispatchAllMessages() {
+	while (!_queue.empty() && !_vm->shouldQuit()) {
+		Message *message = _queue.pop();
+
+		switch (message->getMessageType()) {
+		case kMessageTypeEraseBackground:
+			onEraseBackground();
+			break;
+		case kMessageTypeKeyUp:
+			onKeyUp(((KeyUpMessage *)message)->getKeyState(), ((KeyUpMessage *)message)->getFlags());
+			break;
+		case kMessageTypeKeyDown:
+			onKeyDown(((KeyDownMessage *)message)->getKeyState(), ((KeyDownMessage *)message)->getFlags());
+			break;
+		case kMessageTypeTimer:
+			onTimer(((TimerMessage *)message)->getTimer());
+			break;
+		case kMessageTypeSetFocus:
+			onSetFocus(((SetFocusMessage *)message)->getWindow());
+			break;
+		case kMessageTypeKillFocus:
+			onKillFocus(((KillFocusMessage *)message)->getWindow());
+			break;
+		case kMessageTypeQueryNewPalette:
+			onQueryNewPalette();
+			break;
+		case kMessageTypeLButtonUp:
+			onLButtonUp(((LButtonUpMessage *)message)->getPoint(), ((LButtonUpMessage *)message)->getFlags());
+			break;
+		case kMessageTypeLButtonDown:
+			onLButtonDown(((LButtonDownMessage *)message)->getPoint(), ((LButtonDownMessage *)message)->getFlags());
+			break;
+		case kMessageTypeLButtonDoubleClick:
+			onLButtonDoubleClick(((LButtonDoubleClickMessage *)message)->getPoint(), ((LButtonDoubleClickMessage *)message)->getFlags());
+			break;
+		case kMessageTypeMButtonUp:
+			onMButtonUp(((MButtonUpMessage *)message)->getPoint(), ((MButtonUpMessage *)message)->getFlags());
+			break;
+		case kMessageTypeRButtonUp:
+			onRButtonUp(((RButtonUpMessage *)message)->getPoint(), ((RButtonUpMessage *)message)->getFlags());
+			break;
+		case kMessageTypeRButtonDown:
+			onRButtonDown(((RButtonDownMessage *)message)->getPoint(), ((RButtonDownMessage *)message)->getFlags());
+			break;
+		case kMessageTypeSetCursor:
+			onSetCursor(((SetCursorMessage *)message)->getWindow(), ((SetCursorMessage *)message)->getCursor());
+			break;
+		case kMessageTypeEnable:
+			onEnable(((EnableMessage *)message)->getEnable());
+			break;
+		default:
+			error("Unknown message type %d", message->getMessageType());
+		}
+
+		delete message;
 	}
-
-	delete message;
 }
 
 } // End of namespace Buried
