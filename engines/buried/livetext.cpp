75ddf082711794ec72e61bc6d06150818eb9d21e
Matthew Hoops <clone2727@gmail.com>
2013-08-04 20:43:29 -0400

    BURIED: Add first stab at the LiveText window

---
+++ engines/buried/livetext.cpp
@@ -0,0 +1,143 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * Additional copyright for this file:
+ * Copyright (C) 1995 Presto Studios, Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+#include "common/str-array.h"
+#include "graphics/font.h"
+#include "graphics/surface.h"
+
+#include "buried/buried.h"
+#include "buried/graphics.h"
+#include "buried/livetext.h"
+#include "buried/resources.h"
+
+namespace Buried {
+
+LiveTextWindow::LiveTextWindow(BuriedEngine *vm, Window *parent) : Window(vm) {
+	// Initialize member variables to empty
+	_textTranslation = false;
+
+	// Create font
+	_font = _vm->_gfx->createFont(14);
+
+	// Create window
+	createChild(Common::Rect(137, 21, 447, 87), parent);
+
+	// Update the text in the window
+	updateLiveText(_vm->getString(IDS_SAVE_GAME_MESSAGE), false);
+}
+
+LiveTextWindow::~LiveTextWindow() {
+	delete _font;
+}
+
+bool LiveTextWindow::updateLiveText(const Common::String &text, bool notifyUser) {
+	// Set translated text flag
+	_textTranslation = false;
+
+	if (text.empty()) {
+		_text.clear();
+
+		invalidateRect(Common::Rect(), false);
+		updateWindow();
+
+		// TODO: Set warning state to false
+		return true;
+	}
+
+	_text = text;
+
+	// Redraw the window
+	invalidateRect(Common::Rect(), false);
+	updateWindow();
+
+	if (notifyUser) {
+		// TODO: Flash the warning light
+	}
+
+	return true;
+}
+
+bool LiveTextWindow::updateTranslationText(const Common::String &text, bool notifyUser) {
+	if (text.empty()) {
+		_text.clear();
+
+		invalidateRect(Common::Rect(), false);
+		updateWindow();
+
+		// TODO: Set warning state to false
+		return true;
+	}
+
+	_text = text;
+
+	// Set translated text flag
+	_textTranslation = true;
+
+	// Redraw the window
+	invalidateRect(Common::Rect(), false);
+	updateWindow();
+
+	// TODO: Set warning state to false
+
+	return true;
+}
+
+void LiveTextWindow::translateBiochipClosing() {
+	// If the current text is translated text, then kill it now
+	if (_textTranslation)
+		updateLiveText();
+}
+
+void LiveTextWindow::onPaint() {
+	// Draw the background bitmap
+	Graphics::Surface *surface = _vm->_gfx->getBitmap(IDB_LIVE_TEXT_BACKGROUND);
+
+	// (This probably needs a look-through)
+	if (!_text.empty()) {
+		uint32 textColor = _vm->_gfx->getColor(212, 109, 0);
+
+		Common::StringArray lines;
+		_font->wordWrapText(_text, 270, lines);
+
+		uint32 y = 4;
+		for (uint32 i = 0; i < lines.size(); i++) {
+			_font->drawString(surface, lines[i], 30, y, 270, textColor);
+			y += _font->getFontHeight() + 1;
+		}
+	}
+
+	_vm->_gfx->blit(surface, _rect.left, _rect.top);
+
+	surface->free();
+	delete surface;
+}
+
+void LiveTextWindow::onEnable(bool enable) {
+	if (enable) {
+		// TODO: Clear out mouse messages (why???)
+	}
+}
+
+} // End of namespace Buried
