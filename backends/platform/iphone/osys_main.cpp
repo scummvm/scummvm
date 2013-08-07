952a119ae16da8795785ca05a3f62ba7f5aee50b
Johannes Schickel <lordhoto@scummvm.org>
2013-08-07 21:41:24 +0200

    IPHONE: Fix accidental double free.
    This was a copy&paste error. I also adapted the comment to be more clear.

--- backends/platform/iphone/osys_main.cpp
+++ backends/platform/iphone/osys_main.cpp
@@ -77,8 +77,8 @@ OSystem_IPHONE::~OSystem_IPHONE() {
 	delete _mixer;
 	// Prevent accidental freeing of the screen texture here. This needs to be
 	// checked since we might use the screen texture as framebuffer in the case
-	// of hi-color games for example.
-	if (_framebuffer.getPixels() == _videoContext->screenTexture.getPixels())
+	// of hi-color games for example. Otherwise this can lead to a double free.
+	if (_framebuffer.getPixels() != _videoContext->screenTexture.getPixels())
 		_framebuffer.free();
 	_mouseBuffer.free();
 }
