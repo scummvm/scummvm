799652e442b9eac5acfcec6b14d0e11250209903
Dries Harnie <dries@harnie.be>
2013-08-07 20:23:07 +0200

    EMI: Rotate overworld actors in the right direction

--- engines/grim/actor.cpp
+++ engines/grim/actor.cpp
@@ -2043,7 +2043,7 @@ Math::Quaternion Actor::getRotationQuat() const {
 	if (g_grim->getGameType() == GType_MONKEY4) {
 		Math::Quaternion ret = Math::Quaternion::fromEuler(_yaw, _pitch, _roll);
 		if (_inOverworld)
-			ret = Math::Quaternion::fromEuler(_roll, _pitch, _yaw);
+			ret = Math::Quaternion::fromEuler(-_roll, -_pitch, -_yaw);
 
 		if (isAttached()) {
 			Actor *attachedActor = Actor::getPool().getObject(_attachedActor);
