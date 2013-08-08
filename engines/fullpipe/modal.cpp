3b86b149ed0f920659b2473feef86aad8c4b5853
Eugene Sandulenko <sev@scummvm.org>
2013-08-08 13:25:47 +0300

    FULLPIPE: Added missing file

---
+++ engines/fullpipe/modal.cpp
@@ -0,0 +1,94 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+#include "fullpipe/fullpipe.h"
+#include "fullpipe/modal.h"
+#include "fullpipe/messages.h"
+#include "fullpipe/constants.h"
+#include "fullpipe/scenes.h"
+
+namespace Fullpipe {
+
+bool CBaseModalObject::handleMessage(ExCommand *message) {
+	warning("STUB: CBaseModalObject::handleMessage()");
+
+	return true;
+}
+
+bool CBaseModalObject::init(int counterdiff) {
+	warning("STUB: CBaseModalObject::init(%d)", counterdiff);
+
+	return true;
+}
+
+bool CBaseModalObject::update() {
+	warning("STUB: CBaseModalObject::update()");
+
+	return true;
+}
+
+void CBaseModalObject::saveload() {
+	warning("STUB: CBaseModalObject::saveload()");
+}
+
+
+CModalIntro::CModalIntro() {
+	_field_8 = 0;
+	_countDown = 0;
+	_needRedraw = 0;
+	if (g_vars->sceneIntro_skipIntro) {
+		_introFlags = 4;
+	} else {
+		_introFlags = 33;
+		_countDown = 150;
+
+		PictureObject *pict = g_fullpipe->accessScene(SC_INTRO1)->getPictureObjectById(PIC_IN1_PIPETITLE, 0);
+		pict->setFlags(pict->_flags & 0xFFFB);
+	}
+	g_vars->sceneIntro_skipIntro = false;
+	_sfxVolume = g_fullpipe->_sfxVolume;
+}
+
+bool CModalIntro::handleMessage(ExCommand *message) {
+	if (message->_messageKind != 17)
+		return false;
+
+	if (message->_messageNum != 36)
+		return false;
+
+	if (message->_keyCode != 13 && message->_keyCode != 27 && message->_keyCode != 32 )
+		return false;
+
+	if (_needRedraw) {
+		if (!(_introFlags & 0x10)) {
+			_countDown = 0;
+			g_vars->sceneIntro_needBlackout = true;
+			return true;
+		}
+		g_vars->sceneIntro_playing = false;
+		g_vars->sceneIntro_needBlackout = true;
+	}
+
+	return true;
+}
+
+} // End of namespace Fullpipe
