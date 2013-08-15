934aef2379eb9ae4908320da964fd0b6794e1980
Narek Mailian <narek.mailian@gmail.com>
2013-08-16 00:44:16 +0200

    GUI: Allow the user to select a file without scrolling up the list

--- gui/browser.cpp
+++ gui/browser.cpp
@@ -131,11 +131,6 @@ void BrowserDialog::handleCommand(CommandSender *sender, uint32 cmd, uint32 data
 		}
 		break;
 	case kListSelectionChangedCmd:
-		// We do not allow selecting directories in directory
-		// browser mode, thus we will invalidate the selection
-		// when the user selects an directory over here.
-		if (data != (uint32)-1 && _isDirBrowser && !_nodeContent[data].isDirectory())
-			_fileList->setSelected(-1);
 		break;
 	case kHiddenCmd:
 		// Update whether the user wants hidden files to be shown
