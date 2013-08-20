e824ebaa44a2b6c97fd98e404588e72f520e53a7
Einar Johan Trøan Sømåen <einarjohants@gmail.com>
2013-08-20 16:52:29 +0200

    WINTERMUTE: Change pixel-format to RGBA.

--- engines/wintermute/graphics/transparent_surface.h
+++ engines/wintermute/graphics/transparent_surface.h
@@ -114,8 +114,6 @@ struct TransparentSurface : public Graphics::Surface {
 
 	TransparentSurface *scale(uint16 newWidth, uint16 newHeight) const;
 	TransparentSurface *rotoscale(const TransformStruct &transform) const;
-private:
-	static void doBlitAlpha(byte *ino, byte *outo, uint32 width, uint32 height, uint32 pitch, int32 inStep, int32 inoStep);
 };
 
 /**
