dd897be038e795612f756914253b0be9f7cedd00
Strangerke <strangerke@scummvm.org>
2013-09-06 00:17:13 +0200

    AVALANCHE: Identify currently dead code in enid2

--- engines/avalanche/enid2.h
+++ engines/avalanche/enid2.h
@@ -38,42 +38,39 @@ class AvalancheEngine;
 class Enid {
 public:
 	Enid(AvalancheEngine *vm);
-	void ednaSave(Common::String name);
-	void ednaLoad(Common::String name);
-	void ednaReload(); // From Bootstrap's storage.
 	void dir(Common::String where);
-	void avvyBackground();
 	void backToBootstrap(byte what);
-	bool thereWasAProblem();
 
 private:
 	AvalancheEngine *_vm;
 
-	static const Common::String kCrlf;
-	static const char kTab, kEof;
-	static const Common::String kEdnaFirst, kEdnaId, kWasHere;
-	static const int16 kAge;
+//  CHECKME: useless?
+//	static const Common::String kCrlf;
+//	static const char kTab, kEof;
+//	static const Common::String kEdnaFirst, kEdnaId, kWasHere;
+//	static const int16 kAge;
+//	void expandMonthName(Common::String x);
+//	Common::String expandDate(byte d, byte m, uint16 y);
+//	void ednaSave(Common::String name);
+//	void showBug(char icon, Common::String strn);
+//	bool testBug(byte what);
+//	void loadError(Common::String x, char icon);
+//	void ednaLoad(Common::String name);
+//	void showHeader();
+//	void toSundry(sundry &sund);
+//	void fromSundry(sundry sund);
+//	void avvyBackground();
+//	void ednaReload(); // From Bootstrap's storage.
+//	void restoreDna();
+//	bool thereWasAProblem();
 
-	typedef char FourType[5];
-	static const FourType kAvariciusFile;
+//	typedef char FourType[5];
+//	static const FourType kAvariciusFile;
 
-	bool _bug;
-	Common::String _path, _groi;
-	Common::String _month, _day;
+//	bool _bug;
+//	Common::String _path, _groi;
+//	Common::String _month, _day;
 
-	void addon(Common::String x);
-	Common::String expandDate(byte d, byte m, uint16 y);
-
-	void showBug(char icon, Common::String strn);
-	bool testBug(byte what);
-
-	void loadError(Common::String x, char icon);
-	void showHeader();
-
-	void toSundry(sundry &sund);
-	void fromSundry(sundry sund);
-
-	void restoreDna();
 };
 
 } // End of namespace Avalanche.
