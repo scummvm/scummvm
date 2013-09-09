4d104c12e2d9c871da0d2bf4ca2111234829d81d
Matthew Hoops <clone2727@gmail.com>
2013-09-08 22:53:24 -0400

    COMMON: Fix some memory leaks

--- common/winexe_ne.cpp
+++ common/winexe_ne.cpp
@@ -134,7 +134,7 @@ bool NEResources::loadFromCompressedEXE(const String &fileName) {
 	}
 
 	delete[] window;
-	SeekableReadStream *stream = new MemoryReadStream(unpackedData, unpackedLength);
+	SeekableReadStream *stream = new MemoryReadStream(unpackedData, unpackedLength, DisposeAfterUse::YES);
 
 	return loadFromEXE(stream);
 }
@@ -353,6 +353,7 @@ String NEResources::loadString(uint32 stringID) {
 	while (size--)
 		string += (char)stream->readByte();
 
+	delete stream;
 	return string;
 }
 
