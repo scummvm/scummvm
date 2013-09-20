6943fcfb031d21823a2ca4a93b80159bd516e586
Strangerke <strangerke@scummvm.org>
2013-09-20 07:55:10 +0200

    AVALANCHE: Janitorial - leftover Bristish -> American English

--- engines/avalanche/menu.h
+++ engines/avalanche/menu.h
@@ -25,7 +25,7 @@
  * Copyright (c) 1994-1995 Mike, Mark and Thomas Thurman.
  */
 
-/* Original name: DROPDOWN		A customised version of Oopmenu (qv). */
+/* Original name: DROPDOWN		A customized version of Oopmenu (qv). */
 
 #ifndef AVALANCHE_DROPDOWN2_H
 #define AVALANCHE_DROPDOWN2_H
