1140c8b48d6d814aa512af348bcb0463006250d3
RichieSams <adastley@gmail.com>
2013-10-01 18:49:01 -0500

    ZVISION: Fix space formatting

--- engines/zvision/zork_raw.h
+++ engines/zvision/zork_raw.h
@@ -89,28 +89,28 @@ public:
 /**
  * Creates an audio stream, which plays from the given buffer.
  *
- * @param buffer Buffer to play from.
- * @param size   Size of the buffer in bytes.
- * @param rate   Rate of the sound data.
- * @param disposeAfterUse Whether to free the buffer after use (with free!).
- * @return The new SeekableAudioStream (or 0 on failure).
+ * @param buffer     Buffer to play from.
+ * @param size       Size of the buffer in bytes.
+ * @param rate       Rate of the sound data.
+ * @param dispose    AfterUse Whether to free the buffer after use (with free!).
+ * @return           The new SeekableAudioStream (or 0 on failure).
  */
 Audio::RewindableAudioStream *makeRawZorkStream(const byte *buffer, uint32 size,
                                                 int rate,
-								                bool stereo,
+                                                bool stereo,
                                                 DisposeAfterUse::Flag disposeAfterUse = DisposeAfterUse::YES);
 
 /**
  * Creates an audio stream, which plays from the given stream.
  *
- * @param stream Stream object to play from.
- * @param rate   Rate of the sound data.
- * @param disposeAfterUse Whether to delete the stream after use.
- * @return The new SeekableAudioStream (or 0 on failure).
+ * @param stream     Stream object to play from.
+ * @param rate       Rate of the sound data.
+ * @param dispose    AfterUse Whether to delete the stream after use.
+ * @return           The new SeekableAudioStream (or 0 on failure).
  */
 Audio::RewindableAudioStream *makeRawZorkStream(Common::SeekableReadStream *stream,
                                                 int rate,
-								                bool stereo,
+                                                bool stereo,
                                                 DisposeAfterUse::Flag disposeAfterUse = DisposeAfterUse::YES);
 
 Audio::RewindableAudioStream *makeRawZorkStream(const Common::String &filePath, ZVision *engine);
