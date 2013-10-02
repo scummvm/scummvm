scummvm/scummvm

commit 81a2892229a053bb9c76dea4dbfb63ca13164946

TSAGE: Clean up of R2R scene 1550 player position tracking

engines/tsage/globals.cpp
@@ -475,11 +475,8 @@ void Ringworld2Globals::reset() {
 	_v5780E = 0;
 	_v57810 = 0;
 	_v57C2C = 0;
-	_v565EC[0] = 0;
-	_v565EC[1] = 27;
-	_v565EC[2] = 27;
-	_v565EC[3] = 4;
-	_v565EC[4] = 4;
+	_s1550PlayerArea[R2_QUINN] = Common::Point(27, 4);
+	_s1550PlayerArea[R2_SEEKER] = Common::Point(27, 4);
 	Common::fill(&_scannerFrequencies[0], &_scannerFrequencies[MAX_CHARACTERS], 1);
 	_speechSubtitles = SPEECH_VOICE | SPEECH_TEXT;
 	_insetUp = 0;
@@ -539,8 +536,12 @@ void Ringworld2Globals::synchronize(Serializer &s) {
 	s.syncAsSint16LE(_v57C2C);
 	s.syncAsSint16LE(_speechSubtitles);
 
-	for (i = 0; i < 5; i++)
-		s.syncAsByte(_v565EC[i]);
+	byte temp;
+	s.syncAsByte(temp);
+	s.syncAsByte(_s1550PlayerArea[R2_QUINN].x);
+	s.syncAsByte(_s1550PlayerArea[R2_SEEKER].x);
+	s.syncAsByte(_s1550PlayerArea[R2_QUINN].y);
+	s.syncAsByte(_s1550PlayerArea[R2_SEEKER].y);
 
 	for (i = 0; i < MAX_CHARACTERS; ++i)
 		s.syncAsByte(_scannerFrequencies[i]);

engines/tsage/globals.h
@@ -304,7 +304,7 @@ public:
 	int _v57810;
 	int _v57C2C;
 	int _speechSubtitles;
-	byte _v565EC[5];
+	Common::Point _s1550PlayerArea[3]; // only used for Quinn and Seeker
 	byte _scannerFrequencies[4];
 	byte _stripManager_lookupList[12];
 	ScannerDialog *_scannerDialog;

engines/tsage/ringworld2/ringworld2_logic.cpp
@@ -2274,14 +2274,14 @@ void ScannerDialog::Button::reset() {
 
 			scanner._obj5.postInit();
 			scanner._obj5.setup(4, 4, 1);
-			scanner._obj5.setPosition(Common::Point(R2_GLOBALS._v565EC[1] + 145,
-				R2_GLOBALS._v565EC[3] + 59));
+			scanner._obj5.setPosition(Common::Point(R2_GLOBALS._s1550PlayerArea[R2_QUINN].x + 145,
+				R2_GLOBALS._s1550PlayerArea[R2_QUINN].y + 59));
 			scanner._obj5.fixPriority(257);
 
 			scanner._obj6.postInit();
 			scanner._obj6.setup(4, 4, 2);
-			scanner._obj6.setPosition(Common::Point(R2_GLOBALS._v565EC[2] + 145,
-				R2_GLOBALS._v565EC[4] + 59));
+			scanner._obj6.setPosition(Common::Point(R2_GLOBALS._s1550PlayerArea[R2_SEEKER].x + 145,
+				R2_GLOBALS._s1550PlayerArea[R2_SEEKER].y + 59));
 			scanner._obj6.fixPriority(257);
 			break;
 		case 1700:

engines/tsage/ringworld2/ringworld2_scenes1.h
@@ -487,7 +487,7 @@ class Scene1550 : public SceneExt {
 		virtual bool startAction(CursorType action, Event &event);
 	};
 
-	class Actor7 : public SceneActor {
+	class Companion : public SceneActor {
 	public:
 		virtual bool startAction(CursorType action, Event &event);
 	};
@@ -533,13 +533,13 @@ public:
 	Hotspot1 _item1;
 	Hotspot1 _item2;
 	Hotspot3 _item3;
-	SceneActor _actor1;
+	SceneActor _landingStrut;
 	SceneActor _actor2;
 	SceneActor _actor3;
 	SceneActor _actor4;
 	SceneActor _actor5;
 	Actor6 _actor6;
-	Actor7 _actor7;
+	Companion _companion;
 	Actor8 _actor8;
 	Actor9 _actor9;
 	Actor10 _actor10;
@@ -566,7 +566,7 @@ public:
 
 	Scene1550();
 	void synchronize(Serializer &s);
-	void subA2B2F();
+	void enterArea();
 
 	virtual void postInit(SceneObjectList *OwnerList = NULL);
 	virtual void signal();

commit df266bc1663bd57e6465faaf66912615e9b9b6b0

TSAGE: Fix uninitialized variable.
This was caused by commit 81a2892229a053bb9c76dea4dbfb63ca13164946.

engines/tsage/globals.cpp
@@ -541,7 +541,7 @@ void Ringworld2Globals::synchronize(Serializer &s) {
 	s.syncAsSint16LE(_v57C2C);
 	s.syncAsSint16LE(_speechSubtitles);
 
-	byte temp;
+	byte temp = 0;
 	s.syncAsByte(temp);
 	s.syncAsByte(_s1550PlayerArea[R2_QUINN].x);
 	s.syncAsByte(_s1550PlayerArea[R2_SEEKER].x);