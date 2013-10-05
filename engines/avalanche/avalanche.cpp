scummvm/scummvm

commit badc727883c14d13702fc61a0a6912a3d61ce9a4

AVALANCHE: Fix savegame naming. This breaks previous savegames

engines/avalanche/avalanche.cpp
@@ -314,9 +314,7 @@ bool AvalancheEngine::saveGame(const int16 slot, const Common::String &desc) {
 }
 
 Common::String AvalancheEngine::getSaveFileName(const int slot) {
-	Common::String upperName = _targetName;
-	upperName.toUppercase();
-	return upperName+ Common::String::format("-%02d.SAV", slot);
+	return Common::String::format("%s.%03d", _targetName, slot);
 }
 
 bool AvalancheEngine::canLoadGameStateCurrently() { // TODO: Refine these!!!

commit 03ca6ef76f3d0087d7ddf855bf84fc0d327b17c2

AVALANCHE: Fix bug introduced in badc727883c14d13702fc61a0a6912a3d61ce9a4

engines/avalanche/avalanche.cpp
@@ -304,7 +304,7 @@ bool AvalancheEngine::saveGame(const int16 slot, const Common::String &desc) {
 }
 
 Common::String AvalancheEngine::getSaveFileName(const int slot) {
-	return Common::String::format("%s.%03d", _targetName, slot);
+	return Common::String::format("%s.%03d", _targetName.c_str(), slot);
 }
 
 bool AvalancheEngine::canLoadGameStateCurrently() { // TODO: Refine these!!!