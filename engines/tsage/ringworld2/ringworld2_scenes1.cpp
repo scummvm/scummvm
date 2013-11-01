217164c6c9b17e46f38e02acdc2450caf7fcc21a
Paul Gilbert <dreammaster@scummvm.org>
2013-10-31 20:09:13 -0400

    TSAGE: Improved comments about uninitialised variable in original code

--- engines/tsage/ringworld2/ringworld2_scenes1.cpp
+++ engines/tsage/ringworld2/ringworld2_scenes1.cpp
@@ -8267,8 +8267,10 @@ void Scene1550::dispatch() {
 
 	if ((R2_GLOBALS._s1550PlayerArea[R2_GLOBALS._player._characterIndex].x == 15) && (R2_GLOBALS._s1550PlayerArea[R2_GLOBALS._player._characterIndex].y == 16)) {
 		R2_GLOBALS._player._shade = 0;
-		// Original game contains a switch based on an uninitialized variable.
-		// Until we understand what should really happen there, this code is unused on purpose
+
+		// WORKAROUND: Original game contains a switch based on an uninitialized variable.
+		// The switch is being left in place, but with a fixed switch value of zero, in case
+		// we can ever figure out what the original programmers intended the value to come from
 		int missingVariable = 0;
 		switch (missingVariable) {
 		case 144:
