de8da01b0e8a309b9ed3f5b0f152ebbcf8f4af37
D G Turner <digitall@scummvm.org>
2013-11-03 17:10:18 +0000

    SDL: Remove side effect of setlocale() call to get Language.
    This is to fix bug #3615148 -  "ALL: sscanf("%f"), atof() etc. not
    portable due to Locale"
    
    The side effect of setlocale("") is to change the active locale
    from the default of "C" to the detected system locale, but this
    changes the behaviour of sscanf() and several other functions
    in a system dependent and non-portable way. This has caused
    bugs in the ZVISION engine when running Zork Nemesis.
    
    The solution is to restore the default "C" locale after the call
    to get the language.
    
    Thanks to criezy for working out this fix.

--- backends/platform/sdl/sdl.cpp
+++ backends/platform/sdl/sdl.cpp
@@ -393,6 +393,11 @@ Common::String OSystem_SDL::getSystemLanguage() const {
 #else // WIN32
 	// Activating current locale settings
 	const char *locale = setlocale(LC_ALL, "");
+ 
+	// Restore default C locale to prevent issues with
+	// portability of sscanf(), atof(), etc.
+	// See bug #3615148
+	setlocale(LC_ALL, "C");
 
 	// Detect the language from the locale
 	if (!locale) {
