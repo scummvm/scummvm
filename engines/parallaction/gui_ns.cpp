4decd8c8083a6ff85c092d336d70b61bf8ec30bf
Eugene Sandulenko <sev@scummvm.org>
2013-11-09 13:17:30 +0200

    PARALLACTION: Initialize class variable. CID 1002715

--- engines/parallaction/gui_ns.cpp
+++ engines/parallaction/gui_ns.cpp
@@ -117,6 +117,7 @@ public:
 		_allowChoice = false;
 		_nextState = "selectgame";
 		_label = 0;
+		_blocks = 0;
 
 		_dosLanguageSelectBlocks[0] = Common::Rect(  80, 110, 128, 180 );	// Italian
 		_dosLanguageSelectBlocks[1] = Common::Rect( 129,  85, 177, 155 );	// French
