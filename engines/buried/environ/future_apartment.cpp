d5c978b10cba526a9f15adb5cda6846c5d0f40d8
Matthew Hoops <clone2727@gmail.com>
2013-11-23 23:54:14 -0500

    BURIED: Implement toy shelf zooming

--- engines/buried/environ/future_apartment.cpp
+++ engines/buried/environ/future_apartment.cpp
@@ -24,6 +24,7 @@
  */
 
 #include "buried/buried.h"
+#include "buried/graphics.h"
 #include "buried/resources.h"
 #include "buried/scene_view.h"
 #include "buried/sound.h"
@@ -31,6 +32,54 @@
 #include "buried/environ/scene_common.h"
 
 namespace Buried {
+
+class ClickZoomToyShelf : public SceneBase {
+public:
+	ClickZoomToyShelf(BuriedEngine *vm, Window *viewWindow, const LocationStaticData &sceneStaticData, const Location &priorLocation);
+
+	int mouseUp(Window *viewWindow, const Common::Point &pointLocation);
+	int specifyCursor(Window *viewWindow, const Common::Point &pointLocation);
+
+private:
+	Common::Rect _toyZooms[4];
+};
+
+ClickZoomToyShelf::ClickZoomToyShelf(BuriedEngine *vm, Window *viewWindow, const LocationStaticData &sceneStaticData, const Location &priorLocation) : SceneBase(vm, viewWindow, sceneStaticData, priorLocation) {
+	_toyZooms[0] = Common::Rect(102, 8, 170, 108);
+	_toyZooms[1] = Common::Rect(188, 24, 228, 108);
+	_toyZooms[2] = Common::Rect(282, 24, 330, 108);
+	_toyZooms[3] = Common::Rect(330, 28, 366, 108);
+}
+
+int ClickZoomToyShelf::mouseUp(Window *viewWindow, const Common::Point &pointLocation) {
+	for (int i = 0; i < 4; i++) {
+		if (_toyZooms[i].contains(pointLocation)) {
+			DestinationScene newScene;
+			newScene.destinationScene = _staticData.location;
+			newScene.destinationScene.depth = i + 1;
+			newScene.transitionType = TRANSITION_VIDEO;
+			newScene.transitionStartFrame = -1;
+			newScene.transitionLength = -1;
+
+			static const int transitionData[4] = { 13, 16, 19, 22 };
+			newScene.transitionData = transitionData[i];
+
+			((SceneViewWindow *)viewWindow)->moveToDestination(newScene);
+
+			return SC_TRUE;
+		}
+	}
+
+	return SC_FALSE;
+}
+
+int ClickZoomToyShelf::specifyCursor(Window *viewWindow, const Common::Point &pointLocation) {
+	for (int i = 0; i < 4; i++)
+		if (_toyZooms[i].contains(pointLocation))
+			return kCursorMagnifyingGlass;
+
+	return kCursorArrow;
+}
 
 bool SceneViewWindow::startFutureApartmentAmbient(int oldTimeZone, int oldEnvironment, int environment, bool fade) {
 	_vm->_sound->setAmbientSound(_vm->getFilePath(4, environment, SF_AMBIENT));
@@ -42,7 +91,9 @@ SceneBase *SceneViewWindow::constructFutureApartmentSceneObject(Window *viewWind
 
 	switch (sceneStaticData.classID) {
 	case 30:
-		return new PlayStingers(_vm, viewWindow, sceneStaticData, priorLocation, 128, offsetof(GlobalFlags, faStingerID), offsetof(GlobalFlags, faStingerChannelID), 10, 14); 
+		return new PlayStingers(_vm, viewWindow, sceneStaticData, priorLocation, 128, offsetof(GlobalFlags, faStingerID), offsetof(GlobalFlags, faStingerChannelID), 10, 14);
+	case 37:
+		return new ClickZoomToyShelf(_vm, viewWindow, sceneStaticData, priorLocation);
 	}
 
 	warning("TODO: Future apartment scene object %d", sceneStaticData.classID);
