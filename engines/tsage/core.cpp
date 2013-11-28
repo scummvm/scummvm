6b61eb62ab8f672be35ced69aa18e9793eb27772
Paul Gilbert <dreammaster@scummvm.org>
2013-11-28 15:00:44 -0500

    TSAGE: R2R fix for zoomed speaking characters appearing correctly

--- engines/tsage/core.cpp
+++ engines/tsage/core.cpp
@@ -2748,6 +2748,12 @@ GfxSurface SceneObject::getFrame() {
 }
 
 void SceneObject::reposition() {
+	if (g_vm->getGameID() == GType_Ringworld2) {
+		if (!(_flags & OBJFLAG_ZOOMED)) {
+			setZoom(g_globals->_sceneManager._scene->_zoomPercents[MIN(_position.y, (int16)255)]);
+		}
+	}
+
 	GfxSurface frame = getFrame();
 
 	_bounds.resize(frame, _position.x, _position.y - _yDiff, _percent);
