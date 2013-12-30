c4c21c6702ec5b74c83186a8e57f772dc6668c70
Eugene Sandulenko <sev@scummvm.org>
2013-12-30 23:18:52 +0200

    FULLPIPE: Started scene14

---
+++ engines/fullpipe/scenes/scene14.cpp
@@ -0,0 +1,84 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+#include "fullpipe/fullpipe.h"
+
+#include "fullpipe/objectnames.h"
+#include "fullpipe/constants.h"
+
+#include "fullpipe/gameloader.h"
+#include "fullpipe/motion.h"
+#include "fullpipe/scenes.h"
+#include "fullpipe/statics.h"
+
+#include "fullpipe/interaction.h"
+#include "fullpipe/behavior.h"
+
+
+namespace Fullpipe {
+
+void scene14_initScene(Scene *sc) {
+	g_vars->scene14_grandma = Scene_getStaticANIObject1ById(sc, ANI_GRANDMA, -1);
+	g_vars->scene14_var01 = 200;
+	g_vars->scene14_var02 = 200;
+	g_vars->scene14_var03 = 0;
+	g_vars->scene14_var04 = 0;
+	g_vars->scene14_var05 = 0;
+	g_vars->scene14_var06 = 0;
+	g_vars->scene14_var07 = 300;
+	g_vars->scene14_var08 = 300;
+	g_vars->scene14_pink = 0;
+	g_vars->scene14_var10 = 0;
+	g_vars->scene14_var11.clear();
+	g_vars->scene14_var12.clear();
+
+	if (g_fp->getObjectState(sO_Grandma) == g_fp->getObjectEnumState(sO_Grandma, sO_In_14)) {
+		g_vars->scene14_var13 = 1;
+
+		StaticANIObject *ball = sc->getStaticANIObject1ById(ANI_BALL14, -1);
+
+		if (ball) {
+			ball->_flags &= 0xFFFB;
+			g_vars->scene14_var11.push_back(ball);
+		}
+		
+		for (uint i = 0; i < 3; i++) {
+			ball = new StaticANIObject(ball); // create a copy
+
+			ball->_flags &= 0xFFFB;
+			g_vars->scene14_var11.push_back(ball);
+
+			sc->addStaticANIObject(ball, 1);
+		}
+	} else {
+		g_vars->scene14_var13 = 0;
+		g_vars->scene14_grandma->hide();
+	}
+
+	g_fp->lift_setButton(sO_Level4, ST_LBN_4N);
+	g_fp->lift_sub5(sc, QU_SC14_ENTERLIFT, QU_SC14_EXITLIFT);
+
+	g_fp->initArcadeKeys("SC_14");
+	g_fp->setArcadeOverlay(PIC_CSR_ARCADE6);
+}
+
+} // End of namespace Fullpipe
