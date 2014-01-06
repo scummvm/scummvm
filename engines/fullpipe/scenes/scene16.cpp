48fef827a5093495e97d58b6e7842d882587960c
Eugene Sandulenko <sev@scummvm.org>
2014-01-06 22:41:41 +0200

    FULLPIPE: Remove redundant check. CID 1148380

--- engines/fullpipe/scenes/scene16.cpp
+++ engines/fullpipe/scenes/scene16.cpp
@@ -191,7 +191,7 @@ void sceneHandler16_fillMug() {
 			mq = new MessageQueue(g_fp->_currentScene->getMessageQueueById(QU_SC16_GIRLOUT), 0, 1);
 
 			mq->replaceKeyCode(-1, g_vars->scene16_walkingGirl->_okeyCode);
-			if (mq->chain(g_vars->scene16_walkingGirl) || !mq)
+			if (mq->chain(g_vars->scene16_walkingGirl))
 				return;
 		}
 		delete mq;
