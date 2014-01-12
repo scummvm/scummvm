40be9172834de73b2745fe2fa782a05e8243d23d
Max Lingua <max.lingua@gmail.com>
2014-01-11 21:13:04 -0500

    PS2: enhanced Makefile.ps2 (incl. modules & debug)

--- backends/platform/ps2/module.mk
+++ backends/platform/ps2/module.mk
@@ -2,20 +2,20 @@ MODULE := backends/platform/ps2
 
 MODULE_OBJS := \
 	DmaPipe.o \
-    Gs2dScreen.o \
-    irxboot.o \
+	Gs2dScreen.o \
+	irxboot.o \
 	ps2input.o \
 	ps2pad.o \
 	savefilemgr.o \
-    fileio.o \
-    asyncfio.o \
+	fileio.o \
+	asyncfio.o \
 	icon.o \
-    cd.o \
-    eecodyvdfs.o \
-    rpckbd.o \
-    systemps2.o \
-    ps2mutex.o \
-    ps2time.o \
+	cd.o \
+	eecodyvdfs.o \
+	rpckbd.o \
+	systemps2.o \
+	ps2mutex.o \
+	ps2time.o \
 	ps2debug.o
 
 # We don't use rules.mk but rather manually update OBJS and MODULE_DIRS.
