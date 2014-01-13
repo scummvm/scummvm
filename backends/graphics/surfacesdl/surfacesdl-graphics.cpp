2c936f03346ab1f805813eabcdc2ad6efa0cd52b
Dries Harnie <dries@harnie.be>
2014-01-14 00:26:41 +0100

    GRAPHICS: Disable overlay rendering on GL3/ES2

--- backends/graphics/surfacesdl/surfacesdl-graphics.cpp
+++ backends/graphics/surfacesdl/surfacesdl-graphics.cpp
@@ -318,6 +318,7 @@ Graphics::PixelBuffer SurfaceSdlGraphicsManager::setupScreen(int screenW, int sc
 void SurfaceSdlGraphicsManager::updateScreen() {
 #ifdef USE_OPENGL
 	if (_opengl) {
+#ifndef USE_OPENGL_SHADERS
 		if (_overlayVisible) {
 			if (_overlayDirty) {
 				// remove if already exist
@@ -412,6 +413,7 @@ void SurfaceSdlGraphicsManager::updateScreen() {
 
 			glPopAttrib();
 		}
+#endif
 		SDL_GL_SwapBuffers();
 	} else
 #endif
