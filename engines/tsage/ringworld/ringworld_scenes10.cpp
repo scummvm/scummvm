0e6c4de8c644784c7404efb759a3274ccadcb20d
Strangerke <strangerke@scummvm.org>
2014-02-09 00:36:50 +0100

    TSAGE: Ringworld - Remove a redundant check on the previous scene number in scene 9500

--- engines/tsage/ringworld/ringworld_scenes10.cpp
+++ engines/tsage/ringworld/ringworld_scenes10.cpp
@@ -1217,7 +1217,7 @@ void Scene9500::postInit(SceneObjectList *OwnerList) {
 	g_globals->_events.setCursor(CURSOR_WALK);
 	g_globals->_player.disableControl();
 
-	if ((g_globals->_sceneManager._previousScene == 9200) || (g_globals->_sceneManager._previousScene != 9850)) {
+	if (g_globals->_sceneManager._previousScene != 9850) {
 		_sceneMode = 0;
 		if (RING_INVENTORY._helmet._sceneNumber != 1) {
 			setAction(&_sequenceManager, this, 9501, &g_globals->_player, &_candle, NULL);
