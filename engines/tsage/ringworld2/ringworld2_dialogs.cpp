scummvm/scummvm

commit a41db1939cdff632d16aa3849e23b844c46bafef

TSAGE: Fix bug #6519 - Mouse pointer behavior in dialog menu

engines/tsage/ringworld2/ringworld2_dialogs.cpp
@@ -197,6 +197,11 @@ int RightClickDialog::execute() {
 
 void CharacterDialog::show() {
 	CharacterDialog *dlg = new CharacterDialog();
+	CursorType cursorNum = R2_GLOBALS._events.getCursor();
+
+	// Reset the current cursor
+	R2_GLOBALS._events.setCursor(CURSOR_ARROW);
+
 	dlg->draw();
 
 	// Make the default button the currently active character
@@ -315,12 +320,11 @@ void CharacterDialog::show() {
 			}
 		}
 
-		// Reset the current cursor
-		R2_GLOBALS._events.setCursor(CURSOR_USE);
-
 		// Change to whichever scene the newly selected character is in
 		R2_GLOBALS._sceneManager.changeScene(R2_GLOBALS._player._characterScene[R2_GLOBALS._player._characterIndex]);
-	}
+	} else
+		// Restore previous cursor
+		R2_GLOBALS._events.setCursor(cursorNum);
 }
 
 CharacterDialog::CharacterDialog() {

commit 0657b7a072dc745b7290c159d73c1f6924886554

TSAGE: Fix again #6507 - Regression introduced in a41db1939cdff632d16aa3849e23b844c46bafef

engines/tsage/ringworld2/ringworld2_dialogs.cpp
@@ -322,6 +322,10 @@ void CharacterDialog::show() {
 
 		// Change to whichever scene the newly selected character is in
 		R2_GLOBALS._sceneManager.changeScene(R2_GLOBALS._player._characterScene[R2_GLOBALS._player._characterIndex]);
+
+		// Force the reset the current cursor
+		R2_GLOBALS._events.setCursor(CURSOR_USE);
+
 	} else
 		// Restore previous cursor
 		R2_GLOBALS._events.setCursor(cursorNum);