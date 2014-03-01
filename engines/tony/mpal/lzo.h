2218d14fb5276724c757406d5ac1ec581160721b
Strangerke <strangerke@scummvm.org>
2014-03-01 16:50:02 +0100

    TONY: Remove unused error defines

--- engines/tony/mpal/lzo.h
+++ engines/tony/mpal/lzo.h
@@ -75,16 +75,8 @@ namespace MPAL {
  * normal events.
  */
 #define LZO_E_OK                    0
-#define LZO_E_ERROR                 (-1)
-#define LZO_E_OUT_OF_MEMORY         (-2)    /* [lzo_alloc_func_t failure] */
-#define LZO_E_NOT_COMPRESSIBLE      (-3)    /* [not used right now] */
 #define LZO_E_INPUT_OVERRUN         (-4)
-#define LZO_E_OUTPUT_OVERRUN        (-5)
-#define LZO_E_LOOKBEHIND_OVERRUN    (-6)
-#define LZO_E_EOF_NOT_FOUND         (-7)
 #define LZO_E_INPUT_NOT_CONSUMED    (-8)
-#define LZO_E_NOT_YET_IMPLEMENTED   (-9)    /* [not used right now] */
-#define LZO_E_INVALID_ARGUMENT      (-10)
 
 /**
  * Decompresses an LZO compressed resource
