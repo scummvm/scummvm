6ba5570de52344da602cf02fb83725b468e3d8c5
johndoe123 <john_doe@techie.com>
2014-04-04 10:53:00 +0200

    ILLUSIONS: Move SpriteDrawQueue to screen

--- engines/illusions/screen.cpp
+++ engines/illusions/screen.cpp
@@ -22,8 +22,6 @@
 
 #include "illusions/illusions.h"
 #include "illusions/screen.h"
-#include "illusions/graphics.h"
-#include "illusions/spritedrawqueue.h"
 
 namespace Illusions {
 
@@ -132,6 +130,170 @@ void SpriteDecompressQueue::decompress(SpriteDecompressQueueItem *item) {
 
 	*item->_drawFlags &= ~1;
  
+}
+
+// SpriteDrawQueue
+
+SpriteDrawQueue::SpriteDrawQueue(Screen *screen)
+	: _screen(screen) {
+}
+
+SpriteDrawQueue::~SpriteDrawQueue() {
+}
+
+bool SpriteDrawQueue::draw(SpriteDrawQueueItem *item) {
+
+	// Check if the sprite has finished decompressing
+	if (item->_kind != 0 && (*item->_drawFlags & 1)) {		
+		insert(item, item->_priority);
+		return false;
+	}
+
+	if (!_screen->isDisplayOn()) {
+		if (item->_drawFlags)
+			*item->_drawFlags &= ~4;
+		return true;			
+	}	
+
+	Common::Rect srcRect, dstRect;
+	
+	// Check if the sprite is on-screen
+	if (!calcItemRect(item, srcRect, dstRect))
+		return true;
+
+	if (item->_scale == 100) {
+		if (item->_flags & 1)
+			_screen->drawSurface10(dstRect.left, dstRect.top, item->_surface, srcRect, _screen->getColorKey2());
+		else
+			_screen->drawSurface11(dstRect.left, dstRect.top, item->_surface, srcRect);
+	} else {
+		if (item->_flags & 1)
+			_screen->drawSurface20(dstRect, item->_surface, srcRect, _screen->getColorKey2());
+		else
+			_screen->drawSurface21(dstRect, item->_surface, srcRect);
+	}
+	
+	if (item->_drawFlags)
+		*item->_drawFlags &= ~4;
+
+	return true;
+}
+
+void SpriteDrawQueue::drawAll() {
+	SpriteDrawQueueListIterator it = _queue.begin();
+	while (it != _queue.end()) {
+		if (draw(*it)) {
+			delete *it;
+			it = _queue.erase(it);
+		} else
+			++it;
+	}
+}
+
+void SpriteDrawQueue::insertSprite(byte *drawFlags, Graphics::Surface *surface, WidthHeight &dimensions,
+	Common::Point &drawPosition, Common::Point &controlPosition, uint32 priority, int16 scale, uint16 flags) {
+	SpriteDrawQueueItem *item = new SpriteDrawQueueItem();
+	item->_drawFlags = drawFlags;
+	*item->_drawFlags &= 4;
+	item->_surface = surface;
+	item->_dimensions = dimensions;
+	item->_controlPosition = controlPosition;
+	item->_scale = scale;
+	item->_priority = priority;
+	item->_drawPosition = drawPosition;
+	item->_kind = 1;
+	item->_flags = flags;
+	insert(item, priority);
+}
+
+void SpriteDrawQueue::insertSurface(Graphics::Surface *surface, WidthHeight &dimensions,
+	Common::Point &drawPosition, uint32 priority) {
+	SpriteDrawQueueItem *item = new SpriteDrawQueueItem();
+	item->_surface = surface;
+	item->_dimensions = dimensions;
+	item->_drawFlags = 0;
+	item->_kind = 0;
+	item->_drawPosition.x = -drawPosition.x;
+	item->_drawPosition.y = -drawPosition.y;
+	item->_controlPosition.x = 0;
+	item->_controlPosition.y = 0;
+	item->_flags = 0;
+	item->_scale = 100;
+	item->_priority = priority;// << 16;
+	insert(item, priority);
+}
+
+void SpriteDrawQueue::insertTextSurface(Graphics::Surface *surface, WidthHeight &dimensions,
+	Common::Point &drawPosition, uint32 priority) {
+	SpriteDrawQueueItem *item = new SpriteDrawQueueItem();
+	item->_surface = surface;
+	item->_drawPosition = drawPosition;
+	item->_dimensions = dimensions;
+	item->_drawFlags = 0;
+	item->_kind = 0;
+	item->_controlPosition.x = 0;
+	item->_controlPosition.y = 0;
+	item->_flags = 0;
+	item->_priority = priority;
+	item->_scale = 100;
+	insert(item, priority);
+}
+
+void SpriteDrawQueue::insert(SpriteDrawQueueItem *item, uint32 priority) {
+	SpriteDrawQueueListIterator insertionPos = Common::find_if(_queue.begin(), _queue.end(),
+		FindInsertionPosition(priority));
+	_queue.insert(insertionPos, item);
+}
+
+bool SpriteDrawQueue::calcItemRect(SpriteDrawQueueItem *item, Common::Rect &srcRect, Common::Rect &dstRect) {
+
+	srcRect.left = 0;
+	srcRect.top = 0;
+	srcRect.right = item->_dimensions._width;
+	srcRect.bottom = item->_dimensions._height;
+	
+	dstRect.left = item->_drawPosition.x - item->_scale * item->_controlPosition.x / 100;
+	dstRect.top = item->_drawPosition.y - item->_scale * item->_controlPosition.y / 100;
+	dstRect.right = item->_drawPosition.x + item->_scale * (item->_dimensions._width - item->_controlPosition.x) / 100;
+	dstRect.bottom = item->_drawPosition.y + item->_scale * (item->_dimensions._height - item->_controlPosition.y) / 100;
+	
+	/* CHECKME This seems to be unused basically and only called from debug code
+		Left here just in case...
+	if (gfx_seemsAlways0) {
+		dstRect.left += screenOffsetPt.x;
+		dstRect.right = screenOffsetPt.x + dstRect.right;
+		dstRect.top = screenOffsetPt.y + dstRect.top;
+		dstRect.bottom = screenOffsetPt.y + dstRect.bottom;
+	}
+	*/
+
+	// Check if the sprite is on-screen
+	if (dstRect.left >= 640 || dstRect.right <= 0 || dstRect.top >= 480 || dstRect.bottom <= 0)
+		return false;
+
+	// Clip the sprite rect if neccessary
+
+	if (dstRect.left < 0) {
+		srcRect.left += -100 * dstRect.left / item->_scale;
+		dstRect.left = 0;
+	}
+
+	if (dstRect.top < 0) {
+		srcRect.top += -100 * dstRect.top / item->_scale;
+		dstRect.top = 0;
+	}
+
+	if (dstRect.right > 640) {
+		srcRect.right += 100 * (640 - dstRect.right) / item->_scale;
+		dstRect.right = 640;
+	}
+
+	if (dstRect.bottom > 480) {
+		srcRect.bottom += 100 * (480 - dstRect.bottom) / item->_scale;
+		dstRect.bottom = 480;
+	}
+
+	return true;
 }
 
 // Screen
