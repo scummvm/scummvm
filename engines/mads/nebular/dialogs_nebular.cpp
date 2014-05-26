cda1a9402ac9d993cf58373d43af5a78981166fb
Strangerke <strangerke@scummvm.org>
2014-05-26 23:29:42 +0200

    MADS: Initialize a variable in ScreenDialog

--- engines/mads/nebular/dialogs_nebular.cpp
+++ engines/mads/nebular/dialogs_nebular.cpp
@@ -484,6 +484,7 @@ ScreenDialog::ScreenDialog(MADSEngine *vm) : _vm(vm),
 	_selectedLine = 0;
 	_dirFlag = false;
 	_textLineCount = 0;
+	_screenId = 920;
 
 	game.loadQuoteSet(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
 		17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33,
