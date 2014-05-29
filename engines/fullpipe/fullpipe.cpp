aed5de6dfa29226e22b22ef1366956285a07bf07
Eugene Sandulenko <sev@scummvm.org>
2014-05-29 09:18:34 +0300

    FULLPIPE: Implement FullpipeEngine::disableSaves()

--- engines/fullpipe/fullpipe.cpp
+++ engines/fullpipe/fullpipe.cpp
@@ -510,7 +510,20 @@ void FullpipeEngine::setObjectState(const char *name, int state) {
 }
 
 void FullpipeEngine::disableSaves(ExCommand *ex) {
-	warning("STUB: FullpipeEngine::disableSaves()");
+	if (_isSaveAllowed) {
+		_isSaveAllowed = false;
+
+		if (_globalMessageQueueList->size() && (*_globalMessageQueueList)[0] != 0) {
+			for (int i = 0; i < _globalMessageQueueList->size(); i++) {
+				if ((*_globalMessageQueueList)[i]->_flags & 1)
+					if ((*_globalMessageQueueList)[i]->_id != ex->_parId && !(*_globalMessageQueueList)[i]->_isFinished)
+						return;
+			}
+		}
+
+		if (_currentScene)
+			_gameLoader->writeSavegame(_currentScene, "savetmp.sav");
+	}
 }
 
 
