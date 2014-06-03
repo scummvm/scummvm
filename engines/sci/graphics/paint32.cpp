cebb3e340a5c7ab74e7ea1a34a39c7dab0040022
Martin Kiewitz <m_kiewitz@users.sourceforge.net>
2014-06-03 22:42:56 +0200

    SCI32: fix crash during gabriel knight intro

--- engines/sci/graphics/paint32.cpp
+++ engines/sci/graphics/paint32.cpp
@@ -43,8 +43,12 @@ GfxPaint32::~GfxPaint32() {
 
 void GfxPaint32::fillRect(Common::Rect rect, byte color) {
 	int16 y, x;
-	for (y = rect.top; y < rect.bottom; y++) {
-		for (x = rect.left; x < rect.right; x++) {
+	Common::Rect clipRect = rect;
+
+	clipRect.clip(_screen->getWidth(), _screen->getHeight());
+	
+	for (y = clipRect.top; y < clipRect.bottom; y++) {
+		for (x = clipRect.left; x < clipRect.right; x++) {
 			_screen->putPixel(x, y, GFX_SCREEN_MASK_VISUAL, color, 0, 0);
 		}
 	}
