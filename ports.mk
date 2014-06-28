7ce31f3d0ab40bd8b4c1d6b1ffccac1a47125325
raziel- <raziel-@users.noreply.github.com>
2014-06-28 21:58:24 +0300

    Remove AmigaOS4 specific changes from ports.mk

--- ports.mk
+++ ports.mk
@@ -324,22 +324,5 @@ endif
 	@echo Now run
 	@echo "\tgit commit 'DISTS: Generated Code::Blocks and MSVC project files'"
 
-#
-# AmigaOS specific
-#
-
-# Special target to create an AmigaOS snapshot installation
-aos4dist: $(EXECUTABLE)
-	mkdir -p $(AOS4PATH)
-	mkdir -p $(AOS4PATH)/themes
-	mkdir -p $(AOS4PATH)/extras
-	$(STRIP) $(EXECUTABLE) -o $(AOS4PATH)/$(EXECUTABLE)
-	cp ${srcdir}/icons/scummvm.info $(AOS4PATH)/$(EXECUTABLE).info
-	cp $(DIST_FILES_THEMES) $(AOS4PATH)/themes/
-ifdef DIST_FILES_ENGINEDATA
-	cp $(DIST_FILES_ENGINEDATA) $(AOS4PATH)/extras/
-endif
-	cp $(DIST_FILES_DOCS) $(AOS4PATH)
-
 # Mark special targets as phony
 .PHONY: deb bundle osxsnap win32dist install uninstall
