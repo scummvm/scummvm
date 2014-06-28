subr3v/residualvm

TINYGL: Refactored fill functions inside ZBuffer struct.

graphics/tinygl/clip.cpp
@@ -66,7 +66,7 @@ void gl_draw_point(GLContext *c, GLVertex *p0) {
 		if (c->render_mode == TGL_SELECT) {
 			gl_add_select(c, p0->zp.z, p0->zp.z);
 		} else {
-			ZB_plot(c->zb, &p0->zp);
+			c->zb->plot(&p0->zp);
 		}
 	}
 }
@@ -123,9 +123,9 @@ void gl_draw_line(GLContext *c, GLVertex *p1, GLVertex *p2) {
 			gl_add_select1(c, p1->zp.z, p2->zp.z, p2->zp.z);
 		} else {
 			if (c->depth_test)
-				ZB_line_z(c->zb, &p1->zp, &p2->zp);
+				c->zb->line_z(&p1->zp, &p2->zp);
 			else
-				ZB_line(c->zb, &p1->zp, &p2->zp);
+				c->zb->line(&p1->zp, &p2->zp);
 		}
 	} else if ((cc1 & cc2) != 0) {
 		return;
@@ -153,9 +153,9 @@ void gl_draw_line(GLContext *c, GLVertex *p1, GLVertex *p2) {
 			gl_transform_to_viewport(c, &q2);
 
 			if (c->depth_test)
-				ZB_line_z(c->zb, &q1.zp, &q2.zp);
+				c->zb->line_z(&q1.zp, &q2.zp);
 			else
-				ZB_line(c->zb, &q1.zp, &q2.zp);
+				c->zb->line(&q1.zp, &q2.zp);
 		}
 	}
 }
@@ -389,24 +389,24 @@ void gl_draw_triangle_fill(GLContext *c, GLVertex *p0, GLVertex *p1, GLVertex *p
 
 	if (c->color_mask == 0) {
 		// FIXME: Accept more than just 0 or 1.
-		ZB_fillTriangleDepthOnly(c->zb, &p0->zp, &p1->zp, &p2->zp);
+		c->zb->fillTriangleDepthOnly(&p0->zp, &p1->zp, &p2->zp);
 	}
 	if (c->shadow_mode & 1) {
 		assert(c->zb->shadow_mask_buf);
-		ZB_fillTriangleFlatShadowMask(c->zb, &p0->zp, &p1->zp, &p2->zp);
+		c->zb->fillTriangleFlatShadowMask(&p0->zp, &p1->zp, &p2->zp);
 	} else if (c->shadow_mode & 2) {
 		assert(c->zb->shadow_mask_buf);
-		ZB_fillTriangleFlatShadow(c->zb, &p0->zp, &p1->zp, &p2->zp);
+		c->zb->fillTriangleFlatShadow(&p0->zp, &p1->zp, &p2->zp);
 	} else if (c->texture_2d_enabled) {
 #ifdef TINYGL_PROFILE
 		count_triangles_textured++;
 #endif
 		c->zb->setTexture(c->current_texture->images[0].pixmap);
-		ZB_fillTriangleMappingPerspective(c->zb, &p0->zp, &p1->zp, &p2->zp);
+		c->zb->fillTriangleMappingPerspective(&p0->zp, &p1->zp, &p2->zp);
 	} else if (c->current_shade_model == TGL_SMOOTH) {
-		ZB_fillTriangleSmooth(c->zb, &p0->zp, &p1->zp, &p2->zp);
+		c->zb->fillTriangleSmooth(&p0->zp, &p1->zp, &p2->zp);
 	} else {
-		ZB_fillTriangleFlat(c->zb, &p0->zp, &p1->zp, &p2->zp);
+		c->zb->fillTriangleFlat(&p0->zp, &p1->zp, &p2->zp);
 	}
 }
 
@@ -415,29 +415,29 @@ void gl_draw_triangle_fill(GLContext *c, GLVertex *p0, GLVertex *p1, GLVertex *p
 void gl_draw_triangle_line(GLContext *c, GLVertex *p0, GLVertex *p1, GLVertex *p2) {
 	if (c->depth_test) {
 		if (p0->edge_flag)
-			ZB_line_z(c->zb, &p0->zp, &p1->zp);
+			c->zb->line_z(&p0->zp, &p1->zp);
 		if (p1->edge_flag)
-			ZB_line_z(c->zb, &p1->zp, &p2->zp);
+			c->zb->line_z(&p1->zp, &p2->zp);
 		if (p2->edge_flag)
-			ZB_line_z(c->zb, &p2->zp, &p0->zp);
+			c->zb->line_z(&p2->zp, &p0->zp);
 	} else {
 		if (p0->edge_flag)
-			ZB_line(c->zb, &p0->zp, &p1->zp);
+			c->zb->line(&p0->zp, &p1->zp);
 		if (p1->edge_flag)
-			ZB_line(c->zb, &p1->zp, &p2->zp);
+			c->zb->line(&p1->zp, &p2->zp);
 		if (p2->edge_flag)
-			ZB_line(c->zb, &p2->zp, &p0->zp);
+			c->zb->line(&p2->zp, &p0->zp);
 	}
 }
 
 // Render a clipped triangle in point mode
 void gl_draw_triangle_point(GLContext *c, GLVertex *p0, GLVertex *p1, GLVertex *p2) {
 	if (p0->edge_flag)
-		ZB_plot(c->zb, &p0->zp);
+		c->zb->plot(&p0->zp);
 	if (p1->edge_flag)
-		ZB_plot(c->zb, &p1->zp);
+		c->zb->plot(&p1->zp);
 	if (p2->edge_flag)
-		ZB_plot(c->zb, &p2->zp);
+		c->zb->plot(&p2->zp);
 }
 
 } // end of namespace TinyGL

graphics/tinygl/zbuffer.h
@@ -26,7 +26,7 @@ namespace TinyGL {
 // display modes
 #define ZB_MODE_5R6G5B  1  // true color 16 bits
 
-#define RGB_TO_PIXEL(r,g,b) zb->cmode.RGBToColor(r, g, b)
+#define RGB_TO_PIXEL(r,g,b) cmode.RGBToColor(r, g, b)
 typedef byte PIXEL;
 
 #define PSZSH 4
@@ -37,6 +37,14 @@ struct Buffer {
 	byte *pbuf;
 	unsigned int *zbuf;
 	bool used;
+};
+
+struct ZBufferPoint {
+	int x, y, z;   // integer coordinates in the zbuffer
+	int s, t;      // coordinates for the mapping
+	int r, g, b;   // color indexes
+
+	float sz, tz;  // temporary coordinates for mapping
 };
 
 struct ZBuffer {
@@ -56,6 +64,22 @@ struct ZBuffer {
 	void selectOffscreenBuffer(Buffer *buffer);
 	void clearOffscreenBuffer(Buffer *buffer);
 	void setTexture(const Graphics::PixelBuffer &texture);
+
+	void fillTriangleMappingPerspective(ZBufferPoint *p0, ZBufferPoint *p1, ZBufferPoint *p2);
+	void fillTriangleDepthOnly(ZBufferPoint *p0, ZBufferPoint *p1, ZBufferPoint *p2);
+	void fillTriangleFlat(ZBufferPoint *p0, ZBufferPoint *p1, ZBufferPoint *p2);
+	void fillTriangleFlatShadowMask(ZBufferPoint *p0, ZBufferPoint *p1, ZBufferPoint *p2);
+	void fillTriangleFlatShadow(ZBufferPoint *p0, ZBufferPoint *p1, ZBufferPoint *p2);
+	void fillTriangleSmooth(ZBufferPoint *p0, ZBufferPoint *p1, ZBufferPoint *p2);
+	void fillTriangleMapping(ZBufferPoint *p0, ZBufferPoint *p1, ZBufferPoint *p2);
+
+	void plot(ZBufferPoint *p);
+	void line(ZBufferPoint *p1, ZBufferPoint *p2);
+	void line_z(ZBufferPoint *p1, ZBufferPoint *p2);
+	void line_flat_z(ZBufferPoint *p1, ZBufferPoint *p2, int color);
+	void line_interp_z(ZBufferPoint *p1, ZBufferPoint *p2);
+	void line_flat(ZBufferPoint *p1, ZBufferPoint *p2, int color);
+	void line_interp(ZBufferPoint *p1, ZBufferPoint *p2);
 
 	int xsize, ysize;
 	int linesize; // line size, in bytes
@@ -77,40 +101,6 @@ struct ZBuffer {
 	int *ctable;
 	Graphics::PixelBuffer current_texture;
 };
-
-struct ZBufferPoint {
-	int x, y, z;   // integer coordinates in the zbuffer
-	int s, t;      // coordinates for the mapping
-	int r, g, b;   // color indexes
-
-	float sz, tz;  // temporary coordinates for mapping
-};
-
-// zline.c
-
-void ZB_plot(ZBuffer *zb, ZBufferPoint *p);
-void ZB_line(ZBuffer *zb, ZBufferPoint *p1, ZBufferPoint *p2);
-void ZB_line_z(ZBuffer *zb, ZBufferPoint *p1, ZBufferPoint *p2);
-
-// ztriangle.c */
-
-//void ZB_setTexture(ZBuffer *zb, const Graphics::PixelBuffer &texture);
-void ZB_fillTriangleDepthOnly(ZBuffer *zb, ZBufferPoint *p1,
-							  ZBufferPoint *p2, ZBufferPoint *p3);
-void ZB_fillTriangleFlat(ZBuffer *zb, ZBufferPoint *p1,
-						 ZBufferPoint *p2, ZBufferPoint *p3);
-void ZB_fillTriangleFlatShadowMask(ZBuffer *zb, ZBufferPoint *p1,
-								   ZBufferPoint *p2, ZBufferPoint *p3);
-void ZB_fillTriangleFlatShadow(ZBuffer *zb, ZBufferPoint *p1,
-							   ZBufferPoint *p2, ZBufferPoint *p3);
-void ZB_fillTriangleSmooth(ZBuffer *zb, ZBufferPoint *p1,
-						   ZBufferPoint *p2, ZBufferPoint *p3);
-void ZB_fillTriangleMapping(ZBuffer *zb, ZBufferPoint *p1,
-							ZBufferPoint *p2, ZBufferPoint *p3);
-void ZB_fillTriangleMappingPerspective(ZBuffer *zb, ZBufferPoint *p0,
-									   ZBufferPoint *p1, ZBufferPoint *p2);
-typedef void (*ZB_fillTriangleFunc)(ZBuffer *, ZBufferPoint *,
-									ZBufferPoint *, ZBufferPoint *);
 
 // memory.c
 void gl_free(void *p);

graphics/tinygl/zline.cpp
@@ -5,12 +5,12 @@ namespace TinyGL {
 
 #define ZCMP(z,zpix) ((z) >= (zpix))
 
-void ZB_plot(ZBuffer *zb, ZBufferPoint *p) {
+void ZBuffer::plot(ZBufferPoint *p) {
 	unsigned int *pz;
 	PIXEL *pp;
 
-	pz = zb->zbuf + (p->y * zb->xsize + p->x);
-	pp = (PIXEL *)((char *) zb->pbuf.getRawBuffer() + zb->linesize * p->y + p->x * PSZB);
+	pz = zbuf + (p->y * xsize + p->x);
+	pp = (PIXEL *)((char *) pbuf.getRawBuffer() + linesize * p->y + p->x * PSZB);
 	if (ZCMP((unsigned int)p->z, *pz)) {
 		*pp = RGB_TO_PIXEL(p->r, p->g, p->b);
 		*pz = p->z;
@@ -18,28 +18,28 @@ void ZB_plot(ZBuffer *zb, ZBufferPoint *p) {
 }
 
 #define INTERP_Z
-static void ZB_line_flat_z(ZBuffer *zb, ZBufferPoint *p1, ZBufferPoint *p2, int color) {
+void ZBuffer::line_flat_z(ZBufferPoint *p1, ZBufferPoint *p2, int color) {
 #include "graphics/tinygl/zline.h"
 }
 
 // line with color interpolation
 #define INTERP_Z
 #define INTERP_RGB
-static void ZB_line_interp_z(ZBuffer *zb, ZBufferPoint *p1, ZBufferPoint *p2) {
+void ZBuffer::line_interp_z(ZBufferPoint *p1, ZBufferPoint *p2) {
 #include "graphics/tinygl/zline.h"
 }
 
 // no Z interpolation
-static void ZB_line_flat(ZBuffer *zb, ZBufferPoint *p1, ZBufferPoint *p2, int color) {
+void ZBuffer::line_flat(ZBufferPoint *p1, ZBufferPoint *p2, int color) {
 #include "graphics/tinygl/zline.h"
 }
 
 #define INTERP_RGB
-static void ZB_line_interp(ZBuffer *zb, ZBufferPoint *p1, ZBufferPoint *p2) {
+void ZBuffer::line_interp(ZBufferPoint *p1, ZBufferPoint *p2) {
 #include "graphics/tinygl/zline.h"
 }
 
-void ZB_line_z(ZBuffer *zb, ZBufferPoint *p1, ZBufferPoint *p2) {
+void ZBuffer::line_z(ZBufferPoint *p1, ZBufferPoint *p2) {
 	int color1, color2;
 
 	color1 = RGB_TO_PIXEL(p1->r, p1->g, p1->b);
@@ -47,13 +47,13 @@ void ZB_line_z(ZBuffer *zb, ZBufferPoint *p1, ZBufferPoint *p2) {
 
 	// choose if the line should have its color interpolated or not
 	if (color1 == color2) {
-		ZB_line_flat_z(zb, p1, p2, color1);
+		line_flat_z(p1, p2, color1);
 	} else {
-		ZB_line_interp_z(zb, p1, p2);
+		line_interp_z(p1, p2);
 	}
 }
 
-void ZB_line(ZBuffer *zb, ZBufferPoint *p1, ZBufferPoint *p2) {
+void ZBuffer::line(ZBufferPoint *p1, ZBufferPoint *p2) {
 	int color1, color2;
 
 	color1 = RGB_TO_PIXEL(p1->r, p1->g, p1->b);
@@ -61,9 +61,9 @@ void ZB_line(ZBuffer *zb, ZBufferPoint *p1, ZBufferPoint *p2) {
 
 	// choose if the line should have its color interpolated or not
 	if (color1 == color2) {
-		ZB_line_flat(zb, p1, p2, color1);
+		line_flat(p1, p2, color1);
 	} else {
-		ZB_line_interp(zb, p1, p2);
+		line_interp(p1, p2);
 	}
 }
 

scummvm/scummvm

graphics/tinygl/clip.cpp
@@ -66,7 +66,7 @@ void gl_draw_point(GLContext *c, GLVertex *p0) {
 		if (c->render_mode == TGL_SELECT) {
 			gl_add_select(c, p0->zp.z, p0->zp.z);
 		} else {
-			ZB_plot(c->zb, &p0->zp);
+			c->zb->plot(&p0->zp);
 		}
 	}
 }
@@ -118,9 +118,9 @@ void gl_draw_line(GLContext *c, GLVertex *p1, GLVertex *p2) {
 			gl_add_select1(c, p1->zp.z, p2->zp.z, p2->zp.z);
 		} else {
 			if (c->depth_test)
-				ZB_line_z(c->zb, &p1->zp, &p2->zp);
+				c->zb->fillLineZ(&p1->zp, &p2->zp);
 			else
-				ZB_line(c->zb, &p1->zp, &p2->zp);
+				c->zb->fillLine(&p1->zp, &p2->zp);
 		}
 	} else if ((cc1 & cc2) != 0) {
 		return;
@@ -148,9 +148,9 @@ void gl_draw_line(GLContext *c, GLVertex *p1, GLVertex *p2) {
 			gl_transform_to_viewport(c, &q2);
 
 			if (c->depth_test)
-				ZB_line_z(c->zb, &q1.zp, &q2.zp);
+				c->zb->fillLineZ(&q1.zp, &q2.zp);
 			else
-				ZB_line(c->zb, &q1.zp, &q2.zp);
+				c->zb->fillLine(&q1.zp, &q2.zp);
 		}
 	}
 }
@@ -385,24 +385,24 @@ void gl_draw_triangle_fill(GLContext *c, GLVertex *p0, GLVertex *p1, GLVertex *p
 
 	if (c->color_mask == 0) {
 		// FIXME: Accept more than just 0 or 1.
-		ZB_fillTriangleDepthOnly(c->zb, &p0->zp, &p1->zp, &p2->zp);
+		c->zb->fillTriangleDepthOnly(&p0->zp, &p1->zp, &p2->zp);
 	}
 	if (c->shadow_mode & 1) {
 		assert(c->zb->shadow_mask_buf);
-		ZB_fillTriangleFlatShadowMask(c->zb, &p0->zp, &p1->zp, &p2->zp);
+		c->zb->fillTriangleFlatShadowMask(&p0->zp, &p1->zp, &p2->zp);
 	} else if (c->shadow_mode & 2) {
 		assert(c->zb->shadow_mask_buf);
-		ZB_fillTriangleFlatShadow(c->zb, &p0->zp, &p1->zp, &p2->zp);
+		c->zb->fillTriangleFlatShadow(&p0->zp, &p1->zp, &p2->zp);
 	} else if (c->texture_2d_enabled) {
 #ifdef TINYGL_PROFILE
 		count_triangles_textured++;
 #endif
 		c->zb->setTexture(c->current_texture->images[0].pixmap);
-		ZB_fillTriangleMappingPerspective(c->zb, &p0->zp, &p1->zp, &p2->zp);
+		c->zb->fillTriangleMappingPerspective(&p0->zp, &p1->zp, &p2->zp);
 	} else if (c->current_shade_model == TGL_SMOOTH) {
-		ZB_fillTriangleSmooth(c->zb, &p0->zp, &p1->zp, &p2->zp);
+		c->zb->fillTriangleSmooth(&p0->zp, &p1->zp, &p2->zp);
 	} else {
-		ZB_fillTriangleFlat(c->zb, &p0->zp, &p1->zp, &p2->zp);
+		c->zb->fillTriangleFlat(&p0->zp, &p1->zp, &p2->zp);
 	}
 }
 
@@ -411,29 +411,29 @@ void gl_draw_triangle_fill(GLContext *c, GLVertex *p0, GLVertex *p1, GLVertex *p
 void gl_draw_triangle_line(GLContext *c, GLVertex *p0, GLVertex *p1, GLVertex *p2) {
 	if (c->depth_test) {
 		if (p0->edge_flag)
-			ZB_line_z(c->zb, &p0->zp, &p1->zp);
+			c->zb->fillLineZ(&p0->zp, &p1->zp);
 		if (p1->edge_flag)
-			ZB_line_z(c->zb, &p1->zp, &p2->zp);
+			c->zb->fillLineZ(&p1->zp, &p2->zp);
 		if (p2->edge_flag)
-			ZB_line_z(c->zb, &p2->zp, &p0->zp);
+			c->zb->fillLineZ(&p2->zp, &p0->zp);
 	} else {
 		if (p0->edge_flag)
-			ZB_line(c->zb, &p0->zp, &p1->zp);
+			c->zb->fillLine(&p0->zp, &p1->zp);
 		if (p1->edge_flag)
-			ZB_line(c->zb, &p1->zp, &p2->zp);
+			c->zb->fillLine(&p1->zp, &p2->zp);
 		if (p2->edge_flag)
-			ZB_line(c->zb, &p2->zp, &p0->zp);
+			c->zb->fillLine(&p2->zp, &p0->zp);
 	}
 }
 
 // Render a clipped triangle in point mode
 void gl_draw_triangle_point(GLContext *c, GLVertex *p0, GLVertex *p1, GLVertex *p2) {
 	if (p0->edge_flag)
-		ZB_plot(c->zb, &p0->zp);
+		c->zb->plot(&p0->zp);
 	if (p1->edge_flag)
-		ZB_plot(c->zb, &p1->zp);
+		c->zb->plot(&p1->zp);
 	if (p2->edge_flag)
-		ZB_plot(c->zb, &p2->zp);
+		c->zb->plot(&p2->zp);
 }
 
 } // end of namespace TinyGL

graphics/tinygl/zbuffer.h
@@ -26,7 +26,7 @@ namespace TinyGL {
 // display modes
 #define ZB_MODE_5R6G5B  1  // true color 16 bits
 
-#define RGB_TO_PIXEL(r,g,b) zb->cmode.RGBToColor(r, g, b)
+#define RGB_TO_PIXEL(r,g,b) cmode.RGBToColor(r, g, b)
 typedef byte PIXEL;
 
 #define PSZSH 4
@@ -37,6 +37,14 @@ struct Buffer {
 	byte *pbuf;
 	unsigned int *zbuf;
 	bool used;
+};
+
+struct ZBufferPoint {
+	int x, y, z;   // integer coordinates in the zbuffer
+	int s, t;      // coordinates for the mapping
+	int r, g, b;   // color indexes
+
+	float sz, tz;  // temporary coordinates for mapping
 };
 
 struct ZBuffer {
@@ -56,6 +64,22 @@ struct ZBuffer {
 	void selectOffscreenBuffer(Buffer *buffer);
 	void clearOffscreenBuffer(Buffer *buffer);
 	void setTexture(const Graphics::PixelBuffer &texture);
+
+	void fillTriangleMappingPerspective(ZBufferPoint *p0, ZBufferPoint *p1, ZBufferPoint *p2);
+	void fillTriangleDepthOnly(ZBufferPoint *p0, ZBufferPoint *p1, ZBufferPoint *p2);
+	void fillTriangleFlat(ZBufferPoint *p0, ZBufferPoint *p1, ZBufferPoint *p2);
+	void fillTriangleFlatShadowMask(ZBufferPoint *p0, ZBufferPoint *p1, ZBufferPoint *p2);
+	void fillTriangleFlatShadow(ZBufferPoint *p0, ZBufferPoint *p1, ZBufferPoint *p2);
+	void fillTriangleSmooth(ZBufferPoint *p0, ZBufferPoint *p1, ZBufferPoint *p2);
+	void fillTriangleMapping(ZBufferPoint *p0, ZBufferPoint *p1, ZBufferPoint *p2);
+
+	void plot(ZBufferPoint *p);
+	void fillLine(ZBufferPoint *p1, ZBufferPoint *p2);
+	void fillLineZ(ZBufferPoint *p1, ZBufferPoint *p2);
+	void fillLineFlatZ(ZBufferPoint *p1, ZBufferPoint *p2, int color);
+	void fillLineInterpZ(ZBufferPoint *p1, ZBufferPoint *p2);
+	void fillLineFlat(ZBufferPoint *p1, ZBufferPoint *p2, int color);
+	void fillLineInterp(ZBufferPoint *p1, ZBufferPoint *p2);
 
 	int xsize, ysize;
 	int linesize; // line size, in bytes
@@ -77,35 +101,6 @@ struct ZBuffer {
 	int *ctable;
 	Graphics::PixelBuffer current_texture;
 };
-
-struct ZBufferPoint {
-	int x, y, z;   // integer coordinates in the zbuffer
-	int s, t;      // coordinates for the mapping
-	int r, g, b;   // color indexes
-
-	float sz, tz;  // temporary coordinates for mapping
-};
-
-void ZB_plot(ZBuffer *zb, ZBufferPoint *p);
-void ZB_line(ZBuffer *zb, ZBufferPoint *p1, ZBufferPoint *p2);
-void ZB_line_z(ZBuffer *zb, ZBufferPoint *p1, ZBufferPoint *p2);
-
-void ZB_fillTriangleDepthOnly(ZBuffer *zb, ZBufferPoint *p1,
-							  ZBufferPoint *p2, ZBufferPoint *p3);
-void ZB_fillTriangleFlat(ZBuffer *zb, ZBufferPoint *p1,
-						 ZBufferPoint *p2, ZBufferPoint *p3);
-void ZB_fillTriangleFlatShadowMask(ZBuffer *zb, ZBufferPoint *p1,
-								   ZBufferPoint *p2, ZBufferPoint *p3);
-void ZB_fillTriangleFlatShadow(ZBuffer *zb, ZBufferPoint *p1,
-							   ZBufferPoint *p2, ZBufferPoint *p3);
-void ZB_fillTriangleSmooth(ZBuffer *zb, ZBufferPoint *p1,
-						   ZBufferPoint *p2, ZBufferPoint *p3);
-void ZB_fillTriangleMapping(ZBuffer *zb, ZBufferPoint *p1,
-							ZBufferPoint *p2, ZBufferPoint *p3);
-void ZB_fillTriangleMappingPerspective(ZBuffer *zb, ZBufferPoint *p0,
-									   ZBufferPoint *p1, ZBufferPoint *p2);
-typedef void (*ZB_fillTriangleFunc)(ZBuffer *, ZBufferPoint *,
-									ZBufferPoint *, ZBufferPoint *);
 
 // memory.c
 void gl_free(void *p);

graphics/tinygl/zline.cpp
@@ -5,12 +5,12 @@ namespace TinyGL {
 
 #define ZCMP(z,zpix) ((z) >= (zpix))
 
-void ZB_plot(ZBuffer *zb, ZBufferPoint *p) {
+void ZBuffer::plot(ZBufferPoint *p) {
 	unsigned int *pz;
 	PIXEL *pp;
 
-	pz = zb->zbuf + (p->y * zb->xsize + p->x);
-	pp = (PIXEL *)((char *) zb->pbuf.getRawBuffer() + zb->linesize * p->y + p->x * PSZB);
+	pz = zbuf + (p->y * xsize + p->x);
+	pp = (PIXEL *)((char *) pbuf.getRawBuffer() + linesize * p->y + p->x * PSZB);
 	if (ZCMP((unsigned int)p->z, *pz)) {
 		*pp = RGB_TO_PIXEL(p->r, p->g, p->b);
 		*pz = p->z;
@@ -18,28 +18,28 @@ void ZB_plot(ZBuffer *zb, ZBufferPoint *p) {
 }
 
 #define INTERP_Z
-static void ZB_line_flat_z(ZBuffer *zb, ZBufferPoint *p1, ZBufferPoint *p2, int color) {
+void ZBuffer::fillLineFlatZ(ZBufferPoint *p1, ZBufferPoint *p2, int color) {
 #include "graphics/tinygl/zline.h"
 }
 
 // line with color interpolation
 #define INTERP_Z
 #define INTERP_RGB
-static void ZB_line_interp_z(ZBuffer *zb, ZBufferPoint *p1, ZBufferPoint *p2) {
+void ZBuffer::fillLineInterpZ(ZBufferPoint *p1, ZBufferPoint *p2) {
 #include "graphics/tinygl/zline.h"
 }
 
 // no Z interpolation
-static void ZB_line_flat(ZBuffer *zb, ZBufferPoint *p1, ZBufferPoint *p2, int color) {
+void ZBuffer::fillLineFlat(ZBufferPoint *p1, ZBufferPoint *p2, int color) {
 #include "graphics/tinygl/zline.h"
 }
 
 #define INTERP_RGB
-static void ZB_line_interp(ZBuffer *zb, ZBufferPoint *p1, ZBufferPoint *p2) {
+void ZBuffer::fillLineInterp(ZBufferPoint *p1, ZBufferPoint *p2) {
 #include "graphics/tinygl/zline.h"
 }
 
-void ZB_line_z(ZBuffer *zb, ZBufferPoint *p1, ZBufferPoint *p2) {
+void ZBuffer::fillLineZ(ZBufferPoint *p1, ZBufferPoint *p2) {
 	int color1, color2;
 
 	color1 = RGB_TO_PIXEL(p1->r, p1->g, p1->b);
@@ -47,13 +47,13 @@ void ZB_line_z(ZBuffer *zb, ZBufferPoint *p1, ZBufferPoint *p2) {
 
 	// choose if the line should have its color interpolated or not
 	if (color1 == color2) {
-		ZB_line_flat_z(zb, p1, p2, color1);
+		fillLineFlatZ(p1, p2, color1);
 	} else {
-		ZB_line_interp_z(zb, p1, p2);
+		fillLineInterpZ(p1, p2);
 	}
 }
 
-void ZB_line(ZBuffer *zb, ZBufferPoint *p1, ZBufferPoint *p2) {
+void ZBuffer::fillLine(ZBufferPoint *p1, ZBufferPoint *p2) {
 	int color1, color2;
 
 	color1 = RGB_TO_PIXEL(p1->r, p1->g, p1->b);
@@ -61,9 +61,9 @@ void ZB_line(ZBuffer *zb, ZBufferPoint *p1, ZBufferPoint *p2) {
 
 	// choose if the line should have its color interpolated or not
 	if (color1 == color2) {
-		ZB_line_flat(zb, p1, p2, color1);
+		fillLineFlat(p1, p2, color1);
 	} else {
-		ZB_line_interp(zb, p1, p2);
+		fillLineInterp(p1, p2);
 	}
 }
 