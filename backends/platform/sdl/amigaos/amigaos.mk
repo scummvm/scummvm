a3f1f9bd252729b96efc6174d28fae6ebcc526d5
raziel- <raziel-@users.noreply.github.com>
2014-06-29 21:07:53 +0300

    Make it clear that it's "amiga"os4

--- backends/platform/sdl/amigaos/amigaos.mk
+++ backends/platform/sdl/amigaos/amigaos.mk
@@ -1,11 +1,11 @@
-aos4dist: $(EXECUTABLE)
-	mkdir -p $(AOS4PATH)
-	mkdir -p $(AOS4PATH)/themes
-	mkdir -p $(AOS4PATH)/extras
-	$(STRIP) $(EXECUTABLE) -o $(AOS4PATH)/$(EXECUTABLE)
-	cp ${srcdir}/icons/scummvm.info $(AOS4PATH)/$(EXECUTABLE).info
-	cp $(DIST_FILES_THEMES) $(AOS4PATH)/themes/
+amigaos4dist: $(EXECUTABLE)
+	mkdir -p $(AMIGAOS4PATH)
+	mkdir -p $(AMIGAOS4PATH)/themes
+	mkdir -p $(AMIGAOS4PATH)/extras
+	$(STRIP) $(EXECUTABLE) -o $(AMIGAOS4PATH)/$(EXECUTABLE)
+	cp ${srcdir}/icons/scummvm.info $(AMIGAOS4PATH)/$(EXECUTABLE).info
+	cp $(DIST_FILES_THEMES) $(AMIGAOS4PATH)/themes/
 ifdef DIST_FILES_ENGINEDATA
-	cp $(DIST_FILES_ENGINEDATA) $(AOS4PATH)/extras/
+	cp $(DIST_FILES_ENGINEDATA) $(AMIGAOS4PATH)/extras/
 endif
-	cp $(DIST_FILES_DOCS) $(AOS4PATH)
+	cp $(DIST_FILES_DOCS) $(AMIGAOS4PATH)
