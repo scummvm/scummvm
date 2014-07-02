aeaac02332a8faa32a7f6c534309b71432dd3a2b
uruk <koppirnyo@gmail.com>
2014-07-03 00:50:42 +0200

    CGE2: Silence warning.

--- engines/cge2/spare.cpp
+++ engines/cge2/spare.cpp
@@ -36,6 +36,7 @@ void Spare::sync(Common::Serializer &s) {
 
 void Spare::clear() {
 	for (int i = 0; i < _container.size(); i++)
+	for (uint i = 0; i < _container.size(); i++)
 		delete _container[i];
 
 	_container.clear();
