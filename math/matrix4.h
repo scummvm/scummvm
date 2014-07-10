46a91377d8a151deebe0dbdf1c30c8aadfd2f900
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2014-07-10 07:27:18 +0200

    MATH: added few GLU helpers

--- math/matrix4.h
+++ math/matrix4.h
@@ -26,6 +26,7 @@
 #include "math/rotation3d.h"
 #include "math/squarematrix.h"
 #include "math/vector3d.h"
+#include "math/vector4d.h"
 #include "math/matrix3.h"
 
 namespace Math {
@@ -92,6 +93,153 @@ public:
 		}
 
 		return result;
+	}
+
+	inline Vector4d transform(const Vector4d &v) const {
+		Vector4d result;
+		const float *d1 = getData();
+		const float *d2 = v.getData();
+		float *r = result.getData();
+
+		for (int i = 0; i < 4; i++) {
+			r[i] = d2[0] * d1[0 * 4 + i] +
+			       d2[1] * d1[1 * 4 + i] +
+			       d2[2] * d1[2 * 4 + i] +
+			       d2[3] * d1[3 * 4 + i];
+		}
+
+		return result;
+	}
+
+	inline bool inverse() const {
+		Matrix<4, 4> invMatrix;
+		float *inv = invMatrix.getData();
+		float *m = (float *)getData();
+
+		inv[0] = m[5]  * m[10] * m[15] - 
+			m[5]  * m[11] * m[14] - 
+			m[9]  * m[6]  * m[15] + 
+			m[9]  * m[7]  * m[14] +
+			m[13] * m[6]  * m[11] - 
+			m[13] * m[7]  * m[10];
+
+		inv[4] = -m[4]  * m[10] * m[15] + 
+			m[4]  * m[11] * m[14] + 
+			m[8]  * m[6]  * m[15] - 
+			m[8]  * m[7]  * m[14] - 
+			m[12] * m[6]  * m[11] + 
+			m[12] * m[7]  * m[10];
+
+		inv[8] = m[4]  * m[9] * m[15] - 
+			m[4]  * m[11] * m[13] - 
+			m[8]  * m[5] * m[15] + 
+			m[8]  * m[7] * m[13] + 
+			m[12] * m[5] * m[11] - 
+			m[12] * m[7] * m[9];
+
+		inv[12] = -m[4]  * m[9] * m[14] + 
+			m[4]  * m[10] * m[13] +
+			m[8]  * m[5] * m[14] - 
+			m[8]  * m[6] * m[13] - 
+			m[12] * m[5] * m[10] + 
+			m[12] * m[6] * m[9];
+
+		inv[1] = -m[1]  * m[10] * m[15] + 
+			m[1]  * m[11] * m[14] + 
+			m[9]  * m[2] * m[15] - 
+			m[9]  * m[3] * m[14] - 
+			m[13] * m[2] * m[11] + 
+			m[13] * m[3] * m[10];
+
+		inv[5] = m[0]  * m[10] * m[15] - 
+			m[0]  * m[11] * m[14] - 
+			m[8]  * m[2] * m[15] + 
+			m[8]  * m[3] * m[14] + 
+			m[12] * m[2] * m[11] - 
+			m[12] * m[3] * m[10];
+
+		inv[9] = -m[0]  * m[9] * m[15] + 
+			m[0]  * m[11] * m[13] + 
+			m[8]  * m[1] * m[15] - 
+			m[8]  * m[3] * m[13] - 
+			m[12] * m[1] * m[11] + 
+			m[12] * m[3] * m[9];
+
+		inv[13] = m[0]  * m[9] * m[14] - 
+			m[0]  * m[10] * m[13] - 
+			m[8]  * m[1] * m[14] + 
+			m[8]  * m[2] * m[13] + 
+			m[12] * m[1] * m[10] - 
+			m[12] * m[2] * m[9];
+
+		inv[2] = m[1]  * m[6] * m[15] - 
+			m[1]  * m[7] * m[14] - 
+			m[5]  * m[2] * m[15] + 
+			m[5]  * m[3] * m[14] + 
+			m[13] * m[2] * m[7] - 
+			m[13] * m[3] * m[6];
+
+		inv[6] = -m[0]  * m[6] * m[15] + 
+			m[0]  * m[7] * m[14] + 
+			m[4]  * m[2] * m[15] - 
+			m[4]  * m[3] * m[14] - 
+			m[12] * m[2] * m[7] + 
+			m[12] * m[3] * m[6];
+
+		inv[10] = m[0]  * m[5] * m[15] - 
+			m[0]  * m[7] * m[13] - 
+			m[4]  * m[1] * m[15] + 
+			m[4]  * m[3] * m[13] + 
+			m[12] * m[1] * m[7] - 
+			m[12] * m[3] * m[5];
+
+		inv[14] = -m[0]  * m[5] * m[14] + 
+			m[0]  * m[6] * m[13] + 
+			m[4]  * m[1] * m[14] - 
+			m[4]  * m[2] * m[13] - 
+			m[12] * m[1] * m[6] + 
+			m[12] * m[2] * m[5];
+
+		inv[3] = -m[1] * m[6] * m[11] + 
+			m[1] * m[7] * m[10] + 
+			m[5] * m[2] * m[11] - 
+			m[5] * m[3] * m[10] - 
+			m[9] * m[2] * m[7] + 
+			m[9] * m[3] * m[6];
+
+		inv[7] = m[0] * m[6] * m[11] - 
+			m[0] * m[7] * m[10] - 
+			m[4] * m[2] * m[11] + 
+			m[4] * m[3] * m[10] + 
+			m[8] * m[2] * m[7] - 
+			m[8] * m[3] * m[6];
+
+		inv[11] = -m[0] * m[5] * m[11] + 
+			m[0] * m[7] * m[9] + 
+			m[4] * m[1] * m[11] - 
+			m[4] * m[3] * m[9] - 
+			m[8] * m[1] * m[7] + 
+			m[8] * m[3] * m[5];
+
+		inv[15] = m[0] * m[5] * m[10] - 
+			m[0] * m[6] * m[9] - 
+			m[4] * m[1] * m[10] + 
+			m[4] * m[2] * m[9] + 
+			m[8] * m[1] * m[6] - 
+			m[8] * m[2] * m[5];
+
+		float det = m[0] * inv[0] + m[1] * inv[4] + m[2] * inv[8] + m[3] * inv[12];
+
+		if (det == 0)
+			return false;
+
+		det = 1.0 / det;
+
+		for (int i = 0; i < 16; i++) {
+			m[i] = inv[i] * det;
+		}
+
+		return true;
 	}
 };
 
--- math/module.mk
+++ math/module.mk
@@ -1,19 +1,20 @@
 MODULE := math
 
 MODULE_OBJS := \
+	aabb.o \
 	angle.o \
+	frustum.o \
+	glmath.o \
+	line2d.o \
+	line3d.o \
 	matrix3.o \
 	matrix4.o \
-	line3d.o \
-	line2d.o \
+	plane.o \
 	quat.o \
 	rect2d.o \
 	vector2d.o \
 	vector3d.o \
-	vector4d.o \
-	aabb.o \
-	frustum.o \
-	plane.o
+	vector4d.o
 
 # Include common rules
 include $(srcdir)/rules.mk
