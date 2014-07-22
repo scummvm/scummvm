ff58b301b95ac96d5b20a5a176f5fdcee07f1a5c
Stefano Musumeci <subr3v@gmail.com>
2014-07-22 20:26:42 +0200

    TINYGL: Added stub implementation for the two draw call categories.

--- graphics/tinygl/zrect.cpp
+++ graphics/tinygl/zrect.cpp
@@ -2,6 +2,30 @@
 
 namespace Graphics {
 
+	DrawCall::DrawCall(DrawCallType type) : _type(type) {
+	}
 
+	BlittingDrawCall::BlittingDrawCall(TinyGL::GLContext *c) : DrawCall(DrawCall_Blitting) {
 
+	}
+
+	RasterizationDrawCall::RasterizationDrawCall(TinyGL::GLContext *c) : DrawCall(DrawCall_Rasterization) {
+
+	}
+
+	void RasterizationDrawCall::execute() const {
+
+	}
+
+	void RasterizationDrawCall::execute( const Common::Rect &clippingRectangle ) const {
+
+	}
+
+	void BlittingDrawCall::execute() const {
+
+	}
+
+	void BlittingDrawCall::execute(const Common::Rect &clippingRectangle) const {
+
+	}
 }
