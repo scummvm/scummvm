0b7c8140f8e4ca37fcef7be7617a0f6a4ef1b99e
Stefano Musumeci <subr3v@gmail.com>
2014-07-23 15:14:35 +0200

    TINYGL: Fixed formatting namepace indentation.

--- graphics/tinygl/zrect.cpp
+++ graphics/tinygl/zrect.cpp
@@ -6,30 +6,31 @@ void tglPresentBuffer() {
 
 namespace Graphics {
 
-	DrawCall::DrawCall(DrawCallType type) : _type(type) {
-	}
-
-	RasterizationDrawCall::RasterizationDrawCall(TinyGL::GLContext *c) : DrawCall(DrawCall_Rasterization) {
-
-	}
-
-	void RasterizationDrawCall::execute() const {
-
-	}
-
-	void RasterizationDrawCall::execute( const Common::Rect &clippingRectangle ) const {
-
-	}
-
-	BlittingDrawCall::BlittingDrawCall(TinyGL::GLContext *c) : DrawCall(DrawCall_Blitting) {
-
-	}
-
-	void BlittingDrawCall::execute() const {
-
-	}
-
-	void BlittingDrawCall::execute(const Common::Rect &clippingRectangle) const {
-
-	}
+DrawCall::DrawCall(DrawCallType type) : _type(type) {
 }
+
+RasterizationDrawCall::RasterizationDrawCall(TinyGL::GLContext *c) : DrawCall(DrawCall_Rasterization) {
+
+}
+
+void RasterizationDrawCall::execute() const {
+
+}
+
+void RasterizationDrawCall::execute( const Common::Rect &clippingRectangle ) const {
+
+}
+
+BlittingDrawCall::BlittingDrawCall(TinyGL::GLContext *c) : DrawCall(DrawCall_Blitting) {
+
+}
+
+void BlittingDrawCall::execute() const {
+
+}
+
+void BlittingDrawCall::execute(const Common::Rect &clippingRectangle) const {
+
+}
+
+} // end of namespace Graphics
