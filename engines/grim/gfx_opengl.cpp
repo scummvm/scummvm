372be9011180ff75ff8a20db51b9f18abce9abbb
Joseph Jezak <josejx@gentoo.org>
2014-07-31 15:54:35 -0400

    EMI: Make the bounding box coordinate orientation match grim's.

--- engines/grim/gfx_opengl.cpp
+++ engines/grim/gfx_opengl.cpp
@@ -491,9 +491,9 @@ void GfxOpenGL::getScreenBoundingBox(const EMIModel *model, int *x1, int *y1, in
 	}
 	
 	*x1 = (int)left;
-	*y1 = (int)bottom;
+	*y1 = (int)top;
 	*x2 = (int)right;
-	*y2 = (int)top;
+	*y2 = (int)bottom;
 }
 
 void GfxOpenGL::startActorDraw(const Actor *actor) {
