1c1b5dde73c24b5d5c607b9d0ee3f796cad279fd
Dries Harnie <dries.harnie@vub.ac.be>
2014-07-31 13:08:39 +0200

    EMI: Bump SaveGame MINOR_VERSION to 18
    As noted in #977 and #956.

--- engines/grim/savegame.cpp
+++ engines/grim/savegame.cpp
@@ -35,7 +35,7 @@ namespace Grim {
 #define SAVEGAME_FOOTERTAG  'ESAV'
 
 uint SaveGame::SAVEGAME_MAJOR_VERSION = 22;
-uint SaveGame::SAVEGAME_MINOR_VERSION = 17;
+uint SaveGame::SAVEGAME_MINOR_VERSION = 18;
 
 SaveGame *SaveGame::openForLoading(const Common::String &filename) {
 	Common::InSaveFile *inSaveFile = g_system->getSavefileManager()->openForLoading(filename);
