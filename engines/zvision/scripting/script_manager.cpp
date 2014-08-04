73d26bc2337e8afd03bfd73e6ccc0c0fb44a5950
Marisa-Chan <thunder_8888@mail.ru>
2014-08-04 16:33:36 +0700

    ZVISION: Don't focus on already focused control

--- engines/zvision/scripting/script_manager.cpp
+++ engines/zvision/scripting/script_manager.cpp
@@ -361,6 +361,8 @@ Control *ScriptManager::getControl(uint32 key) {
 void ScriptManager::focusControl(uint32 key) {
 	if (!_activeControls)
 		return;
+	if (_currentlyFocusedControl == key)
+		return;
 	for (ControlList::iterator iter = _activeControls->begin(); iter != _activeControls->end(); ++iter) {
 		uint32 controlKey = (*iter)->getKey();
 
