c70b2b04c60137ebbe50a940deb81fdc9c193c55
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2014-08-15 13:53:16 +0200

    AMIGAOS4: update cross build

--- backends/platform/sdl/amigaos/amigaos.mk
+++ backends/platform/sdl/amigaos/amigaos.mk
@@ -10,3 +10,30 @@ ifdef DIST_FILES_ENGINEDATA
 	cp $(DIST_FILES_ENGINEDATA) $(AMIGAOS4PATH)/extras/
 endif
 	cp $(DIST_FILES_DOCS) $(AMIGAOS4PATH)
+
+# Special target to cross create an AmigaOS snapshot installation
+amigaos4cross: $(EXECUTABLE)
+	mkdir -p ResidualVM
+	mkdir -p ResidualVM/themes
+	mkdir -p ResidualVM/extras
+	cp $(EXECUTABLE) ResidualVM/$(EXECUTABLE)
+	cp ${srcdir}/icons/residualvm.info ResidualVM/$(EXECUTABLE).info
+	cp $(DIST_FILES_THEMES) ResidualVM/themes/
+ifdef DIST_FILES_ENGINEDATA
+	cp $(DIST_FILES_ENGINEDATA) ResidualVM/extras/
+endif
+	cp $(srcdir)/AUTHORS ResidualVM/AUTHORS.txt
+	cp $(srcdir)/COPYING ResidualVM/COPYING.txt
+	cp $(srcdir)/COPYING.BSD ResidualVM/COPYING.BSD.txt
+	cp $(srcdir)/COPYING.LGPL ResidualVM/COPYING.LGPL.txt
+	cp $(srcdir)/COPYING.FREEFONT ResidualVM/COPYING.FREEFONT.txt
+	cp $(srcdir)/COPYING.ISC ResidualVM/COPYING.ISC.txt
+	cp $(srcdir)/COPYING.LUA ResidualVM/COPYING.LUA.txt
+	cp $(srcdir)/COPYING.MIT ResidualVM/COPYING.MIT.txt
+	cp $(srcdir)/COPYING.TINYGL ResidualVM/COPYING.TINYGL.txt
+	cp $(srcdir)/COPYRIGHT ResidualVM/COPYRIGHT.txt
+	cp $(srcdir)/KNOWN_BUGS ResidualVM/KNOWN_BUGS.txt
+	cp $(srcdir)/NEWS ResidualVM/NEWS.txt
+	cp $(srcdir)/doc/QuickStart ResidualVM/QuickStart.txt
+	cp $(srcdir)/README ResidualVM/README.txt
+	lha a residualvm-amigaos4.lha ResidualVM
--- ports.mk
+++ ports.mk
@@ -334,30 +334,5 @@ endif
 	unix2dos ResidualVMWin32/*.txt
 	unix2dos ResidualVMWin32/ResidualVM.iss
 
-# Special target to cross create an AmigaOS snapshot installation
-aos4dist-cross: $(EXECUTABLE)
-	mkdir -p ResidualVM
-	cp $(EXECUTABLE) ResidualVM/$(EXECUTABLE)
-	cp ${srcdir}/icons/residualvm.info ResidualVM/$(EXECUTABLE).info
-	cp $(DIST_FILES_THEMES) ResidualVM
-ifdef DIST_FILES_ENGINEDATA
-	cp $(DIST_FILES_ENGINEDATA) ResidualVM
-endif
-	cp $(srcdir)/AUTHORS ResidualVM/AUTHORS.txt
-	cp $(srcdir)/COPYING ResidualVM/COPYING.txt
-	cp $(srcdir)/COPYING.BSD ResidualVM/COPYING.BSD.txt
-	cp $(srcdir)/COPYING.LGPL ResidualVM/COPYING.LGPL.txt
-	cp $(srcdir)/COPYING.FREEFONT ResidualVM/COPYING.FREEFONT.txt
-	cp $(srcdir)/COPYING.ISC ResidualVM/COPYING.ISC.txt
-	cp $(srcdir)/COPYING.LUA ResidualVM/COPYING.LUA.txt
-	cp $(srcdir)/COPYING.MIT ResidualVM/COPYING.MIT.txt
-	cp $(srcdir)/COPYING.TINYGL ResidualVM/COPYING.TINYGL.txt
-	cp $(srcdir)/COPYRIGHT ResidualVM/COPYRIGHT.txt
-	cp $(srcdir)/KNOWN_BUGS ResidualVM/KNOWN_BUGS.txt
-	cp $(srcdir)/NEWS ResidualVM/NEWS.txt
-	cp $(srcdir)/doc/QuickStart ResidualVM/QuickStart.txt
-	cp $(srcdir)/README ResidualVM/README.txt
-	lha a residualvm-amigaos4.lha ResidualVM
-
 # Mark special targets as phony
 .PHONY: deb bundle osxsnap win32dist install uninstall
