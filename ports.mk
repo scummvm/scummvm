bc237fc7cef4a07a82f5dd0966f87b042bd062e8
raziel- <raziel-@users.noreply.github.com>
2014-09-05 11:35:25 +0300

    AMIGAOS: Make use of the new drawer icon
    On AmigaOS the user can customize every icon including drawers.
    
    This is a customized drawer icon for ScummVM with the original "S"
    scaled on top of it.
    
    This icon may change with further revisions of AmigaOS4 (if the
    default icon images change).

--- ports.mk
+++ ports.mk
@@ -334,6 +334,7 @@ aos4dist: $(EXECUTABLE)
 	mkdir -p $(AOS4PATH)/themes
 	mkdir -p $(AOS4PATH)/extras
 	$(STRIP) $(EXECUTABLE) -o $(AOS4PATH)/$(EXECUTABLE)
+	cp ${srcdir}/icons/scummvm_drawer.info $(AOS4PATH).info
 	cp ${srcdir}/icons/scummvm.info $(AOS4PATH)/$(EXECUTABLE).info
 	cp $(DIST_FILES_THEMES) $(AOS4PATH)/themes/
 ifdef DIST_FILES_ENGINEDATA
