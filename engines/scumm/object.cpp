27db4ac5bf339eec8876595d361a180e8077541c
Ben Castricum <github@bencastricum.nl>
2014-09-08 20:10:35 +0200

    SCUMM: Fix bug #6724 by changing the assert into a warning

--- engines/scumm/object.cpp
+++ engines/scumm/object.cpp
@@ -1013,8 +1013,11 @@ void ScummEngine::resetRoomObject(ObjectData *od, const byte *room, const byte *
 		od->actordir = (byte)READ_LE_UINT16(&imhd->v7.actordir);
 
 	} else if (_game.version == 6) {
-		assert(imhd);
 		od->obj_nr = READ_LE_UINT16(&(cdhd->v6.obj_id));
+		// This used to be an assert(imhd) but the Russion version of FF1 triggered it.
+		// FIXME: Needs a real fix (tm) where we skip the assert for only the Russian version 
+		if (!imhd)
+			warning("resetRoomObject() called but no imhd was found for object %d in room %d",od->obj_nr,int(room));
 
 		od->width = READ_LE_UINT16(&cdhd->v6.w);
 		od->height = READ_LE_UINT16(&cdhd->v6.h);
