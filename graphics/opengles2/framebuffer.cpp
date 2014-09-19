15799c9cfe8a8aee215ee334d9902d8df45a2d4d
Dries Harnie <dries@harnie.be>
2014-09-19 21:02:08 +0200

    JANITORIAL: Fix style issues

--- graphics/opengles2/framebuffer.cpp
+++ graphics/opengles2/framebuffer.cpp
@@ -80,9 +80,9 @@ static bool usePackedBuffer() {
 	return true;
 }
 
-FrameBuffer::FrameBuffer(uint width, uint height)
-	: _managedTexture(true), _width(width), _height(height),
-	  _texWidth(nextHigher2(width)), _texHeight(nextHigher2(height)) {
+FrameBuffer::FrameBuffer(uint width, uint height) :
+		_managedTexture(true), _width(width), _height(height),
+		_texWidth(nextHigher2(width)), _texHeight(nextHigher2(height)) {
 #ifndef USE_OPENGL_SHADERS
 	if (!Graphics::isExtensionSupported("GL_EXT_framebuffer_object")) {
 		error("GL_EXT_framebuffer_object extension is not supported!");
@@ -99,9 +99,9 @@ FrameBuffer::FrameBuffer(uint width, uint height)
 	init();
 }
 
-FrameBuffer::FrameBuffer(GLuint texture_name, uint width, uint height, uint texture_width, uint texture_height)
-	: _managedTexture(false), _colorTexture(texture_name), _width(width), _height(height),
-	  _texWidth(texture_width), _texHeight(texture_height) {
+FrameBuffer::FrameBuffer(GLuint texture_name, uint width, uint height, uint texture_width, uint texture_height) :
+		_managedTexture(false), _colorTexture(texture_name), _width(width), _height(height),
+		_texWidth(texture_width), _texHeight(texture_height) {
 	init();
 }
 
