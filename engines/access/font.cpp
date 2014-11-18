8e483e037f1851fb96fd2125bf526828cdfa6e98
Strangerke <strangerke@scummvm.org>
2014-11-18 12:10:07 +0100

    ACCESS: Fix the handling of the very last line of a text in getLine

--- engines/access/font.cpp
+++ engines/access/font.cpp
@@ -110,8 +110,11 @@ bool Font::getLine(Common::String &s, int maxWidth, Common::String &line, int &w
 
 		// Reached maximum allowed size
 		// If this was the last character of the string, let it go
-		if (*src == '\0')
+		if (*src == '\0') {
+			line = Common::String(s.c_str(), src);
+			s = "";
 			return true;
+		}
 		
 		// Work backwards to find space at the start of the current word 
 		// as a point to split the line on
