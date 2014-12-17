8fe8cf8e437df59874b0337946708cff6bb56c6d
Strangerke <strangerke@scummvm.org>
2014-12-17 23:35:12 +0100

    ACCESS: Use AD_ENTRY1s in detection tables

--- engines/access/detection_tables.h
+++ engines/access/detection_tables.h
@@ -24,14 +24,13 @@ namespace Access {
 
 static const AccessGameDescription gameDescriptions[] = {
 	{
-		// Amazon Guadians of Eden - Floppy English
+		// Amazon Guardians of Eden - Floppy English
+		// 3.5" and 5.25" floppies provided by Strangerke had the same md5
+		// Except the sound file. The executable is also identical
 		{
 			"amazon",
 			0,
-			{
-				{ "c00.ap", 0, "dcabf69d5a0d911168cb73511ebaead0", 331481 },
-				AD_LISTEND
-			},
+			AD_ENTRY1s("c00.ap", "dcabf69d5a0d911168cb73511ebaead0", 331481),
 			Common::EN_ANY,
 			Common::kPlatformDOS,
 			ADGF_NO_FLAGS,
@@ -44,12 +43,9 @@ static const AccessGameDescription gameDescriptions[] = {
 	// Amazon Guardians of Eden - Demo English
 	{
 		{
-		"amazon",
-		"Demo",
-			{
-				{ "c25.ap", 0, "5baba0c052d22157499bfa05cb1ed5b7", 65458 },
-				AD_LISTEND
-			},
+			"amazon",
+			"Demo",
+			AD_ENTRY1s("c25.ap", "5baba0c052d22157499bfa05cb1ed5b7", 65458),
 			Common::EN_ANY,
 			Common::kPlatformDOS,
 			ADGF_DEMO,
@@ -64,10 +60,7 @@ static const AccessGameDescription gameDescriptions[] = {
 		{
 			"amazon",
 			"CD",
-			{
-				{ "checksum.crc", 0, "bef85478132fec74cb5d9067f3a37d24", 8 },
-				AD_LISTEND
-			},
+			AD_ENTRY1s("checksum.crc", "bef85478132fec74cb5d9067f3a37d24", 8),
 			Common::EN_ANY,
 			Common::kPlatformDOS,
 			ADGF_CD,
@@ -82,10 +75,7 @@ static const AccessGameDescription gameDescriptions[] = {
 		{
 			"martian",
 			nullptr,
-			{
-				{ "r00.ap", 0, "af98db5ee7f9ef86c6b1f43187a3691b", 31 },
-				AD_LISTEND
-			},
+			AD_ENTRY1s("r00.ap", "af98db5ee7f9ef86c6b1f43187a3691b", 31),
 			Common::EN_ANY,
 			Common::kPlatformDOS,
 			ADGF_NO_FLAGS,
