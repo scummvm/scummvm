scummvm/scummvm

commit 3f22c12c56572d7c0b46e734179255062f81f45c

SDL: Handle icon setup in SdlWindow.

backends/platform/sdl/macosx/macosx.cpp
@@ -47,6 +47,9 @@ OSystem_MacOSX::OSystem_MacOSX()
 }
 
 void OSystem_MacOSX::init() {
+	// Use an iconless window on OS X, as we use a nicer external icon there.
+	_window = new SdlIconlessWindow();
+
 #if defined(USE_TASKBAR)
 	// Initialize taskbar manager
 	_taskbarManager = new MacOSXTaskbarManager();
@@ -101,8 +104,6 @@ void OSystem_MacOSX::addSysArchivesToSearchSet(Common::SearchSet &s, int priorit
 	}
 }
 
-void OSystem_MacOSX::setupIcon() {
-	// Don't set icon on OS X, as we use a nicer external icon there.
 }
 
 bool OSystem_MacOSX::hasFeature(Feature f) {

backends/platform/maemo/maemo.cpp
@@ -84,6 +84,15 @@ static void registerDefaultKeyBindings(Common::KeymapperDefaultBindings *_keymap
 }
 #endif
 
+void OSystem_SDL_Maemo::init() {
+	// Use an iconless window for Maemo
+	// also N900 is hit by SDL_WM_SetIcon bug (window cannot receive input)
+	// http://bugzilla.libsdl.org/show_bug.cgi?id=586
+	_window = new SdlIconlessWindow();
+
+	OSystem_POSIX::init();
+}
+
 void OSystem_SDL_Maemo::initBackend() {
 	ConfMan.registerDefault("fullscreen", true);
 	ConfMan.registerDefault("aspect_ratio", true);
@@ -176,12 +185,6 @@ const Maemo::Model OSystem_SDL_Maemo::detectModel() {
 			return *model;
 	}
 	return *model;
-}
-
-void OSystem_SDL_Maemo::setupIcon() {
-	// no Maemo version needs setupIcon
-	// also N900 is hit by SDL_WM_SetIcon bug (window cannot receive input)
-	// http://bugzilla.libsdl.org/show_bug.cgi?id=586
 }
 
 #ifdef ENABLE_KEYMAPPER

backends/platform/maemo/maemo.h
@@ -36,11 +36,11 @@ public:
 	OSystem_SDL_Maemo();
 	~OSystem_SDL_Maemo();
 
+	virtual void init();
 	virtual void initBackend();
 	virtual void quit();
 	virtual void fatalError();
 	virtual void setWindowCaption(const char *caption);
-	virtual void setupIcon();
 #ifdef ENABLE_KEYMAPPER
 	virtual Common::HardwareInputSet *getHardwareInputSet();
 	virtual Common::Keymap *getGlobalKeymap();

backends/platform/sdl/macosx/macosx.h
@@ -38,7 +38,6 @@ public:
 	virtual void init();
 	virtual void initBackend();
 	virtual void addSysArchivesToSearchSet(Common::SearchSet &s, int priority = 0);
-	virtual void setupIcon();
 };
 
 #endif

backends/platform/sdl/module.mk
@@ -20,6 +20,7 @@ endif
 ifdef WIN32
 MODULE_OBJS += \
 	win32/win32-main.o \
+	win32/win32-window.o \
 	win32/win32.o
 endif
 

backends/platform/sdl/sdl-window.cpp
@@ -24,6 +24,10 @@
 
 #include "backends/platform/sdl/sdl-window.h"
 
+#include "common/textconsole.h"
+
+#include "icons/scummvm.xpm"
+
 SdlWindow::SdlWindow()
 #if SDL_VERSION_ATLEAST(2, 0, 0)
 	: _window(nullptr), _inputGrabState(false), _windowCaption("ScummVM"), _windowIcon(nullptr)
@@ -38,6 +42,77 @@ SdlWindow::~SdlWindow() {
 #endif
 }
 
+void SdlWindow::setupIcon() {
+	int x, y, w, h, ncols, nbytes, i;
+	unsigned int rgba[256];
+	unsigned int *icon;
+
+	if (sscanf(scummvm_icon[0], "%d %d %d %d", &w, &h, &ncols, &nbytes) != 4) {
+		warning("Wrong format of scummvm_icon[0] (%s)", scummvm_icon[0]);
+
+		return;
+	}
+	if ((w > 512) || (h > 512) || (ncols > 255) || (nbytes > 1)) {
+		warning("Could not load the built-in icon (%d %d %d %d)", w, h, ncols, nbytes);
+		return;
+	}
+	icon = (unsigned int*)malloc(w*h*sizeof(unsigned int));
+	if (!icon) {
+		warning("Could not allocate temp storage for the built-in icon");
+		return;
+	}
+
+	for (i = 0; i < ncols; i++) {
+		unsigned char code;
+		char color[32];
+		memset(color, 0, sizeof(color));
+		unsigned int col;
+		if (sscanf(scummvm_icon[1 + i], "%c c %s", &code, color) != 2) {
+			warning("Wrong format of scummvm_icon[%d] (%s)", 1 + i, scummvm_icon[1 + i]);
+		}
+		if (!strcmp(color, "None"))
+			col = 0x00000000;
+		else if (!strcmp(color, "black"))
+			col = 0xFF000000;
+		else if (color[0] == '#') {
+			if (sscanf(color + 1, "%06x", &col) != 1) {
+				warning("Wrong format of color (%s)", color + 1);
+			}
+			col |= 0xFF000000;
+		} else {
+			warning("Could not load the built-in icon (%d %s - %s) ", code, color, scummvm_icon[1 + i]);
+			free(icon);
+			return;
+		}
+
+		rgba[code] = col;
+	}
+	for (y = 0; y < h; y++) {
+		const char *line = scummvm_icon[1 + ncols + y];
+		for (x = 0; x < w; x++) {
+			icon[x + w * y] = rgba[(int)line[x]];
+		}
+	}
+
+	SDL_Surface *sdl_surf = SDL_CreateRGBSurfaceFrom(icon, w, h, 32, w * 4, 0xFF0000, 0x00FF00, 0x0000FF, 0xFF000000);
+	if (!sdl_surf) {
+		warning("SDL_CreateRGBSurfaceFrom(icon) failed");
+	}
+
+#if SDL_VERSION_ATLEAST(2, 0, 0)
+	SDL_FreeSurface(_windowIcon);
+	_windowIcon = sdl_surf;
+	if (_window) {
+		SDL_SetWindowIcon(_window, sdl_surf);
+	}
+#else
+	SDL_WM_SetIcon(sdl_surf, NULL);
+	SDL_FreeSurface(sdl_surf);
+#endif
+
+	free(icon);
+}
+
 void SdlWindow::setWindowCaption(const Common::String &caption) {
 #if SDL_VERSION_ATLEAST(2, 0, 0)
 	_windowCaption = caption;
@@ -46,19 +121,6 @@ void SdlWindow::setWindowCaption(const Common::String &caption) {
 	}
 #else
 	SDL_WM_SetCaption(caption.c_str(), caption.c_str());
-#endif
-}
-
-void SdlWindow::setWindowIcon(SDL_Surface *icon) {
-#if SDL_VERSION_ATLEAST(2, 0, 0)
-	SDL_FreeSurface(_windowIcon);
-	_windowIcon = icon;
-	if (_window) {
-		SDL_SetWindowIcon(_window, icon);
-	}
-#else
-	SDL_WM_SetIcon(icon, NULL);
-	SDL_FreeSurface(icon);
 #endif
 }
 

backends/platform/sdl/sdl-window.h
@@ -33,19 +33,16 @@ public:
 	virtual ~SdlWindow();
 
 	/**
+	 * Setup the window icon.
+	 */
+	virtual void setupIcon();
+
+	/**
 	 * Change the caption of the window.
 	 *
 	 * @param caption New window caption in UTF-8 encoding.
 	 */
 	void setWindowCaption(const Common::String &caption);
-
-	/**
-	 * Attach an icon to the window.
-	 *
-	 * @param icon The surface to use as icon. SdlGraphicsManager takes
-	 *             ownership over it.
-	 */
-	void setWindowIcon(SDL_Surface *icon);
 
 	/**
 	 * Toggle mouse grab state. This decides whether the cursor can leave the
@@ -98,6 +95,11 @@ private:
 	Common::String _windowCaption;
 	SDL_Surface *_windowIcon;
 #endif
+};
+
+class SdlIconlessWindow : public SdlWindow {
+public:
+	virtual void setupIcon() {}
 };
 
 #endif

backends/platform/sdl/sdl.cpp
@@ -52,8 +52,6 @@
 #include "graphics/cursorman.h"
 #endif
 
-#include "icons/scummvm.xpm"
-
 #include <time.h>	// for getTimeAndDate()
 
 #ifdef USE_DETECTLANG
@@ -258,7 +256,7 @@ void OSystem_SDL::initBackend() {
 	}
 
 	// Setup a custom program icon.
-	setupIcon();
+	_window->setupIcon();
 
 	_inited = true;
 
@@ -440,67 +438,6 @@ Common::String OSystem_SDL::getSystemLanguage() const {
 	return ModularBackend::getSystemLanguage();
 #endif // USE_DETECTLANG
 }
-
-void OSystem_SDL::setupIcon() {
-	int x, y, w, h, ncols, nbytes, i;
-	unsigned int rgba[256];
-	unsigned int *icon;
-
-	if (sscanf(scummvm_icon[0], "%d %d %d %d", &w, &h, &ncols, &nbytes) != 4) {
-		warning("Wrong format of scummvm_icon[0] (%s)", scummvm_icon[0]);
-
-		return;
-	}
-	if ((w > 512) || (h > 512) || (ncols > 255) || (nbytes > 1)) {
-		warning("Could not load the built-in icon (%d %d %d %d)", w, h, ncols, nbytes);
-		return;
-	}
-	icon = (unsigned int*)malloc(w*h*sizeof(unsigned int));
-	if (!icon) {
-		warning("Could not allocate temp storage for the built-in icon");
-		return;
-	}
-
-	for (i = 0; i < ncols; i++) {
-		unsigned char code;
-		char color[32];
-		memset(color, 0, sizeof(color));
-		unsigned int col;
-		if (sscanf(scummvm_icon[1 + i], "%c c %s", &code, color) != 2) {
-			warning("Wrong format of scummvm_icon[%d] (%s)", 1 + i, scummvm_icon[1 + i]);
-		}
-		if (!strcmp(color, "None"))
-			col = 0x00000000;
-		else if (!strcmp(color, "black"))
-			col = 0xFF000000;
-		else if (color[0] == '#') {
-			if (sscanf(color + 1, "%06x", &col) != 1) {
-				warning("Wrong format of color (%s)", color + 1);
-			}
-			col |= 0xFF000000;
-		} else {
-			warning("Could not load the built-in icon (%d %s - %s) ", code, color, scummvm_icon[1 + i]);
-			free(icon);
-			return;
-		}
-
-		rgba[code] = col;
-	}
-	for (y = 0; y < h; y++) {
-		const char *line = scummvm_icon[1 + ncols + y];
-		for (x = 0; x < w; x++) {
-			icon[x + w * y] = rgba[(int)line[x]];
-		}
-	}
-
-	SDL_Surface *sdl_surf = SDL_CreateRGBSurfaceFrom(icon, w, h, 32, w * 4, 0xFF0000, 0x00FF00, 0x0000FF, 0xFF000000);
-	if (!sdl_surf) {
-		warning("SDL_CreateRGBSurfaceFrom(icon) failed");
-	}
-	_window->setWindowIcon(sdl_surf);
-	free(icon);
-}
-
 
 uint32 OSystem_SDL::getMillis(bool skipRecord) {
 	uint32 millis = SDL_GetTicks();

commit 5936f7c9d1faef575584cff69ca6263d82a9d773

MACOSX: Fix compilation
Broken by 3f22c12

backends/platform/sdl/macosx/macosx.cpp
@@ -104,8 +104,6 @@ void OSystem_MacOSX::addSysArchivesToSearchSet(Common::SearchSet &s, int priorit
 	}
 }
 
-}
-
 bool OSystem_MacOSX::hasFeature(Feature f) {
 	if (f == kFeatureDisplayLogFile)
 		return true;