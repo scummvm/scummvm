4c88d81cc2cbaf244922f01466447a1cef60f7fa
Paul Gilbert <dreammaster@scummvm.org>
2015-03-06 08:30:26 -0500

    MADS: Fix another incorrect car door opening animation

--- engines/mads/nebular/nebular_scenes6.cpp
+++ engines/mads/nebular/nebular_scenes6.cpp
@@ -4614,7 +4614,7 @@ void Scene612::actions() {
 		case 0:
 			_game._player._stepEnabled = false;
 			_scene->_sequences.remove(_globals._sequenceIndexes[1]);
-			_globals._sequenceIndexes[1] = _scene->_sequences.startPingPongCycle(_globals._spriteIndexes[1], false, 6, 1, 0, 0);
+			_globals._sequenceIndexes[1] = _scene->_sequences.addReverseSpriteCycle(_globals._spriteIndexes[1], false, 6, 1, 0, 0);
 			_scene->_sequences.setDepth(_globals._sequenceIndexes[1], 2);
 			_scene->_sequences.addSubEntry(_globals._sequenceIndexes[1], SEQUENCE_TRIGGER_EXPIRE, 0, 1);
 			break;
