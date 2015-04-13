3483799959926d130bcabc1da7e424501a6ef173
Martin Kiewitz <m_kiewitz@users.sourceforge.net>
2015-04-13 18:26:12 +0200

    SCI: add french qfg3 export filename for fix
    Sierra translator translated the filename

--- engines/sci/engine/kgraphics.cpp
+++ engines/sci/engine/kgraphics.cpp
@@ -955,8 +955,9 @@ reg_t kDrawControl(EngineState *s, int argc, reg_t *argv) {
 		reg_t textReference = readSelector(s->_segMan, controlObject, SELECTOR(text));
 		if (!textReference.isNull()) {
 			Common::String text = s->_segMan->getString(textReference);
-			if ((text == "a:hq1_hero.sav") || (text == "a:glory1.sav") || (text == "a:glory2.sav") || (text == "a:glory3.sav")) {
+			if ((text == "a:hq1_hero.sav") || (text == "a:glory1.sav") || (text == "a:glory2.sav") || (text == "a:glory3.sav") || (text == "a:gloire3.sauv")) {
 				// Remove "a:" from hero quest / quest for glory export default filenames
+				// The french version of Quest For Glory 3 uses "gloire3.sauv". It seems a translator translated the filename.
 				text.deleteChar(0);
 				text.deleteChar(0);
 				s->_segMan->strcpy(textReference, text.c_str());
