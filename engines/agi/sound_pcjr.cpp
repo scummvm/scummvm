scummvm/scummvm

commit ceb2909e0ad0ee4db8846bc2e6e9009364e24731

AGI: Split all sound generators into separate modules.
Now the sound subsystem of the engine finally is possible to grasp.
Also now it is obvious why CoCo3 sounds are not functioning.

svn-id: r49757

engines/agi/sound_pcjr.cpp
@@ -105,7 +105,7 @@ const int8 dissolveDataV3[] = {
 };
 
 
-SoundGenPCJr::SoundGenPCJr(AgiEngine *vm) : _vm(vm) {
+SoundGenPCJr::SoundGenPCJr(AgiEngine *vm, Audio::Mixer *pMixer) : SoundGen(vm, pMixer) {
 	_chanAllocated = 10240; // preallocate something which will most likely fit
 	_chanData = (int16 *)malloc(_chanAllocated << 1);
 
@@ -122,13 +122,19 @@ SoundGenPCJr::SoundGenPCJr(AgiEngine *vm) : _vm(vm) {
 		_dissolveMethod = 2;
 	else
 		_dissolveMethod = 0;
+
+	_dissolveMethod = 3;
+
+	_mixer->playStream(Audio::Mixer::kMusicSoundType, &_soundHandle, this, -1, Audio::Mixer::kMaxChannelVolume, 0, DisposeAfterUse::NO, true);
 }
 
 SoundGenPCJr::~SoundGenPCJr() {
 	free(_chanData);
+
+	_mixer->stopHandle(_soundHandle);
 }
 
-void SoundGenPCJr::play(int resnum, int flag) {
+void SoundGenPCJr::play(int resnum) {
 	PCjrSound *pcjrSound = (PCjrSound *)_vm->_game.sounds[resnum];
 
 	for (int i = 0; i < CHAN_MAX; i++) {
@@ -467,7 +473,7 @@ int SoundGenPCJr::fillNoise(ToneChan *t, int16 *buf, int len) {
 	return len;
 }
 
-void SoundGenPCJr::premixerCall(int16 *stream, int len) {
+int SoundGenPCJr::readBuffer(int16 *stream, const int len) {
 	int streamCount;
 	int16 *sPtr, *cPtr;
 
@@ -480,6 +486,8 @@ void SoundGenPCJr::premixerCall(int16 *stream, int len) {
 
 	assert(stream);
 
+	bool finished = true;
+
 	for (int i = 0; i < CHAN_MAX; i++) {
 		// get channel data(chan.userdata)
 		if (chanGen(i, _chanData, len) == 0) {
@@ -487,11 +495,18 @@ void SoundGenPCJr::premixerCall(int16 *stream, int len) {
 			streamCount = len;
 			sPtr = stream;
 			cPtr = _chanData;
-					
+
 			while (streamCount--)
 				*(sPtr++) += *(cPtr++) / CHAN_MAX;
+
+			finished = false;
 		}
 	}
+
+	if (finished)
+		_vm->_sound->soundIsFinished();
+
+	return len;
 }
 
 } // End of namespace Agi

engines/agi/module.mk
@@ -31,8 +31,10 @@ MODULE_OBJS := \
 	saveload.o \
 	sound.o \
 	sound_2gs.o \
+	sound_coco3.o \
 	sound_midi.o \
 	sound_pcjr.o \
+	sound_sarien.o \
 	sprite.o \
 	text.o \
 	view.o \

engines/agi/sound.cpp
@@ -23,6 +23,15 @@
  *
  */
 
+#include "agi/agi.h"
+
+#include "agi/sound_2gs.h"
+#include "agi/sound_coco3.h"
+#include "agi/sound_midi.h"
+#include "agi/sound_sarien.h"
+#include "agi/sound_pcjr.h"
+
+#if 0
 #include "common/md5.h"
 #include "common/config-manager.h"
 #include "common/fs.h"
@@ -31,15 +40,10 @@
 
 #include "sound/mididrv.h"
 
-#include "agi/agi.h"
 
-#include "agi/sound_2gs.h"
-#include "agi/sound_midi.h"
-#include "agi/sound_pcjr.h"
+#endif
 
 namespace Agi {
-
-#define USE_INTERPOLATION
 
 //
 // TODO: add support for variable sampling rate in the output device
@@ -84,58 +88,16 @@ const uint8 *PCjrSound::getVoicePointer(uint voiceNum) {
 	return _data + voiceStartOffset;
 }
 
-static const int16 waveformRamp[WAVEFORM_SIZE] = {
-	0, 8, 16, 24, 32, 40, 48, 56,
-	64, 72, 80, 88, 96, 104, 112, 120,
-	128, 136, 144, 152, 160, 168, 176, 184,
-	192, 200, 208, 216, 224, 232, 240, 255,
-	0, -248, -240, -232, -224, -216, -208, -200,
-	-192, -184, -176, -168, -160, -152, -144, -136,
-	-128, -120, -112, -104, -96, -88, -80, -72,
-	-64, -56, -48, -40, -32, -24, -16, -8	// Ramp up
-};
-
-static const int16 waveformSquare[WAVEFORM_SIZE] = {
-	255, 230, 220, 220, 220, 220, 220, 220,
-	220, 220, 220, 220, 220, 220, 220, 220,
-	220, 220, 220, 220, 220, 220, 220, 220,
-	220, 220, 220, 220, 220, 220, 220, 110,
-	-255, -230, -220, -220, -220, -220, -220, -220,
-	-220, -220, -220, -220, -220, -220, -220, -220,
-	-220, -220, -220, -220, -220, -220, -220, -220,
-	-220, -220, -220, -110, 0, 0, 0, 0	// Square
-};
-
-static const int16 waveformMac[WAVEFORM_SIZE] = {
-	45, 110, 135, 161, 167, 173, 175, 176,
-	156, 137, 123, 110, 91, 72, 35, -2,
-	-60, -118, -142, -165, -170, -176, -177, -179,
-	-177, -176, -164, -152, -117, -82, -17, 47,
-	92, 137, 151, 166, 170, 173, 171, 169,
-	151, 133, 116, 100, 72, 43, -7, -57,
-	-99, -141, -156, -170, -174, -177, -178, -179,
-	-175, -172, -165, -159, -137, -114, -67, -19
-};
-
+#if 0
 static const uint16 period[] = {
 	1024, 1085, 1149, 1218, 1290, 1367,
 	1448, 1534, 1625, 1722, 1825, 1933
 };
 
-#if 0
 static int noteToPeriod(int note) {
 	return 10 * (period[note % 12] >> (note / 12 - 3));
 }
 #endif
-
-int SoundMgr::readBuffer(int16 *buffer, const int numSamples) {
-	if (_vm->_soundemu == SOUND_EMU_PCJR)
-		_soundGen->premixerCall(buffer, numSamples);
-	else
-		premixerCall(buffer, numSamples / 2);
-	
-	return numSamples;
-}
 
 void SoundMgr::unloadSound(int resnum) {
 	if (_vm->_game.dirSound[resnum].flags & RES_LOADED) {
@@ -151,7 +113,6 @@ void SoundMgr::unloadSound(int resnum) {
 }
 
 void SoundMgr::startSound(int resnum, int flag) {
-	int i;
 	AgiSoundEmuType type;
 
 	if (_vm->_game.sounds[resnum] != NULL && _vm->_game.sounds[resnum]->isPlaying())
@@ -172,46 +133,8 @@ void SoundMgr::startSound(int resnum, int flag) {
 
 	debugC(3, kDebugLevelSound, "startSound(resnum = %d, flag = %d) type = %d", resnum, flag, type);
 
-	switch (type) {
-	case AGI_SOUND_SAMPLE: {
-		IIgsSample *sampleRes = (IIgsSample *) _vm->_game.sounds[_playingSound];
-		_gsSound->playSampleSound(sampleRes->getHeader(), sampleRes->getSample());
-		break;
-	}
-	case AGI_SOUND_MIDI:
-		((IIgsMidi *) _vm->_game.sounds[_playingSound])->rewind();
-		break;
-	case AGI_SOUND_4CHN:
-		if (_vm->_soundemu == SOUND_EMU_MIDI) {
-			_musicPlayer->playMIDI((MIDISound *)_vm->_game.sounds[resnum]);
-		} else if (_vm->_soundemu == SOUND_EMU_PCJR) {
-			_soundGen->play(resnum, flag);
-		} else {
+	_soundGen->play(resnum);
 
-			PCjrSound *pcjrSound = (PCjrSound *) _vm->_game.sounds[resnum];
-
-			// Initialize channel info
-			for (i = 0; i < NUM_CHANNELS; i++) {
-				_chn[i].type = type;
-				_chn[i].flags = AGI_SOUND_LOOP;
-				
-				if (_env) {
-					_chn[i].flags |= AGI_SOUND_ENVELOPE;
-					_chn[i].adsr = AGI_SOUND_ENV_ATTACK;
-				}
-
-				_chn[i].ins = _waveform;
-				_chn[i].size = WAVEFORM_SIZE;
-				_chn[i].ptr = pcjrSound->getVoicePointer(i % 4);
-				_chn[i].timer = 0;
-				_chn[i].vol = 0;
-				_chn[i].end = 0;
-			}
-		}
-		break;
-	}
-
-	memset(_sndBuffer, 0, BUFFER_SIZE << 1);
 	_endflag = flag;
 
 	// Nat Budin reports that the flag should be reset when sound starts
@@ -219,32 +142,15 @@ void SoundMgr::startSound(int resnum, int flag) {
 }
 
 void SoundMgr::stopSound() {
-	int i;
-
 	debugC(3, kDebugLevelSound, "stopSound() --> %d", _playingSound);
 
 	_endflag = -1;
-
-	if (_vm->_soundemu != SOUND_EMU_APPLE2GS && _vm->_soundemu != SOUND_EMU_PCJR) {
-		for (i = 0; i < NUM_CHANNELS; i++)
-			stopNote(i);
-	}
 
 	if (_playingSound != -1) {
 		if (_vm->_game.sounds[_playingSound]) // sanity checking
 			_vm->_game.sounds[_playingSound]->stop();
 
-		if (_vm->_soundemu == SOUND_EMU_APPLE2GS) {
-			_gsSound->stopSounds();
-		}
-
-		if (_vm->_soundemu == SOUND_EMU_MIDI) {
-			_musicPlayer->stop();
-		}
-
-		if (_vm->_soundemu == SOUND_EMU_PCJR) {
-			_soundGen->stop();
-		}
+		_soundGen->stop();
 
 		_playingSound = -1;
 	}
@@ -254,419 +160,50 @@ void SoundMgr::stopSound() {
 }
 
 int SoundMgr::initSound() {
-	int r = -1;
-
-	memset(_sndBuffer, 0, BUFFER_SIZE << 1);
-	_env = false;
-
-	switch (_vm->_soundemu) {
-	case SOUND_EMU_NONE:
-		_waveform = waveformRamp;
-		_env = true;
-		break;
-	case SOUND_EMU_AMIGA:
-	case SOUND_EMU_PC:
-		_waveform = waveformSquare;
-		break;
-	case SOUND_EMU_MAC:
-		_waveform = waveformMac;
-		break;
-	case SOUND_EMU_APPLE2GS:
-		_disabledMidi = !loadInstruments();
-		break;
-	case SOUND_EMU_COCO3:
-		break;
-	case SOUND_EMU_MIDI:
-		break;
-	case SOUND_EMU_PCJR:
-		_soundGen = new SoundGenPCJr(_vm);
-		break;
-	}
-
-	report("Initializing sound:\n");
-
-	report("sound: envelopes ");
-	if (_env) {
-		report("enabled (decay=%d, sustain=%d)\n", ENV_DECAY, ENV_SUSTAIN);
-	} else {
-		report("disabled\n");
-	}
-
-	if (_vm->_soundemu != SOUND_EMU_MIDI)
-		_mixer->playStream(Audio::Mixer::kMusicSoundType, &_soundHandle, this, -1, Audio::Mixer::kMaxChannelVolume, 0, DisposeAfterUse::NO, true);
-
-	return r;
+	return -1;
 }
 
 void SoundMgr::deinitSound() {
-	debugC(3, kDebugLevelSound, "()");
-
 	stopSound();
-	_mixer->stopHandle(_soundHandle);
+
+	delete _soundGen;
 }
 
-void SoundMgr::stopNote(int i) {
-	_chn[i].adsr = AGI_SOUND_ENV_RELEASE;
+void SoundMgr::soundIsFinished() {
+	if (_endflag != -1)
+		_vm->setflag(_endflag, true);
 
-	if (_useChorus) {
-		// Stop chorus ;)
-		if (_chn[i].type == AGI_SOUND_4CHN &&
-			_vm->_soundemu == SOUND_EMU_NONE && i < 3) {
-			stopNote(i + 4);
-		}
-	}
+	if (_playingSound != -1)
+		_vm->_game.sounds[_playingSound]->stop();
+	_playingSound = -1;
+	_endflag = -1;
 }
 
-void SoundMgr::playNote(int i, int freq, int vol) {
-	if (!_vm->getflag(fSoundOn))
-		vol = 0;
-	else if (vol && _vm->_soundemu == SOUND_EMU_PC)
-		vol = 160;
-
-	_chn[i].phase = 0;
-	_chn[i].freq = freq;
-	_chn[i].vol = vol;
-	_chn[i].env = 0x10000;
-	_chn[i].adsr = AGI_SOUND_ENV_ATTACK;
-
-	if (_useChorus) {
-		// Add chorus ;)
-		if (_chn[i].type == AGI_SOUND_4CHN &&
-			_vm->_soundemu == SOUND_EMU_NONE && i < 3) {
-
-			int newfreq = freq * 1007 / 1000;
-
-			if (freq == newfreq)
-				newfreq++;
-
-			playNote(i + 4, newfreq, vol * 2 / 3);
-		}
-	}
-}
-
-static int cocoFrequencies[] = {
-	 130,  138,  146,  155,  164,  174,  184,  195,  207,  220,  233,  246,
-	 261,  277,  293,  311,  329,  349,  369,  391,  415,  440,  466,  493,
-	 523,  554,  587,  622,  659,  698,  739,  783,  830,  880,  932,  987,
-	1046, 1108, 1174, 1244, 1318, 1396, 1479, 1567, 1661, 1760, 1864, 1975,
-	2093, 2217, 2349, 2489, 2637, 2793, 2959, 3135, 3322, 3520, 3729, 3951
-};
-
-void SoundMgr::playCoCoSound() {
-	int i = 0;
-	CoCoNote note;
-
-	do {
-		note.read(_chn[i].ptr);
-
-		if (note.freq != 0xff) {
-			playNote(0, cocoFrequencies[note.freq], note.volume);
-
-			uint32 start_time = _vm->_system->getMillis();
-
-			while (_vm->_system->getMillis() < start_time + note.duration) {
-                _vm->_system->updateScreen();
-
-                _vm->_system->delayMillis(10);
-			}
-		}
-	} while (note.freq != 0xff);
-}
-
-void SoundMgr::playAgiSound() {
-	int i;
-	AgiNote note;
-
-	_playing = false;
-	for (i = 0; i < (_vm->_soundemu == SOUND_EMU_PC ? 1 : 4); i++) {
-		_playing |= !_chn[i].end;
-		note.read(_chn[i].ptr); // Read a single note (Doesn't advance the pointer)
-
-		if (_chn[i].end)
-			continue;
-
-		if ((--_chn[i].timer) <= 0) {
-			stopNote(i);
-
-			if (note.freqDiv != 0) {
-				int volume = (note.attenuation == 0x0F) ? 0 : (0xFF - note.attenuation * 2);
-				playNote(i, note.freqDiv * 10, volume);
-			}
-
-			_chn[i].timer = note.duration;
-
-			if (_chn[i].timer == 0xffff) {
-				_chn[i].end = 1;
-				_chn[i].vol = 0;
-				_chn[i].env = 0;
-
-				if (_useChorus) {
-					// chorus
-					if (_chn[i].type == AGI_SOUND_4CHN && _vm->_soundemu == SOUND_EMU_NONE && i < 3) {
-						_chn[i + 4].vol = 0;
-						_chn[i + 4].env = 0;
-					}
-				}
-			}
-			_chn[i].ptr += 5; // Advance the pointer to the next note data (5 bytes per note)
-		}
-	}
-}
-
-void SoundMgr::playSound() {
-	int i;
-
-	if (_endflag == -1)
-		return;
-
-	if (_vm->_soundemu == SOUND_EMU_APPLE2GS) {
-		if (_playingSound != -1) {
-			if (_vm->_game.sounds[_playingSound]->type() == AGI_SOUND_MIDI) {
-				playMidiSound();
-				//warning("playSound: Trying to play an Apple IIGS MIDI sound. Not yet implemented");
-			} else if (_vm->_game.sounds[_playingSound]->type() == AGI_SOUND_SAMPLE) {
-				//debugC(3, kDebugLevelSound, "playSound: Trying to play an Apple IIGS sample");
-				playSampleSound();
-			}
-		}
-	} else if (_vm->_soundemu == SOUND_EMU_COCO3) {
-		playCoCoSound();
-	} else {
-		//debugC(3, kDebugLevelSound, "playSound: Trying to play a PCjr 4-channel sound");
-		playAgiSound();
-	}
-
-	if (!_playing) {
-		if (_vm->_soundemu != SOUND_EMU_APPLE2GS) {
-			for (i = 0; i < NUM_CHANNELS; _chn[i++].vol = 0)
-				;
-		}
-
-		if (_endflag != -1)
-			_vm->setflag(_endflag, true);
-
-		if (_playingSound != -1)
-			_vm->_game.sounds[_playingSound]->stop();
-		_playingSound = -1;
-		_endflag = -1;
-	}
-}
-
-uint32 SoundMgr::mixSound() {
-	register int i, p;
-	const int16 *src;
-	int c, b, m;
-
-	memset(_sndBuffer, 0, BUFFER_SIZE << 1);
-
-	if (!_playing || _playingSound == -1)
-		return BUFFER_SIZE;
-
-	// Handle Apple IIGS sound mixing here
-	// TODO: Implement playing both waves in an oscillator
-	// TODO: Implement swap-mode in an oscillator
-	if (_vm->_soundemu == SOUND_EMU_APPLE2GS) {
-		for (uint midiChan = 0; midiChan < _gsSound->_midiChannels.size(); midiChan++) {
-			for (uint gsChan = 0; gsChan < _gsSound->_midiChannels[midiChan]._gsChannels.size(); gsChan++) {
-				IIgsChannelInfo &channel = _gsSound->_midiChannels[midiChan]._gsChannels[gsChan];
-				if (channel.playing()) { // Only mix in actively playing channels
-					// Frequency multiplier was 1076.0 based on tests made with MESS 0.117.
-					// Tests made with KEGS32 averaged the multiplier to around 1045.
-					// So this is a guess but maybe it's 1046.5... i.e. C6's frequency?
-					double hertz = C6_FREQ * pow(SEMITONE, fracToDouble(channel.note));
-					channel.posAdd = doubleToFrac(hertz / getRate());
-					channel.vol = doubleToFrac(fracToDouble(channel.envVol) * fracToDouble(channel.chanVol) / 127.0);
-					double tempVol = fracToDouble(channel.vol)/127.0;
-					for (i = 0; i < IIGS_BUFFER_SIZE; i++) {
-						b = channel.relocatedSample[fracToInt(channel.pos)];
-						// TODO: Find out what volume/amplification setting is loud enough
-						//       but still doesn't clip when playing many channels on it.
-						_sndBuffer[i] += (int16) (b * tempVol * 256/4);
-						channel.pos += channel.posAdd;
-
-						if (channel.pos >= intToFrac(channel.size)) {
-							if (channel.loop) {
-								// Don't divide by zero on zero length samples
-								channel.pos %= intToFrac(channel.size + (channel.size == 0));
-								// Probably we should loop the envelope too
-								channel.envSeg = 0;
-								channel.envVol = channel.startEnvVol;
-							} else {
-								channel.pos = channel.chanVol = 0;
-								channel.end = true;
-								break;
-							}
-						}
-					}
-
-					if (channel.envSeg < ENVELOPE_SEGMENT_COUNT) {
-						const IIgsEnvelopeSegment &seg = channel.ins->env.seg[channel.envSeg];
-						// I currently assume enveloping works with the same speed as the MIDI
-						// (i.e. with 1/60ths of a second ticks).
-						// TODO: Check if enveloping really works with the same speed as MIDI
-						frac_t envVolDelta = doubleToFrac(seg.inc/256.0);
-						if (intToFrac(seg.bp) >= channel.envVol) {
-							channel.envVol += envVolDelta;
-							if (channel.envVol >= intToFrac(seg.bp)) {
-								channel.envVol = intToFrac(seg.bp);
-								channel.envSeg += 1;
-							}
-						} else {
-							channel.envVol -= envVolDelta;
-							if (channel.envVol <= intToFrac(seg.bp)) {
-								channel.envVol = intToFrac(seg.bp);
-								channel.envSeg += 1;
-							}
-						}
-					}
-				}
-			}
-		}
-		_gsSound->removeStoppedSounds();
-		return IIGS_BUFFER_SIZE;
-	} // else ...
-
-	// Handle PCjr 4-channel sound mixing here
-	for (c = 0; c < NUM_CHANNELS; c++) {
-		if (!_chn[c].vol)
-			continue;
-
-		m = _chn[c].flags & AGI_SOUND_ENVELOPE ?
-		    _chn[c].vol * _chn[c].env >> 16 : _chn[c].vol;
-
-		if (_chn[c].type != AGI_SOUND_4CHN || c != 3) {
-			src = _chn[c].ins;
-
-			p = _chn[c].phase;
-			for (i = 0; i < BUFFER_SIZE; i++) {
-				b = src[p >> 8];
-#ifdef USE_INTERPOLATION
-				b += ((src[((p >> 8) + 1) % _chn[c].size] - src[p >> 8]) * (p & 0xff)) >> 8;
-#endif
-				_sndBuffer[i] += (b * m) >> 4;
-
-				p += (uint32) 118600 *4 / _chn[c].freq;
-
-				// FIXME: Fingolfin asks: why is there a FIXME here? Please either clarify what
-				// needs fixing, or remove it!
-				// FIXME
-				if (_chn[c].flags & AGI_SOUND_LOOP) {
-					p %= _chn[c].size << 8;
-				} else {
-					if (p >= _chn[c].size << 8) {
-						p = _chn[c].vol = 0;
-						_chn[c].end = 1;
-						break;
-					}
-				}
-
-			}
-			_chn[c].phase = p;
-		} else {
-			// Add white noise
-			for (i = 0; i < BUFFER_SIZE; i++) {
-				b = _vm->_rnd->getRandomNumber(255) - 128;
-				_sndBuffer[i] += (b * m) >> 4;
-			}
-		}
-
-		switch (_chn[c].adsr) {
-		case AGI_SOUND_ENV_ATTACK:
-			// not implemented
-			_chn[c].adsr = AGI_SOUND_ENV_DECAY;
-			break;
-		case AGI_SOUND_ENV_DECAY:
-			if (_chn[c].env > _chn[c].vol * ENV_SUSTAIN + ENV_DECAY) {
-				_chn[c].env -= ENV_DECAY;
-			} else {
-				_chn[c].env = _chn[c].vol * ENV_SUSTAIN;
-				_chn[c].adsr = AGI_SOUND_ENV_SUSTAIN;
-			}
-			break;
-		case AGI_SOUND_ENV_SUSTAIN:
-			break;
-		case AGI_SOUND_ENV_RELEASE:
-			if (_chn[c].env >= ENV_RELEASE) {
-				_chn[c].env -= ENV_RELEASE;
-			} else {
-				_chn[c].env = 0;
-			}
-		}
-	}
-
-	return BUFFER_SIZE;
-}
-
-/**
- * Convert sample from 8-bit unsigned to 8-bit signed format.
- * @param source  Source stream containing the 8-bit unsigned sample data.
- * @param dest  Destination buffer for the 8-bit signed sample data.
- * @param length  Length of the sample data to be converted.
- */
-bool SoundMgr::convertWave(Common::SeekableReadStream &source, int8 *dest, uint length) {
-	// Convert the wave from 8-bit unsigned to 8-bit signed format
-	for (uint i = 0; i < length; i++)
-		dest[i] = (int8) ((int) source.readByte() - 128);
-	return !(source.eos() || source.err());
-}
-
-void SoundMgr::fillAudio(void *udata, int16 *stream, uint len) {
-	SoundMgr *soundMgr = (SoundMgr *)udata;
-	uint32 p = 0;
-
-	// current number of audio bytes in _sndBuffer
-	static uint32 data_available = 0;
-	// offset of start of audio bytes in _sndBuffer
-	static uint32 data_offset = 0;
-
-	len <<= 2;
-
-	debugC(5, kDebugLevelSound, "(%p, %p, %d)", (void *)udata, (void *)stream, len);
-
-	while (len > data_available) {
-		memcpy((uint8 *)stream + p, (uint8*)_sndBuffer + data_offset, data_available);
-		p += data_available;
-		len -= data_available;
-
-		soundMgr->playSound();
-		data_available = soundMgr->mixSound() << 1;
-		data_offset = 0;
-	}
-
-	memcpy((uint8 *)stream + p, (uint8*)_sndBuffer + data_offset, len);
-	data_offset += len;
-	data_available -= len;
-}
-
-SoundMgr::SoundMgr(AgiEngine *agi, Audio::Mixer *pMixer) : _chn() {
+SoundMgr::SoundMgr(AgiEngine *agi, Audio::Mixer *pMixer) {
 	_vm = agi;
-	_mixer = pMixer;
-	_sampleRate = pMixer->getOutputRate();
 	_endflag = -1;
 	_playingSound = -1;
-	_env = false;
-	_playing = false;
-	_sndBuffer = (int16 *)calloc(2, BUFFER_SIZE);
-	_waveform = 0;
-	_disabledMidi = false;
-	_useChorus = true;	// FIXME: Currently always true?
-	_midiDriver = 0;
-	_musicPlayer = 0;
-	_soundGen = 0;
 
-	_gsSound = new IIgsSoundMgr;
-
-	if (_vm->_soundemu == SOUND_EMU_MIDI) {
-		MidiDriverType midiDriver = MidiDriver::detectMusicDriver(MDT_MIDI | MDT_ADLIB);
-
-		_midiDriver = MidiDriver::createMidi(midiDriver);
-		_musicPlayer = new MusicPlayer(_midiDriver, this);
+	switch (_vm->_soundemu) {
+	case SOUND_EMU_NONE:
+	case SOUND_EMU_AMIGA:
+	case SOUND_EMU_PC:
+	case SOUND_EMU_MAC:
+		_soundGen = new SoundGenSarien(_vm, pMixer);
+		break;
+	case SOUND_EMU_PCJR:
+		_soundGen = new SoundGenPCJr(_vm, pMixer);
+		break;
+	case SOUND_EMU_APPLE2GS:
+		_soundGen = new SoundGen2GS(_vm, pMixer);
+		break;
+	case SOUND_EMU_COCO3:
+		_soundGen = new SoundGenCoCo3(_vm, pMixer);
+		break;
+	case SOUND_EMU_MIDI:
+		_soundGen = new SoundGenMIDI(_vm, pMixer);
+		break;
 	}
-}
-
-void SoundMgr::premixerCall(int16 *data, uint len) {
-	if (_vm->_soundemu != SOUND_EMU_MIDI)
-		fillAudio(this, data, len);
 }
 
 void SoundMgr::setVolume(uint8 volume) {
@@ -674,12 +211,6 @@ void SoundMgr::setVolume(uint8 volume) {
 }
 
 SoundMgr::~SoundMgr() {
-	free(_sndBuffer);
-	delete _gsSound;
-
-	delete _soundGen;
-	delete _musicPlayer;
-	delete _midiDriver;
 }
 
 } // End of namespace Agi

engines/agi/sound.h
@@ -26,14 +26,9 @@
 #ifndef AGI_SOUND_H
 #define AGI_SOUND_H
 
-#include "sound/audiostream.h"
 #include "sound/mixer.h"
 
-class MidiDriver;
-
 namespace Agi {
-
-#define BUFFER_SIZE	410
 
 #define SOUND_EMU_NONE	0
 #define SOUND_EMU_PC	1
@@ -43,13 +38,6 @@ namespace Agi {
 #define SOUND_EMU_APPLE2GS 5
 #define SOUND_EMU_COCO3 6
 #define SOUND_EMU_MIDI 7
-
-#define WAVEFORM_SIZE   64
-#define ENV_ATTACK	10000		/**< envelope attack rate */
-#define ENV_DECAY       1000		/**< envelope decay rate */
-#define ENV_SUSTAIN     100		/**< envelope sustain level */
-#define ENV_RELEASE	7500		/**< envelope release rate */
-#define NUM_CHANNELS    7		/**< number of sound channels */
 
 /**
  * AGI sound note structure.
@@ -70,19 +58,6 @@ struct AgiNote {
 	}
 };
 
-struct CoCoNote {
-	uint8  freq;
-	uint8  volume;
-	uint16 duration;    ///< Note duration
-
-	/** Reads a CoCoNote through the given pointer. */
-	void read(const uint8 *ptr) {
-		freq = *ptr;
-		volume = *(ptr + 1);
-		duration = READ_LE_UINT16(ptr + 2);
-	}
-};
-
 /**
  * AGI sound resource types.
  * It's probably coincidence that all the values here are powers of two
@@ -94,47 +69,26 @@ enum AgiSoundEmuType {
 	AGI_SOUND_MIDI		= 0x0002,
 	AGI_SOUND_4CHN		= 0x0008
 };
-enum AgiSoundFlags {
-	AGI_SOUND_LOOP		= 0x0001,
-	AGI_SOUND_ENVELOPE	= 0x0002
-};
-enum AgiSoundEnv {
-	AGI_SOUND_ENV_ATTACK	= 3,
-	AGI_SOUND_ENV_DECAY		= 2,
-	AGI_SOUND_ENV_SUSTAIN	= 1,
-	AGI_SOUND_ENV_RELEASE	= 0
-};
-
-
-/**
- * AGI engine sound channel structure.
- */
-struct ChannelInfo {
-	AgiSoundEmuType type;
-	const uint8 *ptr; // Pointer to the AgiNote data
-	const int16 *ins;
-	int32 size;
-	uint32 phase;
-	uint32 flags;	// ORs values from AgiSoundFlags
-	AgiSoundEnv adsr;
-	int32 timer;
-	uint32 end;
-	uint32 freq;
-	uint32 vol;
-	uint32 env;
-};
 
 class SoundMgr;
 
 class SoundGen {
 public:
-	SoundGen() {}
+	SoundGen(AgiEngine *vm, Audio::Mixer *pMixer) : _vm(vm), _mixer(pMixer) {
+		_sampleRate = pMixer->getOutputRate();
+	}
+
 	virtual ~SoundGen() {}
 
-	virtual void play(int resnum, int flag) = 0;
+	virtual void play(int resnum) = 0;
 	virtual void stop(void) = 0;
 
-	virtual void premixerCall(int16 *stream, int len) = 0;
+	AgiEngine *_vm;
+
+	Audio::Mixer *_mixer;
+	Audio::SoundHandle _soundHandle;
+
+	uint32 _sampleRate;
 };
 
 /**
@@ -177,81 +131,30 @@ protected:
 	uint16 _type; ///< Sound resource type
 };
 
-class AgiEngine;
-class IIgsSoundMgr;
-class MusicPlayer;
-
-struct IIgsExeInfo;
-
-class SoundMgr : public Audio::AudioStream {
+class SoundMgr {
 
 public:
 	SoundMgr(AgiEngine *agi, Audio::Mixer *pMixer);
 	~SoundMgr();
-	virtual void setVolume(uint8 volume);
 
-	// AudioStream API
-	int readBuffer(int16 *buffer, const int numSamples);
+	void setVolume(uint8 volume);
 
-	bool isStereo() const {
-		return false;
-	}
-
-	bool endOfData() const {
-		return false;
-	}
-
-	int getRate() const {
-		// FIXME: Ideally, we should use _sampleRate.
-		return 22050;
-	}
-
-	int _endflag;
-	AgiEngine *_vm;
-
-private:
-	Audio::Mixer *_mixer;
-	Audio::SoundHandle _soundHandle;
-
-	MusicPlayer *_musicPlayer;
-	MidiDriver *_midiDriver;
-
-	uint32 _sampleRate;
-
-	bool _playing;
-	ChannelInfo _chn[NUM_CHANNELS];
-	IIgsSoundMgr *_gsSound;
-	int _playingSound;
-	uint8 _env;
-	bool _disabledMidi;
-
-	int16 *_sndBuffer;
-	const int16 *_waveform;
-
-	bool _useChorus;
-
-	void premixerCall(int16 *buf, uint len);
-	void fillAudio(void *udata, int16 *stream, uint len);
-
-	SoundGen *_soundGen;
-
-public:
 	void unloadSound(int);
 	void playSound();
 	int initSound();
 	void deinitSound();
 	void startSound(int, int);
 	void stopSound();
-	void stopNote(int i);
-	void playNote(int i, int freq, int vol);
-	void playAgiSound();
-	void playCoCoSound();
-	uint32 mixSound();
-	bool loadInstruments();
-	void playMidiSound();
-	void playSampleSound();
-	const IIgsExeInfo *getIIgsExeInfo(enum AgiGameID gameid) const;
-	static bool convertWave(Common::SeekableReadStream &source, int8 *dest, uint length);
+
+	void soundIsFinished();
+
+private:
+	int _endflag;
+	AgiEngine *_vm;
+
+	SoundGen *_soundGen;
+
+	int _playingSound;
 };
 
 } // End of namespace Agi

commit 9c3a71b38d2d769333f35c6c66c7c3cca81292cf

AGI: Fix PCjr dissolve method used for early versions
This was a regression from ceb2909.

engines/agi/sound_pcjr.cpp
@@ -120,8 +120,6 @@ SoundGenPCJr::SoundGenPCJr(AgiBase *vm, Audio::Mixer *pMixer) : SoundGen(vm, pMi
 	else
 		_dissolveMethod = 0;
 
-	_dissolveMethod = 3;
-
 	memset(_channel, 0, sizeof(_channel));
 	memset(_tchannel, 0, sizeof(_tchannel));
 
@@ -207,7 +205,7 @@ int SoundGenPCJr::volumeCalc(SndGenChan *chan) {
 				chan->attenuationCopy = attenuation;
 
 				attenuation &= 0x0F;
-				attenuation += (16 - _mixer->getVolumeForSoundType(Audio::Mixer::kSFXSoundType) / 17);
+				attenuation += (16 - _mixer->getVolumeForSoundType(Audio::Mixer::kMusicSoundType) / 17);
 				if (attenuation > 0x0F)
 					attenuation = 0x0F;
 			}