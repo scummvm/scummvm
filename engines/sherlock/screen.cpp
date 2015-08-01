222de5a2f5fbce178fdcb4f6244bdae20a20847f
Paul Gilbert <dreammaster@scummvm.org>
2015-08-01 19:34:13 -0400

    SHERLOCK: RT: Replace constant

--- engines/sherlock/screen.cpp
+++ engines/sherlock/screen.cpp
@@ -441,7 +441,7 @@ void Screen::flushScaleImage(ImageFrame *frame, const Common::Point &pt, int16 *
 void Screen::flushImage(ImageFrame *frame, const Common::Point &pt, Common::Rect &newBounds, int scaleVal) {
 	Common::Point newPos, newSize;
 
-	if (scaleVal == 256)
+	if (scaleVal == SCALE_THRESHOLD)
 		flushImage(frame, pt, &newPos.x, &newPos.y, &newSize.x, &newSize.y);
 	else
 		flushScaleImage(frame, pt, &newPos.x, &newPos.y, &newSize.x, &newSize.y, scaleVal);
