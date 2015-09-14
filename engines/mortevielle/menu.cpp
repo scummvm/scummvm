fc7df50ed0e81aa694c7d6dbd97dbd962943702b
Thierry Crozat <criezy@scummvm.org>
2015-09-14 22:28:42 +0100

    MORTEVIELLE: Do not try to use menu.mor file
    This file has a different format from menufr.mor and menual.mor and
    we don't support it. Trying to use this file would lead to wrong menu.
    If the game version does not contain menufr.mor it has to get the
    menu from the DAT file.

--- engines/mortevielle/menu.cpp
+++ engines/mortevielle/menu.cpp
@@ -682,13 +682,10 @@ void Menu::initMenu() {
 	if (!menuLoaded) {
 		// Load menu from game data using the original language
 		if (_vm->getOriginalLanguage() == Common::FR_FRA) {
-			if (f.exists("menufr.mor")) {
-				if (!f.open("menufr.mor"))
-					error("Missing file - menufr.mor");
-			} else {
-				if (!f.open("menu.mor"))
-					error("Missing file - menu.mor");
-			}
+			// We do not support reading menu.mor (it has a different format).
+			// If the game version does not have menufr.mor it has to get the menu from the DAT file.
+			if (!f.open("menufr.mor"))
+				error("Missing file - menufr.mor");
 		} else { // Common::DE_DEU
 			if (!f.open("menual.mor"))
 				error("Missing file - menual.mor");
