1779fba59bb94b136be640834036df0f67881319
Eugene Sandulenko <sev@scummvm.org>
2015-12-04 11:52:46 +0100

    SWORD25: Mark Sword 2.5 as testing

--- engines/sword25/detection_tables.h
+++ engines/sword25/detection_tables.h
@@ -29,7 +29,7 @@ static const ADGameDescription gameDescriptions[] = {
 		AD_ENTRY1s("data.b25c", "f8b6e03ada2d2f6cf27fbc11ad1572e9", 654310588),
 		Common::EN_ANY,
 		Common::kPlatformUnknown,
-		ADGF_UNSTABLE,
+		ADGF_TESTING,
 		GUIO1(GUIO_NOASPECT)
 	},
 	{
@@ -38,7 +38,7 @@ static const ADGameDescription gameDescriptions[] = {
 		AD_ENTRY1s("lang_fr.b25c", "690caf157387e06d2c3d1ca53c43f428", 1006043),
 		Common::FR_FRA,
 		Common::kPlatformUnknown,
-		ADGF_UNSTABLE,
+		ADGF_TESTING,
 		GUIO1(GUIO_NOASPECT)
 	},
 	{
@@ -47,7 +47,7 @@ static const ADGameDescription gameDescriptions[] = {
 		AD_ENTRY1s("data.b25c", "f8b6e03ada2d2f6cf27fbc11ad1572e9", 654310588),
 		Common::DE_DEU,
 		Common::kPlatformUnknown,
-		ADGF_UNSTABLE,
+		ADGF_TESTING,
 		GUIO1(GUIO_NOASPECT)
 	},
 	{
@@ -56,7 +56,7 @@ static const ADGameDescription gameDescriptions[] = {
 		AD_ENTRY1s("lang_hr.b25c", "e881054d1f8ec1e527422fc521c25405", 1273217),
 		Common::HR_HRV,
 		Common::kPlatformUnknown,
-		ADGF_UNSTABLE,
+		ADGF_TESTING,
 		GUIO1(GUIO_NOASPECT)
 	},
 	{
@@ -65,7 +65,7 @@ static const ADGameDescription gameDescriptions[] = {
 		AD_ENTRY1s("lang_it.b25c", "f3325666da0515cc2b42062e953c0889", 996197),
 		Common::IT_ITA,
 		Common::kPlatformUnknown,
-		ADGF_UNSTABLE,
+		ADGF_TESTING,
 		GUIO1(GUIO_NOASPECT)
 	},
 	{
@@ -74,7 +74,7 @@ static const ADGameDescription gameDescriptions[] = {
 		AD_ENTRY1s("lang_pl.b25c", "49dc1a20f95391a808e475c49be2bac0", 1281799),
 		Common::PL_POL,
 		Common::kPlatformUnknown,
-		ADGF_UNSTABLE,
+		ADGF_TESTING,
 		GUIO1(GUIO_NOASPECT)
 	},
 	{
@@ -83,7 +83,7 @@ static const ADGameDescription gameDescriptions[] = {
 		AD_ENTRY1s("lang_pt.b25c", "1df701432f9e13dcefe1adeb890b9c69", 993812),
 		Common::PT_BRA,
 		Common::kPlatformUnknown,
-		ADGF_UNSTABLE,
+		ADGF_TESTING,
 		GUIO1(GUIO_NOASPECT)
 	},
 	{
@@ -92,7 +92,7 @@ static const ADGameDescription gameDescriptions[] = {
 		AD_ENTRY1s("lang_ru.b25c", "deb33dd2f90a71ff60181918a8ce5063", 1235378),
 		Common::RU_RUS,
 		Common::kPlatformUnknown,
-		ADGF_UNSTABLE,
+		ADGF_TESTING,
 		GUIO1(GUIO_NOASPECT)
 	},
 	{
@@ -101,7 +101,7 @@ static const ADGameDescription gameDescriptions[] = {
 		AD_ENTRY1s("lang_es.b25c", "384c19072d83725f351bb9ecb4d3f02b", 987965),
 		Common::ES_ESP,
 		Common::kPlatformUnknown,
-		ADGF_UNSTABLE,
+		ADGF_TESTING,
 		GUIO1(GUIO_NOASPECT)
 	},
 	// Hungarian "psylog" version.
@@ -112,7 +112,7 @@ static const ADGameDescription gameDescriptions[] = {
 		AD_ENTRY1s("lang_hu.b25c", "7de51a3b4926a192549e75b1a7d81667", 1864915),
 		Common::HU_HUN,
 		Common::kPlatformUnknown,
-		ADGF_UNSTABLE,
+		ADGF_TESTING,
 		GUIO1(GUIO_NOASPECT)
 	},
 
@@ -126,7 +126,7 @@ static const ADGameDescription gameDescriptions[] = {
 		 AD_LISTEND},
 		Common::EN_ANY,
 		Common::kPlatformUnknown,
-		GF_EXTRACTED | ADGF_UNSTABLE,
+		GF_EXTRACTED | ADGF_TESTING,
 		GUIO1(GUIO_NOASPECT)
 	},
 
@@ -138,7 +138,7 @@ static const ADGameDescription gameDescriptions[] = {
 		AD_ENTRY1s("data.b25c", "880a8a67faf4a4e7ab62cf114b771428", 827397764),
 		Common::EN_ANY,
 		Common::kPlatformUnknown,
-		ADGF_UNSTABLE,
+		ADGF_TESTING,
 		GUIO1(GUIO_NOASPECT)
 	},
 
