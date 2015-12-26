scummvm/scummvm

commit be1fd471be362eff1d6e05bef8b8469655174b16

LAB: better fix of loadMapData, taking into account the other loops on _maps

engines/lab/map.cpp
@@ -87,8 +87,8 @@ void LabEngine::loadMapData() {
 		_music->stopSoundEffect();
 
 	_maxRooms = mapFile->readUint16LE();
-	_maps = new MapData[_maxRooms];	// will be freed when the user exits the map
-	for (int i = 0; i < _maxRooms; i++) {
+	_maps = new MapData[_maxRooms + 1];	// will be freed when the user exits the map
+	for (int i = 1; i <= _maxRooms; i++) {
 		_maps[i]._x = mapFile->readUint16LE();
 		_maps[i]._y = mapFile->readUint16LE();
 		_maps[i]._pageNumber = mapFile->readUint16LE();

commit e4fbcabbc96d10077c3bfd7af548d1d7ab6ca09e

LAB: Fix map
Regression from be1fd471be362eff1d6e05bef8b8469655174b16.

engines/lab/map.cpp
@@ -86,7 +86,7 @@ void LabEngine::loadMapData() {
 
 	_maxRooms = mapFile->readUint16LE();
 	_maps = new MapData[_maxRooms + 1];	// will be freed when the user exits the map
-	for (int i = 1; i <= _maxRooms; i++) {
+	for (int i = 0; i <= _maxRooms; i++) {
 		_maps[i]._x = mapFile->readUint16LE();
 		_maps[i]._y = mapFile->readUint16LE();
 		_maps[i]._pageNumber = mapFile->readUint16LE();