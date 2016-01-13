scummvm/scummvm

commit cb5db404c005e9ce982bee2a7abccad3ba043dc4

CONFIGURE: Update dependency list for fluidsynth on OS X
Also add missing cc_check_clean after fluid synth check.

ports.mk
@@ -247,7 +247,7 @@ endif
 
 ifdef USE_FLUIDSYNTH
 OSX_STATIC_LIBS += \
-                -liconv -framework CoreMIDI \
+                -liconv -lreadline -framework CoreMIDI -framework CoreAudio\
                 $(STATICLIBPATH)/lib/libfluidsynth.a \
                 $(STATICLIBPATH)/lib/libglib-2.0.a \
                 $(STATICLIBPATH)/lib/libintl.a

configure
@@ -3906,10 +3906,7 @@ case $_host_os in
 	;;
 
 	darwin*)
-		# TODO: Check actual requirements for static FluidSynth and
-		# dependencies linking. This is currently based on iOS dependencies
-		# which might be different due to less features enabled for iOS.
-		FLUIDSYNTH_STATIC_LIBS="$FLUIDSYNTH_LIBS -framework Foundation -framework CoreMIDI -lglib-2.0 -lintl -liconv"
+		FLUIDSYNTH_STATIC_LIBS="$FLUIDSYNTH_LIBS -framework Foundation -framework CoreMIDI -framework CoreAudio -lglib-2.0 -lintl -liconv -lreadline"
 	;;
 
 	iphone)
@@ -3932,6 +3929,7 @@ EOF
 		FLUIDSYNTH_LIBS="$FLUIDSYNTH_STATIC_LIBS"
 		cc_check_no_clean $FLUIDSYNTH_CFLAGS $FLUIDSYNTH_LIBS && _fluidsynth=yes
 	fi
+	cc_check_clean
 fi
 if test "$_fluidsynth" = yes; then
 	append_var LIBS "$FLUIDSYNTH_LIBS"

commit 6224f97cf060cb9cb0a2cac0ca003020047b1d00

BUILD: Fix ios7bundle/iphone bundle targets again.
iOS SDKs do not contain a libreadline, thus only link that for Mac OS X.
Regression from: cb5db404c005e9ce982bee2a7abccad3ba043dc4

ports.mk
@@ -247,10 +247,16 @@ endif
 
 ifdef USE_FLUIDSYNTH
 OSX_STATIC_LIBS += \
-                -liconv -lreadline -framework CoreMIDI -framework CoreAudio\
+                -liconv -framework CoreMIDI -framework CoreAudio\
                 $(STATICLIBPATH)/lib/libfluidsynth.a \
                 $(STATICLIBPATH)/lib/libglib-2.0.a \
                 $(STATICLIBPATH)/lib/libintl.a
+
+ifneq ($(BACKEND), iphone)
+ifneq ($(BACKEND), ios7)
+OSX_STATIC_LIBS += -lreadline
+endif
+endif
 endif
 
 ifdef USE_MAD