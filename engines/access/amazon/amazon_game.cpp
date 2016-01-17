bd3f64a8cf95278aa9ec70ce29d0c50f48a2e4a9
Paul Gilbert <dreammaster@scummvm.org>
2016-01-17 11:14:48 -0500

    ACCESS: Handle differences in CD vs Floppy establishment text offsets

--- engines/access/amazon/amazon_game.cpp
+++ engines/access/amazon/amazon_game.cpp
@@ -617,7 +617,12 @@ void AmazonEngine::startChapter(int chapter) {
 
 	_establishGroup = 1;
 	loadEstablish(0x40 + _chapter);
-	uint16 msgOffset = READ_LE_UINT16(_establish->data() + ((0x40 + _chapter) * 2));
+
+	byte *entryOffset = _establish->data() + ((0x40 + _chapter) * 2);
+	if (isCD())
+		entryOffset += 2;
+
+	uint16 msgOffset = READ_LE_UINT16(entryOffset);
 	_printEnd = 170;
 
 	Common::String msg((const char *)_establish->data() + msgOffset);
