7a7c783fec16387556dd432cd7f32552595d80af
Johannes Schickel <lordhoto@scummvm.org>
2016-01-26 02:40:48 +0100

    DRASCULA: Only request actual save slots in listSaves.

--- engines/drascula/detection.cpp
+++ engines/drascula/detection.cpp
@@ -352,7 +352,7 @@ SaveStateList DrasculaMetaEngine::listSaves(const char *target) const {
 	Common::SaveFileManager *saveFileMan = g_system->getSavefileManager();
 	Common::StringArray filenames;
 	Common::String pattern = target;
-	pattern += ".???";
+	pattern += ".###";
 
 	filenames = saveFileMan->listSavefiles(pattern);
 	sort(filenames.begin(), filenames.end());	// Sort (hopefully ensuring we are sorted numerically..)
