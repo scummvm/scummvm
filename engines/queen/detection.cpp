7bfd43222312c9384ec0badb164979b3d7431ec6
Johannes Schickel <lordhoto@scummvm.org>
2016-01-26 02:42:55 +0100

    QUEEN: Only request actual save slots in listSaves.

--- engines/queen/detection.cpp
+++ engines/queen/detection.cpp
@@ -461,7 +461,7 @@ SaveStateList QueenMetaEngine::listSaves(const char *target) const {
 	Common::SaveFileManager *saveFileMan = g_system->getSavefileManager();
 	Common::StringArray filenames;
 	char saveDesc[32];
-	Common::String pattern("queen.s??");
+	Common::String pattern("queen.s##");
 
 	filenames = saveFileMan->listSavefiles(pattern);
 	sort(filenames.begin(), filenames.end());	// Sort (hopefully ensuring we are sorted numerically..)
