24daecd8f3acb82ec6b0717da6eb2c27a7412bdd
Johannes Schickel <lordhoto@scummvm.org>
2016-01-26 02:40:05 +0100

    CGE: Only request actual save slots in listSaves.

--- engines/cge/detection.cpp
+++ engines/cge/detection.cpp
@@ -196,7 +196,7 @@ SaveStateList CGEMetaEngine::listSaves(const char *target) const {
 	Common::SaveFileManager *saveFileMan = g_system->getSavefileManager();
 	Common::StringArray filenames;
 	Common::String pattern = target;
-	pattern += ".???";
+	pattern += ".###";
 
 	filenames = saveFileMan->listSavefiles(pattern);
 	sort(filenames.begin(), filenames.end());   // Sort (hopefully ensuring we are sorted numerically..)
