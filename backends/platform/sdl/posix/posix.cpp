79acfd28e9e774631f2e0af2bba34a3c0de9b40f
Johannes Schickel <lordhoto@scummvm.org>
2016-01-29 19:26:05 +0100

    POSIX: Use modern path for log file.
    This makes use use the XDG Base Directory Specification for the log file path.
    This tackles one part of bug #6036 "POSIX: Use XDG dirs instead of HOME".

--- backends/platform/sdl/posix/posix.cpp
+++ backends/platform/sdl/posix/posix.cpp
@@ -180,12 +180,20 @@ Common::WriteStream *OSystem_POSIX::createLogFile() {
 	prefix = nullptr;
 	logFile = "/mtd_ram";
 #else
-	prefix = getenv("HOME");
-	if (prefix == nullptr) {
-		return 0;
+	// On POSIX systems we follow the XDG Base Directory Specification for
+	// where to store files. The version we based our code upon can be found
+	// over here: http://standards.freedesktop.org/basedir-spec/basedir-spec-0.8.html
+	prefix = getenv("XDG_CACHE_HOME");
+	if (prefix == nullptr || !*prefix) {
+		prefix = getenv("HOME");
+		if (prefix == nullptr) {
+			return 0;
+		}
+
+		logFile = ".cache/";
 	}
 
-	logFile = ".scummvm/logs";
+	logFile += "scummvm/logs";
 #endif
 
 	if (!assureDirectoryExists(logFile, prefix)) {
