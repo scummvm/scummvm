b24be406d27373b5ea1a041eceba4dfea42cbb82
Eugene Sandulenko <sev@scummvm.org>
2016-02-04 11:37:47 +0100

    WAGE: Fixes to selection highlighting

--- engines/wage/gui.cpp
+++ engines/wage/gui.cpp
@@ -508,8 +508,8 @@ void Gui::renderConsole(Graphics::Surface *g, Common::Rect &r) {
 
 		if (line == _selectionStartY || line == _selectionEndY) {
 			if (_selectionStartY != _selectionEndY) {
-				int color1 = kColorWhite;
-				int color2 = kColorBlack;
+				int color1 = kColorBlack;
+				int color2 = kColorWhite;
 				int midpoint = _selectionStartX;
 
 				if (_selectionStartY > _selectionEndY)
@@ -523,8 +523,6 @@ void Gui::renderConsole(Graphics::Surface *g, Common::Rect &r) {
 				Common::String beg(_lines[line].c_str(), &_lines[line].c_str()[midpoint]);
 				Common::String end(&_lines[line].c_str()[midpoint]);
 
-				warning("beg: %s  end: %s", beg.c_str(), end.c_str());
-
 				int rectW = font->getStringWidth(beg) + kConWPadding + kConWOverlap;
 				Common::Rect trect(0, y1, _console.w, y1 + _consoleLineHeight);
 				if (color1 == kColorWhite)
@@ -535,7 +533,7 @@ void Gui::renderConsole(Graphics::Surface *g, Common::Rect &r) {
 				Design::drawFilledRect(&_console, trect, kColorBlack, _patterns, kPatternSolid);
 
 				font->drawString(&_console, beg, x1, y1, textW, color1);
-				font->drawString(&_console, end, x1 + rectW, y1, textW, color2);
+				font->drawString(&_console, end, x1 + rectW - kConWPadding - kConWOverlap, y1, textW, color2);
 			}
 		} else {
 			if (*str)
