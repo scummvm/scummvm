1b27abafdc17ca4f5855b63cb53712fabd9693cb
Eugene Sandulenko <sev@scummvm.org>
2016-02-12 19:18:54 +0100

    WAGE: Skip disabled command on shortcuts

--- engines/wage/menu.cpp
+++ engines/wage/menu.cpp
@@ -547,7 +547,7 @@ void Menu::processMenuShortCut(byte flags, uint16 ascii) {
 	if (flags & (Common::KBD_CTRL | Common::KBD_META)) {
 		for (int i = 0; i < _items.size(); i++)
 			for (int j = 0; j < _items[i]->subitems.size(); j++)
-				if (tolower(_items[i]->subitems[j]->shortcut) == ascii) {
+				if (_items[i]->subitems[j]->enabled && tolower(_items[i]->subitems[j]->shortcut) == ascii) {
 					executeCommand(_items[i]->subitems[j]);
 					break;
 				}
