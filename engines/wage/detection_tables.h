b007821d36d73b2b86c3f5aba075eb577a49d92e
Eugene Sandulenko <sev@scummvm.org>
2016-02-15 19:16:11 +0100

    WAGE: Move detection tables to separate file

---
+++ engines/wage/detection_tables.h
@@ -0,0 +1,114 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+namespace Wage {
+
+#define ADGF_DEFAULT (ADGF_DROPLANGUAGE|ADGF_DROPPLATFORM)
+#define ADGF_GENERIC (ADGF_DROPLANGUAGE|ADGF_DROPPLATFORM|ADGF_USEEXTRAASTITLE)
+
+static const ADGameDescription gameDescriptions[] = {
+	{
+		"afm",
+		"v1.8",
+		AD_ENTRY1s("Another Fine Mess 1.8", "8e5aa915f3253efb2aab52435647b25e", 1456000),
+		Common::EN_ANY,
+		Common::kPlatformMacintosh,
+		ADGF_DEFAULT,
+		GUIO0()
+	},
+	{
+		"amot",
+		"v1.8",
+		AD_ENTRY1s("A Mess O' Trouble 1.8", "b3ef53afed282671b704e45df829350c", 1895552),
+		Common::EN_ANY,
+		Common::kPlatformMacintosh,
+		ADGF_DEFAULT,
+		GUIO0()
+	},
+	{
+		"cantitoe",
+		"",
+		AD_ENTRY1s("Camp Cantitoe", "098aa5c11c58e1ef274a30a9e01b4755", 621440),
+		Common::EN_ANY,
+		Common::kPlatformMacintosh,
+		ADGF_DEFAULT,
+		GUIO0()
+	},
+	{
+		"wage",
+		"Deep Angst",
+		AD_ENTRY1s("Deep Angst", "635f62bbc569e72b03cab9107927d03d", 335232),
+		Common::EN_ANY,
+		Common::kPlatformMacintosh,
+		ADGF_GENERIC,
+		GUIO0()
+	},
+	{
+		"wage",
+		"Escape from School!",
+		AD_ENTRY1s("Escape from School!", "a854be48d4af20126d18a9cad93a969b", 51840),
+		Common::EN_ANY,
+		Common::kPlatformMacintosh,
+		ADGF_GENERIC,
+		GUIO0()
+	},
+	{
+		"wage",
+		"Queen Quest",
+		AD_ENTRY1s("Queen Quest", "730605d312efedb5e3ff108522fcac18", 59776),
+		Common::EN_ANY,
+		Common::kPlatformMacintosh,
+		ADGF_GENERIC,
+		GUIO0()
+	},
+	{
+		"scepters",
+		"",
+		AD_ENTRY1s("Scepters", "b80bff315897776dda7689cdf829fab4", 360832),
+		Common::EN_ANY,
+		Common::kPlatformMacintosh,
+		ADGF_DEFAULT,
+		GUIO0()
+	},
+	{
+		"wage",
+		"ZikTuria",
+		AD_ENTRY1s("ZikTuria", "e793155bed1a70fa2074a3fcd696b751", 54784),
+		Common::EN_ANY,
+		Common::kPlatformMacintosh,
+		ADGF_GENERIC,
+		GUIO0()
+	},
+	{
+		"wage",
+		"Zoony",
+		AD_ENTRY1s("Zoony", "e6cc8a914a4215dafbcce6315dd12cf5", 160256),
+		Common::EN_ANY,
+		Common::kPlatformMacintosh,
+		ADGF_GENERIC,
+		GUIO0()
+	},
+
+	AD_TABLE_END_MARKER
+};
+
+} // End of namespace Wage
