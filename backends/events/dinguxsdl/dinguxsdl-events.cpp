4411155869e6345c235364169753257ac7212906
Eugene Sandulenko <sev@scummvm.org>
2016-02-24 23:31:37 +0100

    GCW0: Trigger keyboard only on button down, not on up, otherwise it required holding

--- backends/events/dinguxsdl/dinguxsdl-events.cpp
+++ backends/events/dinguxsdl/dinguxsdl-events.cpp
@@ -175,7 +175,9 @@ bool DINGUXSdlEventSource::remapKey(SDL_Event &ev, Common::Event &event) {
 		return true;
 	} else if (ev.key.keysym.sym == BUT_SELECT) { // virtual keyboard
 #ifdef ENABLE_VKEYBD
-		event.type = Common::EVENT_VIRTUAL_KEYBOARD;
+		if (ev.type == SDL_KEYDOWN)
+			event.type = Common::EVENT_VIRTUAL_KEYBOARD;
+
 		return true;
 #endif
 	} else if (ev.key.keysym.sym == BUT_START) { // F5, menu in some games
