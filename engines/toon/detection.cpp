d7ae0df8e105e8d5593a1b92a0834e8c19aece21
Johannes Schickel <lordhoto@scummvm.org>
2016-02-25 19:55:14 +0100

    TOON: Let listSaves return list sorted on slot numbers.

--- engines/toon/detection.cpp
+++ engines/toon/detection.cpp
@@ -176,7 +176,6 @@ SaveStateList ToonMetaEngine::listSaves(const char *target) const {
 	pattern += ".###";
 
 	filenames = saveFileMan->listSavefiles(pattern);
-	sort(filenames.begin(), filenames.end());   // Sort (hopefully ensuring we are sorted numerically..)
 
 	SaveStateList saveList;
 	for (Common::StringArray::const_iterator filename = filenames.begin(); filename != filenames.end(); ++filename) {
@@ -208,6 +207,8 @@ SaveStateList ToonMetaEngine::listSaves(const char *target) const {
 		}
 	}
 
+	// Sort saves based on slot number.
+	Common::sort(saveList.begin(), saveList.end(), SaveStateDescriptorSlotComparator());
 	return saveList;
 }
 
