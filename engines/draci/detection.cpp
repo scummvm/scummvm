0170982a53c5b7a620e4ce91ba9a79a2cb4f3fc8
Johannes Schickel <lordhoto@scummvm.org>
2016-02-25 19:55:13 +0100

    DRACI: Let listSaves return list sorted on slot numbers.

--- engines/draci/detection.cpp
+++ engines/draci/detection.cpp
@@ -120,7 +120,6 @@ SaveStateList DraciMetaEngine::listSaves(const char *target) const {
 	Common::String pattern("draci.s##");
 
 	filenames = saveFileMan->listSavefiles(pattern);
-	sort(filenames.begin(), filenames.end());	// Sort (hopefully ensuring we are sorted numerically..)
 
 	SaveStateList saveList;
 	for (Common::StringArray::const_iterator file = filenames.begin(); file != filenames.end(); ++file) {
@@ -143,6 +142,8 @@ SaveStateList DraciMetaEngine::listSaves(const char *target) const {
 		}
 	}
 
+	// Sort saves based on slot number.
+	Common::sort(saveList.begin(), saveList.end(), SaveStateDescriptorSlotComparator());
 	return saveList;
 }
 
