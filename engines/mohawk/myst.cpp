scummvm/scummvm

commit d6881100dd5613c8573f8f8235b93772ae513e49

MOHAWK: Only allow saving/loading from the main event loop

engines/mohawk/myst.cpp
@@ -76,6 +76,7 @@ MohawkEngine_Myst::MohawkEngine_Myst(OSystem *syst, const MohawkGameDescription 
 	_showResourceRects = false;
 	_curCard = 0;
 	_needsUpdate = false;
+	_canSafelySaveLoad = false;
 	_curResource = -1;
 	_hoverResource = nullptr;
 
@@ -267,7 +268,7 @@ Common::Error MohawkEngine_Myst::run() {
 		_needsUpdate = _video->updateMovies();
 		_scriptParser->runPersistentScripts();
 
-		while (_eventMan->pollEvent(event)) {
+		while (pollEvent(event)) {
 			switch (event.type) {
 			case Common::EVENT_MOUSEMOVE: {
 				_needsUpdate = true;
@@ -312,7 +313,9 @@ Common::Error MohawkEngine_Myst::run() {
 					_needsShowMap = false;
 					_needsShowDemoMenu = false;
 
+					_canSafelySaveLoad = true;
 					runDialog(*_optionsDialog);
+					_canSafelySaveLoad = false;
 
 					if (_needsPageDrop) {
 						dropPage();
@@ -348,6 +351,15 @@ Common::Error MohawkEngine_Myst::run() {
 	}
 
 	return Common::kNoError;
+}
+
+bool MohawkEngine_Myst::pollEvent(Common::Event &event) {
+	// Saving / Loading is allowed from the GMM only when the main event loop is running
+	_canSafelySaveLoad = true;
+	bool eventReturned =  _eventMan->pollEvent(event);
+	_canSafelySaveLoad = false;
+
+	return eventReturned;
 }
 
 bool MohawkEngine_Myst::skippableWait(uint32 duration) {
@@ -1083,10 +1095,14 @@ Common::Error MohawkEngine_Myst::saveGameState(int slot, const Common::String &d
 
 bool MohawkEngine_Myst::canLoadGameStateCurrently() {
 	// No loading in the demo/makingof
-	return !(getFeatures() & GF_DEMO) && getGameType() != GType_MAKINGOF;
+	return _canSafelySaveLoad && !(getFeatures() & GF_DEMO) && getGameType() != GType_MAKINGOF;
 }
 
 bool MohawkEngine_Myst::canSaveGameStateCurrently() {
+	if (!_canSafelySaveLoad) {
+		return false;
+	}
+
 	// There's a limited number of stacks the game can save in
 	switch (_curStack) {
 	case kChannelwoodStack:

engines/mohawk/myst.h
@@ -28,6 +28,7 @@
 #include "mohawk/resource_cache.h"
 #include "mohawk/myst_scripts.h"
 
+#include "common/events.h"
 #include "common/random.h"
 
 namespace Mohawk {
@@ -239,6 +240,13 @@ private:
 	MystView _view;
 
 	bool _runExitScript;
+
+	/**
+	 * Saving / Loading is only allowed from the main event loop
+	 */
+	bool _canSafelySaveLoad;
+
+	bool pollEvent(Common::Event &event);
 
 	void dropPage();
 

commit faff49d001bfaa4a6cce45509b4f80fe3ea0ea6f

MOHAWK: Fix loading a save from the launcher
Was broken by d6881100dd5613c8573f8f8235b93772ae513e49

engines/mohawk/myst.cpp
@@ -235,7 +235,7 @@ Common::Error MohawkEngine_Myst::run() {
 	_cursor->showCursor();
 
 	// Load game from launcher/command line if requested
-	if (ConfMan.hasKey("save_slot") && canLoadGameStateCurrently()) {
+	if (ConfMan.hasKey("save_slot") && hasGameSaveSupport()) {
 		uint32 gameToLoad = ConfMan.getInt("save_slot");
 		Common::StringArray savedGamesList = MystGameState::generateSaveGameList();
 		if (gameToLoad > savedGamesList.size())
@@ -1093,9 +1093,13 @@ Common::Error MohawkEngine_Myst::saveGameState(int slot, const Common::String &d
 	return _gameState->save(desc) ? Common::kNoError : Common::kUnknownError;
 }
 
+bool MohawkEngine_Myst::hasGameSaveSupport() const {
+	return !(getFeatures() & GF_DEMO) && getGameType() != GType_MAKINGOF;
+}
+
 bool MohawkEngine_Myst::canLoadGameStateCurrently() {
 	// No loading in the demo/makingof
-	return _canSafelySaveLoad && !(getFeatures() & GF_DEMO) && getGameType() != GType_MAKINGOF;
+	return _canSafelySaveLoad && hasGameSaveSupport();
 }
 
 bool MohawkEngine_Myst::canSaveGameStateCurrently() {

engines/mohawk/myst.h
@@ -245,6 +245,7 @@ private:
 	 * Saving / Loading is only allowed from the main event loop
 	 */
 	bool _canSafelySaveLoad;
+	bool hasGameSaveSupport() const;
 
 	bool pollEvent(Common::Event &event);
 