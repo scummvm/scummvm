c36e29f1addb8ec8b44639c6ae5d97f70b015038
Tobia Tesan <tobia.tesan@gmail.com>
2016-02-29 14:58:12 +0100

    WINTERMUTE: Add BlankListing class

--- engines/wintermute/module.mk
+++ engines/wintermute/module.mk
@@ -92,6 +92,7 @@ MODULE_OBJS := \
 	base/timer.o \
 	debugger/breakpoint.o \
 	debugger/error.o \
+	debugger/listing_providers/blank_listing.o \
 	debugger/listing_providers/source_listing.o \
 	debugger/listing.o \
 	debugger/script_monitor.o \
