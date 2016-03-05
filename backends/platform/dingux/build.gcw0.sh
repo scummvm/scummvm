8b3f27753ea456639923d830eecdd781df1faded
Eugene Sandulenko <sev@scummvm.org>
2016-03-05 13:05:21 +0100

    GCW0: Disabled more 640x480-only engines

--- backends/platform/dingux/build.gcw0.sh
+++ backends/platform/dingux/build.gcw0.sh
@@ -3,4 +3,4 @@
 export PATH=/opt/gcw0-toolchain/usr/bin:$PATH
 
 # Disable high resolution engines since we have 320x240 hardware
-./configure --host=gcw0 --enable-plugins --default-dynamic --enable-release  --disable-engine=mohawk,neverhood,sword25,toltecs,wintermute,zvision --disable-mt32emu --disable-hq-scalers && make -j6 gcw-opk && ls -l scummvm.opk
+./configure --host=gcw0 --enable-plugins --default-dynamic --enable-release  --disable-engine=he,mohawk,neverhood,sword1,sword2,sword25,toltecs,wintermute,zvision --disable-mt32emu --disable-hq-scalers && make -j6 gcw-opk && ls -l scummvm.opk
