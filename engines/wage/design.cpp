24af06b682c9d3a9eddc7ad8d19fbaeb0e750265
Eugene Sandulenko <sev@scummvm.org>
2016-03-09 23:59:08 +0100

    WAGE: Disabling bounds calculation code as too buggy ATM.

--- engines/wage/design.cpp
+++ engines/wage/design.cpp
@@ -87,16 +87,16 @@ void Design::paint(Graphics::Surface *surface, Patterns &patterns, int x, int y)
 	bool needRender = false;
 
 	if (_surface == NULL) {
-		_boundsCalculationMode = true;
+		//_boundsCalculationMode = true;
 		_bounds->debugPrint(4, "Internal bounds:");
-		_bounds->left = _bounds->top = 10000;
-		_bounds->right =_bounds->bottom = -10000;
-		render(patterns);
+		//_bounds->left = _bounds->top = 10000;
+		//_bounds->right =_bounds->bottom = -10000;
+		//render(patterns);
 		_boundsCalculationMode = false;
 		if (_bounds->right == -10000) {
 			_bounds->left = _bounds->top = _bounds->right = _bounds->bottom = 0;
 		}
-		_bounds->debugPrint(4, "Calculated bounds:");
+		//_bounds->debugPrint(4, "Calculated bounds:");
 
 		_surface = new Graphics::Surface;
 		_surface->create(_bounds->width(), _bounds->height(), Graphics::PixelFormat::createFormatCLUT8());
