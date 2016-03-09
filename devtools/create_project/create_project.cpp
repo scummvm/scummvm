5510fdc7e47d0b2a1bd734bef812cdf819a2a4c2
Johannes Schickel <lordhoto@scummvm.org>
2016-03-09 18:55:42 +0100

    DEVTOOLS: Use libpng16.lib for libpng for MSVC.
    Modern libpng releases use this library name as output for their MSVC project files.

--- devtools/create_project/create_project.cpp
+++ devtools/create_project/create_project.cpp
@@ -939,7 +939,7 @@ const Feature s_features[] = {
 	{       "mad",         "USE_MAD", "libmad",           true, "libmad (MP3) support" },
 	{    "vorbis",      "USE_VORBIS", "libvorbisfile_static libvorbis_static libogg_static", true, "Ogg Vorbis support" },
 	{      "flac",        "USE_FLAC", "libFLAC_static win_utf8_io_static",   true, "FLAC support" },
-	{       "png",         "USE_PNG", "libpng",           true, "libpng support" },
+	{       "png",         "USE_PNG", "libpng16",         true, "libpng support" },
 	{      "faad",        "USE_FAAD", "libfaad",          false, "AAC support" },
 	{     "mpeg2",       "USE_MPEG2", "libmpeg2",         false, "MPEG-2 support" },
 	{    "theora",   "USE_THEORADEC", "libtheora_static", true, "Theora decoding support" },
