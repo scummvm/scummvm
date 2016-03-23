031e062252d00fc7e6d930287b6dbd4a845d2a2f
Ori Avtalion <ori@avtalion.name>
2016-03-23 21:24:43 +0200

    GUI: Improve handling of button presses while moving the cursor
    Keep track of where a mouse press started when deciding how mouse-over
    and mouse-up should behave.
    
    This handles the following situations:
    
    1) If a mouse press starts outside a button (e.g. the UI background),
    mouse-up inside the button has no effect. Previously, it triggered a
    button click.
    2) If a mouse press starts inside a button, the cursor moves outside of
    its region, then back inside, the button will show as pressed.
    Previously, it showed as highlighted instead of pressed.

--- gui/widget.h
+++ gui/widget.h
@@ -198,7 +198,7 @@ public:
 
 	void handleMouseUp(int x, int y, int button, int clickCount);
 	void handleMouseDown(int x, int y, int button, int clickCount);
-	void handleMouseEntered(int button)	{ setFlags(WIDGET_HILITED); draw(); }
+	void handleMouseEntered(int button)	{ if (_duringPress) { setFlags(WIDGET_PRESSED); } else { setFlags(WIDGET_HILITED); } draw(); }
 	void handleMouseLeft(int button)	{ clearFlags(WIDGET_HILITED | WIDGET_PRESSED); draw(); }
 	void handleTickle();
 
@@ -211,6 +211,7 @@ public:
 protected:
 	void drawWidget();
 	void wantTickle(bool tickled);
+	bool _duringPress;
 private:
 	uint32 _lastTime;
 };
