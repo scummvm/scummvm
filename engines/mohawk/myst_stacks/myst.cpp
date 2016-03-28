77c2630049d4854fffeeb7686d27adeabd253da3
Bastien Bouclet <bastien.bouclet@gmail.com>
2016-03-28 09:31:55 +0200

    MOHAWK: Play the gears sound when resetting the Myst clock puzzle
    Fixes #7093.

--- engines/mohawk/myst_stacks/myst.cpp
+++ engines/mohawk/myst_stacks/myst.cpp
@@ -3089,6 +3089,8 @@ void Myst::clockReset() {
 }
 
 void Myst::clockResetWeight() {
+	_vm->_sound->replaceSoundMyst(9113);
+
 	_clockWeightVideo = _vm->_video->playMovie(_vm->wrapMovieFilename("cl1wlfch", kMystStack));
 	if (!_clockWeightVideo)
 		error("Failed to open cl1wlfch movie");
