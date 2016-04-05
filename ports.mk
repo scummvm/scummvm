63e55cefd44e16a4d6499adc51905f5ca511f813
Eugene Sandulenko <sev@scummvm.org>
2016-04-05 15:27:28 +0200

    WEB: Put Sparkle appcast to a standard place

--- ports.mk
+++ ports.mk
@@ -407,7 +407,7 @@ osxsnap: bundle
 	rm -rf ScummVM-snapshot
 
 publish-appcast:
-	scp dists/macosx/scummvm_appcast.xml www.scummvm.org:/var/www/html/
+	scp dists/macosx/scummvm_appcast.xml www.scummvm.org:/var/www/html/appcasts/macosx/release.xml
 
 #
 # Windows specific
