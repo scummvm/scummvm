c22ca8ffb515b03712a0f0e36dd40c7e38c4b8ed
Eugene Sandulenko <sev@scummvm.org>
2016-05-15 13:19:27 +0200

    KYRA: Plug memory leak

--- engines/kyra/staticres.cpp
+++ engines/kyra/staticres.cpp
@@ -839,6 +839,8 @@ void KyraEngine_LoK::initStaticResource() {
 		_sound->initAudioResourceInfo(kMusicIntro, &resInfoIntro);
 		_sound->initAudioResourceInfo(kMusicIngame, &resInfoIngame);
 	}
+
+	delete[] soundFiles;
 }
 
 void KyraEngine_LoK::loadMouseShapes() {
