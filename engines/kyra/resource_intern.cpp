9909cc615d527bc8d43f930d8c88dc014974f2de
Eugene Sandulenko <sev@scummvm.org>
2016-05-15 13:14:37 +0200

    KYRA: Plug memory leak

--- engines/kyra/resource_intern.cpp
+++ engines/kyra/resource_intern.cpp
@@ -1180,6 +1180,8 @@ Common::Archive *InstallerLoader::load(Resource *owner, const Common::String &fi
 			delete tmpFile;
 			tmpFile = 0;
 		}
+		delete[] outbuffer;
+		outbuffer = 0;
 	}
 
 	archives.clear();
