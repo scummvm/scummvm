a7eb756233d554239a8343bde213a725ebd2d5d6
Strangerke <strangerke@scummvm.org>
2016-06-06 00:22:48 +0200

    GUI: Add credits for Gnap

--- devtools/credits.pl
+++ devtools/credits.pl
@@ -614,6 +614,11 @@ begin_credits("Credits");
 				add_person("Willem Jan Palenstijn", "wjp", "");
 			end_section();
 
+			begin_section("Gnap");
+				add_person("Arnaud Boutonn&eacute;", "Strangerke", "");
+				add_person("Benjamin Haisch", "john_doe", "");
+			end_section();
+
 			begin_section("Gob");
 				add_person("Torbj&ouml;rn Andersson", "eriktorbjorn", "");
 				add_person("Arnaud Boutonn&eacute;", "Strangerke", "");
