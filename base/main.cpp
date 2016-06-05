8294d55a53d2afe45029a07f336f19bf6ead1b79
Bastien Bouclet <bastien.bouclet@gmail.com>
2016-06-05 11:43:07 +0200

    BASE: Allow setting the debug flags from the configuration file
    Useful for devices where passing command line arguments is not possible.

--- base/main.cpp
+++ base/main.cpp
@@ -388,7 +388,8 @@ extern "C" int scummvm_main(int argc, const char * const argv[]) {
 	if (settings.contains("debugflags")) {
 		specialDebug = settings["debugflags"];
 		settings.erase("debugflags");
-	}
+	} else if (ConfMan.hasKey("debugflags"))
+		specialDebug = ConfMan.get("debugflags");
 
 	PluginManager::instance().init();
  	PluginManager::instance().loadAllPlugins(); // load plugins for cached plugin manager
