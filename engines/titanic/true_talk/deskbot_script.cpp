16797bd75ff0bbbfbd52c99ad3f7d5ac884b37a5
Paul Gilbert <dreammaster@scummvm.org>
2016-06-06 22:40:34 -0400

    TITANIC: Add loading of Deskbot ranges

--- engines/titanic/true_talk/deskbot_script.cpp
+++ engines/titanic/true_talk/deskbot_script.cpp
@@ -40,6 +40,7 @@ DeskbotScript::DeskbotScript(int val1, const char *charClass, int v2,
 	if (_field74 == 1)
 		_field74 = 0;
 
+	loadRanges("Ranges/Deskbot");
 	loadResponses("Responses/Deskbot", 4);
 }
 
