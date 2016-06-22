0ad91dfe6723e04271cbb44e7ccbce3071e1af6a
Strangerke <strangerke@scummvm.org>
2016-06-22 23:21:18 +0200

    DM: Add missing explicit keyword

--- engines/dm/text.h
+++ engines/dm/text.h
@@ -9,7 +9,7 @@ namespace DM {
 class TextMan {
 	DMEngine *_vm;
 public:
-	TextMan(DMEngine *vm);
+	explicit TextMan(DMEngine *vm);
 	void printTextToBitmap(byte *destBitmap, uint16 destPixelWidth, uint16 destX, uint16 destY,
 						   Color textColor, Color bgColor, const char *text, uint16 destHeight, Viewport &viewport = gDefultViewPort); // @ F0040_TEXT_Print
 	void printTextToScreen(uint16 destX, uint16 destY, Color textColor, Color bgColor, const char *text, Viewport &viewport = gDefultViewPort); // @ F0053_TEXT_PrintToLogicalScreen
