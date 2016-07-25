a21c6778314a17e56d9faf5c5d6693a63cc634e5
Strangerke <strangerke@scummvm.org>
2016-07-25 16:17:50 +0200

    DM: Remove useless cast and the associated obsolete comment

--- engines/dm/timeline.cpp
+++ engines/dm/timeline.cpp
@@ -380,8 +380,7 @@ T0261053:
 }
 
 bool Timeline::f240_isFirstEventExpiered() {
-	warning(false, "possibly dangerous cast to uint32");
-	return (_vm->_timeline->_g372_eventCount && ((uint32)M30_time(_vm->_timeline->_g370_events[_vm->_timeline->_g371_timeline[0]]._mapTime) <= _vm->_g313_gameTime));
+	return (_vm->_timeline->_g372_eventCount && (M30_time(_vm->_timeline->_g370_events[_vm->_timeline->_g371_timeline[0]]._mapTime) <= _vm->_g313_gameTime));
 }
 
 void Timeline::f239_timelineExtractFirstEvent(TimelineEvent* event) {
