06d9e188dfbe465c3adcc3e35d9547213c759d65
Borja Lorente <blorente@ucm.es>
2016-08-02 18:55:18 +0200

    MACVENTURE: Fix scroll in GUI

--- engines/macventure/gui.h
+++ engines/macventure/gui.h
@@ -340,6 +340,7 @@ public:
 	ConsoleText(Gui *gui) {
 		_gui = gui;
 		_lines.push_back("");
+		updateScroll();
 	}
 
 	~ConsoleText() {
@@ -377,11 +378,10 @@ public:
 	}
 
 	void scrollDown() {
-		if (_scrollPos < _lines.size() - 1) _scrollPos++;
+		if (_scrollPos < (int)(_lines.size() - 1)) _scrollPos++;
 	}
 
 	void scrollUp() {
-		debug("scroll up");
 		if (_scrollPos > 0) _scrollPos--;
 	}
 
