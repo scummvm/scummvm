381e8fc51d34ebcb02566b608c201dbe96aad1a0
Eugene Sandulenko <sev@scummvm.org>
2016-08-24 23:23:47 +0200

    WAGE: Fix think line drawing, as pointed by md5

--- engines/wage/design.cpp
+++ engines/wage/design.cpp
@@ -235,9 +235,9 @@ void drawPixel(int x, int y, int color, void *data) {
 					color : kColorWhite;
 		}
 	} else {
-		int x1 = x;
+		int x1 = x - p->thickness / 2;
 		int x2 = x1 + p->thickness;
-		int y1 = y;
+		int y1 = y - p->thickness / 2;
 		int y2 = y1 + p->thickness;
 
 		for (y = y1; y < y2; y++)
