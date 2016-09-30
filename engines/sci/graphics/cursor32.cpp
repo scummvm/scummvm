scummvm/scummvm

commit d0517f515eb7f36ec7222f3d50f4917fbd7df5e7

SCI32: Update screen on frameout, instead of in the event loop

engines/sci/graphics/cursor32.cpp
@@ -122,6 +122,7 @@ void GfxCursor32::drawToHardware(const DrawRegion &source) {
 	byte *sourcePixel = source.data + (sourceYOffset * source.rect.width()) + sourceXOffset;
 
 	g_system->copyRectToScreen(sourcePixel, source.rect.width(), drawRect.left, drawRect.top, drawRect.width(), drawRect.height());
+	g_system->updateScreen();
 }
 
 void GfxCursor32::unhide() {

engines/sci/graphics/frameout.cpp
@@ -1108,6 +1108,7 @@ void GfxFrameout::mergeToShowList(const Common::Rect &drawRect, RectList &showLi
 
 void GfxFrameout::showBits() {
 	if (!_showList.size()) {
+		g_system->updateScreen();
 		return;
 	}
 
@@ -1146,6 +1147,7 @@ void GfxFrameout::showBits() {
 	_cursor->donePainting();
 
 	_showList.clear();
+	g_system->updateScreen();
 }
 
 void GfxFrameout::alterVmap(const Palette &palette1, const Palette &palette2, const int8 style, const int8 *const styleRanges) {

engines/sci/graphics/palette32.cpp
@@ -295,7 +295,7 @@ void GfxPalette32::updateHardware(const bool updateScreen) {
 
 	g_system->getPaletteManager()->setPalette(bpal, 0, 256);
 	if (updateScreen) {
-		g_sci->getEventManager()->updateScreen();
+		g_system->updateScreen();
 	}
 }
 

engines/sci/engine/kevent.cpp
@@ -236,7 +236,7 @@ reg_t kGetEvent(EngineState *s, int argc, reg_t *argv) {
 	// check bugs #3058865 and #3127824
 	if (s->_gameIsBenchmarking) {
 		// Game is benchmarking, don't add a delay
-	} else {
+	} else if (getSciVersion() < SCI_VERSION_2) {
 		g_system->delayMillis(10);
 	}
 

engines/sci/event.cpp
@@ -370,7 +370,9 @@ SciEvent EventManager::getSciEvent(uint32 mask) {
 	SciEvent event = { SCI_EVENT_NONE, 0, 0, Common::Point() };
 #endif
 
-	EventManager::updateScreen();
+	if (getSciVersion() < SCI_VERSION_2) {
+		updateScreen();
+	}
 
 	// Get all queued events from graphics driver
 	do {

commit f17b158b45141a31396d4d74769cc3b4d6cbf8ff

SCI32: Restore return to launcher functionality
RTL functionality has been broken by commit d0517f515e.
There is a check in EventManager::updateScreen() for shouldQuit(),
which is throttled to 60fps. This also restores the screen update
throttling to 60fps via the EventManager

engines/sci/graphics/cursor32.cpp
@@ -25,6 +25,7 @@
 #include "common/memstream.h"
 #include "graphics/cursorman.h"     // for CursorMan
 #include "graphics/maccursor.h"
+#include "sci/event.h"
 #include "sci/graphics/celobj32.h"  // for CelObjView, CelInfo32, Ratio
 #include "sci/graphics/cursor32.h"
 #include "sci/graphics/frameout.h"  // for GfxFrameout
@@ -122,7 +123,7 @@ void GfxCursor32::drawToHardware(const DrawRegion &source) {
 	byte *sourcePixel = source.data + (sourceYOffset * source.rect.width()) + sourceXOffset;
 
 	g_system->copyRectToScreen(sourcePixel, source.rect.width(), drawRect.left, drawRect.top, drawRect.width(), drawRect.height());
-	g_system->updateScreen();
+	g_sci->getEventManager()->updateScreen();
 }
 
 void GfxCursor32::unhide() {

engines/sci/graphics/frameout.cpp
@@ -35,6 +35,7 @@
 
 #include "sci/sci.h"
 #include "sci/console.h"
+#include "sci/event.h"
 #include "sci/engine/kernel.h"
 #include "sci/engine/state.h"
 #include "sci/engine/selector.h"
@@ -1123,7 +1124,7 @@ void GfxFrameout::mergeToShowList(const Common::Rect &drawRect, RectList &showLi
 
 void GfxFrameout::showBits() {
 	if (!_showList.size()) {
-		g_system->updateScreen();
+		g_sci->getEventManager()->updateScreen();
 		return;
 	}
 
@@ -1162,7 +1163,7 @@ void GfxFrameout::showBits() {
 	_cursor->donePainting();
 
 	_showList.clear();
-	g_system->updateScreen();
+	g_sci->getEventManager()->updateScreen();
 }
 
 void GfxFrameout::alterVmap(const Palette &palette1, const Palette &palette2, const int8 style, const int8 *const styleRanges) {

engines/sci/graphics/palette32.cpp
@@ -284,7 +284,7 @@ void GfxPalette32::updateHardware(const bool updateScreen) {
 
 	g_system->getPaletteManager()->setPalette(bpal, 0, 256);
 	if (updateScreen) {
-		g_system->updateScreen();
+		g_sci->getEventManager()->updateScreen();
 	}
 }
 