2b835eb14dd5974b89e860066140ee35243f1c9a
Peter Kohaut <peter.kohaut@gmail.com>
2016-10-04 18:19:45 +0200

    BLADERUNNER: added missing license & code formatting

--- engines/bladerunner/obstacles.cpp
+++ engines/bladerunner/obstacles.cpp
@@ -1,14 +1,36 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
 #include "bladerunner/obstacles.h"
 
 #include "bladerunner/bladerunner.h"
 
 namespace BladeRunner {
 
-Obstacles::Obstacles(BladeRunnerEngine* vm) {
+Obstacles::Obstacles(BladeRunnerEngine *vm) {
 	_vm = vm;
 	_polygons  = new ObstaclesPolygon[50];
 	_polygons2 = new ObstaclesPolygon[50];
-	_unknown = new int[50];
+	_unknown   = new int[50];
 	clear();
 }
 
@@ -19,10 +41,10 @@ Obstacles::~Obstacles() {
 }
 
 void Obstacles::clear() {
-	for(int i = 0; i < 50; i++) {
+	for (int i = 0; i < 50; i++) {
 		_polygons[i]._isPresent = false;
 		_polygons[i]._verticesCount = 0;
-		for(int j = 0; j < 160; j++) {
+		for (int j = 0; j < 160; j++) {
 			_polygons[i]._vertices[j].x = 0.0f;
 			_polygons[i]._vertices[j].y = 0.0f;
 		}
