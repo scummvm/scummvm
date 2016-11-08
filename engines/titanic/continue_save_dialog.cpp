d85f56e1fe66752a747b92c92a44161b2ff56349
Paul Gilbert <dreammaster@scummvm.org>
2016-11-07 20:26:27 -0500

    TITANIC: Fix showing Continue dialog when more than 5 saves present

--- engines/titanic/continue_save_dialog.cpp
+++ engines/titanic/continue_save_dialog.cpp
@@ -54,9 +54,10 @@ CContinueSaveDialog::~CContinueSaveDialog() {
 }
 
 void CContinueSaveDialog::addSavegame(int slot, const CString &name) {
-	assert(_saves.size() < SAVEGAME_SLOTS_COUNT);
-	_slotNames[_saves.size()].setText(name);
-	_saves.push_back(SaveEntry(slot, name));
+	if (_saves.size() < SAVEGAME_SLOTS_COUNT) {
+		_slotNames[_saves.size()].setText(name);
+		_saves.push_back(SaveEntry(slot, name));
+	}
 }
 
 Rect CContinueSaveDialog::getSlotBounds(int index) {
