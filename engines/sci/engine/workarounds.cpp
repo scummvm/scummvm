4f7173b13744fd15ed7677aad4785779b80cb5eb
Colin Snover <github.com@zetafleet.com>
2016-12-19 12:35:27 -0600

    SCI32: Generalize Phant1 kArraySetElements workaround
    This bug exists in a system script that is called from many, many
    rooms, so it is simpler to just make the workaround apply to all
    rooms.

--- engines/sci/engine/workarounds.cpp
+++ engines/sci/engine/workarounds.cpp
@@ -421,8 +421,7 @@ const SciWorkaroundEntry kAbs_workarounds[] = {
 //    gameID,           room,script,lvl,          object-name, method-name, local-call-signature, index,                workaround
 const SciWorkaroundEntry kArraySetElements_workarounds[] = {
 	{ GID_GK1,           302, 64918,  0,                "Str", "callKernel",                NULL,     0, { WORKAROUND_FAKE, 0 } }, // when erasing a letter on the wall in St Louis Cemetery
-	{ GID_PHANTASMAGORIA,902, 64918,  0,                "Str", "callKernel",                NULL,     0, { WORKAROUND_FAKE, 0 } }, // when starting a new game and selecting a chapter above 1
-	{ GID_PHANTASMAGORIA,47480, 64918,0,                "Str", "callKernel",                NULL,     0, { WORKAROUND_FAKE, 0 } }, // after completing the chase successfully
+	{ GID_PHANTASMAGORIA, -1, 64918,  0,                "Str", "callKernel",                NULL,     0, { WORKAROUND_FAKE, 0 } }, // when starting a new game and selecting a chapter above 1, or when quitting the chase (in every chase room), or when completing chase successfully
 	SCI_WORKAROUNDENTRY_TERMINATOR
 };
 
