68de00b36727424c2ea6c1a2d078288659492ab3
D G Turner <digitall@scummvm.org>
2017-01-11 15:10:01 +0000

    BLADERUNNER: Fix Unused Variable Compiler Warnings.

--- engines/bladerunner/vqa_decoder.cpp
+++ engines/bladerunner/vqa_decoder.cpp
@@ -434,10 +434,9 @@ bool VQADecoder::readCINF(Common::SeekableReadStream *s, uint32 size) {
 		return false;
 
 	for (int i = 0; i != _clipInfo.clipCount; ++i) {
-		uint16 a;
-		uint32 b;
-		a = s->readUint16LE();
-		b = s->readUint32LE();
+		uint16 a = s->readUint16LE();
+		uint32 b = s->readUint32LE();
+		debug("VQADecoder::readCINF() i: %d a: 0x%04x b: 0x%08x", i, a, b);
 	}
 
 	return true;
@@ -471,13 +470,13 @@ bool VQADecoder::readLNIN(Common::SeekableReadStream *s, uint32 size) {
 	if (chd.id != kLNIH || chd.size != 10)
 		return false;
 
-	uint16 loopNamesCount, loopUnk1, loopUnk2, loopUnk3, loopUnk4;
+	uint16 loopNamesCount = s->readUint16LE();
+	uint16 loopUnk1       = s->readUint16LE();
+	uint16 loopUnk2       = s->readUint16LE();
+	uint16 loopUnk3       = s->readUint16LE();
+	uint16 loopUnk4       = s->readUint16LE();
 
-	loopNamesCount = s->readUint16LE();
-	loopUnk1       = s->readUint16LE();
-	loopUnk2       = s->readUint16LE();
-	loopUnk3       = s->readUint16LE();
-	loopUnk4       = s->readUint16LE();
+	debug("VQADecoder::readLNIN() Unknown Values: 0x%04x 0x%04x 0x%04x 0x%04x", loopUnk1, loopUnk2, loopUnk3, loopUnk4);
 
 	if (loopNamesCount != _loopInfo.loopCount)
 		return false;
