4d062fc7a0157f2044f3ab69c7487a841babadeb
Colin Snover <github.com@zetafleet.com>
2017-01-18 21:38:07 -0600

    SCI32: Do not default to General MIDI for SCI2.1 games
    The only SCI32 game that uses MIDI and does not support AdLib is
    MGDX, and it its MIDI playback is currently broken regardless of
    the synth setting.

--- engines/sci/sound/music.cpp
+++ engines/sci/sound/music.cpp
@@ -70,10 +70,9 @@ void SciMusic::init() {
 	Common::Platform platform = g_sci->getPlatform();
 	uint32 deviceFlags = MDT_PCSPK | MDT_PCJR | MDT_ADLIB | MDT_MIDI;
 
-	// Default to MIDI in SCI2.1+ games, as many don't have AdLib support.
-	// Also, default to MIDI for Windows versions of SCI1.1 games, as their
+	// Default to MIDI for Windows versions of SCI1.1 games, as their
 	// soundtrack is written for GM.
-	if (getSciVersion() >= SCI_VERSION_2_1_EARLY || g_sci->_features->useAltWinGMSound())
+	if (g_sci->_features->useAltWinGMSound())
 		deviceFlags |= MDT_PREFER_GM;
 
 	// Currently our CMS implementation only supports SCI1(.1)
