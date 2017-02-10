1db02c7bd8ffee2aa0a27264669d11f83d95b491
Filippos Karapetis <bluegr@gmail.com>
2017-02-10 02:50:55 +0200

    CRYO: Rename create_led_dat to create_cryo_dat

--- devtools/create_cryo/module.mk
+++ devtools/create_cryo/module.mk
@@ -2,10 +2,10 @@
 MODULE := devtools/create_cryo
 
 MODULE_OBJS := \
-	create_led_dat.o
+	create_cryo_dat.o
 
 # Set the name of the executable
-TOOL_EXECUTABLE := create_led_dat
+TOOL_EXECUTABLE := create_cryo_dat
 
 # Include common rules
 include $(srcdir)/rules.mk
