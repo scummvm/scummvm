cbe576a4bc5a2ed25a5fb68ee770960f76042b76
Filippos Karapetis <bluegr@gmail.com>
2017-02-10 10:09:23 +0200

    Revert "CRYO: Rename create_led_dat to create_cryo_dat"
    This reverts commit 1db02c7bd8ffee2aa0a27264669d11f83d95b491.

--- devtools/create_cryo/module.mk
+++ devtools/create_cryo/module.mk
@@ -2,10 +2,10 @@
 MODULE := devtools/create_cryo
 
 MODULE_OBJS := \
-	create_cryo_dat.o
+	create_led_dat.o
 
 # Set the name of the executable
-TOOL_EXECUTABLE := create_cryo_dat
+TOOL_EXECUTABLE := create_led_dat
 
 # Include common rules
 include $(srcdir)/rules.mk
