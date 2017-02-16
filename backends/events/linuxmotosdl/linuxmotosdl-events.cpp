613c6f4f7f3a64f857ca59e453b5e53d01e64899
rsn8887 <rsn8887@users.noreply.github.com>
2017-02-16 03:19:59 -0600

    SDL: More fixes to jerky analog joystick pointer
    For some engines such as Dreamweb that a previous fix did not apply
    to, this fixes bug 6996: Android: Mouse pointer control with
    analog joystick is unusable

--- backends/events/linuxmotosdl/linuxmotosdl-events.cpp
+++ backends/events/linuxmotosdl/linuxmotosdl-events.cpp
@@ -130,53 +130,53 @@ bool LinuxmotoSdlEventSource::remapKey(SDL_Event &ev, Common::Event &event) {
 // Joystick to Mouse
 	else if (ev.key.keysym.sym == SDLK_LEFT) {
 		if (ev.type == SDL_KEYDOWN) {
-			_km.x_vel = -1;
+			_km.x_vel = -1 * _km.multiplier;
 			_km.x_down_count = 1;
 		} else {
-			_km.x_vel = 0;
+			_km.x_vel = 0 * _km.multiplier;
 			_km.x_down_count = 0;
 		}
 
 		event.type = Common::EVENT_MOUSEMOVE;
-		processMouseEvent(event, _km.x, _km.y);
+		processMouseEvent(event, _km.x / _km.multiplier, _km.y / _km.multiplier);
 		return true;
 	} else if (ev.key.keysym.sym == SDLK_RIGHT) {
 		if (ev.type == SDL_KEYDOWN) {
-			_km.x_vel = 1;
+			_km.x_vel = 1 * _km.multiplier;
 			_km.x_down_count = 1;
 		} else {
-			_km.x_vel = 0;
+			_km.x_vel = 0 * _km.multiplier;
 			_km.x_down_count = 0;
 		}
 
 		event.type = Common::EVENT_MOUSEMOVE;
-		processMouseEvent(event, _km.x, _km.y);
+		processMouseEvent(event, _km.x / _km.multiplier, _km.y / _km.multiplier);
 
 		return true;
 	} else if (ev.key.keysym.sym == SDLK_DOWN) {
 		if (ev.type == SDL_KEYDOWN) {
-			_km.y_vel = 1;
+			_km.y_vel = 1 * _km.multiplier;
 			_km.y_down_count = 1;
 		} else {
-			_km.y_vel = 0;
+			_km.y_vel = 0 * _km.multiplier;
 			_km.y_down_count = 0;
 		}
 
 		event.type = Common::EVENT_MOUSEMOVE;
-		processMouseEvent(event, _km.x, _km.y);
+		processMouseEvent(event, _km.x / _km.multiplier, _km.y / _km.multiplier);
 
 		return true;
 	} else if (ev.key.keysym.sym == SDLK_UP) {
 		if (ev.type == SDL_KEYDOWN) {
-			_km.y_vel = -1;
+			_km.y_vel = -1 * _km.multiplier;
 			_km.y_down_count = 1;
 		} else {
-			_km.y_vel = 0;
+			_km.y_vel = 0 * _km.multiplier;
 			_km.y_down_count = 0;
 		}
 
 		event.type = Common::EVENT_MOUSEMOVE;
-		processMouseEvent(event, _km.x, _km.y);
+		processMouseEvent(event, _km.x / _km.multiplier, _km.y / _km.multiplier);
 
 		return true;
 	} else if (ev.key.keysym.sym == SDLK_RETURN) {
@@ -187,7 +187,7 @@ bool LinuxmotoSdlEventSource::remapKey(SDL_Event &ev, Common::Event &event) {
 			event.type = Common::EVENT_LBUTTONUP;
 		}
 
-		processMouseEvent(event, _km.x, _km.y);
+		processMouseEvent(event, _km.x / _km.multiplier, _km.y / _km.multiplier);
 
 		return true;
 	} else if (ev.key.keysym.sym == SDLK_PLUS) {
@@ -197,7 +197,7 @@ bool LinuxmotoSdlEventSource::remapKey(SDL_Event &ev, Common::Event &event) {
 		} else {
 			event.type = Common::EVENT_RBUTTONUP;
 		}
-		processMouseEvent(event, _km.x, _km.y);
+		processMouseEvent(event, _km.x / _km.multiplier, _km.y / _km.multiplier);
 
 		return true;
 	} else if (ev.key.keysym.sym == SDLK_MINUS) {
@@ -208,7 +208,7 @@ bool LinuxmotoSdlEventSource::remapKey(SDL_Event &ev, Common::Event &event) {
 			event.type = Common::EVENT_LBUTTONUP;
 		}
 
-		processMouseEvent(event, _km.x, _km.y);
+		processMouseEvent(event, _km.x / _km.multiplier, _km.y / _km.multiplier);
 
 		return true;
 	} else {
--- backends/events/sdl/sdl-events.h
+++ backends/events/sdl/sdl-events.h
@@ -58,8 +58,9 @@ protected:
 	//@{
 
 	struct KbdMouse {
-		int16 x, y, x_vel, y_vel, x_max, y_max, x_down_count, y_down_count;
+		int16 x, y, x_vel, y_vel, x_max, y_max, x_down_count, y_down_count, multiplier;
 		uint32 last_time, delay_time, x_down_time, y_down_time;
+		bool modifier;
 	};
 	KbdMouse _km;
 
@@ -106,7 +107,7 @@ protected:
 	virtual bool handleJoyButtonDown(SDL_Event &ev, Common::Event &event);
 	virtual bool handleJoyButtonUp(SDL_Event &ev, Common::Event &event);
 	virtual bool handleJoyAxisMotion(SDL_Event &ev, Common::Event &event);
-	virtual void handleKbdMouse(Common::Event &event);
+	virtual bool handleKbdMouse(Common::Event &event);
 
 	//@}
 
