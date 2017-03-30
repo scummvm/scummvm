30435ec0abee168af13b5c41671ee5ca41db8a88
Nick Renieris <makren67@gmail.com>
2017-03-30 23:56:54 +0300

    DIRECTOR: Add detection for Arnie and its demo (#928)
    DIRECTOR: Add detection for Arnie and its demo

--- engines/director/detection_tables.h
+++ engines/director/detection_tables.h
@@ -527,6 +527,34 @@ static const DirectorGameDescription gameDescriptions[] = {
 		2
 	},
 
+	{
+		{
+			"ernie",
+			"Demo",
+			AD_ENTRY1s("ERNIE.EXE", "1a7acbba10a7246ba58c1d53fc7203f5", 1417371),
+			Common::EN_ANY,
+			Common::kPlatformWindows,
+			ADGF_DEMO,
+			GUIO1(GUIO_NOASPECT)
+		},
+		GID_GENERIC,
+		5
+	},
+
+	{
+		{
+			"ernie",
+			"",
+			AD_ENTRY1s("Ernie.exe", "1a7acbba10a7246ba58c1d53fc7203f5", 1417481),
+			Common::EN_ANY,
+			Common::kPlatformWindows,
+			ADGF_NO_FLAGS,
+			GUIO1(GUIO_NOASPECT)
+		},
+		GID_GENERIC,
+		5
+	},
+
 	{ AD_TABLE_END_MARKER, GID_GENERIC, 0 }
 };
 
