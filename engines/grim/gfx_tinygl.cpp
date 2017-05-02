198554a21109e3310cbe7ab616f994bc89cdfcb3
Vincent Pelletier <plr.vincent@gmail.com>
2017-05-02 13:05:33 +0000

    GRIM: Log front buffer pixel format on TinyGL initialisation

--- engines/grim/gfx_tinygl.cpp
+++ engines/grim/gfx_tinygl.cpp
@@ -93,6 +93,7 @@ byte *GfxTinyGL::setupScreen(int screenW, int screenH, bool fullscreen) {
 	g_system->setWindowCaption("ResidualVM: Software 3D Renderer");
 
 	_pixelFormat = buf.getFormat();
+	debug("INFO: TinyGL front buffer pixel format: %s", _pixelFormat.toString().c_str());
 	_zb = new TinyGL::FrameBuffer(screenW, screenH, buf);
 	TinyGL::glInit(_zb, 256);
 	tglEnableDirtyRects(ConfMan.getBool("dirtyrects"));
