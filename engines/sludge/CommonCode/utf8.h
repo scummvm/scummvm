83ab05630c149d8fb61a306797dec2f4c9d75784
yinsimei <roseline.yin@gmail.com>
2017-06-05 22:53:30 +0200

    SLUDGE: replace int type by scummvm types in utf8

--- engines/sludge/CommonCode/utf8.h
+++ engines/sludge/CommonCode/utf8.h
@@ -19,20 +19,20 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  */
-#include <stdint.h>
-#include <stdarg.h>
+
+#include "common/system.h"
 
 /* is c the start of a utf8 sequence? */
 #define isutf(c) (((c)&0xC0)!=0x80)
 
 /* convert UTF-8 data to wide character */
-int u8_toucs(uint32_t *dest, int sz, char *src, int srcsz);
+int u8_toucs(uint32 *dest, int sz, char *src, int srcsz);
 
 /* the opposite conversion */
-int u8_toutf8(char *dest, int sz, uint32_t *src, int srcsz);
+int u8_toutf8(char *dest, int sz, uint32 *src, int srcsz);
 
 /* single character to UTF-8 */
-int u8_wc_toutf8(char *dest, uint32_t ch);
+int u8_wc_toutf8(char *dest, uint32 ch);
 
 /* character number to byte offset */
 int u8_offset(char *str, int charnum);
@@ -41,7 +41,7 @@ int u8_offset(char *str, int charnum);
 int u8_charnum(char *s, int offset);
 
 /* return next character, updating an index variable */
-uint32_t u8_nextchar(const char *s, int *i);
+uint32 u8_nextchar(const char *s, int *i);
 
 /* move to next character */
 void u8_inc(char *s, int *i);
@@ -55,11 +55,11 @@ int u8_seqlen(char *s);
 /* assuming src points to the character after a backslash, read an
    escape sequence, storing the result in dest and returning the number of
    input characters processed */
-int u8_read_escape_sequence(char *src, uint32_t *dest);
+int u8_read_escape_sequence(char *src, uint32 *dest);
 
 /* given a wide character, convert it to an ASCII escape sequence stored in
    buf, where buf is "sz" bytes. returns the number of characters output. */
-int u8_escape_wchar(char *buf, int sz, uint32_t ch);
+int u8_escape_wchar(char *buf, int sz, uint32 ch);
 
 /* convert a string "src" containing escape sequences to UTF-8 */
 int u8_unescape(char *buf, int sz, char *src);
@@ -75,11 +75,11 @@ int hex_digit(char c);
 
 /* return a pointer to the first occurrence of ch in s, or NULL if not
    found. character index of found character returned in *charn. */
-char *u8_strchr(char *s, uint32_t ch, int *charn);
+char *u8_strchr(char *s, uint32 ch, int *charn);
 
 /* same as the above, but searches a buffer of a given size instead of
    a NUL-terminated string. */
-char *u8_memchr(char *s, uint32_t ch, size_t sz, int *charn);
+char *u8_memchr(char *s, uint32 ch, size_t sz, int *charn);
 
 /* count the number of characters in a UTF-8 string */
 int u8_strlen(char *s);
