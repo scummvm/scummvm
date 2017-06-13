e59d2278a3234affb9ef4a15263f869f7a8c702d
Joseph-Eugene Winzer <m999@openmailbox.org>
2017-06-13 14:24:13 +0200

    SUPERNOVA: Setup image and section viewer
    Pressing 'w' increases the image index and 'e' the section index.

--- engines/supernova/supernova.h
+++ engines/supernova/supernova.h
@@ -37,10 +37,6 @@
 
 namespace Supernova {
 
-const int kScreenWidth  = 320;
-const int kScreenHeight = 200;
-const int kTextSpeed[] = {19, 14, 10, 7, 4};
-
 class SupernovaEngine : public Engine {
 public:
 	SupernovaEngine(OSystem *syst);
@@ -54,7 +50,8 @@ private:
 	Audio::SoundHandle _soundHandle;
 	bool _gameRunning;
 	MSNImageDecoder _image;
-	byte _incIndex;
+	byte _imageIndex;
+	byte _sectionIndex;
 	byte _menuBrightness;
 	byte _brightness;
 	
