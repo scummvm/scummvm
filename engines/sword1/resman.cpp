a70767503e6a27d4f42a97033971ca4d760df32c
Torbjörn Andersson <eriktorbjorn@users.sourceforge.net>
2017-08-06 16:38:03 +0200

    SWORD1: Silence GCC warning about potential overflow
    Probably doesn't ever happen, but now 'fileName' is as large as it
    needs to be if 'label' is as long as it can be.

--- engines/sword1/resman.cpp
+++ engines/sword1/resman.cpp
@@ -309,7 +309,7 @@ Common::File *ResMan::resFile(uint32 id) {
 			_openCluEnd = cluster;
 		}
 		cluster->file = new Common::File();
-		char fileName[15];
+		char fileName[36];
 		// Supposes that big endian means mac cluster file and little endian means PC cluster file.
 		// This works, but we may want to separate the file name from the endianess or try .CLM extension if opening.clu file fail.
 		if (_isBigEndian)
