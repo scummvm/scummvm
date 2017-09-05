e9aa47f55ce60bce599b4db35d69261ac8932eba
Cameron Cawley <ccawley2011@gmail.com>
2017-09-05 20:57:26 +0100

    GRAPHICS: Compare all fields in a PixelFormat individually instead of using memcmp.

--- graphics/pixelformat.h
+++ graphics/pixelformat.h
@@ -159,7 +159,15 @@ struct PixelFormat {
 
 	inline bool operator==(const PixelFormat &fmt) const {
 		// TODO: If aLoss==8, then the value of aShift is irrelevant, and should be ignored.
-		return 0 == memcmp(this, &fmt, sizeof(PixelFormat));
+		return bytesPerPixel == fmt.bytesPerPixel &&
+		       rLoss == fmt.rLoss &&
+		       gLoss == fmt.gLoss &&
+		       bLoss == fmt.bLoss &&
+		       aLoss == fmt.aLoss &&
+		       rShift == fmt.rShift &&
+		       gShift == fmt.gShift &&
+		       bShift == fmt.bShift &&
+		       aShift == fmt.aShift;
 	}
 
 	inline bool operator!=(const PixelFormat &fmt) const {
