e26a677f6214a1c88d63a41e136dc78ad07cca11
Colin Snover <github.com@zetafleet.com>
2017-09-13 00:51:02 -0500

    SCI32: Tell OSystem to show/hide cursors as appropriate
    This is needed so that the system cursor can be appropriately
    hidden outside the game's draw area, to match the normal behaviour
    of ScummVM.

--- engines/sci/graphics/cursor32.cpp
+++ engines/sci/graphics/cursor32.cpp
@@ -57,6 +57,7 @@ void GfxCursor32::hide() {
 		return;
 	}
 
+	g_system->showMouse(false);
 	if (!_cursorBack.rect.isEmpty()) {
 		drawToScreen(_cursorBack);
 	}
@@ -136,12 +137,14 @@ void GfxCursor32::unhide() {
 		return;
 	}
 
+	g_system->showMouse(true);
 	_cursor.rect.moveTo(_position.x - _hotSpot.x, _position.y - _hotSpot.y);
 	revealCursor();
 }
 
 void GfxCursor32::show() {
 	if (_hideCount) {
+		g_system->showMouse(true);
 		_hideCount = 0;
 		_cursor.rect.moveTo(_position.x - _hotSpot.x, _position.y - _hotSpot.y);
 		revealCursor();
