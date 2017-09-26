64b36f36887d6368a8fd954ad120de0349f84242
Thierry Crozat <criezy@scummvm.org>
2017-09-26 23:05:46 +0100

    SUPERNOVA: Add tool skeleton to generate engine data file
    The tool is only a skeleton at this point. Most of the logic is there
    but it is missing the data to write the German strings and it is missing
    the logic to write the translated strings. This means that currently the
    only data written are bitmap images.

---
+++ devtools/create_supernova/gametext.h
@@ -0,0 +1,271 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * This is a utility for generating a data file for the supernova engine.
+ * It contains strings extracted from the original executable as well
+ * as translations and is required for the engine to work properly.
+ */
+
+#ifndef GAMETEXT_H
+#define GAMETEXT_H
+
+#include <stddef.h>
+
+// This file contains the strings in German and is encoded using CP850 encoding.
+// Other language should be provided as po files also using the CP850 encoding.
+
+// TODO: add the strings from the engine here, add an Id string in comment.
+// And in the engine add a StringId enum with all the Ids = index in this array.
+
+const char *gameText[] = {
+	// 0
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 5
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 10
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 15
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 20
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 25
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 30
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 35
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 40
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 45
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 50
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 55
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 60
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 65
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 70
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 75
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 80
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 85
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 90
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 95
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 100
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 105
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 110
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 115
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 120
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 125
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 130
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 135
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 140
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 145
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 150
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 155
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 160
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 165
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 170
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 175
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 180
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	// 185
+	"", //
+	"", //
+	"", //
+	"", //
+	"", //
+	NULL
+};
+
+
+
+#endif // GAMETEXT_H
