4e374cc9be1a7d9051b5f0620538befb0c2f843c
Thierry Crozat <criezy@scummvm.org>
2017-09-30 01:18:41 +0100

    SUPERNOVA: Add some German strings to the create_supernova tool

--- devtools/create_supernova/gametext.h
+++ devtools/create_supernova/gametext.h
@@ -36,55 +36,55 @@
 
 const char *gameText[] = {
 	// 0
-	"", //
-	"", //
-	"", //
-	"", //
-	"", //
+	"Gehe",         // kCommandGo
+	"Schau",        // kCommandLook
+	"Nimm",         // kCommandTake
+	"\231ffne",     // kCommandOpen
+	"Schlie\341e",  // kCommandClose
 	// 5
-	"", //
-	"", //
-	"", //
-	"", //
-	"", //
+	"Dr\201cke",    // kCommandPress
+	"Ziehe",        // kCommandPull
+	"Benutze",      // kCommandUse
+	"Rede",         // kCommandTalk
+	"Gib",          // kCommandGive
 	// 10
-	"", //
-	"", //
-	"", //
-	"", //
-	"", //
+	"Gehe zu ",     // kStatusCommandGo
+	"Schau ",       // kStatusCommandLook
+	"Nimm ",        // kStatusCommandTake
+	"\231ffne ",    // kStatusCommandOpen
+	"Schlie\341e ", // kStatusCommandClose
 	// 15
-	"", //
-	"", //
-	"", //
-	"", //
-	"", //
+	"Dr\201cke ",   // kStatusCommandPress
+	"Ziehe ",       // kStatusCommandPull
+	"Benutze ",     // kStatusCommandUse
+	"Rede mit ",    // kStatusCommandTalk
+	"Gib ",         // kStatusCommandGive
 	// 20
-	"", //
-	"", //
-	"", //
-	"", //
-	"", //
+	"Hmm, er scheint kaputt zu sein.",       // kBroken
+	"^(C) 1994 Thomas und Steffen Dingel#",  // kIntro1
+	"Story und Grafik:^ Thomas Dingel#",     // kIntro2
+	"Programmierung:^ Steffen Dingel#",      // kIntro3
+	"Musik:^ Bernd Hoffmann#",               // kIntro4
 	// 25
-	"", //
-	"", //
-	"", //
-	"", //
-	"", //
+	"Getestet von ...#",                     // kIntro5
+	"^Matthias Neef#",                       // kIntro6
+	"^Sascha Otterbach#",                    // kIntro7
+	"^Thomas Mazzoni#",                      // kIntro8
+	"^Matthias Klein#",                      // kIntro9
 	// 30
-	"", //
-	"", //
-	"", //
-	"", //
-	"", //
+	"^Gerrit Rothmaier#",                    // kIntro10
+	"^Thomas Hassler#",                      // kIntro11
+	"^Rene Koch#",                           // kIntro12
+	"°",                                     // kIntro13
+	"Keycard",                               // kKeycard
 	// 35
-	"", //
-	"", //
-	"", //
-	"", //
-	"", //
+	"Die Keycard f\224r deine Schr\204nke.", // kKeycardDesc
+	"Taschenmesser",                         // kKnife
+	"Es ist nicht mehr das sch\204rfste.",   // kKnifeDesc
+	"Armbanduhr",                            // kWatch
+	"Discman",                               // kDiscman
 	// 40
-	"", //
+	"Es ist eine \"Mad Monkeys\"-CD darin.", // kDiscmanDesc
 	"", //
 	"", //
 	"", //
