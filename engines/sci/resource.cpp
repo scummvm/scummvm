dc7e37fdb6800124cccf231282d4b1475249a654
Colin Snover <github.com@zetafleet.com>
2017-09-29 23:01:58 -0500

    SCI32: Fix extremely broken patch for RAMA RESOURCE.SFX
    Obviously, games other than RAMA need to have sound effects
    volumes.

--- engines/sci/resource.cpp
+++ engines/sci/resource.cpp
@@ -1928,15 +1928,13 @@ int ResourceManager::readResourceMapSCI1(ResourceSource *map) {
 						volumeName = "RESAUD.001";
 					}
 				} else if (resId.getNumber() == 65535) {
-					if (g_sci->getGameId() == GID_RAMA) {
-						volumeName = Common::String::format("RESSFX.%03d", mapVolumeNr);
+					volumeName = Common::String::format("RESSFX.%03d", mapVolumeNr);
 
-						if (g_sci->getGameId() == GID_RAMA && !Common::File::exists(volumeName)) {
-							if (Common::File::exists("RESOURCE.SFX")) {
-								volumeName = "RESOURCE.SFX";
-							} else if (Common::File::exists("RESSFX.001")) {
-								volumeName = "RESSFX.001";
-							}
+					if (g_sci->getGameId() == GID_RAMA && !Common::File::exists(volumeName)) {
+						if (Common::File::exists("RESOURCE.SFX")) {
+							volumeName = "RESOURCE.SFX";
+						} else if (Common::File::exists("RESSFX.001")) {
+							volumeName = "RESSFX.001";
 						}
 					}
 				} else {
