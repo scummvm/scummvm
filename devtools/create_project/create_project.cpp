61a63f84006317b2ece155a0dd80f080b4d8069e
Colin Snover <github.com@zetafleet.com>
2017-11-11 11:46:47 -0600

    CREATE_PROJECT: Fix compilation failure in C++11 mode
    Getting the address of an rvalue is invalid, and not necessary
    here.
    
    Once C++11 can be used always, this utility function can just be
    replaced with std::to_string.

--- devtools/create_project/create_project.cpp
+++ devtools/create_project/create_project.cpp
@@ -1170,7 +1170,9 @@ bool producesObjectFile(const std::string &fileName) {
 }
 
 std::string toString(int num) {
-	return static_cast<std::ostringstream*>(&(std::ostringstream() << num))->str();
+	std::ostringstream os;
+	os << num;
+	return os.str();
 }
 
 /**
