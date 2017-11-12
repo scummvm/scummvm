1bd7912da4ee74d6c1b48f0f269b203438ff6033
Colin Snover <github.com@zetafleet.com>
2017-11-12 17:05:04 -0600

    COMMON: Add support for arrays & limited custom deleters to smart pointers
    This enables ScopedPtr to be used for all cases where raw pointers
    are currently used to manage owned memory (assuming C malloc APIs
    are replaced with C++ APIs), and brings ScopedPtr closer to
    std::unique_ptr in terms of functionality. DisposablePtr also comes
    along for the ride, for the moment.
    
    Custom deleters of ScopedPtr are not currently fully conforming to
    std::unique_ptr for the sake of simplicity of implementation.
    Unlike in the standard library, plain functions and lvalue
    references are not supported, nor may custom deleters be passed
    to the constructor at runtime. This can be improved in the future,
    if necessary, by creating a Pair class that uses the Empty Base
    Optimization idiom to avoid extra storage overhead of the deleter
    instance when it is not needed, as in typical standard library
    implementations, plus some additional type traits to support the
    necessary metaprogramming for the different type overloads.
    
    Use of array types in SharedPtr also do not conform to C++17
    std::shared_ptr, which checks for convertability between the
    argument and an array type instead of having an explicit array
    specialization. This should be an OK compromise for now, and
    also might even be preferable (earlier C++ standards did not have
    any explicit handling of array types in std::shared_ptr, so now
    the C++ standard library needs to maintain backwards-compatibility
    where we do not).
    
    This patch unfortunately introduces some code duplication due to
    the fact that C++98 does not allow default arguments in function
    template parameters. Using C++11, there would be no code
    duplication.

--- common/type-traits.h
+++ common/type-traits.h
@@ -24,11 +24,39 @@
 #define COMMON_TYPE_TRAITS_H
 
 namespace Common {
-	template <bool b, class T, class U> struct Conditional { typedef T type; };
-	template <class T, class U> struct Conditional<false, T, U> { typedef U type; };
+	template <typename T, T v>
+	struct IntegralConstant {
+		typedef T value_type;
+		typedef IntegralConstant<T, v> type;
+		static const T value = v;
+	};
+
+	typedef IntegralConstant<bool, true> TrueType;
+	typedef IntegralConstant<bool, false> FalseType;
+
+	template <bool Condition, typename T = void> struct EnableIf {};
+	template <typename T> struct EnableIf<true, T> { typedef T type; };
+
+	template <bool Condition, typename T = void> struct DisableIf { typedef T type; };
+	template <typename T> struct DisableIf<true, T> {};
+
+	template <bool b, typename T, typename> struct Conditional { typedef T type; };
+	template <typename T, typename U> struct Conditional<false, T, U> { typedef U type; };
+
 	template <typename T> struct RemoveConst { typedef T type; };
 	template <typename T> struct RemoveConst<const T> { typedef T type; };
+
 	template <typename T> struct AddConst { typedef const T type; };
+
+	template <typename T> struct RemovePointer { typedef T type; };
+	template <typename T> struct RemovePointer<T *> { typedef T type; };
+
+	template <typename T> struct RemoveArray { typedef T type; };
+	template <typename T> struct RemoveArray<T []> { typedef T type; };
+
+	template <class> struct IsArray : FalseType {};
+	template <class T> struct IsArray<T []> : TrueType {};
+	template <class T> struct IsArray<T const []> : TrueType {};
 } // End of namespace Common
 
 #endif
