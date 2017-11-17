54236cbdfd4635244a711d7c0a29d0f68ee540c4
Colin Snover <github.com@zetafleet.com>
2017-11-17 00:05:43 -0600

    FULLPIPE: Make list item ownership comments more consistent

--- engines/fullpipe/statics.h
+++ engines/fullpipe/statics.h
@@ -177,9 +177,9 @@ class StaticANIObject : public GameObject {
 	int _initialCounter;
 	void (*_callback1)(int, Common::Point *point, int, int);
 	void (*_callback2)(int *);
-	/** items in list are owned */
+	/** list items are owned */
 	Common::Array<Movement *> _movements;
-	/** items in list are owned */
+	/** list items are owned */
 	Common::Array<Statics *> _staticsList;
 	StepArray _stepArray;
 	int16 _field_96;
