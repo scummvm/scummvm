2abbbbaa0b78641a042fe4ed3c03a7b86ca22f52
Cameron Cawley <ccawley2011@gmail.com>
2017-11-19 11:49:24 +0000

    DEVTOOLS: Remove NSIS script from update-version.pl The NSIS script was removed in commit 4a1f3d7

--- devtools/update-version.pl
+++ devtools/update-version.pl
@@ -41,7 +41,6 @@ my @subs_files = qw(
 	dists/iphone/Info.plist
 	dists/ios7/Info.plist
 	dists/irix/scummvm.spec
-	dists/win32/scummvm.nsi
 	dists/wii/meta.xml
 	dists/android/AndroidManifest.xml
 	dists/openpandora/PXML.xml
