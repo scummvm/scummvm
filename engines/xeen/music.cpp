f2766d743bab00a8389b82f5fb0e2873bbf92677
Paul Gilbert <dreammaster@scummvm.org>
2017-11-23 14:29:28 -0500

    XEEN: Fix crash playing Fx when music is playing

--- engines/xeen/music.cpp
+++ engines/xeen/music.cpp
@@ -200,7 +200,6 @@ bool MusicDriver::fxEndSubroutine(const byte *&srcP, byte param) {
 
 void MusicDriver::playFX(uint effectId, const byte *data) {
 	if (!_fxPlaying || effectId < 7 || effectId >= 11) {
-		_musStartPtr = nullptr;
 		_fxDataPtr = _fxStartPtr = data;
 		_fxCountdownTimer = 0;
 		_channels[7]._changeFrequency = _channels[8]._changeFrequency = false;
