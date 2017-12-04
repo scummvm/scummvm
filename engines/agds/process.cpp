85c03f9e83707d82e113baa13a2553d068899102
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2017-12-04 22:54:33 +0000

    AGDS: implemented setSpecialVariable stub

--- engines/agds/process.cpp
+++ engines/agds/process.cpp
@@ -29,7 +29,9 @@ enum Opcode {
 	kEnter				= 5,
 	kPop				= 10,
 	kPushImm8			= 18,
-	kOp142				= 142,
+	kSetSpecialVariable	= 142,
+	kGetRegionWidth		= 146,
+	kGetRegionHeight	= 147,
 	kLoadPicture		= 198,
 	kMax				= 248
 };
@@ -72,7 +74,7 @@ void Process::execute() {
 			OP		(kPop, pop);
 			OP_C	(kPushImm8, push);
 			OP		(kLoadPicture, loadPicture);
-			OP		(kOp142, stub142);
+			OP		(kSetSpecialVariable, setSpecialVariable);
 		default:
 			debug("%08x: unknown opcode 0x%02x (%u)", _ip - 1, (unsigned)op, (unsigned)op);
 			_failed = true;
