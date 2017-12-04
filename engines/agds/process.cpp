90cfa82602e3a8d22528c1e0a77217f4c8bdeb98
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2017-12-04 01:06:15 +0000

    AGDS: implemented first few opcodes

--- engines/agds/process.cpp
+++ engines/agds/process.cpp
@@ -25,15 +25,54 @@
 
 namespace AGDS {
 
+enum Opcode {
+	kEnter				= 5,
+	kPushImm8			= 18,
+	kOp142				= 142,
+	kLoadPicture		= 198,
+	kMax				= 248
+};
+
+
+//fixme: add trace here
+#define OP(NAME, METHOD) \
+	case NAME: METHOD (); break
+
+#define OP_C(NAME, METHOD) \
+	case NAME: { int8 arg = next(); METHOD (arg); } break
+
+#define OP_B(NAME, METHOD) \
+	case NAME: { uint8 arg = next(); METHOD (arg); } break
+
+#define OP_U(NAME, METHOD) \
+	case NAME: { uint16 arg = next16(); METHOD (arg); } break
+
+#define OP_UU(NAME, METHOD) \
+	case NAME: { uint16 arg1 = next16(); uint16 arg2 = next16(); METHOD (arg1, arg2); } break
+
 Process::Process(Object* object) : _object(object), _ip(0), _failed(false) { }
+
+void Process::push(int32 value) {
+	_stack.push(value);
+}
+
+int32 Process::pop() {
+	if (_stack.empty())
+		error("stack underflow, ip: %08x", _ip);
+	return _stack.pop();
+}
 
 void Process::execute() {
 	const Object::CodeType &code = _object->getCode();
 	while(!_failed && _ip < code.size()) {
 		uint8 op = next();
 		switch(op) {
+			OP_UU	(kEnter, enter);
+			OP_C	(kPushImm8, push);
+			OP		(kLoadPicture, loadPicture);
+			OP		(kOp142, stub142);
 		default:
-			debug("%08x: unknown opcode %02x", _ip - 1, (unsigned)op);
+			debug("%08x: unknown opcode 0x%02x (%u)", _ip - 1, (unsigned)op, (unsigned)op);
 			_failed = true;
 			break;
 		}
