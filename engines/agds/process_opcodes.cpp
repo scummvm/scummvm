2a6248bdae247545053c039def1f83d6ed210241
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2017-12-04 08:56:05 +0000

    AGDS: added resource table parsing

--- engines/agds/process_opcodes.cpp
+++ engines/agds/process_opcodes.cpp
@@ -22,11 +22,45 @@
 
 #include "agds/process.h"
 #include "common/debug.h"
+#include "common/memstream.h"
 
 namespace AGDS {
 
-void Process::enter(int16 dead, int16 jump) {
-	_ip += jump;
+void Process::enter(uint16 magic, uint16 size) {
+	if (magic != 0xdead || size != 0x0c)
+		error("invalid enter() magic: 0x%04x or size: %u", magic, size);
+	uint16 unk1			= next16();
+	uint16 unk2			= next16();
+	uint16 unk3			= next16();
+	unsigned resOffset	= next16();
+	uint16 resCount		= next16();
+	uint16 unk4			= next16();
+
+	debug("resource block %04x %04x %04x %04x, resources table with %u entries", unk1, unk2, unk3, unk4, resCount);
+
+	const Object::CodeType &code = _object->getCode();
+	resOffset += 5 /*instruction*/ + 0x11 /*another header*/;
+	if (resOffset >= code.size())
+		error("invalid resource table offset");
+
+	debug("resource table at %08x", resOffset);
+	Common::MemoryReadStream stream(code.data() + resOffset, code.size() - resOffset);
+	for(uint16 i = 0; i < resCount; ++i) {
+		uint16 offset = stream.readUint16LE();
+		uint16 flags = stream.readUint16LE();
+
+		unsigned nameOffset = resOffset + offset;
+		if (nameOffset > code.size())
+			error("invalid resource name offset");
+
+		const char * nameBegin = reinterpret_cast<const char *>(code.data() + nameOffset);
+		const char * codeEnd = reinterpret_cast<const char *>(code.data() + code.size());
+		const char * nameEnd = Common::find(nameBegin, codeEnd, 0);
+
+		Common::String name(nameBegin, nameEnd - nameBegin);
+
+		debug("resource table 1[%04u]: 0x%04x %s", i, flags, name.c_str());
+	}
 }
 
 void Process::stub142() {
