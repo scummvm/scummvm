4a042c8465e286d884607ae513c03f9cbe74b0cf
Thierry Crozat <criezy@scummvm.org>
2017-12-20 15:22:55 +0000

    SUPERNOVA: Save/restore _playerHidden flag in savegames

--- engines/supernova/supernova.h
+++ engines/supernova/supernova.h
@@ -43,7 +43,7 @@
 namespace Supernova {
 
 #define SAVEGAME_HEADER MKTAG('M','S','N','1')
-#define SAVEGAME_VERSION 7
+#define SAVEGAME_VERSION 8
 
 #define SUPERNOVA_DAT "supernova.dat"
 #define SUPERNOVA_DAT_VERSION 1
