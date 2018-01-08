c2762bf6e11b4387440b1ecf44d19218888faa93
Joseph-Eugene Winzer <joewinzer@googlemail.com>
2018-01-08 03:10:53 +0100

    AUDIO: Fixes Protracker Portamento effect
    .period overflowed if .portaToNoteSpeed > .period in the else if case

--- audio/mods/protracker.cpp
+++ audio/mods/protracker.cpp
@@ -101,15 +101,13 @@ private:
 
 	void doPorta(int track) {
 		if (_track[track].portaToNote && _track[track].portaToNoteSpeed) {
-			if (_track[track].period < _track[track].portaToNote) {
-				_track[track].period += _track[track].portaToNoteSpeed;
-				if (_track[track].period > _track[track].portaToNote)
-					_track[track].period = _track[track].portaToNote;
-			} else if (_track[track].period > _track[track].portaToNote) {
-				_track[track].period -= _track[track].portaToNoteSpeed;
-				if (_track[track].period < _track[track].portaToNote)
-					_track[track].period = _track[track].portaToNote;
-			}
+			int distance = _track[track].period - _track[track].portaToNote;
+			int sign = distance > 0 ? 1 : -1;
+
+			if ((sign * distance) > _track[track].portaToNoteSpeed)
+				_track[track].period -= sign * _track[track].portaToNoteSpeed;
+			else
+				_track[track].period = _track[track].portaToNote;
 		}
 	}
 	void doVibrato(int track) {
