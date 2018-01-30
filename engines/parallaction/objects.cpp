69df2e2d0e8a7254da1000c4b2b6cec2edd0b102
Colin Snover <github.com@zetafleet.com>
2018-01-30 10:42:32 -0600

    PARALLACTION: Fix null pointer dereference on quit

--- engines/parallaction/objects.cpp
+++ engines/parallaction/objects.cpp
@@ -64,7 +64,9 @@ Animation::Animation() {
 
 Animation::~Animation() {
 	free(_scriptName);
-	gfxobj->release();
+	if (gfxobj) {
+		gfxobj->release();
+	}
 }
 
 void Animation::getFrameRect(Common::Rect &r) const {
