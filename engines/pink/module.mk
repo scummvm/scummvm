cf79308141ab9778376d261bb52c0016f3299559
whitertandrek <whiterandrek@gmail,com>
2018-03-22 22:03:40 +0200

    PINK: added SupportingActor and HandlerMgr

--- engines/pink/module.mk
+++ engines/pink/module.mk
@@ -1,38 +1,40 @@
 MODULE := engines/pink
 
 MODULE_OBJS = \
-	pink.o \
-	console.o \
+    archive.o \
+    console.o \
+    cursor_mgr.o \
 	detection.o \
 	director.o \
-	sound.o \
 	file.o \
-	archive.o \
-	cursor_mgr.o \
+	pink.o \
+	resource_mgr.o \
+	sound.o \
     objects/object.o \
     objects/module.o \
-    objects/pages/page.o \
-    objects/pages/game_page.o \
     objects/inventory.o \
     objects/side_effect.o \
     objects/condition.o \
-    resource_mgr.o \
     objects/actions/action.o \
     objects/actions/action_cel.o \
     objects/actions/action_hide.o \
     objects/actions/action_play.o \
     objects/actions/action_sound.o \
     objects/actions/action_still.o \
+    objects/actions/walk_action.o \
     objects/actors/actor.o \
     objects/actors/lead_actor.o \
-    objects/walk/walk_mgr.o \
-    objects/walk/walk_location.o \
+    objects/actors/supporting_actor.o \
+    objects/handlers/handler.o \
+    objects/handlers/handler_mgr.o \
+    objects/handlers/handler_timer.o \
+    objects/pages/page.o \
+    objects/pages/game_page.o \
     objects/sequences/sequence.o \
     objects/sequences/sequence_item.o \
     objects/sequences/sequencer.o \
-    objects/handlers/handler.o \
-    objects/handlers/handler_timer.o \
-
+    objects/walk/walk_mgr.o \
+    objects/walk/walk_location.o \
 
 # This module can be built as a plugin
 ifeq ($(ENABLE_PLUMBERS), DYNAMIC_PLUGIN)
--- engines/pink/objects/handlers/handler_mgr.cpp
+++ engines/pink/objects/handlers/handler_mgr.cpp
@@ -3,3 +3,28 @@
 //
 
 #include "handler_mgr.h"
+#include "handler.h"
+#include "handler_timer.h"
+#include <pink/archive.h>
+#include <common/debug.h>
+
+namespace Pink {
+
+void HandlerMgr::deserialize(Pink::Archive &archive) {
+    archive >> _leftClickHandlers >> _useClickHandlers >> _timerHandlers;
+}
+
+void HandlerMgr::toConsole() {
+    debug("HandlerMgr:");
+    for (int i = 0; i < _leftClickHandlers.size(); ++i) {
+        _leftClickHandlers[i]->toConsole();
+    }
+    for (int i = 0; i < _leftClickHandlers.size(); ++i) {
+        _useClickHandlers[i]->toConsole();
+    }
+    for (int i = 0; i < _leftClickHandlers.size(); ++i) {
+        _timerHandlers[i]->toConsole();
+    }
+}
+
+}
