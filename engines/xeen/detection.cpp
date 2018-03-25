f026186cdf20c6df24d16b8b99ee03922b5e75f9
Adrian Frühwirth <bonki@users.noreply.github.com>
2018-03-26 01:24:25 +0200

    XEEN: Be strict about matching digits in savegame filenames

--- engines/xeen/detection.cpp
+++ engines/xeen/detection.cpp
@@ -137,7 +137,7 @@ SaveStateList XeenMetaEngine::listSaves(const char *target) const {
 	Common::SaveFileManager *saveFileMan = g_system->getSavefileManager();
 	Common::StringArray filenames;
 	Common::String saveDesc;
-	Common::String pattern = Common::String::format("%s.0??", target);
+	Common::String pattern = Common::String::format("%s.###", target);
 	Xeen::XeenSavegameHeader header;
 
 	filenames = saveFileMan->listSavefiles(pattern);
