8ed5969ea1208f3c25fd193dbcdbae9436a7c75d
Adrian Frühwirth <bonki@users.noreply.github.com>
2018-03-26 00:51:34 +0200

    PRINCE: Let listSaves return list sorted on slot numbers

--- engines/prince/detection.cpp
+++ engines/prince/detection.cpp
@@ -188,7 +188,6 @@ SaveStateList PrinceMetaEngine::listSaves(const char *target) const {
 	pattern += ".###";
 
 	filenames = saveFileMan->listSavefiles(pattern);
-	sort(filenames.begin(), filenames.end()); // Sort (hopefully ensuring we are sorted numerically..)
 
 	SaveStateList saveList;
 	for (Common::StringArray::const_iterator filename = filenames.begin(); filename != filenames.end(); filename++) {
@@ -224,6 +223,7 @@ SaveStateList PrinceMetaEngine::listSaves(const char *target) const {
 		}
 	}
 
+	Common::sort(saveList.begin(), saveList.end(), SaveStateDescriptorSlotComparator());
 	return saveList;
 }
 
