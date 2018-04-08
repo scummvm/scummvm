07fb37ed70794dd42c8ab8895ddc2dffe745a023
Paul Gilbert <dreammaster@scummvm.org>
2018-04-08 18:53:59 -0400

    XEEN: Fix restarting World game after exiting to menu from Dark Side

--- engines/xeen/xeen.cpp
+++ engines/xeen/xeen.cpp
@@ -216,6 +216,8 @@ void XeenEngine::play() {
 		_party->_mazeDirection = DIR_NORTH;
 		_party->_mazePosition.x = 25;
 		_party->_mazePosition.y = 21;
+	} else {
+		_map->_loadCcNum = g_vm->_files->_ccNum;
 	}
 
 	_map->clearMaze();
