a7bc08992e2acd361c485179d2142bc9121cda65
Cameron Cawley <ccawley2011@gmail.com>
2018-04-13 20:19:47 +0100

    IMAGE: INDEO: Use the system pixel format for non-8bpp screen modes

--- image/codecs/indeo/indeo.cpp
+++ image/codecs/indeo/indeo.cpp
@@ -465,22 +465,26 @@ IVI45DecContext::IVI45DecContext() : _gb(nullptr), _frameNum(0), _frameType(0),
 /*------------------------------------------------------------------------*/
 
 IndeoDecoderBase::IndeoDecoderBase(uint16 width, uint16 height, uint bitsPerPixel) : Codec() {
-	switch (bitsPerPixel) {
-	case 15:
-		_pixelFormat = Graphics::PixelFormat(2, 5, 5, 5, 0, 0, 5, 10, 0);
-		break;
-	case 16:
-		_pixelFormat = Graphics::PixelFormat(2, 5, 6, 5, 0, 11, 5, 0, 0);
-		break;
-	case 24:
-		_pixelFormat = Graphics::PixelFormat(4, 8, 8, 8, 0, 16, 8, 0, 0);
-		break;
-	case 32:
-		_pixelFormat = Graphics::PixelFormat(4, 8, 8, 8, 8, 24, 16, 8, 0);
-		break;
-	default:
-		error("Invalid color depth");
-		break;
+	_pixelFormat = g_system->getScreenFormat();
+
+	if (_pixelFormat.bytesPerPixel == 1) {
+		switch (bitsPerPixel) {
+		case 15:
+			_pixelFormat = Graphics::PixelFormat(2, 5, 5, 5, 0, 0, 5, 10, 0);
+			break;
+		case 16:
+			_pixelFormat = Graphics::PixelFormat(2, 5, 6, 5, 0, 11, 5, 0, 0);
+			break;
+		case 24:
+			_pixelFormat = Graphics::PixelFormat(4, 8, 8, 8, 0, 16, 8, 0, 0);
+			break;
+		case 32:
+			_pixelFormat = Graphics::PixelFormat(4, 8, 8, 8, 8, 24, 16, 8, 0);
+			break;
+		default:
+			error("Invalid color depth");
+			break;
+		}
 	}
 
 	_surface.create(width, height, _pixelFormat);
