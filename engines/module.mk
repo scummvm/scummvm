ec8e5f7701ef89efd211bae8269543c55623614a
Lothar Serra Mari <serra@scummvm.org>
2018-04-17 10:01:16 +0200

    ENGINES: Fix alphabetical order in module.mk

--- engines/module.mk
+++ engines/module.mk
@@ -1,13 +1,13 @@
 MODULE := engines
 
 MODULE_OBJS := \
-	unknown-game-dialog.o \
 	advancedDetector.o \
 	dialogs.o \
 	engine.o \
 	game.o \
 	obsolete.o \
-	savestate.o
+	savestate.o \
+	unknown-game-dialog.o \
 
 # Include common rules
 include $(srcdir)/rules.mk
