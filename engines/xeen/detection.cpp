b71bfdd53175214207659f3b80146bf9c4534ff2
Paul Gilbert <dreammaster@scummvm.org>
2018-05-04 19:36:22 -0400

    XEEN: Savegame name wasn't showing up for slot 99

--- engines/xeen/detection.cpp
+++ engines/xeen/detection.cpp
@@ -181,7 +181,7 @@ SaveStateList XeenMetaEngine::listSaves(const char *target) const {
 		const char *ext = strrchr(file->c_str(), '.');
 		int slot = ext ? atoi(ext + 1) : -1;
 
-		if (slot >= 0 && slot < MAX_SAVES) {
+		if (slot >= 0 && slot <= MAX_SAVES) {
 			Common::InSaveFile *in = g_system->getSavefileManager()->openForLoading(*file);
 
 			if (in) {
