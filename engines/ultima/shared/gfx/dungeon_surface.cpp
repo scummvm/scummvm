2384abbbec5962f4a096c9d02424cf8e7caea2da
Paul Gilbert <dreammaster@scummvm.org>
2018-05-16 22:14:49 -0400

    ULTIMA1: Implement dungeon drawBeams method

--- engines/ultima/shared/gfx/dungeon_surface.cpp
+++ engines/ultima/shared/gfx/dungeon_surface.cpp
@@ -27,12 +27,13 @@
 namespace Ultima {
 namespace Shared {
 
-const byte WALL_ARRAY_X[] = { 0, 72, 108, 126, 135, 144 };
-const byte WALL_ARRAY_Y[] = { 0, 36, 54, 63, 68, 72 };
+const byte ARRAY_X[] = { 0, 72, 108, 126, 135, 144 };
+const byte ARRAY_Y[] = { 0, 36, 54, 63, 68, 72 };
 
 DungeonSurface::DungeonSurface(const Graphics::ManagedSurface &src, const Rect &bounds, Game *game, DrawWidgetFn widgetFn) :
 		Gfx::VisualSurface(src, bounds), _widgetFn(widgetFn) {
 	_edgeColor = game->_edgeColor;
+	_highlightColor = game->_highlightColor;
 	_widgetColor = 0;
 }
 
@@ -40,10 +41,10 @@ void DungeonSurface::drawWall(uint distance) {
 	int offsetX = !distance ? 8 : 0, offsetY = !distance ? 8 : 0;
 
 	if (distance <= 5) {
-		hLine(WALL_ARRAY_X[distance] + 16 + offsetX, WALL_ARRAY_Y[distance] + 8 + offsetY,
-			303 - WALL_ARRAY_X[distance] - offsetX, _edgeColor);
-		hLine(WALL_ARRAY_X[distance] + 16 + offsetX, 151 - WALL_ARRAY_Y[distance] - offsetY,
-			303 - WALL_ARRAY_X[distance] - offsetX, _edgeColor);
+		hLine(ARRAY_X[distance] + 16 + offsetX, ARRAY_Y[distance] + 8 + offsetY,
+			303 - ARRAY_X[distance] - offsetX, _edgeColor);
+		hLine(ARRAY_X[distance] + 16 + offsetX, 151 - ARRAY_Y[distance] - offsetY,
+			303 - ARRAY_X[distance] - offsetX, _edgeColor);
 	}
 }
 
@@ -53,20 +54,20 @@ void DungeonSurface::drawDoorway(uint distance) {
 
 	if (distance < 5) {
 		drawWall(distance);
-		frameRect(Rect(WALL_ARRAY_X[distance + 1] + 16, WALL_ARRAY_Y[distance + 1] + 8,
-			303 - WALL_ARRAY_X[distance + 1], 152 - WALL_ARRAY_Y[distance] - offsetY), color);
+		frameRect(Rect(ARRAY_X[distance + 1] + 16, ARRAY_Y[distance + 1] + 8,
+			303 - ARRAY_X[distance + 1], 152 - ARRAY_Y[distance] - offsetY), color);
 	}
 }
 
 void DungeonSurface::drawLeftEdge(uint distance) {
 	if (distance <= 5) {
-		vLine(WALL_ARRAY_X[distance] + 16, WALL_ARRAY_Y[distance] + 8, 151 - WALL_ARRAY_Y[distance], _edgeColor);
+		vLine(ARRAY_X[distance] + 16, ARRAY_Y[distance] + 8, 151 - ARRAY_Y[distance], _edgeColor);
 	}
 }
 
 void DungeonSurface::drawRightEdge(uint distance) {
 	if (distance <= 5) {
-		vLine(303 - WALL_ARRAY_X[distance], WALL_ARRAY_Y[distance] + 8, 151 - WALL_ARRAY_Y[distance], _edgeColor);
+		vLine(303 - ARRAY_X[distance], ARRAY_Y[distance] + 8, 151 - ARRAY_Y[distance], _edgeColor);
 	}
 }
 
@@ -119,8 +120,8 @@ void DungeonSurface::drawLeftDoor(uint distance) {
 	if (distance <= 5) {
 		drawLeftWall(distance);
 
-		Point p1(WALL_ARRAY_X[distance], WALL_ARRAY_Y[distance]),
-			p2(WALL_ARRAY_X[distance - 1], WALL_ARRAY_Y[distance - 1]);
+		Point p1(ARRAY_X[distance], ARRAY_Y[distance]),
+			p2(ARRAY_X[distance - 1], ARRAY_Y[distance - 1]);
 		Point diff = p1 - p2;
 		diff.x /= 9;
 		diff.y /= 5;
@@ -134,8 +135,8 @@ void DungeonSurface::drawLeftDoor(uint distance) {
 
 void DungeonSurface::drawLeftWall(uint distance) {
 	if (distance <= 5) {
-		Point p1(WALL_ARRAY_X[distance], WALL_ARRAY_Y[distance]),
-			p2(WALL_ARRAY_X[distance - 1], WALL_ARRAY_Y[distance - 1]);
+		Point p1(ARRAY_X[distance], ARRAY_Y[distance]),
+			p2(ARRAY_X[distance - 1], ARRAY_Y[distance - 1]);
 		drawLine(p2.x + 16, p2.y + 8, p1.x + 16, p1.y + 8, _edgeColor);
 		drawLine(p2.x + 16, 151 - p2.y, p1.x + 16, 151 - p1.y, _edgeColor);
 	}
@@ -143,8 +144,8 @@ void DungeonSurface::drawLeftWall(uint distance) {
 
 void DungeonSurface::drawLeftBlank(uint distance) {
 	if (distance <= 5) {
-		Point p1(WALL_ARRAY_X[distance], WALL_ARRAY_Y[distance]),
-			p2(WALL_ARRAY_X[distance - 1], WALL_ARRAY_Y[distance - 1]);
+		Point p1(ARRAY_X[distance], ARRAY_Y[distance]),
+			p2(ARRAY_X[distance - 1], ARRAY_Y[distance - 1]);
 		drawLine(p2.x + 16, p1.y + 8, p1.x + 16, p1.y + 8, _edgeColor);
 		drawLine(p2.x + 16, 151 - p1.y, p1.x + 16, 151 - p1.y, _edgeColor);
 	}
@@ -154,8 +155,8 @@ void DungeonSurface::drawRightDoor(uint distance) {
 	if (distance <= 5) {
 		drawRightWall(distance);
 
-		Point p1(WALL_ARRAY_X[distance], WALL_ARRAY_Y[distance]),
-			p2(WALL_ARRAY_X[distance - 1], WALL_ARRAY_Y[distance - 1]);
+		Point p1(ARRAY_X[distance], ARRAY_Y[distance]),
+			p2(ARRAY_X[distance - 1], ARRAY_Y[distance - 1]);
 		Point diff = p1 - p2;
 		diff.x /= 9;
 		diff.y /= 5;
@@ -169,8 +170,8 @@ void DungeonSurface::drawRightDoor(uint distance) {
 
 void DungeonSurface::drawRightWall(uint distance) {
 	if (distance <= 5) {
-		Point p1(WALL_ARRAY_X[distance], WALL_ARRAY_Y[distance]),
-			p2(WALL_ARRAY_X[distance - 1], WALL_ARRAY_Y[distance - 1]);
+		Point p1(ARRAY_X[distance], ARRAY_Y[distance]),
+			p2(ARRAY_X[distance - 1], ARRAY_Y[distance - 1]);
 
 		drawLine(303 - p2.x, p2.y + 8, 303 - p1.x, p1.y + 8, _edgeColor);
 		drawLine(303 - p2.x, 151 - p2.y, 303 - p1.x, 151 - p1.y, _edgeColor);
@@ -179,15 +180,26 @@ void DungeonSurface::drawRightWall(uint distance) {
 
 void DungeonSurface::drawRightBlank(uint distance) {
 	if (distance <= 5) {
-		Point p1(WALL_ARRAY_X[distance], WALL_ARRAY_Y[distance]),
-			p2(WALL_ARRAY_X[distance - 1], WALL_ARRAY_Y[distance - 1]);
+		Point p1(ARRAY_X[distance], ARRAY_Y[distance]),
+			p2(ARRAY_X[distance - 1], ARRAY_Y[distance - 1]);
 		drawLine(303 - p2.x, p1.y + 8, 303 - p1.x, p1.y + 8, _edgeColor);
 		drawLine(303 - p2.x, 151 - p1.y, 303 - p1.x, 151 - p1.y, _edgeColor);
 	}
 }
 
 void DungeonSurface::drawBeams(uint distance) {
-	// TODO
+	if (distance <= 5) {
+		// Figure out the Y difference between each beam
+		const int Y_START = ARRAY_Y[distance] + 8;
+		const int Y_END = 151 - ARRAY_Y[distance];
+		const int HEIGHT = (Y_END - Y_START) / 4;
+
+		for (int beamNum = 0; beamNum < 4; ++beamNum) {
+			const int YP = Y_START + HEIGHT * beamNum + (distance / 2);
+
+			drawLine(ARRAY_X[distance] + 16, YP, 303 - ARRAY_X[distance], YP, _highlightColor);
+		}
+	}
 }
 
 } // End of namespace Shared
