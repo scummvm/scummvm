1f6aee019c85771cabddc0e807a4c2cad503cca0
johndoe123 <john_doe@techie.com>
2018-05-24 06:26:27 +1000

    ILLUSIONS: BBDOU: Rename variables (bubble and special code)
    (cherry picked from commit 93255ca)

--- engines/illusions/bbdou/bbdou_bubble.h
+++ engines/illusions/bbdou/bbdou_bubble.h
@@ -32,23 +32,22 @@ class IllusionsEngine_BBDOU;
 class BbdouSpecialCode;
 class Control;
 
-struct Item0 {
-	uint32 _sequenceId1;
-	uint32 _sequenceId2;
+struct BubbleStyle {
+	uint32 _showSequenceId;
+	uint32 _hideSequenceId;
 	int16 _count;
 	uint32 _progResKeywordId;
 	uint32 _baseNamedPointId;
 	uint32 _namedPointIds[32];
 	uint32 _objectId;
-	Common::Point _pt;
-	Item0() : _count(0) {}
+	Common::Point _position;
+	BubbleStyle() : _count(0) {}
 };
 
-struct Item141C {
+struct BubbleIcon {
 	uint32 _objectId;
-	int16 _enabled;
+	bool _enabled;
 	Common::Point _position;
-	int16 _fieldA;
 	uint32 _sequenceId;
 };
 
@@ -57,26 +56,26 @@ public:
 	BbdouBubble(IllusionsEngine_BBDOU *vm, BbdouSpecialCode *bbdou);
 	~BbdouBubble();
 	void init();
-	void addItem0(uint32 sequenceId1, uint32 sequenceId2, uint32 progResKeywordId,
+	void addBubbleStyle(uint32 showSequenceId, uint32 hideSequenceId, uint32 progResKeywordId,
 		uint32 namedPointId, int16 count, uint32 *namedPointIds);
 	void show();
 	void hide();
-	void setup(int16 minCount, Common::Point pt1, Common::Point pt2, uint32 progResKeywordId);
-	uint32 addItem(uint positionIndex, uint32 sequenceId);
-	void calcBubbles(Common::Point &pt1, Common::Point &pt2);
+	void selectBubbleStyle(int16 minCount, Common::Point sourcePt, Common::Point destPt, uint32 progResKeywordId);
+	uint32 addBubbleIcon(uint positionIndex, uint32 sequenceId);
+	void calcBubbleTrail(Common::Point &sourcePt, Common::Point &destPt);
 protected:
 	IllusionsEngine_BBDOU *_vm;
 	BbdouSpecialCode *_bbdou;
-	Common::Array<Item0> _item0s;
-	Item0 *_currItem0;
-	Item0 *_prevItem0;
-	Item0 *_someItem0;
-	uint32 _objectIds[32];
-	Common::Point _pt1;
-	Common::Point _pt2;
-	int _objectId1414;
-	int _objectId1418;
-	Item141C _items[32];
+	Common::Array<BubbleStyle> _bubbleStyles;
+	BubbleStyle *_currBubbleStyle;
+	BubbleStyle *_showingBubbleStyle;
+	BubbleStyle *_hidingBubbleStyle;
+	uint32 _trailObjectIds[32];
+	Common::Point _sourcePt;
+	Common::Point _destPt;
+	int _bubbleObjectId1;
+	int _bubbleObjectId2;
+	BubbleIcon _icons[32];
 };
 
 } // End of namespace Illusions
