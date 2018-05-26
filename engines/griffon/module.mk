a8041a970634fc39285ffeac817dfb3d3b0cdac9
Eugene Sandulenko <sev@scummvm.org>
2018-05-26 20:32:53 +0200

    GRIFFON: Added skeleton engine with detection

---
+++ engines/griffon/module.mk
@@ -0,0 +1,16 @@
+MODULE := engines/griffon
+
+MODULE_OBJS := \
+	griffon.o \
+	detection.o
+
+MODULE_DIRS += \
+	engines/griffon
+
+# This module can be built as a plugin
+ifeq ($(ENABLE_GRIFFON), DYNAMIC_PLUGIN)
+PLUGIN := 1
+endif
+
+# Include common rules
+include $(srcdir)/rules.mk
