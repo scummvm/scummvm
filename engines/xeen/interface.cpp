b191cbf90967b59f075dbf5f51a5c6fb5587bfc7
Paul Gilbert <dreammaster@scummvm.org>
2018-05-30 18:15:50 -0400

    XEEN: Stop re-rolling new character stats when some areas of screen clicked

--- engines/xeen/interface.cpp
+++ engines/xeen/interface.cpp
@@ -163,6 +163,7 @@ Interface::Interface(XeenEngine *vm) : ButtonContainer(vm), InterfaceScene(vm),
 	_upDoorText = false;
 	_tillMove = 0;
 	Common::fill(&_charFX[0], &_charFX[MAX_ACTIVE_PARTY], 0);
+	_waitBounds = Common::Rect(8, 8, 224, 140);
 }
 
 void Interface::setup() {
