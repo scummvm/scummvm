8d1263cfb026bebb6f64a522964dde3dcd74a4bb
whiterandrek <whiterandrek@gmail.com>
2018-06-21 17:15:14 +0300

    PINK: add commands to list and set game variables

--- engines/pink/console.cpp
+++ engines/pink/console.cpp
@@ -29,6 +29,9 @@ Console::Console(PinkEngine *vm)
 	: _vm(vm) {
 	registerCmd("listModules", WRAP_METHOD(Console, Cmd_ListModules));
 	registerCmd("goToModule", WRAP_METHOD(Console, Cmd_GoToModule));
+
+	registerCmd("listGameVars", WRAP_METHOD(Console, Cmd_ListGameVars));
+	registerCmd("setGameVar", WRAP_METHOD(Console, Cmd_SetGameVar));
 }
 
 bool Console::Cmd_ListModules(int argc, const char **argv) {
@@ -42,7 +45,7 @@ bool Console::Cmd_ListModules(int argc, const char **argv) {
 bool Console::Cmd_GoToModule(int argc, const char **argv) {
 	if (argc != 2) {
 		debugPrintf("Usage: %s moduleName\n", argv[0]);
-		debugPrintf("Module may not work properly because of Game vars");
+		debugPrintf("Module may not work properly because of Game vars\n");
 		return true;
 	}
 	const Array<NamedObject*> modules = _vm->_modules;
@@ -52,7 +55,24 @@ bool Console::Cmd_GoToModule(int argc, const char **argv) {
 			return true;
 		}
 	}
-	debugPrintf("Module %s doesn't exist", argv[1]);
+	debugPrintf("Module %s doesn't exist\n", argv[1]);
+	return true;
+}
+
+bool Console::Cmd_ListGameVars(int argc, const char **argv) {
+	const StringMap &vars = _vm->_variables;
+	for (StringMap::const_iterator it = vars.begin(); it != vars.end() ; ++it) {
+		debugPrintf("%s %s \n", it->_key.c_str(), it->_value.c_str());
+	}
+	return true;
+}
+
+bool Console::Cmd_SetGameVar(int argc, const char **argv) {
+	if (argc != 3) {
+		debugPrintf("Usage: %s varName value\n", argv[0]);
+		return true;
+	}
+	_vm->_variables[argv[1]] = argv[2];
 	return true;
 }
 
