247115d009f90397bfcdc746ad495f03008b4bd1
David Fioramonti <dafioram@gmail.com>
2018-07-14 04:52:58 -0700

    GROOVIE: Allow saving via GMM and increase save slots to 25
    If the user enters in an invalid description for the save then it will be
    simplified by the saved name cache process in Script::savegame.
    
    I checked this with t7g, but not with the 11th guest or any other Groovie game.
    
    The original in-game loading/saving can still only access the first 10 save
    slots (0-9).
    
    Its possible to saving via the GMM when the game is interactive.
    
    Loading a game saved via the gui using the original in-game load menu
    works fine.
    
    Saving via the GMM with timestamp works, but only only saves the characters
    up to the first non-alpha non-letter characer.

--- engines/groovie/script.h
+++ engines/groovie/script.h
@@ -23,6 +23,8 @@
 #ifndef GROOVIE_SCRIPT_H
 #define GROOVIE_SCRIPT_H
 
+#include "groovie/groovie.h"
+
 #include "common/random.h"
 #include "common/rect.h"
 
@@ -43,7 +45,6 @@ enum EngineVersion {
 
 class CellGame;
 class Debugger;
-class GroovieEngine;
 
 class Script {
 	friend class Debugger;
@@ -59,6 +60,7 @@ public:
 
 	bool loadScript(Common::String scriptfile);
 	void directGameLoad(int slot);
+	void directGameSave(int slot, const Common::String &desc);
 	void step();
 
 	void setMouseClick(uint8 button);
@@ -81,7 +83,7 @@ private:
 	Common::String _savedScriptFile;
 
 	// Save names
-	Common::String _saveNames[10];
+	Common::String _saveNames[MAX_SAVES];
 
 	// Code
 	byte *_code;
