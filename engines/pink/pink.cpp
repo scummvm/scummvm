32a0e69045e952635c1525fc6929817c4d81c397
Andrei Prykhodko <whiterandrek@gmail.com>
2018-07-18 22:11:21 +0300

    PINK: fixed redrawing screen when menu disappears

--- engines/pink/pink.cpp
+++ engines/pink/pink.cpp
@@ -161,6 +161,9 @@ Common::Error Pink::PinkEngine::run() {
 void PinkEngine::pauseEngine(void *engine, bool pause) {
 	PinkEngine *vm = (PinkEngine*)engine;
 	vm->pauseEngineIntern(pause);
+	if (!pause) {
+		vm->_director->addDirtyRect(Common::Rect(0, 0, 640, 480));
+	}
 }
 
 void PinkEngine::load(Archive &archive) {
