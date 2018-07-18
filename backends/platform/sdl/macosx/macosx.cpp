ff56446d6a0f754176909b5ebc07bc36a1e25337
Bastien Bouclet <bastien.bouclet@gmail.com>
2018-07-18 19:54:38 +0200

    MACOSX: Set the search depth to 2 for the app bundle
    The shader files are in a subdirectory.

--- backends/platform/sdl/macosx/macosx.cpp
+++ backends/platform/sdl/macosx/macosx.cpp
@@ -91,7 +91,8 @@ void OSystem_MacOSX::addSysArchivesToSearchSet(Common::SearchSet &s, int priorit
 		if (CFURLGetFileSystemRepresentation(fileUrl, true, buf, sizeof(buf))) {
 			// Success: Add it to the search path
 			Common::String bundlePath((const char *)buf);
-			s.add("__OSX_BUNDLE__", new Common::FSDirectory(bundlePath), priority);
+			// ResidualVM: Search with a depth of 2 so the shaders are found
+			s.add("__OSX_BUNDLE__", new Common::FSDirectory(bundlePath, 2), priority);
 		}
 		CFRelease(fileUrl);
 	}
