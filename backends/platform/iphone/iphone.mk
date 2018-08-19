d0169e739c8db999b0ce44ee5a1d4c30e7059958
Cameron Cawley <ccawley2011@gmail.com>
2018-08-19 19:11:43 +0100

    IPHONE: Move packaging rules into separate files

---
+++ backends/platform/iphone/iphone.mk
@@ -0,0 +1,20 @@
+bundle_name = ScummVM.app
+
+iphonebundle: all
+	mkdir -p $(bundle_name)
+	cp $(srcdir)/dists/iphone/Info.plist $(bundle_name)/
+	cp $(DIST_FILES_DOCS) $(bundle_name)/
+	cp $(DIST_FILES_THEMES) $(bundle_name)/
+ifdef DIST_FILES_NETWORKING
+	cp $(DIST_FILES_NETWORKING) $(bundle_name)/
+endif
+ifdef DIST_FILES_ENGINEDATA
+	cp $(DIST_FILES_ENGINEDATA) $(bundle_name)/
+endif
+	$(STRIP) scummvm
+	ldid -S scummvm
+	chmod 755 scummvm
+	cp scummvm $(bundle_name)/ScummVM
+	cp $(srcdir)/dists/iphone/icon.png $(bundle_name)/
+	cp $(srcdir)/dists/iphone/icon-72.png $(bundle_name)/
+	cp $(srcdir)/dists/iphone/Default.png $(bundle_name)/
