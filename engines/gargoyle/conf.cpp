af75b737fd9175cc336490b5911bafcd094cbd18
Paul Gilbert <dreammaster@scummvm.org>
2018-10-29 21:12:06 -0700

    GLK: Add missing configuration defaults

--- engines/gargoyle/conf.cpp
+++ engines/gargoyle/conf.cpp
@@ -89,8 +89,8 @@ Conf::Conf() {
 	get("propi", _propI);
 	get("propz", _propZ);
 	get("propfont", _propFont, "Linux Libertine O");
-	get("leading", _leading);
-	get("baseline", _baseLine);
+	get("leading", _leading, 20);
+	get("baseline", _baseLine, 15);
 	get("rows", _rows, 25);
 	get("cols", _cols, 60);
 
