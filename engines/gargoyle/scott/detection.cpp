22db439498cef462b313a532639181dadb5d450e
Paul Gilbert <dreammaster@scummvm.org>
2018-11-12 09:44:04 -0800

    GLK: SCOTT: Fix detecting .dat gamefiles, added Windows game versions detections

--- engines/gargoyle/scott/detection.cpp
+++ engines/gargoyle/scott/detection.cpp
@@ -35,6 +35,25 @@ struct ScottGame {
 };
 
 const ScottGame SCOTT_GAMES[] = {
+	// PC game versions
+	{ "7c6f495d757a54e73d259efc718d8024", "adventureland",     15896, "Adventureland" },
+	{ "ea535fa7684508410151b4561de1f323", "pirateadventure",   16325, "Pirate Adventure" },
+	{ "379c77a9a483886366b3b5c425e56410", "missionimpossible", 15275, "Mission Impossible" },
+	{ "a530a6857d1092eaa177eee575c94c71", "voodoocastle",      15852, "Voodoo Castle" },
+	{ "5ebb4ade985670bb2eac54f8fa202214", "thecount",          17476, "The Count" },
+	{ "c57bb6df04dc77a2b232bc5bcab6e417", "strangeodyssey",    17489, "Strange Odyssey" },
+	{ "ce2931ac3d5cbc270a5cb7be9e614f6e", "mysteryfunhouse",   17165, "Mystery Fun House" },
+	{ "4e6127fad6b5d75eccd3f3b101f8c9c8", "pyramidofdoom",     17673, "Pyramid Of Doom" },
+	{ "2c08327ab06d5490bd9e367ddaeca627", "ghosttown",         17831, "Ghost Town" },
+	{ "8feb77f11d32e9567ce2fc7d435eaf44", "savageisland1",     19533, "Savage Island, Part 1" },
+	{ "20c40a349f7a214ac515fb1d63c30a87", "savageisland2",     18367, "Savage Island, Part 2" },
+	{ "e2a8f956ab215012d1495550c4c11ee8", "goldenvoyage",      18513, "The Golden Voyage" },
+	{ "f986d7e1ee074f65b6c1d00461c9b3c3", "adventure13",       19232, "Adventure 13" },
+	{ "6d98f422cc986d959a3c74351785aea3", "adventure14",       19013, "Adventure 14" },
+	{ "aadcc04e6b37eb9d30a58b5bc775842e", "marveladventure",   18876, "Marvel Adventure #1" },
+	{ "d569a769f304dc02b3062d97458ddd01", "scottsampler",      13854, "Adventure International's Mini-Adventure Sampler" },
+
+	// PDA game versions
 	{ "ae541fc1085da2f7d561b72ed20a6bc1", "adventureland", 18003, "Adventureland" },
 	{ "cbd47ab4fcfe00231ffd71d52378d410", "pirateadventure", 18482, "Pirate Adventure" },
 	{ "9251ab2c64e63559d8a6e9e6246760a5", "missionimpossible", 17227, "Mission Impossible" },
@@ -59,7 +78,8 @@ bool ScottMetaEngine::detectGames(const Common::FSList &fslist, DetectedGames &g
 
 	// Loop through the files of the folder
 	for (Common::FSList::const_iterator file = fslist.begin(); file != fslist.end(); ++file) {
-		if (file->isDirectory() || !file->getName().hasSuffix(".saga"))
+		if (file->isDirectory() || !(file->getName().hasSuffixIgnoreCase(".saga")
+				|| file->getName().hasSuffixIgnoreCase(".dat")))
 			continue;
 
 		if (gameFile.open(*file)) {
