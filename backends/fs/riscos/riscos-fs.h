0f45b7f9bcdb0956de8d80c81c257be1fe112083
Cameron Cawley <ccawley2011@gmail.com>
2018-11-24 22:20:25 +0000

    RISCOS: Add RISC OS support

---
+++ backends/fs/riscos/riscos-fs.h
@@ -0,0 +1,105 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+#ifndef RISCOS_FILESYSTEM_H
+#define RISCOS_FILESYSTEM_H
+
+#include "backends/fs/abstract-fs.h"
+
+/**
+ * Implementation of the ScummVM file system API.
+ *
+ * Parts of this class are documented in the base interface class, AbstractFSNode.
+ */
+class RISCOSFilesystemNode : public AbstractFSNode {
+protected:
+	Common::String _displayName;
+	Common::String _path;
+	bool _isDirectory;
+	bool _isValid;
+
+	virtual AbstractFSNode *makeNode(const Common::String &path) const {
+		return new RISCOSFilesystemNode(path);
+	}
+
+	/**
+	 * Plain constructor, for internal use only (hence protected).
+	 */
+	RISCOSFilesystemNode() : _isDirectory(false), _isValid(false) {}
+public:
+	/**
+	 * Creates a RISCOSFilesystemNode for a given path.
+	 *
+	 * @param path the path the new node should point to.
+	 */
+	RISCOSFilesystemNode(const Common::String &path);
+
+	virtual bool exists() const;
+	virtual Common::String getDisplayName() const { return _displayName; }
+	virtual Common::String getName() const { return _displayName; }
+	virtual Common::String getPath() const { return _path; }
+	virtual bool isDirectory() const { return _isDirectory; }
+	virtual bool isReadable() const;
+	virtual bool isWritable() const;
+
+	virtual AbstractFSNode *getChild(const Common::String &n) const;
+	virtual bool getChildren(AbstractFSList &list, ListMode mode, bool hidden) const;
+	virtual AbstractFSNode *getParent() const;
+
+	virtual Common::SeekableReadStream *createReadStream();
+	virtual Common::WriteStream *createWriteStream();
+	virtual bool create(bool isDirectoryFlag);
+
+private:
+	/**
+	 * Converts a Unix style path to a RISC OS style path.
+	 *
+	 * @param str Unix style path to convert.
+	 * @return RISC OS style path.
+	 */
+	static Common::String toRISCOS(Common::String &path);
+
+	/**
+	 * Converts a RISC OS style path to a Unix style path.
+	 *
+	 * @param str RISC OS style path to convert.
+	 * @return Unix style path.
+	 */
+	static Common::String toUnix(Common::String &path);
+
+};
+
+namespace Riscos {
+
+/**
+ * Assure that a directory path exists.
+ *
+ * @param dir The path which is required to exist.
+ * @param prefix An (optional) prefix which should not be created if non existent.
+ *               prefix is prepended to dir if supplied.
+ * @return true in case the directoy exists (or was created), false otherwise.
+ */
+bool assureDirectoryExists(const Common::String &dir, const char *prefix = nullptr);
+
+} // End of namespace RISCOS
+
+#endif
---
+++ backends/platform/sdl/riscos/riscos.mk
@@ -0,0 +1,43 @@
+ifeq ($(shell echo a | iconv --to-code=RISCOS-LATIN1//TRANSLIT >/dev/null 2>&1; echo $$?),0)
+ENCODING=RISCOS-LATIN1//TRANSLIT
+else
+ENCODING=ISO-8859-1//TRANSLIT
+endif
+
+APP_NAME=!ResidualVM
+
+# Special target to create an RISC OS snapshot installation
+riscosdist: all
+	mkdir -p $(APP_NAME)
+	elf2aif $(EXECUTABLE) $(APP_NAME)/residualvm,ff8
+	cp ${srcdir}/dists/riscos/!Boot,feb $(APP_NAME)/!Boot,feb
+	cp ${srcdir}/dists/riscos/!Run,feb $(APP_NAME)/!Run,feb
+	cp ${srcdir}/dists/riscos/!Sprites,ff9 $(APP_NAME)/!Sprites,ff9
+	sed -i -e "s/|WimpSlot/WimpSlot -min `du -k $(APP_NAME)/residualvm,ff8 | cut -f1`K/g" $(APP_NAME)/!Run,feb
+	mkdir -p $(APP_NAME)/data
+	cp $(DIST_FILES_THEMES) $(APP_NAME)/data/
+ifdef DIST_FILES_NETWORKING
+	cp $(DIST_FILES_NETWORKING) $(APP_NAME)/data/
+endif
+ifdef DIST_FILES_ENGINEDATA
+	cp $(DIST_FILES_ENGINEDATA) $(APP_NAME)/data/
+endif
+	# ResidualVM specific
+ifdef USE_OPENGL_SHADERS
+	mkdir -p $(APP_NAME)/data/shaders
+	cp $(DIST_FILES_SHADERS) $(APP_NAME)/data/shaders/
+endif
+ifdef DYNAMIC_MODULES
+	mkdir -p $(APP_NAME)/plugins
+	cp $(PLUGINS) $(APP_NAME)/plugins/
+endif
+	mkdir -p $(APP_NAME)/docs
+	cp ${srcdir}/dists/riscos/!Help,feb $(APP_NAME)/!Help,feb
+	@$(foreach file, $(DIST_FILES_DOCS) $(srcdir)/doc/QuickStart, echo '   ' ICONV '  ' $(APP_NAME)/docs/$(notdir $(file)),fff;iconv --to-code=$(ENCODING) $(file) > $(APP_NAME)/docs/$(notdir $(file)),fff;)
+
+clean: riscosclean
+
+riscosclean:
+	$(RM_REC) $(APP_NAME)
+
+.PHONY: riscosdist riscosclean
