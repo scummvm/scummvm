3dd2114636f18d3acfa0900956ec303de3bdf08e
antoniou79 <a.antoniou79@gmail.com>
2018-12-24 14:59:38 +0200

    DEVTOOLS: Blade Runner subtitles python scripts

---
+++ devtools/blade_runner/subtitles/fontCreator/fonFileLib.py
@@ -0,0 +1,233 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+#
+import os, sys, shutil
+import struct
+from struct import *
+import Image
+
+my_module_version = "0.50"
+my_module_name = "fonFileLib"
+
+
+class FonHeader:
+	maxEntriesInTableOfDetails = -1			# this is probably always the number of entries in table of details, but it won't work for the corrupted TAHOMA18.FON file
+	maxGlyphWidth = -1						# in pixels
+	maxGlyphHeight = -1						# in pixels
+	graphicSegmentByteSize = -1				# Graphic segment byte size
+
+	def __init__(self):
+		return
+
+
+class fonFile:
+	m_header = FonHeader()
+
+	simpleFontFileName = "GENERIC.FON"
+	realNumOfCharactersInImageSegment = 0	# this is used for the workaround for the corrupted TAHOME18.FON
+	nonEmptyCharacters = 0
+
+	glyphDetailEntriesLst = []	# list of 5-value tuples. Tuple values are (X-offset, Y-offset, Width, Height, Offset in Graphics segment)
+	glyphPixelData = None		# buffer of pixel data for glyphs
+
+	def __init__(self):
+		del self.glyphDetailEntriesLst[:]
+		self.glyphPixelData = None		# buffer of pixel data for glyphs
+		self.simpleFontFileName = "GENERIC.FON"
+		self.realNumOfCharactersInImageSegment = 0	# this is used for the workaround for the corrupted TAHOME18.FON
+		self.nonEmptyCharacters = 0
+
+		return
+
+	def loadFonFile(self, fonBytesBuff, maxLength, fonFileName):
+		self.simpleFontFileName =  fonFileName
+
+
+		offsInFonFile = 0
+		localLstOfDataOffsets = []
+		del localLstOfDataOffsets[:]
+		#
+		# parse FON file fields for header
+		#
+		try:
+			tmpTuple = struct.unpack_from('I', fonBytesBuff, offsInFonFile)	 # unsigned integer 4 bytes
+			self.header().maxEntriesInTableOfDetails = tmpTuple[0]
+			offsInFonFile += 4
+
+			if self.simpleFontFileName == 'TAHOMA18.FON': # deal with corrupted original 'TAHOMA18.FON' file
+				self.realNumOfCharactersInImageSegment = 176
+				print "SPECIAL CASE. WORKAROUND FOR CORRUPTED %s FILE. Only %d characters supported!" % (self.simpleFontFileName, self.realNumOfCharactersInImageSegment)
+			else:
+				self.realNumOfCharactersInImageSegment = self.header().maxEntriesInTableOfDetails
+
+			tmpTuple = struct.unpack_from('I', fonBytesBuff, offsInFonFile)	 # unsigned integer 4 bytes
+			self.header().maxGlyphWidth = tmpTuple[0]
+			offsInFonFile += 4
+
+			tmpTuple = struct.unpack_from('I', fonBytesBuff, offsInFonFile)	 # unsigned integer 4 bytes
+			self.header().maxGlyphHeight = tmpTuple[0]
+			offsInFonFile += 4
+
+			tmpTuple = struct.unpack_from('I', fonBytesBuff, offsInFonFile)	 # unsigned integer 4 bytes
+			self.header().graphicSegmentByteSize = tmpTuple[0]
+			offsInFonFile += 4
+
+			print "FON Header Info: "
+			print "Num of entries: %d\tGlyph max-Width: %d\tGlyph max-Height: %d\tGraphic Segment size %d" % (self.header().maxEntriesInTableOfDetails, self.header().maxGlyphWidth, self.header().maxGlyphHeight, self.header().graphicSegmentByteSize)
+			#print "Num of entries: %d\tGlyph max-Width: %d\tGlyph max-Height: %d\tGraphic Segment size %d" % (self.realNumOfCharactersInImageSegment, self.header().maxGlyphWidth, self.header().maxGlyphHeight, self.header().graphicSegmentByteSize)
+			#
+			# Glyph details table (each entry is 5 unsigned integers == 5*4 = 20 bytes)
+			# For most characters, their ASCII value + 1 is the index of their glyph's entry in the details table. The 0 entry of this table is reserved
+			#
+			#tmpXOffset, tmpYOffset, tmpWidth, tmpHeight, tmpDataOffset
+			print "FON glyph details table: "
+			for idx in range(0, self.realNumOfCharactersInImageSegment):
+				tmpTuple = struct.unpack_from('i', fonBytesBuff, offsInFonFile)	 # unsigned integer 4 bytes
+				tmpXOffset = tmpTuple[0]
+				offsInFonFile += 4
+
+				tmpTuple = struct.unpack_from('I', fonBytesBuff, offsInFonFile)	 # unsigned integer 4 bytes
+				tmpYOffset = tmpTuple[0]
+				offsInFonFile += 4
+
+				tmpTuple = struct.unpack_from('I', fonBytesBuff, offsInFonFile)	 # unsigned integer 4 bytes
+				tmpWidth = tmpTuple[0]
+				offsInFonFile += 4
+
+				tmpTuple = struct.unpack_from('I', fonBytesBuff, offsInFonFile)	 # unsigned integer 4 bytes
+				tmpHeight = tmpTuple[0]
+				offsInFonFile += 4
+
+				tmpTuple = struct.unpack_from('I', fonBytesBuff, offsInFonFile)	 # unsigned integer 4 bytes
+				tmpDataOffset = tmpTuple[0]
+				offsInFonFile += 4
+
+				if tmpWidth == 0 or tmpHeight == 0:
+					print "Index: %d\t UNUSED *****************************************************************" % (idx)
+				else:
+					self.nonEmptyCharacters += 1
+					print "Index: %d\txOffs: %d\tyOffs: %d\twidth: %d\theight: %d\tdataOffs: %d" % (idx, tmpXOffset, tmpYOffset, tmpWidth, tmpHeight, tmpDataOffset)
+					if tmpDataOffset not in localLstOfDataOffsets:
+						localLstOfDataOffsets.append(tmpDataOffset)
+					else:
+						# This never happens in the original files. Offsets are "re-used" but not really because it happens only for empty (height = 0) characters which all seem to point to the next non-empty character
+						print "Index: %d\t RE-USING ANOTHER GLYPH *****************************************************************" % (idx)
+
+				self.glyphDetailEntriesLst.append( ( tmpXOffset, tmpYOffset, tmpWidth, tmpHeight, tmpDataOffset) )
+
+			offsInFonFile = (4 * 4) + (self.header().maxEntriesInTableOfDetails * 5 * 4) # we need the total self.header().maxEntriesInTableOfDetails here and not self.realNumOfCharactersInImageSegment
+			self.glyphPixelData = fonBytesBuff[offsInFonFile:]
+			return True
+		except:
+			print "Loading failure!"
+			raise
+			return False
+
+	def outputFonToPNG(self):
+		targWidth = 0
+		targHeight = 0
+		paddingFromTopY = 2
+		paddingBetweenGlyphsX = 10
+
+
+		if len(self.glyphDetailEntriesLst) == 0 or (len(self.glyphDetailEntriesLst) != self.realNumOfCharactersInImageSegment and len(self.glyphDetailEntriesLst) != self.header().maxEntriesInTableOfDetails) :
+			print "Error. Fon file load process did not complete correctly. Missing important data in structures. Cannot output image!"
+			return
+
+		# TODO asdf refine this code here. the dimensions calculation is very crude for now
+		if self.header().maxGlyphWidth > 0 :
+			targWidth = (self.header().maxGlyphWidth + paddingBetweenGlyphsX) * (self.realNumOfCharactersInImageSegment + 1)
+		else:
+			targWidth = 1080
+
+		# TODO asdf refine this code here. the dimensions calculation is very crude for now
+		if self.header().maxGlyphHeight > 0 :
+			targHeight = self.header().maxGlyphHeight * 2
+		else:
+			targHeight = 480
+
+		imTargetGameFont = Image.new("RGBA",(targWidth, targHeight), (0,0,0,0))
+		#print imTargetGameFont.getbands()
+		#
+		# Now fill in the image segment
+		# Fonts in image segment are stored in pixel colors from TOP to Bottom, Left to Right per GLYPH.
+		# Each pixel is 16 bit (2 bytes). Highest bit seems to determine transparency (on/off flag).
+		# There seem to be 5 bits per RGB channel and the value is the corresponding 8bit value (from the 24 bit pixel color) shifting out (right) the 3 LSBs
+		# First font image is the special character (border of top row and left column) - color of font pixels should be "0x7FFF" for filled and "0x8000" for transparent
+		drawIdx = 0
+		drawIdxDeductAmount = 0
+		for idx in range(0, self.realNumOfCharactersInImageSegment):
+			# TODO check for size > 0 for self.glyphPixelData
+			# TODO mark glyph OUTLINES? (optional by switch)
+			(glyphXoffs, glyphYoffs, glyphWidth, glyphHeight, glyphDataOffs) = self.glyphDetailEntriesLst[idx]
+			glyphDataOffs = glyphDataOffs * 2
+			#print idx, glyphDataOffs
+			currX = 0
+			currY = 0
+			if (glyphWidth == 0 or glyphHeight == 0):
+				drawIdxDeductAmount += 1
+			drawIdx = idx - drawIdxDeductAmount
+
+			for colorIdx in range(0, glyphWidth*glyphHeight):
+				tmpTuple = struct.unpack_from('H', self.glyphPixelData, glyphDataOffs)	# unsigned short 2 bytes
+				pixelColor = tmpTuple[0]
+				glyphDataOffs += 2
+
+#				 if pixelColor > 0x8000:
+#					 print "WEIRD CASE" # NEVER HAPPENS - TRANSPARENCY IS ON/OFF. There's no grades of transparency
+				rgbacolour = (0,0,0,0)
+				if pixelColor == 0x8000:
+					rgbacolour = (0,0,0,0) # alpha: 0.0 fully transparent
+				else:
+					tmp8bitR1 =	 ( (pixelColor >> 10) ) << 3
+					tmp8bitG1 =	 ( (pixelColor & 0x3ff) >> 5 ) << 3
+					tmp8bitB1 =	 ( (pixelColor & 0x1f) ) << 3
+					rgbacolour = (tmp8bitR1,tmp8bitG1,tmp8bitB1, 255)	# alpha: 1.0 fully opaque
+					#rgbacolour = (255,255,255, 255)   # alpha: 1.0 fully opaque
+
+				if currX == glyphWidth:
+					currX = 0
+					currY += 1
+
+				imTargetGameFont.putpixel(( (drawIdx + 1) * (self.header().maxGlyphWidth + paddingBetweenGlyphsX ) + currX, paddingFromTopY + glyphYoffs + currY), rgbacolour)
+				currX += 1
+
+		imTargetGameFont.save(os.path.join('.', self.simpleFontFileName + ".PNG"), "PNG")
+
+	def header(self):
+		return self.m_header
+#
+#
+#
+if __name__ == '__main__':
+	#	 main()
+	print "Running %s as main module" % (my_module_name)
+	# assumes a file of name TAHOMA24.FON in same directory
+	inFONFile = None
+	#inFONFileName =  'TAHOMA24.FON'		# USED IN CREDIT END-TITLES and SCORERS BOARD AT POLICE STATION
+	#inFONFileName =	 'TAHOMA18.FON'		   # USED IN CREDIT END-TITLES
+	#inFONFileName =  '10PT.FON'			# BLADE RUNNER UNUSED FONT?
+	#inFONFileName =  'KIA6PT.FON'			# BLADE RUNNER MAIN FONT
+	inFONFileName =  'SUBTLS_E.FON'			# Subtitles font custom
+
+	errorFound = False
+	try:
+		print "Opening %s" % (inFONFileName)
+		inFONFile = open(os.path.join('.',inFONFileName), 'rb')
+	except:
+		errorFound = True
+		print "Unexpected error:", sys.exc_info()[0]
+		raise
+	if not errorFound:
+		allOfFonFileInBuffer = inFONFile.read()
+		fonFileInstance = fonFile()
+		if (fonFileInstance.loadFonFile(allOfFonFileInBuffer, len(allOfFonFileInBuffer), inFONFileName)):
+			print "FON file loaded successfully!"
+			fonFileInstance.outputFonToPNG()
+		else:
+			print "Error while loading FON file!"
+		inFONFile.close()
+else:
+	#debug
+	#print "Running	 %s imported from another module" % (my_module_name)
+	pass
---
+++ devtools/blade_runner/subtitles/quotesSpreadsheetCreator/audFileDecode.py
@@ -0,0 +1,210 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+#
+import ctypes
+from struct import *
+
+my_module_version = "0.50"
+my_module_name = "audFileDecode"
+
+aud_ima_index_adjust_table = [-1, -1, -1, -1, 2, 4, 6, 8]
+
+# aud_ima_step_table has 89 entries
+aud_ima_step_table = [
+	7,	   8,	  9,	 10,	11,	   12,	   13,	  14,	 16,
+	17,	   19,	  21,	 23,	25,	   28,	   31,	  34,	 37,
+	41,	   45,	  50,	 55,	60,	   66,	   73,	  80,	 88,
+	97,	   107,	  118,	 130,	143,   157,	   173,	  190,	 209,
+	230,   253,	  279,	 307,	337,   371,	   408,	  449,	 494,
+	544,   598,	  658,	 724,	796,   876,	   963,	  1060,	 1166,
+	1282,  1411,  1552,	 1707,	1878,  2066,   2272,  2499,	 2749,
+	3024,  3327,  3660,	 4026,	4428,  4871,   5358,  5894,	 6484,
+	7132,  7845,  8630,	 9493,	10442, 11487,  12635, 13899, 15289,
+	16818, 18500, 20350, 22385, 24623, 27086,  29794, 32767 ]
+
+aud_ws_step_table2 = [-2, -1, 0, 1]
+
+aud_ws_step_table4 = [
+	-9, -8, -6, -5, -4, -3, -2, -1,
+	 0,	 1,	 2,	 3,	 4,	 5,	 6,	 8
+]
+
+# (const xccTHA::byte* audio_in, short* audio_out, int& index, int& sample, int cs_chunk)
+# index and sample are passed by reference and changed here...
+# audio_out is definitely affected!
+def aud_decode_ima_chunk(audioBufferIn, index, sample, cs_chunk):
+	code = -1
+	delta = -1
+	step = -1
+	
+	audioBufferOut = []
+	#for i in range(0, len(audioBufferIn)):
+	#	print '%d: %d'%(i, int(audioBufferIn[i]))
+	
+	for sample_index in range (0, cs_chunk):
+		try:
+			code = audioBufferIn[sample_index >> 1]
+		except:
+			code = 0xa9 # dummy workaround because the c code is accessing an out of bounds index sometimes due to this shift here
+		#print "cs_chunk %d, sample_index %d, shifted %d, code: %d" % (cs_chunk, sample_index, sample_index >> 1, int(audioBufferIn[sample_index >> 1]))
+		#print "cs_chunk %s, sample_index %s, shifted %s, code: %s" % \
+		#		(''.join('{:04X}'.format(cs_chunk)), ''.join('{:02X}'.format(sample_index)), ''.join('{:02X}'.format(sample_index >> 1)), ''.join('{:04X}'.format(int(code))))
+		code = code >> 4  if (sample_index & 1) else code & 0xf
+		step = aud_ima_step_table[index]
+		delta = step >> 3
+		if (code & 1):
+			delta += step >> 2
+		if (code & 2):
+			delta += step >> 1
+		if (code & 4):
+			delta += step
+		if (code & 8):
+			sample -= delta
+			if (sample < -32768):
+				sample = -32768
+		else:
+			sample += delta
+			if (sample > 32767):
+				sample = 32767
+		audioBufferOut.append(ctypes.c_short( sample ).value )
+		#audioBufferOut.append(sample) # it's not different from above... ctypes.c_short( sample ).value
+		#print "audio_out[%s]: %s" % (''.join('{:02X}'.format(sample_index)), ''.join('{:02X}'.format(audioBufferOut[sample_index])));
+		index += aud_ima_index_adjust_table[code & 7]
+		if (index < 0):
+			index = 0
+		elif (index > 88):
+			index = 88
+	## output buffer of shorts		
+	#binDataOut = struct.pack('h'*len(audioBufferOut), *audioBufferOut)
+	#return (binDataOut, index, sample)
+	return (audioBufferOut, index, sample)
+#
+#
+#
+def aud_decode_clip8(v):
+	if (v < 0):
+		return 0
+	return 0xff if (v > 0xff) else v
+#
+#
+#
+
+# (const xccTHA::byte* r, char* w, int cb_s, int cb_d)
+def aud_decode_ws_chunk(inputChunkBuffer, cb_s, cb_d):
+	outputChunkBuffer = []
+	inpChBuffIter = 0
+	outChBuffIter = 0
+
+	if (cb_s == cb_d):
+		# outputChunkBuffer = inputChunkBuffer[:cb_s] #	memcpy(w, r, cb_s)  #  FIX
+		for mcp in range(0, cb_s):
+			outputChunkBuffer.append(ctypes.c_char(inputChunkBuffer[inpChBuffIter + mcp]).value)
+		#binDataOut = struct.pack('b'*len(outputChunkBuffer), *outputChunkBuffer)
+		#return binDataOut
+		return outputChunkBuffer
+		
+#	const xccTHA::byte* s_end = inputChunkBuffer + cb_s;	 #  FIX
+
+	s_end = inpChBuffIter + cb_s
+	sample = ctypes.c_int(0x80).value													#int sample				
+	while (inpChBuffIter < s_end):
+		inpChBuffIter += 1
+		count = ctypes.c_char(inputChunkBuffer[inpChBuffIter] & 0x3f).value  			# char count
+		switchKey = inputChunkBuffer[inpChBuffIter - 1] >> 6							# inputChunkBuffer[-1] # b[-1] is  *(b - 1) 
+		if switchKey == 0:
+			count += 1
+			for iter in range (count, 0, -1):
+				inpChBuffIter += 1
+				code = ctypes.c_int(inputChunkBuffer[inpChBuffIter]).value				# int code
+				# assignment in C was right to left so:
+				# *(outputChunkBuffer++) = sample = clip8(sample + aud_ws_step_table2[code & 3]) 
+				# is:
+				# *(outputChunkBuffer++) = (sample = clip8(sample + aud_ws_step_table2[code & 3])) 
+				# which is equivalent to these two commands:
+				# sample = clip8(sample + aud_ws_step_table2[code & 3])
+				# *(outputChunkBuffer++) = sample
+				# SO:
+				sample = aud_decode_clip8(sample + aud_ws_step_table2[code & 3])
+				outputChunkBuffer.append(ctypes.c_char(sample).value)
+				outChBuffIter += 1
+				sample = aud_decode_clip8(sample + aud_ws_step_table2[code >> 2 & 3])
+				outputChunkBuffer.append(ctypes.c_char(sample).value)
+				outChBuffIter += 1
+				sample = aud_decode_clip8(sample + aud_ws_step_table2[code >> 4 & 3])
+				outputChunkBuffer.append(ctypes.c_char(sample).value)
+				outChBuffIter += 1
+				sample = aud_decode_clip8(sample + aud_ws_step_table2[code >> 6])
+				outputChunkBuffer.append(ctypes.c_char(sample).value)
+				outChBuffIter += 1
+		elif switchKey == 1:
+			count += 1
+			for iter in range (count, 0, -1):
+				inpChBuffIter += 1
+				code = inputChunkBuffer[inpChBuffIter]	# int code	
+				sample += aud_ws_step_table4[code & 0xf]
+				sample = aud_decode_clip8(sample)
+				outputChunkBuffer.append(ctypes.c_char(sample).value)
+				outChBuffIter += 1
+				sample += aud_ws_step_table4[code >> 4]
+				sample = aud_decode_clip8(sample)
+				outputChunkBuffer.append(ctypes.c_char(sample).value)
+				outChBuffIter += 1
+		elif switchKey == 2:
+			if (count & 0x20):
+				#sample += static_cast<char>(count << 3) >> 3
+				#*(outputChunkBuffer++) = sample
+				sample += ((count & 0xFF) << 3 ) >> 3
+				outputChunkBuffer.append(ctypes.c_char(sample).value)
+				outChBuffIter += 1
+			else:
+				count += 1
+				# memcpy(outputChunkBuffer, inputChunkBuffer, count) 	# FIX
+				for mcp in range(0, count):
+					outputChunkBuffer.append(ctypes.c_char(inputChunkBuffer[inpChBuffIter + mcp]).value)
+				inpChBuffIter += count
+				outChBuffIter += count
+				sample = inputChunkBuffer[inpChBuffIter - 1]
+		else:
+			count += 1
+			# memset(outputChunkBuffer, sample, ++count)
+			for mst in range(0, count):
+				outputChunkBuffer.append(ctypes.c_char(sample).value)
+			outChBuffIter += count;
+	# output buffer of chars		
+	#binDataOut = struct.pack('b'*len(outputChunkBuffer), *outputChunkBuffer)
+	#return binDataOut
+	return outputChunkBuffer
+
+#
+#
+#
+	
+class audFileDecode:
+	m_index = -1
+	m_sample = -1
+	
+	def __init__(self, index = 0, sample = 0):
+		self.m_index = index;
+		self.m_sample = sample;
+		return
+	
+	def index(self):
+		return self.m_index
+		
+	# (const xccTHA::byte* audio_in, short* audio_out, int cs_chunk)
+	def decode_chunk(self, audio_in, cs_chunk):
+		(audio_Out, outIndex, outSample) = aud_decode_ima_chunk(audio_in, self.m_index, self.m_sample, cs_chunk)
+		self.m_index = outIndex
+		self.m_sample = outSample
+		return audio_Out
+
+if __name__ == '__main__':
+	#	 main()
+	print "Running %s as main module" % (my_module_name)
+	decodeInstance = audFileDecode()
+	
+else:
+	#debug
+	#print "Running	 %s imported from another module" % (my_module_name)
+	pass
+	
