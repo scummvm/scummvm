39f9517326b6acb1795c7ad446c158a056936b15
antoniou79 <a.antoniou79@gmail.com>
2019-01-03 03:43:34 +0200

    DEVTOOLS: Improved Ids for dev I_SEZ quotes

--- devtools/create_bladerunner/subtitles/quotesSpreadsheetCreator/devCommentaryText.py
+++ devtools/create_bladerunner/subtitles/quotesSpreadsheetCreator/devCommentaryText.py
@@ -12,19 +12,21 @@ DEV_AUDIO_COMMENTART_TUPLE_LIST = [
 	("HC01VO1A.AUD", "This is an area known as Hawker's Circle. It's sort of a swamp pit, so to speak where a lot of the scum of the earth hang out. Notice again the constant Asian reminder here showing you that Asians are often living in poor rather dingy areas of the city.")
 ]
 
+# we use the spoken quote id that triggers the comment as an ID for the DEV commentary quote
+# Ids 00-9990 and above correspond to clicks or other transitions (no corresponding spoken in-game quote)
 DEV_ISEZ_QUOTES_TUPLE_LIST = [
-	("I_SEZ", "Blade Runner\nFrom the dark recesses of David Leary's imagination comes a game unlike any\nother. Blade Runner immerses you in the underbelly of future Los Angeles. Right\nfrom the start, the story pulls you in with graphic descriptions of a\ngrandmother doing the shimmy in her underwear, child molestation, brutal\ncold-blooded slaying of innocent animals, vomiting on desks, staring at a\nwoman's ass, the list goes on. And when the game starts, the real fun begins -\nshoot down-on-their-luck homeless people and toss them into a dumpster. Watch\nwith sadistic glee as a dog gets blown into chunky, bloody, bits by an\nexplosive, and even murder a shy little girl who loves you. If you think David\nLeary is sick, and you like sick, this is THE game for you.\n\nJW: Don't forget the wasting of helpless mutated cripples in the underground.\nIt's such a beautiful thing!\n\nDL: Go ahead.  Just keep beating that snarling pit bull...ignore the foam\naround his jaws. There's room on the top shelf of my fridge for at least one\nmore head... - Psychotic Dave\n"),
-	("I_SEZ", "MG: Is David Leary a self-respecting human or is he powered by rechargeable\nBatteries?"),
-	("I_SEZ", "JM: That McCoy--he's one funny guy! Jet-black fire truck, hehehehe..."),
-	("I_SEZ", "JM: Did it have a huge, ugly piece of chrome on it?"),
-	("I_SEZ", "JM: This officer has a talent for vivid metaphors."),
-	("I_SEZ", "DL: What is that supposed to mean?  I didn't write this line..."),
-	("I_SEZ", "MG: Hey, leave that officer alone. Can't you see he's busy?\nJM: (...mmm, donuts...)"),
-	("I_SEZ", "MG: It's all fun and games until someone loses a tiger cub."),
-	("I_SEZ", "JM: Chrome...is that what that is?"),
-	("I_SEZ", "JM: It's hard to imagine that thing on either a car or a horse.\nMG: McCoy! What a witty chap...\nJM: He keeps me chuckling non-stop!"),
-	("I_SEZ", "MG: Leaving already?  The fun is just beginning!"),
-	("I_SEZ", "MG: We don't want any of that abstract art oozing out onto the street.")
+	("00-9990", "Blade Runner\nFrom the dark recesses of David Leary's imagination comes a game unlike any\nother. Blade Runner immerses you in the underbelly of future Los Angeles. Right\nfrom the start, the story pulls you in with graphic descriptions of a\ngrandmother doing the shimmy in her underwear, child molestation, brutal\ncold-blooded slaying of innocent animals, vomiting on desks, staring at a\nwoman's ass, the list goes on. And when the game starts, the real fun begins -\nshoot down-on-their-luck homeless people and toss them into a dumpster. Watch\nwith sadistic glee as a dog gets blown into chunky, bloody, bits by an\nexplosive, and even murder a shy little girl who loves you. If you think David\nLeary is sick, and you like sick, this is THE game for you.\n\nJW: Don't forget the wasting of helpless mutated cripples in the underground.\nIt's such a beautiful thing!\n\nDL: Go ahead.  Just keep beating that snarling pit bull...ignore the foam\naround his jaws. There's room on the top shelf of my fridge for at least one\nmore head... - Psychotic Dave\n"),
+	("99-1860", "MG: Is David Leary a self-respecting human or is he powered by rechargeable\nBatteries?"),  # voice-over
+	("99-1890", "JM: That McCoy--he's one funny guy! Jet-black fire truck, hehehehe..."), # voice-over
+	("23-0130", "JM: Did it have a huge, ugly piece of chrome on it?"), # Officer Leary
+	("23-0090", "JM: This officer has a talent for vivid metaphors."), # Officer Leary
+	("00-4540", "DL: What is that supposed to mean?  I didn't write this line..."),
+	("00-4515", "MG: Hey, leave that officer alone. Can't you see he's busy?\nJM: (...mmm, donuts...)"), # clicking on Leary after we get his statement 
+	("23-0060", "MG: It's all fun and games until someone loses a tiger cub."), # Officer Leary
+	("00-9991", "JM: Chrome...is that what that is?"), # pick up chrome
+	("00-4510", "JM: It's hard to imagine that thing on either a car or a horse.\nMG: McCoy! What a witty chap...\nJM: He keeps me chuckling non-stop!"),
+	("00-9992", "MG: Leaving already?  The fun is just beginning!"), # leaving Runciter's zoo with Spinner
+	("00-4500", "MG: We don't want any of that abstract art oozing out onto the street.")
 ]
 #
 #
