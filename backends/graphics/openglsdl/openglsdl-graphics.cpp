a79c3b7961f7ef4d7003742d3140303117c37188
Bastien Bouclet <bastien.bouclet@gmail.com>
2019-01-28 12:50:07 +0100

    SDL: Don't set a profile when requesting an OpenGL context
    It does not make too much sense to request a compatibility profile with
    OpenGL 2.1 since that notion was added in GL 3.2.
    
    Fixes context creation on Windows with old nvidia drivers (v310.90).

--- backends/graphics/openglsdl/openglsdl-graphics.cpp
+++ backends/graphics/openglsdl/openglsdl-graphics.cpp
@@ -344,11 +344,9 @@ bool OpenGLSdlGraphicsManager::createOrUpdateGLContext(uint gameWidth, uint game
 		#ifdef __amigaos4__
 			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 1);
 			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-			SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, 0);
 		#else
 			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
 			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
-			SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
 		#endif
 #endif
 #endif
