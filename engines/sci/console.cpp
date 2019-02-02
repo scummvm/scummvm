75daa49d112e63b9206167f39b783b9fd55c5a1e
Vhati <tvtronix@yahoo.com>
2019-02-02 14:44:14 -0500

    SCI: Fix inconsistent debugger breakpoint messages
    Fixes bp_function and bp_address not printing the created breakpoint, as
    happens with the other bp_ commands.

--- engines/sci/console.cpp
+++ engines/sci/console.cpp
@@ -4256,6 +4256,8 @@ bool Console::cmdBreakpointFunction(int argc, const char **argv) {
 	_debugState._breakpoints.push_back(bp);
 	_debugState._activeBreakpointTypes |= BREAK_EXPORT;
 
+	printBreakpoint(_debugState._breakpoints.size() - 1, bp);
+
 	return true;
 }
 
@@ -4291,6 +4293,8 @@ bool Console::cmdBreakpointAddress(int argc, const char **argv) {
 
 	_debugState._breakpoints.push_back(bp);
 	_debugState._activeBreakpointTypes |= BREAK_ADDRESS;
+
+	printBreakpoint(_debugState._breakpoints.size() - 1, bp);
 
 	return true;
 }
