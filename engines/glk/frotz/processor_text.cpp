088b09c3be99b273f26e2bb14706ad372c363ef6
Torbjörn Andersson <eriktorbjorn@users.sourceforge.net>
2019-02-14 18:07:23 +0100

    GLK: FROTZ: Fix expansion of "g" to "again" in some ancient games.

--- engines/glk/frotz/processor_text.cpp
+++ engines/glk/frotz/processor_text.cpp
@@ -570,7 +570,7 @@ void Processor::handleAbbreviations() {
 
 	// Check for standard abbreviations
 	if (word == "g")
-		word == "again";
+		word = "again";
 	else if (word == "o")
 		word = "oops";
 	else if (word == "x")
