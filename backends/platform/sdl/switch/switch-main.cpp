875bc0b1c21aac75e55947ff421f2a8cc9f36c29
rsn8887 <rsn8887@users.noreply.github.com>
2019-05-07 13:52:25 -0500

    Switch: fix build, add build instructions, avoid using delete()

--- backends/platform/sdl/switch/switch-main.cpp
+++ backends/platform/sdl/switch/switch-main.cpp
@@ -52,7 +52,7 @@ int main(int argc, char *argv[]) {
 	int res = scummvm_main(argc, argv);
 
 	// Free OSystem
-	delete (OSystem_Switch *)g_system;
+	g_system->destroy();
 
 #ifdef __SWITCH_DEBUG__
 	socketExit();
