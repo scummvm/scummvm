acfa5a83581f325a9a51cf388e4825426a4cb92f
SupSuper <supsuper@gmail.com>
2019-05-09 03:07:07 +0100

    SDL: Fix MSVC warnings
    Fixes warning C4121: alignment of a member was sensitive to packing
    by hiding warnings from system headers

--- backends/platform/sdl/sdl-sys.h
+++ backends/platform/sdl/sdl-sys.h
@@ -144,7 +144,11 @@
 #include <SDL.h>
 #endif
 
+// Ignore warnings from system headers pulled by SDL
+#pragma warning(push)
+#pragma warning(disable:4121) // alignment of a member was sensitive to packing
 #include <SDL_syswm.h>
+#pragma warning(pop)
 
 // Restore the forbidden exceptions from the hack above
 #if !defined(FORBIDDEN_SYMBOL_ALLOW_ALL) && defined(_MSC_VER)
