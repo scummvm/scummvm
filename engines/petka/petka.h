0b40c18c3975b29be6ce3824952317a534c4fa3a
Andrei Prykhodko <whiterandrek@gmail.com>
2019-05-28 09:59:37 +0300

    PETKA: Initial engine commit

---
+++ engines/petka/petka.h
@@ -0,0 +1,60 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+#ifndef PETKA_PETKA_H
+#define PETKA_PETKA_H
+
+#include "engines/engine.h"
+
+#include "gui/debugger.h"
+
+struct ADGameDescription;
+
+namespace Petka {
+
+class Console;
+
+enum {
+	kPetkaDebugGeneral = 1 << 0,
+};
+
+class PetkaEngine : public Engine {
+public:
+	PetkaEngine(OSystem *syst, const ADGameDescription *desc);
+	~PetkaEngine();
+
+	virtual Common::Error run();
+
+private:
+	Console *_console;
+	const ADGameDescription *_desc;
+};
+
+class Console : public GUI::Debugger {
+public:
+	Console(PetkaEngine *vm) {}
+	virtual ~Console() {}
+};
+
+} // End of namespace Petka
+
+#endif
