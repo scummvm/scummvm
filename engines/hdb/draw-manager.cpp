f90d06bee94ee5b3b42a9fd2873a03c77d961ca5
Nipun Garg <nipun@nipungarg.me>
2019-06-08 02:25:11 +0530

    HDB: Add _stars3D, _stars3DSlow + setup functions

--- engines/hdb/draw-manager.cpp
+++ engines/hdb/draw-manager.cpp
@@ -144,14 +144,33 @@ void DrawMan::setSky(int skyIndex) {
 
 	// Setup current sky
 	if (tileIndex == _tileSkyStars) {
-		warning("STUB: DRAWMAN::setSky: Setup3DStars");
+		setup3DStars();
 		return;
 	} else if (skyIndex == _tileSkyStarsLeft) {
-		warning("STUB: DRAWMAN::setSky: Setup3DStarsLeft");
+		setup3DStarsLeft();
 		return;
 	} else if (skyIndex == _tileSkyStars) {
 		warning("STUB: DRAWMAN::setSky: getPicture( CLOUDY_SKIES )");
 		return;
+	}
+}
+
+void DrawMan::setup3DStars() {
+	for (int i = 0; i < kNum3DStars; i++) {
+		_stars3D[i].x = g_hdb->_rnd->getRandomNumber(kScreenWidth) % kScreenWidth;
+		_stars3D[i].y = g_hdb->_rnd->getRandomNumber(kScreenHeight) % kScreenHeight;
+		_stars3D[i].speed = (g_hdb->_rnd->getRandomNumber(256) % 256);
+		_stars3D[i].speed >>= 1;
+		_stars3D[i].color = _stars3D[i].speed / 64;
+	}
+}
+
+void DrawMan::setup3DStarsLeft() {
+	for (int i = 0; i < kNum3DStars; i++) {
+		_stars3DSlow[i].x = g_hdb->_rnd->getRandomNumber(kScreenWidth) % kScreenWidth;
+		_stars3DSlow[i].y = g_hdb->_rnd->getRandomNumber(kScreenHeight) % kScreenHeight;
+		_stars3DSlow[i].speed = ((double) (1 + (g_hdb->_rnd->getRandomNumber(5) % 5))) / 6.0;
+		_stars3DSlow[i].color = (int) (_stars3DSlow[i].speed * 4.00);
 	}
 }
 
