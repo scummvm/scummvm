79023f0f9e6ed8237cfc8061bc30dd47a5ce85fd
Nipun Garg <nipun@nipungarg.me>
2019-06-20 00:14:05 +0530

    HDB: Pass correct variable into warning()

--- engines/hdb/ai-cinematic.cpp
+++ engines/hdb/ai-cinematic.cpp
@@ -144,7 +144,7 @@ void AI::processCines() {
 				_cine[i]->e->y = (int)_cine[i]->y;
 				_cine[i]->e->level = (int)_cine[i]->x2;
 			} else {
-				warning("Can't locate '%s' in setEntity", _cine[i]->title);
+				warning("Can't locate '%s' in setEntity", _cine[i]->string);
 			}
 			complete = true;
 			break;
