01ecd7db4008648ab4504792b585413fd875ae48
lolbot-iichan <lolbot_iichan@mail.ru>
2019-06-22 02:56:34 +0300

    WINTERMUTE: Add detection table for "One Helluva Day" demo
    "One Helluva Day" is a point-and-click adventure demo in English / Czech
    / Russian.
    Free download:
    https://store.steampowered.com/app/603680/One_helluva_day/

--- engines/wintermute/detection_tables.h
+++ engines/wintermute/detection_tables.h
@@ -69,6 +69,7 @@ static const PlainGameDescriptor wintermuteGames[] = {
 	{"mirage",          "Mirage"},
 	{"nighttrain",      "Night Train"},
 	{"oknytt",          "Oknytt"},
+	{"onehelluvaday",   "One Helluva Day"},
 	{"openquest",       "Open Quest"},
 	{"paintaria",       "Paintaria"},
 	{"pigeons",         "Pigeons in the Park"},
@@ -414,6 +415,9 @@ static const WMEGameDescription gameDescriptions[] = {
 	WME_WINENTRY("oknytt", "Version 1.13",
 		WME_ENTRY2s("spanish.dcp", "10c46152cb29581671f3b6b7c229c957", 319406572,
 					"d_sounds.dcp", "7d04dff8ca11174486bd4b7a80fdcabb", 154943401), Common::ES_ESP, ADGF_UNSTABLE, LATEST_VERSION),
+	// One Helluva Day (Demo) (multi-language)
+	WME_WINENTRY("onehelluvaday", "",
+		WME_ENTRY1s("data.dcp", "144e23fca7c1c54103dad9c1342de2b6", 229963509), Common::UNK_LANG, ADGF_UNSTABLE | ADGF_DEMO, LATEST_VERSION),
 	// Open Quest
 	WME_WINENTRY("openquest", "",
 		WME_ENTRY1s("data.dcp", "16893e3fc15a211a49654ae66f684f28", 82281736), Common::EN_ANY, ADGF_UNSTABLE | GF_LOWSPEC_ASSETS, LATEST_VERSION),
