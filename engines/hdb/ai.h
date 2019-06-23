da528445f905011a93b9346a9345aa39abc98acb
Nipun Garg <nipun@nipungarg.me>
2019-06-23 06:50:09 +0530

    HDB: Add cineDialog()

--- engines/hdb/ai.h
+++ engines/hdb/ai.h
@@ -720,6 +720,7 @@ public:
 	void cineSetEntity(const char *entName, int x, int y, int level);
 	void cineMoveEntity(const char *entName, int x, int y, int level, int speed);
 	void cineEntityFace(const char *luaName, double dir);
+	void cineDialog(const char *title, const char *string, int seconds);
 	void cineUse(const char *entName);
 	void cineFadeIn(bool isBlack, int steps);
 	void cineFadeOut(bool isBlack, int steps);
