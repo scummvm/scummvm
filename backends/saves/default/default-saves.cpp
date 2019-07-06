35c2f9ade51451de8dc88477500b82cfe69f2a5f
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2019-07-06 23:19:42 +0300

    BACKENDS: Fix crash when save file is owned by another user
    fopen() fails, but the null handle was passed on to OutSaveFile.

--- backends/saves/default/default-saves.cpp
+++ backends/saves/default/default-saves.cpp
@@ -169,6 +169,8 @@ Common::OutSaveFile *DefaultSaveFileManager::openForSaving(const Common::String 
 
 	// Open the file for saving.
 	Common::WriteStream *const sf = fileNode.createWriteStream();
+	if (!sf)
+		return nullptr;
 	Common::OutSaveFile *const result = new Common::OutSaveFile(compress ? Common::wrapCompressedWriteStream(sf) : sf);
 
 	// Add file to cache now that it exists.
