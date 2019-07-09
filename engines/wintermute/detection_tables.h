5f5671e2a2d355f9f6b10b4edc425c6cab83cea6
lolbot-iichan <lolbot_iichan@mail.ru>
2019-07-09 03:42:03 +0300

    WINTERMUTE: Update detection for The White Chamber
    Download:
    http://www.studiotrophis.com/downloads/thewhitechamber1.7SETUP%20-%20Definitive%20Edition.exe
    Download:
    http://www.indiedb.com/games/the-white-chamber/downloads/the-white-chamber-17

--- engines/wintermute/detection_tables.h
+++ engines/wintermute/detection_tables.h
@@ -2023,11 +2023,11 @@ static const WMEGameDescription gameDescriptions[] = {
 
 	// the white chamber (Version 1.7 - Definitive Edition) (built 2008.06.26) (multi-language)
 	WME_WINENTRY("twc", "Definitive Edition",
-		WME_ENTRY1s("data.dcp", "325abfaeb5fbfcc30d91296f1390a454", 186451273), Common::UNK_LANG, ADGF_UNSTABLE, LATEST_VERSION),
+		WME_ENTRY1s("data.dcp", "325abfaeb5fbfcc30d91296f1390a454", 186451273), Common::UNK_LANG, ADGF_UNSTABLE, WME_1_8_6),
 
 	// the white chamber (Version 1.7 - Definitive Edition) (built 2008.06.29) (multi-language)
 	WME_WINENTRY("twc", "Definitive Edition",
-		WME_ENTRY1s("data.dcp", "0011d01142547c61e51ba24dc42b579e", 186451273), Common::UNK_LANG, ADGF_UNSTABLE, LATEST_VERSION),
+		WME_ENTRY1s("data.dcp", "0011d01142547c61e51ba24dc42b579e", 186451273), Common::UNK_LANG, ADGF_UNSTABLE, WME_1_8_6),
 
 	// Vsevolod Prologue (Demo)
 	WME_WINENTRY("vsevolod", "Prologue",
