4c012861bcf9b9babb95df2d2faaed380d914859
Nipun Garg <nipun@nipungarg.me>
2019-07-24 02:53:28 +0530

    HDB: Play a MP3 sound from hdb.cpp

--- engines/hdb/hdb.cpp
+++ engines/hdb/hdb.cpp
@@ -858,6 +858,13 @@ Common::Error HDBGame::run() {
 	initGraphics(kScreenWidth, kScreenHeight, &_format);
 	_console = new Console();
 
+#if USE_MAD
+	Common::SeekableReadStream *soundStream = _fileMan->findFirstData("M00_AIRLOCK_01_MP3", TYPE_BINARY);
+	Audio::SeekableAudioStream *audioStream = Audio::makeMP3Stream(soundStream, DisposeAfterUse::YES);
+	Audio::SoundHandle *handle = new Audio::SoundHandle();
+	g_hdb->_mixer->playStream(Audio::Mixer::kPlainSoundType, handle, audioStream);
+#endif
+
 	start();
 
 #if 0
