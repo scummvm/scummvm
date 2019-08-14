e34d58ca78203044bec8018abf6b3fb4bf2aeb66
Misty De Meo <mistydemeo@gmail.com>
2019-08-13 18:19:26 -0700

    SAGA: add PC-98 CD version of Inherit the Earth

--- engines/saga/saga.h
+++ engines/saga/saga.h
@@ -140,7 +140,8 @@ enum GameFeatures {
 	GF_ITE_DOS_DEMO      = 1 << 1,
 	GF_EXTRA_ITE_CREDITS = 1 << 2,
 	GF_8BIT_UNSIGNED_PCM = 1 << 3,
-	GF_IHNM_COLOR_FIX    = 1 << 4
+	GF_IHNM_COLOR_FIX    = 1 << 4,
+	GF_ITE_PC98          = 1 << 5
 };
 
 enum VerbTypeIds {
