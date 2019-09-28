7b68555626579a3f0c3e701c5916fec3a3f34c09
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-09-28 23:20:34 +0200

    LASTEXPRESS: The game doesn't use MIDI, so mark it as such.

--- engines/lastexpress/detection.cpp
+++ engines/lastexpress/detection.cpp
@@ -49,7 +49,7 @@ static const ADGameDescription gameDescriptions[] = {
 		Common::EN_ANY,
 		Common::kPlatformUnknown,
 		ADGF_UNSTABLE,
-		GUIO1(GUIO_NOASPECT)
+		GUIO2(GUIO_NOASPECT, GUIO_NOMIDI)
 	},
 
 	// The Last Express (English) - UK Broderbund Release
@@ -68,7 +68,7 @@ static const ADGameDescription gameDescriptions[] = {
 		Common::EN_ANY,
 		Common::kPlatformUnknown,
 		ADGF_UNSTABLE,
-		GUIO1(GUIO_NOASPECT)
+		GUIO2(GUIO_NOASPECT, GUIO_NOMIDI)
 	},
 
 	// The Last Express (English) - Interplay Release
@@ -87,7 +87,7 @@ static const ADGameDescription gameDescriptions[] = {
 		Common::EN_ANY,
 		Common::kPlatformUnknown,
 		ADGF_UNSTABLE,
-		GUIO1(GUIO_NOASPECT)
+		GUIO2(GUIO_NOASPECT, GUIO_NOMIDI)
 	},
 
 	// The Last Express (Demo - English) - Broderbund
@@ -103,7 +103,7 @@ static const ADGameDescription gameDescriptions[] = {
 		Common::EN_ANY,
 		Common::kPlatformUnknown,
 		ADGF_DEMO | ADGF_UNSTABLE,
-		GUIO1(GUIO_NOASPECT)
+		GUIO2(GUIO_NOASPECT, GUIO_NOMIDI)
 	},
 
 	// The Last Express (French) - Broderbund Release
@@ -122,7 +122,7 @@ static const ADGameDescription gameDescriptions[] = {
 		Common::FR_FRA,
 		Common::kPlatformUnknown,
 		ADGF_UNSTABLE,
-		GUIO1(GUIO_NOASPECT)
+		GUIO2(GUIO_NOASPECT, GUIO_NOMIDI)
 	},
 
 	// The Last Express (German)
@@ -141,7 +141,7 @@ static const ADGameDescription gameDescriptions[] = {
 		Common::DE_DEU,
 		Common::kPlatformUnknown,
 		ADGF_UNSTABLE,
-		GUIO1(GUIO_NOASPECT)
+		GUIO2(GUIO_NOASPECT, GUIO_NOMIDI)
 	},
 
 	// The Last Express (Spanish)
@@ -160,7 +160,7 @@ static const ADGameDescription gameDescriptions[] = {
 		Common::ES_ESP,
 		Common::kPlatformUnknown,
 		ADGF_UNSTABLE,
-		GUIO1(GUIO_NOASPECT)
+		GUIO2(GUIO_NOASPECT, GUIO_NOMIDI)
 	},
 
 	// The Last Express (Italian)
@@ -179,7 +179,7 @@ static const ADGameDescription gameDescriptions[] = {
 		Common::IT_ITA,
 		Common::kPlatformUnknown,
 		ADGF_UNSTABLE,
-		GUIO1(GUIO_NOASPECT)
+		GUIO2(GUIO_NOASPECT, GUIO_NOMIDI)
 	},
 
 	// The Last Express (Russian)
@@ -198,7 +198,7 @@ static const ADGameDescription gameDescriptions[] = {
 		Common::RU_RUS,
 		Common::kPlatformUnknown,
 		ADGF_UNSTABLE,
-		GUIO1(GUIO_NOASPECT)
+		GUIO2(GUIO_NOASPECT, GUIO_NOMIDI)
 	},
 
 	// The Last Express (GOG)
@@ -217,7 +217,7 @@ static const ADGameDescription gameDescriptions[] = {
 		Common::EN_ANY,
 		Common::kPlatformUnknown,
 		ADGF_UNSTABLE,
-		GUIO1(GUIO_NOASPECT)
+		GUIO2(GUIO_NOASPECT, GUIO_NOMIDI)
 	},
 
 	AD_TABLE_END_MARKER
