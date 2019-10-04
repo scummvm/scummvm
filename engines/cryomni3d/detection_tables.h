0b5ea11e3c8259447f46426c8bd5a2d12045a44a
Eugene Sandulenko <sev@scummvm.org>
2019-10-04 23:00:36 +0200

    CRYOMNI3D: Remove ADGF_TESTING flag

--- engines/cryomni3d/detection_tables.h
+++ engines/cryomni3d/detection_tables.h
@@ -35,7 +35,7 @@ static const CryOmni3DGameDescription gameDescriptions[] = {
 			AD_ENTRY1s("VERSAILL.EXE", "3775004b96f056716ce615b458b1f394", 372736),
 			Common::FR_FRA,
 			Common::kPlatformWindows,
-			ADGF_TESTING,
+			ADGF_NO_FLAGS,
 			GUI_OPTIONS_VERSAILLES
 		},
 		GType_VERSAILLES,
@@ -52,7 +52,7 @@ static const CryOmni3DGameDescription gameDescriptions[] = {
 			AD_ENTRY1s("PROGRAM.Z", "a07b5d86af5f3a8883ba97db2bade87d", 293223),
 			Common::FR_FRA,
 			Common::kPlatformWindows,
-			ADGF_TESTING,
+			ADGF_NO_FLAGS,
 			GUI_OPTIONS_VERSAILLES
 		},
 		GType_VERSAILLES,
@@ -69,7 +69,7 @@ static const CryOmni3DGameDescription gameDescriptions[] = {
 			AD_ENTRY1s("VERSAILL.PGM", "1c992f034f43418a5da2e8ebd0b92620", 630431),
 			Common::FR_FRA,
 			Common::kPlatformDOS,
-			ADGF_TESTING,
+			ADGF_NO_FLAGS,
 			GUI_OPTIONS_VERSAILLES
 		},
 		GType_VERSAILLES,
@@ -86,7 +86,7 @@ static const CryOmni3DGameDescription gameDescriptions[] = {
 			AD_ENTRY1s("VERSAILL.PGM", "1c992f034f43418a5da2e8ebd0b92620", 598767),
 			Common::FR_FRA,
 			Common::kPlatformDOS,
-			ADGF_TESTING,
+			ADGF_NO_FLAGS,
 			GUI_OPTIONS_VERSAILLES
 		},
 		GType_VERSAILLES,
@@ -103,7 +103,7 @@ static const CryOmni3DGameDescription gameDescriptions[] = {
 			AD_ENTRY1s("PROGRAM.Z", "167ac4d6e60856ee84d7369107d858d4", 230056),
 			Common::FR_FRA,
 			Common::kPlatformWindows,
-			ADGF_TESTING,
+			ADGF_NO_FLAGS,
 			GUI_OPTIONS_VERSAILLES
 		},
 		GType_VERSAILLES,
@@ -120,7 +120,7 @@ static const CryOmni3DGameDescription gameDescriptions[] = {
 			AD_ENTRY1s("Versailles", "f81935517b1bbb58acf70f25efa5c7f3", 375868),
 			Common::FR_FRA,
 			Common::kPlatformMacintosh,
-			ADGF_TESTING,
+			ADGF_NO_FLAGS,
 			GUI_OPTIONS_VERSAILLES
 		},
 		GType_VERSAILLES,
@@ -137,7 +137,7 @@ static const CryOmni3DGameDescription gameDescriptions[] = {
 			AD_ENTRY1s("Versaill.exe", "5c3c10ec821b8d96016041ab649af8c7", 377856),
 			Common::EN_ANY,
 			Common::kPlatformWindows,
-			ADGF_TESTING,
+			ADGF_NO_FLAGS,
 			GUI_OPTIONS_VERSAILLES
 		},
 		GType_VERSAILLES,
@@ -154,7 +154,7 @@ static const CryOmni3DGameDescription gameDescriptions[] = {
 			AD_ENTRY1s("Versailles", "7fa3cb6a3c18f6b4ba6be85dcd433cff", 366199),
 			Common::EN_ANY,
 			Common::kPlatformMacintosh,
-			ADGF_TESTING,
+			ADGF_NO_FLAGS,
 			GUI_OPTIONS_VERSAILLES
 		},
 		GType_VERSAILLES,
@@ -171,7 +171,7 @@ static const CryOmni3DGameDescription gameDescriptions[] = {
 			AD_ENTRY1s("VERSAILL.PGM", "1c992f034f43418a5da2e8ebd0b92620", 598639),
 			Common::EN_ANY,
 			Common::kPlatformDOS,
-			ADGF_TESTING,
+			ADGF_NO_FLAGS,
 			GUI_OPTIONS_VERSAILLES
 		},
 		GType_VERSAILLES,
@@ -188,7 +188,7 @@ static const CryOmni3DGameDescription gameDescriptions[] = {
 			AD_ENTRY1s("PROGRAM.Z", "f5327cb860a67a24a52b6125ddc5e00b", 256146),
 			Common::EN_ANY,
 			Common::kPlatformWindows,
-			ADGF_TESTING,
+			ADGF_NO_FLAGS,
 			GUI_OPTIONS_VERSAILLES
 		},
 		GType_VERSAILLES,
@@ -205,7 +205,7 @@ static const CryOmni3DGameDescription gameDescriptions[] = {
 			AD_ENTRY1s("VERSAILL.PGM", "1c992f034f43418a5da2e8ebd0b92620", 710467),
 			Common::EN_ANY,
 			Common::kPlatformDOS,
-			ADGF_TESTING,
+			ADGF_NO_FLAGS,
 			GUI_OPTIONS_VERSAILLES
 		},
 		GType_VERSAILLES,
@@ -222,7 +222,7 @@ static const CryOmni3DGameDescription gameDescriptions[] = {
 			AD_ENTRY1s("PROGRAM.Z", "daeeb8bce80fe74fe28ecc22b6a97f83", 237679),
 			Common::EN_ANY,
 			Common::kPlatformWindows,
-			ADGF_TESTING,
+			ADGF_NO_FLAGS,
 			GUI_OPTIONS_VERSAILLES
 		},
 		GType_VERSAILLES,
@@ -239,7 +239,7 @@ static const CryOmni3DGameDescription gameDescriptions[] = {
 			AD_ENTRY1s("VERSAILL.PGM", "1c992f034f43418a5da2e8ebd0b92620", 603023),
 			Common::IT_ITA,
 			Common::kPlatformDOS,
-			ADGF_TESTING,
+			ADGF_NO_FLAGS,
 			GUI_OPTIONS_VERSAILLES
 		},
 		GType_VERSAILLES,
@@ -256,7 +256,7 @@ static const CryOmni3DGameDescription gameDescriptions[] = {
 			AD_ENTRY1s("PROGRAM.Z", "8b0dcf71a7eb21b8378add8b16857bae", 237878),
 			Common::IT_ITA,
 			Common::kPlatformWindows,
-			ADGF_TESTING,
+			ADGF_NO_FLAGS,
 			GUI_OPTIONS_VERSAILLES
 		},
 		GType_VERSAILLES,
@@ -273,7 +273,7 @@ static const CryOmni3DGameDescription gameDescriptions[] = {
 			AD_ENTRY1s("Versailles", "f1aa0603e7f71404f936e4189b4c5b2b", 348614),
 			Common::DE_DEU,
 			Common::kPlatformMacintosh,
-			ADGF_TESTING,
+			ADGF_NO_FLAGS,
 			GUI_OPTIONS_VERSAILLES
 		},
 		GType_VERSAILLES,
@@ -290,7 +290,7 @@ static const CryOmni3DGameDescription gameDescriptions[] = {
 			AD_ENTRY1s("PROGRAM.Z", "2e1a40237f8b28cb6ef29cff137fa561", 238041),
 			Common::DE_DEU,
 			Common::kPlatformWindows,
-			ADGF_TESTING,
+			ADGF_NO_FLAGS,
 			GUI_OPTIONS_VERSAILLES
 		},
 		GType_VERSAILLES,
@@ -307,7 +307,7 @@ static const CryOmni3DGameDescription gameDescriptions[] = {
 			AD_ENTRY1s("VERSAILL.EXE", "78d90d656ec3b76f158721d38bc68083", 346112),
 			Common::ES_ESP,
 			Common::kPlatformWindows,
-			ADGF_TESTING,
+			ADGF_NO_FLAGS,
 			GUI_OPTIONS_VERSAILLES
 		},
 		GType_VERSAILLES,
@@ -324,7 +324,7 @@ static const CryOmni3DGameDescription gameDescriptions[] = {
 			AD_ENTRY1s("PROGRAM.Z", "9e7c0c3125124010d45dde9dc62744ef", 237800),
 			Common::ES_ESP,
 			Common::kPlatformWindows,
-			ADGF_TESTING,
+			ADGF_NO_FLAGS,
 			GUI_OPTIONS_VERSAILLES
 		},
 		GType_VERSAILLES,
@@ -341,7 +341,7 @@ static const CryOmni3DGameDescription gameDescriptions[] = {
 			AD_ENTRY1s("VERSAILL.PGM", "1c992f034f43418a5da2e8ebd0b92620", 715887),
 			Common::ES_ESP,
 			Common::kPlatformDOS,
-			ADGF_TESTING,
+			ADGF_NO_FLAGS,
 			GUI_OPTIONS_VERSAILLES
 		},
 		GType_VERSAILLES,
