7384a8c35e97870e654aebbf6378b7250bb205ad
Eugene Sandulenko <sev@scummvm.org>
2019-11-07 23:04:32 +0100

    GRIFFON: Initialize variables

--- engines/griffon/griffon.cpp
+++ engines/griffon/griffon.cpp
@@ -68,6 +68,9 @@ GriffonEngine::GriffonEngine(OSystem *syst) : Engine(syst) {
 
 	_dontDrawOver = false;
 
+	_firsty = 0;
+	_lasty = 0;
+
 	for (int i = 0; i < 33; ++i) {
 		for (int j = 0; j < 6; ++j) {
 			_objectInfo[i].nFrames = 0;
