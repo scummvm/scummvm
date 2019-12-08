4ba53aa05891b52f06f00a013944ad26fc7f50c5
Paul Gilbert <dreammaster@scummvm.org>
2019-12-08 07:54:45 -0800

    ULTIMA8: Cleanup and fixes for AudioChannel

--- engines/ultima8/audio/audio_channel.h
+++ engines/ultima8/audio/audio_channel.h
@@ -31,175 +31,80 @@ namespace Pentagram {
 class AudioSample;
 
 class AudioChannel {
+private:
 	// We have:
 	// 1x decompressor size
 	// 2x frame size
+	Common::Array<byte> _playData;
+
 	Audio::SoundHandle _soundHandle;
 	Audio::Mixer *_mixer;
-	uint8           *playdata;          //
-	uint32          playdata_size;
-	uint32          decompressor_size;  // Persistent data for the decompressor
-	uint32          frame_size;         //
+	uint32          _decompressorSize;  // Persistent data for the decompressor
+	uint32          _frameSize;         //
 
-	uint32          sample_rate;
-	bool            stereo;
+	uint32          _sampleRate;
+	bool            _stereo;
 
-	int32           loop;
-	AudioSample     *sample;
+	int32           _loop;
+	AudioSample     *_sample;
 
 	// Info for sampling
-	uint32          frame_evenodd;  // which buffer is 'frame0'
-	uint32          frame0_size;    // Size of the frame0 buffer in samples
-	uint32          frame1_size;    // Size of the frame1 buffer in samples
-	uint32          position;       // Position in frame0 buffer
-	int             lvol, rvol;     // 0-256
-	uint32          pitch_shift;    // 0x10000 = no shift
-	int             priority;       // anything.
-	bool            paused;         // true/false
-
+	uint32          _frameEvenOdd;  // which buffer is 'frame0'
+	int             _lVol, _rVol;   // 0-256
+	uint32          _pitchShift;    // 0x10000 = no shift
+	int             _priority;      // anything.
+	bool            _paused;        // true/false
+private:
+	/**
+	 * Decompresses the next frame of sample data
+	 */
+	void decompressNextFrame();
 public:
-	AudioChannel(Audio::Mixer *mixer, uint32 sample_rate, bool stereo);
+	AudioChannel(Audio::Mixer *mixer, uint32 sampleRate, bool stereo);
 	~AudioChannel(void);
 
-	void stop() {
-		sample = 0;
-	}
+	void stop();
 
-	void playSample(AudioSample *sample, int loop, int priority, bool paused, uint32 pitch_shift, int lvol, int rvol);
-	void resampleAndMix(int16 *stream, uint32 bytes);
+	void playSample(AudioSample *sample, int loop, int priority, bool paused,
+		uint32 pitchShift, int lvol, int rvol);
 
-	bool isPlaying() {
-		return sample != 0;
-	}
+	bool isPlaying();
 
-	void setPitchShift(int pitch_shift_) {
-		pitch_shift = pitch_shift_;
+	void setPitchShift(int pitchShift) {
+		_pitchShift = pitchShift;
 	}
 	uint32 getPitchShift() const {
-		return pitch_shift;
+		return _pitchShift;
 	}
 
-	void setLoop(int loop_) {
-		loop = loop_;
+	void setLoop(int loop) {
+		_loop = loop;
 	}
 	int32 getLoop() const {
-		return loop;
+		return _loop;
 	}
 
-	void setVolume(int lvol_, int rvol_) {
-		lvol = lvol_;
-		rvol = rvol_;
+	void setVolume(int lvol, int rvol) {
+		_lVol = lvol;
+		_rVol = rvol;
 	}
-	void getVolume(int &lvol_, int &rvol_) const {
-		lvol_ = lvol;
-		rvol_ = rvol;
+	void getVolume(int &lvol, int &rvol) const {
+		lvol = lvol;
+		rvol = rvol;
 	}
 
-	void setPriority(int priority_) {
-		priority = priority_;
+	void setPriority(int priority) {
+		_priority = priority;
 	}
 	int getPriority() const {
-		return priority;
+		return _priority;
 	}
 
-	void setPaused(bool paused_) {
-		paused = paused_;
-	}
+	void setPaused(bool paused);
+
 	bool isPaused() const {
-		return paused;
+		return _paused;
 	}
-private:
-
-	//
-	void DecompressNextFrame();
-
-	//
-	// Resampling
-	//
-	class CubicInterpolator {
-	protected:
-		int x0, x1, x2, x3;
-		int a, b, c, d;
-
-	public:
-		CubicInterpolator() : x0(0), x1(0), x2(0), x3(0) {
-			updateCoefficients();
-		}
-
-		CubicInterpolator(int a0, int a1, int a2, int a3) : x0(a0), x1(a1), x2(a2), x3(a3) {
-			updateCoefficients();
-		}
-
-		CubicInterpolator(int a1, int a2, int a3) : x0(2 * a1 - a2), x1(a1), x2(a2), x3(a3) {
-			// We use a simple linear interpolation for x0
-			updateCoefficients();
-		}
-
-		inline void init(int a0, int a1, int a2, int a3) {
-			x0 = a0;
-			x1 = a1;
-			x2 = a2;
-			x3 = a3;
-			updateCoefficients();
-		}
-
-		inline void init(int a1, int a2, int a3) {
-			// We use a simple linear interpolation for x0
-			x0 = 2 * a1 - a2;
-			x1 = a1;
-			x2 = a2;
-			x3 = a3;
-			updateCoefficients();
-		}
-
-		inline void feedData() {
-			x0 = x1;
-			x1 = x2;
-			x2 = x3;
-			x3 = 2 * x2 - x1; // Simple linear interpolation
-			updateCoefficients();
-		}
-
-		inline void feedData(int xNew) {
-			x0 = x1;
-			x1 = x2;
-			x2 = x3;
-			x3 = xNew;
-			updateCoefficients();
-		}
-
-		/* t must be a 16.16 fixed point number between 0 and 1 */
-		inline int interpolate(uint32 fp_pos) {
-			int result = 0;
-			int t = fp_pos >> 8;
-			result = (a * t + b) >> 8;
-			result = (result * t + c) >> 8;
-			result = (result * t + d) >> 8;
-			result = (result / 3 + 1) >> 1;
-
-			return result;
-		}
-
-	protected:
-		inline void updateCoefficients() {
-			a = ((-x0 * 2) + (x1 * 5) - (x2 * 4) + x3);
-			b = ((x0 + x2 - (2 * x1)) * 6) << 8;
-			c = ((-4 * x0) + x1 + (x2 * 4) - x3) << 8;
-			d = (x1 * 6) << 8;
-		}
-	};
-
-	// Resampler stuff
-	CubicInterpolator   interp_l;
-	CubicInterpolator   interp_r;
-	int                 fp_pos;
-	int                 fp_speed;
-
-	void resampleFrameM8toS(int16 *&samples, uint32 &bytes);
-	void resampleFrameM8toM(int16 *&samples, uint32 &bytes);
-
-
-
 };
 
 } // End of namespace Pentagram
