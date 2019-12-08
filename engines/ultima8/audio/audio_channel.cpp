55e638545278267c1b7993502074ea48ee719132
Paul Gilbert <dreammaster@scummvm.org>
2019-12-08 07:54:45 -0800

    ULTIMA8: Cleanup and fixes for AudioChannel

--- engines/ultima8/audio/audio_channel.cpp
+++ engines/ultima8/audio/audio_channel.cpp
@@ -30,7 +30,7 @@
 namespace Ultima8 {
 namespace Pentagram {
 
-// We divide the data by 2, to prevent overshots. Imagine this sample pattern:
+// We divide the data by 2, to prevent overshots. Imagine this _sample pattern:
 // 0, 65535, 65535, 0. Now you want to compute a value between the two 65535.
 // Obviously, it will be *bigger* than 65535 (it can get to about 80,000).
 // It is possibly to clamp it, but that leads to a distored wave form. Compare
@@ -41,299 +41,95 @@ namespace Pentagram {
 // floating point, we approximate that by 27/32
 #define RANGE_REDUX(x)  (((x) * 27) >> 5)
 
-AudioChannel::AudioChannel(Audio::Mixer *mixer, uint32 sample_rate_, bool stereo_) :
-		_mixer(mixer), sample_rate(sample_rate_), stereo(stereo_),
-		playdata(0), playdata_size(0), decompressor_size(0), frame_size(0),
-		loop(0), sample(0),
-		frame_evenodd(0), frame0_size(0), frame1_size(0), position(0), paused(false),
-		fp_pos(0), fp_speed(0) {
+AudioChannel::AudioChannel(Audio::Mixer *mixer, uint32 sampleRate, bool stereo) :
+		_mixer(mixer), _sampleRate(sampleRate), _stereo(stereo),
+		_decompressorSize(0), _frameSize(0), _loop(0), _sample(0), _frameEvenOdd(0),
+		_paused(false) {
 }
 
 AudioChannel::~AudioChannel(void) {
 }
 
-void AudioChannel::playSample(AudioSample *sample_, int loop_, int priority_, bool paused_, uint32 pitch_shift_, int lvol_, int rvol_) {
-	sample = sample_;
-	loop = loop_;
-	priority = priority_;
-	lvol = lvol_;
-	rvol = rvol_;
-	paused = paused_;
-	pitch_shift = pitch_shift_;
+void AudioChannel::playSample(AudioSample *sample, int loop, int priority, bool paused, uint32 pitchShift, int lvol, int rvol) {
+	_sample = sample;
+	_loop = loop;
+	_priority = priority;
+	_lVol = lvol;
+	_rVol = rvol;
+	_paused = paused;
+	_pitchShift = pitchShift;
 
-	if (!sample) return;
+	if (!_sample)
+		return;
 
 	// Setup buffers
-	decompressor_size = sample->getDecompressorDataSize();
-	frame_size = sample->getFrameSize();
+	_decompressorSize = _sample->getDecompressorDataSize();
+	_frameSize = _sample->getFrameSize();
 
-	if ((decompressor_size + frame_size * 2) > playdata_size) {
-		delete [] playdata;
-		playdata_size = decompressor_size + frame_size * 2;
-		playdata = new uint8[playdata_size];
+	if ((_decompressorSize + _frameSize * 2) > _playData.size()) {
+		_playData.resize(_decompressorSize + _frameSize * 2);
 	}
 
-	// Init the sample decompressor
-	sample->initDecompressor(playdata);
+	// Init the _sample decompressor
+	_sample->initDecompressor(&_playData[0]);
 
 	// Reset counter and stuff
-	frame_evenodd = 0;
-	position = 0;
-	fp_pos = 0;
-	fp_speed = (pitch_shift * sample->getRate()) / sample_rate;
+	_frameEvenOdd = 0;
 
-#ifdef TODO
-	// Decompress frame 0
-	frame0_size = sample->decompressFrame(playdata, playdata + decompressor_size);
-
-	// Decompress frame 1
-	DecompressNextFrame();
-#else
-	// Get the data for the sample
+	// Get the data for the _sample
 	Common::MemoryWriteStreamDynamic streamData(DisposeAfterUse::NO);
 	int frameSize;
-	byte *framePtr = playdata + decompressor_size;
+	byte *framePtr = &_playData[_decompressorSize];
 
-	while ((frameSize = sample->decompressFrame(playdata, framePtr)) != 0)
+	while ((frameSize = _sample->decompressFrame(&_playData[0], framePtr)) != 0)
 		streamData.write(framePtr, frameSize);
 
-	// Create the sample
+	// Create the _sample
 	Audio::SeekableAudioStream *audioStream = Audio::makeRawStream(
 		new Common::MemoryReadStream(streamData.getData(), streamData.size(), DisposeAfterUse::YES),
-		sample->getRate(),
-		sample->isStereo() ? Audio::FLAG_STEREO | Audio::FLAG_UNSIGNED : Audio::FLAG_UNSIGNED,
+		_sample->getRate(),
+		_sample->isStereo() ? Audio::FLAG_STEREO | Audio::FLAG_UNSIGNED : Audio::FLAG_UNSIGNED,
 		DisposeAfterUse::YES
 	);
 
-	Audio::AudioStream *stream = !loop_ ? (Audio::AudioStream *)audioStream :
-		new Audio::LoopingAudioStream(audioStream, loop_);
+	Audio::AudioStream *stream = !_loop ? (Audio::AudioStream *)audioStream :
+		new Audio::LoopingAudioStream(audioStream, _loop);
 
 	// Play it
 	_mixer->playStream(Audio::Mixer::kPlainSoundType, &_soundHandle, stream);
-
-#endif
-
-	// Setup resampler
-	if (sample->getBits() == 8 && !sample->isStereo()) {
-		uint8 *src = playdata + decompressor_size;
-		int a = *(src + 0);
-		a = (a | (a << 8)) - 32768;
-		int b = *(src + 1);
-		b = (a | (b << 8)) - 32768;
-		int c = *(src + 2);
-		c = (a | (c << 8)) - 32768;
-
-		interp_l.init(RANGE_REDUX(a), RANGE_REDUX(b), RANGE_REDUX(c));
-	}
-
+	if (paused)
+		_mixer->pauseHandle(_soundHandle, true);
 }
 
-void AudioChannel::resampleAndMix(int16 *stream, uint32 bytes) {
-#ifdef TODO
-	if (!sample || paused) return;
+bool AudioChannel::isPlaying() {
+	if (!_mixer->isSoundHandleActive(_soundHandle))
+		_sample = nullptr;
 
-	// Update fp_speed
-	fp_speed = (pitch_shift * sample->getRate()) / sample_rate;
-
-	// Get and Mix data
-	do {
-		// 8 bit resampling
-		if (sample->getBits() == 8) {
-			// Mono resampling required
-			if (!sample->isStereo() && stereo)
-				resampleFrameM8toS(stream, bytes);
-			if (!sample->isStereo() && !stereo)
-				resampleFrameM8toM(stream, bytes);
-		}
-		// 16 bit resampling (or not)
-		/*
-		else
-		{
-		    uint8 *src = playdata + decompressor_size + (frame_size*frame_evenodd) + position;
-		    uint8 *src_end = src + frame0_size;
-
-		    do {
-		        int c = *(src++);
-		        c = (c|(c << 8))-32768;
-
-		        int lresult = *(stream+0) + c;
-		        int rresult = *(stream+1) + c;
-
-		        // Enforce range in case of an "overshot". Shouldn't happen since we
-		        // scale down already, but safe is safe.
-		        if (lresult < -32768) lresult = -32768;
-		        else if (lresult > 32767) lresult = 32767;
-
-		        if (rresult < -32768) rresult = -32768;
-		        else if (rresult > 32767) rresult = 32767;
-
-		        *stream++ = lresult;
-		        *stream++ = rresult;
-		        bytes -= 4;
-		    } while (bytes!=0 && src != src_end);
-
-		    position = frame0_size - (src_end - src);
-		}
-		*/
-
-		// We ran out of data
-		if (bytes || (position == frame0_size)) {
-
-			// No more data
-			if (!frame1_size) {
-				sample = 0;
-				return;
-			}
-
-			// Invert evenodd
-			frame_evenodd = 1 - frame_evenodd;
-
-			// Set frame1 to be frame0
-			frame0_size = frame1_size;
-			position = 0;
-
-			DecompressNextFrame();
-		}
-
-	} while (bytes != 0);
-#else
-	error("TODO: Unused");
-
-#endif
+	return _sample != nullptr;
 }
 
-// Decompress a frame
-void AudioChannel::DecompressNextFrame() {
+void AudioChannel::stop() {
+	_mixer->stopHandle(_soundHandle);
+	_sample = nullptr;
+}
+
+void AudioChannel::setPaused(bool paused) {
+	_paused = paused;
+	_mixer->pauseHandle(_soundHandle, paused);
+}
+
+void AudioChannel::decompressNextFrame() {
 	// Get next frame of data
-	uint8 *src2 = playdata + decompressor_size + (frame_size * (1 - frame_evenodd));
-	frame1_size = sample->decompressFrame(playdata, src2);
+	uint8 *playData = &_playData[0];
+	uint8 *src2 = playData + _decompressorSize + (_frameSize * (1 - _frameEvenOdd));
+	int frame1Size = _sample->decompressFrame(playData, src2);
 
 	// No stream, go back to beginning and get first frame
-	if (!frame1_size && loop) {
-		if (loop != -1) loop--;
-		sample->rewind(playdata);
-		frame1_size = sample->decompressFrame(playdata, src2);
+	if (!frame1Size && _loop) {
+		if (_loop != -1) _loop--;
+		_sample->rewind(playData);
+		(void)_sample->decompressFrame(playData, src2);
 	}
-}
-
-// Resample a frame of mono 8bit unsigned to Stereo 16bit
-void AudioChannel::resampleFrameM8toS(int16 *&stream, uint32 &bytes) {
-#ifdef TODO
-	uint8 *src = playdata + decompressor_size + (frame_size * frame_evenodd);
-	uint8 *src2 = playdata + decompressor_size + (frame_size * (1 - frame_evenodd));
-
-	uint8 *src_end = src + frame0_size;
-	uint8 *src2_end = src2 + frame1_size;
-
-	src += position;
-
-	int result;
-
-	do {
-		// Add a new src sample (if required)
-		if (fp_pos >= 0x10000) {
-			if (src + 2 < src_end) {
-				int c = *(src + 2);
-				c = (c | (c << 8)) - 32768;
-				interp_l.feedData(RANGE_REDUX(c));
-			} else if (src2 < src2_end) {
-				int c = *(src2);
-				c = (c | (c << 8)) - 32768;
-				interp_l.feedData(RANGE_REDUX(c));
-				src2++;
-			} else {
-				interp_l.feedData();
-			}
-			src++;
-			fp_pos -= 0x10000;
-		}
-
-		if (fp_pos < 0x10000) do {
-				// Do the interpolation
-				result = interp_l.interpolate(fp_pos);
-
-				int lresult = *(stream + 0) + (result * lvol) / 256;
-				int rresult = *(stream + 1) + (result * rvol) / 256;
-
-				// Enforce range in case of an "overshot". Shouldn't happen since we
-				// scale down already, but safe is safe.
-				if (lresult < -32768) lresult = -32768;
-				else if (lresult > 32767) lresult = 32767;
-
-				if (rresult < -32768) rresult = -32768;
-				else if (rresult > 32767) rresult = 32767;
-
-				*stream++ = lresult;
-				*stream++ = rresult;
-				bytes -= 4;
-				fp_pos += fp_speed;
-
-			} while (fp_pos < 0x10000 && bytes != 0);
-
-	} while (bytes != 0 && src != src_end);
-
-	position = frame0_size - (src_end - src);
-#else
-	error("TODO: Unused");
-#endif
-}
-
-// Resample a frame of mono 8bit unsigned to Mono 16bit
-void AudioChannel::resampleFrameM8toM(int16 *&stream, uint32 &bytes) {
-#ifdef TODO
-	uint8 *src = playdata + decompressor_size + (frame_size * frame_evenodd);
-	uint8 *src2 = playdata + decompressor_size + (frame_size * (1 - frame_evenodd));
-
-	uint8 *src_end = src + frame0_size;
-	uint8 *src2_end = src2 + frame1_size;
-
-	src += position;
-
-	int result;
-	int volume = (rvol + lvol) / 2;
-
-	do {
-		// Add a new src sample (if required)
-		if (fp_pos >= 0x10000) {
-			if (src + 2 < src_end) {
-				int c = *(src + 2);
-				c = (c | (c << 8)) - 32768;
-				interp_l.feedData(RANGE_REDUX(c));
-			} else if (src2 < src2_end) {
-				int c = *(src2);
-				c = (c | (c << 8)) - 32768;
-				interp_l.feedData(RANGE_REDUX(c));
-				src2++;
-			} else {
-				interp_l.feedData();
-			}
-			src++;
-			fp_pos -= 0x10000;
-		}
-
-		if (fp_pos < 0x10000) do {
-				// Do the interpolation
-				result = (interp_l.interpolate(fp_pos) * volume) / 256;
-
-				result += *stream;
-
-				// Enforce range in case of an "overshot". Shouldn't happen since we
-				// scale down already, but safe is safe.
-				if (result < -32768) result = -32768;
-				else if (result > 32767) result = 32767;
-
-				*stream++ = result;
-				bytes -= 2;
-				fp_pos += fp_speed;
-
-			} while (fp_pos < 0x10000 && bytes != 0);
-
-	} while (bytes != 0 && src != src_end);
-
-	position = frame0_size - (src_end - src);
-#else
-	error("TODO: unused");
-#endif
 }
 
 } // End of namespace Pentagram
