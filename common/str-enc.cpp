4b0fcd411b8723aea48755dedfeefe5c21952d70
Eugene Sandulenko <sev@scummvm.org>
2019-12-12 23:59:58 +0100

    COMMON: Fix string length in U32String::encode()
    Our method expects length in bytes, not in characters, which is 4x for U32

--- common/str-enc.cpp
+++ common/str-enc.cpp
@@ -395,7 +395,7 @@ String U32String::encode(CodePage page) const {
 	        page >= ARRAYSIZE(g_codePageMap)) {
 		error("Invalid codepage");
 	}
-	char *result = Encoding::convert(g_codePageMap[page], "UTF-32", (const char *)_str, _size);
+	char *result = Encoding::convert(g_codePageMap[page], "UTF-32", (const char *)_str, _size * 4);
 	if (result) {
 		// Encodings in CodePage all use '\0' as string ending
 		// That would be problematic if CodePage has UTF-16 or UTF-32
