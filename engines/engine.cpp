scummvm/scummvm

commit b868400876f06a7f304175e4be39aad46af62946

ENGINES: Disable event polling while splash screen is shown
Co-authored-by: jdgleaver <jdgleaver@users.noreply.github.com>

engines/engine.cpp
@@ -270,7 +270,6 @@ void splashScreen() {
 	uint time0 = g_system->getMillis();
 	Common::Event event;
 	while (time0 + 600 > g_system->getMillis()) {
-		(void)g_system->getEventManager()->pollEvent(event);
 		g_system->delayMillis(10);
 	}
 	g_system->hideOverlay();

commit d17464e86bf0b6780945036fd828b02caa16e04a

ENGINES: We must poll events in order to show the window on some platforms.
This is a regression from b86840087. The side effect of it that on Mac the
splash screen in not visible at all. I hope that this does not trigger same crash,
as it happens now only once.

engines/engine.cpp
@@ -261,6 +261,8 @@ void splashScreen() {
 	// Delay 0.6 secs
 	uint time0 = g_system->getMillis();
 	Common::Event event;
+	g_system->getEventManager()->pollEvent(event);
+
 	while (time0 + 600 > g_system->getMillis()) {
 		g_system->delayMillis(10);
 	}