9579e5fc152362475ac269176ffd910f97bd5f67
Paul Gilbert <dreammaster@scummvm.org>
2020-01-18 17:45:18 -0800

    ULTIMA1: Stub classes for monsters

---
+++ engines/ultima/shared/core/monsters.h
@@ -0,0 +1,67 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+#ifndef ULTIMA_SHARED_CORE_MONSTERS_H
+#define ULTIMA_SHARED_CORE_MONSTERS_H
+
+#include "ultima/shared/core/map.h"
+
+namespace Ultima {
+namespace Shared {
+
+class Monster : public MapWidget {
+protected:
+	int _hitPoints;
+public:
+	/**
+	 * Constructor
+	 */
+	Monster(Game *game, Map *map, int hitPoints) : MapWidget(game, map), _hitPoints(hitPoints) {}
+
+	/**
+	 * Destructor
+	 */
+	virtual ~Monster() {}
+};
+
+class DungeonMonster : public Monster {
+public:
+	/**
+	 * Constructor
+	 */
+	DungeonMonster(Game *game, Map *map, int hitPoints) : Monster(game, map, hitPoints) {}
+
+	/**
+	 * Destructor
+	 */
+	virtual ~DungeonMonster() {}
+
+	/**
+	 * Returns true if a monster blocks the background behind him
+	 */
+	virtual bool isBlockedView() const = 0;
+};
+
+} // End of namespace Ultima1
+} // End of namespace Ultima
+
+#endif
---
+++ engines/ultima/ultima1/core/monsters.h
@@ -0,0 +1,65 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+#ifndef ULTIMA_ULTIMA1_CORE_MONSTERS_H
+#define ULTIMA_ULTIMA1_CORE_MONSTERS_H
+
+#include "ultima/shared/core/monsters.h"
+
+namespace Ultima {
+namespace Ultima1 {
+
+enum DungeonMonsterId {
+	MONSTER_NONE = -1, MONSTER_RANGER = 0, MONSTER_SKELETON = 1, MONSTER_THIEF = 2, MONSTER_GIANT_RAT = 3,
+	MONSTER_RAT = 4, MONSTER_SPIDER = 5, MONSTER_VIPER = 6, MONSTER_ORC = 7, MONSTER_CYCLOPS = 8,
+	MONSTER_GELATINOUS_CUBE = 9, MONSTER_ETTIN = 10, MONSTER_MIMIC = 11, MONSTER_LIZARD_MAN = 12,
+	MONSTER_MINOTAUR = 13, MONSTER_CARRION_CREEPER = 14, MONSTER_TANGLER = 15, MONSTER_GREMLIN = 16,
+	MONSTER_WANDERING_EYES = 17, MONSTER_WRAITH = 18, MONSTER_LICH = 19, MONSTER_INVISIBLE_SEEKER = 20,
+	MONSTER_WIND_WHIPPER = 21, MONSTER_ZORN = 22, MONSTER_DAEMON = 23, MONSTER_BALRON = 24, UITEM_COFFIN = 25,
+	UITEM_26 = 26, UITEM_27 = 27
+};
+
+class U1DungeonMonster : public Shared::DungeonMonster {
+private:
+	DungeonMonsterId _monsterId;
+public:
+	/**
+	 * Constructor
+	 */
+	U1DungeonMonster(Shared::Game *game, Shared::Map *map, int hitPoints) : Shared::DungeonMonster(game, map, hitPoints) {}
+
+	/**
+	 * Destructor
+	 */
+	virtual ~U1DungeonMonster() {}
+
+	/**
+	 * Returns true if a monster blocks the background behind him
+	 */
+	virtual bool isBlockedView() const;
+};
+
+
+} // End of namespace Ultima1
+} // End of namespace Ultima
+
+#endif
