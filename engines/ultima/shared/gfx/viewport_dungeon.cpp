c14535b1ae4e5a4aefe215978424cb6ed2ca08de
Paul Gilbert <dreammaster@scummvm.org>
2020-01-18 17:51:01 -0800

    ULTIMA1: Beginnings of dungeon drawing support methods

--- engines/ultima/shared/gfx/viewport_dungeon.cpp
+++ engines/ultima/shared/gfx/viewport_dungeon.cpp
@@ -21,11 +21,74 @@
  */
 
 #include "ultima/shared/gfx/viewport_dungeon.h"
+#include "ultima/shared/core/monsters.h"
+#include "ultima/shared/core/map.h"
 
 namespace Ultima {
 namespace Shared {
 
 EMPTY_MESSAGE_MAP(ViewportDungeon, Shared::Gfx::VisualItem);
+
+void ViewportDungeon::draw() {
+	// Get a surface reference and clear it's contents
+	Gfx::VisualSurface s = getSurface();
+	s.clear();
+
+	// Get the position delta for the cells to the left and right of the player's position
+	Map *map = getMap();
+	Point rightDelta, leftDelta, delta;
+	switch (map->_direction) {
+	case DIR_LEFT:
+		leftDelta.y = 1;
+		rightDelta.y = -1;
+		delta.x = -1;
+		break;
+	case DIR_RIGHT:
+		leftDelta.y = -1;
+		rightDelta.y = 1;
+		delta.x = 1;
+		break;
+	case DIR_UP:
+		leftDelta.x = -1;
+		rightDelta.x = 1;
+		delta.y = -1;
+		break;
+	case DIR_DOWN:
+		leftDelta.x = 1;
+		rightDelta.x = -1;
+		delta.y = 1;
+		break;
+	}
+
+	bool isDoor = map->isDoor(map->getPosition());
+	bool isWall = map->isWallOrSecretDoor(map->getPosition());
+	int distance = distanceToOccupiedCell(delta);
+
+}
+
+int ViewportDungeon::distanceToOccupiedCell(const Point &delta) {
+	Point d = delta;
+	int distance;
+	for (distance = 1; !isCellOccupied(d); ++distance, d.x += ABS(d.x), d.y += ABS(d.y)) {}
+
+	return MIN(distance, 5);
+}
+
+bool ViewportDungeon::isCellOccupied(const Point &delta) {
+	Map *map = getMap();
+	Point pt = map->getPosition() + delta;
+	if (map->isWallOrDoorway(pt))
+		return true;
+
+	return isMonsterBlocking(pt);
+}
+
+bool ViewportDungeon::isMonsterBlocking(const Point &pt) {
+	MapTile tile;
+	getMap()->getTileAt(pt, &tile);
+	DungeonMonster *monster = dynamic_cast<DungeonMonster *>(tile._widget);
+	return monster != nullptr && monster->isBlockingView();
+}
 
 } // End of namespace Shared
 } // End of namespace Ultima
