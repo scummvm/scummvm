12a742c3922f33bd6bb7ea005fc3b1235e39dac2
Paul Gilbert <dreammaster@scummvm.org>
2020-01-19 15:08:17 -0800

    ULTIMA1: Beginnings of popup dialogs display

---
+++ engines/ultima/ultima1/u1dialogs/grocer.cpp
@@ -0,0 +1,52 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+#include "ultima/ultima1/u1dialogs/grocer.h"
+#include "ultima/shared/engine/messages.h"
+
+namespace Ultima {
+namespace Ultima1 {
+namespace U1Dialogs {
+
+BEGIN_MESSAGE_MAP(Grocer, BuySellDialog)
+	ON_MESSAGE(KeypressMsg)
+END_MESSAGE_MAP()
+
+Grocer::Grocer(Shared::GameBase *game, BuySell buySell, int groceryNum) :
+		BuySellDialog(game, buySell, ""),
+		_groceryNum(groceryNum) {	
+}
+
+void Grocer::draw() {
+	BuySellDialog::draw();
+
+	// TODO
+}
+
+bool Grocer::KeypressMsg(CKeypressMsg &msg) {
+	// TODO
+	return true;
+}
+
+} // End of namespace U1Dialogs
+} // End of namespace Ultima1
+} // End of namespace Ultima
