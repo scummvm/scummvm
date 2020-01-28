b4022f641ade960a7867cde4bb494e04987684d3
Paul Gilbert <dreammaster@scummvm.org>
2020-01-27 19:32:31 -0800

    DISTS: Remove --build-events from create_msvc to fix VS compilation

--- dists/msvc/create_msvc.bat
+++ dists/msvc/create_msvc.bat
@@ -55,7 +55,7 @@ goto done
 echo.
 echo Creating project files with all engines enabled (stable and unstable)
 echo.
-create_project ..\.. --enable-all-engines --disable-fluidsynth --msvc --build-events
+create_project ..\.. --enable-all-engines --disable-fluidsynth --msvc
 goto done
 
 :stable
