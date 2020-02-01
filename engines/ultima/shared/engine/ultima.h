4e47a3fae55ba15ea611a4ee143f33ca248e3592
Eugene Sandulenko <sev@scummvm.org>
2020-02-01 23:30:27 +0100

    JANITORIAL: Whitespace fixes

--- engines/ultima/shared/engine/ultima.h
+++ engines/ultima/shared/engine/ultima.h
@@ -34,19 +34,19 @@ namespace Ultima {
 namespace Shared {
 
 enum UltimaDebugChannels {
-    kDebugPath = 1 << 0,
-    kDebugGraphics = 1 << 1
+	kDebugPath = 1 << 0,
+	kDebugGraphics = 1 << 1
 };
 
 class UltimaEngine : public Engine, public EventsCallback {
 private:
-    Common::RandomSource _randomSource;
+	Common::RandomSource _randomSource;
 protected:
 	const UltimaGameDescription *_gameDescription;
 	Common::Archive *_dataArchive;
 	Debugger *_debugger;
 protected:
-    /**
+	/**
 	 * Initializes needed data for the engine
 	 */
 	virtual bool initialize();
@@ -56,61 +56,61 @@ protected:
 	 */
 	virtual void deinitialize() {}
 
-    /**
-     * Returns the data archive folder and version that's required
-     */
-    virtual bool isDataRequired(Common::String &folder, int &majorVersion, int &minorVersion) {
-        return false;
-    }
+	/**
+	 * Returns the data archive folder and version that's required
+	 */
+	virtual bool isDataRequired(Common::String &folder, int &majorVersion, int &minorVersion) {
+		return false;
+	}
 
-    /**
-     * Returns the Ultima meta engine
-     */
-    UltimaMetaEngine *getMetaEngine() const;
+	/**
+	 * Returns the Ultima meta engine
+	 */
+	UltimaMetaEngine *getMetaEngine() const;
 public:
 	EventsManager *_events;
 public:
 	UltimaEngine(OSystem *syst, const Ultima::UltimaGameDescription *gameDesc);
 	~UltimaEngine();
 
-    /**
-     * Returns supported engine features
-     */
-    virtual bool hasFeature(EngineFeature f) const;
+	/**
+	 * Returns supported engine features
+	 */
+	virtual bool hasFeature(EngineFeature f) const;
 
-    /**
-     * Returns game features
-     */
-    uint32 getFeatures() const;
+	/**
+	 * Returns game features
+	 */
+	uint32 getFeatures() const;
 
 	/**
 	 * Return the game's language
 	 */
 	Common::Language getLanguage() const;
 
-    /**
-     * Returns the game type being played
-     */
+	/**
+	 * Returns the game type being played
+	 */
 	GameId getGameId() const;
 
-    /**
-     * Returns true if the game is running an enhanced version
-     * as compared to the original game
-     */
-    bool isEnhanced() const {
+	/**
+	 * Returns true if the game is running an enhanced version
+	 * as compared to the original game
+	 */
+	bool isEnhanced() const {
 		return getFeatures() & GF_VGA_ENHANCED;
-    }
+	}
 
-    /**
-     * Returns the filename for a savegame given it's slot
-     */
-    Common::String getSaveFilename(int slotNumber) {
-        return getMetaEngine()->getSavegameFile(slotNumber, _targetName.c_str());
-    }
+	/**
+	 * Returns the filename for a savegame given it's slot
+	 */
+	Common::String getSaveFilename(int slotNumber) {
+		return getMetaEngine()->getSavegameFile(slotNumber, _targetName.c_str());
+	}
 
-    /**
-     * Show a messae in a GUI dialog
-     */
+	/**
+	 * Show a messae in a GUI dialog
+	 */
 	void GUIError(const Common::String &msg);
 
 	/**
@@ -125,12 +125,12 @@ public:
 		return min + _randomSource.getRandomNumber(max - min);
 	}
 
-    /**
-     * Get a reference to the data archive
-     */
-    Common::Archive *getDataArchive() const {
-        return _dataArchive;
-    }
+	/**
+	 * Get a reference to the data archive
+	 */
+	Common::Archive *getDataArchive() const {
+		return _dataArchive;
+	}
 
 	/**
 	 * Checks if an auto save should be done, and if so, takes care of it
@@ -144,66 +144,66 @@ public:
 		return _debugger;
 	}
 
-    /**
-     * Returns a file system node for the game directory
-     */
-    Common::FSNode getGameDirectory() const;
+	/**
+	 * Returns a file system node for the game directory
+	 */
+	Common::FSNode getGameDirectory() const;
 
-    /**
-     * Shows the ScummVM save dialog, allowing users to save their game
-     */
-    virtual bool saveGame();
+	/**
+	 * Shows the ScummVM save dialog, allowing users to save their game
+	 */
+	virtual bool saveGame();
 
-    /**
-     * Shows the ScummVM Restore dialog, allowing users to restore a game
-     */
-    virtual bool loadGame();
+	/**
+	 * Shows the ScummVM Restore dialog, allowing users to restore a game
+	 */
+	virtual bool loadGame();
 
-    /**
-     * Indicates whether a game state can be loaded.
-     * @param isAutosave	Flags whether it's an autosave check
-     */
-    virtual bool canLoadGameStateCurrently(bool isAutosave) = 0;
+	/**
+	 * Indicates whether a game state can be loaded.
+	 * @param isAutosave	Flags whether it's an autosave check
+	 */
+	virtual bool canLoadGameStateCurrently(bool isAutosave) = 0;
 
-    /**
-     * Indicates whether a game state can be loaded.
-     */
-    virtual bool canLoadGameStateCurrently() override {
-        return canLoadGameStateCurrently(false);
-    }
+	/**
+	 * Indicates whether a game state can be loaded.
+	 */
+	virtual bool canLoadGameStateCurrently() override {
+		return canLoadGameStateCurrently(false);
+	}
 
-    /**
-     * Indicates whether a game state can be saved.
-     * @param isAutosave	Flags whether it's an autosave check
-     */
-    virtual bool canSaveGameStateCurrently(bool isAutosave) = 0;
+	/**
+	 * Indicates whether a game state can be saved.
+	 * @param isAutosave	Flags whether it's an autosave check
+	 */
+	virtual bool canSaveGameStateCurrently(bool isAutosave) = 0;
 
-    /**
-     * Indicates whether a game state can be saved.
-     */
-    virtual bool canSaveGameStateCurrently() override {
-        return canSaveGameStateCurrently(false);
-    }
+	/**
+	 * Indicates whether a game state can be saved.
+	 */
+	virtual bool canSaveGameStateCurrently() override {
+		return canSaveGameStateCurrently(false);
+	}
 
-    /**
-     * Save a game state.
-     * @param slot	the slot into which the savestate should be stored
-     * @param desc	a description for the savestate, entered by the user
-     * @param isAutosave If true, autosave is being created
-     * @return returns kNoError on success, else an error code.
-     */
-    virtual Common::Error saveGameState(int slot, const Common::String &desc) override {
-        return saveGameState(slot, desc, false);
-    }
+	/**
+	 * Save a game state.
+	 * @param slot	the slot into which the savestate should be stored
+	 * @param desc	a description for the savestate, entered by the user
+	 * @param isAutosave If true, autosave is being created
+	 * @return returns kNoError on success, else an error code.
+	 */
+	virtual Common::Error saveGameState(int slot, const Common::String &desc) override {
+		return saveGameState(slot, desc, false);
+	}
 
-    /**
-     * Save a game state.
-     * @param slot	the slot into which the savestate should be stored
-     * @param desc	a description for the savestate, entered by the user
-     * @param isAutosave If true, autosave is being created
-     * @return returns kNoError on success, else an error code.
-     */
-    virtual Common::Error saveGameState(int slot, const Common::String &desc, bool isAutosave) = 0;
+	/**
+	 * Save a game state.
+	 * @param slot	the slot into which the savestate should be stored
+	 * @param desc	a description for the savestate, entered by the user
+	 * @param isAutosave If true, autosave is being created
+	 * @return returns kNoError on success, else an error code.
+	 */
+	virtual Common::Error saveGameState(int slot, const Common::String &desc, bool isAutosave) = 0;
 };
 
 extern UltimaEngine *g_ultima;
