de24fd743b492fce7e4931215aa3c108b8abb334
Eugene Sandulenko <sev@scummvm.org>
2020-02-05 15:30:39 +0100

    HUGO: Added override keywords

--- engines/hugo/hugo.h
+++ engines/hugo/hugo.h
@@ -239,7 +239,7 @@ public:
 	Maze      _maze;                                // Maze control structure
 	hugoBoot  _boot;                                // Boot info structure
 
-	GUI::Debugger *getDebugger();
+	GUI::Debugger *getDebugger() override;
 
 	Common::RandomSource *_rnd;
 
@@ -263,8 +263,8 @@ public:
 		return *s_Engine;
 	}
 
-	virtual bool canLoadGameStateCurrently();
-	virtual bool canSaveGameStateCurrently();
+	virtual bool canLoadGameStateCurrently() override;
+	virtual bool canSaveGameStateCurrently() override;
 	bool loadHugoDat();
 
 	int8 getTPS() const;
@@ -277,7 +277,7 @@ public:
 	void readScreenFiles(const int screen);
 	void setNewScreen(const int screen);
 	void shutdown();
-	void syncSoundSettings();
+	void syncSoundSettings() override;
 
 	Status &getGameStatus();
 	int getScore() const;
@@ -285,8 +285,8 @@ public:
 	void adjustScore(const int adjustment);
 	int getMaxScore() const;
 	void setMaxScore(const int newScore);
-	Common::Error saveGameState(int slot, const Common::String &desc);
-	Common::Error loadGameState(int slot);
+	Common::Error saveGameState(int slot, const Common::String &desc) override;
+	Common::Error loadGameState(int slot) override;
 	bool hasFeature(EngineFeature f) const override;
 	const char *getCopyrightString() const;
 
@@ -309,7 +309,7 @@ public:
 protected:
 
 	// Engine APIs
-	Common::Error run();
+	Common::Error run() override;
 
 private:
 	static const int kTurboTps = 16;                // This many in turbo mode
