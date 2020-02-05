fbaaba896096c7a609ee40692516ebc7d125313d
Eugene Sandulenko <sev@scummvm.org>
2020-02-05 14:50:20 +0100

    CGE: Added override keywords

--- engines/cge/cge.h
+++ engines/cge/cge.h
@@ -138,10 +138,10 @@ public:
 	CGEEngine(OSystem *syst, const ADGameDescription *gameDescription);
 	~CGEEngine();
 	virtual bool hasFeature(EngineFeature f) const override;
-	virtual bool canLoadGameStateCurrently();
-	virtual bool canSaveGameStateCurrently();
-	virtual Common::Error loadGameState(int slot);
-	virtual Common::Error saveGameState(int slot, const Common::String &desc);
+	virtual bool canLoadGameStateCurrently() override;
+	virtual bool canSaveGameStateCurrently() override;
+	virtual Common::Error loadGameState(int slot) override;
+	virtual Common::Error saveGameState(int slot, const Common::String &desc) override;
 
 	static const int _maxSceneArr[5];
 	bool _quitFlag;
@@ -205,8 +205,8 @@ public:
 	BitmapPtr *_miniShpList;
 	int        _startGameSlot;
 
-	virtual Common::Error run();
-	GUI::Debugger *getDebugger() {
+	virtual Common::Error run() override;
+	GUI::Debugger *getDebugger() override {
 		return _console;
 	}
 
