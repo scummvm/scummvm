43d17fba98db0d5206b9296995d4f680398b6345
Eugene Sandulenko <sev@scummvm.org>
2020-02-07 17:48:59 +0100

    DRAGONS: Whitespace fixes

--- engines/dragons/module.mk
+++ engines/dragons/module.mk
@@ -1,21 +1,21 @@
 MODULE := engines/dragons
- 
+
 MODULE_OBJS := \
-    actor.o \
-    actorresource.o \
-    background.o \
-    bag.o \
-    bigfile.o \
-    credits.o \
-    cursor.o \
-    cutscene.o \
+	actor.o \
+	actorresource.o \
+	background.o \
+	bag.o \
+	bigfile.o \
+	credits.o \
+	cursor.o \
+	cutscene.o \
 	detection.o \
-    dragonflg.o \
-    dragonimg.o \
-    dragonini.o \
-    dragonobd.o \
-    dragonrms.o \
-    dragonvar.o \
+	dragonflg.o \
+	dragonimg.o \
+	dragonini.o \
+	dragonobd.o \
+	dragonrms.o \
+	dragonvar.o \
 	dragons.o \
 	font.o \
 	inventory.o \
@@ -36,11 +36,11 @@ MODULE_OBJS := \
 
 MODULE_DIRS += \
 	engines/dragons
- 
+
 # This module can be built as a plugin
 ifeq ($(ENABLE_QUUX), DYNAMIC_PLUGIN)
 PLUGIN := 1
 endif
- 
-# Include common rules 
+
+# Include common rules
 include $(srcdir)/rules.mk
