fa9b0e9983b7e4071e57419017e7b14e5891e4e7
Eugene Sandulenko <sev@scummvm.org>
2020-02-07 19:20:56 +0100

    DIRECTOR: Proper check for script presense in the movie text

--- engines/director/score.cpp
+++ engines/director/score.cpp
@@ -1053,6 +1053,13 @@ void Score::loadScriptText(Common::SeekableSubReadStreamEndian &stream) {
 	if (script.empty() || !script.hasPrefix("--"))
 		return;
 
+	int pos = 2;
+	while (script[pos] == ' ' || script[pos] == '\t')
+		pos++;
+
+	if (script[pos] != '\n')
+		return;
+
 	if (ConfMan.getBool("dump_scripts"))
 		dumpScript(script.c_str(), kMovieScript, _movieScriptCount);
 
