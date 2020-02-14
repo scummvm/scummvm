ac06a679f9cf8dedf196a4aaacc64eeed0b2174e
BeWorld <36823759+BeWorld2018@users.noreply.github.com>
2020-02-14 09:29:54 +0100

    MORPHOS: Update sdl.cpp
    SDL_iconv doesnt work with MorphOS' SDL 1.2

--- backends/platform/sdl/sdl.cpp
+++ backends/platform/sdl/sdl.cpp
@@ -739,7 +739,7 @@ void OSystem_SDL::setupGraphicsModes() {
 #endif
 
 char *OSystem_SDL::convertEncoding(const char *to, const char *from, const char *string, size_t length) {
-#if SDL_VERSION_ATLEAST(1, 2, 10)
+#if SDL_VERSION_ATLEAST(1, 2, 10) && !defined(__MORPHOS__)
 	int zeroBytes = 1;
 	if (Common::String(from).hasPrefixIgnoreCase("utf-16"))
 		zeroBytes = 2;
