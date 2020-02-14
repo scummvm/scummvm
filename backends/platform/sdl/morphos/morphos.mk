638e494f27ee42d5d918a0adeb29214375197f30
BeWorld <36823759+BeWorld2018@users.noreply.github.com>
2020-02-14 10:06:02 +0100

    Update morphos.mk
    Update with AmigaOS

--- backends/platform/sdl/morphos/morphos.mk
+++ backends/platform/sdl/morphos/morphos.mk
@@ -1,24 +1,48 @@
-# Special target to create an MorphOS snapshot installation
-morphosdist: $(EXECUTABLE)
+# Special target to create an MorphOS snapshot installation.
+# AmigaOS shell doesn't like indented comments.
+morphosdist: $(EXECUTABLE) $(PLUGINS)
 	mkdir -p $(MORPHOSPATH)
-	mkdir -p $(MORPHOSPATH)themes
-	mkdir -p $(MORPHOSPATH)extras
-	$(STRIP) $(EXECUTABLE) -o $(MORPHOSPATH)$(EXECUTABLE)
-	cp ${srcdir}/icons/scummvm_drawer.info $(MORPHOSPATH).info
-	cp ${srcdir}/icons/scummvm.info $(MORPHOSPATH)$(EXECUTABLE).info
-	cp $(DIST_FILES_THEMES) $(MORPHOSPATH)themes/
-ifdef DIST_FILES_ENGINEDATA
-	cp $(DIST_FILES_ENGINEDATA) $(MORPHOSPATH)extras/
-endif
-	cat ${srcdir}/README | sed -f ${srcdir}/dists/amiga/convertRM.sed > README.conv
-# MorphOS's shell is not happy with indented comments, thus don't do it.
-# AREXX seems to have problems when ${srcdir} is '.'. It will break with a
-# "Program not found" error. Therefore we copy the script to the cwd and
-# remove it again, once it has finished.
-	cp ${srcdir}/dists/amiga/RM2AG.rx .
-	rx RM2AG.rexx README.conv
-	cp README.guide $(MORPHOSPATH)
-	rm RM2AG.rexx
+	mkdir -p $(MORPHOSPATH)/extras
+	cp ${srcdir}/dists/amiga/scummvm_drawer.info $(MORPHOSPATH).info
+	cp ${srcdir}/dists/amiga/scummvm.info $(MORPHOSPATH)/$(EXECUTABLE).info
+# Copy mandatory installation files.
+# Prepare README.md for AmigaGuide conversion.
+	cat ${srcdir}/README.md | sed -f ${srcdir}/dists/amiga/convertRM.sed > README.conv
+# AmigaOS AREXX has a problem when ${srcdir} is '.'.
+# It will break with a "Program not found" error.
+# Copy the script to cwd and, once it has finished, remove it.
+	cp ${srcdir}/dists/amiga/RM2AG.rexx .
+	rx RM2AG.rexx README.conv $(MORPHOSPATH)
 	rm README.conv
-	rm README.guide
-	cp $(DIST_FILES_DOCS) $(MORPHOSPATH)
+	rm RM2AG.rexx
+ifdef DIST_FILES_DOCS
+	mkdir -p $(MORPHOSPATH)/doc
+	cp -r $(srcdir)/doc/ $(MORPHOSPATH)
+	cp $(DIST_FILES_DOCS) $(MORPHOSPATH)/doc/
+endif
+ifdef DIST_FILES_ENGINEDATA
+	cp $(DIST_FILES_ENGINEDATA) $(MORPHOSPATH)/extras/
+endif
+ifdef DIST_FILES_NETWORKING
+	cp $(DIST_FILES_NETWORKING) $(MORPHOSPATH)/extras/
+endif
+ifdef DIST_FILES_VKEYBD
+	cp $(DIST_FILES_VKEYBD) $(MORPHOSPATH)/extras/
+endif
+ifdef DIST_FILES_THEMES
+	mkdir -p $(MORPHOSPATH)/themes
+	cp $(DIST_FILES_THEMES) $(MORPHOSPATH)/themes/
+endif
+# Strip and copy engine plugins.
+ifdef DYNAMIC_MODULES
+	mkdir -p $(MORPHOSPATH)/plugins
+	$(foreach plugin, $(PLUGINS), $(STRIP) $(plugin) -o $(MORPHOSPATH)/$(plugin);)
+# Extract and install compiled-in shared libraries.
+# Not every AmigaOS installation, especially vanilla ones,
+# come with every mandatory shared library.
+	mkdir -p $(MORPHOSPATH)/sobjs
+	cp ${srcdir}/dists/amiga/Ext_Inst_so.rexx .
+	rx Ext_Inst_so.rexx $(EXECUTABLE) $(MORPHOSPATH)
+	rm Ext_Inst_so.rexx
+endif
+	$(STRIP) $(EXECUTABLE) -o $(AMIGAOSPATH)/$(EXECUTABLE)
