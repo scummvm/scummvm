5a5aba9504c9235f976694e020130d5d968373ff
Paul Gilbert <dreammaster@scummvm.org>
2020-02-29 09:29:04 -0800

    ULTIMA8: Fix startup when lastSave points to a save no longer present

--- engines/ultima/ultima8/ultima8.cpp
+++ engines/ultima/ultima8/ultima8.cpp
@@ -1185,6 +1185,13 @@ void Ultima8Engine::setupCoreGumps() {
 bool Ultima8Engine::newGame(int saveSlot) {
 	debugN(MM_INFO, "Starting New Game...\n");
 
+	// First validate we still have a save file for the slot
+	if (saveSlot != -1) {
+		SaveStateDescriptor desc = getMetaEngine().querySaveMetaInfos(_targetName.c_str(), saveSlot);
+		if (desc.getSaveSlot() != saveSlot)
+			saveSlot = -1;
+	}
+
 	resetEngine();
 
 	setupCoreGumps();
