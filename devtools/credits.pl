a6fbb6817c5ef72a2799d782f5e6c0aeb588ebfe
Paul Gilbert <dreammaster@scummvm.org>
2020-03-08 15:27:39 -0700

    CREDITS: Add all the other listed authors for Nuvie and Pentagram

--- devtools/credits.pl
+++ devtools/credits.pl
@@ -1017,13 +1017,27 @@ begin_credits("Credits");
 			begin_section("Ultima");
 				add_person("Paul Gilbert", "dreammaster", "");
 				add_person("Matthew Duggan", "stauff", "");
+
+				add_person("Daniel c. Würl", "dwuerl", "(Nuvie)");
 				add_person("Eric Fry", "yuv422", "(Nuvie)");
-				add_person("", "malignantmanor", "(Nuvie)");
-				add_person("", "sb-x", "(Nuvie)");
+				add_person("Jeremy Newman", "laxdragon", "(Nuvie)");
+				add_person("Jonathan E. Wright", "nelno", "(Nuvie)");
+				add_person("Joseph Applegate", "sb-x", "(Nuvie)");
+				add_person("Malignant Manor", "malignantmanor", "(Nuvie)");
+				add_person("Markus Niemistö", "niemisto", "(Nuvie)");
+				add_person("Michael Fink", "vividos", "(Nuvie)");
+				add_person("Pieter Luteijn", "luteijn", "(Nuvie)");
+				add_person("Sam Matthews", "samuelmatthews", "(Nuvie)");
+				add_person("Travis Howell", "kirben", "(Nuvie)");
+				add_person("Willem Jan Palenstijn", "wjp", "(Nuvie)");
+
+				add_person("Brian Tietz", "btietz", "(Pentagram)");
 				add_person("Dominik Reichardt", "Dominus Dragon", "(Pentagram)");
+				add_person("Matthew Jimenez", "mjimenez", "(Pentagram)");
+				add_person("Max Horn", "Fingolfin", "(Pentagram)");
+				add_person("Patrick Burke", "takhisis", "(Pentagram)");
+				add_person("Ryan Nunn", "Colourless Dragon", "(Pentagram)");
 				add_person("Willem Jan Palenstijn", "wjp", "(Pentagram)");
-				add_person("", "Colourless Dragon", "(Pentagram)");
-				add_person("", "mjimenez", "(Pentagram)");
 			end_section();
 
 			begin_section("Voyeur");
