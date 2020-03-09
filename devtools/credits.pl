cf9b97950dcb578911bc462805776dfbf33c7ece
Paul Gilbert <dreammaster@scummvm.org>
2020-03-08 19:30:35 -0700

    CREDITS: Reintroduce accent characters for past Nuvie developers

--- devtools/credits.pl
+++ devtools/credits.pl
@@ -1018,13 +1018,13 @@ begin_credits("Credits");
 				add_person("Paul Gilbert", "dreammaster", "");
 				add_person("Matthew Duggan", "stauff", "");
 
-				add_person("Daniel c. Wurl", "dwuerl", "(Nuvie)");
+				add_person("Daniel c. W&uuml:rl", "dwuerl", "(Nuvie)");
 				add_person("Eric Fry", "yuv422", "(Nuvie)");
 				add_person("Jeremy Newman", "laxdragon", "(Nuvie)");
 				add_person("Jonathan E. Wright", "nelno", "(Nuvie)");
 				add_person("Joseph Applegate", "sb-x", "(Nuvie)");
 				add_person("Malignant Manor", "malignantmanor", "(Nuvie)");
-				add_person("Markus Niemisto", "niemisto", "(Nuvie)");
+				add_person("Markus Niemist&ouml;", "niemisto", "(Nuvie)");
 				add_person("Michael Fink", "vividos", "(Nuvie)");
 				add_person("Pieter Luteijn", "luteijn", "(Nuvie)");
 				add_person("Sam Matthews", "samuelmatthews", "(Nuvie)");
