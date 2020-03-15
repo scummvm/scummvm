9a7dac41a76429300333093e143bf233d060b8ed
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2020-03-15 14:36:19 +0100

    PARALLACTION: Debugger, call base preEnter & postEnter

--- engines/parallaction/debug.cpp
+++ engines/parallaction/debug.cpp
@@ -51,12 +51,12 @@ Debugger::Debugger(Parallaction *vm)
 
 void Debugger::preEnter() {
 	_mouseState = _vm->_input->getMouseState();
-	_vm->pauseEngine(true);
+	GUI::Debugger::preEnter();
 }
 
 
 void Debugger::postEnter() {
-	_vm->pauseEngine(false);
+	GUI::Debugger::postEnter();
 	_vm->_input->setMouseState(_mouseState);
 	_vm->_input->setArrowCursor();	// unselects the active item, if any
 }
