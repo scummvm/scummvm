12e4eab5de6975ddd257fe9f1792525d96ff6f85
djsrv <dservilla@gmail.com>
2020-03-26 23:21:51 -0400

    DIRECTOR: LINGO: Fix Datum::toString() for arrays
    Previously it overrwrote array contents with strings, breaking arrays
    whenever they were printed.

--- engines/director/lingo/lingo.cpp
+++ engines/director/lingo/lingo.cpp
@@ -518,7 +518,8 @@ Common::String *Datum::toString() {
 		for (uint i = 0; i < u.farr->size(); i++) {
 			if (i > 0)
 				*s += ", ";
-			*s += *u.farr->operator[](i).toString();
+			Datum d = u.farr->operator[](i);
+			*s += *d.toString();
 		}
 
 		*s += "]";
