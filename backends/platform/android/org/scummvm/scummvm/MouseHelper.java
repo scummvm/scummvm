46e446a7708ea02599d094f9dbee2e9bfac4c313
Cameron Cawley <ccawley2011@gmail.com>
2020-04-01 21:00:21 +0100

    ANDROID: Add support for additional mouse buttons

--- backends/platform/android/org/scummvm/scummvm/MouseHelper.java
+++ backends/platform/android/org/scummvm/scummvm/MouseHelper.java
@@ -15,6 +15,10 @@ public class MouseHelper {
 	private boolean _rmbPressed;
 	private boolean _lmbPressed;
 	private boolean _mmbPressed;
+	private boolean _bmbPressed;
+	private boolean _fmbPressed;
+	private boolean _srmbPressed;
+	private boolean _smmbPressed;
 
 	/**
 	 * Class initialization fails when this throws an exception.
@@ -77,6 +81,25 @@ public class MouseHelper {
 		       ((sources & InputDevice.SOURCE_TOUCHPAD) == InputDevice.SOURCE_TOUCHPAD);
 	}
 
+	private boolean handleButton(MotionEvent e, boolean mbPressed, int mask, int downEvent, int upEvent) {
+		boolean mbDown = (e.getButtonState() & mask) == mask;
+		if (mbDown) {
+			if (!mbPressed) {
+				// left mouse button was pressed just now
+				_scummvm.pushEvent(downEvent, (int)e.getX(), (int)e.getY(), e.getButtonState(), 0, 0, 0);
+			}
+
+			return true;
+		} else {
+			if (mbPressed) {
+				// left mouse button was released just now
+				_scummvm.pushEvent(upEvent, (int)e.getX(), (int)e.getY(), e.getButtonState(), 0, 0, 0);
+			}
+
+			return false;
+		}
+	}
+
 	public boolean onMouseEvent(MotionEvent e, boolean hover) {
 		_scummvm.pushEvent(ScummVMEvents.JE_MOUSE_MOVE, (int)e.getX(), (int)e.getY(), 0, 0, 0, 0);
 
@@ -105,39 +128,12 @@ public class MouseHelper {
 			_lmbPressed = false;
 		}
 
-		boolean rmbDown = (buttonState & MotionEvent.BUTTON_SECONDARY) == MotionEvent.BUTTON_SECONDARY;
-		if (rmbDown) {
-			if (!_rmbPressed) {
-				// right mouse button was pressed just now
-				_scummvm.pushEvent(ScummVMEvents.JE_RMB_DOWN, (int)e.getX(), (int)e.getY(), e.getButtonState(), 0, 0, 0);
-			}
-
-			_rmbPressed = true;
-		} else {
-			if (_rmbPressed) {
-				// right mouse button was released just now
-				_scummvm.pushEvent(ScummVMEvents.JE_RMB_UP, (int)e.getX(), (int)e.getY(), e.getButtonState(), 0, 0, 0);
-			}
-
-			_rmbPressed = false;
-		}
-
-		boolean mmbDown = (buttonState & MotionEvent.BUTTON_TERTIARY) == MotionEvent.BUTTON_TERTIARY;
-		if (mmbDown) {
-			if (!_mmbPressed) {
-				// middle mouse button was pressed just now
-				_scummvm.pushEvent(ScummVMEvents.JE_MMB_DOWN, (int)e.getX(), (int)e.getY(), e.getButtonState(), 0, 0, 0);
-			}
-
-			_mmbPressed = true;
-		} else {
-			if (_mmbPressed) {
-				// middle mouse button was released just now
-				_scummvm.pushEvent(ScummVMEvents.JE_MMB_UP, (int)e.getX(), (int)e.getY(), e.getButtonState(), 0, 0, 0);
-			}
-
-			_mmbPressed = false;
-		}
+		_rmbPressed = handleButton(e, _rmbPressed, MotionEvent.BUTTON_SECONDARY, ScummVMEvents.JE_RMB_DOWN, ScummVMEvents.JE_RMB_UP);
+		_mmbPressed = handleButton(e, _mmbPressed, MotionEvent.BUTTON_TERTIARY, ScummVMEvents.JE_MMB_DOWN, ScummVMEvents.JE_MMB_UP);
+		_bmbPressed = handleButton(e, _bmbPressed, MotionEvent.BUTTON_BACK, ScummVMEvents.JE_BMB_DOWN, ScummVMEvents.JE_BMB_UP);
+		_fmbPressed = handleButton(e, _fmbPressed, MotionEvent.BUTTON_FORWARD, ScummVMEvents.JE_FMB_DOWN, ScummVMEvents.JE_FMB_UP);
+		_srmbPressed = handleButton(e, _srmbPressed, MotionEvent.BUTTON_STYLUS_PRIMARY, ScummVMEvents.JE_RMB_DOWN, ScummVMEvents.JE_RMB_UP);
+		_smmbPressed = handleButton(e, _smmbPressed, MotionEvent.BUTTON_STYLUS_SECONDARY, ScummVMEvents.JE_MMB_DOWN, ScummVMEvents.JE_MMB_UP);
 
 		return true;
 	}
