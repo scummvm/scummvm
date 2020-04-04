cecedb4f37327e02b4170b36a6afbce7195065c7
Matthew Duggan <mgithub@guarana.org>
2020-04-04 20:53:07 +0900

    ULTIMA8: Refactor DataSources to inherit from Common::Streams

--- test/engines/ultima/ultima8/filesys/idata_source.h
+++ test/engines/ultima/ultima8/filesys/idata_source.h
@@ -20,7 +20,7 @@ class U8IDataSourceTestSuite : public CxxTest::TestSuite {
 	void test_ibuffer_source() {
 		uint8 buf[256];
 		for (int i = 0; i < 255; i++) {
-			buf[i] = i+1;
+			buf[i] = 0x80 + i+1;
 		}
 
 		Ultima::Ultima8::IBufferDataSource source(buf, 256, false, false);
@@ -28,18 +28,18 @@ class U8IDataSourceTestSuite : public CxxTest::TestSuite {
 		TS_ASSERT_EQUALS(source.pos(), 0);
 		TS_ASSERT(!source.eos());
 
-		TS_ASSERT_EQUALS(source.readByte(), 1);
-		TS_ASSERT_EQUALS(source.readUint16LE(), 0x0302);
-		TS_ASSERT_EQUALS(source.read3(), 0x060504);
-		TS_ASSERT_EQUALS(source.readUint32LE(), 0x0A090807);
+		TS_ASSERT_EQUALS(source.readByte(), 0x81);
+		TS_ASSERT_EQUALS(source.readUint16LE(), 0x8382);
+		TS_ASSERT_EQUALS(source.readUint24LE(), 0x868584);
+		TS_ASSERT_EQUALS(source.readUint32LE(), 0x8A898887);
 		source.skip(-2);
-		TS_ASSERT_EQUALS(source.readUint16LE(), 0x0A09);
+		TS_ASSERT_EQUALS(source.readUint16LE(), 0x8A89);
 		source.seek(16);
-		TS_ASSERT_EQUALS(source.readUint16LE(), 0x1211);
-		TS_ASSERT_EQUALS(source.readX(1), 0x13);
-		TS_ASSERT_EQUALS(source.readX(3), 0x161514);
-		TS_ASSERT_EQUALS(source.readXS(1), 0x17);
-		TS_ASSERT_EQUALS(source.readXS(3), 0x1A1918);
+		TS_ASSERT_EQUALS(source.readUint16LE(), 0x9291);
+		TS_ASSERT_EQUALS(source.readX(1), 0x93);
+		TS_ASSERT_EQUALS(source.readX(3), 0x969594);
+		TS_ASSERT_EQUALS(source.readXS(1), (int8)0x97);
+		TS_ASSERT_EQUALS(source.readXS(3), (int32)0xFF9A9998);
 		source.seek(256);
 		TS_ASSERT(source.eos());
 	}
