f789f3be8fd76a84f3d05e7b95e8cb57eb4e77fa
dreammaster <dreammaster@scummvm.org>
2020-04-08 01:58:02 +0100

    ULTIMA4: Formatting

--- engines/ultima/ultima4/core/lzw/u4decode.cpp
+++ engines/ultima/ultima4/core/lzw/u4decode.cpp
@@ -34,87 +34,87 @@ namespace LZW {
  * the decompressed file length, on success
  */
 long decompress_u4_file(Common::SeekableReadStream *in, long filesize, void **out) {
-    byte *compressed_mem, *decompressed_mem;
-    long compressed_filesize, decompressed_filesize;
-    long errorCode;
+	byte *compressed_mem, *decompressed_mem;
+	long compressed_filesize, decompressed_filesize;
+	long errorCode;
 
-    /* size of the compressed input file */
-    compressed_filesize = filesize;
+	/* size of the compressed input file */
+	compressed_filesize = filesize;
 
-    /* input file should be longer than 0 bytes */
-    if (compressed_filesize == 0)
-        return(-1);
+	/* input file should be longer than 0 bytes */
+	if (compressed_filesize == 0)
+		return (-1);
 
-    /* check if the input file is _not_ a valid LZW-compressed file */
-    if (!mightBeValidCompressedFile(in))
-        return(-1);
+	/* check if the input file is _not_ a valid LZW-compressed file */
+	if (!mightBeValidCompressedFile(in))
+		return (-1);
 
-    /* load compressed file into compressed_mem[] */
-    compressed_mem = (byte *) malloc(compressed_filesize);
+	/* load compressed file into compressed_mem[] */
+	compressed_mem = (byte *) malloc(compressed_filesize);
 	in->read(compressed_mem, compressed_filesize);
 
-    /*
-     * determine decompressed file size
-     * if lzw_get_decompressed_size() can't determine the decompressed size (i.e. the compressed
-     * data is corrupt), it returns -1
-     */
-    decompressed_filesize = lzwGetDecompressedSize(compressed_mem,compressed_filesize);
+	/*
+	 * determine decompressed file size
+	 * if lzw_get_decompressed_size() can't determine the decompressed size (i.e. the compressed
+	 * data is corrupt), it returns -1
+	 */
+	decompressed_filesize = lzwGetDecompressedSize(compressed_mem, compressed_filesize);
 
-    if (decompressed_filesize <= 0) {
-        return(-1);
-    }
+	if (decompressed_filesize <= 0) {
+		return (-1);
+	}
 
-    /* decompress file from compressed_mem[] into decompressed_mem[] */
-    decompressed_mem = (byte *) malloc(decompressed_filesize);
+	/* decompress file from compressed_mem[] into decompressed_mem[] */
+	decompressed_mem = (byte *) malloc(decompressed_filesize);
 
-    /* testing: clear destination mem */
-    memset(decompressed_mem, 0, decompressed_filesize);
+	/* testing: clear destination mem */
+	memset(decompressed_mem, 0, decompressed_filesize);
 
-    errorCode = lzwDecompress(compressed_mem, decompressed_mem, compressed_filesize);
+	errorCode = lzwDecompress(compressed_mem, decompressed_mem, compressed_filesize);
 
-    free(compressed_mem);
+	free(compressed_mem);
 
-    *out = decompressed_mem;
+	*out = decompressed_mem;
 
-    return(errorCode);
+	return (errorCode);
 }
 
 long decompress_u4_memory(void *in, long inlen, void **out) {
-    byte *compressed_mem, *decompressed_mem;
-    long compressed_filesize, decompressed_filesize;
-    long errorCode;
+	byte *compressed_mem, *decompressed_mem;
+	long compressed_filesize, decompressed_filesize;
+	long errorCode;
 
-    /* size of the compressed input */
-    compressed_filesize = inlen;
+	/* size of the compressed input */
+	compressed_filesize = inlen;
 
-    /* input file should be longer than 0 bytes */
-    if (compressed_filesize == 0)
-        return(-1);
+	/* input file should be longer than 0 bytes */
+	if (compressed_filesize == 0)
+		return (-1);
 
-    compressed_mem = (byte *) in;
+	compressed_mem = (byte *) in;
 
-    /*
-     * determine decompressed data size
-     * if lzw_get_decompressed_size() can't determine the decompressed size (i.e. the compressed
-     * data is corrupt), it returns -1
-     */
-    decompressed_filesize = lzwGetDecompressedSize(compressed_mem,compressed_filesize);
+	/*
+	 * determine decompressed data size
+	 * if lzw_get_decompressed_size() can't determine the decompressed size (i.e. the compressed
+	 * data is corrupt), it returns -1
+	 */
+	decompressed_filesize = lzwGetDecompressedSize(compressed_mem, compressed_filesize);
 
-    if (decompressed_filesize <= 0) {
-        return(-1);
-    }
+	if (decompressed_filesize <= 0) {
+		return (-1);
+	}
 
-    /* decompress file from compressed_mem[] into decompressed_mem[] */
-    decompressed_mem = (byte *) malloc(decompressed_filesize);
+	/* decompress file from compressed_mem[] into decompressed_mem[] */
+	decompressed_mem = (byte *) malloc(decompressed_filesize);
 
-    /* testing: clear destination mem */
-    memset(decompressed_mem, 0, decompressed_filesize);
+	/* testing: clear destination mem */
+	memset(decompressed_mem, 0, decompressed_filesize);
 
-    errorCode = lzwDecompress(compressed_mem, decompressed_mem, compressed_filesize);
+	errorCode = lzwDecompress(compressed_mem, decompressed_mem, compressed_filesize);
 
-    *out = decompressed_mem;
+	*out = decompressed_mem;
 
-    return(errorCode);
+	return (errorCode);
 }
 
 /*
@@ -130,25 +130,25 @@ long getFilesize(Common::SeekableReadStream *input_file) {
  * the first byte must be 0, because the first codeword is always a root.
  */
 byte mightBeValidCompressedFile(Common::SeekableReadStream *input_file) {
-    byte firstByte;
-    byte c1, c2, c3;   /* booleans */
-    long input_filesize;
+	byte firstByte;
+	byte c1, c2, c3;   /* booleans */
+	long input_filesize;
 
-    /*  check if the input file has a valid size             */
-    /*  the compressed file is made up of 12-bit codewords,  */
-    /*  so there are either 0 or 4 bits of wasted space      */
-    input_filesize = getFilesize(input_file);
-    c1 = (input_filesize * 8) % 12 == 0;
-    c2 = (input_filesize * 8 - 4) % 12 == 0;
+	/*  check if the input file has a valid size             */
+	/*  the compressed file is made up of 12-bit codewords,  */
+	/*  so there are either 0 or 4 bits of wasted space      */
+	input_filesize = getFilesize(input_file);
+	c1 = (input_filesize * 8) % 12 == 0;
+	c2 = (input_filesize * 8 - 4) % 12 == 0;
 
-    // read first byte, and then reset back file pointer
+	// read first byte, and then reset back file pointer
 	input_file->seek(0);
 	firstByte = input_file->readByte();
 	input_file->seek(0);
-    c3 = (firstByte >> 4) == 0;
+	c3 = (firstByte >> 4) == 0;
 
-    // check if upper 4 bits are 0
-    return ((c1 || c2) && c3);
+	// check if upper 4 bits are 0
+	return ((c1 || c2) && c3);
 }
 
 } // End of namespace LZW
