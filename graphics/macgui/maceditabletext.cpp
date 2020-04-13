fb11113d00e09943d83409c4c42bf5705243c884
Eugene Sandulenko <sev@scummvm.org>
2020-04-14 00:40:35 +0200

    GRAPHICS: MACGUI: Properly initialize font in MacEditableText

--- graphics/macgui/maceditabletext.cpp
+++ graphics/macgui/maceditabletext.cpp
@@ -45,12 +45,14 @@ enum {
 
 static void cursorTimerHandler(void *refCon);
 
-MacEditableText::MacEditableText(MacWidget *parent,int x, int y, int w, int h, MacWindowManager *wm, Common::U32String s, const MacFont *macFont, int fgcolor, int bgcolor, int maxWidth, TextAlign textAlignment, int interlinear) :
+MacEditableText::MacEditableText(MacWidget *parent, int x, int y, int w, int h, MacWindowManager *wm, Common::U32String s, const MacFont *macFont, int fgcolor, int bgcolor, int maxWidth, TextAlign textAlignment, int interlinear) :
 		MacWidget(parent, x, y, w, h, true), MacText(s, wm, macFont, fgcolor, bgcolor, maxWidth, textAlignment, interlinear) {
 
 	_maxWidth = maxWidth;
 
 	init();
+
+	_font = macFont;
 }
 
 MacEditableText::MacEditableText(MacWidget *parent, int x, int y, int w, int h, MacWindowManager *wm, const Common::String &s, const MacFont *macFont, int fgcolor, int bgcolor, int maxWidth, TextAlign textAlignment, int interlinear) :
@@ -59,6 +61,8 @@ MacEditableText::MacEditableText(MacWidget *parent, int x, int y, int w, int h, 
 	_maxWidth = maxWidth;
 
 	init();
+
+	_font = macFont;
 }
 
 void MacEditableText::init() {
