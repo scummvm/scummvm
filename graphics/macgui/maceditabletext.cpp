d36016c9deb8daff57478c2a9b6fc17b32107812
Eugene Sandulenko <sev@scummvm.org>
2020-04-22 01:09:01 +0200

    GRAPHICS: MACGUI: Fix drawing empty maceditabletext

--- graphics/macgui/maceditabletext.cpp
+++ graphics/macgui/maceditabletext.cpp
@@ -166,7 +166,7 @@ bool MacEditableText::draw(bool forceRedraw) {
 	_contentIsDirty = false;
 
 	// Compose
-	MacText::draw(&_composeSurface, 0, _scrollPos, _surface->w - 2, _scrollPos + _surface->h - 2, kConWOverlap - 2, kConWOverlap - 2);
+	MacText::draw(&_composeSurface, 0, _scrollPos, _surface->w, _scrollPos + _surface->h, kConWOverlap - 2, kConWOverlap - 2);
 
 	if (_cursorState)
 		_composeSurface.blitFrom(*_cursorSurface, *_cursorRect, Common::Point(_cursorX + kConWOverlap - 2, _cursorY + kConHOverlap - 2));
