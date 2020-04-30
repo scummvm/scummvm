cc58bbb05a030111475b2d7d9bb8390fd3fcc4fd
Eugene Sandulenko <sev@scummvm.org>
2020-05-01 00:41:06 +0200

    DIRECTOR: The created window should receive keyboard events

--- engines/director/score.cpp
+++ engines/director/score.cpp
@@ -1519,7 +1519,7 @@ void Score::startLoop() {
 
 	initGraphics(_movieRect.width(), _movieRect.height());
 
-	_window = _vm->_wm->addWindow(false, false, false);
+	_window = _vm->_wm->addWindow(false, false, true);
 	_window->disableBorder();
 	_window->resize(_movieRect.width(), _movieRect.height());
 
