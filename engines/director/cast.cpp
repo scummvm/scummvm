d8cbadd20158984d88ec1fd9b0a8ec5e91e2c7a3
Scott Percival <code@moral.net.au>
2020-05-17 00:00:16 +0800

    DIRECTOR: Invert sound looping bit

--- engines/director/cast.cpp
+++ engines/director/cast.cpp
@@ -137,7 +137,7 @@ SoundCast::SoundCast(Common::ReadStreamEndian &stream, uint16 version) {
 		for (int i = 0; i < 0xe; i++) {
 			stream.readByte();
 		}
-		_looping = stream.readByte() & 0x10;
+		_looping = stream.readByte() & 0x10 ? 0 : 1;
 	}
 }
 
