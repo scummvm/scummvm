scummvm/scummvm

commit 1c5cbbb18665996de382d27ec875e961051dbec4

QUEEN: Remove autosave code

engines/queen/queen.cpp
@@ -154,10 +154,6 @@ void QueenEngine::update(bool checkPlayerInput) {
 			_input->quickLoadReset();
 			loadGameState(SLOT_QUICKSAVE);
 		}
-		if (shouldPerformAutoSave(_lastSaveTime)) {
-			saveGameState(SLOT_AUTOSAVE, "Autosave");
-			_lastSaveTime = _system->getMillis();
-		}
 	}
 	if (!_input->cutawayRunning()) {
 		if (checkPlayerInput) {
@@ -274,15 +270,20 @@ Common::InSaveFile *QueenEngine::readGameStateHeader(int slot, GameStateHeader *
 	return file;
 }
 
-void QueenEngine::makeGameStateName(int slot, char *buf) const {
+Common::String QueenEngine::getSaveStateName(int slot) const {
 	if (slot == SLOT_LISTPREFIX) {
-		strcpy(buf, "queen.s??");
+		return "queen.s??";
 	} else if (slot == SLOT_AUTOSAVE) {
-		strcpy(buf, "queen.asd");
-	} else {
-		assert(slot >= 0);
-		sprintf(buf, "queen.s%02d", slot);
+		slot = getAutosaveSlot();
 	}
+
+	assert(slot >= 0);
+	return Common::String::format("queen.s%02d", slot);
+}
+
+void QueenEngine::makeGameStateName(int slot, char *buf) const {
+	Common::String name = getSaveStateName(slot);
+	strcpy(buf, name.c_str());
 }
 
 int QueenEngine::getGameStateSlot(const char *filename) const {
@@ -354,7 +355,6 @@ Common::Error QueenEngine::run() {
 	if (ConfMan.hasKey("save_slot") && canLoadOrSave()) {
 		loadGameState(ConfMan.getInt("save_slot"));
 	}
-	_lastSaveTime = _lastUpdateTime = _system->getMillis();
 
 	while (!shouldQuit()) {
 		if (_logic->newRoom() > 0) {

engines/queen/queen.h
@@ -98,6 +98,8 @@ public:
 	bool canSaveGameStateCurrently() override;
 	Common::Error saveGameState(int slot, const Common::String &desc, bool isAutosave = false) override;
 	Common::Error loadGameState(int slot) override;
+	virtual int getAutosaveSlot() const { return 99; }
+	virtual Common::String getSaveStateName(int slot) const override;
 	void makeGameStateName(int slot, char *buf) const;
 	int getGameStateSlot(const char *filename) const;
 	void findGameStateDescriptions(char descriptions[100][32]);
@@ -126,7 +128,6 @@ protected:
 
 	int _talkSpeed;
 	bool _subtitles;
-	uint32 _lastSaveTime;
 	uint32 _lastUpdateTime;
 	bool _gameStarted;
 

commit 5d1592c1ed395adc197f7f69fdeaeea133673c96

QUEEN: Fix random long delay when starting game
The _lastUpdateTime variable was not initialized before its first
use, which resulted in the first call to update() having a random,
possibly very long, delay.

This was a regression from commit 1c5cbbb (QUEEN: Remove autosave
code). So fortunately the bug is not present in the last release!

engines/queen/queen.cpp
@@ -355,6 +355,7 @@ Common::Error QueenEngine::run() {
 	if (ConfMan.hasKey("save_slot") && canLoadOrSave()) {
 		loadGameState(ConfMan.getInt("save_slot"));
 	}
+	_lastUpdateTime = _system->getMillis();
 
 	while (!shouldQuit()) {
 		if (_logic->newRoom() > 0) {