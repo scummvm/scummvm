6bb6589eca4d28cab471cdb34aa949d8796fbe89
Gunnar Birke <gunnar.birke@online.de>
2020-05-27 01:44:28 +0200

    WINTERMUTE: Implement simple vertex buffer filling and rendering

--- engines/wintermute/base/gfx/opengl/mesh.cpp
+++ engines/wintermute/base/gfx/opengl/mesh.cpp
@@ -2,11 +2,17 @@
 #include "loader3ds.h"
 
 Wintermute::Mesh::Mesh()
-	: vertexData(nullptr), vertexCount(0), indexData(nullptr), indexCount(0) {
+	: vertexData(nullptr), vertexCount(0),
+	  indexData(nullptr), indexCount(0),
+	  vertexBuffer(0), indexBuffer(0) {
 
 }
 
 Wintermute::Mesh::~Mesh() {
+
+	GLuint bufferNames[2] = {vertexBuffer, indexBuffer};
+	glDeleteBuffers(2, bufferNames);
+
 	delete[] vertexData;
 	delete[] indexData;
 }
@@ -16,7 +22,30 @@ void Wintermute::Mesh::computeNormals() {
 }
 
 void Wintermute::Mesh::fillVertexBuffer(uint32 color) {
+	if (vertexBuffer == 0) {
+		GLuint bufferNames[2];
+		glGenBuffers(2, bufferNames);
+		vertexBuffer = *bufferNames;
+		indexBuffer = *(bufferNames + 1);
+	}
 
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, vertexCount * 4, 0, GL_STATIC_DRAW);
+	uint32* bufferData = reinterpret_cast<uint32*>(glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY));
+
+	for (int i = 0; i < vertexCount; ++i) {
+		*reinterpret_cast<uint32*>(bufferData + 4 * i) = color;
+		*reinterpret_cast<float*>(bufferData + 4 * i + 1) = *reinterpret_cast<float*>(vertexData + 4 * 3 * i);
+		*reinterpret_cast<float*>(bufferData + 4 * i + 2) = *reinterpret_cast<float*>(vertexData + 4 * 3 * i + 4);
+		*reinterpret_cast<float*>(bufferData + 4 * i + 3) = *reinterpret_cast<float*>(vertexData + 4 * 3 * i + 8);
+	}
+
+	glUnmapBuffer(GL_ARRAY_BUFFER);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexCount * 2, indexData, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
 bool Wintermute::Mesh::loadFrom3DS(byte** buffer) {
@@ -76,4 +105,20 @@ bool Wintermute::Mesh::loadFrom3DS(byte** buffer) {
 	}
 
 	return true;
+}
+
+void Wintermute::Mesh::render()
+{
+	if (vertexBuffer == 0) {
+		return;
+	}
+
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+	glInterleavedArrays(GL_C4UB_V3F, 0, 0);
+
+	glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_SHORT, 0);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
--- engines/wintermute/base/gfx/opengl/mesh.h
+++ engines/wintermute/base/gfx/opengl/mesh.h
@@ -2,6 +2,7 @@
 #define WINTERMUTE_MESH_H
 
 #include "common/scummsys.h"
+#include "graphics/opengl/system_headers.h"
 
 namespace Wintermute {
 
@@ -13,12 +14,15 @@ public:
 	void computeNormals();
 	void fillVertexBuffer(uint32 color);
 	bool loadFrom3DS(byte** buffer);
+	void render();
 
 private:
 	byte* vertexData;
 	uint16 vertexCount;
 	uint16* indexData;
 	uint16 indexCount;
+	GLuint vertexBuffer;
+	GLuint indexBuffer;
 };
 
 }
