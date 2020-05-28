27430b86cf3044edaaa3127e704d3b23adbf82a0
Thierry Crozat <criezy@scummvm.org>
2020-05-28 23:05:08 +0100

    BASE: Add test with c++11 std::initializer_list replacement
    This code is adapted from similar code in SerenityOS and allows the
    LIST_INITIALIZER1 test to pass with c++11 compliant compiler when
    targeting an old c++ std librray that does not have
    std::initializer_list.

--- base/test_new_standards.cpp
+++ base/test_new_standards.cpp
@@ -49,7 +49,42 @@
 #include "common/rect.h"
 
 #ifndef DONT_TEST_INITIALIZIER_LIST1
+#ifndef USE_INITIALIZIER_LIST_REPLACEMENT
 #include <initializer_list>
+#else
+namespace std {
+template<class T> class initializer_list {
+public:
+	typedef T value_type;
+	typedef const T& reference;
+	typedef const T& const_reference;
+	typedef size_t size_type;
+	typedef const T* iterator;
+	typedef const T* const_iterator;
+
+	constexpr initializer_list() noexcept = default;
+	constexpr size_t size() const noexcept { return m_size; };
+	constexpr const T* begin() const noexcept { return m_begin; };
+	constexpr const T* end() const noexcept { return m_begin + m_size; }
+
+private:
+	// Note: begin has to be first or the compiler gets very upset
+	const T* m_begin = { nullptr };
+	size_t m_size = { 0 };
+
+	// The compiler is allowed to call this constructor
+	constexpr initializer_list(const T* t, size_t s) noexcept : m_begin(t) , m_size(s) {}
+};
+
+template<class T> constexpr const T* begin(initializer_list<T> il) noexcept {
+	return il.begin();
+}
+
+template<class T> constexpr const T* end(initializer_list<T> il) noexcept {
+	return il.end();
+}
+} // end namespace std
+#endif
 #endif
 
 #ifndef DONT_TEST_CLASS_ENUM
