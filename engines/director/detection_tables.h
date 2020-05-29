f16660ecbce6c1505ae4b5569f75e3f1e882be8b
Eugene Sandulenko <sev@scummvm.org>
2020-05-29 18:34:37 +0200

    DIRECTOR: Added generic D2 target, added director version as an extra

--- engines/director/detection_tables.h
+++ engines/director/detection_tables.h
@@ -63,14 +63,16 @@ static const DirectorGameDescription gameDescriptions[] = {
 		4
 	},
 
+	// Generic D2 Mac entry
+	MACGAME("director", "D2-mac", "D2-mac", 0, -1, 2),
 	// Generic D3 Mac entry
-	MACGAME("director", "", "D3-mac", 0, -1, 3),
+	MACGAME("director", "D3-mac", "D3-mac", 0, -1, 3),
 	// Generic D4 Mac entry
-	MACGAME("director", "", "D4-mac", 0, -1, 4),
+	MACGAME("director", "D4-mac", "D4-mac", 0, -1, 4),
 	// Generic D3 Win entry
-	WINGAME("director", "", "D3-win", 0, -1, 3),
+	WINGAME("director", "D3-win", "D3-win", 0, -1, 3),
 	// Generic D4 Win entry
-	WINGAME("director", "", "D4-win", 0, -1, 4),
+	WINGAME("director", "D4-win", "D4-win", 0, -1, 4),
 
 	MACGAME("theapartment", "D2", "Main Menu", "fc56c179cb8c6d4938e61ee61fd0032c", 48325, 2), // Original name is "•Main Menu"
 	MACGAME("theapartment", "D3", "Main Menu", "9e838fe1a6af7992d656ca325e38dee5", 47911, 3), // Original name is "•Main Menu"
