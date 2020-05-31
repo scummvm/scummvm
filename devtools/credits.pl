9315728f60d092437f4f1bafeed49cd6dc777ecc
Strangerke <Strangerke@scummvm.org>
2020-05-31 20:29:32 +0100

    KINGDOM: Add TMM in the engine credits

--- devtools/credits.pl
+++ devtools/credits.pl
@@ -811,6 +811,7 @@ begin_credits("Credits");
 			begin_section("Kingdom");
 				add_person("Arnaud Boutonn&eacute;", "Strangerke", "");
 				add_person("Thomas Fach-Pedersen", "madmoose", "");
+				add_person("Hein-Pieter van Braam-Stewart", "TMM", "");
 			end_section();
 
 			begin_section("Kyra");
