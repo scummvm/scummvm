9c4e6244100f89b0e078006de7ba73dbb36d46a4
Paul Gilbert <dreammaster@scummvm.org>
2020-06-03 20:46:26 -0700

    CREDITS: Update Ultima entry for OMGPizzaGuy

--- devtools/credits.pl
+++ devtools/credits.pl
@@ -1017,6 +1017,7 @@ begin_credits("Credits");
 			begin_section("Ultima");
 				add_person("Paul Gilbert", "dreammaster", "");
 				add_person("Matthew Duggan", "stauff", "");
+				add_person("Matthew Jimenez", "OMGPizzaGuy", "");
 
 				add_person("Daniel c. W&uuml;rl", "dwuerl", "(Nuvie)");
 				add_person("Eric Fry", "yuv422", "(Nuvie)");
@@ -1033,7 +1034,6 @@ begin_credits("Credits");
 
 				add_person("Brian Tietz", "btietz", "(Pentagram)");
 				add_person("Dominik Reichardt", "Dominus Dragon", "(Pentagram)");
-				add_person("Matthew Jimenez", "mjimenez", "(Pentagram)");
 				add_person("Max Horn", "Fingolfin", "(Pentagram)");
 				add_person("Patrick Burke", "takhisis", "(Pentagram)");
 				add_person("Ryan Nunn", "Colourless Dragon", "(Pentagram)");
