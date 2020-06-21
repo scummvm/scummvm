59a1a3934e673302b3ba758af98dabf2e1f90f88
Thierry Crozat <criezy@scummvm.org>
2020-06-22 00:01:15 +0100

    GLK: Try to properly handle speech with multiple TextBufferWindows

--- engines/glk/speech.cpp
+++ engines/glk/speech.cpp
@@ -21,19 +21,44 @@
  */
 
 #include "glk/speech.h"
+#include "common/debug.h"
+#include "glk/glk.h"
 
 #if defined(USE_TTS)
 #include "common/config-manager.h"
 #include "common/system.h"
-#include "common/debug.h"
-#include "glk/glk.h"
 #endif
 
 namespace Glk {
 
-void Speech::gli_initialize_tts(void) {
+SpeechManager *SpeechManager::_instance = nullptr;
+
+SpeechManager* SpeechManager::getSpeechManagerInstance() {
 #if defined(USE_TTS)
-	debugC(kDebugSpeech, "gli_initialize_tts()");
+	if (!_instance)
+		_instance = new SpeechManager();
+	++_instance->_refCount;
+	return _instance;
+#else
+	return nullptr;
+#endif
+}
+
+void SpeechManager::releaseSpeechManagerInstance() {
+	if (--_refCount == 0) {
+		_instance = nullptr;
+		delete this;
+	}
+}
+
+SpeechManager::SpeechManager() :
+	_refCount(0)
+#if defined(USE_TTS)
+	, _ttsMan(nullptr), _lastSpeechSource(nullptr), _nextSpeechAction(Common::TextToSpeechManager::QUEUE)
+#endif
+{
+#if defined(USE_TTS)
+	debugC(kDebugSpeech, "Initialize Glk::SpeechManager");
 	if (_ttsMan != nullptr)
 		return;
 	_ttsMan = g_system->getTextToSpeechManager();
@@ -55,61 +80,122 @@ void Speech::gli_initialize_tts(void) {
 		} else
 			voice = _ttsMan->getDefaultVoice();
 		_ttsMan->setVoice(voice);
-	}
+	} else
+		debugC(kDebugSpeech, "Text to Speech is not available");
 #endif
 }
 
-void Speech::gli_tts_flush(void) {
+SpeechManager::~SpeechManager() {
 #if defined(USE_TTS)
-	debugC(kDebugSpeech, "gli_tts_flush()");
-	if (_ttsMan != nullptr && !_speechBuffer.empty()) {
-		// Curently the TextToSpeechManager takes a String, which does not properly support
-		// UTF-32. So convert to UTF-8.
-		Common::String text = _speechBuffer.encode();
-		debugC(kDebugSpeech, "Speaking: \"%s\"", text.c_str());
-		_ttsMan->say(text, Common::TextToSpeechManager::QUEUE, "utf-8");
-		_speechBuffer.clear();
-	}
-#endif
-}
-
-void Speech::gli_tts_purge(void) {
-#if defined(USE_TTS)
-	debugC(kDebugSpeech, "gli_tts_purge()");
-	if (_ttsMan != nullptr) {
-		_speechBuffer.clear();
-		_ttsMan->stop();
-	}
-#endif
-}
-
-void Speech::gli_tts_speak(const uint32 *buf, size_t len) {
-#if defined(USE_TTS)
-	debugC(1, kDebugSpeech, "gli_tts_speak()");
-	if (_ttsMan != nullptr) {
-		for (int i = 0 ; i < len ; ++i, ++buf) {
-			// Should we automatically flush on new lines without waiting for the call to gli_tts_flush?
-			// Should we also flush on '.', '?', and '!'?
-//			if (*buf == '\n') {
-//				debugC(1, kDebugSpeech, "flush speech buffer on new line");
-//				gli_tts_flush();
-//			} else {
-				_speechBuffer += *buf;
-				debugC(1, kDebugSpeech, "speech buffer: %s", _speechBuffer.encode().c_str());
-//			}
-		}
-	}
-#endif
-}
-
-void Speech::gli_free_tts(void) {
-#if defined(USE_TTS)
-	debugC(kDebugSpeech, "gli_free_tts()");
+	debugC(kDebugSpeech, "Destroy Glk::SpeechManager");
 	if (_ttsMan != nullptr) {
 		_ttsMan->popState();
 		_ttsMan = nullptr;
 	}
 #endif
+}
+
+void SpeechManager::flushSpeech(Speech * source) {
+#if defined(USE_TTS)
+	if (source != _lastSpeechSource) {
+		debugC(kDebugSpeech, "SpeechManager::flushSpeech() called with a different source than the last text source");
+		purgeSpeech(source);
+	} else {
+		if (_ttsMan != nullptr && !_speechBuffer.empty()) {
+			// Curently the TextToSpeechManager takes a String, which does not properly support
+			// UTF-32. So convert to UTF-8.
+			Common::String text = _speechBuffer.encode();
+			debugC(kDebugSpeech, "Speaking: \"%s\"", text.c_str());
+			_ttsMan->say(text, _nextSpeechAction, "utf-8");
+			_speechBuffer.clear();
+			_nextSpeechAction = Common::TextToSpeechManager::QUEUE;
+		}
+	}
+#endif
+}
+
+void SpeechManager::purgeSpeech(Speech *source) {
+#if defined(USE_TTS)
+	debugC(kDebugSpeech, "SpeechManager::purgeSpeech()");
+	if (_ttsMan != nullptr) {
+		_speechBuffer.clear();
+		_ttsMan->stop();
+	}
+	_nextSpeechAction = Common::TextToSpeechManager::QUEUE;
+	_lastSpeechSource = source;
+#endif
+}
+
+void SpeechManager::addSpeech(const uint32 *buf, size_t len, Speech *source) {
+#if defined(USE_TTS)
+	if (source != _lastSpeechSource) {
+		debugC(kDebugSpeech, "Flushing SpeechManager buffer for a different speech source");
+		if (!_speechBuffer.empty()) {
+			// Flush the pending speech, but allow interupting it if we flush the text
+			// for the new source before it has finished.
+			flushSpeech(_lastSpeechSource);
+			_nextSpeechAction = Common::TextToSpeechManager::INTERRUPT;
+		}
+		_lastSpeechSource = source;
+	}
+	if (_ttsMan != nullptr) {
+		debugC(1, kDebugSpeech, "SpeechManager add speech");
+		for (int i = 0 ; i < len ; ++i, ++buf) {
+			// Should we automatically flush on new lines without waiting for the call to gli_tts_flush?
+			// Should we also flush on '.', '?', and '!'?
+			//if (*buf == '\n') {
+			//	debugC(1, kDebugSpeech, "Flushing SpeechManager buffer on new line");
+			//	gli_tts_flush();
+			//} else {
+			_speechBuffer += *buf;
+			//}
+		}
+//		debugC(1, kDebugSpeech, "SpeechManager buffer: %s", _speechBuffer.encode().c_str());
+	}
+#endif
+}
+
+
+Speech::Speech() : _speechManager(nullptr) {
+}
+
+Speech::~Speech() {
+	if (_speechManager) {
+		warning("Unbalanced calls to gli_initialize_tts and gli_free_tts");
+		_speechManager->releaseSpeechManagerInstance();
+	}
+}
+
+void Speech::gli_initialize_tts(void) {
+	debugC(kDebugSpeech, "gli_initialize_tts");
+	if (!_speechManager)
+		_speechManager = SpeechManager::getSpeechManagerInstance();
+}
+
+void Speech::gli_tts_flush(void) {
+	debugC(kDebugSpeech, "gli_tts_flush");
+	if (_speechManager)
+		_speechManager->flushSpeech(this);
+}
+
+void Speech::gli_tts_purge(void) {
+	debugC(kDebugSpeech, "gli_tts_purge");
+	if (_speechManager)
+		_speechManager->purgeSpeech(this);
+}
+
+void Speech::gli_tts_speak(const uint32 *buf, size_t len) {
+	debugC(1, kDebugSpeech, "gli_tts_speak");
+	if (_speechManager)
+		_speechManager->addSpeech(buf, len, this);
+}
+
+void Speech::gli_free_tts(void) {
+	debugC(kDebugSpeech, "gli_free_tts");
+	if (_speechManager) {
+		_speechManager->releaseSpeechManagerInstance();
+		_speechManager = nullptr;
+	}
 }
 
 } // End of namespace Glk
