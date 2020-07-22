51c7e019d92cdb50879569dd0401299fb5af2cba
Nathanael Gentry <nathanael.gentrydb8@gmail.com>
2020-07-22 09:52:01 -0400

    GRAPHICS: MacCursor: Make two virtual functions
    This is for the Director engine, which has a cursor class inheriting from MacCursor.

--- graphics/maccursor.h
+++ graphics/maccursor.h
@@ -54,11 +54,11 @@ public:
 	/** Return the cursor's hotspot's y coordinate. */
 	uint16 getHotspotY() const { return _hotspotY; }
 	/** Return the cursor's transparent key. */
-	byte getKeyColor() const { return 0xFF; }
+	virtual byte getKeyColor() const { return 0xFF; }
 
 	const byte *getSurface() const { return _surface; }
 
-	const byte *getPalette() const { return _palette; }
+	virtual const byte *getPalette() const { return _palette; }
 	byte getPaletteStartIndex() const { return 0; }
 	uint16 getPaletteCount() const { return 256; }
 
