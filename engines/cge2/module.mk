13a66834a7104dcffa1b6352f5cac388ad1b6c8e
aryanrawlani28 <aryanrawlani007@gmail.com>
2020-08-04 21:45:44 +0530

    CGE2: Split detection code & adapt to new plugins.

--- engines/cge2/module.mk
+++ engines/cge2/module.mk
@@ -2,10 +2,9 @@ MODULE := engines/cge2
 
 MODULE_OBJS = \
 	cge2.o \
-	detection.o \
-	fileio.o \
 	vga13h.o \
 	bitmap.o \
+	fileio.o \
 	sound.o \
 	cge2_main.o \
 	text.o \
@@ -15,6 +14,7 @@ MODULE_OBJS = \
 	talk.o \
 	events.o \
 	map.o \
+	metaengine.o \
 	vmenu.o \
 	saveload.o \
 	toolbar.o \
@@ -28,3 +28,13 @@ endif
 
 # Include common rules
 include $(srcdir)/rules.mk
+
+# Detection objects
+DETECT_OBJS += $(MODULE)/detection.o
+
+# Only include if building as a dynamic module.
+# Static module already has the contents.
+ifeq ($(ENABLE_CGE2), DYNAMIC_PLUGIN)
+# External dependencies for detection.
+DETECT_OBJS += $(MODULE)/fileio.o
+endif
