033ad0baa1abb53c179953b777c2ffcd6e34bb62
aryanrawlani28 <aryanrawlani007@gmail.com>
2020-08-06 22:47:46 +0530

    KINGDOM: Split detection features & adapt to new plugins.

--- engines/kingdom/module.mk
+++ engines/kingdom/module.mk
@@ -3,13 +3,13 @@ MODULE := engines/kingdom
 MODULE_OBJS = \
 	kingdom.o \
 	constants.o \
+	console.o \
 	logic1.o \
 	logic2.o \
 	logic3.o \
 	logic4.o \
 	logic.o \
-	console.o \
-	detection.o
+	metaengine.o
 
 # This module can be built as a plugin
 ifeq ($(ENABLE_KINGDOM), DYNAMIC_PLUGIN)
@@ -18,3 +18,6 @@ endif
 
 # Include common rules
 include $(srcdir)/rules.mk
+
+# Detection objects
+DETECT_OBJS += $(MODULE)/detection.o
