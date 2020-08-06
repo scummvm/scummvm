bb8fad39bff0cfeb1cd86417acb8a813be336bf2
aryanrawlani28 <aryanrawlani007@gmail.com>
2020-08-06 23:04:28 +0530

    NEVERHOOD: Split detection features & adapt to new plugins.

--- engines/neverhood/module.mk
+++ engines/neverhood/module.mk
@@ -4,7 +4,6 @@ MODULE_OBJS = \
 	background.o \
 	blbarchive.o \
 	console.o \
-	detection.o \
 	diskplayerscene.o \
 	entity.o \
 	gamemodule.o \
@@ -12,6 +11,7 @@ MODULE_OBJS = \
 	graphics.o \
 	klaymen.o \
 	menumodule.o \
+	metaengine.o \
 	microtiles.o \
 	module.o \
 	modules/module1000.o \
@@ -75,3 +75,6 @@ endif
 
 # Include common rules
 include $(srcdir)/rules.mk
+
+# Detection objects
+DETECT_OBJS += $(MODULE)/detection.o
