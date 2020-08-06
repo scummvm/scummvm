0fbfa709a236abbd3281efce2b959877a43ebc26
aryanrawlani28 <aryanrawlani007@gmail.com>
2020-08-06 22:52:04 +0530

    LAB: Split detection features & adapt to new plugins.

--- engines/lab/detection.cpp
+++ engines/lab/detection.cpp
@@ -29,8 +29,7 @@
  */
 
 #include "engines/advancedDetector.h"
-
-#include "lab/lab.h"
+#include "lab/detection_enums.h"
 
 static const PlainGameDescriptor lab_setting[] = {
 	{ "lab", "Labyrinth of Time" },
@@ -100,17 +99,7 @@ static const char *const directoryGlobs[] = {
 		0
 };
 
-namespace Lab {
 
-Common::Platform LabEngine::getPlatform() const {
-	return _gameDescription->platform;
-}
-
-uint32 LabEngine::getFeatures() const {
-	return _gameDescription->flags | _extraGameFeatures;
-}
-
-} // End of namespace Lab
 
 class LabMetaEngine : public AdvancedMetaEngine {
 public:
@@ -131,104 +120,6 @@ public:
 	const char *getOriginalCopyright() const override {
 		return "Labyrinth of Time (C) 2004 The Wyrmkeep Entertainment Co. and Terra Nova Development";
 	}
-
-	bool createInstance(OSystem *syst, Engine **engine, const ADGameDescription *desc) const override {
-		// Instantiate Engine even if the game data is not found.
-		*engine = new Lab::LabEngine(syst, desc);
-		return true;
-	}
-
-	bool hasFeature(MetaEngineFeature f) const override;
-	SaveStateList listSaves(const char *target) const override;
-	int getMaximumSaveSlot() const override;
-	void removeSaveState(const char *target, int slot) const override;
-	SaveStateDescriptor querySaveMetaInfos(const char *target, int slot) const override;
 };
 
-bool LabMetaEngine::hasFeature(MetaEngineFeature f) const {
-	return
-		(f == kSupportsListSaves) ||
-		(f == kSupportsLoadingDuringStartup) ||
-		(f == kSupportsDeleteSave) ||
-		(f == kSavesSupportMetaInfo) ||
-		(f == kSavesSupportThumbnail) ||
-		(f == kSavesSupportCreationDate) ||
-		(f == kSavesSupportPlayTime) ||
-		(f == kSimpleSavesNames);
-}
-
-bool Lab::LabEngine::hasFeature(EngineFeature f) const {
-	return
-		(f == kSupportsReturnToLauncher) ||
-		(f == kSupportsLoadingDuringRuntime) ||
-		(f == kSupportsSavingDuringRuntime);
-}
-
-SaveStateList LabMetaEngine::listSaves(const char *target) const {
-	Common::SaveFileManager *saveFileMan = g_system->getSavefileManager();
-	Lab::SaveGameHeader header;
-	Common::String pattern = target;
-	pattern += ".###";
-
-	Common::StringArray filenames;
-	filenames = saveFileMan->listSavefiles(pattern.c_str());
-
-	SaveStateList saveList;
-
-	for (Common::StringArray::const_iterator file = filenames.begin(); file != filenames.end(); ++file) {
-		// Obtain the last 3 digits of the filename, since they correspond to the save slot
-		int slotNum = atoi(file->c_str() + file->size() - 3);
-
-		if ((slotNum >= 0) && (slotNum <= 999)) {
-			Common::InSaveFile *in = saveFileMan->openForLoading(file->c_str());
-			if (in) {
-				if (Lab::readSaveGameHeader(in, header))
-					saveList.push_back(SaveStateDescriptor(slotNum, header._descr.getDescription()));
-				delete in;
-			}
-		}
-	}
-
-	// Sort saves based on slot number.
-	Common::sort(saveList.begin(), saveList.end(), SaveStateDescriptorSlotComparator());
-	return saveList;
-}
-
-int LabMetaEngine::getMaximumSaveSlot() const {
-	return 999;
-}
-
-void LabMetaEngine::removeSaveState(const char *target, int slot) const {
-	Common::SaveFileManager *saveFileMan = g_system->getSavefileManager();
-	saveFileMan->removeSavefile(Common::String::format("%s.%03u", target, slot));
-}
-
-SaveStateDescriptor LabMetaEngine::querySaveMetaInfos(const char *target, int slot) const {
-	Common::String filename = Common::String::format("%s.%03u", target, slot);
-	Common::InSaveFile *in = g_system->getSavefileManager()->openForLoading(filename.c_str());
-
-	if (in) {
-		Lab::SaveGameHeader header;
-
-		bool successfulRead = Lab::readSaveGameHeader(in, header, false);
-		delete in;
-
-		if (successfulRead) {
-			SaveStateDescriptor desc(slot, header._descr.getDescription());
-			// Do not allow save slot 0 (used for auto-saving) to be deleted or
-			// overwritten.
-			//desc.setDeletableFlag(slot != 0);
-			//desc.setWriteProtectedFlag(slot == 0);
-
-			return header._descr;
-		}
-	}
-
-	return SaveStateDescriptor();
-}
-
-#if PLUGIN_ENABLED_DYNAMIC(LAB)
-	REGISTER_PLUGIN_DYNAMIC(LAB, PLUGIN_TYPE_ENGINE, LabMetaEngine);
-#else
-	REGISTER_PLUGIN_STATIC(LAB, PLUGIN_TYPE_ENGINE, LabMetaEngine);
-#endif
+REGISTER_PLUGIN_STATIC(LAB_DETECTION, PLUGIN_TYPE_METAENGINE, LabMetaEngine);
