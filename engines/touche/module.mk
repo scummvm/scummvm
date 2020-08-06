4bbf332ba6da077f63d107454cd61a5af8019d94
aryanrawlani28 <aryanrawlani007@gmail.com>
2020-08-06 23:29:46 +0530

    TOUCHE: Split detection features & adapt to new plugins.

--- engines/touche/module.mk
+++ engines/touche/module.mk
@@ -2,9 +2,9 @@ MODULE := engines/touche
 
 MODULE_OBJS := \
 	console.o \
-	detection.o \
 	graphics.o \
 	menu.o \
+	metaengine.o \
 	midi.o \
 	opcodes.o \
 	resource.o \
@@ -19,3 +19,6 @@ endif
 
 # Include common rules
 include $(srcdir)/rules.mk
+
+# Detection objects
+DETECT_OBJS += $(MODULE)/detection.o
