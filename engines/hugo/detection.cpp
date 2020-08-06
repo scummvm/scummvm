a39261441536b3a918d8b20ebd1e208a052e0a8b
aryanrawlani28 <aryanrawlani007@gmail.com>
2020-08-06 22:45:30 +0530

    HUGO: Split detection features & adapt to new plugins.

--- engines/hugo/detection.cpp
+++ engines/hugo/detection.cpp
@@ -20,30 +20,13 @@
  *
  */
 
+#include "base/plugins.h"
 #include "engines/advancedDetector.h"
-#include "common/system.h"
-#include "common/savefile.h"
-#include "common/textconsole.h"
-#include "graphics/thumbnail.h"
-#include "graphics/surface.h"
 
-#include "hugo/hugo.h"
+#include "hugo/detection_enums.h"
+#include "hugo/detection.h"
 
 namespace Hugo {
-
-struct HugoGameDescription {
-	ADGameDescription desc;
-	GameType gameType;
-};
-
-uint32 HugoEngine::getFeatures() const {
-	return _gameDescription->desc.flags;
-}
-
-const char *HugoEngine::getGameId() const {
-	return _gameDescription->desc.gameId;
-}
-
 
 static const PlainGameDescriptor hugoGames[] = {
 	// Games
@@ -147,156 +130,8 @@ public:
 	const char *getOriginalCopyright() const override {
 		return "Hugo Engine (C) 1989-1997 David P. Gray";
 	}
-
-	bool createInstance(OSystem *syst, Engine **engine, const ADGameDescription *gd) const override;
-	bool hasFeature(MetaEngineFeature f) const override;
-
-	int getMaximumSaveSlot() const override;
-	SaveStateList listSaves(const char *target) const override;
-	SaveStateDescriptor querySaveMetaInfos(const char *target, int slot) const override;
-	void removeSaveState(const char *target, int slot) const override;
 };
 
-bool HugoMetaEngine::createInstance(OSystem *syst, Engine **engine, const ADGameDescription *gd) const {
-	if (gd) {
-		*engine = new HugoEngine(syst, (const HugoGameDescription *)gd);
-		((HugoEngine *)*engine)->initGame((const HugoGameDescription *)gd);
-	}
-	return gd != 0;
-}
-
-bool HugoMetaEngine::hasFeature(MetaEngineFeature f) const {
-	return
-	    (f == kSupportsListSaves) ||
-	    (f == kSupportsLoadingDuringStartup) ||
-	    (f == kSupportsDeleteSave) ||
-	    (f == kSavesSupportMetaInfo) ||
-	    (f == kSavesSupportThumbnail) ||
-	    (f == kSavesSupportCreationDate);
-}
-
-int HugoMetaEngine::getMaximumSaveSlot() const { return 99; }
-
-SaveStateList HugoMetaEngine::listSaves(const char *target) const {
-	Common::SaveFileManager *saveFileMan = g_system->getSavefileManager();
-	Common::StringArray filenames;
-	Common::String pattern = target;
-	pattern += "-##.SAV";
-
-	filenames = saveFileMan->listSavefiles(pattern);
-
-	SaveStateList saveList;
-	char slot[3];
-	for (Common::StringArray::const_iterator filename = filenames.begin(); filename != filenames.end(); ++filename) {
-		slot[0] = filename->c_str()[filename->size() - 6];
-		slot[1] = filename->c_str()[filename->size() - 5];
-		slot[2] = '\0';
-		// Obtain the last 2 digits of the filename (without extension), since they correspond to the save slot
-		int slotNum = atoi(slot);
-		if (slotNum >= 0 && slotNum <= getMaximumSaveSlot()) {
-			Common::InSaveFile *file = saveFileMan->openForLoading(*filename);
-			if (file) {
-				int saveVersion = file->readByte();
-
-				if (saveVersion != kSavegameVersion) {
-					warning("Savegame of incompatible version");
-					delete file;
-					continue;
-				}
-
-				// read name
-				uint16 nameSize = file->readUint16BE();
-				if (nameSize >= 255) {
-					delete file;
-					continue;
-				}
-				char name[256];
-				file->read(name, nameSize);
-				name[nameSize] = 0;
-
-				saveList.push_back(SaveStateDescriptor(slotNum, name));
-				delete file;
-			}
-		}
-	}
-
-	// Sort saves based on slot number.
-	Common::sort(saveList.begin(), saveList.end(), SaveStateDescriptorSlotComparator());
-	return saveList;
-}
-
-SaveStateDescriptor HugoMetaEngine::querySaveMetaInfos(const char *target, int slot) const {
-	Common::String fileName = Common::String::format("%s-%02d.SAV", target, slot);
-	Common::InSaveFile *file = g_system->getSavefileManager()->openForLoading(fileName);
-
-	if (file) {
-		int saveVersion = file->readByte();
-
-		if (saveVersion != kSavegameVersion) {
-			warning("Savegame of incompatible version");
-			delete file;
-			return SaveStateDescriptor();
-		}
-
-		uint32 saveNameLength = file->readUint16BE();
-		char saveName[256];
-		file->read(saveName, saveNameLength);
-		saveName[saveNameLength] = 0;
-
-		SaveStateDescriptor desc(slot, saveName);
-
-		Graphics::Surface *thumbnail;
-		if (!Graphics::loadThumbnail(*file, thumbnail)) {
-			warning("Missing or broken savegame thumbnail");
-			delete file;
-			return SaveStateDescriptor();
-		}
-		desc.setThumbnail(thumbnail);
-
-		uint32 saveDate = file->readUint32BE();
-		uint16 saveTime = file->readUint16BE();
-
-		int day = (saveDate >> 24) & 0xFF;
-		int month = (saveDate >> 16) & 0xFF;
-		int year = saveDate & 0xFFFF;
-
-		desc.setSaveDate(year, month, day);
-
-		int hour = (saveTime >> 8) & 0xFF;
-		int minutes = saveTime & 0xFF;
-
-		desc.setSaveTime(hour, minutes);
-
-		// Slot 0 is used for the 'restart game' save in all Hugo games, thus
-		// we prevent it from being deleted.
-		desc.setDeletableFlag(slot != 0);
-		desc.setWriteProtectedFlag(slot == 0);
-
-		delete file;
-		return desc;
-	}
-	return SaveStateDescriptor();
-}
-
-void HugoMetaEngine::removeSaveState(const char *target, int slot) const {
-	Common::String fileName = Common::String::format("%s-%02d.SAV", target, slot);
-	g_system->getSavefileManager()->removeSavefile(fileName);
-}
 } // End of namespace Hugo
 
-#if PLUGIN_ENABLED_DYNAMIC(HUGO)
-	REGISTER_PLUGIN_DYNAMIC(HUGO, PLUGIN_TYPE_ENGINE, Hugo::HugoMetaEngine);
-#else
-	REGISTER_PLUGIN_STATIC(HUGO, PLUGIN_TYPE_ENGINE, Hugo::HugoMetaEngine);
-#endif
-
-namespace Hugo {
-
-void HugoEngine::initGame(const HugoGameDescription *gd) {
-	_gameType = gd->gameType;
-	_platform = gd->desc.platform;
-	_packedFl = (getFeatures() & GF_PACKED);
-	_gameVariant = _gameType - 1 + ((_platform == Common::kPlatformWindows) ? 0 : 3);
-}
-
-} // End of namespace Hugo
+REGISTER_PLUGIN_STATIC(HUGO_DETECTION, PLUGIN_TYPE_METAENGINE, Hugo::HugoMetaEngine);
