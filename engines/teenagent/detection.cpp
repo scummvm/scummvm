2aca677fcc9c7bd1487fae8be51758dd325f4486
aryanrawlani28 <aryanrawlani007@gmail.com>
2020-08-06 23:16:45 +0530

    TEENAGENT: Split detection features & adapt to new plugins.

--- engines/teenagent/detection.cpp
+++ engines/teenagent/detection.cpp
@@ -20,16 +20,11 @@
  *
  */
 
-#include "common/system.h"
-#include "common/savefile.h"
 #include "common/algorithm.h"
 
 #include "base/plugins.h"
 
 #include "engines/advancedDetector.h"
-#include "teenagent/resources.h"
-#include "teenagent/teenagent.h"
-#include "graphics/thumbnail.h"
 
 static const PlainGameDescriptor teenAgentGames[] = {
 	{ "teenagent", "Teen Agent" },
@@ -81,9 +76,7 @@ static const ADGameDescription teenAgentGameDescriptions[] = {
 	AD_TABLE_END_MARKER,
 };
 
-enum {
-	MAX_SAVES = 20
-};
+
 
 class TeenAgentMetaEngine : public AdvancedMetaEngine {
 public:
@@ -101,98 +94,6 @@ public:
 	const char *getOriginalCopyright() const override {
 		return "TEENAGENT (C) 1994 Metropolis";
 	}
-
-	bool hasFeature(MetaEngineFeature f) const override {
-		switch (f) {
-		case kSupportsListSaves:
-		case kSupportsDeleteSave:
-		case kSupportsLoadingDuringStartup:
-		case kSavesSupportMetaInfo:
-		case kSavesSupportThumbnail:
-			return true;
-		default:
-			return false;
-		}
-	}
-
-	bool createInstance(OSystem *syst, Engine **engine, const ADGameDescription *desc) const override {
-		if (desc) {
-			*engine = new TeenAgent::TeenAgentEngine(syst, desc);
-		}
-		return desc != 0;
-	}
-
-	static Common::String generateGameStateFileName(const char *target, int slot) {
-		return Common::String::format("%s.%02d", target, slot);
-	}
-
-	SaveStateList listSaves(const char *target) const override {
-		Common::String pattern = target;
-		pattern += ".##";
-
-		Common::StringArray filenames = g_system->getSavefileManager()->listSavefiles(pattern);
-
-		SaveStateList saveList;
-		for (Common::StringArray::const_iterator file = filenames.begin(); file != filenames.end(); ++file) {
-			int slot = atoi(file->c_str() + file->size() - 2);
-			if (slot >= 0 && slot < MAX_SAVES) {
-				Common::ScopedPtr<Common::InSaveFile> in(g_system->getSavefileManager()->openForLoading(*file));
-				if (!in)
-					continue;
-
-				char buf[25];
-				in->seek(0);
-				in->read(buf, 24);
-				buf[24] = 0;
-				saveList.push_back(SaveStateDescriptor(slot, buf));
-			}
-		}
-		// Sort saves based on slot number.
-		Common::sort(saveList.begin(), saveList.end(), SaveStateDescriptorSlotComparator());
-		return saveList;
-	}
-
-	int getMaximumSaveSlot() const override {
-		return MAX_SAVES - 1;
-	}
-
-	void removeSaveState(const char *target, int slot) const override {
-		Common::String filename = generateGameStateFileName(target, slot);
-		g_system->getSavefileManager()->removeSavefile(filename);
-	}
-
-	SaveStateDescriptor querySaveMetaInfos(const char *target, int slot) const override {
-		Common::String filename = generateGameStateFileName(target, slot);
-		Common::ScopedPtr<Common::InSaveFile> in(g_system->getSavefileManager()->openForLoading(filename));
-		if (!in)
-			return SaveStateDescriptor();
-
-		char buf[25];
-		in->seek(0);
-		in->read(buf, 24);
-		buf[24] = 0;
-
-		Common::String desc = buf;
-
-		in->seek(TeenAgent::saveStateSize);
-		if (!Graphics::checkThumbnailHeader(*in))
-			return SaveStateDescriptor(slot, desc);
-
-		SaveStateDescriptor ssd(slot, desc);
-
-		//checking for the thumbnail
-		Graphics::Surface *thumbnail;
-		if (!Graphics::loadThumbnail(*in, thumbnail)) {
-			return SaveStateDescriptor();
-		}
-		ssd.setThumbnail(thumbnail);
-
-		return ssd;
-	}
 };
 
-#if PLUGIN_ENABLED_DYNAMIC(TEENAGENT)
-	REGISTER_PLUGIN_DYNAMIC(TEENAGENT, PLUGIN_TYPE_ENGINE, TeenAgentMetaEngine);
-#else
-	REGISTER_PLUGIN_STATIC(TEENAGENT, PLUGIN_TYPE_ENGINE, TeenAgentMetaEngine);
-#endif
+REGISTER_PLUGIN_STATIC(TEENAGENT_DETECTION, PLUGIN_TYPE_METAENGINE, TeenAgentMetaEngine);
