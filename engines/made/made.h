53131399f9b10a9c9622f96faba60b5757398f26
aryanrawlani28 <aryanrawlani007@gmail.com>
2020-08-06 22:58:18 +0530

    MADE: Move common game/detection related code into new header files
    - Move enums from made.h -> detection_enums.h
    - Move MadeGameDescription struct inside detector tables -> detection.h

--- engines/made/made.h
+++ engines/made/made.h
@@ -24,6 +24,7 @@
 #define MADE_MADE_H
 
 #include "made/sound.h"
+#include "made/detection_enums.h"
 
 #include "engines/engine.h"
 
@@ -41,20 +42,6 @@
  * - Rodney's Funscreen
  */
 namespace Made {
-
-enum MadeGameID {
-	GID_RTZ		= 0,
-	GID_MANHOLE	= 1,
-	GID_LGOP2	= 2,
-	GID_RODNEY	= 3
-};
-
-enum MadeGameFeatures {
-	GF_DEMO				= 1 << 0,
-	GF_CD				= 1 << 1,
-	GF_CD_COMPRESSED	= 1 << 2,
-	GF_FLOPPY			= 1 << 3
-};
 
 const uint32 kTimerResolution = 40;
 
