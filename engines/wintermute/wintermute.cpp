0b9df0411259fe9c90e5f66c183fc8317805eff9
aryanrawlani28 <aryanrawlani007@gmail.com>
2020-08-07 19:17:09 +0530

    WINTERMUTE: Rename game_description.h -> detection.h
    - Done to match consistency with other engines.
    - Remove external header file.
    - Match namecalling of header where needed.

--- engines/wintermute/wintermute.cpp
+++ engines/wintermute/wintermute.cpp
@@ -35,9 +35,9 @@
 #include "engines/wintermute/ad/ad_game.h"
 #include "engines/wintermute/wintermute.h"
 #include "engines/wintermute/debugger.h"
-#include "engines/wintermute/game_description.h"
 #include "engines/wintermute/platform_osystem.h"
 #include "engines/wintermute/base/base_engine.h"
+#include "engines/wintermute/detection.h"
 
 #include "engines/wintermute/base/sound/base_sound_manager.h"
 #include "engines/wintermute/base/base_file_manager.h"
