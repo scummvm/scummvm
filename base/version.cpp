453a5ad2813738667085ca711231b712f522d48c
BeWorld <36823759+BeWorld2018@users.noreply.github.com>
2020-08-07 19:22:13 +0200

    MORPHOS: simplify VERSION
    - Same as AmigaOS4, add AMIGA format DATE
    - Configure : force optimization level on release build

--- base/version.cpp
+++ base/version.cpp
@@ -56,11 +56,8 @@
  * to properly work in exports (i.e. release tar balls etc.).
  */
 const char *gScummVMVersion = SCUMMVM_VERSION SCUMMVM_REVISION;
-#ifdef __amigaos4__
+#if defined(__amigaos4__) || defined(__MORPHOS__)
 static const char *version_cookie __attribute__((used)) = "$VER: ScummVM " SCUMMVM_VERSION SCUMMVM_REVISION " (" AMIGA_DATE ")";
-#endif
-#ifdef __MORPHOS__
-static const char *version_cookie __attribute__((used)) = "$VER: ScummVM " SCUMMVM_VERSION SCUMMVM_REVISION " (" __DATE__ ")";
 #endif
 const char *gScummVMBuildDate = __DATE__ " " __TIME__;
 const char *gScummVMVersionDate = SCUMMVM_VERSION SCUMMVM_REVISION " (" __DATE__ " " __TIME__ ")";
