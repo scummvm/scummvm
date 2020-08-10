3bd3dee13d196ef8d051b60146ef9e9d0213624e
Nathanael Gentry <nathanael.gentrydb8@gmail.com>
2020-08-10 14:41:51 -0400

    DIRECTOR: Make missing surface warning more helpful

--- engines/director/window.cpp
+++ engines/director/window.cpp
@@ -188,7 +188,7 @@ void Window::inkBlitFrom(Channel *channel, Common::Rect destRect, Graphics::Mana
 			inkBlitSurface(&pd, srcRect, channel->getMask());
 		}
 	} else {
-		warning("Window::inkBlitFrom: No source surface");
+		warning("Window::inkBlitFrom: No source surface: spriteType: %d, castType: %d, castId: %d", channel->_sprite->_spriteType, channel->_sprite->_cast ? channel->_sprite->_cast->_type : 0, channel->_sprite->_castId);
 	}
 }
 
