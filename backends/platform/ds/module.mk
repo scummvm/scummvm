3c306f33ed8c31c9136a0b1c186470af917b6b5a
Cameron Cawley <ccawley2011@gmail.com>
2020-08-11 17:52:49 +0100

    DS: Simplify the directory structure

--- backends/platform/ds/module.mk
+++ backends/platform/ds/module.mk
@@ -1,10 +1,10 @@
 MODULE := backends/platform/ds
 
 MODULE_OBJS := \
-	arm9/source/blitters_arm.o \
-	arm9/source/dsmain.o \
-	arm9/source/osystem_ds.o \
-	arm9/source/dsoptions.o
+	blitters_arm.o \
+	dsmain.o \
+	osystem_ds.o \
+	dsoptions.o
 
 # We don't use rules.mk but rather manually update OBJS and MODULE_DIRS.
 MODULE_OBJS := $(addprefix $(MODULE)/, $(MODULE_OBJS))
