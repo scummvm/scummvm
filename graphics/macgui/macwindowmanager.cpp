29533d9c5e55362e47ce661c64385342071377a3
Eugene Sandulenko <sev@scummvm.org>
2020-08-15 12:17:57 +0200

    GRAPHICS: MACGUI: Fix typo in findBestColor

--- graphics/macgui/macwindowmanager.cpp
+++ graphics/macgui/macwindowmanager.cpp
@@ -943,7 +943,7 @@ uint MacWindowManager::findBestColor(byte cr, byte cg, byte cb) {
 	uint bestColor = 0;
 	double min = 0xFFFFFFFF;
 
-	if (_pixelformat.bytesPerPixel == 24)
+	if (_pixelformat.bytesPerPixel == 4)
 		return cr << 24 | cg << 16 | cb << 8 | 0xff;
 
 	uint32 color = cr << 16 | cg << 8 | cb;
