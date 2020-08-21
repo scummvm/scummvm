dd13d1087f1e57593eccaff653631a33ddf4fde7
aryanrawlani28 <aryanrawlani007@gmail.com>
2020-08-21 14:10:20 +0530

    SCUMM: GUI: U32: Remove redundant code
    - Translations are removed from the md5 warning message because logMessage doesn't support unicode.

--- engines/scumm/detection.cpp
+++ engines/scumm/detection.cpp
@@ -1135,23 +1135,16 @@ Common::Error ScummMetaEngine::createInstance(OSystem *syst, Engine **engine) co
 	// debugging purposes).
 	if (!findInMD5Table(res.md5.c_str())) {
 		Common::String md5Warning;
-		Common::String additionalInfo;
-		// Common::U32String md5WarningTranslated;
 
 		md5Warning = ("Your game version appears to be unknown. If this is *NOT* a fan-modified\n"
 		               "version (in particular, not a fan-made translation), please, report the\n"
 		               "following data to the ScummVM team along with the name of the game you tried\n"
 		               "to add and its version, language, etc.:\n");
 
-		// md5WarningTranslated = _(md5Warning);
-
-		additionalInfo = Common::String::format("  SCUMM gameid '%s', file '%s', MD5 '%s'\n\n",
+		md5Warning += Common::String::format("  SCUMM gameid '%s', file '%s', MD5 '%s'\n\n",
 				res.game.gameid,
 				generateFilenameForDetection(res.fp.pattern, res.fp.genMethod, res.game.platform).c_str(),
 				res.md5.c_str());
-
-		md5Warning += additionalInfo;
-		// md5WarningTranslated += additionalInfo;
 
 		g_system->logMessage(LogMessageType::kWarning, md5Warning.c_str());
 	} else {
