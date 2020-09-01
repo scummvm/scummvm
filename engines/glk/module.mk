a56b85089c1c1080c28f70e53970c47d3c042f41
aryanrawlani28 <aryanrawlani007@gmail.com>
2020-09-01 17:30:30 +0530

    GLK: Fix modules to build
    - Creating an instance of GLK requires searching through all sub-engine detection lists.
    - So, these must be there at the time of creating an instance of GLK.
    - Hence, all these objects will ONLY be skipped from static-detection, if GLK itself is static.
    - The above point is because it will need to avoid duplicate symbols.

--- engines/glk/module.mk
+++ engines/glk/module.mk
@@ -296,7 +296,25 @@ MODULE_OBJS := \
 	zcode/quetzal.o \
 	zcode/screen.o \
 	zcode/sound_folder.o \
-	zcode/windows.o
+	zcode/windows.o \
+	detection.o \
+	adrift/detection.o \
+	advsys/detection.o \
+	agt/detection.o \
+	alan2/detection.o \
+	alan3/detection.o \
+	archetype/detection.o \
+	comprehend/detection.o \
+	glulx/detection.o \
+	hugo/detection.o \
+	jacl/detection.o \
+	level9/detection.o \
+	magnetic/detection.o \
+	quest/detection.o \
+	scott/detection.o \
+	tads/detection.o \
+	zcode/detection.o
+
 
 # This module can be built as a plugin
 ifeq ($(ENABLE_GLK), DYNAMIC_PLUGIN)
@@ -306,6 +324,9 @@ endif
 # Include common rules
 include $(srcdir)/rules.mk
 
+# Skip building the following objects if a static
+# module is enabled, because it already has the contents.
+ifneq ($(ENABLE_GLK), STATIC_PLUGIN)
 # Detection objects
 DETECT_OBJS += $(MODULE)/detection.o
 
@@ -327,9 +348,6 @@ DETECT_OBJS += $(MODULE)/scott/detection.o
 DETECT_OBJS += $(MODULE)/tads/detection.o
 DETECT_OBJS += $(MODULE)/zcode/detection.o
 
-# Skip building the following objects if a static
-# module is enabled, because it already has the contents.
-ifneq ($(ENABLE_GLK), STATIC_PLUGIN)
 # Dependencies of detection objects
 DETECT_OBJS += $(MODULE)/blorb.o
 DETECT_OBJS += $(MODULE)/advsys/game.o
