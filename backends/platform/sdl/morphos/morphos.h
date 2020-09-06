c4d418d3531b28c8f0d576ec16a789de2a83016c
BeWorld <36823759+BeWorld2018@users.noreply.github.com>
2020-09-06 08:37:37 +0200

    MORPHOS : add kFeatureOpenUrl
    Enable OpenURL

--- backends/platform/sdl/morphos/morphos.h
+++ backends/platform/sdl/morphos/morphos.h
@@ -28,6 +28,7 @@
 class OSystem_MorphOS : public OSystem_SDL {
 public:
 	virtual void init() override;
+	virtual bool hasFeature(Feature f);
 	virtual bool openUrl(const Common::String &url) override;
 };
 
