5d3fed88f7a81ff1b72225a37f7ee33d21fb6b60
Eugene Sandulenko <sev@scummvm.org>
2020-09-13 17:54:45 +0200

    GCW0: Disable GLK engine

--- backends/platform/dingux/build.gcw0.sh
+++ backends/platform/dingux/build.gcw0.sh
@@ -3,4 +3,4 @@
 export PATH=/opt/gcw0-toolchain/usr/bin:$PATH
 
 # Disable high resolution engines since we have 320x240 hardware
-./configure --host=gcw0 --enable-plugins --default-dynamic --enable-release && make -j6 gcw-opk && ls -l scummvm.opk
+./configure --host=gcw0 --enable-plugins --default-dynamic --enable-release --disable-engine=glk && make -j6 gcw-opk && ls -l scummvm.opk
