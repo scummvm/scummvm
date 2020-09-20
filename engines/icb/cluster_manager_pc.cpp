94d692748c7d8ff2aa9461a1f125b52362970ed8
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2020-09-20 20:52:28 +0200

    ICB: Fixed strings encondings

--- engines/icb/cluster_manager_pc.cpp
+++ engines/icb/cluster_manager_pc.cpp
@@ -131,7 +131,7 @@ void ClusterManager::Initialise() {
 
 	if (strcmp(testline, "Please insert disc %d") == 0)
 		m_currentLanguage = T_ENGLISH;
-	else if (strcmp(testline, "Veuillez insйrer le disque %d") == 0)
+	else if (strcmp(testline, "Veuillez ins\xE9rer le disque %d") == 0)
 		m_currentLanguage = T_FRENCH;
 	else if (strcmp(testline, "Inserisci il disco %d") == 0)
 		m_currentLanguage = T_ITALIAN;
@@ -139,7 +139,7 @@ void ClusterManager::Initialise() {
 		m_currentLanguage = T_GERMAN;
 	else if (strcmp(testline, "Por favor, inserta el disco %d") == 0)
 		m_currentLanguage = T_SPANISH;
-	else if (strcmp(testline, "Вставьте диск %d") == 0)
+	else if (strcmp(testline, "\xC2\xF1\xF2\xE0\xE2\xFC\xF2\xE5 \xE4\xE8\xF1\xEA %d") == 0)
 		m_currentLanguage = T_RUSSIAN;
 	else
 		// Must be polish by default
