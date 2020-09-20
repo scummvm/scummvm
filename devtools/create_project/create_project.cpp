5836a766fe81e113be2e475d923c5a7e28b256a8
Lothar Serra Mari <mail@serra.me>
2020-09-20 23:38:23 +0200

    DISTS: Add BUILTIN_RESOURCES define to create_project

--- devtools/create_project/create_project.cpp
+++ devtools/create_project/create_project.cpp
@@ -1036,27 +1036,28 @@ const Feature s_features[] = {
 	{   "discord",     "USE_DISCORD", true, false, "Discord support" },
 
 	// Feature flags
-	{            "bink",                      "USE_BINK", false, true,  "Bink video support" },
-	{         "scalers",                   "USE_SCALERS", false, true,  "Scalers" },
-	{       "hqscalers",                "USE_HQ_SCALERS", false, true,  "HQ scalers" },
-	{           "16bit",                 "USE_RGB_COLOR", false, true,  "16bit color support" },
-	{         "highres",                   "USE_HIGHRES", false, true,  "high resolution" },
-	{         "mt32emu",                   "USE_MT32EMU", false, true,  "integrated MT-32 emulator" },
-	{             "lua",                       "USE_LUA", false, true,  "lua" },
-	{            "nasm",                      "USE_NASM", false, true,  "IA-32 assembly support" }, // This feature is special in the regard, that it needs additional handling.
-	{          "opengl",                    "USE_OPENGL", false, true,  "OpenGL support" },
-	{        "opengles",                      "USE_GLES", false, true,  "forced OpenGL ES mode" },
-	{         "taskbar",                   "USE_TASKBAR", false, true,  "Taskbar integration support" },
-	{           "cloud",                     "USE_CLOUD", false, true,  "Cloud integration support" },
-	{     "translation",               "USE_TRANSLATION", false, true,  "Translation support" },
-	{          "vkeybd",                 "ENABLE_VKEYBD", false, false, "Virtual keyboard support"},
-	{   "eventrecorder",          "ENABLE_EVENTRECORDER", false, false, "Event recorder support"},
-	{         "updates",                   "USE_UPDATES", false, false, "Updates support"},
-	{         "dialogs",                "USE_SYSDIALOGS", false, true,  "System dialogs support"},
-	{      "langdetect",                "USE_DETECTLANG", false, true,  "System language detection support" }, // This feature actually depends on "translation", there
+	{             "bink",                      "USE_BINK", false, true,  "Bink video support" },
+	{          "scalers",                   "USE_SCALERS", false, true,  "Scalers" },
+	{        "hqscalers",                "USE_HQ_SCALERS", false, true,  "HQ scalers" },
+	{            "16bit",                 "USE_RGB_COLOR", false, true,  "16bit color support" },
+	{          "highres",                   "USE_HIGHRES", false, true,  "high resolution" },
+	{          "mt32emu",                   "USE_MT32EMU", false, true,  "integrated MT-32 emulator" },
+	{              "lua",                       "USE_LUA", false, true,  "lua" },
+	{             "nasm",                      "USE_NASM", false, true,  "IA-32 assembly support" }, // This feature is special in the regard, that it needs additional handling.
+	{           "opengl",                    "USE_OPENGL", false, true,  "OpenGL support" },
+	{         "opengles",                      "USE_GLES", false, true,  "forced OpenGL ES mode" },
+	{          "taskbar",                   "USE_TASKBAR", false, true,  "Taskbar integration support" },
+	{            "cloud",                     "USE_CLOUD", false, true,  "Cloud integration support" },
+	{      "translation",               "USE_TRANSLATION", false, true,  "Translation support" },
+	{           "vkeybd",                 "ENABLE_VKEYBD", false, false, "Virtual keyboard support"},
+	{    "eventrecorder",          "ENABLE_EVENTRECORDER", false, false, "Event recorder support"},
+	{          "updates",                   "USE_UPDATES", false, false, "Updates support"},
+	{          "dialogs",                "USE_SYSDIALOGS", false, true,  "System dialogs support"},
+	{       "langdetect",                "USE_DETECTLANG", false, true,  "System language detection support" }, // This feature actually depends on "translation", there
 	                                                                                                           // is just no current way of properly detecting this...
-	{    "text-console", "USE_TEXT_CONSOLE_FOR_DEBUGGER", false, false, "Text console debugger" }, // This feature is always applied in xcode projects
-	{             "tts",                       "USE_TTS", false, true,  "Text to speech support"}
+	{     "text-console", "USE_TEXT_CONSOLE_FOR_DEBUGGER", false, false, "Text console debugger" }, // This feature is always applied in xcode projects
+	{              "tts",                       "USE_TTS", false, true,  "Text to speech support"},
+	{"builtin-resources",             "BUILTIN_RESOURCES", false, true,  "include resources (e.g. engine data, fonts) into the binary"}
 };
 
 const Tool s_tools[] = {
