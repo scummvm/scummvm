b15b394f0c6629d80349622f0024fd11d824cb78
antoniou <a.antoniou79@gmail.com>
2020-09-24 10:13:12 +0300

    ANDROID: Comment out the logfile code
    Since currently the logFile stuff are specific to SDL_BACKED

--- backends/platform/android/android.h
+++ backends/platform/android/android.h
@@ -129,8 +129,7 @@ public:
 	virtual Audio::Mixer *getMixer();
 	virtual void getTimeAndDate(TimeDate &t) const;
 	virtual void logMessage(LogMessageType::Type type, const char *message);
-	virtual void addSysArchivesToSearchSet(Common::SearchSet &s,
-											int priority = 0);
+	virtual void addSysArchivesToSearchSet(Common::SearchSet &s, int priority = 0);
 	virtual bool openUrl(const Common::String &url);
 	virtual bool hasTextInClipboard();
 	virtual Common::String getTextFromClipboard();
