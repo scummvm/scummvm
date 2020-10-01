76ec66bfa6149a124cfb2b0e4fd5287ae08f68c3
Eugene Sandulenko <sev@scummvm.org>
2020-10-01 02:08:39 +0200

    Revert "GUI: Properly load fonts when translations are disabled"
    This reverts commit 62c4d6cc0a7c004565d0cc093b81e4813588ad60.
    
    This broke non-Latin fonts loading.

--- gui/ThemeEngine.cpp
+++ gui/ThemeEngine.cpp
@@ -529,7 +529,7 @@ bool ThemeEngine::addFont(TextData textId, const Common::String &language, const
 	if (textId == -1)
 		return false;
 
-	if (!language.empty() && !language.equals("*")) {
+	if (!language.empty()) {
 #ifdef USE_TRANSLATION
 		Common::String cl = TransMan.getCurrentLanguage();
 		if (!cl.matchString(language, true))
