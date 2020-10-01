4b4b057bcc13570ce837f73f1b23226ceb4895aa
cadihowley <cadihowley@gmail.com>
2020-10-01 20:16:13 +1300

    DOCS: adds tabs extension

--- doc/docportal/conf.py
+++ doc/docportal/conf.py
@@ -33,8 +33,10 @@ import sphinx_rtd_theme
 extensions = [
     
     "sphinx_rtd_theme",
-    "recommonmark"
+    "recommonmark",
+    
 ]
+extensions.append('sphinx_tabs.tabs')
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -55,6 +57,7 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+
+
 master_doc = 'index'
 html_sidebars = {'**': ['localtoc.html']}
