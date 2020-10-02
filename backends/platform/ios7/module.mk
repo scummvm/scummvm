b1fcc7cb89d06f65f706227e2c6d7208d41c3c14
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2020-10-02 20:01:17 +0200

    BACKENDS: Remove not used code

--- backends/platform/ios7/module.mk
+++
@@ -1,8 +0,0 @@
-MODULE := backends/platform/ios7
-
-MODULE_OBJS := \
-
-# We don't use rules.mk but rather manually update OBJS and MODULE_DIRS.
-MODULE_OBJS := $(addprefix $(MODULE)/, $(MODULE_OBJS))
-OBJS := $(MODULE_OBJS) $(OBJS)
-MODULE_DIRS += $(sort $(dir $(MODULE_OBJS)))
