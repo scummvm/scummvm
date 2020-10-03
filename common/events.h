2ef7bfc3efc0bac9fd57ca2f4dfe3a5cfaf8f598
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2020-10-03 17:34:00 +0200

    COMMON: Remove not used anymore joystick events entries

--- common/events.h
+++ common/events.h
@@ -155,12 +155,7 @@ enum JoystickButton {
 	JOYSTICK_BUTTON_DPAD_UP,
 	JOYSTICK_BUTTON_DPAD_DOWN,
 	JOYSTICK_BUTTON_DPAD_LEFT,
-	JOYSTICK_BUTTON_DPAD_RIGHT,
-/* START of ResidualVM-specific code */
-	// Left and right trigger button events are generated in place of the axis events for now
-	JOYSTICK_BUTTON_LEFT_TRIGGER,
-	JOYSTICK_BUTTON_RIGHT_TRIGGER
-/* END of ResidualVM-specific code */
+	JOYSTICK_BUTTON_DPAD_RIGHT
 };
 
 /**
