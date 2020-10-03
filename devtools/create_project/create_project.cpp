7b20d552fddb44b58a9d5531daa417cd30f297c5
Thierry Crozat <criezy@scummvm.org>
2020-10-03 12:32:38 +0100

    DEVTOOLS: Add cxx11 feature to create_project

--- devtools/create_project/create_project.cpp
+++ devtools/create_project/create_project.cpp
@@ -1066,6 +1066,7 @@ const Feature s_features[] = {
 	{              "tts",                       "USE_TTS", false, true,  "Text to speech support"},
 	{"builtin-resources",             "BUILTIN_RESOURCES", false, true,  "include resources (e.g. engine data, fonts) into the binary"},
 	{"detection-static", "USE_DETECTION_FEATURES_STATIC",  "", true,  "Static linking of detection objects for engines."}
+	{            "cxx11",                     "USE_CXX11", false, true,  "Compile with c++11 support"}
 };
 
 const Tool s_tools[] = {
