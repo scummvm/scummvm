57e1084c27103fbab8f41a141b88fcddedd5a6cc
Paul Gilbert <dreammaster@scummvm.org>
2020-10-07 19:08:47 -0700

    GLK: Don't bother detecting game id clashes in release builds
    The code for detecting multiple Glk sub-engines using the same
    game Id is only relevant to debug builds when new games are
    first added. It's not needed for proper release builds

--- engines/glk/detection.cpp
+++ engines/glk/detection.cpp
@@ -315,8 +315,10 @@ PlainGameDescriptor GlkMetaEngine::findGame(const char *gameId) const {
 #undef FIND_GAME
 
 DetectedGames GlkMetaEngine::detectGames(const Common::FSList &fslist) const {
+#ifndef RELEASE_BUILD
 	// This is as good a place as any to detect multiple sub-engines using the same Ids
 	detectClashes();
+#endif
 
 	DetectedGames detectedGames;
 	Glk::Adrift::AdriftMetaEngine::detectGames(fslist, detectedGames);
