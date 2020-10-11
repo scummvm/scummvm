375de3894f311dca94fe9caf5e834e0526930d7c
BeWorld <36823759+BeWorld2018@users.noreply.github.com>
2020-10-11 13:10:07 +0200

    WINTERMUTE : mission one conversion int to int32

--- engines/wintermute/ad/ad_actor_3dx.cpp
+++ engines/wintermute/ad/ad_actor_3dx.cpp
@@ -2441,7 +2441,7 @@ bool AdActor3DX::updatePartEmitter() {
 
 	Math::Vector3d bonePos;
 	getBonePosition3D(_partBone.c_str(), &bonePos, &_partOffset);
-	int x = 0, y = 0;
+	int32 x = 0, y = 0;
 	static_cast<AdGame *>(_gameRef)->_scene->_sceneGeometry->convert3Dto2D(&bonePos, &x, &y);
 
 	_partEmitter->_posX = x - _gameRef->_renderer->_drawOffsetX;
