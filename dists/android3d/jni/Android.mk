cba8556622e2738ceccd5fcd51289d3db32e800f
Cameron Cawley <ccawley2011@gmail.com>
2020-10-14 20:37:14 +0100

    ANDROID3D: Get it working again

--- dists/android3d/jni/Android.mk
+++ dists/android3d/jni/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 APP_ABI := $(ABI)
-LOCAL_MODULE := residualvm
-LOCAL_SRC_FILES := ../../libresidualvm.so
+LOCAL_MODULE := scummvm
+LOCAL_SRC_FILES := ../libscummvm.so
 
 include $(PREBUILT_SHARED_LIBRARY)
