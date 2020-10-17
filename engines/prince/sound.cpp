0e3e70a4a44900132ed931e7d47721bb20b64dd6
Денис Телюх <iliotropion2006@ya.ru>
2020-10-18 00:49:34 +0700

    PRINCE: fixed bug in the conversation with the priest
    BUG: https://bugs.scummvm.org/ticket/11771
    This conversation have no audio files, because it is performed using gestures. So it shouldn't try to load audio.

--- engines/prince/sound.cpp
+++ engines/prince/sound.cpp
@@ -157,7 +157,7 @@ void PrinceEngine::setVoice(uint16 slot, uint32 sampleSlot, uint16 flag) {
 		sampleName = Common::String::format("inv%02d-01.WAV", currentString - 70000);
 	} else if (currentString >= 60000) {
 		sampleName = Common::String::format("M%04d-%02d.WAV", currentString - 60000, flag);
-	} else if (currentString >= 2000) {
+	} else if (currentString == 316 || currentString >= 2000) {
 		return;
 	} else if (flag >= 100) {
 		sampleName = Common::String::format("%03d-%03d.WAV", currentString, flag);
