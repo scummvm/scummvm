11368b7d9040c97e0b161cacfb6131a94a649615
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2020-10-18 14:24:11 -0700

    KYRA: Support Russian LoK translation by Siberian GRemlin (#2544)
    * KYRA: Support Russian LoK translation by Siberian Gremlin
    
    * Mention source of engine data translation
    
    * Mark Russian CD as fan translation to force subtitles

--- devtools/create_kyradat/games.cpp
+++ devtools/create_kyradat/games.cpp
@@ -50,6 +50,7 @@ const Game kyra1Games[] = {
 	{ kKyra1, kPlatformDOS, kTalkieVersion, FR_FRA },
 	{ kKyra1, kPlatformDOS, kTalkieVersion, IT_ITA },
 	{ kKyra1, kPlatformDOS, kTalkieVersion, ES_ESP },
+	{ kKyra1, kPlatformDOS, kTalkieVersion, RU_RUS },
 
 	{ kKyra1, kPlatformFMTowns, kNoSpecial, EN_ANY },
 	{ kKyra1, kPlatformFMTowns, kNoSpecial, JA_JPN },
