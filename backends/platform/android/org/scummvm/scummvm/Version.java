e1bff1cbb3b51f18dac28a8432fc5be3d5783b2c
Matthew Duggan <mgithub@guarana.org>
2020-10-20 20:41:38 +0900

    ANDROID: Avoid crash if version no is not int
    This can happen during development - shouldn't happen for any official release,
    but even if it does let's just assume versions are different and default to
    upgrading the config.

--- backends/platform/android/org/scummvm/scummvm/Version.java
+++ backends/platform/android/org/scummvm/scummvm/Version.java
@@ -36,15 +36,19 @@ public class Version implements Comparable<Version> {
 		String[] thisParts = this.get().split("\\.");
 		String[] thatParts = that.get().split("\\.");
 		int length = Math.max(thisParts.length, thatParts.length);
-		for(int i = 0; i < length; i++) {
-			int thisPart = i < thisParts.length ?
-				Integer.parseInt(thisParts[i]) : 0;
-			int thatPart = i < thatParts.length ?
-				Integer.parseInt(thatParts[i]) : 0;
-			if(thisPart < thatPart)
-				return -1;
-			if(thisPart > thatPart)
-				return 1;
+		try {
+			for (int i = 0; i < length; i++) {
+				int thisPart = i < thisParts.length ?
+					Integer.parseInt(thisParts[i]) : 0;
+				int thatPart = i < thatParts.length ?
+					Integer.parseInt(thatParts[i]) : 0;
+				if (thisPart < thatPart)
+					return -1;
+				if (thisPart > thatPart)
+					return 1;
+			}
+		} catch (NumberFormatException e) {
+			return 1;
 		}
 		return 0;
 	}
