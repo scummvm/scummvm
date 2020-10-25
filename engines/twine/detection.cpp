1cf46c4ee0232dd3b2aee5b5d4728697e7282fb3
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-10-25 09:22:58 +0100

    TWINE: added german and french to the detection entry
    see https://github.com/scummvm/scummvm/pull/2557

--- engines/twine/detection.cpp
+++ engines/twine/detection.cpp
@@ -68,6 +68,24 @@ static const ADGameDescription twineGameDescriptions[] = {
 		ADGF_UNSTABLE,
 		GUIO1(GUIO_NONE)
 	},
+	{
+		"twine",
+		"",
+		AD_ENTRY1s("text.hqr", "31d880f658cc6cc6d6cf70df732aec4f", 248829),
+		Common::FR_FRA,
+		Common::kPlatformDOS,
+		ADGF_UNSTABLE,
+		GUIO1(GUIO_NONE)
+	},
+	{
+		"twine",
+		"",
+		AD_ENTRY1s("text.hqr", "31d880f658cc6cc6d6cf70df732aec4f", 248829),
+		Common::DE_DEU,
+		Common::kPlatformDOS,
+		ADGF_UNSTABLE,
+		GUIO1(GUIO_NONE)
+	},
 	AD_TABLE_END_MARKER
 };
 
