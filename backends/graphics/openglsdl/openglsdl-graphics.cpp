0128d5e3b5efccf3fae40d225f151d6621486f91
SupSuper <supsuper@gmail.com>
2020-11-01 20:10:15 +0000

    OPENGLSDL: Fix SDL asserting when getting fullscreen state
    This fixes a null pointer being passed to SDL_GetWindowFlags
    when there is no window yet, since the condition was incomplete

--- backends/graphics/openglsdl/openglsdl-graphics.cpp
+++ backends/graphics/openglsdl/openglsdl-graphics.cpp
@@ -237,7 +237,7 @@ bool OpenGLSdlGraphicsManager::getFeatureState(OSystem::Feature f) const {
 	switch (f) {
 	case OSystem::kFeatureFullscreenMode:
 #if SDL_VERSION_ATLEAST(2, 0, 0)
-		if (_window) {
+		if (_window && _window->getSDLWindow()) {
 			return (SDL_GetWindowFlags(_window->getSDLWindow()) & SDL_WINDOW_FULLSCREEN) != 0;
 		} else {
 			return _wantsFullScreen;
