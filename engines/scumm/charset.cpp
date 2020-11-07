6000a0b472f4b6794d2f9e78f421c34aac4cd13e
athrxx <athrxx@scummvm.org>
2020-11-07 19:47:23 +0100

    SCUMM: (MI1/SegaCD/JP) -  fix bug 11943
    (SCUMM: Invalid text rendering with Monkey Island 1 (SegaCD Japanese))
    
    Only add vs->xstart offset if the font is actually rendered on the vs.

--- engines/scumm/charset.cpp
+++ engines/scumm/charset.cpp
@@ -1124,7 +1124,7 @@ void CharsetRendererClassic::printCharIntern(bool is2byte, const byte *charPtr, 
 	} else {
 		Graphics::Surface dstSurface;
 		Graphics::Surface backSurface;
-		if ((ignoreCharsetMask || !vs->hasTwoBuffers)) {
+		if (ignoreCharsetMask || !vs->hasTwoBuffers) {
 			dstSurface = *vs;
 			dstPtr = vs->getPixels(_left, drawTop);
 		} else {
@@ -1144,7 +1144,7 @@ void CharsetRendererClassic::printCharIntern(bool is2byte, const byte *charPtr, 
 		}
 
 		if (is2byte && _vm->_game.platform != Common::kPlatformFMTowns)
-			drawBits1(dstSurface, _left + vs->xstart, drawTop, charPtr, drawTop, origWidth, origHeight);
+			drawBits1(dstSurface, (ignoreCharsetMask || !vs->hasTwoBuffers) ? _left + vs->xstart : _left, drawTop, charPtr, drawTop, origWidth, origHeight);
 		else
 			drawBitsN(dstSurface, dstPtr, charPtr, *_fontPtr, drawTop, origWidth, origHeight);
 
