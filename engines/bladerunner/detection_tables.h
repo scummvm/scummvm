ab3ad6e1fe36a4efb53bd2eb1f294034d3e693e7
Eugene Sandulenko <sev@scummvm.org>
2020-11-10 02:15:03 +0100

    BLADERUNNER: Disable Siberian Studio Russian fan translations

--- engines/bladerunner/detection_tables.h
+++ engines/bladerunner/detection_tables.h
@@ -23,6 +23,7 @@
 #ifndef BLADERUNNER_DETECTION_TABLES_H
 #define BLADERUNNER_DETECTION_TABLES_H
 
+#include "common/translation.h"
 #include "engines/advancedDetector.h"
 
 #define GAMEOPTION_SITCOM                     GUIO_GAMEOPTIONS1
@@ -92,22 +93,22 @@ static const ADGameDescription gameDescriptions[] = {
 	// BladeRunner (Russian - Fargus Multimedia + Home Systems, Inc. + Siberian Studio, R3) - RUS
 	{
 		"bladerunner",
-		0,
+		_s("The fan translator does not wish his translation to be incorporated into ScummVM."),
 		AD_ENTRY1s("STARTUP.MIX", "c198b54a5366b88b1734bbca21d3b192", 2678672),
 		Common::RU_RUS,
 		Common::kPlatformWindows,
-		ADGF_NO_FLAGS,
+		ADGF_UNSUPPORTED,
 		GUIO6(GAMEOPTION_SITCOM, GAMEOPTION_SHORTY, GAMEOPTION_FRAMELIMITER_NODELAYMILLIS, GAMEOPTION_FRAMELIMITER_FPS, GAMEOPTION_DISABLE_STAMINA_DRAIN, GUIO_NOMIDI)
 	},
 
 	// BladeRunner (Russian - Fargus Multimedia + Home Systems, Inc. + Siberian Studio, R4) - RUS
 	{
 		"bladerunner",
-		0,
+		_s("The fan translator does not wish his translation to be incorporated into ScummVM."),
 		AD_ENTRY1s("STARTUP.MIX", "d62498a7415682bb3ff86a894303c836", 2810053),
 		Common::RU_RUS,
 		Common::kPlatformWindows,
-		ADGF_NO_FLAGS,
+		ADGF_UNSUPPORTED,
 		GUIO6(GAMEOPTION_SITCOM, GAMEOPTION_SHORTY, GAMEOPTION_FRAMELIMITER_NODELAYMILLIS, GAMEOPTION_FRAMELIMITER_FPS, GAMEOPTION_DISABLE_STAMINA_DRAIN, GUIO_NOMIDI)
 	},
 
