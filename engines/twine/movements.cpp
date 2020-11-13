scummvm/scummvm

commit 955d2e71cfa648a83854b0f0c3eefc77724aa24c

TWINE: converted to boolean

engines/twine/movements.cpp
@@ -231,8 +231,7 @@ void Movements::update() {
 
 void Movements::processManualAction(int actorIdx) {
 	ActorStruct *actor = _engine->_scene->getActor(actorIdx);
-	// take this out when we want to give manual movements to other characters than Hero
-	if (actor == _engine->_scene->sceneHero) {
+	if (IS_HERO(actorIdx)) {
 		heroAction = false;
 
 		// If press W for action
@@ -254,7 +253,7 @@ void Movements::processManualAction(int actorIdx) {
 					heroMoved = true;
 					actor->angle = getRealAngle(&actor->move);
 					// TODO: previousLoopActionKey must be handled properly
-					if (!previousLoopActionKey || !actor->anim) {
+					if (!previousLoopActionKey || actor->anim == kAnimNone) {
 						const int32 aggresiveMode = _engine->getRandomNumber(3);
 
 						switch (aggresiveMode) {
@@ -291,7 +290,7 @@ void Movements::processManualAction(int actorIdx) {
 	}
 
 	if (_engine->_input->isActionActive(TwinEActionType::ThrowMagicBall) && !_engine->_gameState->gameFlags[GAMEFLAG_INVENTORY_DISABLED]) {
-		if (_engine->_gameState->usingSabre == 0) { // Use Magic Ball
+		if (!_engine->_gameState->usingSabre) { // Use Magic Ball
 			if (_engine->_gameState->gameFlags[InventoryItems::kiMagicBall]) {
 				if (_engine->_gameState->magicBallIdx == -1) {
 					_engine->_animations->initAnim(kThrowBall, 1, 0, actorIdx);

engines/twine/gamestate.cpp
@@ -103,7 +103,7 @@ void GameState::initHeroVars() {
 	inventoryNumKeys = 0;
 	inventoryMagicPoints = 0;
 
-	usingSabre = 0;
+	usingSabre = false;
 
 	_engine->_scene->sceneHero->body = 0;
 	_engine->_scene->sceneHero->life = 50;
@@ -139,7 +139,7 @@ void GameState::initEngineVars() {
 	_engine->_actor->cropBottomScreen = 0;
 
 	magicLevelIdx = 0;
-	usingSabre = 0;
+	usingSabre = false;
 
 	gameChapter = 0;
 

engines/twine/gamestate.h
@@ -139,7 +139,7 @@ public:
 	int16 inventoryNumGas = 0;
 
 	/** Its using FunFrock Sabre */
-	int16 usingSabre = 0;
+	bool usingSabre = false;
 
 	/** Inventory used flags */
 	uint8 inventoryFlags[NUM_INVENTORY_ITEMS];

engines/twine/twine.cpp
@@ -498,10 +498,10 @@ int32 TwinEEngine::runGameEngine() { // mainLoopInteration
 				warning("Use inventory [kiHolomap] not implemented!\n");
 				break;
 			case kiMagicBall:
-				if (_gameState->usingSabre == 1) {
+				if (_gameState->usingSabre) {
 					_actor->initModelActor(0, 0);
 				}
-				_gameState->usingSabre = 0;
+				_gameState->usingSabre = false;
 				break;
 			case kiUseSabre:
 				if (_scene->sceneHero->body != InventoryItems::kiUseSabre) {
@@ -511,7 +511,7 @@ int32 TwinEEngine::runGameEngine() { // mainLoopInteration
 					_actor->initModelActor(InventoryItems::kiUseSabre, 0);
 					_animations->initAnim(kSabreUnknown, 1, 0, 0);
 
-					_gameState->usingSabre = 1;
+					_gameState->usingSabre = true;
 				}
 				break;
 			case kiBookOfBu: {

commit 30cb2fb3cf62f954e79d36b65674ff2aaad7d566

TWINE: fixed kAggressive mode
broken in 955d2e71cfa648a83854b0f0c3eefc77724aa24c

engines/twine/movements.cpp
@@ -253,7 +253,7 @@ void Movements::processManualAction(int actorIdx) {
 					heroMoved = true;
 					actor->angle = getRealAngle(&actor->move);
 					// TODO: previousLoopActionKey must be handled properly
-					if (!previousLoopActionKey || actor->anim == kAnimNone) {
+					if (!previousLoopActionKey || actor->anim == kStanding) {
 						const int32 aggresiveMode = _engine->getRandomNumber(3);
 
 						switch (aggresiveMode) {