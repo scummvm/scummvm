fc90c205f19792ab161c2dc198e3241f7cad93f0
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2020-11-13 22:20:56 +0000

    AGDS: fixed object restart

--- engines/agds/screen.cpp
+++ engines/agds/screen.cpp
@@ -235,8 +235,11 @@ void Screen::load(const PatchPtr &patch) {
 		debug("patch object %s %d", object.name.c_str(), object.flag);
 		if (object.flag <= 0)
 			remove(object.name);
-		else if (!find(object.name)) {
-			_engine->runObject(object.name);
+		else {
+			auto instance = find(object.name); //find by name finds removed objects too
+			if (!instance || !instance->inScene()) {
+				_engine->runObject(object.name);
+			}
 		}
 	}
 	_applyingPatch = false;
