c9cba70a28a96a4e69a02bc64c6e7ce70ead1ac1
Matthew Duggan <mgithub@guarana.org>
2020-11-16 13:09:49 +0900

    ULTIMA8: Add weapon info field for default ammo count

--- engines/ultima/ultima8/world/weapon_info.h
+++ engines/ultima/ultima8/world/weapon_info.h
@@ -48,6 +48,7 @@ struct WeaponInfo {
 	uint16 _displayGumpShape; //! The gump shape to use for inventory display (3,4,5)
 	uint16 _displayGumpFrame; //!< The frame to use in the inventory gump
 	uint8 _small;	//! A flag whether or not the weapon is "small" (changes the animations used)
+	uint16 _defaultAmmo; //! Count of ammo the weapon starts with
 	uint8 _field8;	//! Not totally sure, used like "cycle time" in Attack Process
 
 	enum DmgType {
