e76e6a190774435b60629e5dd9debcc7f14c0cd0
Eugene Sandulenko <sev@scummvm.org>
2020-12-07 23:55:22 +0100

    TWINE: Fix Amiga compilation

--- engines/twine/sound.cpp
+++ engines/twine/sound.cpp
@@ -54,7 +54,7 @@ void Sound::setSamplePosition(int32 channelIdx, int32 x, int32 y, int32 z) {
 	const int32 camZ = _engine->_grid->newCameraZ << 9;
 	int32 distance = _engine->_movements->getDistance3D(camX, camY, camZ, x, y, z);
 	distance = _engine->_collision->getAverageValue(0, distance, 10000, 255);
-	const byte targetVolume = CLIP(255 - distance, 0, 255);
+	const byte targetVolume = CLIP<byte>(255 - distance, 0, 255);
 	_engine->_system->getMixer()->setChannelVolume(samplesPlaying[channelIdx], targetVolume);
 }
 
