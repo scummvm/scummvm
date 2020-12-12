9731722bd2044453db079dd6809b9224499ea8ee
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-12-12 13:03:51 +0100

    TWINE: renamed method and stack vars

--- engines/twine/animations.h
+++ engines/twine/animations.h
@@ -35,8 +35,8 @@ class Animations {
 private:
 	TwinEEngine *_engine;
 	void applyAnimStepRotation(uint8 *ptr, int32 deltaTime, int32 keyFrameLength, const uint8 *keyFramePtr, const uint8 *lastKeyFramePtr);
+	void applyAnimStepTranslation(uint8 *ptr, int32 deltaTime, int32 keyFrameLength, const uint8 *keyFramePtr, const uint8 *lastKeyFramePtr);
 	int32 getAnimMode(uint8 *ptr, const uint8 *keyFramePtr);
-	void applyAnimStep(uint8 *ptr, int32 deltaTime, int32 keyFrameLength, const uint8 *keyFramePtr, const uint8 *lastKeyFramePtr);
 
 	/**
 	 * Verify animation at keyframe
