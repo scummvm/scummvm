9b4d72151ecdd518d6ad873632aafadaf3393209
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-12-12 11:59:29 +0100

    TWINE: reverted "only load the entity data once"
    This reverts commit ab8aab37c0c2afc3a066e53006b7fbce48a5a354 and fixes a regression
    in switching to the attack mode animation

--- engines/twine/entity.cpp
+++ engines/twine/entity.cpp
@@ -147,8 +147,6 @@ bool EntityData::loadAnim(Common::SeekableReadStream &stream) {
 }
 
 bool EntityData::loadFromStream(Common::SeekableReadStream &stream) {
-	_bodies.clear();
-	_animations.clear();
 	do {
 		const uint8 opcode = stream.readByte();
 		if (opcode == 1) {
