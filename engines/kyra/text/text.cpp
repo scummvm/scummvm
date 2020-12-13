e872c56e33b227e5d797163d6a7bcbdb08dd79d0
BLooperZ <blooperz@users.noreply.github.com>
2020-12-13 21:26:01 +0200

    KYRA: use memset and rename variables

--- engines/kyra/text/text.cpp
+++ engines/kyra/text/text.cpp
@@ -204,13 +204,14 @@ void TextDisplayer::printTalkTextMessage(const char *text, int x, int y, uint8 c
 }
 
 void TextDisplayer::printText(const char *str, int x, int y, uint8 c0, uint8 c1, uint8 c2) {
-	char rev[384] = {0};
+	char revBuffer[384];
+	memset(revBuffer, 0, sizeof(revBuffer));
 	if (_vm->gameFlags().lang == Common::HE_ISR) {
 		int len = strlen(str);
 		for (int i = 0; i < len; i++) {
-			rev[i] = str[len - i - 1];
+			revBuffer[i] = str[len - i - 1];
 		}
-		str = rev;
+		str = revBuffer;
 	}
 	uint8 colorMap[] = { 0, 15, 12, 12 };
 	colorMap[3] = c1;
