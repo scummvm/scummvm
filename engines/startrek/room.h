0384822f858f984ae795aa3bef804da880ad5dfb
Filippos Karapetis <bluegr@gmail.com>
2020-12-19 01:43:50 +0200

    STARTREK: Change action type to signed
    We now use an action array terminator with a negative value

--- engines/startrek/room.h
+++ engines/startrek/room.h
@@ -102,19 +102,19 @@ public:
 	 * Check if a particular action is defined for this room.
 	 */
 	bool actionHasCode(const Action &action);
-	bool actionHasCode(byte type, byte b1, byte b2, byte b3);
+	bool actionHasCode(char type, byte b1, byte b2, byte b3);
 
 	/**
 	 * Execute a particular action for this room, if defined.
 	 */
 	bool handleAction(const Action &action);
-	bool handleAction(byte type, byte b1, byte b2, byte b3);
+	bool handleAction(char type, byte b1, byte b2, byte b3);
 
 	/**
 	 * Same as above, but if any byte in the action is -1 (0xff), it matches any value.
 	 */
 	bool handleActionWithBitmask(const Action &action);
-	bool handleActionWithBitmask(byte type, byte b1, byte b2, byte b3);
+	bool handleActionWithBitmask(char type, byte b1, byte b2, byte b3);
 
 	uint16 getFirstHotspot() {
 		return readRdfWord(0x12);
