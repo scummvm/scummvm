cf559249d55f91a59e54eae512afb41b7c8d8f4a
Filippos Karapetis <bluegr@gmail.com>
2020-12-18 15:22:48 +0200

    STARTREK: Add a console command to show the score of all missions

--- engines/startrek/console.h
+++ engines/startrek/console.h
@@ -43,6 +43,7 @@ private:
 	bool Cmd_Bg(int argc, const char **argv);
 	bool Cmd_DumpFile(int argc, const char **argv);
 	bool Cmd_SearchFile(int argc, const char **argv);
+	bool Cmd_Score(int argc, const char **argv);
 
 	Common::String EventToString(uint32 action);
 	Common::String ItemToString(byte index);
