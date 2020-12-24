515d1560d92765772019fe6026e457c7269fdf72
neuromancer <>
2020-12-24 19:02:12 -0300

    PRIVATE: some code to decode videos

--- engines/private/private.cpp
+++ engines/private/private.cpp
@@ -1,5 +1,8 @@
 #include "common/scummsys.h"
 
+#include "audio/decoders/wave.h"
+#include "audio/audiostream.h"
+
 #include "common/config-manager.h"
 #include "common/debug.h"
 #include "common/debug-channels.h"
@@ -25,8 +28,8 @@ PrivateEngine::PrivateEngine(OSystem *syst)
 	// Do not initialize audio devices here
 
 	// However this is the place to specify all default directories
-	const Common::FSNode gameDataDir(ConfMan.get("path"));
-	SearchMan.addSubDirectoryMatching(gameDataDir, "sound");
+	//const Common::FSNode gameDataDir(ConfMan.get("path"));
+        //SearchMan.addSubDirectoryMatching(gameDataDir, "/run/media/g/PVTEYE");
 
 	// Here is the right place to set up the engine specific debug channels
 	DebugMan.addDebugChannel(kPrivateDebugExample, "example", "this is just an example for a engine specific debug channel");
@@ -50,7 +53,9 @@ PrivateEngine::~PrivateEngine() {
 
 Common::Error PrivateEngine::run() {
 	// Initialize graphics using following:
-	initGraphics(320, 200);
+	_screenW = 640;
+	_screenH = 480;
+	initGraphics(_screenW, _screenH);
 
 	// You could use backend transactions directly as an alternative,
 	// but it isn't recommended, until you want to handle the error values
@@ -85,9 +90,22 @@ Common::Error PrivateEngine::run() {
 
 	// Simple main event loop
 	Common::Event evt;
+        _videoDecoder = new Video::SmackerDecoder();
+  	playVideo("intro/intro.smk");
 	while (!shouldQuit()) {
 		g_system->getEventManager()->pollEvent(evt);
 		g_system->delayMillis(10);
+
+		if (_videoDecoder) {
+			if (_videoDecoder->endOfVideo()) {
+				_videoDecoder->close();
+				delete _videoDecoder;
+				_videoDecoder = nullptr;
+			} else if (_videoDecoder->needsUpdate()) {
+				drawScreen();
+			}
+		}
+
 	}
 
 	return Common::kNoError;
@@ -117,5 +135,59 @@ void PrivateEngine::syncGameStream(Common::Serializer &s) {
 	int dummy = 0;
 	s.syncAsUint16LE(dummy);
 }
+
+void PrivateEngine::playSound(const Common::String &name) {
+	debugC(1, kPrivateDebugExample, "%s : %s", __FUNCTION__, name.c_str());
+	Common::File *file = new Common::File();
+	if (!file->open(name))
+		error("unable to find sound file %s", name.c_str());
+
+	Audio::AudioStream *stream;
+	stream = Audio::makeWAVStream(file, DisposeAfterUse::YES);
+	stopSound();
+	_mixer->playStream(Audio::Mixer::kSFXSoundType, &_soundHandle, stream, -1, Audio::Mixer::kMaxChannelVolume);
+}
+
+void PrivateEngine::playVideo(const Common::String &name) {
+	debugC(1, kPrivateDebugExample, "%s : %s", __FUNCTION__, name.c_str());
+	Common::File *file = new Common::File();
+	if (!file->open(name))
+		error("unable to find video file %s", name.c_str());
+
+	if (!_videoDecoder->loadStream(file))
+	        error("unable to load video %s", name.c_str());
+	_videoDecoder->start();
+
+}
+
+void PrivateEngine::stopSound() {
+	debugC(3, kPrivateDebugExample, "%s", __FUNCTION__);
+	if (_mixer->isSoundHandleActive(_soundHandle))
+		_mixer->stopHandle(_soundHandle);
+}
+
+void PrivateEngine::drawScreen() {
+	if (_videoDecoder && _videoDecoder->needsUpdate()) {
+		Graphics::Surface *screen = g_system->lockScreen();
+		screen->fillRect(Common::Rect(0, 0, g_system->getWidth(), g_system->getHeight()), 0);
+
+		const Graphics::Surface *surface;
+		surface = _videoDecoder->decodeNextFrame();
+
+		int w = surface->w; //CLIP<int>(surface->w, 0, _screenW);
+		int h = surface->h; //CLIP<int>(surface->h, 0, _screenH);
+
+		//int x = (_screenW - w) / 2;
+		//int y = (_screenH - h) / 2;
+     	        //debug("%d %d %d %d", w, h, x, y);
+
+		screen->copyRectToSurface(*surface, 0, 0, Common::Rect(0, 0, w, h));
+
+		g_system->unlockScreen();
+		g_system->updateScreen();
+	}
+}
+
+
 
 } // End of namespace Private
