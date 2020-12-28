4dd902b40b53e95ffbae85de7f7482677fa93a7d
SupSuper <supsuper@gmail.com>
2020-12-28 21:44:02 +0000

    SCI: Fix missing translations in Engine options tab

--- engines/sci/detection.cpp
+++ engines/sci/detection.cpp
@@ -367,17 +367,17 @@ OptionsWidget::OptionsWidget(GuiObject *boss, const Common::String &name, const 
 
 	for (const ADExtraGuiOptionsMap *entry = optionsList; entry->guioFlag; ++entry)
 		if (checkGameGUIOption(entry->guioFlag, _guiOptions))
-			_checkboxes[entry->option.configOption] = new GUI::CheckboxWidget(widgetsBoss(), _dialogLayout + "." + entry->option.configOption, Common::U32String(entry->option.label), Common::U32String(entry->option.tooltip));
+			_checkboxes[entry->option.configOption] = new GUI::CheckboxWidget(widgetsBoss(), _dialogLayout + "." + entry->option.configOption, _(entry->option.label), _(entry->option.tooltip));
 
 	for (const PopUpOptionsMap *entry = popUpOptionsList; entry->guioFlag; ++entry)
 		if (checkGameGUIOption(entry->guioFlag, _guiOptions)) {
-			GUI::StaticTextWidget *textWidget = new GUI::StaticTextWidget(widgetsBoss(), _dialogLayout + "." + entry->configOption + "_desc", Common::U32String(entry->label), Common::U32String(entry->tooltip));
+			GUI::StaticTextWidget *textWidget = new GUI::StaticTextWidget(widgetsBoss(), _dialogLayout + "." + entry->configOption + "_desc", _(entry->label), _(entry->tooltip));
 			textWidget->setAlign(Graphics::kTextAlignRight);
 
 			_popUps[entry->configOption] = new GUI::PopUpWidget(widgetsBoss(), _dialogLayout + "." + entry->configOption);
 
 			for (uint i = 0; entry->items[i].label; ++i)
-				_popUps[entry->configOption]->appendEntry(entry->items[i].label, entry->items[i].configValue);
+				_popUps[entry->configOption]->appendEntry(_(entry->items[i].label), entry->items[i].configValue);
 		}
 }
 
