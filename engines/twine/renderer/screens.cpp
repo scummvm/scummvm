scummvm/scummvm

commit 725f75d4afb77831ba4ba5d75cb8ea0dd4c91174

TWINE: allow to use a higher resolution for rendering images

engines/twine/renderer/screens.cpp
@@ -22,6 +22,7 @@
 
 #include "twine/renderer/screens.h"
 #include "common/system.h"
+#include "graphics/managed_surface.h"
 #include "graphics/surface.h"
 #include "twine/audio/music.h"
 #include "twine/resources/hqr.h"
@@ -41,11 +42,13 @@ bool Screens::adelineLogo() {
 }
 
 void Screens::loadMenuImage(bool fade_in) {
-	if (HQR::getEntry((uint8 *)_engine->workVideoBuffer.getPixels(), Resources::HQR_RESS_FILE, RESSHQR_MENUIMG) == 0) {
+	Graphics::ManagedSurface& src = _engine->imageBuffer;
+	if (HQR::getEntry((uint8 *)src.getPixels(), Resources::HQR_RESS_FILE, RESSHQR_MENUIMG) == 0) {
 		warning("Failed to load menu image");
 		return;
 	}
-	copyScreen(_engine->workVideoBuffer, _engine->frontVideoBuffer);
+	Graphics::ManagedSurface& target = _engine->frontVideoBuffer;
+	target.transBlitFrom(src, src.getBounds(), target.getBounds());
 	if (fade_in) {
 		fadeToPal(paletteRGBA);
 	} else {
@@ -75,12 +78,14 @@ void Screens::convertPalToRGBA(const uint8 *in, uint32 *out) {
 }
 
 void Screens::loadImage(int32 index, int32 paletteIndex, bool fade_in) {
-	if (HQR::getEntry((uint8 *)_engine->workVideoBuffer.getPixels(), Resources::HQR_RESS_FILE, index) == 0) {
+	Graphics::ManagedSurface& src = _engine->imageBuffer;
+	if (HQR::getEntry((uint8 *)src.getPixels(), Resources::HQR_RESS_FILE, index) == 0) {
 		warning("Failed to load image with index %i", index);
 		return;
 	}
 	debug(0, "Load image: %i", index);
-	copyScreen(_engine->workVideoBuffer, _engine->frontVideoBuffer);
+	Graphics::ManagedSurface& target = _engine->frontVideoBuffer;
+	target.transBlitFrom(src, src.getBounds(), target.getBounds());
 	loadCustomPalette(paletteIndex);
 	if (fade_in) {
 		fadeToPal(paletteRGBACustom);

engines/twine/twine.cpp
@@ -305,10 +305,11 @@ void TwinEEngine::autoSave() {
 
 void TwinEEngine::allocVideoMemory() {
 	const Graphics::PixelFormat format = Graphics::PixelFormat::createFormatCLUT8();
+
+	imageBuffer.create(640, 480, format); // original lba1 resolution for a lot of images.
+
 	workVideoBuffer.create(SCREEN_WIDTH, SCREEN_HEIGHT, format);
 	frontVideoBuffer.create(SCREEN_WIDTH, SCREEN_HEIGHT, format);
-
-	// initVideoVar1 = -1;
 }
 
 static int getLanguageTypeIndex(const char *languageName) {

engines/twine/twine.h
@@ -270,6 +270,7 @@ public:
 	int32 quitGame = 0;
 	int32 lbaTime = 0;
 
+	Graphics::ManagedSurface imageBuffer;
 	/** Work video buffer */
 	Graphics::ManagedSurface workVideoBuffer;
 	/** Main game video buffer */

commit 76cdc468636009bd2f825270d1a1a3bb7a4288c9

TWINE: fixed missing reset of background buffer
regression introduced in 725f75d4afb77831ba4ba5d75cb8ea0dd4c91174

engines/twine/renderer/screens.cpp
@@ -39,6 +39,7 @@ bool Screens::adelineLogo() {
 
 void Screens::loadMenuImage(bool fadeIn) {
 	loadImage(RESSHQR_MENUIMG, -1, fadeIn);
+	_engine->workVideoBuffer.blitFrom(_engine->frontVideoBuffer);
 }
 
 void Screens::loadCustomPalette(int32 index) {