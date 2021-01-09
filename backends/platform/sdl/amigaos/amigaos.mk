c4d0f4fa8e96c42a85cba6a6ab96b223982a68f1
Hubert Maier <raziel-@users.noreply.github.com>
2021-01-09 14:39:41 +0100

    AMIGAOS: Fixes to the automatic installation (#2721)
    * AMIGAOS: amigaos.mk fixes
    
    switch to AmigaOS makedir, as mkdir seems to break pathes/dirs
    
    * AMIGAOS: Fix RM2AG.rexx
    
    New tables have been introduced which start with a bunch of minus (-) signs.
    That broke the script at exactly 3.10 (where the tables are) by leaving it.
    
    Updated the complete condition.
    
    * AMIGAOS: amigaos.mk
    
    Fix some comment oversights
    
    * AMIGAOS: RM2AG.rexx
    
    Fix an oversight
    
    * AMIGAOS: amigaos.mk
    
    Last oversight
    
    * AMIGAOS: Update RM2AG.rexx.in

--- backends/platform/sdl/amigaos/amigaos.mk
+++ backends/platform/sdl/amigaos/amigaos.mk
@@ -1,11 +1,20 @@
 # Special target to create an AmigaOS snapshot installation.
+#
+# WORKAROUNDS:
+#
+# 'mkdir' seems to incorrectly set permissions to path/dirs on AmigaOS.
+# Once a vanilla installation was created, none of the special subdirectories
+# are found/accessible (extras, themes, plugins), instead ScummVM reports
+# missing theme files and a valid translation.dat.
+# Switching to AmigaOS' own "makedir" until there is a fix or other solution. 
+#
 amigaosdist: $(EXECUTABLE) $(PLUGINS)
-	mkdir -p $(AMIGAOSPATH)
-	mkdir -p $(AMIGAOSPATH)/extras
+	makedir ALL FORCE $(AMIGAOSPATH)
+	makedir ALL FORCE $(AMIGAOSPATH)/extras
 	cp ${srcdir}/dists/amigaos/scummvm_drawer.info $(AMIGAOSPATH).info
 	cp ${srcdir}/dists/amigaos/scummvm.info $(AMIGAOSPATH)/$(EXECUTABLE).info
 ifdef DIST_FILES_DOCS
-	mkdir -p $(AMIGAOSPATH)/doc
+	makedir ALL FORCE $(AMIGAOSPATH)/doc
 	cp -r $(srcdir)/doc/ $(AMIGAOSPATH)
 	cp $(DIST_FILES_DOCS) $(AMIGAOSPATH)/doc/
 	# Prepare README.md for AmigaGuide conversion.
@@ -28,17 +37,17 @@ ifdef DIST_FILES_VKEYBD
 	cp $(DIST_FILES_VKEYBD) $(AMIGAOSPATH)/extras/
 endif
 ifdef DIST_FILES_THEMES
-	mkdir -p $(AMIGAOSPATH)/themes
+	makedir ALL FORCE $(AMIGAOSPATH)/themes
 	cp $(DIST_FILES_THEMES) $(AMIGAOSPATH)/themes/
 endif
 	# Strip and copy engine plugins.
 ifdef DYNAMIC_MODULES
-	mkdir -p $(AMIGAOSPATH)/plugins
+	makedir ALL FORCE $(AMIGAOSPATH)/plugins
 	$(foreach plugin, $(PLUGINS), $(STRIP) $(plugin) -o $(AMIGAOSPATH)/$(plugin);)
 	# Shared objects on AmigaOS change. To avoid conflicts with old or
 	# outdated .so's, always remove and install them completely.
 	rm -rf $(AMIGAOSPATH)/sobjs
-	mkdir -p $(AMIGAOSPATH)/sobjs
+	makedir ALL FORCE $(AMIGAOSPATH)/sobjs
 	# Extract and install compiled-in shared libraries.
 	# Not every AmigaOS installation, especially vanilla ones, have
 	# every mandatory shared library installed in the correct place.
