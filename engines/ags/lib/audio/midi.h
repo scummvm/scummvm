1726bc0cc28c08e5338f58be7c2d696f52e66325
Paul Gilbert <dreammaster@scummvm.org>
2021-01-10 19:51:50 -0800

    AGS: Remove deprecated MIDI_DRIVER

--- engines/ags/lib/audio/midi.h
+++ engines/ags/lib/audio/midi.h
@@ -24,66 +24,19 @@
 #define AGS_LIB_AUDIO_MIDI_H
 
 #include "common/scummsys.h"
+#include "common/array.h"
 #include "ags/lib/allegro/base.h"
 #include "ags/lib/allegro/alconfig.h"
 
 namespace AGS3 {
 
-/* Theoretical maximums: */
-#define MIDI_VOICES           64       /* actual drivers may not be */
-#define MIDI_TRACKS           32       /* able to handle this many */
-
 /* a midi file */
-struct MIDI {
-	int divisions;                      /* number of ticks per quarter note */
-	struct {
-		unsigned char *data;             /* MIDI message stream */
-		int len;                         /* length of the track data */
-	} track[MIDI_TRACKS];
-};
+typedef Common::Array<byte> MIDI;
 
 
 #define MIDI_AUTODETECT       -1
 #define MIDI_NONE             0
 #define MIDI_DIGMID           AL_ID('D','I','G','I')
-
-/* driver for playing midi music */
-struct MIDI_DRIVER {
-	int  id;                            /* driver ID code */
-	AL_CONST char *name;                /* driver name */
-	AL_CONST char *desc;                /* description string */
-	AL_CONST char *ascii_name;          /* ASCII format name string */
-	int  voices;                        /* available voices */
-	int  basevoice;                     /* voice number offset */
-	int  max_voices;                    /* maximum voices we can support */
-	int  def_voices;                    /* default number of voices to use */
-	int  xmin, xmax;                    /* reserved voice range */
-
-	/* setup routines */
-	AL_METHOD(int, detect, (int input));
-	AL_METHOD(int, init, (int input, int voices));
-	AL_METHOD(void, exit, (int input));
-	AL_METHOD(int, set_mixer_volume, (int volume));
-	AL_METHOD(int, get_mixer_volume, (void));
-
-	/* raw MIDI output to MPU-401, etc. */
-	AL_METHOD(void, raw_midi, (int data));
-
-	/* dynamic patch loading routines */
-	AL_METHOD(int, load_patches, (AL_CONST char *patches, AL_CONST char *drums));
-	AL_METHOD(void, adjust_patches, (AL_CONST char *patches, AL_CONST char *drums));
-
-	/* note control functions */
-	AL_METHOD(void, key_on, (int inst, int note, int bend, int vol, int pan));
-	AL_METHOD(void, key_off, (int voice));
-	AL_METHOD(void, set_volume, (int voice, int vol));
-	AL_METHOD(void, set_pitch, (int voice, int note, int bend));
-	AL_METHOD(void, set_pan, (int voice, int pan));
-	AL_METHOD(void, set_vibrato, (int voice, int amount));
-};
-
-
-AL_VAR(MIDI_DRIVER, midi_digmid);
 
 AL_ARRAY(_DRIVER_INFO, _midi_driver_list);
 
@@ -100,10 +53,6 @@ AL_ARRAY(_DRIVER_INFO, _midi_driver_list);
 #define MIDI_DRIVER_DIGMID                                     \
 	{  MIDI_DIGMID,      &midi_digmid,        TRUE  },
 
-
-AL_VAR(MIDI_DRIVER *, midi_driver);
-
-AL_VAR(MIDI_DRIVER *, midi_input_driver);
 
 AL_VAR(int, midi_card);
 
