scummvm/scummvm

commit 219daa614645f0f518b4b01e85d914283823b546

TWINE: added scene parsing for lba2

engines/twine/scene/scene.h
@@ -93,10 +93,14 @@ struct ZoneStruct {
 			int16 used;
 		} Bonus;
 		struct {
-			int16 info0;
-			int16 info1;
-			int16 info2;
-			int16 info3;
+			int32 info0;
+			int32 info1;
+			int32 info2;
+			int32 info3;
+			int32 info4;
+			int32 info5;
+			int32 info6;
+			int32 info7;
 		} generic;
 	} infoData;
 	int16 snap = 0;
@@ -266,9 +270,11 @@ private:
 
 	/** Process zone extra bonus */
 	void processZoneExtraBonus(ZoneStruct *zone);
-	void setActorStaticFlags(ActorStruct *act, uint16 staticFlags);
+	void setActorStaticFlags(ActorStruct *act, uint32 staticFlags);
 	void setBonusParameterFlags(ActorStruct *act, uint16 bonusFlags);
 	bool loadSceneLBA1();
+	bool loadSceneLBA2();
+
 	/** Initialize new scene */
 	bool initScene(int32 index);
 	/** Reset scene */
@@ -277,12 +283,15 @@ private:
 	// the first actor is the own hero
 	ActorStruct _sceneActors[NUM_MAX_ACTORS];
 	int32 _currentSceneSize = 0;
+	bool _isOutsideScene = false; // lba2
 
 	/** Timer for the next sample ambience in scene */
 	int32 _sampleAmbienceTime = 0;
 	int16 _sampleAmbiance[4]{0};
 	int16 _sampleRepeat[4]{0};
 	int16 _sampleRound[4]{0};
+	int16 _sampleFrequency[4]{0}; // lba2
+	int16 _sampleVolume[4]{0}; // lba2
 	int16 _sampleMinDelay = 0;
 	int16 _sampleMinDelayRnd = 0;
 

engines/twine/scene/actor.h
@@ -78,23 +78,29 @@ struct AnimTimerDataStruct {
 
 /** Actors static flags structure */
 struct StaticFlagsStruct {
-	uint16 bComputeCollisionWithObj : 1;    // 0x0001
-	uint16 bComputeCollisionWithBricks : 1; // 0x0002
-	uint16 bIsZonable : 1;                  // 0x0004
-	uint16 bUsesClipping : 1;               // 0x0008
-	uint16 bCanBePushed : 1;                // 0x0010
-	uint16 bComputeLowCollision : 1;        // 0x0020
-	uint16 bCanDrown : 1;                   // 0x0040
-	uint16 bComputeCollisionWithFloor : 1;  // 0x0080
-	uint16 bUnk0100 : 1;                    // 0x0100
-	uint16 bIsHidden : 1;                   // 0x0200
-	uint16 bIsSpriteActor : 1;              // 0x0400
-	uint16 bCanFall : 1;                    // 0x0800
-	uint16 bDoesntCastShadow : 1;           // 0x1000
-	uint16 bIsBackgrounded : 1;             // 0x2000
-	uint16 bIsCarrierActor : 1;             // 0x4000
+	uint32 bComputeCollisionWithObj : 1;    // 0x000001
+	uint32 bComputeCollisionWithBricks : 1; // 0x000002
+	uint32 bIsZonable : 1;                  // 0x000004
+	uint32 bUsesClipping : 1;               // 0x000008
+	uint32 bCanBePushed : 1;                // 0x000010
+	uint32 bComputeLowCollision : 1;        // 0x000020
+	uint32 bCanDrown : 1;                   // 0x000040
+	uint32 bComputeCollisionWithFloor : 1;  // 0x000080
+	uint32 bUnk0100 : 1;                    // 0x000100
+	uint32 bIsHidden : 1;                   // 0x000200
+	uint32 bIsSpriteActor : 1;              // 0x000400
+	uint32 bCanFall : 1;                    // 0x000800
+	uint32 bDoesntCastShadow : 1;           // 0x001000
+	uint32 bIsBackgrounded : 1;             // 0x002000
+	uint32 bIsCarrierActor : 1;             // 0x004000
 	// take smaller value for bound, or if not set take average for bound
-	uint16 bUseMiniZv : 1; // 0x8000
+	uint32 bUseMiniZv : 1;                  // 0x008000
+	uint32 bHasInvalidPosition : 1;          // 0x010000
+	uint32 bNoElectricShock : 1;             // 0x020000
+	uint32 bHasSpriteAnim3D : 1;             // 0x040000
+	uint32 bNoPreClipping : 1;               // 0x080000
+	uint32 bHasZBuffer : 1;                  // 0x100000
+	uint32 bHasZBufferInWater : 1;           // 0x200000
 };
 
 /** Actors dynamic flags structure */

engines/twine/scene/scene.cpp
@@ -47,7 +47,7 @@ Scene::~Scene() {
 	free(currentScene);
 }
 
-void Scene::setActorStaticFlags(ActorStruct *act, uint16 staticFlags) {
+void Scene::setActorStaticFlags(ActorStruct *act, uint32 staticFlags) {
 	if (staticFlags & 0x1) {
 		act->staticFlags.bComputeCollisionWithObj = 1;
 	}
@@ -89,13 +89,31 @@ void Scene::setActorStaticFlags(ActorStruct *act, uint16 staticFlags) {
 		act->staticFlags.bDoesntCastShadow = 1;
 	}
 	if (staticFlags & 0x2000) {
-		//sceneActors[actorIdx].staticFlags.bIsBackgrounded = 1;
+		//act->staticFlags.bIsBackgrounded = 1;
 	}
 	if (staticFlags & 0x4000) {
 		act->staticFlags.bIsCarrierActor = 1;
 	}
 	if (staticFlags & 0x8000) {
 		act->staticFlags.bUseMiniZv = 1;
+	}
+	if (staticFlags & 0x10000) {
+		act->staticFlags.bHasInvalidPosition = 1;
+	}
+	if (staticFlags & 0x20000) {
+		act->staticFlags.bNoElectricShock = 1;
+	}
+	if (staticFlags & 0x40000) {
+		act->staticFlags.bHasSpriteAnim3D = 1;
+	}
+	if (staticFlags & 0x80000) {
+		act->staticFlags.bNoPreClipping = 1;
+	}
+	if (staticFlags & 0x100000) {
+		act->staticFlags.bHasZBuffer = 1;
+	}
+	if (staticFlags & 0x200000) {
+		act->staticFlags.bHasZBufferInWater = 1;
 	}
 }
 
@@ -129,6 +147,138 @@ void Scene::setBonusParameterFlags(ActorStruct *act, uint16 bonusFlags) {
 	}
 }
 
+bool Scene::loadSceneLBA2() {
+	Common::MemoryReadStream stream(currentScene, _currentSceneSize);
+	sceneTextBank = stream.readByte();
+	_currentGameOverScene = stream.readByte();
+	stream.skip(4);
+
+	alphaLight = ClampAngle(stream.readUint16LE());
+	betaLight = ClampAngle(stream.readUint16LE());
+	debug(2, "Using %i and %i as light vectors", alphaLight, betaLight);
+
+	_isOutsideScene = stream.readByte();
+
+	for (int i = 0; i < 4; ++i) {
+		_sampleAmbiance[i] = stream.readUint16LE();
+		_sampleRepeat[i] = stream.readUint16LE();
+		_sampleRound[i] = stream.readUint16LE();
+		_sampleFrequency[i] = stream.readUint16LE();
+		_sampleVolume[i] = stream.readUint16LE();
+	}
+
+	_sampleMinDelay = stream.readUint16LE();
+	_sampleMinDelayRnd = stream.readUint16LE();
+
+	_sceneMusic = stream.readByte();
+
+	// load hero properties
+	_sceneHeroX = stream.readUint16LE();
+	_sceneHeroY = stream.readUint16LE();
+	_sceneHeroZ = stream.readUint16LE();
+
+	sceneHero->moveScriptSize = stream.readUint16LE();
+	sceneHero->moveScript = currentScene + stream.pos();
+	stream.skip(sceneHero->moveScriptSize);
+
+	sceneHero->lifeScriptSize = stream.readUint16LE();
+	sceneHero->lifeScript = currentScene + stream.pos();
+	stream.skip(sceneHero->lifeScriptSize);
+
+	sceneNumActors = stream.readUint16LE();
+	int cnt = 1;
+	for (int32 i = 1; i < sceneNumActors; i++, cnt++) {
+		_engine->_actor->resetActor(i);
+		ActorStruct *act = &_sceneActors[i];
+		setActorStaticFlags(act, stream.readUint32LE());
+
+		act->loadModel(stream.readUint16LE());
+
+		act->body = (BodyType)stream.readSint16LE();
+		act->anim = (AnimationTypes)stream.readByte();
+		act->sprite = stream.readUint16LE();
+		act->x = stream.readUint16LE();
+		act->collisionX = act->x;
+		act->y = stream.readUint16LE();
+		act->collisionY = act->y;
+		act->z = stream.readUint16LE();
+		act->collisionZ = act->z;
+		act->strengthOfHit = stream.readByte();
+		setBonusParameterFlags(act, stream.readUint16LE());
+		act->angle = stream.readUint16LE();
+		act->speed = stream.readUint16LE();
+		act->controlMode = (ControlMode)stream.readByte();
+		act->cropLeft = stream.readSint16LE();
+		act->delayInMillis = act->cropLeft; // TODO: this might not be needed
+		act->cropTop = stream.readSint16LE();
+		act->cropRight = stream.readSint16LE();
+		act->cropBottom = stream.readSint16LE();
+		act->followedActor = act->cropBottom; // TODO: is this needed? and valid?
+		act->bonusAmount = stream.readSint16LE();
+		act->talkColor = stream.readByte();
+		if (act->staticFlags.bHasSpriteAnim3D) {
+			/*act->spriteAnim3DNumber = */stream.readSint32LE();
+			/*act->spriteSizeHit = */stream.readSint16LE();
+			/*act->cropBottom = act->spriteSizeHit;*/
+		}
+		act->armor = stream.readByte();
+		act->life = stream.readByte();
+
+		act->moveScriptSize = stream.readUint16LE();
+		act->moveScript = currentScene + stream.pos();
+		stream.skip(act->moveScriptSize);
+
+		act->lifeScriptSize = stream.readUint16LE();
+		act->lifeScript = currentScene + stream.pos();
+		stream.skip(act->lifeScriptSize);
+
+		if (_engine->_debugScene->onlyLoadActor != -1 && _engine->_debugScene->onlyLoadActor != cnt) {
+			sceneNumActors--;
+			i--;
+		}
+	}
+
+	sceneNumZones = stream.readUint16LE();
+	for (int32 i = 0; i < sceneNumZones; i++) {
+		ZoneStruct *zone = &sceneZones[i];
+		zone->bottomLeft.x = stream.readSint32LE();
+		zone->bottomLeft.y = stream.readSint32LE();
+		zone->bottomLeft.z = stream.readSint32LE();
+
+		zone->topRight.x = stream.readSint32LE();
+		zone->topRight.y = stream.readSint32LE();
+		zone->topRight.z = stream.readSint32LE();
+
+		zone->infoData.generic.info0 = stream.readSint32LE();
+		zone->infoData.generic.info1 = stream.readSint32LE();
+		zone->infoData.generic.info2 = stream.readSint32LE();
+		zone->infoData.generic.info3 = stream.readSint32LE();
+		zone->infoData.generic.info4 = stream.readSint32LE();
+		zone->infoData.generic.info5 = stream.readSint32LE();
+		zone->infoData.generic.info6 = stream.readSint32LE();
+		zone->infoData.generic.info7 = stream.readSint32LE();
+
+		zone->type = stream.readUint16LE();
+		zone->snap = stream.readUint16LE();
+	}
+
+	sceneNumTracks = stream.readUint16LE();
+	for (int32 i = 0; i < sceneNumTracks; i++) {
+		ScenePoint *point = &sceneTracks[i];
+		point->x = stream.readSint32LE();
+		point->y = stream.readSint32LE();
+		point->z = stream.readSint32LE();
+	}
+
+	int32 sceneNumPatches = stream.readUint16LE();
+	for (int32 i = 0; i < sceneNumPatches; i++) {
+		/*size = */stream.readUint16LE();
+		/*offset = */stream.readUint16LE();
+	}
+
+	return true;
+}
+
 bool Scene::loadSceneLBA1() {
 	Common::MemoryReadStream stream(currentScene, _currentSceneSize);
 
@@ -143,21 +293,11 @@ bool Scene::loadSceneLBA1() {
 	betaLight = ClampAngle(stream.readUint16LE());
 	debug(2, "Using %i and %i as light vectors", alphaLight, betaLight);
 
-	_sampleAmbiance[0] = stream.readUint16LE();
-	_sampleRepeat[0] = stream.readUint16LE();
-	_sampleRound[0] = stream.readUint16LE();
-
-	_sampleAmbiance[1] = stream.readUint16LE();
-	_sampleRepeat[1] = stream.readUint16LE();
-	_sampleRound[1] = stream.readUint16LE();
-
-	_sampleAmbiance[2] = stream.readUint16LE();
-	_sampleRepeat[2] = stream.readUint16LE();
-	_sampleRound[2] = stream.readUint16LE();
-
-	_sampleAmbiance[3] = stream.readUint16LE();
-	_sampleRepeat[3] = stream.readUint16LE();
-	_sampleRound[3] = stream.readUint16LE();
+	for (int i = 0; i < 4; ++i) {
+		_sampleAmbiance[i] = stream.readUint16LE();
+		_sampleRepeat[i] = stream.readUint16LE();
+		_sampleRound[i] = stream.readUint16LE();
+	}
 
 	_sampleMinDelay = stream.readUint16LE();
 	_sampleMinDelayRnd = stream.readUint16LE();
@@ -267,6 +407,8 @@ bool Scene::initScene(int32 index) {
 
 	if (_engine->isLBA1()) {
 		return loadSceneLBA1();
+	} else if (_engine->isLBA2()) {
+		return loadSceneLBA2();
 	}
 
 	return false;

commit 87bac099e49adcd504db5abc336b44915b56853e

TWINE: fixed camera-center-issue
introduced in 219daa614645f0f518b4b01e85d914283823b546

the lba2 scene is using int32 for the infodata unions. lba1 is
using int16 - so we had to switch every single member to int32.

engines/twine/scene/scene.h
@@ -60,37 +60,37 @@ struct ZoneStruct {
 	int16 type = 0;
 	union {
 		struct {
-			int16 newSceneIdx;
-			int16 x;
-			int16 y;
-			int16 z;
+			int32 newSceneIdx;
+			int32 x;
+			int32 y;
+			int32 z;
 		} ChangeScene;
 		struct {
-			int16 dummy;
-			int16 x;
-			int16 y;
-			int16 z;
+			int32 dummy;
+			int32 x;
+			int32 y;
+			int32 z;
 		} CameraView;
 		struct {
-			int16 zoneIdx;
+			int32 zoneIdx;
 		} Sceneric;
 		struct {
-			int16 newGrid;
+			int32 newGrid;
 		} CeillingGrid;
 
 		/** show a text (e.g. when reading a sign) */
 		struct {
-			int16 textIdx;   /*!< text index in the current active text bank */
-			int16 textColor; /*!< text color (see @c ActorStruct::talkColor) */
+			int32 textIdx;   /*!< text index in the current active text bank */
+			int32 textColor; /*!< text color (see @c ActorStruct::talkColor) */
 		} DisplayText;
 		struct {
-			int16 info0;
+			int32 info0;
 			BonusParameter typesFlag;
-			int16 amount;
+			int32 amount;
 			/**
 			 * Already used
 			 */
-			int16 used;
+			int32 used;
 		} Bonus;
 		struct {
 			int32 info0;