bc16fa16b710df1f0008a561ef13d8c5ea046fc4
Alexandre Fontoura <xesfnet@gmail.com>
2021-02-19 14:56:50 +0000

    ASYLUM: Game detection improvements

--- engines/asylum/detection.cpp
+++ engines/asylum/detection.cpp
@@ -22,78 +22,7 @@
 
 #include "engines/advancedDetector.h"
 #include "base/plugins.h"
-
-static const PlainGameDescriptor asylumGames[] = {
-	{ "asylum", "Sanitarium" },
-	{ 0, 0 }
-};
-
-namespace Asylum {
-
-static const ADGameDescription gameDescriptions[] = {
-
-	// Sanitarium - English Demo
-	{ "asylum", "Demo", {
-		{"SNTRM.DAT", 0, "6a8079e0b49affc99d148624fbb584a1", 326},
-		{"RES.000", 0, "57446ce089c6f9decb7f3faf41d09edf", 9022},
-		AD_LISTEND},
-		Common::EN_ANY, Common::kPlatformWindows, ADGF_DEMO | ADGF_UNSTABLE, GUIO0()
-	},
-
-	{ "asylum", "Unpatched",{
-		{"SNTRM.DAT", 0, "7cfcc457c1f579fbf9878ac175d29374", 8930},
-		{"RES.000", 0, "4d13f1412f9e7dd3eaf0a58f0e00e662", 272057},
-		{"SCN.006", 0, "71a5f49cbda597a11e877589075e27b5", 2918330},
-		AD_LISTEND},
-		Common::EN_ANY, Common::kPlatformWindows, ADGF_UNSTABLE, GUIO0()
-	},
-
-	// Level 2 Lockout Bug patch was applied
-	{ "asylum", "Patched", {
-		{"SNTRM.DAT", 0, "7cfcc457c1f579fbf9878ac175d29374", 8930},
-		{"RES.000", 0, "4d13f1412f9e7dd3eaf0a58f0e00e662", 272057},
-		{"SCN.006", 0, "3a5b54da08198012dc0614114782d5fb", 2918330},
-		 AD_LISTEND}, Common::EN_ANY, Common::kPlatformWindows, ADGF_UNSTABLE, GUIO0()
-	},
-
-	{ "asylum", "GOG.com", {
-		{"SNTRM.DAT", 0, "7cfcc457c1f579fbf9878ac175d29374", 8930},
-		{"RES.000", 0, "f58f8dc3e63663f174977d359e11132c", 272057},
-		{"SCN.006", 0, "3a5b54da08198012dc0614114782d5fb", 2918330},
-		AD_LISTEND},
-		Common::EN_ANY, Common::kPlatformWindows, ADGF_UNSTABLE, GUIO0()
-	},
-
-	{ "asylum", "French Version", {
-		{"SNTRM.DAT", 0, "e09a12543c6ede1727c8ecffb74e7fd2", 8930},
-		{"RES.000", 0, "2a4677ee3fd7bfa3161e16cd59371238", 277931},
-		{"SCN.006", 0, "3a5b54da08198012dc0614114782d5fb", 2918330},
-		AD_LISTEND},
-		Common::FR_FRA, Common::kPlatformWindows, ADGF_UNSTABLE, GUIO0()
-	},
-
-	{ "asylum", "Russian Version", {
-		{"SNTRM.DAT", 0, "7cfcc457c1f579fbf9878ac175d29374", 8930},
-		{"RES.000", 0, "1f8a262bf8b3035bd3cfda24068c4651", 263624},
-		{"SCN.006", 0, "3a5b54da08198012dc0614114782d5fb", 2918330},
-		AD_LISTEND},
-		Common::RU_RUS, Common::kPlatformWindows, ADGF_UNSTABLE, GUIO0()
-	},
-
-	AD_TABLE_END_MARKER};
-
-static const ADFileBasedFallback fileBasedFallback[] = {
-	{ &Asylum::gameDescriptions[0], { "SNTRM.DAT", 0 } }, // default to english version
-	{ 0, {0} } };
-
-
-static const char *directoryGlobs[] = {
-	"Data",
-	0
-};
-
-} // End of namespace Asylum
-
+#include "asylum/detection_table.h"
 
 class AsylumMetaEngineDetection : public AdvancedMetaEngineDetection {
 public:
@@ -112,7 +41,7 @@ public:
 	}
 
 	const char *getName() const {
-		return "Asylum";
+		return "Sanitarium";
 	}
 
 	const char *getOriginalCopyright() const {
--- engines/asylum/metaengine.cpp
+++ engines/asylum/metaengine.cpp
@@ -29,7 +29,7 @@
 class AsylumMetaEngine : public AdvancedMetaEngine {
 public:
 	virtual const char *getName() const override {
-		return "Asylum";
+		return "Sanitairum";
 	}
 
 	virtual const char *getOriginalCopyright() const {
