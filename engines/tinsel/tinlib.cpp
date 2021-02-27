5901737aa74e0daf113b428b41efb69c47fdb5a5
Einar Johan Trøan Sømåen <somaen@scummvm.org>
2021-02-28 00:36:12 +0100

    TINSEL: Map WHICHCD

--- engines/tinsel/tinlib.cpp
+++ engines/tinsel/tinlib.cpp
@@ -4359,6 +4359,10 @@ NoirMapping translateNoirLibCode(int libCode, int32 *pp) {
 		pp -= mapping.numArgs - 1;
 		debug(7, "%s(%d, %d)", mapping.name, pp[0], pp[1]);
 		break;
+	case 207:
+		mapping = NoirMapping{"WHICHCD", WHICHCD, 0};
+		debug(7, "%s()", mapping.name);
+		break;
 	case 208:
 		mapping = NoirMapping{"WHICHINVENTORY", WHICHINVENTORY, 0};
 		debug(7, "%s()", mapping.name);
@@ -5860,7 +5864,7 @@ int CallLibraryRoutine(CORO_PARAM, int operand, int32 *pp, const INT_CONTEXT *pi
 		return 0;
 
 	case WHICHCD:
-		// DW2 only
+		// DW2 / Noir
 		pp[0] = WhichCd();
 		return 0;
 
