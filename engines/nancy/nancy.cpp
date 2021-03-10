ee3e95c107f838de4c1d1648ff25ecb9ba6ad71e
fracturehill <strahy@outlook.com>
2021-03-11 01:21:48 +0200

    NANCY: Use serializer when reading boot summary
    Changed the readBootSummary function so it uses a serializer with the game type enum as its version. With this change, we don't need separate subclasses just to read every different BSUM format, so all of the NancyEngine child classes have been removed.

--- engines/nancy/nancy.cpp
+++ engines/nancy/nancy.cpp
@@ -345,31 +345,12 @@ Common::String NancyEngine::readFilename(Common::ReadStream *stream) const {
 	return Common::String(buf);
 }
 
-void NancyEngine::readImageList(const IFF &boot, const Common::String &prefix, ImageList &list) {
-	Common::SeekableReadStream *bsum = getBootChunkStream("BSUM");
-	byte count = bsum->readByte();
-	debugC(1, kDebugEngine, "Found %i %s images", count, prefix.c_str());
-
-	for (int i = 0; i < count; ++i) {
-		Common::String chunkName = Common::String::format("%s%d", prefix.c_str(), i);
-		Common::SeekableReadStream *chunkStream = boot.getChunkStream(chunkName);
-
-		if (!chunkStream)
-			error("Failed to read BOOT %s", chunkName.c_str());
-
-		Image image;
-		image.name = readFilename(chunkStream);
-		chunkStream->skip(1);
-		image.width = chunkStream->readUint16LE();
-		image.height = chunkStream->readUint16LE();
-
-		if (chunkStream->err())
-			error("Error reading %s%d", prefix.c_str(), i);
-
-		debugC(1, kDebugEngine, "Adding %s (%dx%d)", image.name.c_str(), image.width, image.height);
-		list.push_back(image);
-
-		delete chunkStream;
+void NancyEngine::readChunkList(const IFF &boot, Common::Serializer &ser, const Common::String &prefix) {
+	byte numChunks;
+	ser.syncAsByte(numChunks);
+	for (byte i = 0; i < numChunks; ++ i) {
+		Common::String name = Common::String::format("%s%d", prefix.c_str(), i);
+		addBootChunk(name, boot.getChunkStream(name));
 	}
 }
 
@@ -456,70 +437,59 @@ private:
 	virtual void readBootSummary(const IFF &boot);
 };
 
-void NancyEngine_v0::readBootSummary(const IFF &boot) {
+void NancyEngine::readBootSummary(const IFF &boot) {
 	Common::SeekableReadStream *bsum = getBootChunkStream("BSUM");
-	bsum->seek(0xa3);
-	firstSceneID = bsum->readUint16LE();
-	bsum->skip(4);
-	startTimeHours = bsum->readUint16LE(); // this is a whole Time struct but we just take the hours for now
+	bsum->seek(0);
+
+	// Use a serializer to handle several games' BSUMs in the same function
+	Common::Serializer ser(bsum, nullptr);
+	ser.setVersion(_gameDescription->gameType);
+
+	ser.skip(0xA3, kGameTypeVampire, kGameTypeNancy2);
+	ser.syncAsUint16LE(firstSceneID);
+	ser.skip(4, kGameTypeVampire, kGameTypeNancy2);
+	ser.syncAsUint16LE(startTimeHours);
+	
+	ser.skip(0x80, kGameTypeVampire, kGameTypeVampire);
+	ser.skip(0xA6, kGameTypeNancy1, kGameTypeNancy1);
+	ser.skip(0xA0, kGameTypeNancy2, kGameTypeNancy2);
+
+	// nancy3 has not been looked into, skip straight to images
+	ser.skip(0xA7, kGameTypeNancy3, kGameTypeNancy3);
+
 	bsum->seek(0x151);
-	readImageList(boot, "FR", _frames);
-	readImageList(boot, "LG", _logos);
-	readImageList(boot, "OB", _objects);
-	bsum->seek(0x1D1);
-	_fontSize = bsum->readSint32LE() * 1346;
-	bsum->seek(0x1ED);
-	NancySceneState.playerTimeMinuteLength = bsum->readSint16LE();
-	bsum->seek(0x1F1);
-    overrideMovementTimeDeltas = bsum->readByte();
+	readChunkList(boot, ser, "FR");
+	readChunkList(boot, ser, "LG");
+
+	if (ser.getVersion() < kGameTypeNancy3) {
+		readChunkList(boot, ser, "OB");
+	}
+
+	ser.skip(0x99, kGameTypeNancy1, kGameTypeNancy1);
+	int16 time = 0;
+	ser.syncAsSint16LE(time, kGameTypeNancy1, kGameTypeNancy1);
+	NancySceneState.playerTimeMinuteLength = time;
+	ser.skip(2, kGameTypeNancy1, kGameTypeNancy1);
+    ser.syncAsByte(overrideMovementTimeDeltas, kGameTypeNancy1, kGameTypeNancy1);
 
     if (overrideMovementTimeDeltas) {
-        slowMovementTimeDelta = bsum->readUint16LE();
-        fastMovementTimeDelta = bsum->readUint16LE();
+		ser.syncAsSint16LE(time, kGameTypeNancy1, kGameTypeNancy1);
+		slowMovementTimeDelta = time;
+		ser.syncAsSint16LE(time, kGameTypeNancy1, kGameTypeNancy1);
+		fastMovementTimeDelta = time;
     }
-}
-
-class NancyEngine_v1 : public NancyEngine_v0 {
-public:
-	NancyEngine_v1(OSystem *syst, const NancyGameDescription *gd) : NancyEngine_v0(syst, gd) { }
-
-private:
-	virtual void readBootSummary(const IFF &boot);
-};
-
-void NancyEngine_v1::readBootSummary(const IFF &boot) {
-	Common::SeekableReadStream *bsum = getBootChunkStream("BSUM");
-	bsum->seek(0xa3);
-	firstSceneID = bsum->readUint16LE();
-	bsum->seek(0x14b);
-	readImageList(boot, "FR", _frames);
-	readImageList(boot, "LG", _logos);
-}
-
-class NancyEngine_v2 : public NancyEngine_v1 {
-public:
-	NancyEngine_v2(OSystem *syst, const NancyGameDescription *gd) : NancyEngine_v1(syst, gd) { }
-
-private:
-	virtual uint getFilenameLen() const { return 32; }
-	virtual void readBootSummary(const IFF &boot);
-};
-
-void NancyEngine_v2::readBootSummary(const IFF &boot) {
-	Common::SeekableReadStream *bsum = getBootChunkStream("BSUM");
-	bsum->seek(0xa7);
-	readImageList(boot, "FR", _frames);
-	readImageList(boot, "LG", _logos);
 }
 
 NancyEngine *NancyEngine::create(GameType type, OSystem *syst, const NancyGameDescription *gd) {
 	switch (type) {
+	case kGameTypeVampire:
+		return new NancyEngine(syst, gd);
 	case kGameTypeNancy1:
-		return new NancyEngine_v0(syst, gd);
+		return new NancyEngine(syst, gd);
 	case kGameTypeNancy2:
-		return new NancyEngine_v1(syst, gd);
+		return new NancyEngine(syst, gd);
 	case kGameTypeNancy3:
-		return new NancyEngine_v2(syst, gd);
+		return new NancyEngine(syst, gd);
 	default:
 		error("Unknown GameType");
 	}
