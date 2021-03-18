41c4852195aecf06284dc2ceb1a8a3489ace5d7d
D G Turner <digitall@scummvm.org>
2021-03-18 18:31:41 +0000

    PRIVATE: Fix Unused Function GCC Warning
    This was in the lexer parser code autogenerated by flex. The relevant
    functions have been commented out, rather than removed to make it easier
    to merge changes if this parser code needs to be regenerated.

--- engines/private/lexer.cpp
+++ engines/private/lexer.cpp
@@ -438,7 +438,7 @@ extern FILE *yyin, *yyout;
 		YY_DO_BEFORE_ACTION; /* set up yytext again */ \
 		} \
 	while ( 0 )
-#define unput(c) yyunput( c, (yytext_ptr)  )
+//#define unput(c) yyunput( c, (yytext_ptr)  )
 
 #ifndef YY_STRUCT_YY_BUFFER_STATE
 #define YY_STRUCT_YY_BUFFER_STATE
@@ -848,11 +848,11 @@ extern int yywrap ( void );
 #endif
 #endif
 
-#ifndef YY_NO_UNPUT
-    
-    static void yyunput ( int c, char *buf_ptr  );
-    
-#endif
+//#ifndef YY_NO_UNPUT
+//    
+//    static void yyunput ( int c, char *buf_ptr  );
+//    
+//#endif
 
 #ifndef yytext_ptr
 static void yy_flex_strncpy ( char *, const char *, int );
@@ -1512,46 +1512,46 @@ static int yy_get_next_buffer (void)
 		return yy_is_jam ? 0 : yy_current_state;
 }
 
-#ifndef YY_NO_UNPUT
-
-    static void yyunput (int c, char * yy_bp )
-{
-	char *yy_cp;
-    
-    yy_cp = (yy_c_buf_p);
-
-	/* undo effects of setting up yytext */
-	*yy_cp = (yy_hold_char);
-
-	if ( yy_cp < YY_CURRENT_BUFFER_LVALUE->yy_ch_buf + 2 )
-		{ /* need to shift things up to make room */
-		/* +2 for EOB chars. */
-		int number_to_move = (yy_n_chars) + 2;
-		char *dest = &YY_CURRENT_BUFFER_LVALUE->yy_ch_buf[
-					YY_CURRENT_BUFFER_LVALUE->yy_buf_size + 2];
-		char *source =
-				&YY_CURRENT_BUFFER_LVALUE->yy_ch_buf[number_to_move];
-
-		while ( source > YY_CURRENT_BUFFER_LVALUE->yy_ch_buf )
-			*--dest = *--source;
-
-		yy_cp += (int) (dest - source);
-		yy_bp += (int) (dest - source);
-		YY_CURRENT_BUFFER_LVALUE->yy_n_chars =
-			(yy_n_chars) = (int) YY_CURRENT_BUFFER_LVALUE->yy_buf_size;
-
-		if ( yy_cp < YY_CURRENT_BUFFER_LVALUE->yy_ch_buf + 2 )
-			YY_FATAL_ERROR( "flex scanner push-back overflow" );
-		}
-
-	*--yy_cp = (char) c;
-
-	(yytext_ptr) = yy_bp;
-	(yy_hold_char) = *yy_cp;
-	(yy_c_buf_p) = yy_cp;
-}
-
-#endif
+//#ifndef YY_NO_UNPUT
+//
+//    static void yyunput (int c, char * yy_bp )
+//{
+//	char *yy_cp;
+//    
+//    yy_cp = (yy_c_buf_p);
+//
+//	/* undo effects of setting up yytext */
+//	*yy_cp = (yy_hold_char);
+//
+//	if ( yy_cp < YY_CURRENT_BUFFER_LVALUE->yy_ch_buf + 2 )
+//		{ /* need to shift things up to make room */
+//		/* +2 for EOB chars. */
+//		int number_to_move = (yy_n_chars) + 2;
+//		char *dest = &YY_CURRENT_BUFFER_LVALUE->yy_ch_buf[
+//					YY_CURRENT_BUFFER_LVALUE->yy_buf_size + 2];
+//		char *source =
+//				&YY_CURRENT_BUFFER_LVALUE->yy_ch_buf[number_to_move];
+//
+//		while ( source > YY_CURRENT_BUFFER_LVALUE->yy_ch_buf )
+//			*--dest = *--source;
+//
+//		yy_cp += (int) (dest - source);
+//		yy_bp += (int) (dest - source);
+//		YY_CURRENT_BUFFER_LVALUE->yy_n_chars =
+//			(yy_n_chars) = (int) YY_CURRENT_BUFFER_LVALUE->yy_buf_size;
+//
+//		if ( yy_cp < YY_CURRENT_BUFFER_LVALUE->yy_ch_buf + 2 )
+//			YY_FATAL_ERROR( "flex scanner push-back overflow" );
+//		}
+//
+//	*--yy_cp = (char) c;
+//
+//	(yytext_ptr) = yy_bp;
+//	(yy_hold_char) = *yy_cp;
+//	(yy_c_buf_p) = yy_cp;
+//}
+//
+//#endif
 
 #ifndef YY_NO_INPUT
 #ifdef __cplusplus
