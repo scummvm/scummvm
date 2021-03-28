4b5eefb731a34d05a8c79d96ef9db1924ef65286
Cameron Cawley <ccawley2011@gmail.com>
2021-03-28 14:33:22 +0100

    NANCY: Update the console code

--- engines/nancy/input.cpp
+++ engines/nancy/input.cpp
@@ -43,10 +43,7 @@ void InputManager::processEvents() {
 	while (g_nancy->getEventManager()->pollEvent(event)) {
 		switch (event.type) {
 		case EVENT_KEYDOWN:
-			if (event.kbd.keycode == KEYCODE_d && event.kbd.flags & Common::KBD_CTRL) {
-				// Launch debug console
-				g_nancy->_launchConsole = true;
-			} else if (event.kbd.keycode == KEYCODE_q && event.kbd.flags & Common::KBD_CTRL) {
+			if (event.kbd.keycode == KEYCODE_q && event.kbd.flags & Common::KBD_CTRL) {
 				// Quit
 				g_nancy->quitGame();
 			} else {
