cece8db3ce2e98002cbe2874c4521c1cc0c90467
Strangerke <arnaud.boutonne@gmail.com>
2021-03-31 23:32:57 +0100

    TRECISION: Revert accidental change in create_msvc.bat

--- dists/msvc/create_msvc.bat
+++ dists/msvc/create_msvc.bat
@@ -55,7 +55,7 @@ goto done
 echo.
 echo Creating project files with all engines enabled (stable and unstable)
 echo.
-create_project ..\.. --disable-all-engines --use-canonical-lib-names --msvc --enable-engine=trecision
+create_project ..\.. --enable-all-engines --use-canonical-lib-names --msvc
 goto done
 
 :stable
