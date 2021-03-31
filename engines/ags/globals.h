scummvm/scummvm

commit 3a3f31cd56bbc9a335d3ad9b5841136cc48ac8f3

AGS: Move Allegro color.cpp globals to Globals

engines/ags/globals.h
@@ -31,6 +31,7 @@
 #include "ags/lib/std/array.h"
 #include "ags/lib/std/set.h"
 #include "ags/lib/allegro/fixed.h"
+#include "ags/lib/allegro/aintern.h"
 
 namespace AGS3 {
 
@@ -87,6 +88,7 @@ struct CCInventory;
 struct CCObject;
 struct CCRegion;
 struct CharacterCache;
+struct COLOR_MAP;
 struct DirtyRects;
 struct ExecutingScript;
 struct GameFrameSetup;
@@ -99,6 +101,7 @@ struct MoveList;
 struct NonBlockingScriptFunction;
 struct ObjectCache;
 struct ResourcePaths;
+struct RGB_MAP;
 struct RoomCameraDrawData;
 struct RoomStatus;
 struct RuntimeScriptValue;
@@ -121,13 +124,31 @@ struct ViewStruct;
 class Globals {
 public:
 	/**
-	 * \defgroup Overall flags
+	 * \defgroup Allegro globals
 	 * @{
 	 */
 
-	// Major overall flags
-	bool _want_exit = false;
-	bool _abort_engine = false;
+	int __rgb_r_shift_15 = DEFAULT_RGB_R_SHIFT_15;     /* truecolor pixel format */
+	int __rgb_g_shift_15 = DEFAULT_RGB_G_SHIFT_15;
+	int __rgb_b_shift_15 = DEFAULT_RGB_B_SHIFT_15;
+	int __rgb_r_shift_16 = DEFAULT_RGB_R_SHIFT_16;
+	int __rgb_g_shift_16 = DEFAULT_RGB_G_SHIFT_16;
+	int __rgb_b_shift_16 = DEFAULT_RGB_B_SHIFT_16;
+	int __rgb_r_shift_24 = DEFAULT_RGB_R_SHIFT_24;
+	int __rgb_g_shift_24 = DEFAULT_RGB_G_SHIFT_24;
+	int __rgb_b_shift_24 = DEFAULT_RGB_B_SHIFT_24;
+	int __rgb_r_shift_32 = DEFAULT_RGB_R_SHIFT_32;
+	int __rgb_g_shift_32 = DEFAULT_RGB_G_SHIFT_32;
+	int __rgb_b_shift_32 = DEFAULT_RGB_B_SHIFT_32;
+	int __rgb_a_shift_32 = DEFAULT_RGB_A_SHIFT_32;
+
+	RGB_MAP *_rgb_map;
+	COLOR_MAP *_color_map;
+	int _trans_blend_alpha = 0;
+	int _trans_blend_red = 0;
+	int _trans_blend_green = 0;
+	int _trans_blend_blue = 0;
+	BlenderMode __blender_mode = kRgbToRgbBlender;
 
 	/**@}*/
 
@@ -338,6 +359,10 @@ public:
 	AGS::Shared::String _saveGameDirectory;
 	AGS::Shared::String _saveGameParent;
 	AGS::Shared::String _saveGameSuffix;
+
+	// Major overall flags
+	bool _want_exit = false;
+	bool _abort_engine = false;
 
 	 /**@}*/
 

engines/ags/engine/ac/draw.cpp
@@ -183,9 +183,9 @@ Bitmap *convert_16_to_16bgr(Bitmap *tempbl) {
 				ds = _rgb_scale_6[(c >> 5) & 0x3F];
 				r = _rgb_scale_5[(c >> 11) & 0x1F];
 				// allegro assumes 5-6-5 for 16-bit
-				p16[x] = (((r >> _places_r) << _rgb_r_shift_16) |
-				          ((ds >> _places_g) << _rgb_g_shift_16) |
-				          ((b >> _places_b) << _rgb_b_shift_16));
+				p16[x] = (((r >> _places_r) << _G(_rgb_r_shift_16)) |
+				          ((ds >> _places_g) << _G(_rgb_g_shift_16)) |
+				          ((b >> _places_b) << _G(_rgb_b_shift_16)));
 
 			}
 		}

engines/ags/engine/ac/draw.h
@@ -50,11 +50,11 @@ using namespace AGS; // FIXME later
 // were included, because they override few Allegro function names;
 // otherwise Allegro headers should not be included at all to the same
 // code unit which uses these defines.
-#define getr32(xx) ((xx >> _rgb_r_shift_32) & 0xFF)
-#define getg32(xx) ((xx >> _rgb_g_shift_32) & 0xFF)
-#define getb32(xx) ((xx >> _rgb_b_shift_32) & 0xFF)
-#define geta32(xx) ((xx >> _rgb_a_shift_32) & 0xFF)
-#define makeacol32(r,g,b,a) ((r << _rgb_r_shift_32) | (g << _rgb_g_shift_32) | (b << _rgb_b_shift_32) | (a << _rgb_a_shift_32))
+#define getr32(xx) ((xx >> _G(_rgb_r_shift_32)) & 0xFF)
+#define getg32(xx) ((xx >> _G(_rgb_g_shift_32)) & 0xFF)
+#define getb32(xx) ((xx >> _G(_rgb_b_shift_32)) & 0xFF)
+#define geta32(xx) ((xx >> _G(_rgb_a_shift_32)) & 0xFF)
+#define makeacol32(r,g,b,a) ((r << _G(_rgb_r_shift_32)) | (g << _G(_rgb_g_shift_32)) | (b << _G(_rgb_b_shift_32)) | (a << _G(_rgb_a_shift_32)))
 
 
 struct CachedActSpsData {

engines/ags/engine/ac/global_debug.cpp
@@ -98,7 +98,7 @@ void script_debug(int cmdd, int dataa) {
 	} else if (cmdd == 1) {
 		String toDisplay = GetRuntimeInfo();
 		Display(toDisplay.GetCStr());
-		//    Display("shftR: %d  shftG: %d  shftB: %d", _rgb_r_shift_16, _rgb_g_shift_16, _rgb_b_shift_16);
+		//    Display("shftR: %d  shftG: %d  shftB: %d", _G(_rgb_r_shift_16), _G(_rgb_g_shift_16), _G(_rgb_b_shift_16));
 		//    Display("Remaining memory: %d kb",_go32_dpmi_remaining_virtual_memory()/1024);
 		//Display("Play char bcd: %d",->GetColorDepth(_GP(spriteset)[_G(views)[playerchar->view].frames[playerchar->loop][playerchar->frame].pic]));
 	} else if (cmdd == 2) {

engines/ags/engine/ac/region.cpp
@@ -123,9 +123,9 @@ void Region_RunInteraction(ScriptRegion *ssr, int mood) {
 //=============================================================================
 
 void generate_light_table() {
-	if (_GP(game).color_depth == 1 && color_map == nullptr) {
+	if (_GP(game).color_depth == 1 && _G(color_map) == nullptr) {
 		create_light_table(&maincoltable, palette, 0, 0, 0, nullptr);
-		color_map = &maincoltable;
+		_G(color_map) = &maincoltable;
 	}
 }
 

engines/ags/engine/ac/room.cpp
@@ -682,7 +682,7 @@ void load_new_room(int newnum, CharacterInfo *forchar) {
 				palette[ff].b = 63;
 		}
 		create_rgb_table(&rgb_table, palette, nullptr);
-		rgb_map = &rgb_table;
+		_G(rgb_map) = &rgb_table;
 	}
 	our_eip = 211;
 	if (forchar != nullptr) {
@@ -860,7 +860,7 @@ void load_new_room(int newnum, CharacterInfo *forchar) {
 		else forchar->view = _GP(thisroom).Options.PlayerView - 1;
 		forchar->frame = 0; // make him standing
 	}
-	color_map = nullptr;
+	_G(color_map) = nullptr;
 
 	our_eip = 209;
 	update_polled_stuff_if_runtime();

engines/ags/engine/gfx/gfxdriverbase.cpp
@@ -27,6 +27,7 @@
 #include "ags/engine/gfx/gfxdriverbase.h"
 #include "ags/engine/gfx/gfx_util.h"
 #include "ags/engine/ac/draw.h"
+#include "ags/globals.h"
 
 namespace AGS3 {
 

engines/ags/engine/main/engine_setup.cpp
@@ -179,21 +179,21 @@ void engine_pre_gfxsystem_screen_destroy() {
 // Setup color conversion parameters
 void engine_setup_color_conversions(int coldepth) {
 	// default shifts for how we store the sprite data1
-	_rgb_r_shift_32 = 16;
-	_rgb_g_shift_32 = 8;
-	_rgb_b_shift_32 = 0;
-	_rgb_r_shift_16 = 11;
-	_rgb_g_shift_16 = 5;
-	_rgb_b_shift_16 = 0;
-	_rgb_r_shift_15 = 10;
-	_rgb_g_shift_15 = 5;
-	_rgb_b_shift_15 = 0;
+	_G(_rgb_r_shift_32) = 16;
+	_G(_rgb_g_shift_32) = 8;
+	_G(_rgb_b_shift_32) = 0;
+	_G(_rgb_r_shift_16) = 11;
+	_G(_rgb_g_shift_16) = 5;
+	_G(_rgb_b_shift_16) = 0;
+	_G(_rgb_r_shift_15) = 10;
+	_G(_rgb_g_shift_15) = 5;
+	_G(_rgb_b_shift_15) = 0;
 
 	// Most cards do 5-6-5 RGB, which is the format the files are saved in
 	// Some do 5-6-5 BGR, or  6-5-5 RGB, in which case convert the gfx
-	if ((coldepth == 16) && ((_rgb_b_shift_16 != 0) || (_rgb_r_shift_16 != 11))) {
+	if ((coldepth == 16) && ((_G(_rgb_b_shift_16) != 0) || (_G(_rgb_r_shift_16) != 11))) {
 		convert_16bit_bgr = 1;
-		if (_rgb_r_shift_16 == 10) {
+		if (_G(_rgb_r_shift_16) == 10) {
 			// some very old graphics cards lie about being 16-bit when they
 			// are in fact 15-bit ... get around this
 			_places_r = 3;
@@ -205,43 +205,43 @@ void engine_setup_color_conversions(int coldepth) {
 		// the wrong way round - so fix that
 
 #if AGS_PLATFORM_OS_IOS || AGS_PLATFORM_OS_ANDROID
-		_rgb_b_shift_16 = 0;
-		_rgb_g_shift_16 = 5;
-		_rgb_r_shift_16 = 11;
+		_G(_rgb_b_shift_16) = 0;
+		_G(_rgb_g_shift_16) = 5;
+		_G(_rgb_r_shift_16) = 11;
 
-		_rgb_b_shift_15 = 0;
-		_rgb_g_shift_15 = 5;
-		_rgb_r_shift_15 = 10;
+		_G(_rgb_b_shift_15) = 0;
+		_G(_rgb_g_shift_15) = 5;
+		_G(_rgb_r_shift_15) = 10;
 
-		_rgb_r_shift_32 = 0;
-		_rgb_g_shift_32 = 8;
-		_rgb_b_shift_32 = 16;
+		_G(_rgb_r_shift_32) = 0;
+		_G(_rgb_g_shift_32) = 8;
+		_G(_rgb_b_shift_32) = 16;
 #else
-		_rgb_r_shift_16 = 11;
-		_rgb_g_shift_16 = 5;
-		_rgb_b_shift_16 = 0;
+		_G(_rgb_r_shift_16) = 11;
+		_G(_rgb_g_shift_16) = 5;
+		_G(_rgb_b_shift_16) = 0;
 #endif
 	} else if (coldepth == 16) {
 		// ensure that any 32-bit graphics displayed are converted
 		// properly to the current depth
-		_rgb_r_shift_32 = 16;
-		_rgb_g_shift_32 = 8;
-		_rgb_b_shift_32 = 0;
+		_G(_rgb_r_shift_32) = 16;
+		_G(_rgb_g_shift_32) = 8;
+		_G(_rgb_b_shift_32) = 0;
 	} else if (coldepth < 16) {
 		// ensure that any 32-bit graphics displayed are converted
 		// properly to the current depth
 #if AGS_PLATFORM_OS_WINDOWS
-		_rgb_r_shift_32 = 16;
-		_rgb_g_shift_32 = 8;
-		_rgb_b_shift_32 = 0;
+		_G(_rgb_r_shift_32) = 16;
+		_G(_rgb_g_shift_32) = 8;
+		_G(_rgb_b_shift_32) = 0;
 #else
-		_rgb_r_shift_32 = 0;
-		_rgb_g_shift_32 = 8;
-		_rgb_b_shift_32 = 16;
+		_G(_rgb_r_shift_32) = 0;
+		_G(_rgb_g_shift_32) = 8;
+		_G(_rgb_b_shift_32) = 16;
 
-		_rgb_b_shift_15 = 0;
-		_rgb_g_shift_15 = 5;
-		_rgb_r_shift_15 = 10;
+		_G(_rgb_b_shift_15) = 0;
+		_G(_rgb_g_shift_15) = 5;
+		_G(_rgb_r_shift_15) = 10;
 #endif
 	}
 

commit 329351a279e5a7edb51df17556b81c876a34f262

AGS: Fix access to uninitialized memory
This is a regression from 3a3f31cd56.
Global variables are initialized to 0 implicitly, but class
variables are not. So those two pointers were no longer
initialized to nullptr, causing possible use of uninitialized
memory and then of unallocated memory when trying to access
what they were pointing to. This caused random crashes in
paletted games.

engines/ags/globals.h
@@ -185,8 +185,8 @@ public:
 	int __rgb_b_shift_32 = DEFAULT_RGB_B_SHIFT_32;
 	int __rgb_a_shift_32 = DEFAULT_RGB_A_SHIFT_32;
 
-	RGB_MAP *_rgb_map;
-	COLOR_MAP *_color_map;
+	RGB_MAP *_rgb_map = nullptr;
+	COLOR_MAP *_color_map = nullptr;
 	int _trans_blend_alpha = 0;
 	int _trans_blend_red = 0;
 	int _trans_blend_green = 0;