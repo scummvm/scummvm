72402472a20a5d02e5564e9224cdf7aae7b0fe32
Eugene Sandulenko <sev@scummvm.org>
2021-04-04 21:48:07 +0200

    GUI: Added SVG support to theme parser

--- gui/themes/scummtheme.py
+++ gui/themes/scummtheme.py
@@ -5,7 +5,7 @@ import re
 import os
 import zipfile
 
-THEME_FILE_EXTENSIONS = ('.stx', '.bmp', '.fcc', '.ttf', '.png')
+THEME_FILE_EXTENSIONS = ('.stx', '.bmp', '.fcc', '.ttf', '.png', '.svg')
 
 def buildTheme(themeName):
 	if not os.path.isdir(themeName) or not os.path.isfile(os.path.join(themeName, "THEMERC")):
