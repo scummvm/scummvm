916859ab864c76fd2266a5d88c3b6ed57af41819
Eugene Sandulenko <sev@scummvm.org>
2021-04-06 01:07:44 +0200

    GRAPHICS: Added (hacky) blitManagedSurface
    So far, it is hardcoded, but in reality we need to merge
    ManagedSurface and TransparentSurface and add blit() methods
    to TransparentSurface

--- graphics/VectorRendererSpec.h
+++ graphics/VectorRendererSpec.h
@@ -90,6 +90,7 @@ public:
 	void fillSurface() override;
 	void blitSurface(const Graphics::Surface *source, const Common::Rect &r) override;
 	void blitSubSurface(const Graphics::Surface *source, const Common::Point &p) override;
+	void blitManagedSurface(const Graphics::ManagedSurface *source, const Common::Point &p) override;
 	void blitKeyBitmap(const Graphics::Surface *source, const Common::Point &p) override;
 	void blitAlphaBitmap(Graphics::TransparentSurface *source, const Common::Rect &r,
 			GUI::ThemeEngine::AutoScaleMode autoscale = GUI::ThemeEngine::kAutoScaleNone,
