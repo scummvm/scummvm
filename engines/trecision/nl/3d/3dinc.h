6b2c0c22a736cf039459f5734db4fe74b455ebe4
Filippos Karapetis <bluegr@gmail.com>
2021-04-09 17:47:33 +0300

    TRECISION: Remove 3dproto.h

--- engines/trecision/nl/3d/3dinc.h
+++ engines/trecision/nl/3d/3dinc.h
@@ -22,6 +22,7 @@
 #ifndef TRECISION_3DINC_H
 #define TRECISION_3DINC_H
 
+#include "common/str.h"
 #include "trecision/nl/3d/3drend.h"
 
 /*--------------------------------------------------
@@ -183,8 +184,50 @@ extern uint8  _defActionLen[];
 
 extern bool   _shadowSplit;
 
-} // End of namespace Trecision
+int actionInRoom(int curA);
+void actorDoAction(int whatAction);
+void actorStop();
+void setPosition(int num);
+void goToPosition(int num);
+void lookAt(float x, float z);
+float sinCosAngle(float sinus, float cosinus);
+void textureTriangle(int32 x1, int32 y1, int32 z1, int32 c1, int32 tx1, int32 ty1, int32 x2, int32 y2, int32 z2, int32 c2, int32 tx2, int32 ty2, int32 x3, int32 y3, int32 z3, int32 c3, int32 tx3, int32 ty3, STexture *t);
+void textureScanEdge(int32 x1, int32 y1, int32 z1, int32 c1, int32 tx1, int32 ty1, int32 x2, int32 y2, int32 z2, int32 c2, int32 tx2, int32 ty2);
+void shadowTriangle(int32 x1, int32 y1, int32 x2, int32 y2, int32 x3, int32 y3, uint8 cv, int32 zv);
+void shadowScanEdge(int32 x1, int32 y1, int32 x2, int32 y2);
+void init3DRoom(int16 dx, uint16 *destBuffer, int16 *zBuffer);
+void setClipping(int16 x1, int16 y1, int16 x2, int16 y2);
+void setZBufferRegion(int16 sx, int16 sy, int16 dx);
+signed char clockWise(int16 x1, int16 y1, int16 x2, int16 y2, int16 x3, int16 y3);
+void drawCharacter(uint8 flag);
+int read3D(Common::String c);
+void findPath();
+void findShortPath();
+float evalPath(int a, float destX, float destZ, int nearP);
+void buildFramelist();
+int nextStep();
+void displayPath();
+int findAttachedPanel(int srcP, int destP);
+int pathCompare(const void *arg1, const void *arg2);
+void sortPath();
+float distF(float x1, float y1, float x2, float y2);
+float dist3D(float x1, float y1, float z1, float x2, float y2, float z2);
+void putPix(int x, int y, uint16 c);
+void whereIs(int px, int py);
+void pointOut();
+void putLine(int x0, int y0, int x1, int y1, uint16 c);
+void viewPanel(SPan *p);
+void pointProject(float x, float y, float z);
+void invPointProject(int x, int y);
+int intersectLinePanel(SPan *p, float x, float y, float z);
+int intersectLineFloor(float x, float y, float z);
+int intersectLineLine(float xa, float ya, float xb, float yb, float xc, float yc, float xd, float yd);
+void initSortPan();
+int panCompare(const void *arg1, const void *arg2);
+void sortPanel();
+void actorOrder();
+char waitKey();
 
-#include "trecision/nl/3d/3dproto.h"
+} // End of namespace Trecision
 
 #endif
