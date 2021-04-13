6880518164c203b40aaaa09347ea93d58adb5ee7
Filippos Karapetis <bluegr@gmail.com>
2021-04-14 02:54:36 +0300

    TRECISION: Add a helper for empty events in the SScriptFrame struct

--- engines/trecision/script.cpp
+++ engines/trecision/script.cpp
@@ -52,7 +52,7 @@ void TrecisionEngine::playScript(uint16 id) {
 
 	SScriptFrame *curFrame = &_scriptFrame[_curScriptFrame[_curStack]];
 	// If the event is empty, terminate the script
-	if ((curFrame->_class == 0) && (curFrame->_event == 0)) {
+	if (curFrame->eventIsEmpty()) {
 		endScript();
 		return;
 	}
@@ -63,7 +63,7 @@ void TrecisionEngine::playScript(uint16 id) {
 		curFrame = &_scriptFrame[_curScriptFrame[_curStack]];
 		SScriptFrame *nextFrame = &_scriptFrame[_curScriptFrame[_curStack] + 1];
 		curFrame->sendFrame();
-		if (curFrame->_noWait && !((nextFrame->_class == 0) && (nextFrame->_event == 0))) {
+		if (curFrame->_noWait && !nextFrame->eventIsEmpty()) {
 			_curScriptFrame[_curStack]++;
 			loop = true;
 		}
@@ -79,7 +79,7 @@ void TrecisionEngine::evalScript() {
 		g_vm->hideCursor();
 
 		SScriptFrame *curFrame = &_scriptFrame[_curScriptFrame[_curStack]];
-		if ((curFrame->_class == 0) && (curFrame->_event == 0)) {
+		if (curFrame->eventIsEmpty()) {
 			endScript();
 			return;
 		}
@@ -90,7 +90,7 @@ void TrecisionEngine::evalScript() {
 			curFrame = &_scriptFrame[_curScriptFrame[_curStack]];
 			SScriptFrame *nextFrame = &_scriptFrame[_curScriptFrame[_curStack] + 1];
 			curFrame->sendFrame();
-			if (curFrame->_noWait && !((nextFrame->_class == 0) && (nextFrame->_event == 0))) {
+			if (curFrame->_noWait && !nextFrame->eventIsEmpty()) {
 				_curScriptFrame[_curStack]++;
 				loop = true;
 			}
