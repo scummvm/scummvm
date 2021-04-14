954b66bd44cf9207437acf86d72ff025528fd978
ysj1173886760 <1173886760@qq.com>
2021-04-14 16:39:28 +0800

    PINK: add Win95 mode flag to pink engine

--- engines/pink/director.cpp
+++ engines/pink/director.cpp
@@ -92,7 +92,7 @@ Director::Director()
 	: _surface(640, 480), _textRendered(false) {
 	_wm = new Graphics::MacWindowManager(Graphics::kWMModeNoDesktop | Graphics::kWMModeAutohideMenu
 		| Graphics::kWMModalMenuMode | Graphics::kWMModeForceBuiltinFonts
-		| Graphics::kWMModeUnicode);
+		| Graphics::kWMModeUnicode | Graphics::kWMModeWin95);
 
 	_wm->setScreen(&_surface);
 	_wm->setMenuHotzone(Common::Rect(0, 0, 640, 23));
