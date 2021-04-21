89a6d5ae108a1745d513f6972d856a4ece47ad12
Eugene Sandulenko <sev@scummvm.org>
2021-04-21 02:21:25 +0200

    GOB: Added detection for alternate French ween. Bugreport #11524

--- engines/gob/detection/tables_ween.h
+++ engines/gob/detection/tables_ween.h
@@ -73,7 +73,21 @@
 	{
 		"ween",
 		"",
-		AD_ENTRY1("intro.stk", "4b10525a3782aa7ecd9d833b5c1d308b"),
+		AD_ENTRY1s("intro.stk", "4b10525a3782aa7ecd9d833b5c1d308b", 7029591),
+		FR_FRA,
+		kPlatformDOS,
+		ADGF_NO_FLAGS,
+		GUIO2(GUIO_NOSUBTITLES, GUIO_NOSPEECH)
+	},
+	kGameTypeWeen,
+	kFeaturesAdLib,
+	0, 0, 0
+},
+{ // Bugreport #11524
+	{
+		"ween",
+		"",
+		AD_ENTRY1s("intro.stk", "cae57980940b919305e33a65d0f1dcc3", 7017982),
 		FR_FRA,
 		kPlatformDOS,
 		ADGF_NO_FLAGS,
