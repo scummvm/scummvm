scummvm/scummvm

commit 280f83242e1e31bd87aeefcaf84cd7f6c6793b81

KYRA: replace const char * with Common::String around getTableString. (#2603)
Currently const char * points into some random buffer with unclear
lifetime.

Instead use Common::String which ensures lifetime as long as a reference is
held.

This also removes the need of copying into existing buffers in HoF and MR.

UnkBuf* are gone as well.

Co-authored-by: athrxx <athrxx@scummvm.org>

engines/kyra/gui/gui_v2.cpp
@@ -430,7 +430,8 @@ void GUI_v2::restorePage1(const uint8 *buffer) {
 
 void GUI_v2::setupSavegameNames(Menu &menu, int num) {
 	for (int i = 0; i < num; ++i) {
-		strcpy(getTableString(menu.item[i].itemId), "");
+		menu.item[i].useItemString = true;
+		menu.item[i].itemString = "";
 		menu.item[i].saveSlot = -1;
 		menu.item[i].enabled = false;
 	}
@@ -443,36 +444,35 @@ void GUI_v2::setupSavegameNames(Menu &menu, int num) {
 	Common::InSaveFile *in;
 	for (int i = startSlot; i < num && uint(_savegameOffset + i) < _saveSlots.size(); ++i) {
 		if ((in = _vm->openSaveForReading(_vm->getSavegameFilename(_saveSlots[i + _savegameOffset]), header)) != 0) {
-			char *s = getTableString(menu.item[i].itemId);
-			Common::strlcpy(s, header.description.c_str(), 80);
-			Util::convertISOToDOS(s);
+			Common::String s = header.description;
+			s = Util::convertISOToDOS(s);
 
 			// Trim long GMM save descriptions to fit our save slots
 			_screen->_charSpacing = -2;
-			int fC = _screen->getTextWidth(s);
-			while (s[0] && fC > 240) {
-				s[strlen(s) - 1]  = 0;
-				fC = _screen->getTextWidth(s);
+			int fC = _screen->getTextWidth(s.c_str());
+			while (!s.empty() && fC > 240) {
+				s.deleteLastChar();
+				fC = _screen->getTextWidth(s.c_str());
 			}
 			_screen->_charSpacing = 0;
 
 			menu.item[i].saveSlot = _saveSlots[i + _savegameOffset];
 			menu.item[i].enabled = true;
+			menu.item[i].useItemString = true;
+			menu.item[i].itemString = s;
 			delete in;
 		}
 	}
 
 	if (_savegameOffset == 0) {
 		if (_isSaveMenu) {
-			char *dst = getTableString(menu.item[0].itemId);
-			const char *src = getTableString(_vm->gameFlags().isTalkie ? 10 : 18);
-			strcpy(dst, src);
 			menu.item[0].saveSlot = -2;
 			menu.item[0].enabled = true;
+			menu.item[0].useItemString = true;
+			menu.item[0].itemString = getTableString(_vm->gameFlags().isTalkie ? 10 : 18);
 		} else {
-			char *dst = getTableString(menu.item[0].itemId);
-			const char *src = getTableString(_vm->gameFlags().isTalkie ? 34 : 42, _vm->gameFlags().lang == Common::RU_RUS);
-			strcpy(dst, src);
+			menu.item[0].useItemString = true;
+			menu.item[0].itemString = getTableString(_vm->gameFlags().isTalkie ? 34 : 42, _vm->gameFlags().lang == Common::RU_RUS);
 		}
 	}
 }
@@ -644,7 +644,7 @@ int GUI_v2::clickSaveSlot(Button *caller) {
 			return 0;
 		} else {
 			_saveSlot = item.saveSlot;
-			strcpy(_saveDescription, getTableString(item.itemId));
+			strcpy(_saveDescription, getTableString(item.itemId).c_str());
 		}
 	} else if (item.saveSlot == -2) {
 		_saveSlot = getNextSavegameSlot();

engines/kyra/engine/items_mr.cpp
@@ -117,7 +117,7 @@ bool KyraEngine_MR::dropItem(int unk1, Item item, int x, int y, int unk2) {
 			showMessageFromCCode(14, 0xB3, 0);
 	}
 
-	if (!_chatText)
+	if (_chatText.empty())
 		snd_playSoundEffect(13, 200);
 	return false;
 }

engines/kyra/engine/kyra_hof.cpp
@@ -94,9 +94,7 @@ KyraEngine_HoF::KyraEngine_HoF(OSystem *system, const GameFlags &flags) : KyraEn
 	_dbgPass = 0;
 
 	_gamePlayBuffer = 0;
-	_unkBuf500Bytes = 0;
 	_inventorySaved = false;
-	_unkBuf200kByte = 0;
 	memset(&_sceneShapeTable, 0, sizeof(_sceneShapeTable));
 
 	_talkObjectList = 0;
@@ -287,7 +285,6 @@ void KyraEngine_HoF::startup() {
 
 	memset(_sceneShapeTable, 0, sizeof(_sceneShapeTable));
 	_gamePlayBuffer = new uint8[46080];
-	_unkBuf500Bytes = new uint8[500];
 
 	loadMouseShapes();
 	loadItemShapes();
@@ -295,7 +292,6 @@ void KyraEngine_HoF::startup() {
 	_screen->setMouseCursor(0, 0, getShapePtr(0));
 
 	_screenBuffer = new uint8[64000];
-	_unkBuf200kByte = new uint8[200000];
 
 	loadChapterBuffer(_newChapterFile);
 
@@ -313,7 +309,7 @@ void KyraEngine_HoF::startup() {
 		_optionsBuffer = _cCodeBuffer;
 	}
 
-	showMessage(0, 207);
+	clearMessage();
 
 	_screen->setShapePages(5, 3);
 
@@ -621,7 +617,7 @@ void KyraEngine_HoF::updateWithText() {
 	restorePage3();
 	drawAnimObjects();
 
-	if (_chatTextEnabled && _chatText) {
+	if (_chatTextEnabled && !_chatText.empty()) {
 		int pageBackUp = _screen->_curPage;
 		_screen->_curPage = 2;
 		objectChatPrintText(_chatText, _chatObject);
@@ -741,8 +737,6 @@ void KyraEngine_HoF::cleanup() {
 	delete[] _inventoryButtons; _inventoryButtons = 0;
 
 	delete[] _gamePlayBuffer; _gamePlayBuffer = 0;
-	delete[] _unkBuf500Bytes; _unkBuf500Bytes = 0;
-	delete[] _unkBuf200kByte; _unkBuf200kByte = 0;
 
 	freeSceneShapePtrs();
 
@@ -821,19 +815,17 @@ uint8 *KyraEngine_HoF::getTableEntry(uint8 *buffer, int id) {
 	return buffer + READ_LE_UINT16(buffer + (id<<1));
 }
 
-char *KyraEngine_HoF::getTableString(int id, uint8 *buffer, bool decode) {
-	char *string = (char *)getTableEntry(buffer, id);
+Common::String KyraEngine_HoF::getTableString(int id, uint8 *buffer, bool decode) {
+	Common::String string((char *)getTableEntry(buffer, id));
 
 	if (decode && _flags.lang != Common::JA_JPN) {
-		Util::decodeString1(string, _internStringBuf);
-		Util::decodeString2(_internStringBuf, _internStringBuf);
-		string = _internStringBuf;
+		string = Util::decodeString2(Util::decodeString1(string));
 	}
 
 	return string;
 }
 
-const char *KyraEngine_HoF::getChapterString(int id) {
+Common::String KyraEngine_HoF::getChapterString(int id) {
 	if (_currentChapter != _newChapterFile)
 		loadChapterBuffer(_newChapterFile);
 
@@ -843,15 +835,14 @@ const char *KyraEngine_HoF::getChapterString(int id) {
 #pragma mark -
 
 void KyraEngine_HoF::showMessageFromCCode(int id, int16 palIndex, int) {
-	const char *string = getTableString(id, _cCodeBuffer, true);
-	showMessage(string, palIndex);
+	showMessage(getTableString(id, _cCodeBuffer, true), palIndex);
 }
 
-void KyraEngine_HoF::showMessage(const char *string, int16 palIndex) {
+void KyraEngine_HoF::showMessage(const Common::String &string, int16 palIndex) {
 	_shownMessage = string;
 	_screen->fillRect(0, 190, 319, 199, 0xCF);
 
-	if (string) {
+	if (!string.empty()) {
 		if (palIndex != -1 || _fadeMessagePalette) {
 			palIndex *= 3;
 			memcpy(_messagePal, _screen->getPalette(0).getData() + palIndex, 3);
@@ -868,32 +859,34 @@ void KyraEngine_HoF::showMessage(const char *string, int16 palIndex) {
 	_fadeMessagePalette = false;
 }
 
+void KyraEngine_HoF::clearMessage() {
+	_shownMessage = "";
+	_screen->fillRect(0, 190, 319, 199, 0xCF);
+	_fadeMessagePalette = false;
+}
+
 void KyraEngine_HoF::showChapterMessage(int id, int16 palIndex) {
 	showMessage(getChapterString(id), palIndex);
 }
 
 void KyraEngine_HoF::updateCommandLineEx(int str1, int str2, int16 palIndex) {
-	char buffer[0x51];
-	char *src = buffer;
-
-	strcpy(src, getTableString(str1, _cCodeBuffer, true));
-
-	if (_flags.lang != Common::JA_JPN) {
-		while (*src != 0x20)
-			++src;
-		++src;
-		*src = toupper(*src);
-	}
-
-	strcpy((char *)_unkBuf500Bytes, src);
-
-	if (str2 > 0) {
-		if (_flags.lang != Common::JA_JPN)
-			strcat((char *)_unkBuf500Bytes, " ");
-		strcat((char *)_unkBuf500Bytes, getTableString(str2, _cCodeBuffer, true));
-	}
-
-	showMessage((char *)_unkBuf500Bytes, palIndex);
+	Common::String str = getTableString(str1, _cCodeBuffer, true);
+ 
+ 	if (_flags.lang != Common::JA_JPN) {
+		uint i = 0;
+		while (str[i] != ' ')
+			++i;
+		++i;
+		str.setChar(toupper(str[i]), i);
+ 	}
+ 
+ 	if (str2 > 0) {
+ 		if (_flags.lang != Common::JA_JPN)
+			str += " ";
+		str += getTableString(str2, _cCodeBuffer, 1);
+ 	}
+ 
+	showMessage(str, palIndex);
 }
 
 void KyraEngine_HoF::fadeMessagePalette() {
@@ -1771,7 +1764,7 @@ bool KyraEngine_HoF::updateCauldron() {
 		}
 
 		if (cauldronState >= 0) {
-			showMessage(0, 0xCF);
+			clearMessage();
 			setCauldronState(cauldronState, true);
 			if (cauldronState == 7)
 				objectChat(getTableString(0xF2, _cCodeBuffer, true), 0, 0x83, 0xF2);
@@ -1784,7 +1777,7 @@ bool KyraEngine_HoF::updateCauldron() {
 }
 
 void KyraEngine_HoF::cauldronRndPaletteFade() {
-	showMessage(0, 0xCF);
+	clearMessage();
 	int index = _rnd.getRandomNumberRng(0x0F, 0x16);
 	Common::SeekableReadStream *file = _res->createReadStream("_POTIONS.PAL");
 	if (!file)
@@ -1831,30 +1824,21 @@ void KyraEngine_HoF::listItemsInCauldron() {
 	} else {
 		objectChat(getTableString(0xF7, _cCodeBuffer, true), 0, 0x83, 0xF7);
 
-		char buffer[80];
 		for (int i = 0; i < itemsInCauldron-1; ++i) {
-			char *str = buffer;
-			strcpy(str, getTableString(_cauldronTable[i]+54, _cCodeBuffer, true));
+			Common::String str = getTableString(_cauldronTable[i]+54, _cCodeBuffer, true);
 			if (_lang == 1) {
-				if (*str == 37)
-					str += 2;
+			    if (str[0] == 37)
+				str = str.substr(2);
 			}
-			strcpy((char *)_unkBuf500Bytes, "...");
-			strcat((char *)_unkBuf500Bytes, str);
-			strcat((char *)_unkBuf500Bytes, "...");
-			objectChat((const char *)_unkBuf500Bytes, 0, 0x83, _cauldronTable[i]+54);
+			objectChat("..." + str + "...", 0, 0x83, _cauldronTable[i]+54);
 		}
 
-		char *str = buffer;
-		strcpy(str, getTableString(_cauldronTable[itemsInCauldron-1]+54, _cCodeBuffer, true));
+		Common::String str = getTableString(_cauldronTable[itemsInCauldron-1]+54, _cCodeBuffer, true);
 		if (_lang == 1) {
-			if (*str == 37)
-				str += 2;
+			if (str[0] == 37)
+			    str = str.substr(2);
 		}
-		strcpy((char *)_unkBuf500Bytes, "...");
-		strcat((char *)_unkBuf500Bytes, str);
-		strcat((char *)_unkBuf500Bytes, ".");
-		objectChat((const char *)_unkBuf500Bytes, 0, 0x83, _cauldronTable[itemsInCauldron-1]+54);
+		objectChat("..." + str + ".", 0, 0x83, _cauldronTable[itemsInCauldron-1]+54);
 	}
 }
 
@@ -1905,7 +1889,7 @@ void KyraEngine_HoF::playTim(const char *filename) {
 	_tim->resetFinishedFlag();
 	while (!shouldQuit() && !_tim->finished()) {
 		_tim->exec(tim, 0);
-		if (_chatText)
+		if (!_chatText.empty())
 			updateWithText();
 		else
 			update();

engines/kyra/engine/kyra_hof.h
@@ -333,8 +333,8 @@ protected:
 	int _newChapterFile;
 
 	uint8 *getTableEntry(uint8 *buffer, int id);
-	char *getTableString(int id, uint8 *buffer, bool decode);
-	const char *getChapterString(int id);
+	Common::String getTableString(int id, uint8 *buffer, bool decode);
+	Common::String getChapterString(int id);
 
 	void changeFileExtension(char *buffer);
 
@@ -367,12 +367,13 @@ protected:
 
 	// text
 	void showMessageFromCCode(int id, int16 palIndex, int);
-	void showMessage(const char *string, int16 palIndex);
+	void showMessage(const Common::String &string, int16 palIndex);
+	void clearMessage();
 	void showChapterMessage(int id, int16 palIndex);
 
 	void updateCommandLineEx(int str1, int str2, int16 palIndex);
 
-	const char *_shownMessage;
+	Common::String _shownMessage;
 
 	byte _messagePal[3];
 	bool _fadeMessagePalette;
@@ -382,11 +383,11 @@ protected:
 	bool _chatIsNote;
 
 	int chatGetType(const char *text);
-	int chatCalcDuration(const char *text);
+	int chatCalcDuration(const Common::String &text);
 
-	void objectChat(const char *text, int object, int vocHigh = -1, int vocLow = -1);
-	void objectChatInit(const char *text, int object, int vocHigh = -1, int vocLow = -1);
-	void objectChatPrintText(const char *text, int object);
+	void objectChat(const Common::String &text, int object, int vocHigh = -1, int vocLow = -1);
+	void objectChatInit(const Common::String &text, int object, int vocHigh = -1, int vocLow = -1);
+	void objectChatPrintText(const Common::String &text, int object);
 	void objectChatProcess(const char *script);
 	void objectChatWaitToFinish();
 
@@ -397,7 +398,7 @@ protected:
 	void updateDlgBuffer();
 	void loadDlgHeader(int &csEntry, int &vocH, int &scIndex1, int &scIndex2);
 	void processDialogue(int dlgOffset, int vocH = 0, int csEntry = 0);
-	void npcChatSequence(const char *str, int objectId, int vocHigh = -1, int vocLow = -1);
+	void npcChatSequence(const Common::String &str, int objectId, int vocHigh = -1, int vocLow = -1);
 	void setDlgIndex(int dlgIndex) override;
 
 	int _npcTalkChpIndex;
@@ -615,9 +616,6 @@ protected:
 
 	EMCData _npcScriptData;
 
-	// pathfinder
-	uint8 *_unkBuf500Bytes;
-	uint8 *_unkBuf200kByte;
 	bool _chatAltFlag;
 
 	// sequence player

engines/kyra/engine/kyra_mr.cpp
@@ -1065,7 +1065,7 @@ void KyraEngine_MR::updateWithText() {
 
 	restorePage3();
 	drawAnimObjects();
-	if (_chatTextEnabled && _chatText) {
+	if (_chatTextEnabled && !_chatText.empty()) {
 		int curPage = _screen->_curPage;
 		_screen->_curPage = 2;
 		objectChatPrintText(_chatText, _chatObject);

engines/kyra/engine/kyra_mr.h
@@ -419,7 +419,7 @@ private:
 
 	void objectChat(const char *text, int object, int vocHigh, int vocLow);
 	void objectChatInit(const char *text, int object, int vocHigh, int vocLow);
-	void objectChatPrintText(const char *text, int object);
+	void objectChatPrintText(const Common::String &text, int object);
 	void objectChatProcess(const char *script);
 	void objectChatWaitToFinish();
 

engines/kyra/engine/kyra_v2.cpp
@@ -61,7 +61,7 @@ KyraEngine_v2::KyraEngine_v2(OSystem *system, const GameFlags &flags, const Engi
 	_vocHigh = -1;
 	_chatVocHigh = -1;
 	_chatVocLow = -1;
-	_chatText = 0;
+	_chatText = "";
 	_chatObject = -1;
 	_chatTextEnabled = false;
 
@@ -150,7 +150,7 @@ void KyraEngine_v2::delay(uint32 amount, bool updateGame, bool isMainLoop) {
 	uint32 start = _system->getMillis();
 	do {
 		if (updateGame) {
-			if (_chatText)
+			if (!_chatText.empty())
 				updateWithText();
 			else
 				update();

engines/kyra/engine/kyra_v2.h
@@ -343,7 +343,7 @@ protected:
 	// chat
 	int _vocHigh;
 
-	const char *_chatText;
+	Common::String _chatText;
 	int _chatObject;
 	uint32 _chatEndTime;
 	int _chatVocHigh, _chatVocLow;

commit b03bcd1f8505b5b62404d1ec71b5bfdb6e9fc268

KYRA: (HOF/MR) - fix broken save name input dialog
(regression from 280f8324)

engines/kyra/gui/gui_v2.cpp
@@ -644,7 +644,7 @@ int GUI_v2::clickSaveSlot(Button *caller) {
 			return 0;
 		} else {
 			_saveSlot = item.saveSlot;
-			strcpy(_saveDescription, getTableString(item.itemId).c_str());
+			Common::strlcpy(_saveDescription, item.itemString.c_str(), sizeof(_saveDescription));
 		}
 	} else if (item.saveSlot == -2) {
 		_saveSlot = getNextSavegameSlot();