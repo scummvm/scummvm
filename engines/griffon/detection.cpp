fc172fd901326c1841e1670a2e47521b4b24cf5c
taylorzhancher <taylorzhancher@gmail.com>
2021-04-23 18:06:46 +0800

    GRIFFON: GUI option in main menu, synced with in game option

--- engines/griffon/detection.cpp
+++ engines/griffon/detection.cpp
@@ -22,11 +22,34 @@
 
 #include "base/plugins.h"
 #include "engines/advancedDetector.h"
+#ifdef USE_TTS
+#include "common/text-to-speech.h"
+#include "common/translation.h"
+#endif
 
 static const PlainGameDescriptor griffonGames[] = {
 	{"griffon", "The Griffon Legend"},
 	{NULL, NULL}
 };
+
+#ifdef USE_TTS
+#define GAMEOPTION_TTS_NARRATOR 	GUIO_GAMEOPTIONS1
+
+static const ADExtraGuiOptionsMap optionsList[] = {
+	{
+		GAMEOPTION_TTS_NARRATOR,
+		{
+			_s("tts_enabled"),
+			_s("Use TTS to read the descriptions (if TTS is available)"),
+			"tts_enabled",
+			false
+		}
+	},
+
+	AD_EXTRA_GUI_OPTIONS_TERMINATOR
+};
+
+#endif
 
 namespace Griffon {
 
@@ -37,10 +60,13 @@ static const ADGameDescription gameDescriptions[] = {
 		AD_ENTRY1s("objectdb.dat", "ec5371da28f01ccf88980b32d9de2232", 27754),
 		Common::EN_ANY,
 		Common::kPlatformWindows,
-		ADGF_DROPPLATFORM,
+		ADGF_DROPPLATFORM,	
+#ifdef USE_TTS
+		GUIO2(GUIO_NOMIDI, GAMEOPTION_TTS_NARRATOR)
+#else
 		GUIO1(GUIO_NOMIDI)
+#endif
 	},
-
 	AD_TABLE_END_MARKER
 };
 
@@ -48,7 +74,11 @@ static const ADGameDescription gameDescriptions[] = {
 
 class GriffonMetaEngineDetection: public AdvancedMetaEngineDetection {
 public:
-	GriffonMetaEngineDetection() : AdvancedMetaEngineDetection(Griffon::gameDescriptions, sizeof(ADGameDescription), griffonGames) {
+	GriffonMetaEngineDetection() : AdvancedMetaEngineDetection(Griffon::gameDescriptions, sizeof(ADGameDescription), griffonGames 
+#ifdef USE_TTS
+			, optionsList
+#endif
+			) {
 	}
 
 	const char *getEngineId() const override {
@@ -63,5 +93,4 @@ public:
 		return "The Griffon Legend (c) 2005 Syn9 (Daniel Kennedy)";
 	}
 };
-
 REGISTER_PLUGIN_STATIC(GRIFFON_DETECTION, PLUGIN_TYPE_ENGINE_DETECTION, GriffonMetaEngineDetection);
