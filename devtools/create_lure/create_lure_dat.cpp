ad43ce6945553957f3cc2d69d82aa91bcd35172f
Orgad Shaneh <orgads@gmail.com>
2021-04-28 19:57:10 +0300

    DEVTOOLS: Fix GCC warnings (#2986)
    Appear in travis build.
    
    create_lure_dat.cpp:1491:1: error: narrowing conversion of '\37777777665' from `char` to `unsigned char` inside { } [-Wnarrowing]

--- devtools/create_lure/create_lure_dat.cpp
+++ devtools/create_lure/create_lure_dat.cpp
@@ -27,6 +27,10 @@
 // Disable symbol overrides so that we can use system headers.
 #define FORBIDDEN_SYMBOL_ALLOW_ALL
 
+#ifdef __GNUC__
+#pragma GCC diagnostic ignored "-Wnarrowing"
+#endif
+
 // HACK to allow building with the SDL backend on MinGW
 // see bug #3412 "TOOLS: MinGW tools building broken"
 #ifdef main
