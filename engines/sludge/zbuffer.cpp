scummvm/scummvm

commit 7c5acdaa4b390198df20afc0c88ff22a87203342

SLUDGE: Fix a potentially uninitialized variable
Variable "n" was not initialized if "stillToGo" was non-zero

engines/sludge/zbuffer.cpp
@@ -137,7 +137,7 @@ bool GraphicsManager::setZBuffer(int num) {
 
 	for (uint y = 0; y < _sceneHeight; y++) {
 		for (uint x = 0; x < _sceneWidth; x++) {
-			int n;
+			int n = 0;
 			if (stillToGo == 0) {
 				n = readStream->readByte();
 				stillToGo = n >> 4;

commit 855c2e439229254448abfa3ceeddf92011024a24

SLUDGE: Fix zbuffer loading. Regression from 7c5acdaa4b390198df20afc0c88ff22a87203342

engines/sludge/zbuffer.cpp
@@ -135,9 +135,10 @@ bool GraphicsManager::setZBuffer(int num) {
 		_zBuffer->sprites[i].create(picWidth, picHeight, *g_sludge->getScreenPixelFormat());
 	}
 
+	int n = 0;
+
 	for (uint y = 0; y < _sceneHeight; y++) {
 		for (uint x = 0; x < _sceneWidth; x++) {
-			int n = 0;
 			if (stillToGo == 0) {
 				n = readStream->readByte();
 				stillToGo = n >> 4;
@@ -147,6 +148,9 @@ bool GraphicsManager::setZBuffer(int num) {
 					stillToGo++;
 				n &= 15;
 			}
+
+			byte *target = (byte *)_zBuffer->sprites[i].getBasePtr(x, y);
+			*target = n;
 			for (int i = 0; i < _zBuffer->numPanels; ++i) {
 				byte *target = (byte *)_zBuffer->sprites[i].getBasePtr(x, y);
 				if (n && (sorted[i] == n || i == 0)) {
@@ -165,6 +169,7 @@ bool GraphicsManager::setZBuffer(int num) {
 			stillToGo--;
 		}
 	}
+
 	g_sludge->_resMan->finishAccess();
 	setResourceForFatal(-1);
 	return true;