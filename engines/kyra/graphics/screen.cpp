scummvm/scummvm

commit 8f41adc9697f633b00fb678b1a7931202ab273eb

KYRA: - fix copyBlockToPage() glitch
(fix for negative out-of-bounds x offsets - apparently never used like that before)

engines/kyra/graphics/screen.cpp
@@ -1074,6 +1074,7 @@ void Screen::copyBlockToPage(int pageNum, int x, int y, int w, int h, const uint
 		h = _screenHeight - y;
 	}
 
+	int pitch = w;
 	if (x < 0) {
 		src += -x * _bytesPerPixel;
 		w += x;
@@ -1095,7 +1096,7 @@ void Screen::copyBlockToPage(int pageNum, int x, int y, int w, int h, const uint
 	while (h--) {
 		memcpy(dst, src, w * _bytesPerPixel);
 		dst += SCREEN_W * _bytesPerPixel;
-		src += w * _bytesPerPixel;
+		src += pitch * _bytesPerPixel;
 	}
 }
 

commit 376df2f14cf1047bacdf25512ce872361aa13d61

KYRA: fix bug no. 12514 ("Graphic glitch in Legend of Kyrandia 3")
(regression from 8f41adc9)

engines/kyra/graphics/screen.cpp
@@ -1035,6 +1035,7 @@ void Screen::copyRegionToBuffer(int pageNum, int x, int y, int w, int h, uint8 *
 		h = _screenHeight - y;
 	}
 
+	int pitch = w;
 	if (x < 0) {
 		dest += -x * _bytesPerPixel;
 		w += x;
@@ -1049,7 +1050,7 @@ void Screen::copyRegionToBuffer(int pageNum, int x, int y, int w, int h, uint8 *
 	uint8 *pagePtr = getPagePtr(pageNum);
 
 	for (int i = y; i < y + h; ++i)
-		memcpy(dest + (i - y) * w * _bytesPerPixel, pagePtr + i * SCREEN_W * _bytesPerPixel + x * _bytesPerPixel, w * _bytesPerPixel);
+		memcpy(dest + (i - y) * pitch * _bytesPerPixel, pagePtr + i * SCREEN_W * _bytesPerPixel + x * _bytesPerPixel, w * _bytesPerPixel);
 }
 
 void Screen::copyPage(uint8 srcPage, uint8 dstPage) {