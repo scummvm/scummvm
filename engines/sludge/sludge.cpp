7d61637afdebfe83d7f098dd0c2eaf3b574a4c7d
Eugene Sandulenko <sev@scummvm.org>
2021-05-12 01:05:37 +0200

    SLUDGE: Renamed debug channels and disabled the pre-enabled ones

--- engines/sludge/sludge.cpp
+++ engines/sludge/sludge.cpp
@@ -56,16 +56,16 @@ SludgeEngine::SludgeEngine(OSystem *syst, const SludgeGameDescription *gameDesc)
 	_rnd = new Common::RandomSource("sludge");
 
 	// Add debug channels
-	DebugMan.addDebugChannel(kSludgeDebugFatal, "Script", "Script debug level");
-	DebugMan.addDebugChannel(kSludgeDebugDataLoad, "Data Load", "Data loading debug level");
-	DebugMan.addDebugChannel(kSludgeDebugStackMachine, "Stack Machine", "Stack Machine debug level");
-	DebugMan.addDebugChannel(kSludgeDebugBuiltin, "Built-in", "Built-in debug level");
-	DebugMan.addDebugChannel(kSludgeDebugGraphics, "Graphics", "Graphics debug level");
-	DebugMan.addDebugChannel(kSludgeDebugZBuffer, "ZBuffer", "ZBuffer debug level");
-	DebugMan.addDebugChannel(kSludgeDebugSound, "Sound", "Sound debug level");
+	DebugMan.addDebugChannel(kSludgeDebugFatal, "script", "Script debug level");
+	DebugMan.addDebugChannel(kSludgeDebugDataLoad, "loading", "Data loading debug level");
+	DebugMan.addDebugChannel(kSludgeDebugStackMachine, "stack", "Stack Machine debug level");
+	DebugMan.addDebugChannel(kSludgeDebugBuiltin, "builtin", "Built-in debug level");
+	DebugMan.addDebugChannel(kSludgeDebugGraphics, "graphics", "Graphics debug level");
+	DebugMan.addDebugChannel(kSludgeDebugZBuffer, "zBuffer", "ZBuffer debug level");
+	DebugMan.addDebugChannel(kSludgeDebugSound, "sound", "Sound debug level");
 
-	DebugMan.enableDebugChannel("Data Load");
-	DebugMan.enableDebugChannel("Built-in");
+	//DebugMan.enableDebugChannel("loading");
+	//DebugMan.enableDebugChannel("builtin");
 
 	_dumpScripts = ConfMan.getBool("dump_scripts");
 
