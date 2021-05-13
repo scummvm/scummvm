5b70ddd8026074b0f32381b01fc2612caee0ace6
Torbjörn Andersson <eriktorbjorn@users.sourceforge.net>
2021-05-13 14:13:08 +0200

    SCUMM: Fix text color and shadow
    Text positioning is still all over the place, though.

--- engines/scumm/scumm.h
+++ engines/scumm/scumm.h
@@ -954,6 +954,7 @@ protected:
 	void updateDirtyScreen(VirtScreenNumber slot);
 	void drawStripToScreen(VirtScreen *vs, int x, int width, int top, int bottom);
 	void mac_drawStripToScreen(VirtScreen *vs, int top, int x, int y, int width, int height);
+	void mac_restoreCharsetBg();
 	void ditherCGA(byte *dst, int dstPitch, int x, int y, int width, int height) const;
 
 public:
