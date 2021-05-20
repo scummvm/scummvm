scummvm/scummvm

commit a3d363fedc87216f2873fc88cbca3432c7bb6a49

TRECISION: Refactor object visibility
- Added methods to toggle and query object visibility
- Added methods to update object animations
- All dirty rect handling is done inside setObjectVisible()
- The above allows the complete removal of the associated object
  visibility methods and variables (RegenRoom(), PaintRegenRoom(),
  VideoObjStatus, OldObjStatus
- Object dirty rects are now set once when the room changes in
  InitRegenRoom(), and are updated when any call to change object
  visibility is made to setObjectVisible()

engines/trecision/trecision.cpp
@@ -1112,4 +1112,35 @@ void TrecisionEngine::addDirtyRect(Common::Rect rect) {
 	_dirtyRects.push_back(rect);
 }
 
+void TrecisionEngine::setObjectVisible(uint16 objectId, bool visible) {
+	if (visible)
+		_obj[objectId]._mode |= OBJMODE_OBJSTATUS;
+	else
+		_obj[objectId]._mode &= ~OBJMODE_OBJSTATUS;
+
+	if (_obj[objectId]._mode & (OBJMODE_MASK | OBJMODE_FULL)) {
+		// Find the object in the room, and update its status
+		uint16 index;
+		for (index = 0; index < MAXOBJINROOM; index++) {
+			if (_room[_curRoom]._object[index] == objectId || _room[_curRoom]._object[index] == 0)
+				break;
+		}
+
+		SortTable[_curSortTableNum]._index = objectId;
+		SortTable[_curSortTableNum]._roomIndex = index;
+		SortTable[_curSortTableNum]._remove = !isObjectVisible(objectId);
+		SortTable[_curSortTableNum]._curFrame = 0;
+		SortTable[_curSortTableNum]._isBitmap = true;
+		_curSortTableNum++;	
+	}
+}
+
+bool TrecisionEngine::isObjectVisible(uint16 objectId) const {
+	return _obj[objectId]._mode & OBJMODE_OBJSTATUS;
+}
+
+void TrecisionEngine::setObjectAnim(uint16 objectId, uint16 animId) {
+	_obj[objectId]._anim = animId;
+}
+
 } // End of namespace Trecision

engines/trecision/trecision.h
@@ -217,6 +217,11 @@ public:
 	bool canPlayerInteract();
 	void addDirtyRect(Common::Rect rect);
 
+	// Objects
+	void setObjectVisible(uint16 objectId, bool visible);
+	bool isObjectVisible(uint16 objectId) const;
+	void setObjectAnim(uint16 objectId, uint16 animId);
+
 	Graphics::Surface _thumbnail;
 
 	uint16 _curRoom;

engines/trecision/dialog.cpp
@@ -194,8 +194,8 @@ void DialogManager::afterChoice() {
 
 	case dTRAMP1717:
 		if (_curChoice == 136) {
-			_vm->_obj[ocTRAMP17]._mode &= ~OBJMODE_OBJSTATUS;
-			_vm->_obj[oTRAMPD17]._mode |= OBJMODE_OBJSTATUS;
+			_vm->setObjectVisible(ocTRAMP17, false);
+			_vm->setObjectVisible(oTRAMPD17, true);
 			_vm->_room[kRoom17]._bkgAnim = aBKG17B;
 			_vm->addIcon(kItemSkate);
 		} else if (_curChoice == 137) {
@@ -238,24 +238,24 @@ void DialogManager::afterChoice() {
 		_vm->_obj[oCHOCOLATES4A]._action = 1106;
 		_vm->_obj[oDOORC4A]._action = 1118;
 		_vm->_animMgr->_animTab[aBKG4A]._flag |= SMKANIM_OFF1;
-		_vm->_obj[ocHELLEN4A]._mode &= ~OBJMODE_OBJSTATUS;
-		_vm->_obj[oHELLENA4A]._mode |= OBJMODE_OBJSTATUS;
+		_vm->setObjectVisible(ocHELLEN4A, false);
+		_vm->setObjectVisible(oHELLENA4A, true);
 		break;
 
 	case dC581:
-		_vm->_obj[oWINDOWB58]._mode |= OBJMODE_OBJSTATUS;
+		_vm->setObjectVisible(oWINDOWB58, true);
 		if (_curChoice == 262)
 			_vm->_obj[oTASTIERA56]._examine = 1307;
 		break;
 
 	case dF542:
-		_vm->_obj[oGRATAC54]._mode &= ~OBJMODE_OBJSTATUS;
-		_vm->_obj[oDOORC54]._mode &= ~OBJMODE_OBJSTATUS;
-		_vm->_obj[oLAVATRICEL54]._mode &= ~OBJMODE_OBJSTATUS;
-		_vm->_obj[oLAVATRICEF54]._mode &= ~OBJMODE_OBJSTATUS;
-		_vm->_obj[oGRATAA54]._mode |= OBJMODE_OBJSTATUS;
-		_vm->_obj[oCHIAVI54]._mode |= OBJMODE_OBJSTATUS;
-		_vm->_obj[od54ALLA55]._mode |= OBJMODE_OBJSTATUS;
+		_vm->setObjectVisible(oGRATAC54, false);
+		_vm->setObjectVisible(oDOORC54, false);
+		_vm->setObjectVisible(oLAVATRICEL54, false);
+		_vm->setObjectVisible(oLAVATRICEF54, false);
+		_vm->setObjectVisible(oGRATAA54, true);
+		_vm->setObjectVisible(oCHIAVI54, true);
+		_vm->setObjectVisible(od54ALLA55, true);
 		break;
 	default:
 		break;
@@ -268,7 +268,7 @@ void DialogManager::afterChoice() {
 		switch (_curDialog) {
 		case dPOLIZIOTTO16:
 			if ((_choice[61]._flag & kObjFlagDone) && (_choice[62]._flag & kObjFlagDone) && (_vm->_obj[ocPOLIZIOTTO16]._flag & kObjFlagExtra))
-				_vm->_obj[ocPOLIZIOTTO16]._mode &= ~OBJMODE_OBJSTATUS;
+				_vm->setObjectVisible(ocPOLIZIOTTO16, false);
 			break;
 
 		case dTRAMP171:
@@ -283,7 +283,7 @@ void DialogManager::afterChoice() {
 
 		case dGUARDIAN18:
 			if (_curChoice == 152)
-				_vm->_obj[ocGUARD18]._mode &= ~OBJMODE_OBJSTATUS;
+				_vm->setObjectVisible(ocGUARD18, false);
 			else if (_curChoice == 155)
 				_vm->StartCharacterAction(a184ENTRACLUB, kRoom19, 2, 0);
 			break;
@@ -300,20 +300,20 @@ void DialogManager::afterChoice() {
 				doEvent(MC_SYSTEM, ME_CHANGEROOM, MP_SYSTEM, kRoom18, a1810ENTRADALNEGOZIO, 10, _vm->_curObj);
 				_vm->_obj[oDOORN18]._flag &= ~kObjFlagRoomOut;
 				_vm->_obj[oDOORN18]._action = 218;
-				_vm->_obj[oDOORN18]._anim = 0;
+				_vm->setObjectAnim(oDOORN18, 0);
 			} else if (_curChoice == 183)
 				_vm->_obj[oTESSERA1A]._action = 239;
 			break;
 
 		case dF181:
-			_vm->_obj[oRETE17]._mode |= OBJMODE_OBJSTATUS;
+			_vm->setObjectVisible(oRETE17, true);
 			_vm->_obj[oDOORA17]._examine = 196;
 			_vm->_obj[oDOORUA17]._examine = 187;
 			_vm->_obj[oDOORUB17]._examine = 192;
 			_vm->_obj[oDOORA17]._action = 188;
 			_vm->_obj[oDOORUA17]._action = 193;
 			_vm->_obj[oDOORUB17]._action = 197;
-			_vm->_obj[oFINGERPAD17]._mode &= ~OBJMODE_OBJSTATUS;
+			_vm->setObjectVisible(oFINGERPAD17, false);
 			_vm->_room[kRoom17]._flag &= ~kObjFlagDone;
 			_vm->_room[kRoom17]._flag |= kObjFlagExtra;
 			break;
@@ -345,8 +345,8 @@ void DialogManager::afterChoice() {
 			_vm->_obj[oSWITCH29]._action = 479;
 			_vm->_obj[od22TO29]._goRoom = kRoom29L;
 			_vm->_obj[od2ATO29]._goRoom = kRoom29L;
-			_vm->_obj[od22TO29]._mode &= ~OBJMODE_OBJSTATUS;
-			_vm->_obj[od22TO29I]._mode |= OBJMODE_OBJSTATUS;
+			_vm->setObjectVisible(od22TO29, false);
+			_vm->setObjectVisible(od22TO29I, true);
 			break;
 
 		case dF2G1:
@@ -385,7 +385,7 @@ void DialogManager::afterChoice() {
 			break;
 
 		case dF371:
-			_vm->_obj[oSCAFFALE36]._anim = a3615AAPRENDESCAFFALE;
+			_vm->setObjectAnim(oSCAFFALE36, a3615AAPRENDESCAFFALE);
 			_vm->_animMgr->smkVolumePan(0, 1, 1);
 			break;
 
@@ -403,8 +403,8 @@ void DialogManager::afterChoice() {
 			for (int c = oPULSANTE1AD; c <= oPULSANTE33AD; c++) {
 				if (!(_vm->_obj[c]._goRoom)) {
 					_vm->_obj[c]._goRoom = kRoom4A;
-					_vm->_obj[c]._mode |= OBJMODE_OBJSTATUS;
-					_vm->_obj[c - 40]._mode &= ~OBJMODE_OBJSTATUS;
+					_vm->setObjectVisible(c, true);
+					_vm->setObjectVisible(c - 40, false);
 					break;
 				}
 			}
@@ -456,17 +456,17 @@ void DialogManager::afterChoice() {
 
 		case dF562:
 			_vm->_obj[oDOOR58C55]._action = 1287;
-			_vm->_obj[oDOOR58C55]._anim = 0;
+			_vm->setObjectAnim(oDOOR58C55, 0);
 			_vm->_obj[oWINDOW58P55]._action = 1292;
-			_vm->_obj[oWINDOW58P55]._mode |= OBJMODE_OBJSTATUS;
-			_vm->_obj[oWINDOW58P55]._anim = 0;
+			_vm->setObjectVisible(oWINDOW58P55, true);
+			_vm->setObjectAnim(oWINDOW58P55, 0);
 			break;
 
 		case dF5A1:
 			_vm->_obj[oDOOR58C55]._action = 1286;
 			_vm->_obj[oWINDOW58P55]._action = 1291;
 			_vm->_obj[oWINDOWA5A]._action = 1403;
-			_vm->_obj[oGUARDIA58]._mode |= OBJMODE_OBJSTATUS;
+			_vm->setObjectVisible(oGUARDIA58, true);
 			_choice[286]._flag |= kObjFlagDone;
 			break;
 
@@ -478,7 +478,7 @@ void DialogManager::afterChoice() {
 			break;
 
 		case dC582:
-			_vm->_obj[oWINDOWA58]._mode |= OBJMODE_OBJSTATUS;
+			_vm->setObjectVisible(oWINDOWA58, true);
 			_vm->addIcon(kItemGovernorsCode);
 			break;
 

engines/trecision/nl/do.cpp
@@ -52,15 +52,13 @@ AnimType[3] = {	{1}, {1}, {1}	};
 void ExecuteAtFrameDoit(ATFHandle *h, int doit, int obj) {
 	switch (doit) {
 	case fCLROBJSTATUS:
-		g_vm->_obj[obj]._mode &= ~OBJMODE_OBJSTATUS;
-		RegenRoom();
+		g_vm->setObjectVisible(obj, false);
 		break;
 	case fSETOBJSTATUS:
-		g_vm->_obj[obj]._mode |= OBJMODE_OBJSTATUS;
-		RegenRoom();
+		g_vm->setObjectVisible(obj, true);
 		break;
 	case fONETIME:
-		g_vm->_obj[obj]._anim = 0;
+		g_vm->setObjectAnim(obj, 0);
 		break;
 	case fCREPACCIO:
 		if (g_vm->_room[kRoom2E]._flag & kObjFlagExtra)
@@ -72,16 +70,16 @@ void ExecuteAtFrameDoit(ATFHandle *h, int doit, int obj) {
 		doEvent(g_vm->_snake52._class, g_vm->_snake52._event, g_vm->_snake52._priority, g_vm->_snake52._u16Param1, g_vm->_snake52._u16Param2, g_vm->_snake52._u8Param, g_vm->_snake52._u32Param);
 		break;
 	case fPIRANHA:
-		g_vm->_obj[oLUCCHETTO53]._anim = 0;
-		g_vm->_obj[oGRATAC53]._anim = 0;
-		g_vm->_obj[oGRATAA53]._anim = 0;
+		g_vm->setObjectAnim(oLUCCHETTO53, 0);
+		g_vm->setObjectAnim(oGRATAC53, 0);
+		g_vm->setObjectAnim(oGRATAA53, 0);
 		g_vm->_obj[oLUCCHETTO53]._action = 1240;
 		g_vm->_obj[oGRATAC53]._action = 1243;
 		g_vm->_obj[oGRATAA53]._action = 1246;
 		g_vm->_obj[oLAGO53]._examine = 1237;
 		break;
 	case fMOREAU:
-		g_vm->_obj[oWINDOWB58]._anim = 0;
+		g_vm->setObjectAnim(oWINDOWB58, 0);
 		g_vm->_obj[oWINDOWB58]._action = 1358;
 		break;
 	case fDOOR58:
@@ -92,8 +90,8 @@ void ExecuteAtFrameDoit(ATFHandle *h, int doit, int obj) {
 		break;
 	case fVALVEON34:
 		if (!(g_vm->_dialogMgr->_choice[616]._flag & kObjFlagDone) &&		// if the fmv is not done
-		    (g_vm->_obj[oTUBOA34]._mode & OBJMODE_OBJSTATUS) && // if there's a cut pipe
-		    !(g_vm->_obj[oTUBOFT34]._mode & OBJMODE_OBJSTATUS)) // if there's not tube outside
+		    (g_vm->isObjectVisible(oTUBOA34)) && // if there's a cut pipe
+		    !(g_vm->isObjectVisible(oTUBOFT34))) // if there's not tube outside
 			g_vm->_animMgr->smkVolumePan(0, 2, 1);
 		break;
 	case fVALVEOFF34:
@@ -189,8 +187,7 @@ void ProcessAtFrame(ATFHandle *h, int type, int atf) {
 		CharacterTalkInAction(g_vm->_obj[h->_object]._examine);
 		break;
 	case ATFCLR:
-		g_vm->_obj[h->_curAnim->_atFrame[atf]._index]._mode &= ~OBJMODE_OBJSTATUS;
-		RegenRoom();
+		g_vm->setObjectVisible(h->_curAnim->_atFrame[atf]._index, false);
 		break;
 	case ATFCLRI:
 		g_vm->removeIcon(h->_curAnim->_atFrame[atf]._index);
@@ -202,8 +199,7 @@ void ProcessAtFrame(ATFHandle *h, int type, int atf) {
 		g_vm->_obj[h->_object]._action = h->_curAnim->_atFrame[atf]._index;
 		break;
 	case ATFSET:
-		g_vm->_obj[h->_curAnim->_atFrame[atf]._index]._mode |= OBJMODE_OBJSTATUS;
-		RegenRoom();
+		g_vm->setObjectVisible(h->_curAnim->_atFrame[atf]._index, true);
 		break;
 	case ATFSETI:
 		g_vm->addIcon(h->_curAnim->_atFrame[atf]._index);
@@ -222,27 +218,21 @@ void ProcessAtFrame(ATFHandle *h, int type, int atf) {
 		break;
 	case ATFCOBJANIM:
 		g_vm->_obj[h->_object]._anim = h->_curAnim->_atFrame[atf]._index;
-		RegenRoom();
 		break;
 	case ATFCOBJBOX:
 		g_vm->_obj[h->_object]._nbox = h->_curAnim->_atFrame[atf]._index;
-		RegenRoom();
 		break;
 	case ATFCOBJPOS:
 		g_vm->_obj[h->_object]._position = h->_curAnim->_atFrame[atf]._index;
-		RegenRoom();
 		break;
 	case ATFSETFORE:
 		g_vm->_obj[h->_curAnim->_atFrame[atf]._index]._nbox = BOX_FOREGROUND;
-		RegenRoom();
 		break;
 	case ATFSETBACK:
 		g_vm->_obj[h->_curAnim->_atFrame[atf]._index]._nbox = BOX_BACKGROUND;
-		RegenRoom();
 		break;
 	case ATFSWITCH:
-		g_vm->_obj[h->_curAnim->_atFrame[atf]._index]._mode ^= OBJMODE_OBJSTATUS;
-		RegenRoom();
+		g_vm->setObjectVisible(h->_curAnim->_atFrame[atf]._index, !g_vm->isObjectVisible(h->_curAnim->_atFrame[atf]._index));
 		break;
 	case ATFSETROOMT:
 		g_vm->setRoom(h->_curAnim->_atFrame[atf]._index, true);

engines/trecision/nl/extern.h
@@ -30,9 +30,6 @@ namespace Trecision {
 /*-------------------------------------------------------------------------*/
 /*                          VARIABILI DI SERVIZIO         				   */
 /*-------------------------------------------------------------------------*/
-extern bool OldObjStatus[];
-extern bool VideoObjStatus[];
-
 extern struct SSortTable SortTable[];
 
 /*-------------------------------------------------------------------------*/

engines/trecision/nl/globvar.cpp
@@ -27,9 +27,6 @@ namespace Trecision {
 /*-------------------------------------------------------------------------*/
 /*                          VARIABILI DI SERVIZIO         				   */
 /*-------------------------------------------------------------------------*/
-bool OldObjStatus[MAXOBJINROOM];
-bool VideoObjStatus[MAXOBJINROOM];
-
 SSortTable SortTable[200];
 
 /*-------------------------------------------------------------------------*/

engines/trecision/nl/ll/llinc.h
@@ -104,8 +104,7 @@ void TendIn();
 void ReadObj(Common::SeekableReadStream *stream);
 void ReadExtraObj2C();
 void ReadExtraObj41D();
-void RegenRoom();
-void PaintRegenRoom();
+void InitRegenRoom();
 void SoundFadIn(int num);
 
 } // End of namespace Trecision

commit 3c2ed4e9b0af97c95f3bf57c6d7649b6a072a5ff

TRECISION: Refresh the object sort table after a full screen video
Fixes graphics glitches after full motion videos. A regression from
a3d363fedc87216f2873fc88cbca3432c7bb6a49

engines/trecision/resource.cpp
@@ -216,12 +216,7 @@ void TrecisionEngine::readObject(Common::SeekableReadStream *stream, uint16 objI
 			_maskPointers[objIndex][i] = (uint8)stream->readByte();
 	}
 
-	if (obj->_mode & (OBJMODE_MASK | OBJMODE_FULL)) {
-		SSortTable entry;
-		entry._objectId = objectId;
-		entry._remove = !isObjectVisible(objectId);
-		_sortTable.push_back(entry);
-	}
+	refreshObject(objectId);
 }
 
 void TrecisionEngine::readObj(Common::SeekableReadStream *stream) {

engines/trecision/trecision.cpp
@@ -361,11 +361,16 @@ void TrecisionEngine::setObjectVisible(uint16 objectId, bool visible) {
 	else
 		_obj[objectId]._mode &= ~OBJMODE_OBJSTATUS;
 
+	refreshObject(objectId);
+}
+
+void TrecisionEngine::refreshObject(uint16 objectId) {
 	if (_obj[objectId]._mode & (OBJMODE_MASK | OBJMODE_FULL)) {
 		SSortTable entry;
 		entry._objectId = objectId;
 		entry._remove = !isObjectVisible(objectId);
 		_sortTable.push_back(entry);
+		_sortTableReplay.push_back(entry);
 	}
 }
 
@@ -397,6 +402,7 @@ void TrecisionEngine::readLoc() {
 
 	_graphicsMgr->loadBackground(picFile, bgInfo._rect.width(), bgInfo._rect.height());
 	_sortTable.clear();
+	_sortTableReplay.clear();
 	readObj(picFile);
 
 	_soundMgr->stopAll();
@@ -447,6 +453,7 @@ void TrecisionEngine::redrawRoom() {
 
 	_graphicsMgr->loadBackground(picFile, bgInfo._rect.width(), bgInfo._rect.height());
 	_sortTable.clear();
+	_sortTable = _sortTableReplay;
 
 	if (bgAnim)
 		_animMgr->startSmkAnim(bgAnim);

engines/trecision/trecision.h
@@ -112,7 +112,6 @@ class TrecisionEngine : public Engine {
 	void doScreenUseWithScreen();
 	void doInvExamine();
 	void doInvOperate();
-	void doDoing();
 	void doScript();
 	void processCurrentMessage();
 
@@ -202,6 +201,7 @@ public:
 
 	// Objects
 	void setObjectVisible(uint16 objectId, bool visible);
+	void refreshObject(uint16 objectId);
 	bool isObjectVisible(uint16 objectId) const;
 	void setObjectAnim(uint16 objectId, uint16 animId);
 	void redrawRoom();
@@ -222,6 +222,7 @@ public:
 	SRoom _room[MAXROOMS];
 
 	Common::List<SSortTable> _sortTable;
+	Common::List<SSortTable> _sortTableReplay;
 
 	uint16 _curObj;
 	SObject _obj[MAXOBJ];