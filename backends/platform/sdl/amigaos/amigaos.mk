4b58074dfc643ef15bdc390710ee6e37d143f459
Hubert Maier <raziel-@users.noreply.github.com>
2021-05-24 16:54:27 +0200

    AMIGAOS: Secure Readme.md
    - readme.md is already in root
    - don't want to delete the original

--- backends/platform/sdl/amigaos/amigaos.mk
+++ backends/platform/sdl/amigaos/amigaos.mk
@@ -18,9 +18,8 @@ ifdef DIST_FILES_DOCS
 	# AmigaOS AREXX will error with a "Program not found" message
 	# if srcdir is '.'. Copy the script to cwd instead.
 	cp ${srcdir}/dists/amigaos/md2ag.rexx .
-	cp "${srcdir}/README.md" .
 	rx md2ag.rexx README.md "$(AMIGAOSPATH)/doc/"
-	rm -f md2ag.rexx README.md
+	rm -f md2ag.rexx
 endif
 	# Copy mandatory installation files.
 	makedir all $(AMIGAOSPATH)/extras
