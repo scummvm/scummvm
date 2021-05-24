3f3a0a688638fabfdc0d1a540d6d4c208b4493d2
Eugene Sandulenko <sev@scummvm.org>
2021-05-25 00:20:01 +0200

    DIRECTOR: Improved support for Mac sounds

--- engines/director/cast.cpp
+++ engines/director/cast.cpp
@@ -628,19 +628,14 @@ void Cast::loadSoundCasts() {
 
 		Common::SeekableReadStreamEndian *sndData = NULL;
 
-		switch (tag) {
-		case MKTAG('S', 'N', 'D', ' '):
-			if (_castArchive->hasResource(MKTAG('S', 'N', 'D', ' '), sndId)) {
-				debugC(2, kDebugLoading, "****** Loading 'SND ' id: %d", sndId);
-				sndData = _castArchive->getResource(MKTAG('S', 'N', 'D', ' '), sndId);
-			}
-			break;
-		case MKTAG('s', 'n', 'd', ' '):
-			if (_castArchive->hasResource(MKTAG('s', 'n', 'd', ' '), sndId)) {
-				debugC(2, kDebugLoading, "****** Loading 'snd ' id: %d", sndId);
-				sndData = _castArchive->getResource(MKTAG('s', 'n', 'd', ' '), sndId);
-			}
-			break;
+		if (!_castArchive->hasResource(tag, sndId)) {
+			if (_castArchive->hasResource(MKTAG('s', 'n', 'd', ' '), sndId))
+				tag = MKTAG('s', 'n', 'd', ' ');
+		}
+
+		if (_castArchive->hasResource(tag, sndId)) {
+			debugC(2, kDebugLoading, "****** Loading '%s' id: %d", tag2str(tag), sndId);
+			sndData = _castArchive->getResource(tag, sndId);
 		}
 
 		if (sndData != NULL) {
