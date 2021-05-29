687bbd923ac091f1d0672782c0f40e76384349d9
Strangerke <arnaud.boutonne@gmail.com>
2021-05-29 07:52:13 +0100

    TRECISION: Move some code from struct.h to struct.cpp

--- engines/trecision/struct.h
+++ engines/trecision/struct.h
@@ -115,7 +115,7 @@ struct SInvObject {
 	void syncGameStream(Common::Serializer &ser);
 	void loadObj(Common::File *file);
 
-	private:
+private:
 	uint8 _flag = 0;
 };
 
@@ -185,10 +185,7 @@ struct SVertex {
 	float _x, _y, _z;
 	float _nx, _ny, _nz;
 
-	void clear() {
-		_x = _y = _z = 0.0f;
-		_nx = _ny = _nz = 0.0f;
-	}
+	void clear();
 };
 
 struct SFace {
@@ -205,15 +202,7 @@ struct SLight {
 	int8 _inten;
 	int8 _position;
 
-	void clear() {
-		_x = _y = _z = 0.0f;
-		_dx = _dy = _dz = 0.0f;
-		_inr = _outr = 0.0f;
-		_hotspot = 0;
-		_fallOff = 0;
-		_inten = 0;
-		_position = 0;
-	}
+	void clear();
 };
 
 struct SCamera {
@@ -223,12 +212,7 @@ struct SCamera {
 	float _e3[3];
 	float _fovX, _fovY;
 
-	void clear() {
-		_ex = _ey = _ez = 0.0f;
-		_fovX = _fovY = 0.0f;
-		for (uint8 i = 0; i < 3; ++i)
-			_e1[i] = _e2[i] = _e3[i] = 0.0f;
-	}
+	void clear();
 };
 
 } // End of namespace Trecision
