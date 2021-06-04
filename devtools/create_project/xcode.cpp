c9287b330702bbff4183aee92649d0c97c06cb23
lb_ii <lolbot_iichan@mail.ru>
2021-06-04 03:52:44 +0300

    DISTS: Add achievements.dat to various dat file lists

--- devtools/create_project/xcode.cpp
+++ devtools/create_project/xcode.cpp
@@ -829,6 +829,7 @@ XcodeProvider::ValueList& XcodeProvider::getResourceFiles() const {
 		files.push_back("gui/themes/residualvm.zip");
 		files.push_back("gui/themes/translations.dat");
 		files.push_back("dists/engine-data/access.dat");
+		files.push_back("dists/engine-data/achievements.dat");
 		files.push_back("dists/engine-data/cryo.dat");
 		files.push_back("dists/engine-data/cryomni3d.dat");
 		files.push_back("dists/engine-data/drascula.dat");
