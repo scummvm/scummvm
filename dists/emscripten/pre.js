93e1c779b6d6220f7dec6eab562fef38273ff05f
Christian Kündig <christian@kuendig.info>
2021-06-06 14:36:48 +0200

    Fix remaining Codacy issues and improve README readability

--- dists/emscripten/pre.js
+++ dists/emscripten/pre.js
@@ -1,7 +1,5 @@
-Module['arguments'] = [];
-//Module['arguments'].push('--debuglevel=5');
-Module['arguments'].push('--config=/data/local/scummvm.ini');
-//Module['arguments'].push('--add');
-//Module['arguments'].push('--recursive');
-//Module['arguments'].push('--path=/data/games');
+/*global Module*/
+Module["arguments"] = [];
+Module["arguments"].push("--debuglevel=5");
+Module["arguments"].push("--config=/data/local/scummvm.ini");
 
