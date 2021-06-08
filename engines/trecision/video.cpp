scummvm/scummvm

commit 6ce7d3a985bdd9dac719d8f6dc7883e9f2e006eb

TRECISION: Remove mask color params and simplify the blinking char code
The mask or shadow color was always MASKCOL, so we don't need to
specify it as a parameter

engines/trecision/video.cpp
@@ -220,7 +220,6 @@ void AnimManager::drawFrameSubtitles(Graphics::Surface *surface, int frameNum) {
 	// fixed coordinates
 	_vm->_drawText._rect = Common::Rect(20, 380 - TOP, MAXX - 40 + 20, _vm->_drawText.calcHeight(_vm) + (380 - TOP));
 	_vm->_drawText._subtitleRect = Common::Rect(MAXX, MAXY);
-	_vm->_drawText._shadowCol = MASKCOL;
 	_vm->_drawText.draw(_vm, surface);
 }
 

engines/trecision/dialog.cpp
@@ -93,7 +93,6 @@ void DialogManager::dialogPrint(int x, int y, int c, const Common::String &txt) 
 		Common::Rect(x, y, _vm->textLength(txt) + x, y),
 		Common::Rect(0, 0, MAXX, MAXY),
 		c,
-		MASKCOL,
 		txt
 	);
 	curChoice.draw(_vm);

engines/trecision/graphics.cpp
@@ -339,6 +339,12 @@ void GraphicsManager::clearScreenBufferSaveSlotDescriptions() {
 	_screenBuffer.fillRect(Common::Rect(0, FIRSTLINE + ICONDY + 10, MAXX, MAXY), 0);
 }
 
+uint16 GraphicsManager::convertToScreenFormat(uint16 color) const {
+	uint8 r, g, b;
+	kImageFormat.colorToRGB(color, r, g, b);
+	return (uint16)_screenFormat.RGBToColor(r, g, b);
+}
+
 void GraphicsManager::updatePixelFormat(uint16 *p, uint32 len) const {
 	if (_screenFormat == kImageFormat)
 		return;
@@ -685,14 +691,14 @@ uint16 GraphicsManager::getCharWidth(byte character) {
 	return _font[character * 3 + 2];
 }
 
-void GraphicsManager::drawChar(byte curChar, uint16 shadowCol, uint16 textCol, uint16 line, Common::Rect rect, Common::Rect subtitleRect, uint16 inc, Graphics::Surface *externalSurface) {
+void GraphicsManager::drawChar(byte curChar, uint16 textColor, uint16 line, Common::Rect rect, Common::Rect subtitleRect, uint16 inc, Graphics::Surface *externalSurface) {
 	const uint16 charOffset = _font[curChar * 3] + (uint16)(_font[curChar * 3 + 1] << 8);
 	uint16 fontDataOffset = 768;
 	const uint16 charWidth = getCharWidth(curChar);
 
 	for (uint16 y = line * CARHEI; y < (line + 1) * CARHEI; ++y) {
 		uint16 curPos = 0;
-		uint16 curColor = shadowCol;
+		uint16 curColor = MASKCOL;
 
 		while (curPos <= charWidth - 1) {
 			if (y >= subtitleRect.top && y < subtitleRect.bottom) {
@@ -714,12 +720,12 @@ void GraphicsManager::drawChar(byte curChar, uint16 shadowCol, uint16 textCol, u
 			curPos += _font[charOffset + fontDataOffset];
 			++fontDataOffset;
 
-			if (curColor == shadowCol)
+			if (curColor == MASKCOL)
 				curColor = 0;
 			else if (curColor == 0)
-				curColor = textCol;
-			else if (curColor == textCol)
-				curColor = shadowCol;
+				curColor = textColor;
+			else if (curColor == textColor)
+				curColor = MASKCOL;
 		}
 	}
 }

engines/trecision/graphics.h
@@ -90,6 +90,7 @@ public:
 	void setSaveSlotThumbnail(byte iconSlot, const Graphics::Surface *thumbnail);
 	void readSurface(Common::SeekableReadStream *stream, Graphics::Surface *surface, uint16 width, uint16 height, uint16 count = 1);
 
+	uint16 convertToScreenFormat(uint16 color) const;
 	void updatePixelFormat(uint16 *p, uint32 len) const;
 
 	void shadow(uint16 x, uint16 y, uint8 num);
@@ -99,7 +100,7 @@ public:
 	void addDirtyRect(Common::Rect rect, bool translateRect, bool updateActorRect = false);
 
 	uint16 getCharWidth(byte character);
-	void drawChar(byte curChar, uint16 shadowCol, uint16 textCol, uint16 line, Common::Rect rect, Common::Rect subtitleRect, uint16 inc, Graphics::Surface *externalSurface);
+	void drawChar(byte curChar, uint16 textColor, uint16 line, Common::Rect rect, Common::Rect subtitleRect, uint16 inc, Graphics::Surface *externalSurface);
 
 	bool isCursorVisible();
 	void showCursor();

engines/trecision/inventory.cpp
@@ -246,7 +246,7 @@ void TrecisionEngine::showInventoryName(uint16 obj, bool showhide) {
 		_lastInv = (obj | 0x8000);
 		if (_lastInv)
 			_textMgr->clearLastText();
-		_textMgr->addText(pos, desc.c_str(), COLOR_INVENTORY, MASKCOL);
+		_textMgr->addText(pos, desc.c_str(), COLOR_INVENTORY);
 	} else {
 		if (obj == _lastInv)
 			return;
@@ -268,7 +268,7 @@ void TrecisionEngine::showInventoryName(uint16 obj, bool showhide) {
 			_textMgr->clearLastText();
 
 		if (_inventoryObj[obj]._name)
-			_textMgr->addText(pos, _objName[_inventoryObj[obj]._name], COLOR_INVENTORY, MASKCOL);
+			_textMgr->addText(pos, _objName[_inventoryObj[obj]._name], COLOR_INVENTORY);
 	}
 }
 

engines/trecision/saveload.cpp
@@ -88,7 +88,6 @@ bool TrecisionEngine::dataSave() {
 		Common::Rect(0, TOP - 20, MAXX, CARHEI + (TOP - 20)),
 		Common::Rect(0, 0, MAXX, CARHEI),
 		MOUSECOL,
-		MASKCOL,
 		_sysText[kMessageSavePosition]);
 	drawText.draw(this);
 
@@ -138,7 +137,6 @@ insave:
 					Common::Rect(posx, FIRSTLINE + ICONDY + 10, LenText + posx, CARHEI + (FIRSTLINE + ICONDY + 10)),
 					Common::Rect(0, 0, LenText, CARHEI),
 					MOUSECOL,
-					MASKCOL,
 					saveNames[CurPos].c_str());
 				drawText.draw(this);
 
@@ -209,14 +207,10 @@ insave:
 				Common::Rect(posx, FIRSTLINE + ICONDY + 10, LenText + posx, CARHEI + (FIRSTLINE + ICONDY + 10)),
 				Common::Rect(0, 0, LenText, CARHEI),
 				MOUSECOL,
-				MASKCOL,
 				saveNames[CurPos].c_str());
 
-			if ((readTime() / 8) & 1)
-				_blinkLastDTextChar = 0x0000;
-
-			drawText.draw(this);
-			_blinkLastDTextChar = MASKCOL;
+			const bool hideLastChar = (readTime() / 8) & 1;
+			drawText.draw(this, hideLastChar);
 
 			saveNames[CurPos].deleteLastChar(); // remove blinking cursor
 
@@ -280,7 +274,6 @@ bool TrecisionEngine::dataLoad() {
 		Common::Rect(0, TOP - 20, MAXX, CARHEI + (TOP - 20)),
 		Common::Rect(0, 0, MAXX, CARHEI),
 		MOUSECOL,
-		MASKCOL,
 		_sysText[kMessageLoadPosition]);
 	drawText.draw(this);
 
@@ -330,7 +323,6 @@ bool TrecisionEngine::dataLoad() {
 					Common::Rect(posX, FIRSTLINE + ICONDY + 10, lenText + posX, CARHEI + (FIRSTLINE + ICONDY + 10)),
 					Common::Rect(0, 0, lenText, CARHEI),
 					MOUSECOL,
-					MASKCOL,
 					saveNames[CurPos].c_str());
 				drawText.draw(this);
 

engines/trecision/script.cpp
@@ -107,7 +107,6 @@ bool TrecisionEngine::quitPrompt() {
 		Common::Rect(0, TOP - 20, MAXX, CARHEI + (TOP - 20)),
 		Common::Rect(0, 0, MAXX, CARHEI),
 		MOUSECOL,
-		MASKCOL,
 		_sysText[kMessageConfirmExit]
 	);
 	drawText.draw(this);
@@ -131,7 +130,6 @@ void TrecisionEngine::demoOver() {
 		Common::Rect(0, TOP - 20, MAXX, CARHEI + (TOP - 20)),
 		Common::Rect(0, 0, MAXX, CARHEI),
 		MOUSECOL,
-		MASKCOL,
 		_sysText[kMessageDemoOver]
 	);
 	drawText.draw(this);

engines/trecision/struct.h
@@ -165,14 +165,13 @@ struct SDText {
 	Common::Rect _rect;
 	Common::Rect _subtitleRect;
 	uint16 _textColor;
-	uint16 _shadowCol;
 	Common::String _text;
 	Common::String _drawTextLines[MAXDTEXTLINES];
 
 	void set(SDText *org);
-	void set(Common::Rect rect, Common::Rect subtitleRect, uint16 textCol, uint16 shadowCol, const Common::String &text);
+	void set(Common::Rect rect, Common::Rect subtitleRect, uint16 textCol, const Common::String &text);
 
-	void draw(TrecisionEngine *vm, Graphics::Surface *externalSurface = nullptr);
+	void draw(TrecisionEngine *vm, bool hideLastChar = false, Graphics::Surface *externalSurface = nullptr);
 	uint16 calcHeight(TrecisionEngine *vm);
 };
 

commit d721ecceeaf9de4ac42ee76f14c602594bdbf5f9

TRECISION: Draw video subtitles correctly
A regression from 6ce7d3a985bdd9dac719d8f6dc7883e9f2e006eb

engines/trecision/video.cpp
@@ -220,7 +220,7 @@ void AnimManager::drawFrameSubtitles(Graphics::Surface *surface, int frameNum) {
 	// fixed coordinates
 	_vm->_drawText._rect = Common::Rect(20, 380 - TOP, MAXX - 40 + 20, _vm->_drawText.calcHeight(_vm) + (380 - TOP));
 	_vm->_drawText._subtitleRect = Common::Rect(MAXX, MAXY);
-	_vm->_drawText.draw(_vm, surface);
+	_vm->_drawText.draw(_vm, false, surface);
 }
 
 void AnimManager::openSmkAnim(int slot, const Common::String &name) {