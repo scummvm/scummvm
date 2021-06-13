c9d0eb45d0db44bf9ce8a1b25833f3b1a0a0dab6
Eugene Sandulenko <sev@scummvm.org>
2021-06-13 18:11:36 +0200

    SAGA2: Rename class viariables in weapons.h

--- engines/saga2/weapons.h
+++ engines/saga2/weapons.h
@@ -41,9 +41,9 @@ ProtoEffect *createNewProtoEffect(Common::SeekableReadStream *stream);
 
 class WeaponEffect {
 public:
-	WeaponEffect *next;                      // pointer to additional effects
+	WeaponEffect *_next;                      // pointer to additional effects
 
-	WeaponEffect(void) : next(NULL) {}
+	WeaponEffect(void) : _next(NULL) {}
 	virtual ~WeaponEffect(void) {}
 	virtual void implement(Actor *enactor, GameObject *target, GameObject *strikingObj, uint8  strength) = 0;
 };
@@ -51,10 +51,10 @@ public:
 //-----------------------------------------------------------------------
 
 class WeaponProtoEffect : public WeaponEffect {
-	ProtoEffect *effect;
+	ProtoEffect *_effect;
 
 public:
-	WeaponProtoEffect(Common::SeekableReadStream *stream) : effect(createNewProtoEffect(stream)) {
+	WeaponProtoEffect(Common::SeekableReadStream *stream) : _effect(createNewProtoEffect(stream)) {
 	}
 	~WeaponProtoEffect(void);
 
@@ -64,16 +64,16 @@ public:
 //-----------------------------------------------------------------------
 
 class WeaponStrikeEffect : public WeaponEffect {
-	effectDamageTypes type;	// damage type
-	int8 dice;				// # of dice to roll
-	int8 sides;				// # of sides on dice
-	int8 skillDice;			// multiply additional dice
-	int8 base;				// absolute damage amount
-	int8 skillBase;
+	effectDamageTypes _type;// damage type
+	int8 _dice;				// # of dice to roll
+	int8 _sides;			// # of sides on dice
+	int8 _skillDice;		// multiply additional dice
+	int8 _base;				// absolute damage amount
+	int8 _skillBase;
 
 public:
 	WeaponStrikeEffect(effectDamageTypes t, int8 d, int8 s, int8 sd, int8 b, int8 sb) :
-		type(t), dice(d), sides(s), skillDice(sd), base(b), skillBase(sb) {
+		_type(t), _dice(d), _sides(s), _skillDice(sd), _base(b), _skillBase(sb) {
 	}
 
 	void implement(Actor *enactor, GameObject *target, GameObject *strikingObj, uint8 trength);
@@ -82,14 +82,14 @@ public:
 //-----------------------------------------------------------------------
 
 class WeaponStuff {
-	weaponID master;             // index in array
-	WeaponEffect *effects;           // the effects of this weapon
+	weaponID _master;             // index in array
+	WeaponEffect *_effects;           // the effects of this weapon
 
 public:
 	WeaponStuff();
 	~WeaponStuff();
 	void setID(weaponID id) {
-		master = id;
+		_master = id;
 	}
 	void addEffect(WeaponEffect *we);
 	void addEffect(Common::SeekableReadStream *stream);
