96d35230b18cd00b7206d70a211d0af795156f16
ysj1173886760 <1173886760@qq.com>
2021-06-13 10:07:06 +0800

    DIRECTOR: fix indent problem, and for custom target file name, we won't save it since we didn't use it.

--- engines/director/detection.cpp
+++ engines/director/detection.cpp
@@ -32,89 +32,89 @@
 #include "director/detection_tables.h"
 
 namespace Director {
-	const char *directoryGlobs[] = {
-		"install",
-		"l_zone",
-		"win_data",						// L-ZONE
-		"data",
-		"gadget",						// Gadget
-		"vnc",
-		"vnc2",							// Virtual Nightclub
-		"program",						// Arc Media products
-		"demodata",						// Edmark demos
-		"media",						// Gundam
-		"mvm",							// Master of the Elements
-		"module_1",
-		"module_2",						// Takeru
-		"simpdata",
-		"simpsons",						// Simpsons Cartoon Studio
-		"dane",							// Polish Kontyngent '99 catalog
-		"datas",						// O!KAY! subscription from Domino Verlag
-		"daten",						// Bibi Blocksberg games (German)
-		"source",						// Grand Prix 2006 directory
-		"data_k",
-		"data_1",
-		"data_2",						// JumpStart World
-		"numbers",						// JumpStart Learning Games
-		"xtras",
-		"files",						// Babar
-		"swanlake",						// Barbie Swan Lake
-		"assets",						// Barbie Nail Designer
-		"fjpc",							// Forestia Jr
-		"movies",						// Mulle Meck (Gary Gadget) series
-		"demos",						// Headbone samplers
-		"blender",						// Blender CD-ROM magazines
-		"bilder",
-		"endsoung",
-		"gerhelp",
-		"gervideo",
-		"mausejagd mit ton",			// Rename from Mäusejagd mit Ton
-		"music",
-		"solos",
-		"sounds",
-		"startsnd",						// GGMD German
-		"duthelp",
-		"dutvideo",
-		"endsound",
-		"muizen in \'t hotel, geluid",	// GGMD Dutch
-		"macos9",
-		"macosx",						// Sethi 3
-		"kripos",
-		"strisser",
-		"polis",
-		"poliisi",
-		"cd_data",						// Polis
-		"fusion macintosh folder",		// Fusion
-		"oslo",							// Oslo's Tools & Gadgets
-		"ojo2000",
-		"stuff",						// Abrapalabra
-		"tooned",						// Stay Tooned
-		"sam rupert",					// Who Killed Sam Rupert?
-		"content",
-		"pcinst",						// Gus and the CyberBuds series
-		"p2data",
-		"p2media",
-		"p3media",
-		"annat",						// Pettson & Findus
-		"fctdata",
-		"fct",					   		// Felix the Cat
-		"dayfiles",
-		"xmasfun",						// Fisher-Price Xmas Fun
-		"95instal",						// Fisher-Price Little People
-		"01",					   		// Doing it in C++
-		"freading",
-		"fsetup",						// My First Reading Words (Eureka)
-		"english",
-		"deutsch",
-		"francais",						// Rename from Français
-		"italiano",						// Virtual Tourism Paris
-		"please copy to hd. G3",		// Rename from HDにｺﾋﾟｰして下さい。G3
-		"_files_",						// The Gate
-		"popup",						// Pop Up Computer
-		"alpha",						// Interactive Alphabet
-		"technik",						// Glasklar Technology Interactive
-		0
-	};
+const char *directoryGlobs[] = {
+	"install",
+	"l_zone",
+	"win_data",						// L-ZONE
+	"data",
+	"gadget",						// Gadget
+	"vnc",
+	"vnc2",							// Virtual Nightclub
+	"program",						// Arc Media products
+	"demodata",						// Edmark demos
+	"media",						// Gundam
+	"mvm",							// Master of the Elements
+	"module_1",
+	"module_2",						// Takeru
+	"simpdata",
+	"simpsons",						// Simpsons Cartoon Studio
+	"dane",							// Polish Kontyngent '99 catalog
+	"datas",						// O!KAY! subscription from Domino Verlag
+	"daten",						// Bibi Blocksberg games (German)
+	"source",						// Grand Prix 2006 directory
+	"data_k",
+	"data_1",
+	"data_2",						// JumpStart World
+	"numbers",						// JumpStart Learning Games
+	"xtras",
+	"files",						// Babar
+	"swanlake",						// Barbie Swan Lake
+	"assets",						// Barbie Nail Designer
+	"fjpc",							// Forestia Jr
+	"movies",						// Mulle Meck (Gary Gadget) series
+	"demos",						// Headbone samplers
+	"blender",						// Blender CD-ROM magazines
+	"bilder",
+	"endsoung",
+	"gerhelp",
+	"gervideo",
+	"mausejagd mit ton",			// Rename from Mäusejagd mit Ton
+	"music",
+	"solos",
+	"sounds",
+	"startsnd",						// GGMD German
+	"duthelp",
+	"dutvideo",
+	"endsound",
+	"muizen in \'t hotel, geluid",	// GGMD Dutch
+	"macos9",
+	"macosx",						// Sethi 3
+	"kripos",
+	"strisser",
+	"polis",
+	"poliisi",
+	"cd_data",						// Polis
+	"fusion macintosh folder",		// Fusion
+	"oslo",							// Oslo's Tools & Gadgets
+	"ojo2000",
+	"stuff",						// Abrapalabra
+	"tooned",						// Stay Tooned
+	"sam rupert",					// Who Killed Sam Rupert?
+	"content",
+	"pcinst",						// Gus and the CyberBuds series
+	"p2data",
+	"p2media",
+	"p3media",
+	"annat",						// Pettson & Findus
+	"fctdata",
+	"fct",					   		// Felix the Cat
+	"dayfiles",
+	"xmasfun",						// Fisher-Price Xmas Fun
+	"95instal",						// Fisher-Price Little People
+	"01",					   		// Doing it in C++
+	"freading",
+	"fsetup",						// My First Reading Words (Eureka)
+	"english",
+	"deutsch",
+	"francais",						// Rename from Français
+	"italiano",						// Virtual Tourism Paris
+	"please copy to hd. G3",		// Rename from HDにｺﾋﾟｰして下さい。G3
+	"_files_",						// The Gate
+	"popup",						// Pop Up Computer
+	"alpha",						// Interactive Alphabet
+	"technik",						// Glasklar Technology Interactive
+	0
+};
 }
 
 static const char *customTargetList[] = {
@@ -231,8 +231,6 @@ ADDetectedGame DirectorMetaEngineDetection::fallbackDetect(const FileMap &allFil
 			Common::String platform = f.readString('\n');
 			Common::String version = f.readString('\n');
 
-			Common::strlcpy(s_fallbackFileNameBuffer, fileName.c_str(), sizeof(s_fallbackFileNameBuffer) - 1);
-			desc->desc.filesDescriptions[0].fileName = s_fallbackFileNameBuffer;
 			desc->version = atoi(version.c_str());
 			desc->desc.platform = Common::parsePlatform(platform);
 
