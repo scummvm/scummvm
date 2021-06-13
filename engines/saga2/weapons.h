9d8887f2970f09fe139924e60e8c3e0245241887
Eugene Sandulenko <sev@scummvm.org>
2021-06-13 18:05:49 +0200

    SAGA2: Cleanup weapons.h

--- engines/saga2/weapons.h
+++ engines/saga2/weapons.h
@@ -45,69 +45,45 @@ public:
 
 	WeaponEffect(void) : next(NULL) {}
 	virtual ~WeaponEffect(void) {}
-	virtual void implement(
-	    Actor       *enactor,
-	    GameObject  *target,
-	    GameObject  *strikingObj,
-	    uint8       strength) = 0;
+	virtual void implement(Actor *enactor, GameObject *target, GameObject *strikingObj, uint8  strength) = 0;
 };
 
 //-----------------------------------------------------------------------
 
 class WeaponProtoEffect : public WeaponEffect {
-	ProtoEffect     *effect;
+	ProtoEffect *effect;
 
 public:
-	WeaponProtoEffect(Common::SeekableReadStream *stream) :
-		effect(createNewProtoEffect(stream)) {
+	WeaponProtoEffect(Common::SeekableReadStream *stream) : effect(createNewProtoEffect(stream)) {
 	}
 	~WeaponProtoEffect(void);
 
-	void implement(
-	    Actor       *enactor,
-	    GameObject  *target,
-	    GameObject  *strikingObj,
-	    uint8       strength);
+	void implement(Actor *enactor, GameObject*target, GameObject *strikingObj, uint8 strength);
 };
 
 //-----------------------------------------------------------------------
 
 class WeaponStrikeEffect : public WeaponEffect {
-	effectDamageTypes   type;               // damage type
-	int8                dice,               // # of dice to roll
-	                    sides,              // # of sides on dice
-	                    skillDice,          // multiply additional dice
-	                    base,               // absolute damage amount
-	                    skillBase;
+	effectDamageTypes type;	// damage type
+	int8 dice;				// # of dice to roll
+	int8 sides;				// # of sides on dice
+	int8 skillDice;			// multiply additional dice
+	int8 base;				// absolute damage amount
+	int8 skillBase;
 
 public:
-	WeaponStrikeEffect(
-	    effectDamageTypes t,
-	    int8 d,
-	    int8 s,
-	    int8 sd,
-	    int8 b,
-	    int8 sb) :
-		type(t),
-		dice(d),
-		sides(s),
-		skillDice(sd),
-		base(b),
-		skillBase(sb) {
+	WeaponStrikeEffect(effectDamageTypes t, int8 d, int8 s, int8 sd, int8 b, int8 sb) :
+		type(t), dice(d), sides(s), skillDice(sd), base(b), skillBase(sb) {
 	}
 
-	void implement(
-	    Actor       *enactor,
-	    GameObject  *target,
-	    GameObject  *strikingObj,
-	    uint8       strength);
+	void implement(Actor *enactor, GameObject *target, GameObject *strikingObj, uint8 trength);
 };
 
 //-----------------------------------------------------------------------
 
 class WeaponStuff {
-	weaponID            master;             // index in array
-	WeaponEffect        *effects;           // the effects of this weapon
+	weaponID master;             // index in array
+	WeaponEffect *effects;           // the effects of this weapon
 
 public:
 	WeaponStuff();
@@ -118,11 +94,7 @@ public:
 	void addEffect(WeaponEffect *we);
 	void addEffect(Common::SeekableReadStream *stream);
 	void killEffects(void);
-	void implement(
-	    Actor       *enactor,
-	    GameObject  *target,
-	    GameObject  *strikingObj,
-	    uint8       strength);
+	void implement(Actor *enactor, GameObject *target, GameObject *strikingObj, uint8 strength);
 };
 
 //-----------------------------------------------------------------------
