e3d66a8d052a6018176b4be76b1dca710bfe156f
a/ <yuri.kgpps@gmail.com>
2021-06-14 18:01:49 +0900

    SAGA2: Cleanup tileload.h

--- engines/saga2/tileload.h
+++ engines/saga2/tileload.h
@@ -42,13 +42,6 @@ const int           maxBanks = 64;          // 64 banks maximum
 //  To facilitate loading of tile banks on demand, a bit-array
 //  is used to indicate which banks are needed for each metatile
 //  to render.
-/*
-struct BankBits {
-    uint32          b[maxBanks / 32];
-
-    bool isSet( int i )  { return b[i >> 5] & (1 << (i & 31)); }
-};
- */
 
 class BankBits {
 public:
@@ -65,10 +58,6 @@ public:
 	friend BankBits operator+ (BankBits c, BankBits d) {
 		return BankBits(c.b[0] + d.b[0], c.b[1] + d.b[1]);
 	}
-	/*
-	friend BankBits operator- (BankBits c)
-	                  { return BankBits( -c.b[0], -c.b[1] ); }
-	*/
 
 	friend BankBits operator- (BankBits c, BankBits d) {
 		return BankBits(c.b[0] - d.b[0], c.b[1] - d.b[1]);
@@ -229,141 +218,7 @@ public:
 			t.b[i] = c.b[i] ^ d.b[i];
 		return t;
 	}
-
-#if 0
-	void operator>>=(int a) {
-		bool c = b[0] & 1;
-		b[0] >>= (uint32)a;
-		b[1] >>= (uint32)a;
-		b[1] |= ((1 << 31) & (uint32)c);
-	}
-
-	void operator<<=(int a) {
-		bool c = b[1] & (1 << 31);
-		b[0] >>= (uint32)a;
-		b[0] |= (1 & (uint32)c);
-		b[1] >>= (uint32)a;
-	}
-
-	friend FixedBitArray operator+ (FixedBitArray c, FixedBitArray d) {
-		FixedBitArray t(max(c.currentSize(), d.currentSize()));
-		for (uint16 i = 0; i < t.currentSize(); i++)    t.b[i] = c.b[i] + d.b[i];
-		return t;
-	}
-
-	friend FixedBitArray operator- (FixedBitArray c) {
-		return FixedBitArray(-c.b[0], -c.b[1]);
-	}
-
-	friend FixedBitArray operator- (FixedBitArray c, FixedBitArray d) {
-		FixedBitArray t(max(c.currentSize(), d.currentSize()));
-		for (uint16 i = 0; i < t.currentSize(); i++)    t.b[i] = c.b[i] - d.b[i];
-		return t;
-	}
-	bool isSet(int i) {
-		return b[i >> 5] & ((uint32) 1 << (i & 31));
-	}
-
-	void SetBit(int16 i) {
-		b[i / 32] |= ((uint32) 1 << (i % 32))  ;
-	}
-	void NotBit(int16 i) {
-		b[i / 32] &= ~((uint32) 1 << (i % 32));
-	}
-	void Reset(uint32 c, uint32 d) {
-		b[0] = c;
-		b[1] = d;
-	}
-	bool Test() {
-		return (b[0] || b[1]);
-	}
-#endif
-
 };
-
-/*
-template <class RES> class loadOnCall {
-private:
-    uint16      handles;
-    RES         *handle;
-    bitarray    loaded(0);
-    bitArray    last(0);
-
-    RES *loadRes( uint16 i);
-    void freeRes( uint16 i);
-    void initRes( uint16 i);
-
-
-public:
-    RES *loadOnCall(uint16 size) ;
-    ~loadOnCall();
-
-    setSize( uint16 newSize );
-
-    RES *operator[] (uint16 i)
-        { if (i>=handles) return ((RES *) NULL);
-          if (!loaded[i]) handle[i]=loadRes(i);
-          return handle[i]; }
-    void set(uint16 i)
-        { if i<size b[i/32] |= ((uint32) 1<<(i%32));    }
-    void clr(uint16 i)
-        { if i<size b[i/32] &= ~((uint32) 1<<(i%32));   }
-    void clear( void )
-        {  for (int i=0;i<=(size/32)+1;i++) b[i]=0;     }
-    void flush( void )
-        {  for (int i=0;i<=(size/32)+1;i++)
-            {
-            if (last[i] && !loaded[i])  freeRes(i);
-            last[i]=loaded[i];
-            }
-        }
-};
-
-template <class RES> RES *loadOnCall<RES>::loadRes( uint16 i )
-{
-    RES h;
-
-    if (handle[i])
-    {
-        loaded.SetBit(i);
-        RLockHandle( (RHANDLE) handle[i] );
-        return handle[i];
-    }
-
-    h = (RES) tileRes->load( tileID + RES_ID( 0,0,0,i ) );
-
-    loaded.SetBit(i);
-
-    handle[i] = h;
-    return h;
-}
-
-template <class RES> RES *loadOnCall<RES>::initRes( uint16 i )
-{
-    RES h;
-
-    if (!handle[i])
-    {
-        h = (RES) tileRes->load( tileID + RES_ID( 0,0,0,i ) );
-        loaded.SetBit(i);
-        handle[i] = h;
-        return h;
-    }
-}
-
-template <class RES> void loadOnCall<RES>::freeRes( uint16 i )
-{
-    if (handle[i])
-    {
-        RUnlockHandle((RHANDLE) handles[i]);
-        loaded.NotBit(i);
-    }
-}
-*/
-
-#ifdef _WIN32   //  Set structure alignment packing value to 1 byte
-#pragma pack( pop )
-#endif
 
 } // end of namespace Saga2
 
