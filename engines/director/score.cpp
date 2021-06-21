44b2f54a4d2151a821a7571980f24fb9250dd2b0
ysj1173886760 <1173886760@qq.com>
2021-06-21 18:28:29 +0800

    DIRECTOR: fix use-after-free issue for the sprites stored in channel

--- engines/director/score.cpp
+++ engines/director/score.cpp
@@ -516,6 +516,9 @@ void Score::renderSprites(uint16 frameId, RenderMode mode) {
 				_movie->_videoPlayback = true;
 
 			_window->addDirtyRect(channel->getBbox());
+
+			// after setclean, we may get currentSprite deleted. thus we need to update it
+			currentSprite = channel->_sprite;
 			debugC(2, kDebugImages, "Score::renderSprites(): CH: %-3d castId: %03d(%s) [ink: %d, puppet: %d, moveable: %d, visible: %d] [bbox: %d,%d,%d,%d] [type: %d fg: %d bg: %d] [script: %d]", i, currentSprite->_castId, numToCastNum(currentSprite->_castId), currentSprite->_ink, currentSprite->_puppet, currentSprite->_moveable, channel->_visible, PRINT_RECT(channel->getBbox()), currentSprite->_spriteType, currentSprite->_foreColor, currentSprite->_backColor, currentSprite->_scriptId);
 		} else {
 			channel->setClean(nextSprite, i, true);
