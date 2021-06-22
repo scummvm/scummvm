f2a47788fc672e8c18ceaa256532269fddff6c47
ysj1173886760 <1173886760@qq.com>
2021-06-22 21:49:49 +0800

    DIRECTOR: use initialRect to represent sprite dimensions

--- engines/director/sprite.cpp
+++ engines/director/sprite.cpp
@@ -200,20 +200,8 @@ void Sprite::setCast(uint16 castId) {
 		// them properly.
 		if (_cast->_type != kCastShape) {
 			Common::Rect dims = _cast->getInitialRect();
-			//for the text cast members, we use the max number of size of the first sprite we saw and the initialRect, and we reset that cast too
-			// quite strange logic, but it's useful for now, but still we need to figure out the correct solution
-			if (_cast->_type == kCastButton || _cast->_type == kCastText) {
-				if (_cast->_boundingRect.isEmpty()) {
-					warning("Sprite::setCast(): trying to modify initialRect of cast with the first sprite size");
-					_cast->_boundingRect = Common::Rect(MAX<int>(_width, dims.width()), MAX<int>(_height, dims.height()));
-					_cast->_initialRect = _cast->_boundingRect;
-				}
-				_width = _cast->_initialRect.width();
-				_height = _cast->_initialRect.height();
-			} else {
-				_width = dims.width();
-				_height = dims.height();
-			}
+			_width = dims.width();
+			_height = dims.height();
 		}
 	} else {
 		warning("Sprite::setCast(): CastMember id %d(%s) has null member", castId, numToCastNum(castId));
