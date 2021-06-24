c3abec68e396f99ebfffd8ffb9e40ac79898583a
Nicola Mettifogo <nicola_@gc4.so-net.ne.jp>
2021-06-24 23:28:37 +0900

    PARALLACTION: style.

--- engines/parallaction/parser_br.cpp
+++ engines/parallaction/parser_br.cpp
@@ -502,8 +502,9 @@ DECLARE_LOCATION_PARSER(zeta)  {
 DECLARE_COMMAND_PARSER(ifchar)  {
 	debugC(7, kDebugParser, "COMMAND_PARSER(ifchar) ");
 
-	if (scumm_stricmp(_vm->_char.getName(), _tokens[1]))
+	if (scumm_stricmp(_vm->_char.getName(), _tokens[1])) {
 		_script->skip("endif");
+	}
 }
 
 
