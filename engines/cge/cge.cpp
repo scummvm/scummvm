6a371010f919ef4f5b771bf9fd5d2e68187c00c4
taylorzhancher <taylorzhancher@gmail.com>
2021-07-07 23:17:13 +0800

    CGE2: Add tts for ending message

--- engines/cge/cge.cpp
+++ engines/cge/cge.cpp
@@ -223,6 +223,9 @@ Common::Error CGEEngine::run() {
 			g_system->delayMillis(10);
 			GUI::MessageDialog dialog(msg);
 			dialog.runModal();
+			Common::TextToSpeechManager *ttsMan = g_system->getTextToSpeechManager();
+			if (ttsMan != nullptr && ConfMan.getBool("tts_enabled"))
+				ttsMan->say(msg);
 		}
 	}
 
