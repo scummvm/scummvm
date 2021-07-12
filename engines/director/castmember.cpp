92ea5a7d76ffc19cf4921788211dd0d633b3eafe
djsrv <dservilla@gmail.com>
2021-07-12 01:33:52 -0400

    DIRECTOR: Use String::format instead of U32String::format
    U32String::format doesn't support all the formatting types
    Common::String does. So just use String::format then cast to U32String.

--- engines/director/castmember.cpp
+++ engines/director/castmember.cpp
@@ -778,7 +778,7 @@ void TextCastMember::setText(const Common::U32String &text) {
 		return;
 
 	// If text has changed, use the cached formatting from first STXT in this castmember.
-	Common::U32String formatting = Common::U32String::format("\001\016%04x%02x%04x%04x%04x%04x", _fontId, _textSlant, _fontSize, _fgpalinfo1, _fgpalinfo2, _fgpalinfo3);
+	Common::U32String formatting = Common::String::format("\001\016%04x%02x%04x%04x%04x%04x", _fontId, _textSlant, _fontSize, _fgpalinfo1, _fgpalinfo2, _fgpalinfo3);
 	_ptext = text;
 	_ftext = formatting + text;
 
