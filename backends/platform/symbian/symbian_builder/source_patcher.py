1783836a0f38845dbcc54f7bf58b3814e700f45b
Fiodar Stryzhniou <fedor_qd@mail.ru>
2021-07-12 11:12:39 +0300

    SYMBIAN: symbian_builder can apply single line patches.

--- backends/platform/symbian/symbian_builder/source_patcher.py
+++ backends/platform/symbian/symbian_builder/source_patcher.py
@@ -8,6 +8,14 @@ from collections import defaultdict as defdict
 va_macro_src = [("engines\\sci", "resource_patcher.cpp"), ("engines\\ags", "managed_object_pool.h"),
 ("graphics\\macgui", "mactext.cpp")]
 
+#Format: "file to edit" = "parent folder", src, dst
+oneline_patch_src = [
+("ptr.h", ["common", r'#include <cstddef>', r'//#include <cstddef>']),
+
+("object.cpp", ["gui", r'error("Unable to load widget position for \'%s\'. Please check your theme files", _name.c_str());', r'warning("Unable to load widget position for \'%s\'. Please check your theme files", _name.c_str());']),
+# ("", ["", "", ""]),
+]
+
 pt = '..\..\..'
 
 def fix_va_string(file):
@@ -42,6 +50,34 @@ def fix_src():
    for k, v in va_macro_src:
       d[k].append(v)
    [fix_va_macro(folder, file) for folder, file in d.iteritems()]
+   one_line_patch()
+
+def apply_patch(file, val):
+   print "File to process: %s\n" %file
+   with open(file) as d:
+      x = d.read()
+   if val[-1] in x: #Already patched
+      print "Already patched"
+      return
+   x = x.replace(val[1], val[-1])
+   with open(file, 'w') as f:
+      f.write(x)
+
+def patch_line(file, val):
+   val = val[0] #get nested list
+   folder = val[0]
+   if not folder:
+      raise "Prent folder for file %s not set!" %file
+   files = find_file(folder, file)
+   if not files:
+      return
+   [apply_patch(x, val) for x in files]
+
+def one_line_patch():
+   d = defdict(list)
+   for k, v in oneline_patch_src:
+      d[k].append(v)
+   [patch_line(file, val) for file, val in d.iteritems()]
 
 if __name__ == '__main__':
    fix_src()
