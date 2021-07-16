4961a867fb54cf3044fe91c09bf973e86f2b278d
av-dx <aashwin.vaish@research.iiit.ac.in>
2021-07-16 17:21:29 +0530

    TTS: Read correct displayed entry from the list

--- gui/widgets/list.cpp
+++ gui/widgets/list.cpp
@@ -297,7 +297,7 @@ void ListWidget::handleMouseMoved(int x, int y, int button) {
 
 	if (item != -1) {
 		if(_lastRead != item) {
-			read(_dataList[item]);
+			read(_list[item]);
 			_lastRead = item;
 		}
 	}
