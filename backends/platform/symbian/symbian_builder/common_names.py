82f5d0ffc597335515cb3458e786b851eb0a319a
huhungahuru <72929651+huhungahuru@users.noreply.github.com>
2021-07-17 18:10:01 +0000

    SYMBIAN/BUILDER: Minor fixes for comments.

--- backends/platform/symbian/symbian_builder/common_names.py
+++ backends/platform/symbian/symbian_builder/common_names.py
@@ -1,5 +1,5 @@
 # ScummVM - Graphic Adventure Engine
-# Copyright (C) 2020 Stryzhniou Fiodar
+# Copyright (C) 2020-2021 Stryzhniou Fiodar
 
 # ScummVM is the legal property of its developers, whose names
 # are too numerous to list here. Please refer to the COPYRIGHT
@@ -19,6 +19,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
+
 from datetime import datetime
 
 mmps = "mmp"
@@ -33,14 +34,13 @@ build_time = "ScummVM_build_time%s.log" %timestamp
 whitelist = 'whitelist.txt'
 
 build = 'full'
-# build = 'release'
+#build = 'release'
 
-#sis package version
+# Sis package version.
 sis_major_version, sis_minor_version, sis_build_number = 2, '03', 0
 
-
 uids = ("0xA0000657", "0xA0000658", "0x2006FE7C", "0x2006FE7D", "0x2006FE7E", "0x2006FE7F", "0x2006FE80")
-# Test uids 3
+# Test uids 3.
 uids_tests = ("0xAA000657", "0xAA000658", "0xA006FE7C", "0xA006FE7D", "0xA006FE7E", "0xA006FE7F", "0xA006FE80")
 
 def get_UIDs(build):
@@ -62,9 +62,9 @@ def AppendToFile(path, data):
 
 active_config = ("DISABLE_NUKED_OPL", "USE_A52", "USE_MPEG2",  "USE_BINK", "USE_THEORADEC", "USE_TINYGL",
 "ENABLE_VKEYBD")
-#activate USE_SCALERS USE_ARM_SCALER_ASM USE_TTS USE_SPEECH_DISPATCHER USE_CLOUD USE_LIBCURL
-# USE_SDL_NET USE_DISCORD USE_UPDATES
-#LUA disabled by default due to conflicting declaration of struct TKey in lobject.h and system header e32std.h
+# Activate USE_SCALERS USE_ARM_SCALER_ASM USE_TTS USE_SPEECH_DISPATCHER USE_CLOUD USE_LIBCURL
+# USE_SDL_NET USE_DISCORD USE_UPDATES.
+# LUA disabled by default due to conflicting declaration of struct TKey in lobject.h and system header e32std.h
 disabled_config = ("USE_ALSA", "ENABLE_OPL2LPT", "USE_SCALERS", "USE_ARM_SCALER_ASM",
 "USE_HQ_SCALERS", "USE_NASM", "USE_ELF_LOADER", "USE_SDL2", "USE_FLUIDSYNTH", "USE_TTS", "USE_SPEECH_DISPATCHER",
 "USE_CLOUD", "USE_LIBCURL", "USE_SDL_NET", "USE_OPENGL", "USE_DISCORD", "USE_LINUXCD", "ENABLE_EVENTRECORDER",
