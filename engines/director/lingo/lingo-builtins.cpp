4bf08861ec1693ff3abe7e4bc05020363b6735ed
djsrv <dservilla@gmail.com>
2021-07-20 22:47:45 -0400

    DIRECTOR: LINGO: Move utility macros to lingo-utils.h

--- engines/director/lingo/lingo-builtins.cpp
+++ engines/director/lingo/lingo-builtins.cpp
@@ -46,39 +46,9 @@
 #include "director/lingo/lingo-code.h"
 #include "director/lingo/lingo-codegen.h"
 #include "director/lingo/lingo-object.h"
+#include "director/lingo/lingo-utils.h"
 
 namespace Director {
-
-#define ARGNUMCHECK(n) \
-	if (nargs != (n)) { \
-		warning("%s: expected %d argument%s, got %d", __FUNCTION__, (n), ((n) == 1 ? "" : "s"), nargs); \
-		g_lingo->dropStack(nargs); \
-		return; \
-	}
-
-#define TYPECHECK(datum,t) \
-	if ((datum).type != (t)) { \
-		warning("%s: %s arg should be of type %s, not %s", __FUNCTION__, #datum, #t, (datum).type2str()); \
-		return; \
-	}
-
-#define TYPECHECK2(datum, t1, t2)	\
-	if ((datum).type != (t1) && (datum).type != (t2)) { \
-		warning("%s: %s arg should be of type %s or %s, not %s", __FUNCTION__, #datum, #t1, #t2, (datum).type2str()); \
-		return; \
-	}
-
-#define TYPECHECK3(datum, t1, t2, t3)	\
-	if ((datum).type != (t1) && (datum).type != (t2) && (datum).type != (t3)) { \
-		warning("%s: %s arg should be of type %s, %s, or %s, not %s", __FUNCTION__, #datum, #t1, #t2, #t3, (datum).type2str()); \
-		return; \
-	}
-
-#define ARRBOUNDSCHECK(idx,array) \
-	if ((idx)-1 < 0 || (idx) > (int)(array).u.farr->size()) { \
-		warning("%s: index out of bounds (%d of %d)", __FUNCTION__, (idx), (array).u.farr->size()); \
-		return; \
-	}
 
 static BuiltinProto builtins[] = {
 	// Math
