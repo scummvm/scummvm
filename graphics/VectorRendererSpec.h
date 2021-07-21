3da93977bb30c330d6d44307638ecc28d41d45e3
av-dx <aashwin.vaish@research.iiit.ac.in>
2021-07-21 16:30:47 +0530

    Revert "GUI: Add horizontal triangle drawing algorithm"
    This reverts commit 6d2732e598500df36c793896a9a7bc3876fbb4e6.

--- graphics/VectorRendererSpec.h
+++ graphics/VectorRendererSpec.h
@@ -218,9 +218,6 @@ protected:
 	virtual void drawTriangleVertAlgClip(int x, int y, int w, int h,
 		bool inverted, PixelType color, FillMode fill_m);
 
-	virtual void drawTriangleHorAlg(int x, int y, int w, int h,
-	    bool inverted, PixelType color, FillMode fill_m);
-
 	virtual void drawTriangleFastV(int x, int y, int size,
 	    bool inverted, PixelType color, FillMode fill_m);
 
