c9c13f49d8e6810aa56004684d55d16803648a5d
Max Horn <max@quendi.de>
2021-08-04 09:14:43 +0200

    GRAPHICS: fix global constructor warnings
    Arguably it's better to use a hard code constant anyway, as the result
    of sqrt(2) can vary between platforms (not that it is likely to matter
    in practice ;-) ).

--- graphics/scaler/edge.cpp
+++ graphics/scaler/edge.cpp
@@ -123,7 +123,8 @@
 #define GREY_SHIFT 12           /* bit shift for greyscale precision */
 #define RGB_SHIFT 13            /* bit shift for RGB precision */
 
-static const int16 one_sqrt2 = (int16)(((int16)1 << GREY_SHIFT) / sqrt(2.0) + 0.5);
+#define SQRT2 1.41421356237309504880
+static const int16 one_sqrt2 = (int16)(((int16)1 << GREY_SHIFT) / SQRT2 + 0.5);
 // static const int16 int32_sqrt3 = (int16)(((int16)1 << GREY_SHIFT) * sqrt(3.0) + 0.5);
 
 
