25116a6eee0b2dbd34ac02377ba06cd8a145c203
Martin Gerhardy <martin.gerhardy@gmail.com>
2021-08-09 11:05:19 +0200

    TWINE: fixed warning

--- engines/twine/parser/text.cpp
+++ engines/twine/parser/text.cpp
@@ -52,7 +52,7 @@ bool TextData::loadFromHQR(const char *name, TextBankId textBankId, int language
 	_texts[(int)textBankId].clear();
 	initCustomTexts(textBankId);
 
-	const int numIdxEntries = indexStream->size() / 2;
+	const int numIdxEntries = (int)indexStream->size() / 2;
 	_texts[(int)textBankId].reserve(numIdxEntries + _texts[(int)textBankId].size());
 
 	for (int entry = 0; entry < numIdxEntries; ++entry) {
