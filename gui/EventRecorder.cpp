15dd09a527a54aa77e1e893be6a4275f20732766
Martin Gerhardy <martin.gerhardy@gmail.com>
2021-08-10 09:34:48 +0200

    EVENTRECORDER: fixed compilation

--- gui/EventRecorder.cpp
+++ gui/EventRecorder.cpp
@@ -631,7 +631,7 @@ Common::StringArray EventRecorder::listSaveFiles(const Common::String &pattern) 
 	if (_recordMode == kRecorderPlayback) {
 		Common::StringArray result;
 		for (Common::HashMap<Common::String, Common::PlaybackFile::SaveFileBuffer>::iterator  i = _playbackFile->getHeader().saveFiles.begin(); i != _playbackFile->getHeader().saveFiles.end(); ++i) {
-			if (i->_key.matchString(pattern, false, true)) {
+			if (i->_key.matchString(pattern, false, "/")) {
 				result.push_back(i->_key);
 			}
 		}
