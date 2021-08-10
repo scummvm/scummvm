b5171e35670baf3996af7784a6bea67729aa2118
athrxx <athrxx@scummvm.org>
2021-08-10 19:16:59 +0200

    SCUMM: (FT) - fix actor facing in calcMovementFactor
    (verified from disasm that FT uses the same method as DIG and COMI)

--- engines/scumm/actor.cpp
+++ engines/scumm/actor.cpp
@@ -518,14 +518,12 @@ int Actor::calcMovementFactor(const Common::Point& next) {
 	_walkdata.deltaXFactor = deltaXFactor;
 	_walkdata.deltaYFactor = deltaYFactor;
 
-	if (_vm->_game.version >= 5 && _vm->_game.version <= 6) {
+	if (_vm->_game.version >= 7)
+		_targetFacing = normalizeAngle((int)(atan2((double)deltaXFactor, (double)-deltaYFactor) * 180 / M_PI));
+	else if (_vm->_game.version >= 5)
 		_targetFacing = ((ABS(diffY) * 3) > ABS(diffX)) ? (deltaYFactor > 0 ? 180 : 0) : (deltaXFactor > 0 ? 90 : 270);
-	} else if (_vm->_game.id == GID_DIG || _vm->_game.id == GID_CMI) {
-		double temp = atan2((double)deltaXFactor, (double)-deltaYFactor);
-		_targetFacing = normalizeAngle((int)(temp * 180 / M_PI));
-	} else {
+	else 
 		_targetFacing = getAngleFromPos(deltaXFactor, deltaYFactor);
-	}
 
 	return actorWalkStep();
 }
