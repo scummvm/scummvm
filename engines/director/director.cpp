43d676e56f9f4206c78050160efca80cbc2a6557
djsrv <dservilla@gmail.com>
2021-08-13 13:18:23 -0400

    Revert "DIRECTOR: Set default palette based on platform"
    This reverts commit 49122e135ba41e10bd9c6d4fa110e5f8b07f8ea8.
    This made Meet MediaBand for Windows use the wrong palette.
    More investigation is needed.

--- engines/director/director.cpp
+++ engines/director/director.cpp
@@ -173,7 +173,7 @@ Common::Error DirectorEngine::run() {
 	_wm->setScreen(_surface);
 	_wm->addWindowInitialized(_stage);
 	_wm->setActiveWindow(_stage->getId());
-	setPalette((getPlatform() == Common::kPlatformWindows) ? kClutSystemWin : kClutSystemMac);
+	setPalette(-1);
 
 	_currentWindow = _stage;
 
