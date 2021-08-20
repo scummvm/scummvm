797ca3568dc2d507cb3b7c18ba8de78eb369cde1
Eugene Sandulenko <sev@scummvm.org>
2021-08-20 21:17:03 +0200

    JANITORIAL: Fix code formatting

--- engines/grim/grim.cpp
+++ engines/grim/grim.cpp
@@ -1116,12 +1116,12 @@ void GrimEngine::mainLoop() {
 			g_imuseState = -1;
 		}
 
-		#if defined(__EMSCRIPTEN__)
-		// If SDL_HINT_EMSCRIPTEN_ASYNCIFY is enabled, SDL pauses the application and gives 
-		// back control to the browser automatically by calling emscripten_sleep via SDL_Delay. 
-		// Without this the page would completely lock up. 
+#if defined(__EMSCRIPTEN__)
+		// If SDL_HINT_EMSCRIPTEN_ASYNCIFY is enabled, SDL pauses the application and gives
+		// back control to the browser automatically by calling emscripten_sleep via SDL_Delay.
+		// Without this the page would completely lock up.
 		g_system->delayMillis(0);
-		#endif
+#endif
 
 		uint32 endTime = g_system->getMillis();
 		if (startTime > endTime)
