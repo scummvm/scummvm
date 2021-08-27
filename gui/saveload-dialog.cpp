scummvm/scummvm

commit 44e5d3f9bdb64e000f97f234e96f85637c3bc1b0

GUI: Factor out save/load activation to a function

gui/saveload-dialog.cpp
@@ -330,6 +330,17 @@ void SaveLoadChooserDialog::listSaves() {
 #endif
 }
 
+void SaveLoadChooserDialog::activate(int slot, const Common::U32String &description) {
+	if (!_saveList.empty() && slot < int(_saveList.size())) {
+		const SaveStateDescriptor &desc = _saveList[slot];
+		if (_saveMode) {
+			_resultString = description.empty() ? desc.getDescription() : description;
+		}
+		setResult(desc.getSaveSlot());
+	}
+	close();
+}
+
 #ifndef DISABLE_SAVELOADCHOOSER_GRID
 void SaveLoadChooserDialog::addChooserButtons() {
 	if (_listButton) {
@@ -441,22 +452,14 @@ void SaveLoadChooserSimple::handleCommand(CommandSender *sender, uint32 cmd, uin
 		if (selItem >= 0 && _chooseButton->isEnabled()) {
 			if (_list->isEditable() || !_list->getSelectedString().empty()) {
 				_list->endEditMode();
-				if (!_saveList.empty()) {
-					setResult(_saveList[selItem].getSaveSlot());
-					_resultString = _list->getSelectedString();
-				}
-				close();
+				activate(selItem, _list->getSelectedString());
 			}
 		}
 		break;
 	case kChooseCmd:
 		_list->endEditMode();
 		if (selItem >= 0) {
-			if (!_saveList.empty()) {
-				setResult(_saveList[selItem].getSaveSlot());
-				_resultString = _list->getSelectedString();
-			}
-			close();
+			activate(selItem, _list->getSelectedString());
 		}
 		break;
 	case kListSelectionChangedCmd:
@@ -793,15 +796,9 @@ const Common::U32String &SaveLoadChooserGrid::getResultString() const {
 }
 
 void SaveLoadChooserGrid::handleCommand(CommandSender *sender, uint32 cmd, uint32 data) {
-	if (cmd <= _entriesPerPage && cmd + _curPage * _entriesPerPage <= _saveList.size()) {
-		const SaveStateDescriptor &desc = _saveList[cmd - 1 + _curPage * _entriesPerPage];
-
-		if (_saveMode) {
-			_resultString = desc.getDescription();
-		}
-
-		setResult(desc.getSaveSlot());
-		close();
+	const int slot = cmd + _curPage * _entriesPerPage - 1;
+	if (cmd <= _entriesPerPage) {
+		activate(slot, Common::U32String());
 	}
 
 	switch (cmd) {

gui/saveload-dialog.h
@@ -110,6 +110,8 @@ protected:
 	*/
 	virtual void listSaves();
 
+	void activate(int slot, const Common::U32String &description);
+
 	const bool					_saveMode;
 	const MetaEngine		    *_metaEngine;
 	bool						_delSupport;
@@ -120,6 +122,7 @@ protected:
 	Common::String				_target;
 	bool _dialogWasShown;
 	SaveStateList				_saveList;
+	Common::U32String			_resultString;
 
 #ifndef DISABLE_SAVELOADCHOOSER_GRID
 	ButtonWidget *_listButton;
@@ -165,8 +168,6 @@ private:
 	StaticTextWidget	*_time;
 	StaticTextWidget	*_playtime;
 	StaticTextWidget	*_pageTitle;
-
-	U32String			_resultString;
 
 	void addThumbnailContainer();
 	void updateSelection(bool redraw);
@@ -227,7 +228,6 @@ private:
 
 	ContainerWidget *_newSaveContainer;
 	int _nextFreeSaveSlot;
-	Common::U32String _resultString;
 
 	SavenameDialog _savenameDialog;
 	bool selectDescription();

commit 626d0ed4b6570864d199b5208e9aebb4ae642fc6

GUI: Fix assert when double clicking on item in list save dialog
This was a regression from 44e5d3f9 (GUI: Factor out save/load
activation to a function).

This fixes part of bug #12850

gui/saveload-dialog.cpp
@@ -464,14 +464,20 @@ void SaveLoadChooserSimple::handleCommand(CommandSender *sender, uint32 cmd, uin
 		if (selItem >= 0 && _chooseButton->isEnabled()) {
 			if (_list->isEditable() || !_list->getSelectedString().empty()) {
 				_list->endEditMode();
-				activate(selItem, _list->getSelectedString());
+				Common::U32String description;
+				if (!_saveList.empty())
+					description = _list->getSelectedString();
+				activate(selItem, description);
 			}
 		}
 		break;
 	case kChooseCmd:
 		_list->endEditMode();
 		if (selItem >= 0) {
-			activate(selItem, _list->getSelectedString());
+			Common::U32String description;
+			if (!_saveList.empty())
+				description = _list->getSelectedString();
+			activate(selItem, description);
 		}
 		break;
 	case kListSelectionChangedCmd: