2e440e5d964962d9a8a7315a54f54e044fffd43a
Cameron Cawley <ccawley2011@gmail.com>
2021-08-29 16:08:33 +0100

    RISCOS: Replace the StrongHelp manual with the PDF manual

--- backends/platform/sdl/riscos/riscos.mk
+++ backends/platform/sdl/riscos/riscos.mk
@@ -6,6 +6,8 @@ endif
 
 APP_NAME=!ScummVM
 
+DIST_FILES_DOCS_plaintext := $(filter-out $(DIST_FILES_MANUAL),$(DIST_FILES_DOCS))
+
 # Special target to create an RISC OS snapshot installation
 riscosdist: all $(DIST_FILES_DOCS)
 	mkdir -p $(APP_NAME)
@@ -35,43 +37,15 @@ endif
 ifdef TOKENIZE
 	$(TOKENIZE) ${srcdir}/dists/riscos/FindHelp,fd1 -out $(APP_NAME)/FindHelp,ffb
 endif
-	@$(foreach file, $(DIST_FILES_DOCS) $(srcdir)/doc/QuickStart, echo '   ' ICONV '  ' $(APP_NAME)/docs/$(notdir $(file)),fff;iconv --to-code=$(ENCODING) $(file) > $(APP_NAME)/docs/$(notdir $(file)),fff;)
+ifdef DIST_FILES_MANUAL
+	cp $(DIST_FILES_MANUAL) $(APP_NAME)/docs/ScummVM,adf
+endif
+	@$(foreach file, $(DIST_FILES_DOCS_plaintext) $(srcdir)/doc/QuickStart, echo '   ' ICONV '  ' $(APP_NAME)/docs/$(notdir $(file)),fff;iconv --to-code=$(ENCODING) $(file) > $(APP_NAME)/docs/$(notdir $(file)),fff;)
 	@$(foreach lang, $(DIST_FILES_DOCS_languages), mkdir -p $(APP_NAME)/docs/$(lang); $(foreach file, $(DIST_FILES_DOCS_$(lang)), echo '   ' ICONV '  ' $(APP_NAME)/docs/$(lang)/$(notdir $(file)),fff;iconv --from-code=UTF-8 --to-code=$(ENCODING) $(file) > $(APP_NAME)/docs/$(lang)/$(notdir $(file)),fff;))
 
 clean: riscosclean
 
 riscosclean:
 	$(RM_REC) $(APP_NAME)
-
-ifdef BINDHELP
-ifdef PANDOC
-
-riscosdist: $(APP_NAME)/docs/ScummVM,3d6 # $(APP_NAME)/docs/de/ScummVM,3d6 $(APP_NAME)/docs/cz/ScummVM,3d6 $(APP_NAME)/se/ScummVM,3d6
-
-README=${srcdir}/README.md
-NEWS=${srcdir}/NEWS.md
-$(APP_NAME)/docs/de/ScummVM,3d6: README=${srcdir}/doc/de/LIESMICH
-$(APP_NAME)/docs/de/ScummVM,3d6: NEWS=${srcdir}/doc/de/NEUES.md
-$(APP_NAME)/docs/cz/ScummVM,3d6: README=${srcdir}/doc/cz/PrectiMe
-$(APP_NAME)/docs/se/ScummVM,3d6: README=${srcdir}/doc/se/LasMig
-
-define manual-markdown
-	echo Converting markdown file '$1'
-	echo "ScummVM - $(notdir $(basename $1)) " > $(APP_NAME)/tmp/$2,fff
-	$(PANDOC) -f gfm -t ${srcdir}/dists/riscos/manual/stronghelp.lua $1 | iconv --to-code=$(ENCODING) >> $(APP_NAME)/tmp/$2,fff
-endef
-
-%,3d6: $(README) $(NEWS) ${srcdir}/dists/riscos/manual/stronghelp.lua ${srcdir}/devtools/credits.pl $(DIST_FILES_DOCS)
-	$(MKDIR) $(APP_NAME)/tmp
-	@$(call manual-markdown,$(README),!Root)
-	@$(call manual-markdown,$(NEWS),NEWS)
-	@$(call manual-markdown,${srcdir}/CONTRIBUTING.md,CONTRIBUTING)
-	${srcdir}/devtools/credits.pl --stronghelp > $(APP_NAME)/tmp/AUTHORS,fff
-	$(MKDIR) $(APP_NAME)/docs
-	$(BINDHELP) $(APP_NAME)/tmp $@ -r -f
-	$(RM_REC) $(APP_NAME)/tmp
-
-endif
-endif
 
 .PHONY: riscosdist riscosclean
