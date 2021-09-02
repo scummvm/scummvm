e60b0be71a472aea2419dc58721f3fb985102d68
Cameron Cawley <ccawley2011@gmail.com>
2021-09-02 22:13:59 +0100

    DS: Update scripts for creating split builds

--- backends/platform/ds/setup-builddirs.sh
+++ backends/platform/ds/setup-builddirs.sh
@@ -1,55 +1,14 @@
 #!/bin/sh
 
-# TODO: Allow specifying (a list of) build ids (a, b, ...) on the command line.
+srcdir=$(realpath $(dirname $0)/../../..)
+mkdir -p all-builds
 
-# TODO: Computer srcdir in a better way
-srcdir=../../../`dirname $0`/..
-
-BASE_PARAM="--host=ds --disable-translation --disable-debug --disable-all-engines"
-
-for build in a b c d e f g h i k; do
+for build in scumm access agi agos bbvs cge cge2 cine cruise director draci dragons drascula dreamweb gob griffon hugo illusions kyra lab lure made mads parallaction queen saga sci sherlock sky supernova teenagent tinsel tsage tucker voyeur xeen; do
 	echo "--- Setting up build $build ---"
 	mkdir -p build-$build && cd build-$build
-	case $build in
-	a)
-		EXTRA_PARAM="--enable-scumm"
-		;;
-	b)
-		EXTRA_PARAM="--enable-sky --enable-queen"
-		;;
-	c)
-		EXTRA_PARAM="--enable-agos"
-		;;
-	d)
-		EXTRA_PARAM="--enable-gob --enable-cine --enable-agi"
-		;;
-	e)
-		EXTRA_PARAM="--enable-saga --disable-mad"
-		;;
-	f)
-		EXTRA_PARAM="--enable-kyra --disable-mad"
-		;;
-	g)
-		EXTRA_PARAM="--enable-lure"
-		;;
-	h)
-		EXTRA_PARAM="--enable-parallaction"
-		;;
-	i)
-		EXTRA_PARAM="--enable-made --disable-mad"
-		;;
-	k)
-		EXTRA_PARAM="--enable-cruise --disable-mad"
-		;;
-	*)
-		echo "Invalid build $build selected"
-		exit 1
-		;;
-	esac
-
-	defname="DS_BUILD_`echo $build | tr '[a-z]' '[A-Z]'`"
-	CPPFLAGS="$CPPFLAGS -D$defname"
-	$srcdir/configure $BASE_PARAM $EXTRA_PARAM
+	$srcdir/configure --host=ds --disable-translation --disable-debug --disable-all-engines --enable-engine=$build --disable-ogg
+	make -j$(nproc) DESCRIPTION="Enabled engines: $build"
+	cp scummvm.nds ../all-builds/scummvm-$build.nds
 	cd ..
 	echo DONE
 	echo
