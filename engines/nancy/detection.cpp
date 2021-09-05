a2870482751b1f9d0b0f4019d7f18d795f019cc8
Cameron Cawley <ccawley2011@gmail.com>
2021-09-05 19:58:17 +0100

    NANCY: Remove dead code

--- engines/nancy/detection.cpp
+++ engines/nancy/detection.cpp
@@ -248,7 +248,6 @@ public:
 	}
 
 	virtual void registerDefaultSettings(const Common::String &target) const override;
-	//virtual GUI::OptionsContainerWidget *buildEngineOptionsWidgetStatic(GUI::GuiObject *boss, const Common::String &name, const Common::String &target) const override;
 };
 
 void NancyMetaEngineDetection::registerDefaultSettings(const Common::String &target) const {
@@ -257,11 +256,5 @@ void NancyMetaEngineDetection::registerDefaultSettings(const Common::String &tar
 	ConfMan.setInt("sfx_volume", 51 * 255 / 100, target);
 	ConfMan.setBool("subtitles", true, target);
 }
-
-/*
-GUI::OptionsContainerWidget *NancyMetaEngineDetection::buildEngineOptionsWidgetStatic(GUI::GuiObject *boss, const Common::String &name, const Common::String &target) const {
-	return new Nancy::NancyOptionsWidget(boss, name, target);
-}
-*/
 
 REGISTER_PLUGIN_STATIC(NANCY_DETECTION, PLUGIN_TYPE_ENGINE_DETECTION, NancyMetaEngineDetection);
