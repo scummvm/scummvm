c608f28f05f976685bfad3b9f30bc8e906d138e2
neuromancer <gustavo.grieco@gmail.com>
2021-09-10 15:38:58 +0200

    COMMON: removed convertPath

--- common/installshieldv3_archive.h
+++ common/installshieldv3_archive.h
@@ -59,8 +59,6 @@ private:
 
 	typedef Common::HashMap<Common::String, FileEntry, Common::IgnoreCase_Hash, Common::IgnoreCase_EqualTo> FileMap;
 	FileMap _map;
-
-	Common::String convertPath(const Common::String &name);
 };
 
 } // End of namespace Common
