e869c55993b45a4453b3eaa000f4bfc6c62dfbfe
Paul Gilbert <dreammaster@scummvm.org>
2021-09-17 18:52:57 -0700

    CHEWY: Move some variables from mcga to mcga_grafik

--- engines/chewy/mcga_grafik.cpp
+++ engines/chewy/mcga_grafik.cpp
@@ -37,9 +37,39 @@ namespace Chewy {
 
 byte pal_table[PALETTE_SIZE];
 
+int16 FuNr;
+real_regs rr;
+real_regs *rp = &rr;
+maus_info *m_info;
+VesaInfo vi;
+
+int16 clipx1, clipx2, clipy1, clipy2;
+int16 gcurx, gcury, fvorx, fvory;
+int16 fontbr, fonth;
+int16 scr_w, scr_h;
+int16 fontfirst, fontlast;
+char writemode;
+
+char MausMode;
+char enter_key;
+char escape_key;
+char einfuegen = 0;
+
+int16 crlfx = 0, crlfy = 0;
+int16 r_gef = -1, r_end = false;
+int16 *rlist = 0;
+bool mono = false;
+uint8 svga;
 
 mcga_grafik::mcga_grafik() {
 	int16 i;
+
+	crlfx = crlfy = 0;
+	r_gef = -1;
+	r_end = false;
+	rlist = 0;
+	mono = false;
+	svga = 0;
 
 #if 0
 	char *enstr;
