053cb66b41761e19bee088583a6529e378e7fb1d
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2021-09-27 20:42:09 +0200

    CGE: Always include all detection objects

--- engines/cge/module.mk
+++ engines/cge/module.mk
@@ -30,10 +30,4 @@ include $(srcdir)/rules.mk
 
 # Detection objects
 DETECT_OBJS += $(MODULE)/detection.o
-
-# Skip building the following objects if a static
-# module is enabled, because it already has the contents.
-ifneq ($(ENABLE_CGE), STATIC_PLUGIN)
-# External dependencies for detection.
 DETECT_OBJS += $(MODULE)/fileio.o
-endif
