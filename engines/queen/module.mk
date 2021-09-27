641b1f04e060d025a7866fdc98f026634266a6a5
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2021-09-27 20:42:58 +0200

    QUEEN: Always include all detection objects

--- engines/queen/module.mk
+++ engines/queen/module.mk
@@ -34,11 +34,5 @@ include $(srcdir)/rules.mk
 
 # Detection objecs
 DETECT_OBJS += $(MODULE)/detection.o
-
-# Skip building the following objects if a static
-# module is enabled, because it already has the contents.
-ifneq ($(ENABLE_QUEEN), STATIC_PLUGIN)
-# External dependencies for detection.
 DETECT_OBJS += $(MODULE)/resource.o
 DETECT_OBJS += $(MODULE)/restables.o
-endif
