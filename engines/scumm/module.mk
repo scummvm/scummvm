a3a3e7ec0c51eacf02549783477a75658a650ab0
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2021-09-27 20:43:10 +0200

    SCUMM: Always include all detection objects

--- engines/scumm/module.mk
+++ engines/scumm/module.mk
@@ -174,10 +174,5 @@ include $(srcdir)/rules.mk
 
 # Detection objects
 DETECT_OBJS += $(MODULE)/detection.o
-
-# Skip building the following objects if a static
-# module is enabled, because it already has the contents.
-ifneq ($(ENABLE_SCUMM), STATIC_PLUGIN)
 DETECT_OBJS += $(MODULE)/file.o
 DETECT_OBJS += $(MODULE)/file_nes.o
-endif
