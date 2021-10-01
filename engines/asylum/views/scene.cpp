scummvm/scummvm

commit 5fe5a128a6ed005ed4d8bd603e351a09bd4ff156

ASYLUM: fix improper use of negative value
CID 1453216

engines/asylum/views/scene.cpp
@@ -800,7 +800,8 @@ void Scene::updateMouse() {
 		newDirection = kDirectionSE;
 	}
 
-	updateCursor(newDirection, actorRect);
+	if (newDirection != kDirectionInvalid)
+		updateCursor(newDirection, actorRect);
 
 	if (newDirection >= kDirectionN)
 		if (player->getStatus() == kActorStatusWalking || player->getStatus() == kActorStatusWalking2)

commit 3533979ea49d6f718ffa7f0d78950138888a940d

ASYLUM: fix setting cursor when hovering over the player
Regression from 5fe5a12

engines/asylum/views/scene.cpp
@@ -800,8 +800,7 @@ void Scene::updateMouse() {
 		newDirection = kDirectionSE;
 	}
 
-	if (newDirection != kDirectionInvalid)
-		updateCursor(newDirection, actorRect);
+	updateCursor(newDirection, actorRect);
 
 	if (newDirection >= kDirectionN)
 		if (player->getStatus() == kActorStatusWalking || player->getStatus() == kActorStatusWalking2)
@@ -1150,10 +1149,12 @@ void Scene::updateCursor(ActorDirection direction, const Common::Rect &rect) {
 	if (getCursor()->getState() & kCursorStateRight) {
 		if (player->getStatus() == kActorStatusWalking || player->getStatus() == kActorStatusWalking2) {
 
-			ResourceId resourceId =_ws->cursorResources[direction];
+			if (direction >= kDirectionN) {
+				ResourceId resourceId =_ws->cursorResources[direction];
 
-			if (direction >= kDirectionN && getCursor()->getResourceId() != resourceId)
-				getCursor()->set(resourceId);
+				if (getCursor()->getResourceId() != resourceId)
+					getCursor()->set(resourceId);
+			}
 		}
 
 		return;