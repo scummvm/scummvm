32ec986518c2cfd925ae8da252917e166ba2a813
alxpnv <alxpnv22@yahoo.com>
2021-10-15 14:47:10 +0300

    ASYLUM: fix vertical alignment for movie subtitles
    Bug reported by Attx

--- engines/asylum/views/video.cpp
+++ engines/asylum/views/video.cpp
@@ -101,13 +101,12 @@ bool VideoPlayer::handleEvent(const AsylumEvent &evt) {
 			getScreen()->fillRect(0, 400, 640, 80, 0);
 
 			if (_subtitleIndex >= 0) {
-				char *text1 = getText()->get((ResourceId)_currentMovie);
+				char *text = getText()->get(_subtitles[_subtitleIndex].resourceId);
 
-				int16 y = (int16)(10 * (44 - getText()->draw(0, 99, kTextCalculate, Common::Point(10, 400), 20, 620, text1)));
+				int16 y = (int16)(10 * (44 - getText()->draw(0, 99, kTextCalculate, Common::Point(10, 400), 20, 620, text)));
 				if (y <= 400)
 					y = 405;
 
-				char *text = getText()->get(_subtitles[_subtitleIndex].resourceId);
 				getText()->draw(0, 99, kTextCenter, Common::Point(10, y), 20, 620, text);
 
 				if (_vm->checkGameVersion("Steam")) {
