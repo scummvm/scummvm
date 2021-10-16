ffbc5f37b7d3e2a1f358d3aafa392bc0fd5b2f91
sluicebox <22204938+sluicebox@users.noreply.github.com>
2021-10-15 21:30:53 -0500

    SCI: Add LAURABOW2 workaround for uninit read in museum
    Prevents a warning that frequently occurs during most of the game
    as actors walk around the museum.

--- engines/sci/engine/workarounds.cpp
+++ engines/sci/engine/workarounds.cpp
@@ -352,6 +352,7 @@ const SciWorkaroundEntry uninitializedReadForParamWorkarounds[] = {
 	{ GID_HOYLE5,         -1,    15, -1,               "Hand", "add",                             NULL,     1,     1,{ WORKAROUND_FAKE,   0 } }, // When the game adds cards to your hand in any mini-game
 	{ GID_HOYLE5,        700,   730,  0,                 NULL, "runningSuit",                     NULL,     2,     2,{ WORKAROUND_FAKE,   0 } }, // when an opponent is playing in Bridge
 	{ GID_HOYLE5,       1100,    22, -1,           "HandPile", "show",                            NULL,     1,     1,{ WORKAROUND_FAKE,   0 } }, // when showing money piles in Poker
+	{ GID_LAURABOW2,      -1,    90,  0,          "MuseumRgn", "crowdInRoom",                     NULL,     3,     4,{ WORKAROUND_FAKE,   0 } }, // When actors walk around the museum. Out-of-bounds parameters are read but not used. Doesn't matter, but generates a lot of warnings.
 	{ GID_PHANTASMAGORIA2,-1, 63019,  0,     "WynDocTextView", "cue",                             NULL,     2,     2,{ WORKAROUND_FAKE,   0 } }, // When dragging the slider next to an e-mail message
 	{ GID_SHIVERS,        -1, 64918,  0,                "Str", "strip",                           NULL,     1,     1,{ WORKAROUND_FAKE,   0 } }, // When starting a new game and entering a name
 	{ GID_SQ4,            35,   928,  0,           "Narrator", "say",                             NULL,     1,     1,{ WORKAROUND_FAKE,  11 } }, // Clicking smell on sidewalk, fixes message due to missing say parameter in sidewalk1:doVerb(6) - bug #10917
