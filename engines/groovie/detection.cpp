022b107ccb671ebc761b2b63dc409cda7d5fd081
Filippos Karapetis <bluegr@gmail.com>
2021-10-26 04:52:17 +0300

    GROOVIE: Fix incorrect GUIO flags

--- engines/groovie/detection.cpp
+++ engines/groovie/detection.cpp
@@ -281,7 +281,7 @@ static const GroovieGameDescription gameDescriptions[] = {
 			AD_ENTRY2s("clanmain.grv", "dd424120fa1daa9d6b576d0ba22a4936", 54253,
 						"ACT01MUS.MPG", NULL, -1),
 			Common::EN_ANY, Common::kPlatformWindows, ADGF_UNSTABLE,
-			GUIO3(GUIO_NOMIDI, GUIO_NOASPECT)
+			GUIO2(GUIO_NOMIDI, GUIO_NOASPECT)
 		},
 		kGroovieCDY
 	},
@@ -293,7 +293,7 @@ static const GroovieGameDescription gameDescriptions[] = {
             AD_ENTRY2s("CLANMAIN.GRV", "dd424120fa1daa9d6b576d0ba22a4936", 54253,
                        "ACT01MUS.m4a", NULL, -1),
             Common::EN_ANY, Common::kPlatformUnknown, ADGF_UNSTABLE,
-            GUIO3(GUIO_NOMIDI, GUIO_NOASPECT)
+            GUIO2(GUIO_NOMIDI, GUIO_NOASPECT)
         },
         kGroovieCDY
     },
