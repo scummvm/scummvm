2606eb5a33553a1718fc190541c046665bf9d7b4
Paweł Kołodziejski <aquadran@gmail.com>
2021-10-28 11:06:55 +0200

    STARK: Improve messages while creating renderer.

--- engines/stark/gfx/driver.cpp
+++ engines/stark/gfx/driver.cpp
@@ -26,12 +26,15 @@
 #include "engines/stark/gfx/tinygl.h"
 
 #include "common/config-manager.h"
+#include "common/translation.h"
 
 #include "graphics/renderer.h"
 #include "graphics/surface.h"
 #if defined(USE_OPENGL_GAME) || defined(USE_OPENGL_SHADERS) || defined(USE_GLES2)
 #include "graphics/opengl/context.h"
 #endif
+
+#include "gui/error.h"
 
 #include "engines/util.h"
 
@@ -56,7 +59,7 @@ Driver *Driver::create() {
 
 	if (matchingRendererType != desiredRendererType && desiredRendererType != Graphics::kRendererTypeDefault) {
 		// Display a warning if unable to use the desired renderer
-		warning("Unable to create a '%s' renderer", rendererConfig.c_str());
+		warning("Unable to match a '%s' renderer", rendererConfig.c_str());
 	}
 
 	Driver *driver = nullptr;
@@ -75,12 +78,13 @@ Driver *Driver::create() {
 #endif
 	if (matchingRendererType == Graphics::kRendererTypeTinyGL) {
 		//driver = new TinyGLDriver();
-		error("This game does not currently support software rendering");
 	}
 
 	if (driver)
 		return driver;
-	error("No renderers have been found for this game");
+	warning("No renderers have been found for this game");
+	GUI::displayErrorDialog(Common::U32String::format(_("No renderers have been found for this game")));
+	return driver;
 }
 
 const Graphics::PixelFormat Driver::getRGBAPixelFormat() {
