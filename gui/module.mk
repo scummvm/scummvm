9a01b16ada7bc442277574d80fad5e0d6c51f554
Eugene Sandulenko <sev@scummvm.org>
2021-11-10 15:58:44 +0100

    GUI: Sort module.mk alphabetically

--- gui/module.mk
+++ gui/module.mk
@@ -15,6 +15,7 @@ MODULE_OBJS := \
 	launcher.o \
 	massadd.o \
 	message.o \
+	MetadataParser.o \
 	object.o \
 	options.o \
 	predictivedialog.o \
@@ -25,19 +26,18 @@ MODULE_OBJS := \
 	ThemeEval.o \
 	ThemeLayout.o \
 	ThemeParser.o \
-	MetadataParser.o \
 	Tooltip.o \
 	unknown-game-dialog.o \
+	widget.o \
 	animation/Animation.o \
 	animation/RepeatAnimationWrapper.o \
 	animation/SequenceAnimationComposite.o \
-	widget.o \
 	widgets/editable.o \
 	widgets/edittext.o \
-	widgets/list.o \
-	widgets/groupedlist.o \
-	widgets/popup.o \
 	widgets/grid.o \
+	widgets/groupedlist.o \
+	widgets/list.o \
+	widgets/popup.o \
 	widgets/scrollbar.o \
 	widgets/scrollcontainer.o \
 	widgets/tab.o
