76f7fad096a257fbaf21bfaa14a60eddec7182a8
Strangerke <arnaud.boutonne@gmail.com>
2021-11-11 10:55:16 +0100

    EFH: Partial implementation of initEngine()

--- engines/efh/efh.h
+++ engines/efh/efh.h
@@ -56,6 +56,38 @@ static const int kSavegameVersion = 1;
 
 struct EfhGameDescription;
 
+struct EfhGraphicsStruct {
+	uint16 _vgaLineBuffer[200];
+	uint16 _shiftValue;
+	uint16 _width;
+	uint16 _height;
+	Common::Rect _area;
+};
+
+struct Font {
+	uint8 _lines[8];
+};
+
+struct FontDescr {
+	uint8 *_widthArray;
+	uint8 *_extraLines;
+	Font  *_fontData;
+	uint8 _charHeight;
+	uint8 _extraVerticalSpace;
+	uint8 _extraHorizontalSpace;
+};
+
+struct BufferBM {
+	uint8 *_dataPtr;
+	uint16 _width;
+	uint16 _startX;
+	uint16 _startY;
+	uint16 _height;
+	uint16 _fieldA;
+	uint8 _paletteTransformation;
+	uint16 _fieldD;
+};
+
 class EfhEngine : public Engine {
 public:
 	EfhEngine(OSystem *syst, const EfhGameDescription *gd);
@@ -97,6 +129,65 @@ private:
 	Common::Platform _platform;
 
 	void initialize();
+	int32 readFileToBuffer(Common::String &filename, uint8 *destBuffer);
+	void readAnimInfo();
+	void displayAnimFrames(int16 animId, bool displayMenuBoxFl);
+	void readTileFact();
+	void readItems();
+	void loadNPCS();
+	void initEngine();
+	void saveAnimImageSetId();
+	void sub15150(int i);
+	void sub12A7F();
+	void displayLowStatusScreen(int i);
+	void loadImageSet(int imageSetId, uint8 *buffer, uint8 **subFilesArray, char CGAVal, char EGAVal, uint8 *destBuffer, uint8 *transfBuffer);
+	void displayFctFullScreen();
+	void displayBitmapAtPos(int16 minX, int16 minY, int16 maxX, int16 maxY);
+	void sub10B77_unkDisplayFct1(uint8 *imagePtr, int16 posX, int16 posY, uint8 guess_paletteTransformation);
+	void sub24D92(BufferBM *bufferBM, int16 posX, int16 posY);
+	void loadImageSetToTileBank(int16 tileBankId, int16 imageSetId);
+
+	uint8 _videoMode;
+	uint8 _bufferCharBM[128];
+	EfhGraphicsStruct *_graphicsStruct;
+	uint8 _tileBank[3][12000];
+	uint8 _circleImageBuf[40100];
+	uint8 _portraitBuf[25000];
+	uint8 _hiResImageBuf[40100];
+	uint8 _loResImageBuf[40100];
+	uint8 _menuBuf[12500];
+	uint8 _windowWithBorderBuf[1500];
+	uint8 _map[7000];
+	uint8 _places[12000];
+	uint8 _curPlace[600];
+	uint8 _npcBuf[13400];
+	uint8 _imp1[13000];
+	uint8 _imp2[10000];
+	uint8 _titleSong[1024];
+	uint8 _items[8100];
+	uint8 _tileFact[864];
+	uint8 _animInfo[9000];
+	uint8 _history[256];
+	uint8 _techData[4096];
+
+	uint8 *_mapBitmapRef;
+	uint8 *_mapUnknownPtr;
+	uint8 *_mapMonstersPtr;
+	uint8 *_mapGameMapPtr;
+
+	uint8 _defaultBoxColor;
+	FontDescr _fontDescr;
+
+	uint8 _word31E9E;
+
+	int16 _oldAnimImageSetId;
+	int16 _animImageSetId;
+	uint8 _paletteTransformationConstant;
+	uint8 *_circleImageSubFileArray[12];
+	uint8 *_imageSetSubFilesArray[214]; // CHECKME : logically it should be 216
+	BufferBM _imageDataPtr;
+	int16 _currentTileBankImageSetId[3];
+	
 };
 
 } // End of namespace Efh
