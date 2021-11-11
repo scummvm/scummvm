8649723c97887ea8c7a82e41d08705d85e067703
Le Philousophe <lephilousophe@users.noreply.github.com>
2021-11-11 11:33:44 +0100

    OPENDINGUX: Rework build_odbeta.sh to avoid environment variables

--- backends/platform/sdl/opendingux/build_odbeta.sh
+++ backends/platform/sdl/opendingux/build_odbeta.sh
@@ -1,37 +1,46 @@
 #!/bin/bash
 
-CONFIG="./configure --host=opendingux-$target --enable-release --disable-detection-full"
+set -e
+
+target=$1
+if [ "$2" = "dualopk" ]; then
+	dualopk="dualopk=yes"
+else
+	dualopk=
+fi
 
 case $target in
 
         gcw0)
 	target2=$target
+	libc=uclibc
         ;;
 
         lepus)
 	target2=$target
+	libc=musl
         ;;
 
         rg99)
 	target2=rs90
+	libc=musl
         ;;
         
         *)
         echo "please provide a valid target for the build: gcw0, lepus or rg99"
-        exit 0
+        exit 1
         ;;
 esac
 
 TOOLCHAIN=/opt/$target2-toolchain
-
-if [ $target == "gcw0" ]; then
-	SYSROOT=$TOOLCHAIN/mipsel-$target2-linux-uclibc
-else
-	SYSROOT=$TOOLCHAIN/mipsel-$target2-linux-musl
-fi
+SYSROOT=$TOOLCHAIN/mipsel-$target2-linux-$libc
 
 export PATH=$TOOLCHAIN/usr/bin:$SYSROOT/usr/include:$TOOLCHAIN/bin:$PATH
 export CXX=mipsel-linux-g++
 export CXXFLAGS="-funsigned-char" # workaround for a scummvm tolower() bug when adding games
 
-$CONFIG && make -j12 od-make-opk && ls -lh scummvm_$target*.opk
+./configure --host=opendingux-$target --enable-release --disable-detection-full
+
+make -j12 od-make-opk $dualopk
+
+ls -lh scummvm_$target*.opk
