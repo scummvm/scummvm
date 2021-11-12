cb97a47828e00ec94f23e4e44f5416d7c8777633
Coen Rampen <crampen@gmail.com>
2021-11-12 22:41:26 +0100

    GROOVIE: Add MIDI controller defaults for T7G and 11H
    The 7th Guest uses the orchestra drumkit by setting it during initialization,
    but not at the start of each track. Some tracks temporarily change the drumkit;
    if these tracks are stopped at a bad moment, this changed drumkit remains in
    effect. This commit sets the drumkit default to the orchestra drumkit to make
    sure it is applied at the start of each track.
    
    Some of the MIDI tracks of The 11th Hour use the modulation controller, but not
    all tracks reset this controller at the start. This commit sets the modulation
    default to 0 to make sure it is reset at the start of each track.

--- engines/groovie/music.cpp
+++ engines/groovie/music.cpp
@@ -467,6 +467,19 @@ MusicPlayerXMI::MusicPlayerXMI(GroovieEngine *vm, const Common::String &gtlName)
 	_multisourceDriver->property(MidiDriver::PROP_USER_VOLUME_SCALING, true);
 	_multisourceDriver->property(MidiDriver::PROP_MILES_VERSION,
 		_vm->getEngineVersion() == kGroovieT7G ? Audio::MILES_VERSION_2 : Audio::MILES_VERSION_3);
+	if (_vm->getEngineVersion() == kGroovieT7G && musicType == MT_GM)
+		// The 7th Guest GM init sets drumkit to 0x30 (Orchestra) and relies on
+		// this remaining set; tracks don't set this at start. Some tracks
+		// temporarily change the drumkit; if playback is stopped at the wrong
+		// time this will cause the changed drumkit to remain in effect.
+		// Set a default drumkit value to make sure it is set correctly at the
+		// start of each track.
+		_multisourceDriver->setControllerDefault(MidiDriver_Multisource::CONTROLLER_DEFAULT_DRUMKIT, 0x30);
+	if (_vm->getEngineVersion() == kGroovieT11H)
+		// Some The 11th Hour tracks use modulation, but not all tracks reset
+		// it at start. Set a default value to make sure it is reset at the
+		// start of each track.
+		_multisourceDriver->setControllerDefault(MidiDriver_Multisource::CONTROLLER_DEFAULT_MODULATION, 0);
 
 	int result = _driver->open();
 	if (result > 0 && result != MidiDriver::MERR_ALREADY_OPEN)
