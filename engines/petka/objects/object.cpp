9394b5301ffece0802cfe595583eec0721252acb
Orgad Shaneh <orgads@gmail.com>
2021-11-13 23:40:36 +0200

    PETKA: Use nullptr
    Using clang-tidy modernize-use-nullptr

--- engines/petka/objects/object.cpp
+++ engines/petka/objects/object.cpp
@@ -535,10 +535,10 @@ void QObject::update(int time) {
 			g_vm->videoSystem()->addDirtyRect(Common::Point(_x, _y), *flc);
 			flc->setFrame(-1);
 			if (flc->getCurFrame() == (int32)flc->getFrameCount() - 1) {
-				g_vm->getQSystem()->addMessage(_id, kEnd, _resourceId, 0, 0, 0, 0);
+				g_vm->getQSystem()->addMessage(_id, kEnd, _resourceId, 0, 0, 0, nullptr);
 			}
 			if (flc->getCurFrame() + 1 == (int32)flc->getFrameCount() / 2) {
-				g_vm->getQSystem()->addMessage(_id, kHalf, _resourceId, 0, 0, 0, 0);
+				g_vm->getQSystem()->addMessage(_id, kHalf, _resourceId, 0, 0, 0, nullptr);
 			}
 			g_vm->videoSystem()->addDirtyRect(Common::Point(_x, _y), *flc);
 			_time -= flc->getDelay();
