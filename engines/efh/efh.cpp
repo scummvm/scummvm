825d755a63df8436aea5bd16643d8a45449b6b81
Strangerke <arnaud.boutonne@gmail.com>
2021-11-17 00:25:29 +0100

    EFH: Fix issue in loadTechMapImp

--- engines/efh/efh.cpp
+++ engines/efh/efh.cpp
@@ -292,8 +292,8 @@ void EfhEngine::loadTechMapImp(int16 fileId) {
 	readFileToBuffer(fileName, _hiResImageBuf);
 	uncompressBuffer(_hiResImageBuf, _map);
 
-	loadImageSetToTileBank(1, _mapBitmapRef[0]);
-	loadImageSetToTileBank(1, _mapBitmapRef[1]);
+	loadImageSetToTileBank(1, _mapBitmapRef[0] + 1);
+	loadImageSetToTileBank(2, _mapBitmapRef[1] + 1);
 
 	initMapMonsters();
 	readImpFile(_techId, true);
@@ -768,12 +768,11 @@ void EfhEngine::rImageFile(Common::String filename, uint8 *buffer, uint8 **subFi
 }
 
 void EfhEngine::displayFctFullScreen() {
-	// CHECKME: 200 is in the original but looks suspicious.
+	// CHECKME: 319 is in the original but looks suspicious.
 	displayBitmapAtPos(0, 0, 319, 200);
 }
 
 void EfhEngine::displayBitmapAtPos(int16 minX, int16 minY, int16 maxX, int16 maxY) {
-	warning("STUB - displayBitmapAtPos");
 	_graphicsStruct->copy(_vgaGraphicsStruct2);
 	_initRect = Common::Rect(minX, minY, maxX, maxY);
 	displayBitmap(_vgaGraphicsStruct2, _vgaGraphicsStruct1, _initRect, minX, minY);
@@ -784,7 +783,52 @@ void EfhEngine::displayBitmap(EfhGraphicsStruct *efh_graphics_struct, EfhGraphic
 }
 
 void EfhEngine::sub24D92(BufferBM *bufferBM, int16 posX, int16 posY) {
+	static uint16 byte2C80C[72] = {
+		   0,    1,    2,    3,    4,    5,    6,    7,
+		   8,    9, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+		0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+		0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F,
+		0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27,
+		0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F,
+		0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37,
+		0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F,
+		   0,    0,    0,    0, 0x3F,    1, 0xC7,    0
+	};
+
 	warning("STUB - sub24D92");
+	
+#if 0
+	if (bufferBM == nullptr)
+		return;
+
+	Common::Rect unkRect;
+	unkRect.left = posX - bufferBM->_startX;
+	unkRect.right = unkRect.left + bufferBM->_width - 1;
+	unkRect.top = posY - bufferBM->_startY;
+	unkRect.bottom = unkRect.top + bufferBM->_height - 1;
+
+	Common::Rect destRect;
+	if (!computeLargeRect(_graphicsStruct->_area, &unkRect, &destRect))
+		return;
+	
+	uint16 bufferFieldA = bufferBM->_fieldA;
+	int16 deltaMinY = (destRect.top - unkRect.top) * bufferFieldA;
+	int16 destWidth = destRect.width() + 1;
+
+	int16 deltaMinX = -1;
+	int16 tmpVal = (destRect.left - unkRect.left) * 2;
+
+	if (tmpVal < 0)
+		deltaMinX = 0;
+
+	int16 deltaWidth = (unkRect.right - unkRect.left) * 2 - (destRect.right - unkRect.left) * 2 + tmpVal;
+
+	uint8 *si = &bufferBM->_dataPtr[deltaMinY + tmpVal];
+	uint8 ch = bufferBM->_paletteTransformation;
+	uint8 cl = 4;
+	uint16 var3A = byte2C80C[bufferBM->_fieldD << 3];
+	//incomplete
+#endif
 }
 
 void EfhEngine::sub133E5(uint8 *impPtr, int posX, int posY, int maxX, int maxY, int argC) {
