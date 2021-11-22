6bb322696b7ab4668f6182cd8bca169920aa6b6c
Paul Gilbert <dreammaster@scummvm.org>
2021-11-21 21:25:00 -0800

    CHEWY: In-progress fleshing out ats_action

---
+++ engines/chewy/episode5.cpp
@@ -0,0 +1,76 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+#include "chewy/defines.h"
+#include "chewy/global.h"
+#include "chewy/ani_dat.h"
+#include "chewy/episode5.h"
+
+namespace Chewy {
+
+void r66_talk1() {
+}
+
+void r66_talk2() {
+}
+
+void r66_talk3() {
+}
+
+void r66_talk4() {
+}
+
+int r66_proc2() {
+	return 0;
+}
+
+void r76_talk1() {
+}
+
+void r76_talk2() {
+}
+
+void r81_proc1() {
+}
+
+void r82_talk1() {
+}
+
+void r82_talk2() {
+}
+
+void r82_talk3() {
+}
+
+void r84_talk1() {
+}
+
+void r84_talk2() {
+}
+
+void r89_talk1() {
+}
+
+void r94_talk1() {
+}
+
+} // namespace Chewy
