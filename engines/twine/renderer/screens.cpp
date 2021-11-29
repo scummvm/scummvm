534c5718b428772654034876fe6ee133318aa3b5
Martin Gerhardy <martin.gerhardy@gmail.com>
2021-11-29 17:20:46 +0100

    TWINE: Adeline software jingle keeps looping through the startup screens

--- engines/twine/renderer/screens.cpp
+++ engines/twine/renderer/screens.cpp
@@ -32,7 +32,7 @@
 namespace TwinE {
 
 bool Screens::adelineLogo() {
-	_engine->_music->playMidiMusic(31);
+	_engine->_music->playMidiMusic(31, 1);
 
 	return loadImageDelay(_engine->_resources->adelineLogo(), 7);
 }
