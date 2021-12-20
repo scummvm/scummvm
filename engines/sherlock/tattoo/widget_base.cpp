d612ce55cd0b09b0f8211a5b608aef84c72ab8ce
Torbjörn Andersson <eriktorbjorn@users.sourceforge.net>
2021-12-20 10:40:43 +0100

    SHERLOCK: Fix scroll position bar glitch

--- engines/sherlock/tattoo/widget_base.cpp
+++ engines/sherlock/tattoo/widget_base.cpp
@@ -269,7 +269,7 @@ void WidgetBase::drawScrollBar(int index, int pageSize, int count) {
 	int barY = (count <= pageSize) ? r.top + BUTTON_SIZE : r.top + BUTTON_SIZE +
 		(r.height() - BUTTON_SIZE * 2 - barHeight) * index / (count - pageSize);
 
-	_surface.fillRect(Common::Rect(r.left + 2, barY + 2, r.right - 2, barY + barHeight - 3), INFO_MIDDLE);
+	_surface.fillRect(Common::Rect(r.left + 2, barY + 2, r.right - 2, barY + barHeight - 2), INFO_MIDDLE);
 	ui.drawDialogRect(_surface, Common::Rect(r.left, barY, r.right, barY + barHeight), true);
 }
 
