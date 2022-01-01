1a604c7bd329b756612e979018ea48e516938b12
neuromancer <gustavo.grieco@gmail.com>
2022-01-01 18:51:41 +0100

    PRIVATE: more portable code to avoid endianness issues

--- engines/private/private.cpp
+++ engines/private/private.cpp
@@ -1386,9 +1386,8 @@ void PrivateEngine::drawScreen() {
 
 		byte newPalette[768];
 		for (int c = 0; c < 256; c++) { // This avoids any endianness issues
-				newPalette[c * 3 + 0] = cPalette[c * 4 + 0];
-				newPalette[c * 3 + 1] = cPalette[c * 4 + 1];
-				newPalette[c * 3 + 2] = cPalette[c * 4 + 2];
+				uint32 y = READ_UINT32(&cPalette[c * 4]) & 0x00FFFFFF;
+				WRITE_LE_UINT24(&newPalette[c * 3], y);
 		}
 
 		g_system->getPaletteManager()->setPalette(newPalette, 0, 256);
