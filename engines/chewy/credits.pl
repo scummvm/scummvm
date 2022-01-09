05ed7f1b6bb2a85995eab25aac55d5d93b2f222b
Paul Gilbert <dreammaster@scummvm.org>
2022-01-09 10:08:07 -0800

    CHEWY: Further add to credits

--- engines/chewy/credits.pl
+++ engines/chewy/credits.pl
@@ -2,4 +2,5 @@ begin_section("Chewy");
 	add_person("Paul Gilbert", "dreammaster", "");
 	add_person("Eugene Sandulenko", "sev", "");
 	add_person("Arnaud Boutonn&eacute;", "Strangerke", "");
+	add_person("Filippos Karapetis", "bluegr", "");
 end_section();
