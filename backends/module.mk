dd72e0e4885d8ee8b72b0ebd15d865534633ca7f
Cameron Cawley <ccawley2011@gmail.com>
2022-01-14 13:08:17 +0000

    RISCOS: Add a native MIDI driver

--- backends/module.mk
+++ backends/module.mk
@@ -275,6 +275,7 @@ MODULE_OBJS += \
 	events/riscossdl/riscossdl-events.o \
 	fs/riscos/riscos-fs.o \
 	fs/riscos/riscos-fs-factory.o \
+	midi/riscos.o \
 	plugins/riscos/riscos-provider.o
 ifndef SDL_BACKEND
 # This is needed for null backend but already included in SDL backend
