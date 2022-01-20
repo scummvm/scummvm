71aff3ff5fcbc47e20bca6aac27b9aefba821133
Paul Gilbert <dreammaster@scummvm.org>
2022-01-19 23:23:02 -0800

    CHEWY: Room 42 logic fixes

--- engines/chewy/rooms/room42.cpp
+++ engines/chewy/rooms/room42.cpp
@@ -30,25 +30,34 @@ namespace Chewy {
 namespace Rooms {
 
 void Room42::entry() {
+	if (!_G(spieler).R42BeamterWach) {
+		det->play_sound(0, 0);
+		det->start_detail(0, 255, 0);
+	}
+
 	if (_G(spieler).PersonRoomNr[P_HOWARD] == 42) {
 		SetUpScreenFunc = setup_func;
+
 		if (!flags.LoadGame) {
 			det->stop_detail(0);
 			_G(timer_nr)[0] = room->set_timer(8, 5);
 			det->set_static_ani(8, -1);
 			_G(spieler).R42BeamterWach = true;
+			det->disable_sound(0, 0);
+
 			SetUpScreenFunc = setup_func;
 
 			set_person_pos(80, 43, P_HOWARD, P_LEFT);
-
 			atds->set_ats_str(263, 1, ATS_DATEI);
 			atds->set_ats_str(264, 1, ATS_DATEI);
 		}
+
 		if (obj->check_inventar(HOTEL_INV) && obj->check_inventar(TICKET_INV) &&
-			!_G(spieler).R42BriefOk)
+				!_G(spieler).R42BriefOk)
 			start_aad_wait(302, -1);
+
 		if (obj->check_inventar(HOTEL_INV) && obj->check_inventar(TICKET_INV) &&
-			_G(spieler).R42BriefOk)
+				_G(spieler).R42BriefOk)
 			start_aad_wait(301, -1);
 	}
 }
@@ -65,12 +74,10 @@ void Room42::setup_func() {
 
 void Room42::calc_xit() {
 	if (_G(spieler).PersonRoomNr[P_HOWARD] == 42) {
-		if (flags.ExitMov) {
-			atds->set_ats_str(264, 1, ATS_DATEI);
-			stop_spz();
-			_G(spieler).R42HoToBeamter = false;
-
-		}
+		atds->set_ats_str(264, 1, ATS_DATEI);
+		stop_spz();
+		_G(spieler).R42HoToBeamter = false;
+		_G(spieler).PersonRoomNr[P_HOWARD] = 40;
 	}
 }
 
@@ -82,6 +89,7 @@ void Room42::get_kuerbis(int16 aad_nr) {
 	_G(spieler).PersonHide[P_CHEWY] = false;
 	start_spz(CH_TALK5, 255, ANI_VOR, P_CHEWY);
 	start_aad_wait(aad_nr, -1);
+
 	_G(spieler).PersonHide[P_CHEWY] = true;
 	start_detail_wait(7, 1, ANI_VOR);
 	_G(spieler).PersonHide[P_CHEWY] = false;
@@ -90,13 +98,14 @@ void Room42::get_kuerbis(int16 aad_nr) {
 
 int16 Room42::use_psack() {
 	int16 action_flag = false;
+
 	if (!_G(spieler).R42BeamterWach && !_G(spieler).inv_cur) {
 		action_flag = true;
 		get_kuerbis(136);
-	} else if (_G(spieler).R42HoToBeamter &&
-		!_G(spieler).inv_cur &&
-		!_G(spieler).R42MarkeOk) {
 
+	} else if (_G(spieler).R42HoToBeamter &&
+			!_G(spieler).inv_cur &&
+			!_G(spieler).R42MarkeOk) {
 		action_flag = true;
 		auto_move(3, P_CHEWY);
 		_G(spieler).PersonHide[P_CHEWY] = true;
@@ -112,6 +121,7 @@ int16 Room42::use_psack() {
 		auto_move(4, P_CHEWY);
 		start_aad_wait(185, -1);
 		_G(spieler).R42HoToBeamter = false;
+
 	} else if (is_cur_inventar(BRIEF2_INV)) {
 		action_flag = true;
 		auto_move(3, P_CHEWY);
@@ -126,15 +136,18 @@ int16 Room42::use_psack() {
 		_G(spieler).R40TrainMove = true;
 		_G(spieler).R28PostCar = true;
 		_G(spieler).R42BriefOk = true;
+
 	} else if (is_cur_inventar(BRIEF_INV)) {
 		action_flag = true;
 		start_aad_wait(182, -1);
 	}
+
 	return action_flag;
 }
 
 int16 Room42::use_beamter() {
 	int16 action_flag = false;
+
 	if (menu_item == CUR_HOWARD) {
 		action_flag = true;
 		atds->set_ats_str(264, 2, ATS_DATEI);
@@ -148,12 +161,14 @@ int16 Room42::use_beamter() {
 		start_spz(HO_TALK_L, 255, ANI_VOR, P_HOWARD);
 		flags.MausLinks = false;
 	}
+
 	return action_flag;
 }
 
 void Room42::talk_beamter() {
 	int16 dia_nr;
 	auto_move(1, P_CHEWY);
+
 	if (!_G(spieler).R42BeamterWach) {
 		dia_nr = 10;
 	} else if (!_G(spieler).R42MarkeOk) {
@@ -161,22 +176,29 @@ void Room42::talk_beamter() {
 	} else {
 		dia_nr = 14;
 	}
+
 	start_ads_wait(dia_nr);
 }
 
 void Room42::dia_beamter(int16 str_end_nr) {
 	hide_cur();
+
 	if (str_end_nr != 4) {
-		while (atds->aad_get_status() != -1 && !SHOULD_QUIT)
+		while (atds->aad_get_status() != -1) {
 			set_up_screen(DO_SETUP);
+			SHOULD_QUIT_RETURN;
+		}
+
 		_G(spieler).PersonHide[P_CHEWY] = true;
 		flags.NoDiaBox = true;
+
 		switch (str_end_nr) {
 		case 1:
 			start_detail_wait(3, 4, ANI_VOR);
 			break;
 
 		case 2:
+			det->play_sound(4, 0);
 			start_detail_wait(4, 4, ANI_VOR);
 			break;
 
@@ -184,20 +206,31 @@ void Room42::dia_beamter(int16 str_end_nr) {
 			start_detail_wait(5, 2, ANI_VOR);
 			break;
 
+		default:
+			break;
 		}
 	} else {
-		while (atds->aad_get_status() < 2 && !SHOULD_QUIT) {
+		while (atds->aad_get_status() < 2) {
 			set_up_screen(DO_SETUP);
+			SHOULD_QUIT_RETURN;
 		}
+
 		det->stop_detail(0);
+		det->disable_sound(0, 0);
 		start_detail_wait(1, 1, ANI_VOR);
 		det->start_detail(2, 255, ANI_VOR);
-		while (atds->aad_get_status() != -1 && !SHOULD_QUIT) {
+
+		while (atds->aad_get_status() != -1) {
 			set_up_screen(DO_SETUP);
+			SHOULD_QUIT_RETURN;
 		}
+
 		det->stop_detail(2);
 		det->start_detail(0, 255, ANI_VOR);
+		det->enable_sound(0, 0);
+		det->play_sound(0, 0);
 	}
+
 	_G(spieler).PersonHide[P_CHEWY] = true;
 	det->start_detail(6, 255, ANI_VOR);
 	start_aad_wait(135, -1);
