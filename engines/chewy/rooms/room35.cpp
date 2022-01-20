ff6029e3fc33c75fcf4c70da9169efc5139852a3
Paul Gilbert <dreammaster@scummvm.org>
2022-01-19 19:55:23 -0800

    CHEWY: Room 35 logic fixes

--- engines/chewy/rooms/room35.cpp
+++ engines/chewy/rooms/room35.cpp
@@ -43,6 +43,7 @@ void Room35::entry() {
 int16 Room35::schublade() {
 	int16 action_flag = false;
 	hide_cur();
+
 	if (!_G(spieler).inv_cur) {
 		if (!_G(spieler).R35Schublade) {
 			action_flag = true;
@@ -65,6 +66,7 @@ int16 Room35::schublade() {
 			atds->set_ats_str(234, 2, ATS_DATEI);
 		}
 	}
+
 	show_cur();
 	return action_flag;
 }
@@ -72,60 +74,73 @@ int16 Room35::schublade() {
 int16 Room35::use_cat() {
 	int16 action_flag = false;
 	hide_cur();
-	if (is_cur_inventar(TRANSLATOR_INV)) {
-		action_flag = true;
-		auto_move(4, P_CHEWY);
-		_G(spieler).R35TransCat = true;
-		start_spz_wait(CH_TRANS, 1, ANI_VOR, P_CHEWY);
-		flic_cut(FCUT_045, FLC_MODE);
-		start_spz(CH_TRANS, 1, ANI_VOR, P_CHEWY);
-		start_aad_wait(94, -1);
-	} else if (is_cur_inventar(PUTENKEULE_INV)) {
-		action_flag = true;
-		disable_timer();
-		auto_move(4, P_CHEWY);
-		_G(spieler).R35CatEat = true;
-		start_spz_wait(CH_LGET_O, 1, ANI_VOR, P_CHEWY);
-		del_inventar(_G(spieler).AkInvent);
-		det->stop_detail(0);
-		det->del_static_ani(0);
-		start_detail_wait(1, 1, ANI_VOR);
-		det->start_detail(2, 1, ANI_VOR);
-		start_detail_wait(3, 1, ANI_VOR);
-		obj->show_sib(SIB_KNOCHEN_R35);
-		obj->calc_rsi_flip_flop(SIB_KNOCHEN_R35);
-		det->show_static_spr(7);
-		atds->del_steuer_bit(237, ATS_AKTIV_BIT, ATS_DATEI);
-		while (det->get_ani_status(2) && !SHOULD_QUIT) {
-			set_up_screen(DO_SETUP);
+
+	if (_G(spieler).ChewyAni != 5) {
+		if (is_cur_inventar(TRANSLATOR_INV)) {
+			action_flag = true;
+			auto_move(4, P_CHEWY);
+			_G(spieler).R35TransCat = true;
+			start_spz_wait(CH_TRANS, 1, ANI_VOR, P_CHEWY);
+			flic_cut(FCUT_045, FLC_MODE);
+			start_spz(CH_TRANS, 1, ANI_VOR, P_CHEWY);
+			start_aad_wait(94, -1);
+		} else if (is_cur_inventar(PUTENKEULE_INV)) {
+			action_flag = true;
+			disable_timer();
+			auto_move(4, P_CHEWY);
+			_G(spieler).R35CatEat = true;
+			start_spz_wait(CH_LGET_O, 1, ANI_VOR, P_CHEWY);
+			del_inventar(_G(spieler).AkInvent);
+			det->stop_detail(0);
+			det->del_static_ani(0);
+			start_detail_wait(1, 1, ANI_VOR);
+			det->start_detail(2, 1, ANI_VOR);
+			start_detail_wait(3, 1, ANI_VOR);
+			obj->show_sib(SIB_KNOCHEN_R35);
+			obj->calc_rsi_flip_flop(SIB_KNOCHEN_R35);
+			det->show_static_spr(7);
+			atds->del_steuer_bit(237, ATS_AKTIV_BIT, ATS_DATEI);
+
+			while (det->get_ani_status(2) && !SHOULD_QUIT) {
+				set_up_screen(DO_SETUP);
+			}
+
+			det->set_static_ani(0, -1);
+			enable_timer();
+			start_spz(CH_TALK3, 255, ANI_VOR, P_CHEWY);
+			start_aad_wait(95, -1);
 		}
-		det->set_static_ani(0, -1);
-		enable_timer();
-		start_spz(CH_TALK3, 255, ANI_VOR, P_CHEWY);
-		start_aad_wait(95, -1);
 	}
+
 	show_cur();
 	return action_flag;
 }
 
 void Room35::talk_cat() {
 	int16 dia_nr;
+	hide_cur();
 	auto_move(4, P_CHEWY);
+
 	if (_G(spieler).R35TransCat) {
 		if (!_G(spieler).R35CatEat) {
 			dia_nr = 7;
 		} else {
 			dia_nr = 8;
 		}
+
 		_G(spieler).PersonHide[P_CHEWY] = true;
 		switch_room(36);
+		show_cur();
 		start_ads_wait(dia_nr);
 		_G(spieler).PersonHide[P_CHEWY] = false;
 		switch_room(35);
+
 	} else {
 		start_spz(CH_TALK5, 255, ANI_VOR, P_CHEWY);
 		start_aad_wait(96, -1);
 	}
+
+	show_cur();
 }
 
 } // namespace Rooms
