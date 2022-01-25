837728c91aaeb3615e8ef5eb2c980206e671c155
Strangerke <arnaud.boutonne@gmail.com>
2022-01-25 23:39:55 +0100

    CHEWY: Fix some differences in the logic of Room67

--- engines/chewy/rooms/room67.cpp
+++ engines/chewy/rooms/room67.cpp
@@ -22,7 +22,6 @@
 #include "chewy/defines.h"
 #include "chewy/events.h"
 #include "chewy/global.h"
-#include "chewy/ani_dat.h"
 #include "chewy/room.h"
 #include "chewy/rooms/room67.h"
 
@@ -33,6 +32,7 @@ void Room67::entry() {
 	_G(spieler).ScrollxStep = 2;
 	SetUpScreenFunc = setup_func;
 	spieler_mi[P_HOWARD].Mode = true;
+	spieler_mi[P_NICHELLE].Mode = true;
 	_G(spieler).DiaAMov = 7;
 	if (_G(spieler).R67KommodeAuf)
 		det->show_static_spr(9);
@@ -44,9 +44,11 @@ void Room67::entry() {
 	if (!flags.LoadGame) {
 		hide_cur();
 		set_person_pos(102, 132, P_CHEWY, P_RIGHT);
-		set_person_pos(12, 100, P_HOWARD, P_RIGHT);
-		set_person_pos(47, 106, P_NICHELLE, P_RIGHT);
-		go_auto_xy(214, 112, P_NICHELLE, ANI_GO);
+		if (_G(spieler).PersonRoomNr[P_HOWARD] == 67) {
+			set_person_pos(12, 100, P_HOWARD, P_RIGHT);
+			set_person_pos(47, 106, P_NICHELLE, P_RIGHT);
+			go_auto_xy(214, 112, P_NICHELLE, ANI_GO);
+		}
 		auto_move(7, P_CHEWY);
 		show_cur();
 	}
@@ -54,14 +56,17 @@ void Room67::entry() {
 
 void Room67::xit() {
 	_G(spieler).ScrollxStep = 1;
-	_G(spieler).PersonRoomNr[P_HOWARD] = 66;
-	_G(spieler).PersonRoomNr[P_NICHELLE] = 66;
+
+	if (_G(spieler).PersonRoomNr[P_HOWARD] == 67) {
+		_G(spieler).PersonRoomNr[P_HOWARD] = 66;
+		_G(spieler).PersonRoomNr[P_NICHELLE] = 66;
+	}
 }
 
 void Room67::setup_func() {
-	int16 x, y;
 	calc_person_look();
-	y = 117;
+	const int16 y = 117;
+	int16 x;
 	if (spieler_vector[P_CHEWY].Xypos[0] > 320) {
 		x = 178;
 	} else {
@@ -79,7 +84,11 @@ int16 Room67::use_grammo() {
 		auto_move(6, P_CHEWY);
 		start_spz_wait(CH_LGET_O, 1, false, P_CHEWY);
 		det->start_detail(0, 255, ANI_VOR);
-		start_aad_wait(376, -1);
+		if (_G(spieler).PersonRoomNr[P_HOWARD] == 67)
+			start_aad_wait(376, -1);
+		else
+			start_aad_wait(614, -1);
+
 		start_spz_wait(CH_LGET_O, 1, false, P_CHEWY);
 		det->stop_detail(0);
 		show_cur();
@@ -129,46 +138,43 @@ void Room67::kostuem_aad(int16 aad_nr) {
 
 int16 Room67::talk_papagei() {
 	int16 action_flag = false;
-	if (!_G(spieler).R67PapageiWeg) {
-		if (!_G(spieler).inv_cur) {
-			action_flag = true;
-			hide_cur();
-			room->set_timer_status(1, TIMER_STOP);
-			if (menu_item == CUR_HOWARD) {
-				show_cur();
-
-				ssi[2].X = 270 - _G(spieler).scrollx;
-				ssi[2].Y = 10;
-				atds->set_split_win(2, &ssi[2]);
-				start_ads_wait(19);
-				room->set_timer_status(1, TIMER_START);
-			} else if (menu_item == CUR_NICHELLE) {
-				start_aad_wait(380, -1);
-				room->set_timer_status(1, TIMER_START);
-			} else if (menu_item == CUR_TALK) {
-				auto_move(5, P_CHEWY);
-				show_cur();
-
-				ssi[2].X = 270 - _G(spieler).scrollx;
-				ssi[2].Y = 10;
-				atds->set_split_win(2, &ssi[2]);
-				start_ads_wait(18);
-				room->set_timer_status(1, TIMER_START);
-			} else if (menu_item == CUR_USE) {
-				hide_cur();
-				action_flag = true;
-				auto_move(4, P_CHEWY);
-				start_spz_wait(CH_LGET_O, 1, false, P_CHEWY);
-				_G(spieler).R67PapageiWeg = true;
-				det->stop_detail(1);
-				det->del_static_ani(1);
-				det->show_static_spr(0);
-				invent_2_slot(PAPAGEI_INV);
-				show_cur();
-				atds->set_steuer_bit(394, ATS_AKTIV_BIT, ATS_DATEI);
-			}
+	if (!_G(spieler).R67PapageiWeg && !_G(spieler).inv_cur) {
+		action_flag = true;
+		hide_cur();
+		room->set_timer_status(1, TIMER_STOP);
+		if (menu_item == CUR_HOWARD) {
 			show_cur();
+
+			ssi[2].X = 270 - _G(spieler).scrollx;
+			ssi[2].Y = 10;
+			atds->set_split_win(2, &ssi[2]);
+			start_ads_wait(19);
+			room->set_timer_status(1, TIMER_START);
+		} else if (menu_item == CUR_NICHELLE) {
+			start_aad_wait(380, -1);
+			room->set_timer_status(1, TIMER_START);
+		} else if (menu_item == CUR_TALK) {
+			auto_move(5, P_CHEWY);
+			show_cur();
+
+			ssi[2].X = 270 - _G(spieler).scrollx;
+			ssi[2].Y = 10;
+			atds->set_split_win(2, &ssi[2]);
+			start_ads_wait(18);
+			room->set_timer_status(1, TIMER_START);
+		} else if (menu_item == CUR_USE) {
+			hide_cur();
+			auto_move(4, P_CHEWY);
+			start_spz_wait(CH_LGET_O, 1, false, P_CHEWY);
+			_G(spieler).R67PapageiWeg = true;
+			det->stop_detail(1);
+			det->del_static_ani(1);
+			det->show_static_spr(0);
+			invent_2_slot(PAPAGEI_INV);
+			show_cur();
+			atds->set_steuer_bit(394, ATS_AKTIV_BIT, ATS_DATEI);
 		}
+		show_cur();
 	}
 	return action_flag;
 }
