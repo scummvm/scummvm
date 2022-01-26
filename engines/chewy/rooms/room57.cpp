aa44c787863ede2dd1561c3df8a5307c021f4800
Strangerke <arnaud.boutonne@gmail.com>
2022-01-26 23:27:15 +0100

    CHEWY: Fix logic in Room 57

--- engines/chewy/rooms/room57.cpp
+++ engines/chewy/rooms/room57.cpp
@@ -22,7 +22,6 @@
 #include "chewy/defines.h"
 #include "chewy/events.h"
 #include "chewy/global.h"
-#include "chewy/ani_dat.h"
 #include "chewy/room.h"
 #include "chewy/rooms/room57.h"
 
@@ -39,18 +38,17 @@ void Room57::entry() {
 	spieler_mi[P_HOWARD].Mode = true;
 	if (_G(spieler).R57StudioAuf)
 		det->hide_static_spr(4);
-	if (!flags.LoadGame) {
-		if (_G(spieler).R48TaxiEntry) {
-			hide_cur();
-			_G(spieler).R48TaxiEntry = false;
-			_G(spieler).scrollx = 0;
-			_G(spieler).scrolly = 0;
-			set_person_pos(4, 144, P_HOWARD, P_LEFT);
-			set_person_pos(40, 160, P_CHEWY, P_RIGHT);
-			auto_move(2, P_CHEWY);
-			_G(maus_links_click) = false;
-			show_cur();
-		}
+	
+	if (!flags.LoadGame &&_G(spieler).R48TaxiEntry) {
+		hide_cur();
+		_G(spieler).R48TaxiEntry = false;
+		_G(spieler).scrollx = 0;
+		_G(spieler).scrolly = 0;
+		set_person_pos(4, 144, P_HOWARD, P_LEFT);
+		set_person_pos(40, 160, P_CHEWY, P_RIGHT);
+		auto_move(2, P_CHEWY);
+		_G(maus_links_click) = false;
+		show_cur();
 	}
 }
 
@@ -63,11 +61,10 @@ void Room57::xit(int16 eib_nr) {
 }
 
 void Room57::setup_func() {
-	int16 x, y;
-	int16 ch_y;
 	if (_G(spieler).PersonRoomNr[P_HOWARD] == 57) {
 		calc_person_look();
-		ch_y = spieler_vector[P_CHEWY].Xypos[1];
+		const int16 ch_y = spieler_vector[P_CHEWY].Xypos[1];
+		int16 x, y;
 		if (ch_y < 145) {
 			x = 176;
 			y = 126;
@@ -85,20 +82,26 @@ int16 Room57::use_taxi() {
 		action_ret = true;
 		hide_cur();
 		auto_move(3, P_CHEWY);
+		det->enable_sound(3, 0);
+		det->play_sound(3, 0);
 		det->show_static_spr(7);
 		go_auto_xy(16, 160, P_CHEWY, ANI_WAIT);
 		_G(spieler).PersonHide[P_CHEWY] = true;
 		_G(spieler).R48TaxiPerson[P_CHEWY] = true;
 		if (_G(spieler).PersonRoomNr[P_HOWARD] == 57) {
+			SetUpScreenFunc = nullptr;
 			go_auto_xy(11, 144, P_HOWARD, ANI_WAIT);
 			_G(spieler).PersonHide[P_HOWARD] = true;
 			_G(spieler).R48TaxiPerson[P_HOWARD] = true;
 			_G(spieler).PersonRoomNr[P_HOWARD] = 48;
 		}
 		det->hide_static_spr(7);
+		det->enable_sound(3, 1);
+		det->play_sound(3, 1);
 		room->set_timer_status(3, TIMER_STOP);
 		det->del_static_ani(3);
 		start_detail_wait(5, 1, ANI_VOR);
+		det->disable_sound(5, 0);
 		switch_room(48);
 	}
 	return action_ret;
@@ -113,8 +116,13 @@ int16 Room57::use_pfoertner() {
 	auto_move(1, P_CHEWY);
 	if (is_cur_inventar(CUTMAG_INV)) {
 		action_ret = true;
-		start_aad_wait(339, -1);
-		new_invent_2_cur(BESTELL_INV);
+		if (_G(spieler).flags37_10)
+			start_aad_wait(596, -1);
+		else {
+			_G(spieler).flags37_10 = true;
+			start_aad_wait(339, -1);
+			new_invent_2_cur(BESTELL_INV);
+		}
 	} else if (is_cur_inventar(JMKOST_INV)) {
 		action_ret = true;
 		start_aad_wait(340, -1);
@@ -130,6 +138,7 @@ int16 Room57::use_pfoertner() {
 			_G(spieler).room_e_obj[91].Attribut = AUSGANG_OBEN;
 			det->hide_static_spr(4);
 			start_detail_wait(6, 1, ANI_WAIT);
+			det->disable_sound(6, 0);
 			atds->set_steuer_bit(358, ATS_AKTIV_BIT, ATS_DATEI);
 		} else {
 			start_aad_wait(349, -1);
@@ -144,12 +153,12 @@ int16 Room57::use_pfoertner() {
 }
 
 void Room57::talk_pfoertner() {
-	int16 aad_nr;
 	hide_cur();
 	auto_move(1, P_CHEWY);
 	room->set_timer_status(1, TIMER_STOP);
 	det->del_static_ani(1);
 	det->set_static_ani(3, -1);
+	int16 aad_nr;
 	if (!_G(spieler).R57StudioAuf) {
 		aad_nr = 338;
 	} else
