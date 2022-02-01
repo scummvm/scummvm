a78b3d32cfb4c65e83039258807636ecdf6e76b0
Coen Rampen <crampen@gmail.com>
2022-02-01 17:15:30 +0100

    SAGA: Update ItE detection entry info

--- engines/saga/detection_tables.h
+++ engines/saga/detection_tables.h
@@ -557,12 +557,12 @@ static const SAGAGameDescription gameDescriptions[] = {
 		ITEPatch_Files,
 	},
 
-	// Inherit the earth - Windows/Linux/DOS CD version
-	// FIXME Unclear which version(s) this matches - Windows and Linux versions have separate entries above.
+	// Inherit the earth - DOS CD version - 17-06-1994
+	// Reported by eriktorbjorn
 	{
 		{
 			"ite",
-			"Windows/Linux/DOS CD Version",
+			"DOS CD Version 1",
 			{
 				{"ite.rsc",		GAME_RESOURCEFILE,	"8f4315a9bb10ec839253108a032c8b54", 8928678},
 				{"scripts.rsc",	GAME_SCRIPTFILE,	"a891405405edefc69c9d6c420c868b84", -1},
@@ -632,12 +632,11 @@ static const SAGAGameDescription gameDescriptions[] = {
 		ITEPatch_Files,
 	},
 
-	// Inherit the earth - Italian Windows/Linux/DOS CD version (fan translation)
-	// FIXME Unclear which version(s) this matches - Windows and Linux versions have separate entries above.
+	// Inherit the earth - Italian DOS CD version (fan translation)
 	{
 		{
 			"ite",
-			"Windows/Linux/DOS CD Version",
+			"DOS CD Version",
 			{
 				{"ite.rsc",		GAME_RESOURCEFILE,	"8f4315a9bb10ec839253108a032c8b54", 8929956},
 				{"scripts.rsc",	GAME_SCRIPTFILE,	"a891405405edefc69c9d6c420c868b84", 350243},
@@ -707,12 +706,11 @@ static const SAGAGameDescription gameDescriptions[] = {
 		ITEPatch_Files,
 	},
 
-	// Inherit the earth - French Windows/Linux/DOS CD version (fan translation)
-	// FIXME Unclear which version(s) this matches - Windows and Linux versions have separate entries above.
+	// Inherit the earth - French DOS CD version (fan translation)
 	{
 		{
 			"ite",
-			"Windows/Linux/DOS CD Version",
+			"DOS CD Version",
 			{
 				{"ite.rsc",		GAME_RESOURCEFILE,	"8f4315a9bb10ec839253108a032c8b54", 8929384},
 				{"scripts.rsc",	GAME_SCRIPTFILE,	"a891405405edefc69c9d6c420c868b84", 355877},
@@ -732,11 +730,11 @@ static const SAGAGameDescription gameDescriptions[] = {
 		ITEPatch_Files,
 	},
 
-	// Inherit the earth - DOS CD version
+	// Inherit the earth - DOS CD version - 30-06-1994
 	{
 		{
 			"ite",
-			"CD Version",
+			"DOS CD Version 2",
 			{
 				{"ite.rsc",		GAME_RESOURCEFILE,	"8f4315a9bb10ec839253108a032c8b54", -1},
 				{"scripts.rsc",	GAME_SCRIPTFILE,	"50a0d2d7003c926a3832d503c8534e90", -1},
