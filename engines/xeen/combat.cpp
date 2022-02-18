f9a2f7750bee2134eb351f002de75863650337ac
Paul Gilbert <dreammaster@scummvm.org>
2022-02-17 20:08:42 -0800

    XEEN: More properly fixed the item drop range checks

--- engines/xeen/combat.cpp
+++ engines/xeen/combat.cpp
@@ -101,7 +101,11 @@ static const int POW_WEAPON_VOCS[35] = {
 	5, 5, 5, 1, 3, 2, 5, 1, 1, 1, 0, 0, 0, 2, 2
 };
 
-static const int MONSTER_ITEM_RANGES[7] = { 10, 20, 50, 100, 100, 100, 0 };
+static const int MONSTER_ITEM_RANGES[21] = {
+	10, 20, 50, 100, 100, 100, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0
+};
 
 #define monsterSavingThrow(MONINDEX) (_vm->getRandomNumber(1, 50 + (MONINDEX)) <= (MONINDEX))
 
--- engines/xeen/map.cpp
+++ engines/xeen/map.cpp
@@ -123,6 +123,7 @@ void MonsterStruct::synchronize(Common::SeekableReadStream &s) {
 	_gold = s.readUint16LE();
 	_gems = s.readByte();
 	_itemDrop = s.readByte();
+	assert(_itemDrop >= 0 && _itemDrop <= 20);
 	_flying = s.readByte() != 0;
 	_imageNumber = s.readByte();
 	_loopAnimation = s.readByte();
