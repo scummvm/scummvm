72e3090a5cd69b76d5ae86a15031b153a196f4f7
Thierry Crozat <criezy@scummvm.org>
2022-02-23 09:10:54 +0000

    AGS: Update POTFILES
    The code for the engine options dialog for AGS was emoved from
    detection.cpp to its own file a while back but we forgot to update
    the POTFILES, so that dialog was no longer translated.
    
    Also remove an include for translation.h in detection.cpp as it is
    no longer needed.

--- engines/ags/detection.cpp
+++ engines/ags/detection.cpp
@@ -24,7 +24,6 @@
 #include "common/file.h"
 #include "common/md5.h"
 #include "common/str-array.h"
-#include "common/translation.h"
 #include "common/util.h"
 #include "ags/detection.h"
 #include "ags/detection_tables.h"
