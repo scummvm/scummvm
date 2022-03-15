1a3e55a257c595f1332c9faaf4dd43a41084ae71
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2022-03-15 22:55:17 +0000

    AGDS: fix tile object opcode used with no resource

--- engines/agds/process.cpp
+++ engines/agds/process.cpp
@@ -32,7 +32,7 @@ Process::Process(AGDSEngine *engine, ObjectPtr object, unsigned ip) :
 	_engine(engine), _parentScreen(engine->getCurrentScreenName()), _object(object),
 	_ip(ip), _lastIp(ip),
 	_exited(false), _status(kStatusActive), _exitCode(kExitCodeDestroy),
-	_tileWidth(16), _tileHeight(16), _tileResource(0), _tileIndex(0),
+	_tileWidth(16), _tileHeight(16), _tileResource(-1), _tileIndex(0),
 	_timer(0),
 	_animationCycles(1), _animationLoop(false), _animationZ(0), _animationDelay(-1), _animationRandom(0),
 	_phaseVarControlled(false), _animationSpeed(100),
