52549c1678332786c01ebe47aedc4e6577621bff
Torbjörn Andersson <eriktorbjorn@users.sourceforge.net>
2022-03-17 19:17:30 +0100

    SCUMM: Make correcting position of green tank in Sam & Max optional

--- engines/scumm/detection.cpp
+++ engines/scumm/detection.cpp
@@ -229,7 +229,7 @@ const ExtraGuiOptions ScummMetaEngineDetection::getExtraGuiOptions(const Common:
 	const Common::String guiOptions = parseGameGUIOptions(guiOptionsString);
 	const Common::Platform platform = Common::parsePlatform(ConfMan.get("platform", target));
 
-	if (target.empty() || gameid == "monkey" || gameid == "monkey2") {
+	if (target.empty() || gameid == "monkey" || gameid == "monkey2" || gameid == "samnmax") {
 		options.push_back(enableEnhancements);
 	}
 
