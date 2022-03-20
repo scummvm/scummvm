f3542bc2d62cfb96f9e62d1877228f0961ca2497
Torbjörn Andersson <eriktorbjorn@users.sourceforge.net>
2022-03-20 10:50:47 +0100

    SCUMM: Read the "enable_enhancements" setting only once
    I can't change during gameplay, it's shorter to type, and we won't
    accidentally involve the config manater in some tight loop.

--- engines/scumm/actor.cpp
+++ engines/scumm/actor.cpp
@@ -20,7 +20,6 @@
  */
 
 #include "common/system.h"	// for setFocusRectangle/clearFocusRectangle
-#include "common/config-manager.h"
 #include "scumm/scumm.h"
 #include "scumm/actor.h"
 #include "scumm/actor_he.h"
@@ -1610,7 +1609,7 @@ void Actor::putActor(int dstX, int dstY, int newRoom) {
 	// WORKAROUND: The green transparency of the tank in the Hall of Oddities
 	// is positioned one pixel too far to the left. This appears to be a bug
 	// in the original game as well.
-	if (_vm->_game.id == GID_SAMNMAX && newRoom == 16 && _number == 5 && dstX == 235 && dstY == 236 && ConfMan.getBool("enable_enhancements"))
+	if (_vm->_game.id == GID_SAMNMAX && newRoom == 16 && _number == 5 && dstX == 235 && dstY == 236 && _vm->_enableEnhancements)
 		dstX++;
 
 	_pos.x = dstX;
