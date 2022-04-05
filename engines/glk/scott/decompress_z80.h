94d3667a1c374d1612f5a5054d62c824b8a9d48f
Avijeet <am388488@gmail.com>
2022-04-05 17:30:37 +0530

    GLK SCOTT: Implement z80 decompression

--- engines/glk/scott/decompress_z80.h
+++ engines/glk/scott/decompress_z80.h
@@ -1,8 +1,13 @@
 #ifndef DECOMPRESSZ80_H
 #define DECOMPRESSZ80_H
 
+#include "common/scummsys.h"
+
 namespace Glk {
 namespace Scott {
+
+// Will return nullptr on error or 0xc000 (49152) bytes of uncompressed raw data on success
+uint8_t *decompressZ80(uint8_t *rawData, size_t length);
 
 } // End of namespace Scott
 } // End of namespace Glk
