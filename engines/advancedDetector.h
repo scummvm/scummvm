726f187442d66f97fbe96df7fcd8e1cffe039ae6
Eugene Sandulenko <sev@scummvm.org>
2022-04-06 00:38:17 +0200

    JANITORIAL: Fix code formatting

--- engines/advancedDetector.h
+++ engines/advancedDetector.h
@@ -386,7 +386,7 @@ public:
 
 	static Common::StringArray getPathsFromEntry(const ADGameDescription *g);
 
-	uint getMD5Bytes() const {return _md5Bytes;}
+	uint getMD5Bytes() const { return _md5Bytes; }
 
 protected:
 	/**
