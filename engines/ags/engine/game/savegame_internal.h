9b5f0c6221ff030b52949345372ffa1596202cf5
Paul Gilbert <dreammaster@scummvm.org>
2022-04-10 16:08:16 -0700

    AGS: Fixed default values for restoring of audiochannels
    From upstream 970e023af4db037e2fe24488e583b9dd3ad935aa

--- engines/ags/engine/game/savegame_internal.h
+++ engines/ags/engine/game/savegame_internal.h
@@ -91,7 +91,7 @@ struct RestoredData {
 	int                     CursorMode;
 	// General audio
 	struct ChannelInfo {
-		int ClipID = 0;
+		int ClipID = -1;
 		int Pos = 0;
 		int Priority = 0;
 		int Repeat = 0;
