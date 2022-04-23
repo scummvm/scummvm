229676c239403047fa5a474dba85b4af2bad5dbc
Paul Gilbert <dreammaster@scummvm.org>
2022-04-22 20:26:39 -0700

    AGS: Skip WaitMouse/Key* when skipping cutscene
    From upstream 587214734100d43348bfe6433e8c88b5df5c4915

--- engines/ags/engine/ac/global_game.cpp
+++ engines/ags/engine/ac/global_game.cpp
@@ -771,6 +771,10 @@ void SetGraphicalVariable(const char *varName, int p_value) {
 }
 
 int WaitImpl(int skip_type, int nloops) {
+	// if skipping cutscene and expecting user input: don't wait at all
+	if (_GP(play).fast_forward && ((skip_type & ~SKIP_AUTOTIMER) != 0))
+		return 0;
+
 	_GP(play).wait_counter = nloops;
 	_GP(play).wait_skipped_by = SKIP_NONE;
 	_GP(play).wait_skipped_by = SKIP_AUTOTIMER; // we set timer flag by default to simplify that case
