6b1c43f90b3102baaa1ac2db6ed66faa5f156974
Paul Gilbert <dreammaster@scummvm.org>
2022-04-24 10:37:36 -0700

    AGS: Do explicit type cast in FileReadRawChar and FileWriteRawChar
    From upstream e3eb59bd4d72148a762ac93d0df36a56e101f257

--- engines/ags/engine/ac/global_file.cpp
+++ engines/ags/engine/ac/global_file.cpp
@@ -185,9 +185,9 @@ int FileReadInt(int32_t handle) {
 
 int8 FileReadRawChar(int32_t handle) {
 	Stream *in = get_valid_file_stream_from_handle(handle, "FileReadRawChar");
-	if (in->EOS())
-		return -1;
-	return in->ReadInt8();
+	return static_cast<uint8_t>(in->ReadByte());
+	// NOTE: this function has incorrect return value for historical reasons;
+	// we keep this strictly for backwards compatibility with old scripts
 }
 
 int FileReadRawInt(int32_t handle) {
@@ -202,7 +202,7 @@ void FileWriteRawChar(int32_t handle, int chartoWrite) {
 	if ((chartoWrite < 0) || (chartoWrite > 255))
 		debug_script_warn("!FileWriteRawChar: can only write values 0-255");
 
-	out->WriteInt8(chartoWrite);
+	out->WriteByte(static_cast<uint8_t>(chartoWrite));
 }
 
 } // namespace AGS3
