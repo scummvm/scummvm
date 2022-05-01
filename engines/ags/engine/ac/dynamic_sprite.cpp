3b91a1124f0467bbab39d9b163049d87540ae18d
Paul Gilbert <dreammaster@scummvm.org>
2022-05-01 16:33:30 -0700

    AGS: Made internal BitmapFlip enum match the script, for convenience
    From upstream 8be85c08ed87a56265d2f18fb798ec463edb3ef7

--- engines/ags/engine/ac/dynamic_sprite.cpp
+++ engines/ags/engine/ac/dynamic_sprite.cpp
@@ -127,13 +127,8 @@ void DynamicSprite_Flip(ScriptDynamicSprite *sds, int direction) {
 	// resize the sprite to the requested size
 	Bitmap *newPic = BitmapHelper::CreateTransparentBitmap(_GP(game).SpriteInfos[sds->slot].Width, _GP(game).SpriteInfos[sds->slot].Height, _GP(spriteset)[sds->slot]->GetColorDepth());
 
-	if (direction == 1)
-		newPic->FlipBlt(_GP(spriteset)[sds->slot], 0, 0, Shared::kBitmap_HFlip);
-	else if (direction == 2)
-		newPic->FlipBlt(_GP(spriteset)[sds->slot], 0, 0, Shared::kBitmap_VFlip);
-	else if (direction == 3)
-		newPic->FlipBlt(_GP(spriteset)[sds->slot], 0, 0, Shared::kBitmap_HVFlip);
-
+	// AGS script FlipDirection corresponds to internal BitmapFlip
+	newPic->FlipBlt(_GP(spriteset)[sds->slot], 0, 0, static_cast<BitmapFlip>(direction));
 	delete _GP(spriteset)[sds->slot];
 
 	// replace the bitmap in the sprite set
