089064327911ee82da8e73204a458176d3d5dd89
Andrea Boscarino <andywinxp@gmail.com>
2022-05-05 23:45:48 +0200

    SCUMM: INSANE: Set correct video framerate

--- engines/scumm/insane/insane.cpp
+++ engines/scumm/insane/insane.cpp
@@ -629,7 +629,7 @@ void Insane::startVideo(const char *filename, int num, int argC, int frameRate,
 		smush_setupSanFromStart(filename, 0, -1, -1, 0);
 	}
 
-	_player->play(filename, _speed, offset, startFrame);
+	_player->play(filename, frameRate, offset, startFrame);
 }
 
 void Insane::smush_warpMouse(int x, int y, int buttons) {
