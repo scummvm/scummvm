scummvm/scummvm

commit 70d1dbb00f8afadc7e66d61c93efa0e48286ffbd

TINSEL: Add barebones Notebook subsystem
And implement the BOOKADDHYPERLINK libcall.
The subsystem contains known fields and constants.

engines/tinsel/dialogs.h
@@ -86,8 +86,11 @@ enum InvCursorFN { IC_AREA,
 #define PERMACONV 0x20
 #define CONVENDITEM 0x40
 // Noir only
+#define V3ATTR_X80 0x80
 #define V3ATTR_X200 0x200
 #define V3ATTR_X400 0x400
+#define NOTEBOOK_TITLE 0x800 // is a notebook title
+#define V3ATTR_X2000 0x2000
 
 #define sliderRange (_sliderYmax - _sliderYmin)
 #define MAXSLIDES 4
@@ -124,6 +127,10 @@ struct INV_OBJECT {
 	SCNHANDLE hIconFilm;	// inventory objects animation film
 	SCNHANDLE hScript;	// inventory objects event handling script
 	int32 attribute;		// inventory object's attribute
+
+	// Noir
+	int32 unknown;
+	int32 title;	// id of associated notebook title
 };
 
 struct INV_DEF {

engines/tinsel/noir/notebook.cpp
@@ -1,0 +1,62 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "tinsel/noir/notebook.h"
+
+#include "tinsel/dialogs.h"
+
+namespace Tinsel {
+
+void Notebook::AddHyperlink(int32 id1, int32 id2) {
+	INV_OBJECT *invObject = _vm->_dialogs->GetInvObject(id1);
+	if (invObject->title != 0) {
+		error("A clue can only be hyperlinked if it only has one title!");
+		return;
+	}
+
+	invObject = _vm->_dialogs->GetInvObject(id2);
+	if (invObject->title != 0) {
+		error("A clue can only be hyperlinked if it only has one title!");
+		return;
+	}
+
+	uint32 i;
+	for (i = 0; i < MAX_HYPERS; ++i) {
+		int32 curr_id1 = _hyperlinks[i].id1;
+		if (curr_id1 == 0) {
+			_hyperlinks[i].id1 = id1;
+			_hyperlinks[i].id2 = id2;
+			return;
+		}
+
+		if ((curr_id1 == id1) || (id1 == _hyperlinks[i].id2)) {
+			if ((curr_id1 != id2) && (id2 != _hyperlinks[i].id2)) {
+				error("A clue/title can only be hyperlinked to one other clue/title!");
+			}
+			return;
+		}
+	}
+
+	// No free hyperlink entry was found
+	error("Too many hyperlinks");
+}
+
+} // End of namespace Tinsel

engines/tinsel/module.mk
@@ -46,7 +46,8 @@ MODULE_OBJS := \
 	timers.o \
 	tinlib.o \
 	tinsel.o \
-	token.o
+	token.o \
+	noir/notebook.o \
 
 # This module can be built as a plugin
 ifeq ($(ENABLE_TINSEL), DYNAMIC_PLUGIN)

engines/tinsel/noir/notebook.h
@@ -1,0 +1,83 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef TINSEL_NOTEBOOK_H // prevent multiple includes
+#define TINSEL_NOTEBOOK_H
+
+#include "common/scummsys.h"
+
+#include "tinsel/events.h"
+
+namespace Tinsel {
+// links two clue/title objects together
+struct HYPERLINK {
+	int32 id1;
+	int32 id2;
+};
+
+// 6 bytes large
+struct ENTRY {
+	int32 id;
+	bool active;
+	int32 page1;
+	int32 indexOnPage1;
+	int32 page2;
+	int32 indexOnPage2;
+};
+
+enum class BOOKSTATE {
+	CLOSED = 0,
+	OPEN_UNKNOWN = 1,
+	OPEN_ANIMATING = 2,
+	OPENED = 3
+};
+
+class Notebook {
+public:
+	Notebook() = default;
+
+	// can be a title or clue
+	void AddEntry(int32 entryIdx, int32 page1, int32 page2);
+	// Adds a connection between a clue/title
+	void AddHyperlink(int32 id1, int32 id2);
+	// Called within InventoryProcess loop
+	void Redraw();
+	// Called by EventToInventory
+	void EventToNotebook(PLR_EVENT event, bool p2, bool p3);
+
+private:
+	const static uint32 MAX_ENTRIES = 100;
+	const static uint32 MAX_PAGES = 0x15;
+	const static uint32 MAX_HYPERS = 0xf;
+	const static uint32 MAX_ENTRIES_PER_PAGE = 8;
+
+	HYPERLINK _hyperlinks[MAX_HYPERS];
+
+	const static uint32 _numEntries = 0;
+
+	ENTRY _entries[MAX_ENTRIES];
+
+	BOOKSTATE _state;
+};
+
+} // End of namespace Tinsel
+
+#endif

engines/tinsel/tinlib.cpp
@@ -65,6 +65,7 @@
 #include "tinsel/tinlib.h"
 #include "tinsel/tinsel.h"
 #include "tinsel/token.h"
+#include "tinsel/noir/notebook.h"
 
 #include "common/textconsole.h"
 
@@ -152,7 +153,8 @@ enum MASTER_LIB_CODES {
 	TRYPLAYSAMPLE, UNDIMMUSIC, UNHOOKSCENE, UNTAGACTOR, VIBRATE, WAITFRAME, WAITKEY,
 	WAITSCROLL, WAITTIME, WALK, WALKED, WALKEDPOLY, WALKEDTAG, WALKINGACTOR, WALKPOLY,
 	WALKTAG, WALKXPOS, WALKYPOS, WHICHCD, WHICHINVENTORY, ZZZZZZ, DEC3D, DECINVMAIN,
-	ADDNOTEBOOK, ADDINV3, ADDCONV, SET3DTEXTURE, FADEMUSIC, VOICEOVER, SETVIEW, HELDOBJECTORTOPIC, HIGHEST_LIBCODE
+	ADDNOTEBOOK, ADDINV3, ADDCONV, SET3DTEXTURE, FADEMUSIC, VOICEOVER, SETVIEW,
+	HELDOBJECTORTOPIC, BOOKADDHYPERLINK, HIGHEST_LIBCODE
 };
 
 static const MASTER_LIB_CODES DW1DEMO_CODES[] = {
@@ -4647,8 +4649,7 @@ NoirMapping translateNoirLibCode(int libCode, int32 *pp) {
 		debug(7, "%s(0x%08X, 0x%08X, 0x%08X)", mapping.name, pp[0], pp[1], pp[2]);
 		break;
 	case 86: // 2 parameters
-		warning("TODO: Implement notebook_add_hyperlink");
-		mapping = NoirMapping{"NODEBOOKADDHYPERLINK", ZZZZZZ, 2};
+		mapping = NoirMapping{"BOOKADDHYPERLINK", BOOKADDHYPERLINK, 2};
 		pp -= mapping.numArgs - 1;
 		debug(7, "%s(0x%08X, 0x%08X)", mapping.name, pp[0], pp[1]);
 		break;
@@ -5417,6 +5418,12 @@ int CallLibraryRoutine(CORO_PARAM, int operand, int32 *pp, const INT_CONTEXT *pi
 		// Common to DW1 / DW2 / Noir
 		startBackground(coroParam, pp[0]);
 		return -1;
+
+	case BOOKADDHYPERLINK:
+		// Noir
+		pp -= 1; // 2 parameters
+		_vm->_notebook->AddHyperlink(pp[0], pp[1]);
+		return -2;
 
 	case BLOCKING:
 		// DW2 only

engines/tinsel/tinsel.cpp
@@ -59,6 +59,7 @@
 #include "tinsel/sysvar.h"
 #include "tinsel/timers.h"
 #include "tinsel/tinsel.h"
+#include "tinsel/noir/notebook.h"
 
 namespace Tinsel {
 
@@ -1012,6 +1013,10 @@ Common::Error TinselEngine::run() {
 	_handle = new Handle();
 	_scroll = new Scroll();
 	_dialogs = new Dialogs();
+
+	if (TinselVersion == 3) {
+		_notebook = new Notebook();
+	}
 
 	// Initialize backend
 	if (getGameID() == GID_NOIR) {

engines/tinsel/tinsel.h
@@ -64,6 +64,7 @@ class Actor;
 class Handle;
 class Scroll;
 class Dialogs;
+class Notebook;
 
 typedef Common::List<Common::Rect> RectList;
 
@@ -175,6 +176,7 @@ public:
 	Config *_config;
 	Scroll *_scroll;
 	Dialogs *_dialogs;
+	Notebook *_notebook = nullptr;
 
 	KEYFPTR _keyHandler;
 

commit 0b84a042d5362e133bfb9cb36c07b80887b8dfbc

TINSEL: Fix regression with inventory objects in DW1 and DW2
Regression caused by 70d1dbb00f8afadc7e66d61c93efa0e48286ffbd.

This change will break the inventory changes done for DW3, and will
need to be fixed accordingly for it.

DW3 has two new fields in its INV_OBJECT struct. However, there are
variables in the code that are cast from memory blobs, so adding
these new fields breaks DW1 and DW2. We'll either need to add a new
structure for DW3, or read the struct members individually instead of
casting the memory blobs to this struct.

engines/tinsel/dialogs.h
@@ -150,9 +150,15 @@ struct INV_OBJECT {
 	SCNHANDLE hScript;	// inventory objects event handling script
 	int32 attribute;		// inventory object's attribute
 
+	// TODO: Commented out because there are variables
+	// with this struct type that are cast from memory blobs,
+	// so this breaks DW1 and DW2. We need to read these
+	// struct members individually instead of casting the blobs
+	// to this struct
+
 	// Noir
-	int32 unknown;
-	int32 title;	// id of associated notebook title
+	//int32 unknown;
+	//int32 title;	// id of associated notebook title
 };
 
 struct INV_DEF {

engines/tinsel/noir/notebook.cpp
@@ -27,16 +27,22 @@ namespace Tinsel {
 
 void Notebook::AddHyperlink(int32 id1, int32 id2) {
 	INV_OBJECT *invObject = _vm->_dialogs->GetInvObject(id1);
+
+#if 0
 	if (invObject->title != 0) {
 		error("A clue can only be hyperlinked if it only has one title!");
 		return;
 	}
+#endif
 
 	invObject = _vm->_dialogs->GetInvObject(id2);
+
+#if 0
 	if (invObject->title != 0) {
 		error("A clue can only be hyperlinked if it only has one title!");
 		return;
 	}
+#endif
 
 	uint32 i;
 	for (i = 0; i < MAX_HYPERS; ++i) {