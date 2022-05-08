d30e952802c196aea0459418962533408485ccc4
antoniou79 <a.antoniou79@gmail.com>
2022-05-08 19:04:49 +0300

    BLADERUNNER: Mute music and sfx for on-demand outtakes

--- engines/bladerunner/bladerunner.cpp
+++ engines/bladerunner/bladerunner.cpp
@@ -1219,13 +1219,21 @@ void BladeRunnerEngine::gameTick() {
 		//      Fixing this (storing ambient sounds and restoring them after the outtake has finished)
 		//      is too cumbersome to be worth it.
 		int ambientSoundsPreOuttakeVol = _mixer->getVolumeForSoundType(_mixer->kPlainSoundType);
+		int musicPreOuttakeVol = _mixer->getVolumeForSoundType(_mixer->kMusicSoundType);
+		int sfxPreOuttakeVol = _mixer->getVolumeForSoundType(_mixer->kSFXSoundType);
+		// Speech Sound Type (kSpeechSoundType) is the volume of the outtake video,
+		// so we don't mute that one
+		_mixer->setVolumeForSoundType(_mixer->kMusicSoundType, 0);
 		_mixer->setVolumeForSoundType(_mixer->kPlainSoundType, 0);
+		_mixer->setVolumeForSoundType(_mixer->kSFXSoundType, 0);
 		if (_debugger->_dbgPendingOuttake.outtakeId == -1 && _debugger->_dbgPendingOuttake.container < -1) {
 			outtakePlay(_debugger->_dbgPendingOuttake.externalFilename, _debugger->_dbgPendingOuttake.notLocalized, _debugger->_dbgPendingOuttake.container);
 		} else {
 			outtakePlay(_debugger->_dbgPendingOuttake.outtakeId, _debugger->_dbgPendingOuttake.notLocalized, _debugger->_dbgPendingOuttake.container);
 		}
+		_mixer->setVolumeForSoundType(_mixer->kSFXSoundType, sfxPreOuttakeVol);
 		_mixer->setVolumeForSoundType(_mixer->kPlainSoundType, ambientSoundsPreOuttakeVol);
+		_mixer->setVolumeForSoundType(_mixer->kMusicSoundType, musicPreOuttakeVol);
 		_debugger->resetPendingOuttake();
 	}
 
