5942e87fb319013212a93c7a2208953f6c49599c
Thierry Crozat <criezy@scummvm.org>
2022-05-22 23:54:32 +0100

    BUILD: Add the original LDFLAGS and sanitizer LDFLAGS to PLUGIN_LDFLAGS
    Adding the sanitizer flags to the PLUGIN_LDFLAGS fixes link errors
    for the plugins when asan, tsan, or ubsan is enabled.
    
    Adding the original LDFLAGS to PLUGIN_LDFLAGS means we no longer needs
    to use both in the link command for plugins.

--- rules.mk
+++ rules.mk
@@ -48,7 +48,7 @@ ifdef PLUGIN
 PLUGIN-$(MODULE) := plugins/$(PLUGIN_PREFIX)$(notdir $(MODULE))$(PLUGIN_SUFFIX)
 $(PLUGIN-$(MODULE)): $(MODULE_OBJS-$(MODULE)) $(PLUGIN_EXTRA_DEPS)
 	$(QUIET)$(MKDIR) plugins
-	+$(QUIET_PLUGIN)$(LD) $(SAVED_LDFLAGS) $(filter-out $(PLUGIN_EXTRA_DEPS),$+) $(PLUGIN_LDFLAGS) -o $@
+	+$(QUIET_PLUGIN)$(LD) $(filter-out $(PLUGIN_EXTRA_DEPS),$+) $(PLUGIN_LDFLAGS) -o $@
 
 # Reset PLUGIN var
 PLUGIN:=
