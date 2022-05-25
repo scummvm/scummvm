3f75ffadafb407e659de5e2b2a9d0ca74dfa7ad6
Athanasios Antoniou <a.antoniou79@gmail.com>
2022-05-25 18:21:55 +0300

    SCUMM: Add Explore the Farm Mac version to detection
    The new entry uses the "The Farm" pattern, the existing entry uses "farm"
    
    The existing entry was also updated to refer to the Dutch version, for which it was added
    (https://github.com/scummvm/scummvm/commit/2e497bb9f83d0435e88bcd283b636c4e2c9043e3).
    This commit fixes the issue reported in https://bugs.scummvm.org/ticket/13483
    Related forum thread is here: https://forums.scummvm.org/viewtopic.php?t=16585
    and Discord discussion here: https://discord.com/channels/581224060529148060/581224061091446795/975285829696102410
    
    As explained, the same CD works in PC and Mac Os
    
    The "farm" pattern corresponds to PC version, and "The Farm" pattern to the Mac OS.

--- engines/scumm/detection_tables.h
+++ engines/scumm/detection_tables.h
@@ -614,7 +614,7 @@ static const GameFilenamePattern gameFilenamesTable[] = {
 	{ "dog", "Springparadijs", kGenHEMac, Common::NL_NLD, Common::kPlatformMacintosh, 0 },
 
 	{ "farm", "farm", kGenHEPC, UNK_LANG, UNK, 0 },
-	{ "farm", "farm", kGenHEMac, UNK_LANG, Common::kPlatformMacintosh, 0 },
+	{ "farm", "The Farm", kGenHEMac, UNK_LANG, Common::kPlatformMacintosh, 0 },
 	{ "farm", "farmdemo", kGenHEPC, UNK_LANG, UNK, 0 },
 	{ "farm", "Farm Demo", kGenHEMac, UNK_LANG, Common::kPlatformMacintosh, 0 },
 
