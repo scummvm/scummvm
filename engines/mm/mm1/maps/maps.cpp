12019b6c49b712686b6d7b829669367f9ce281b0
Paul Gilbert <dreammaster@scummvm.org>
2022-05-27 21:06:16 -0700

    MM: MM1: Added placeholder map classes

--- engines/mm/mm1/maps/maps.cpp
+++ engines/mm/mm1/maps/maps.cpp
@@ -26,7 +26,20 @@ namespace MM1 {
 namespace Maps {
 
 Maps::Maps() :
-		_map00(this) {
+	_map00(this), _map01(this), _map02(this), _map03(this),
+	_map04(this), _map05(this), _map06(this), _map07(this),
+	_map08(this), _map09(this), _map10(this), _map11(this),
+	_map12(this), _map13(this), _map14(this), _map15(this),
+	_map16(this), _map17(this), _map18(this), _map19(this),
+	_map20(this), _map21(this), _map22(this), _map23(this),
+	_map24(this), _map25(this), _map26(this), _map27(this),
+	_map28(this), _map29(this), _map30(this), _map31(this),
+	_map32(this), _map33(this), _map34(this), _map35(this),
+	_map36(this), _map37(this), _map38(this), _map39(this),
+	_map40(this), _map41(this), _map42(this), _map43(this),
+	_map44(this), _map45(this), _map46(this), _map47(this),
+	_map48(this), _map49(this), _map50(this), _map51(this),
+	_map52(this), _map53(this), _map54(this) {
 }
 
 void Maps::load(uint mapId) {
