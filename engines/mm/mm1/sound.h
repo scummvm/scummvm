2fe7c8cc49312e537a6b3bc44753464de9e3e7f0
Paul Gilbert <dreammaster@scummvm.org>
2022-06-05 14:02:02 -0700

    MM: MM1: Added stubbed Sound class

---
+++ engines/mm/mm1/sound.h
@@ -0,0 +1,39 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MM_MM1_SOUND_H
+#define MM_MM1_SOUND_H
+
+namespace MM {
+namespace MM1 {
+
+enum SoundId { SOUND_OBSTRUCTED = 1 };
+
+class Sound {
+public:
+	static void sound(SoundId soundNum);
+	static void sound2(SoundId soundNum);
+};
+
+} // namespace MM1
+} // namespace MM
+
+#endif
