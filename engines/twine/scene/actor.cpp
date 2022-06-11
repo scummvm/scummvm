scummvm/scummvm

commit 3d5d5317a88b8e96b220ad65b0d930c93dd024bb

TWINE: fixed invalid processActor reset
was the cause of this blocker bug: https://bugs.scummvm.org/ticket/13177

the _processCollision variable is overwritten in getBrickShape and the 'backup' is lost

engines/twine/scene/animations.cpp
@@ -668,7 +668,7 @@ void Animations::processActorAnimations(int32 actorIdx) {
 
 		_engine->_collision->_causeActorDamage = 0;
 
-		_engine->_collision->_processCollision = processActor;
+		const IVec3 processActorSave = processActor;
 
 		if (IS_HERO(actorIdx) && !actor->_staticFlags.bComputeLowCollision) {
 			// check hero collisions with bricks
@@ -683,6 +683,7 @@ void Animations::processActorAnimations(int32 actorIdx) {
 			_engine->_collision->checkActorCollisionWithBricks(actor->_boudingBox.maxs.x, actor->_boudingBox.mins.y, actor->_boudingBox.maxs.z, 4);
 			_engine->_collision->checkActorCollisionWithBricks(actor->_boudingBox.mins.x, actor->_boudingBox.mins.y, actor->_boudingBox.maxs.z, 8);
 		}
+		processActor = processActorSave;
 
 		// process wall hit while running
 		if (_engine->_collision->_causeActorDamage && !actor->_dynamicFlags.bIsFalling && IS_HERO(_currentlyProcessedActorIdx) && _engine->_actor->_heroBehaviour == HeroBehaviourType::kAthletic && actor->_anim == AnimationTypes::kForward) {

commit 89813d45d96c5e79edce79f3170cf0e27f343231

TWINE: fixed regression introduced in 3d5d5317a8
the modification of the processActor values from checkActorCollisionWithBricks and checkHeroCollisionWithBricks were
lost. The bug was that you could move crates into walls in the 'sokoban'-scene (scene 35).

See https://bugs.scummvm.org/ticket/13177

This also renamed a few members to the one given in the original sources and marks some methods with the original
sources function names for easier tracking

This re-introduced the tank bug in scene 63

engines/twine/scene/actor.cpp
@@ -263,7 +263,7 @@ void Actor::resetActor(int16 actorIdx) {
 	*actor = ActorStruct();
 
 	actor->_actorIdx = actorIdx;
-	actor->_pos = IVec3(0, -1, 0);
+	actor->_pos = IVec3(0, BRICK_HEIGHT, 0);
 
 	memset(&actor->_staticFlags, 0, sizeof(StaticFlagsStruct));
 	memset(&actor->_dynamicFlags, 0, sizeof(DynamicFlagsStruct));

engines/twine/scene/actor.h
@@ -180,7 +180,7 @@ public:
 	int32 _strengthOfHit = 0;
 	int32 _hitBy = -1;
 	BonusParameter _bonusParameter;
-	int32 _angle = 0; // facing angle of actor. Minumum is 0 (SW). Going counter clock wise
+	int32 _angle = 0; // facing angle of actor. Minumum is 0 (SW). Going counter clock wise (BETA in original sources)
 	int32 _speed = 40; // speed of movement
 	ControlMode _controlMode = ControlMode::kNoMove;
 	int32 _delayInMillis = 0;
@@ -194,11 +194,11 @@ public:
 	int32 _armor = 1;
 	int32 _life = kActorMaxLife;
 
-	/** Process actor coordinate */
+	/** Process actor coordinate Nxw, Nyw, Nzw */
 	IVec3 _processActor;
 	/** Previous process actor coordinate */
 	IVec3 _previousActor;
-	IVec3 _collisionPos;
+	IVec3 _oldPos;
 
 	int32 _positionInMoveScript = -1;
 	uint8 *_moveScript = nullptr;
@@ -215,7 +215,7 @@ public:
 	/**
 	 * colliding actor id
 	 */
-	int32 _collision = -1;
+	int32 _collision = -1; // ObjCol
 	/**
 	 * actor id we are standing on
 	 */
@@ -229,7 +229,7 @@ public:
 	int32 _animPosition = 0;
 	AnimType _animType = AnimType::kAnimationTypeLoop;
 	int32 _spriteActorRotation = 0;
-	uint8 _brickSound = 0U;
+	uint8 _brickSound = 0U; // CodeJeu
 
 	BoundingBox _boundingBox;
 	ActorMoveStruct _move;
@@ -284,7 +284,7 @@ public:
 
 	ActorStruct *_processActorPtr = nullptr;
 
-	HeroBehaviourType _heroBehaviour = HeroBehaviourType::kNormal;
+	HeroBehaviourType _heroBehaviour = HeroBehaviourType::kNormal; // Comportement
 	/** Hero auto aggressive mode */
 	bool _autoAggressive = true;
 	/** Previous Hero behaviour */
@@ -354,7 +354,7 @@ public:
 	void processActorCarrier(int32 actorIdx);
 
 	/** Process actor extra bonus */
-	void processActorExtraBonus(int32 actorIdx);
+	void processActorExtraBonus(int32 actorIdx); // GiveExtraBonus
 };
 
 } // namespace TwinE

engines/twine/scene/animations.cpp
@@ -467,7 +467,7 @@ void Animations::processActorAnimations(int32 actorIdx) {
 	}
 
 	IVec3 &previousActor = actor->_previousActor;
-	previousActor = actor->_collisionPos;
+	previousActor = actor->_oldPos;
 
 	IVec3 &processActor = actor->_processActor;
 	if (actor->_staticFlags.bIsSpriteActor) {
@@ -632,7 +632,7 @@ void Animations::processActorAnimations(int32 actorIdx) {
 	// actor standing on another actor
 	if (actor->_carryBy != -1) {
 		const ActorStruct *standOnActor = _engine->_scene->getActor(actor->_carryBy);
-		processActor -= standOnActor->_collisionPos;
+		processActor -= standOnActor->_oldPos;
 		processActor += standOnActor->pos();
 
 		if (!collision->standingOnActor(actorIdx, actor->_carryBy)) {
@@ -670,22 +670,22 @@ void Animations::processActorAnimations(int32 actorIdx) {
 			collision->stopFalling();
 		}
 
+		collision->setCollisionPos(processActor);
+
 		if (IS_HERO(actorIdx) && !actor->_staticFlags.bComputeLowCollision) {
 			// check hero collisions with bricks
 			const BoundingBox &aabb = actor->_boundingBox;
-			IVec3 collisionPos = actor->_processActor;
-			causeActorDamage |= collision->checkHeroCollisionWithBricks(collisionPos, actor->_previousActor, aabb.mins.x, aabb.mins.y, aabb.mins.z);
-			causeActorDamage |= collision->checkHeroCollisionWithBricks(collisionPos, actor->_previousActor, aabb.maxs.x, aabb.mins.y, aabb.mins.z);
-			causeActorDamage |= collision->checkHeroCollisionWithBricks(collisionPos, actor->_previousActor, aabb.maxs.x, aabb.mins.y, aabb.maxs.z);
-			causeActorDamage |= collision->checkHeroCollisionWithBricks(collisionPos, actor->_previousActor, aabb.mins.x, aabb.mins.y, aabb.maxs.z);
+			causeActorDamage |= collision->checkHeroCollisionWithBricks(processActor, actor->_previousActor, aabb.mins.x, aabb.mins.y, aabb.mins.z);
+			causeActorDamage |= collision->checkHeroCollisionWithBricks(processActor, actor->_previousActor, aabb.maxs.x, aabb.mins.y, aabb.mins.z);
+			causeActorDamage |= collision->checkHeroCollisionWithBricks(processActor, actor->_previousActor, aabb.maxs.x, aabb.mins.y, aabb.maxs.z);
+			causeActorDamage |= collision->checkHeroCollisionWithBricks(processActor, actor->_previousActor, aabb.mins.x, aabb.mins.y, aabb.maxs.z);
 		} else {
 			// check other actors collisions with bricks
 			const BoundingBox &aabb = actor->_boundingBox;
-			IVec3 collisionPos = actor->_processActor;
-			causeActorDamage |= collision->checkActorCollisionWithBricks(collisionPos, actor->_previousActor, aabb.mins.x, aabb.mins.y, aabb.mins.z);
-			causeActorDamage |= collision->checkActorCollisionWithBricks(collisionPos, actor->_previousActor, aabb.maxs.x, aabb.mins.y, aabb.mins.z);
-			causeActorDamage |= collision->checkActorCollisionWithBricks(collisionPos, actor->_previousActor, aabb.maxs.x, aabb.mins.y, aabb.maxs.z);
-			causeActorDamage |= collision->checkActorCollisionWithBricks(collisionPos, actor->_previousActor, aabb.mins.x, aabb.mins.y, aabb.maxs.z);
+			causeActorDamage |= collision->checkActorCollisionWithBricks(processActor, actor->_previousActor, aabb.mins.x, aabb.mins.y, aabb.mins.z);
+			causeActorDamage |= collision->checkActorCollisionWithBricks(processActor, actor->_previousActor, aabb.maxs.x, aabb.mins.y, aabb.mins.z);
+			causeActorDamage |= collision->checkActorCollisionWithBricks(processActor, actor->_previousActor, aabb.maxs.x, aabb.mins.y, aabb.maxs.z);
+			causeActorDamage |= collision->checkActorCollisionWithBricks(processActor, actor->_previousActor, aabb.mins.x, aabb.mins.y, aabb.maxs.z);
 		}
 
 		// process wall hit while running

engines/twine/scene/animations.h
@@ -118,7 +118,7 @@ public:
 	 * Process main loop actor animations
 	 * @param actorIdx Actor index
 	 */
-	void processActorAnimations(int32 actorIdx);
+	void processActorAnimations(int32 actorIdx); // DoAnim
 };
 
 } // namespace TwinE

engines/twine/scene/collision.cpp
@@ -94,7 +94,6 @@ int32 Collision::getAverageValue(int32 start, int32 end, int32 maxDelay, int32 d
 	return (((end - start) * delay) / maxDelay) + start;
 }
 
-// ReajustPos
 void Collision::reajustActorPosition(IVec3 &processActor, ShapeType brickShape) const {
 	if (brickShape == ShapeType::kNone) {
 		return;
@@ -186,10 +185,10 @@ void Collision::reajustActorPosition(IVec3 &processActor, ShapeType brickShape) 
 
 void Collision::handlePushing(const IVec3 &minsTest, const IVec3 &maxsTest, ActorStruct *actor, ActorStruct *actorTest) {
 	IVec3 &processActor = actor->_processActor;
-	const IVec3 &previousActor = actor->_previousActor;
 
 	const int32 newAngle = _engine->_movements->getAngleAndSetTargetActorDistance(processActor, actorTest->pos());
 
+	// protect against chain reactions
 	if (actorTest->_staticFlags.bCanBePushed && !actor->_staticFlags.bCanBePushed) {
 		actorTest->_animStep.y = 0;
 
@@ -207,8 +206,8 @@ void Collision::handlePushing(const IVec3 &minsTest, const IVec3 &maxsTest, Acto
 				actorTest->_animStep.z = BRICK_SIZE / 4 + BRICK_SIZE / 8;
 			}
 		} else {
-			actorTest->_animStep.x = processActor.x - actor->_collisionPos.x;
-			actorTest->_animStep.z = processActor.z - actor->_collisionPos.z;
+			actorTest->_animStep.x = processActor.x - actor->_oldPos.x;
+			actorTest->_animStep.z = processActor.z - actor->_oldPos.z;
 		}
 	}
 
@@ -227,6 +226,7 @@ void Collision::handlePushing(const IVec3 &minsTest, const IVec3 &maxsTest, Acto
 			processActor.z = minsTest.z - actor->_boundingBox.maxs.z;
 		}
 	} else if (!actor->_dynamicFlags.bIsFalling) {
+		const IVec3 &previousActor = actor->_previousActor;
 		processActor = previousActor;
 	}
 }
@@ -248,7 +248,9 @@ int32 Collision::checkCollisionWithActors(int32 actorIdx) {
 			const IVec3 &minsTest = actorTest->pos() + actorTest->_boundingBox.mins;
 			const IVec3 &maxsTest = actorTest->pos() + actorTest->_boundingBox.maxs;
 
-			if (mins.x < maxsTest.x && maxs.x > minsTest.x && mins.y < maxsTest.y && maxs.y > minsTest.y && mins.z < maxsTest.z && maxs.z > minsTest.z) {
+			if (mins.x < maxsTest.x && maxs.x > minsTest.x
+			 && mins.y < maxsTest.y && maxs.y > minsTest.y
+			 && mins.z < maxsTest.z && maxs.z > minsTest.z) {
 				actor->_collision = a; // mark as collision with actor a
 
 				if (actorTest->_staticFlags.bIsCarrierActor) {
@@ -293,7 +295,10 @@ int32 Collision::checkCollisionWithActors(int32 actorIdx) {
 	return actor->_collision;
 }
 
-// DoCornerReajustTwinkel
+void Collision::setCollisionPos(const IVec3 &pos) {
+	_processCollision = pos;
+}
+
 bool Collision::checkHeroCollisionWithBricks(IVec3 &pos, const IVec3 &previousPos, int32 x, int32 y, int32 z) {
 	ShapeType brickShape = _engine->_grid->getBrickShape(pos);
 
@@ -327,7 +332,6 @@ bool Collision::checkHeroCollisionWithBricks(IVec3 &pos, const IVec3 &previousPo
 	return causeActorDamage;
 }
 
-// DoCornerReajust
 bool Collision::checkActorCollisionWithBricks(IVec3 &pos, const IVec3 &previousPos, int32 x, int32 y, int32 z) {
 	ShapeType brickShape = _engine->_grid->getBrickShape(pos);
 

engines/twine/scene/collision.h
@@ -38,7 +38,7 @@ private:
 	void handlePushing(const IVec3 &minsTest, const IVec3 &maxsTest, ActorStruct *actor, ActorStruct *actorTest);
 
 	/** Actor collision coordinate */
-	IVec3 _processCollision;
+	IVec3 _processCollision; // SaveNxw, SaveNyw, SaveNzw
 public:
 	Collision(TwinEEngine *engine);
 	/** Actor collision coordinate */
@@ -57,7 +57,7 @@ public:
 	 * Reajust actor position in scene according with brick shape bellow actor
 	 * @param brickShape Shape of brick bellow the actor
 	 */
-	void reajustActorPosition(IVec3 &processActor, ShapeType brickShape) const;
+	void reajustActorPosition(IVec3 &processActor, ShapeType brickShape) const; // ReajustPos
 
 	/**
 	 * Check collision with actors
@@ -65,13 +65,14 @@ public:
 	 */
 	int32 checkCollisionWithActors(int32 actorIdx);
 
+	void setCollisionPos(const IVec3 &pos);
 	/**
 	 * Check Hero collision with bricks
 	 * @param x Hero X coordinate
 	 * @param y Hero Y coordinate
 	 * @param z Hero Z coordinate
 	 */
-	bool checkHeroCollisionWithBricks(IVec3 &processActor, const IVec3 &previousActor, int32 x, int32 y, int32 z);
+	bool checkHeroCollisionWithBricks(IVec3 &processActor, const IVec3 &previousActor, int32 x, int32 y, int32 z); // DoCornerReajustTwinkel
 
 	/**
 	 * Check other actor collision with bricks
@@ -79,7 +80,7 @@ public:
 	 * @param y Actor Y coordinate
 	 * @param z Actor Z coordinate
 	 */
-	bool checkActorCollisionWithBricks(IVec3 &processActor, const IVec3 &previousActor, int32 x, int32 y, int32 z);
+	bool checkActorCollisionWithBricks(IVec3 &processActor, const IVec3 &previousActor, int32 x, int32 y, int32 z); // DoCornerReajust
 
 	/** Make actor to stop falling */
 	void stopFalling();

engines/twine/scene/grid.h
@@ -286,13 +286,14 @@ public:
 	/** Redraw grid background */
 	void redrawGrid();
 
-	ShapeType getBrickShape(int32 x, int32 y, int32 z);
+	ShapeType getBrickShape(int32 x, int32 y, int32 z); // WorldColBrick
 
+	// FullWorldColBrick
 	ShapeType getBrickShapeFull(int32 x, int32 y, int32 z, int32 y2);
 
-	uint8 getBrickSoundType(int32 x, int32 y, int32 z);
+	uint8 getBrickSoundType(int32 x, int32 y, int32 z); // WorldCodeBrick
 
-	inline ShapeType getBrickShape(const IVec3 &pos) {
+	inline ShapeType getBrickShape(const IVec3 &pos) { // WorldColBrick
 		return getBrickShape(pos.x, pos.y, pos.z);
 	}
 

engines/twine/scene/movements.h
@@ -189,7 +189,7 @@ public:
 	 */
 	void moveActor(int32 angleFrom, int32 angleTo, int32 speed, ActorMoveStruct *movePtr) const;
 
-	void processActorMovements(int32 actorIdx);
+	void processActorMovements(int32 actorIdx); // DoTrack
 };
 
 inline bool Movements::shouldTriggerZoneAction() const {

engines/twine/scene/scene.cpp
@@ -201,7 +201,7 @@ bool Scene::loadSceneLBA2() {
 		act->_pos.x = (int16)stream.readUint16LE();
 		act->_pos.y = (int16)stream.readUint16LE();
 		act->_pos.z = (int16)stream.readUint16LE();
-		act->_collisionPos = act->pos();
+		act->_oldPos = act->pos();
 		act->_strengthOfHit = stream.readByte();
 		setBonusParameterFlags(act, stream.readUint16LE());
 		act->_angle = (int16)stream.readUint16LE();
@@ -333,7 +333,7 @@ bool Scene::loadSceneLBA1() {
 		act->_pos.x = (int16)stream.readUint16LE();
 		act->_pos.y = (int16)stream.readUint16LE();
 		act->_pos.z = (int16)stream.readUint16LE();
-		act->_collisionPos = act->pos();
+		act->_oldPos = act->pos();
 		act->_strengthOfHit = stream.readByte();
 		setBonusParameterFlags(act, stream.readUint16LE());
 		act->_bonusParameter.givenNothing = 0;
@@ -396,11 +396,11 @@ bool Scene::loadSceneLBA1() {
 	if (_enableEnhancements) {
 		switch (_currentSceneIdx) {
 		case LBA1SceneId::Hamalayi_Mountains_landing_place:
-			_sceneActors[21]._pos.x = _sceneActors[21]._collisionPos.x = 6656 + 256;
-			_sceneActors[21]._pos.z = _sceneActors[21]._collisionPos.z = 768;
+			_sceneActors[21]._pos.x = _sceneActors[21]._oldPos.x = 6656 + 256;
+			_sceneActors[21]._pos.z = _sceneActors[21]._oldPos.z = 768;
 			break;
 		case LBA1SceneId::Principal_Island_outside_the_fortress:
-			_sceneActors[29]._pos.z = _sceneActors[29]._collisionPos.z = 1795;
+			_sceneActors[29]._pos.z = _sceneActors[29]._oldPos.z = 1795;
 #if 0
 			_sceneZones[15].mins.x = 1104;
 			_sceneZones[15].mins.z = 8448;

engines/twine/scene/scene.h
@@ -174,7 +174,7 @@ public:
 	ScenePositionType _heroPositionType = ScenePositionType::kNoPosition; // twinsenPositionModeInNewCube
 
 	// ACTORS
-	int32 _sceneNumActors = 0;
+	int32 _sceneNumActors = 0; // NbObjets
 	ActorStruct *_sceneHero = nullptr;
 
 	/** Meca penguin actor index */
@@ -221,7 +221,7 @@ public:
 	 * Process actor zones
 	 * @param actorIdx Process actor index
 	 */
-	void processActorZones(int32 actorIdx);
+	void processActorZones(int32 actorIdx); // CheckZoneSce
 };
 
 inline bool Scene::isGameRunning() const {

engines/twine/script/script_life_v1.h
@@ -42,7 +42,7 @@ public:
 	 * Process actor life script
 	 * @param actorIdx Current processed actor index
 	 */
-	void processLifeScript(int32 actorIdx);
+	void processLifeScript(int32 actorIdx); // DoLife
 };
 
 } // namespace TwinE

engines/twine/twine.cpp
@@ -943,7 +943,7 @@ bool TwinEEngine::runGameEngine() { // mainLoopInteration
 
 		_movements->processActorMovements(a);
 
-		actor->_collisionPos = actor->pos();
+		actor->_oldPos = actor->pos();
 
 		if (actor->_positionInMoveScript != -1) {
 			_scriptMove->processMoveScript(a);