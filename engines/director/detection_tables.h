9deb66e3db4d708846af8eb449c93ee8a805ac57
Paweł Kołodziejski <aquadran@gmail.com>
2022-06-11 11:07:30 +0200

    DIRECTOR: Added detection entry for "Ritter Rost" ticket #12907

--- engines/director/detection_tables.h
+++ engines/director/detection_tables.h
@@ -739,6 +739,7 @@ static const PlainGameDescriptor directorGames[] = {
 	{ "otelo",				"o.tel.o Online 01011" },
 	{ "ravensburger",		"Ravensburger Interactive Demo-Sampler" },
 	{ "raveshuttle",		"Rave Shuttle: The Cosmic Challenge" },
+	{ "ritterrost",			"Ritter Rost - Die Eiserne Burg" },
 	{ "tkkgkit",			"TKKG: Identikit" },
 	{ "zwergnase",			"Zwerg Nase: Ein interaktives Märchen" },
 
@@ -6116,6 +6117,8 @@ static const DirectorGameDescription gameDescriptions[] = {
 
 	WINGAME1("relaxation", "ECD",  "solitude.exe", "7a4164849d026c0f2aa8ea4c7d5447ba", 1807680, 702),
 	MACGAME1("relaxplayer", "ECD", "Relax",		   "0944b962ebb00f4b5d5149d220f8449b", 112738, 702),
+
+	WINGAME1_l("ritterrost", "",  "start.exe", "7c18c9a6af2694156bf09ed195c1ab09", 2595758, Common::DE_DEU, 702),
 
 	// original filename is a single space
 	MACGAME1("sentence", "", "xn-- -", "0944b962ebb00f4b5d5149d220f8449b", 113458, 702),
