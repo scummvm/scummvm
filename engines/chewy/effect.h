64eb1c50e5c3bff4254c0b883979e727a3e2ceff
Filippos Karapetis <bluegr@gmail.com>
2022-06-13 14:36:27 +0300

    CHEWY: Remove unused parameters

--- engines/chewy/effect.h
+++ engines/chewy/effect.h
@@ -41,10 +41,10 @@ public:
 
 	void blende1(byte *memPtr,
 	             byte *palette, uint8 mode, int16 color);
-	void border(byte *screen, int16 val1, uint8 mode, int16 color);
+	void border(byte *screen, uint8 mode, int16 color);
 
 	void rnd_blende(byte *rnd_speicher, byte *sram_speicher,
-		byte *screen, byte *palette, int16 col, int16 skip_line);
+	                byte *screen, byte *palette, int16 col);
 
 };
 
