a149f7d96516612978895973316215b7ec59317b
grisenti <emanuele@grisenti.net>
2022-06-14 09:57:54 +0200

    HPL1: add license header

--- engines/hpl1/engine/graphics/Material_EnvMap_Reflect.cpp
+++ engines/hpl1/engine/graphics/Material_EnvMap_Reflect.cpp
@@ -1,3 +1,24 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 /*
  * Copyright (C) 2006-2010 - Frictional Games
  *
@@ -16,6 +37,7 @@
  * You should have received a copy of the GNU General Public License
  * along with HPL1 Engine.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #include "hpl1/engine/graphics/Material_EnvMap_Reflect.h"
 #include "hpl1/engine/graphics/Renderer2D.h"
 #include "hpl1/engine/graphics/Renderer3D.h"
