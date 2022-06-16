1db0b37b1938df6aec5cfa0e70b7f8ae3a27caab
Paul Gilbert <dreammaster@scummvm.org>
2022-06-15 23:04:44 -0700

    MM: MM1: Skeleton subviews for blacksmith buy/sell views

--- engines/mm/mm1/views/locations/blacksmith_buy_weapons.h
+++ engines/mm/mm1/views/locations/blacksmith_buy_weapons.h
@@ -19,54 +19,23 @@
  *
  */
 
-#ifndef MM1_VIEWS_LOCATIONS_BLACKSMITH_H
-#define MM1_VIEWS_LOCATIONS_BLACKSMITH_H
+#ifndef MM1_VIEWS_LOCATIONS_BLACKSMITH_BUY_WEAPONS_H
+#define MM1_VIEWS_LOCATIONS_BLACKSMITH_BUY_WEAPONS_H
 
 #include "mm/mm1/views/locations/location.h"
-#include "mm/mm1/views/locations/blacksmith_buy_weapons.h"
-#include "mm/mm1/views/locations/blacksmith_buy_armor.h"
-#include "mm/mm1/views/locations/blacksmith_buy_misc.h"
-#include "mm/mm1/views/locations/blacksmith_sell_item.h"
 
 namespace MM {
 namespace MM1 {
 namespace Views {
 namespace Locations {
 
-class Blacksmith : public Location {
-private:
-	BlacksmithBuyWeapons _buyWeapons;
-	BlacksmithBuyArmor _buyArmor;
-	BlacksmithBuyMisc _buyMisc;
-	BlacksmithSellItem _sellItem;
-private:
-	/**
-	 * Buy weapons
-	 */
-	void buyWeapons();
-
-	/**
-	 * Buy armor
-	 */
-	void buyArmor();
-
-	/**
-	 * Buy misc items
-	 */
-	void buyMisc();
-
-	/**
-	 * Sell an item
-	 */
-	void sellItem();
-
+class BlacksmithBuyWeapons : public Location {
 public:
-	Blacksmith();
-	virtual ~Blacksmith() {}
+	BlacksmithBuyWeapons();
+	virtual ~BlacksmithBuyWeapons() {}
 
-	bool msgFocus(const FocusMessage &msg) override;
-	bool msgKeypress(const KeypressMessage &msg) override;
 	void draw() override;
+	bool msgKeypress(const KeypressMessage &msg) override;
 };
 
 } // namespace Locations
