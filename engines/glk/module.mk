3173dfa41b2b038275f50b14a9bc461bf8a88f98
Avijeet <am388488@gmail.com>
2022-06-16 11:56:38 +0530

    GLK: SCOTT: Implement tbc_multicomp.cpp

--- engines/glk/module.mk
+++ engines/glk/module.mk
@@ -268,6 +268,7 @@ MODULE_OBJS := \
 	scott/unp64/scanners/megabyte.o \
 	scott/unp64/scanners/pu_crunch.o \
 	scott/unp64/scanners/section8.o \
+	scott/unp64/scanners/tbc_multicomp.o \
 	tads/os_banners.o \
 	tads/os_buffer.o \
 	tads/os_glk.o \
