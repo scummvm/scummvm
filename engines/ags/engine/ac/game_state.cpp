fa3c524d1320ea7da40351685f09f157497a36e3
Thierry Crozat <criezy@scummvm.org>
2022-06-18 18:15:51 +0100

    AGS: GetWaitSkipResult: clear SKIP_AUTOTIMER flag (return 0)
    From upstream f5159c707a0c8616b0cca39f138c2d7e53058bf5

--- engines/ags/engine/ac/game_state.cpp
+++ engines/ags/engine/ac/game_state.cpp
@@ -376,7 +376,9 @@ void GameState::SetWaitSkipResult(int how, int data) {
 }
 
 int GameState::GetWaitSkipResult() const {
-	return wait_skipped_by << 16 | (wait_skipped_by_data & 0x0000FFFF);
+	// NOTE: we remove timer flag to make timeout reason = 0
+	return ((wait_skipped_by & ~SKIP_AUTOTIMER) << 16)
+		| (wait_skipped_by_data & 0x0000FFFF);
 }
 
 bool GameState::IsBlockingVoiceSpeech() const {
