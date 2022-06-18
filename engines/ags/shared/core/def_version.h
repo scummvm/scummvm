ca70e9c618067a69846d279c09cabcda366a9a96
Thierry Crozat <criezy@scummvm.org>
2022-06-19 00:46:32 +0100

    AGS: Updated build version (3.6.0.27)
    From upstream 9d5e422251075b270c0a8a59b63916f81b3a9ea2

--- engines/ags/shared/core/def_version.h
+++ engines/ags/shared/core/def_version.h
@@ -22,9 +22,9 @@
 #ifndef AGS_SHARED_CORE_DEFVERSION_H
 #define AGS_SHARED_CORE_DEFVERSION_H
 
-#define ACI_VERSION_STR      "3.6.0.26"
+#define ACI_VERSION_STR      "3.6.0.27"
 #if defined (RC_INVOKED) // for MSVC resource compiler
-#define ACI_VERSION_MSRC_DEF  3.6.0.26
+#define ACI_VERSION_MSRC_DEF  3.6.0.27
 #endif
 
 #define SPECIAL_VERSION ""
