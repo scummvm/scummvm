8d7314cfb9f760218c10f64d3c3a442534368634
grisenti <emanuele@grisenti.net>
2022-06-22 17:58:39 +0200

    HPL1: remove system dependent include

--- engines/hpl1/engine/impl/LowLevelPhysicsNewton.h
+++ engines/hpl1/engine/impl/LowLevelPhysicsNewton.h
@@ -29,9 +29,6 @@
 #define HPL_LOWLEVELPHYSICS_NEWTON_H
 
 #include "hpl1/engine/physics/LowLevelPhysics.h"
-#if defined(__linux__) || defined(__APPLE__)
-#include <unistd.h>
-#endif
 #include "hpl1/engine/libraries/newton/Newton.h"
 
 namespace hpl {
