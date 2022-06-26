28fcbeb2306f3a0d5e29c7d28283f204303502fb
Avijeet <am388488@gmail.com>
2022-06-26 17:13:45 +0530

    GLK: SCOTT: Add TI-99/4A games to detection

--- engines/glk/scott/detection.cpp
+++ engines/glk/scott/detection.cpp
@@ -55,7 +55,7 @@ GameDescriptor ScottMetaEngine::findGame(const char *gameId) {
 }
 
 bool ScottMetaEngine::detectGames(const Common::FSList &fslist, DetectedGames &gameList) {
-	const char *const EXTENSIONS[] = {".z80", ".saga", ".dat", ".D64", ".T64", nullptr};
+	const char *const EXTENSIONS[] = {".z80", ".saga", ".dat", ".D64", ".T64", "fiad", nullptr};
 
 	// Loop through the files of the folder
 	for (Common::FSList::const_iterator file = fslist.begin(); file != fslist.end(); ++file) {
