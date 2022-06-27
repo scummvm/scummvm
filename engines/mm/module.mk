c8f260ed038759291696d2a2583c1791bac2c70e
Paul Gilbert <dreammaster@scummvm.org>
2022-06-26 19:08:51 -0700

    MM: MM1: Skeleton encounter view class

--- engines/mm/module.mk
+++ engines/mm/module.mk
@@ -46,6 +46,7 @@ MODULE_OBJS := \
 	mm1/views/character_manage.o \
 	mm1/views/characters.o \
 	mm1/views/create_characters.o \
+	mm1/views/encounter.o \
 	mm1/views/game.o \
 	mm1/views/game_commands.o \
 	mm1/views/game_messages.o \
