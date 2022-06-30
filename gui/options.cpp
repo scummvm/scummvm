scummvm/scummvm

commit d6dbf721b62e773f529276f5b1c0c3ce59df6f47

ALL: add support for --subtitles

gui/options.cpp
@@ -886,8 +886,8 @@ void OptionsDialog::apply() {
 					ConfMan.removeKey("opl_driver", _domain);
 			}
 		} else {
-			ConfMan.removeKey("opl_driver", _domain);
 			_oplPopUpDesc->setFontColor(ThemeEngine::FontColor::kFontColorNormal); 
+			ConfMan.removeKey("opl_driver", _domain);
 		}
 	}
 
@@ -965,10 +965,14 @@ void OptionsDialog::apply() {
 					break;
 				}
 
-				ConfMan.setBool("subtitles", subtitles, _domain);
+				if (subtitles != ConfMan.getBool("subtitles")) {
+					ConfMan.setBool("subtitles", subtitles, _domain);
+					_subToggleDesc->setFontColor(ThemeEngine::FontColor::kFontColorNormal); 
+				}
 				ConfMan.setBool("speech_mute", speech_mute, _domain);
 			} else if (!_domain.empty()) {
 				ConfMan.removeKey("subtitles", _domain);
+				_subToggleDesc->setFontColor(ThemeEngine::FontColor::kFontColorNormal); 
 				ConfMan.removeKey("speech_mute", _domain);
 			}
 
@@ -1722,6 +1726,8 @@ void OptionsDialog::addSubtitleControls(GuiObject *boss, const Common::String &p
 	
 	if (ConfMan.isKeyTemporary("talkspeed"))
 		_subSpeedDesc->setFontColor(ThemeEngine::FontColor::kFontColorOverride); 
+	if (ConfMan.isKeyTemporary("subtitles"))
+		_subToggleDesc->setFontColor(ThemeEngine::FontColor::kFontColorOverride); 
 
 	// Subtitle speed
 	_subSpeedSlider = new SliderWidget(boss, prefix + "subSubtitleSpeedSlider", Common::U32String(), kSubtitleSpeedChanged);

base/commandLine.cpp
@@ -1893,6 +1893,9 @@ bool processSettings(Common::String &command, Common::StringMap &settings, Commo
 	if (settings.contains("talkspeed")) {
 		ConfMan.set("talkspeed", settings["talkspeed"], Common::ConfigManager::kSessionDomain); 
 	}
+	if (settings.contains("subtitles")) {
+		ConfMan.set("subtitles", settings["subtitles"], Common::ConfigManager::kSessionDomain);
+	}
 
 	// Finally, store the command line settings into the config manager.
 	for (Common::StringMap::const_iterator x = settings.begin(); x != settings.end(); ++x) {

commit 25401cdf922ca629d8e6c8d8664a07b0c755b897

GUI: Add missing domain when checking for subtitle config - bug #13629
This disallowed selecting "both" for speech and subtitles, as the
subtitles setting was never saved. A regression from d6dbf721b62e773f.

gui/options.cpp
@@ -965,7 +965,7 @@ void OptionsDialog::apply() {
 					break;
 				}
 
-				if (subtitles != ConfMan.getBool("subtitles")) {
+				if (subtitles != ConfMan.getBool("subtitles", _domain)) {
 					ConfMan.setBool("subtitles", subtitles, _domain);
 					_subToggleDesc->setFontColor(ThemeEngine::FontColor::kFontColorNormal); 
 				}