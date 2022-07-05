ea9b18fb852fb1985db5f93c32abbc9f5dbf2c85
neuromancer <gustavo.grieco@gmail.com>
2022-07-05 09:08:32 +0200

    FREESCAPE: refactored instruction code

--- engines/freescape/language/instruction.cpp
+++ engines/freescape/language/instruction.cpp
@@ -7,6 +7,8 @@
 //
 
 #include "freescape/language/instruction.h"
+#include "freescape/language/8bitDetokeniser.h"
+#include "freescape/freescape.h"
 
 namespace Freescape {
 
@@ -23,20 +25,6 @@ FCLInstruction::FCLInstruction() {
 	elseInstructions = nullptr;
 }
 
-/*FCLInstruction::FCLInstruction(const FCLInstruction &source) {
-	type = source.type;
-	arguments.source = source.arguments.source;
-	arguments.destination = source.arguments.destination;
-	arguments.option = source.arguments.option;
-	conditional.thenInstructions = source.conditional.thenInstructions;
-	conditional.elseInstructions = source.conditional.elseInstructions;
-}*/
-
-/*
-	Very routine setters for now; this code does not currently enforce good behaviour.
-	TODO: allow mutation only once; delete supplied objects and raise an error if a
-	second attempt at mutation is made
-*/
 void FCLInstruction::setSource(int32 _source) {
 	source = _source;
 }
@@ -50,26 +38,142 @@ void FCLInstruction::setBranches(FCLInstructionVector *thenBranch, FCLInstructio
 	elseInstructions = elseBranch;
 }
 
-/*
-	Similarly routine getters...
-*/
-/*void FCLInstruction::getValue(CGameState &gameState, int32_t &source) {
-	source = arguments.source.getValue(gameState, source);
-}
-
-void FCLInstruction::getValue(CGameState &gameState, int32_t &source, int32_t &destination) {
-	source = arguments.source.getValue(gameState, source);
-	destination = arguments.destination.getValue(gameState, destination);
-}
-
-void FCLInstruction::getValue(CGameState &gameState, int32_t &source, int32_t &destination, int32_t &option) {
-	source = arguments.source.getValue(gameState, source);
-	destination = arguments.destination.getValue(gameState, destination);
-	option = arguments.option.getValue(gameState, option);
-}*/
-
 Token::Type FCLInstruction::getType() {
 	return type;
 }
+
+void FreescapeEngine::executeCode(FCLInstructionVector &code, bool shot, bool collided) {
+	assert(!(shot && collided));
+	int ip = 0;
+	int codeSize = code.size();
+	while (ip <= codeSize - 1) {
+		FCLInstruction &instruction = code[ip];
+		debug("Executing ip: %d in code with size: %d", ip, codeSize);
+		switch (instruction.getType()) {
+			default:
+			break;
+			case Token::COLLIDEDQ:
+			if (collided)
+				executeCode(*instruction.thenInstructions, shot, collided);
+			// else branch is always empty
+			assert(instruction.elseInstructions == nullptr);
+			break;
+			case Token::SHOTQ:
+			if (shot)
+				executeCode(*instruction.thenInstructions, shot, collided);
+			// else branch is always empty
+			assert(instruction.elseInstructions == nullptr);
+			break;
+			case Token::VARNOTEQ:
+			if (executeEndIfNotEqual(instruction))
+				ip = codeSize;
+			break;
+			case Token::ADDVAR:
+			executeIncrementVariable(instruction);
+			break;
+			case Token::SUBVAR:
+			executeDecrementVariable(instruction);
+			break;
+			case Token::GOTO:
+			executeGoto(instruction);
+			break;
+			case Token::TOGVIS:
+			executeToggleVisibility(instruction);
+			break;
+			case Token::INVIS:
+			executeMakeInvisible(instruction);
+			break;
+			case Token::VIS:
+			executeMakeVisible(instruction);
+			break;
+			case Token::DESTROY:
+			executeDestroy(instruction);
+			break;
+			case Token::REDRAW:
+			executeRedraw(instruction);
+			break;
+			case Token::DELAY:
+			executeDelay(instruction);
+			break;
+		}
+		ip++;
+	}
+	return;
+}
+
+
+void FreescapeEngine::executeRedraw(FCLInstruction &instruction) {
+	debug("Redrawing screen");
+	drawFrame();
+}
+
+void FreescapeEngine::executeDelay(FCLInstruction &instruction) {
+	uint16 delay = instruction.source;
+	debug("Delaying %d * 1/50 seconds", delay);
+	g_system->delayMillis(20 * delay);
+}
+
+bool FreescapeEngine::executeEndIfNotEqual(FCLInstruction &instruction) {
+	uint16 variable1 = instruction.source;
+	uint16 variable2 = instruction.destination;
+	debug("End condition if variable %d is not equal to variable %d!", variable1, variable2);
+	return (_gameState[variable1] != _gameState[variable2]);
+}
+
+void FreescapeEngine::executeIncrementVariable(FCLInstruction &instruction) {
+	uint16 variable = instruction.source;
+	uint16 increment = instruction.destination;
+	_gameState[variable] = _gameState[variable] + increment;
+	if (variable == k8bitVariableScore) {
+		debug("Score incremented by %d up to %d", increment, _gameState[variable]);
+	} else
+		debug("Variable %d by %d incremented up to %d!", variable, increment, _gameState[variable]);
+}
+
+void FreescapeEngine::executeDecrementVariable(FCLInstruction &instruction) {
+	uint16 variable = instruction.source;
+	uint16 decrement = instruction.destination;
+	_gameState[variable] = _gameState[variable] - decrement;
+	if (variable == k8bitVariableEnergy) {
+		debug("Energy decrement by %d up to %d", decrement, _gameState[variable]);
+	} else
+		debug("Variable %d by %d incremented up to %d!", variable, decrement, _gameState[variable]);
+}
+
+void FreescapeEngine::executeDestroy(FCLInstruction &instruction) {
+	uint16 objectID = instruction.source;
+	debug("Destroying obj %d!", objectID);
+	Object *obj = _currentArea->objectWithID(objectID);
+	assert(!obj->isDestroyed());
+	obj->destroy();
+}
+
+void FreescapeEngine::executeMakeInvisible(FCLInstruction &instruction) {
+	uint16 objectID = instruction.source;
+	debug("Making obj %d invisible!", objectID);
+	Object *obj = _currentArea->objectWithID(objectID);
+	obj->makeInvisible();
+}
+
+void FreescapeEngine::executeMakeVisible(FCLInstruction &instruction) {
+	uint16 objectID = instruction.source;
+	debug("Making obj %d visible!", objectID);
+	Object *obj = _currentArea->objectWithID(objectID);
+	obj->makeVisible();
+}
+
+void FreescapeEngine::executeToggleVisibility(FCLInstruction &instruction) {
+	uint16 objectID = instruction.source;
+	debug("Toggling obj %d visibility!", objectID);
+	Object *obj = _currentArea->objectWithID(objectID);
+	obj->toggleVisibility();
+}
+
+void FreescapeEngine::executeGoto(FCLInstruction &instruction) {
+	uint16 areaID = instruction.source;
+	uint16 entranceID = instruction.destination;
+	gotoArea(areaID, entranceID);
+}
+
 
 } // End of namespace Freescape
