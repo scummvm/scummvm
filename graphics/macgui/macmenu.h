5fccd843f84aed8200ac58118fc3589e06ce0975
Avijeet <am388488@gmail.com>
2022-07-08 17:21:07 +0530

    GRAPHICS: MACGUI: Add insertMenuItem

--- graphics/macgui/macmenu.h
+++ graphics/macgui/macmenu.h
@@ -77,6 +77,8 @@ public:
 	MacMenuSubMenu *addSubMenu(MacMenuSubMenu *submenu, int index = -1);
 	int addMenuItem(MacMenuSubMenu *submenu, const Common::String &text, int action = -1, int style = 0, char shortcut = 0, bool enabled = true, bool checked = false);
 	int addMenuItem(MacMenuSubMenu *submenu, const Common::U32String &text, int action = 0, int style = 0, char shortcut = 0, bool enabled = true, bool checked = false);
+	void insertMenuItem(MacMenuSubMenu *submenu, const Common::String &text, int pos, int action = -1, int style = 0, char shortcut = 0, bool enabled = true, bool checked = false);
+	void insertMenuItem(MacMenuSubMenu *submenu, const Common::U32String &text, int pos, int action = 0, int style = 0, char shortcut = 0, bool enabled = true, bool checked = false);
 	void loadMenuResource(Common::MacResManager *resFork, uint16 id);
 	void loadMenuBarResource(Common::MacResManager *resFork, uint16 id);
 	void createSubMenuFromString(int id, const char *string, int commandId);
