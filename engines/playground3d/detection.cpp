353bb8387f1de6879aaa8ec5c91accba946c74b7
Cameron Cawley <ccawley2011@gmail.com>
2022-07-23 22:09:25 +0100

    PLAYGROUND3D: Fix typos

--- engines/playground3d/detection.cpp
+++ engines/playground3d/detection.cpp
@@ -25,7 +25,7 @@
 #include "playground3d/playground3d.h"
 
 static const PlainGameDescriptor playground3d_setting[] = {
-	{ "playground3d", "Playground 3d: the testing and plaground environment for 3d renderers" },
+	{ "playground3d", "Playground 3d: the testing and playground environment for 3d renderers" },
 	{ nullptr, nullptr }
 };
 
@@ -53,7 +53,7 @@ public:
 	}
 
 	const char *getName() const override {
-		return "Playground 3d: the testing and plaground environment for 3d renderers";
+		return "Playground 3d: the testing and playground environment for 3d renderers";
 	}
 
 	const char *getOriginalCopyright() const override {
