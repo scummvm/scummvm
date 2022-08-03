26f0fab159b932b65fc363c6a021e347e5a83f89
Carlo Bramini <30959007+carlo-bramini@users.noreply.github.com>
2022-08-03 19:40:13 +0200

    BACKENDS: OPENGLSDL: Fix corrupted graphics.
    Size of the alpha channel is not set to its default value and this caused totally corrupted graphics on my PC.
    See:
    https://bugs.scummvm.org/ticket/13712

--- backends/graphics/openglsdl/openglsdl-graphics.cpp
+++ backends/graphics/openglsdl/openglsdl-graphics.cpp
@@ -44,6 +44,7 @@ OpenGLSdlGraphicsManager::OpenGLSdlGraphicsManager(SdlEventSource *eventSource, 
 	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
