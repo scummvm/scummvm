scummvm/scummvm

commit ef184a6cef98a1756c0669a03fa9b6fa2386b6e7

VIDEO: Fix QuickTime decoding of edits with mediaTime
QuickTimeDecoder has a bug which causes the mediaTime offset to be
ignored when a track begins with an empty edit and is followed by an
edit with a non-zero mediaTime. This causes the KQ6 Mac opening movie
to start several tracks at unintended frames (they're never supposed to
be displayed) and the intended frames at the end of the edit to never
be displayed. (Bug #11085)

video/qt_decoder.cpp
@@ -297,9 +297,9 @@ QuickTimeDecoder::VideoTrackHandler::VideoTrackHandler(QuickTimeDecoder *decoder
 	}
 
 	_curEdit = 0;
-	enterNewEditListEntry(false);
-
 	_curFrame = -1;
+	enterNewEditListEntry(true); // might set _curFrame
+
 	_durationOverride = -1;
 	_scaledSurface = 0;
 	_curPalette = 0;
@@ -514,6 +514,9 @@ const Graphics::Surface *QuickTimeDecoder::VideoTrackHandler::decodeNextFrame() 
 			return 0;
 
 		enterNewEditListEntry(true);
+
+		if (isEmptyEdit())
+			return 0;
 	}
 
 	const Graphics::Surface *frame = bufferNextFrame();
@@ -697,13 +700,21 @@ uint32 QuickTimeDecoder::VideoTrackHandler::findKeyFrame(uint32 frame) const {
 	return frame;
 }
 
-void QuickTimeDecoder::VideoTrackHandler::enterNewEditListEntry(bool bufferFrames) {
-	// Bypass all empty edit lists first
-	while (!atLastEdit() && _parent->editList[_curEdit].mediaTime == -1)
-		_curEdit++;
+bool QuickTimeDecoder::VideoTrackHandler::isEmptyEdit() const {
+	return (_parent->editList[_curEdit].mediaTime == -1);
+}
 
+void QuickTimeDecoder::VideoTrackHandler::enterNewEditListEntry(bool bufferFrames) {
 	if (atLastEdit())
 		return;
+
+	// if this is an empty edit then the only thing to do is set the
+	// time for the next frame, which is the duration of this edit.
+	if (isEmptyEdit()) {
+		_curFrame = -1;
+		_nextFrameStartTime = getCurEditTimeOffset() + getCurEditTrackDuration();
+		return;
+	}
 
 	uint32 mediaTime = _parent->editList[_curEdit].mediaTime;
 	uint32 frameNum = 0;
@@ -792,8 +803,12 @@ const Graphics::Surface *QuickTimeDecoder::VideoTrackHandler::bufferNextFrame() 
 }
 
 uint32 QuickTimeDecoder::VideoTrackHandler::getRateAdjustedFrameTime() const {
-	// Figure out what time the next frame is at taking the edit list rate into account
-	Common::Rational offsetFromEdit = Common::Rational(_nextFrameStartTime - getCurEditTimeOffset()) / _parent->editList[_curEdit].mediaRate;
+	// Figure out what time the next frame is at taking the edit list rate into account,
+	// unless this is an empty edit, in which case the rate isn't applicable.
+	Common::Rational offsetFromEdit = Common::Rational(_nextFrameStartTime - getCurEditTimeOffset());
+	if (!isEmptyEdit()) {
+		offsetFromEdit /= _parent->editList[_curEdit].mediaRate;
+	}
 	uint32 convertedTime = offsetFromEdit.toInt();
 
 	if ((offsetFromEdit.getNumerator() % offsetFromEdit.getDenominator()) > (offsetFromEdit.getDenominator() / 2))

video/qt_decoder.h
@@ -171,6 +171,7 @@ private:
 		Common::SeekableReadStream *getNextFramePacket(uint32 &descId);
 		uint32 getCurFrameDuration();            // media time
 		uint32 findKeyFrame(uint32 frame) const;
+		bool isEmptyEdit() const;
 		void enterNewEditListEntry(bool bufferFrames);
 		const Graphics::Surface *bufferNextFrame();
 		uint32 getRateAdjustedFrameTime() const; // media time

commit 917567120413f2ffca0d8c9c164ff145a1bb8a03

VIDEO: Fix QuickTime regression with mediaTime and dithering
Fixes an error when playing a QuickTime video that has a mediaTime set
on its first edit. This was caused by mediaTime changes in:
ef184a6cef98a1756c0669a03fa9b6fa2386b6e7

Those changes buffered frames during initialization so that the keyframe
and other frames don't play instead of the intended start frame that
mediaTime specifies. My mistake was that decoding isn't allowed during
VideoDecoder::loadStream(); VideoDecoder::setDitheringPalette() requires
that no frames have been decoded yet, and at least Director and Mohawk
call that.

Now the initial mediaTime buffering is delayed until the first decode.

Fixes bug #13479 where certain Myst videos error.
Fixes the opening movie in the Director game Chop Suey.

video/qt_decoder.cpp
@@ -299,7 +299,8 @@ QuickTimeDecoder::VideoTrackHandler::VideoTrackHandler(QuickTimeDecoder *decoder
 
 	_curEdit = 0;
 	_curFrame = -1;
-	enterNewEditListEntry(true); // might set _curFrame
+	_delayedFrameToBufferTo = -1;
+	enterNewEditListEntry(true, true); // might set _curFrame
 
 	_durationOverride = -1;
 	_scaledSurface = 0;
@@ -369,6 +370,8 @@ bool QuickTimeDecoder::VideoTrackHandler::endOfTrack() const {
 }
 
 bool QuickTimeDecoder::VideoTrackHandler::seek(const Audio::Timestamp &requestedTime) {
+	_delayedFrameToBufferTo = -1; // abort any delayed buffering
+
 	uint32 convertedFrames = requestedTime.convertToFramerate(_decoder->_timeScale).totalNumberOfFrames();
 	for (_curEdit = 0; !atLastEdit(); _curEdit++)
 		if (convertedFrames >= _parent->editList[_curEdit].timeOffset && convertedFrames < _parent->editList[_curEdit].timeOffset + _parent->editList[_curEdit].trackDuration)
@@ -580,6 +583,8 @@ const byte *QuickTimeDecoder::VideoTrackHandler::getPalette() const {
 }
 
 bool QuickTimeDecoder::VideoTrackHandler::setReverse(bool reverse) {
+	_delayedFrameToBufferTo = -1; // abort any delayed buffering
+
 	_reversed = reverse;
 
 	if (_reversed) {
@@ -721,7 +726,7 @@ bool QuickTimeDecoder::VideoTrackHandler::isEmptyEdit() const {
 	return (_parent->editList[_curEdit].mediaTime == -1);
 }
 
-void QuickTimeDecoder::VideoTrackHandler::enterNewEditListEntry(bool bufferFrames) {
+void QuickTimeDecoder::VideoTrackHandler::enterNewEditListEntry(bool bufferFrames, bool initializingTrack) {
 	if (atLastEdit())
 		return;
 
@@ -765,8 +770,15 @@ void QuickTimeDecoder::VideoTrackHandler::enterNewEditListEntry(bool bufferFrame
 		// Track down the keyframe
 		// Then decode until the frame before target
 		_curFrame = findKeyFrame(frameNum) - 1;
-		while (_curFrame < (int32)frameNum - 1)
-			bufferNextFrame();
+		if (initializingTrack) {
+			// We can't decode frames during track initialization,
+			// so delay buffering until the first decode.
+			_delayedFrameToBufferTo = (int32)frameNum - 1;
+		} else {
+			while (_curFrame < (int32)frameNum - 1) {
+				bufferNextFrame();
+			}
+		}
 	} else {
 		// Since frameNum is the frame that needs to be displayed
 		// we'll set _curFrame to be the "last frame displayed"
@@ -777,6 +789,16 @@ void QuickTimeDecoder::VideoTrackHandler::enterNewEditListEntry(bool bufferFrame
 }
 
 const Graphics::Surface *QuickTimeDecoder::VideoTrackHandler::bufferNextFrame() {
+	// Buffer any frames that were identified during track initialization
+	// and delayed until decoding.
+	if (_delayedFrameToBufferTo != -1) {
+		int32 frameNum = _delayedFrameToBufferTo;
+		_delayedFrameToBufferTo = -1;
+		while (_curFrame < frameNum) {
+			bufferNextFrame();
+		}
+	}
+
 	_curFrame++;
 
 	// Get the next packet

video/qt_decoder.h
@@ -155,6 +155,7 @@ private:
 		Common::QuickTimeParser::Track *_parent;
 		uint32 _curEdit;
 		int32 _curFrame;
+		int32 _delayedFrameToBufferTo;
 		uint32 _nextFrameStartTime; // media time
 		Graphics::Surface *_scaledSurface;
 		int32 _durationOverride;    // media time
@@ -172,7 +173,7 @@ private:
 		uint32 getCurFrameDuration();            // media time
 		uint32 findKeyFrame(uint32 frame) const;
 		bool isEmptyEdit() const;
-		void enterNewEditListEntry(bool bufferFrames);
+		void enterNewEditListEntry(bool bufferFrames, bool intializingTrack = false);
 		const Graphics::Surface *bufferNextFrame();
 		uint32 getRateAdjustedFrameTime() const; // media time
 		uint32 getCurEditTimeOffset() const;     // media time