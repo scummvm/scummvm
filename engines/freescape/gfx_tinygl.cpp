22395f03003671362faa9c4b657c61d994b862ad
neuromancer <gustavo.grieco@gmail.com>
2022-08-13 11:16:55 +0200

    FREESCAPE: added new licenses and cleaned up includes

--- engines/freescape/gfx_tinygl.cpp
+++ engines/freescape/gfx_tinygl.cpp
@@ -1,13 +1,13 @@
-/* ResidualVM - A 3D game interpreter
+/* ScummVM - Graphic Adventure Engine
  *
- * ResidualVM is the legal property of its developers, whose names
- * are too numerous to list here. Please refer to the AUTHORS
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
  * file distributed with this source distribution.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,25 +15,19 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
+// Based on Phantasma code by Thomas Harte (2013)
+
 #include "common/config-manager.h"
 #include "common/rect.h"
-#include "common/textconsole.h"
-
-#include "graphics/colormasks.h"
-#include "graphics/surface.h"
-
-#include "math/vector2d.h"
 #include "math/glmath.h"
+#include "graphics/tinygl/tinygl.h"
 
-#include "engines/freescape/gfx.h"
 #include "engines/freescape/gfx_tinygl.h"
 #include "engines/freescape/gfx_tinygl_texture.h"
-#include "graphics/tinygl/tinygl.h"
 
 namespace Freescape {
 
--- engines/freescape/language/token.h
+++ engines/freescape/language/token.h
@@ -1,10 +1,25 @@
-//
-//  Token.h
-//  Phantasma
-//
-//  Created by Thomas Harte on 08/12/2013.
-//  Copyright (c) 2013 Thomas Harte. All rights reserved.
-//
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+// Based on Phantasma code by Thomas Harte (2013)
 
 #ifndef FREESCAPE_TOKEN_H
 #define FREESCAPE_TOKEN_H
@@ -96,14 +111,8 @@ public:
 		value = 0;
 	}
 
-	//Token(Common::String &string);
-	//Token(Type type, int32_t value);
-	//Token(const Token &other);
-	//Token &operator=(const Token &rhs);
-
 private:
 	Type type;
-
 	int32 value;
 	Common::String string;
 };
