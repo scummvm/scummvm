2fe65d95ef86433460c2c59eca9de022564c2926
athrxx <athrxx@scummvm.org>
2022-08-18 20:32:28 +0200

    SCUMM: limit render modes to suitable targets
    While we already tried to prevent invalid render mode
    selections there were still some possible misconfigurations.
    I have no idea why on earth people do things like select
    CGA mode on Mac or Amiga, but it happens...

--- engines/scumm/scumm.cpp
+++ engines/scumm/scumm.cpp
@@ -265,7 +265,7 @@ ScummEngine::ScummEngine(OSystem *syst, const DetectorResult &dr)
 	switch (_renderMode) {
 	case Common::kRenderHercA:
 	case Common::kRenderHercG:
-		if (_game.version > 2 && _game.id != GID_MONKEY_EGA)
+		if ((_game.version > 2 && _game.id != GID_MONKEY_EGA) || _game.platform != Common::kPlatformDOS)
 			_renderMode = Common::kRenderDefault;
 		break;
 
@@ -277,11 +277,18 @@ ScummEngine::ScummEngine(OSystem *syst, const DetectorResult &dr)
 
 	case Common::kRenderCGA:
 	case Common::kRenderEGA:
+		if ((_game.version >= 4 && !(_game.features & GF_16COLOR) && !_supportsEGADithering)
+			|| (_game.features & GF_OLD256) || _game.platform != Common::kPlatformDOS)
+			_renderMode = Common::kRenderDefault;
+		break;
+
 	case Common::kRenderAmiga:
-		if ((_game.version >= 4 && !(_game.features & GF_16COLOR)
-			&& !(_game.platform == Common::kPlatformAmiga && _renderMode == Common::kRenderEGA)
-			&& !_supportsEGADithering)
-			|| (_game.features & GF_OLD256))
+		if (_game.platform != Common::kPlatformAmiga)
+			_renderMode = Common::kRenderDefault;
+		break;
+
+	case Common::kRenderMacintosh:
+		if (_game.platform != Common::kPlatformMacintosh)
 			_renderMode = Common::kRenderDefault;
 		break;
 
@@ -289,6 +296,11 @@ ScummEngine::ScummEngine(OSystem *syst, const DetectorResult &dr)
 		if (_game.platform != Common::kPlatformMacintosh || (_game.id != GID_LOOM && _game.id != GID_INDY3)) {
 			_renderMode = Common::kRenderDefault;
 		}
+		break;
+
+	case Common::kRenderFMTowns:
+		if (_game.platform != Common::kPlatformFMTowns)
+			_renderMode = Common::kRenderDefault;
 		break;
 
 	default:
