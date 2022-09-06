fd0569d3ceb231564404983cb7aca880c3bfeecb
Quote58 <michael.hayman54@gmail.com>
2022-09-06 17:17:39 -0400

    IMMORTAL: Implement utilities::inside() and insideRect()

--- engines/immortal/utilities.cpp
+++ engines/immortal/utilities.cpp
@@ -35,10 +35,66 @@ void Utilities::delay8(int j) {            // 1/8 jiffies are 7.02ms
 	g_system->delayMillis(j * 7);
 }
 
-bool Utilities::inside(int x1, int y1, int a, int x2, int y2) {
-	return false;
+bool Utilities::inside(uint8 dist, uint8 cenX, uint8 cenY, uint8 pointX, uint8 pointY) {
+	// you can't be within 0 distance of something
+	if (dist == 0) {
+		return false;
+	}
+
+	// we want the negative distance because this is a rectangle all the way around a point
+	uint8 negDist = ((dist ^ 0xFF) + 1) + 1;
+
+	// First is the X, so we get delta X from the points
+	uint8 dX = cenX - pointX;
+	if (dX < 0x80) {
+		// Our point is beyond the other point
+		if (dX >= dist) {
+			// And it is further than point + distance, so it's not useable
+			return false;
+		}
+
+	} else if (dX >= negDist) {
+		// If the negative delta X is *greater* than the negative distance, that means we're not far *enough* in the X
+		return false;
+	}
+
+	// Exact same system here but with the Y positions instead
+	uint8 dY = cenY - pointY;
+	if (dY < 0x80) {
+		if (dY >= dist) {
+			return false;
+		}
+
+	} else if (dY >= negDist) {
+		return false;
+	}
+
+	// If all conditions are met, we are within the distance of the point
+	return true;
 }
-bool Utilities::insideRect(int x, int y, int r) {
+
+bool Utilities::insideRect(uint8 rectX, uint8 rectY, uint8 w, uint8 h, uint8 pointX, uint8 pointY) {
+	/* Very simple comapred to inside, we simply check
+	 * first if width and height are >0, to make sure
+	 * the rectangle has a size, and then we see if
+	 * the point is between the point X,Y and the
+	 * point X,Y + the width,height of the rect.
+	 * This is done by grabbing the delta X,Y and
+	 * making sure it is not negative.
+	 */
+	if ((w | h) == 0) {
+		return false;
+	}
+
+	uint8 dX = pointX - rectX;
+	uint8 dY = pointY - rectY;
+
+	if ((dX < 0x80) && (dX < w)) {
+		if ((dY < 0x80) && (dY < h)) {
+			return true;
+		}
+	}
+
 	return false;
 }
 
