f440529512cc4252d4cbf63b07ab4263331b7d67
Roland van Laar <roland@rolandvanlaar.nl>
2022-09-17 23:51:57 +0200

    DIRECTOR: only cleanup sharedcast when it exists
    Fixes segfault when the sharedcast was a nullptr.

--- engines/director/window.cpp
+++ engines/director/window.cpp
@@ -338,13 +338,15 @@ bool Window::step() {
 				_currentMovie->_sharedCast = sharedCast;
 			} else {
 				// clear reference in openResFile before deleting shared cast
-				g_director->_openResFiles.erase(sharedCast->getArchive()->getPathName());
+				if (sharedCast)
+					g_director->_openResFiles.erase(sharedCast->getArchive()->getPathName());
 				delete sharedCast;
 				_currentMovie->loadSharedCastsFrom(sharedCastPath);
 			}
 		} else {
 			// clear reference in openResFile before deleting shared cast
-			g_director->_openResFiles.erase(sharedCast->getArchive()->getPathName());
+			if (sharedCast)
+				g_director->_openResFiles.erase(sharedCast->getArchive()->getPathName());
 			delete sharedCast;
 		}
 
