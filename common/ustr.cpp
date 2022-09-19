f321c83192e4a27cc806e41b511d68489848539d
elasota <ejlasota@gmail.com>
2022-09-19 17:25:16 -0400

    COMMON: Pass U32String format params by reference and fix warnings about passing a non-trivial class to va_start.

--- common/ustr.cpp
+++ common/ustr.cpp
@@ -145,12 +145,12 @@ void U32String::insertString(const String &s, uint32 p, CodePage page) {
 	insertString(U32String(s, page), p);
 }
 
-U32String U32String::format(U32String fmt, ...) {
+U32String U32String::formatInternal(const U32String *fmt, ...) {
 	U32String output;
 
 	va_list va;
 	va_start(va, fmt);
-	U32String::vformat(fmt.c_str(), fmt.c_str() + fmt.size(), output, va);
+	U32String::vformat(fmt->c_str(), fmt->c_str() + fmt->size(), output, va);
 	va_end(va);
 
 	return output;
