67feb0e942639757392a50c4525c029a988ab1aa
Die4Ever <die4ever2005@gmail.com>
2022-09-21 14:51:44 -0500

    GROOVIE: speedrun mode option

--- engines/groovie/script.cpp
+++ engines/groovie/script.cpp
@@ -904,6 +904,10 @@ bool Script::playvideofromref(uint32 fileref, bool loopUntilAudioDone) {
 	// It isn't the current video, open it
 	if (fileref != _videoRef) {
 
+		if (_fastForwarding && !ConfMan.getBool("speedrun_mode")) {
+			resetFastForward();
+		}
+
 		// Debug bitflags
 		debugCN(1, kDebugScript, "Groovie::Script: Play video 0x%04X (bitflags:", fileref);
 		for (int i = 15; i >= 0; i--) {
@@ -973,7 +977,7 @@ bool Script::playvideofromref(uint32 fileref, bool loopUntilAudioDone) {
 			// End the playback
 			return true;
 		}
-		if (_fastForwarding) {
+		if (_fastForwarding && !ConfMan.getBool("speedrun_mode")) {
 			resetFastForward();
 			if (!_fastForwarding)
 				_vm->_videoPlayer->setOverrideSpeed(false);
