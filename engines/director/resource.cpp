scummvm/scummvm

commit 12bb1a495fa5d98ef51a16f3668fd2ab81e7142c

DIRECTOR: Fix memory leaks D3 win EXE loading
Fixed Archives that were instantiated but not closed.

engines/director/resource.cpp
@@ -270,6 +270,8 @@ void Window::loadEXE(const Common::String movie) {
 		}
 	}
 
+	delete exeStream;
+
 	if (_mainArchive)
 		_mainArchive->setPathName(movie);
 }
@@ -318,10 +320,12 @@ void Window::loadEXEv3(Common::SeekableReadStream *stream) {
 
 		_mainArchive = new RIFFArchive();
 
-		if (!_mainArchive->openStream(stream, riffOffset))
-			warning("Failed to load RIFF from EXE");
-		else
+		if (_mainArchive->openStream(stream, riffOffset))
 			return;
+
+		warning("Failed to load RIFF from EXE");
+		delete _mainArchive;
+		_mainArchive = nullptr;
 	}
 
 	openMainArchive(mmmFileName);

commit 1ff57a4c0f580de442ec986e5f46f4f2462a88d4

DIRECTOR: Fix segfault in D4 movies
The exeStream should only be deleted in `loadExeV3`

One `loadExeV?` is not like the others. They all load RIFX files
except for the V3 variant. They all error on an error during the opening of
the stream, except voor the V3 variant.

The segfault was introduced in 12bb1a495fa5d98ef by fixing a memory leak
for the `exeStream`.

engines/director/resource.cpp
@@ -270,8 +270,6 @@ void Window::loadEXE(const Common::String movie) {
 		}
 	}
 
-	delete exeStream;
-
 	if (_mainArchive)
 		_mainArchive->setPathName(movie);
 }
@@ -326,6 +324,7 @@ void Window::loadEXEv3(Common::SeekableReadStream *stream) {
 		warning("Failed to load RIFF from EXE");
 		delete _mainArchive;
 		_mainArchive = nullptr;
+		delete stream;
 	}
 
 	openMainArchive(mmmFileName);