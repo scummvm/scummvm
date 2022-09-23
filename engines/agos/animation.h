c98001f68ee75f713b9bd564807e35e6838d4849
BLooperZ <blooperz@users.noreply.github.com>
2022-09-23 14:08:44 +0300

    AGOS: FEEBLE: Add subtitles support for SMK videos

--- engines/agos/animation.h
+++ engines/agos/animation.h
@@ -26,6 +26,7 @@
 
 #include "video/dxa_decoder.h"
 #include "video/smk_decoder.h"
+#include "video/subtitles.h"
 #include "audio/mixer.h"
 
 namespace AGOS {
@@ -61,11 +62,13 @@ public:
 	virtual void playVideo() = 0;
 	virtual void nextFrame() = 0;
 	virtual void stopVideo() = 0;
+	void loadSubtitles(const char *fname) { _subtitles.loadSRTFile(fname); }
 
 protected:
 	virtual void handleNextFrame();
 	virtual bool processFrame() = 0;
 	virtual void startSound() {}
+	Video::Subtitles _subtitles;
 };
 
 class MoviePlayerDXA : public MoviePlayer, Video::DXADecoder {
