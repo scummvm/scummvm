97f56a23d9907006ec185451e634dc60a687f1b9
Walter Agazzi <walter.agazzi@protonmail.com>
2022-09-24 19:30:39 +0200

    GLK: AGT: First attempt to fix TMD Thief, and room handling in general

--- engines/glk/agt/agtread.cpp
+++ engines/glk/agt/agtread.cpp
@@ -1048,7 +1048,7 @@ noun inside information; this is used by agtout */
 	if (aver == AGTCOS)
 		MaxQuestion = 10;
 	if (aver == AGT15 || aver == AGT15F)
-		MaxQuestion = 57;
+		MaxQuestion = 41;
 	first_room = 2;
 	if (ver == 1) {
 		first_noun = 200;
@@ -1274,6 +1274,16 @@ static int try_read_da1(fc_type fc, genfile fda1, rbool diag)
 
 		for (i = 0; i < numglobal; i++)
 			globalnoun[i] = readdict(fda1); /* Global nouns */
+
+	} else if (aver >= AGT15) {
+
+		(void)readdict(fda1);
+
+		report("Reading flag nouns",fda1);
+
+		for(i = 0; i < MAX_FLAG_NOUN; i++)
+			flag_noun[i] = readdict(fda1);
+
 	} else
 		for (i = 0; i < MAX_FLAG_NOUN; i++)
 			flag_noun[i] = 0;
@@ -1661,7 +1671,7 @@ static void finish_read(rbool cleanup)
 	rfree(t_songlist);
 	rfree(t_fontlist);
 
-	/* Reallocate questions and asnwers to only use the space that they need */
+	/* Reallocate questions and answers to only use the space that they need */
 	if (!RAW_CMD_OUT && top_quest < MaxQuestion) {
 		MaxQuestion = top_quest; /* top_quest is computed by fixcmd */
 		if (top_quest == 0) {
@@ -1678,6 +1688,18 @@ static void finish_read(rbool cleanup)
 				quest_ptr = (descr_ptr *)rrealloc(quest_ptr, top_quest * sizeof(descr_ptr));
 			if (ans_ptr != nullptr)
 				ans_ptr = (descr_ptr *)rrealloc(ans_ptr, top_quest * sizeof(descr_ptr));
+		}
+	}
+
+	int flagnum = 1;
+	for (i = 0; i <MAX_FLAG_NOUN; i++) {
+		if (flag_noun[i] != 0) {
+			int j;
+			for (j = 0; j <= maxnoun-first_noun; j++) {
+				if (noun[j].name == flag_noun[i])
+					noun[j].flagnum = flagnum;
+			}
+		flagnum++;
 		}
 	}
 }
