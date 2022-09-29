scummvm/scummvm

commit c39ea9499bdc600aba16f81898fc298d0eec72f6

TWINE: depth sorting issue for doors
https://bugs.scummvm.org/ticket/12085

engines/twine/shared.h
@@ -617,6 +617,11 @@ struct TwineImage {
 #define ANGLE_1 5 // 1.75
 #define ANGLE_0 0
 
+#define VIEW_X0 (-50)
+#define VIEW_Y0 (-30)
+#define VIEW_X1(engine) ((engine)->width() - 40)
+#define VIEW_Y1(engine) ((engine)->height() + 100)
+
 inline int32 NormalizeAngle(int32 angle) {
 	if (angle < -ANGLE_180) {
 		angle += ANGLE_360;

engines/twine/renderer/redraw.cpp
@@ -199,7 +199,7 @@ int32 Redraw::fillActorDrawingList(DrawListStruct *drawList, bool bgRedraw) {
 			// get actor position on screen
 			const IVec3 &projPos = _engine->_renderer->projectPositionOnScreen(actor->pos() - _engine->_grid->_camera);
 			// check if actor is visible on screen, otherwise don't display it
-			if (projPos.x > -50 && projPos.x < _engine->width() + 40 && projPos.y > -30 && projPos.y < _engine->height() + 100) {
+			if (projPos.x > VIEW_X0 && projPos.x < VIEW_X1(_engine) && projPos.y > VIEW_Y0 && projPos.y < VIEW_Y1(_engine)) {
 				actor->_dynamicFlags.bIsDrawn = 1;
 			}
 			continue;
@@ -212,9 +212,9 @@ int32 Redraw::fillActorDrawingList(DrawListStruct *drawList, bool bgRedraw) {
 		const IVec3 &projPos = _engine->_renderer->projectPositionOnScreen(actor->pos() - _engine->_grid->_camera);
 
 		if ((actor->_staticFlags.bUsesClipping && projPos.x > -112 && projPos.x < _engine->width() + 112 && projPos.y > -50 && projPos.y < _engine->height() + 171) ||
-		    ((!actor->_staticFlags.bUsesClipping) && projPos.x > -50 && projPos.x < _engine->width() + 40 && projPos.y > -30 && projPos.y < _engine->height() + 100)) {
+		    ((!actor->_staticFlags.bUsesClipping) && projPos.x > VIEW_X0 && projPos.x < VIEW_X1(_engine) && projPos.y > VIEW_Y0 && projPos.y < VIEW_Y1(_engine))) {
 
-			int32 ztri = actor->_pos.z + actor->_pos.x - _engine->_grid->_camera.x - _engine->_grid->_camera.z;
+			int32 ztri = actor->_pos.x - _engine->_grid->_camera.x + actor->_pos.z - _engine->_grid->_camera.z;
 
 			// if actor is above another actor
 			if (actor->_carryBy != -1) {
@@ -229,7 +229,7 @@ int32 Redraw::fillActorDrawingList(DrawListStruct *drawList, bool bgRedraw) {
 					ztri = actor->_animStep.x - _engine->_grid->_camera.x + actor->_animStep.z - _engine->_grid->_camera.z;
 				}
 			} else {
-				drawList[drawListPos].type = 0;
+				drawList[drawListPos].type = DrawListType::DrawObject3D;
 				drawList[drawListPos].actorIdx = a;
 			}
 
@@ -265,7 +265,7 @@ int32 Redraw::fillActorDrawingList(DrawListStruct *drawList, bool bgRedraw) {
 	return drawListPos;
 }
 
-int32 Redraw::fillExtraDrawingList(DrawListStruct *drawList, int32 drawListPos) {
+int32 Redraw::fillExtraDrawingList(DrawListStruct *drawList, int32 drawListPos) { // part of AffScene
 	for (int32 i = 0; i < EXTRA_MAX_ENTRIES; i++) {
 		ExtraListStruct *extra = &_engine->_extra->_extraList[i];
 		if (extra->sprite == -1) {
@@ -279,10 +279,10 @@ int32 Redraw::fillExtraDrawingList(DrawListStruct *drawList, int32 drawListPos) 
 			}
 			continue;
 		}
-		if ((extra->type & ExtraType::TIME_OUT) || (extra->type & ExtraType::FLASH) || (extra->payload.lifeTime + extra->spawnTime - 150 < _engine->_lbaTime) || (!((_engine->_lbaTime + extra->spawnTime) & 8))) {
+		if ((extra->type & ExtraType::TIME_OUT) || (extra->type & ExtraType::FLASH) || (extra->payload.lifeTime + extra->spawnTime - TO_SECONDS(3) < _engine->_lbaTime) || (!((_engine->_lbaTime + extra->spawnTime) & 8))) {
 			const IVec3 &projPos = _engine->_renderer->projectPositionOnScreen(extra->pos - _engine->_grid->_camera);
 
-			if (projPos.x > -50 && projPos.x < _engine->width() + 40 && projPos.y > -30 && projPos.y < _engine->height() + 100) {
+			if (projPos.x > VIEW_X0 && projPos.x < VIEW_X1(_engine) && projPos.y > VIEW_Y0 && projPos.y < VIEW_Y1(_engine)) {
 				const int16 tmpVal = extra->pos.x - _engine->_grid->_camera.x + extra->pos.z - _engine->_grid->_camera.z;
 				drawList[drawListPos].posValue = tmpVal;
 				drawList[drawListPos].actorIdx = i;
@@ -478,6 +478,123 @@ void Redraw::processDrawListExtras(const DrawListStruct &drawCmd) {
 		// show clipping area
 		//drawRectBorders(renderRect);
 		_engine->_interface->resetClip();
+	}
+}
+
+void Redraw::correctZLevels(DrawListStruct *drawList, int32 drawListPos) {
+	ActorStruct *ptrobj = _engine->_scene->getActor(OWN_ACTOR_SCENE_INDEX);
+	if (ptrobj->_staticFlags.bIsHidden || ptrobj->_body == -1) {
+		return;
+	}
+
+	IVec3 tmin = ptrobj->pos() + ptrobj->_boundingBox.mins;
+	IVec3 tmax = ptrobj->pos() + ptrobj->_boundingBox.maxs;
+	int32 twinsenpos = -1;
+	int32 twinsenz = -1;
+	for (int32 pos = 0; pos < drawListPos; ++pos) {
+		DrawListStruct &drawCmd = drawList[pos];
+		if (drawCmd.type == DrawListType::DrawObject3D && drawCmd.actorIdx == OWN_ACTOR_SCENE_INDEX) {
+			twinsenpos = pos;
+			twinsenz = drawCmd.posValue;
+			break;
+		}
+	}
+
+	if (twinsenpos == -1) {
+		return;
+	}
+
+	for (int32 n = 0; n < drawListPos; ++n) {
+		DrawListStruct &ptrtri = drawList[n];
+		uint32 typeobj = ptrtri.type;
+		int32 numobj = ptrtri.actorIdx;
+		ptrobj = _engine->_scene->getActor(numobj);
+		switch (typeobj) {
+		default:
+			break;
+		case DrawListType::DrawActorSprites:
+			if (ptrobj->_staticFlags.bUsesClipping) {
+				IVec3 pmin = ptrobj->_animStep + ptrobj->_boundingBox.mins;
+				IVec3 pmax = ptrobj->_animStep + ptrobj->_boundingBox.maxs;
+
+				if (pmax.x > tmin.x && pmin.x < tmax.x) {
+					if (pmax.z <= tmin.z) {
+						// twinsen after
+						if (twinsenz < ptrtri.posValue) {
+							// correct the error
+							drawList[twinsenpos].posValue = ptrtri.posValue;
+							drawList[twinsenpos].actorIdx = ptrtri.actorIdx;
+							drawList[twinsenpos].type = ptrtri.type;
+
+							ptrtri.actorIdx = OWN_ACTOR_SCENE_INDEX;
+							ptrtri.type = DrawListType::DrawObject3D;
+							ptrtri.posValue = (int16)twinsenz;
+
+							twinsenpos = n;
+							numobj = -1;
+							break;
+						}
+					}
+					if (pmin.z >= tmax.z) {
+						// twinsen before
+						if (twinsenz > ptrtri.posValue) {
+							// correct the error
+							drawList[twinsenpos].posValue = ptrtri.posValue;
+							drawList[twinsenpos].actorIdx = ptrtri.actorIdx;
+							drawList[twinsenpos].type = ptrtri.type;
+
+							ptrtri.actorIdx = OWN_ACTOR_SCENE_INDEX;
+							ptrtri.type = DrawListType::DrawObject3D;
+							ptrtri.posValue = (int16)twinsenz;
+
+							twinsenpos = n;
+							numobj = -1;
+							break;
+						}
+					}
+				}
+
+				if (pmax.z > tmin.z && pmin.z < tmax.z) {
+					if (pmax.x <= tmin.x) {
+						// twinsen after
+						if (twinsenz < ptrtri.posValue) {
+							// correct the error
+							drawList[twinsenpos].posValue = ptrtri.posValue;
+							drawList[twinsenpos].actorIdx = ptrtri.actorIdx;
+							drawList[twinsenpos].type = ptrtri.type;
+
+							ptrtri.actorIdx = OWN_ACTOR_SCENE_INDEX;
+							ptrtri.type = DrawListType::DrawObject3D;
+							ptrtri.posValue = (int16)twinsenz;
+
+							twinsenpos = n;
+							numobj = -1;
+							break;
+						}
+					} else {
+						// twinsen before
+						if (twinsenz > ptrtri.posValue) {
+							// correct the error
+							drawList[twinsenpos].posValue = ptrtri.posValue;
+							drawList[twinsenpos].actorIdx = ptrtri.actorIdx;
+							drawList[twinsenpos].type = ptrtri.type;
+
+							ptrtri.actorIdx = OWN_ACTOR_SCENE_INDEX;
+							ptrtri.type = DrawListType::DrawObject3D;
+							ptrtri.posValue = (int16)twinsenz;
+
+							twinsenpos = n;
+							numobj = -1;
+							break;
+						}
+					}
+				}
+			}
+			break;
+		}
+		if (numobj == -1) {
+			break;
+		}
 	}
 }
 
@@ -698,12 +815,13 @@ void Redraw::redrawEngineActions(bool bgRedraw) { // AffScene
 		blitBackgroundAreas();
 	}
 
-	DrawListStruct drawList[150];
+	DrawListStruct drawList[NUM_MAX_ACTORS + EXTRA_MAX_ENTRIES]; // ListTri[MAX_OBJECTS + MAX_EXTRAS]
 	int32 drawListPos = fillActorDrawingList(drawList, bgRedraw);
 	drawListPos = fillExtraDrawingList(drawList, drawListPos);
 	sortDrawingList(drawList, drawListPos);
 
 	_currNumOfRedrawBox = 0;
+	correctZLevels(drawList, drawListPos);
 	processDrawList(drawList, drawListPos, bgRedraw);
 
 	if (_engine->_cfgfile.Debug) {

engines/twine/renderer/redraw.h
@@ -128,6 +128,7 @@ private:
 
 	int32 fillActorDrawingList(DrawListStruct *drawList, bool bgRedraw);
 	int32 fillExtraDrawingList(DrawListStruct *drawList, int32 drawListPos);
+	void correctZLevels(DrawListStruct *drawList, int32 drawListPos);
 	void processDrawList(DrawListStruct *drawList, int32 drawListPos, bool bgRedraw);
 	void renderOverlays();
 	void renderText();
@@ -135,7 +136,7 @@ private:
 public:
 	Redraw(TwinEEngine *engine);
 
-	bool _inSceneryView = false;
+	bool _inSceneryView = false; // FlagMCGA
 
 	/** Request background redraw */
 	bool _firstTime = false;

engines/twine/renderer/renderer.h
@@ -239,7 +239,7 @@ public:
 	void fillVertices(int vtop, int32 vsize, uint8 renderType, uint16 color);
 	void renderPolygons(const CmdRenderPolygon &polygon, Vertex *vertices, int vtop, int vbottom);
 
-	inline IVec3 &projectPositionOnScreen(const IVec3& pos) {
+	inline IVec3 &projectPositionOnScreen(const IVec3& pos) { // ProjettePoint
 		return projectPositionOnScreen(pos.x, pos.y, pos.z);
 	}
 

engines/twine/scene/actor.h
@@ -176,7 +176,7 @@ public:
 	EntityData *_entityDataPtr = nullptr;
 
 	int16 _actorIdx = 0; // own actor index
-	IVec3 _pos;
+	IVec3 _pos; // PosObjX, PosObjY, PosObjZ
 	int32 _strengthOfHit = 0;
 	int32 _hitBy = -1;
 	BonusParameter _bonusParameter;
@@ -231,7 +231,7 @@ public:
 	int32 _spriteActorRotation = 0;
 	uint8 _brickSound = 0U; // CodeJeu
 
-	BoundingBox _boundingBox;
+	BoundingBox _boundingBox; // Xmin, YMin, Zmin, Xmax, Ymax, Zmax
 	ActorMoveStruct _move;
 	AnimTimerDataStruct _animTimerData;
 };

engines/twine/scene/extra.cpp
@@ -231,7 +231,7 @@ int32 Extra::addExtraBonus(int32 x, int32 y, int32 z, int32 xAngle, int32 yAngle
 		extra->type = ExtraType::STOP_COL | ExtraType::TAKABLE | ExtraType::WAIT_SOME_TIME;
 
 		if (type != SPRITEHQR_KEY) {
-			extra->type = ExtraType::TIME_OUT | ExtraType::TAKABLE | ExtraType::FLASH | ExtraType::STOP_COL | ExtraType::WAIT_SOME_TIME;
+			extra->type |= ExtraType::TIME_OUT | ExtraType::FLASH;
 		}
 
 		extra->pos.x = x;

engines/twine/scene/scene.h
@@ -125,7 +125,7 @@ private:
 	void resetScene();
 
 	// the first actor is the own hero
-	ActorStruct _sceneActors[NUM_MAX_ACTORS];
+	ActorStruct _sceneActors[NUM_MAX_ACTORS]; // ListObjet
 	int32 _currentSceneSize = 0;
 	bool _isOutsideScene = false; // lba2
 

commit 105a94c967fa4fa66753ca6ea9997926f7f19e00

TWINE: fixed clipping error
introduced in c39ea9499bdc600aba16f81898fc298d0eec72f6

engines/twine/shared.h
@@ -619,7 +619,7 @@ struct TwineImage {
 
 #define VIEW_X0 (-50)
 #define VIEW_Y0 (-30)
-#define VIEW_X1(engine) ((engine)->width() - 40)
+#define VIEW_X1(engine) ((engine)->width() + 40)
 #define VIEW_Y1(engine) ((engine)->height() + 100)
 
 inline int32 NormalizeAngle(int32 angle) {