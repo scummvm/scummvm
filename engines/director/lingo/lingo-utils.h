1c2835c5a70905a0439b0c72d7df8e7b19ae7562
eientei <einstein95@users.noreply.github.com>
2022-09-29 20:51:15 +1300

    DIRECTOR: Add BUILDBOT to warnings in lingo-utils.h

--- engines/director/lingo/lingo-utils.h
+++ engines/director/lingo/lingo-utils.h
@@ -24,32 +24,32 @@
 
 #define ARGNUMCHECK(n) \
 	if (nargs != (n)) { \
-		warning("%s: expected %d argument%s, got %d", __FUNCTION__, (n), ((n) == 1 ? "" : "s"), nargs); \
+		warning("BUILDBOT: %s: expected %d argument%s, got %d", __FUNCTION__, (n), ((n) == 1 ? "" : "s"), nargs); \
 		g_lingo->dropStack(nargs); \
 		return; \
 	}
 
 #define TYPECHECK(datum,t) \
 	if ((datum).type != (t)) { \
-		warning("%s: %s arg should be of type %s, not %s", __FUNCTION__, #datum, #t, (datum).type2str()); \
+		warning("BUILDBOT: %s: %s arg should be of type %s, not %s", __FUNCTION__, #datum, #t, (datum).type2str()); \
 		return; \
 	}
 
 #define TYPECHECK2(datum, t1, t2)	\
 	if ((datum).type != (t1) && (datum).type != (t2)) { \
-		warning("%s: %s arg should be of type %s or %s, not %s", __FUNCTION__, #datum, #t1, #t2, (datum).type2str()); \
+		warning("BUILDBOT: %s: %s arg should be of type %s or %s, not %s", __FUNCTION__, #datum, #t1, #t2, (datum).type2str()); \
 		return; \
 	}
 
 #define TYPECHECK3(datum, t1, t2, t3)	\
 	if ((datum).type != (t1) && (datum).type != (t2) && (datum).type != (t3)) { \
-		warning("%s: %s arg should be of type %s, %s, or %s, not %s", __FUNCTION__, #datum, #t1, #t2, #t3, (datum).type2str()); \
+		warning("BUILDBOT: %s: %s arg should be of type %s, %s, or %s, not %s", __FUNCTION__, #datum, #t1, #t2, #t3, (datum).type2str()); \
 		return; \
 	}
 
 #define ARRBOUNDSCHECK(idx,array) \
 	if ((idx)-1 < 0 || (idx) > (int)(array).u.farr->arr.size()) { \
-		warning("%s: index out of bounds (%d of %d)", __FUNCTION__, (idx), (array).u.farr->arr.size()); \
+		warning("BUILDBOT: %s: index out of bounds (%d of %d)", __FUNCTION__, (idx), (array).u.farr->arr.size()); \
 		return; \
 	}
 
