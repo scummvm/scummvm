ebb69067a2014cd0d1cbf124aa08f4f5659d2028
Eugene Sandulenko <sev@scummvm.org>
2022-10-05 16:15:43 +0200

    GUI: Make shaders/scalers work mutually exclusive

--- engines/engine.cpp
+++ engines/engine.cpp
@@ -215,11 +215,17 @@ void initCommonGFX() {
 		if (gameDomain->contains("stretch_mode"))
 			g_system->setStretchMode(ConfMan.get("stretch_mode").c_str());
 
-		if (gameDomain->contains("scaler") || gameDomain->contains("scale_factor"))
-			g_system->setScaler(ConfMan.get("scaler").c_str(), ConfMan.getInt("scale_factor"));
-
-		if (gameDomain->contains("shader"))
-			g_system->setShader(ConfMan.get("shader"));
+		if (ConfMan.hasKey("useshaders") && ConfMan.get("useshaders") == "shaders") {
+			if (gameDomain->contains("shader")) {
+				g_system->setScaler(g_system->getDefaultScaler(), g_system->getDefaultScaleFactor());
+				g_system->setShader(ConfMan.get("shader"));
+			}
+		} else {
+			if (gameDomain->contains("scaler") || gameDomain->contains("scale_factor")) {
+				g_system->setScaler(ConfMan.get("scaler").c_str(), ConfMan.getInt("scale_factor"));
+				g_system->setShader("");
+			}
+		}
 	}
 }
 
