92b746c59cc93feab1434a3906194d0995c825fa
Roland van Laar <roland@rolandvanlaar.nl>
2022-10-08 10:35:03 +0200

    DIRECTOR: LINGO: Fix derefence befe null check
    If movie is there is a check earlier in the function.
    Meaning, it's guarenteed to exist at this point.
    
    Fixes COVERITY: 1498918

--- engines/director/lingo/lingo-the.cpp
+++ engines/director/lingo/lingo-the.cpp
@@ -1635,7 +1635,7 @@ void Lingo::setTheSprite(Datum &id1, int field, Datum &d) {
 		warning("Lingo::setTheSprite(): Unprocessed setting field \"%s\" of sprite", field2str(field));
 	}
 
-	if (channel->_dirty && movie)
+	if (channel->_dirty)
 		movie->getWindow()->addDirtyRect(channel->getBbox());
 }
 
