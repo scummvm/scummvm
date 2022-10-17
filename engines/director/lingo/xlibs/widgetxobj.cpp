b989c490180b2436fab0277362476e2a74569171
Eugene Sandulenko <sev@scummvm.org>
2022-10-17 16:08:58 +0200

    JANITORIAL: Indentation fixes

--- engines/director/lingo/xlibs/widgetxobj.cpp
+++ engines/director/lingo/xlibs/widgetxobj.cpp
@@ -19,15 +19,15 @@
  *
  */
 
-/* 
+/*
  * Widget External Factory
  * Used in: "Alice: An Interactive Museum"
- * 
+ *
  * Widget
  * I      mNew                --Creates a new instance of the XObject
  * X      mDispose            --Disposes of XObject instance
- * S      mGetPro             -- 
- * I      mAskQuit            -- 
+ * S      mGetPro             --
+ * I      mAskQuit            --
  *
  */
 
@@ -41,15 +41,15 @@ namespace Director {
 
 const char *WidgetXObj::xlibName = "widget";
 const char *WidgetXObj::fileNames[] = {
-    "widget",
-    nullptr
+	"widget",
+	nullptr
 };
 
 static MethodProto xlibMethods[] = {
 	{ "new",				WidgetXObj::m_new,			0, 0,	400 },	// D4
-	{ "Dispose",			WidgetXObj::m_dispose,	    0, 0,	400 },	// D4
-	{ "GetPro",		        WidgetXObj::m_getPro,	    0, 0,	400 },	// D4
-	{ "AskQuit",			WidgetXObj::m_askQuit,	    0, 0,	400 },	// D4
+	{ "Dispose",			WidgetXObj::m_dispose,		0, 0,	400 },	// D4
+	{ "GetPro",				WidgetXObj::m_getPro,		0, 0,	400 },	// D4
+	{ "AskQuit",			WidgetXObj::m_askQuit,		0, 0,	400 },	// D4
 	{ nullptr, nullptr, 0, 0, 0 }
 };
 
@@ -78,17 +78,17 @@ void WidgetXObj::m_new(int nargs) {
 
 void WidgetXObj::m_dispose(int nargs) {
 	g_lingo->printSTUBWithArglist("WidgetXObj::m_dispose", nargs);
-    g_lingo->dropStack(nargs);
+	g_lingo->dropStack(nargs);
 }
 
 void WidgetXObj::m_getPro(int nargs) {
-    // seems to want a disk drive letter
-    g_lingo->push(Datum("D"));
+	// seems to want a disk drive letter
+	g_lingo->push(Datum("D"));
 }
 
 void WidgetXObj::m_askQuit(int nargs) {
 	g_lingo->printSTUBWithArglist("WidgetXObj::m_askQuit", nargs);
-    g_lingo->dropStack(nargs);
+	g_lingo->dropStack(nargs);
 }
 
 }
