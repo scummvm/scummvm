4f6076c8e9e0c30d67f0a128f3776d6ef3fedbfd
Paul Gilbert <dreammaster@scummvm.org>
2022-10-19 20:49:36 -0700

    MM: MM1: Added map 12

--- engines/mm/mm1/maps/map12.cpp
+++ engines/mm/mm1/maps/map12.cpp
@@ -29,13 +29,16 @@ namespace MM {
 namespace MM1 {
 namespace Maps {
 
+#define CORRECT 464
+#define SELECTIONS 473
+
 void Map12::special() {
 	// Scan for special actions on the map cell
 	for (uint i = 0; i < _data[50]; ++i) {
 		if (g_maps->_mapOffset == _data[51 + i]) {
 			// Found a specially handled cell, but it
 			// only triggers in designated direction(s)
-			if (g_maps->_forwardMask & _data[75 + i]) {
+			if (g_maps->_forwardMask & _data[69 + i]) {
 				
 				(this->*SPECIAL_FN[i])();
 			} else {
@@ -44,14 +47,174 @@ void Map12::special() {
 			return;
 		}
 	}
-/*
+
 	// All other cells on the map are encounters
 	g_maps->clearSpecial();
 	g_globals->_encounters.execute();
-	*/
 }
 
 void Map12::special00() {
+	send(SoundMessage(
+		STRING["maps.ladder_up"],
+		[]() {
+			g_maps->_mapPos = Common::Point(7, 13);
+			g_maps->changeMap(0x105, 2);
+		}
+	));
+}
+
+void Map12::special01() {
+	Common::String line1 = STRING["maps.map12.lever"];
+	Common::String line2;
+
+	for (int i = 0; i < 9; ++i) {
+		if (!_data[SELECTIONS + i]) {
+			line2 = STRING["maps.map12.wont_budge"];
+			break;
+		}
+	}
+
+	if (!line2.empty()) {
+		send(SoundMessage(0, 1, line1, 0, 2, line2));
+	} else {
+		line2 = STRING["maps.map12.pull_it"];
+		send(SoundMessage(
+			0, 1, line1,
+			0, 2, line2,
+			[]() {
+				Map12 &map = *static_cast<Map12 *>(g_maps->_currentMap);
+				int i;
+				for (i = 0; i < 9; ++i) {
+					if (map[SELECTIONS + i] != map[CORRECT + i])
+						break;
+				}
+
+				if (i < 9) {
+					g_events->send(SoundMessage(STRING["maps.map12.incorrect"]));
+
+				} else {
+					// Reward the party
+					for (i = 0; i < (int)g_globals->_party.size(); ++i) {
+						Character &c = g_globals->_party[i];
+						if (c._intelligence._base < 25) {
+							c._intelligence._current = c._intelligence._base += 2;
+						}
+
+						c._gems = MIN((int)c._gems + 20, 0xffff);
+						c._gold += 200;
+						c._exp += 2000;
+					}
+
+					g_events->send(SoundMessage(STRING["maps.map12.correct"]));
+				}
+			}
+		));
+	}
+}
+
+void Map12::special02() {
+	polyhedron('1', (unsigned char)182);
+}
+
+void Map12::special03() {
+	polyhedron('1', (unsigned char)179);
+}
+
+void Map12::special04() {
+	polyhedron('1', (unsigned char)176);
+}
+
+void Map12::special05() {
+	polyhedron('1', '1');
+}
+
+void Map12::special06() {
+	polyhedron('1', (unsigned char)178);
+}
+
+void Map12::special07() {
+	polyhedron('1', (unsigned char)181);
+}
+
+void Map12::special08() {
+	polyhedron('1', (unsigned char)180);
+}
+
+void Map12::special09() {
+	setPolyhedron(0);
+}
+
+void Map12::special10() {
+	setPolyhedron(1);
+}
+
+void Map12::special11() {
+	setPolyhedron(2);
+}
+
+void Map12::special12() {
+	setPolyhedron(3);
+}
+
+void Map12::special13() {
+	setPolyhedron(4);
+}
+
+void Map12::special14() {
+	setPolyhedron(5);
+}
+
+void Map12::special15() {
+	setPolyhedron(6);
+}
+
+void Map12::special16() {
+	setPolyhedron(7);
+}
+
+void Map12::special17() {
+	setPolyhedron(8);
+}
+
+void Map12::polyhedron(unsigned char side1, unsigned char side2) {
+	Common::String msg = Common::String::format(
+		STRING["maps.map12.polyhedron3"].c_str(), side1, side2);
+	send(SoundMessage(msg));
+}
+
+void Map12::setPolyhedron(int polyIndex) {
+	_polyIndex = polyIndex;
+
+	if (_data[SELECTIONS + polyIndex]) {
+		Common::String msg = Common::String::format(
+			STRING["maps.map12.polyhedron2"].c_str(),
+			_data[SELECTIONS + polyIndex]);
+		send(SoundMessage(
+			msg,
+			[]() {
+				static_cast<Map12 *>(g_maps->_currentMap)->spinPolyhedron(0);
+				g_events->send("Game", GameMessage("UPDATE"));
+			}
+		));
+
+	} else {
+		send(SoundMessage(
+			STRING["maps.map12.polyhedron1"],
+			[](const Common::KeyState &ks) {
+				if (ks.keycode >= Common::KEYCODE_0 &&
+						ks.keycode <= Common::KEYCODE_9) {
+					g_events->close();
+					static_cast<Map12 *>(g_maps->_currentMap)->spinPolyhedron(
+						ks.ascii | 0x80);
+					g_events->addAction(KEYBIND_SEARCH);
+				}	
+			}
+		));
+	}
+}
+
+void Map12::spinPolyhedron(byte newSide) {
+	_data[SELECTIONS + _polyIndex] = newSide;
 }
 
 } // namespace Maps
--- engines/mm/mm1/maps/map12.h
+++ engines/mm/mm1/maps/map12.h
@@ -31,10 +31,48 @@ namespace Maps {
 class Map12 : public Map {
 	typedef void (Map12:: *SpecialFn)();
 private:
-	void special00();
+	int _polyIndex = 0;
 
-	const SpecialFn SPECIAL_FN[1] = {
-		&Map12::special00
+	void special00();
+	void special01();
+	void special02();
+	void special03();
+	void special04();
+	void special05();
+	void special06();
+	void special07();
+	void special08();
+	void special09();
+	void special10();
+	void special11();
+	void special12();
+	void special13();
+	void special14();
+	void special15();
+	void special16();
+	void special17();
+	void polyhedron(unsigned char side1, unsigned char side2);
+	void setPolyhedron(int polyIndex);
+
+	const SpecialFn SPECIAL_FN[18] = {
+		&Map12::special00,
+		&Map12::special01,
+		&Map12::special02,
+		&Map12::special03,
+		&Map12::special04,
+		&Map12::special05,
+		&Map12::special06,
+		&Map12::special07,
+		&Map12::special08,
+		&Map12::special09,
+		&Map12::special10,
+		&Map12::special11,
+		&Map12::special12,
+		&Map12::special13,
+		&Map12::special14,
+		&Map12::special15,
+		&Map12::special16,
+		&Map12::special17
 	};
 public:
 	Map12() : Map(12, "cave8", 0x601) {}
@@ -43,6 +81,11 @@ public:
 	 * Handles all special stuff that happens on the map
 	 */
 	void special() override;
+
+	/**
+	 * Spins a polyhedron
+	 */
+	void spinPolyhedron(byte newSide);
 };
 
 } // namespace Maps
