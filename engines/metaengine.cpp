33ef74c95dcb368647d6e4c1b5ab89681dcf0427
macca8 <brugenerol@tpg.com.au>
2022-10-21 19:02:16 +1100

    ENGINES: Use slot test to test where save is stored
    Also reinstated desc.setAutosave(header.isAutosave) call in querySaveMetaInfos().
    These changes are simply a reversion of the relevant changes made in #6231, including any subsequent changes since then.

--- engines/metaengine.cpp
+++ engines/metaengine.cpp
@@ -370,9 +370,11 @@ SaveStateList MetaEngine::listSaves(const char *target, bool saveMode) const {
 
 	// Check to see if an autosave is present
 	for (SaveStateList::iterator it = saveList.begin(); it != saveList.end(); ++it) {
-		// It has an autosave
-		if (it->isAutosave())
+		int slot = it->getSaveSlot();
+		if (slot == autosaveSlot) {
+			// It has an autosave
 			return saveList;
+		}
 	}
 
 	// No autosave yet. We want to add a dummy one in so that it can be marked as
@@ -428,6 +430,7 @@ SaveStateDescriptor MetaEngine::querySaveMetaInfos(const char *target, int slot)
 		SaveStateDescriptor desc(this, slot, Common::U32String());
 		parseSavegameHeader(&header, &desc);
 		desc.setThumbnail(header.thumbnail);
+		desc.setAutosave(header.isAutosave);
 		return desc;
 	}
 
