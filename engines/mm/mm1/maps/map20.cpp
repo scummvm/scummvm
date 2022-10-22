636bee1461f1c4337f3de39ffe838a327f1af200
Paul Gilbert <dreammaster@scummvm.org>
2022-10-21 21:10:28 -0700

    MM: MM1: Added most of map 20

--- engines/mm/mm1/maps/map20.cpp
+++ engines/mm/mm1/maps/map20.cpp
@@ -29,14 +29,17 @@ namespace MM {
 namespace MM1 {
 namespace Maps {
 
+#define VAL1 177
+
 void Map20::special() {
+	Game::Encounter &enc = g_globals->_encounters;
+
 	// Scan for special actions on the map cell
-	for (uint i = 0; i < _data[50]; ++i) {
+	for (uint i = 0; i < 9; ++i) {
 		if (g_maps->_mapOffset == _data[51 + i]) {
 			// Found a specially handled cell, but it
 			// only triggers in designated direction(s)
-			if (g_maps->_forwardMask & _data[75 + i]) {
-				
+			if (g_maps->_forwardMask & _data[60 + i]) {				
 				(this->*SPECIAL_FN[i])();
 			} else {
 				checkPartyDead();
@@ -44,14 +47,104 @@ void Map20::special() {
 			return;
 		}
 	}
-/*
-	// All other cells on the map are encounters
-	g_maps->clearSpecial();
-	g_globals->_encounters.execute();
-	*/
+
+	if (_states[g_maps->_mapOffset]) {
+		g_maps->clearSpecial();
+		enc.execute();
+
+	} else if (getRandomNumber(100) != 100) {
+		none160();
+
+	} else {
+		int id1 = getRandomNumber(5);
+		int monsterCount = getRandomNumber(13);
+
+		enc.clearMonsters();
+		for (int i = 0; i < monsterCount; ++i)
+			enc.addMonster(id1, 11);
+
+		enc._flag = true;
+		enc._levelIndex = 80;
+		enc.execute();
+	}
 }
 
 void Map20::special00() {
+	send(SoundMessage(
+		STRING["maps.map20.stairs_down"],
+		[]() {
+			g_maps->_mapPos = Common::Point(1, 15);
+			g_maps->changeMap(0xc03, 1);
+		}
+	));
+}
+
+void Map20::special01() {
+	send(SoundMessage(
+		STRING["maps.map20.cave"],
+		[]() {
+			g_maps->_mapPos = Common::Point(15, 0);
+			g_maps->changeMap(0x51b, 1);
+		}
+	));
+}
+
+void Map20::special02() {
+	if (_data[VAL1] & 0x80) {
+		g_maps->_mapPos = Common::Point(15, 8);
+		g_maps->changeMap(0xa11, 3);
+
+	} else if (_data[VAL1]) {
+		_data[VAL1] = 0;
+
+	} else {
+		send(SoundMessage(
+			STRING["maps.map20.cave"],
+			[](const Common::KeyState &ks) {
+				Map20 &map = *static_cast<Map20 *>(g_maps->_currentMap);
+				if (ks.keycode == Common::KEYCODE_y) {
+					g_events->close();
+					map[VAL1] = 0xff;
+				} else if (ks.keycode == Common::KEYCODE_n) {
+					g_events->close();
+					map[VAL1]++;
+				}
+			}
+		));
+	}
+}
+
+void Map20::special03() {
+	// TODO
+}
+
+void Map20::special04() {
+	send(SoundMessage(STRING["maps.map20.sign1"]));
+}
+
+void Map20::special05() {
+	send(SoundMessage(STRING["maps.map20.sign2"]));
+}
+
+void Map20::special06() {
+	for (uint i = 0; i < g_globals->_party.size(); ++i) {
+		g_globals->_currCharacter = &g_globals->_party[i];
+		g_globals->_currCharacter->_flags[5] |= CHARFLAG5_2;
+	}
+
+	SoundMessage info(0, 0, STRING["maps.map20.peak"]);
+	info._largeMessage = true;
+	send(info);
+}
+
+void Map20::special07() {
+	g_maps->_mapPos.y = 6;
+	g_events->send("Game", GameMessage("UPDATE"));
+}
+
+void Map20::special08() {
+	g_maps->_mapPos.x = 9;
+	g_events->send("Game", GameMessage("UPDATE"));
 }
 
 } // namespace Maps
