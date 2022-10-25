f0a699a87e0182d8c4fb3dd770efe55084a3c734
lolbot-iichan <lolbot_iichan@mail.ru>
2022-10-25 23:19:13 +0300

    JANITORIAL: Fix formatting

--- engines/wintermute/wintermute.cpp
+++ engines/wintermute/wintermute.cpp
@@ -439,10 +439,8 @@ bool WintermuteEngine::getGameInfo(const Common::FSList &fslist, Common::String 
 					}
 					caption = value;
 
-					for (uint i = 0; i< value.size(); i++)
-					{
-						if ( int(value[i]) < 16 || int(value[i]) >= 127 )
-						{
+					for (uint i = 0; i< value.size(); i++) {
+						if ( int(value[i]) < 16 || int(value[i]) >= 127 ) {
 							caption = "(invalid)";
 						}
 					}
