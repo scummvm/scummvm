9fef3c7b72709e2fed40b16d269c26fbc33dd8a0
Simon Delamarre <simon.delamarre14@gmail.com>
2022-10-29 00:43:40 +0200

    GOB: add save handler stubs for Adibou1
    Remove a blocking "floppy disk error" screen on some versions.

--- engines/gob/gob.cpp
+++ engines/gob/gob.cpp
@@ -643,7 +643,7 @@ Common::Error GobEngine::initGameParts() {
 		_map      = new Map_v2(this);
 		_goblin   = new Goblin_v2(this);
 		_scenery  = new Scenery_v2(this);
-		_saveLoad = new SaveLoad_v2(this, _targetName.c_str());
+		_saveLoad = new SaveLoad_Adibou1(this, _targetName.c_str());
 		break;
 
 	case kGameTypeAbracadabra:
