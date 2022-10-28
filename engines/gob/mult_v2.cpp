76d07e01a198abe337064113611400f2efdeec84
Simon Delamarre <simon.delamarre14@gmail.com>
2022-10-29 00:43:38 +0200

    GOB: fix disappearing objects in Adibou2 after video play
    The video slot was sometimes not closed, preventing the mult object to be reused with a static image.

--- engines/gob/mult_v2.cpp
+++ engines/gob/mult_v2.cpp
@@ -791,14 +791,14 @@ void Mult_v2::newCycleAnim(Mult_Object &animObj) {
 	case 7:
 		animData.frame--;
 		animData.isPaused = 1;
-/*
+
 		if ((animData.animation < 0) && (animObj.videoSlot > 0)) {
 			if (_vm->_vidPlayer->getFlags(animObj.videoSlot - 1) & 0x1000) {
 				_vm->_vidPlayer->closeVideo(animObj.videoSlot - 1);
 				animObj.videoSlot = 0;
 			}
 		}
-*/
+
 		break;
 
 	case 10:
