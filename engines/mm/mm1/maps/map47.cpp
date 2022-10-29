284bed8afa6fb360c36802d30693f3f23e8d70e4
Paul Gilbert <dreammaster@scummvm.org>
2022-10-28 22:09:59 -0700

    MM: MM1: Added map 47

--- engines/mm/mm1/maps/map47.cpp
+++ engines/mm/mm1/maps/map47.cpp
@@ -29,14 +29,15 @@ namespace MM {
 namespace MM1 {
 namespace Maps {
 
+#define ARRAY1 250
+
 void Map47::special() {
 	// Scan for special actions on the map cell
-	for (uint i = 0; i < _data[50]; ++i) {
+	for (uint i = 0; i < 26; ++i) {
 		if (g_maps->_mapOffset == _data[51 + i]) {
 			// Found a specially handled cell, but it
 			// only triggers in designated direction(s)
-			if (g_maps->_forwardMask & _data[75 + i]) {
-				
+			if (g_maps->_forwardMask & _data[77 + i]) {				
 				(this->*SPECIAL_FN[i])();
 			} else {
 				checkPartyDead();
@@ -44,14 +45,132 @@ void Map47::special() {
 			return;
 		}
 	}
-/*
-	// All other cells on the map are encounters
-	g_maps->clearSpecial();
-	g_globals->_encounters.execute();
-	*/
+
+	if (getRandomNumber(100) < 10) {
+		g_globals->_encounters.execute();
+
+	} else {
+		g_maps->_mapPos = Common::Point(
+			getRandomNumber(16) - 1, getRandomNumber(16) - 1);
+		updateGame();
+		send(SoundMessage(STRING["maps.map47.poof"]));
+	}
 }
 
 void Map47::special00() {
+	send(SoundMessage(STRING["maps.map47.message5"]));
+}
+
+void Map47::special01() {
+	send(SoundMessage(
+		STRING["maps.map47.gong"],
+		[]() {
+			Map47 &map = *static_cast<Map47 *>(g_maps->_currentMap);
+			int toneNum = getRandomNumber(6);
+			if (toneNum < 4) {
+				g_events->send(SoundMessage(STRING[
+					Common::String::format("maps.map47.tones.%d", toneNum)]));
+			} else {
+				map.poof();
+			}
+		}
+	));
+}
+
+void Map47::special02() {
+	if (_data[ARRAY1] && _data[ARRAY1 + 1] && _data[ARRAY1 + 2]) {
+		send(SoundMessage(STRING["maps.map47.clerics3"]));
+		for (uint i = 0; i < g_globals->_party.size(); ++i) {
+			Character &c = g_globals->_party[i];
+			c._flags[11] = CHARFLAG11_CLERICS;
+		}
+	}
+}
+
+void Map47::special03() {
+	send(SoundMessage(
+		STRING["maps.stairs_up"],
+		[]() {
+			g_maps->changeMap(0xa00, 3);
+		}
+	));
+}
+
+void Map47::special04() {
+	send(SoundMessage(STRING["maps.map47.painting"]));
+}
+
+void Map47::special05() {
+	encounter(&_data[587], &_data[594]);
+}
+
+void Map47::special06() {
+	encounter(&_data[601], &_data[608]);
+}
+
+void Map47::special07() {
+	encounter(&_data[615], &_data[624]);
+}
+
+void Map47::special08() {
+	encounter(&_data[633], &_data[644]);
+}
+
+void Map47::special09() {
+	encounter(&_data[655], &_data[665]);
+}
+
+void Map47::special10() {
+	encounter(&_data[675], &_data[684]);
+}
+
+void Map47::special11() {
+	encounter(&_data[693], &_data[700]);
+}
+
+void Map47::special12() {
+	encounter(&_data[707], &_data[718]);
+}
+
+void Map47::special13() {
+	encounter(&_data[729], &_data[740]);
+}
+
+void Map47::special14() {
+	encounter(&_data[751], &_data[762]);
+}
+
+void Map47::special15() {
+	encounter(&_data[773], &_data[782]);
+}
+
+void Map47::special16() {
+	encounter(&_data[791], &_data[801]);
+}
+
+void Map47::special17() {
+	encounter(&_data[811], &_data[821]);
+}
+
+void Map47::special23() {
+	Common::String line = Common::String::format(
+		STRING["maps.map47.door_number"].c_str(),
+		'0' + (g_maps->_mapPos.x - 5)
+	);
+	send(SoundMessage(line));
+}
+
+void Map47::poof() {
+	if (getRandomNumber(100) < 10) {
+		g_globals->_encounters.execute();
+
+	} else {
+		g_maps->_mapPos = Common::Point(
+			getRandomNumber(16) - 1, getRandomNumber(16) - 1);
+		updateGame();
+
+		send(SoundMessage(STRING["maps.map47.poof"]));
+	}
 }
 
 } // namespace Maps
--- engines/mm/mm1/maps/map47.h
+++ engines/mm/mm1/maps/map47.h
@@ -32,9 +32,52 @@ class Map47 : public Map {
 	typedef void (Map47:: *SpecialFn)();
 private:
 	void special00();
+	void special01();
+	void special02();
+	void special03();
+	void special04();
+	void special05();
+	void special06();
+	void special07();
+	void special08();
+	void special09();
+	void special10();
+	void special11();
+	void special12();
+	void special13();
+	void special14();
+	void special15();
+	void special16();
+	void special17();
+	void special23();
 
-	const SpecialFn SPECIAL_FN[1] = {
-		&Map47::special00
+	const SpecialFn SPECIAL_FN[26] = {
+		&Map47::special00,
+		&Map47::special01,
+		&Map47::special02,
+		&Map47::special03,
+		&Map47::special04,
+		&Map47::special05,
+		&Map47::special06,
+		&Map47::special07,
+		&Map47::special08,
+		&Map47::special09,
+		&Map47::special10,
+		&Map47::special11,
+		&Map47::special12,
+		&Map47::special13,
+		&Map47::special14,
+		&Map47::special15,
+		&Map47::special16,
+		&Map47::special17,
+		&Map47::special01,
+		&Map47::special01,
+		&Map47::special01,
+		&Map47::special01,
+		&Map47::special01,
+		&Map47::special23,
+		&Map47::special23,
+		&Map47::special23
 	};
 public:
 	Map47() : Map(47, "udrag3", 0x705) {}
@@ -43,6 +86,8 @@ public:
 	 * Handles all special stuff that happens on the map
 	 */
 	void special() override;
+
+	void poof();
 };
 
 } // namespace Maps
