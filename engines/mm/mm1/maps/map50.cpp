79ff7b710975bb5f50258002b407d3e33043927b
Paul Gilbert <dreammaster@scummvm.org>
2022-10-29 11:59:23 -0700

    MM: MM1: Added map 50

--- engines/mm/mm1/maps/map50.cpp
+++ engines/mm/mm1/maps/map50.cpp
@@ -31,12 +31,11 @@ namespace Maps {
 
 void Map50::special() {
 	// Scan for special actions on the map cell
-	for (uint i = 0; i < _data[50]; ++i) {
+	for (uint i = 0; i < 24; ++i) {
 		if (g_maps->_mapOffset == _data[51 + i]) {
 			// Found a specially handled cell, but it
 			// only triggers in designated direction(s)
-			if (g_maps->_forwardMask & _data[75 + i]) {
-				
+			if (g_maps->_forwardMask & _data[75 + i]) {	
 				(this->*SPECIAL_FN[i])();
 			} else {
 				checkPartyDead();
@@ -44,14 +43,132 @@ void Map50::special() {
 			return;
 		}
 	}
-/*
+
 	// All other cells on the map are encounters
 	g_maps->clearSpecial();
 	g_globals->_encounters.execute();
-	*/
 }
 
 void Map50::special00() {
+	send(SoundMessage(STRING["maps.map50.message7"]));
+}
+
+void Map50::special01() {
+	send(SoundMessage(
+		STRING["maps.map50.passage"],
+		[]() {
+			g_maps->_mapPos = Common::Point(10, 5);
+			g_maps->changeMap(0x11b, 2);
+		}
+	));
+}
+
+void Map50::special02() {
+	send(SoundMessage(
+		STRING["maps.stairs_down"],
+		[]() {
+			g_maps->changeMap(0x701, 3);
+		}
+	));
+}
+
+void Map50::special03() {
+	send(SoundMessage(
+		STRING["maps.stairs_down"],
+		[]() {
+			g_maps->changeMap(0xe00, 3);
+		}
+	));
+}
+
+void Map50::special04() {
+	send(SoundMessage(
+		STRING["maps.stairs_down"],
+		[]() {
+			g_maps->changeMap(0x201, 3);
+		}
+	));
+}
+
+void Map50::special05() {
+	encounter(&_data[427], &_data[440]);
+}
+
+void Map50::special06() {
+	encounter(&_data[453], &_data[464]);
+}
+
+void Map50::special07() {
+	encounter(&_data[475], &_data[486]);
+}
+
+void Map50::special08() {
+	encounter(&_data[497], &_data[500]);
+}
+
+void Map50::special09() {
+	encounter(&_data[503], &_data[516]);
+}
+
+void Map50::special10() {
+	encounter(&_data[529], &_data[542]);
+}
+
+void Map50::special11() {
+	encounter(&_data[555], &_data[569]);
+}
+
+void Map50::special12() {
+	encounter(&_data[583], &_data[597]);
+}
+
+void Map50::special13() {
+	encounter(&_data[611], &_data[626]);
+}
+
+void Map50::special14() {
+	encounter(&_data[641], &_data[647]);
+}
+
+void Map50::special15() {
+	encounter(&_data[653], &_data[664]);
+}
+
+void Map50::special16() {
+	send(SoundMessage(STRING["maps.map50.sign1"]));
+}
+
+void Map50::special17() {
+	send(SoundMessage(STRING["maps.map50.sign2"]));
+}
+
+void Map50::special18() {
+	send(SoundMessage(STRING["maps.map50.sign3"]));
+}
+
+void Map50::special19() {
+	send(SoundMessage(STRING["maps.map50.sign4"]));
+}
+
+void Map50::special20() {
+	send(SoundMessage(STRING["maps.map50.sign5"]));
+}
+
+void Map50::special21() {
+	send(SoundMessage(STRING["maps.map50.sign6"]));
+}
+
+void Map50::special22() {
+	send(SoundMessage(STRING["maps.map50.sign6"]));
+}
+
+void Map50::special23() {
+	if (!(g_globals->_party.hasItem(CRYSTAL_KEY_ID))) {
+		g_maps->_mapPos.y--;
+		updateGame();
+
+		send(SoundMessage(STRING["maps.map50.grate"]));
+	}
 }
 
 } // namespace Maps
--- engines/mm/mm1/maps/map50.h
+++ engines/mm/mm1/maps/map50.h
@@ -32,9 +32,55 @@ class Map50 : public Map {
 	typedef void (Map50:: *SpecialFn)();
 private:
 	void special00();
+	void special01();
+	void special02();
+	void special03();
+	void special04();
+	void special05();
+	void special06();
+	void special07();
+	void special08();
+	void special09();
+	void special10();
+	void special11();
+	void special12();
+	void special13();
+	void special14();
+	void special15();
+	void special16();
+	void special17();
+	void special18();
+	void special19();
+	void special20();
+	void special21();
+	void special22();
+	void special23();
 
-	const SpecialFn SPECIAL_FN[1] = {
-		&Map50::special00
+	const SpecialFn SPECIAL_FN[24] = {
+		&Map50::special00,
+		&Map50::special01,
+		&Map50::special02,
+		&Map50::special03,
+		&Map50::special04,
+		&Map50::special05,
+		&Map50::special06,
+		&Map50::special07,
+		&Map50::special08,
+		&Map50::special09,
+		&Map50::special10,
+		&Map50::special11,
+		&Map50::special12,
+		&Map50::special13,
+		&Map50::special14,
+		&Map50::special15,
+		&Map50::special16,
+		&Map50::special17,
+		&Map50::special18,
+		&Map50::special19,
+		&Map50::special20,
+		&Map50::special21,
+		&Map50::special22,
+		&Map50::special23
 	};
 public:
 	Map50() : Map(50, "pp1", 0xF01) {}
