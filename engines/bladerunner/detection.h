53a70986ac7803a168531aa781517f224f651cf3
Cameron Cawley <ccawley2011@gmail.com>
2022-11-03 18:46:40 +0000

    BLADERUNNER: Move the engine options into the MetaEngine subclass

---
+++ engines/bladerunner/detection.h
@@ -0,0 +1,32 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef BLADERUNNER_DETECTION_H
+#define BLADERUNNER_DETECTION_H
+
+#define GAMEOPTION_SITCOM                     GUIO_GAMEOPTIONS1
+#define GAMEOPTION_SHORTY                     GUIO_GAMEOPTIONS2
+#define GAMEOPTION_FRAMELIMITER_NODELAYMILLIS GUIO_GAMEOPTIONS3
+#define GAMEOPTION_FRAMELIMITER_FPS           GUIO_GAMEOPTIONS4
+#define GAMEOPTION_DISABLE_STAMINA_DRAIN      GUIO_GAMEOPTIONS5
+#define GAMEOPTION_SHOW_SUBS_IN_CRAWL         GUIO_GAMEOPTIONS6
+
+#endif
