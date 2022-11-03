209e650ff05d5be36c75e93fc5ab9cbeb34f0ae9
Cameron Cawley <ccawley2011@gmail.com>
2022-11-03 21:37:50 +0000

    SHERLOCK: Move the engine options into the MetaEngine subclass

--- engines/sherlock/metaengine.cpp
+++ engines/sherlock/metaengine.cpp
@@ -25,11 +25,101 @@
 #include "sherlock/tattoo/tattoo.h"
 
 #include "common/system.h"
+#include "common/translation.h"
 #include "engines/advancedDetector.h"
 
 #include "sherlock/detection.h"
 
 namespace Sherlock {
+
+static const ADExtraGuiOptionsMap optionsList[] = {
+	{
+		GAMEOPTION_ORIGINAL_SAVES,
+		{
+			_s("Use original save/load screens"),
+			_s("Use the original save/load screens instead of the ScummVM ones"),
+			"originalsaveload",
+			false,
+			0,
+			0
+		}
+	},
+
+	{
+		GAMEOPTION_FADE_STYLE,
+		{
+			_s("Pixellated scene transitions"),
+			_s("When changing scenes, a randomized pixel transition is done"),
+			"fade_style",
+			true,
+			0,
+			0
+		}
+	},
+
+	{
+		GAMEOPTION_HELP_STYLE,
+		{
+			_s("Don't show hotspots when moving mouse"),
+			_s("Only show hotspot names after you actually click on a hotspot or action button"),
+			"help_style",
+			false,
+			0,
+			0
+		}
+	},
+
+	{
+		GAMEOPTION_PORTRAITS_ON,
+		{
+			_s("Show character portraits"),
+			_s("Show portraits for the characters when conversing"),
+			"portraits_on",
+			true,
+			0,
+			0
+		}
+	},
+
+	{
+		GAMEOPTION_WINDOW_STYLE,
+		{
+			_s("Slide dialogs into view"),
+			_s("Slide UI dialogs into view, rather than simply showing them immediately"),
+			"window_style",
+			true,
+			0,
+			0
+		}
+	},
+
+	{
+		GAMEOPTION_TRANSPARENT_WINDOWS,
+		{
+			_s("Transparent windows"),
+			_s("Show windows with a partially transparent background"),
+			"transparent_windows",
+			true,
+			0,
+			0
+		}
+	},
+
+#ifdef USE_TTS
+	{
+		GAMEOPTION_TTS_NARRATOR,
+		{
+			_s("TTS Narrator"),
+			_s("Use TTS to read the descriptions (if TTS is available)"),
+			"tts_narrator",
+			false,
+			0,
+			0
+		}
+	},
+#endif
+	AD_EXTRA_GUI_OPTIONS_TERMINATOR
+};
 
 GameType SherlockEngine::getGameID() const {
 	return _gameDescription->gameID;
@@ -50,6 +140,10 @@ class SherlockMetaEngine : public AdvancedMetaEngine {
 public:
 	const char *getName() const override {
 		return "sherlock";
+	}
+
+	const ADExtraGuiOptionsMap *getAdvancedExtraGuiOptions() const override {
+		return Sherlock::optionsList;
 	}
 
 	/**
