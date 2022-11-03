0d8c60f081ade83691cd0c4f46d45d626707c496
Cameron Cawley <ccawley2011@gmail.com>
2022-11-03 20:35:47 +0000

    DREAMWEB: Move the engine options into the MetaEngine subclass

--- engines/dreamweb/detection.cpp
+++ engines/dreamweb/detection.cpp
@@ -23,7 +23,6 @@
 
 #include "common/algorithm.h"
 #include "common/system.h"
-#include "common/translation.h"
 #include "common/text-to-speech.h"
 
 #include "engines/advancedDetector.h"
@@ -44,66 +43,11 @@ static const DebugChannelDef debugFlagList[] = {
 
 #include "dreamweb/detection_tables.h"
 
-static const ADExtraGuiOptionsMap gameGuiOptions[] = {
-	{
-		GAMEOPTION_ORIGINAL_SAVELOAD,
-		{
-			_s("Use original save/load screens"),
-			_s("Use the original save/load screens instead of the ScummVM ones"),
-			"originalsaveload",
-			false,
-			0,
-			0
-		}
-	},
-
-	{
-		GAMEOPTION_BRIGHTPALETTE,
-		{
-			_s("Use bright palette mode"),
-			_s("Display graphics using the game's bright palette"),
-			"bright_palette",
-			true,
-			0,
-			0
-		}
-	},
-
-#ifdef USE_TTS
-	{
-		GAMEOPTION_TTS_THINGS,
-		{
-			_s("Enable Text to Speech for Objects, Options, and the Bible Quote"),
-			_s("Use TTS to read the descriptions (if TTS is available)"),
-			"tts_enabled_objects",
-			false,
-			0,
-			0
-		}
-	},
-
-	{
-		GAMEOPTION_TTS_SPEECH,
-		{
-			_s("Enable Text to Speech for Subtitles"),
-			_s("Use TTS to read the subtitles (if TTS is available)"),
-			"tts_enabled_speech",
-			false,
-			0,
-			0
-		}
-	},
-#endif
-
-	AD_EXTRA_GUI_OPTIONS_TERMINATOR
-};
-
 class DreamWebMetaEngineDetection : public AdvancedMetaEngineDetection {
 public:
 	DreamWebMetaEngineDetection():
 	AdvancedMetaEngineDetection(DreamWeb::gameDescriptions,
-	sizeof(DreamWeb::DreamWebGameDescription), dreamWebGames,
-	gameGuiOptions) {
+	sizeof(DreamWeb::DreamWebGameDescription), dreamWebGames) {
 		_guiOptions = GUIO5(GUIO_NOMIDI, GAMEOPTION_ORIGINAL_SAVELOAD, GAMEOPTION_BRIGHTPALETTE, GAMEOPTION_TTS_THINGS, GAMEOPTION_TTS_SPEECH);
 	}
 
