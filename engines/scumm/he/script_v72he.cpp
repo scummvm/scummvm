810f6830f16c9e987170264b42d559b7ac20e418
BLooperZ <blooperz@users.noreply.github.com>
2022-11-15 09:13:58 +0200

    SCUMM: prevent possible issues when range is reversed

--- engines/scumm/he/script_v72he.cpp
+++ engines/scumm/he/script_v72he.cpp
@@ -1174,14 +1174,14 @@ void ScummEngine_v72he::o72_arrayOps() {
 
 		offs = (b >= c) ? 1 : -1;
 		tmp2 = c;
-		tmp3 = c - b + 1;
+		tmp3 = ABS(c - b) + 1;
 		while (dim2start <= dim2end) {
 			tmp = dim1start;
 			while (tmp <= dim1end) {
 				writeArray(array, dim2start, tmp, tmp2);
 				if (--tmp3 == 0) {
 					tmp2 = c;
-					tmp3 = c - b + 1;
+					tmp3 = ABS(c - b) + 1;
 				} else {
 					tmp2 += offs;
 				}
