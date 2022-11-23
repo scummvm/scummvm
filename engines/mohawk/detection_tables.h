af62351225f449801b9f7a90128b039a14809ba8
Donovan Watteau <contrib@dwatteau.fr>
2022-11-23 13:55:20 +0100

    MOHAWK: Fix build

--- engines/mohawk/detection_tables.h
+++ engines/mohawk/detection_tables.h
@@ -877,7 +877,7 @@ static const MohawkGameDescription gameDescriptions[] = {
 			"zoombini",
 			_s("Missing game code"), // Reason for being unsupported,
 			AD_ENTRY1s("ZOOMBINI.MHK", "3133e8d164958f8a3f740cb1f4e49f15", 24120142),
-			Common::JA_JAP,
+			Common::JA_JPN,
 			Common::kPlatformMacintosh,
 			ADGF_UNSUPPORTED | ADGF_DEMO,
 			GUIO1(GUIO_NOASPECT)
