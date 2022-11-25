0c30c442b93fe4ec815179b613bd3586dea969d7
Donovan Watteau <contrib@dwatteau.fr>
2022-11-25 17:53:34 +0100

    SKY: Use MSVC_PRINTF when GCC_PRINTF is used

--- engines/sky/control.h
+++ engines/sky/control.h
@@ -198,7 +198,7 @@ public:
 	void saveDescriptions(const Common::StringArray &list);
 
 private:
-	int displayMessage(const char *altButton, const char *message, ...) GCC_PRINTF(3, 4);
+	int displayMessage(const char *altButton, MSVC_PRINTF const char *message, ...) GCC_PRINTF(3, 4);
 
 	void initPanel();
 	void removePanel();
