d2462684e1227dd019412235d2ffd63cce63d689
Eugene Sandulenko <sev@scummvm.org>
2022-12-02 11:47:13 +0100

    GOB: Added sdelamarre to credits

--- engines/gob/credits.pl
+++ engines/gob/credits.pl
@@ -1,6 +1,7 @@
 begin_section("Gob");
 	add_person("Torbj&ouml;rn Andersson", "eriktorbjorn", "");
 	add_person("Arnaud Boutonn&eacute;", "Strangerke", "");
+	add_person("Simon Delamarre", "sdelamarre", "");
 	add_person("Sven Hesse", "DrMcCoy", "");
 	add_person("Eugene Sandulenko", "sev", "");
 end_section();
