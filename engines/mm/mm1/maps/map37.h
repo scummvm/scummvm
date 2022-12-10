ed5cbdbf0e4a6a8db38d042916c58135e9d36ca9
Paul Gilbert <dreammaster@scummvm.org>
2022-12-09 19:40:43 -0800

    MM: MM1: Desert maps will only be mappable if party has map

--- engines/mm/mm1/maps/map37.h
+++ engines/mm/mm1/maps/map37.h
@@ -73,14 +73,19 @@ private:
 		&Map37::special19
 	};
 public:
-	Map37() : Map(37, "qvl1", 0xf03, 3) {
-		_mappingAllowed = false;
-	}
+	Map37() : Map(37, "qvl1", 0xf03, 3) {}
 
 	/**
 	 * Handles all special stuff that happens on the map
 	 */
 	void special() override;
+
+	/**
+	 * This map has some random spinners, so we want to
+	 * disable mapping in enhanced mode to avoid spoiling
+	 * the surprise
+	 */
+	bool mappingAllowed() const override { return false; }
 };
 
 } // namespace Maps
