1a77e59c5281075b78e71a0131a8578f1e13f57b
Vladimir Serbinenko <phcoder@gmail.com>
2022-12-17 19:50:59 +0100

    SAGA: Load extra palette for ECS
    Amiga ITE switches palette after line 137.
    Additionally options uses a separate palette.
    Rather than jungling with 3 palettes we just load
    all 3 palettes.

--- engines/saga/gfx.h
+++ engines/saga/gfx.h
@@ -145,6 +145,7 @@ public:
 
 	void initPalette();
 	void setPalette(const PalEntry *pal, bool full = false);
+	void loadECSExtraPalettes();
 	void setPaletteColor(int n, int r, int g, int b);
 	void getCurrentPal(PalEntry *src_pal);
 	void savePalette() { getCurrentPal(_savedPalette); }
