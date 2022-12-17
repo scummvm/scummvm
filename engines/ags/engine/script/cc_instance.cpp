scummvm/scummvm

commit dec92f05ea9156961d17d9a7f62f11d73b8dba07

AGS: Support passing partial argument list to a script callback
From upstream e38df03c5d1658a2c44a26ba4ee6f22663c1e701

engines/ags/engine/script/cc_instance.cpp
@@ -284,22 +284,25 @@ int ccInstance::CallScriptFunction(const char *funcname, int32_t numargs, const 
 	int32_t startat = -1;
 	int k;
 	char mangledName[200];
-	sprintf(mangledName, "%s$", funcname);
+	size_t mangled_len = snprintf(mangledName, sizeof(mangledName), "%s$", funcname);
+	int export_args = 0;
 
 	for (k = 0; k < instanceof->numexports; k++) {
 		char *thisExportName = instanceof->exports[k];
 		int match = 0;
 
 		// check for a mangled name match
-		if (strncmp(thisExportName, mangledName, strlen(mangledName)) == 0) {
+		if (strncmp(thisExportName, mangledName, mangled_len) == 0) {
 			// found, compare the number of parameters
-			char *numParams = thisExportName + strlen(mangledName);
-			if (atoi(numParams) != numargs) {
-				cc_error("wrong number of parameters to exported function '%s' (expected %d, supplied %d)", funcname, atoi(numParams), numargs);
+			export_args = atoi(thisExportName + mangled_len);
+			if (export_args > numargs) {
+				cc_error("wrong number of parameters to exported function '%s' (expected %d, supplied %d)",
+					funcname, export_args, numargs);
 				return -1;
 			}
 			match = 1;
 		}
+
 		// check for an exact match (if the script was compiled with
 		// an older version)
 		if ((match == 1) || (strcmp(thisExportName, funcname) == 0)) {
@@ -317,6 +320,9 @@ int ccInstance::CallScriptFunction(const char *funcname, int32_t numargs, const 
 		cc_error("function '%s' not found", funcname);
 		return -2;
 	}
+
+	// Allow to pass less parameters if script callback has less declared args
+	numargs = std::min(numargs, export_args);
 
 	//numargs++;                    // account for return address
 	flags &= ~INSTF_ABORTED;

commit 57728933409f8c89b33f007119d5ce52df41a111

AGS: Engine: fixed CallScriptFunction for the ancient games + added comments
Was broken by dec92f05ea9156961d17d9a7f62f11d73b8dba07
(e38df03c5d1658a2c44a26ba4ee6f22663c1e701 in upstream).

Some games do not have function arg count appended to the function names in
the export table; in that case `export_args` remained uninitialized.

From upstream cdbb7a7367db57e14983015232ecc97f6a3dd212

engines/ags/engine/script/cc_instance.cpp
@@ -313,31 +313,32 @@ int ccInstance::CallScriptFunction(const char *funcname, int32_t numargs, const 
 		return -4;
 	}
 
+	// NOTE: passing more parameters than expected by the function is fine:
+	// the function args are pushed to the stack in REVERSE order, first
+	// parameters are always the last, so function code knows how to find them
+	// using negative offsets, and does not care about any preceding entries.
 	int32_t startat = -1;
-	int k;
 	char mangledName[200];
 	size_t mangled_len = snprintf(mangledName, sizeof(mangledName), "%s$", funcname);
-	int32_t export_args = 0;
+	int export_args = numargs;
 
-	for (k = 0; k < instanceof->numexports; k++) {
+	for (int k = 0; k < instanceof->numexports; k++) {
 		char *thisExportName = instanceof->exports[k];
-		int match = 0;
+		bool match = false;
 
 		// check for a mangled name match
 		if (strncmp(thisExportName, mangledName, mangled_len) == 0) {
 			// found, compare the number of parameters
 			export_args = atoi(thisExportName + mangled_len);
 			if (export_args > numargs) {
-				cc_error("wrong number of parameters to exported function '%s' (expected %d, supplied %d)",
+				cc_error("Not enough parameters to exported function '%s' (expected %d, supplied %d)",
 					funcname, export_args, numargs);
 				return -1;
 			}
-			match = 1;
+			match = true;
 		}
-
-		// check for an exact match (if the script was compiled with
-		// an older version)
-		if ((match == 1) || (strcmp(thisExportName, funcname) == 0)) {
+		// check for an exact match (if the script was compiled with an older version)
+		if (match || (strcmp(thisExportName, funcname) == 0)) {
 			int32_t etype = (instanceof->export_addr[k] >> 24L) & 0x000ff;
 			if (etype != EXPORT_FUNCTION) {
 				cc_error("symbol is not a function");