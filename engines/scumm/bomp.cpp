3774b88a425a305a1b45deb79e6707311e7705bf
BLooperZ <blooperz@users.noreply.github.com>
2022-12-22 00:27:29 +0200

    SCUMM: Optimize BOMP raw copy when transparency is not needed

--- engines/scumm/bomp.cpp
+++ engines/scumm/bomp.cpp
@@ -68,11 +68,18 @@ void bompDecodeLine(byte *dst, const byte *src, int len, bool setZero) {
 				memset(dst, color, num);
 			dst += num;
 		} else {
-			while (num--) {
-				color = *src++;
-				if (setZero || color)
-					*dst = color;
-				dst++;
+			if (setZero) {
+				// Copy optimization when transparency is not needed
+				memcpy(dst, src, num);
+				src += num;
+				dst += num;
+			} else {
+				while (num--) {
+					color = *src++;
+					if (color)
+						*dst = color;
+					dst++;
+				}
 			}
 		}
 	}
