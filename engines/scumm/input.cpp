bc531b373847436f0e4251b8a510e1307f94e445
AndywinXp <andywinxp@gmail.com>
2022-12-27 15:37:30 +0100

    SCUMM: GUI: Allow SHIFT-B to be also used for the buffer shortcut

--- engines/scumm/input.cpp
+++ engines/scumm/input.cpp
@@ -492,7 +492,8 @@ void ScummEngine_v8::processKeyboard(Common::KeyState lastKeyHit) {
 
 void ScummEngine_v7::processKeyboard(Common::KeyState lastKeyHit) {
 	if (isUsingOriginalGUI()) {
-		if (lastKeyHit.keycode == Common::KEYCODE_b && lastKeyHit.hasFlags(Common::KBD_CTRL)) {
+		if (lastKeyHit.keycode == Common::KEYCODE_b &&
+			(lastKeyHit.hasFlags(Common::KBD_CTRL) || lastKeyHit.hasFlags(Common::KBD_SHIFT))) {
 			int curBufferCount = _imuseDigital->roundRobinSetBufferCount();
 			// "iMuse buffer count changed to %d"
 			showBannerAndPause(0, 90, getGUIString(gsIMuseBuffer), curBufferCount);
