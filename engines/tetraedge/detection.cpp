9cdfb0deee3d3c8c4815fb497b3b217d85ff9107
Matthew Duggan <mgithub@guarana.org>
2023-01-08 08:59:11 +0900

    TETRAEDGE: Clean up includes

--- engines/tetraedge/detection.cpp
+++ engines/tetraedge/detection.cpp
@@ -19,13 +19,6 @@
  *
  */
 
-#include "base/plugins.h"
-#include "common/config-manager.h"
-#include "common/file.h"
-#include "common/md5.h"
-#include "common/str-array.h"
-#include "common/translation.h"
-#include "common/util.h"
 #include "tetraedge/detection.h"
 #include "tetraedge/detection_tables.h"
 
