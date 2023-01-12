66202f3a328b3f978ec0a54f8079e55244882ffb
Vladimir Serbinenko <phcoder@gmail.com>
2023-01-12 09:21:26 +0100

    MIYOO: Configure and makefile support for Miyoo mini

--- backends/platform/sdl/miyoo/miyoo.mk
+++ backends/platform/sdl/miyoo/miyoo.mk
@@ -1,7 +1,11 @@
 OD_EXE_STRIPPED := scummvm_stripped$(EXEEXT)
 bundle = sd-root
 
+ifeq ($(MIYOO_TARGET), miyoomini)
+G2X_CATEGORY = App
+else
 G2X_CATEGORY = games
+endif
 
 all: $(OD_EXE_STRIPPED)
 
@@ -30,16 +34,31 @@ ifdef DYNAMIC_MODULES
 	$(CP) $(PLUGINS) $(bundle)/$(G2X_CATEGORY)/scummvm/plugins/
 endif
 	$(CP) $(EXECUTABLE) $(bundle)/$(G2X_CATEGORY)/scummvm/scummvm
+ifeq ($(MIYOO_TARGET), miyoomini)
+	$(CP) $(srcdir)/dists/miyoo/scummvm-miyoomini.png $(bundle)/$(G2X_CATEGORY)/scummvm/scummvm.png
+endif
 	$(CP) $(srcdir)/backends/platform/sdl/miyoo/README.MIYOO $(bundle)/$(G2X_CATEGORY)/scummvm/README.man.txt
 	echo >> $(bundle)/$(G2X_CATEGORY)/scummvm/README.man.txt
 	echo '[General README]' >> $(bundle)/$(G2X_CATEGORY)/scummvm/README.man.txt
 	echo >> $(bundle)/$(G2X_CATEGORY)/scummvm/README.man.txt
 	cat $(srcdir)/README.md | sed -e 's/\[/⟦/g' -e 's/\]/⟧/g' -e '/^1\.1)/,$$ s/^[0-9][0-9]*\.[0-9][0-9]*.*/\[&\]/' >> $(bundle)/$(G2X_CATEGORY)/scummvm/README.man.txt
 	echo '[General README]' >> $(bundle)/$(G2X_CATEGORY)/scummvm/README.man.txt
+ifeq ($(MIYOO_TARGET), miyoomini)
+	$(CP) $(srcdir)/dists/miyoo/launch.miyoomini.sh $(bundle)/$(G2X_CATEGORY)/scummvm/launch.sh
+	$(CP) $(srcdir)/dists/miyoo/config.miyoomini.json $(bundle)/$(G2X_CATEGORY)/scummvm/config.json
+# Workaround for mismatch between SDK and actual device
+	$(CP) /opt/miyoomini-toolchain/arm-linux-gnueabihf/libc/usr/lib/libpng16.so.16.* $(bundle)/$(G2X_CATEGORY)/scummvm/libpng16.so.16
+	$(CP) /opt/miyoomini-toolchain/arm-linux-gnueabihf/libc/usr/lib/libz.so.1.2.11 $(bundle)/$(G2X_CATEGORY)/scummvm/libz.so.1
+else
 	$(MKDIR) -p $(bundle)/gmenu2x/sections/$(G2X_CATEGORY)
 	$(CP) $(srcdir)/dists/miyoo/scummvm.miyoo $(bundle)/gmenu2x/sections/$(G2X_CATEGORY)/scummvm
+endif
 	$(STRIP) $(bundle)/$(G2X_CATEGORY)/scummvm/scummvm
 
 sd-zip: $(bundle)
 	$(RM) scummvm_$(MIYOO_TARGET).zip
+ifeq ($(MIYOO_TARGET), miyoomini)
+	cd $(bundle) && zip -r ../scummvm_$(MIYOO_TARGET).zip $(G2X_CATEGORY)
+else
 	cd $(bundle) && zip -r ../scummvm_$(MIYOO_TARGET).zip $(G2X_CATEGORY) gmenu2x
+endif
