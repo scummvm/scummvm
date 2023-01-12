845c739801f356ce0744e0531e0e5280847da6b1
AndywinXp <andywinxp@gmail.com>
2023-01-12 22:11:12 +0100

    SCUMM: DIMUSE: Avoid using a nullptr stream

--- engines/scumm/imuse_digi/dimuse_tracks.cpp
+++ engines/scumm/imuse_digi/dimuse_tracks.cpp
@@ -261,6 +261,7 @@ void IMuseDigital::tracksLowLatencyCallback() {
 		// We use a separate queue cardinality handling, since SMUSH audio and iMUSE audio can overlap...
 		bool canQueueBufs = (int)_internalMixer->getStream(idx)->numQueuedStreams() < (_maxQueuedStreams + 1);
 		bool canQueueFtSmush = (int)_internalMixer->getStream(-1)->numQueuedStreams() < (_maxQueuedStreams + 1);
+		canQueueFtSmush &= _internalMixer->getStream(-1) != nullptr;
 
 		if (canQueueBufs) {
 			if (track)
