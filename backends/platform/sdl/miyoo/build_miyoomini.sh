4eaf52ce3adca450234ad2db11a96efa5ad912ec
Vladimir Serbinenko <phcoder@gmail.com>
2023-01-12 09:21:26 +0100

    MIYOO: Configure and makefile support for Miyoo mini

---
+++ backends/platform/sdl/miyoo/build_miyoomini.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+TOOLCHAIN=/opt/miyoomini-toolchain
+SYSROOT=$TOOLCHAIN/arm-linux-gnueabihf
+
+export PATH=$TOOLCHAIN/usr/bin:$SYSROOT/usr/include:$TOOLCHAIN/bin:$PATH
+export CXX=arm-linux-gnueabihf-g++
+
+./configure --host=miyoomini --enable-release --disable-detection-full --enable-plugins --default-dynamic --enable-engine=testbed
+
+make -j5 all sd-root sd-zip
