3058bdb8be0c057dab73c15529214d59d9b4ece7
Eugene Sandulenko <sev@scummvm.org>
2023-01-27 15:11:20 +0100

    DIRECTOR: Do not leak memory on failed EXEv4 load

--- engines/director/resource.cpp
+++ engines/director/resource.cpp
@@ -461,6 +461,8 @@ Archive *Window::loadEXERIFX(Common::SeekableReadStream *stream, uint32 offset) 
 		warning("Window::loadEXERIFX(): Failed to load RIFX from EXE");
 		delete result;
 		result = nullptr;
+		delete stream;
+		stream = nullptr;
 	}
 	return result;
 }
