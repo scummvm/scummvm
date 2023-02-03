7daa2b5d85e644cefac039eb412e78d44a1637e4
Vladimir Serbinenko <phcoder@gmail.com>
2023-02-04 00:59:10 +0100

    SHERLOCK: Add rendering of Serrated scalpel chinese texts

--- engines/sherlock/fonts.h
+++ engines/sherlock/fonts.h
@@ -24,6 +24,7 @@
 
 #include "common/rect.h"
 #include "common/platform.h"
+#include "graphics/big5.h"
 #include "graphics/surface.h"
 
 namespace Sherlock {
@@ -34,14 +35,21 @@ class BaseSurface;
 
 class Fonts {
 private:
+	struct ChinaFontCodElement {
+		uint16 a;
+		uint16 b;
+		uint8 c;
+	};
 	static ImageFile *_font;
 	static byte *_chineseFont;
+	static Graphics::Big5Font *_big5Font;
 	static byte _yOffsets[255];
 	static int _fontNumber;
 	static int _fontHeight;
 	static int _widestChar;
 	static uint16 _charCount;
 	static bool _isModifiedEucCn;
+	static bool _isBig5;
 
 	static inline byte translateChar(byte c);
 protected:
@@ -65,6 +73,7 @@ public:
 	static void freeFont();
 
 	static bool isModifiedEucCn() { return _isModifiedEucCn; }
+	static bool isBig5() { return _isBig5; }
 
 	/**
 	 * Set the font to use for writing text on the screen
@@ -96,7 +105,7 @@ public:
 	/**
 	 * Return the font height
 	 */
-	int fontHeight() const { return _chineseFont ? MAX(_fontHeight, 16) : _fontHeight; }
+	int fontHeight() const { return _chineseFont || _isBig5 ? MAX(_fontHeight, 16) : _fontHeight; }
 
 	/**
 	 * Return the width of the widest character in the font
