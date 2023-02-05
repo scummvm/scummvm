65769981dc53991021c3422491d76c785884926b
elasota <ejlasota@gmail.com>
2023-02-05 17:42:23 -0500

    VCRUISE: Initial commit, boilerplate

---
+++ engines/vcruise/credits.pl
@@ -0,0 +1,3 @@
+begin_section("V-Cruise");
+	add_person("Eric Lasota", "OneEightHundred", "");
+end_section();
---
+++ engines/vcruise/detection.cpp
@@ -0,0 +1,105 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "base/plugins.h"
+
+#include "common/config-manager.h"
+#include "common/language.h"
+
+#include "engines/advancedDetector.h"
+
+#include "vcruise/detection.h"
+
+static const PlainGameDescriptor vCruiseGames[] = {
+	{"reah", "Reah: Face the Unknown"},
+	{"schizm", "Schizm: Mysterious Journey"},
+	{nullptr, nullptr}
+};
+
+#include "vcruise/detection_tables.h"
+
+static const char *directoryGlobs[] = {
+	nullptr
+};
+
+class VCruiseMetaEngineDetection : public AdvancedMetaEngineDetection {
+public:
+	VCruiseMetaEngineDetection() : AdvancedMetaEngineDetection(VCruise::gameDescriptions, sizeof(VCruise::VCruiseGameDescription), vCruiseGames) {
+		_guiOptions = GUIO1(GAMEOPTION_LAUNCH_DEBUG);
+		_maxScanDepth = 1;
+		_directoryGlobs = directoryGlobs;
+		_flags = kADFlagCanPlayUnknownVariants;
+	}
+
+	const char *getName() const override {
+		return "vcruise";
+	}
+
+	const char *getEngineName() const override {
+		return "V-Cruise";
+	}
+
+	const char *getOriginalCopyright() const override {
+		return "V-Cruise (C) LK Avalon";
+	}
+
+	DetectedGame toDetectedGame(const ADDetectedGame &adGame, ADDetectedGameExtraInfo *extraInfo) const override {
+		DetectedGame game = AdvancedMetaEngineDetection::toDetectedGame(adGame, extraInfo);
+
+		VCruise::VCruiseGameID gameID = reinterpret_cast<const VCruise::VCruiseGameDescription *>(adGame.desc)->gameID;
+
+		if (gameID == VCruise::GID_REAH) {
+			game.appendGUIOptions(Common::getGameGUIOptionsDescriptionLanguage(Common::NL_NLD));
+			game.appendGUIOptions(Common::getGameGUIOptionsDescriptionLanguage(Common::FR_FRA));
+			game.appendGUIOptions(Common::getGameGUIOptionsDescriptionLanguage(Common::IT_ITA));
+			game.appendGUIOptions(Common::getGameGUIOptionsDescriptionLanguage(Common::DE_DEU));
+			game.appendGUIOptions(Common::getGameGUIOptionsDescriptionLanguage(Common::PL_POL));
+			game.appendGUIOptions(Common::getGameGUIOptionsDescriptionLanguage(Common::ES_ESP));
+		} else if (gameID == VCruise::GID_SCHIZM) {
+			game.appendGUIOptions(Common::getGameGUIOptionsDescriptionLanguage(Common::NL_NLD));
+			game.appendGUIOptions(Common::getGameGUIOptionsDescriptionLanguage(Common::FR_FRA));
+			game.appendGUIOptions(Common::getGameGUIOptionsDescriptionLanguage(Common::IT_ITA));
+			game.appendGUIOptions(Common::getGameGUIOptionsDescriptionLanguage(Common::DE_DEU));
+			game.appendGUIOptions(Common::getGameGUIOptionsDescriptionLanguage(Common::PL_POL));
+			game.appendGUIOptions(Common::getGameGUIOptionsDescriptionLanguage(Common::ES_ESP));
+			game.appendGUIOptions(Common::getGameGUIOptionsDescriptionLanguage(Common::EL_GRC));
+			game.appendGUIOptions(Common::getGameGUIOptionsDescriptionLanguage(Common::RU_RUS));
+		}
+
+		return game;
+	}
+
+	/*
+	Common::String parseAndCustomizeGuiOptions(const Common::String &optionsString, const Common::String &domain) const {
+		Common::String guiOptions = AdvancedMetaEngineDetection::parseAndCustomizeGuiOptions(optionsString, domain);
+
+		if (domain.hasPrefix("reah")) {
+			guiOptions += " lang_Dutch lang_French lang_Italian lang_German lang_Polish lang_Spanish";
+		} else if (domain.hasPrefix("schizm")) {
+			guiOptions += " lang_Dutch lang_French lang_Italian lang_German lang_Greek lang_Polish lang_Russian lang_Spanish";
+		}
+
+		return guiOptions;
+	}
+	*/
+};
+
+REGISTER_PLUGIN_STATIC(VCRUISE_DETECTION, PLUGIN_TYPE_ENGINE_DETECTION, VCruiseMetaEngineDetection);
