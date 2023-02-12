91c6ddacbaafad7028161a2b5e3b0c9aa1f836a3
Little Cat <toontownlittlecat@gmail.com>
2023-02-12 04:24:27 -0400

    GUI: Online session selector for Football 2002.

---
+++ gui/sessionselector.cpp
@@ -0,0 +1,111 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "engines/metaengine.h"
+#include "common/algorithm.h"
+#include "common/config-manager.h"
+#include "common/debug.h"
+#include "common/system.h"
+#include "common/taskbar.h"
+#include "common/translation.h"
+
+#include "engines/advancedDetector.h"
+
+#include "gui/sessionselector.h"
+
+namespace GUI {
+
+enum {
+	kOkCmd = 'OK  ',
+	kCancelCmd = 'CNCL'
+};
+
+SessionSelectorDialog::SessionSelectorDialog(Scumm::ScummEngine_v90he *vm)
+	: Dialog("SessionSelector"),
+	_vm(vm),
+	_joinButton(nullptr),
+	_queryProgressText(nullptr) {
+
+	_timestamp = 0;
+
+	_queryProgressText = new StaticTextWidget(this, "SessionSelector.QueryProgressText",
+						_("... progress ..."));
+
+	_queryProgressText->setAlign(Graphics::kTextAlignCenter);
+
+	_list = new ListWidget(this, "SessionSelector.SessionList");
+	_list->setEditable(false);
+	_list->setNumberingMode(kListNumberingOff);
+
+	_joinButton = new ButtonWidget(this, "SessionSelector.Join", _("Join"), Common::U32String(), kOkCmd, Common::ASCII_RETURN);
+	_joinButton->setEnabled(false);
+
+	new ButtonWidget(this, "SessionSelector.Cancel", _("Cancel"), Common::U32String(), kCancelCmd, Common::ASCII_ESCAPE);
+}
+
+void SessionSelectorDialog::handleCommand(CommandSender *sender, uint32 cmd, uint32 data) {
+	switch (cmd) {
+	case GUI::kListSelectionChangedCmd:
+		_joinButton->setEnabled(true);
+		break;
+	case GUI::kListItemDoubleClickedCmd:
+	case kOkCmd:
+		if (_list->getSelected() > -1) {
+			setResult(_list->getSelected());
+			close();
+		}
+		break;
+	case kCancelCmd:
+		// User cancelled, so we don't do anything and just leave.
+		setResult(-2);
+		close();
+		break;
+	default:
+		Dialog::handleCommand(sender, cmd, data);
+	}
+}
+
+void SessionSelectorDialog::handleTickle() {
+	// Keep the connection active.
+	_vm->_net->doNetworkOnceAFrame(12);
+
+	// Query for new sessions every 5 seconds.
+	if (g_system->getMillis() - _timestamp > 5000) {
+		int numSessions = _vm->_net->querySessions();
+
+		// Clear list
+		Common::U32StringArray l;
+		_list->setList(l);
+
+		for (int i = 0; i < numSessions; i++) {
+			char name[MAX_SESSION_NAME];
+			_vm->_net->getSessionName(i, name, MAX_SESSION_NAME);
+			_list->append(name);
+		}
+
+		_timestamp = g_system->getMillis();
+	}
+
+	drawDialog(kDrawLayerForeground);
+}
+
+
+} // End of namespace GUI
---
+++ gui/sessionselector.h
@@ -0,0 +1,61 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef SESSION_SELECTOR_DIALOG_H
+#define SESSION_SELECTOR_DIALOG_H
+
+#include "gui/dialog.h"
+#include "gui/widgets/list.h"
+#include "common/fs.h"
+#include "common/hashmap.h"
+#include "common/stack.h"
+#include "common/str.h"
+
+#include "scumm/he/intern_he.h"
+#include "scumm/he/net/net_main.h"
+#include "scumm/he/net/net_defines.h"
+
+namespace GUI {
+
+class StaticTextWidget;
+
+class SessionSelectorDialog : public Dialog {
+public:
+	SessionSelectorDialog(Scumm::ScummEngine_v90he *vm);
+
+	void handleCommand(CommandSender *sender, uint32 cmd, uint32 data) override;
+	void handleTickle() override;
+
+private:
+	Scumm::ScummEngine_v90he *_vm;
+
+	uint32 _timestamp;
+
+	Widget *_joinButton;
+	StaticTextWidget *_queryProgressText;
+
+	ListWidget *_list;
+};
+
+
+} // End of namespace GUI
+
+#endif
