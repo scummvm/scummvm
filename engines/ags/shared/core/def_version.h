c387b37714b5a2ff7a3b9727f29ac74a6c7ab020
Walter Agazzi <walter.agazzi@protonmail.com>
2023-03-05 13:18:57 +0100

    AGS: Updated build version (3.6.0.42)
    Partially from upstream 0432b26a2b875b7ee36f32fb321342d11901c459

--- engines/ags/shared/core/def_version.h
+++ engines/ags/shared/core/def_version.h
@@ -22,9 +22,9 @@
 #ifndef AGS_SHARED_CORE_DEFVERSION_H
 #define AGS_SHARED_CORE_DEFVERSION_H
 
-#define ACI_VERSION_STR      "3.6.0.41"
+#define ACI_VERSION_STR      "3.6.0.42"
 #if defined (RC_INVOKED) // for MSVC resource compiler
-#define ACI_VERSION_MSRC_DEF  3.6.0.41
+#define ACI_VERSION_MSRC_DEF  3.6.0.42
 #endif
 
 #define SPECIAL_VERSION ""
