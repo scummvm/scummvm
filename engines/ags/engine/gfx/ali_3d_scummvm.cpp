e402e69539ce55f9f6ce14102bb43fbcfb90261c
Walter Agazzi <walter.agazzi@protonmail.com>
2023-03-05 12:50:39 +0100

    AGS: Engine: safety hotfix for render pass without sprite batches
    Partially from upstream 7ad4f424f473f480c830faf3f7a2aca4388aa8fe

--- engines/ags/engine/gfx/ali_3d_scummvm.cpp
+++ engines/ags/engine/gfx/ali_3d_scummvm.cpp
@@ -369,6 +369,11 @@ void ScummVMRendererGraphicsDriver::RenderToBackBuffer() {
 	while (_actSpriteBatch != UINT32_MAX)
 		EndSpriteBatch();
 
+	if (_spriteBatchDesc.size() == 0) {
+		ClearDrawLists();
+		return; // no batches - no render
+	}
+
 	// Render all the sprite batches with necessary transformations
 	//
 	// NOTE: that's not immediately clear whether it would be faster to first draw upon a camera-sized
