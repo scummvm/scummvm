863f715cfc564a5d9aba6ea920009192c8bd10e3
Giovanni Cascione <ing.cascione@gmail.com>
2023-03-08 01:53:14 +0100

    LIBRETRO: BUILD: Improve engines selection script

--- backends/platform/libretro/scripts/configure_engines.sh
+++ backends/platform/libretro/scripts/configure_engines.sh
@@ -17,22 +17,32 @@
 
 # Externally passed variables shall be:
 
-# $1 [REQ] BUILD_PATH
-# $2 [REQ] SCUMMVM_PATH
-# $3 [REQ] NO_HIGH_DEF [0,1]
-# $4 [REQ] NO_WIP [0,1]
-# $5 [REQ] STATIC_LINKING [0,1]
-# $6 [REQ] LITE [0,1]
+# $1     [REQ] BUILD_PATH
+# $2     [REQ] SCUMMVM_PATH
+# $3     [REQ] NO_WIP [0,1]
+# $4     [REQ] STATIC_LINKING [0,1]
+# $5     [REQ] LITE [0,1]
+# $[...] [OPT] Engines dependencies not available
 
 set -e
 
 # Exit if in parameters are not provided
-if [ -z $1 ] || [ -z $2 ] || [ -z $3 ] || [ -z $4 ] || [ -z $5 ] || [ -z $6 ] ; then
+if [ -z $1 ] || [ -z $2 ] || [ -z $3 ] || [ -z $4 ] || [ -z $5 ] ; then
 	exit 1
 fi
 
+# Get parameters
 BUILD_PATH="$1"
-SCUMMVM_PATH="$2"
+shift
+SCUMMVM_PATH="$1"
+shift
+NO_WIP=$1
+shift
+STATIC_LINKING=$1
+shift
+LITE=$1
+shift
+no_deps=$@
 
 cd "${SCUMMVM_PATH}"
 
@@ -42,49 +52,38 @@ sed -i.bak -e "s/exit 0/return 0/g" configure
 mv configure.bak configure > /dev/null 2>&1
 
 _parent_engines_list=""
-
-# Collect all default engines dependencies and force to yes
 tot_deps=""
+
+# Test NO_WIP
+[ $NO_WIP -ne 1 ] && engine_enable_all
+
+# Test LITE
+if [ $LITE -eq 1 ] ; then
+	engine_disable_all
+	for eng in $(cat "${BUILD_PATH}"/lite_engines.list) ; do
+		engine_enable "$eng"
+	done
+fi
+
+# Define engines list
 for a in $_engines ; do
-	engine_deps_var=_engine_${a}_deps
-	for dep in ${!engine_deps_var} ; do
+	# Collect all default engines dependencies and force to yes
+	for dep in $(get_var _engine_${a}_deps) ; do
 		found=0
 		for rec_dep in $tot_deps ; do
 			[ $dep = $rec_dep ] && found=1
 		done
-		[ $found -eq 0 ] && tot_deps+=" $dep"
+		[ $found -eq 0 ] && append_var tot_deps "$dep"
 	done
-
-	# Static linking support files
-	not_subengine_var=_engine_${a}_sub
-	not_wip_engine_var=_engine_${a}_build_default
-	if [ $5 -eq 1 ] && [ -z ${!not_subengine_var} ] ; then
-		good_to_go=1
-		# Test NO_HIGH_DEF
-		[ $3 -eq 1 ] && [ $((echo ${!engine_deps_var} | grep -q highres); echo $?) -eq 0 ]  && good_to_go=0
-		[ $4 -eq 1 ] && [ $(echo ${!not_wip_engine_var} = no) ] && good_to_go=0
-		[ $6 -eq 1 ] && [ $((cat ${BUILD_PATH}/lite_engines.list | grep -wq ${a}); echo $?) -eq 1 ] && good_to_go=0
-		[ $good_to_go -eq 1 ] &&  _parent_engines_list+="ADDLIB libtemp/lib${a}.a"$'\n'
-	fi
 done
-
-[ $5 -eq 1 ] && printf "$_parent_engines_list" >> "$BUILD_PATH"/script.mri
 
 for dep in $tot_deps ; do
 	eval _$dep=yes
 done
 
-# Test NO_HIGH_DEF
-if [ $3 -eq 1 ] ; then
-	_highres=no
-fi
-
-# Test LITE
-if [ $6 -eq 1 ] ; then
-	cp "${BUILD_PATH}"/lite_engines.list "${BUILD_PATH}"/config.mk.engines.lite
-	sed -i.bak -e "y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/" -e "s/^/ENABLE_/g;s/$/ = STATIC_PLUGIN/g" "${BUILD_PATH}"/config.mk.engines.lite
-	rm "${BUILD_PATH}"/config.mk.engines.lite.bak
-fi
+for dep in $no_deps ; do
+	eval _$dep=no
+done
 
 # Create needed engines build files
 awk -f "engines.awk" < /dev/null > /dev/null 2>&1
@@ -94,8 +93,5 @@ mkdir -p "engines"
 copy_if_changed engines/engines.mk.new "engines/engines.mk"
 copy_if_changed engines/detection_table.h.new "engines/detection_table.h"
 copy_if_changed engines/plugins_table.h.new "engines/plugins_table.h"
-
-# Test NO_WIP
-[ $4 -ne 1 ] && sed -i.bak -e "s/# \(.*\)/\1 = STATIC_PLUGIN/g" "config.mk.engines"
 
 echo 0
