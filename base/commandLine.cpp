377df0d5cc48b1cc4a372ae73ad6d00059a45c6b
Lothar Serra Mari <mail@serra.me>
2023-03-20 18:37:24 +0100

    BASE: SDL: Get SDL version info for SDL1 as well instead of hardcoding it

--- base/commandLine.cpp
+++ base/commandLine.cpp
@@ -1776,7 +1776,7 @@ bool processSettings(Common::String &command, Common::StringMap &settings, Commo
 		SDL_GetVersion(&sdlLinkedVersion);
 		printf("Using SDL backend with SDL %d.%d.%d\n", sdlLinkedVersion.major, sdlLinkedVersion.minor, sdlLinkedVersion.patch);
 #else
-		printf("Using SDL backend with SDL1.2\n");
+		printf("Using SDL backend with SDL %d.%d.%d\n", SDL_Linked_Version()->major, SDL_Linked_Version()->minor, SDL_Linked_Version()->patch);
 #endif
 #endif
 		printf("Features compiled in: %s\n", gScummVMFeatures);
