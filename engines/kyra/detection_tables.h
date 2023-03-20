926ab0e3faa5c7f730f78a9550043509069d4741
Vladimir Serbinenko <phcoder@gmail.com>
2023-03-20 19:05:39 +0100

    KYRA: Change eob2 Chinese from ZH_ANY to more accurate ZH_TWN

--- engines/kyra/detection_tables.h
+++ engines/kyra/detection_tables.h
@@ -2055,7 +2055,7 @@ const KYRAGameDescription adGameDescs[] = {
 			"eob2",
 			msg_missingLangResources, // Reason for being unsupported
 			AD_ENTRY1s("LEVEL15.INF", "f972f628d21bae404a7d52bb287c0012", -1),
-			Common::ZH_ANY,
+			Common::ZH_TWN,
 			Common::kPlatformDOS,
 			ADGF_UNSUPPORTED,
 			GUIO7(GUIO_NOSPEECH, GUIO_MIDIADLIB, GUIO_MIDIPCSPK, GUIO_RENDERVGA, GUIO_RENDEREGA, GAMEOPTION_EOB_HPGRAPHS, GAMEOPTION_EOB_MOUSESWAP)
