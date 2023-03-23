10a5566dab986315037da8da2fbf968901f7bee9
antoniou79 <a.antoniou79@gmail.com>
2023-03-23 13:53:04 +0200

    AGS: Fix formatting of debug message
    About bad index of audio channel

--- engines/ags/engine/ac/system.cpp
+++ engines/ags/engine/ac/system.cpp
@@ -168,7 +168,7 @@ int System_GetAudioChannelCount() {
 ScriptAudioChannel *System_GetAudioChannels(int index) {
 	if ((index < 0) || (index >= _GP(game).numGameChannels))
 		quitprintf("!System.AudioChannels: invalid sound channel index %d, supported %d - %d",
-			0, _GP(game).numGameChannels);
+			index, 0, _GP(game).numGameChannels - 1);
 
 	return &_G(scrAudioChannel)[index];
 }
