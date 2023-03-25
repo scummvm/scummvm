4f7aa8edb21bc568c39ffbd6c171fd2b580e20c2
PushmePullyu <127053144+PushmePullyu@users.noreply.github.com>
2023-03-25 21:22:51 +0100

    NUVIE: Fix Ultima 6 crash on opening spellbook
    Fix a NULL pointer dereference when opening the "new style"
    spellbook gump.
    
    In an attempt to initialize a new SpellViewGump the code erroneously
    called the init() method of the parent class (SpellView). This left
    the "font" member variable set to NULL.
    
    To reproduce the bug:
    
    1. Enable "new style" in the video options (requires a restart)
    
    2. Get a spellbook (e.g. from the Avatars room SW of the throne room)
    
    3. Open the inventory by pressing TAB and click the spellbook to ready it
    
    4. Press c to open the spellbook gump
    
    Fixes #12536

--- engines/ultima/nuvie/views/spell_view_gump.h
+++ engines/ultima/nuvie/views/spell_view_gump.h
@@ -49,7 +49,7 @@ public:
 	SpellViewGump(Configuration *cfg);
 	~SpellViewGump() override;
 
-	bool init(Screen *tmp_screen, void *view_manager, uint16 x, uint16 y, Font *f, Party *p, TileManager *tm, ObjManager *om);
+	bool init(Screen *tmp_screen, void *view_manager, uint16 x, uint16 y, Font *f, Party *p, TileManager *tm, ObjManager *om) override;
 
 	void Display(bool full_redraw) override;
 
