0d004bb8399be4e1a0fa66202ab6794356587df3
Harishankar Kumar <hari01584@gmail.com>
2023-03-30 01:25:58 +0530

    DIRECTOR: Set value of 'the result' from call returns
    Used when determining save/load success in the totaldistortion-win

--- engines/director/lingo/lingo-code.cpp
+++ engines/director/lingo/lingo-code.cpp
@@ -191,10 +191,15 @@ void Lingo::push(Datum d) {
 	_stack.push_back(d);
 }
 
-void Lingo::pushVoid() {
+Datum Lingo::getVoid() {
 	Datum d;
 	d.u.s = nullptr;
 	d.type = VOID;
+	return d;
+}
+
+void Lingo::pushVoid() {
+	Datum d = getVoid();
 	push(d);
 }
 
@@ -1650,11 +1655,20 @@ void LC::call(const Symbol &funcSym, int nargs, bool allowRetVal) {
 
 		if (funcSym.u.bltin != LB::b_return && funcSym.u.bltin != LB::b_value) {
 			if (stackSize == stackSizeBefore + 1) {
+				// Set "the result" to return value!, when a method
+				// this is for handling result after execution!
+				Datum top = g_lingo->peek(0);
+				if (top.type == INT)
+					g_lingo->_theResult = top;
+
 				if (!allowRetVal) {
 					Datum extra = g_lingo->pop();
 					warning("Builtin '%s' dropping return value: %s", funcSym.name->c_str(), extra.asString(true).c_str());
 				}
 			} else if (stackSize == stackSizeBefore) {
+				// No value, hence "the result" is VOID
+				g_lingo->_theResult = g_lingo->getVoid();
+
 				if (allowRetVal)
 					error("Builtin '%s' did not return value", funcSym.name->c_str());
 			} else if (stackSize > stackSizeBefore) {
