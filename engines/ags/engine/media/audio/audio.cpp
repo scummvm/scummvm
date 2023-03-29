scummvm/scummvm

commit d2b03800c6a8dea780b6eb544568476db59d4b88

AGS: Allow to place sounds on a crossfade channels for compatibility
From upstream 86564e983572b4ffb3cd12b00c116ae91c6fcbe4

engines/ags/engine/media/audio/audio.cpp
@@ -152,8 +152,9 @@ static int find_free_audio_channel(ScriptAudioClip *clip, int priority, bool int
 	if (!interruptEqualPriority)
 		priority--;
 
+	// NOTE: in backward compat mode we allow to place sound on a crossfade channel
 	int startAtChannel = _G(reserved_channel_count);
-	int endBeforeChannel = _GP(game).numGameChannels;
+	int endBeforeChannel = _GP(game).numCompatGameChannels;
 
 	if (_GP(game).audioClipTypes[clip->type].reservedChannels > 0) {
 		startAtChannel = 0;
@@ -161,7 +162,7 @@ static int find_free_audio_channel(ScriptAudioClip *clip, int priority, bool int
 			startAtChannel += MIN(MAX_SOUND_CHANNELS,
 				_GP(game).audioClipTypes[i].reservedChannels);
 		}
-		endBeforeChannel = MIN(_GP(game).numGameChannels,
+		endBeforeChannel = MIN(_GP(game).numCompatGameChannels,
 			startAtChannel + _GP(game).audioClipTypes[clip->type].reservedChannels);
 	}
 

engines/ags/engine/game/game_init.cpp
@@ -90,7 +90,7 @@ String GetGameInitErrorText(GameInitErrorType err) {
 
 // Initializes audio channels and clips and registers them in the script system
 void InitAndRegisterAudioObjects(GameSetupStruct &game) {
-	for (int i = 0; i < game.numGameChannels; ++i) {
+	for (int i = 0; i < game.numCompatGameChannels; ++i) {
 		_G(scrAudioChannel)[i].id = i;
 		ccRegisterManagedObject(&_G(scrAudioChannel)[i], &_GP(ccDynamicAudio));
 	}
@@ -374,10 +374,13 @@ HGameInitError InitGameState(const LoadedGameEntities &ents, GameDataVersion dat
 	_G(old_speech_lines) = ents.OldSpeechLines;
 
 	// Set number of game channels corresponding to the loaded game version
-	if (_G(loaded_game_file_version) < kGameVersion_360)
-		game.numGameChannels = MAX_GAME_CHANNELS_v320;
-	else
-		game.numGameChannels = MAX_GAME_CHANNELS;
+	if (_G(loaded_game_file_version) < kGameVersion_360) {
+		_GP(game).numGameChannels = MAX_GAME_CHANNELS_v320;
+		_GP(game).numCompatGameChannels = TOTAL_AUDIO_CHANNELS_v320;
+	} else {
+		_GP(game).numGameChannels = MAX_GAME_CHANNELS;
+		_GP(game).numCompatGameChannels = MAX_GAME_CHANNELS;
+	}
 
 	HError err = InitAndRegisterGameEntities(game);
 	if (!err)

engines/ags/shared/ac/game_setup_struct.h
@@ -90,6 +90,7 @@ struct GameSetupStruct : public GameSetupStructBase {
 	int               scoreClipID;
 	// number of allowed game audio channels (the ones under direct user control)
 	int               numGameChannels = 0;
+	int               numCompatGameChannels = 0;
 
 	// TODO: I converted original array of sprite infos to vector here, because
 	// statistically in most games sprites go in long continious sequences with minimal

commit 2808192828a44590ac004c939d96454ecf593631

AGS: fixed find_free_audio_channel() incorrect compat fix
From upstream b3f4909.

Fixes a regression from d2b0380 (86564e9 in upstream)  where the extra
channel allowance was added excessively for all cases.

Fixes bug #14353 (crash in unavowed).

engines/ags/engine/media/audio/audio.cpp
@@ -155,7 +155,7 @@ static int find_free_audio_channel(ScriptAudioClip *clip, int priority, bool int
 
 	// NOTE: in backward compat mode we allow to place sound on a crossfade channel
 	int startAtChannel = _G(reserved_channel_count);
-	int endBeforeChannel = _GP(game).numCompatGameChannels;
+	int endBeforeChannel = _GP(game).numGameChannels;
 
 	if (_GP(game).audioClipTypes[clip->type].reservedChannels > 0) {
 		startAtChannel = 0;
@@ -163,6 +163,7 @@ static int find_free_audio_channel(ScriptAudioClip *clip, int priority, bool int
 			startAtChannel += MIN(MAX_SOUND_CHANNELS,
 				_GP(game).audioClipTypes[i].reservedChannels);
 		}
+		// NOTE: we allow to place sound on a crossfade channel for backward compatibility
 		endBeforeChannel = MIN(_GP(game).numCompatGameChannels,
 			startAtChannel + _GP(game).audioClipTypes[clip->type].reservedChannels);
 	}