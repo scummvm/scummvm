21effbad63c4074bbb6fa1af38b189c1cadc05c7
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2023-03-31 11:19:22 +0100

    AGDS: restore suspension of main "tell" process lost in refactoring

--- engines/agds/process_opcodes.cpp
+++ engines/agds/process_opcodes.cpp
@@ -1210,6 +1210,10 @@ void Process::tell(bool npc, const Common::String &sound) {
 	Common::String region = popString();
 	debug("%s '%s' '%s' '%s'", npc? "npcSay": "playerSay", region.c_str(), text.c_str(), sound.c_str());
 	_engine->tell(*this, region, text, sound, npc);
+
+	//close inventory here if close flag was set
+	Common::String inventoryClose = _engine->getSystemVariable("inv_close")->getString();
+	suspend(!inventoryClose.empty()? kExitCodeCloseInventory: kExitCodeSuspend);
 }
 
 void Process::npcSay() {
