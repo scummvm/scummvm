1805db93d980914741d25dcf9080da087f31b0a9
hax0kartik <agarwala.kartik@gmail.com>
2023-04-04 08:33:30 +0530

    CRAB: Enable HandleEvents() of GameEventManager

--- engines/crab/event/gameeventmanager.h
+++ engines/crab/event/gameeventmanager.h
@@ -95,6 +95,8 @@ public:
 	// cur_per is also updated here
 	void InternalEvents(Info &info, pyrodactyl::level::Level &level, std::vector<EventResult> &result);
 
+	void HandleEvents(Info &info, const std::string &player_id, Common::Event &Event,
+					  pyrodactyl::ui::HUD &hud, pyrodactyl::level::Level &level, std::vector<EventResult> &result);
 #if 0
 	void HandleEvents(Info &info, const std::string &player_id, SDL_Event &Event,
 					  pyrodactyl::ui::HUD &hud, pyrodactyl::level::Level &level, std::vector<EventResult> &result);
