8d6700f34640e22232ec0699c99a821a6d19b887
Giovanni Cascione <ing.cascione@gmail.com>
2023-04-08 16:25:42 +0200

    LIBRETRO: fix first loop with fixed frameskip

--- backends/platform/libretro/src/libretro.cpp
+++ backends/platform/libretro/src/libretro.cpp
@@ -722,8 +722,9 @@ void retro_run(void) {
 			/* Determine frameskip need based on settings */
 			if ((frameskip_type == 2) || (performance_switch & PERF_SWITCH_ON))
 				skip_frame = (audio_status & AUDIO_STATUS_BUFFER_UNDERRUN);
-			else if (frameskip_type == 1)
-				skip_frame = !(current_frame % frameskip_no == 0);
+			else if (frameskip_type == 1){
+				skip_frame = !((current_frame - 1) % frameskip_no == 0);
+}
 			else if (frameskip_type == 3)
 				skip_frame = (retro_audio_buff_occupancy < frameskip_threshold);
 
