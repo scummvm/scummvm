45341141068d71a3f82842f7ff1ef97ac3e38128
antoniou79 <a.antoniou79@gmail.com>
2023-05-08 00:54:50 +0300

    BLADERUNNER: Fix glitches in CT01 and PS15
    PS15 was rogue pixels with bad z-buffer. Both are original bugs
    
    CT01 fix is an expansion of the existing fix to cover other loops and chapters affected.

--- engines/bladerunner/zbuffer.cpp
+++ engines/bladerunner/zbuffer.cpp
@@ -167,7 +167,8 @@ bool ZBuffer::decodeData(const uint8 *data, int size) {
 	} else {
 		clean();
 		decodePartialZBuffer(data, _zbuf1, size);
-		decodePartialZBuffer(data, _zbuf2, size);
+		//decodePartialZBuffer(data, _zbuf2, size);
+		memcpy(_zbuf2, _zbuf1, size);
 	}
 
 	return true;
@@ -176,6 +177,15 @@ bool ZBuffer::decodeData(const uint8 *data, int size) {
 uint16 *ZBuffer::getData() const {
 	return _zbuf2;
 }
+
+#if !BLADERUNNER_ORIGINAL_BUGS
+void ZBuffer::setDataZbufExplicit(int x, int y, uint16 overidingVal) {
+	assert(x >= 0 && x < _width);
+	assert(y >= 0 && y < _height);
+	_zbuf1[y * _width + x] = overidingVal;
+	_zbuf2[y * _width + x] = overidingVal;
+}
+#endif // BLADERUNNER_ORIGINAL_BUGS
 
 uint16 ZBuffer::getZValue(int x, int y) const {
 	assert(x >= 0 && x < _width);
