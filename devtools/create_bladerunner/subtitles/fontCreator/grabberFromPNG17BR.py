d63c37890659270035e929b98a79baa8a7ecacba
Vladimir Serbinenko <phcoder@gmail.com>
2023-05-21 23:06:16 +0200

    CREATE_BLADERUNNER: Change header from 'UTF-8' to 'utf-8'
    Current python and emacs prefer "utf-8".

--- devtools/create_bladerunner/subtitles/fontCreator/grabberFromPNG17BR.py
+++ devtools/create_bladerunner/subtitles/fontCreator/grabberFromPNG17BR.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 #-------------------------------------------------------------------------------
 # Name:		   grabberFromPNG15BR
 # Purpose:	   Parse the character fonts from a PNG file in order to create a
