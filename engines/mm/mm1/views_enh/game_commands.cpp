1db125aa8e58d8acb884529b33c613c1c7eab12a
Paul Gilbert <dreammaster@scummvm.org>
2023-05-21 20:33:22 -0700

    MM: MM1: Further tweaks to interface buttons

--- engines/mm/mm1/views_enh/game_commands.cpp
+++ engines/mm/mm1/views_enh/game_commands.cpp
@@ -30,27 +30,26 @@ GameCommands::GameCommands(UIElement *owner) :
 		ButtonContainer("GameCommands", owner),
 		_minimap(this) {
 	Shared::Xeen::SpriteResource *spr = &g_globals->_mainIcons;
-	_iconSprites.load("view.icn");
+	_iconSprites.load("cast.icn");
 
 	addButton(Common::Rect(235, 75, 259, 95), KEYBIND_SPELL, 2, spr);
 	addButton(Common::Rect(260, 75, 284, 95), KEYBIND_PROTECT, 10, spr);
 	addButton(Common::Rect(286, 75, 310, 95), KEYBIND_REST, 4, spr);
 
 	addButton(Common::Rect(235, 96, 259, 116), KEYBIND_BASH, 6, spr);
-	addButton(Common::Rect(260, 96, 284, 116), KEYBIND_SEARCH, 40, &_iconSprites);
-	addButton(Common::Rect(286, 96, 310, 116), KEYBIND_ORDER, 44, &_iconSprites);
+	addButton(Common::Rect(260, 96, 284, 116), KEYBIND_SEARCH, 2, &_iconSprites);
+	addButton(Common::Rect(286, 96, 310, 116), KEYBIND_UNLOCK, 14, spr);
 
 	addButton(Common::Rect(235, 117, 259, 137), KEYBIND_MAP, 12, spr);
-	addButton(Common::Rect(260, 117, 284, 137), KEYBIND_UNLOCK, 14, spr);
-	addButton(Common::Rect(286, 117, 310, 137), KEYBIND_QUICKREF, 16, spr);
+	addButton(Common::Rect(260, 117, 284, 137), KEYBIND_QUICKREF, 16, spr);
 
 	addButton(Common::Rect(109, 137, 122, 147), KEYBIND_MENU, 18, spr);
-	addButton(Common::Rect(235, 148, 259, 168), KEYBIND_TURN_LEFT, spr);
-	addButton(Common::Rect(260, 148, 284, 168), KEYBIND_FORWARDS, spr);
-	addButton(Common::Rect(286, 148, 310, 168), KEYBIND_TURN_RIGHT, spr);
-	addButton(Common::Rect(235, 169, 259, 189), KEYBIND_STRAFE_LEFT, spr);
-	addButton(Common::Rect(260, 169, 284, 189), KEYBIND_BACKWARDS, spr);
-	addButton(Common::Rect(286, 169, 310, 189), KEYBIND_STRAFE_RIGHT, spr);
+	addButton(Common::Rect(235, 148, 259, 168), KEYBIND_TURN_LEFT, 20, spr);
+	addButton(Common::Rect(260, 148, 284, 168), KEYBIND_FORWARDS, 22, spr);
+	addButton(Common::Rect(286, 148, 310, 168), KEYBIND_TURN_RIGHT, 24, spr);
+	addButton(Common::Rect(235, 169, 259, 189), KEYBIND_STRAFE_LEFT, 26, spr);
+	addButton(Common::Rect(260, 169, 284, 189), KEYBIND_BACKWARDS, 28, spr);
+	addButton(Common::Rect(286, 169, 310, 189), KEYBIND_STRAFE_RIGHT, 30, spr);
 
 	addButton(_minimap.getBounds(), KEYBIND_MINIMAP);
 }
