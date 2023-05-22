ecf7283251cc02513884cfce9c7fbc255c05e9f2
Vladimir Serbinenko <phcoder@gmail.com>
2023-05-22 22:16:45 +0200

    CREATE_BLADERUNNER: Remove Python 2.7 guard

--- devtools/create_bladerunner/subtitles/mixResourceCreator/mixResourceCreator.py
+++ devtools/create_bladerunner/subtitles/mixResourceCreator/mixResourceCreator.py
@@ -12,10 +12,6 @@ if 	(not sysLibFound):
 	sys.stdout.write("[Error] Errors were found when trying to import required python libraries\n")
 	sys.exit(1)
 
-if not (sys.version_info[0] == 2 and  sys.version_info[1] == 7):
-	sys.stdout.write("[Error] Blade Runner MIX Resource Creator script requires Python 2.7\n")
-	sys.exit(1)
-
 import packBladeRunnerMIXFromPCTLKXLS04
 if __name__ == "__main__":
 	packBladeRunnerMIXFromPCTLKXLS04.main(sys.argv[0:])
