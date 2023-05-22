677a1287caa37c84beb021dadad6a1d199e5e576
Vladimir Serbinenko <phcoder@gmail.com>
2023-05-22 22:16:45 +0200

    CREATE_BLADERUNNER: Remove Python 2.7 guard

--- devtools/create_bladerunner/subtitles/fontCreator/fontCreator.py
+++ devtools/create_bladerunner/subtitles/fontCreator/fontCreator.py
@@ -12,10 +12,6 @@ if (not sysLibFound):
 	sys.stdout.write("[Error] Errors were found when trying to import required python libraries\n")
 	sys.exit(1)
 
-if not (sys.version_info[0] == 2 and  sys.version_info[1] == 7):
-	sys.stdout.write("[Error] Blade Runner Font Creator script requires Python 2.7\n")
-	sys.exit(1)
-
 import grabberFromPNG17BR
 if __name__ == "__main__":
 	grabberFromPNG17BR.main(sys.argv[0:])
