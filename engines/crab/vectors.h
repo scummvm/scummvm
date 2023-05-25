41f29967404dbb447782b2a7241b56189b29cd53
hax0kartik <agarwala.kartik@gmail.com>
2023-05-25 14:15:06 +0530

    CRAB: Stub out Vector2D::SaveState()

--- engines/crab/vectors.h
+++ engines/crab/vectors.h
@@ -56,10 +56,13 @@ public:
 	}
 
 	void SaveState(rapidxml::xml_document<> &doc, rapidxml::xml_node<char> *root, const char *name) {
+		warning("STUB: Vector2D::SaveState()");
+#if 0
 		rapidxml::xml_node<char> *child = doc.allocate_node(rapidxml::node_element, name);
 		child->append_attribute(doc.allocate_attribute("x", gStrPool.Get((int)x)));
 		child->append_attribute(doc.allocate_attribute("y", gStrPool.Get((int)y)));
 		root->append_node(child);
+#endif
 	}
 
 	double Magnitude() { return sqrt(x * x + y * y); }
