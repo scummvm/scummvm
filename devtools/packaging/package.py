5dd9f8b6d0092360dca5622bb34d86730c72c634
Ankush Dutt <ankushd100@gmail.com>
2023-06-05 20:07:29 +0530

    DEVTOOLS: Enable bundling of DLC with base binary for Android

--- devtools/packaging/package.py
+++ devtools/packaging/package.py
@@ -4,22 +4,48 @@ import subprocess
 import sys
 
 
-def prepare(ex, game, location):
+def prepare(ex, game, dict):
     if ex["prepare"]["type"] == "python-script":
         cmd = sys.executable + " scripts/" + ex["prepare"]["location"]
         for key in ex["prepare"]["options"].keys():
             cmd += " " + key + " "
             value = ex["prepare"]["options"][key]
-            if value == "$location":
-                cmd += location
-            elif value[0] == '$':
+            # $ -> get from here (package.py), $$ -> get from games.json, $$$ -> get from environment variables
+            if len(value) >= 3 and value[2] == '$':
+                cmd += value[2:]
+            elif len(value) >= 2 and value[1] == '$':
                 # check in game's metadata
-                cmd += game[value[1:]]
+                cmd += game[value[2:]]
+            elif len(value) >= 1 and value[0] == '$':
+                cmd += dict[value[1:]]
             else:
                 cmd += value
 
         subprocess.run(cmd, shell=True)
         print(ex["prepare"]["successMessage"])
+
+
+def bundle(ex, game, dict):
+    if ex["bundle"]["type"] == "python-script":
+        cmd = sys.executable + " scripts/" + ex["bundle"]["location"]
+        for key in ex["bundle"]["options"].keys():
+            cmd += " " + key + " "
+            value = ex["bundle"]["options"][key]
+            # $ -> get from here (package.py), $$ -> get from games.json, $$$ -> get from environment variables
+            if len(value) >= 3 and value[2] == '$':
+                cmd += value[2:]
+            elif len(value) >= 2 and value[1] == '$':
+                # check in game's metadata
+                cmd += game[value[2:]]
+                if value == "$$packname":
+                    cmd += ".zip"  # temporary hack
+            elif len(value) >= 1 and value[0] == '$':
+                cmd += dict[value[1:]]
+            else:
+                cmd += value
+
+        subprocess.run(cmd, shell=True)
+        print(ex["bundle"]["successMessage"])
 
 
 def main():
@@ -28,10 +54,15 @@ def main():
                         help='Export platform name as per export-platforms.json')
     parser.add_argument('--game', required=True,
                         help='Game name as per dlc-games.json')
-    parser.add_argument('--location', required=True,
+    parser.add_argument('--game-location', required=True,
                         help='Location of game\'s datafiles')
+    parser.add_argument('--binary-location', required=False,
+                        help='Location of ScummVM executable (Required for Android)')
 
     args = parser.parse_args()
+
+    dict = {"game_location": args.game_location,
+            "binary_location": args.binary_location}
 
     f = open('export-platforms.json')
     export_platforms = json.load(f)
@@ -41,8 +72,12 @@ def main():
 
     if args.export_platform in export_platforms:
         if args.game in games:
+            print()
             prepare(export_platforms[args.export_platform],
-                    games[args.game], args.location)
+                    games[args.game], dict)
+
+            bundle(export_platforms[args.export_platform],
+                   games[args.game], dict)
 
         else:
             print(
