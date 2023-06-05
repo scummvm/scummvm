b9175e1810f893935fea508f8d9e8b73fdb2c82e
elasota <ejlasota@gmail.com>
2023-06-05 11:39:30 -0400

    COMMON: Fix up comments

--- common/compression/gentee_installer.h
+++ common/compression/gentee_installer.h
@@ -24,27 +24,39 @@
 
 #include "common/archive.h"
 
+/**
+ * @file
+ * Gentee Installer decompressor used in engines:
+ *  - vcruise
+ */
+
 namespace Common {
 
-// NOTE for future implementation:
-// A Gentee Installer setup executable can be deployed with a package file (pak1.pak, etc.) or the
-// data can be embedded in the setup executable.  If it's deployed with the executable, then you
-// must read 12 bytes from offset 1008 in the executable file.  Of that, the first 4 bytes is the
-// position of the embedded ginstall.dll compressed data, the next 4 are the compressed size of
-// ginstall.dll, and the next 4 are the decompressed size of ginstall.dll.
-//
-// From that, compute the end position of the DLL data from the DLL position + the compressed size.
-// If that position is less than the end of the file, then the package is embedded in the setup
-// executable starting at that location.
+/** NOTE for future implementation:
+ * A Gentee Installer setup executable can be deployed with a package file (disk1.pak, etc.) or the
+ * data can be embedded in the setup executable.  If it's deployed with the executable, then you
+ * must read 12 bytes from offset 1008 in the executable file.  Of that, the first 4 bytes is the
+ * position of the embedded ginstall.dll compressed data, the next 4 are the compressed size of
+ * ginstall.dll, and the next 4 are the decompressed size of ginstall.dll.
+ *
+ * From that, compute the end position of the DLL data from the DLL position + the compressed size.
+ * If that position is less than the end of the file, then the package is embedded in the setup
+ * executable starting at that location.
+ */
+
 
 /**
  * Loads a Gentee Installer package.
  *
+ * This product appears to have been renamed to CreateInstall in later versions, which also adopted
+ * other compression methods like PPMd.  This version uses LZ77 with adaptive Huffman coding.
+ *
  * @param stream          Data stream to load
  * @param prefixToRemove  Specifies the prefix of extract directives to include, and removes the prefix
+ *                        If you pass an empty string or null, all directives will be included
  * @param threadSafe      If true, all read operations will be wrapped in a mutex-guarded substream
  *
- * @return The number of members added to list.
+ * @return                The Gentee Installer package archive
  */
 Common::Archive *createGenteeInstallerArchive(Common::SeekableReadStream *stream, const char *prefixToRemove, bool threadSafe);
 
