72cd889d41f721e2d997d229bcf43fc20e956f97
hax0kartik <agarwala.kartik@gmail.com>
2023-06-05 20:54:37 +0530

    CRAB: Cleanup and stub out OpenURL()

--- engines/crab/url.cpp
+++ engines/crab/url.cpp
@@ -32,38 +32,8 @@
 
 namespace Crab {
 
-#ifdef __WIN32__
-#include <ShellAPI.h>
-#include <windows.h>
-#pragma comment(lib, "ws2_32.lib")
-#endif
-
-#ifdef __APPLE__
-#include <ApplicationServices/ApplicationServices.h>
-#include <CoreFoundation/CFBundle.h>
-#endif
-
 void OpenURL(const Common::String &url_str) {
-#ifdef __WIN32__
-	ShellExecuteA(NULL, "open", url_str.c_str(), NULL, NULL, SW_SHOW);
-#endif
-
-#ifdef __APPLE__
-	CFURLRef url = CFURLCreateWithBytes(
-		NULL,                     // allocator
-		(UInt8 *)url_str.c_str(), // URLBytes
-		url_str.length(),         // length
-		kCFStringEncodingASCII,   // encoding
-		NULL                      // baseURL
-	);
-	LSOpenCFURLRef(url, 0);
-	CFRelease(url);
-#endif
-
-#ifdef __GNUC__
-	Common::String command = "xdg-open " + url_str;
-	system(command.c_str());
-#endif
+	warning("STUB: OpenUrl()");
 }
 
 } // End of namespace Crab
