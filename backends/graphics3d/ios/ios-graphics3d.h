6a74f7b32b147a9352a56b20c00277cd8257fa12
Lars Sundström <l.sundstrom@gmail.com>
2023-06-10 15:41:28 +0200

    IOS7: Implement iOSGraphics3dManager
    Add a graphic manager rendering 3D graphics for engines supporting
    3D games. The manager is implemented using the 3D graphic managers
    for Android and SDL as models.
    
    Most probably Android and iOS can share much more of the code, but
    that will be a separate work to refactor.
    The iOSGraphics3dManager handles resize since the screen dimension
    changes on rotation.
    
    Games not supporting arbitary resolutions, e.g. Grim, are rendered
    on an intermediate framebuffer with the size requested by the
    engine and then rendered to the backbuffer (a framebuffer bound to
    the renderbuffer) and stretched to the screen resolution off the
    device.
    
    This commit just adds the manager. It will be utilised in next
    commit.
    
    Update gitlab ci worker and update documentation.

---
+++ backends/graphics3d/ios/ios-graphics3d.h
@@ -0,0 +1,147 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef BACKENDS_GRAPHICS3D_IOS_IOS_GRAPHICS3D_H
+#define BACKENDS_GRAPHICS3D_IOS_IOS_GRAPHICS3D_H
+
+#if defined(USE_OPENGL_GAME) || defined(USE_OPENGL_SHADERS)
+
+#include "common/scummsys.h"
+
+#include "backends/graphics/windowed.h"
+#include "backends/graphics/ios/ios-graphics.h"
+#include "backends/graphics3d/opengl/framebuffer.h"
+#include "backends/graphics3d/opengl/tiledsurface.h"
+#include "backends/graphics3d/opengl/surfacerenderer.h"
+
+class iOSGraphics3dManager : virtual public WindowedGraphicsManager, public iOSCommonGraphics {
+public:
+	iOSGraphics3dManager();
+	virtual ~iOSGraphics3dManager();
+
+	//WindowedGraphicsManager
+	bool gameNeedsAspectRatioCorrection() const override;
+	void handleResizeImpl(const int width, const int height) override;
+	virtual void setSystemMousePosition(const int x, const int y) override {};
+
+	//iOSCommonGraphics
+	void initSurface() override;
+	void deinitSurface() override;
+
+	void notifyResize(const int width, const int height) override;
+
+	virtual iOSCommonGraphics::State getState() const override;
+	virtual bool setState(const iOSCommonGraphics::State &state) override;
+
+	bool notifyMousePosition(Common::Point &mouse) override;
+	Common::Point getMousePosition() override { return Common::Point(_cursorX, _cursorY); }
+
+	// GraphicsManager API - Features
+	bool hasFeature(OSystem::Feature f) const override;
+	bool getFeatureState(OSystem::Feature f) const override;
+	void setFeatureState(OSystem::Feature f, bool enable) override;
+
+	const OSystem::GraphicsMode *getSupportedGraphicsModes() const override;
+	int getDefaultGraphicsMode() const override;
+	bool setGraphicsMode(int mode, uint flags = OSystem::kGfxModeNoFlags) override;
+	int getGraphicsMode() const override;
+
+	void beginGFXTransaction() override {}
+	OSystem::TransactionError endGFXTransaction() override {
+		return OSystem::kTransactionSuccess;
+	}
+
+	// GraphicsManager API - Graphics mode
+#ifdef USE_RGB_COLOR
+	Graphics::PixelFormat getScreenFormat() const override { return _overlayFormat; }
+	Common::List<Graphics::PixelFormat> getSupportedFormats() const override {
+		Common::List<Graphics::PixelFormat> supportedFormats;
+		return supportedFormats;
+	}
+#endif
+	int getScreenChangeID() const override { return _screenChangeCount; }
+	void initSize(uint w, uint h, const Graphics::PixelFormat *format) override;
+	int16 getHeight() const override;
+	int16 getWidth() const override;
+
+	float getHiDPIScreenFactor() const override;
+
+	// GraphicsManager API - Draw methods
+	void updateScreen() override;
+	// Following methods are not used by 3D graphics managers
+	void setPalette(const byte *colors, uint start, uint num) override {}
+	void grabPalette(byte *colors, uint start, uint num) const override {}
+	void copyRectToScreen(const void *buf, int pitch, int x, int y, int w, int h) override {}
+	Graphics::Surface *lockScreen() override { return nullptr; }
+	void unlockScreen() override {}
+	void fillScreen(uint32 col) override {}
+	void setShakePos(int shakeXOffset, int shakeYOffset) override {};
+	void setFocusRectangle(const Common::Rect& rect) override {}
+	void clearFocusRectangle() override {}
+
+	// GraphicsManager API - Overlay
+	const OSystem::GraphicsMode *getSupportedStretchModes() const override;
+	int getDefaultStretchMode() const override;
+	bool setStretchMode(int mode) override;
+	int getStretchMode() const override;
+	void showOverlay(bool inGUI) override;
+	void hideOverlay() override;
+	Graphics::PixelFormat getOverlayFormat() const override { return _overlayFormat; }
+	void clearOverlay() override;
+	void grabOverlay(Graphics::Surface &surface) const override;
+	void copyRectToOverlay(const void *buf, int pitch, int x, int y, int w, int h) override;
+	int16 getOverlayWidth() const override;
+	int16 getOverlayHeight() const override;
+
+	// GraphicsManager API - Mouse
+	bool showMouse(bool visible) override;
+	void setMouseCursor(const void *buf, uint w, uint h, int hotspotX, int hotspotY, uint32 keycolor, bool dontScale = false, const Graphics::PixelFormat *format = NULL, const byte *mask = NULL) override;
+	void setCursorPalette(const byte *colors, uint start, uint num) override {}
+
+protected:
+	void updateCursorScaling();
+	void createDepthAndStencilBuffer(int width, int height);
+	void updateDepthAndStencilBuffer(int width, int height);
+
+	int _screenChangeCount;
+	int _stretchMode;
+	bool _aspectRatioCorrection;
+	bool _mouseDontScale;
+	Common::Point _mouseHotspot;
+	Common::Point _mouseHotspotScaled;
+	int _mouseWidthScaled, _mouseHeightScaled;
+
+	Graphics::PixelFormat _overlayFormat;
+	OpenGL::TiledSurface *_overlayScreen;
+	OpenGL::TiledSurface *_overlayBackground;
+	OpenGL::TiledSurface *_mouseSurface;
+	OpenGL::SurfaceRenderer *_surfaceRenderer;
+
+	// FBO used to render games not supporting arbitary resolutions
+	OpenGL::FrameBuffer *_frameBuffer;
+	// FBO used as a backbuffer for Apple API
+	GLuint _glFBO;
+	// RBOs used for depth and stencil buffer only
+	GLuint _glRBOs[2];
+};
+
+#endif
+#endif
