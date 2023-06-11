f9174a2c20038da8fe7c6306f1a7409f2e4acd6e
Le Philousophe <lephilousophe@users.noreply.github.com>
2023-06-11 20:48:52 +0200

    GUI: Fix bug #7124
    A fake mouse moved event is generated to force a refresh of the current
    hovered widget

--- gui/dialog.cpp
+++ gui/dialog.cpp
@@ -232,7 +232,11 @@ void Dialog::handleMouseUp(int x, int y, int button, int clickCount) {
 	if (w)
 		w->handleMouseUp(x - (w->getAbsX() - _x), y - (w->getAbsY() - _y), button, clickCount);
 
-	_dragWidget = nullptr;
+	if (_dragWidget) {
+		_dragWidget = nullptr;
+		// Fake a mouse move to refresh now hovered widget
+		handleMouseMoved(x, y, button);
+	}
 }
 
 void Dialog::handleMouseWheel(int x, int y, int direction) {
