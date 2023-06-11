f704f5415cd42eb816299a332598343beb70634a
Paul Gilbert <dreammaster@scummvm.org>
2023-06-11 16:38:46 -0700

    M4: Skeleton globals class

--- engines/m4/m4.cpp
+++ engines/m4/m4.cpp
@@ -54,41 +54,8 @@ Common::String M4Engine::getGameId() const {
 Common::Error M4Engine::run() {
 	// Initialize 320x200 paletted graphics mode
 	initGraphics(320, 200);
-	_screen = new Graphics::Screen();
 
-	// Set the engine's debugger console
-	setDebugger(new Console());
-
-	// If a savegame was selected from the launcher, load it
-	int saveSlot = ConfMan.getInt("save_slot");
-	if (saveSlot != -1)
-		(void)loadGameState(saveSlot);
-
-	// Draw a series of boxes on screen as a sample
-	for (int i = 0; i < 100; ++i)
-		_screen->frameRect(Common::Rect(i, i, 320 - i, 200 - i), i);
-	_screen->update();
-
-	// Simple event handling loop
-	byte pal[256 * 3] = { 0 };
-	Common::Event e;
-	int offset = 0;
-
-	while (!shouldQuit()) {
-		while (g_system->getEventManager()->pollEvent(e)) {
-		}
-
-		// Cycle through a simple palette
-		++offset;
-		for (int i = 0; i < 256; ++i)
-			pal[i * 3 + 1] = (i + offset) % 256;
-		g_system->getPaletteManager()->setPalette(pal, 0, 256);
-		_screen->update();
-
-		// Delay for a bit. All events loops should have a delay
-		// to prevent the system being unduly loaded
-		g_system->delayMillis(10);
-	}
+	Globals globals;
 
 	return Common::kNoError;
 }
