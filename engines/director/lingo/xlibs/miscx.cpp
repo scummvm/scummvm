a5a7ebe7f7cc338ffe493ae4418eb9760b33de8e
Scott Percival <code@moral.net.au>
2023-06-14 20:43:01 +0800

    DIRECTOR: Add alias to Misc_X

--- engines/director/lingo/xlibs/miscx.cpp
+++ engines/director/lingo/xlibs/miscx.cpp
@@ -72,6 +72,7 @@ namespace Director {
 const char *MiscX::xlibName = "Misc_X";
 const char *MiscX::fileNames[] = {
 	"MISC_X",
+	"sharCOPY", // TD loads this up using openXLib("@:sharCOPY.DLL")
 	0
 };
 
