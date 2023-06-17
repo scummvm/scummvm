cc73fd7f5f9984ce30ddb0cb5ab6b30b179e29da
antoniou79 <a.antoniou79@gmail.com>
2023-06-17 20:40:27 +0300

    GUI: Ommit aspect ratio checkbox if this feature is missing
    Also fix compilation when building for --disable-aspect (buggy end bracket in backends/graphics/surfacesdl/surfacesdl-graphics.cpp)

--- backends/graphics/opengl/opengl-graphics.cpp
+++ backends/graphics/opengl/opengl-graphics.cpp
@@ -108,7 +108,9 @@ OpenGLGraphicsManager::~OpenGLGraphicsManager() {
 
 bool OpenGLGraphicsManager::hasFeature(OSystem::Feature f) const {
 	switch (f) {
+#ifdef USE_ASPECT
 	case OSystem::kFeatureAspectRatioCorrection:
+#endif
 	case OSystem::kFeatureCursorPalette:
 	case OSystem::kFeatureFilteringMode:
 	case OSystem::kFeatureStretchMode:
