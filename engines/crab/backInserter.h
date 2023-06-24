a4aff67661e0bc090164017e5b4b373dfcb1ad23
hax0kartik <agarwala.kartik@gmail.com>
2023-06-24 07:27:03 +0530

    CRAB: Add backInserter()

---
+++ engines/crab/backInserter.h
@@ -0,0 +1,39 @@
+#include "crab/common_header.h"
+
+#ifndef CRAB_BACKINSERTER_H
+#define CRAB_BACKINSERTER_H
+
+namespace Crab {
+
+template<class Container>
+class backInserterIterator {
+protected:
+    Container *_container;
+
+public:
+    explicit backInserterIterator(Container& x) : _container(&x) {}
+
+    backInserterIterator& operator=(const typename Container::value_type& value) {
+        *_container += value;
+        return *this;
+    }
+
+    backInserterIterator& operator=(const typename Container::value_type&& value) {
+        *_container += Common::move(value);
+        return *this;
+    }
+
+    backInserterIterator& operator*() { return *this; }
+    backInserterIterator& operator++() { return *this; }
+    backInserterIterator& operator++(int) { return *this; }
+};
+
+template<class Container>
+backInserterIterator<Container>
+backInserter(Container &x) {
+    return backInserterIterator<Container>(x);
+}
+
+} // End of namespace Crab
+
+#endif
