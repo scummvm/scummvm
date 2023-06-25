43a0e5531fde52d5ce98fb5a0548f01a54b3f76b
Le Philousophe <lephilousophe@users.noreply.github.com>
2023-06-25 11:23:42 +0200

    BACKENDS: OPENGL: Don't depend on USE_ASPECT
    USE_ASPECT disables software aspect ratio correction.
    In OpenGL it's done by GPU.

--- backends/graphics/opengl/opengl-graphics.cpp
+++ backends/graphics/opengl/opengl-graphics.cpp
@@ -108,9 +108,7 @@ OpenGLGraphicsManager::~OpenGLGraphicsManager() {
 
 bool OpenGLGraphicsManager::hasFeature(OSystem::Feature f) const {
 	switch (f) {
-#ifdef USE_ASPECT
 	case OSystem::kFeatureAspectRatioCorrection:
-#endif
 	case OSystem::kFeatureCursorPalette:
 	case OSystem::kFeatureFilteringMode:
 	case OSystem::kFeatureStretchMode:
