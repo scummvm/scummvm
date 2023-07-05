945a7d193dbb7e6d1a1ec5a548d3468b0731b751
Walter Agazzi <walter.agazzi@protonmail.com>
2023-07-05 16:19:23 +0200

    AGS: Freetype: Add const casts to fix warnings in t1driver

--- engines/ags/lib/freetype-2.1.3/src/type1/t1driver.cpp
+++ engines/ags/lib/freetype-2.1.3/src/type1/t1driver.cpp
@@ -211,7 +211,7 @@ Get_Kerning( T1_Face     face,
 	kerning->x = 0;
 	kerning->y = 0;
 
-	afm = (T1_AFM*)face->afm_data;
+	afm = const_cast<T1_AFM *>(reinterpret_cast<const T1_AFM *>(face->afm_data));
 	if ( afm )
 		T1_Get_Kerning( afm, left_glyph, right_glyph, kerning );
 
