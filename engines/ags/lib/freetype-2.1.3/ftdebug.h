338ef939866466bac6459ab0a6f5f2ee9296ed28
Walter Agazzi <walter.agazzi@protonmail.com>
2023-07-21 14:06:10 +0200

    AGS: Freetype: Add license and cleanup ftdebug/fttrace.h

--- engines/ags/lib/freetype-2.1.3/ftdebug.h
+++ engines/ags/lib/freetype-2.1.3/ftdebug.h
@@ -1,33 +1,36 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 /***************************************************************************/
 /*                                                                         */
 /*  ftdebug.h                                                              */
-/*                                                                         */
 /*    Debugging and logging component (specification).                     */
 /*                                                                         */
-/*  Copyright 1996-2001, 2002 by                                           */
-/*  David Turner, Robert Wilhelm, and Werner Lemberg.                      */
-/*                                                                         */
-/*  This file is part of the FreeType project, and may only be used,       */
-/*  modified, and distributed under the terms of the FreeType project      */
-/*  license, LICENSE.TXT.  By continuing to use, modify, or distribute     */
-/*  this file you indicate that you have read the license and              */
-/*  understand and accept it fully.                                        */
-/*                                                                         */
-/*                                                                         */
-/*  IMPORTANT: A description of FreeType's debugging support can be        */
-/*             found in "docs/DEBUG.TXT".  Read it if you need to use or   */
-/*             understand this code.                                       */
-/*                                                                         */
 /***************************************************************************/
-
 
 #ifndef AGS_LIB_FREETYPE_FTDEBUG_H
 #define AGS_LIB_FREETYPE_FTDEBUG_H
 
-
-#include "engines/ags/lib/freetype-2.1.3/ft213build.h"
 #include "engines/ags/lib/freetype-2.1.3/config/ftconfig.h"
-
+#include "engines/ags/lib/freetype-2.1.3/ft213build.h"
 
 FT2_1_3_BEGIN_HEADER
 
@@ -36,10 +39,9 @@ FT2_1_3_BEGIN_HEADER
 /* is already defined; this simplifies the following #ifdefs            */
 /*                                                                      */
 #ifdef FT2_1_3_DEBUG_LEVEL_TRACE
-#undef  FT2_1_3_DEBUG_LEVEL_ERROR
+#undef FT2_1_3_DEBUG_LEVEL_ERROR
 #define FT2_1_3_DEBUG_LEVEL_ERROR
 #endif
-
 
 /*************************************************************************/
 /*                                                                       */
@@ -50,32 +52,29 @@ FT2_1_3_BEGIN_HEADER
 
 #ifdef FT2_1_3_DEBUG_LEVEL_TRACE
 
-#define FT2_1_3_TRACE_DEF( x )  trace_ ## x ,
+#define FT2_1_3_TRACE_DEF(x) trace_##x,
 
 /* defining the enumeration */
 typedef enum {
 #include "engines/ags/lib/freetype-2.1.3/fttrace.h"
 	trace_count
-
 } FT2_1_3_Trace;
 
-
 /* defining the array of trace levels, provided by `src/base/ftdebug.c' */
-extern int  FT2_1_3_trace_levels[trace_count];
+extern int FT2_1_3_trace_levels[trace_count];
 
 #undef FT2_1_3_TRACE_DEF
 
 #endif /* FT2_1_3_DEBUG_LEVEL_TRACE */
 
-
 /*************************************************************************/
 /*                                                                       */
-/* Define the FT2_1_3_TRACE macro                                             */
+/* Define the FT2_1_3_TRACE macro                                        */
 /*                                                                       */
 /* IMPORTANT!                                                            */
 /*                                                                       */
-/* Each component must define the macro FT2_1_3_COMPONENT to a valid FT2_1_3_Trace */
-/* value before using any TRACE macro.                                   */
+/* Each component must define the macro FT2_1_3_COMPONENT to a valid     */
+/* FT2_1_3_Trace value before using any TRACE macro.                     */
 /*                                                                       */
 /*************************************************************************/
 
@@ -84,50 +83,47 @@ extern int  FT2_1_3_trace_levels[trace_count];
 #define FT2_1_3_TRACE(level, varformat)                       \
 	do {                                                      \
 		if (FT2_1_3_trace_levels[FT2_1_3_COMPONENT] >= level) \
-			FT_Message varformat;                        \
+			FT_Message varformat;                             \
 	} while (0)
 
 #else /* !FT2_1_3_DEBUG_LEVEL_TRACE */
 
-#define FT2_1_3_TRACE( level, varformat )  do ; while ( 0 )      /* nothing */
+#define FT2_1_3_TRACE( level, varformat )  do ; while (0)      /* nothing */
 
 #endif /* !FT2_1_3_DEBUG_LEVEL_TRACE */
-
 
 /*************************************************************************/
 /*                                                                       */
 /* You need two opening resp. closing parentheses!                       */
 /*                                                                       */
-/* Example: FT2_1_3_TRACE0(( "Value is %i", foo ))                            */
+/* Example: FT2_1_3_TRACE0(( "Value is %i", foo ))                       */
 /*                                                                       */
 /*************************************************************************/
 
-#define FT2_1_3_TRACE0( varformat )  FT2_1_3_TRACE( 0, varformat )
-#define FT2_1_3_TRACE1( varformat )  FT2_1_3_TRACE( 1, varformat )
-#define FT2_1_3_TRACE2( varformat )  FT2_1_3_TRACE( 2, varformat )
-#define FT2_1_3_TRACE3( varformat )  FT2_1_3_TRACE( 3, varformat )
-#define FT2_1_3_TRACE4( varformat )  FT2_1_3_TRACE( 4, varformat )
-#define FT2_1_3_TRACE5( varformat )  FT2_1_3_TRACE( 5, varformat )
-#define FT2_1_3_TRACE6( varformat )  FT2_1_3_TRACE( 6, varformat )
-#define FT2_1_3_TRACE7( varformat )  FT2_1_3_TRACE( 7, varformat )
-
+#define FT2_1_3_TRACE0(varformat) FT2_1_3_TRACE(0, varformat)
+#define FT2_1_3_TRACE1(varformat) FT2_1_3_TRACE(1, varformat)
+#define FT2_1_3_TRACE2(varformat) FT2_1_3_TRACE(2, varformat)
+#define FT2_1_3_TRACE3(varformat) FT2_1_3_TRACE(3, varformat)
+#define FT2_1_3_TRACE4(varformat) FT2_1_3_TRACE(4, varformat)
+#define FT2_1_3_TRACE5(varformat) FT2_1_3_TRACE(5, varformat)
+#define FT2_1_3_TRACE6(varformat) FT2_1_3_TRACE(6, varformat)
+#define FT2_1_3_TRACE7(varformat) FT2_1_3_TRACE(7, varformat)
 
 /*************************************************************************/
 /*                                                                       */
-/*  Define the FT2_1_3_ERROR macro                                            */
+/*  Define the FT2_1_3_ERROR macro                                       */
 /*                                                                       */
 /*************************************************************************/
 
 #ifdef FT2_1_3_DEBUG_LEVEL_ERROR
 
-#define FT2_1_3_ERROR( varformat )  FT_Message  varformat
+#define FT2_1_3_ERROR(varformat) FT_Message varformat
 
-#else  /* !FT2_1_3_DEBUG_LEVEL_ERROR */
+#else /* !FT2_1_3_DEBUG_LEVEL_ERROR */
 
-#define FT2_1_3_ERROR( varformat )  do ; while ( 0 )      /* nothing */
+#define FT2_1_3_ERROR( varformat )  do ; while (0)      /* nothing */
 
 #endif /* !FT2_1_3_DEBUG_LEVEL_ERROR */
-
 
 /*************************************************************************/
 /*                                                                       */
@@ -137,19 +133,18 @@ extern int  FT2_1_3_trace_levels[trace_count];
 
 #ifdef FT2_1_3_DEBUG_LEVEL_ERROR
 
-#define FT2_1_3_ASSERT(condition)                                     \
-	do {                                                              \
-		if (!(condition))                                             \
+#define FT2_1_3_ASSERT(condition)                                \
+	do {                                                         \
+		if (!(condition))                                        \
 			FT_Panic("assertion failed on line %d of file %s\n", \
-						  __LINE__, __FILE__);                        \
+					 __LINE__, __FILE__);                        \
 	} while (0)
 
 #else /* !FT2_1_3_DEBUG_LEVEL_ERROR */
 
-#define FT2_1_3_ASSERT( condition )  do ; while ( 0 )
+#define FT2_1_3_ASSERT( condition )  do ; while (0)
 
 #endif /* !FT2_1_3_DEBUG_LEVEL_ERROR */
-
 
 /*************************************************************************/
 /*                                                                       */
@@ -159,28 +154,26 @@ extern int  FT2_1_3_trace_levels[trace_count];
 
 #ifdef FT2_1_3_DEBUG_LEVEL_ERROR
 
-#include "stdio.h"  /* for vprintf() */
+#include "stdio.h" /* for vprintf() */
 
 /* print a message */
-FT2_1_3_EXPORT( void )
-FT_Message( const char*  fmt, ... );
+FT2_1_3_EXPORT(void)
+FT_Message(const char *fmt, ...);
 
 /* print a message and exit */
-FT2_1_3_EXPORT( void )
-FT_Panic( const char*  fmt, ... );
+FT2_1_3_EXPORT(void)
+FT_Panic(const char *fmt, ...);
 
 #endif /* FT2_1_3_DEBUG_LEVEL_ERROR */
 
+FT2_1_3_BASE(void)
+ft_debug_init(void);
 
-FT2_1_3_BASE( void )
-ft_debug_init( void );
-
-
-#if defined( _MSC_VER )      /* Visual C++ (and Intel C++) */
+#if defined(_MSC_VER) /* Visual C++ (and Intel C++) */
 
 /* we disable the warning `conditional expression is constant' here */
 /* in order to compile cleanly with the maximum level of warnings   */
-#pragma warning( disable : 4127 )
+#pragma warning(disable : 4127)
 
 #endif /* _MSC_VER */
 
@@ -188,6 +181,3 @@ ft_debug_init( void );
 FT2_1_3_END_HEADER
 
 #endif /* AGS_LIB_FREETYPE_FTDEBUG_H */
-
-
-/* END */
--- engines/ags/lib/freetype-2.1.3/fttrace.h
+++ engines/ags/lib/freetype-2.1.3/fttrace.h
@@ -1,17 +1,28 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 /***************************************************************************/
 /*                                                                         */
 /*  fttrace.h                                                              */
-/*                                                                         */
 /*    Tracing handling (specification only).                               */
-/*                                                                         */
-/*  Copyright 2002 by                                                      */
-/*  David Turner, Robert Wilhelm, and Werner Lemberg.                      */
-/*                                                                         */
-/*  This file is part of the FreeType project, and may only be used,       */
-/*  modified, and distributed under the terms of the FreeType project      */
-/*  license, LICENSE.TXT.  By continuing to use, modify, or distribute     */
-/*  this file you indicate that you have read the license and              */
-/*  understand and accept it fully.                                        */
 /*                                                                         */
 /***************************************************************************/
 
@@ -19,88 +30,85 @@
 /* definitions of trace levels for FreeType 2 */
 
 /* the first level must always be `trace_any' */
-FT2_1_3_TRACE_DEF( any )
+FT2_1_3_TRACE_DEF(any)
 
 /* base components */
-FT2_1_3_TRACE_DEF( calc )      /* calculations            (ftcalc.c)   */
-FT2_1_3_TRACE_DEF( memory )    /* memory manager          (ftobjs.c)   */
-FT2_1_3_TRACE_DEF( stream )    /* stream manager          (ftstream.c) */
-FT2_1_3_TRACE_DEF( io )        /* i/o interface           (ftsystem.c) */
-FT2_1_3_TRACE_DEF( list )      /* list management         (ftlist.c)   */
-FT2_1_3_TRACE_DEF( init )      /* initialization          (ftinit.c)   */
-FT2_1_3_TRACE_DEF( objs )      /* base objects            (ftobjs.c)   */
-FT2_1_3_TRACE_DEF( outline )   /* outline management      (ftoutln.c)  */
-FT2_1_3_TRACE_DEF( glyph )     /* glyph management        (ftglyph.c)  */
+FT2_1_3_TRACE_DEF(calc)      /* calculations            (ftcalc.c)   */
+FT2_1_3_TRACE_DEF(memory)    /* memory manager          (ftobjs.c)   */
+FT2_1_3_TRACE_DEF(stream)    /* stream manager          (ftstream.c) */
+FT2_1_3_TRACE_DEF(io)        /* i/o interface           (ftsystem.c) */
+FT2_1_3_TRACE_DEF(list)      /* list management         (ftlist.c)   */
+FT2_1_3_TRACE_DEF(init)      /* initialization          (ftinit.c)   */
+FT2_1_3_TRACE_DEF(objs)      /* base objects            (ftobjs.c)   */
+FT2_1_3_TRACE_DEF(outline)   /* outline management      (ftoutln.c)  */
+FT2_1_3_TRACE_DEF(glyph)     /* glyph management        (ftglyph.c)  */
 
-FT2_1_3_TRACE_DEF( raster )    /* monochrome rasterizer   (ftraster.c) */
-FT2_1_3_TRACE_DEF( smooth )    /* anti-aliasing raster    (ftgrays.c)  */
-FT2_1_3_TRACE_DEF( mm )        /* MM interface            (ftmm.c)     */
+FT2_1_3_TRACE_DEF(raster)    /* monochrome rasterizer   (ftraster.c) */
+FT2_1_3_TRACE_DEF(smooth)    /* anti-aliasing raster    (ftgrays.c)  */
+FT2_1_3_TRACE_DEF(mm)        /* MM interface            (ftmm.c)     */
 
 /* Cache sub-system */
-FT2_1_3_TRACE_DEF( cache )     /* cache sub-system        (ftcache.c, etc..) */
+FT2_1_3_TRACE_DEF(cache)     /* cache sub-system        (ftcache.c, etc..) */
 
 /* SFNT driver components */
-FT2_1_3_TRACE_DEF( sfobjs )    /*  SFNT object handler     (sfobjs.c)   */
-FT2_1_3_TRACE_DEF( ttcmap )    /* charmap handler         (ttcmap.c)    */
-FT2_1_3_TRACE_DEF( ttload )    /* basic TrueType tables   (ttload.c)    */
-FT2_1_3_TRACE_DEF( ttpost )    /* PS table processing     (ttpost.c)   */
-FT2_1_3_TRACE_DEF( ttsbit )    /* TrueType sbit handling  (ttsbit.c)   */
+FT2_1_3_TRACE_DEF(sfobjs)    /* SFNT object handler     (sfobjs.c)   */
+FT2_1_3_TRACE_DEF(ttcmap)    /* charmap handler         (ttcmap.c)   */
+FT2_1_3_TRACE_DEF(ttload)    /* basic TrueType tables   (ttload.c)   */
+FT2_1_3_TRACE_DEF(ttpost)    /* PS table processing     (ttpost.c)   */
+FT2_1_3_TRACE_DEF(ttsbit)    /* TrueType sbit handling  (ttsbit.c)   */
 
 /* TrueType driver components */
-FT2_1_3_TRACE_DEF( ttdriver )  /* TT font driver          (ttdriver.c) */
-FT2_1_3_TRACE_DEF( ttgload )   /* TT glyph loader         (ttgload.c)  */
-FT2_1_3_TRACE_DEF( ttinterp )  /* bytecode interpreter    (ttinterp.c) */
-FT2_1_3_TRACE_DEF( ttobjs )    /* TT objects manager      (ttobjs.c)   */
-FT2_1_3_TRACE_DEF( ttpload )   /* TT data/program loader  (ttpload.c)  */
+FT2_1_3_TRACE_DEF(ttdriver)  /* TT font driver          (ttdriver.c) */
+FT2_1_3_TRACE_DEF(ttgload)   /* TT glyph loader         (ttgload.c)  */
+FT2_1_3_TRACE_DEF(ttinterp)  /* bytecode interpreter    (ttinterp.c) */
+FT2_1_3_TRACE_DEF(ttobjs)    /* TT objects manager      (ttobjs.c)   */
+FT2_1_3_TRACE_DEF(ttpload)   /* TT data/program loader  (ttpload.c)  */
 
 /* Type 1 driver components */
-FT2_1_3_TRACE_DEF( t1driver )
-FT2_1_3_TRACE_DEF( t1gload )
-FT2_1_3_TRACE_DEF( t1hint )
-FT2_1_3_TRACE_DEF( t1load )
-FT2_1_3_TRACE_DEF( t1objs )
-FT2_1_3_TRACE_DEF( t1parse )
+FT2_1_3_TRACE_DEF(t1driver)
+FT2_1_3_TRACE_DEF(t1gload)
+FT2_1_3_TRACE_DEF(t1hint)
+FT2_1_3_TRACE_DEF(t1load)
+FT2_1_3_TRACE_DEF(t1objs)
+FT2_1_3_TRACE_DEF(t1parse)
 
 /* PostScript helper module `psaux' */
-FT2_1_3_TRACE_DEF( t1decode )
-FT2_1_3_TRACE_DEF( psobjs )
+FT2_1_3_TRACE_DEF(t1decode)
+FT2_1_3_TRACE_DEF(psobjs)
 
 /* PostScript hinting module `pshinter' */
-FT2_1_3_TRACE_DEF( pshrec )
-FT2_1_3_TRACE_DEF( pshalgo1 )
-FT2_1_3_TRACE_DEF( pshalgo2 )
+FT2_1_3_TRACE_DEF(pshrec)
+FT2_1_3_TRACE_DEF(pshalgo1)
+FT2_1_3_TRACE_DEF(pshalgo2)
 
 /* Type 2 driver components */
-FT2_1_3_TRACE_DEF( cffdriver )
-FT2_1_3_TRACE_DEF( cffgload )
-FT2_1_3_TRACE_DEF( cffload )
-FT2_1_3_TRACE_DEF( cffobjs )
-FT2_1_3_TRACE_DEF( cffparse )
+FT2_1_3_TRACE_DEF(cffdriver)
+FT2_1_3_TRACE_DEF(cffgload)
+FT2_1_3_TRACE_DEF(cffload)
+FT2_1_3_TRACE_DEF(cffobjs)
+FT2_1_3_TRACE_DEF(cffparse)
 
 /* Type 42 driver component */
-FT2_1_3_TRACE_DEF( t42 )
+FT2_1_3_TRACE_DEF(t42)
 
 /* CID driver components */
-FT2_1_3_TRACE_DEF( cidafm )
-FT2_1_3_TRACE_DEF( ciddriver )
-FT2_1_3_TRACE_DEF( cidgload )
-FT2_1_3_TRACE_DEF( cidload )
-FT2_1_3_TRACE_DEF( cidobjs )
-FT2_1_3_TRACE_DEF( cidparse )
+FT2_1_3_TRACE_DEF(cidafm)
+FT2_1_3_TRACE_DEF(ciddriver)
+FT2_1_3_TRACE_DEF(cidgload)
+FT2_1_3_TRACE_DEF(cidload)
+FT2_1_3_TRACE_DEF(cidobjs)
+FT2_1_3_TRACE_DEF(cidparse)
 
 /* Windows fonts component */
-FT2_1_3_TRACE_DEF( winfnt )
+FT2_1_3_TRACE_DEF(winfnt)
 
 /* PCF fonts components */
-FT2_1_3_TRACE_DEF( pcfdriver )
-FT2_1_3_TRACE_DEF( pcfread )
+FT2_1_3_TRACE_DEF(pcfdriver)
+FT2_1_3_TRACE_DEF(pcfread)
 
 /* BDF fonts component */
-FT2_1_3_TRACE_DEF( bdfdriver )
-FT2_1_3_TRACE_DEF( bdflib )
+FT2_1_3_TRACE_DEF(bdfdriver)
+FT2_1_3_TRACE_DEF(bdflib)
 
 /* PFR fonts component */
-FT2_1_3_TRACE_DEF( pfr )
-
-
-/* END */
+FT2_1_3_TRACE_DEF(pfr)
