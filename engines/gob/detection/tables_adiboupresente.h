0a9d9ddb235cc37535e1a34a7042997549e9d767
CrystalBen <bjnfne@web.de>
2023-07-27 15:41:29 +0200

    GOB: added Adibou presente Dessin to detection tables

--- engines/gob/detection/tables_adiboupresente.h
+++ engines/gob/detection/tables_adiboupresente.h
@@ -30,7 +30,7 @@
 	{
 		"adiboudessin",
 		_s("Missing game code"), // Adibou presente Dessin 1.00 (Engine: DEV7 version 1.1.0.0)
-	 AD_ENTRY1s("adibou.stk", "14e3f8e9c237d4236d93e08c60b784bc", 217172),
+		AD_ENTRY1s("adibou.stk", "14e3f8e9c237d4236d93e08c60b784bc", 217172),
 		FR_FRA,
 		kPlatformWindows,
 		ADGF_UNSUPPORTED,
