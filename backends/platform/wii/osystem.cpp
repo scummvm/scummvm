f5dfa6b8d119cff1589b9a3da49f276d2915d5d8
Wyatt Radkiewicz <wyattradkiewicz@Wyatts-MacBook-Air.local>
2023-08-07 16:22:25 -0700

    WII: Removed extra ")" in backend hasFeature

--- backends/platform/wii/osystem.cpp
+++ backends/platform/wii/osystem.cpp
@@ -176,7 +176,7 @@ bool OSystem_Wii::hasFeature(Feature f) {
 	return (f == kFeatureFullscreenMode) ||
 			(f == kFeatureAspectRatioCorrection) ||
 			(f == kFeatureCursorPalette) ||
-			(f == kFeatureOverlaySupportsAlpha));
+			(f == kFeatureOverlaySupportsAlpha);
 }
 
 void OSystem_Wii::setFeatureState(Feature f, bool enable) {
