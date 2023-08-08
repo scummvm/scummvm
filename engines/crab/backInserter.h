8f1b0116aed73608fefbdc80534282ee44da75ca
hax0kartik <agarwala.kartik@gmail.com>
2023-08-08 09:25:39 +0530

    CRAB: JANITORIAL: Convert spaces to tab in backInserter.h

--- engines/crab/backInserter.h
+++ engines/crab/backInserter.h
@@ -29,30 +29,30 @@ namespace Crab {
 template<class Container>
 class backInserterIterator {
 protected:
-    Container *_container;
+	Container *_container;
 
 public:
-    explicit backInserterIterator(Container& x) : _container(&x) {}
+	explicit backInserterIterator(Container &x) : _container(&x) {}
 
-    backInserterIterator& operator=(const typename Container::value_type& value) {
-        *_container += value;
-        return *this;
-    }
+	backInserterIterator &operator=(const typename Container::value_type &value) {
+		*_container += value;
+		return *this;
+	}
 
-    backInserterIterator& operator=(const typename Container::value_type&& value) {
-        *_container += Common::move(value);
-        return *this;
-    }
+	backInserterIterator &operator=(const typename Container::value_type &&value) {
+		*_container += Common::move(value);
+		return *this;
+	}
 
-    backInserterIterator& operator*() { return *this; }
-    backInserterIterator& operator++() { return *this; }
-    backInserterIterator& operator++(int) { return *this; }
+	backInserterIterator &operator*() { return *this; }
+	backInserterIterator &operator++() { return *this; }
+	backInserterIterator &operator++(int) { return *this; }
 };
 
 template<class Container>
 backInserterIterator<Container>
 backInserter(Container &x) {
-    return backInserterIterator<Container>(x);
+	return backInserterIterator<Container>(x);
 }
 
 } // End of namespace Crab
