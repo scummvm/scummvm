c45918ea91f5b58ce9abc85b8ac61c863ff6de44
Wyatt Radkiewicz <wyattradkiewicz@Wyatts-MacBook-Air.local>
2023-08-08 15:49:41 -0700

    BUILD: SIMD flags only enabled on blit-blend.o

--- graphics/module.mk
+++ graphics/module.mk
@@ -140,5 +140,15 @@ endif
 
 endif
 
+ifeq ($(SCUMMVM_NEON),1)
+$(MODULE)/blit/blit-blend.o: CXXFLAGS += -mfpu=neon
+endif
+ifeq ($(SCUMMVM_SSE2),1)
+$(MODULE)/blit/blit-blend.o: CXXFLAGS += -msse2 -msse
+endif
+ifeq ($(SCUMMVM_AVX2),1)
+$(MODULE)/blit/blit-blend.o: CXXFLAGS += -mavx2 -mavx -msse2 -msse
+endif
+
 # Include common rules
 include $(srcdir)/rules.mk
