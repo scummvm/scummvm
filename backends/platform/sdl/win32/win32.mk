d117a48aaff513408e6bea2ec955d11026dd54f8
Walter Agazzi <walter.agazzi@protonmail.com>
2023-08-29 11:30:11 +0200

    DISTS: Keep Catharon license original naming

--- backends/platform/sdl/win32/win32.mk
+++ backends/platform/sdl/win32/win32.mk
@@ -29,7 +29,7 @@ win32-data: all
 	cp $(srcdir)/LICENSES/COPYING.MKV $(WIN32PATH)/COPYING.MKV.txt
 	cp $(srcdir)/LICENSES/COPYING.TINYGL $(WIN32PATH)/COPYING.TINYGL.txt
 	cp $(srcdir)/LICENSES/COPYING.GLAD $(WIN32PATH)/COPYING.GLAD.txt
-	cp $(srcdir)/LICENSES/COPYING.CATHARON $(WIN32PATH)/COPYING.CATHARON.txt
+	cp $(srcdir)/LICENSES/CatharonLicense.txt $(WIN32PATH)/CatharonLicense.txt
 	cp $(srcdir)/COPYRIGHT $(WIN32PATH)/COPYRIGHT.txt
 	cp $(srcdir)/doc/cz/PrectiMe $(WIN32PATH)/doc/cz/PrectiMe.txt
 	cp $(srcdir)/doc/QuickStart $(WIN32PATH)/doc/QuickStart.txt
