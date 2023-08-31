6d008fb1d32cf43338ba5ce8e362ada0a2e27adc
Paul Gilbert <dreammaster@scummvm.org>
2023-08-30 23:00:21 -0700

    M4: In progress room 702 daemon

--- engines/m4/burger/rooms/section7/room702.cpp
+++ engines/m4/burger/rooms/section7/room702.cpp
@@ -42,15 +42,180 @@ static const char *SAID[][4] = {
 
 
 void Room702::init() {
+	pal_fade_set_start(0);
+	_flag1 = false;
+	_flag2 = false;
+	_val1 = 1;
+
+	_series1 = series_load("702FLT01");
+	_series2 = series_load("702FLX02");
+	_series3 = series_load("702C1FL1");
+	_series4 = series_load("702C1FL2");
+	_series5 = series_load("702C1FL3");
+	_series1 = series_load("702C1FX1");
+
+	digi_preload("700_001");
+	digi_preload("702f003b");
+	digi_preload("702_007");
+	digi_preload("702_006");
+
+	kernel_trigger_dispatch_now(1);
 }
 
 void Room702::daemon() {
-}
+	switch (_G(kernel).trigger) {
+	case 1:
+		player_set_commands_allowed(true);
+		ws_demand_location(302, 284, 6);
+		_val2 = 12;
+		_G(roomVal4) = 8;
+		_G(wilbur_should) = 10;
+		digi_play_loop("700_001", 3, 50, -1, 700);
+		kernel_trigger_dispatch_now(gCHANGE_WILBUR_ANIMATION);
+		kernel_trigger_dispatch_now(5);
+		kernel_trigger_dispatch_now(6);
+		_G(flags)[V298] = 1;
+		pal_fade_init(0, 255, 100, 30, -1);
+		break;
 
-void Room702::pre_parser() {
+	case 2:
+		conv_load_and_prepare("conv82", 3);
+		conv_play_curr();
+		break;
+
+	case 5:
+		switch (_val1) {
+		case 1:
+			switch (_G(roomVal4)) {
+			case 3:
+				_val1 = 2;
+				break;
+
+			case 4:
+				if (_flag1)
+					terminateMachineAndNull(_series10);
+
+				_series10 = series_play("702FLX02", 0, 0, 4);
+				break;
+
+			case 5:
+				_G(roomVal4) = getRoomVal();
+				_flag1 = true;
+				_series10 = series_play("702C1FL2", 0, 2, 5);
+
+				if (imath_ranged_rand(0, 1) == 1) {
+					digi_play("702F003B", 2, 100);
+				} else {
+					digi_play("702_007", 2, 100);
+				}
+				break;
+
+			case 6:
+				_G(roomVal4) = getRoomVal();
+				_flag1 = true;
+				_series10 = series_play("702C1FL3", 0, 1, 5);
+
+				if (imath_ranged_rand(0, 1) == 1) {
+					digi_play("702F003B", 2, 100);
+				} else {
+					digi_play("702_007", 2, 100);
+				}
+				break;
+
+			case 7:
+				_G(roomVal4) = getRoomVal();
+				_flag1 = true;
+				_series10 = series_play("702C1FL2", 0, 0, 5);
+
+				if (imath_ranged_rand(0, 1) == 1) {
+					digi_play("702F003B", 2, 100);
+				} else {
+					digi_play("702_007", 2, 100);
+				}
+				break;
+
+			case 8:
+				_G(roomVal4) = getRoomVal();
+				_flag1 = true;
+				_series10 = series_play("702C1FL1", 0, 0, 5);
+				digi_play("702F003B", 2, 120);
+				break;
+
+			case 9:
+				_G(roomVal4) = getRoomVal();
+				_flag1 = true;
+				_series10 = series_play("702FLT01", 0, 0, 5, 60);
+				break;
+
+			default:
+				break;
+			}
+			break;
+
+		}
+	}
 }
 
 void Room702::parser() {
+	_G(kernel).trigger_mode = KT_DAEMON;
+
+	if (player_said("TALK", "FLUMIX")) {
+		kernel_trigger_dispatch_now(2);
+
+	} else if (player_said("conv82")) {
+		conv82();
+
+	} else if (player_said("DOOR", "LOOK AT")) {
+		if (_flag2) {
+			wilbur_speech("702W004");
+		} else {
+			_flag2 = true;
+			wilbur_speech("702W003");
+		}
+	} else if (player_said("DOOR", "GEAR")) {
+		ws_walk(174, 316, 0, -1, 10);
+		wilbur_speech("702W005");
+
+	} else if (!_G(walker).wilbur_said(SAID)) {
+		return;
+	}
+
+	_G(player).command_ready = false;
+}
+
+void Room702::conv82() {
+	int who = conv_whos_talking();
+
+	if (conv_sound_to_play()) {
+		if (who <= 0) {
+			_G(roomVal4) = 3;
+			kernel_trigger_dispatch_now(5);
+
+		} else if (who == 1) {
+			wilbur_speech(conv_sound_to_play(), 10001);
+		}
+	}
+}
+
+int Room702::getRoomVal() {
+	switch (imath_ranged_rand(1, 12)) {
+	case 1:
+	case 2:
+	case 3:
+		return 7;
+	case 4:
+	case 5:
+	case 6:
+		return 5;
+	case 7:
+	case 8:
+		return 6;
+	case 9:
+	case 10:
+		return 8;
+	default:
+		return 9;
+	}
 }
 
 } // namespace Rooms
