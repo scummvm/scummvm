f3ab611ee839481e936a2477e1d291a5097ad1c6
Le Philousophe <lephilousophe@users.noreply.github.com>
2023-09-02 10:49:14 +0200

    GRAPHICS: Remove spurious warning
    This partially reverts commit aa9d13f84ab75cc925ce067bf1309d42c0cc2d92.
    It looks like a debug leftover more than an actual warning.

--- graphics/surface.h
+++ graphics/surface.h
@@ -25,7 +25,6 @@
 #include "common/scummsys.h"
 #include "common/endian.h"
 #include "common/list.h"
-#include "common/textconsole.h"
 
 namespace Common {
 struct Rect;
@@ -125,7 +124,7 @@ public:
 	 *
 	 * @param newPixels The new pixel data.
 	 */
-	void setPixels(void *newPixels) { if ((unsigned long long)newPixels & 0xf) warning("unaligned pixels!"); pixels = newPixels; }
+	void setPixels(void *newPixels) { pixels = newPixels; }
 
 	/**
 	 * Return a pointer to the pixel at the specified point.
