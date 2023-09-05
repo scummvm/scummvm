f6d478c6fc9c0dcf0d00c7460c7ff07b900acbf4
Paul Gilbert <dreammaster@scummvm.org>
2023-09-04 22:14:38 -0700

    M4: Skeleton rooms for section 5

---
+++ engines/m4/burger/rooms/section5/room508.cpp
@@ -0,0 +1,46 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/ },.
+ *
+ */
+
+#include "m4/burger/rooms/section5/room508.h"
+#include "m4/burger/rooms/section5/section5.h"
+#include "m4/burger/vars.h"
+
+namespace M4 {
+namespace Burger {
+namespace Rooms {
+
+void Room508::init() {
+}
+
+void Room508::daemon() {
+}
+
+void Room508::pre_parser() {
+
+}
+
+void Room508::parser() {
+
+}
+
+} // namespace Rooms
+} // namespace Burger
+} // namespace M4
