scummvm/scummvm

commit 9aa095873683b990ac85b4c06b5fa843dcbd682a

DIRECTOR: Properly keep track of open resource files and their precedence
We used to close resources when we were closing movies, but with making
Archives owned by Director class, we now have list of all _seen_ resources.

This change implements a proper, ordered list of resources which are
overriding the ones loaded earlier.

Fixes cursors in the7colors which were read from previously loaded Shared Casts.
--start-movie=04CLUB:FloorColidor:IntoFloo

engines/director/cursor.cpp
@@ -184,8 +184,8 @@ void Cursor::readFromResource(Datum resourceId) {
 				break;
 		}
 
-		for (auto &it : g_director->_allSeenResFiles) {
-			readSuccessful = readFromArchive(it._value, resourceId.asInt());
+		for (auto &it : g_director->_allOpenResFiles) {
+			readSuccessful = readFromArchive(g_director->_allSeenResFiles[it], resourceId.asInt());
 			if (readSuccessful)
 				break;
 		}

engines/director/director.h
@@ -212,6 +212,7 @@ public:
 
 	Archive *createArchive();
 	Archive *openArchive(const Common::String movie);
+	void addArchiveToOpenList(const Common::String path);
 	Archive *loadEXE(const Common::String movie);
 	Archive *loadEXEv3(Common::SeekableReadStream *stream);
 	Archive *loadEXEv4(Common::SeekableReadStream *stream);
@@ -256,6 +257,8 @@ public:
 	Common::HashMap<Common::String, Archive *, Common::IgnoreCase_Hash, Common::IgnoreCase_EqualTo> _allSeenResFiles;
 	// Handles to resource files that were opened by OpenResFile.
 	Common::HashMap<Common::String, Archive *, Common::IgnoreCase_Hash, Common::IgnoreCase_EqualTo> _openResFiles;
+	// List of all currently open resource files
+	Common::List<Common::String> _allOpenResFiles;
 
 	Common::Array<Graphics::WinCursorGroup *> _winCursor;
 

engines/director/lingo/lingo-builtins.cpp
@@ -1147,6 +1147,9 @@ void LB::b_closeResFile(int nargs) {
 	// closeResFile closes only resource files that were opened with openResFile.
 
 	if (nargs == 0) { // Close all open resource files
+		for (auto &it : g_director->_openResFiles)
+			g_director->_allOpenResFiles.remove(it._key);
+
 		g_director->_openResFiles.clear();
 		return;
 	}
@@ -1156,6 +1159,8 @@ void LB::b_closeResFile(int nargs) {
 
 	if (g_director->_openResFiles.contains(resFileName)) {
 		g_director->_openResFiles.erase(resFileName);
+
+		g_director->_allOpenResFiles.remove(resFileName);
 	}
 }
 
@@ -1251,6 +1256,7 @@ void LB::b_openResFile(int nargs) {
 		if (arch->openFile(pathMakeRelative(resPath))) {
 			g_director->_openResFiles.setVal(resPath, arch);
 			g_director->_allSeenResFiles.setVal(resPath, arch);
+			g_director->addArchiveToOpenList(resPath);
 		} else {
 			delete arch;
 		}
@@ -1315,8 +1321,8 @@ void LB::b_showResFile(int nargs) {
 	if (nargs)
 		g_lingo->pop();
 	Common::String out;
-	for (auto &it : g_director->_allSeenResFiles)
-		out += it._key + "\n";
+	for (auto &it : g_director->_allOpenResFiles)
+		out += it + "\n";
 	g_debugger->debugLogFile(out, false);
 }
 

engines/director/movie.cpp
@@ -95,6 +95,10 @@ Movie::Movie(Window *window) {
 }
 
 Movie::~Movie() {
+	if (_sharedCast)
+		g_director->_allOpenResFiles.remove(_sharedCast->getArchive()->getPathName());
+	g_director->_allOpenResFiles.remove(_cast->getArchive()->getPathName());
+
 	delete _cast;
 	delete _sharedCast;
 	delete _score;
@@ -342,6 +346,8 @@ void Movie::loadFileInfo(Common::SeekableReadStreamEndian &stream) {
 void Movie::clearSharedCast() {
 	if (!_sharedCast)
 		return;
+
+	g_director->_allOpenResFiles.remove(_sharedCast->getArchive()->getPathName());
 
 	delete _sharedCast;
 	_sharedCast = nullptr;

engines/director/resource.cpp
@@ -203,6 +203,14 @@ void Window::probeResources(Archive *archive) {
 	}
 }
 
+void DirectorEngine::addArchiveToOpenList(const Common::String path) {
+	// First, remove it if it is present
+	_allOpenResFiles.remove(path);
+
+	// And add it to the list front
+	_allOpenResFiles.push_front(path);
+}
+
 Archive *DirectorEngine::openArchive(const Common::String path) {
 	debug(1, "DirectorEngine::openArchive(\"%s\")", path.c_str());
 
@@ -210,6 +218,7 @@ Archive *DirectorEngine::openArchive(const Common::String path) {
 	// just init from the existing archive. This prevents errors that
 	// can happen when trying to load the same archive more than once.
 	if (_allSeenResFiles.contains(path)) {
+		addArchiveToOpenList(path);
 		return _allSeenResFiles.getVal(path);
 	}
 
@@ -228,6 +237,8 @@ Archive *DirectorEngine::openArchive(const Common::String path) {
 	}
 	result->setPathName(path);
 	_allSeenResFiles.setVal(path, result);
+
+	addArchiveToOpenList(path);
 
 	return result;
 }

engines/director/sound.cpp
@@ -367,12 +367,12 @@ void DirectorSound::loadSampleSounds(uint type) {
 	uint id = 0xFF;
 	Archive *archive = nullptr;
 
-	for (auto &it : g_director->_allSeenResFiles) {
-		Common::Array<uint16> idList = it._value->getResourceIDList(tag);
+	for (auto &it : g_director->_allOpenResFiles) {
+		Common::Array<uint16> idList = g_director->_allSeenResFiles[it]->getResourceIDList(tag);
 		for (uint j = 0; j < idList.size(); j++) {
 			if ((idList[j] & 0xFF) == type) {
 				id = idList[j];
-				archive = it._value;
+				archive = g_director->_allSeenResFiles[it];
 				break;
 			}
 		}
@@ -573,10 +573,10 @@ void DirectorSound::playFPlaySound() {
 	Archive *archive = nullptr;
 
 	// iterate opened ResFiles
-	for (auto &it : g_director->_allSeenResFiles) {
-		id = it._value->findResourceID(tag, sndName, true);
+	for (auto &it : g_director->_allOpenResFiles) {
+		id = g_director->_allSeenResFiles[it]->findResourceID(tag, sndName, true);
 		if (id != 0xFFFF) {
-			archive = it._value;
+			archive = g_director->_allSeenResFiles[it];
 			break;
 		}
 	}

commit d5601a28d1b2b09ea95337f037b1b1ef5e82cf1c

DIRECTOR: fix loading cursor from main archive
Fixes a bug introduced in 9aa095873683b990ac85b4c06b5fa843dcbd682a.
That commit limited reading cursors to open res files, not all
*seen* res files. However, that broke the custom cursor in Small
Stories for Sleepless Nights for Mac (nemurenu-mac-ja), which stores
its cursor in the game's main archive which is already closed at the
time the cursor gets loaded.

engines/director/cursor.cpp
@@ -190,6 +190,12 @@ void Cursor::readFromResource(Datum resourceId) {
 				break;
 		}
 
+		// Cursors can be located in the main archive, which may not
+		// be in _allOpenResFiles
+		if (!readSuccessful && g_director->getPlatform() == Common::kPlatformMacintosh) {
+			readSuccessful = readFromArchive(g_director->getMainArchive(), resourceId.asInt());
+		}
+
 		// TODO: figure out where to read custom cursor in windows platform
 		// currently, let's just set arrow for default one.
 		if (g_director->getPlatform() == Common::kPlatformWindows) {