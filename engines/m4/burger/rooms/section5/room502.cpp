c2be748180c5a57d6c7f305818ce4a8e31861ea3
Paul Gilbert <dreammaster@scummvm.org>
2023-09-07 21:23:41 -0700

    M4: Added room 502 preparser/parser

--- engines/m4/burger/rooms/section5/room502.cpp
+++ engines/m4/burger/rooms/section5/room502.cpp
@@ -39,6 +39,35 @@ const Section5Room::BorkPoint Room502::BORK_TABLE[] = {
 	{ 328, 113 }, { 346, 103 }, { 353, 80 }, { 354, 58 }, { 364, 53 }
 };
 
+static const char *SAID[][4] = {
+	{ "FRONT DOOR",      "502w003", "500w001", "502w057" },
+	{ "KITCHEN",         nullptr,   "500w001", nullptr   },
+	{ "STAIRS",          "502w007", "500w001", nullptr   },
+	{ "BORK",            "502w009", "500w002", "500w002" },
+	{ "BORK ",           "502w011", "500w002", "500w002" },
+	{ "CHARRED OUTLINE", "502w012", "502w013", "500w001" },
+	{ "RAILING",         "502w014", nullptr,   "502w016" },
+	{ "KINDLING ",       "502w018", nullptr,   "502w021" },
+	{ "FIREPLACE",       "502w023", "500w001", "502w022" },
+	{ "WINDOW",          "500w003", "500w001", "500w004" },
+	{ "CHANDELIER",      "502w027", "502w028", "502w028" },
+	{ "WIRES",           "502w029", "502w030", "502w030" },
+	{ "WIRES ",          "502w031", "502w030", "502w030" },
+	{ "PHONE JACK",      "502w032", nullptr,   "502w032" },
+	{ "ARMCHAIR",        "502w034", nullptr,   "502w035" },
+	{ "ROCKER",          "502w036", nullptr,   "502w035" },
+	{ "LAMP",            "502w038", "502w039", "502w039" },
+	{ "PIANO",           "502w040", "502w041", "502w042" },
+	{ "SHEET MUSIC",     "502w043", "502w044", "502w045" },
+	{ "BOOKS",           "502w046", "502w047", "502w047" },
+	{ "BOOK",            "502w048", "502w049", "502w047" },
+	{ "PICTURE",         "502w050", "500w005", "502w051" },
+	{ "PICTURE ",        "502w052", "500w005", "502w051" },
+	{ "PAINTING",        "502w053", "502w054", "502w051" },
+	{ nullptr, nullptr, nullptr, nullptr }
+};
+
+
 void Room502::init() {
 	_G(flags)[V194] = 0;
 	Section5Room::init();
@@ -103,13 +132,87 @@ void Room502::daemon() {
 }
 
 void Room502::pre_parser() {
+	_G(kernel).trigger_mode = KT_DAEMON;
 
+	if (player_said("KITCHEN") && player_said_any("LOOK AT", "GEAR"))
+		player_set_facing_hotspot();
 }
 
 void Room502::parser() {
+	_G(kernel).trigger_mode = KT_DAEMON;
+	bool railing = player_said("RAILING") && _G(flags)[V200] == 5003;
+	bool takeKindling = player_said("KINDLING ") && player_said("TAKE");
+	bool gearKindling = player_said("KINDLING ") && player_said("GEAR");
+	bool fireplace = player_said("FIREPLACE") && _G(flags)[V198] != 0;
 
+	if (player_said("LOOK AT", "FRONT DOOR") && _G(flags)[V195]) {
+		wilbur_speech("502w004");
+	} else if (player_said("LOOK AT STAIRS") && _G(flags)[V200] == 5003) {
+		wilbur_speech("502w008");
+	} else if (railing && player_said("LOOK AT")) {
+		wilbur_speech("502w015");
+	} else if (railing && player_said("GEAR")) {
+		wilbur_speech("502w017");
+	} else if (takeKindling && inv_player_has("kindling")) {
+		wilbur_speech("502w019");
+	} else if (takeKindling && _G(flags)[V198]) {
+		wilbur_speech("502w020");
+	} else if (gearKindling && inv_player_has("KINDLING")) {
+		wilbur_speech("502w022");
+	} else if (gearKindling && _G(flags)[V198]) {
+		wilbur_speech("502w020");
+	} else if (player_said("KINDLING") && player_said("FIREPLACE")) {
+		wilbur_speech(_G(flags)[V198] ? "500w063" : "500w062");
+	} else if (player_said("KINDLING") && player_said("WIRES")) {
+		wilbur_speech("500w065");
+	} else if (fireplace && player_said("LOOK AT")) {
+		wilbur_speech("502w024");
+	} else if (fireplace && player_said("GEAR")) {
+		wilbur_speech("502w025");
+	} else if (fireplace && player_said("RUBBER DUCK")) {
+		wilbur_speech("502w030");
+	} else if (player_said("GEAR", "WINDOW") && _G(flags)[V200] == 5003) {
+		wilbur_speech("502w026");
+	} else if ((player_said("LOOK AT") || player_said("GEAR")) &&
+			player_said("PHONE JACK") && _G(flags)[V197] != 0) {
+		wilbur_speech("502w033");
+	} else if (player_said("LOOK AT", "ROCKER") && _G(flags)[V197] != 0) {
+		wilbur_speech("502w037");
+	} else if (_G(walker).wilbur_said(SAID)) {
+		// Already handled
+	} else if (player_said("GEAR", "FRONT DOOR")) {
+		_val1 = 2;
+		kernel_trigger_dispatch_now(gCHANGE_WILBUR_ANIMATION);
+	} else if (player_said("KITCHEN") && player_said_any("LOOK AT", "GEAR")) {
+		_val6 = 5007;
+		kernel_trigger_dispatch_now(1);
+	} else if (player_said("GEAR", "STAIRS")) {
+		_val1 = 3;
+		kernel_trigger_dispatch_now(gCHANGE_WILBUR_ANIMATION);
+	} else if (player_said("BORK ") && player_said("LOOK AT") &&
+			!player_said_any("GIZMO", "ROLLING PIN", "DIRTY SOCK", "SOAPY WATER", "RUBBER GLOVES") &&
+			!player_said("LAXATIVE")) {
+		player_set_commands_allowed(false);
+		_val3 = 26;
+		++_val2;
+	} else if (player_said("TAKE", "KINDLING ")) {
+		if (!_G(flags)[V198] && !inv_player_has("KINDLING")) {
+			_val1 = 10;
+			kernel_trigger_dispatch_now(gCHANGE_WILBUR_ANIMATION);
+		}
+	} else if (player_said("SOAPY WATER", "RAILING")) {
+		_val1 = 5;
+		kernel_trigger_dispatch_now(gCHANGE_WILBUR_ANIMATION);
+	} else if (player_said("BURNING KINDLING", "FIREPLACE")) {
+		_val1 = 12;
+		kernel_trigger_dispatch_now(gCHANGE_WILBUR_ANIMATION);
+		inv_move_object("BURNING KINDLING", NOWHERE);
+	} else {
+		return;
+	}
+
+	_G(player).command_ready = false;
 }
-
 void Room502::loadSeries1() {
 	static const char *NAMES[6] = {
 		"500_008", "500_009", "500_010", "500_011", "500_012", "500_013"
