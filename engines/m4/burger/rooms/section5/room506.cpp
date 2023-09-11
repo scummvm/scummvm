b80277cf42f443a77db8f7e22bbd545eb3fbdc39
Paul Gilbert <dreammaster@scummvm.org>
2023-09-10 18:32:30 -0700

    M4: Adding room 506 methods

--- engines/m4/burger/rooms/section5/room506.cpp
+++ engines/m4/burger/rooms/section5/room506.cpp
@@ -121,17 +121,143 @@ Room506::Room506() : Section5Room() {
 }
 
 void Room506::init() {
+	Section5Room::init();
+	pal_cycle_init(112, 127, 6);
+
+	for (_ctr = 0; _ctr < 5; ++_ctr)
+		_array1[_ctr] = -1;
+
+	if (_G(flags)[V219]) {
+		kernel_trigger_dispatch_now(12);
+	} else {
+		hotspot_set_active("FIRE", false);
+	}
+
+	if (_G(flags)[V219] == 0) {
+		_val1 = 19;
+		kernel_trigger_dispatch_now(10);
+	}
+
+	if (_G(flags)[V219]) {
+		series_show("506windo", 0xf00);
+	} else {
+		hotspot_set_active("ROOF", false);
+	}
+
+	switch (_G(game).previous_room) {
+	case RESTORING_GAME:
+		player_set_commands_allowed(true);
+		break;
+
+	case 505:
+		if (_G(flags)[V218] != 5003)
+			_G(flags)[V218] = 5000;
+
+		player_set_commands_allowed(true);
+		ws_demand_location(543, 252, 9);
+
+		if (player_been_here(506)) {
+			ws_walk(434, 254, nullptr, -1, 9);
+		} else {
+			ws_walk(434, 254, nullptr, 6, 9);
+		}
+		break;
+
+	case 508:
+		if (_G(flags)[V218] != 5003)
+			_G(flags)[V218] = 5000;
+
+		player_set_commands_allowed(false);
+		_G(wilbur_should) = 3;
+		kernel_trigger_dispatch_now(gCHANGE_WILBUR_ANIMATION);
+		break;
+
+	case 510:
+		ws_demand_location(_G(flags)[V187], _G(flags)[V188], _G(flags)[V189]);
+		_G(wilbur_should) = 10001;
+		kernel_trigger_dispatch_now(gCHANGE_WILBUR_ANIMATION);
+		break;
+
+	default:
+		player_set_commands_allowed(true);
+		ws_demand_location(434, 254, 9);
+		break;
+	}
+
+	_state2 = 0;
+
+	if (_G(flags)[V218] == 5003) {
+		hotspot_set_active("BORK", false);
+	} else {
+		loadSeries();
+		_val2 = 12;
+		kernel_trigger_dispatch_now(7);
+		_walk1 = intr_add_no_walk_rect(308, 278, 430, 314, 303, 328);
+	}
+
+	_initFlag = true;
+	Section5Room::init();
 }
 
 void Room506::daemon() {
+	// TODO
 }
 
 void Room506::pre_parser() {
-
+	if (player_said("AMPLIFIER", "TELEVISION") && _G(flags)[V218] != 5003) {
+		_G(wilbur_should) = 4;
+		player_hotspot_walk_override(456, 245, 8, gCHANGE_WILBUR_ANIMATION);
+		_G(player).command_ready = false;
+	} else if (player_said("FIRE", "KINDLING")) {
+		_G(wilbur_should) = 7;
+		player_hotspot_walk_override(409, 311, 4, gCHANGE_WILBUR_ANIMATION);
+		_G(player).command_ready = false;
+	} else if (player_said("HALLWAY") && player_said_any("LEAVE", "LOOK AT", "GEAR")) {
+		player_set_facing_hotspot();
+	}
 }
 
 void Room506::parser() {
+	_G(kernel).trigger_mode = KT_DAEMON;
 
+	if (player_said("GEAR") && player_said_any("WINDOW", "ROOF") && !_G(flags)[V219]) {
+		wilbur_speech("500w004");
+	} else if (player_said("TELEVISION") && player_said("LOOK AT")) {
+		wilbur_speech("506w006");
+	} else if (player_said("TELEVISION") && player_said("GEAR")) {
+		wilbur_speech("506w008");
+	} else if (_G(flags)[V219] != 0 && player_said("VIDEO GAME") &&
+			player_said_any("TAKE", "GEAR")) {
+		// No implementation
+	} else if (_G(walker).wilbur_said(SAID)) {
+		// No implementation
+	} else if (player_said("BORK") && player_said("LOOK AT") &&
+			!player_said_any("GIZMO", "ROLLING PIN", "DIRTY SOCK", "SOAPY WATER", "RUBBER GLOVES") &&
+			!player_said("LAXATIVE")) {
+		player_set_commands_allowed(false);
+		_val2 = 15;
+		++_state2;
+	} else if (player_said_any("ROOF", "WINDOW") && !player_said_any("ENTER", "GEAR") &&
+			_G(flags)[V219]) {
+		_G(wilbur_should) = 2;
+		kernel_trigger_dispatch_now(gCHANGE_WILBUR_ANIMATION);
+	} else if (player_said("HALLWAY") && player_said_any("LEAVE", "LOOK AT", "GEAR")) {
+		_val3 = 5009;
+		kernel_trigger_dispatch_now(3);
+	} else {
+		return;
+	}
+
+	_G(player).command_ready = false;
+}
+
+void Room506::loadSeries() {
+	series_load("506bk01");
+	series_load("506bk01s");
+	series_load("506bk02");
+	series_load("506bk02s");
+	series_load("506bk03");
+	series_load("506bk03s");
 }
 
 } // namespace Rooms
