5572bbddba9747929d74d8aced54ea1bd03e0349
Paul Gilbert <dreammaster@scummvm.org>
2023-09-15 21:36:42 -0700

    M4: Further section 4 static arrays

--- engines/m4/burger/rooms/section4/room407.cpp
+++ engines/m4/burger/rooms/section4/room407.cpp
@@ -35,6 +35,130 @@ const char *Room407::SAID[][4] = {
 	{ "FORCE FIELD ", "400w005", nullptr,   "400w001" }
 };
 
+const WilburMatch Room407::MATCH[] = {
+	{ "TALK", "DRUMZ",           11, 0, 0, nullptr, 0 },
+	{ "AMPLIFIER", "DRUMZ",       4, 0, 0, &_state1, 4 },
+	{ nullptr, "DRUMZ",           4, 0, 0, &_state1, 7 },
+	{ "LOOK AT", "ROXY",      10016, 0, 0, &Vars::_wilbur_should, 1 },
+	{ "TALK", "ROXY",         10016, 0, 0, &Vars::_wilbur_should, 2 },
+	{ "AMPLIFIER", "ROXY",        4, 0, 0, &_state1, 4 },
+	{ "DOG COLLAR", "ROXY",       4, 0, 0, &_state1, 5 },
+	{ nullptr, "ROXY",            4, 0, 0, &_state1, 6 },
+	{ "AMPLIFIER", "INSTRUMENTS", 4, 0, 0, &_state1, 8 },
+	{ nullptr, "INSTRUMENTS",     4, 0, 0, &_state1, 9 },
+	{ nullptr, "TOUR BUS",        4, 0, 0, &_state1, 3 },
+	{ nullptr, nullptr, -1, 0, 0, nullptr, 0 }
+};
+
+const seriesPlayBreak Room407::PLAY1[] = {
+	{ 0, -1, 0, 0, 0, -1, 0, 0, 0, 0 },
+	PLAY_BREAK_END
+};
+
+const seriesPlayBreak Room407::PLAY2[] = {
+	{ 0, -1, 0, 0, 0, -1, 0, 0, 0, 0 },
+	PLAY_BREAK_END
+};
+
+const seriesPlayBreak Room407::PLAY3[] = {
+	{ 0, 1, 0, 0, 0, -1, 0, 0, 0, 0 },
+	{ 2, -1, 0, 0, 0, -1, 0, 4, 0, 0 },
+	{ 0, 1, 0, 0, 0, -1, 2, 0, 0, 0 },
+	PLAY_BREAK_END
+};
+
+const seriesPlayBreak Room407::PLAY4[] = {
+	{ 0, -1, 0, 0, 0, -1, 0, 0, 0, 0 },
+	PLAY_BREAK_END
+};
+
+const seriesPlayBreak Room407::PLAY5[] = {
+	{ 0, -1, "145_004", 2, 255, -1, 1, 0, 0, 0 },
+	PLAY_BREAK_END
+};
+
+const seriesPlayBreak Room407::PLAY6[] = {
+	{ 0, -1, 0, 0, 0, -1, 0, 0, 0, 0 },
+	PLAY_BREAK_END
+};
+
+const seriesPlayBreak Room407::PLAY7[] = {
+	{ 0, -1, 0, 0, 0, -1, 0, 0, 0, 0 },
+	PLAY_BREAK_END
+};
+
+const seriesPlayBreak Room407::PLAY8[] = {
+	{ 0, -1, "407r901", 1, 255, -1, 4, -1, 0, 0 },
+	PLAY_BREAK_END
+};
+
+const seriesPlayBreak Room407::PLAY9[] = {
+	{ 0, -1, "407r903", 1, 255, -1, 4, -1, 0, 0 },
+	PLAY_BREAK_END
+};
+
+const seriesPlayBreak Room407::PLAY10[] = {
+	{ 0, -1, 0, 0, 0, -1, 2, 0, 0, 0 },
+	PLAY_BREAK_END
+};
+
+const seriesPlayBreak Room407::PLAY11[] = {
+	{ 0, 2, 0, 0, 0, -1, 0, 0, 0, 0 },
+	{ 3, -1, "407r904x", 1, 255, -1, 4, -1, 0, 0 },
+	PLAY_BREAK_END
+};
+
+const seriesPlayBreak Room407::PLAY12[] = {
+	{ 0, 6, 0, 0, 0, -1, 0, 0, 0, 0 },
+	PLAY_BREAK_END
+};
+
+const seriesPlayBreak Room407::PLAY13[] = {
+	{ 7, -1, 0, 0, 0, -1, 0, 0, 0, 0 },
+	PLAY_BREAK_END
+};
+
+const seriesPlayBreak Room407::PLAY14[] = {
+	{ 0, -1, "407r904y", 1, 255, -1, 4, -1, 0, 0 },
+	PLAY_BREAK_END
+};
+
+const seriesPlayBreak Room407::PLAY15[] = {
+	{ 0, 3, "407v901", 1, 255, -1, 4, -1, 0, 0 },
+	PLAY_BREAK_END
+};
+
+const seriesPlayBreak Room407::PLAY16[] = {
+	{ 5, -1, 0, 0, 0, -1, 0, 0, 0, 0 },
+	PLAY_BREAK_END
+};
+
+const seriesPlayBreak Room407::PLAY17[] = {
+	{ 1, -1, 0, 0, 0, -1, 0, 0, 0, 0 },
+	PLAY_BREAK_END
+};
+
+const seriesPlayBreak Room407::PLAY18[] = {
+	{ 0, 1, "999_003", 1, 255, -1, 0, 0, 0, 0 },
+	{ 2, 9, 0, 0, 0, 7, 0, 0, 0, 0 },
+	{ 10, 11, "999_003", 1, 255, -1, 0, 0, 0, 0 },
+	{ 12, -1, 0, 0, 0, 9, 0, 0, 0, 0 },
+	PLAY_BREAK_END
+};
+
+const seriesPlayBreak Room407::PLAY19[] = {
+	{ 0, 25, 0, 1, 0, -1, 2048, 0, 0, 0 },
+	{ 26, 42, "500_030", 1, 255, -1, 0, 0, 0, 0 },
+	{ 43, -1, "500_030", 1, 255, -1, 0, 0, 0, 0 },
+	PLAY_BREAK_END
+};
+
+long Room407::_state1;
+
+
+Room407::Room407() : Room() {
+	_state1 = 0;
+}
 
 void Room407::init() {
 }
--- engines/m4/burger/rooms/section4/room407.h
+++ engines/m4/burger/rooms/section4/room407.h
@@ -23,6 +23,7 @@
 #define M4_BURGER_ROOMS_SECTION4_ROOM407_H
 
 #include "m4/burger/rooms/room.h"
+#include "m4/burger/walker.h"
 
 namespace M4 {
 namespace Burger {
@@ -31,9 +32,30 @@ namespace Rooms {
 class Room407 : public Room {
 private:
 	static const char *SAID[][4];
+	static const WilburMatch MATCH[];
+	static const seriesPlayBreak PLAY1[];
+	static const seriesPlayBreak PLAY2[];
+	static const seriesPlayBreak PLAY3[];
+	static const seriesPlayBreak PLAY4[];
+	static const seriesPlayBreak PLAY5[];
+	static const seriesPlayBreak PLAY6[];
+	static const seriesPlayBreak PLAY7[];
+	static const seriesPlayBreak PLAY8[];
+	static const seriesPlayBreak PLAY9[];
+	static const seriesPlayBreak PLAY10[];
+	static const seriesPlayBreak PLAY11[];
+	static const seriesPlayBreak PLAY12[];
+	static const seriesPlayBreak PLAY13[];
+	static const seriesPlayBreak PLAY14[];
+	static const seriesPlayBreak PLAY15[];
+	static const seriesPlayBreak PLAY16[];
+	static const seriesPlayBreak PLAY17[];
+	static const seriesPlayBreak PLAY18[];
+	static const seriesPlayBreak PLAY19[];
+	static long _state1;
 
 public:
-	Room407() : Room() {}
+	Room407();
 	~Room407() override {}
 
 	void init() override;
