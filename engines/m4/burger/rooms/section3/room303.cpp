1cbd06f672e35ee2aac6323bd1e7ba32e14cddbc
Paul Gilbert <dreammaster@scummvm.org>
2023-09-17 16:09:43 -0700

    M4: Remainder of room 303

--- engines/m4/burger/rooms/section3/room303.cpp
+++ engines/m4/burger/rooms/section3/room303.cpp
@@ -209,6 +209,432 @@ void Room303::init() {
 }
 
 void Room303::daemon() {
+	doDaemon(_G(kernel).trigger);
+}
+
+void Room303::doDaemon(int trigger) {
+	switch (trigger) {
+	case 1:
+		for (_ctr = 0; _ctr < 5; ++_ctr) {
+			if (_triggers[_ctr] != -1) {
+				kernel_trigger_dispatch_now(_triggers[_ctr]);
+				_triggers[_ctr] = -1;
+			}
+		}
+		break;
+
+	case 2:
+		if (_G(flags)[V115] > 1) {
+			player_set_commands_allowed(true);
+			ws_walk(290, 325, 0, 4, 1);
+		} else {
+			_G(wilbur_should) = 18;
+			player_set_commands_allowed(false);
+			ws_walk(290, 325, 0, gCHANGE_WILBUR_ANIMATION, 1);
+		}
+		break;
+
+	case 3:
+		if (_G(flags)[V115] > 1) {
+			player_set_commands_allowed(true);
+			ws_walk(261, 276, 0, 4, 3);
+		} else {
+			_G(wilbur_should) = 18;
+			player_set_commands_allowed(false);
+			ws_walk(261, 276, 0, gCHANGE_WILBUR_ANIMATION, 3);
+		}
+		break;
+
+	case 4:
+		player_set_commands_allowed(true);
+		break;
+
+	case 5:
+		setupDigi();
+		break;
+
+	case 6:
+		switch (_val7) {
+		case 25:
+			if (_G(flags)[V119] && _G(flags)[V118] == 3002) {
+				terminateMachineAndNull(_series7);
+				series_unload(_series6);
+
+				_G(flags)[V120] = 1;
+				_val7 = 26;
+				digi_preload_stream_breaks(SERIES1);
+				series_stream_with_breaks(SERIES1, "303dist", 6, 0xc10, 6);
+				player_set_commands_allowed(false);
+				intr_cancel_sentence();
+				ws_walk(330, 350, nullptr, -1, 2);
+			}
+			break;
+
+		case 26:
+			digi_unload_stream_breaks(SERIES1);
+			player_set_commands_allowed(true);
+
+			_G(flags)[V119] = 0;
+			_series6 = series_load("303stil");
+			_series7 = series_show("303stil", 0xd10);
+
+			if (_G(flags)[V121] == 3002) {
+				_val8 = 27;
+				_G(flags)[V122] = 1;
+				terminateMachineAndNull(_series8);
+				_val6 = 33;
+				kernel_trigger_dispatch_now(10);
+			} else {
+				_val8 = 28;
+			}
+
+			kernel_trigger_dispatch_now(9);
+			break;
+
+		default:
+			break;
+		}
+		break;
+
+	case 7:
+		switch (_val3) {
+		case 1:
+			freeSeries1();
+			series_play_with_breaks(PLAY6, "303wi07", 0xd00, 7, 3);
+
+			_val3 = 2;
+			_G(flags)[V117] = 1;
+			setupDigi();
+			ws_hide_walker();
+
+			_G(wilbur_should) = 19;
+			_timer = timer_read_60();
+			break;
+
+		case 2:
+			_series2 = series_load("303ft_on");
+			_series3 = series_play("303ft_on", 0xd00, 0, -1, 0, -1);
+
+			if (_G(wilbur_should) == 19)
+				kernel_trigger_dispatch_now(gCHANGE_WILBUR_ANIMATION);
+			break;
+
+		case 3:
+			freeSeries1();
+			series_play_with_breaks(PLAY7, "303wi08", 0xd00, 7, 3);
+			_val3 = 4;
+			ws_hide_walker();
+			_G(wilbur_should) = 20;
+			break;
+
+		case 4:
+			_series2 = series_load("303ft");
+			_series3 = series_show("303ft", 0xd00);
+			_G(flags)[V117] = 0;
+			setupDigi();
+
+			if (_G(flags)[V118] == 3002) {
+				_val4 = 8;
+				doDaemon(8);
+			}
+			if (_G(wilbur_should) == 20)
+				kernel_trigger_dispatch_now(gCHANGE_WILBUR_ANIMATION);
+		}
+		break;
+
+	case 8:
+		switch (_val4) {
+		case 5:
+			freeSeries2();
+			player_update_info();
+
+			if (timer_read_60() - _timer > 900) {
+				_G(flags)[V118] = 3003;
+				series_play_with_breaks(PLAY8, "303wi10", 0xd01, 3007, 1);
+			} else {
+				_G(flags)[V118] = 3002;
+				series_play_with_breaks(PLAY9, "303wi02", 0xd01, 8, 3);
+			}
+
+			_val4 = 7;
+			ws_hide_walker();
+			_G(wilbur_should) = 10001;
+			break;
+
+		case 6:
+			freeSeries2();
+			series_play_with_breaks(PLAY9, "303wi09", 0xd01, 8, 3);
+			_val4 = 9;
+			ws_hide_walker();
+			_G(wilbur_should) = 10001;
+			break;
+
+		case 7:
+			setupDigi();
+			_series4 = series_load("303burn");
+			_series5 = series_play("303burn", 0xd14, 0, -1, 0, -1);
+
+			if (_G(wilbur_should) == 10001) {
+				ws_unhide_walker();
+				_G(wilbur_should) = 10002;
+				player_set_commands_allowed(true);
+			}
+
+			_val7 = 25;
+			kernel_timing_trigger(180, 6);
+			break;
+
+		case 8:
+			freeSeries2();
+			_series4 = series_load("303burnr");
+			_series5 = series_show("303burnr", 0xd14);
+			_G(flags)[V118] = 3001;
+			setupDigi();
+			break;
+
+		case 9:
+			_series4 = series_load("303burnr");
+			_series5 = series_show("303burnr", 0xd14);
+			_G(flags)[V118] = 3001;
+
+			if (_G(wilbur_should) == 10001) {
+				ws_unhide_walker();
+				_G(wilbur_should) = 10002;
+				player_set_commands_allowed(true);
+			}
+			break;
+
+		default:
+			break;
+		}
+		break;
+
+	case 9:
+		switch (_val8) {
+		case 27:
+			if (_val5 < 15) {
+				++_val5;
+				_val8 = 27;
+				terminateMachineAndNull(_series9);
+				_series9 = series_play("303cj01", 0xd15, 0, 9, 8);
+			}
+			break;
+		case 28:
+			if (_val5 < 15) {
+				++_val5;
+				_val8 = 28;
+				terminateMachineAndNull(_series9);
+				_series9 = series_play("303cj02", 0xd15, 0, 9, 6);
+			}
+			break;
+
+		default:
+			break;
+		}
+		break;
+
+	case 10:
+		switch (_val6) {
+		case 30:
+			inv_move_object("JUG", 303);
+			inv_move_object("DISTILLED CARROT JUICE", 303);
+
+			_G(flags)[V121] = 3001;
+			_series8 = series_show("303juga", 0xbff);
+			hotspot_set_active("JUG  ", true);
+			break;
+
+		case 31:
+			inv_move_object("JUG", 303);
+			inv_move_object("DISTILLED CARROT JUICE", 303);
+
+			_G(flags)[V121] = 3002;
+			_series8 = series_show("303JUG", 0xd14);
+			hotspot_set_active("JUG ", true);
+			break;
+
+		case 32:
+			inv_move_object("JUG", 303);
+			inv_move_object("DISTILLED CARROT JUICE", 303);
+			_G(flags)[V122] = 1;
+			_G(flags)[V121] = 3001;
+
+			_series8 = series_show("303juga", 0xbff);
+			hotspot_set_active("DISTILLED CARROT JUICE  ", true);
+			break;
+
+		case 33:
+			inv_move_object("JUG", 303);
+			inv_move_object("DISTILLED CARROT JUICE", 303);
+			_G(flags)[V122] = 1;
+			_G(flags)[V121] = 3002;
+
+			_series8 = series_show("303JUG", 0xd14);
+			hotspot_set_active("DISTILLED CARROT JUICE ", true);
+			hotspot_set_active("JUG ", true);
+			break;
+
+		case 34:
+			_G(flags)[V121] = 3003;
+			inv_give_to_player(_G(flags)[V122] ? "DISTILLED CARROT JUICE" : "JUG");
+			break;
+
+		default:
+			break;
+		}
+		break;
+
+	case 11:
+		terminateMachineAndNull(_series8);
+		hotspot_set_active("DISTILLED CARROT JUICE ", false);
+		hotspot_set_active("DISTILLED CARROT JUICE  ", false);
+		hotspot_set_active("JUG ", false);
+		hotspot_set_active("JUG  ", false);
+		break;
+
+	case gCHANGE_WILBUR_ANIMATION:
+		switch (_G(wilbur_should)) {
+		case 11:
+			player_set_commands_allowed(false);
+			ws_hide_walker();
+			_G(wilbur_should) = 10001;
+			_triggers[0] = gCHANGE_WILBUR_ANIMATION;
+
+			if (_val5 > 0 && _val5 < 15) {
+				_val8 = 27;
+				_G(flags)[V122] = 1;
+			} else {
+				_val8 = 29;
+			}
+
+			_val6 = _G(flags)[V122] ? 33 : 31;
+			_val9 = 10;
+			series_play_with_breaks(PLAY1, "303wi4b", 0x100, 1, 3);
+			break;
+
+		case 12:
+			player_set_commands_allowed(false);
+			ws_hide_walker();
+			_G(wilbur_should) = 10001;
+			_triggers[0] = gCHANGE_WILBUR_ANIMATION;
+			kernel_trigger_dispatch_now(11);
+
+			_val6 = 34;
+			_val9 = 10;
+			_val8 = (_val5 > 0 && _val5 < 15) ? 28 : 29;
+			series_play_with_breaks(PLAY3, "303wi4b", 0x100, 1, 3);
+			break;
+
+		case 13:
+			ws_hide_walker();
+			player_set_commands_allowed(false);
+			kernel_trigger_dispatch_now(11);
+			_G(wilbur_should) = 10001;
+			_triggers[0] = gCHANGE_WILBUR_ANIMATION;
+			_val6 = 34;
+			_val9 = 10;
+			series_play_with_breaks(PLAY4, "303wi4a", 0x100, 1, 3);
+			break;
+
+		case 14:
+			player_set_commands_allowed(false);
+			ws_hide_walker();
+
+			_G(wilbur_should) = 10001;
+			_triggers[0] = gCHANGE_WILBUR_ANIMATION;
+			_val6 = _G(flags)[V122] ? 32 : 30;
+			_val9 = 10;
+			series_play_with_breaks(PLAY5, "303wi4a", 0x100, 1, 3);
+			break;
+
+		case 15:
+			player_set_commands_allowed(false);
+			ws_hide_walker();
+			_G(wilbur_should) = 23;
+			series_play_with_breaks(PLAY10, "303wi15", 0x100, gCHANGE_WILBUR_ANIMATION, 3);
+			break;
+
+		case 16:
+			player_set_commands_allowed(false);
+			ws_hide_walker();
+			_G(wilbur_should) = 17;
+			series_play_with_breaks(PLAY2, "303wi03", 0x100, gCHANGE_WILBUR_ANIMATION, 3);
+			break;
+
+		case 17:
+			_G(flags)[V119] = 1;
+			_G(flags)[V126] = 1;
+			inv_put_thing_in("CARROT JUICE", NOWHERE);
+			inv_give_to_player("BOTTLE");
+			_val7 = 25;
+			kernel_trigger_dispatch_now(6);
+			enable_player();
+			break;
+
+		case 18:
+			_G(wilbur_should) = 10001;
+			wilbur_speech(_G(flags)[V115] ? "303w002" : "303w001");
+			_G(flags)[V115]++;
+			break;
+
+		case 19:
+			player_set_commands_allowed(true);
+			ws_unhide_walker();
+			_G(wilbur_should) = 10002;
+
+			if (_G(flags)[V116]) {
+				wilbur_speech("303w006");
+			} else {
+				wilbur_speech("303w005");
+				_G(flags)[V116] = 1;
+			}
+			break;
+
+		case 20:
+			player_set_commands_allowed(true);
+			ws_unhide_walker();
+			_G(wilbur_should) = 10002;
+			wilbur_speech("303w007");
+			break;
+
+		case 21:
+			player_set_commands_allowed(true);
+			ws_unhide_walker();
+			_G(wilbur_should) = 10002;
+			wilbur_speech(_G(flags)[V118] == 3001 ? "303w008" : "303w009");
+			break;
+
+		case 22:
+			player_set_commands_allowed(true);
+			ws_unhide_walker();
+			_G(wilbur_should) = 10002;
+			wilbur_speech(_G(flags)[V118] == 3001 ? "303w011" : "303w012");
+			break;
+
+		case 23:
+			player_set_commands_allowed(true);
+			ws_unhide_walker();
+			_G(wilbur_should) = 10002;
+			wilbur_speech("303w016");
+			break;
+
+		case 24:
+			player_set_commands_allowed(true);
+			ws_unhide_walker();
+			_G(wilbur_should) = 10002;
+			wilbur_speech("303w017");
+			break;
+
+		default:
+			_G(kernel).continue_handling_trigger = true;
+			break;
+		}
+		break;
+
+	default:
+		_G(kernel).continue_handling_trigger = true;
+		break;
+	}
 }
 
 void Room303::pre_parser() {
@@ -319,6 +745,19 @@ void Room303::frontYard() {
 	}
 }
 
+void Room303::freeSeries1() {
+	if (_series3)
+		terminateMachineAndNull(_series3);
+	if (_series2)
+		series_unload(_series2);
+}
+
+void Room303::freeSeries2() {
+	if (_series5)
+		terminateMachineAndNull(_series5);
+	if (_series4)
+		series_unload(_series4);
+}
 
 } // namespace Rooms
 } // namespace Burger
