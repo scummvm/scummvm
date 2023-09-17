f6cf6f0e95810dad16b81bb2738f11604c8fe9fe
Paul Gilbert <dreammaster@scummvm.org>
2023-09-16 22:41:04 -0700

    M4: Finished room 302

--- engines/m4/burger/rooms/section3/room302.cpp
+++ engines/m4/burger/rooms/section3/room302.cpp
@@ -226,7 +226,7 @@ void Room302::init() {
 
 		if (_G(flags)[V112]) {
 			_G(flags)[V113] = 1;
-			_val3 = 125;
+			_volume = 125;
 			_val4 = 29;
 		} else {
 			_val4 = 30;
@@ -246,6 +246,283 @@ void Room302::init() {
 }
 
 void Room302::daemon() {
+	switch (_G(kernel).trigger) {
+	case 1:
+		wilbur_speech("302w009z");
+		break;
+
+	case 2:
+		ws_unhide_walker();
+		ws_walk(500, 250, nullptr, 3, 8, 1);
+		break;
+
+	case 3:
+		player_set_commands_allowed(true);
+		break;
+
+	case 4:
+		_volume -= 20;
+		if (_volume > 0) {
+			term_message("fading propellor, current volume = %d", _volume);
+			digi_change_volume(2, _volume);
+			kernel_timing_trigger(10, 4);
+		} else {
+			digi_stop(2);
+		}
+		break;
+
+	case 5:
+		terminateMachineAndNull(_series1);
+		inv_give_to_player("BURGER MORSEL");
+		hotspot_set_active("BURGER MORSEL ", false);
+		_G(flags)[V107] = 1;
+		break;
+
+	case 6:
+		inv_put_thing_in("BURGER MORSEL", NOWHERE);
+		break;
+
+	case 7:
+		if (!digi_play_state(2)) {
+			_val7 = imath_ranged_rand(0, 6);
+			digi_play(Common::String::format("302t001%c", 'a' + _val7).c_str(), 2, 55, -1, 302);
+		}
+		break;
+
+	case 8:
+		if (_val2 == 19) {
+			digi_unload_stream_breaks(SERIES3);
+			_series2.terminate();
+			pal_fade_set_start(0);
+			_series3 = series_stream_with_breaks(SERIES1, "302pe02", 6, 1, 3007);
+			pal_fade_init(0, 255, 100, 15, -1);
+		}
+		break;
+
+	case 9:
+		switch (_val1) {
+		case 20:
+			series_show("302tr02", 0x900, 0, 9, 30, 13);
+			series_show("302tr02s", 0x901, 0, -1, 30, 13);
+			_val1 = 22;
+			break;
+
+		case 21:
+			_series2.show("302tr02", 0x900);
+			_val1 = 22;
+			break;
+
+		case 22:
+			_val6 = imath_ranged_rand(0, 1);
+			_val1 = _val6 ? 24 : 23;
+			kernel_trigger_dispatch_now(9);
+			break;
+
+		case 23:
+			kernel_trigger_dispatch_now(7);
+			_val1 = 20;
+			series_play_with_breaks(PLAY10, "302tr02", 0x900, 9, 3);
+			break;
+
+		case 24:
+			kernel_trigger_dispatch_now(7);
+			_val1 = 20;
+			series_play_with_breaks(PLAY11, "302tr02", 0x900, 9, 3);
+			break;
+
+		case 25:
+			_val1 = 20;
+			series_play_with_breaks(PLAY12, "302tr03", 0x900, 9, 3);
+			break;
+
+		case 26:
+			_val1 = 20;
+			series_play_with_breaks(PLAY13, "302tr03", 0x900, 9, 3);
+			break;
+
+		case 27:
+			ws_hide_walker();
+			_G(wilbur_should) = 11;
+			kernel_trigger_dispatch_now(gCHANGE_WILBUR_ANIMATION);
+			_val1 = 28;
+			series_play_with_breaks(PLAY7, "302tr01", 0x700, 9, 3);
+			break;
+
+		case 28:
+			hotspot_set_active("truffles", false);
+			_G(flags)[V111] = 1;
+			_G(wilbur_should) = 8;
+			kernel_trigger_dispatch_now(gCHANGE_WILBUR_ANIMATION);
+			series_play_with_breaks(PLAY8, "302tr01", 0x900, -1, 3);
+			break;
+
+		default:
+			break;
+		}
+		break;
+
+	case 10:
+		switch (_val4) {
+		case 29:
+			digi_preload_stream_breaks(SERIES2);
+			_G(wilbur_should) = 9;
+			series_stream_with_breaks(SERIES2, "302pr01", 6, 1, gCHANGE_WILBUR_ANIMATION);
+			break;
+
+		case 30:
+			digi_preload_stream_breaks(SERIES3);
+			_val2 = 19;
+			series_stream_with_breaks(SERIES3, "302pr02", 6, 1, 8);
+			break;
+
+		default:
+			break;
+		}
+		break;
+
+	case 10008:
+		_val1 = 26;
+		break;
+
+	case gCHANGE_WILBUR_ANIMATION:
+		switch (_G(wilbur_should)) {
+		case 1:
+			ws_hide_walker();
+			player_set_commands_allowed(false);
+			series_play_with_breaks(PLAY1, "302wi03", 0x500, 3004, 3);
+			pal_fade_init(_G(kernel).first_fade, 255, 0, 120, 3004);
+			break;
+
+		case 2:
+			ws_demand_location(427, 228, 4);
+			ws_hide_walker();
+			player_set_commands_allowed(false);
+			series_play_with_breaks(PLAY2, "302wi04", 0x500, 2, 3);
+			break;
+
+		case 3:
+			ws_hide_walker();
+			player_set_commands_allowed(false);
+			series_play_with_breaks(PLAY3, "302wi01", 0x500, 3003, 3);
+			pal_fade_init(_G(kernel).first_fade, 255, 0, 120, 3003);
+			break;
+
+		case 4:
+			player_first_walk(233, 222, 7, 180, 270, 7, true);
+			ws_unhide_walker();
+			player_set_commands_allowed(true);
+			break;
+
+		case 5:
+			ws_unhide_walker();
+			player_set_commands_allowed(true);
+			_G(wilbur_should) = 10002;
+			player_first_walk(483, 210, 5, 490, 245, 7, true);
+			break;
+
+		case 6:
+			ws_unhide_walker();
+			player_set_commands_allowed(true);
+			_G(wilbur_should) = 10002;
+			player_first_walk(184, 189, 8, 78, 225, 8, true);
+			break;
+
+		case 7:
+			_val1 = 27;
+			break;
+
+		case 8:
+			ws_unhide_walker();
+			_G(wilbur_should) = _G(flags)[V110] ? 10001 : 18;
+			ws_turn_to_face(3, gCHANGE_WILBUR_ANIMATION);
+			break;
+
+		case 9:
+			digi_unload_stream_breaks(SERIES2);
+			_G(wilbur_should) = 12;
+			hotspot_set_active("BURGER MORSEL ", true);
+			series_play_with_breaks(PLAY4, "302wi10", 0x200, gCHANGE_WILBUR_ANIMATION, 2);
+			break;
+
+		case 10:
+			ws_hide_walker();
+			_G(wilbur_should) = 10001;
+			series_play_with_breaks(PLAY6, "302wi06", 0x200, gCHANGE_WILBUR_ANIMATION, 3);
+			break;
+
+		case 11:
+			_G(wilbur_should) = 17;
+			series_play_with_breaks(PLAY9, "302wi07", 0x200, gCHANGE_WILBUR_ANIMATION, 3, 4);
+			break;
+
+		case 12:
+			_series1 = series_show("302BURG", 0x200);
+			_G(flags)[V107] = 0;
+			_G(wilbur_should) = 15;
+			series_play_with_breaks(PLAY5, "302wi09", 0x200, gCHANGE_WILBUR_ANIMATION, 3);
+			break;
+
+		case 13:
+			_G(wilbur_should) = 14;
+			kernel_timing_trigger(30, gCHANGE_WILBUR_ANIMATION);
+			break;
+
+		case 14:
+			player_set_commands_allowed(true);
+			wilbur_speech("302w009z");
+			break;
+
+		case 15:
+			ws_unhide_walker();
+			_G(wilbur_should) = 10001;
+
+			if (_G(flags)[V108]) {
+				_val5 = imath_ranged_rand(0, 9);
+				wilbur_speech(Common::String::format("302w004%c", 'a' + _val5).c_str(), gCHANGE_WILBUR_ANIMATION);
+			} else {
+				wilbur_speech("302w003", gCHANGE_WILBUR_ANIMATION);
+			}
+			break;
+
+		case 16:
+			_G(wilbur_should) = 7;
+			player_set_commands_allowed(false);
+			wilbur_speech(_G(flags)[V110] ? "300w010" : "300w007", gCHANGE_WILBUR_ANIMATION);
+			break;
+
+		case 17:
+			ws_unhide_walker();
+			if (!_G(flags)[V110])
+				wilbur_speech("300w008");
+			break;
+
+		case 18:
+			_G(flags)[V110] = 1;
+			_G(wilbur_should) = 10001;
+			wilbur_speech("300w009");
+			break;
+
+		case 10013:
+			ws_unhide_walker();
+			player_set_commands_allowed(true);
+			_G(wilbur_should) = 10002;
+
+			if (_G(flags)[V111]) {
+				_G(flags)[V002] = 1;
+				wilbur_speech("300w059");
+			}
+			break;
+
+		default:
+			_G(kernel).continue_handling_trigger = true;
+			break;
+		}
+		break;
+
+	default:
+		_G(kernel).continue_handling_trigger = true;
+		break;
+	}
 }
 
 void Room302::pre_parser() {
