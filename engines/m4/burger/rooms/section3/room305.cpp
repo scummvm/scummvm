6509f6aed19e0f72146e310fa91a5c204eb323a0
Paul Gilbert <dreammaster@scummvm.org>
2023-09-18 22:01:49 -0700

    M4: Added room 305

--- engines/m4/burger/rooms/section3/room305.cpp
+++ engines/m4/burger/rooms/section3/room305.cpp
@@ -106,11 +106,163 @@ void Room305::init() {
 }
 
 void Room305::daemon() {
+	MineRoom::daemon();
+	if (!_G(kernel).continue_handling_trigger)
+		return;
+	_G(kernel).continue_handling_trigger = false;
+
+	switch (_G(kernel).trigger) {
+	case 100:
+		_series1 = series_show("305genx", 0x200);
+		_G(flags)[V134] = 1;
+		_G(wilbur_should) = 105;
+		kernel_trigger_dispatch_now(gCHANGE_WILBUR_ANIMATION);
+		break;
+
+	case 3001:
+		_G(game).new_room = 302;
+		break;
+
+	case gCHANGE_WILBUR_ANIMATION:
+		switch (_G(wilbur_should)) {
+		case 101:
+			ws_demand_location(372, 267, 9);
+			player_set_commands_allowed(false);
+			ws_hide_walker();
+
+			if (_G(flags)[V133]) {
+				_G(wilbur_should) = 10001;
+			} else {
+				_G(flags)[V133] = 1;
+				_G(wilbur_should) = 104;
+			}
+
+			series_play_with_breaks(PLAY1, "305wi01", 0x700, gCHANGE_WILBUR_ANIMATION, 3);
+			break;
+
+		case 102:
+			player_set_commands_allowed(false);
+			ws_demand_location(375, 280, 2);
+			ws_hide_walker();
+			series_play_with_breaks(PLAY2, "305wi02", 0x700, -1, 3);
+			pal_fade_init(_G(kernel).first_fade, 255, 0, 60, 3001);
+			break;
+
+		case 103:
+			hotspot_set_active("DARKNESS", false);
+			hotspot_set_active("MINE", true);
+			player_set_commands_allowed(false);
+			ws_hide_walker();
+			terminateMachineAndNull(_series1);
+			series_stream_with_breaks(SERIES1, "305wi04", 6, 0x200, 100);
+			break;
+
+		case 104:
+			ws_unhide_walker();
+			player_set_commands_allowed(true);
+			wilbur_speech("305w001");
+			break;
+
+		case 105:
+			ws_unhide_walker();
+
+			if (_G(flags)[V135]) {
+				_G(wilbur_should) = 106;
+				kernel_trigger_dispatch_now(gCHANGE_WILBUR_ANIMATION);
+			} else {
+				_G(flags)[V135] = 1;
+				_G(wilbur_should) = 106;
+				wilbur_speech("305w011", gCHANGE_WILBUR_ANIMATION);
+			}
+			break;
+
+		case 106:
+			_G(wilbur_should) = 10001;
+			wilbur_speech(_G(flags)[V137] ? "305w013" : "305w012", gCHANGE_WILBUR_ANIMATION);
+			break;
+
+		case 107:
+			_G(wilbur_should) = 10001;
+			wilbur_speech("305w012", gCHANGE_WILBUR_ANIMATION);
+			break;
+
+		case 108:
+			changeRoom(DIR_NORTH);
+			break;
+
+		default:
+			_G(kernel).continue_handling_trigger = true;
+			break;
+		}
+		break;
+
+	default:
+		_G(kernel).continue_handling_trigger = true;
+		break;
+	}
 }
 
 void Room305::parser() {
 	_G(kernel).trigger_mode = KT_DAEMON;
 
+	// Handle any generic mine stuff
+	MineRoom::parser();
+	if (!_G(player).command_ready)
+		// Already handled
+		return;
+
+	// Check for standard actions
+	if (_G(walker).wilbur_said(SAID)) {
+		// Handled
+	} else if (player_said("climb", "stairs") || player_said("GEAR", "STAIRS")) {
+		player_set_commands_allowed(false);
+		kernel_trigger_dispatch_now(102);
+	} else if (player_said("ENTER", "MINE") || player_said("ENTER", "DARKNESS")) {
+		player_set_commands_allowed(false);
+
+		if (!_G(flags)[V124]) {
+			player_set_commands_allowed(true);
+			wilbur_speech("305w004");
+		} else {
+			_G(wilbur_should) = 108;
+
+			if (_G(flags)[V137]) {
+				kernel_trigger_dispatch_now(gCHANGE_WILBUR_ANIMATION);
+			} else {
+				wilbur_speech("305w005");
+			}
+		}
+	} else if (player_said("DISTILLED CARROT JUICE", "GENERATOR")) {
+		wilbur_speech(_G(flags)[V134] ? "300w037" : "300w036");
+	} else if (player_said("CARROT JUICE", "GENERATOR")) {
+		wilbur_speech(_G(flags)[V134] ? "300w054" : "300w053");
+	} else if (player_said_any("CARROT JUICE", "DISTILLED CARROT JUICE") &&
+			player_said("BARREL")) {
+		wilbur_speech("300w038");
+	} else if (player_said("LOOK AT", "GENERATOR")) {
+		wilbur_speech(_G(flags)[V134] ? "300w007" : "300w006");
+	} else if (player_said("MATCHES", "GENERATOR")) {
+		wilbur_speech(_G(flags)[V134] ? "300w022" : "300w021");
+	} else if (player_said("LOOK AT", "DEBRIS")) {
+		if (!_G(flags)[V136]) {
+			_G(flags)[V136] = 1;
+			wilbur_speech("305w019");
+		} else {
+			wilbur_speech(Common::String::format("305w020%c",
+				'a' + imath_ranged_rand(0, 6)).c_str());
+		}
+	} else if (player_said("GEAR", "GENERATOR")) {
+		if (_G(flags)[V134]) {
+			wilbur_speech("305w014");
+		} else {
+			_G(wilbur_should) = 103;
+			kernel_trigger_dispatch_now(gCHANGE_WILBUR_ANIMATION);
+		}
+	} else if (!player_said("LOOK AT WALL")) {
+		return;
+	}
+
+	_G(player).command_ready = false;
 }
 
 } // namespace Rooms
