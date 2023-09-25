f8935d41be5159c89336c77592d3647ee99bb996
eientei <einstein95@users.noreply.github.com>
2023-09-25 14:24:05 +1300

    MOHAWK: Add detection for Maths Workshop Deluxe (US)

--- engines/mohawk/detection.cpp
+++ engines/mohawk/detection.cpp
@@ -75,6 +75,7 @@ static const PlainGameDescriptor mohawkGames[] = {
 	{"arthurreading", "Arthur's Reading Games"},
 	{"wsg", "Williams-Sonoma Guide to Good Cooking" },
 	{"mathsworkshop", "Maths Workshop" },
+	{"mathsworkshopdx", "Maths Workshop Deluxe" },
 	{"wricamact", "Write, Camera, Action!"},
 	{"amazingwriting", "The Amazing Writing Machine"},
 	{nullptr, nullptr}
