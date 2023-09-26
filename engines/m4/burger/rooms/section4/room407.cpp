bf65c714cc539db36ad1162f3a531d8b1025ff09
Paul Gilbert <dreammaster@scummvm.org>
2023-09-25 20:42:04 -0700

    M4: Added room 407 daemon

--- engines/m4/burger/rooms/section4/room407.cpp
+++ engines/m4/burger/rooms/section4/room407.cpp
@@ -186,7 +186,7 @@ void Room407::init() {
 		kernel_trigger_dispatch_now(8);
 
 		if (_G(flags)[V171] == 4003)
-			_vp02.series_show("407vp02", 0x300);
+			_vp02.show("407vp02", 0x300);
 	}
 
 	switch (_G(game).previous_room) {
@@ -210,7 +210,377 @@ void Room407::init() {
 }
 
 void Room407::daemon() {
-	// TODO
+	switch (_G(kernel).trigger) {
+	case 1:
+		poof(2);
+		break;
+
+	case 2:
+		disable_player_commands_and_fade_init(_val1);
+		break;
+
+	case 3:
+		_G(walker).wilbur_poof();
+
+		if (!_G(flags)[V181] && _G(flags)[V171] == 4003) {
+			ws_unhide_walker();
+		} else {
+			enable_player();
+		}
+		break;
+
+	case 4:
+		switch (_state1) {
+		case 3:
+			wilbur_speech("400w001");
+			break;
+		case 4:
+			wilbur_speech("407w004");
+			break;
+		case 5:
+			wilbur_speech("407w005");
+			break;
+		case 6:
+			wilbur_speech("407w006");
+			break;
+		case 7:
+			wilbur_speech("407w008");
+			break;
+		case 8:
+			wilbur_speech("407w011");
+			break;
+		case 9:
+			wilbur_speech("407w012");
+			break;
+		default:
+			break;
+		}
+		break;
+
+	case 5:
+		_val2 = 17;
+
+		if (_val4) {
+			kernel_timing_trigger(120, 7);
+		} else {
+			_digiName = Common::String::format("407r902%c", 'a' + imath_ranged_rand(1, 25));
+
+			_flag1 = true;
+			digi_preload(_digiName);
+			digi_play(_digiName.c_str(), 2, 180, 6);
+		}
+		break;
+
+	case 6:
+		digi_unload(_digiName);
+		kernel_trigger_dispatch_now(7);
+		break;
+
+	case 7:
+		switch (_val2) {
+		case 10:
+			if (!digi_play_state(2))
+				digi_play_loop("407_001", 2);
+
+			_val2 = getRandomState();
+			series_play_with_breaks(PLAY1, "407dz01", 0xa01, 7, 3);
+			break;
+
+		case 11:
+			_val2 = 14;
+			series_play_with_breaks(PLAY2, "407dz02", 0xa01, 7, 3);
+			break;
+
+		case 12:
+			_val2 = 14;
+			series_play_with_breaks(PLAY3, "407dz03", 0xa01, 7, 3);
+			break;
+
+		case 13:
+			digi_stop(2);
+			_val2 = 14;
+			series_play_with_breaks(PLAY4, "407dz04", 0xa01, 7, 3);
+			break;
+
+		case 14:
+			_val2 = getRandomState();
+			kernel_trigger_dispatch_now(7);
+			kernel_trigger_dispatch_now(10001);
+			break;
+
+		case 15:
+			digi_stop(2);
+			_val2 = getRandomState();
+			series_play_with_breaks(PLAY5, "407dz05", 0xa01, 7, 3);
+			break;
+
+		case 16:
+			digi_stop(2);
+			_dzS1 = series_load("407dz06");
+			_dzS2 = series_load("407dz06s");
+			_dz.show("407dz06", 0xa01, 18);
+			kernel_trigger_dispatch_now(5);
+			break;
+
+		case 17:
+			_flag1 = false;
+			freeDz();
+			_val2 = getRandomState();
+			series_play_with_breaks(PLAY6, "407dz06", 0xa01, 7, 3);
+			break;
+
+		case 18:
+			_flag1 = false;
+			freeDz();
+			_val2 = 19;
+			series_play_with_breaks(PLAY6, "407dz06", 0xa01, 7, 3);
+			break;
+
+		case 19:
+			if (!digi_play_state(2))
+				digi_play_loop("407_001", 2);
+
+			conv88();
+			kernel_trigger_dispatch_now(7);
+			break;
+
+		case 20:
+			digi_stop(2);
+			_dz.play("407dz02", 0xa01, 16, -1, 6, 0, 100, 0, 0, 0, 2);
+			break;
+
+		case 21:
+			_dz.terminate();
+			hotspot_set_active("DRUMZ", false);
+			intr_remove_no_walk_rect(_walk1);
+			break;
+
+		default:
+			break;
+		}
+		break;
+
+	case 8:
+		switch (_val3) {
+		case 22:
+			_rx = series_show("407rx02", 0x901);
+			break;
+
+		case 23:
+			terminateMachineAndNull(_rx);
+
+			switch (_val4) {
+			case 1:
+				_val3 = 26;
+				break;
+			case 2:
+				_val3 = 24;
+				break;
+			case 3:
+				_val3 = 27;
+				break;
+			default:
+				break;
+			}
+			break;
+
+		case 24:
+			_rx = series_show("407rx02", 0x901, 0, -1, -1, 4);
+			kernel_trigger_dispatch_now(10);
+			break;
+
+		case 25:
+			terminateMachineAndNull(_rx);
+			_rx = series_show("407rx02", 0x901, 0, -1, -1, 4);
+			conv_resume_curr();
+			break;
+
+		case 26:
+			terminateMachineAndNull(_rx);
+			_val3 = 30;
+			series_play_with_breaks(PLAY9, "407rx04", 0x901, 8, 2);
+			break;
+
+		case 27:
+			_val3 = 28;
+			series_play_with_breaks(PLAY8, "407rx04", 0x901, 8, 2);
+			break;
+
+		case 28:
+			_val3 = 29;
+			series_play_with_breaks(PLAY10, "407rx02", 0x901, 8, 2);
+			break;
+
+		case 29:
+			_val3 = 22;
+			kernel_trigger_dispatch_now(8);
+			kernel_trigger_dispatch_now(1);
+			break;
+
+		case 30:
+			_val3 = 31;
+			series_play_with_breaks(PLAY11, "407rx06", 0x901, 8, 2);
+			break;
+
+		case 31:
+			_val5 = 36;
+			kernel_trigger_dispatch_now(9);
+			_val3 = 32;
+			series_play_with_breaks(PLAY12, "407rx08", 0x901, 8, 2);
+			break;
+
+		case 32:
+			_val3 = 33;
+			series_play_with_breaks(PLAY13, "407rx08", 0x901, 8, 2);
+			break;
+
+		case 33:
+			_val3 = 34;
+			series_play_with_breaks(PLAY14, "407rx09", 0x901, 8, 2);
+			break;
+
+		case 34:
+			kernel_trigger_dispatch_now(14);
+			terminateMachineAndNull(_rx);
+			_rx = series_show("407rx09", 0x901);
+			break;
+
+		default:
+			break;
+		}
+		break;
+
+	case 9:
+		switch (_val5) {
+		case 35:
+			_vp02.show("407vp02", 0x300);
+			_val4 = 1;
+			_val3 = 23;
+			kernel_trigger_dispatch_now(8);
+			break;
+
+		case 36:
+			_vp02.terminate();
+			_val5 = 37;
+			series_play_with_breaks(PLAY16, "407vp02", 0x300, 9, 3);
+			break;
+
+		case 37:
+			_val5 = 39;
+			series_play_with_breaks(PLAY17, "407vp03", 0x300, 9, 3);
+			break;
+
+		case 38:
+			_vp02.terminate();
+			_val2 = 20;
+			_val5 = 35;
+			series_play_with_breaks(PLAY15, "407vp02", 0x300, 9, 3);
+			player_set_facing_at(412, 336);
+			break;
+
+		case 39:
+			_vp02.show("407vp05", 0x300);
+			break;
+
+		case 40:
+			terminateMachineAndNull(_rx);
+			_vp02.terminate();
+			hotspot_set_active("ROXY", false);
+			break;
+
+		default:
+			break;
+		}
+		break;
+
+	case 10:
+		_G(flags)[V298] = 1;
+		_G(flags)[V299] = 1;
+		conv_load_and_prepare("conv87", 12);
+		conv_export_pointer_curr(&_G(flags)[V180], 0);
+		conv_play_curr();
+		break;
+
+	case 11:
+		_G(flags)[V298] = 1;
+		_G(flags)[V299] = 1;
+		player_set_commands_allowed(false);
+		conv_load_and_prepare("conv88", 13);
+		conv_play_curr();
+		break;
+
+	case 12:
+		_G(flags)[V298] = 0;
+		_G(flags)[V299] = 0;
+		_G(flags)[V180] = 1;
+		terminateMachineAndNull(_rx);
+		_val3 = 28;
+		kernel_trigger_dispatch_now(8);
+		break;
+
+	case 13:
+		_G(flags)[V298] = 0;
+		_G(flags)[V299] = 0;
+		player_set_commands_allowed(true);
+		break;
+
+	case 14:
+		_G(flags)[V298] = 0;
+		_val2 = 21;
+		_val5 = 40;
+		series_play_with_breaks(PLAY18, "407poof", 0x2ff, 4008, 2);
+		_G(flags)[V181] = 1;
+		break;
+
+	case gCHANGE_WILBUR_ANIMATION:
+		switch (_G(wilbur_should)) {
+		case 1:
+			player_set_commands_allowed(false);
+
+			if (_flag1) {
+				digi_stop(2);
+				_val2 = 17;
+				kernel_trigger_dispatch_now(7);
+			}
+
+			_val4 = 3;
+			_val3 = 20;
+			kernel_trigger_dispatch_now(8);
+			break;
+
+		case 2:
+			player_set_commands_allowed(false);
+
+			if (_flag1) {
+				digi_stop(2);
+				_val2 = 17;
+				kernel_trigger_dispatch_now(7);
+			}
+
+			_val4 = 2;
+			_val3 = 23;
+			kernel_trigger_dispatch_now(8);
+			break;
+
+		default:
+			_G(kernel).continue_handling_trigger = true;
+			break;
+		}
+		break;
+
+	case gSET_COMMANDS_ALLOWED:
+		if (!_G(flags)[V181] && _G(flags)[V171] == 4003) {
+			_val5 = 38;
+			kernel_timing_trigger(60, 9);
+		} else {
+			player_set_commands_allowed(true);
+		}
+		break;
+
+	default:
+		_G(kernel).continue_handling_trigger = true;
+		break;
+	}
 }
 
 void Room407::pre_parser() {
