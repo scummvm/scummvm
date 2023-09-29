51a6e0f38d6b758615e141f6a7df452765d5017d
shkupfer <shkupfer@ncsu.edu>
2023-09-28 20:37:29 -0400

    SCUMM HE: Unindent switch case statements

--- engines/scumm/script.cpp
+++ engines/scumm/script.cpp
@@ -636,25 +636,27 @@ int ScummEngine::readVar(uint var) {
 		else
 			assertRange(0, var, 20, "local variable (reading)");
 #if defined(USE_ENET) && defined(USE_LIBCURL)
+		// Mod for Backyard Baseball 2001 online competitive play: change impact of
+		// batter's power stat on hit power
 		if (ConfMan.getBool("enable_competitive_mods")) {
 			if (_game.id == GID_BASEBALL2001 &&
 				_currentRoom == 4 && vm.slot[_currentScript].number == 2090  // The script that calculates hit power
 				&& readVar(399) == 1  // Check that we're playing online
-				&& var == 2  // Local var for batter's hiting power stat
+				&& var == 2  // Local var for batter's hitting power stat
 			) {
 				int swingType = vm.localvar[_currentScript][0];
 				int powerStat, powerStatModified;
 				switch (swingType) {
-					case 2:  // Line drive or grounder swing
-						powerStat = vm.localvar[_currentScript][var];
-						powerStatModified = 20 + vm.localvar[_currentScript][var] * 4 / 5;
-						return powerStatModified;
-					case 1:  // Power swing
-						powerStat = vm.localvar[_currentScript][var];
-						powerStatModified = 20 + vm.localvar[_currentScript][var] * 7 / 10;;
-						return powerStatModified;
-					default:
-						break;
+				case 2:  // Line drive or grounder swing
+					powerStat = vm.localvar[_currentScript][var];
+					powerStatModified = 20 + vm.localvar[_currentScript][var] * 4 / 5;
+					return powerStatModified;
+				case 1:  // Power swing
+					powerStat = vm.localvar[_currentScript][var];
+					powerStatModified = 20 + vm.localvar[_currentScript][var] * 7 / 10;;
+					return powerStatModified;
+				default:
+					break;
 				}
 			}
 		}
--- engines/scumm/script_v6.cpp
+++ engines/scumm/script_v6.cpp
@@ -931,16 +931,16 @@ void ScummEngine_v6::o6_startScriptQuick2() {
 		int hitQuality = -2;
 		if (stance == 2) {  // Batter is in a squared stance
 			switch (zone) {
-				case 25:
-					hitQuality = 3;
-				case 18: case 24: case 26: case 32:
-					hitQuality = 2;
-				case 10: case 11: case 12: case 17: case 19: case 23: case 27: case 31: case 33: case 38: case 39: case 40:
-					hitQuality = 1;
-				case 4: case 16: case 20: case 30: case 34: case 46:
-					hitQuality = 0;
-				case 3: case 5: case 9: case 13: case 15: case 21: case 22: case 28: case 29: case 35: case 37: case 41: case 45: case 47:
-					hitQuality = -1;
+			case 25:
+				hitQuality = 3;
+			case 18: case 24: case 26: case 32:
+				hitQuality = 2;
+			case 10: case 11: case 12: case 17: case 19: case 23: case 27: case 31: case 33: case 38: case 39: case 40:
+				hitQuality = 1;
+			case 4: case 16: case 20: case 30: case 34: case 46:
+				hitQuality = 0;
+			case 3: case 5: case 9: case 13: case 15: case 21: case 22: case 28: case 29: case 35: case 37: case 41: case 45: case 47:
+				hitQuality = -1;
 			}
 			push(hitQuality);
 			return;
@@ -952,16 +952,16 @@ void ScummEngine_v6::o6_startScriptQuick2() {
 			zone  = ((zone - 1) / 7) * 7 + (6 - ((zone - 1) % 7)) + 1;  // "Flip" zone horizontally across center
 		}
 		switch (zone) {
-			case 24:
-				hitQuality = 3;
-			case 17: case 23: case 25: case 31:
-				hitQuality = 2;
-			case 9: case 10: case 16: case 18: case 22: case 26: case 30: case 32: case 37: case 38:
-				hitQuality = 1;
-			case 3: case 11: case 15: case 19: case 29: case 33: case 39: case 45:
-				hitQuality = 0;
-			case 2: case 4: case 8: case 12: case 20: case 27: case 34: case 36: case 40: case 44: case 46:
-				hitQuality = -1;
+		case 24:
+			hitQuality = 3;
+		case 17: case 23: case 25: case 31:
+			hitQuality = 2;
+		case 9: case 10: case 16: case 18: case 22: case 26: case 30: case 32: case 37: case 38:
+			hitQuality = 1;
+		case 3: case 11: case 15: case 19: case 29: case 33: case 39: case 45:
+			hitQuality = 0;
+		case 2: case 4: case 8: case 12: case 20: case 27: case 34: case 36: case 40: case 44: case 46:
+			hitQuality = -1;
 		}
 		push(hitQuality);
 		return;
