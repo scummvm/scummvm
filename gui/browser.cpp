8f9cdff4a76dcb1b4aa7a9ce5cc398be4e635b4f
Thierry Crozat <criezy@scummvm.org>
2023-10-03 23:41:14 +0100

    GUI: Add function in GuiManager to query if low-res GUI should be used
    The same condition was scattered around in the code to switch between
    normal labels and images and their lowres counterparts. Factorizing
    that condition in a function will allow changing it easily.

--- gui/browser.cpp
+++ gui/browser.cpp
@@ -74,7 +74,7 @@ BrowserDialog::BrowserDialog(const Common::U32String &title, bool dirBrowser)
 	_showHiddenWidget = new CheckboxWidget(this, "Browser.Hidden", _("Show hidden files"), _("Show files marked with the hidden attribute"), kHiddenCmd);
 
 	// Buttons
-	if (g_gui.getGUIWidth() > 320)
+	if (!g_gui.useLowResGUI())
 		new ButtonWidget(this, "Browser.Up", _("Go up"), _("Go to previous directory level"), kGoUpCmd);
 	else
 		new ButtonWidget(this, "Browser.Up", _c("Go up", "lowres"), _("Go to previous directory level"), kGoUpCmd);
