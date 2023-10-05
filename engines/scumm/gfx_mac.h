62af89334841a465e5db8a8137c7a04e0992d97d
Torbjörn Andersson <eriktorbjorn@users.sourceforge.net>
2023-10-05 07:43:24 +0200

    SCUMM: Major (incomplete) refactoring of Indy 3 Mac GUI
    Widgets are now proper objects. I'm back to the point where verbs work
    again, but not the inventory. Still, this is too scary not to commit now
    that it's beginning to work again.

--- engines/scumm/gfx_mac.h
+++ engines/scumm/gfx_mac.h
@@ -38,11 +38,22 @@ public:
 	MacIndy3Gui(OSystem *system, ScummEngine *vm);
 	~MacIndy3Gui();
 
-	bool isActive();
+	// There is a distinction between the GUI being active and being
+	// visible. Active means that it's allowed to draw verbs. Visible
+	// means that it's actually drawn verbs. From the outside, only the
+	// visibility is relevant.
+	//
+	// One case where this makes a difference is when boxing with the
+	// coach. During the "10 minutes later" sign, the GUI is active but
+	// it's not drawing verbs, so the SCUMM engine is allowed to draw in
+	// the verb area to clear the power meters and text.
+
+	bool isVisible() { return _visible; }
+
+	const Graphics::Font *getFont(int n) { return _fonts[n]; }
+
 	void resetAfterLoad();
-
 	void update();
-
 	void handleEvent(Common::Event &event);
 
 private:
@@ -53,61 +64,179 @@ private:
 
 	bool _visible = false;
 
-	struct Widget {
-		Common::Rect bounds;
-		int verbid = 0;
-		int verbslot = -1;
-		byte *text = nullptr;
-		int timer = 0;
-		bool visible = false;
-		bool enabled = false;
-		bool redraw = false;
-		bool kill = false;
+	class Widget {
+	public:
+		static ScummEngine *_vm;
+		static MacIndy3Gui *_gui;
+		static Graphics::Surface *_surface;
+
+		Common::Rect _bounds;
+		int _timer = 0;
+		bool _enabled = false;
+		bool _redraw = false;
+
+		Widget(int x, int y, int width, int height);
+		virtual ~Widget() {}
+
+		void reset();
+
+		virtual void draw() = 0;
+		virtual void undraw() {}
+
+		virtual bool updateTimer();
+
+		// Primitives
+		void copyRectToScreen(Common::Rect r);
+		void fill(Common::Rect r);
+		void drawBitmap(Common::Rect r, const uint16 *bitmap, byte color);
+		void drawShadowBox(Common::Rect r);
+		void drawShadowFrame(Common::Rect r, byte shadowColor, byte fillColor);
 	};
 
-	Widget _widgets[29];
+	class GuiWidget : public Widget {
+	public:
+		int _verbid = 0;
+		int _verbslot = -1;
+		bool _visible = false;
+		bool _kill = false;
 
-	struct InventorySlot {
-		Common::Rect bounds;
-		byte *name = nullptr;
-		int slot = -1;
-		int obj = -1;
-		int timer = 0;
-		bool redraw = false;
+		GuiWidget(int verbid, int x, int y, int width, int height);
+
+		virtual const char *name() = 0;
+
+		void reset();
+
+		virtual bool handleEvent(Common::Event &event) = 0;
+		virtual void updateVerb(int verbslot);
+
+		void draw();
+		void undraw();
 	};
 
-	InventorySlot _inventorySlots[6];
-	int _inventoryOffset = 0;
+	class Button : public GuiWidget {
+	public:
+		byte *_text = nullptr;
+
+		Button(int verbid, int x, int y, int width, int height);
+		~Button();
+
+		const char *name() { return _text ? (const char *)_text : "(null)"; }
+		bool handleEvent(Common::Event &event);
+
+		void reset();
+		bool updateTimer();
+		void updateVerb(int verbslot);
+		void draw();
+	};
+
+	enum ScrollDirection {
+		kScrollUp,
+		kScrollDown
+	};
+
+	class InventoryScrollButton : public Widget {
+	private:
+		ScrollDirection _direction;
+
+	public:
+		InventoryScrollButton(int x, int y, int width, int height, ScrollDirection direction);
+
+		void draw();
+	};
+
+	class InventorySlot : public Widget {
+	private:
+		byte *_name = nullptr;
+		int _slot = -1;
+		int _obj = -1;
+
+	public:
+		InventorySlot(int slot, int x, int y, int width, int height);
+		void reset();
+		bool updateTimer();
+		void draw();
+	};
+
+	class InventoryWidget : public GuiWidget {
+	private:
+		InventorySlot *_slots[6];
+		InventoryScrollButton *_scrollButtons[2];
+
+	public:
+		InventoryWidget(int verbid, int x, int y, int width, int height);
+
+		const char *name() { return "Inventory"; }
+
+		bool handleEvent(Common::Event &event);
+
+		void draw();
+	};
+
+	GuiWidget *_widgets[29];
+
+	const uint16 _ulCorner[4] = { 0xF000, 0xC000, 0x8000, 0x8000 };
+	const uint16 _urCorner[4] = { 0xF000, 0x3000, 0x1000, 0x1000 };
+	const uint16 _llCorner[4] = { 0x8000, 0x8000, 0xC000, 0xF000 };
+	const uint16 _lrCorner[5] = { 0x1000, 0x1000, 0x3000, 0xF000 };
+
+	const uint16 _upArrowOutline[16] = {
+		0x0000, 0x0000, 0x0000, 0x0080,	0x0140, 0x0220, 0x0410, 0x0808,
+		0x1C1C, 0x0410, 0x0410, 0x0410, 0x07F0, 0x0000, 0x0000, 0x0000
+	};
+
+	uint16 _upArrow[16] = {
+		0x0000, 0x0000, 0x0000, 0x0000, 0x0080, 0x01C0, 0x03E0, 0x07F0,
+		0x03E0, 0x03E0, 0x03E0, 0x03E0, 0x0000, 0x0000, 0x0000, 0x0000
+	};
+
+	uint16 _downArrowOutline[16] = {
+		0x0000, 0x0000, 0x0000, 0x0000, 0x07F0, 0x0410, 0x0410, 0x0410,
+		0x1C1C, 0x0808, 0x0410, 0x0220, 0x0140, 0x0080, 0x0000, 0x0000
+	};
+
+	uint16 _downArrow[16] = {
+		0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x03E0, 0x03E0, 0x03E0,
+		0x03E0, 0x07F0,	0x03E0, 0x01C0, 0x0080, 0x0000, 0x0000, 0x0000
+	};
+
+	bool isActive();
+
+#if 0
+	void initWidget(Widget *w, int x, int y, int width, int height);
+	void resetBaseWidget(Widget *w);
 
 	void initWidget(int n, int verbid, int x, int y, int width, int height);
 	void resetWidget(Widget *w);
 	void initInventorySlot(int n, int x, int y, int width, int height);
 	void resetInventorySlot(InventorySlot *s);
+	void initInventoryScrollButton(int n, int x, int y, int width, int height, ScrollDirection direction);
+	void resetInventoryScrollButton(InventoryScrollButton *b);
+#endif
 
 	void clear();
 	void show();
 	void hide();
 
-	void updateTimers();
-	void updateButtons(bool &keepGuiAlive, bool &inventoryIsActive);
+	void updateWidgets(bool &keepGuiAlive, bool &hasInventory);
+#if 0
 	void updateInventorySlots();
 
 	void drawButtons();
 	void drawInventorySlots();
+	void fillInventoryArrows();
+#endif
 
-	void drawWidget(Widget *w);
-	void undrawWidget(Widget *w);
+#if 0
 	void drawButton(Widget *w);
 	void drawInventoryWidget(Widget *w);
-	void drawInventoryArrow(int arrowX, int arrowY, bool highlighted, bool flipped);
 	void drawInventorySlot(InventorySlot *slot);
-
-	// Primitives
-	void fill(Common::Rect r);
-	void drawShadowBox(Common::Rect r);
-	void drawShadowFrame(Common::Rect r, byte shadowColor, byte fillColor);
+	void drawInventoryScrollButton(InventoryScrollButton *b);
+	void fillInventoryArrow(InventoryScrollButton *b);
+#endif
 
 	void copyRectToScreen(Common::Rect r);
+	void fill(Common::Rect r);
+	void drawBitmap(Common::Rect r, const uint16 *bitmap, byte color);
 };
 
 } // End of namespace Scumm
