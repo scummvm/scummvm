96c4fe3648d754a5e631ed89b179ddd245c1e1f9
AndywinXp <andywinxp@gmail.com>
2023-10-05 09:23:23 +0200

    SWORD1: Remove hack from fnEnterSection()
    This originally fixed a crash at startup, but we seem
    to handle everything correctly now, so this now works
    as it should. Tested on DOS, Mac and PSX versions.

--- engines/sword1/logic.cpp
+++ engines/sword1/logic.cpp
@@ -1407,10 +1407,7 @@ int Logic::fnEnterSection(Object *cpt, int32 id, int32 screen, int32 d, int32 e,
 	if (screen >= TOTAL_SECTIONS)
 		error("mega %d tried entering section %d", id, screen);
 
-	/* if (cpt->o_type == TYPE_PLAYER)
-	   ^= this was the original condition from the game sourcecode.
-	   not sure why it doesn't work*/
-	if (id == PLAYER)
+	if (cpt->o_type == TYPE_PLAYER)
 		_scriptVars[NEW_SCREEN] = screen;
 	else
 		cpt->o_screen = screen; // move the mega
