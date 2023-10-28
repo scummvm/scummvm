scummvm/scummvm

commit 595a3d77241f51a6218f51d5570edbf72d548d40

GUI: Use Keymapper for EditableWidget

gui/gui-manager.cpp
@@ -191,6 +191,96 @@ Common::Keymap *GuiManager::getKeymap() const {
 	act->allowKbdRepeats();
 	guiMap->addAction(act);
 
+	act = new Action("ENTER", _("New Line"));
+	act->setCustomEngineActionEvent(kActionEnter);
+	act->addDefaultInputMapping("RETURN");
+	act->addDefaultInputMapping("KP_ENTER");
+	act->allowKbdRepeats();
+	guiMap->addAction(act);
+
+	act = new Action("ESC", _("Close Dialog"));
+	act->setCustomEngineActionEvent(kActionEscape);
+	act->addDefaultInputMapping("ESCAPE");
+	guiMap->addAction(act);
+
+	act = new Action("BACKSPACE", _("Backspace"));
+	act->setCustomEngineActionEvent(kActionBackspace);
+	act->addDefaultInputMapping("BACKSPACE");
+	act->allowKbdRepeats();
+	guiMap->addAction(act);
+
+	act = new Action("DEL", _("Delete Character"));
+	act->setCustomEngineActionEvent(kActionDelete);
+	act->addDefaultInputMapping("DELETE");
+	act->allowKbdRepeats();
+	guiMap->addAction(act);
+
+	act = new Action("END", _("Go to end of line"));
+	act->setCustomEngineActionEvent(kActionEnd);
+
+#ifdef MACOSX
+	act->addDefaultInputMapping("C+e");
+#endif
+
+	act->addDefaultInputMapping("DOWN");
+	act->addDefaultInputMapping("END");
+	guiMap->addAction(act);
+
+	act = new Action("CHAR_LEFT", _("Move cursor left"));
+	act->setCustomEngineActionEvent(kActionLeft);
+	act->addDefaultInputMapping("LEFT");
+	act->allowKbdRepeats();
+	guiMap->addAction(act);
+
+	act = new Action("CHAR_RIGHT", _("Move cursor right"));
+	act->setCustomEngineActionEvent(kActionRight);
+	act->addDefaultInputMapping("RIGHT");
+	act->allowKbdRepeats();
+	guiMap->addAction(act);
+
+	act = new Action("HOME", _("Go to start of line"));
+	act->setCustomEngineActionEvent(kActionHome);
+
+#ifdef MACOSX
+	act->addDefaultInputMapping("C+a");
+#endif
+
+	act->addDefaultInputMapping("UP");
+	act->addDefaultInputMapping("HOME");
+	guiMap->addAction(act);
+
+#ifdef MACOSX
+	act = new Action(kStandardActionCut, _("Cut"));
+	act->setCustomEngineActionEvent(kActionCut);
+	act->addDefaultInputMapping("M+x");
+	guiMap->addAction(act);
+
+	act = new Action(kStandardActionPaste, _("Paste"));
+	act->setCustomEngineActionEvent(kActionPaste);
+	act->addDefaultInputMapping("M+v");
+	guiMap->addAction(act);
+
+	act = new Action(kStandardActionCopy, _("Copy"));
+	act->setCustomEngineActionEvent(kActionCopy);
+	act->addDefaultInputMapping("M+c");
+	guiMap->addAction(act);
+#else
+	act = new Action(kStandardActionCut, _("Cut"));
+	act->setCustomEngineActionEvent(kActionCut);
+	act->addDefaultInputMapping("C+x");
+	guiMap->addAction(act);
+
+	act = new Action(kStandardActionPaste, _("Paste"));
+	act->setCustomEngineActionEvent(kActionPaste);
+	act->addDefaultInputMapping("C+v");
+	guiMap->addAction(act);
+
+	act = new Action(kStandardActionCopy, _("Copy"));
+	act->setCustomEngineActionEvent(kActionCopy);
+	act->addDefaultInputMapping("C+c");
+	guiMap->addAction(act);
+#endif
+
 	act = new Action(kStandardActionEE, _("???"));
 	act->setKeyEvent(KEYCODE_v);
 	guiMap->addAction(act);
@@ -758,7 +848,6 @@ void GuiManager::processEvent(const Common::Event &event, Dialog *const activeDi
 	if (g_gui.useRTL()) {
 		mouse.x = g_system->getOverlayWidth() - event.mouse.x - activeDialog->_x + g_gui.getOverlayOffset();
 	}
-
 	switch (event.type) {
 	case Common::EVENT_KEYDOWN:
 		activeDialog->handleKeyDown(event.kbd);

gui/widgets/editable.cpp
@@ -209,7 +209,6 @@ bool EditableWidget::handleKeyDown(Common::KeyState state) {
 	bool handled = true;
 	bool dirty = false;
 	bool forcecaret = false;
-	int deleteIndex;
 
 	if (!isEnabled())
 		return false;
@@ -242,201 +241,7 @@ bool EditableWidget::handleKeyDown(Common::KeyState state) {
 		state.keycode = remap[state.keycode - Common::KEYCODE_KP0];
 	}
 
-	switch (state.keycode) {
-	case Common::KEYCODE_RETURN:
-	case Common::KEYCODE_KP_ENTER:
-		// confirm edit and exit editmode
-		endEditMode();
-		dirty = true;
-		break;
-
-	case Common::KEYCODE_ESCAPE:
-		abortEditMode();
-		dirty = true;
-		break;
-
-	case Common::KEYCODE_BACKSPACE:
-		deleteIndex = caretLogicalPos();
-		if (deleteIndex > 0 && _selOffset == 0) {
-			deleteIndex--;
-			_editString.deleteChar(deleteIndex);
-			setCaretPos(caretVisualPos(deleteIndex));
-			_selCaretPos = -1;
-			dirty = true;
-
-			sendCommand(_cmd, 0);
-		} else if (deleteIndex >= 0 && _selOffset != 0) {
-			int selBegin = _selCaretPos;
-			int selEnd = _selCaretPos + _selOffset;
-			if (selBegin > selEnd)
-				SWAP(selBegin, selEnd);
-			_editString.erase(selBegin, selEnd - selBegin);
-			setCaretPos(caretVisualPos(selBegin));
-			_selCaretPos = -1;
-			_selOffset = 0;
-			dirty = true;
-
-			sendCommand(_cmd, 0);
-		}
-		forcecaret = true;
-		break;
-
-	case Common::KEYCODE_DELETE:
-		deleteIndex = caretLogicalPos();
-		if (deleteIndex < (int)_editString.size()) {
-			_editString.deleteChar(deleteIndex);
-			setCaretPos(caretVisualPos(deleteIndex));
-			_selCaretPos = -1;
-			_selOffset = 0;
-			dirty = true;
-
-			sendCommand(_cmd, 0);
-		}
-		forcecaret = true;
-		break;
-
-	case Common::KEYCODE_DOWN:
-	case Common::KEYCODE_END:
-		// Move caret to end
-		setCaretPos(caretVisualPos(_editString.size()));
-		if (state.hasFlags(Common::KBD_SHIFT))
-			setSelectionOffset(_editString.size() - _selCaretPos);
-		else
-			clearSelection();
-		forcecaret = true;
-		dirty = true;
-		break;
-
-	case Common::KEYCODE_LEFT:
-		if (state.hasFlags(Common::KBD_SHIFT)) {
-			if (_disableSelection)
-				break;
-			if (_selCaretPos < 0)
-				_selCaretPos = _caretPos;
-			if (_caretPos > 0)
-				_selOffset--;
-		} else {
-			clearSelection();
-		}
-		// Move caret one left (if possible)
-		if (_caretPos > 0) {
-			dirty = setCaretPos(_caretPos - 1);
-		}
-		forcecaret = true;
-		dirty = true;
-		break;
-
-	case Common::KEYCODE_RIGHT:
-		if (state.hasFlags(Common::KBD_SHIFT)) {
-			if (_disableSelection)
-				break;
-			if (_selCaretPos < 0)
-				_selCaretPos = _caretPos;
-			if (_selOffset + _selCaretPos < (int)_editString.size())
-				_selOffset++;
-		} else {
-			clearSelection();
-		}
-		// Move caret one right (if possible)
-		if (_caretPos < (int)_editString.size()) {
-			dirty = setCaretPos(_caretPos + 1);
-		}
-		forcecaret = true;
-		dirty = true;
-		break;
-
-	case Common::KEYCODE_UP:
-	case Common::KEYCODE_HOME:
-		// Move caret to start
-		setCaretPos(caretVisualPos(0));
-		if (state.hasFlags(Common::KBD_SHIFT))
-			setSelectionOffset(0 - _selCaretPos);
-		else
-			clearSelection();
-		forcecaret = true;
-		dirty = true;
-		break;
-
-	case Common::KEYCODE_v:
-		if (state.flags & Common::KBD_CTRL) {
-			if (g_system->hasTextInClipboard()) {
-				Common::U32String text = g_system->getTextFromClipboard();
-				if (_selOffset != 0) {
-					int selBegin = _selCaretPos;
-					int selEnd = _selCaretPos + _selOffset;
-					if (selBegin > selEnd)
-						SWAP(selBegin, selEnd);
-					_editString.replace(selBegin, selEnd - selBegin, text);
-					setCaretPos(caretVisualPos(selBegin));
-					const int logicalPosition = caretLogicalPos();
-					setCaretPos(caretVisualPos(logicalPosition + text.size()));
-					clearSelection();
-				} else {
-					for (uint32 i = 0; i < text.size(); ++i) {
-					const int logicalPosition = caretLogicalPos();
-					if (tryInsertChar(text[i], logicalPosition))
-						setCaretPos(caretVisualPos(logicalPosition + 1));
-					}
-				}
-				dirty = true;
-			}
-		} else {
-			defaultKeyDownHandler(state, dirty, forcecaret, handled);
-		}
-		break;
-
-	case Common::KEYCODE_c:
-		if (state.flags & Common::KBD_CTRL) {
-			if (!getEditString().empty()) {
-				int selBegin = _selCaretPos;
-				int selEnd = _selCaretPos + _selOffset;
-				if (selBegin > selEnd)
-					SWAP(selBegin, selEnd);
-				const Common::U32String selected(getEditString().begin() + selBegin, getEditString().begin() + selEnd);
-				g_system->setTextInClipboard(selected);
-			}
-		} else {
-			defaultKeyDownHandler(state, dirty, forcecaret, handled);
-		}
-		break;
-
-#ifdef MACOSX
-	// Let ctrl-a / ctrl-e move the caret to the start / end of the line.
-	//
-	// These shortcuts go back a long time for command line programs. As
-	// for edit fields in GUIs, they are supported natively on macOS,
-	// which is why I enabled these shortcuts there.
-	// On other systems (Windows, Gnome), Ctrl-A by default means
-	// "select all", which is why I didn't enable the shortcuts there
-	// for now, to avoid potential confusion.
-	//
-	// But since we don't support text selection, and since at least Gnome
-	// can be configured to also support ctrl-a and ctrl-e, we may want
-	// to extend this code to other targets, maybe even all. I'll leave
-	// this to other porters to decide, though.
-	case Common::KEYCODE_a:
-	case Common::KEYCODE_e:
-		if (state.flags & Common::KBD_CTRL) {
-			if (state.keycode == Common::KEYCODE_a) {
-				// Move caret to start
-				dirty = setCaretPos(0);
-				forcecaret = true;
-			} else if (state.keycode == Common::KEYCODE_e) {
-				// Move caret to end
-				dirty = setCaretPos(_editString.size());
-				forcecaret = true;
-			}
-			clearSelection();
-			break;
-		} else {
-			defaultKeyDownHandler(state, dirty, forcecaret, handled);
-		}
-		break;
-#endif
-
-	default:
-		defaultKeyDownHandler(state, dirty, forcecaret, handled);
-	}
+	defaultKeyDownHandler(state, dirty, forcecaret, handled);
 
 	if (dirty)
 		markAsDirty();
@@ -456,7 +261,7 @@ void EditableWidget::defaultKeyDownHandler(Common::KeyState &state, bool &dirty,
 			if (selBegin > selEnd)
 				SWAP(selBegin, selEnd);
 			_editString.replace(selBegin, selEnd - selBegin, Common::U32String(state.ascii));
-			if(_editString.size() > 0)
+			if (_editString.size() > 0)
 				selBegin++;
 			setCaretPos(caretVisualPos(selBegin));
 			_selCaretPos = -1;
@@ -473,6 +278,215 @@ void EditableWidget::defaultKeyDownHandler(Common::KeyState &state, bool &dirty,
 	} else {
 		handled = false;
 	}
+}
+
+void EditableWidget::handleOtherEvent(const Common::Event &evt) {
+	bool dirty = false;
+	bool forcecaret = false;
+	int deleteIndex;
+
+	if (!isEnabled())
+		return;
+
+	// First remove caret
+	if (_caretVisible)
+		drawCaret(true);
+
+	switch (evt.type) {
+	case Common::EVENT_CUSTOM_ENGINE_ACTION_START:
+		switch (evt.customType) {
+		case kActionEnter:
+			endEditMode();
+			dirty = true;
+			break;
+
+		case kActionEscape:
+			abortEditMode();
+			dirty = true;
+			break;
+
+		case kActionBackspace:
+			deleteIndex = caretLogicalPos();
+			if (deleteIndex > 0 && _selOffset == 0) {
+				deleteIndex--;
+				_editString.deleteChar(deleteIndex);
+				setCaretPos(caretVisualPos(deleteIndex));
+				_selCaretPos = -1;
+				dirty = true;
+
+				sendCommand(_cmd, 0);
+			} else if (deleteIndex >= 0 && _selOffset != 0) {
+				int selBegin = _selCaretPos;
+				int selEnd = _selCaretPos + _selOffset;
+				if (selBegin > selEnd)
+					SWAP(selBegin, selEnd);
+				_editString.erase(selBegin, selEnd - selBegin);
+				setCaretPos(caretVisualPos(selBegin));
+				_selCaretPos = -1;
+				_selOffset = 0;
+				dirty = true;
+
+				sendCommand(_cmd, 0);
+			}
+			forcecaret = true;
+			break;
+
+		case kActionDelete:
+			deleteIndex = caretLogicalPos();
+			if (deleteIndex >= 0 && _selOffset == 0) {
+				_editString.deleteChar(deleteIndex);
+				setCaretPos(caretVisualPos(deleteIndex));
+				_selCaretPos = -1;
+				dirty = true;
+
+				sendCommand(_cmd, 0);
+			} else if (deleteIndex >= 0 && _selOffset != 0) {
+				int selBegin = _selCaretPos;
+				int selEnd = _selCaretPos + _selOffset;
+				if (selBegin > selEnd)
+					SWAP(selBegin, selEnd);
+				_editString.erase(selBegin, selEnd - selBegin);
+				setCaretPos(caretVisualPos(selBegin));
+				_selCaretPos = -1;
+				_selOffset = 0;
+				dirty = true;
+			}
+			forcecaret = true;
+			break;
+
+		case kActionEnd:
+			if (_shiftPressed) {
+				if (_selCaretPos < 0)
+					_selCaretPos = _caretPos;
+
+				setSelectionOffset(_editString.size() - _selCaretPos);
+			} else {
+				clearSelection();
+			}
+
+			// Move caret to end
+			setCaretPos(caretVisualPos(_editString.size()));
+
+			forcecaret = true;
+			dirty = true;
+			break;
+
+		case kActionLeft:
+			if (_shiftPressed) {
+				if (_disableSelection)
+					break;
+				if (_selCaretPos < 0)
+					_selCaretPos = _caretPos;
+				if (_caretPos > 0)
+					_selOffset--;
+			} else {
+				clearSelection();
+			}
+			// Move caret one left (if possible)
+			if (_caretPos > 0) {
+				dirty = setCaretPos(_caretPos - 1);
+			}
+			forcecaret = true;
+			dirty = true;
+			break;
+
+		case kActionRight:
+			if (_shiftPressed) {
+				if (_disableSelection)
+					break;
+				if (_selCaretPos < 0)
+					_selCaretPos = _caretPos;
+				if (_selOffset + _selCaretPos < (int)_editString.size())
+					_selOffset++;
+			} else {
+				clearSelection();
+			}
+			// Move caret one right (if possible)
+			if (_caretPos < (int)_editString.size()) {
+				dirty = setCaretPos(_caretPos + 1);
+			}
+			forcecaret = true;
+			dirty = true;
+			break;
+
+		case kActionHome:
+			if (_shiftPressed) {
+				if (_selCaretPos < 0)
+					_selCaretPos = _caretPos;
+				setSelectionOffset(0 - _selCaretPos);
+			} else {
+				clearSelection();
+			}
+			// Move caret to start
+			setCaretPos(caretVisualPos(0));
+
+			forcecaret = true;
+			dirty = true;
+			break;
+
+		case kActionCut:
+			if (!getEditString().empty() && _selOffset != 0) {
+				int selBegin = _selCaretPos;
+				int selEnd = _selCaretPos + _selOffset;
+				if (selBegin > selEnd)
+					SWAP(selBegin, selEnd);
+				const Common::U32String selected(getEditString().begin() + selBegin, getEditString().begin() + selEnd);
+				g_system->setTextInClipboard(selected);
+
+				_editString.erase(selBegin, selEnd - selBegin);
+				setCaretPos(caretVisualPos(selBegin));
+				_selCaretPos = -1;
+				_selOffset = 0;
+				dirty = true;
+			}
+			break;
+
+		case kActionCopy:
+			if (!getEditString().empty()) {
+				int selBegin = _selCaretPos;
+				int selEnd = _selCaretPos + _selOffset;
+				if (selBegin > selEnd)
+					SWAP(selBegin, selEnd);
+				const Common::U32String selected(getEditString().begin() + selBegin, getEditString().begin() + selEnd);
+				g_system->setTextInClipboard(selected);
+			}
+			break;
+
+		case kActionPaste:
+			if (g_system->hasTextInClipboard()) {
+				Common::U32String text = g_system->getTextFromClipboard();
+				if (_selOffset != 0) {
+					int selBegin = _selCaretPos;
+					int selEnd = _selCaretPos + _selOffset;
+					if (selBegin > selEnd)
+						SWAP(selBegin, selEnd);
+					_editString.replace(selBegin, selEnd - selBegin, text);
+					setCaretPos(caretVisualPos(selBegin));
+					const int logicalPosition = caretLogicalPos();
+					setCaretPos(caretVisualPos(logicalPosition + text.size()));
+					clearSelection();
+				} else {
+					for (uint32 i = 0; i < text.size(); ++i) {
+						const int logicalPosition = caretLogicalPos();
+						if (tryInsertChar(text[i], logicalPosition))
+								setCaretPos(caretVisualPos(logicalPosition + 1));
+					}
+				}
+				dirty = true;
+			}
+			break;
+		default:
+			break;
+	}
+	default:
+		break;
+	}
+
+	if (dirty)
+		markAsDirty();
+
+	if (forcecaret)
+		makeCaretVisible();
 }
 
 int EditableWidget::getCaretOffset() const {

backends/keymapper/standard-actions.cpp
@@ -38,5 +38,8 @@ const char *kStandardActionLoad          = "LOAD";
 const char *kStandardActionSave          = "SAVE";
 const char *kStandardActionOpenSettings  = "OPTS";
 const char *kStandardActionEE            = "WTF";
+const char *kStandardActionCut			 = "CUT";
+const char *kStandardActionCopy		     = "COPY";
+const char *kStandardActionPaste	     = "PASTE";
 
 } //namespace Common

backends/keymapper/standard-actions.h
@@ -52,6 +52,9 @@ extern const char *kStandardActionLoad;
 extern const char *kStandardActionSave;
 extern const char *kStandardActionOpenSettings;
 extern const char *kStandardActionEE;
+extern const char *kStandardActionCut;
+extern const char *kStandardActionCopy;
+extern const char *kStandardActionPaste;
 
 } //namespace Common
 

gui/dialog.cpp
@@ -374,6 +374,11 @@ void Dialog::handleTickle() {
 		_tickleWidget->handleTickle();
 }
 
+void Dialog::handleOtherEvent(const Common::Event &evt) {
+	if (_focusedWidget)
+		_focusedWidget->handleOtherEvent(evt);
+}
+
 void Dialog::handleCommand(CommandSender *sender, uint32 cmd, uint32 data) {
 	switch (cmd) {
 	case kCloseCmd:

gui/dialog.h
@@ -113,7 +113,7 @@ protected:
 	virtual void handleKeyUp(Common::KeyState state);
 	virtual void handleMouseMoved(int x, int y, int button);
 	virtual void handleMouseLeft(int button) {}
-	virtual void handleOtherEvent(const Common::Event &evt) {}
+	virtual void handleOtherEvent(const Common::Event &evt);
 	void handleCommand(CommandSender *sender, uint32 cmd, uint32 data) override;
 
 	Widget *findWidget(int x, int y); // Find the widget at pos x,y if any

gui/gui-manager.h
@@ -47,6 +47,20 @@ namespace Common {
 namespace GUI {
 
 enum {
+	kActionEnter,
+	kActionEscape,
+	kActionBackspace,
+	kActionDelete,
+	kActionEnd,
+	kActionLeft,
+	kActionRight,
+	kActionHome,
+	kActionCopy,
+	kActionCut,
+	kActionPaste,
+};
+
+enum {
 	kIconsSetLoadedCmd  = 'icns'
 };
 

gui/widget.h
@@ -33,6 +33,7 @@
 #include "common/text-to-speech.h"
 #include "common/system.h"
 #include "common/config-manager.h"
+#include "common/events.h"
 
 namespace GUI {
 
@@ -144,6 +145,7 @@ public:
 	void handleMouseWheel(int x, int y, int direction) override { assert(_boss); _boss->handleMouseWheel(x, y, direction); }
 	virtual bool handleKeyDown(Common::KeyState state) { return false; }	// Return true if the event was handled
 	virtual bool handleKeyUp(Common::KeyState state) { return false; }	// Return true if the event was handled
+	virtual void handleOtherEvent(const Common::Event &evt) {}
 	virtual void handleTickle() {}
 
 	/** Mark the widget and its children as dirty so they are redrawn on the next screen update */

commit 7939111dfa36320e35f77d0dff67ace702909b0b

GUI: FIX conflicting mappings between EditableWidget and GUI elements
EditableWidget took over keyboard events targeted towards Debugger and
List. This commit fixes it by reverting some part of the code to its
original state.

Fixes issues caused by PR #5366

gui/gui-manager.cpp
@@ -191,63 +191,44 @@ Common::Keymap *GuiManager::getKeymap() const {
 	act->allowKbdRepeats();
 	guiMap->addAction(act);
 
-	act = new Action("ENTER", _("New Line"));
-	act->setCustomEngineActionEvent(kActionEnter);
-	act->addDefaultInputMapping("RETURN");
-	act->addDefaultInputMapping("KP_ENTER");
-	act->allowKbdRepeats();
-	guiMap->addAction(act);
-
-	act = new Action("ESC", _("Close Dialog"));
-	act->setCustomEngineActionEvent(kActionEscape);
-	act->addDefaultInputMapping("ESCAPE");
-	guiMap->addAction(act);
-
 	act = new Action("BACKSPACE", _("Backspace"));
-	act->setCustomEngineActionEvent(kActionBackspace);
+	act->setKeyEvent(KEYCODE_BACKSPACE);
 	act->addDefaultInputMapping("BACKSPACE");
 	act->allowKbdRepeats();
 	guiMap->addAction(act);
 
 	act = new Action("DEL", _("Delete Character"));
-	act->setCustomEngineActionEvent(kActionDelete);
+	act->setKeyEvent(KEYCODE_DELETE);
 	act->addDefaultInputMapping("DELETE");
 	act->allowKbdRepeats();
 	guiMap->addAction(act);
 
-	act = new Action("END", _("Go to end of line"));
-	act->setCustomEngineActionEvent(kActionEnd);
 
 #ifdef MACOSX
+	act = new Action("MAC_END", _("Go to end of line"));
+	act->setCustomEngineActionEvent(kActionEnd);
 	act->addDefaultInputMapping("C+e");
+	guiMap->addAction(act);
 #endif
 
-	act->addDefaultInputMapping("DOWN");
+	act = new Action("END", _("Go to end of line"));
+	act->setKeyEvent(KEYCODE_END);
 	act->addDefaultInputMapping("END");
 	guiMap->addAction(act);
 
-	act = new Action("CHAR_LEFT", _("Move cursor left"));
-	act->setCustomEngineActionEvent(kActionLeft);
-	act->addDefaultInputMapping("LEFT");
-	act->allowKbdRepeats();
-	guiMap->addAction(act);
-
-	act = new Action("CHAR_RIGHT", _("Move cursor right"));
-	act->setCustomEngineActionEvent(kActionRight);
-	act->addDefaultInputMapping("RIGHT");
-	act->allowKbdRepeats();
-	guiMap->addAction(act);
-
-	act = new Action("HOME", _("Go to start of line"));
-	act->setCustomEngineActionEvent(kActionHome);
 
 #ifdef MACOSX
+	act = new Action("MAC_HOME", _("Go to start of line"));
+	act->setCustomEngineActionEvent(kActionHome);
 	act->addDefaultInputMapping("C+a");
+	guiMap->addAction(act);
 #endif
 
-	act->addDefaultInputMapping("UP");
+	act = new Action("HOME", _("Go to start of line"));
+	act->setKeyEvent(KEYCODE_HOME);
 	act->addDefaultInputMapping("HOME");
 	guiMap->addAction(act);
+
 
 #ifdef MACOSX
 	act = new Action(kStandardActionCut, _("Cut"));

gui/widgets/editable.cpp
@@ -209,6 +209,7 @@ bool EditableWidget::handleKeyDown(Common::KeyState state) {
 	bool handled = true;
 	bool dirty = false;
 	bool forcecaret = false;
+	int deleteIndex;
 
 	if (!isEnabled())
 		return false;
@@ -241,7 +242,124 @@ bool EditableWidget::handleKeyDown(Common::KeyState state) {
 		state.keycode = remap[state.keycode - Common::KEYCODE_KP0];
 	}
 
-	defaultKeyDownHandler(state, dirty, forcecaret, handled);
+	switch (state.keycode) {
+	case Common::KEYCODE_RETURN:
+	case Common::KEYCODE_KP_ENTER:
+		// confirm edit and exit editmode
+		endEditMode();
+		dirty = true;
+		break;
+
+	case Common::KEYCODE_ESCAPE:
+		abortEditMode();
+		dirty = true;
+		break;
+
+	case Common::KEYCODE_BACKSPACE:
+		deleteIndex = caretLogicalPos();
+		if (deleteIndex > 0 && _selOffset == 0) {
+			deleteIndex--;
+			_editString.deleteChar(deleteIndex);
+			setCaretPos(caretVisualPos(deleteIndex));
+			_selCaretPos = -1;
+			dirty = true;
+
+			sendCommand(_cmd, 0);
+		} else if (deleteIndex >= 0 && _selOffset != 0) {
+			int selBegin = _selCaretPos;
+			int selEnd = _selCaretPos + _selOffset;
+			if (selBegin > selEnd)
+				SWAP(selBegin, selEnd);
+			_editString.erase(selBegin, selEnd - selBegin);
+			setCaretPos(caretVisualPos(selBegin));
+			_selCaretPos = -1;
+			_selOffset = 0;
+			dirty = true;
+
+			sendCommand(_cmd, 0);
+		}
+		forcecaret = true;
+		break;
+
+	case Common::KEYCODE_DELETE:
+		deleteIndex = caretLogicalPos();
+		if (deleteIndex < (int)_editString.size()) {
+			_editString.deleteChar(deleteIndex);
+			setCaretPos(caretVisualPos(deleteIndex));
+			_selCaretPos = -1;
+			_selOffset = 0;
+			dirty = true;
+
+			sendCommand(_cmd, 0);
+		}
+		forcecaret = true;
+		break;
+
+	case Common::KEYCODE_DOWN:
+	case Common::KEYCODE_END:
+		// Move caret to end
+		setCaretPos(caretVisualPos(_editString.size()));
+		if (state.hasFlags(Common::KBD_SHIFT))
+			setSelectionOffset(_editString.size() - _selCaretPos);
+		else
+			clearSelection();
+		forcecaret = true;
+		dirty = true;
+		break;
+
+	case Common::KEYCODE_LEFT:
+		if (state.hasFlags(Common::KBD_SHIFT)) {
+			if (_disableSelection)
+				break;
+			if (_selCaretPos < 0)
+				_selCaretPos = _caretPos;
+			if (_caretPos > 0)
+				_selOffset--;
+		} else {
+			clearSelection();
+		}
+		// Move caret one left (if possible)
+		if (_caretPos > 0) {
+			dirty = setCaretPos(_caretPos - 1);
+		}
+		forcecaret = true;
+		dirty = true;
+		break;
+
+	case Common::KEYCODE_RIGHT:
+		if (state.hasFlags(Common::KBD_SHIFT)) {
+			if (_disableSelection)
+				break;
+			if (_selCaretPos < 0)
+				_selCaretPos = _caretPos;
+			if (_selOffset + _selCaretPos < (int)_editString.size())
+				_selOffset++;
+		} else {
+			clearSelection();
+		}
+		// Move caret one right (if possible)
+		if (_caretPos < (int)_editString.size()) {
+			dirty = setCaretPos(_caretPos + 1);
+		}
+		forcecaret = true;
+		dirty = true;
+		break;
+
+	case Common::KEYCODE_UP:
+	case Common::KEYCODE_HOME:
+		// Move caret to start
+		setCaretPos(caretVisualPos(0));
+		if (state.hasFlags(Common::KBD_SHIFT))
+			setSelectionOffset(0 - _selCaretPos);
+		else
+			clearSelection();
+		forcecaret = true;
+		dirty = true;
+		break;
+
+	default:
+		defaultKeyDownHandler(state, dirty, forcecaret, handled);
+	}
 
 	if (dirty)
 		markAsDirty();
@@ -283,7 +401,6 @@ void EditableWidget::defaultKeyDownHandler(Common::KeyState &state, bool &dirty,
 void EditableWidget::handleOtherEvent(const Common::Event &evt) {
 	bool dirty = false;
 	bool forcecaret = false;
-	int deleteIndex;
 
 	if (!isEnabled())
 		return;
@@ -295,135 +412,26 @@ void EditableWidget::handleOtherEvent(const Common::Event &evt) {
 	switch (evt.type) {
 	case Common::EVENT_CUSTOM_ENGINE_ACTION_START:
 		switch (evt.customType) {
-		case kActionEnter:
-			endEditMode();
-			dirty = true;
-			break;
-
-		case kActionEscape:
-			abortEditMode();
-			dirty = true;
-			break;
-
-		case kActionBackspace:
-			deleteIndex = caretLogicalPos();
-			if (deleteIndex > 0 && _selOffset == 0) {
-				deleteIndex--;
-				_editString.deleteChar(deleteIndex);
-				setCaretPos(caretVisualPos(deleteIndex));
-				_selCaretPos = -1;
-				dirty = true;
-
-				sendCommand(_cmd, 0);
-			} else if (deleteIndex >= 0 && _selOffset != 0) {
-				int selBegin = _selCaretPos;
-				int selEnd = _selCaretPos + _selOffset;
-				if (selBegin > selEnd)
-					SWAP(selBegin, selEnd);
-				_editString.erase(selBegin, selEnd - selBegin);
-				setCaretPos(caretVisualPos(selBegin));
-				_selCaretPos = -1;
-				_selOffset = 0;
-				dirty = true;
-
-				sendCommand(_cmd, 0);
-			}
-			forcecaret = true;
-			break;
-
-		case kActionDelete:
-			deleteIndex = caretLogicalPos();
-			if (deleteIndex >= 0 && _selOffset == 0) {
-				_editString.deleteChar(deleteIndex);
-				setCaretPos(caretVisualPos(deleteIndex));
-				_selCaretPos = -1;
-				dirty = true;
-
-				sendCommand(_cmd, 0);
-			} else if (deleteIndex >= 0 && _selOffset != 0) {
-				int selBegin = _selCaretPos;
-				int selEnd = _selCaretPos + _selOffset;
-				if (selBegin > selEnd)
-					SWAP(selBegin, selEnd);
-				_editString.erase(selBegin, selEnd - selBegin);
-				setCaretPos(caretVisualPos(selBegin));
-				_selCaretPos = -1;
-				_selOffset = 0;
-				dirty = true;
-			}
-			forcecaret = true;
-			break;
-
-		case kActionEnd:
-			if (_shiftPressed) {
-				if (_selCaretPos < 0)
-					_selCaretPos = _caretPos;
-
-				setSelectionOffset(_editString.size() - _selCaretPos);
-			} else {
-				clearSelection();
-			}
-
-			// Move caret to end
-			setCaretPos(caretVisualPos(_editString.size()));
-
-			forcecaret = true;
-			dirty = true;
-			break;
-
-		case kActionLeft:
-			if (_shiftPressed) {
-				if (_disableSelection)
-					break;
-				if (_selCaretPos < 0)
-					_selCaretPos = _caretPos;
-				if (_caretPos > 0)
-					_selOffset--;
-			} else {
-				clearSelection();
-			}
-			// Move caret one left (if possible)
-			if (_caretPos > 0) {
-				dirty = setCaretPos(_caretPos - 1);
-			}
-			forcecaret = true;
-			dirty = true;
-			break;
-
-		case kActionRight:
-			if (_shiftPressed) {
-				if (_disableSelection)
-					break;
-				if (_selCaretPos < 0)
-					_selCaretPos = _caretPos;
-				if (_selOffset + _selCaretPos < (int)_editString.size())
-					_selOffset++;
-			} else {
-				clearSelection();
-			}
-			// Move caret one right (if possible)
-			if (_caretPos < (int)_editString.size()) {
-				dirty = setCaretPos(_caretPos + 1);
-			}
-			forcecaret = true;
-			dirty = true;
-			break;
-
 		case kActionHome:
-			if (_shiftPressed) {
-				if (_selCaretPos < 0)
-					_selCaretPos = _caretPos;
-				setSelectionOffset(0 - _selCaretPos);
-			} else {
-				clearSelection();
-			}
 			// Move caret to start
 			setCaretPos(caretVisualPos(0));
-
+			if (_shiftPressed)
+				setSelectionOffset(0 - _selCaretPos);
+			else
+				clearSelection();
 			forcecaret = true;
 			dirty = true;
 			break;
-
+		case kActionEnd:
+			// Move caret to end
+			setCaretPos(caretVisualPos(_editString.size()));
+			if (_shiftPressed)
+				setSelectionOffset(_editString.size() - _selCaretPos);
+			else
+				clearSelection();
+			forcecaret = true;
+			dirty = true;
+			break;
 		case kActionCut:
 			if (!getEditString().empty() && _selOffset != 0) {
 				int selBegin = _selCaretPos;