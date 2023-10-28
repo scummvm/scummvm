a75be8de5715ab95ac81a0fa7e35b78940996ab5
Kaloyan Chehlarski <strahy@outlook.com>
2023-10-28 16:38:50 +0300

    Revert "SCI: Fix uninitialized variable warnings"
    This reverts commit b9fb4e22ec6f25be693341f9d3f5e0a3213aadaf.

--- engines/sci/engine/vm_types.h
+++ engines/sci/engine/vm_types.h
@@ -38,8 +38,8 @@ enum {
 
 struct reg_t {
 	// Segment and offset. These should never be accessed directly
-	SegmentId _segment = 0;
-	uint16 _offset = 0;
+	SegmentId _segment;
+	uint16 _offset;
 
 	SegmentId getSegment() const;
 	void setSegment(SegmentId segment);
