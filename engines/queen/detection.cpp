f88281ab7ff62479e4f3c9977270323d152d7420
BLooperZ <blooperz@users.noreply.github.com>
2023-11-17 13:08:17 +0200

    QUEEN: Allow improved hebrew font in fallback

--- engines/queen/detection.cpp
+++ engines/queen/detection.cpp
@@ -528,6 +528,8 @@ ADDetectedGame QueenMetaEngineDetection::fallbackDetect(const FileMap &allFiles,
 				} else if (version.features & Queen::GF_TALKIE) {
 					desc.extra = "CD";
 					desc.guiOptions = GAMEOPTION_ALT_INTRO;
+					if (desc.language == Common::HE_ISR)
+						desc.guiOptions = GUIO2(GAMEOPTION_ALT_INTRO, GAMEOPTION_ALT_FONT);
 				}
 
 				return ADDetectedGame(&desc);
