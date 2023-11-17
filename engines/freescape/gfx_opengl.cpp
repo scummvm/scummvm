7421d3be17f19fc56af16be89967b44f26c4b79b
neuromancer <gustavo.grieco@gmail.com>
2023-11-17 15:46:55 +0100

    FREESCAPE: avoid truncation from double to float (again)

--- engines/freescape/gfx_opengl.cpp
+++ engines/freescape/gfx_opengl.cpp
@@ -299,7 +299,7 @@ void OpenGLRenderer::renderPlayerShootBall(byte color, const Common::Point posit
 
 	glColor3ub(r, g, b);
 	int triangleAmount = 20;
-	float twicePi = 2 * 3.1416;
+	float twicePi = 2.f * 3.1416f;
 	float coef = (9 - frame) / 9.0;
 	float radius = (1 - coef) * 4.0;
 
