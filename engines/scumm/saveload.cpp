d9d4c278e1d7ac8389d598f6948ef081d127f68b
AndywinXp <andywinxp@gmail.com>
2023-11-30 23:09:35 +0100

    SCUMM: Attempt at fixing blocker #14614
    We limit this piece of code to the one and only game having a
    PC Engine port, which is Loom.

--- engines/scumm/saveload.cpp
+++ engines/scumm/saveload.cpp
@@ -2085,7 +2085,7 @@ void ScummEngine_v5::saveLoadWithSerializer(Common::Serializer &s) {
 	// This avoids color issues when loading savegames that have been saved with a different ScummVM port
 	// that uses a different 16bit color mode than the ScummVM port which is currently used.
 #ifdef USE_RGB_COLOR
-	if (_game.platform == Common::kPlatformPCEngine && s.isLoading()) {
+	if (_game.id == GID_LOOM && _game.platform == Common::kPlatformPCEngine && s.isLoading()) {
 		for (int i = 0; i < 256; ++i)
 			_16BitPalette[i] = get16BitColor(_currentPalette[i * 3 + 0], _currentPalette[i * 3 + 1], _currentPalette[i * 3 + 2]);
 	}
