9b3abdb286538946b93cc4af7ccf4638a4b864e3
Lothar Serra Mari <mail@serra.me>
2023-12-03 21:50:27 +0100

    IOS7: Fix broken character in help screen

--- backends/platform/ios7/ios7_osys_misc.mm
+++ backends/platform/ios7/ios7_osys_misc.mm
@@ -250,7 +250,7 @@ _s("Touch Controls"),
 "ios-help.zip",
 _s(
 "## Touch control modes\n"
-"The touch control mode can be changed by tapping or clicking on the controller icon in the upper right corner, by swiping two fingers from left to right or in the global settings From the Launcher, go to **Global Options > BackenChoose the preferred touch mode. It’s possible to configure the touch mode for three situations (ScummVM menus, 2D games and 3D games).\n"
+"The touch control mode can be changed by tapping or clicking on the controller icon in the upper right corner, by swiping two fingers from left to right or in the global settings From the Launcher, go to **Global Options > Backend > Choose the preferred touch mode**. It's possible to configure the touch mode for three situations (ScummVM menus, 2D games and 3D games).\n"
 "\n"
 "### Direct mouse \n"
 "\n"
