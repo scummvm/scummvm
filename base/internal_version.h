bfe03dad48cc93fe8055829e47b77b72d04e2704
Eugene Sandulenko <sev@scummvm.org>
2023-12-16 22:13:26 +0100

    RELEASE: This is 2.8.1pre

--- base/internal_version.h
+++ base/internal_version.h
@@ -16,8 +16,8 @@
 #define SCUMMVM_REVISION
 #endif
 
-#define SCUMMVM_VERSION "2.8.0pre"
+#define SCUMMVM_VERSION "2.8.1pre"
 
 #define SCUMMVM_VER_MAJOR 2
 #define SCUMMVM_VER_MINOR 8
-#define SCUMMVM_VER_PATCH 0
+#define SCUMMVM_VER_PATCH 1
