f490760955b728e5ff4662e1b4b86f6430d0bdda
Miro Kropacek <miro.kropacek@gmail.com>
2023-12-19 13:17:45 +0100

    BACKENDS: ATARI: Re-add classicmacfonts.dat
    As it seems to be used in some lowres games as well. Also remove
    hugo.dat (which is explicitly disabled in the full version).
    
    Change 'rm -f' to 'rm' to see what is actually deleted for real.

--- backends/platform/atari/build-release.sh
+++ backends/platform/atari/build-release.sh
@@ -49,7 +49,7 @@ ${PLATFORM}-nm -C dist-generic/scummvm/scummvm.ttp | grep -vF ' .L' | grep ' [Tt
 ${PLATFORM}-strip -s dist-generic/scummvm/scummvm.ttp
 
 # remove unused files; absent gui-icons.dat massively speeds up startup time (used for the grid mode)
-rm -f dist-generic/scummvm/data/{classicmacfonts,achievements,encoding,gui-icons,macgui,shaders}.dat
+rm dist-generic/scummvm/data/{achievements,encoding,gui-icons,hugo,macgui,shaders}.dat
 
 # move themes into 'themes' folder (with compression level zero for faster depacking)
 mkdir -p dist-generic/scummvm/themes
