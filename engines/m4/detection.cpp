a250d0a77bfe3b9fe4efd878c7b7d772be731695
Eugene Sandulenko <sev@scummvm.org>
2023-12-24 02:31:33 +0100

    M4: Remove unused include

--- engines/m4/detection.cpp
+++ engines/m4/detection.cpp
@@ -24,7 +24,6 @@
 #include "common/file.h"
 #include "common/md5.h"
 #include "common/str-array.h"
-#include "common/translation.h"
 #include "common/util.h"
 #include "m4/detection.h"
 #include "m4/detection_tables.h"
