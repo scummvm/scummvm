7b3d56a7b1cdd81abe7fc0b25649566bac82ff51
Cameron Cawley <ccawley2011@gmail.com>
2023-12-26 16:32:05 +0000

    COMMON: Allow passing const pointers to BitArray::set_bits()

--- common/bitarray.h
+++ common/bitarray.h
@@ -48,7 +48,7 @@ public:
 		_bits = (byte*)malloc((bitcount + 7) / 8);
 		clear();
 	}
-	void set_bits(byte *bits) {
+	void set_bits(const byte *bits) {
 		memcpy(_bits, bits, (_bitcount + 7) / 8);
 	}
 
