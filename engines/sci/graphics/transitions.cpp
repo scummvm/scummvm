01e7071315bffda85b012686e6c0434c26ad99d3
Cameron Cawley <ccawley2011@gmail.com>
2023-12-29 17:55:25 +0000

    SCI: Use OSystem::fillScreen() where possible

--- engines/sci/graphics/transitions.cpp
+++ engines/sci/graphics/transitions.cpp
@@ -283,16 +283,14 @@ void GfxTransitions::copyRectToScreen(const Common::Rect rect, bool blackoutFlag
 	if (!blackoutFlag) {
 		_screen->copyRectToScreen(rect);
 	} else {
-		Graphics::Surface *surface = g_system->lockScreen();
 		if (!_screen->getUpscaledHires()) {
-			surface->fillRect(rect, 0);
+			g_system->fillScreen(rect, 0);
 		} else {
 			Common::Rect upscaledRect = rect;
 			_screen->adjustToUpscaledCoordinates(upscaledRect.top, upscaledRect.left);
 			_screen->adjustToUpscaledCoordinates(upscaledRect.bottom, upscaledRect.right);
-			surface->fillRect(upscaledRect, 0);
+			g_system->fillScreen(upscaledRect, 0);
 		}
-		g_system->unlockScreen();
 	}
 }
 
