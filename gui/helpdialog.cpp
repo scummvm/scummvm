70515f2d9338c7085eb2b46cebe0daf3d8eea230
Eugene Sandulenko <sev@scummvm.org>
2023-12-31 09:49:36 +0100

    GUI: Disable translations in the HelpDialog
    MacFonts have only ASCII, not even accented characters, making
    all of the translation renderings broken.

--- gui/helpdialog.cpp
+++ gui/helpdialog.cpp
@@ -130,7 +130,7 @@ _s(
 
 void HelpDialog::addTabs(const char * const *tabData) {
 	while (*tabData) {
-		Common::U32String tabName(_(*tabData++));
+		Common::U32String tabName(*tabData++);
 		const char *imagePack = nullptr;
 
 		if (*tabData && **tabData)
@@ -138,7 +138,7 @@ void HelpDialog::addTabs(const char * const *tabData) {
 
 		tabData++;
 
-		Common::U32String tabText(_(*tabData++));
+		Common::U32String tabText(*tabData++);
 
 		_tab->addTab(tabName, "HelpContentDialog", false);
 
