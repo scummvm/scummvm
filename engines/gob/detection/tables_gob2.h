559208800e73408e7fdc0f2c004127d5026d2dbc
Benjamin Funke <bjnfne@web.de>
2024-01-13 08:10:50 +0100

    GOB: add langcode and version number to mac GOB2 version

--- engines/gob/detection/tables_gob2.h
+++ engines/gob/detection/tables_gob2.h
@@ -484,10 +484,10 @@
 { // Supplied by fac76 in bug report #3108
 	{
 		"gob2",
-		"",
+		"v1.02",
 		AD_ENTRY2s("intro.stk", "b45b984ee8017efd6ea965b9becd4d66", 828443,
 				"musmac1.mid",	"7f96f491448c7a001b32df89cf8d2af2", 1658),
-		UNK_LANG,
+		EN_ANY,
 		kPlatformMacintosh,
 		ADGF_NO_FLAGS,
 		GUIO2(GUIO_NOSUBTITLES, GUIO_NOSPEECH)
