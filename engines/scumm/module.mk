b60bb0ec644156a8e398d38a39c2e01b76b14798
Little Cat <toontownlittlecat@gmail.com>
2024-01-13 03:27:29 -0400

    SCUMM HE: Added Katton generator.

--- engines/scumm/module.mk
+++ engines/scumm/module.mk
@@ -175,6 +175,7 @@ MODULE_OBJS += \
 	he/moonbase/ai_tree.o \
 	he/moonbase/ai_types.o \
 	he/moonbase/ai_weapon.o \
+	he/moonbase/map_katton.o \
 	he/moonbase/map_main.o \
 	he/moonbase/map_mif.o \
 	he/moonbase/map_spiff.o \
