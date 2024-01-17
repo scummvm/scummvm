bb530c76ee46adbeee3350de3abe45cc41110c89
Vladimir Serbinenko <phcoder@gmail.com>
2024-01-17 04:42:57 +0300

    KOLIBRI: build-kolibri: Temporarily disable ags engine as it fails to compile

--- backends/platform/sdl/kolibrios/build-kolibri.sh
+++ backends/platform/sdl/kolibrios/build-kolibri.sh
@@ -6,6 +6,6 @@ export KOS32_SDK_DIR=$HOME/kolibrios/contrib/sdk
 export KOS32_AUTOBUILD=$HOME/autobuild
 
 # Use plugins for both engines and detection as KolibriOS has a limit per executable module
-./configure --host=kos32 --enable-release --enable-plugins --default-dynamic --enable-detection-dynamic --enable-engine=testbed
+./configure --host=kos32 --enable-release --enable-plugins --default-dynamic --enable-detection-dynamic --enable-engine=testbed --disable-engine=ags
 
 make -j5 all zip-root scummvm-zip
