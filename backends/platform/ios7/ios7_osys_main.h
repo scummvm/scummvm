c8d93ad820140a5c3c4a457d75d74a77e82ab28d
Lars Sundström <l.sundstrom@gmail.com>
2024-01-19 13:16:42 +0100

    IOS7: Pass key modifier flags to key events
    Prepare the backend to send key modifier flags in the key events.
    No modifier flags are sent at this point but will be added in
    upcoming commits.

--- backends/platform/ios7/ios7_osys_main.h
+++ backends/platform/ios7/ios7_osys_main.h
@@ -181,7 +181,7 @@ protected:
 	bool handleEvent_swipe(Common::Event &event, int direction, int touches);
 	bool handleEvent_tap(Common::Event &event, UIViewTapDescription type, int touches);
 	bool handleEvent_longPress(Common::Event &event, UIViewLongPressDescription type, int touches);
-	void handleEvent_keyPressed(Common::Event &event, int keyPressed);
+	void handleEvent_keyPressed(Common::Event &event, int keyPressed, int modifierFlags);
 	void handleEvent_orientationChanged(int orientation);
 	void handleEvent_touchModeChanged();
 	void handleEvent_applicationSuspended();
