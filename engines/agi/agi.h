671298f2d1a2eadd21c4ea4a78c6dca3c6fc9beb
Filippos Karapetis <bluegr@gmail.com>
2024-01-27 04:28:47 +0200

    AGI: Remove superfluous AGI version setup for AGDS games

--- engines/agi/agi.h
+++ engines/agi/agi.h
@@ -117,7 +117,7 @@ enum AgiGameType {
 
 enum AgiGameFeatures {
 	GF_AGIMOUSE    = (1 << 0), // this disables "Click-to-walk mouse interface"
-	GF_AGDS        = (1 << 1),
+	GF_AGDS        = (1 << 1), // marks games created with AGDS - all using AGI version 2.440
 	GF_AGI256      = (1 << 2), // marks fanmade AGI-256 games
 	GF_FANMADE     = (1 << 3), // marks fanmade games
 	GF_2GSOLDSOUND = (1 << 5),
@@ -761,7 +761,6 @@ public:
 	void initFeatures();
 	void setFeature(uint32 feature);
 	void initVersion();
-	void setVersion(uint16 version);
 
 	const char *getDiskName(uint16 id);
 
--- engines/agi/loader_v2.cpp
+++ engines/agi/loader_v2.cpp
@@ -33,11 +33,6 @@ int AgiLoader_v2::detectGame() {
 	    !Common::File::exists(VIEWDIR))
 		return errInvalidAGIFile;
 
-	// Should this go above the previous lines, so we can force emulation versions
-	// even for AGDS games? -- dsymonds
-	if (_vm->getFeatures() & GF_AGDS)
-		_vm->setVersion(0x2440);   // ALL AGDS games built for 2.440
-
 	return errOK;
 }
 
