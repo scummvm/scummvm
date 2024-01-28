0d2f87768ea80501f4bc0ed6d1785b9f613d6304
Vladimir Serbinenko <phcoder@gmail.com>
2024-01-28 14:31:31 +0300

    MIYOOMINI: Enable neon and freetype2 in documented compilation script

--- backends/platform/sdl/miyoo/build_miyoomini.sh
+++ backends/platform/sdl/miyoo/build_miyoomini.sh
@@ -8,6 +8,6 @@ SYSROOT=$TOOLCHAIN/arm-linux-gnueabihf
 export PATH=$TOOLCHAIN/usr/bin:$SYSROOT/usr/include:$TOOLCHAIN/bin:$PATH
 export CXX=arm-linux-gnueabihf-g++
 
-./configure --host=miyoomini --enable-release --enable-plugins --default-dynamic --enable-engine=testbed
+./configure --host=miyoomini --enable-release --enable-plugins --default-dynamic --enable-engine=testbed --enable-ext-neon --with-freetype2-prefix=/opt/miyoomini-toolchain/arm-linux-gnueabihf/libc/usr/bin/
 
 make -j5 all sd-root sd-zip
