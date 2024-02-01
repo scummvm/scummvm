9cb227e000d8b06dcd6a1d486ad98bf3668563f3
Matthew Jimenez <matthew.jimenez@outlook.com>
2024-02-01 17:01:28 -0600

    GUI: Replace byte arrays with Palette class in image album dialog

--- engines/mtropolis/plugin/mti.cpp
+++ engines/mtropolis/plugin/mti.cpp
@@ -38,6 +38,7 @@
 #include "video/mpegps_decoder.h"
 
 #include "graphics/managed_surface.h"
+#include "graphics/palette.h"
 
 #include "common/file.h"
 namespace MTropolis {
@@ -416,7 +417,7 @@ class PrintModifierImageSupplier : public GUI::ImageAlbumImageSupplier {
 public:
 	PrintModifierImageSupplier(const Common::String &inputPath, bool isMacVersion);
 
-	bool loadImageSlot(uint slot, const Graphics::Surface *&outSurface, bool &outHasPalette, byte (&outPalette)[256 * 3], GUI::ImageAlbumImageMetadata &outMetadata) override;
+	bool loadImageSlot(uint slot, const Graphics::Surface *&outSurface, bool &outHasPalette, Graphics::Palette &outPalette, GUI::ImageAlbumImageMetadata &outMetadata) override;
 	void releaseImageSlot(uint slot) override;
 	uint getNumSlots() const override;
 	Common::U32String getDefaultFileNameForSlot(uint slot) const override;
@@ -437,7 +438,7 @@ PrintModifierImageSupplier::PrintModifierImageSupplier(const Common::String &inp
 		_decoder.reset(new Image::BitmapDecoder());
 }
 
-bool PrintModifierImageSupplier::loadImageSlot(uint slot, const Graphics::Surface *&outSurface, bool &outHasPalette, byte (&outPalette)[256 * 3], GUI::ImageAlbumImageMetadata &outMetadata) {
+bool PrintModifierImageSupplier::loadImageSlot(uint slot, const Graphics::Surface *&outSurface, bool &outHasPalette, Graphics::Palette &outPalette, GUI::ImageAlbumImageMetadata &outMetadata) {
 	Common::ScopedPtr<Common::SeekableReadStream> dataStream(createReadStreamForSlot(slot));
 
 	if (!dataStream)
@@ -454,7 +455,7 @@ bool PrintModifierImageSupplier::loadImageSlot(uint slot, const Graphics::Surfac
 	outHasPalette = _decoder->hasPalette();
 
 	if (_decoder->hasPalette())
-		memcpy(outPalette + _decoder->getPaletteStartIndex() * 3, _decoder->getPalette(), _decoder->getPaletteColorCount() * 3);
+		outPalette.set(_decoder->getPalette(), _decoder->getPaletteStartIndex(), _decoder->getPaletteColorCount());
 
 	outMetadata = GUI::ImageAlbumImageMetadata();
 	outMetadata._orientation = GUI::kImageAlbumImageOrientationLandscape;
