16ba8872b67df20551dda502913e2751fe15be56
sluicebox <22204938+sluicebox@users.noreply.github.com>
2024-02-05 22:33:12 -0800

    FREESCAPE: Fix MSVC warning
    Hopefully fixes github builds

--- engines/freescape/games/dark/dark.cpp
+++ engines/freescape/games/dark/dark.cpp
@@ -559,7 +559,7 @@ void DarkEngine::pressedKey(const int keycode) {
 			_flyMode = false;
 			insertTemporaryMessage(_messagesList[13], _countdown - 2);
 		} else if (_flyMode) {
-			float hzFreq = 1193180.0 / 0xd537;
+			float hzFreq = 1193180.0f / 0xd537;
 			_speaker->play(Audio::PCSpeaker::kWaveFormSquare, hzFreq, -1);
 			_mixer->playStream(Audio::Mixer::kSFXSoundType, &_soundFxHandle, _speaker, -1, Audio::Mixer::kMaxChannelVolume / 2, 0, DisposeAfterUse::NO);
 			insertTemporaryMessage(_messagesList[11], _countdown - 2);
