6aeb738a2dd8b4820c0ac9f1f8a6013e990ec594
Lars Sundström <l.sundstrom@gmail.com>
2024-02-08 07:10:35 +0100

    IOS7: Update backend options tab layout
    The backend options widget is now in a scrollcontainer widget.
    Change the options widget to not be scrollable since the parent
    widget is and remove unnecessary padding.
    
    Cherry-pick of: 15be37dd64b57703ab821f13a4b34d1665202f13

--- backends/platform/ios7/ios7_options.mm
+++ backends/platform/ios7/ios7_options.mm
@@ -92,7 +92,7 @@ private:
 };
 
 IOS7OptionsWidget::IOS7OptionsWidget(GuiObject *boss, const Common::String &name, const Common::String &domain) :
-		OptionsContainerWidget(boss, name, "IOS7OptionsDialog", true, domain), _enabled(true) {
+		OptionsContainerWidget(boss, name, "IOS7OptionsDialog", false, domain), _enabled(true) {
 
 	_gamepadControllerCheckbox = new GUI::CheckboxWidget(widgetsBoss(), "IOS7OptionsDialog.GamepadController", _("Show Gamepad Controller (iOS 15 and later)"));
 	_gamepadControllerOpacityDesc = new GUI::StaticTextWidget(widgetsBoss(), "IOS7OptionsDialog.GamepadControllerOpacity", _("Gamepad opacity"));
@@ -177,7 +177,6 @@ void IOS7OptionsWidget::defineLayout(GUI::ThemeEval &layouts, const Common::Stri
 
 	layouts.addDialog(layoutName, overlayedLayout)
 	        .addLayout(GUI::ThemeLayout::kLayoutVertical)
-	            .addPadding(16, 16, 16, 16)
 #if TARGET_OS_IOS
 	            .addWidget("OnscreenControl", "Checkbox")
 #endif
