2b15867dac57162c25da2171d0e249af859d2b07
neuromancer <gustavo.grieco@gmail.com>
2024-02-13 19:55:55 +0100

    FREESCAPE: better position of the sun and moon in eclipse

--- engines/freescape/gfx_opengl.cpp
+++ engines/freescape/gfx_opengl.cpp
@@ -327,10 +327,10 @@ void OpenGLRenderer::drawCelestialBody(Math::Vector3d position, float radius, by
 }
 
 void OpenGLRenderer::drawEclipse(byte color1, byte color2, float progress) {
-	Math::Vector3d sunPosition(-5000, 2000, 500);
+	Math::Vector3d sunPosition(-5000, 1200, 0);
 	float radius = 500.0;
 	drawCelestialBody(sunPosition, radius, color1);
-	Math::Vector3d moonPosition(-5000, 2000, 500 + 500 * progress);
+	Math::Vector3d moonPosition(-5000, 1200, 0 + 500 * progress);
 	drawCelestialBody(moonPosition, radius, color2);
 }
 
