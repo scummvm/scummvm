de31994dd0d46b870421afffbe1f5ea8700ec670
Walter Agazzi <walter.agazzi@protonmail.com>
2024-02-28 14:11:10 +0100

    AGS: Engine: safeguard Overlay.Create in case of non-existing sprite
    From upstream 57e21b06b99b53f38d0491e5cb17991738f552bd

--- engines/ags/engine/ac/overlay.cpp
+++ engines/ags/engine/ac/overlay.cpp
@@ -213,6 +213,11 @@ int Overlay_GetValid(ScriptOverlay *scover) {
 }
 
 ScreenOverlay *Overlay_CreateGraphicCore(bool room_layer, int x, int y, int slot, bool transparent, bool clone) {
+	if (!_GP(spriteset).DoesSpriteExist(slot)) {
+		debug_script_warn("Overlay.CreateGraphical: sprite %d is invalid", slot);
+		return nullptr;
+	}
+
 	data_to_game_coords(&x, &y);
 	size_t overid;
 	// We clone only dynamic sprites, because it makes no sense to clone normal ones
