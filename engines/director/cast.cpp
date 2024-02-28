0377f34e8ea47b8560f2a98fabc9c8b08d085b25
Scott Percival <code@moral.net.au>
2024-02-28 22:01:23 +0800

    DIRECTOR: Revert multiple filmloop support
    This reverts commit 21c08536e9ceed58ae678d1d455bb2fd842c5da0.
    
    Unfortunately most of the animations in Eastern Mind stopped working, we
    need a rethink of how we do this.

--- engines/director/cast.cpp
+++ engines/director/cast.cpp
@@ -121,12 +121,6 @@ CastMember *Cast::getCastMember(int castId, bool load) {
 		_loadMutex = false;
 		result->load();
 		while (!_loadQueue.empty()) {
-			// prevents double loading of a filmloop of filmloop
-			if (_loadQueue.back()->_type == kCastFilmLoop) {
-				CastMember *subfilmloop = _loadQueue.back();
-				_loadQueue.pop_back();
-				subfilmloop->load();
-			}
 			_loadQueue.back()->load();
 			_loadQueue.pop_back();
 		}
