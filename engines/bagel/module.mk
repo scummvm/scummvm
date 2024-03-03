5d298ae43ab891b57eeb09c226d865efd7e7ec9b
Eugene Sandulenko <sev@scummvm.org>
2024-03-03 21:29:49 +0100

    BAGEL: Added CBofSprite class

--- engines/bagel/module.mk
+++ engines/bagel/module.mk
@@ -53,6 +53,7 @@ MODULE_OBJS = \
 	boflib/gfx/bitmap.o \
 	boflib/gfx/cursor.o \
 	boflib/gfx/palette.o \
+	boflib/gfx/sprite.o \
 	boflib/gfx/text.o \
 	boflib/gui/display_button.o \
 	boflib/gui/display_object.o \
