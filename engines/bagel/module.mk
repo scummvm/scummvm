aa9e1f0a2c535d4efd44f5524e7afa5fcef1f474
Eugene Sandulenko <sev@scummvm.org>
2024-03-03 23:54:52 +0100

    BAGEL: Added CBofEditText class

--- engines/bagel/module.mk
+++ engines/bagel/module.mk
@@ -60,6 +60,7 @@ MODULE_OBJS = \
 	boflib/gui/display_button.o \
 	boflib/gui/display_object.o \
 	boflib/gui/display_window.o \
+	boflib/gui/edit_text.o \
 	boflib/gui/movie.o \
 	boflib/gui/window.o
 
