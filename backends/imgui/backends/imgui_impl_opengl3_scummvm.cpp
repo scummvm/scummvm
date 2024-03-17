96ce58ac9c80129fa1bcbdc5faabbe186aa27b19
Le Philousophe <lephilousophe@users.noreply.github.com>
2024-03-17 16:37:00 +0100

    BACKENDS: IMGUI: Use ScummVM facilities to get function pointers

--- backends/imgui/backends/imgui_impl_opengl3_scummvm.cpp
+++ backends/imgui/backends/imgui_impl_opengl3_scummvm.cpp
@@ -161,6 +161,7 @@
 // - You may need to regenerate imgui_impl_opengl3_loader.h to add new symbols. See https://github.com/dearimgui/gl3w_stripped
 // - You can temporarily use an unstripped version. See https://github.com/dearimgui/gl3w_stripped/releases
 // Changes to this backend using new APIs should be accompanied by a regenerated stripped loader version.
+#define IMGL3W_SCUMMVM_LOADER
 #define IMGL3W_IMPL
 #include "backends/imgui/backends/imgui_impl_opengl3_loader.h"
 #endif
