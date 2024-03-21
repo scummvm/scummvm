f53c57fed7cd65a56ba27cafc44a149997be53f3
Paul Gilbert <dreammaster@scummvm.org>
2024-03-20 18:52:25 -0700

    BAGEL: Implement the can save/load currently methods

--- engines/bagel/bagel.h
+++ engines/bagel/bagel.h
@@ -82,12 +82,8 @@ public:
 		    (f == kSupportsReturnToLauncher);
 	};
 
-	bool canLoadGameStateCurrently(Common::U32String *msg = nullptr) override {
-		return true;
-	}
-	bool canSaveGameStateCurrently(Common::U32String *msg = nullptr) override {
-		return true;
-	}
+	bool canLoadGameStateCurrently(Common::U32String *msg = nullptr) override;
+	bool canSaveGameStateCurrently(Common::U32String *msg = nullptr) override;
 
 	/**
 	 * Save a game state
