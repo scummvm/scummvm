c7c582cf87729f5b4b77daf793589b21d7041a74
SupSuper <supsuper@gmail.com>
2024-03-24 01:26:27 +0000

    BAGEL: Use cache for CalculateTextRect

--- engines/bagel/boflib/gfx/text.h
+++ engines/bagel/boflib/gfx/text.h
@@ -267,7 +267,6 @@ public:
 	 */
 	ERROR_CODE DisplayTextEx(CBofBitmap *pBmp, const CHAR *pszText, CBofRect *pRect, const INT nSize, const INT nWeight, const BOOL bShadowed, INT nFont = FONT_DEFAULT);
 
-	static Graphics::Font *GetMonoFont(INT nSize, INT nWeight);
 	static Graphics::Font *getFont(INT nFont, INT nSize, INT nWeight);
 
 private:
