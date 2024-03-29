e1abf6d691fa0a2831a9ea0a72be601b664c821d
Giovanni Cascione <ing.cascione@gmail.com>
2024-03-29 15:57:23 +0100

    LIBRETRO: check also AUDIO_STATUS_BUFFER_ACTIVE for auto frameskip

--- backends/platform/libretro/src/libretro-core.cpp
+++ backends/platform/libretro/src/libretro-core.cpp
@@ -1057,7 +1057,7 @@ void retro_run(void) {
 
 		/* Determine frameskip need based on settings */
 		if ((frameskip_type == 2) || (performance_switch & PERF_SWITCH_ON))
-			skip_frame = (audio_status & AUDIO_STATUS_BUFFER_UNDERRUN);
+			skip_frame = ((audio_status & (AUDIO_STATUS_BUFFER_UNDERRUN | AUDIO_STATUS_BUFFER_ACTIVE)) == (AUDIO_STATUS_BUFFER_UNDERRUN | AUDIO_STATUS_BUFFER_ACTIVE));
 		else if (frameskip_type == 1)
 			skip_frame = !(current_frame % frameskip_no == 0);
 		else if (frameskip_type == 3)
