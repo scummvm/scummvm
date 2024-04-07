ce0bba89307ebcc769c38c454f9bfa51943deb55
Benjamin Funke <bjnfne@web.de>
2024-04-07 14:43:36 +0200

    GOB: add pierresmagiques to tables
    It was back then added by Strangerke in 2009 was removed for some reason, since it it also an DEV7 game we can add it to tables with the flag ADGF_UNSUPPORTED like Adibou 3.
    
    commit: add4d124b29a6de01d69cc8f2a8d236deb969425

---
+++ engines/gob/detection/tables_pierresmagiques.h
@@ -0,0 +1,51 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * This file is dual-licensed.
+ * In addition to the GPLv3 license mentioned above, this code is also
+ * licensed under LGPL 2.1. See LICENSES/COPYING.LGPL file for the
+ * full text of the license.
+ *
+ */
+
+/* Detection tables for Le pays des Pierres Magiques / The Land of the Magic Stones */
+
+#ifndef GOB_DETECTION_TABLES_PIERRESMAGIQUES_H
+#define GOB_DETECTION_TABLES_PIERRESMAGIQUES_H
+
+// -- French: Le pays des Pierres Magiques --
+
+{
+	{
+
+		"pierresmagiques",
+		_s("Missing game code"), // Le pays des Pierres Magiques (Engine: DEV7 version 1.2.0.0)
+		AD_ENTRY2s("ed4.stk", "98721a7cfdc5a358d7ac56b7c6d3ba3d", 541882,
+				   "ed4cd.itk", "0627a91d9a6f4772c33747ce752024c2", 606993908),
+		FR_FRA,
+		kPlatformWindows,
+		ADGF_UNSUPPORTED,
+		GUIO0()
+	},
+	kFeatures640x480,
+	0,0,0
+},
+
+#endif // GOB_DETECTION_TABLES_PIERRESMAGIQUES_H
