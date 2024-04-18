38ad9fcbb822e71496b26ff6c27758723e2f6537
Paul Gilbert <dreammaster@scummvm.org>
2024-04-17 22:53:37 -0700

    BAGEL: Cleanup of palette.cpp

--- engines/bagel/boflib/gfx/palette.cpp
+++ engines/bagel/boflib/gfx/palette.cpp
@@ -93,7 +93,7 @@ void CBofPalette::SetPalette(const HPALETTE &hPalette) {
 ErrorCode CBofPalette::LoadPalette(const char *pszFileName, uint16 nFlags) {
 	Assert(IsValidObject(this));
 
-	// validate input
+	// Validate input
 	Assert(pszFileName != nullptr);
 
 	ReleasePalette();
@@ -124,68 +124,13 @@ void CBofPalette::ReleasePalette() {
 CBofPalette *CBofPalette::CopyPalette() {
 	Assert(IsValidObject(this));
 
-	CBofPalette *pBofPal;
-
-	pBofPal = nullptr;
-
-#if BOF_WINDOWS
-	HPALETTE hPal;
-
-	if ((hPal = CopyWindowsPalette(_palette)) != nullptr) {
-
-		if ((pBofPal = new CBofPalette(hPal)) != nullptr) {
-
-		} else {
-			LogError("Unable to allocate a new CBofPalette");
-		}
-
-	} else {
-		LogError("CopyWindowsPalette failed");
-	}
-#elif BOF_MAC
-
-#endif
-
-	return pBofPal;
+	return nullptr;
 }
 
 byte CBofPalette::GetNearestIndex(RGBCOLOR stRGB) {
 	Graphics::PaletteLookup lookup(_palette._data, PALETTE_COUNT);
 	return lookup.findBestColor(GetRed(stRGB), GetGreen(stRGB), GetBlue(stRGB));
 }
-
-#if BOF_WINDOWS
-/**
- * Creates a new palette based on specified palette
- * @param hPal      Palette to copy
- * @return          New palette
- */
-HPALETTE CopyWindowsPalette(HPALETTE hPal) {
-	LPLOGPALETTE ppal;
-	WORD nNumEntries = 0;
-
-	if (hPal != nullptr) {
-
-		GetObject(hPal, sizeof(WORD), (LPSTR)&nNumEntries);
-
-		if (nNumEntries == 0)
-			return nullptr;
-
-		if ((ppal = (LPLOGPALETTE)BofAlloc(sizeof(LOGPALETTE) + sizeof(PALETTEENTRY) * nNumEntries)) != nullptr) {
-
-			ppal->palVersion = 0x0300;
-			ppal->palNumEntries = (WORD)nNumEntries;
-
-			GetPaletteEntries(hPal, 0, nNumEntries, ppal->palPalEntry);
-
-			hPal = CreatePalette(ppal);
-			BofFree(ppal);
-		}
-	}
-
-	return hPal;
-}
-#endif
 
 RGBCOLOR CBofPalette::GetColor(byte nIndex) {
 	const byte *rgb = &_palette._data[nIndex * 3];
@@ -197,22 +142,7 @@ RGBCOLOR CBofPalette::GetColor(byte nIndex) {
 void CBofPalette::AnimateEntry(byte nIndex, RGBCOLOR cColor) {
 	Assert(IsValidObject(this));
 
-#if BOF_WINDOWS
-
-// Palette Animation is not supported in the cross compiler
-#if !BOF_WINMAC
-
-	PALETTEENTRY stColor;
-
-	stColor.peRed = GetRed(cColor);
-	stColor.peGreen = GetGreen(cColor);
-	stColor.peBlue = GetBlue(cColor);
-	stColor.peFlags = PC_RESERVED;
-
-	::AnimatePalette(_palette, nIndex, 1, &stColor);
-#endif
-
-#elif BOF_MAC
+#if BOF_MAC
 	RGBColor stColor;
 	stColor.red = (uint16)GetRed(cColor) << 8;
 	stColor.green = (uint16)GetGreen(cColor) << 8;
@@ -253,11 +183,8 @@ ErrorCode CBofPalette::SetSharedPalette(const char *pszFileName) {
 	}
 
 	// Save name of file used to get the shared palette
-	//
 	if (pszFileName != nullptr) {
 		Common::strcpy_s(m_szSharedPalFile, pszFileName);
-
-		// Don't load it yet
 	}
 
 	return ERR_NONE;
@@ -265,9 +192,7 @@ ErrorCode CBofPalette::SetSharedPalette(const char *pszFileName) {
 
 CBofPalette *CBofPalette::GetSharedPalette() {
 	// Do we need to load the shared palette?
-	//
 	if (m_pSharedPalette == nullptr) {
-
 		if (FileExists(m_szSharedPalFile)) {
 			m_pSharedPalette = new CBofPalette(m_szSharedPalFile);
 		}
