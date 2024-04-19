f9968886aaaaff24a8257b7e203883816631133c
Paul Gilbert <dreammaster@scummvm.org>
2024-04-18 18:36:53 -0700

    BAGEL: Add content for credits.pl

--- engines/bagel/credits.pl
+++ engines/bagel/credits.pl
@@ -1,3 +1,6 @@
 begin_section("Bagel");
-	add_person("Name 1", "Handle 1", "");
+	add_person("Arnaud Boutonn&eacute;", "Strangerke", "");
+	add_person("Daniel Albano", "SupSuper", "");
+	add_person("Eugene Sandulenko", "sev", "");
+	add_person("Paul Gilbert", "dreammaster", "");
 end_section();
