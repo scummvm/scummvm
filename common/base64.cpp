6b80c4c58128af5eb5471412afcd34fecf851dbf
Little Cat <toontownlittlecat@gmail.com>
2024-04-23 20:16:46 -0300

    COMMON: Add Base64 encoding/decoding

---
+++ common/base64.cpp
@@ -0,0 +1,168 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "common/base64.h"
+#include "common/array.h"
+#include "common/memstream.h"
+#include "common/str.h"
+#include "common/stream.h"
+#include "common/types.h"
+
+namespace Common {
+
+static const String encodingTable = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+String b64EncodeString(String &string) {
+	String out;
+	int32 val = 0, valb = -6;
+	for (uint8 c : string) {
+		val = (val << 8) + c;
+		valb += 8;
+		while (valb >= 0) {
+			out += (encodingTable[(val >> valb) & 0x3F]);
+			valb -= 6;
+		}
+	}
+	if (valb > -6)
+		out += (encodingTable[((val << 8) >> (valb + 8)) & 0x3F]);
+	while (out.size() % 4)
+		out += "=";
+	return out;
+}
+
+String b64EncodeStream(ReadStream &stream) {
+	String out;
+	int32 val = 0, valb = -6;
+	while (true) {
+		uint8 b = stream.readByte();
+		if (stream.eos())
+			break;
+		val = (val << 8) + b;
+		valb += 8;
+		while (valb >= 0) {
+			out += (encodingTable[(val >> valb) & 0x3F]);
+			valb -= 6;
+		}
+	}
+	if (valb > -6)
+		out += (encodingTable[((val << 8) >> (valb + 8)) & 0x3F]);
+	while (out.size() % 4)
+		out += "=";
+	return out;
+}
+
+String b64EncodeData(void *dataPtr, size_t dataSize) {
+	String out;
+	int32 val = 0, valb = -6;
+	uint8 *p = (uint8 *)dataPtr;
+
+	for (size_t i = 0; i < dataSize; i++) {
+		uint8 b = p[i];
+		val = (val << 8) + b;
+		valb += 8;
+		while (valb >= 0) {
+			out += (encodingTable[(val >> valb) & 0x3F]);
+			valb -= 6;
+		}
+	}
+	if (valb > -6)
+		out += (encodingTable[((val << 8) >> (valb + 8)) & 0x3F]);
+	while (out.size() % 4)
+		out += "=";
+	return out;
+}
+
+String b64DecodeString(String &string) {
+	String out;
+	Common::Array<int> T(256, -1);
+
+	// TODO: Verify if its a proper Base64 encoded string.
+
+	for (int i = 0; i < 64; i++) {
+		T[encodingTable[i]] = i;
+	}
+
+	int val = 0, valb = -8;
+	for (char c : string) {
+		if (T[c] == -1)
+			break;
+		val = (val << 6) + T[c];
+		valb += 6;
+		if (valb >= 0) {
+			out += (char((val >> valb) & 0xFF));
+			valb -= 8;
+		}
+	}
+
+	return out;
+}
+
+MemoryReadStream *b64DecodeStream(String &string, uint32 outputLength) {
+	char *out = (char *)malloc(outputLength);
+	Common::Array<int> T(256, -1);
+
+	// TODO: Verify if its a proper Base64 encoded string.
+
+	for (int i = 0; i < 64; i++) {
+		T[encodingTable[i]] = i;
+	}
+
+	int val = 0, valb = -8, valc = 0;
+	for (char c : string) {
+		if (T[c] == -1)
+			break;
+		val = (val << 6) + T[c];
+		valb += 6;
+		if (valb >= 0) {
+			out[valc++] = (char((val >> valb) & 0xFF));
+			valb -= 8;
+		}
+	}
+
+	return new MemoryReadStream((const byte *)out, outputLength, DisposeAfterUse::YES);
+}
+
+bool b64DecodeData(String &string, void *dataPtr) {
+	uint8 *p = (uint8 *)dataPtr;
+	Common::Array<int> T(256, -1);
+
+	// TODO: Verify if its a proper Base64 encoded string.
+
+	for (int i = 0; i < 64; i++) {
+		T[encodingTable[i]] = i;
+	}
+
+	int val = 0, valb = -8, valc = 0;
+	for (char c : string) {
+		if (T[c] == -1)
+			break;
+		val = (val << 6) + T[c];
+		valb += 6;
+		if (valb >= 0) {
+			p[valc++] = (char((val >> valb) & 0xFF));
+			valb -= 8;
+		}
+	}
+
+	return true;
+}
+
+} // End of namespace Common
