8253ff64709e5881104c7662740e88786ed8268b
Walter Agazzi <walter.agazzi@protonmail.com>
2024-05-01 22:38:35 +0200

    AGS: Engine: fixes to construct_object_gfx()
    from upstream fb240801322d30eb957fbb6a9e228928a7345553

--- engines/ags/engine/ac/draw.h
+++ engines/ags/engine/ac/draw.h
@@ -188,7 +188,7 @@ void draw_game_screen_callback();
 void GfxDriverOnInitCallback(void *data);
 bool GfxDriverSpriteEvtCallback(int evt, int data);
 void putpixel_compensate(Shared::Bitmap *g, int xx, int yy, int col);
-// Create the actsps[aa] image with the object drawn correctly.
+// Create the actsps[objid] image with the object drawn correctly.
 // Returns true if nothing at all has changed and actsps is still
 // intact from last time; false otherwise.
 // Hardware-accelerated do not require altering the raw bitmap itself,
@@ -197,7 +197,7 @@ void putpixel_compensate(Shared::Bitmap *g, int xx, int yy, int col);
 // effect changes (scaling, tint, etc).
 // * alwaysUseSoftware option forces HW renderers to  construct the image
 // in software mode as well.
-bool construct_object_gfx(int aa, int *drawnWidth, int *drawnHeight, bool alwaysUseSoftware);
+bool construct_object_gfx(int objid, bool alwaysUseSoftware);
 // Returns a cached character image prepared for the render
 Shared::Bitmap *get_cached_character_image(int charid);
 // Returns a cached object image prepared for the render
