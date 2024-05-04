cabbd1288bfbe9058d04d93519484f9923ea320a
Scott Percival <code@moral.net.au>
2024-05-04 17:52:11 +0800

    DIRECTOR: XOBJ: Add new UnitTest methods

--- engines/director/lingo/xlibs/unittest.h
+++ engines/director/lingo/xlibs/unittest.h
@@ -27,6 +27,7 @@ namespace Director {
 class UnitTestXObject : public Object<UnitTestXObject> {
 public:
 	UnitTestXObject(ObjectType objType);
+	Common::Point _mousePos;
 };
 
 namespace UnitTestXObj {
@@ -38,7 +39,12 @@ void open(ObjectType type, const Common::Path &path);
 void close(ObjectType type);
 
 void m_new(int nargs);
+void m_dispose(int nargs);
+void m_isRealDirector(int nargs);
 void m_screenshot(int nargs);
+void m_moveMouse(int nargs);
+void m_leftMouseDown(int nargs);
+void m_leftMouseUp(int nargs);
 
 } // End of namespace UnitTest
 
