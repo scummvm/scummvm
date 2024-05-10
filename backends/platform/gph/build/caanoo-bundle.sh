c59ac26f97138183bbf606e805bb54db9af8b8fb
Eugene Sandulenko <sev@scummvm.org>
2024-05-10 13:37:30 +0200

    BACKENDS: Remove obsolete gph/caanoo/gp2x/gp2xwiz port

--- backends/platform/gph/build/caanoo-bundle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo Quick script to make building a distribution of the GP2X Caanoo backend more consistent.
-
-cd ../../../..
-
-echo Building ScummVM for GP2X Caanoo.
-
-make caanoo-bundle
--- backends/platform/gph/build/gp2xwiz-bundle-debug.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo Quick script to make building a distribution of the GP2X Wiz backend more consistent.
-
-cd ../../../..
-
-echo Building ScummVM for GP2X Wiz.
-
-make gp2xwiz-bundle-debug
