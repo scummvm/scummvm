scummvm/scummvm

commit 829c600a02ad6124006ae8cc784a873f52140637

GUI: Remove remains of the old color lists in *ListWidget

gui/saveload-dialog.cpp
@@ -437,7 +437,7 @@ int SaveLoadChooserSimple::runIntern() {
 	return Dialog::runModal();
 }
 
-const Common::U32String &SaveLoadChooserSimple::getResultString() const {
+const Common::U32String SaveLoadChooserSimple::getResultString() const {
 	int selItem = _list->getSelected();
 	return (selItem >= 0) ? _list->getSelectedString() : _resultString;
 }
@@ -628,8 +628,7 @@ void SaveLoadChooserSimple::updateSelection(bool redraw) {
 		if (startEditMode) {
 			_list->startEditMode();
 
-			if (_chooseButton->isEnabled() && _list->getSelectedString() == _("Untitled saved game") &&
-					_list->getSelectionColor() == ThemeEngine::kFontColorAlternate) {
+			if (_chooseButton->isEnabled() && _list->getSelectedString() == _("Untitled saved game")) {
 				_list->setEditString(Common::U32String());
 				_list->setEditColor(ThemeEngine::kFontColorNormal);
 			}
@@ -691,7 +690,7 @@ void SaveLoadChooserSimple::updateSaveList() {
 	int curSlot = 0;
 	int saveSlot = 0;
 	Common::U32StringArray saveNames;
-	ListWidget::ColorList colors;
+	ThemeEngine::FontColor color = ThemeEngine::kFontColorNormal;
 	for (SaveStateList::const_iterator x = _saveList.begin(); x != _saveList.end(); ++x) {
 		// Handle gaps in the list of save games
 		saveSlot = x->getSaveSlot();
@@ -699,8 +698,7 @@ void SaveLoadChooserSimple::updateSaveList() {
 			while (curSlot < saveSlot) {
 				SaveStateDescriptor dummySave(_metaEngine, curSlot, "");
 				_saveList.insert_at(curSlot, dummySave);
-				saveNames.push_back(dummySave.getDescription());
-				colors.push_back(ThemeEngine::kFontColorNormal);
+				saveNames.push_back(GUI::ListWidget::getThemeColor(color) + dummySave.getDescription());
 				curSlot++;
 			}
 
@@ -717,12 +715,12 @@ void SaveLoadChooserSimple::updateSaveList() {
 		trimmedDescription.trim();
 		if (trimmedDescription.empty()) {
 			description = _("Untitled saved game");
-			colors.push_back(ThemeEngine::kFontColorAlternate);
+			color = ThemeEngine::kFontColorAlternate;
 		} else {
-			colors.push_back((x->getLocked() ? ThemeEngine::kFontColorAlternate : ThemeEngine::kFontColorNormal));
+			color = x->getLocked() ? ThemeEngine::kFontColorAlternate : ThemeEngine::kFontColorNormal;
 		}
 
-		saveNames.push_back(description);
+		saveNames.push_back(GUI::ListWidget::getThemeColor(color) + description);
 		curSlot++;
 	}
 
@@ -738,16 +736,16 @@ void SaveLoadChooserSimple::updateSaveList() {
 	}
 #endif
 
-	Common::String emptyDesc;
+	Common::U32String emptyDesc;
+	color = ThemeEngine::kFontColorNormal;
 	for (int i = curSlot; i <= maximumSaveSlots; i++) {
 		saveNames.push_back(emptyDesc);
 		SaveStateDescriptor dummySave(_metaEngine, i, "");
 		_saveList.push_back(dummySave);
-		colors.push_back(ThemeEngine::kFontColorNormal);
 	}
 
 	int selected = _list->getSelected();
-	_list->setList(saveNames, &colors);
+	_list->setList(saveNames);
 	if (selected >= 0 && selected < (int)saveNames.size())
 		_list->setSelected(selected);
 	else
@@ -798,7 +796,7 @@ SaveLoadChooserGrid::~SaveLoadChooserGrid() {
 	delete _pageDisplay;
 }
 
-const Common::U32String &SaveLoadChooserGrid::getResultString() const {
+const Common::U32String SaveLoadChooserGrid::getResultString() const {
 	return _resultString;
 }
 

engines/testbed/config.cpp
@@ -38,25 +38,23 @@ TestbedOptionsDialog::TestbedOptionsDialog(Common::Array<Testsuite *> &tsList, T
 
 	// Construct a String Array
 	Common::Array<Testsuite *>::const_iterator iter;
-	Common::String description;
+	Common::U32String description;
 	uint selected = 0;
 
 	for (iter = tsList.begin(); iter != tsList.end(); iter++) {
 		_testSuiteArray.push_back(*iter);
 		description = (*iter)->getDescription();
 		if ((*iter)->isEnabled()) {
-			_testSuiteDescArray.push_back(description + "(selected)");
+			_testSuiteDescArray.push_back(GUI::ListWidget::getThemeColor(GUI::ThemeEngine::kFontColorNormal) + description + "(selected)");
 			selected++;
-			_colors.push_back(GUI::ThemeEngine::kFontColorNormal);
 		} else {
-			_testSuiteDescArray.push_back(description);
-			_colors.push_back(GUI::ThemeEngine::kFontColorAlternate);
+			_testSuiteDescArray.push_back(GUI::ListWidget::getThemeColor(GUI::ThemeEngine::kFontColorAlternate) + description);
 		}
 	}
 
 	_testListDisplay = new TestbedListWidget(this, "TestbedOptions.List", _testSuiteArray);
 	_testListDisplay->setNumberingMode(GUI::kListNumberingOff);
-	_testListDisplay->setList(_testSuiteDescArray, &_colors);
+	_testListDisplay->setList(_testSuiteDescArray);
 
 	// This list shouldn't be editable
 	_testListDisplay->setEditable(false);
@@ -157,13 +155,13 @@ void TestbedInteractionDialog::addButton(uint w, uint h, const Common::String na
 	_yOffset += h;
 }
 
-void TestbedInteractionDialog::addList(uint x, uint y, uint w, uint h, const Common::Array<Common::U32String> &strArray, GUI::ListWidget::ColorList *colors, uint yPadding) {
+void TestbedInteractionDialog::addList(uint x, uint y, uint w, uint h, const Common::Array<Common::U32String> &strArray, uint yPadding) {
 	_yOffset += yPadding;
 	GUI::ListWidget *list = new GUI::ListWidget(this, x, y, w, h);
 	list->resize(x, y, w, h);
 	list->setEditable(false);
 	list->setNumberingMode(GUI::kListNumberingOff);
-	list->setList(strArray, colors);
+	list->setList(strArray);
 	_yOffset += h;
 }
 

engines/testbed/config.h
@@ -72,23 +72,16 @@ public:
 
 	void markAsSelected(int i) {
 		if (!_list[i].encode().contains("selected")) {
-			_list[i] += Common::U32String(" (selected)");
+			_list[i] = GUI::ListWidget::getThemeColor(GUI::ThemeEngine::kFontColorNormal) + _testSuiteArray[i]->getDescription() + " (selected)";
 		}
-		_listColors[i] = GUI::ThemeEngine::kFontColorNormal;
 		draw();
 	}
 
 	void markAsDeselected(int i) {
 		if (_list[i].encode().contains("selected")) {
-			_list[i] = _testSuiteArray[i]->getDescription();
+			_list[i] = GUI::ListWidget::getThemeColor(GUI::ThemeEngine::kFontColorAlternate) +
+					_testSuiteArray[i]->getDescription();
 		}
-		_listColors[i] = GUI::ThemeEngine::kFontColorAlternate;
-		draw();
-	}
-
-	void setColor(uint32 indx, GUI::ThemeEngine::FontColor color) {
-		assert(indx < _listColors.size());
-		_listColors[indx] = color;
 		draw();
 	}
 
@@ -103,7 +96,6 @@ public:
 	void handleCommand(GUI::CommandSender *sender, uint32 cmd, uint32 data) override;
 
 private:
-	GUI::ListWidget::ColorList _colors;
 	GUI::ButtonWidget	*_selectButton;
 	Common::Array<Testsuite *> _testSuiteArray;
 	Common::U32StringArray _testSuiteDescArray;
@@ -119,7 +111,7 @@ public:
 	void addButton(uint w, uint h, const Common::String name, uint32 cmd, uint xOffset = 0, uint yPadding = 8);
 	void addButtonXY(uint x, uint y, uint w, uint h, const Common::String name, uint32 cmd);
 	void addText(uint w, uint h, const Common::String text, Graphics::TextAlign textAlign, uint xOffset, uint yPadding = 8);
-	void addList(uint x, uint y, uint w, uint h, const Common::Array<Common::U32String> &strArray, GUI::ListWidget::ColorList *colors = 0, uint yPadding = 8);
+	void addList(uint x, uint y, uint w, uint h, const Common::Array<Common::U32String> &strArray, uint yPadding = 8);
 protected:
 	Common::Array<GUI::ButtonWidget *> _buttonArray;
 	uint _xOffset;

engines/testbed/testbed.cpp
@@ -59,20 +59,21 @@ void TestbedExitDialog::init() {
 	Common::String text = "Thank you for using ScummVM testbed! Here are yor summarized results:";
 	addText(450, 20, text, Graphics::kTextAlignCenter, _xOffset, 15);
 	Common::Array<Common::U32String> strArray;
-	GUI::ListWidget::ColorList colors;
+	Common::U32String color;
 
 	for (Common::Array<Testsuite *>::const_iterator i = _testsuiteList.begin(); i != _testsuiteList.end(); ++i) {
-		strArray.push_back(Common::String::format("%s :", (*i)->getDescription()));
-		colors.push_back(GUI::ThemeEngine::kFontColorNormal);
+		color = GUI::ListWidget::getThemeColor(GUI::ThemeEngine::kFontColorNormal);
+		strArray.push_back(color + Common::U32String::format("%s :", (*i)->getDescription()));
+
+		color = GUI::ListWidget::getThemeColor(GUI::ThemeEngine::kFontColorAlternate);
 		if ((*i)->isEnabled()) {
-			strArray.push_back(Common::String::format("Passed: %d  Failed: %d Skipped: %d", (*i)->getNumTestsPassed(), (*i)->getNumTestsFailed(), (*i)->getNumTestsSkipped()));
+			strArray.push_back(color + Common::U32String::format("Passed: %d  Failed: %d Skipped: %d", (*i)->getNumTestsPassed(), (*i)->getNumTestsFailed(), (*i)->getNumTestsSkipped()));
 		} else {
-			strArray.push_back(Common::U32String("Skipped"));
+			strArray.push_back(color + Common::U32String("Skipped"));
 		}
-		colors.push_back(GUI::ThemeEngine::kFontColorAlternate);
 	}
 
-	addList(0, _yOffset, 500, 200, strArray, &colors);
+	addList(0, _yOffset, 500, 200, strArray);
 	text = "More Details can be viewed in the Log file : " + ConfParams.getLogFilename();
 	addText(450, 20, text, Graphics::kTextAlignLeft, 0, 0);
 	if (ConfParams.getLogDirectory().size()) {

gui/ThemeEngine.h
@@ -279,6 +279,7 @@ public:
 
 	/// Font color selector
 	enum FontColor {
+		kFontColorFormatting = -1,	///< Use color from formatting
 		kFontColorNormal = 0,       ///< The default color of the theme
 		kFontColorAlternate = 1,    ///< Alternative font color
 		kFontColorMax

gui/browser.cpp
@@ -232,25 +232,22 @@ void BrowserDialog::updateListing() {
 
 	// Populate the ListWidget
 	Common::U32StringArray list;
-	ListWidget::ColorList colors;
+	Common::U32String color = ListWidget::getThemeColor(ThemeEngine::kFontColorNormal);
 	for (Common::FSList::iterator i = _nodeContent.begin(); i != _nodeContent.end(); ++i) {
-		if (i->isDirectory())
-			list.push_back(i->getName() + "/");
-		else
-			list.push_back(i->getName());
-
 		if (_isDirBrowser) {
 			if (i->isDirectory())
-				colors.push_back(ThemeEngine::kFontColorNormal);
+				color = ListWidget::getThemeColor(ThemeEngine::kFontColorNormal);
 			else
-				colors.push_back(ThemeEngine::kFontColorAlternate);
+				color = ListWidget::getThemeColor(ThemeEngine::kFontColorAlternate);
 		}
+
+		if (i->isDirectory())
+			list.push_back(color + Common::U32String(i->getName() + "/"));
+		else
+			list.push_back(color + Common::U32String(i->getName()));
 	}
 
-	if (_isDirBrowser)
-		_fileList->setList(list, &colors);
-	else
-		_fileList->setList(list);
+	_fileList->setList(list);
 	_fileList->scrollTo(0);
 
 	// Finally, redraw

gui/launcher.cpp
@@ -1009,7 +1009,6 @@ void LauncherSimple::build() {
 void LauncherSimple::updateListing() {
 	Common::U32StringArray l;
 	Common::Array<const Common::ConfigManager::Domain *> attrs;
-	ListWidget::ColorList colors;
 	ThemeEngine::FontColor color;
 	int numEntries = ConfMan.getInt("gui_list_max_scan_entries");
 
@@ -1067,13 +1066,12 @@ void LauncherSimple::updateListing() {
 		Common::U32String gameDesc = GUI::ListWidget::getThemeColor(color) + Common::U32String(iter->description);
 
 		l.push_back(gameDesc);
-		colors.push_back(color);
 		attrs.push_back(iter->domain);
 		_domains.push_back(iter->key);
 	}
 
 	const int oldSel = _list->getSelected();
-	_list->setList(l, &colors);
+	_list->setList(l);
 
 	groupEntries(attrs);
 

gui/remotebrowser.cpp
@@ -144,18 +144,15 @@ void RemoteBrowserDialog::updateListing() {
 	if (!_navigationLocked) {
 		// Populate the ListWidget
 		Common::U32StringArray list;
-		ListWidget::ColorList colors;
 		for (Common::Array<Cloud::StorageFile>::iterator i = _nodeContent.begin(); i != _nodeContent.end(); ++i) {
 			if (i->isDirectory()) {
-				list.push_back(i->name() + "/");
-				colors.push_back(ThemeEngine::kFontColorNormal);
+				list.push_back(ListWidget::getThemeColor(ThemeEngine::kFontColorNormal) + Common::U32String(i->name() + "/"));
 			} else {
-				list.push_back(i->name());
-				colors.push_back(ThemeEngine::kFontColorAlternate);
+				list.push_back(ListWidget::getThemeColor(ThemeEngine::kFontColorAlternate) + Common::U32String(i->name()));
 			}
 		}
 
-		_fileList->setList(list, &colors);
+		_fileList->setList(list);
 		_fileList->scrollTo(0);
 	}
 

commit 0d7fddba4e1b7c3d5c5f2771ebfde507fb2d22f9

GUI: Fix saveload display of selected empty items
Fixes minor bug introduced in 829c600a02ad6124006ae8cc784a873f52140637

In the saveload list dialog, selecting an empty slot before the last
save stopped coloring most of the row. Empty slots after the last save
continued to display correctly. Now both code blocks are consistent and
the selected row is always fully colored.

gui/saveload-dialog.cpp
@@ -740,6 +740,7 @@ void SaveLoadChooserSimple::updateSaveList() {
 	int saveSlot = 0;
 	Common::U32StringArray saveNames;
 	ThemeEngine::FontColor color = ThemeEngine::kFontColorNormal;
+	Common::U32String emptyDesc;
 	for (SaveStateList::const_iterator x = _saveList.begin(); x != _saveList.end(); ++x) {
 		// Handle gaps in the list of save games
 		saveSlot = x->getSaveSlot();
@@ -747,7 +748,7 @@ void SaveLoadChooserSimple::updateSaveList() {
 			while (curSlot < saveSlot) {
 				SaveStateDescriptor dummySave(_metaEngine, curSlot, "");
 				_saveList.insert_at(curSlot, dummySave);
-				saveNames.push_back(GUI::ListWidget::getThemeColor(color) + dummySave.getDescription());
+				saveNames.push_back(emptyDesc);
 				curSlot++;
 			}
 
@@ -785,8 +786,6 @@ void SaveLoadChooserSimple::updateSaveList() {
 	}
 #endif
 
-	Common::U32String emptyDesc;
-	color = ThemeEngine::kFontColorNormal;
 	for (int i = curSlot; i <= maximumSaveSlots; i++) {
 		saveNames.push_back(emptyDesc);
 		SaveStateDescriptor dummySave(_metaEngine, i, "");