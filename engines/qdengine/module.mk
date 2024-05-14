10ec8fb766f6b5dba6272a149973a8cab51e72f9
kunxl-gg <tiwari.25@iitj.ac.in>
2024-05-14 10:07:53 +0530

    QDENGINE: Add files to module.mk

--- engines/qdengine/module.mk
+++ engines/qdengine/module.mk
@@ -27,14 +27,52 @@ MODULE_OBJS = \
 	core/system/input/input_wndproc.o \
 	core/system/input/input_recorder.o \
 	core/qdcore/qd_animation_frame.o \
-    core/qdcore/qd_animation_info.o \
+	core/qdcore/qd_animation_info.o \
 	core/qdcore/qd_animation_maker.o \
-    core/qdcore/qd_animation_set_info.o \
-    core/qdcore/qd_animation_set_preview.o \
-    core/qdcore/qd_animation_set.o \
+	core/qdcore/qd_animation_set_info.o \
+	core/qdcore/qd_animation_set_preview.o \
+	core/qdcore/qd_animation_set.o \
 	core/qdcore/qd_animation.o \
 	core/qdcore/qd_camera_mode.o \
 	core/qdcore/qd_camera.o \
+	core/qdcore/qd_condition_data.o \
+	core/qdcore/qd_condition_group.o \
+	core/qdcore/qd_condition_object_reference.o \
+	core/qdcore/qd_condition.o \
+	core/qdcore/qd_conditional_object.o \
+	core/qdcore/qd_contour.o \
+	core/qdcore/qd_coords_animation_point.o \
+	core/qdcore/qd_coords_animation.o \
+	core/qdcore/qd_counter.o \
+	core/qdcore/qd_d3dutils.o \
+	core/qdcore/qd_file_owner.o \
+	core/qdcore/qd_game_dispatcher_base.o \
+	core/qdcore/qd_game_end.o \
+	core/qdcore/qd_game_object_mouse.o \
+	core/qdcore/qd_game_object_static.o \
+	core/qdcore/qd_game_object.o \
+	core/qdcore/qd_grid_zone_state.o \
+	core/qdcore/qd_grid_zone.o \
+	core/qdcore/qd_interface_background.o \
+	core/qdcore/qd_interface_button.o \
+	core/qdcore/qd_interface_counter.o \
+	core/qdcore/qd_interface_object_base.o \
+	core/qdcore/qd_interface_screen.o \
+	core/qdcore/qd_interface_slider.o \
+	core/qdcore/qd_inventory_cell.o \
+	core/qdcore/qd_inventory.o \
+	core/qdcore/qd_music_track.o \
+	core/qdcore/qd_named_object_base.o \
+	core/qdcore/qd_named_object_indexer.o \
+	core/qdcore/qd_named_object.o \
+	core/qdcore/qd_resource_file.o \
+	core/qdcore/qd_rnd.o \
+	core/qdcore/qd_scale_info.o \
+	core/qdcore/qd_screen_text_dispatcher.o \
+	core/qdcore/qd_sound_info.o \
+	core/qdcore/qd_trigger_chain.o \
+	core/qdcore/qd_trigger_element.o \
+	core/qdcore/qd_trigger_profiler.o \
 
 # This module can be built as a plugin
 ifeq ($(ENABLE_QDENGINE), DYNAMIC_PLUGIN)
