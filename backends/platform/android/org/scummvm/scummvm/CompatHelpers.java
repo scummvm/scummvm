c84b519fa7bb67b125d06d8668eb7e4c1ffdb184
Le Philousophe <lephilousophe@users.noreply.github.com>
2024-05-16 08:20:16 +0200

    ANDROID: Add compatibility shims to handle shortcuts and drawables

--- backends/platform/android/org/scummvm/scummvm/CompatHelpers.java
+++ backends/platform/android/org/scummvm/scummvm/CompatHelpers.java
@@ -1,5 +1,16 @@
 package org.scummvm.scummvm;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ShortcutInfo;
+import android.content.pm.ShortcutManager;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.Icon;
 import android.media.AudioAttributes;
 import android.media.AudioFormat;
 import android.media.AudioManager;
@@ -10,7 +21,12 @@ import android.view.WindowInsets;
 import android.view.WindowInsetsController;
 import android.view.accessibility.AccessibilityEvent;
 
+import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+
+import java.util.Objects;
 
 class CompatHelpers {
 	static class HideSystemStatusBar {
@@ -146,7 +162,6 @@ class CompatHelpers {
 			} else {
 				return AccessibilityEventConstructorOld.make(eventType);
 			}
-
 		}
 
 		@SuppressWarnings("deprecation")
@@ -162,7 +177,103 @@ class CompatHelpers {
 				return new AccessibilityEvent(eventType);
 			}
 		}
+	}
 
+	static class ShortcutCreator {
+		public static Intent createShortcutResultIntent(@NonNull Context context, String id, @NonNull Intent intent, @NonNull String label, @Nullable Drawable icon, @DrawableRes int fallbackIconId) {
+			if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+				return ShortcutCreatorO.createShortcutResultIntent(context, id, intent, label, icon, fallbackIconId);
+			} else {
+				return ShortcutCreatorOld.createShortcutResultIntent(context, id, intent, label, icon, fallbackIconId);
+			}
+		}
 
+		@SuppressWarnings("deprecation")
+		private static class ShortcutCreatorOld {
+			public static Intent createShortcutResultIntent(@NonNull Context context, String ignoredId, @NonNull Intent intent, @NonNull String label, @Nullable Drawable icon, @DrawableRes int fallbackIconId) {
+				Intent result = new Intent();
+
+				if (icon == null) {
+					icon = DrawableCompat.getDrawable(context, fallbackIconId);
+				}
+				Objects.requireNonNull(icon);
+				Bitmap bmIcon = drawableToBitmap(icon);
+
+				addToIntent(result, intent, label, bmIcon);
+				return result;
+			}
+
+			public static void addToIntent(Intent result, @NonNull Intent intent, @NonNull String label, @NonNull Bitmap icon) {
+				result.putExtra(Intent.EXTRA_SHORTCUT_INTENT, intent);
+				result.putExtra(Intent.EXTRA_SHORTCUT_NAME, label);
+				result.putExtra(Intent.EXTRA_SHORTCUT_ICON, icon);
+			}
+		}
+
+		@RequiresApi(android.os.Build.VERSION_CODES.O)
+		private static class ShortcutCreatorO {
+			public static Intent createShortcutResultIntent(Context context, String id, @NonNull Intent intent, @NonNull String label, @Nullable Drawable icon, @DrawableRes int fallbackIconId) {
+				ShortcutManager shortcutManager = context.getSystemService(ShortcutManager.class);
+				ShortcutInfo.Builder builder = new ShortcutInfo.Builder(context, id);
+				builder.setIntent(intent);
+				builder.setShortLabel(label);
+				Bitmap bm;
+				if (icon != null) {
+					bm = drawableToBitmap(icon);
+					builder.setIcon(Icon.createWithBitmap(bm));
+				} else {
+					icon = DrawableCompat.getDrawable(context, fallbackIconId);
+					Objects.requireNonNull(icon);
+					bm = drawableToBitmap(icon);
+					builder.setIcon(Icon.createWithResource(context, fallbackIconId));
+				}
+				Intent result = shortcutManager.createShortcutResultIntent(builder.build());
+				ShortcutCreatorOld.addToIntent(result, intent, label, bm);
+				return result;
+			}
+		}
+
+		private static Bitmap drawableToBitmap(@NonNull Drawable drawable) {
+			// We resize to 128x128 to avoid having too big bitmaps for Binder
+			if (drawable instanceof BitmapDrawable) {
+				Bitmap bm = ((BitmapDrawable)drawable).getBitmap();
+				bm = Bitmap.createScaledBitmap(bm, 128, 128, true);
+				return bm.copy(bm.getConfig(), false);
+			}
+
+			Bitmap bitmap = Bitmap.createBitmap(128, 128, Bitmap.Config.ARGB_8888);
+			Canvas canvas = new Canvas(bitmap);
+			drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+			drawable.draw(canvas);
+
+			// Create an immutable bitmap
+			return bitmap.copy(bitmap.getConfig(), false);
+		}
+	}
+
+	static class DrawableCompat {
+		public static Drawable getDrawable(@NonNull Context context, @DrawableRes int id) throws Resources.NotFoundException {
+			if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+				return DrawableCompatLollipop.getDrawable(context, id);
+			} else {
+				return DrawableCompatOld.getDrawable(context, id);
+			}
+		}
+
+		@RequiresApi(android.os.Build.VERSION_CODES.LOLLIPOP)
+		private static class DrawableCompatLollipop {
+			@SuppressLint("UseCompatLoadingForDrawables")
+			public static Drawable getDrawable(@NonNull Context context, @DrawableRes int id) throws Resources.NotFoundException {
+				return context.getDrawable(id);
+			}
+		}
+
+		@SuppressWarnings("deprecation")
+		private static class DrawableCompatOld {
+			@SuppressLint("UseCompatLoadingForDrawables")
+			public static Drawable getDrawable(@NonNull Context context, @DrawableRes int id) throws Resources.NotFoundException {
+				return context.getResources().getDrawable(id);
+			}
+		}
 	}
 }
