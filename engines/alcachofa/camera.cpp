15915b540732e8b0a18b7442f1b82eab18a0ace0
Helco <hermann.noll@hotmail.com>
2024-05-20 15:34:43 +0200

    ALCACHOFA: Add camera and 3D draw requests

---
+++ engines/alcachofa/camera.cpp
@@ -0,0 +1,132 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "camera.h"
+#include "alcachofa.h"
+
+#include "common/system.h"
+#include "math/vector4d.h"
+
+using namespace Common;
+using namespace Math;
+
+namespace Alcachofa {
+
+void Camera::setRoomBounds(Rect bgBounds, float bgScale) {
+	float scaleFactor = 1 - bgScale * kInvBaseScale;
+	_roomMin = Vector2d(
+		g_system->getWidth() / 2 * scaleFactor,
+		g_system->getHeight() / 2 * scaleFactor);
+	_roomMax = _roomMin + Vector2d(
+		bgBounds.width() * bgScale * kInvBaseScale,
+		bgBounds.height() * bgScale * kInvBaseScale);
+}
+
+static Matrix4 scaleMatrix(float scale) {
+	Matrix4 m;
+	m(0, 0) = scale;
+	m(1, 1) = scale;
+	m(2, 2) = scale;
+	return m;
+}
+
+void Camera::setupMatricesAround(Vector3d center) {
+	Matrix4 matTemp;
+	matTemp.buildAroundZ(_rotation);
+	_mat3Dto2D.setToIdentity();
+	_mat3Dto2D.translate(-center);
+	_mat3Dto2D = matTemp * _mat3Dto2D;
+	_mat3Dto2D = _mat3Dto2D * scaleMatrix(_scale);
+
+	_mat2Dto3D.setToIdentity();
+	_mat2Dto3D.translate(center);
+	matTemp.buildAroundZ(-_rotation);
+	matTemp = scaleMatrix(1 / _scale) * matTemp;
+	_mat2Dto3D = matTemp * _mat2Dto3D;
+}
+
+void minmax(Vector3d &min, Vector3d &max, Vector3d val)
+{
+	min.set(
+		MIN(min.x(), val.x()),
+		MIN(min.y(), val.y()),
+		MIN(min.z(), val.z()));
+	max.set(
+		MAX(max.x(), val.x()),
+		MAX(max.y(), val.y()),
+		MAX(max.z(), val.z()));
+}
+
+Vector3d Camera::setAppliedCenter(Vector3d center) {
+	setupMatricesAround(center);
+	if (true) { // g_engine->script().getVariable("EncuadrarCamara")
+		const float screenW = g_system->getWidth(), screenH = g_system->getHeight();
+		Vector3d min, max;
+		min = max = transform2Dto3D(Vector3d(0, 0, _roomScale));
+		minmax(min, max, transform2Dto3D(Vector3d(screenW, 0, _roomScale)));
+		minmax(min, max, transform2Dto3D(Vector3d(screenW, screenH, _roomScale)));
+		minmax(min, max, transform2Dto3D(Vector3d(0, screenH, _roomScale)));
+		center.x() += MAX(0.0f, _roomMin.getX() - min.x());
+		center.y() += MAX(0.0f, _roomMin.getY() - min.y());
+		center.x() -= MAX(0.0f, max.x() - _roomMax.getX());
+		center.y() -= MAX(0.0f, max.y() - _roomMax.getY());
+		setupMatricesAround(center);
+	}
+	return _appliedCenter = center;
+}
+
+Vector3d Camera::transform2Dto3D(Vector3d v3d) const {
+	// if this looks like normal 3D math to *someone* please contact.
+	Vector4d vh;
+	vh.w() = 1.0f;
+	vh.z() = v3d.z() - _usedCenter.z();
+	vh.y() = (v3d.y() - g_system->getHeight() * 0.5f) * vh.z() * kInvBaseScale;
+	vh.x() = (v3d.x() - g_system->getWidth() * 0.5f) * vh.z() * kInvBaseScale;
+	vh = _mat2Dto3D * vh;
+	return Vector3d(vh.x(), vh.y(), 0.0f);
+}
+
+Vector3d Camera::transform3Dto2D(Vector3d v2d) const {
+	// I swear there is a better way than this. This is stupid. But it is original.
+	float depthScale = v2d.z() * kInvBaseScale;
+	Vector4d vh;
+	vh.x() = v2d.x() * depthScale + (1 - depthScale) * g_system->getWidth() * 0.5f;
+	vh.y() = v2d.y() * depthScale + (1 - depthScale) * g_system->getHeight() * 0.5f;
+	vh.z() = v2d.z();
+	vh.w() = 1.0f;
+	vh = _mat3Dto2D * vh;
+	return Vector3d(
+		g_system->getWidth() * 0.5f + vh.x() * kBaseScale / vh.z(),
+		g_system->getHeight() * 0.5f + vh.y() * kBaseScale / vh.z(),
+		_scale * kBaseScale / vh.z());
+}
+
+void Camera::update() {
+	// original would be some smoothing of delta times, let's not.
+	uint32 now = g_system->getMillis();
+	float deltaTime = now - _lastUpdateTime;
+	deltaTime = MAX(0.001f, MIN(0.5f, deltaTime));
+	_lastUpdateTime = now;
+
+	setAppliedCenter(_usedCenter + Vector3d(_shake.getX(), _shake.getY(), 0.0f));
+}
+
+}
