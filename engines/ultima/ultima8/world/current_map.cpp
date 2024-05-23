003605e3ea65391158fccf0fac13b6022921b997
Matthew Duggan <mgithub@guarana.org>
2024-05-23 20:30:01 +1000

    ULTIMA8: Don't require land support in Crusader.
    Not everything that supports us in Crusader is a land, so don't require it when
    calculating support.  This makes movement in No Regret a lot smoother and is
    probably better than the hack I tried in d4e2ebe9.

--- engines/ultima/ultima8/world/current_map.cpp
+++ engines/ultima/ultima8/world/current_map.cpp
@@ -798,10 +798,10 @@ PositionInfo CurrentMap::getPositionInfo(const Box &target, const Box &start, ui
 	}
 
 	info.valid = info.blocker == nullptr;
-	// Partial support allowed if land is close. Allow up to 9 to match
-	// the position adjustments in scanForValidPosition for stepping on
-	// to Crusader elevators.
-	if (supportz == target._z && landz + 9 >= target._z)
+	// Partial support allowed if land is close. Allow up to 8 to match the
+	// position adjustments in scanForValidPosition.  In Crusader, we don't
+	// require land - just support.
+	if (supportz == target._z && (landz + 8 >= target._z || GAME_IS_CRUSADER))
 		info.supported = true;
 
 	// Mark supported at minimum z
