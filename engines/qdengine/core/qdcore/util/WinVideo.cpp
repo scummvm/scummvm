af7cd81aa6c9a6aa853153a8ce1750496f66126f
kunxl-gg <tiwari.25@iitj.ac.in>
2024-05-29 18:56:08 +0530

    QDENGINE: Add remaining files to the project
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

--- engines/qdengine/core/qdcore/util/WinVideo.cpp
+++ engines/qdengine/core/qdcore/util/WinVideo.cpp
@@ -54,25 +54,31 @@ bool winVideo::done() {
 }
 
 bool winVideo::set_volume(int volume_db) {
+	warning("STUB: winVideo::set_volume");
+#if 0
 	if (basic_audio_) {
-		basic_audio_ -> put_Volume(volume_db);
+		basic_audio_->put_Volume(volume_db);
 		return true;
 	}
-
+#endif
 	return false;
 }
 
 void winVideo::set_window(void *hwnd, int x, int y, int xsize, int ysize) {
 	winVideo::hwnd_ = hwnd;
 	if (video_window_ && hwnd) { // Set the video window
-		video_window_ -> put_Owner((OAHWND)hwnd);
-		video_window_ -> put_WindowStyle(WS_CHILD | WS_CLIPSIBLINGS);
-//		video_window_ -> put_WindowStyleEx(WS_EX_TOPMOST);
+	warning("STUB: winVideo::set_window");
+#if 0
+		video_window_->put_Owner((OAHWND)hwnd);
+		video_window_->put_WindowStyle(WS_CHILD | WS_CLIPSIBLINGS);
+//		video_window_->put_WindowStyleEx(WS_EX_TOPMOST);
+#endif
 
 		RECT grc = { x, y, xsize, ysize };
 		if (!grc.right || !grc.bottom) GetClientRect((HWND)hwnd, &grc);
-
-		video_window_ -> SetWindowPosition(grc.left, grc.top, grc.right, grc.bottom);
+#if 0
+		video_window_->SetWindowPosition(grc.left, grc.top, grc.right, grc.bottom);
+#endif
 		toggle_cursor(false);
 	}
 }
@@ -87,10 +93,12 @@ bool winVideo::open_file(const char *fname) {
 	}
 
 #ifdef __WINVIDEO_LOG__
-	graph_builder_ -> SetLogFile((DWORD)log_file_handle_);
+	graph_builder_->SetLogFile((DWORD)log_file_handle_);
 #endif
 
-	graph_builder_ -> QueryInterface(IID_IMediaControl, (void **)&media_control_);
+	warning("STUB: winVideo::open_file");
+#if 0
+	graph_builder_->QueryInterface(IID_IMediaControl, (void **)&media_control_);
 	if (!media_control_) {
 		close_file();
 		return false;
@@ -119,14 +127,17 @@ bool winVideo::open_file(const char *fname) {
 		close_file();
 		return false;
 	}
+#endif
 
-	WCHAR wPath[MAX_PATH];
+	wchar_t wPath[MAX_PATH];
 	MultiByteToWideChar(CP_ACP, 0, fname, -1, wPath, MAX_PATH);
 
-	if (graph_builder_ -> RenderFile(wPath, NULL)) {
+#if 0
+	if (graph_builder_->RenderFile(wPath, NULL)) {
 		close_file();
 		return false;
 	}
+#endif
 	if (hwnd_) set_window(hwnd_);
 
 	return true;
@@ -134,11 +145,13 @@ bool winVideo::open_file(const char *fname) {
 
 bool winVideo::play() {
 	if (graph_builder_ && video_window_ && media_control_ && hwnd_) {
-		video_window_ -> put_AutoShow(-1);
-		video_window_ -> put_Visible(-1);
-		video_window_ -> SetWindowForeground(-1);
-		media_control_ -> Run();
-
+		warning("STUB: winVideo::play");
+#if 0
+		video_window_->put_AutoShow(-1);
+		video_window_->put_Visible(-1);
+		video_window_->SetWindowForeground(-1);
+		media_control_->Run();
+#endif
 		return true;
 	}
 	return false;
@@ -146,7 +159,10 @@ bool winVideo::play() {
 
 bool winVideo::stop() {
 	if (graph_builder_ && video_window_ && media_control_ && hwnd_) {
-		media_control_ -> Stop();
+		warning("STUB: winVideo::stop");
+#if 0
+		media_control_->Stop();
+#endif
 		return true;
 	}
 
@@ -155,14 +171,16 @@ bool winVideo::stop() {
 
 winVideo::PlaybackStatus winVideo::playback_status() {
 	if (graph_builder_ && video_window_ && media_control_ && hwnd_) {
+		warning("STUB: winVideo::playback_status");
+#if 0
 		OAFilterState pfs;
 
-		if (media_control_ -> GetState(INFINITE, &pfs) != S_OK)
+		if (media_control_->GetState(INFINITE, &pfs) != S_OK)
 			return VID_STOPPED;
-
 		if (pfs == State_Running) return VID_RUNNING;
 		if (pfs == State_Stopped) return VID_STOPPED;
 		if (pfs == State_Paused) return VID_PAUSED;
+#endif
 	}
 
 	return VID_STOPPED;
@@ -171,7 +189,10 @@ winVideo::PlaybackStatus winVideo::playback_status() {
 bool winVideo::wait_end() {
 	if (media_event_) {
 		long evCode;
-		media_event_ -> WaitForCompletion(INFINITE, &evCode);
+		warning("STUB: winVideo::wait_end()");
+#if 0
+		media_event_->WaitForCompletion(INFINITE, &evCode);
+#endif
 		return true;
 	}
 	return false;
@@ -181,18 +202,23 @@ bool winVideo::is_playback_finished() {
 	if (!media_event_) return true;
 
 	long evCode, param1, param2;
-	while (media_event_ -> GetEvent(&evCode, &param1, &param2, 0) == S_OK) {
-		media_event_ -> FreeEventParams(evCode, param1, param2);
+	warning("STUB: winVideo::is_playback_finished");
+#if 0
+	while (media_event_->GetEvent(&evCode, &param1, &param2, 0) == S_OK) {
+		media_event_->FreeEventParams(evCode, param1, param2);
 		if ((EC_COMPLETE == evCode) || (EC_USERABORT == evCode))
 			return true;
 	}
-
+#endif
 	return false;
 }
 
 bool winVideo::toggle_fullscreen(bool fullscr) {
 	if (video_window_) {
-		video_window_ -> put_FullScreenMode(fullscr);
+		warning("STUB: winVideo::toggle_fullscreen()");
+#if 0
+		video_window_->put_FullScreenMode(fullscr);
+#endif
 		return true;
 	}
 
@@ -200,9 +226,12 @@ bool winVideo::toggle_fullscreen(bool fullscr) {
 }
 
 bool winVideo::toggle_cursor(bool visible) {
-	if (!video_window_) return false;
-	video_window_ -> HideCursor(visible);
-
+	if (!video_window_)
+		return false;
+	warning("STUB: winVideo::toggle_cursor()");
+#if 0
+	video_window_->HideCursor(visible);
+#endif
 	return true;
 }
 
@@ -210,8 +239,8 @@ bool winVideo::get_movie_size(int &sx, int &sy) {
 	/*  if(!video_window_) return false;
 
 	    long sx_,sy_;
-	    video_window_ -> get_Width(&sx_);
-	    video_window_ -> get_Height(&sy_);
+	    video_window_->get_Width(&sx_);
+	    video_window_->get_Height(&sy_);
 
 	    sx = sx_;
 	    sy = sy_;
@@ -219,9 +248,12 @@ bool winVideo::get_movie_size(int &sx, int &sy) {
 	if (!basic_video_) return false;
 
 	long width, height;
-	if (basic_video_ -> GetVideoSize(&width, &height) == E_NOINTERFACE)
-		return false;
 
+	warning("STUB: winVideo::get_movie_size()");
+#if 0
+	if (basic_video_->GetVideoSize(&width, &height) == E_NOINTERFACE)
+		return false;
+#endif
 	sx = width;
 	sy = height;
 
@@ -230,46 +262,62 @@ bool winVideo::get_movie_size(int &sx, int &sy) {
 
 bool winVideo::set_window_size(int sx, int sy) {
 	if (!video_window_) return false;
-
-	video_window_ -> put_Width(sx);
-	video_window_ -> put_Height(sy);
-
+	warning("STUB: winVideo::set_window_size()");
+#if 0
+	video_window_->put_Width(sx);
+	video_window_->put_Height(sy);
+#endif
 	return true;
 }
 
 void winVideo::close_file() {
 	if (video_window_) {
-		video_window_ -> put_Visible(0);
-//		video_window_ -> put_Owner(NULL);
+		warning("STUB: winVideo::close_file()");
+#if 0
+		video_window_->put_Visible(0);
+#endif
+//		video_window_->put_Owner(NULL);
 	}
 
 	if (media_event_) {
-		media_event_ -> Release();
+#if 0
+		media_event_->Release();
+#endif
 		media_event_ = NULL;
 	}
 
 	if (media_control_) {
-		media_control_ -> Release();
+#if 0
+		media_control_->Release();
+#endif
 		media_control_ = NULL;
 	}
 
 	if (basic_video_) {
-		basic_video_ -> Release();
+#if 0
+		basic_video_->Release();
+#endif
 		basic_video_ = NULL;
 	}
 
 	if (basic_audio_) {
-		basic_audio_ -> Release();
+#if 0
+		basic_audio_->Release();
+#endif
 		basic_audio_ = NULL;
 	}
 
 	if (video_window_) {
-		video_window_ -> Release();
+#if 0
+		video_window_->Release();
+#endif
 		video_window_ = NULL;
 	}
 
 	if (graph_builder_) {
-		graph_builder_ -> Release();
+#if 0
+		graph_builder_->Release();
+#endif
 		graph_builder_ = NULL;
 	}
 
