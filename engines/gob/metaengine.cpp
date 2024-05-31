349f09df27d691e1f584f397ab72f360d1741905
Torbjörn Andersson <eriktorbjorn@users.sourceforge.net>
2024-05-31 07:54:51 +0200

    GOB: Add "Enable copy protection" engine checkbox
    I only have a fraction of these (I wouldn't even recognize Adibou if I
    saw it!) so I've just made the assumption that the copy protection for
    Gobliiins 1, Gobliins 2, and Adibou 1 apply to all floppy versions and
    none of the CD versions.
    
    I've never seen the copy protection screen in my GOG floppy version of
    Gobliiins 1 though. Also, the GOG floppy version of Goblins 3 shows a
    copy protection screen, but apparently that's bypassed somehow without
    checking the "copy_protection" setting?

--- engines/gob/metaengine.cpp
+++ engines/gob/metaengine.cpp
@@ -28,12 +28,29 @@
 #include "engines/advancedDetector.h"
 #include "engines/obsolete.h"
 
+#include "common/translation.h"
+
 #include "gob/gameidtotype.h"
 #include "gob/gob.h"
 #include "gob/obsolete.h"
 
 // For struct GOBGameDescription.
 #include "gob/detection/detection.h"
+
+static const ADExtraGuiOptionsMap optionsList[] = {
+	{
+		GAMEOPTION_COPY_PROTECTION,
+                {
+                        _s("Enable copy protection"),
+                        _s("Enable any copy protection that would otherwise be bypassed by default."),
+                        "copy_protection",
+                        false,
+                        0,
+                        0
+                },
+        },
+	AD_EXTRA_GUI_OPTIONS_TERMINATOR
+};
 
 class GobMetaEngine : public AdvancedMetaEngine {
 public:
@@ -49,6 +66,10 @@ public:
 	}
 
 	Common::Error createInstance(OSystem *syst, Engine **engine, const ADGameDescription *desc) const override;
+
+	const ADExtraGuiOptionsMap *getAdvancedExtraGuiOptions() const override {
+		return optionsList;
+	}
 };
 
 bool GobMetaEngine::hasFeature(MetaEngineFeature f) const {
