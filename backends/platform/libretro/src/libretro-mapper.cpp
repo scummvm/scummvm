c6ad212419b9760f1a4bb6ae7138d45b08ad3766
Giovanni Cascione <ing.cascione@gmail.com>
2024-05-31 23:24:05 +0200

    LIBRETRO: remove unneeded translation.h include

--- backends/platform/libretro/src/libretro-mapper.cpp
+++ backends/platform/libretro/src/libretro-mapper.cpp
@@ -17,7 +17,6 @@
 #define FORBIDDEN_SYMBOL_EXCEPTION_strcpy
 #define FORBIDDEN_SYMBOL_EXCEPTION_strcat
 
-#include "common/translation.h"
 #include "backends/platform/libretro/include/libretro-mapper.h"
 
 struct retro_keymap mapper_keys[RETRO_DEVICE_ID_JOYPAD_LAST] = {0};
