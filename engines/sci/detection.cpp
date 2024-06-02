21bd468b9a380cde2409f88e75c33eb58cd56f30
Le Philousophe <lephilousophe@users.noreply.github.com>
2024-06-02 19:20:46 +0200

    SCI: Simplify engine plugin search

--- engines/sci/detection.cpp
+++ engines/sci/detection.cpp
@@ -251,12 +251,7 @@ ADDetectedGame SciMetaEngineDetection::fallbackDetect(const FileMap &allFiles, c
 		}
 	}
 
-	const Plugin *detectionPlugin = EngineMan.findDetectionPlugin(getName());
-	if (!detectionPlugin) {
-		return ADDetectedGame();
-	}
-
-	const Plugin *enginePlugin = PluginMan.getEngineFromDetectionPlugin(detectionPlugin);
+	const Plugin *enginePlugin = PluginMan.findEnginePlugin(getName());
 	if (!enginePlugin) {
 		static bool warn = true;
 		if (warn) {
