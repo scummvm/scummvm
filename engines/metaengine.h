55ac8ba07f9a955938b85b9548c852862e4c7317
Le Philousophe <lephilousophe@users.noreply.github.com>
2024-06-02 16:27:10 +0200

    PLUGINS: Remove default value for EngineMan::getPlugins argument
    This is now obvious of which type is the list of plugins returned.

--- engines/metaengine.h
+++ engines/metaengine.h
@@ -600,12 +600,8 @@ public:
 
 	/**
 	 * Get the list of all plugins for the type specified.
-	 *
-	 * By default, it will get METAENGINES, for now.
-	 * If usage of actual engines never occurs, the default arguments can be skipped,
-	 * and always have it return PLUGIN_TYPE_ENGINE_DETECTION.
 	 */
-	const PluginList &getPlugins(const PluginType fetchPluginType = PLUGIN_TYPE_ENGINE_DETECTION) const;
+	const PluginList &getPlugins(const PluginType fetchPluginType) const;
 
 	/** Find a target. */
 	QualifiedGameDescriptor findTarget(const Common::String &target, const Plugin **plugin = NULL) const;
