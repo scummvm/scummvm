9ed19d3c4c3c5794ea4f696e31a68f8d8486d241
Matthew Jimenez <matthew.jimenez@outlook.com>
2024-06-04 22:47:36 -0500

    ULTIMA8: Remove accidental inclusion of tokenizer

--- engines/ultima/ultima8/games/game_info.cpp
+++ engines/ultima/ultima8/games/game_info.cpp
@@ -19,8 +19,6 @@
  *
  */
 
-#include "common/tokenizer.h"
-
 #include "ultima/ultima8/misc/debugger.h"
 
 #include "ultima/ultima8/games/game_info.h"
@@ -186,8 +184,6 @@ bool GameInfo::load(Common::SeekableReadStream *rs, uint32 ver) {
 	Std::vector<Std::string> parts;
 
 	s = rs->readLine();
-	Common::StringTokenizer tokenizer(s, ",");
-
 	SplitString(s, ',', parts);
 	if (parts.size() != 4) return false;
 
