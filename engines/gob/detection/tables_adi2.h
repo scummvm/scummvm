7f6b97b30025846164d48e93a748f9b0002fd058
Benjamin Funke <bjnfne@web.de>
2024-06-05 07:50:34 +0200

    GOB: add Adi 2 math variant to detection

--- engines/gob/detection/tables_adi2.h
+++ engines/gob/detection/tables_adi2.h
@@ -236,12 +236,24 @@
 	kFeaturesNone,
 	"adi2.stk", "ediintro.tot", 0
 },
-
 {
 	{
 		"adi2", // This version comes from the Sierra's Schoolhouse Math 
 		"Adi 2",
 		AD_ENTRY1s("adi2.stk", "da5c5b4a6b56ed34d10ae5e0acfb9f8d", 11690760),
+		EN_USA,
+		kPlatformDOS,
+		ADGF_UNSTABLE,
+		GUIO0()
+	},
+	kFeaturesNone, // When Assertion bug is fixed check if this version also needs kFeatures600x400
+	"adi2.stk", "ediintro.tot", 0
+},
+{
+	{
+		"adi2", // This version comes from the Sierra's Schoolhouse Math 
+		"Adi 2 Math",
+		AD_ENTRY1s("adi2.stk", "0f102a6e4fac493162dfb70144c662bf", 12112994),
 		EN_USA,
 		kPlatformDOS,
 		ADGF_UNSTABLE,
