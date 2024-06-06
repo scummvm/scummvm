f0bbfb1bcfff3f87db116bf38f7152f802743659
kunxl-gg <tiwari.25@iitj.ac.in>
2024-06-06 22:49:03 +0530

    QDENGINE: Remove redundant files
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

--- engines/qdengine/xlibs/util/xmlrpc/XmlRpcLib.h
+++
@@ -1,7 +0,0 @@
-#ifndef __VISTA_XMLRPC_LIB_H__
-#define __VISTA_XMLRPC_LIB_H__
-
-#define _LIB_NAME "XmlRpc"
-#include "AutomaticLink.h"
-
-#endif //__VISTA_XMLRPC_LIB_H__
--- engines/qdengine/xlibs/xutil/xerrhand/PCH.h
+++
@@ -1,9 +0,0 @@
-/*----------------------------------------------------------------------
-       John Robbins - Microsoft Systems Journal Bugslayer Column
-----------------------------------------------------------------------*/
-
-#include "WarningsOff.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include "qdengine/xlibs/xutil/xerrhand/psapi.h"
-#include "qdengine/xlibs/xutil/xerrhand/WarningsOn.h"
