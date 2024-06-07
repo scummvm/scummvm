362882e805cd5dbedae69a85fe2f1c50c1fb5ecd
codengine <498798+codengine@users.noreply.github.com>
2024-06-07 02:12:16 +0200

    DIRECTOR: XOBJ: Add inixobj

---
+++ engines/director/lingo/xlibs/inixobj.h
@@ -0,0 +1,52 @@
+﻿/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef DIRECTOR_LINGO_XLIBS_INI_H
+#define DIRECTOR_LINGO_XLIBS_INI_H
+
+#include "common/path.h"
+#include "director/types.h"
+#include "director/lingo/lingo-object.h"
+
+namespace Director {
+
+class IniXObject : public Object<IniXObject> {
+public:
+	IniXObject(ObjectType objType);
+};
+
+namespace IniXObj {
+
+extern const char *xlibName;
+extern const char *fileNames[];
+
+void open(ObjectType type, const Common::Path &path);
+void close(ObjectType type);
+
+void m_new(int nargs);
+void m_readString(int nargs);
+void m_writeString(int nargs);
+
+} // End of namespace IniXObj
+
+} // End of namespace Director
+
+#endif
