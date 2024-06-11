c915a65b4aaa9a2e7c3d7af06644fde01cb15335
Eugene Sandulenko <sev@scummvm.org>
2024-06-11 22:17:35 +0200

    QDENGINE: Initial implementation of qdFileManager on top of Common classes

--- engines/qdengine/core/qdcore/qd_file_manager.h
+++ engines/qdengine/core/qdcore/qd_file_manager.h
@@ -24,10 +24,14 @@
 #define __QD_FILE_MANAGER_H__
 
 #define _NO_ZIP_
+
 #include "qdengine/core/qd_precomp.h"
 #include "qdengine/core/qdcore/qd_file_owner.h"
 #include "qdengine/core/qdcore/util/zip_container.h"
 
+namespace Common {
+	class Archive;
+}
 
 namespace QDEngine {
 
@@ -52,26 +56,12 @@ public:
 	}
 
 	bool is_open() {
-#ifndef _NO_ZIP_
-		return container_->isOpen();
-#else
-		return true;
-#endif
+		return _container != nullptr;
 	}
 
 	void init();
-	bool open() {
-#ifndef _NO_ZIP_
-		return container_->open(file_name());
-#else
-		return false;
-#endif
-	}
-	void close() {
-#ifndef _NO_ZIP_
-		container_->close();
-#endif
-	}
+	bool open();
+	void close();
 
 	//! Проверяет наличие файла контейнера.
 	/**
@@ -79,15 +69,7 @@ public:
 	*/
 	bool check_container();
 
-	bool open_file(const char *file_name) {
-#ifndef _NO_ZIP_
-		// IMPORTANT(pabdulin): is this a valid hack?
-		XZipStream xzs = XZipStream(file_name);
-		return container_->fileOpen(xzs, file_name);
-#else
-		return false;
-#endif
-	}
+	Common::Archive *_container = nullptr;
 
 private:
 	//! Номер диска, на котором расположен контейнер.
@@ -103,10 +85,6 @@ private:
 	где X - container_index_
 	*/
 	int container_index_;
-
-#ifndef _NO_ZIP_
-	XZipArchive *container_;
-#endif
 };
 
 //! Менеджер файлов.
@@ -121,72 +99,51 @@ public:
 	}
 
 	bool open_file(XZipStream &fh, const char *file_name, bool err_message = true);
+	bool open_file(Common::SeekableReadStream **fh, const char *file_name, bool err_message = true);
 
 	int last_CD_id() const {
-		return last_CD_id_;
+		return 1;
 	}
 	void set_last_CD_id(int cd_id) {
-		last_CD_id_ = cd_id;
 	}
 
 	int CD_count() const {
-		return drive_IDs_.size();
+		return 1;
 	}
 	bool is_CD_available(int cd_id) const {
-		assert(cd_id >= 0 && cd_id < drive_IDs_.size());
-		return drive_IDs_[cd_id] != NA_DRIVE_ID;
+		return true;
 	}
 	bool is_package_available(int idx) {
-		assert(idx >= 0 && idx < packages_.size());
-		return packages_[idx].is_open();
+		assert(idx >= 0 && idx < _packageCount);
+		return _packages[idx].is_open();
 	}
 	bool is_package_available(const qdFileOwner &file_owner);
 
-	const char *CD_path(const qdFileOwner &file_owner) const;
+	const char *CD_path(const qdFileOwner &file_owner) const { return ""; }
 
-	bool scan_drives(const qdFileOwner *file_owner = NULL);
-	bool scan_drives(int cd_id);
-	void update_packages();
+	bool scan_drives(const qdFileOwner *file_owner = NULL) { return true; }
+	bool scan_drives(int cd_id) { return true; }
+	void update_packages() {}
 
 	void toggle_silent_update_mode(bool mode) {
-		silent_update_mode_ = mode;
 	}
 
-	bool check_drives(const char *cdkey = NULL);
-	bool check_drive(int drive_letter);
+	bool check_drives(const char *cdkey = NULL) { return true; }
+	bool check_drive(int drive_letter)  { return true; }
 
 	static qdFileManager &instance();
 
 	typedef bool (*request_CD_handler_t)(int cd_id);
-	request_CD_handler_t set_request_CD_handler(request_CD_handler_t new_handler) {
-		request_CD_handler_t old_handler = request_CD_handler_;
-		request_CD_handler_ = new_handler;
-		return old_handler;
-	}
+	void set_request_CD_handler(request_CD_handler_t new_handler) {}
 
 private:
 
 	qdFileManager();
 
-	enum {
-		NA_DRIVE_ID = -1,
-		CDKEY_LENGTH = 32
-	};
-
 	bool enable_packages_;
-	bool silent_update_mode_;
+	qdFilePackage _packages[3];
 
-	int last_CD_id_;
-
-	std::vector<int> drive_IDs_;
-
-	typedef std::vector<qdFilePackage> packages_container_t;
-	packages_container_t packages_;
-
-	//! Функция, которая просит вставить CD в привод.
-	request_CD_handler_t request_CD_handler_;
-
-	char cd_key_[CDKEY_LENGTH + 1];
+	int _packageCount = 0;
 };
 
 } // namespace QDEngine
