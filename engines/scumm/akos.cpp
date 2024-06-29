59f752c083bc64a0a9c12391e7f89dc26b563f02
AndywinXp <andywinxp@gmail.com>
2024-06-29 23:21:47 +0200

    SCUMM: HE/FREDDI3: Provide better solution for bug #13532
    "SCUMM/HE: Flashing line at bottom of screen in Freddi 3"
    
    This solves #13532 in a better way: instead of moving the
    animation down by one pixel, we simply avoid marking that
    line of stray pixels as part of the dirty rectangle.
    
    We remove two pixels since AkosRenderer::paintCelByleRLE()
    is going to add one pixel to the rect during the clipping part.

--- engines/scumm/akos.cpp
+++ engines/scumm/akos.cpp
@@ -418,7 +418,7 @@ byte AkosRenderer::drawLimb(const Actor *a, int limb) {
 			// and an horizontal line at the bottom, causing this line to appear at the bottom of the screen.
 			// We draw the whole frame one pixel down so it does not appear on screen.
 			if (_vm->_game.id == GID_FREDDI3 && _vm->_language == Common::EN_USA && a->_costume == 258 && (code & AKC_CelMask) == 35 && _vm->enhancementEnabled(kEnhVisualChanges))
-				yMoveCur += 1;
+				_clipOverride.bottom -= 2;
 
 			if (i == extra - 1) {
 				_xMove += lastDx;
