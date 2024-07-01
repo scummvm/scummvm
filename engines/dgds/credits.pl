6b26cefe9f639632e9f1862f667b44888e157169
Matthew Duggan <mgithub@guarana.org>
2024-07-02 09:42:30 +1000

    DGDS: Add credits file

---
+++ engines/dgds/credits.pl
@@ -0,0 +1,5 @@
+begin_section("DGDS");
+	add_person("Matthew Duggan", "stauff", "");
+	add_person("Filippos Karapetis", "bluegr", "");
+	add_person("Vasco Alexandre da Silva Costa", "vcosta", "");
+end_section();
