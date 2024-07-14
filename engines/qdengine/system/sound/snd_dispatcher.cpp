e8c79a41c4167fb13cd6d8ab3d2b381ea58a1813
Eugene Sandulenko <sev@scummvm.org>
2024-07-14 18:29:51 +0200

    QDENGINE: Merged ds_sndDispatcher with sndDispatcher

--- engines/qdengine/system/sound/snd_dispatcher.cpp
+++ engines/qdengine/system/sound/snd_dispatcher.cpp
@@ -20,24 +20,63 @@
  */
 
 #include "qdengine/qd_precomp.h"
+#include "qdengine/system/app_core.h"
+#include "qdengine/system/graphics/gr_dispatcher.h"
 #include "qdengine/system/sound/snd_dispatcher.h"
+#include "qdengine/qdcore/util/plaympp_api.h"
 
 namespace QDEngine {
 
 sndDispatcher *sndDispatcher::_dispatcher_ptr;
 
-sndDispatcher::sndDispatcher() : _is_enabled(true),
-	_is_paused(false),
-	_volume(255),
-	_volume_dB(0),
-	_frequency_coeff(1.0f) {
+static bool operator == (const dsSound &snd0, const sndSound &snd1) {
+	return snd0.sound() == snd1.sound();
+}
+
+static bool operator == (const dsSound &snd, const sndHandle &h) {
+	return snd.handle() == &h;
+}
+
+sndDispatcher::sndDispatcher() : _sound_device(NULL),
+		_is_enabled(true),
+		_is_paused(false),
+		_volume(255),
+		_volume_dB(0),
+		_frequency_coeff(1.0f) {
+
 	if (!_dispatcher_ptr)
 		_dispatcher_ptr = this;
+
+	warning("STUB: sndDispatcher::sndDispatcher()");
+#if 0
+	HRESULT res = DirectSoundCreate(NULL, &_sound_device, NULL);
+	if (FAILED(res) || _sound_device == NULL)
+		return;
+
+	HWND hWnd = static_cast<HWND>(appGetHandle());
+	grDispatcher *gp = grDispatcher::instance();
+
+	if (gp && gp->is_in_fullscreen_mode()) {
+		res = _sound_device->SetCooperativeLevel(hWnd, DSSCL_EXCLUSIVE);
+		if (FAILED(res))
+			_sound_device->SetCooperativeLevel(hWnd, DSSCL_PRIORITY);
+	} else {
+		_sound_device->SetCooperativeLevel(hWnd, DSSCL_PRIORITY);
+	}
+#endif
 }
 
 sndDispatcher::~sndDispatcher() {
+	_sounds.clear();
+
 	if (_dispatcher_ptr == this)
 		_dispatcher_ptr = NULL;
+
+	warning("STUB: sndDispatcher::~sndDispatcher()");
+#if 0
+	if (_sound_device)
+		_sound_device-> Release();
+#endif
 }
 
 void sndDispatcher::set_volume(unsigned int vol) {
@@ -62,4 +101,138 @@ int sndDispatcher::convert_volume_to_dB(int vol) {
 		return 0;
 }
 
+void sndDispatcher::quant() {
+	_sounds.remove_if(std::mem_fn(&dsSound::is_stopped));
+}
+
+bool sndDispatcher::play_sound(const sndSound *snd, bool loop, float start_position, int vol) {
+	if (!_sound_device) return false;
+
+	if (is_enabled()) {
+		_sounds.push_back(dsSound(*snd, _sound_device));
+		dsSound &p = _sounds.back();
+
+		if (loop)
+			p.toggle_looping();
+
+		int snd_volume = (vol == 255) ? volume_dB() : convert_volume_to_dB((volume() * vol) >> 8);
+
+		p.create_sound_buffer();
+		p.set_volume(snd_volume);
+		p.change_frequency(frequency_coeff());
+		p.set_position(start_position);
+
+		if (!is_paused()) {
+			if (!p.play()) return false;
+		} else
+			p.pause();
+	}
+
+	return true;
+}
+
+bool sndDispatcher::stop_sound(const sndSound *snd) {
+	if (!_sound_device) return false;
+
+	sound_list_t::iterator it = std::find(_sounds.begin(), _sounds.end(), *snd);
+
+	if (it != _sounds.end()) {
+		it->stop();
+		_sounds.erase(it);
+
+		return true;
+	}
+
+	return false;
+}
+
+bool sndDispatcher::stop_sound(const sndHandle *handle) {
+	if (!_sound_device) return false;
+
+	sound_list_t::iterator it = std::find(_sounds.begin(), _sounds.end(), *handle);
+
+	if (it != _sounds.end()) {
+		it->stop();
+		_sounds.erase(it);
+
+		return true;
+	}
+
+	return false;
+}
+
+sndSound::status_t sndDispatcher::sound_status(const sndHandle *handle) const {
+	sound_list_t::const_iterator it = std::find(_sounds.begin(), _sounds.end(), *handle);
+
+	if (it != _sounds.end()) {
+		if (is_paused())
+			return sndSound::SOUND_PAUSED;
+
+		return sndSound::SOUND_PLAYING;
+	}
+
+	return sndSound::SOUND_STOPPED;
+}
+
+sndSound::status_t sndDispatcher::sound_status(const sndSound *snd) const {
+	sound_list_t::const_iterator it = std::find(_sounds.begin(), _sounds.end(), *snd);
+
+	if (it != _sounds.end())
+		return it->status();
+
+	return sndSound::SOUND_STOPPED;
+}
+
+bool sndDispatcher::update_volume() {
+	for (sound_list_t::iterator it = _sounds.begin(); it != _sounds.end(); ++it)
+		it->set_volume(volume_dB());
+
+	return true;
+}
+
+bool sndDispatcher::update_frequency() {
+	for (sound_list_t::iterator it = _sounds.begin(); it != _sounds.end(); ++it)
+		it->change_frequency(frequency_coeff());
+
+	return true;
+}
+
+void sndDispatcher::stop_sounds() {
+	for (sound_list_t::iterator it = _sounds.begin(); it != _sounds.end(); ++it)
+		it->stop();
+
+	_sounds.clear();
+}
+
+bool sndDispatcher::set_sound_frequency(const sndHandle *snd, float coeff) {
+	sound_list_t::iterator it = std::find(_sounds.begin(), _sounds.end(), *snd);
+
+	if (it != _sounds.end()) {
+		it->change_frequency(frequency_coeff() * coeff);
+		return true;
+	}
+
+	return false;
+}
+
+float sndDispatcher::sound_position(const sndHandle *snd) const {
+	sound_list_t::const_iterator it = std::find(_sounds.begin(), _sounds.end(), *snd);
+
+	if (it != _sounds.end())
+		return it->position();
+
+	return 0.0f;
+}
+
+void sndDispatcher::pause_sounds() {
+	for (sound_list_t::iterator it = _sounds.begin(); it != _sounds.end(); ++it)
+		it->pause();
+}
+
+void sndDispatcher::resume_sounds() {
+	for (sound_list_t::iterator it = _sounds.begin(); it != _sounds.end(); ++it) {
+		if (it->is_paused())
+			it->resume();
+	}
+}
 } // namespace QDEngine
