c6bf0820cb086bcee686c36285703044c92813aa
Walter Agazzi <walter.agazzi@protonmail.com>
2024-07-18 21:24:29 +0200

    AGS: Engine: fixed first Sierra speechline not accounting for disabled gui
    From upstream 34957df168f27d6eeffc0f821f0d2632af404b89

--- engines/ags/engine/ac/gui.h
+++ engines/ags/engine/ac/gui.h
@@ -75,8 +75,8 @@ void    update_gui_zorder();
 void    export_gui_controls(int ee);
 void    unexport_gui_controls(int ee);
 void    update_gui_disabled_status();
-int     adjust_x_for_guis(int xx, int yy);
-int     adjust_y_for_guis(int yy);
+int     adjust_x_for_guis(int x, int y, bool assume_blocking = false);
+int     adjust_y_for_guis(int y, bool assume_blocking = false);
 
 int     gui_get_interactable(int x, int y);
 int     gui_on_mouse_move();
