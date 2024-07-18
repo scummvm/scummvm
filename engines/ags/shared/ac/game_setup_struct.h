391249e96c2e19536f4023e90f706f2a9b731cc2
Walter Agazzi <walter.agazzi@protonmail.com>
2024-07-18 15:30:32 +0200

    AGS: Engine: simplified GameSetupStruct and GameState reset in unload_game()
    Partially from upstream 55967364f40b53d33ccb6760dfb94335816dba29

--- engines/ags/shared/ac/game_setup_struct.h
+++ engines/ags/shared/ac/game_setup_struct.h
@@ -111,7 +111,10 @@ struct GameSetupStruct : public GameSetupStructBase {
 
 
 	GameSetupStruct();
+	GameSetupStruct(GameSetupStruct &&gss) = default;
 	~GameSetupStruct();
+
+	GameSetupStruct &operator=(GameSetupStruct &&gss) = default;
 
 	void Free();
 
