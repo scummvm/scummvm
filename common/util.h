915eccfa478dd25c55aec69dc9a113a97a66115b
elasota <1137273+elasota@users.noreply.github.com>
2024-07-23 22:49:50 -0400

    COMMON: Fix forwarding of rvalue refs

--- common/util.h
+++ common/util.h
@@ -211,6 +211,11 @@ constexpr remove_reference_t<T> &&move(T &&t) noexcept {
 }
 
 template<class T>
+constexpr T&& forward(remove_reference_t<T> &&t) noexcept {
+	return static_cast<T &&>(t);
+}
+
+template<class T>
 constexpr T&& forward(remove_reference_t<T> &t) noexcept {
 	return static_cast<T &&>(t);
 }
