0d7993eea031d4d80a74aa7b348280e31a32f2b4
Eugene Sandulenko <sev@scummvm.org>
2024-07-26 09:22:35 +0200

    QDENGINE: Replace random generator with OSystem

--- engines/qdengine/util/xmath/xmath.h
+++ engines/qdengine/util/xmath/xmath.h
@@ -157,53 +157,6 @@ xm_inline float SIGN(float a, float b) {
 	return b >= 0.0f ? fabsf(a) : -fabsf(a);
 }
 
-class RandomGenerator {
-	enum { max_value = 0x7fff };
-	int value;
-public:
-	RandomGenerator(int val = 1) {
-		set(val);
-	}
-	void set(int val) {
-		value = val;
-	}
-	int get() const {
-		return value;
-	}
-	int operator()(); // Generates random value [0..max_value), non-inline due to some bugs with optimization
-	xm_inline int operator()(int m) {
-		return m ? (*this)() % m : 0;    // May by used in random_shuffle
-	}
-	xm_inline int operator()(int min, int max) {
-		return min + (*this)(max - min);
-	}
-	xm_inline float frnd(float x = 1.f) {
-		return (float)((*this)() * 2 - max_value) * x / (float)max_value;
-	}
-	xm_inline float fabsRnd(float x = 1.f) {
-		return (float)((*this)()) * x / (float)max_value;
-	}
-	xm_inline float fabsRnd(float min, float max) {
-		return min + fabsRnd(max - min);
-	}
-	xm_inline float frand() {
-		return (*this)() / (float)max_value;
-	}
-};
-
-#undef random
-extern RandomGenerator xm_random_generator;
-xm_inline unsigned random(unsigned m) {
-	return xm_random_generator(m);
-}
-xm_inline float frnd(float x) {
-	return xm_random_generator.frnd(x);
-}
-xm_inline float fabsRnd(float x) {
-	return xm_random_generator.fabsRnd(x);
-}
-
-
 #define NOMINMAX
 #undef min
 #undef max
