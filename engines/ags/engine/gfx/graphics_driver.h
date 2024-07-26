26a55227aa087f262beffde80349b509d70e2819
Walter Agazzi <walter.agazzi@protonmail.com>
2024-07-26 22:55:36 +0200

    AGS: Engine: Engine: query gfxdriver for texture mem, and optionally restrict cache

--- engines/ags/engine/gfx/graphics_driver.h
+++ engines/ags/engine/gfx/graphics_driver.h
@@ -135,6 +135,8 @@ public:
 	// Gets closest recommended bitmap format (currently - only color depth) for the given original format.
 	// Engine needs to have game bitmaps brought to the certain range of formats, easing conversion into the video bitmaps.
 	virtual int  GetCompatibleBitmapFormat(int color_depth) = 0;
+	// Returns available texture memory, or 0 if this query is not supported
+	virtual size_t GetAvailableTextureMemory() = 0;
 
 	// Creates a "raw" DDB, without pixel initialization
 	virtual IDriverDependantBitmap *CreateDDB(int width, int height, int color_depth, bool opaque = false) = 0;
