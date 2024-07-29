7e9e6040d97a90ef0f47547e0020e378d991c6d4
kunxl-gg <tiwari.25@iitj.ac.in>
2024-07-29 13:15:55 +0530

    QDENGINE: Rename class variables for qdScreenText
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

--- engines/qdengine/qdcore/qd_screen_text.cpp
+++ engines/qdengine/qdcore/qd_screen_text.cpp
@@ -34,16 +34,16 @@
 
 namespace QDEngine {
 
-qdScreenTextFormat qdScreenTextFormat::default_format_;
-qdScreenTextFormat qdScreenTextFormat::global_text_format_;
-qdScreenTextFormat qdScreenTextFormat::global_topic_format_;
+qdScreenTextFormat qdScreenTextFormat::_default_format;
+qdScreenTextFormat qdScreenTextFormat::_global_text_format;
+qdScreenTextFormat qdScreenTextFormat::_global_topic_format;
 
-qdScreenTextFormat::qdScreenTextFormat() : arrangement_(ARRANGE_VERTICAL),
-	alignment_(ALIGN_LEFT),
-	color_(0xFFFFFF),
-	hover_color_(0xFFFFFF),
-	font_type_(QD_FONT_TYPE_NONE),
-	global_depend_(true) {
+qdScreenTextFormat::qdScreenTextFormat() : _arrangement(ARRANGE_VERTICAL),
+	_alignment(ALIGN_LEFT),
+	_color(0xFFFFFF),
+	_hover_color(0xFFFFFF),
+	_font_type(QD_FONT_TYPE_NONE),
+	_global_depend(true) {
 }
 
 bool qdScreenTextFormat::load_script(const xml::tag *p) {
@@ -66,7 +66,7 @@ bool qdScreenTextFormat::load_script(const xml::tag *p) {
 			set_font_type(xml::tag_buffer(*it).get_int());
 			break;
 		case QDSCR_GLOBAL_DEPEND:
-			global_depend_ = (0 != xml::tag_buffer(*it).get_int());
+			_global_depend = (0 != xml::tag_buffer(*it).get_int());
 			load_global_depend = true;
 			break;
 		}
@@ -75,7 +75,7 @@ bool qdScreenTextFormat::load_script(const xml::tag *p) {
 	// Если скрипт старый и инфа о связи с глобальным форматом отсутствует, то
 	// является ли формат связанным с глобальным по содержимому
 	if (!load_global_depend)
-		global_depend_ = (global_text_format() == *this);
+		_global_depend = (global_text_format() == *this);
 
 	return true;
 }
@@ -120,17 +120,17 @@ bool qdScreenTextFormat::save_script(Common::WriteStream &fh, int indent) const 
 	return true;
 }
 
-qdScreenText::qdScreenText(const char *p, const Vect2i &pos, qdGameObjectState *owner) : pos_(pos),
-	size_(0, 0),
-	owner_(owner) {
+qdScreenText::qdScreenText(const char *p, const Vect2i &pos, qdGameObjectState *owner) : _pos(pos),
+	_size(0, 0),
+	_owner(owner) {
 	set_data(p);
 }
 
-qdScreenText::qdScreenText(const char *p, const qdScreenTextFormat &fmt, const Vect2i &pos, qdGameObjectState *owner) : pos_(pos),
-	size_(0, 0),
-	text_format_(fmt),
-	owner_(owner) {
-	hover_mode_ = false;
+qdScreenText::qdScreenText(const char *p, const qdScreenTextFormat &fmt, const Vect2i &pos, qdGameObjectState *owner) : _pos(pos),
+	_size(0, 0),
+	_text_format(fmt),
+	_owner(owner) {
+	_hover_mode = false;
 
 	set_data(p);
 }
@@ -139,30 +139,30 @@ qdScreenText::~qdScreenText() {
 }
 
 void qdScreenText::redraw(const Vect2i &owner_pos) const {
-	int x = owner_pos.x + pos_.x;
-	int y = owner_pos.y + pos_.y;
+	int x = owner_pos.x + _pos.x;
+	int y = owner_pos.y + _pos.y;
 
-	unsigned col = hover_mode_ ? text_format_.hover_color() : text_format_.color();
+	unsigned col = _hover_mode ? _text_format.hover_color() : _text_format.color();
 
 	const grFont *font = qdGameDispatcher::get_dispatcher()->
-	                     find_font(text_format_.font_type());
+	                     find_font(_text_format.font_type());
 
-	grDispatcher::instance()->DrawAlignedText(x, y, size_.x, size_.y, col, data(), grTextAlign(text_format_.alignment()), 0, 0, font);
+	grDispatcher::instance()->DrawAlignedText(x, y, _size.x, _size.y, col, data(), grTextAlign(_text_format.alignment()), 0, 0, font);
 	if (qdGameConfig::get_config().debug_draw())
-		grDispatcher::instance()->Rectangle(x, y, size_.x, size_.y, col, 0, GR_OUTLINED);
+		grDispatcher::instance()->Rectangle(x, y, _size.x, _size.y, col, 0, GR_OUTLINED);
 }
 
 void qdScreenText::set_data(const char *p) {
-	data_ = p;
+	_data = p;
 
 	const grFont *font = qdGameDispatcher::get_dispatcher()->
-	                     find_font(text_format_.font_type());
-	size_.x = grDispatcher::instance()->TextWidth(data(), 0, font);
-	size_.y = grDispatcher::instance()->TextHeight(data(), 0, font);
+	                     find_font(_text_format.font_type());
+	_size.x = grDispatcher::instance()->TextWidth(data(), 0, font);
+	_size.y = grDispatcher::instance()->TextHeight(data(), 0, font);
 }
 
 bool qdScreenText::is_owned_by(const qdNamedObject *p) const {
-	return (owner_ && p == owner_->owner());
+	return (_owner && p == _owner->owner());
 }
 
 bool qdScreenText::format_text(int max_width) {
@@ -173,13 +173,13 @@ bool qdScreenText::format_text(int max_width) {
 	bool correct = true;
 	int safe_space = -1;
 	int cur_wid = 0;
-	data_ += ' '; // Добавляем пробел для упрощения алгоритма (из-за последнего
+	_data += ' '; // Добавляем пробел для упрощения алгоритма (из-за последнего
 	// пробела в конце всегда включится попытка форматирования конца).
 	// Пробел не отразиться на выводе, т.к. он в конце.
 
-	unsigned char *dp = (unsigned char *)data_.c_str();
+	unsigned char *dp = (unsigned char *)_data.c_str();
 
-	for (int i = 0; i < data_.length(); i++) {
+	for (int i = 0; i < _data.length(); i++) {
 		if (dp[i] == '\n') {
 			if (cur_wid > max_width) {
 				// безопасный пробел есть - безопасно режем (т.е. все влезает в max_width)
@@ -227,9 +227,9 @@ bool qdScreenText::format_text(int max_width) {
 		}
 	}
 
-	data_.erase(data_.length() - 1, 1); // Удаляем последний символ (пробел добавленный нами)
+	_data.erase(_data.length() - 1, 1); // Удаляем последний символ (пробел добавленный нами)
 
-	set_data(data_.c_str()); // Устанавливаем данные (для пересчета размера текста)
+	set_data(_data.c_str()); // Устанавливаем данные (для пересчета размера текста)
 
 	return correct;
 }
