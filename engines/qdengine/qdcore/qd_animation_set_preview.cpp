55b0c643f4323db99b8c3a174b29ab3e5e2d51bc
kunxl-gg <tiwari.25@iitj.ac.in>
2024-07-29 14:29:12 +0530

    QDENGINE: Rename remaining class variables for qdAnimationSetPreview
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

--- engines/qdengine/qdcore/qd_animation_set_preview.cpp
+++ engines/qdengine/qdcore/qd_animation_set_preview.cpp
@@ -31,22 +31,22 @@
 namespace QDEngine {
 
 qdAnimationSetPreview::qdAnimationSetPreview(qdAnimationSet *p) :
-	graph_d_(0),
+	_graph_d(0),
 	_animation_set(p),
-	preview_mode_(VIEW_WALK_ANIMATIONS) {
+	_preview_mode(VIEW_WALK_ANIMATIONS) {
 	_animation = new qdAnimation;
 
-	camera_ = new qdCamera;
-	camera_->set_focus(2000.0f);
-	camera_angle_ = 45;
+	_camera = new qdCamera;
+	_camera->set_focus(2000.0f);
+	_camera_angle = 45;
 
 	_start_time = 0;
-	back_color_ = 0x000000FF;
-	grid_color_ = 0x00FF00FF;
+	_back_color = 0x000000FF;
+	_grid_color = 0x00FF00FF;
 
 	set_direction(0);
 
-	cell_size_ = 100;
+	_cell_size = 100;
 
 	_personage_height = float(_animation->size_y());
 
@@ -57,11 +57,11 @@ qdAnimationSetPreview::~qdAnimationSetPreview() {
 	_animation_set->free_animations(NULL);
 
 	delete _animation;
-	delete camera_;
+	delete _camera;
 }
 
 void qdAnimationSetPreview::set_graph_dispatcher(grDispatcher *p) {
-	graph_d_ = p;
+	_graph_d = p;
 	set_screen(Vect2s(0, 0), Vect2s(p->Get_SizeX(), p->Get_SizeY()));
 }
 
@@ -69,7 +69,7 @@ void qdAnimationSetPreview::start() {
 	_start_time = g_system->getMillis();
 
 	_animation->start();
-	cell_offset_ = 0.0f;
+	_cell_offset = 0.0f;
 }
 
 void qdAnimationSetPreview::quant() {
@@ -85,32 +85,32 @@ void qdAnimationSetPreview::quant() {
 void qdAnimationSetPreview::quant(float tm) {
 	_animation->quant(tm);
 
-	cell_offset_ -= _personage_speed * tm;
-	while (cell_offset_ <= -float(cell_size_)) cell_offset_ += float(cell_size_);
+	_cell_offset -= _personage_speed * tm;
+	while (_cell_offset <= -float(_cell_size)) _cell_offset += float(_cell_size);
 }
 
 void qdAnimationSetPreview::redraw() {
-	grDispatcher *gp = grDispatcher::set_instance(graph_d_);
+	grDispatcher *gp = grDispatcher::set_instance(_graph_d);
 
-	grDispatcher::instance()->Fill(back_color_);
+	grDispatcher::instance()->Fill(_back_color);
 
 	redraw_grid();
 
-	Vect2s v = camera_->global2scr(Vect3f(0.0f, 0.0f, _personage_height / 2.0f));
-	float scale = camera_->get_scale(Vect3f(0.0f, 0.0f, _personage_height / 2.0f));
+	Vect2s v = _camera->global2scr(Vect3f(0.0f, 0.0f, _personage_height / 2.0f));
+	float scale = _camera->get_scale(Vect3f(0.0f, 0.0f, _personage_height / 2.0f));
 	_animation->redraw(v.x, v.y, scale);
 
-	Vect2s v0 = camera_->global2scr(Vect3f(0.0f, 0.0f, _personage_height));
-	Vect2s v1 = camera_->global2scr(Vect3f(0.0f, 0.0f, 0.0f));
+	Vect2s v0 = _camera->global2scr(Vect3f(0.0f, 0.0f, _personage_height));
+	Vect2s v1 = _camera->global2scr(Vect3f(0.0f, 0.0f, 0.0f));
 
 	const int rect_sz = 4;
-	grDispatcher::instance()->Rectangle(v.x - rect_sz / 2, v.y - rect_sz / 2, rect_sz, rect_sz, grid_color_, grid_color_, GR_FILLED);
+	grDispatcher::instance()->Rectangle(v.x - rect_sz / 2, v.y - rect_sz / 2, rect_sz, rect_sz, _grid_color, _grid_color, GR_FILLED);
 
 	const int line_sz = 10;
-	grDispatcher::instance()->Line(v0.x - line_sz, v0.y, v0.x + line_sz, v0.y, grid_color_);
-	grDispatcher::instance()->Line(v1.x - line_sz, v1.y, v1.x + line_sz, v1.y, grid_color_);
-	grDispatcher::instance()->Line(v0.x, v0.y, v1.x, v1.y, grid_color_);
-	grDispatcher::instance()->Rectangle(screen_offset_.x, screen_offset_.y, screen_size_.x, screen_size_.y, grid_color_, 0, GR_OUTLINED);
+	grDispatcher::instance()->Line(v0.x - line_sz, v0.y, v0.x + line_sz, v0.y, _grid_color);
+	grDispatcher::instance()->Line(v1.x - line_sz, v1.y, v1.x + line_sz, v1.y, _grid_color);
+	grDispatcher::instance()->Line(v0.x, v0.y, v1.x, v1.y, _grid_color);
+	grDispatcher::instance()->Rectangle(_screen_offset.x, _screen_offset.y, _screen_size.x, _screen_size.y, _grid_color, 0, GR_OUTLINED);
 	grDispatcher::instance()->Flush();
 
 	grDispatcher::set_instance(gp);
@@ -122,19 +122,19 @@ bool qdAnimationSetPreview::set_direction(int dir) {
 	_animation->clear();
 	_personage_speed = 0.0f;
 	_direction = dir;
-	cell_offset_ = 0.0f;
+	_cell_offset = 0.0f;
 
 	float angle = _animation_set->get_index_angle(_direction) * 180.0f / M_PI;
-	camera_->rotate_and_scale(camera_angle_, 0, angle, 1.0f, 1.0f, 1.0f);
+	_camera->rotate_and_scale(_camera_angle, 0, angle, 1.0f, 1.0f, 1.0f);
 
 	qdAnimationInfo *p = NULL;
-	if (preview_mode_ == VIEW_WALK_ANIMATIONS)
+	if (_preview_mode == VIEW_WALK_ANIMATIONS)
 		p = _animation_set->get_animation_info(dir);
-	else if (preview_mode_ == VIEW_STATIC_ANIMATIONS)
+	else if (_preview_mode == VIEW_STATIC_ANIMATIONS)
 		p = _animation_set->get_static_animation_info(dir);
-	else if (preview_mode_ == VIEW_START_ANIMATIONS)
+	else if (_preview_mode == VIEW_START_ANIMATIONS)
 		p = _animation_set->get_start_animation_info(dir);
-	else if (preview_mode_ == VIEW_STOP_ANIMATIONS)
+	else if (_preview_mode == VIEW_STOP_ANIMATIONS)
 		p = _animation_set->get_stop_animation_info(dir);
 
 	if (p) {
@@ -152,80 +152,80 @@ bool qdAnimationSetPreview::set_direction(int dir) {
 }
 
 void qdAnimationSetPreview::set_cell_size(int sz) {
-	cell_size_ = sz;
+	_cell_size = sz;
 
 	_animation->start();
-	cell_offset_ = 0.0f;
+	_cell_offset = 0.0f;
 }
 
 void qdAnimationSetPreview::set_screen(Vect2s offs, Vect2s size) {
-	if (!graph_d_) return;
+	if (!_graph_d) return;
 
-	screen_offset_ = offs;
-	screen_size_ = size;
+	_screen_offset = offs;
+	_screen_size = size;
 
-	camera_->set_scr_size(size.x, size.y);
-	camera_->set_scr_center(offs.x + size.x / 2, offs.y + size.y * 3 / 4);
+	_camera->set_scr_size(size.x, size.y);
+	_camera->set_scr_center(offs.x + size.x / 2, offs.y + size.y * 3 / 4);
 
-	graph_d_->SetClip(offs.x, offs.y, offs.x + size.x, offs.y + size.y);
+	_graph_d->SetClip(offs.x, offs.y, offs.x + size.x, offs.y + size.y);
 }
 
 void qdAnimationSetPreview::redraw_grid() {
 	float size = 0;
-	Vect2f p = camera_->scr2plane(screen_offset_);
+	Vect2f p = _camera->scr2plane(_screen_offset);
 	if (fabs(p.x) > size) size = fabs(p.x);
 	if (fabs(p.y) > size) size = fabs(p.y);
-	p = camera_->scr2plane(screen_offset_ + screen_size_);
+	p = _camera->scr2plane(_screen_offset + _screen_size);
 	if (fabs(p.x) > size) size = fabs(p.x);
 	if (fabs(p.y) > size) size = fabs(p.y);
-	p = camera_->scr2plane(Vect2s(screen_offset_.x + screen_size_.x, screen_offset_.y));
+	p = _camera->scr2plane(Vect2s(_screen_offset.x + _screen_size.x, _screen_offset.y));
 	if (fabs(p.x) > size) size = fabs(p.x);
 	if (fabs(p.y) > size) size = fabs(p.y);
-	p = camera_->scr2plane(Vect2s(screen_offset_.x, screen_offset_.y + screen_size_.y));
+	p = _camera->scr2plane(Vect2s(_screen_offset.x, _screen_offset.y + _screen_size.y));
 	if (fabs(p.x) > size) size = fabs(p.x);
 	if (fabs(p.y) > size) size = fabs(p.y);
 
-	int sz = round(size) + cell_size_;
-	sz -= sz % cell_size_;
-	for (int i = -sz; i <= sz; i += cell_size_) {
-		int dx = round(cell_offset_);
+	int sz = round(size) + _cell_size;
+	sz -= sz % _cell_size;
+	for (int i = -sz; i <= sz; i += _cell_size) {
+		int dx = round(_cell_offset);
 
-		Vect3f v00 = camera_->global2camera_coord(Vect3f(i + dx, -sz, 0));
-		Vect3f v10 = camera_->global2camera_coord(Vect3f(i + dx, sz, 0));
-		if (camera_->line_cutting(v00, v10)) {
-			Vect2s v0 = camera_->camera_coord2scr(v00);
-			Vect2s v1 = camera_->camera_coord2scr(v10);
-			grDispatcher::instance()->Line(v0.x, v0.y, v1.x, v1.y, grid_color_, 0);
+		Vect3f v00 = _camera->global2camera_coord(Vect3f(i + dx, -sz, 0));
+		Vect3f v10 = _camera->global2camera_coord(Vect3f(i + dx, sz, 0));
+		if (_camera->line_cutting(v00, v10)) {
+			Vect2s v0 = _camera->camera_coord2scr(v00);
+			Vect2s v1 = _camera->camera_coord2scr(v10);
+			grDispatcher::instance()->Line(v0.x, v0.y, v1.x, v1.y, _grid_color, 0);
 		}
 
-		v00 = camera_->global2camera_coord(Vect3f(-sz + dx, i, 0));
-		v10 = camera_->global2camera_coord(Vect3f(sz + dx, i, 0));
-		if (camera_->line_cutting(v00, v10)) {
-			Vect2s v0 = camera_->camera_coord2scr(v00);
-			Vect2s v1 = camera_->camera_coord2scr(v10);
-			grDispatcher::instance()->Line(v0.x, v0.y, v1.x, v1.y, grid_color_, 0);
+		v00 = _camera->global2camera_coord(Vect3f(-sz + dx, i, 0));
+		v10 = _camera->global2camera_coord(Vect3f(sz + dx, i, 0));
+		if (_camera->line_cutting(v00, v10)) {
+			Vect2s v0 = _camera->camera_coord2scr(v00);
+			Vect2s v1 = _camera->camera_coord2scr(v10);
+			grDispatcher::instance()->Line(v0.x, v0.y, v1.x, v1.y, _grid_color, 0);
 		}
 		/*
-		        v0 = camera_->global2scr(Vect3f(-sz + dx,i,0));
-		        v1 = camera_->global2scr(Vect3f(sz + dx,i,0));
-		        grDispatcher::instance()->Line(v0.x,v0.y,v1.x,v1.y,grid_color_,0);
+		        v0 = _camera->global2scr(Vect3f(-sz + dx,i,0));
+		        v1 = _camera->global2scr(Vect3f(sz + dx,i,0));
+		        grDispatcher::instance()->Line(v0.x,v0.y,v1.x,v1.y,_grid_color,0);
 		*/
 	}
 }
 
 void qdAnimationSetPreview::set_camera_angle(int ang) {
-	camera_angle_ = ang;
+	_camera_angle = ang;
 
 	float angle = _animation_set->get_index_angle(_direction) * 180.0f / M_PI;
-	camera_->rotate_and_scale(camera_angle_, 0, angle, 1.0f, 1.0f, 1.0f);
+	_camera->rotate_and_scale(_camera_angle, 0, angle, 1.0f, 1.0f, 1.0f);
 }
 
 float qdAnimationSetPreview::camera_focus() {
-	return camera_->get_focus();
+	return _camera->get_focus();
 }
 
 void qdAnimationSetPreview::set_camera_focus(float f) {
-	camera_->set_focus(f);
+	_camera->set_focus(f);
 }
 
 bool qdAnimationSetPreview::set_phase(float phase) {
@@ -233,7 +233,7 @@ bool qdAnimationSetPreview::set_phase(float phase) {
 		if (!_animation->is_playing())
 			_animation->start();
 
-		cell_offset_ = 0.0f;
+		_cell_offset = 0.0f;
 		_animation->set_time(0.0f);
 		quant(_animation->length() * phase);
 		return true;
