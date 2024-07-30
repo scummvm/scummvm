dda9eded3181acb8d5255188b1852698fc066683
kunxl-gg <tiwari.25@iitj.ac.in>
2024-07-30 15:53:14 +0530

    QDENGINE: Eliminate #ifdef and #ifndef _QUEST_EDITOR directives
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

--- engines/qdengine/qdcore/qd_minigame.h
+++ engines/qdengine/qdcore/qd_minigame.h
@@ -97,11 +97,7 @@ public:
 	//! Добавляет в передаваемые списки имена файлов, необходимых для миниигры.
 	bool get_files_list(qdFileNameList &files_to_copy, qdFileNameList &files_to_pack) const;
 
-#ifdef _QUEST_EDITOR
-	typedef std::list<qdMinigameConfigParameter> config_container_t;
-#else
 	typedef std::vector<qdMinigameConfigParameter> config_container_t;
-#endif
 
 	const config_container_t &config() const {
 		return _config;
