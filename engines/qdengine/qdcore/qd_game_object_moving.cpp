9725ec811664f99e990f7260d4e54def273a0001
Eugene Sandulenko <sev@scummvm.org>
2024-08-01 15:30:13 +0200

    QDENGINE: Replace VK_ usage

--- engines/qdengine/qdcore/qd_game_object_moving.cpp
+++ engines/qdengine/qdcore/qd_game_object_moving.cpp
@@ -1908,7 +1908,14 @@ bool qdGameObjectMoving::keyboard_move() {
 		bool keypress = false;
 		warning("STUB: qdGameObjectMoving::keyboard_move()");
 
-		static int vkeys[6] = { VK_DOWN, VK_RIGHT, VK_UP, VK_LEFT, VK_DOWN, VK_RIGHT };
+		static const Common::KeyCode vkeys[6] = {
+			Common::KEYCODE_DOWN,
+			Common::KEYCODE_RIGHT,
+			Common::KEYCODE_UP,
+			Common::KEYCODE_LEFT,
+			Common::KEYCODE_DOWN,
+			Common::KEYCODE_RIGHT
+		};
 
 		bool key_state[6];
 
