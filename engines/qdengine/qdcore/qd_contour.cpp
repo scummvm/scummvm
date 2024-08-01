15308e7b8e604e9547d31469718576239f555951
kunxl-gg <tiwari.25@iitj.ac.in>
2024-08-02 03:17:21 +0530

    QDENGINE: Rename member variables for qdContour
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

--- engines/qdengine/qdcore/qd_contour.cpp
+++ engines/qdengine/qdcore/qd_contour.cpp
@@ -26,94 +26,94 @@
 
 namespace QDEngine {
 
-qdContour::qdContour(qdContourType tp) : contour_type_(tp),
-	size_(0, 0),
-	mask_pos_(0, 0) {
+qdContour::qdContour(qdContourType tp) : _contour_type(tp),
+	_size(0, 0),
+	_mask_pos(0, 0) {
 }
 
-qdContour::qdContour(const qdContour &ct) : contour_type_(ct.contour_type_),
-	size_(ct.size_),
-	contour_(ct.contour_) {
-	mask_pos_ = ct.mask_pos_;
+qdContour::qdContour(const qdContour &ct) : _contour_type(ct._contour_type),
+	_size(ct._size),
+	_contour(ct._contour) {
+	_mask_pos = ct._mask_pos;
 }
 
 qdContour::~qdContour() {
-	contour_.clear();
+	_contour.clear();
 }
 
 qdContour &qdContour::operator = (const qdContour &ct) {
 	if (this == &ct) return *this;
 
-	contour_type_ = ct.contour_type_;
-	size_ = ct.size_;
-	mask_pos_ = ct.mask_pos_;
+	_contour_type = ct._contour_type;
+	_size = ct._size;
+	_mask_pos = ct._mask_pos;
 
-	contour_ = ct.contour_;
+	_contour = ct._contour;
 
 	return *this;
 }
 
 void qdContour::add_contour_point(const Vect2s &pt) {
-	contour_.push_back(pt);
+	_contour.push_back(pt);
 }
 
 void qdContour::insert_contour_point(const Vect2s &pt, int insert_pos) {
-	if (insert_pos < contour_.size()) {
+	if (insert_pos < _contour.size()) {
 		if (insert_pos < 0) insert_pos = 0;
-		contour_.insert(contour_.begin() + insert_pos, pt);
+		_contour.insert(_contour.begin() + insert_pos, pt);
 	} else {
-		contour_.push_back(pt);
+		_contour.push_back(pt);
 	}
 }
 
 bool qdContour::remove_contour_point(int pos) {
-	if (pos >= 0 && pos < contour_.size()) {
-		contour_.erase(contour_.begin() + pos);
+	if (pos >= 0 && pos < _contour.size()) {
+		_contour.erase(_contour.begin() + pos);
 		return true;
 	}
 	return false;
 }
 
 bool qdContour::update_contour_point(const Vect2s &pt, int pos) {
-	if (pos >= 0 && pos < contour_.size()) {
-		contour_[pos] = pt;
+	if (pos >= 0 && pos < _contour.size()) {
+		_contour[pos] = pt;
 		return true;
 	}
 	return false;
 }
 
 bool qdContour::update_contour() {
-	if (contour_type_ != CONTOUR_POLYGON) return false;
+	if (_contour_type != CONTOUR_POLYGON) return false;
 
-	if (contour_.empty())
+	if (_contour.empty())
 		return false;
 
-	int x0 = contour_[0].x;
-	int x1 = contour_[0].x;
-	int y0 = contour_[0].y;
-	int y1 = contour_[0].y;
+	int x0 = _contour[0].x;
+	int x1 = _contour[0].x;
+	int y0 = _contour[0].y;
+	int y1 = _contour[0].y;
 
-	for (int i = 0; i < contour_.size(); i++) {
-		if (contour_[i].x < x0) x0 = contour_[i].x;
-		if (contour_[i].x > x1) x1 = contour_[i].x;
-		if (contour_[i].y < y0) y0 = contour_[i].y;
-		if (contour_[i].y > y1) y1 = contour_[i].y;
+	for (int i = 0; i < _contour.size(); i++) {
+		if (_contour[i].x < x0) x0 = _contour[i].x;
+		if (_contour[i].x > x1) x1 = _contour[i].x;
+		if (_contour[i].y < y0) y0 = _contour[i].y;
+		if (_contour[i].y > y1) y1 = _contour[i].y;
 	}
 
-	size_ = Vect2s(x1 - x0 + 1, y1 - y0 + 1);
-	mask_pos_ = Vect2s(x0 + size_.x / 2, y0 + size_.y / 2);
+	_size = Vect2s(x1 - x0 + 1, y1 - y0 + 1);
+	_mask_pos = Vect2s(x0 + _size.x / 2, y0 + _size.y / 2);
 
 	return true;
 }
 
 bool qdContour::is_inside(const Vect2s &pos) const {
-	switch (contour_type_) {
+	switch (_contour_type) {
 	case CONTOUR_RECTANGLE:
-		if (pos.x >= -size_.x / 2 && pos.x < size_.x / 2 && pos.y >= -size_.y / 2 && pos.y < size_.y / 2)
+		if (pos.x >= -_size.x / 2 && pos.x < _size.x / 2 && pos.y >= -_size.y / 2 && pos.y < _size.y / 2)
 			return true;
 		break;
 	case CONTOUR_CIRCLE:
-		if (pos.x * pos.x + pos.y * pos.y <= size_.x * size_.x)
+		if (pos.x * pos.x + pos.y * pos.y <= _size.x * _size.x)
 			return true;
 		break;
 	case CONTOUR_POLYGON: {
@@ -122,9 +122,9 @@ bool qdContour::is_inside(const Vect2s &pos) const {
 		int intersections_gt0 = 0;
 		int intersections_lt1 = 0;
 		int intersections_gt1 = 0;
-		for (int i = 0; i < contour_.size(); i ++) {
-			Vect2s p0 = contour_[i];
-			Vect2s p1 = (i < contour_.size() - 1) ? contour_[i + 1] : contour_[0];
+		for (int i = 0; i < _contour.size(); i ++) {
+			Vect2s p0 = _contour[i];
+			Vect2s p1 = (i < _contour.size() - 1) ? _contour[i + 1] : _contour[0];
 			if (p0.y != p1.y) {
 				if ((p0.y < p.y && p1.y >= p.y) || (p0.y >= p.y && p1.y < p.y)) {
 					if (p0.x < p.x && p1.x < p.x)
@@ -170,31 +170,31 @@ return false;
 }
 
 bool qdContour::save_script(Common::WriteStream &fh, int indent) const {
-	if (contour_type_ == CONTOUR_POLYGON) {
+	if (_contour_type == CONTOUR_POLYGON) {
 		for (int i = 0; i < indent; i++) {
 			fh.writeString("\t");
 		}
 		fh.writeString(Common::String::format("<contour_polygon>%d", contour_size() * 2));
 		for (int j = 0; j < contour_size(); j++) {
-			fh.writeString(Common::String::format(" %d %d", contour_[j].x, contour_[j].y));
+			fh.writeString(Common::String::format(" %d %d", _contour[j].x, _contour[j].y));
 		}
 		fh.writeString("</contour_polygon>\r\n");
 		return true;
 	}
 
-	if (contour_type_ == CONTOUR_RECTANGLE) {
+	if (_contour_type == CONTOUR_RECTANGLE) {
 		for (int i = 0; i < indent; i++) {
 			fh.writeString("\t");
 		}
-		fh.writeString(Common::String::format("<contour_rect>%d %d</contour_rect>\r\n", size_.x, size_.y));
+		fh.writeString(Common::String::format("<contour_rect>%d %d</contour_rect>\r\n", _size.x, _size.y));
 		return true;
 	}
 
-	if (contour_type_ == CONTOUR_CIRCLE) {
+	if (_contour_type == CONTOUR_CIRCLE) {
 		for (int i = 0; i < indent; i++) {
 			fh.writeString("\t");
 		}
-		fh.writeString(Common::String::format("<contour_circle>%d</contour_circle>\r\n", size_.x));
+		fh.writeString(Common::String::format("<contour_circle>%d</contour_circle>\r\n", _size.x));
 		return true;
 	}
 
@@ -207,16 +207,16 @@ bool qdContour::load_script(const xml::tag *p) {
 	Vect2s v;
 	xml::tag_buffer buf(*p);
 
-	switch (contour_type_) {
+	switch (_contour_type) {
 	case CONTOUR_RECTANGLE:
 		buf > v.x > v.y;
-		size_ = Vect2s(v);
+		_size = Vect2s(v);
 		return true;
 	case CONTOUR_CIRCLE:
-		size_.x = size_.y = buf.get_short();
+		_size.x = _size.y = buf.get_short();
 		return true;
 	case CONTOUR_POLYGON:
-		contour_.reserve(p->data_size() / 2);
+		_contour.reserve(p->data_size() / 2);
 		for (i = 0; i < p->data_size(); i += 2) {
 			buf > v.x > v.y;
 			add_contour_point(v);
@@ -229,7 +229,7 @@ bool qdContour::load_script(const xml::tag *p) {
 }
 
 void qdContour::divide_contour(int shift) {
-	std::vector<Vect2s>::iterator _itr = contour_.begin(), _end = contour_.end();
+	std::vector<Vect2s>::iterator _itr = _contour.begin(), _end = _contour.end();
 	for (; _itr != _end; ++_itr) {
 		Vect2s &v = *_itr;
 		v.x >>= shift;
@@ -238,7 +238,7 @@ void qdContour::divide_contour(int shift) {
 }
 
 void qdContour::mult_contour(int shift) {
-	std::vector<Vect2s>::iterator _itr = contour_.begin(), _end = contour_.end();
+	std::vector<Vect2s>::iterator _itr = _contour.begin(), _end = _contour.end();
 	for (; _itr != _end; ++_itr) {
 		Vect2s &v = *_itr;
 		v.x <<= shift;
@@ -247,7 +247,7 @@ void qdContour::mult_contour(int shift) {
 }
 
 void qdContour::shift_contour(int dx, int dy) {
-	std::vector<Vect2s>::iterator _itr = contour_.begin(), _end = contour_.end();
+	std::vector<Vect2s>::iterator _itr = _contour.begin(), _end = _contour.end();
 	for (; _itr != _end; ++_itr) {
 		Vect2s &v = *_itr;
 		v.x += dx;
@@ -256,15 +256,15 @@ void qdContour::shift_contour(int dx, int dy) {
 }
 
 bool qdContour::can_be_closed() const {
-	if (contour_type_ != CONTOUR_POLYGON)
+	if (_contour_type != CONTOUR_POLYGON)
 		return false;
 
-	return (contour_.size() > 2);
+	return (_contour.size() > 2);
 }
 
 bool qdContour::is_contour_empty() const {
-	if (contour_type_ == CONTOUR_POLYGON)
-		return (contour_.size() < 3);
+	if (_contour_type == CONTOUR_POLYGON)
+		return (_contour.size() < 3);
 
 	return false;
 }
