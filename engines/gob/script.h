ff65b7a6f6cc8c98b24faeb61a3306779a7e9d2d
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2024-08-06 14:32:38 +0200

    JANITORIAL: GOB: use uppercase for workarounds

--- engines/gob/script.h
+++ engines/gob/script.h
@@ -124,7 +124,7 @@ public:
 	/** Push the current script position and branch to the specified offset. */
 	void call(uint32 offset);
 
-	/** Override a byte into the loaded script data (used for some script bugs workarounds) */
+	/** Override a byte into the loaded script data (used for some script bugs Workarounds) */
 	void writeByte(int32 offset, byte v);
 
 	// Fixed properties
