72690a0ea95810010d2de4ade332f3c240307184
SupSuper <supsuper@gmail.com>
2024-08-15 23:43:50 +0100

    QDENGINE: Fix int/int32 mismatch

--- engines/qdengine/system/graphics/gr_tile_animation.h
+++ engines/qdengine/system/graphics/gr_tile_animation.h
@@ -69,7 +69,7 @@ public:
 
 	bool load(Common::SeekableReadStream *fh);
 
-	void drawFrame(const Vect2i &position, int frame_index, int mode = 0) const;
+	void drawFrame(const Vect2i &position, int32 frame_index, int32 mode = 0) const;
 	void drawFrame(const Vect2i &position, int frame_index, float angle, int mode = 0) const;
 
 	static void setProgressHandler(CompressionProgressHandler handler, void *context) {
