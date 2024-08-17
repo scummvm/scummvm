d5cd1e7d86ea85d00fd619d403887bd42ea5cb31
Eugene Sandulenko <sev@scummvm.org>
2024-08-17 21:20:09 +0200

    QDENGINE: Hid noisy debug trace

--- engines/qdengine/qdcore/util/plaympp_api.cpp
+++ engines/qdengine/qdcore/util/plaympp_api.cpp
@@ -132,7 +132,7 @@ mpegPlayer::mpeg_status_t mpegPlayer::status() const {
 	else
 		res = MPEG_STOPPED;
 
-	debugC(1, kDebugSound, "mpegPlayer::status(%s), status: %d", _file.c_str(), res);
+	debugC(5, kDebugSound, "mpegPlayer::status(%s), status: %d", _file.c_str(), res);
 
 	return res;
 }
