09613937f22fe0e95e775f676e9712b1fb42b8c1
Walter Agazzi <walter.agazzi@protonmail.com>
2024-08-19 21:36:32 +0200

    AGS: Engine: unrestricted script name an filename in ScriptAudioClip
    NOTE: we still read the old fields from game data, avoid changing the
    data format in the middle (in the sake of both backward and forward compatibility).
    These fixed-sized values are assigned to the clip properties first.
    For unrestricted values we'll have to add a data extension, appended to the end of data format.
    From upstream 0aa62d14cadcb77dfcf1351a962969069c5d1796

--- engines/ags/shared/ac/dynobj/script_audio_clip.cpp
+++ engines/ags/shared/ac/dynobj/script_audio_clip.cpp
@@ -28,8 +28,8 @@ using namespace AGS::Shared;
 
 void ScriptAudioClip::ReadFromFile(Stream *in) {
 	id = in->ReadInt32();
-	scriptName.ReadCount(in, SCRIPTAUDIOCLIP_SCRIPTNAMELENGTH);
-	fileName.ReadCount(in, SCRIPTAUDIOCLIP_FILENAMELENGTH);
+	scriptName.ReadCount(in, LEGACY_AUDIOCLIP_SCRIPTNAMELENGTH);
+	fileName.ReadCount(in, LEGACY_AUDIOCLIP_FILENAMELENGTH);
 	bundlingType = static_cast<uint8_t>(in->ReadInt8());
 	type = static_cast<uint8_t>(in->ReadInt8());
 	fileType = static_cast<AudioFileType>(in->ReadInt8());
