bf1752eec9861dc6782a1101dad31d90be8d72ca
Hubert Maier <raziel-@users.noreply.github.com>
2024-09-09 20:48:22 +0200

    JANITORIAL: Fix cant and afterwords typos in creature.h

--- engines/ultima/ultima4/game/creature.h
+++ engines/ultima/ultima4/game/creature.h
@@ -309,7 +309,7 @@ public:
 	/**
 	 * Performs a special action for the creature
 	 * Returns true if the action takes up the creatures
-	 * whole turn (i.e. it cant move afterwords)
+	 * whole turn (i.e. it can't move afterwards)
 	 */
 	bool specialAction();
 
