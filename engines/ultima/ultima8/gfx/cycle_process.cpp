5df56155ebae431c706a842aa339c7d0be630f3a
Hubert Maier <raziel-@users.noreply.github.com>
2024-09-09 18:39:02 +0200

    JANITORIAL: Fix persistant typo in cycle_process.cpp

--- engines/ultima/ultima8/gfx/cycle_process.cpp
+++ engines/ultima/ultima8/gfx/cycle_process.cpp
@@ -136,7 +136,7 @@ bool CycleProcess::loadData(Common::ReadStream *rs, uint32 version) {
 	_running = rs->readByte();
 	_instance = this; //static
 
-	_type = 1; // should be persistant but older savegames may not know that.
+	_type = 1; // should be persistent but older savegames may not know that.
 	return true;
 }
 
