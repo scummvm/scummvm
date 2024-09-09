f0d0caa6d1c5178dedae390d4fe77c2fc41c81e0
Walter Agazzi <walter.agazzi@protonmail.com>
2024-09-09 14:33:04 +0200

    AGS: Engine: when fast-forwarding, skip blocking Say instantly
    From upstream 67e77534a64cb6bb464f72ffdeb41630f8126af4

--- engines/ags/engine/ac/display.cpp
+++ engines/ags/engine/ac/display.cpp
@@ -259,9 +259,12 @@ ScreenOverlay *_display_main(int xx, int yy, int wii, const char *text, int disp
 	if (disp_type < DISPLAYTEXT_NORMALOVERLAY)
 		remove_screen_overlay(_GP(play).text_overlay_on);
 
-	int adjustedXX, adjustedYY;
-	bool alphaChannel;
-	Bitmap *text_window_ds = create_textual_image(text, asspch, isThought, xx, yy, adjustedXX, adjustedYY, wii, usingfont, allowShrink, alphaChannel);
+	// If fast-forwarding, then skip any blocking message immediately
+	if (_GP(play).fast_forward && (disp_type < DISPLAYTEXT_NORMALOVERLAY)) {
+		_GP(play).SetWaitSkipResult(SKIP_AUTOTIMER);
+		_GP(play).messagetime = -1;
+		return nullptr;
+	}
 
 	//
 	// Configure and create an overlay object
@@ -274,6 +277,10 @@ ScreenOverlay *_display_main(int xx, int yy, int wii, const char *text, int disp
 	case DISPLAYTEXT_NORMALOVERLAY: ovrtype = OVER_CUSTOM; break;
 	default: ovrtype = disp_type; break; // must be precreated overlay id
 	}
+
+	int adjustedXX, adjustedYY;
+	bool alphaChannel;
+	Bitmap *text_window_ds = create_textual_image(text, asspch, isThought, xx, yy, adjustedXX, adjustedYY, wii, usingfont, allowShrink, alphaChannel);
 
 	size_t nse = add_screen_overlay(roomlayer, xx, yy, ovrtype, text_window_ds, adjustedXX - xx, adjustedYY - yy, alphaChannel);
 	auto *over = get_overlay(nse); // FIXME: optimize return value
@@ -289,13 +296,6 @@ ScreenOverlay *_display_main(int xx, int yy, int wii, const char *text, int disp
 	//
 
 	if (disp_type == DISPLAYTEXT_MESSAGEBOX) {
-		// If fast-forwarding, then skip immediately
-		if (_GP(play).fast_forward) {
-			remove_screen_overlay(OVER_TEXTMSG);
-			_GP(play).SetWaitSkipResult(SKIP_AUTOTIMER);
-			post_display_cleanup();
-			return nullptr;
-		}
 
 		int countdown = GetTextDisplayTime(text);
 		int skip_setting = user_to_internal_skip_speech((SkipSpeechStyle)_GP(play).skip_display);
@@ -366,11 +366,6 @@ ScreenOverlay *_display_main(int xx, int yy, int wii, const char *text, int disp
 		invalidate_screen();
 	} else {
 		/* DISPLAYTEXT_SPEECH */
-		// if the speech does not time out, but we are skipping a cutscene,
-		// allow it to time out
-		if ((_GP(play).messagetime < 0) && (_GP(play).fast_forward))
-			_GP(play).messagetime = 2;
-
 		if (!overlayPositionFixed) {
 			over->SetRoomRelative(true);
 			VpPoint vpt = _GP(play).GetRoomViewport(0)->ScreenToRoom(over->x, over->y, false);
