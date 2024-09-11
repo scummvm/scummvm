60ea0ff71dad1810e8915f76bdd582daf30e17a2
Torbjörn Andersson <eriktorbjorn@users.sourceforge.net>
2024-09-11 22:09:32 +0200

    SCUMM: Rename options variables to better reflect their purpose
    I assume the "ObjectLabels" bit was cut and pasted from the one option
    that actually had anything to do with object labels.

--- engines/scumm/metaengine.cpp
+++ engines/scumm/metaengine.cpp
@@ -650,7 +650,7 @@ static const ExtraGuiOption comiObjectLabelsOption = {
 	0
 };
 
-static const ExtraGuiOption mmnesObjectLabelsOption = {
+static const ExtraGuiOption mmnesClassicPaletteOption = {
 	_s("Use NES Classic Palette"),
 	_s("Use a more neutral color palette that closely emulates the NES Classic"),
 	"mm_nes_classic_palette",
@@ -761,7 +761,7 @@ static const ExtraGuiOption enableCopyProtection = {
 	0
 };
 
-static const ExtraGuiOption mmDemoObjectLabelsOption = {
+static const ExtraGuiOption mmDemoModeOption = {
 	_s("Enable demo/kiosk mode"),
 	_s("Enable demo/kiosk mode in the full retail version of Maniac Mansion."),
 	"enable_demo_mode",
@@ -803,7 +803,7 @@ const ExtraGuiOptions ScummMetaEngine::getExtraGuiOptions(const Common::String &
 		}
 	}
 	if (target.empty() || platform == Common::kPlatformNES) {
-		options.push_back(mmnesObjectLabelsOption);
+		options.push_back(mmnesClassicPaletteOption);
 	}
 	if (target.empty() || platform == Common::kPlatformFMTowns) {
 		options.push_back(smoothScrolling);
@@ -825,7 +825,7 @@ const ExtraGuiOptions ScummMetaEngine::getExtraGuiOptions(const Common::String &
 			 !guiOptionsString.contains("lang_Italian");
 
 		if (isValidTarget)
-			options.push_back(mmDemoObjectLabelsOption);
+			options.push_back(mmDemoModeOption);
     }
 	// The Steam Mac versions of Loom and Indy 3 are more akin to the VGA
 	// DOS versions, and that's how ScummVM usually sees them. But that
