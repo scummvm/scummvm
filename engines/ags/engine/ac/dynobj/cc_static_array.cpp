48bd53d83f2a1a1e2fcb71131eae102713b29560
Walter Agazzi <walter.agazzi@protonmail.com>
2024-09-11 15:24:24 +0200

    AGS: Engine: tidy CCStaticArray
    From upstream 30c56a93d7e66974e3f86c687c2fe169f7de6b81

--- engines/ags/engine/ac/dynobj/cc_static_array.cpp
+++ engines/ags/engine/ac/dynobj/cc_static_array.cpp
@@ -24,15 +24,15 @@
 
 namespace AGS3 {
 
-void CCStaticArray::Create(ICCDynamicObject *mgr, int elem_legacy_size, int elem_real_size, int elem_count) {
+void CCStaticArray::Create(ICCDynamicObject *mgr, size_t elem_script_size, size_t elem_mem_size, size_t elem_count) {
 	_mgr = mgr;
-	_elemLegacySize = elem_legacy_size;
-	_elemRealSize = elem_real_size;
+	_elemScriptSize = elem_script_size;
+	_elemMemSize = elem_mem_size;
 	_elemCount = elem_count;
 }
 
 const char *CCStaticArray::GetElementPtr(const char *address, intptr_t legacy_offset) {
-	return address + (legacy_offset / _elemLegacySize) * _elemRealSize;
+	return address + (legacy_offset / _elemScriptSize) * _elemMemSize;
 }
 
 const char *CCStaticArray::GetFieldPtr(const char *address, intptr_t offset) {
@@ -41,52 +41,52 @@ const char *CCStaticArray::GetFieldPtr(const char *address, intptr_t offset) {
 
 void CCStaticArray::Read(const char *address, intptr_t offset, void *dest, int size) {
 	const char *el_ptr = GetElementPtr(address, offset);
-	return _mgr->Read(el_ptr, offset % _elemLegacySize, dest, size);
+	return _mgr->Read(el_ptr, offset % _elemScriptSize, dest, size);
 }
 
 uint8_t CCStaticArray::ReadInt8(const char *address, intptr_t offset) {
 	const char *el_ptr = GetElementPtr(address, offset);
-	return _mgr->ReadInt8(el_ptr, offset % _elemLegacySize);
+	return _mgr->ReadInt8(el_ptr, offset % _elemScriptSize);
 }
 
 int16_t CCStaticArray::ReadInt16(const char *address, intptr_t offset) {
 	const char *el_ptr = GetElementPtr(address, offset);
-	return _mgr->ReadInt16(el_ptr, offset % _elemLegacySize);
+	return _mgr->ReadInt16(el_ptr, offset % _elemScriptSize);
 }
 
 int32_t CCStaticArray::ReadInt32(const char *address, intptr_t offset) {
 	const char *el_ptr = GetElementPtr(address, offset);
-	return _mgr->ReadInt32(el_ptr, offset % _elemLegacySize);
+	return _mgr->ReadInt32(el_ptr, offset % _elemScriptSize);
 }
 
 float CCStaticArray::ReadFloat(const char *address, intptr_t offset) {
 	const char *el_ptr = GetElementPtr(address, offset);
-	return _mgr->ReadFloat(el_ptr, offset % _elemLegacySize);
+	return _mgr->ReadFloat(el_ptr, offset % _elemScriptSize);
 }
 
 void CCStaticArray::Write(const char *address, intptr_t offset, void *src, int size) {
 	const char *el_ptr = GetElementPtr(address, offset);
-	return _mgr->Write(el_ptr, offset % _elemLegacySize, src, size);
+	return _mgr->Write(el_ptr, offset % _elemScriptSize, src, size);
 }
 
 void CCStaticArray::WriteInt8(const char *address, intptr_t offset, uint8_t val) {
 	const char *el_ptr = GetElementPtr(address, offset);
-	return _mgr->WriteInt8(el_ptr, offset % _elemLegacySize, val);
+	return _mgr->WriteInt8(el_ptr, offset % _elemScriptSize, val);
 }
 
 void CCStaticArray::WriteInt16(const char *address, intptr_t offset, int16_t val) {
 	const char *el_ptr = GetElementPtr(address, offset);
-	return _mgr->WriteInt16(el_ptr, offset % _elemLegacySize, val);
+	return _mgr->WriteInt16(el_ptr, offset % _elemScriptSize, val);
 }
 
 void CCStaticArray::WriteInt32(const char *address, intptr_t offset, int32_t val) {
 	const char *el_ptr = GetElementPtr(address, offset);
-	return _mgr->WriteInt32(el_ptr, offset % _elemLegacySize, val);
+	return _mgr->WriteInt32(el_ptr, offset % _elemScriptSize, val);
 }
 
 void CCStaticArray::WriteFloat(const char *address, intptr_t offset, float val) {
 	const char *el_ptr = GetElementPtr(address, offset);
-	return _mgr->WriteFloat(el_ptr, offset % _elemLegacySize, val);
+	return _mgr->WriteFloat(el_ptr, offset % _elemScriptSize, val);
 }
 
 } // namespace AGS3
