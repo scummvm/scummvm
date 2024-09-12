6229b253922552fdf552bcee248ec9e405cdafdc
Walter Agazzi <walter.agazzi@protonmail.com>
2024-09-12 20:37:04 +0200

    AGS: Engine: store game's ccInstances as unique_ptrs instead
    The script handling part of the engine appeared to be too fragile and dependent on
     a precise timing of **deleting** a ccInstance.
    For example, restoring a saved game may execute in the midst of the call to
    RunScriptFunction(), at the "post script cleanup" stage, while there are still references
    to ccInstance on the engine's call stack (and few other places). If anything prevents
     ccInstance's disposal at this moment, that would in turn lead to logical errors due to
      the game scripts "instances" counting. The most outstanding consequence is that the
       script exports are kept pointing into the deleted memory and not re-registered after the save is restored.
    
    Overall, the script handling part of the engine would benefit from a major refactor,
     with a goal to make it safe and consistent, but this is not something I might do at the moment.
    
    From upstream 1f6abf4b678f77825c710d4ae18b57ff8f882c93

--- engines/ags/engine/script/cc_instance.cpp
+++ engines/ags/engine/script/cc_instance.cpp
@@ -211,13 +211,13 @@ void ccInstance::FreeInstanceStack() {
 	_GP(InstThreads).clear();
 }
 
-PInstance ccInstance::CreateFromScript(PScript scri) {
+ccInstance *ccInstance::CreateFromScript(PScript scri) {
 	return CreateEx(scri, nullptr);
 }
 
-PInstance ccInstance::CreateEx(PScript scri, ccInstance *joined) {
+ccInstance *ccInstance::CreateEx(PScript scri, ccInstance *joined) {
 	// allocate and copy all the memory with data, code and strings across
-	std::shared_ptr<ccInstance> cinst(new ccInstance());
+	ccInstance *cinst = new ccInstance();
 	if (!cinst->_Create(scri, joined)) {
 		return nullptr;
 	}
@@ -263,7 +263,7 @@ ccInstance::~ccInstance() {
 	Free();
 }
 
-PInstance ccInstance::Fork() {
+ccInstance *ccInstance::Fork() {
 	return CreateEx(instanceof, this);
 }
 
@@ -1715,6 +1715,8 @@ bool ccInstance::_Create(PScript scri, ccInstance *joined) {
 }
 
 void ccInstance::Free() {
+	// When the base script has no more "instances",
+	// remove all script exports
 	if (instanceof != nullptr) {
 		instanceof->instances--;
 		if (instanceof->instances == 0) {
