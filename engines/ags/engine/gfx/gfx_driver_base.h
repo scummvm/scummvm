f83f754e690ab286649d96a7802dc34edec825a0
Walter Agazzi <walter.agazzi@protonmail.com>
2024-09-15 16:53:29 +0200

    AGS: Engine: separated BitmapToVideoMemLinearImpl for convenience
    Split BitmapToVideoMemImpl into simpler BitmapToVideoMemImpl and
     BitmapToVideoMemLinearImpl meant for that "linear filter" transparency fix.
    This does not seem to make things noticeably faster with non-linear scaling,
     but may make it easier to understand the code.
    
    Also in case we'd like to remove/replace this hack someday.
    From upstream 5c2129b8f176e8d95b0f26132bf1749d7dabdc29

--- engines/ags/engine/gfx/gfx_driver_base.h
+++ engines/ags/engine/gfx/gfx_driver_base.h
@@ -351,17 +351,14 @@ private:
 	size_t _fxIndex; // next free pool item
 
 	// specialized method to convert bitmap to video memory depending on bit depth
-	template<typename T, bool HasAlpha, bool UsingLinearFiltering>
-	void
-	BitmapToVideoMemImpl(
-		const Bitmap *bitmap, const TextureTile *tile,
-		uint8_t *dst_ptr, const int dst_pitch);
+	template<typename T, bool HasAlpha>
+	void BitmapToVideoMemImpl(const Bitmap *bitmap, const TextureTile *tile, uint8_t *dst_ptr, const int dst_pitch);
 
 	template<typename T>
-	void
-	BitmapToVideoMemOpaqueImpl(
-		const Bitmap *bitmap, const TextureTile *tile,
-		uint8_t *dst_ptr, const int dst_pitch);
+	void BitmapToVideoMemOpaqueImpl(const Bitmap *bitmap, const TextureTile *tile, uint8_t *dst_ptr, const int dst_pitch);
+
+	template<typename T, bool HasAlpha>
+	void BitmapToVideoMemLinearImpl(const Bitmap *bitmap, const TextureTile *tile, uint8_t *dst_ptr, const int dst_pitch);
 
 	// Texture short-term cache:
 	// - caches textures while they are in the immediate use;
