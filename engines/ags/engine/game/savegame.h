8f450ec188e2c51d418fe394e34537e8a0564070
Walter Agazzi <walter.agazzi@protonmail.com>
2024-09-20 20:11:11 +0200

    AGS: Engine: raise SavegameVersion for 3.6.1
    From upstream 06fde28f9965d1b108f28d000bf828348e576bc6

--- engines/ags/engine/game/savegame.h
+++ engines/ags/engine/game/savegame.h
@@ -64,7 +64,8 @@ enum SavegameVersion {
 	kSvgVersion_351 = 13,
 	kSvgVersion_360_beta = 3060023,
 	kSvgVersion_360_final = 3060041,
-	kSvgVersion_Current = kSvgVersion_360_final,
+	kSvgVersion_361 = 3060115,
+	kSvgVersion_Current = kSvgVersion_361,
 	kSvgVersion_LowestSupported = kSvgVersion_321 // change if support dropped
 };
 
