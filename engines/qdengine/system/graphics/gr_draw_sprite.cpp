273b4da5c4811bb98556797dd1e0dc6dbef83a43
Eugene Sandulenko <sev@scummvm.org>
2024-09-20 23:50:30 +0200

    QDENGINE: Fix grDispatcher::putSprMask_a() and remove stub warning

--- engines/qdengine/system/graphics/gr_draw_sprite.cpp
+++ engines/qdengine/system/graphics/gr_draw_sprite.cpp
@@ -1137,7 +1137,6 @@ void grDispatcher::putSprMask_a(int x, int y, int sx, int sy, const byte *p, uin
 	} else
 		dy = 1;
 
-	x <<= 1;
 	sx <<= 2;
 	px <<= 2;
 
@@ -1146,7 +1145,6 @@ void grDispatcher::putSprMask_a(int x, int y, int sx, int sy, const byte *p, uin
 	byte mr, mg, mb;
 	split_rgb565u(mask_color, mr, mg, mb);
 
-	warning("STUB: grDispatcher::putSprMask_a");
 	for (int i = 0; i < psy; i++) {
 		uint16 *scr_buf = (uint16 *)(_screenBuf->getBasePtr(x, y));
 		const byte *data_line = data_ptr;
