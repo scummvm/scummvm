a8589d81d08911b963e3f4069bc39969b6bc0160
Matthew Duggan <mgithub@guarana.org>
2024-09-23 17:20:12 +1000

    DGDS: Init array to make coverity happy

--- engines/dgds/dragon_arcade_ttm.cpp
+++ engines/dgds/dragon_arcade_ttm.cpp
@@ -295,7 +295,7 @@ int16 DragonArcadeTTM::runScriptPage(int16 pageNum) {
 
 	uint16 opcode = scr->readUint16LE();
 	while (opcode != 0x0ff0 && opcode) {
-		int16 ivals[4];
+		int16 ivals[4] { 0, 0, 0, 0 };
 		Common::String sval;
 		byte count = (byte)(opcode & 0xf);
 		if (count <= 4) {
