2a0c3d1343ad13867f976f6e93eee0668b206cc3
Florian Mehm <florian@mehm.net>
2024-10-03 13:15:05 +0200

    MACS2: Add gArray222C and use it in OnTimer channel check
    Replace placeholder comparison with actual gArray222C[bp8] == 0
    check at l0017_1BF3 in the note-on handler.

--- engines/macs2/adlib.h
+++ engines/macs2/adlib.h
@@ -81,6 +81,10 @@ namespace Macs2 {
 		// [225Ah] - TODO: Initial value?
 		uint16 g225A;
 
+		// Array at [222Ch] - accessed with byte values
+		// TODO: Initialization, access pattern
+		Common::Array<uint8> gArray222C;
+
 		bool g229A;
 
 		// [229Bh] - seems to contain song byte - TODO: Initial value?
