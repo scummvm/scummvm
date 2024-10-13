1f1cab483ee6b929fb9fdbb9bf692a971701a99d
Paweł Kołodziejski <aquadran@gmail.com>
2024-10-13 22:43:09 +0200

    GRIM: Bump savegame minor version to flag fixed broken patch

--- engines/grim/savegame.cpp
+++ engines/grim/savegame.cpp
@@ -34,7 +34,7 @@ namespace Grim {
 #define SAVEGAME_FOOTERTAG  'ESAV'
 
 uint SaveGame::SAVEGAME_MAJOR_VERSION = 22;
-uint SaveGame::SAVEGAME_MINOR_VERSION = 28;
+uint SaveGame::SAVEGAME_MINOR_VERSION = 29;
 
 SaveGame *SaveGame::openForLoading(const Common::String &filename) {
 	Common::InSaveFile *inSaveFile = g_system->getSavefileManager()->openForLoading(filename);
