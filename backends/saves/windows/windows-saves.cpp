9eab979bfb6d4abeb3b38d97f67ebf8d576e6237
sluicebox <22204938+sluicebox@users.noreply.github.com>
2024-10-14 23:02:55 -0600

    WIN32: Make deleting save files UNICODE compatible

--- backends/saves/windows/windows-saves.cpp
+++ backends/saves/windows/windows-saves.cpp
@@ -54,4 +54,17 @@ WindowsSaveFileManager::WindowsSaveFileManager(bool isPortable) {
 	ConfMan.registerDefault("savepath", Common::Path(Win32::tcharToString(defaultSavepath), Common::Path::kNativeSeparator));
 }
 
+Common::ErrorCode WindowsSaveFileManager::removeFile(const Common::FSNode &fileNode) {
+	TCHAR *tFile = Win32::stringToTchar(fileNode.getPath().toString(Common::Path::kNativeSeparator));
+	int result = _tremove(tFile);
+	free(tFile);
+	if (result == 0)
+		return Common::kNoError;
+	if (errno == EACCES)
+		return Common::kWritePermissionDenied;
+	if (errno == ENOENT)
+		return Common::kPathDoesNotExist;
+	return Common::kUnknownError;
+}
+
 #endif
