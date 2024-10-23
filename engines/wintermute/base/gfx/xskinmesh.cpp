87ef8d1391b1804d75c37e5f6ee52a9fd312f5a8
Paweł Kołodziejski <aquadran@gmail.com>
2024-10-23 07:48:57 +0200

    WINTERMUTE: Revert back to previous face indices order

--- engines/wintermute/base/gfx/xskinmesh.cpp
+++ engines/wintermute/base/gfx/xskinmesh.cpp
@@ -1122,17 +1122,17 @@ bool DXLoadSkinMesh(XFileData *fileData, DXBuffer &materialsOut, uint32 &numMate
 	for (i = 0; i < meshData._numPolyFaces; i++) {
 		uint32 count = meshData._numTriPerFace[i];
 		uint32 firstIndex = *indexInPtr++;
-		// 1 -> 1
-		// 2 -> 2
-		// 3 -> 3
-		// 1 -> 4
-		// 3 -> 5
-		// 4 -> 6
+		// 1 -> 1 -> 3
+		// 2 -> 2 -> 2
+		// 3 -> 3 -> 1
+		// 1 -> 4 -> 6
+		// 3 -> 5 -> 5
+		// 4 -> 6 -> 4
 		while (count--) {
-			*indices++ = firstIndex;
-			*indices++ = *indexInPtr;
-			indexInPtr++;
-			*indices++ = *indexInPtr;
+			indices[2] = firstIndex;
+			indices[1] = *indexInPtr++;
+			indices[0] = *indexInPtr;
+			indices += 3;
 		}
 		indexInPtr++;
 	}
