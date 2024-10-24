scummvm/scummvm

commit 8ac2ed039fc3e3a3bbdf373d309153bee67b306b

TWINE: renamed members and methods to match original sources

engines/twine/menu/menu.cpp
@@ -1067,7 +1067,7 @@ void Menu::drawCoins(int32 left, int32 top) {
 }
 
 void Menu::drawKeys(int32 left, int32 top) {
-	const Common::String &inventoryNumKeys = Common::String::format("%d", _engine->_gameState->_inventoryNumKeys);
+	const Common::String &inventoryNumKeys = Common::String::format("%d", _engine->_gameState->_nbLittleKeys);
 	drawSpriteAndString(left, top, _engine->_resources->_spriteData[SPRITEHQR_KEY], inventoryNumKeys);
 }
 

engines/twine/debugger/console.cpp
@@ -368,7 +368,7 @@ bool TwinEConsole::doChangeScene(int argc, const char **argv) {
 		return true;
 	}
 	_engine->_scene->_newCube = atoi(argv[1]);
-	_engine->_scene->_heroPositionType = ScenePositionType::kScene;
+	_engine->_scene->_flagChgCube = ScenePositionType::kScene;
 	_engine->_scene->changeCube();
 	return true;
 }

engines/twine/debugger/debugtools.cpp
@@ -456,7 +456,7 @@ static void sceneDetailsWindows(TwinEEngine *engine) {
 		ImGuiEx::InputInt("Alpha light", &scene->_alphaLight);
 		ImGuiEx::InputInt("Beta light", &scene->_betaLight);
 		ImGuiEx::InputInt("Fall Y position", &scene->_startYFalling);
-		ImGui::Text("Hero position type: %i", (int)scene->_heroPositionType);
+		ImGui::Text("Hero position type: %i", (int)scene->_flagChgCube);
 	}
 	ImGui::End();
 }
@@ -511,7 +511,7 @@ static void actorDetailsWindow(int &actorIdx, TwinEEngine *engine) {
 				ImGui::TableNextColumn();
 				ImGui::Text("Control mode");
 				ImGui::TableNextColumn();
-				ImGui::Text("%i", actor->_controlMode);
+				ImGui::Text("%i", actor->_move);
 				ImGui::TableNextColumn();
 				ImGui::Text("Delay");
 				ImGui::TableNextColumn();
@@ -698,7 +698,7 @@ static void actorDetailsWindow(int &actorIdx, TwinEEngine *engine) {
 
 static void gameStateMenu(TwinEEngine *engine) {
 	if (ImGui::BeginMenu("Game State")) {
-		int keys = engine->_gameState->_inventoryNumKeys;
+		int keys = engine->_gameState->_nbLittleKeys;
 		if (ImGui::InputInt("Keys", &keys)) {
 			engine->_gameState->setKeys(keys);
 		}

engines/twine/renderer/redraw.cpp
@@ -872,42 +872,39 @@ void Redraw::renderText() {
 	addPhysBox(redraw);
 }
 
-void Redraw::drawScene(bool bgRedraw) { // AffScene
+void Redraw::drawScene(bool flagflip) { // AffScene
 	int32 tmp_projPosX = _projPosScreen.x;
 	int32 tmp_projPosY = _projPosScreen.y;
 
 	_engine->_interface->unsetClip();
 
-	if (bgRedraw) {
+	if (!flagflip) {
+		clsBoxes();
+	} else {
 		_engine->saveTimer(false);
 		if (_engine->_scene->_newCube != SCENE_CEILING_GRID_FADE_1 && _engine->_scene->_newCube != SCENE_CEILING_GRID_FADE_2) {
 			_engine->_screens->fadeToBlack(_engine->_screens->_ptrPal);
 		}
-		_engine->_screens->clearScreen();
 
 		_engine->_grid->redrawGrid();
-		const IVec3 &projPos = _engine->_renderer->projectPoint(-_engine->_grid->_worldCube);
-		_projPosScreen.x = projPos.x;
-		_projPosScreen.y = projPos.y;
 
 		updateOverlayTypePosition(tmp_projPosX, tmp_projPosY, _projPosScreen.x, _projPosScreen.y);
+
 		_engine->saveFrontBuffer();
 
 		if (_engine->_scene->_newCube != SCENE_CEILING_GRID_FADE_1 && _engine->_scene->_newCube != SCENE_CEILING_GRID_FADE_2) {
 			_engine->_screens->fadeToPal(_engine->_screens->_ptrPal);
 		}
-	} else {
-		clsBoxes();
 	}
 
 	DrawListStruct drawList[NUM_MAX_ACTORS + EXTRA_MAX_ENTRIES]; // ListTri[MAX_OBJECTS + MAX_EXTRAS]
-	int32 drawListPos = fillActorDrawingList(drawList, bgRedraw);
+	int32 drawListPos = fillActorDrawingList(drawList, flagflip);
 	drawListPos = fillExtraDrawingList(drawList, drawListPos);
 
 	_nbPhysBox = 0;
 	sortDrawingList(drawList, drawListPos);
 	correctZLevels(drawList, drawListPos);
-	processDrawList(drawList, drawListPos, bgRedraw);
+	processDrawList(drawList, drawListPos, flagflip);
 
 	_engine->_debugState->renderDebugView();
 
@@ -921,7 +918,7 @@ void Redraw::drawScene(bool bgRedraw) { // AffScene
 		_engine->_scene->_newCube = SCENE_CEILING_GRID_FADE_1;
 	}
 
-	if (bgRedraw) {
+	if (flagflip) {
 		moveNextAreas();
 		_engine->restoreTimer();
 	} else {

engines/twine/renderer/redraw.h
@@ -113,8 +113,6 @@ private:
 	int32 _bubbleActor = -1;
 	int32 _bubbleSpriteIndex;
 
-	IVec3 _projPosScreen; // XpOrgw, YpOrgw
-
 	// big font shadow text in the lower left corner
 	Common::String _text;
 	int32 _textDisappearTime = -1;
@@ -150,6 +148,8 @@ public:
 
 	/** Request background redraw */
 	bool _firstTime = false;
+
+	IVec3 _projPosScreen; // XpOrgw, YpOrgw
 
 	/** Current number of redraw regions in the screen */
 	int32 _nbPhysBox = 0; // fullRedrawVar8

engines/twine/scene/actor.cpp
@@ -42,9 +42,9 @@ namespace TwinE {
 Actor::Actor(TwinEEngine *engine) : _engine(engine) {
 }
 
-void Actor::restartHeroScene() {
+void Actor::restartPerso() {
 	ActorStruct *sceneHero = _engine->_scene->_sceneHero;
-	sceneHero->_controlMode = ControlMode::kManual;
+	sceneHero->_move = ControlMode::kManual;
 	memset(&sceneHero->_workFlags, 0, sizeof(sceneHero->_workFlags));
 	memset(&sceneHero->_flags, 0, sizeof(sceneHero->_flags));
 

engines/twine/scene/actor.h
@@ -194,7 +194,7 @@ public:
 	BonusParameter _bonusParameter;
 	int32 _beta = 0; // facing angle of actor. Minumum is 0 (SW). Going counter clock wise
 	int32 _srot = 40; // speed of rotation
-	ControlMode _controlMode = ControlMode::kNoMove; // Move
+	ControlMode _move = ControlMode::kNoMove; // Move
 	int32 _delayInMillis = 0; // Info
 	int32 _cropLeft = 0;      // Info
 	int32 _cropTop = 0;       // Info1
@@ -326,7 +326,7 @@ public:
 	void setFrame(int32 actorIdx, uint32 frame);
 
 	/** Restart hero variables while opening new scenes */
-	void restartHeroScene();
+	void restartPerso();
 
 	/** Load hero 3D body and animations */
 	void loadHeroEntities();

engines/twine/scene/animations.cpp
@@ -59,9 +59,9 @@ int32 Animations::searchAnim(AnimationTypes animIdx, int32 actorIdx) {
 
 int16 Animations::patchInterAngle(int32 deltaTime, int32 keyFrameLength, int16 newAngle1, int16 lastAngle1) const {
 	const int16 lastAngle = ClampAngle(lastAngle1);
-	const int16 newAngle = ClampAngle(newAngle1);
+	const int16 nextAngle = ClampAngle(newAngle1);
 
-	int16 angleDiff = newAngle - lastAngle;
+	int16 angleDiff = nextAngle - lastAngle;
 
 	int16 computedAngle;
 	if (angleDiff) {

commit fcab03efd6cda1c023456632f608196900bd06c3

TWINE: Glitched background during main menu idle credits
8ac2ed039fc3e3a3bbdf373d309153bee67b306b introduced the regression

engines/twine/menu/menu.cpp
@@ -471,6 +471,7 @@ void Menu::menuDemo() {
 			}
 		}
 	}
+	_engine->_screens->loadMenuImage(false);
 }
 
 int32 Menu::doGameMenu(MenuSettings *menuSettings) {
@@ -952,7 +953,6 @@ EngineState Menu::run() {
 	}
 	case kDemoMenu: {
 		menuDemo();
-		_engine->_screens->loadMenuImage(false);
 		break;
 	}
 	case (int32)TextId::kQuit:

engines/twine/menu/menuoptions.cpp
@@ -104,6 +104,7 @@ void MenuOptions::showCredits() {
 	_engine->_gameState->initEngineVars();
 	_engine->_scene->_numCube = LBA1SceneId::Credits_List_Sequence;
 	_engine->_scene->_newCube = LBA1SceneId::Credits_List_Sequence;
+	_engine->_screens->clearScreen();
 
 	flagCredits = true;
 	_engine->mainLoop();