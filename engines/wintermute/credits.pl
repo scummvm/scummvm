0e56a2453ec1706aae3dc177dbfefc7f4019ba35
Paweł Kołodziejski <aquadran@gmail.com>
2024-10-25 15:01:26 +0200

    WINTERMUTE: Update credits

--- engines/wintermute/credits.pl
+++ engines/wintermute/credits.pl
@@ -1,5 +1,7 @@
 begin_section("Wintermute");
 	add_person("Gunnar Birke", "Fury", "Wintermute 3D");
+	add_person("Pawe&#322; Ko&#322;odziejski", "aquadran", "Wintermute 3D");
 	add_person("Einar Johan T. S&oslash;m&aring;en", "somaen", "");
 	add_person("Tobia Tesan", "t0by", "");
+	add_person("", "lolbot-iichan", "");
 end_section();
