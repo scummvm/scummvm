5e8bd1094fdb9d1930e142002343d3e7c02f2302
neuromancer <gustavo.grieco@gmail.com>
2024-10-26 10:53:55 +0200

    FREESCAPE: missing entrance in castle for dos

--- engines/freescape/games/castle/castle.cpp
+++ engines/freescape/games/castle/castle.cpp
@@ -682,6 +682,7 @@ void CastleEngine::loadAssets() {
 	if (isDOS()) {
 		for (auto &it : _areaMap) {
 			it._value->addStructure(_areaMap[255]);
+			it._value->addObjectFromArea(227, _areaMap[255]);
 			it._value->addObjectFromArea(229, _areaMap[255]);
 			it._value->addObjectFromArea(242, _areaMap[255]);
 			it._value->addObjectFromArea(139, _areaMap[255]);
