scummvm/scummvm

commit ac0340369c4fdd707b8040944ed10393f701a3c9

TSAGE: Remove usage of deprecated ManagedSurface methods

engines/tsage/scenes.cpp
@@ -463,7 +463,8 @@ void Scene::refreshBackground(int xAmount, int yAmount) {
 				// Check if the section is already loaded
 				if ((_enabledSections[xp * 16 + yp] == 0xffff) || ((xAmount == 0) && (yAmount == 0))) {
 					// Chunk isn't loaded, so load it in
-					Graphics::ManagedSurface s = _backSurface.lockSurface();
+					Graphics::ManagedSurface s;
+					s.copyFrom(_backSurface.lockSurface());
 					GfxSurface::loadScreenSection(s, xp - xHalfOffset, yp - yHalfOffset, xp, yp);
 					_backSurface.unlockSurface();
 					changedFlag = true;

engines/tsage/graphics.h
@@ -278,7 +278,7 @@ public:
 	void getStringBounds(const char *s, Rect &bounds, int maxWidth);
 
 	void setDialogPalette();
-	Graphics::ManagedSurface lockSurface() {
+	Graphics::ManagedSurface &lockSurface() {
 		_surface.setBounds(_bounds);
 		return _surface.lockSurface();
 	}

commit a418d03b2579048dacdde708048a0587082c6065

TSAGE: Fix background refresh regression, after ManagedSurface changes
Screen sections should be refreshed into the actual back surface, not a
copy of it. A regression from ac0340369c4fdd707b8040944ed10393f701a3c9

engines/tsage/scenes.cpp
@@ -463,9 +463,8 @@ void Scene::refreshBackground(int xAmount, int yAmount) {
 				// Check if the section is already loaded
 				if ((_enabledSections[xp * 16 + yp] == 0xffff) || ((xAmount == 0) && (yAmount == 0))) {
 					// Chunk isn't loaded, so load it in
-					Graphics::ManagedSurface s;
-					s.copyFrom(_backSurface.lockSurface());
-					GfxSurface::loadScreenSection(s, xp - xHalfOffset, yp - yHalfOffset, xp, yp);
+					Graphics::ManagedSurface *s = &_backSurface.lockSurface();
+					GfxSurface::loadScreenSection(*s, xp - xHalfOffset, yp - yHalfOffset, xp, yp);
 					_backSurface.unlockSurface();
 					changedFlag = true;
 				} else {