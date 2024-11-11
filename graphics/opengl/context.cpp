dea9f5f33ec9ef840dbb6beead546b5c28f99f51
Le Philousophe <lephilousophe@users.noreply.github.com>
2024-11-11 14:05:56 +0100

    GRAPHICS: OPENGL: Fix typo in comment

--- graphics/opengl/context.cpp
+++ graphics/opengl/context.cpp
@@ -21,7 +21,7 @@
 
 #define GLAD_GL_IMPLEMENTATION
 // sscanf_s is used by glad on MSVC only
-// we can't know before c=config.h is loaded that GLAD will be used
+// we can't know before config.h is loaded that GLAD will be used
 // but at this time it will be too late to allow sscanf_s
 #ifdef _MSC_VER
 #define FORBIDDEN_SYMBOL_EXCEPTION_sscanf_s
