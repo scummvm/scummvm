affc1969b07a2170f912b754f3a350d887ca3be8
Eugene Sandulenko <sev@scummvm.org>
2024-11-15 17:46:56 +0100

    SCUMM: Fix multiline translatable options

--- engines/scumm/metaengine.cpp
+++ engines/scumm/metaengine.cpp
@@ -734,8 +734,8 @@ static const ExtraGuiOption audioOverride {
 
 static const ExtraGuiOption enableOriginalGUI = {
 	_s("Enable the original GUI and Menu"),
-	_s("Allow the game to use the in-engine graphical interface and the original save/load menu. \
-		Use it together with the \"Ask for confirmation on exit\" for a more complete experience."),
+	_s("Allow the game to use the in-engine graphical interface and the original save/load menu. "
+	   "Use it together with the \"Ask for confirmation on exit\" for a more complete experience."),
 	"original_gui",
 	true,
 	0,
@@ -744,8 +744,8 @@ static const ExtraGuiOption enableOriginalGUI = {
 
 static const ExtraGuiOption enableLowLatencyAudio = {
 	_s("Enable low latency audio mode"),
-	_s("Allows the game to use low latency audio, at the cost of sound accuracy. \
-		It is recommended to enable this feature only if you incur in audio latency issues during normal gameplay."),
+	_s("Allows the game to use low latency audio, at the cost of sound accuracy. "
+	   "It is recommended to enable this feature only if you incur in audio latency issues during normal gameplay."),
 	"dimuse_low_latency_mode",
 	false,
 	0,
@@ -754,8 +754,8 @@ static const ExtraGuiOption enableLowLatencyAudio = {
 
 static const ExtraGuiOption enableCOMISong = {
 	_s("Enable the \"A Pirate I Was Meant To Be\" song"),
-	_s("Enable the song at the beginning of Part 3 of the game, \"A Pirate I Was Meant To Be\", \
-		which was cut in international releases. Beware though: subtitles may not be fully translated."),
+	_s("Enable the song at the beginning of Part 3 of the game, \"A Pirate I Was Meant To Be\", "
+	   "which was cut in international releases. Beware though: subtitles may not be fully translated."),
 	"enable_song",
 	false,
 	0,
