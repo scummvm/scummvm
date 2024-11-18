0f85939bc5e57116a20bb3a276fb46599cafa4d1
Miro Kropacek <miro.kropacek@gmail.com>
2024-11-18 23:32:14 +0100

    BACKENDS: ATARI: Remove move16 code
    There's no evidence of that code being faster than the already optimized
    memcpy().

--- backends/platform/atari/build-release.sh
+++ backends/platform/atari/build-release.sh
@@ -10,7 +10,7 @@ PLATFORM=m68k-atari-mintelf
 FASTCALL=false
 
 export ASFLAGS="-m68020-60"
-export CXXFLAGS="-m68020-60 -DUSE_MOVE16 -DUSE_SUPERVIDEL -DUSE_SV_BLITTER"
+export CXXFLAGS="-m68020-60 -DUSE_SUPERVIDEL -DUSE_SV_BLITTER"
 export LDFLAGS="-m68020-60"
 export PKG_CONFIG_LIBDIR="$(${PLATFORM}-gcc -print-sysroot)/usr/lib/m68020-60/pkgconfig"
 
