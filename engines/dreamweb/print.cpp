scummvm/scummvm

commit 826660b92a149016bf9655372a71ddf201311f51

DREAMWEB: Avoid out of bound read with empty strings in printDirect
In the French version of the game, right at the start (in the first
scene) there was a crash due to an out of bound read with a string
of length 1 (empty, with only the '\0' character). This commit
moves reading the next character to after checking that that the
character is not '\0' to avoid this out of bound read.
This fixes bug #15546.

engines/dreamweb/print.cpp
@@ -138,11 +138,11 @@ uint8 DreamWebEngine::printDirect(const uint8** string, uint16 x, uint16 *y, uin
 		uint16 i = offset;
 		do {
 			uint8 c = (*string)[0];
-			uint8 nextChar = (*string)[1];
-			++(*string);
 			if ((c == 0) || (c == ':')) {
 				return c;
 			}
+			uint8 nextChar = (*string)[1];
+			++(*string);
 			c = modifyChar(c);
 			uint8 width, height;
 			printChar(charSet, &i, *y, c, nextChar, &width, &height);

commit f3ba6b7e0e17608909cb039d38669d4ba55ed485

Dreamweb: Fix scrolling of end credits
This was a regression introduced in commit 826660b92.

engines/dreamweb/print.cpp
@@ -138,11 +138,11 @@ uint8 DreamWebEngine::printDirect(const uint8** string, uint16 x, uint16 *y, uin
 		uint16 i = offset;
 		do {
 			uint8 c = (*string)[0];
+			++(*string);
 			if ((c == 0) || (c == ':')) {
 				return c;
 			}
-			uint8 nextChar = (*string)[1];
-			++(*string);
+			uint8 nextChar = (*string)[0];
 			c = modifyChar(c);
 			uint8 width, height;
 			printChar(charSet, &i, *y, c, nextChar, &width, &height);