7d491225312c43ea5de0156c6b15306615f52440
Thierry Crozat <criezy@scummvm.org>
2024-12-03 23:05:44 +0000

    BUILD: Restrict -force_cpusubtype_ALL flag to MacOS X PPC
    This flag is only needed for PPC builds. Furthermore it is no longer
    recognized when building with recent Xcode on macOS ARM and causes
    a link error.

--- ports.mk
+++ ports.mk
@@ -590,8 +590,12 @@ endif
 # Special target to create a static linked binary for macOS.
 # We use -force_cpusubtype_ALL to ensure the binary runs on every
 # PowerPC machine.
+ifdef MACOSX_PPC
+OSX_STATIC_LD_FLAGS = -force_cpusubtype_ALL
+endif
+
 scummvm-static: $(DETECT_OBJS) $(OBJS)
-	+$(LD) $(LDFLAGS) -force_cpusubtype_ALL -o scummvm-static $(PRE_OBJS_FLAGS) $(DETECT_OBJS) $(OBJS) $(POST_OBJS_FLAGS) \
+	+$(LD) $(LDFLAGS) $(OSX_STATIC_LD_FLAGS) -o scummvm-static $(PRE_OBJS_FLAGS) $(DETECT_OBJS) $(OBJS) $(POST_OBJS_FLAGS) \
 		-framework CoreMIDI \
 		$(OSX_STATIC_LIBS) \
 		$(OSX_ZLIB)
