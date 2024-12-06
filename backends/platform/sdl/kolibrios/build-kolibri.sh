162da8f134bb15fd5a667c8fb114749ea4ab6aad
Vladimir Serbinenko <phcoder@gmail.com>
2024-12-06 16:42:17 +0300

    KOLIBRI: Bump compilation thread count to 12

--- backends/platform/sdl/kolibrios/build-kolibri.sh
+++ backends/platform/sdl/kolibrios/build-kolibri.sh
@@ -8,4 +8,4 @@ export KOS32_AUTOBUILD=$HOME/autobuild
 # Use plugins for both engines and detection as KolibriOS has a limit per executable module
 ./configure --host=kos32 --enable-release --enable-plugins --default-dynamic --enable-detection-dynamic --with-vorbis-prefix=$KOS32_SDK_DIR/sources/libvorbis-1.3.7  --with-ogg-prefix=$KOS32_SDK_DIR/sources/libogg-1.3.5 --enable-engine=testbed
 
-make -j5 all zip-root scummvm-zip
+make -j12 all zip-root scummvm-zip
