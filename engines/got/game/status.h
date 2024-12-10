2a0b44c87401fe72a9b1cbe779a03f3e5b27bbe0
Paul Gilbert <dreammaster@scummvm.org>
2024-12-10 02:11:32 -0800

    GOT: Added status methods

---
+++ engines/got/game/status.h
@@ -0,0 +1,41 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef GOT_DATA_STATUS_H
+#define GOT_DATA_STATUS_H
+
+#include "got/defines.h"
+
+namespace Got {
+
+extern void add_jewels(int num);
+extern void add_score(int num);
+extern void add_magic(int num);
+extern void add_health(int num);
+extern void add_keys(int num);
+extern void fill_health();
+extern void fill_magic();
+extern void fill_score(int num);
+extern void score_for_inv();
+
+} // namespace Got
+
+#endif
