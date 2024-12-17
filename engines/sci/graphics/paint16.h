ba8a9d420405a6f6ac09a08bc63c8644ff82759b
athrxx <athrxx@scummvm.org>
2024-12-17 01:52:01 +0100

    Update engines/sci/graphics/paint16.h
    Co-authored-by: Filippos Karapetis <bluegr@gmail.com>

--- engines/sci/graphics/paint16.h
+++ engines/sci/graphics/paint16.h
@@ -100,7 +100,7 @@ private:
 	bool _EGAdrawingVisualize;
 
 	// The original KQ6WinCD interpreter saves the hires drawing information in a linked list. There are two use cases: one is redrawing the
-	// window background when recieving WM_PAINT messages (which is irrelevant for us, since that happens in the backend) and the other is
+	// window background when receiving WM_PAINT messages (which is irrelevant for us, since that happens in the backend) and the other is
 	// redrawing the inventory after displaying a text window over it. This only happens in mixed speech+text mode which does not even exist
 	// in the original. We do have that mode as a ScummVM feature, though. That's why we have that code, to be able to refresh the inventory.
 	void removeHiresDrawObject(reg_t handle);
