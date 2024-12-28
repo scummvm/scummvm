46f5e0601986f1834c94aa31c3e7957697f726dd
Torbjörn Andersson <eriktorbjorn@users.sourceforge.net>
2024-12-28 10:34:38 +0100

    SCUMM: MACGUI: Simplify word wrapping in demo About dialogs
    Now that we use \n instead of \r, the word wrapper should be able to
    handle the strings just fine.

--- engines/scumm/macgui/macgui_widgets.cpp
+++ engines/scumm/macgui/macgui_widgets.cpp
@@ -114,25 +114,8 @@ int MacGuiImpl::MacWidget::drawText(Common::String text, int x, int y, int w, ui
 	if (wordWrap) {
 		maxLineWidth = font->wordWrapText(text, w, lines);
 	} else {
-		if (text.contains('\n')) {
-			Common::String line = "";
-
-			for (uint i = 0; i < text.size(); i++) {
-				if (text[i] != '\n')
-					line += text[i];
-
-				if (text[i] == '\n' || i == text.size() - 1) {
-					int lineWidth = font->getStringWidth(line);
-					if (lineWidth > maxLineWidth)
-						maxLineWidth = lineWidth;
-					lines.push_back(line);
-					line = "";
-				}
-			}
-		} else {
-			lines.push_back(text);
-			maxLineWidth = font->getStringWidth(text);
-		}
+		lines.push_back(text);
+		maxLineWidth = font->getStringWidth(text);
 	}
 
 	// Draw the text. Disabled text is implemented as a filter on top of
