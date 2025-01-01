c65b207ec3c348ca2e0388cabab991a7f1117f2f
Vladimir Serbinenko <phcoder@gmail.com>
2025-01-01 03:13:42 +0300

    TETRAEDGE: Mark Android as using UTF-8

--- engines/tetraedge/te/te_i_font.cpp
+++ engines/tetraedge/te/te_i_font.cpp
@@ -51,10 +51,12 @@ TeIFont::GlyphData TeIFont::glyph(uint pxSize, uint charcode) {
 
 Common::CodePage TeIFont::codePage() const {
 	Common::String lang = g_engine->getCore()->language();
-	if (lang == "he")
-		return Common::kWindows1255;
 	if (lang == "ru")
 		return Common::kISO8859_5;
+	if (g_engine->getGamePlatform() == Common::Platform::kPlatformAndroid)
+		return Common::CodePage::kUtf8;
+	if (lang == "he")
+		return Common::kWindows1255;
 	return Common::kLatin1;
 }
 
