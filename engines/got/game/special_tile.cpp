496e01633f629cef4b9955e1a15cc212da26f579
Paul Gilbert <dreammaster@scummvm.org>
2025-01-03 02:17:22 -0800

    GOT: Added game-specific special tile logic

--- engines/got/game/special_tile.cpp
+++ engines/got/game/special_tile.cpp
@@ -21,12 +21,15 @@
 
 #include "got/game/special_tile.h"
 #include "got/game/back.h"
+#include "got/game/object.h"
+#include "got/gfx/image.h"
 #include "got/vars.h"
 
 namespace Got {
 
 int open_door1(int y, int x);
 int cash_door1(int y, int x, int amount);
+void erase_door(int x, int y);
 
 int special_tile_thor(int x, int y, int icon) {
 	int cx, cy, f;
@@ -36,15 +39,41 @@ int special_tile_thor(int x, int y, int icon) {
 	case 201:
 		return open_door1(x, y);
 	case 202:
+		if (GAME3) {
+			if (_G(thor_info).inventory & 64) {
+				if (_G(thor_info).object == 4) {
+					erase_door(y, x);
+					delete_object();
+					return 1;
+				}
+			}
+			return 0;
+		}
+
 		if (_G(thor)->x > 300)
-			_G(end_tile) = 1;     // ending bridge
+			_G(end_tile) = 1;     // Ending bridge
 		return 1;
 	case 203:
+		if (!GAME1) {
+			if ((_G(thor_info).inventory & 64) && _G(thor_info).object == 5) {
+				odin_speaks(2012, 0);
+				delete_object();
+				_G(setup).f10 = 1;
+			} else if (!_G(setup).f10) {
+				odin_speaks(2011, 0);
+				_G(setup).f10 = 1;
+			}
+			return 1;
+		}
 		return 0;
 	case 204:
 		if (GAME2) {
 			if (!_G(setup).f19)
 				_G(slip_flag) = true;
+			return 1;
+		} else if (GAME3) {
+			if (_G(thor)->x < 4)
+				_G(end_tile) = 1;
 			return 1;
 		}
 		return 0;
@@ -69,16 +98,51 @@ int special_tile_thor(int x, int y, int icon) {
 	case 210:
 		return cash_door1(x, y, 100);
 	case 211:
-		place_tile(y, x, 79);
-		_G(exit_flag) = 2;
+		if (GAME1) {
+			place_tile(y, x, 79);
+			_G(exit_flag) = 2;
+		} else if (GAME2) {
+			if (_G(thor)->dir == 0 && _G(setup).f29 && _G(setup).f21 && !_G(setup).f22) {
+				actor_visible(1);
+				actor_visible(2);
+				actor_visible(3);
+				actor_visible(4);
+				actor_visible(5);
+				Common::fill(_G(scrn).actor_invis, _G(scrn).actor_invis + 16, 0);
+				_G(thunder_flag) = 60;
+				play_sound(THUNDER, 1);
+				_G(setup).f22 = 1;
+			}
+		}
 		return 1;
 	case 212:
 	case 213:
 		return 0;
-	case 214:        //teleport tiles
+	case 214:        // Teleport tiles
 	case 215:
 	case 216:
 	case 217:
+		if ((GAME2 && icon == 217) || GAME3) {
+			cx = (_G(thor_x1) + 7) / 16;
+			cy = (_G(thor_real_y1) + 8) / 16;
+			if (_G(scrn).icon[cy][cx] == icon) {
+				_G(thor)->vunerable = STAMINA;
+				play_sound(WOOP, 0);
+
+				int nt = _G(scrn).new_level_loc[icon - 214];
+				int display_page = _G(pge);
+				int draw_page = _G(pge) ^ 1;
+
+				_G(thor)->last_x[display_page] = _G(thor)->x;
+				_G(thor)->last_y[display_page] = _G(thor)->y;
+				_G(thor)->x = (nt % 20) * 16;
+				_G(thor)->y = ((nt / 20) * 16) - 2;
+				_G(thor)->last_x[draw_page] = _G(thor)->x;
+				_G(thor)->last_y[draw_page] = _G(thor)->y;
+				return 0;
+			}
+			return 1;
+		}
 		return 0;
 	case 218:
 	case 219:
