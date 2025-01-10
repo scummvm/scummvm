46980407042e6837e2380d2ff704f0a877297b2e
Bob Pulliam <bpulliam@gmail.com>
2025-01-10 10:12:57 -0500

    LUA: Fix 32-bit/64-bit shift warning (MSVC)

--- common/lua/lobject.h
+++ common/lua/lobject.h
@@ -356,7 +356,7 @@ typedef struct Table {
 	(check_exp((size&(size-1))==0, (cast(int, (s) & ((size)-1)))))
 
 
-#define twoto(x)	(1<<(x))
+#define twoto(x)	((size_t)1<<(x))
 #define sizenode(t)	(twoto((t)->lsizenode))
 
 
