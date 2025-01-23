f8f3c49df31d32a50ab2058f32e8c2176f624464
Eugene Sandulenko <sev@scummvm.org>
2025-01-23 19:04:27 +0100

    DIRECTOR: LINGO: Fix implementation of findPosNear, add relevant tests

--- engines/director/lingo/lingo-builtins.cpp
+++ engines/director/lingo/lingo-builtins.cpp
@@ -799,11 +799,16 @@ void LB::b_findPos(int nargs) {
 void LB::b_findPosNear(int nargs) {
 	Common::String prop = g_lingo->pop().asString();
 	Datum list = g_lingo->pop();
-	Datum res(0);
+	Datum res;
 	TYPECHECK(list, PARRAY);
 
 	// FIXME: Integrate with compareTo framework
 	prop.toLowercase();
+
+	// This requires more testing, but D4-D6 test show that it does not work as described.
+	// The example:
+	//   findPosNear([#Nile:2, #Pharaoh:4, #Raja:0], #Ni)  supposed to return 1, for #Nile, but it returns 4
+	res = Datum((int)list.u.parr->arr.size() + 1); // Set it to the end of array by default
 
 	for (uint i = 0; i < list.u.parr->arr.size(); i++) {
 		Datum p = list.u.parr->arr[i].p;
