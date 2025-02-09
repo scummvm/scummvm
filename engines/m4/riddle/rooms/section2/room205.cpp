b8a135358330944b528611a4d3de6f9f29aa7c19
Paul Gilbert <dreammaster@scummvm.org>
2025-02-08 19:58:06 -0800

    M4: RIDDLE: Fix getting charcoal

--- engines/m4/riddle/rooms/section2/room205.cpp
+++ engines/m4/riddle/rooms/section2/room205.cpp
@@ -537,7 +537,7 @@ void Room205::parser() {
 				break;
 
 			case 3:
-				kernel_examine_inventory_object("PING CHARCOAL", nullptr, 5, 1, 144, 271, 4, nullptr, -1);
+				kernel_examine_inventory_object("PING CHARCOAL", 5, 1, 144, 271, 4, nullptr, -1);
 				break;
 
 			case 4:
