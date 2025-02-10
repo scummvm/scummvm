636a751c6d94a8aaf838f956c8b958e92254e775
Paul Gilbert <dreammaster@scummvm.org>
2025-02-10 07:48:40 -0800

    GOT: Mark detections as testing

--- engines/got/detection_tables.h
+++ engines/got/detection_tables.h
@@ -33,7 +33,7 @@ const ADGameDescription gameDescriptions[] = {
 		 AD_ENTRY1s("gotres.dat", "0fe859bcfd06772be0885bf510c2877d", 739732),
 		 Common::EN_ANY,
 		 Common::kPlatformDOS,
-		 ADGF_UNSTABLE,
+		 ADGF_TESTING,
 		 GUIO1(GUIO_NOSPEECH)
 	},
 	{
@@ -42,7 +42,7 @@ const ADGameDescription gameDescriptions[] = {
 		 AD_ENTRY1s("gotres.dat", "c316221a3b9d12a838faef55fde2f34c", 482738),
 		 Common::EN_ANY,
 		 Common::kPlatformDOS,
-		 ADGF_UNSTABLE | ADGF_DEMO,
+		 ADGF_TESTING | ADGF_DEMO,
 		 GUIO1(GUIO_NOSPEECH)
 	},
 	{
@@ -51,7 +51,7 @@ const ADGameDescription gameDescriptions[] = {
 		 AD_ENTRY1s("gotres.dat", "747ed508ffa3808156a4eb080e9859f8", 739710),
 		 Common::EN_ANY,
 		 Common::kPlatformDOS,
-		 ADGF_UNSTABLE,
+		 ADGF_TESTING,
 		 GUIO1(GUIO_NOSPEECH)
 	},
 	{
@@ -60,7 +60,7 @@ const ADGameDescription gameDescriptions[] = {
 		 AD_ENTRY1s("gotres.dat", "c443aa09450566ee32998c3ebb15cbaa", 485858),
 		 Common::EN_ANY,
 		 Common::kPlatformDOS,
-		 ADGF_UNSTABLE | ADGF_DEMO,
+		 ADGF_TESTING | ADGF_DEMO,
 		 GUIO1(GUIO_NOSPEECH)
 	},
 
