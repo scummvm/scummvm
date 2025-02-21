654cd4690d127ee445bd5a628d6a62a89a237f8e
Florian Mehm <florian@mehm.net>
2025-02-22 00:05:48 +0100

    MACS2: Add loadSongFromSceneData stub function
    Add initial implementation that reads song data from scene data
    array520D by index, creating a StreamHandler for playback.

--- engines/macs2/macs2.h
+++ engines/macs2/macs2.h
@@ -274,6 +274,8 @@ public:
 
 	void loadAnimationFromSceneData(uint16 objectIndex, uint16 slotIndex, uint8 arrayIndex);
 
+	// TODO: Arguments
+	void loadSongFromSceneData();
 
 	// Offset 50D3h - This is used in 0037:10C4 to terminate the loop
 	uint16 word50D3;
