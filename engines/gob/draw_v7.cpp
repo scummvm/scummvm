6b4792b8c8baa30280b02bfc8dba6eb9aa96a875
Simon Delamarre <simon.delamarre14@gmail.com>
2025-02-23 16:33:09 +0100

    GOB: Use Image::IcoCurDecoder to read cursors
    Needed by Adibou2/Anglais CUR files

--- engines/gob/draw_v7.cpp
+++ engines/gob/draw_v7.cpp
@@ -30,6 +30,7 @@
 #include "graphics/blit.h"
 #include "graphics/cursorman.h"
 #include "graphics/wincursor.h"
+#include "image/icocur.h"
 
 #include "gob/dataio.h"
 #include "gob/draw.h"
@@ -74,18 +75,41 @@ bool Draw_v7::loadCursorFromFile(Common::String cursorName) {
 	Graphics::WinCursorGroup *cursorGroup = nullptr;
 	Graphics::Cursor *defaultCursor = nullptr;
 
-	// Load the cursor file and cursor group
-	if (loadCursorFile())
-		cursorGroup = Graphics::WinCursorGroup::createCursorGroup(_cursors, Common::WinResourceID(cursorName));
+	const Graphics::Cursor *cursor = nullptr;
+
+	if (cursorName.hasPrefix("*")) {
+		// Load from an external .CUR file
+		cursorName = cursorName.substr(1);
+		Common::SeekableReadStream *cursorStream = _vm->_dataIO->getFile(cursorName);
+
+		if (cursorStream) {
+			Image::IcoCurDecoder cursorDecoder;
+			cursorDecoder.open(*cursorStream);
+
+			if (cursorDecoder.numItems() > 0) {
+				cursor = cursorDecoder.loadItemAsCursor(0);
+			} else {
+				warning("No cursor item found in file '%s'", cursorName.c_str());
+			}
+		} else {
+			warning("External cursor file '%s' not found", cursorName.c_str());
+		}
+	} else {
+		// Load from a .DLL cursor file and cursor group
+		if (loadCursorFile())
+			cursorGroup = Graphics::WinCursorGroup::createCursorGroup(_cursors, Common::WinResourceID(cursorName));
+
+		if (cursorGroup && !cursorGroup->cursors.empty() && cursorGroup->cursors[0].cursor) {
+			cursor = cursorGroup->cursors[0].cursor;
+		}
+	}
 
 	// If the requested cursor does not exist, create a default one
-	const Graphics::Cursor *cursor = nullptr;
-	if (!cursorGroup || cursorGroup->cursors.empty() || !cursorGroup->cursors[0].cursor) {
+	if (!cursor) {
 		defaultCursor = Graphics::makeDefaultWinCursor();
-
 		cursor = defaultCursor;
-	} else
-		cursor = cursorGroup->cursors[0].cursor;
+	}
+
 
 	// Make sure the cursor sprite is big enough
 	if (_scummvmCursor->getWidth() != cursor->getWidth() || _scummvmCursor->getHeight() != cursor->getHeight()) {
