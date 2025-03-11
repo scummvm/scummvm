d1f67fa63dfdcb7f4ae3752c7f2e2c06990f6bfb
Thomas N McEwan <46427621+tnm23@users.noreply.github.com>
2025-03-11 17:58:05 +0000

    ZVISION: Add workaround for bug in MT-32 emulation. Bug makes MIDI music puzzles in Nemesis impossible. TODO: Inform MT-32 emulator maintainer about this bug.

--- engines/zvision/sound/midi.cpp
+++ engines/zvision/sound/midi.cpp
@@ -24,18 +24,27 @@
 #include "audio/mididrv.h"
 
 #include "zvision/sound/midi.h"
+#include "common/debug.h"
 
 namespace ZVision {
 
 MidiManager::MidiManager() {
-	MidiDriver::DeviceHandle dev = MidiDriver::detectDevice(MDT_MIDI | MDT_ADLIB);
+	MidiDriver::DeviceHandle dev = MidiDriver::detectDevice(MDT_MIDI | MDT_ADLIB);  //NB Bug current in MT-32 emulation: MIDI channel 0 does not play anything!
 	_driver = MidiDriver::createMidi(dev);
-	if (_driver->open()) {
-		warning("Can't open MIDI, no MIDI output!");
-		available = false;
-		}
-	else
+  if (_driver->open()) {
+	  warning("Can't open MIDI, no MIDI output!");
+	  available = false;
+	  }
+	else {
+	  Common::String driverName = MidiDriver::getDeviceString(dev,MidiDriver::DeviceStringType::kDriverName);
+	  Common::String deviceName = MidiDriver::getDeviceString(dev,MidiDriver::DeviceStringType::kDeviceName);
+	  mt32workaround = driverName.contains("MT-32");
+    debug(1,"MIDI opened, driver type: %s, device name: %s", driverName.c_str(), deviceName.c_str());
+    if(mt32workaround)
+      debug(1,"Blocking use of MIDI channel 0 to work around bug in MT-32 emulation.");
 	  available = true;
+	  maxChannels = _driver->MIDI_CHANNEL_COUNT;
+  }
 }
 
 MidiManager::~MidiManager() {
@@ -46,49 +55,47 @@ MidiManager::~MidiManager() {
 
 void MidiManager::stop() {
 	for (int8 i = 0; i < 16; i++)
-		if (_playChannels[i].playing)
+		if (activeChannels[i].playing)
 			noteOff(i);
 }
 
 void MidiManager::noteOn(int8 channel, int8 note, int8 velocity) {
 	assert(channel <= 15);
-
-	_playChannels[channel].playing = true;
-	_playChannels[channel].note = note;
+	activeChannels[channel].playing = true;
+	activeChannels[channel].note = note;
 	_driver->send(channel | (velocity << 16) | (note << 8) | 0x90);
+	debug(1,"MIDI note on, channel %d, note %d, velocity %d", channel, note, velocity);
 }
 
 void MidiManager::noteOff(int8 channel) {
 	assert(channel <= 15);
 
-	if (_playChannels[channel].playing) {
-		_playChannels[channel].playing = false;
-		_driver->send(channel | (_playChannels[channel].note << 8) | 0x80);
+	if (activeChannels[channel].playing) {
+		activeChannels[channel].playing = false;
+		_driver->send(channel | (activeChannels[channel].note << 8) | 0x80);
 	}
 }
 
 int8 MidiManager::getFreeChannel() {
-	for (int8 i = 0; i < 16; i++)
-		if (!_playChannels[i].playing)
+  uint8 start = mt32workaround ? 1 : 0; //MT-32 emulator driver currently fails to play anything when sent notes on channel 0.  Quick & dirty fix, since we only need a few notes for Nemesis music puzzles.  Inform maintainer of MT-32 emulator code of bug for proper fix in the future & then remove this nasty hack once that's sorted out.
+	for (int8 i = start; i < 16; i++)
+		if (!activeChannels[i].playing)
 			return i;
 	return -1;
 }
 
 void MidiManager::setPan(int8 channel, int8 pan) {
 	assert(channel <= 15);
-
 	_driver->send(channel | (pan << 16) | 0xAB0);
 }
 
 void MidiManager::setVolume(int8 channel, int8 volume) {
 	assert(channel <= 15);
-
 	_driver->send(channel | (volume << 16) | 0x7B0);
 }
 
 void MidiManager::setProgram(int8 channel, int8 prog) {
 	assert(channel <= 15);
-
 	_driver->send(channel | (prog << 8) | 0xC0);
 }
 
