0658576196e2c767268eb77e2ee1f14c03b3762a
Nathanael Gentry <nathanael.gentrydb8@gmail.com>
2025-03-21 17:29:14 -0400

    MEDIASTATION: Make path playback non-blocking

--- engines/mediastation/assets/path.cpp
+++ engines/mediastation/assets/path.cpp
@@ -56,43 +56,70 @@ Operand Path::callMethod(BuiltInMethod methodId, Common::Array<Operand> &args) {
 		return Operand();
 	}
 
+	case kIsPlayingMethod: {
+		assert(args.empty());
+		Operand returnValue(kOperandTypeLiteral1);
+		returnValue.putInteger(_isActive);
+		return returnValue;
+	}
+
 	default:
 		error("Path::callMethod(): Got unimplemented method ID %s (%d)", builtInMethodToStr(methodId), static_cast<uint>(methodId));
 	}
 }
 
 void Path::timePlay() {
-	// TODO: Check that itʻs zero before we reset it, since this function isn't re-entrant!
-	_percentComplete = 0.0;
+	if (_isActive) {
+		warning("Path::timePlay(): Attempted to play a path that is already playing");
+		return;
+	}
 
 	if (_header->_duration == 0) {
 		warning("Path::timePlay(): Got zero duration");
 	} else if (_header->_stepRate == 0) {
 		error("Path::timePlay(): Got zero step rate");
 	}
-	debugC(5, kDebugScript, "Path::timePlay(): Path playback started");
-	uint totalSteps = (_header->_duration * _header->_stepRate) / 1000;
-	//uint stepDurationInMilliseconds = 1000 / _header->_stepRate;
+
+	setActive();
+	_percentComplete = 0;
+	_nextPathStepTime = 0;
+	_currentStep = 0;
+	_totalSteps = (_header->_duration * _header->_stepRate) / 1000;
+	_stepDurationInMilliseconds = 1000 / _header->_stepRate;
 
 	// TODO: Run the path start event. Haven't seen one the wild yet, don't know its ID.
 	debugC(5, kDebugScript, "Path::timePlay(): No PathStart event handler");
-
-	// Step the path.
-	for (uint i = 0; i < totalSteps; i++) {
-		_percentComplete = (double)(i + 1) / totalSteps;
-		debugC(5, kDebugScript, "Path::timePlay(): Step %d of %d", i, totalSteps);
-		// TODO: Actually step the path. It seems they mostly just use this for
-		// palette animation in the On Step event handler, so nothing is actually drawn on the screen now.
-
-		runEventHandlerIfExists(kStepEvent);
-	}
-
-	runEventHandlerIfExists(kPathEndEvent);
-	_percentComplete = 0;
 }
 
 void Path::process() {
-	// TODO: Handle this case.
+	uint currentTime = g_system->getMillis();
+	uint pathTime = currentTime - _startTime;
+
+	bool doNextStep = pathTime >= _nextPathStepTime;
+	if (!doNextStep) {
+		return;
+	}
+
+	_percentComplete = static_cast<double>(_currentStep + 1) / _totalSteps;
+	debugC(2, kDebugScript, "Path::timePlay(): Step %d of %d", _currentStep, _totalSteps);
+
+	if (_currentStep < _totalSteps) {
+		// TODO: Actually step the path. It seems they mostly just use this for
+		// palette animation in the On Step event handler, so nothing is actually drawn on the screen now.
+
+		// We donʻt run a step event for the last step.
+		runEventHandlerIfExists(kStepEvent);
+		_nextPathStepTime = ++_currentStep * _stepDurationInMilliseconds;
+	} else {
+		setInactive();
+		_percentComplete = 0;
+		_nextPathStepTime = 0;
+		_currentStep = 0;
+		_totalSteps = 0;
+		_stepDurationInMilliseconds = 0;
+
+		runEventHandlerIfExists(kPathEndEvent);
+	}
 }
 
 void Path::setDuration(uint durationInMilliseconds) {
