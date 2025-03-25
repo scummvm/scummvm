a61aec54dba3fc6a817fa8bd19ed621475542296
Filippos Karapetis <bluegr@gmail.com>
2025-03-25 03:52:47 +0200

    CGE2: Use C++ 11 range-based for loops

--- engines/cge2/metaengine.cpp
+++ engines/cge2/metaengine.cpp
@@ -130,13 +130,13 @@ SaveStateList CGE2MetaEngine::listSaves(const char *target) const {
 	filenames = saveFileMan->listSavefiles(pattern);
 
 	SaveStateList saveList;
-	for (Common::StringArray::const_iterator filename = filenames.begin(); filename != filenames.end(); ++filename) {
+	for (const auto &filename : filenames) {
 		// Obtain the last 3 digits of the filename, since they correspond to the save slot
-		int slotNum = atoi(filename->c_str() + filename->size() - 3);
+		int slotNum = atoi(filename.c_str() + filename.size() - 3);
 
 		if (slotNum >= 0 && slotNum <= 99) {
 
-			Common::InSaveFile *file = saveFileMan->openForLoading(*filename);
+			Common::InSaveFile *file = saveFileMan->openForLoading(filename);
 			if (file) {
 				CGE2::SavegameHeader header;
 
