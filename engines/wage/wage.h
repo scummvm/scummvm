0ae99d2bd0248a55bc9e60a2361fc5b17361c812
InfinityColme1 <mauricio.lavady@gmail.com>
2025-04-07 21:02:24 +0200

    WAGE: Add text-to-speech (TTS)

--- engines/wage/wage.h
+++ engines/wage/wage.h
@@ -55,6 +55,7 @@
 #include "common/macresman.h"
 #include "common/random.h"
 #include "common/timer.h"
+#include "common/text-to-speech.h"
 
 #include "wage/debugger.h"
 
@@ -132,6 +133,8 @@ public:
 	bool hasFeature(EngineFeature f) const override;
 
 	Common::Error run() override;
+
+	Common::Language getLanguage() const;
 
 	bool canLoadGameStateCurrently(Common::U32String *msg = nullptr) override;
 	bool canSaveGameStateCurrently(Common::U32String *msg = nullptr) override;
@@ -219,6 +222,9 @@ public:
 	void updateSoundTimerForScene(Scene *scene, bool firstTime);
 	void setMenu(Common::String soundName);
 	void appendText(const char *str);
+	void sayText(const Common::String str);
+	void sayText(const Common::String str, Common::TextToSpeechManager::Action action);
+	void stopTextSpeech();
 	void gameOver();
 	bool saveDialog();
 	void aboutDialog();
