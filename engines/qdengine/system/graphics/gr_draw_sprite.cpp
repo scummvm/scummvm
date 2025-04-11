3863f8814e9110cde904a50b8ccc80a189096c9b
Alikhan Balpykov <lowliet1990@mail.ru>
2025-04-11 16:09:13 +0500

    QDENGINE: Add STUBs to putSprMask, putSprMask_rot methods

--- engines/qdengine/system/graphics/gr_draw_sprite.cpp
+++ engines/qdengine/system/graphics/gr_draw_sprite.cpp
@@ -242,6 +242,8 @@ void grDispatcher::putSpr_rot(const Vect2i &pos, const Vect2i &size, const byte 
 	int sin_a = round(sn * float(1 << F_PREC));
 	int cos_a = round(cs * float(1 << F_PREC));
 
+	warning("STUB: grDispatcher::putSpr_rot");
+
 	if (has_alpha) {
 		for (int y = 0; y <= sy; y++) {
 			uint16 *screen_ptr = (uint16 *)_screenBuf->getBasePtr(x0, y + y0);
@@ -333,6 +335,8 @@ void grDispatcher::putSpr_rot(const Vect2i &pos, const Vect2i &size, const byte 
 	Vect2i iscale = Vect2i(scale.x * float(1 << F_PREC), scale.y * float(1 << F_PREC));
 	Vect2i scaled_size = Vect2i(iscale.x * size.x, iscale.y * size.y);
 
+	warning("STUB: grDispatcher::putSpr_rot (scaled)");
+
 	if (has_alpha) {
 		for (int y = 0; y <= sy; y++) {
 			uint16 *screen_ptr = (uint16 *)_screenBuf->getBasePtr(x0, y + y0);
@@ -419,6 +423,8 @@ void grDispatcher::putSprMask_rot(const Vect2i &pos, const Vect2i &size, const b
 
 	int sin_a = round(sn * float(1 << F_PREC));
 	int cos_a = round(cs * float(1 << F_PREC));
+
+	warning("STUB: grDispatcher::putSprMask_rot");
 
 	if (has_alpha) {
 		byte mr, mg, mb;
@@ -529,6 +535,8 @@ void grDispatcher::putSprMask_rot(const Vect2i &pos, const Vect2i &size, const b
 
 	Vect2i iscale = Vect2i(scale.x * float(1 << F_PREC), scale.y * float(1 << F_PREC));
 	Vect2i scaled_size = Vect2i(iscale.x * size.x, iscale.y * size.y);
+
+	warning("STUB: grDispatcher::putSprMask_rot (scaled)");
 
 	if (has_alpha) {
 		byte mr, mg, mb;
@@ -1112,6 +1120,8 @@ void grDispatcher::putSprMask(int x, int y, int sx, int sy, const byte *p, uint3
 	int y0 = 0;
 	int y1 = sy_dest;
 	int iy = 1;
+
+	warning("STUB: grDispatcher::putSprMask (scaled)");
 
 	if (mode & GR_FLIP_VERTICAL) {
 		y0 = sy_dest,
