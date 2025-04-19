8fdcaa76c59175413fd229dc01a0da513c35c869
Paul Gilbert <dreammaster@scummvm.org>
2025-04-19 15:45:25 -0700

    AWE: Cleanup of startup code, global static initializations

--- engines/awe/awe.cpp
+++ engines/awe/awe.cpp
@@ -57,11 +57,11 @@ static const struct {
 	{ 0,  -1 }
 };
 
-bool Gfx::_is1991 = false;
-bool Gfx::_use555 = false;
-bool Video::_useEGA = false;
-Difficulty Script::_difficulty = DIFFICULTY_NORMAL;
-bool Script::_useRemasteredAudio = true;
+bool Gfx::_is1991;
+bool Gfx::_use555;
+bool Video::_useEGA;
+Difficulty Script::_difficulty;
+bool Script::_useRemasteredAudio;
 
 static Gfx *createGraphics(int type) {
 	switch (type) {
@@ -80,7 +80,6 @@ static Gfx *createGraphics(int type) {
 	return 0;
 }
 
-
 static int getGraphicsType(DataType type) {
 	switch (type) {
 	case DT_15TH_EDITION:
@@ -92,194 +91,16 @@ static int getGraphicsType(DataType type) {
 	}
 }
 
-static const int DEFAULT_WINDOW_W = 640;
-static const int DEFAULT_WINDOW_H = 400;
-
-
-#ifdef TODO
-static const char USAGE[] =
-"raw(gl) - Another World Interpreter\n"
-"Usage: %s [OPTIONS]...\n"
-"  --datapath=PATH   Path to data files (default '.')\n"
-"  --language=LANG   Language (fr,us,de,es,it)\n"
-"  --part=NUM        Game part to start from (0-35 or 16001-16009)\n"
-"  --render=NAME     Renderer (original,software,gl)\n"
-"  --window=WxH      Windowed display size (default '640x400')\n"
-"  --fullscreen      Fullscreen display (stretched)\n"
-"  --fullscreen-ar   Fullscreen display (16:10 aspect ratio)\n"
-"  --ega-palette     Use EGA palette with DOS version\n"
-"  --demo3-joy       Use inputs from 'demo3.joy' (DOS demo)\n"
-"  --difficulty=DIFF Difficulty (easy,normal,hard)\n"
-"  --audio=AUDIO     Audio (original,remastered)\n"
-;
-
-
-
-int main(int argc, char *argv[]) {
-	char *dataPath = 0;
-	int part = 16001;
-	Language lang = Common::FR_FRA;
-	int graphicsType = GRAPHICS_GL;
-	DisplayMode dm;
-	dm.mode = DisplayMode::WINDOWED;
-	dm.width = DEFAULT_WINDOW_W;
-	dm.height = DEFAULT_WINDOW_H;
-	dm.opengl = (graphicsType == GRAPHICS_GL);
-	Scaler scaler;
-	scaler.name[0] = 0;
-	scaler.factor = 1;
-	bool defaultGraphics = true;
-	bool demo3JoyInputs = false;
-	if (argc == 2) {
-		// data path as the only command line argument
-		struct stat st;
-		if (stat(argv[1], &st) == 0 && S_ISDIR(st.st_mode)) {
-			dataPath = strdup(argv[1]);
-		}
-	}
-	while (1) {
-		static struct option options[] = {
-			{ "datapath", required_argument, 0, 'd' },
-			{ "language", required_argument, 0, 'l' },
-			{ "part",     required_argument, 0, 'p' },
-			{ "render",   required_argument, 0, 'r' },
-			{ "window",   required_argument, 0, 'w' },
-			{ "fullscreen", no_argument,     0, 'f' },
-			{ "fullscreen-ar", no_argument,  0, 'a' },
-			{ "scaler",   required_argument, 0, 's' },
-			{ "ega-palette", no_argument,    0, 'e' },
-			{ "demo3-joy",  no_argument,     0, 'j' },
-			{ "difficulty", required_argument, 0, 'i' },
-			{ "audio",    required_argument, 0, 'u' },
-			{ "help",       no_argument,     0, 'h' },
-			{ 0, 0, 0, 0 }
-		};
-		int index;
-		const int c = getopt_long(argc, argv, "", options, &index);
-		if (c == -1) {
-			break;
-		}
-		switch (c) {
-		case 'd':
-			dataPath = strdup(optarg);
-			break;
-		case 'l':
-			for (int i = 0; LANGUAGES[i].name; ++i) {
-				if (strcmp(optarg, LANGUAGES[i].name) == 0) {
-					lang = (Language)LANGUAGES[i].lang;
-					break;
-				}
-			}
-			break;
-		case 'p':
-			part = atoi(optarg);
-			break;
-		case 'r':
-			for (int i = 0; GRAPHICS[i].name; ++i) {
-				if (strcmp(optarg, GRAPHICS[i].name) == 0) {
-					graphicsType = GRAPHICS[i].type;
-					dm.opengl = (graphicsType == GRAPHICS_GL);
-					defaultGraphics = false;
-					break;
-				}
-			}
-			break;
-		case 'w':
-			sscanf(optarg, "%dx%d", &dm.width, &dm.height);
-			break;
-		case 'f':
-			dm.mode = DisplayMode::FULLSCREEN;
-			break;
-		case 'a':
-			dm.mode = DisplayMode::FULLSCREEN_AR;
-			break;
-		case 's':
-			parseScaler(optarg, &scaler);
-			break;
-		case 'e':
-			Video::_useEGA = true;
-			break;
-		case 'j':
-			demo3JoyInputs = true;
-			break;
-		case 'i':
-			for (int i = 0; DIFFICULTIES[i].name; ++i) {
-				if (strcmp(optarg, DIFFICULTIES[i].name) == 0) {
-					Script::_difficulty = (Difficulty)DIFFICULTIES[i].difficulty;
-					break;
-				}
-			}
-			break;
-		case 'u':
-			if (strcmp(optarg, "remastered") == 0) {
-				Script::_useRemasteredAudio = true;
-			} else if (strcmp(optarg, "original") == 0) {
-				Script::_useRemasteredAudio = false;
-			}
-			break;
-		case 'h':
-			// fall-through
-		default:
-			printf(USAGE, argv[0]);
-			return 0;
-		}
-	}
-
-	Engine *e = new Engine(dataPath, part);
-	if (defaultGraphics) {
-		// if not set, use original software graphics for 199x editions and GL for the anniversary and 3DO versions
-		graphicsType = getGraphicsType(dataType);
-		dm.opengl = (graphicsType == GRAPHICS_GL);
-	}
-	if (graphicsType != GRAPHICS_GL && dataType == DT_3DO) {
-		graphicsType = GRAPHICS_SOFTWARE;
-		Graphics::_use555 = true;
-	}
-	Graphics *graphics = createGraphics(graphicsType);
-	if (dataType == DT_20TH_EDITION) {
-		switch (Script::_difficulty) {
-		case DIFFICULTY_EASY:
-			debugC(kDebugInfo, "Using easy difficulty");
-			break;
-		case DIFFICULTY_NORMAL:
-			debugC(kDebugInfo, "Using normal difficulty");
-			break;
-		case DIFFICULTY_HARD:
-			debugC(kDebugInfo, "Using hard difficulty");
-			break;
-		}
-	}
-	if (dataType == DT_15TH_EDITION || dataType == DT_20TH_EDITION) {
-		if (Script::_useRemasteredAudio) {
-			debugC(kDebugInfo, "Using remastered audio");
-		} else {
-			debugC(kDebugInfo, "Using original audio");
-		}
-	}
-	SystemStub *stub = SystemStub_SDL_create();
-	stub->init(e->getGameTitle(lang), &dm);
-	e->setSystemStub(stub, graphics);
-	if (demo3JoyInputs && dataType == DT_DOS) {
-		e->_res.readDemo3Joy();
-	}
-	e->setup(lang, graphicsType, scaler.name, scaler.factor);
-	while (!stub->_pi.quit) {
-		e->run();
-	}
-	e->finish();
-	delete e;
-	stub->fini();
-	delete stub;
-	return 0;
-}
-
-#endif
-
-
 AweEngine::AweEngine(OSystem *syst, const Awe::AweGameDescription *gameDesc)
 		: Engine(syst), _gameDescription(gameDesc),
 		_random("Awe") {
 	g_engine = this;
+
+	Gfx::_is1991 = false;
+	Gfx::_use555 = false;
+	Video::_useEGA = false;
+	Script::_difficulty = DIFFICULTY_NORMAL;
+	Script::_useRemasteredAudio = true;
 }
 
 AweEngine::~AweEngine() {
@@ -306,15 +127,18 @@ Common::Error AweEngine::run() {
 	bool demo3JoyInputs = false;
 
 	Awe::Engine *e = new Awe::Engine(_mixer, dataType, part);
+
 	if (defaultGraphics) {
 		// if not set, use original software graphics for 199x editions and GL for the anniversary and 3DO versions
 		graphicsType = getGraphicsType(dataType);
 		dm.opengl = (graphicsType == GRAPHICS_GL);
 	}
+
 	if (graphicsType != GRAPHICS_GL && dataType == DT_3DO) {
 		graphicsType = GRAPHICS_SOFTWARE;
 		Gfx::_use555 = true;
 	}
+
 	Gfx *graphics = createGraphics(graphicsType);
 	if (dataType == DT_20TH_EDITION) {
 		switch (Script::_difficulty) {
@@ -329,6 +153,7 @@ Common::Error AweEngine::run() {
 			break;
 		}
 	}
+
 	if (dataType == DT_15TH_EDITION || dataType == DT_20TH_EDITION) {
 		if (Script::_useRemasteredAudio) {
 			debugC(kDebugInfo, "Using remastered audio");
