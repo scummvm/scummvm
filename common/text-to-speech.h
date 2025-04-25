5afda9f4f5454bd1a6390a48514364d60e8fa8ec
Thierry Crozat <criezy@scummvm.org>
2025-04-25 19:20:08 +0100

    DOC: Add a small clarification to a doxygen comment for TTS

--- common/text-to-speech.h
+++ common/text-to-speech.h
@@ -168,7 +168,7 @@ public:
 	 *		INTERRUPT - interrupts the current speech
 	 *		INTERRUPT_NO_REPEAT - interrupts the speech (deletes the whole queue),
 	 *			if the str is the same as the string currently being said,
-	 *			it lets the current string finish.
+	 *			it lets the current string finish and drops the new request.
 	 *		QUEUE - queues the speech
 	 *		QUEUE_NO_REPEAT - queues the speech only if the str is different than
 	 *			the last string in the queue (or the string, that is currently
