89c679b483f51a649e2f3ed69c65b384a18f3dd8
Le Philousophe <lephilousophe@users.noreply.github.com>
2025-05-25 11:00:22 +0200

    SDL: Fix unused function warning on SDL3 too

--- backends/platform/sdl/sdl.cpp
+++ backends/platform/sdl/sdl.cpp
@@ -78,14 +78,16 @@
 #include <SDL_clipboard.h>
 #endif
 
+#if (defined(USE_OPENGL_GAME) || defined(USE_OPENGL_SHADERS)) && !USE_FORCED_GLES2
 #if SDL_VERSION_ATLEAST(3, 0, 0)
 static bool sdlGetAttribute(SDL_GLAttr attr, int *value) {
 	return SDL_GL_GetAttribute(attr, value);
 }
-#elif SDL_VERSION_ATLEAST(2, 0, 0) && !USE_FORCED_GLES2
+#elif SDL_VERSION_ATLEAST(2, 0, 0)
 static bool sdlGetAttribute(SDL_GLattr attr, int *value) {
 	return SDL_GL_GetAttribute(attr, value) == 0;
 }
+#endif
 #endif
 
 OSystem_SDL::OSystem_SDL()
