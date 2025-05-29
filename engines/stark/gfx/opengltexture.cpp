0d050c256dc4db5da7f62c7c840c3acdcfae3837
Cameron Cawley <ccawley2011@gmail.com>
2025-05-29 15:35:41 +0100

    STARK: Check for GL_MIRRORED_REPEAT before using it

--- engines/stark/gfx/opengltexture.cpp
+++ engines/stark/gfx/opengltexture.cpp
@@ -83,8 +83,11 @@ void OpenGlTexture::setLevelCount(uint32 count) {
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		}
 
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+		// TODO: Provide a fallback if this isn't available.
+		if (OpenGLContext.textureMirrorRepeatSupported) {
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+		}
 	}
 }
 
