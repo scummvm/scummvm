scummvm/scummvm

commit 77ee5a90072ada3e769a9efcde52125bd9a87554

IMAGE: Get palette class by reference on image decoders

engines/supernova/graphics.cpp
@@ -36,8 +36,7 @@
 namespace Supernova {
 
 MSNImage::MSNImage(SupernovaEngine *vm)
-	: _vm(vm) {
-	_palette = nullptr;
+	: _palette(0), _vm(vm) {
 	_encodedImage = nullptr;
 	_filenumber = -1;
 	_pitch = 0;
@@ -144,21 +143,22 @@ bool MSNImage::loadStream(Common::SeekableReadStream &stream) {
 	size *= 16;      // a paragraph is 16 bytes
 	_encodedImage = new byte[size];
 
-	_palette = new byte[717];
-	g_system->getPaletteManager()->grabPalette(_palette, 16, 239);
+	byte palette[717];
+	g_system->getPaletteManager()->grabPalette(palette, 16, 239);
+	_palette.resize(239, false);
+	_palette.set(palette, 0, 239);
 
 	byte pal_diff;
 	byte flag = stream.readByte();
 	if (flag == 0) {
 		pal_diff = 0;
-		_palette[141] = 0xE0;
-		_palette[142] = 0xE0;
-		_palette[143] = 0xE0;
+		_palette.set(47, 0xE0, 0xE0, 0xE0);
 	} else {
 		pal_diff = 1;
-		for (int i = flag * 3; i != 0; --i) {
-			_palette[717 - i] = stream.readByte() << 2;
-		}
+		byte r = stream.readByte() << 2;
+		byte g = stream.readByte() << 2;
+		byte b = stream.readByte() << 2;
+		_palette.set(239 - flag, r, g, b);
 	}
 
 	_numSections = stream.readByte();
@@ -273,10 +273,8 @@ bool MSNImage::loadSections() {
 }
 
 void MSNImage::destroy() {
-	if (_palette) {
-		delete[] _palette;
-		_palette = nullptr;
-	}
+	_palette.clear();
+
 	if (_encodedImage) {
 		delete[] _encodedImage;
 		_encodedImage = nullptr;

engines/buried/graphics.cpp
@@ -214,12 +214,12 @@ Graphics::Surface *GraphicsManager::getBitmap(Common::SeekableReadStream *stream
 	// Convert to the screen format, if required
 	if (decoder.getSurface()->format != g_system->getScreenFormat()) {
 		assert(_vm->isTrueColor());
-		return decoder.getSurface()->convertTo(g_system->getScreenFormat(), decoder.getPalette());
+		return decoder.getSurface()->convertTo(g_system->getScreenFormat(), decoder.getPalette().data(), decoder.getPalette().size());
 	}
 
 	// Remap the palette, if required
-	if (!_vm->isTrueColor() && memcmp(decoder.getPalette() + 3, getDefaultPalette() + 3, 256 - 6) != 0)
-		return remapPalettedFrame(decoder.getSurface(), decoder.getPalette());
+	if (!_vm->isTrueColor() && memcmp(decoder.getPalette().data() + 3, getDefaultPalette() + 3, 256 - 6) != 0)
+		return remapPalettedFrame(decoder.getSurface(), decoder.getPalette().data());
 
 	// Just copy the frame
 	Graphics::Surface *surface = new Graphics::Surface();

engines/hopkins/graphics.cpp
@@ -327,7 +327,7 @@ void GraphicsManager::loadPCX640(byte *surface, const Common::Path &file, byte *
 	Common::copy((const byte *)s->getPixels(), (const byte *)s->getBasePtr(0, s->h), surface);
 
 	// Copy out the palette
-	const byte *palSrc = pcxDecoder.getPalette();
+	const byte *palSrc = pcxDecoder.getPalette().data();
 	Common::copy((const byte *)palSrc, (const byte *)palSrc + PALETTE_BLOCK_SIZE, palette);
 
 	f.close();

engines/nancy/graphics.cpp
@@ -219,7 +219,7 @@ void GraphicsManager::loadSurfacePalette(Graphics::ManagedSurface &inSurf, const
 	if (f.open(paletteFilename.append(".bmp"))) {
 		Image::BitmapDecoder dec;
 		if (dec.loadStream(f)) {
-			inSurf.setPalette(dec.getPalette(), paletteStart, paletteSize);
+			inSurf.setPalette(dec.getPalette().data(), paletteStart, paletteSize);
 		}
 	}
 }

engines/prince/graphics.cpp
@@ -373,7 +373,7 @@ byte GraphicsMan::getBlendTableColor(byte pixelColor, byte backgroundPixelColor,
 	if (blendTable[pixelColor] != 255) {
 		currColor = blendTable[pixelColor];
 	} else {
-		const byte *originalPalette = _vm->_roomBmp->getPalette();
+		const byte *originalPalette = _vm->_roomBmp->getPalette().data();
 
 		int redFirstOrg = originalPalette[pixelColor * 3] * _vm->_mst_shadow / 256;
 		redFirstOrg = CLIP(redFirstOrg, 0, 255);
@@ -434,7 +434,7 @@ byte GraphicsMan::getBlendTableColor(byte pixelColor, byte backgroundPixelColor,
 
 void GraphicsMan::makeShadowTable(int brightness, byte *shadowPalette) {
 	int shadow =  brightness * 256 / 100;
-	const byte *originalPalette = _vm->_roomBmp->getPalette();
+	const byte *originalPalette = _vm->_roomBmp->getPalette().data();
 
 	for (int i = 0; i < 256; i++) {
 		int redFirstOrg = originalPalette[3 * i] * shadow / 256;

commit 80b895bdebd912dc8240da3b40611b433ceebb7f

SUPERNOVA: Fix crash when loading images
This was a regression from 77ee5a9.

engines/supernova/graphics.cpp
@@ -155,10 +155,12 @@ bool MSNImage::loadStream(Common::SeekableReadStream &stream) {
 		_palette.set(47, 0xE0, 0xE0, 0xE0);
 	} else {
 		pal_diff = 1;
-		byte r = stream.readByte() << 2;
-		byte g = stream.readByte() << 2;
-		byte b = stream.readByte() << 2;
-		_palette.set(239 - flag, r, g, b);
+		for (int i = flag; i != 0; --i) {
+			byte r = stream.readByte() << 2;
+			byte g = stream.readByte() << 2;
+			byte b = stream.readByte() << 2;
+			_palette.set(239 - i, r, g, b);
+		}
 	}
 
 	_numSections = stream.readByte();