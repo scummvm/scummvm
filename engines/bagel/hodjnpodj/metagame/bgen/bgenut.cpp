c441c30853f5d1a8d581cc68a78aac5123ac7fb1
Eugene Sandulenko <sev@scummvm.org>
2025-06-10 11:14:52 +0200

    JANITORIAL: Code formatting

--- engines/bagel/hodjnpodj/metagame/bgen/bgenut.cpp
+++ engines/bagel/hodjnpodj/metagame/bgen/bgenut.cpp
@@ -30,28 +30,29 @@ namespace Metagame {
 ///DEFS bgenut.h
 
 //* CGenUtil::RandomInteger -- find random integer in specified range
-int CGenUtil::RandomInteger(int iLow, int iHigh)
+int CGenUtil::RandomInteger(int iLow, int iHigh) {
 // iLow, iHigh -- range
 // returns: integer which is ge iLow and le iHigh.
-{
-	JXENTER(CGenUtil::RandomInteger) ;
+	JXENTER(CGenUtil::RandomInteger);
 	int iRetval = 0;            // integer return value
-//	time_t tTime ;              // time -- for initializing random # gen
+//	time_t tTime;              // time -- for initializing random # gen
 
 	// for first call, initialize the random number generator
-	if (!m_bRandomInit)
-		//srand((unsigned) time(&tTime)), m_bRandomInit = TRUE ;
+	if (!m_bRandomInit) {
+		//srand((unsigned) time(&tTime)), m_bRandomInit = TRUE;
 
 		// Note: since brand() returns a value uniform in [0, 2^15-1], the
 		// following method, though simple, gives a slight preference
 		// to lower numbers.  Might be worth fixing sometime.
-		if (iLow < iHigh)           // if there's a valid range
-			iRetval = iLow + (brand() % (iHigh - iLow + 1)) ;  // compute value
-		else                // invalid args
-			iRetval = iLow ;
+		if (iLow < iHigh) {           // if there's a valid range
+			iRetval = iLow + (brand() % (iHigh - iLow + 1));  // compute value
+		} else {               // invalid args
+			iRetval = iLow;
+		}
+	}
 
-	JXELEAVE(CGenUtil::RandomInteger) ;
-	return (iRetval) ;
+	JXELEAVE(CGenUtil::RandomInteger);
+	return (iRetval);
 }
 
 
@@ -59,88 +60,84 @@ int CGenUtil::RandomInteger(int iLow, int iHigh)
 //      Generates a permutation of size iSize of the
 //      integers from 0 to (iNum-1)
 void CGenUtil::RandomPermutation(int iNum,
-                                 int iSize, int *xpIntOut)
+                                 int iSize, int *xpIntOut) {
 // iNum -- number of numbers in permutation
 // iSize -- size of output array
 // xpIntOut -- pointer to output array (size is iSize)
-{
-	int *xpInt ;        // array pointer variable
-	int iI, iJ, iK ;    // loop variables
-	int iRanVal ;       // random value to make a choice
-	BOOL bDup ;         // duplicate found flag
+	int *xpInt;        // array pointer variable
+	int iI, iJ, iK;    // loop variables
+	int iRanVal;       // random value to make a choice
+	BOOL bDup;         // duplicate found flag
 
-	JXENTER(CGenUtil::RandomPermutation) ;
+	JXENTER(CGenUtil::RandomPermutation);
 	if (iSize > iNum)   // can't handle more array than # of numbers
-		iSize = iNum ;
+		iSize = iNum;
 
-	for (iI = 0 ; iI < iSize ; ++iI) {          // count numbers
-		iRanVal = RandomInteger(0, iNum - iI - 1) ;
+	for (iI = 0; iI < iSize; ++iI) {          // count numbers
+		iRanVal = RandomInteger(0, iNum - iI - 1);
 		// get random number in interval
-		for (iJ = 0 ; iJ < iNum && iRanVal >= 0 ; ++iJ) {
-			bDup = FALSE ;              // not a duplicate yet
-			for (xpInt = xpIntOut, iK = 0 ; iK < iI ; ++iK, ++xpInt)
+		for (iJ = 0; iJ < iNum && iRanVal >= 0; ++iJ) {
+			bDup = FALSE;              // not a duplicate yet
+			for (xpInt = xpIntOut, iK = 0; iK < iI; ++iK, ++xpInt)
 				if (*xpInt == iJ)
-					bDup = TRUE ;
+					bDup = TRUE;
 			if (!bDup && iRanVal-- == 0)        // if not a duplicate
 				// and we've counted down the random integer
-				*xpInt = iJ ;           // store into array
+				*xpInt = iJ;           // store into array
 		}
 	}
-	JXELEAVE(CGenUtil::RandomPermutation) ;
+	JXELEAVE(CGenUtil::RandomPermutation);
 }
 
 //* CGenUtil::NormalizeCRect --
-BOOL PASCAL CGenUtil::NormalizeCRect(CRect& cRect)
+BOOL PASCAL CGenUtil::NormalizeCRect(CRect &cRect) {
 // cRect -- rectangle to be normalized
 // returns: TRUE if error, FALSE otherwise
-{
-	JXENTER(CGenUtil::NormalizeCRect) ;
-	int iError = 0 ;            // error code
-	int iSwap ;
+	JXENTER(CGenUtil::NormalizeCRect);
+	int iError = 0;            // error code
+	int iSwap;
 
 	if ((iSwap = cRect.left) > cRect.right)
-		cRect.left = cRect.right, cRect.right = iSwap ;
+		cRect.left = cRect.right, cRect.right = iSwap;
 
 	if ((iSwap = cRect.top) > cRect.bottom)
-		cRect.top = cRect.bottom, cRect.bottom = iSwap ;
+		cRect.top = cRect.bottom, cRect.bottom = iSwap;
 
 // cleanup:
 
-	JXELEAVE(CGenUtil::NormalizeCRect) ;
-	RETURN(iError != 0) ;
+	JXELEAVE(CGenUtil::NormalizeCRect);
+	RETURN(iError != 0);
 }
 
 
 //* CGenUtil::RandomSelection -- random selection of integer array
-BOOL CGenUtil::RandomSelection(int *xpiArray, int iNum, int iSize)
+BOOL CGenUtil::RandomSelection(int *xpiArray, int iNum, int iSize) {
 // xpiArray (input/output) -- array of integers to make selection from
 // iNum -- number of numbers in array
 // iSize -- number of integers in desired selection
-{
-	JXENTER(CGenUtil::RandomSelection) ;
-	int iError = 0 ;
-	int *xpiPermutation ;
-	int iK ;
+	JXENTER(CGenUtil::RandomSelection);
+	int iError = 0;
+	int *xpiPermutation;
+	int iK;
 
 	if (iSize > 0) {
-
 		if ((xpiPermutation = new int [iSize]) != nullptr) {
+			RandomPermutation(iNum, iSize, xpiPermutation);
 
-			RandomPermutation(iNum, iSize, xpiPermutation) ;
+			for (iK = 0; iK < iSize; ++iK)
+				xpiPermutation[iK] = xpiArray[xpiPermutation[iK]];
 
-			for (iK = 0 ; iK < iSize ; ++iK)
-				xpiPermutation[iK] = xpiArray[xpiPermutation[iK]] ;
+			for (iK = 0; iK < iSize; ++iK)
+				xpiArray[iK] = xpiPermutation[iK];
 
-			for (iK = 0 ; iK < iSize ; ++iK)
-				xpiArray[iK] = xpiPermutation[iK] ;
-
-			delete [] xpiPermutation ;
-		} else
-			iError = 100 ;              // can't allocate array
+			delete [] xpiPermutation;
+		} else {
+			iError = 100;              // can't allocate array
+		}
 	}
 
-	JXELEAVE(CGenUtil::RandomSelection) ;
-	RETURN(iError != 0) ;
+	JXELEAVE(CGenUtil::RandomSelection);
+	RETURN(iError != 0);
 }
 
 } // namespace Metagame
