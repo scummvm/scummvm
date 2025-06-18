7f24b9b40dc5a0c2d4c91119e42872078e6e3378
Florian Mehm <florian@mehm.net>
2025-06-19 01:07:34 +0200

    MACS2: implement opcodes 0x48, 0x49, 0x4B for object properties
    Implemented object position/orientation retrieval opcodes with FuncA334
    variable storage and added inventory icon width sanity check.

--- engines/macs2/script/scriptexecutor.h
+++ engines/macs2/script/scriptexecutor.h
@@ -205,6 +205,9 @@ class GameObject;
 			// Returns only the first of the two 16 bit values
 			uint16 Func9F4D_16();
 
+			// Saves the given value in a script variable
+			void FuncA334(uint32 value);
+
 			// fn0037_C991 proc
 			// Implements a walk to
 			void FuncC991();
@@ -262,6 +265,8 @@ class GameObject;
 			Common::Array<ScriptVariable> _variables;
 
 			void SetVariableValue(uint16 index, uint16 a, uint16 b);
+
+			void SetVariableValue(uint16 index, uint32 value);
 
 
 			Common::Point GetCharPosition();
