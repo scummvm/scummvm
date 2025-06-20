b5f6c2786a44dbe194b1dca34050bb67d46a010f
aunnoman1 <aunnoman123@outlook.com>
2025-06-20 23:11:37 +0500

    I18N: Consistent wording in "Open door" keymapper action in engines

--- engines/pegasus/pegasus.cpp
+++ engines/pegasus/pegasus.cpp
@@ -2721,7 +2721,7 @@ Common::KeymapArray PegasusEngine::initKeymaps() {
 
 	Action *act;
 
-	act = new Action(kStandardActionMoveUp, _("Up/Zoom in/Move forward/Open Doors"));
+	act = new Action(kStandardActionMoveUp, _("Up/Zoom in/Move forward/Open door"));
 	act->setCustomEngineActionEvent(kPegasusActionUp);
 	act->addDefaultInputMapping("UP");
 	act->addDefaultInputMapping("KP8");
