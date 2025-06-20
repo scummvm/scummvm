ca5b3e44e2d69049e1570d53737e375badeac014
Florian Mehm <florian@mehm.net>
2025-06-21 00:29:40 +0200

    MACS2: add crowbar, dynamite, and pulley object names
    Added German names for crowbar, dynamite, and pulley game objects.

--- engines/macs2/gameobjects.cpp
+++ engines/macs2/gameobjects.cpp
@@ -161,11 +161,13 @@ void Macs2::GameObjects::Init() {
 	ObjectNames[0x8D] = "Wolle"; // mit Alkohol
 	ObjectNames[0x8E] = "Holzwürfel";
 	ObjectNames[0x8F] = "Brief";
+	ObjectNames[0x99] = "Brecheisen";
 	ObjectNames[0x9C] = "Fackel"; // brennend
 	ObjectNames[0x9D] = "Fackel"; // erloschen
 	ObjectNames[0x98] = "Bohlen";
+	ObjectNames[0xA1] = "Seilzug";
 	ObjectNames[0xA2] = "Haken";
-	ObjectNames[0xA2] = "Haken und Seil";
+	ObjectNames[0xA3] = "Haken und Seil";
 	ObjectNames[0xAE] = "Seil";
 }
 
