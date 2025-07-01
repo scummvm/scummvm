8302c01a82cdb46a2c091bc91bfa843a23cbb4f8
Alikhan Balpykov <luxrage1990@gmail.com>
2025-07-01 19:55:55 +0500

    CREATE_TEENAGENT: JANITORIAL: Add "english" prefix to dialog and message string arrays

--- devtools/create_teenagent/create_teenagent.cpp
+++ devtools/create_teenagent/create_teenagent.cpp
@@ -48,16 +48,16 @@ int main(int argc, char *argv[]) {
 	}
 
 	// Write out message string block
-	for (uint i = 0; i < (sizeof(messages)/sizeof(char*)); i++) {
+	for (uint i = 0; i < (sizeof(englishMessages)/sizeof(char*)); i++) {
 		if (i == 0) {
 			// Write out reject message pointer block
 			uint16 off = DSEG_STARTBLK_SIZE + (4 * 2);
 			writeUint16LE(fout, off);
-			off += strlen(messages[0]) + 2;
+			off += strlen(englishMessages[0]) + 2;
 			writeUint16LE(fout, off);
-			off += strlen(messages[1]) + 2;
+			off += strlen(englishMessages[1]) + 2;
 			writeUint16LE(fout, off);
-			off += strlen(messages[2]) + 2;
+			off += strlen(englishMessages[2]) + 2;
 			writeUint16LE(fout, off);
 		}
 
@@ -65,25 +65,25 @@ int main(int argc, char *argv[]) {
 			// Write out book color pointer block
 			uint16 off = DSEG_STARTBLK_SIZE + (4 * 2);
 			for (uint k = 0; k < 327; k++)
-				off += strlen(messages[k]) + 2;
+				off += strlen(englishMessages[k]) + 2;
 			off += (6 * 2);
 			writeUint16LE(fout, off);
-			off += strlen(messages[327]) + 2;
+			off += strlen(englishMessages[327]) + 2;
 			writeUint16LE(fout, off);
-			off += strlen(messages[328]) + 2;
+			off += strlen(englishMessages[328]) + 2;
 			writeUint16LE(fout, off);
-			off += strlen(messages[329]) + 2;
+			off += strlen(englishMessages[329]) + 2;
 			writeUint16LE(fout, off);
-			off += strlen(messages[330]) + 2;
+			off += strlen(englishMessages[330]) + 2;
 			writeUint16LE(fout, off);
-			off += strlen(messages[331]) + 2;
+			off += strlen(englishMessages[331]) + 2;
 			writeUint16LE(fout, off);
 		}
-		for (uint j = 0; j < strlen(messages[i]); j++) {
-			if (messages[i][j] == '\n')
+		for (uint j = 0; j < strlen(englishMessages[i]); j++) {
+			if (englishMessages[i][j] == '\n')
 				writeByte(fout, '\0');
 			else
-				writeByte(fout, messages[i][j]);
+				writeByte(fout, englishMessages[i][j]);
 		}
 		writeByte(fout, '\0');
 		writeByte(fout, '\0');
@@ -101,23 +101,23 @@ int main(int argc, char *argv[]) {
 
 	// Write out dialog string block
 	static const char nulls[6] = "\0\0\0\0\0";
-	for (uint i = 0; i < (sizeof(dialogs)/sizeof(char**)); i++) {
+	for (uint i = 0; i < (sizeof(englishDialogs)/sizeof(char**)); i++) {
 		//printf("Writing Dialog #%d\n", i);
 		bool dialogEnd = false;
 		uint j = 0;
 		while (!dialogEnd) {
 			uint nullCount = 0;
-			if (strcmp(dialogs[i][j], NEW_LINE) == 0) {
+			if (strcmp(englishDialogs[i][j], NEW_LINE) == 0) {
 				nullCount = 1;
-			} else if (strcmp(dialogs[i][j], DISPLAY_MESSAGE) == 0) {
+			} else if (strcmp(englishDialogs[i][j], DISPLAY_MESSAGE) == 0) {
 				nullCount = 2;
-			} else if (strcmp(dialogs[i][j], CHANGE_CHARACTER) == 0) {
+			} else if (strcmp(englishDialogs[i][j], CHANGE_CHARACTER) == 0) {
 				nullCount = 3;
-			} else if (strcmp(dialogs[i][j], END_DIALOG) == 0) {
+			} else if (strcmp(englishDialogs[i][j], END_DIALOG) == 0) {
 				nullCount = 4;
 				dialogEnd = true;
 			} else { // Deals with normal dialogue and ANIM_WAIT cases
-				if (fwrite(dialogs[i][j], 1, strlen(dialogs[i][j]), fout) != strlen(dialogs[i][j])) {
+				if (fwrite(englishDialogs[i][j], 1, strlen(englishDialogs[i][j]), fout) != strlen(englishDialogs[i][j])) {
 					perror("Writing dialog string");
 					exit(1);
 				}
