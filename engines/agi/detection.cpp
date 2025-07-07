eb26bcbc7a7f2de71d2714d9195d12238faba70a
Filippos Karapetis <bluegr@gmail.com>
2025-07-07 04:22:42 +0300

    AGI: Use GUI options for fan made games in the fallback detector
    Such matched game entries are usually fan made games, so it makes sense
    to use the associated GUI options, too

--- engines/agi/detection.cpp
+++ engines/agi/detection.cpp
@@ -151,6 +151,7 @@ ADDetectedGame AgiMetaEngineDetection::fallbackDetect(const FileMap &allFilesXXX
 	g_fallbackDesc.desc.language = Common::UNK_LANG;
 	g_fallbackDesc.desc.platform = Common::kPlatformDOS;
 	g_fallbackDesc.desc.flags = ADGF_NO_FLAGS;
+	g_fallbackDesc.desc.guiOptions = GAMEOPTIONS_FANMADE_MOUSE;
 
 	// Set default values for the fallback descriptor's AGIGameDescription part.
 	g_fallbackDesc.gameID = GID_FANMADE;
