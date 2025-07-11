531042c1e122ac6af23c5527f91127e37e9a26b0
Alikhan Balpykov <luxrage1990@gmail.com>
2025-07-11 18:55:46 +0500

    WAGE: Add fallback detection

--- engines/wage/detection.cpp
+++ engines/wage/detection.cpp
@@ -22,6 +22,9 @@
 
 #include "base/plugins.h"
 
+#include "common/file.h"
+#include "common/macresman.h"
+
 #include "engines/advancedDetector.h"
 
 static const PlainGameDescriptor wageGames[] = {
@@ -40,7 +43,19 @@ static const PlainGameDescriptor wageGames[] = {
 #include "wage/detection_tables.h"
 #include "wage/detection.h"
 
+static ADGameDescription s_fallbackDesc = {
+	"wage",
+	"",
+	AD_ENTRY1(0, 0),
+	Common::EN_ANY,
+	Common::kPlatformMacintosh,
+	ADGF_NO_FLAGS,
+	GUIO0()
+};
+
 class WageMetaEngineDetection : public AdvancedMetaEngineDetection<ADGameDescription> {
+	mutable Common::String _filenameStr;
+
 public:
 	WageMetaEngineDetection() : AdvancedMetaEngineDetection(Wage::gameDescriptions, wageGames) {
 		_md5Bytes = 2 * 1024 * 1024;
@@ -58,6 +73,53 @@ public:
 	const char *getOriginalCopyright() const override {
 		return "World Builder (C) Silicon Beach Software";
 	}
+
+	ADDetectedGame fallbackDetect(const FileMap &allFiles, const Common::FSList &fslist, ADDetectedGameExtraInfo **extra) const override;
 };
+
+ADDetectedGame WageMetaEngineDetection::fallbackDetect(const FileMap &allFiles, const Common::FSList &fslist, ADDetectedGameExtraInfo **extra) const {
+	SearchMan.addDirectory("WageMetaEngineDetection::fallbackDetect", fslist.begin()->getParent());
+
+	for (Common::FSList::const_iterator fs = fslist.begin(); fs != fslist.end(); ++fs) {
+		if (fs->isDirectory())
+			continue;
+
+		Common::Path filePath = Common::Path(fs->getPathInArchive());
+		Common::MacResManager resManager;
+		if (!resManager.open(filePath)) {
+			continue;
+		}
+
+		Common::MacFinderInfo finderInfo;
+		if (resManager.getFileFinderInfo(filePath, finderInfo)) {
+			if (READ_BE_UINT32(finderInfo.type) != MKTAG('A', 'P', 'P', 'L')) {
+				continue;
+			}
+			if (READ_BE_UINT32(finderInfo.creator) != MKTAG('W', 'E', 'D', 'T')) {
+				continue;
+			}
+
+			Common::Path outPath(fs->getFileName());
+			_filenameStr = outPath.toString();
+
+			s_fallbackDesc.filesDescriptions[0].fileName = _filenameStr.c_str();
+
+			ADDetectedGame game;
+			game.desc = &s_fallbackDesc;
+
+			FileProperties tmp;
+			if (getFileProperties(allFiles, kMD5MacResFork, filePath, tmp)) {
+				game.hasUnknownFiles = true;
+				game.matchedFiles[filePath] = tmp;
+			}
+
+			SearchMan.remove("WageMetaEngineDetection::fallbackDetect");
+			return game;
+		}
+	}
+
+	SearchMan.remove("WageMetaEngineDetection::fallbackDetect");
+	return ADDetectedGame();
+}
 
 REGISTER_PLUGIN_STATIC(WAGE_DETECTION, PLUGIN_TYPE_ENGINE_DETECTION, WageMetaEngineDetection);
