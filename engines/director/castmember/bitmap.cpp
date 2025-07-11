45919f1c3a961228b95ac7047d6d8a897fd5d267
Malhar <themalharbdv2046@gmail.com>
2025-07-11 18:06:21 +0530

    DIRECTOR: Fix bitmap writing for 1/2/4bpp
    Fix a bug where the last column of bits wasn't being written

--- engines/director/castmember/bitmap.cpp
+++ engines/director/castmember/bitmap.cpp
@@ -1068,27 +1068,28 @@ uint32 BitmapCastMember::writeBITDResource(Common::MemoryWriteStream *writeStrea
 	for (int y = 0; y < _picture->_surface.h; y++) {
 		for (int x = 0; x < _picture->_surface.w;) {
 			uint32 color = 0;
+			int startX = x;
 
 			switch (_bitsPerPixel) {
 			case 1:
 				for (int c = 0; c < 8 && x < _picture->_surface.w; c++, x++) {
 					color += (*((byte *)_picture->_surface.getBasePtr(x, y))) & (1 << (7 - c));
 				}
-				pixels[(y * _pitch) + ((x - 8) >> 3)] = color;
+				pixels[(y * _pitch) + (startX >> 3)] = color;
 				break;
 
 			case 2:
 				for (int c = 0; c < 4 && x < _picture->_surface.w; c++, x++) {
 					color += (*((byte *)_picture->_surface.getBasePtr(x, y)) & 0x3) << (2 * (3 - c));
 				}
-				pixels[(y * _pitch) + ((x - 4) >> 2)] = color;
+				pixels[(y * _pitch) + (startX >> 2)] = color;
 				break;
 
 			case 4:
 				for (int c = 0; c < 2 && x < _picture->_surface.w; c++, x++) {
 					color += (*((byte *)_picture->_surface.getBasePtr(x, y)) & 0xF) << (4 * (1 - c));
 				}
-				pixels[(y * _pitch) + ((x - 2) >> 1)] = color;
+				pixels[(y * _pitch) + (startX >> 1)] = color;
 				break;
 
 			case 8:
