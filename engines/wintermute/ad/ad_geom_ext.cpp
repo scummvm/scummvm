9cc5d3ce3583a7ebab02afecf82a6c7eff1c97d2
Paweł Kołodziejski <aquadran@gmail.com>
2025-07-22 23:52:32 +0200

    WINTERMUTE: Small sync with original on ad_geom_ext

--- engines/wintermute/ad/ad_geom_ext.cpp
+++ engines/wintermute/ad/ad_geom_ext.cpp
@@ -41,8 +41,9 @@ AdGeomExt::AdGeomExt(BaseGame *in_gameRef) : BaseClass(in_gameRef) {
 
 //////////////////////////////////////////////////////////////////////////
 AdGeomExt::~AdGeomExt() {
-	for (uint i = 0; i < _nodes.getSize(); i++) {
+	for (uint32 i = 0; i < _nodes.getSize(); i++) {
 		delete _nodes[i];
+		_nodes[i] = nullptr;
 	}
 	_nodes.removeAll();
 }
@@ -144,7 +145,7 @@ AdGeomExtNode *AdGeomExt::matchName(const char *name) {
 		return nullptr;
 	}
 
-	for (uint i = 0; i < _nodes.getSize(); i++) {
+	for (uint32 i = 0; i < _nodes.getSize(); i++) {
 		if (_nodes[i]->matchesName(name)) {
 			return _nodes[i];
 		}
