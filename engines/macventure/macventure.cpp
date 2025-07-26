703be4cef3e83e3f8ea98695867ee0074b7ed756
Alikhan Balpykov <luxrage1990@gmail.com>
2025-07-26 23:40:58 +0500

    MACVENTURE: Display full title of inventory window

--- engines/macventure/macventure.cpp
+++ engines/macventure/macventure.cpp
@@ -775,10 +775,6 @@ void MacVentureEngine::openObject(ObjID objID) {
 		Common::Point p(_world->getObjAttr(objID, kAttrPosX), _world->getObjAttr(objID, kAttrPosY));
 		WindowReference invID = _gui->createInventoryWindow(objID);
 		Common::String title = _world->getText(objID, objID, objID);
-		// HACK, trim titletext to fit initial inventory size
-		while (title.size() > 6) {
-			title.deleteLastChar();
-		}
 		_gui->setWindowTitle(invID, title);
 		_gui->updateWindowInfo(invID, objID, _world->getChildren(objID, true));
 		_gui->updateWindow(invID, _world->getObjAttr(objID, kAttrContainerOpen));
