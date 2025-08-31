113a8f862ae2ffe9ff2980ac3757db7f9e24807f
Donovan Watteau <contrib@dwatteau.fr>
2025-08-31 13:26:28 +0200

    COMMON: Drop STATIC_ASSERT compat code for pre-C++11 compilers
    With C++11 already being a requirement, the compat code for pre-C++11
    compiler is not needed anymore.

--- common/scummsys.h
+++ common/scummsys.h
@@ -131,30 +131,18 @@
 #endif
 
 #ifndef STATIC_ASSERT
-#if __cplusplus >= 201103L || defined(_MSC_VER)
 	/**
 	 * Generates a compile-time assertion.
 	 *
 	 * @param expression An expression that can be evaluated at compile time.
 	 * @param message An underscore-delimited message to be presented at compile
 	 * time if the expression evaluates to false.
+	 *
+	 * TODO: Now that we require C++11, all uses of this macro could just use
+	 * static_assert().
 	 */
 	#define STATIC_ASSERT(expression, message) \
 		static_assert((expression), #message)
-#else
-	/**
-	 * Generates a compile-time assertion.
-	 *
-	 * @param expression An expression that can be evaluated at compile time.
-	 * @param message An underscore-delimited message to be presented at compile
-	 * time if the expression evaluates to false.
-	 */
-	#define STATIC_ASSERT(expression, message) \
-		do { \
-			extern int STATIC_ASSERT_##message[(expression) ? 1 : -1]; \
-			(void)(STATIC_ASSERT_##message); \
-		} while (false)
-#endif
 #endif
 
 // The following math constants are usually defined by the system math.h header, but
