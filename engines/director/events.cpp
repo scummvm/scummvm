scummvm/scummvm

commit 02b3aef3c37746583ac8dd3ad9bd87bfcc7323b2

DIRECTOR: LINGO: Fix 'the clickOn' behavior
It is set only when there is a click. In the previous code
version we also were reacting to mouseUp and mouseMove events

engines/director/events.cpp
@@ -135,6 +135,13 @@ bool Movie::processEvent(Common::Event &event) {
 	}
 	uint16 spriteId = 0;
 
+	if (event.mouse != Common::Point(-1, -1)) {
+		if (g_director->getVersion() < 400)
+			spriteId = _score->getActiveSpriteIDFromPos(event.mouse);
+		else
+			spriteId = _score->getMouseSpriteIDFromPos(event.mouse);
+	}
+
 	Common::Point pos;
 
 	switch (event.type) {
@@ -162,11 +169,6 @@ bool Movie::processEvent(Common::Event &event) {
 
 		// for the list style button, we still have chance to trigger events though button.
 		if (!(g_director->_wm->_mode & Graphics::kWMModeButtonDialogStyle) && g_director->_wm->_mouseDown && g_director->_wm->_hilitingWidget) {
-			if (g_director->getVersion() < 400)
-				spriteId = sc->getActiveSpriteIDFromPos(pos);
-			else
-				spriteId = sc->getMouseSpriteIDFromPos(pos);
-
 			if (spriteId > 0 && sc->_channels[spriteId]->_sprite->shouldHilite()) {
 				_currentHiliteChannelId = spriteId;
 				g_director->getCurrentWindow()->setDirty(true);
@@ -189,8 +191,6 @@ bool Movie::processEvent(Common::Event &event) {
 		}
 
 		if (g_director->getVersion() >= 600) {
-			spriteId = sc->getMouseSpriteIDFromPos(pos);
-
 			if (spriteId > 0) {
 				if (spriteId != _lastEnteredChannelId) {
 					if (_lastEnteredChannelId) {
@@ -217,6 +217,8 @@ bool Movie::processEvent(Common::Event &event) {
 			sc->renderCursor(pos, true);
 		} else {
 			pos = event.mouse;
+
+			_lastClickedSpriteId = spriteId; // for 'the clickOn'
 
 			// FIXME: Check if these are tracked with the right mouse button
 			_lastEventTime = g_director->getMacTicks();

engines/director/lingo/lingo-events.cpp
@@ -128,8 +128,6 @@ void Movie::resolveScriptEvent(LingoEvent &event) {
 			spriteId = _score->getActiveSpriteIDFromPos(event.mousePos);
 		else
 			spriteId = _score->getMouseSpriteIDFromPos(event.mousePos);
-
-		_lastClickedSpriteId = _score->getActiveSpriteIDFromPos(event.mousePos); // the clickOn
 	}
 	// Very occasionally, we want to specify an event with a channel ID
 	// rather than infer it from the position. Allow it to override.

commit cdad826b5ee871c8b9add722a996227a6907cdb1

DIRECTOR: LINGO: Fix regression about `clickOn`
Introduced in 02b3aef3c37746583ac8dd3ad9bd87bfcc7323b2. We are
losing spriteId Movie::processEvent(), it is always 0.

Fixes pip-up window (like Open) in simpsonsstudio

engines/director/events.cpp
@@ -234,8 +234,6 @@ bool Movie::processEvent(Common::Event &event) {
 				}
 			}
 
-			_lastClickedSpriteId = spriteId; // for 'the clickOn'
-
 			// FIXME: Check if these are tracked with the right mouse button
 			_lastEventTime = g_director->getMacTicks();
 			_lastClickTime2 = _lastClickTime;

engines/director/lingo/lingo-events.cpp
@@ -128,6 +128,9 @@ void Movie::resolveScriptEvent(LingoEvent &event) {
 			spriteId = _score->getActiveSpriteIDFromPos(event.mousePos);
 		else
 			spriteId = _score->getMouseSpriteIDFromPos(event.mousePos);
+
+		if (event.event == kEventMouseDown || event.event == kEventRightMouseDown)
+			_lastClickedSpriteId = _score->getActiveSpriteIDFromPos(event.mousePos); // the clickOn
 	}
 	// Very occasionally, we want to specify an event with a channel ID
 	// rather than infer it from the position. Allow it to override.