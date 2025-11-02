72612d55587daaaecc1f5736eaee2164b83948b5
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2025-11-02 23:37:37 +0000

    AGDS: remove tools

--- engines/agds/tools/decrypt.py
+++
@@ -1,8 +0,0 @@
-key = bytes(map(lambda c: ord(c), 'Vyvojovy tym AGDS varuje: Hackerovani skodi obchodu!'))
-key_n = len(key)
-
-def decrypt(src):
-	r = bytearray()
-	for i in range(len(src)):
-		r.append(src[i] ^ key[i % key_n] ^ 0xff)
-	return r
