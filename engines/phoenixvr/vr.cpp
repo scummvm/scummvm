67c5c910d066fc58bf8440787f394a57291d20db
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2025-11-17 11:52:01 +0000

    PHOENIXVR: more RE on picture compression

--- engines/phoenixvr/vr.cpp
+++ engines/phoenixvr/vr.cpp
@@ -19,6 +19,208 @@ const byte ZIGZAG[] = {
 	15, 23, 30, 37, 44, 51, 58, 59, 52, 45, 38, 31, 39,
 	46, 53, 60, 61, 54, 47, 55, 62, 63};
 
+// looks like standard JPEG quantisation matrix
+const char QY[] = {
+	16,
+	11,
+	10,
+	16,
+	24,
+	40,
+	51,
+	61,
+	12,
+	12,
+	14,
+	19,
+	26,
+	58,
+	60,
+	55,
+	14,
+	13,
+	16,
+	24,
+	40,
+	57,
+	69,
+	56,
+	14,
+	17,
+	22,
+	29,
+	51,
+	87,
+	80,
+	62,
+	18,
+	22,
+	37,
+	56,
+	68,
+	109,
+	103,
+	77,
+	24,
+	35,
+	55,
+	64,
+	81,
+	104,
+	113,
+	92,
+	49,
+	64,
+	78,
+	87,
+	103,
+	121,
+	120,
+	101,
+	72,
+	92,
+	95,
+	98,
+	112,
+	100,
+	103,
+	99,
+};
+
+const char QUV[] = {
+	17,
+	18,
+	24,
+	47,
+	99,
+	99,
+	99,
+	99,
+	18,
+	21,
+	26,
+	66,
+	99,
+	99,
+	99,
+	99,
+	24,
+	26,
+	56,
+	99,
+	99,
+	99,
+	99,
+	99,
+	47,
+	66,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+	99,
+};
+
+const uint16 Q[] = {
+	0x4000,
+	0x58C5,
+	0x539F,
+	0x4B42,
+	0x4000,
+	0x3249,
+	0x22A3,
+	0x11A8,
+	0x58C5,
+	0x7B21,
+	0x73FC,
+	0x6862,
+	0x58C5,
+	0x45BF,
+	0x300B,
+	0x187E,
+	0x539F,
+	0x73FC,
+	0x6D41,
+	0x6254,
+	0x539F,
+	0x41B3,
+	0x2D41,
+	0x1712,
+	0x4B42,
+	0x6862,
+	0x6254,
+	0x587E,
+	0x4B42,
+	0x3B21,
+	0x28BA,
+	0x14C3,
+	0x4000,
+	0x58C5,
+	0x539F,
+	0x4B42,
+	0x4000,
+	0x3249,
+	0x22A3,
+	0x11A8,
+	0x3249,
+	0x45BF,
+	0x41B3,
+	0x3B21,
+	0x3249,
+	0x2782,
+	0x1B37,
+	0x0DE0,
+	0x22A3,
+	0x300B,
+	0x2D41,
+	0x28BA,
+	0x22A3,
+	0x1B37,
+	0x12BF,
+	0x98E,
+	0x11A8,
+	0x187E,
+	0x1712,
+	0x14C3,
+	0x11A8,
+	0x0DE0,
+	0x98E,
+	0x4DF,
+};
+
 struct HuffChar {
 	short next;
 	short falseIdx;
@@ -146,7 +348,7 @@ Common::Array<byte> unpackHuffman(const byte *huff, uint huffSize) {
 	return decoded;
 }
 
-void unpack640x480(Graphics::Surface &pic, const byte *huff, uint huffSize, const byte *coeff, uint dataSize) {
+void unpack640x480(Graphics::Surface &pic, const byte *huff, uint huffSize, const byte *coeff, uint coefSize, int quality) {
 	auto decoded = unpackHuffman(huff, huffSize);
 	uint decodedOffset = 0;
 	static const Math::DCT dct(6, Math::DCT::DCT_III);
@@ -155,12 +357,25 @@ void unpack640x480(Graphics::Surface &pic, const byte *huff, uint huffSize, cons
 	static constexpr uint planeSize = planePitch * 480;
 	Common::Array<byte> planes(planeSize * 3, 0);
 
+	auto iquant = [&](uint channel, int idx) {
+		int v = ((channel == 0 ? QY : QUV)[idx] * quality + 50) / 100;
+		if (v > 255)
+			v = 255;
+		else if (v < 8) {
+			v = 8;
+		}
+
+		v *= Q[idx];
+		v >>= 13;
+		return v;
+	};
+
 	BitStream bs(coeff, 0);
 	uint channel = 0;
 	uint x0 = 0, y0 = 0;
 	while (decodedOffset < decoded.size()) {
 		float ac[64] = {};
-		ac[0] = 1.0f * bs.readUInt(8);
+		ac[0] = 1.0f * iquant(channel, 0) * bs.readUInt(8);
 		for (uint idx = 1; idx < 64;) {
 			auto b = decoded[decodedOffset++];
 			if (b == 0x00) {
@@ -172,13 +387,18 @@ void unpack640x480(Graphics::Surface &pic, const byte *huff, uint huffSize, cons
 				auto l = b & 0x0f;
 				idx += h;
 				if (l && idx < 64) {
-					ac[ZIGZAG[idx]] = 1024 * bs.readInt(l);
+					auto ac_idx = ZIGZAG[idx];
+					ac[ac_idx] = 1.0f * iquant(channel, ac_idx) * bs.readInt(l);
 					++idx;
 				}
 			}
 		}
-		dct.calc(ac);
 		Common::String str;
+		for (uint i = 0; i != 64; ++i)
+			str += Common::String::format("%g ", ac[i]);
+		debug("input block %s", str.c_str());
+		str.clear();
+		dct.calc(ac);
 		for (uint i = 0; i != 64; ++i)
 			str += Common::String::format("%g ", ac[i]);
 		debug("decoded block %s", str.c_str());
@@ -194,19 +414,19 @@ void unpack640x480(Graphics::Surface &pic, const byte *huff, uint huffSize, cons
 			}
 		}
 		const auto *src = ac;
-		str.clear();
+		// str.clear();
 		for (unsigned h = 8; h--; dst += planePitch - 8) {
 			for (unsigned w = 8; w--;) {
-				int v = *src++;
+				int v = (channel == 0 ? 128 : 0) + *src++;
 				if (v < 0)
 					v = 0;
 				if (v > 255)
 					v = 255;
-				str += Common::String::format("%d ", v);
+				// str += Common::String::format("%d ", v);
 				*dst++ = v;
 			}
 		}
-		debug("decoded block %s", str.c_str());
+		// debug("decoded block %s", str.c_str());
 	}
 	auto *y = planes.data();
 	auto *u = y + planeSize;
@@ -236,7 +456,8 @@ Graphics::Surface *VR::load640x480(const Graphics::PixelFormat &format, Common::
 			s.read(staticData.data(), staticData.size());
 			Graphics::Surface *pic = new Graphics::Surface();
 			pic->create(640, 480, format);
-			unpack640x480(*pic, staticData.data(), unpHuffSize, staticData.data() + coefOffset, staticData.size());
+			coefOffset += 8;
+			unpack640x480(*pic, staticData.data(), unpHuffSize, staticData.data() + coefOffset, staticData.size() - coefOffset, unk0);
 			return pic;
 		}
 		s.skip(chunkSize - 8);
