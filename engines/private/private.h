680ddbb6ec42d914ca6f40715d7839838e12f233
sluicebox <22204938+sluicebox@users.noreply.github.com>
2025-11-18 20:38:18 -0800

    PRIVATE: Fix wall safe digit rendering
    The digits are masks with transparent backgrounds meant to be drawn on
    top of the safe graphic. We have been drawing a solid black rectangle
    first in order to update digits on the screen. This is not original
    behavior, and it's only needed because we're attempting to partially
    update the screen instead of redrawing it.
    
    Now the original effect is achieved by drawing the digits as regular
    masks without a solid black rectangle, and by responding to digit clicks
    by reloading the current setting.

--- engines/private/private.h
+++ engines/private/private.h
@@ -400,7 +400,6 @@ public:
 	void checkPhoneCall();
 
 	// Safe
-	uint32 _safeColor;
 	Common::String _safeNumberPath;
 	MaskInfo _safeDigitArea[3];
 	Common::Rect _safeDigitRect[3];
@@ -408,7 +407,6 @@ public:
 	void initializeWallSafeValue();
 	bool selectSafeDigit(Common::Point);
 	void addSafeDigit(uint32, Common::Rect*);
-	void renderSafeDigit(uint32);
 	int getSafeDigit(uint32 d);
 	void incrementSafeDigit(uint32 d);
 
