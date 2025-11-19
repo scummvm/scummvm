f3f990df52a08cf8f76f01efaad663045cd6e817
sluicebox <22204938+sluicebox@users.noreply.github.com>
2025-11-18 17:26:47 -0800

    PRIVATE: Fix wall safe initialization
    Wall safe now behaves like the original:
    
    - Wall safe is initialized from kWallSafeValue
    - kWallSafeValue is initialized to random value when starting game
    - Screen no longer flickers on each initial digit draw

--- engines/private/private.cpp
+++ engines/private/private.cpp
@@ -119,7 +119,6 @@ PrivateEngine::PrivateEngine(OSystem *syst, const ADGameDescription *gd)
 	_safeNumberPath = "sg/search_s/sgsaf%d.bmp";
 	for (uint d = 0 ; d < 3; d++) {
 		_safeDigitArea[d].clear();
-		_safeDigit[d] = 0;
 		_safeDigitRect[d] = Common::Rect(0, 0);
 	}
 }
@@ -297,6 +296,7 @@ Common::Error PrivateEngine::run() {
 	initGraphics(_screenW, _screenH, &_pixelFormat);
 	_transparentColor = 250;
 
+	initializeWallSafeValue();
 	_safeColor = _pixelFormat.RGBToColor(65, 65, 65);
 	_screenRect = Common::Rect(0, 0, _screenW, _screenH);
 	loadCursors();
@@ -538,7 +538,6 @@ void PrivateEngine::clearAreas() {
 			delete _safeDigitArea[d].surf;
 		}
 		_safeDigitArea[d].clear();
-		_safeDigit[d] = 0;
 		_safeDigitRect[d] = Common::Rect(0, 0);
 	}
 }
@@ -1244,6 +1243,20 @@ bool PrivateEngine::selectDossierPrevSuspect(Common::Point mousePos) {
 	return false;
 }
 
+void PrivateEngine::initializeWallSafeValue() {
+	if (isDemo()) {
+		return;
+	}
+
+	// initialize to a random value that is not the combination
+	Private::Symbol *sym = maps.variables.getVal(getWallSafeValueVariable());
+	int value;
+	do {
+		value = _rnd->getRandomNumber(999);
+	} while (value == 426);
+	sym->u.val = value;
+}
+
 bool PrivateEngine::selectSafeDigit(Common::Point mousePos) {
 	if (_safeDigitArea[0].surf == nullptr)
 		return false;
@@ -1254,10 +1267,8 @@ bool PrivateEngine::selectSafeDigit(Common::Point mousePos) {
 
 	for (uint d = 0 ; d < 3; d ++)
 		if (_safeDigitRect[d].contains(mousePos)) {
-			_safeDigit[d] = (_safeDigit[d] + 1) % 10;
+			incrementSafeDigit(d);
 			renderSafeDigit(d);
-			Private::Symbol *sym = maps.variables.getVal(getWallSafeValueVariable());
-			sym->u.val = 100*_safeDigit[0] + 10*_safeDigit[1] + _safeDigit[2];
 			return true;
 		}
 
@@ -1269,15 +1280,14 @@ void PrivateEngine::addSafeDigit(uint32 d, Common::Rect *rect) {
 	MaskInfo m;
 	_safeDigitRect[d] = *rect;
 	fillRect(_safeColor, _safeDigitRect[d]);
-	m.surf = loadMask(Common::String::format(_safeNumberPath.c_str(), _safeDigit[d]), _safeDigitRect[d].left, _safeDigitRect[d].top, true);
+	int digitValue = getSafeDigit(d);
+	m.surf = loadMask(Common::String::format(_safeNumberPath.c_str(), digitValue), _safeDigitRect[d].left, _safeDigitRect[d].top, true);
 	m.cursor = g_private->getExitCursor();
 	m.nextSetting = "";
 	m.flag1 = nullptr;
 	m.flag2 = nullptr;
 	_safeDigitArea[d] = m;
-	drawScreen();
 }
-
 
 void PrivateEngine::renderSafeDigit(uint32 d) {
 
@@ -1287,14 +1297,45 @@ void PrivateEngine::renderSafeDigit(uint32 d) {
 		_safeDigitArea[d].clear();
 	}
 	fillRect(_safeColor, _safeDigitRect[d]);
+	int digitValue = getSafeDigit(d);
 	MaskInfo m;
-	m.surf = loadMask(Common::String::format(_safeNumberPath.c_str(), _safeDigit[d]), _safeDigitRect[d].left, _safeDigitRect[d].top, true);
+	m.surf = loadMask(Common::String::format(_safeNumberPath.c_str(), digitValue), _safeDigitRect[d].left, _safeDigitRect[d].top, true);
 	m.cursor = g_private->getExitCursor();
 	m.nextSetting = "";
 	m.flag1 = nullptr;
 	m.flag2 = nullptr;
 	_safeDigitArea[d] = m;
 	drawScreen();
+}
+
+int PrivateEngine::getSafeDigit(uint32 d) {
+	assert(d < 3);
+
+	Private::Symbol *sym = maps.variables.getVal(getWallSafeValueVariable());
+	int value = sym->u.val;
+
+	byte digits[3];
+	digits[0] = value / 100;
+	digits[1] = (value / 10) % 10;
+	digits[2] = value % 10;
+
+	return digits[d];
+}
+
+void PrivateEngine::incrementSafeDigit(uint32 d) {
+	assert(d < 3);
+
+	Private::Symbol *sym = maps.variables.getVal(getWallSafeValueVariable());
+	int value = sym->u.val;
+
+	byte digits[3];
+	digits[0] = value / 100;
+	digits[1] = (value / 10) % 10;
+	digits[2] = value % 10;
+
+	digits[d] = (digits[d] + 1) % 10;
+	
+	sym->u.val = (100 * digits[0]) + (10 * digits[1]) + digits[2];
 }
 
 void PrivateEngine::selectLoadGame(Common::Point mousePos) {
@@ -1353,6 +1394,9 @@ void PrivateEngine::restartGame() {
 
 	// VSPicture
 	_nextVS = "";
+
+	// Wall Safe
+	initializeWallSafeValue();
 }
 
 Common::Error PrivateEngine::loadGameStream(Common::SeekableReadStream *stream) {
