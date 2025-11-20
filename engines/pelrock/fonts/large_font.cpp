371d27a810ba614435a015cea49cc9be1e864da1
kelmer <kelmer@gmail.com>
2025-11-20 22:48:51 +0100

    PELROCK: Adds extra pixel char width on font

--- engines/pelrock/fonts/large_font.cpp
+++ engines/pelrock/fonts/large_font.cpp
@@ -47,7 +47,7 @@ bool LargeFont::load(const Common::String &filename) {
 }
 
 int LargeFont::getCharWidth(uint32 chr) const {
-	return CHAR_WIDTH;
+	return CHAR_WIDTH + 1;
 }
 
 void LargeFont::drawChar(Graphics::Surface *dst, uint32 chr, int x, int y, uint32 color) const {
