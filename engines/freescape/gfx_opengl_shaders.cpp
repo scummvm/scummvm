14b8b690c0dd0079c0f55c3636144ea773793657
neuromancer <gustavo.grieco@gmail.com>
2025-11-22 19:47:03 +0100

    FREESCAPE: extended new rendering to shaders

--- engines/freescape/gfx_opengl_shaders.cpp
+++ engines/freescape/gfx_opengl_shaders.cpp
@@ -118,7 +118,9 @@ void OpenGLShaderRenderer::init() {
 		_defaultShaderStippleArray[i] = _defaultStippleArray[i];
 
 	glDisable(GL_TEXTURE_2D);
-	glEnable(GL_DEPTH_TEST);
+	glDisable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	glFrontFace(GL_CW);
 	glEnable(GL_SCISSOR_TEST);
 	setViewport(_viewport);
 }
@@ -147,6 +149,8 @@ void OpenGLShaderRenderer::drawTexturedRect2D(const Common::Rect &screenRect, co
 	_bitmapShader->use();
 	_bitmapShader->setUniform("flipY", glTexture->_upsideDown);
 
+	glDisable(GL_CULL_FACE);
+	glDisable(GL_DEPTH_TEST);
 	glDepthMask(GL_FALSE);
 
 	glBindTexture(GL_TEXTURE_2D, glTexture->_id);
@@ -155,6 +159,8 @@ void OpenGLShaderRenderer::drawTexturedRect2D(const Common::Rect &screenRect, co
 
 	glDisable(GL_BLEND);
 	glDepthMask(GL_TRUE);
+	glDisable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
 	_bitmapShader->unbind();
 }
 
@@ -178,6 +184,7 @@ void OpenGLShaderRenderer::drawSkybox(Texture *texture, Math::Vector3d camera) {
 	_cubemapShader->setUniform("mvpMatrix", skyboxMVP);
 
 	glDisable(GL_DEPTH_TEST);
+	glDisable(GL_CULL_FACE);
 
 	glBindBuffer(GL_ARRAY_BUFFER, _cubemapTexCoordVBO);
 	if (texture->_width == 1008)
@@ -192,7 +199,8 @@ void OpenGLShaderRenderer::drawSkybox(Texture *texture, Math::Vector3d camera) {
 
 	glDrawElements(GL_TRIANGLES, 24, GL_UNSIGNED_INT, 0);
 
-	glEnable(GL_DEPTH_TEST);
+	glDisable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
 
 	_cubemapShader->unbind();
 }
@@ -276,6 +284,7 @@ void OpenGLShaderRenderer::renderPlayerShootBall(byte color, const Common::Point
 		glBlendFunc(GL_ONE_MINUS_DST_COLOR, GL_ZERO);
 	}
 
+	glDisable(GL_CULL_FACE);
 	glDisable(GL_DEPTH_TEST);
 	glDepthMask(GL_FALSE);
 
@@ -305,7 +314,8 @@ void OpenGLShaderRenderer::renderPlayerShootBall(byte color, const Common::Point
 	glDrawArrays(GL_TRIANGLE_FAN, 0, (triangleAmount + 2));
 
 	glDisable(GL_BLEND);
-	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	glDisable(GL_DEPTH_TEST);
 	glDepthMask(GL_TRUE);
 }
 
@@ -340,6 +350,7 @@ void OpenGLShaderRenderer::renderPlayerShootRay(byte color, const Common::Point 
 		glBlendFunc(GL_ONE_MINUS_DST_COLOR, GL_ZERO);
 	}
 
+	glDisable(GL_CULL_FACE);
 	glDisable(GL_DEPTH_TEST);
 	glDepthMask(GL_FALSE);
 
@@ -367,7 +378,8 @@ void OpenGLShaderRenderer::renderPlayerShootRay(byte color, const Common::Point 
 	glLineWidth(1);
 
 	glDisable(GL_BLEND);
-	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	glDisable(GL_DEPTH_TEST);
 	glDepthMask(GL_TRUE);
 }
 
@@ -449,7 +461,7 @@ void OpenGLShaderRenderer::drawCelestialBody(const Math::Vector3d position, floa
 	}
 
 	// === Restore state ===
-	glEnable(GL_DEPTH_TEST);
+	glDisable(GL_DEPTH_TEST);
 	glDepthMask(GL_TRUE);
 
 	// === Cleanup binding ===
@@ -500,7 +512,7 @@ void OpenGLShaderRenderer::renderCrossair(const Common::Point &crossairPosition)
 
 	glLineWidth(1);
 	glDisable(GL_BLEND);
-	glEnable(GL_DEPTH_TEST);
+	glDisable(GL_DEPTH_TEST);
 	glDepthMask(GL_TRUE);
 }
 
@@ -547,12 +559,14 @@ void OpenGLShaderRenderer::renderFace(const Common::Array<Math::Vector3d> &verti
 }
 
 void OpenGLShaderRenderer::depthTesting(bool enabled) {
+	glDisable(GL_DEPTH_TEST);
+}
+
+void OpenGLShaderRenderer::enableCulling(bool enabled) {
 	if (enabled) {
-		// If we re-enable depth testing, we need to clear the depth buffer
-		glClear(GL_DEPTH_BUFFER_BIT);
-		glEnable(GL_DEPTH_TEST);
+		glEnable(GL_CULL_FACE);
 	} else {
-		glDisable(GL_DEPTH_TEST);
+		glDisable(GL_CULL_FACE);
 	}
 }
 
