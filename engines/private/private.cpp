d791c0e0234c7ba49fe950fede371f0a40307d10
sluicebox <22204938+sluicebox@users.noreply.github.com>
2025-11-22 16:45:59 -0800

    PRIVATE: Fix addMemory crash when helping Mavis

--- engines/private/private.cpp
+++ engines/private/private.cpp
@@ -1108,7 +1108,7 @@ void PrivateEngine::addMemory(const Common::String &path) {
 
 	uint locationIndex = 0;
 	for (auto &it : maps.locationList) {
-		const Private::Symbol *sym = maps.locations.getVal(it);
+		Private::Symbol *sym = maps.locations.getVal(it);
 		locationIndex++;
 
 		Common::String currentLocation = it.substr(9);
@@ -1146,7 +1146,13 @@ void PrivateEngine::addMemory(const Common::String &path) {
 		}
 
 		currentLocation.toLowercase();
-		if (sym->u.val && currentLocation == location) {
+		if (currentLocation == location) {
+			// Ensure that the location is marked as visited.
+			// Police station video spoc00xs can be played before the
+			// police station has been visited if the player has not
+			// been busted by the police yet.
+			setLocationAsVisited(sym);
+
 			diaryPage.locationID = locationIndex;
 			break;
 		}
@@ -2471,6 +2477,15 @@ void PrivateEngine::loadMemories(const Common::Rect &rect, uint rightPageOffset,
 			horizontalOffset = rightPageOffset;
 			currentVerticalOffset = 0;
 		}
+	}
+}
+
+void PrivateEngine::setLocationAsVisited(Symbol *location) {
+	if (location->u.val == 0) {
+		// visited locations have non-zero values.
+		// set to an incrementing value to record the order visited.
+		int maxLocationValue = getMaxLocationValue();
+		setSymbol(location, maxLocationValue + 1);
 	}
 }
 
