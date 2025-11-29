bc184e469fbff56debf72cb660afea6f543ed0d2
Filippos Karapetis <bluegr@gmail.com>
2025-11-29 20:50:41 +0200

    SHERLOCK: Fix obtaining the source pixel value for the 3DO version
    This should fix one of the regressions caused by commit 649170864

--- engines/sherlock/surface.cpp
+++ engines/sherlock/surface.cpp
@@ -86,7 +86,7 @@ void BaseSurface::SHoverrideBlitFrom(const Graphics::Surface &src, const Common:
 
 	for (int y = 0; y < destRect.height(); y++) {
 		for (int x = 0; x < destRect.width(); x++) {
-			const uint8 srcVal = src.getPixel(srcRect.left + x, srcRect.top + y);
+			const uint32 srcVal = src.getPixel(srcRect.left + x, srcRect.top + y);
 			if (srcVal == transColor)
 				continue;
 			setPixel(destRect.left + x, destRect.top + y, overrideColor);
