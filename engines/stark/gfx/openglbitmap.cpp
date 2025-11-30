c20990c7dcadac739917ee612916f44f8ab4d99c
Le Philousophe <lephilousophe@users.noreply.github.com>
2025-11-30 14:16:33 +0100

    STARK: Add support for OpenGL without NPOT
    Only bitmaps need the support as textures (which are mipmapped) are
    already POT sized.

--- engines/stark/gfx/openglbitmap.cpp
+++ engines/stark/gfx/openglbitmap.cpp
@@ -57,19 +57,43 @@ void OpenGlBitmap::bind() const {
 void OpenGlBitmap::update(const Graphics::Surface *surface, const byte *palette) {
 	bind();
 
-	_width = surface->w;
-	_height = surface->h;
-
+	const Graphics::Surface *rgbaSurface = surface;
 	if (surface->format != Driver::getRGBAPixelFormat()) {
 		// Convert the surface to texture format
-		Graphics::Surface *convertedSurface = surface->convertTo(Driver::getRGBAPixelFormat(), palette);
+		rgbaSurface = surface->convertTo(Driver::getRGBAPixelFormat(), palette);
+	}
 
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, convertedSurface->w, convertedSurface->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, convertedSurface->getPixels());
+	_width = rgbaSurface->w;
+	_height = rgbaSurface->h;
 
-		convertedSurface->free();
-		delete convertedSurface;
+	GLfloat s, t;
+
+	if (!OpenGLContext.NPOTSupported) {
+		uint32 texWidth  = Common::nextHigher2(rgbaSurface->w);
+		uint32 texHeight = Common::nextHigher2(rgbaSurface->h);
+		s = (GLfloat)_width / texWidth;
+		t = (GLfloat)_height / texHeight;
+
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texWidth, texHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+		glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, rgbaSurface->w, rgbaSurface->h,
+		                GL_RGBA, GL_UNSIGNED_BYTE, rgbaSurface->getPixels());
 	} else {
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surface->w, surface->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, surface->getPixels());
+		s = t = 1.f;
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, rgbaSurface->w, rgbaSurface->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, rgbaSurface->getPixels());
+	}
+
+	_texCoords[0*2+0] = 0.0f;
+	_texCoords[0*2+1] = 0.0f;
+	_texCoords[1*2+0] = s;
+	_texCoords[1*2+1] = 0.0f;
+	_texCoords[2*2+0] = 0.0f;
+	_texCoords[2*2+1] = t;
+	_texCoords[3*2+0] = s;
+	_texCoords[3*2+1] = t;
+
+	if (rgbaSurface != surface) {
+		const_cast<Graphics::Surface *>(rgbaSurface)->free();
+		delete rgbaSurface;
 	}
 }
 
