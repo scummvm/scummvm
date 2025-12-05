1f866f416f37d5adc6c82a68f6b15948f474e6ad
dhruv <dhruvranger97@gmail.com>
2025-12-05 22:43:52 +0530

    GRAPHICS: MACGUI: scrollbar hides when using mouse wheel to scroll.
    previously the scrollbar used to stay on the screen if we used mouse wheel to scroll, this commit makes sure scroll bar hides it self when we stop scrolling.

--- graphics/macgui/mactextwindow.cpp
+++ graphics/macgui/mactextwindow.cpp
@@ -27,6 +27,8 @@
 #include "graphics/macgui/mactextwindow.h"
 #include "graphics/macgui/macmenu.h"
 
+#define SCROLLBAR_DELAY 300
+
 namespace Graphics {
 
 enum {
@@ -73,6 +75,7 @@ void MacTextWindow::init() {
 	_scrollDirection = kBorderNone;
 	_clickedScrollPart = kBorderNone;
 	_nextScrollTime = 0;
+	_nextWheelEventTime = 0;
 	_scrollDelay = 50;
 
 	// Disable autoselect on activation
@@ -204,8 +207,16 @@ const MacFont *MacTextWindow::getTextWindowFont() {
 
 bool MacTextWindow::draw(bool forceRedraw) {
 
+	uint32 now = g_system->getMillis();
+
+	if (_nextWheelEventTime != 0 && now >= _nextWheelEventTime) {
+		if (_scrollDirection == kBorderNone && _clickedScrollPart == kBorderNone) {
+			setScroll(0, 0);         // hide the scrollbar
+			_nextWheelEventTime = 0; // reset timer
+		}
+	}
+
 	if (_scrollDirection != kBorderNone) {
-		uint32 now = g_system->getMillis();
 		if (now >= _nextScrollTime) {
 			if (_scrollDirection == kBorderScrollUp) {
 				_mactext->scroll(-1);
@@ -373,6 +384,8 @@ bool MacTextWindow::processEvent(Common::Event &event) {
 		//setHighlight(kBorderScrollUp);
 		_mactext->scroll(-2);
 		calcScrollBar();
+
+		_nextWheelEventTime = g_system->getMillis() + SCROLLBAR_DELAY; // hide the bar after 300ms from now
 		return true;
 	}
 
@@ -380,6 +393,8 @@ bool MacTextWindow::processEvent(Common::Event &event) {
 		//setHighlight(kBorderScrollDown);
 		_mactext->scroll(2);
 		calcScrollBar();
+
+		_nextWheelEventTime = g_system->getMillis() + SCROLLBAR_DELAY; // hide the bar after 300ms from now
 		return true;
 	}
 
