3d582fa0629ffd7b678267bc8f4e18703b36cea0
sluicebox <22204938+sluicebox@users.noreply.github.com>
2025-12-10 00:10:33 -0800

    PRIVATE: Resume background music after pausing
    Fixes bug #16351

--- engines/private/savegame.h
+++ engines/private/savegame.h
@@ -36,13 +36,14 @@ namespace Private {
 //
 // Version - new/changed feature
 // =============================
+//       4 - _pausedBackgroundSound (December 2025)
 //       3 - Radio detailed state (December 2025)
 //       2 - Phone clip detailed state (December 2025)
 //       1 - Metadata header and more game state (November 2025)
 //
 // Earlier versions did not have a header and not supported.
 
-const uint16 kCurrentSavegameVersion = 3;
+const uint16 kCurrentSavegameVersion = 4;
 const uint16 kMinimumSavegameVersion = 3;
 
 struct SavegameMetadata {
