2e9d44e9c8b587dfaf83ad937b7df5263d52064b
Eugene Sandulenko <sev@scummvm.org>
2026-01-04 12:17:02 +0100

    WAGE:  original Java implementation author in the credits

--- engines/wage/credits.pl
+++ engines/wage/credits.pl
@@ -1,4 +1,6 @@
 begin_section("WAGE");
 	add_person("Alikhan Balpykov", "Alikhan", "");
 	add_person("Eugene Sandulenko", "sev", "");
+
+	add_person("Alexei Svitkine", "asvitkine", "(Java implementation author)");
 end_section();
