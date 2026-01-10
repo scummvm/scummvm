346ea8c92f04960c75c24f7b4fec4b06c8eb5ef7
kelmer <kelmer@gmail.com>
2026-01-10 17:17:33 +0100

    PELROCK: Swap palette remaps

--- engines/pelrock/graphics.cpp
+++ engines/pelrock/graphics.cpp
@@ -38,7 +38,7 @@ Common::Point GraphicsManager::showOverlay(int height, byte *buf) {
 	for (int x = 0; x < 640; x++) {
 		for (int y = overlayY; y < 400; y++) {
 			int index = y * 640 + x;
-			buf[index] = g_engine->_room->paletteRemaps[1][buf[index]];
+			buf[index] = g_engine->_room->paletteRemaps[0][buf[index]];
 		}
 	}
 	return Common::Point(overlayX, overlayY);
--- engines/pelrock/pelrock.cpp
+++ engines/pelrock/pelrock.cpp
@@ -748,7 +748,7 @@ void PelrockEngine::drawAlfred(byte *buf) {
 	if (shadeCharacter) {
 		for (int i = 0; i < finalWidth * finalHeight; i++) {
 			if (finalBuf[i] != 255) {
-				finalBuf[i] = _room->paletteRemaps[0][finalBuf[i]];
+				finalBuf[i] = _room->paletteRemaps[1][finalBuf[i]];
 			}
 		}
 	}
