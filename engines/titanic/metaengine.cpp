3ff446e4087c7e77072e260252ac7396c5e02ba2
Eugene Sandulenko <sev@scummvm.org>
2026-01-22 23:31:11 +0100

    TITANIC: Normalize keymapper action names

--- engines/titanic/metaengine.cpp
+++ engines/titanic/metaengine.cpp
@@ -221,7 +221,7 @@ Common::KeymapArray TitanicMetaEngine::initKeymaps(const char *target) const {
 	act->addDefaultInputMapping("KP8");
 	act->addDefaultInputMapping("JOY_UP");
 	realLifeKeymap->addAction(act);
-	
+
 	act = new Action("DOWN", _("Scroll down"));
 	act->setCustomEngineActionEvent(kActionDown);
 	act->addDefaultInputMapping("DOWN");
@@ -261,14 +261,14 @@ Common::KeymapArray TitanicMetaEngine::initKeymaps(const char *target) const {
 	act->addDefaultInputMapping("F6");
 	petKeymap->addAction(act);
 
-	act = new Action("SCROLLUP", _("Scroll up page"));
+	act = new Action("SCROLLUP", _("Previous page"));
 	act->setCustomEngineActionEvent(kActionPETScrollPageUp);
 	act->addDefaultInputMapping("PAGEUP");
 	act->addDefaultInputMapping("KP9");
 	act->addDefaultInputMapping("JOY_UP");
 	petKeymap->addAction(act);
 
-	act = new Action("SCROLLDOWN", _("Scroll down page"));
+	act = new Action("SCROLLDOWN", _("Next page"));
 	act->setCustomEngineActionEvent(kActionPETScrollPageDown);
 	act->addDefaultInputMapping("PAGEDOWN");
 	act->addDefaultInputMapping("KP3");
