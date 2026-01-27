159a5eb363cc4090aa8010e16d98ab1e18f48c1a
Scott Percival <code@moral.net.au>
2026-01-28 01:35:04 +0800

    MACGUI: Fix pattern fill for thick pen

--- graphics/macgui/macwindowmanager.cpp
+++ graphics/macgui/macwindowmanager.cpp
@@ -850,7 +850,7 @@ void MacDrawPrimitives<T>::drawPoint(int x, int y, uint32 color, void *data) {
 					uint xu = (uint)x; // for letting compiler optimize it
 					uint yu = (uint)y;
 					*((T *)p->surface->getBasePtr(xu, yu)) = p->invert ? ~(*((T *)p->surface->getBasePtr(xu, yu))) :
-						(pat[(yu + p->fillOriginY) % 8] & (1 << (7 + (xu - p->fillOriginX) % 8))) ? color : p->bgColor;
+						(pat[(yu + p->fillOriginY) % 8] & (1 << (7 - (xu + p->fillOriginX) % 8))) ? color : p->bgColor;
 
 					if (p->mask)
 						*((T *)p->mask->getBasePtr(xu, yu)) = 0xff;
