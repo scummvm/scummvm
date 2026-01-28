8f88bc8a914c6b28afc4ece4f32dba1091aed1d5
Robert Megone <robert.megone@gmail.com>
2026-01-28 17:57:51 +0000

    AGOS: Implement font squeezing routine for DOS Personal Nightmare and the Amiga Elvira 1 demo

--- engines/agos/charset-fontdata.cpp
+++ engines/agos/charset-fontdata.cpp
@@ -3025,7 +3025,91 @@ void AGOSEngine::windowDrawChar(WindowBlock *window, uint x, uint y, byte chr) {
 	_videoLockOut &= ~0x8000;
 }
 
+static inline byte pnPreserveBit7ShiftLeft1(byte x) {
+	byte shifted = (byte)(x << 1);
+	if (x & 0x80)
+		shifted |= 0x80;
+	return shifted;
+}
+
+static inline byte pnSqueezeRow(byte rawRow, bool anyBit1SetAcrossGlyph) {
+	byte x = pnPreserveBit7ShiftLeft1(rawRow);
+
+	byte hi = (byte)(x & 0xF0);
+	byte lo = (byte)(x & 0x0F);
+
+	if (anyBit1SetAcrossGlyph)
+		lo = (byte)(lo << 1);
+
+	byte combined = (byte)(hi | lo);
+	combined = (byte)(combined & 0xFC);
+	return combined;
+}
+
+static inline void pnSqueezeGlyph8Rows(const byte *src8, byte *dst8) {
+	byte orAll = 0;
+	for (int i = 0; i < 8; i++)
+		orAll = (byte)(orAll | src8[i]);
+
+	const bool anyBit1SetAcrossGlyph = (orAll & 0x02) != 0;
+
+	for (int i = 0; i < 8; i++)
+		dst8[i] = pnSqueezeRow(src8[i], anyBit1SetAcrossGlyph);
+}
+
+void AGOSEngine::drawPnSqueezedChar(WindowBlock *window, uint x, uint y, byte chr) {
+	const byte *src;
+	byte color, *dst;
+	uint dstPitch, h, w, i;
+
+	if (chr < 32 || (chr - 32) > 98)
+		return;
+
+	Graphics::Surface *screen = getBackendSurface();
+
+	dst = (byte *)screen->getPixels();
+	dstPitch = screen->pitch;
+	h = 8;
+	w = 6;
+
+	src = english_pnFont + (chr - 32) * 8;
+
+	byte pnTmp[8];
+	pnSqueezeGlyph8Rows(src, pnTmp);
+	src = pnTmp;
+
+	dst += y * dstPitch + x + window->textColumnOffset;
+
+	color = window->textColor;
+
+	do {
+		int8 b = *src++;
+		i = 0;
+		do {
+			if (b < 0) {
+				dst[i] = color;
+			}
+			b <<= 1;
+		} while (++i != w);
+		dst += dstPitch;
+	} while (--h);
+
+	Common::Rect dirtyRect(x + window->textColumnOffset, y,
+	                       x + window->textColumnOffset + 6, y + 8);
+	updateBackendSurface(&dirtyRect);
+}
+
+void AGOSEngine_PN::windowDrawChar(WindowBlock *window, uint x, uint y, byte chr) {
+	_videoLockOut |= 0x8000;
+	drawPnSqueezedChar(window, x, y, chr);
+	_videoLockOut &= ~0x8000;
+}
+
 void AGOSEngine_Elvira1::windowDrawChar(WindowBlock *window, uint x, uint y, byte chr) {
+	if (getPlatform() == Common::kPlatformAmiga && (getFeatures() & GF_DEMO)) {
+		drawPnSqueezedChar(window, x, y, chr);
+		return;
+	}
 	if (_language != Common::JA_JPN || _forceAscii) {
 		AGOSEngine::windowDrawChar(window, x, y, chr);
 		return;
