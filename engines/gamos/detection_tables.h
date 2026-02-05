c11505b884452e78c84047bd4784f378dcc9e41c
Marisa-Chan <thunder_8888@mail.ru>
2026-02-05 12:56:53 +0700

    GAMOS: Add comments with game engine copyright, year and if preserved file date

--- engines/gamos/detection_tables.h
+++ engines/gamos/detection_tables.h
@@ -35,7 +35,8 @@ const PlainGameDescriptor gamosGames[] = {
 
 const GamosGameDescription gameDescriptions[] = {
 	{
-		{
+		{ /* Copyright (C) 1998 by Gamos JSC, Moscow, Russia...  Run-time engine [1.51] by Vadim Sytnikov
+			24.03.1998 */
 			"solgamer",
 			0,
 			AD_ENTRY1s("solgamer.exe", "t:4a06f82f7638e865e27d68d2e071e827", 24658521),
@@ -48,7 +49,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000018
 	},
 	{
-		{
+		{ /* Copyright (C) 1998 by Gamos JSC, Moscow, Russia...  Run-time engine [1.51] by Vadim Sytnikov
+			03.12.1998 */
 			"solgamer",
 			0,
 			AD_ENTRY1s("solgamee.exe", "t:cad7d29da5aa843853fb8e7609a46300", 24624511),
@@ -61,7 +63,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000018
 	},
 	{
-		{
+		{ /* Copyright (C) 1998 by Gamos JSC, Moscow, Russia...  Run-time engine [1.51] by Vadim Sytnikov
+			24.03.1998 */
 			"solgamer",
 			0,
 			AD_ENTRY1s("solgamee.exe", "t:a32bbae8c381d8cdc4cbac9e18acb0a8", 24676118),
@@ -74,7 +77,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000018
 	},
 	{
-		{
+		{ /* Copyright (C) 1997 by Gamos JSC, Moscow, Russia...  Run-time engine [1.47] by Vadim Sytnikov
+			30.08.1997 */
 			"pilots",
 			0,
 			AD_ENTRY1s("pilots.exe", "t:82ae05090898af66447bac4f06e910f3", 48357155),
@@ -87,7 +91,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000016
 	},
 	{
-		{
+		{ /* Copyright (C) 1997 by Gamos JSC, Moscow, Russia...  Run-time engine [1.47] by Vadim Sytnikov
+			04.09.1995 ??? */
 			"pilots",
 			0,
 			AD_ENTRY1s("pilde_r.exe", "t:a1bbaa7e59f69cb2be3223c3336982ac", 10770038),
@@ -100,7 +105,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000016
 	},
 	{
-		{
+		{ /* Copyright (C) 1997 by Gamos JSC, Moscow, Russia...  Run-time engine [1.47] by Vadim Sytnikov
+			04.09.1995 ??? */
 			"pilots",
 			0,
 			AD_ENTRY1s("pildemo.exe", "t:a1bbaa7e59f69cb2be3223c3336982ac", 10770038),
@@ -113,7 +119,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000016
 	},
 	{
-		{
+		{ /* Copyright (C) 1997 by Gamos JSC, Moscow, Russia...  Run-time engine [1.47] by Vadim Sytnikov
+			04.09.1995 ??? */
 			"pilots",
 			0,
 			AD_ENTRY1s("pilotsrd.exe", "t:a1bbaa7e59f69cb2be3223c3336982ac", 10770038),
@@ -126,7 +133,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000016
 	},
 	{
-		{
+		{ /* Copyright (C) 1997 by Gamos JSC, Moscow, Russia...  Run-time engine [1.47] by Vadim Sytnikov
+			20.09.1995 ??? */
 			"pilots",
 			0,
 			AD_ENTRY1s("pilde_e.exe", "t:a784fc52f6923817d8ec8b9101e3f3ea", 10904737),
@@ -139,7 +147,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000016
 	},
 	{
-		{
+		{ /* Copyright (C) 1997 by Gamos JSC, Moscow, Russia...  Run-time engine [1.50] by Vadim Sytnikov
+			17.12.1997 */
 			"pilots",
 			0,
 			AD_ENTRY1s("pilotsed.exe", "t:190cb2dcf936d0c3a891ba333dbc2f33", 10946501),
@@ -152,7 +161,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000018
 	},
 	{
-		{
+		{ /* Copyright (C) 1998 by Gamos JSC, Moscow, Russia...  Run-time engine [1.54] by Vadim Sytnikov
+			14.10.1998 */
 			"pilots2",
 			0,
 			AD_ENTRY1s("pilots2.exe", "t:c434bd8787febde52ccb5ef0e0731c7b", 582283983),
@@ -165,7 +175,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000018
 	},
 	{
-		{
+		{ /* Copyright (C) 1998 by Gamos JSC, Moscow, Russia...  Run-time engine [1.51] by Vadim Sytnikov
+			03.12.1998 */
 			"wild",
 			0,
 			AD_ENTRY1s("wildus.exe", "t:320c8dfd26ae5935b71a37227b7fe67f", 11475754),
@@ -178,7 +189,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000018
 	},
 	{
-		{
+		{ /* Copyright (C) 1997 by Gamos JSC, Moscow, Russia...  Run-time engine [1.43] by Vadim Sytnikov
+			06.08.1997 */
 			"wild",
 			0,
 			AD_ENTRY1s("wildus.exe", "t:a28e6f87a9c5cad5661108678522e27c", 11471282),
@@ -191,7 +203,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000016
 	},
 	{
-		{
+		{ /* Copyright (C) 1997 by Gamos JSC, Moscow, Russia...  Run-time engine [1.43] by Vadim Sytnikov
+			06.08.1997 */
 			"wild",
 			0,
 			AD_ENTRY1s("wildru.exe", "t:35bcdf2b9a0b022fe546de08ab685bd9", 11510210),
@@ -204,7 +217,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000016
 	},
 	{
-		{
+		{ /* Copyright (C) 1997 by Gamos JSC, Moscow, Russia...  Run-time engine [1.42] by Vadim Sytnikov
+			31.07.1997 */
 			"wild",
 			0,
 			AD_ENTRY1s("wildrudm.exe", "t:335fa22e185426843190460bc57d78a7", 11517820),
@@ -217,7 +231,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000016
 	},
 	{
-		{
+		{ /* Copyright (C) 1997 by Gamos JSC, Moscow, Russia...  Run-time engine [1.43] by Vadim Sytnikov
+			06.08.1997 */
 			"wild",
 			0,
 			AD_ENTRY1s("wildusdm.exe", "t:bddc7069986caedcfaa38ce35d42520b", 11516932),
@@ -230,7 +245,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000016
 	},
 	{
-		{
+		{ /* Copyright (C) 1997 by Gamos JSC, Moscow, Russia...  Run-time engine [1.43] by Vadim Sytnikov
+			06.08.1997 */
 			"flop",
 			0,
 			AD_ENTRY1s("ffloprd.exe", "t:a09793d184d211f819299a5286a75f56", 4637680),
@@ -243,7 +259,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000016
 	},
 	{
-		{
+		{ /* Copyright (C) 1997 by Gamos JSC, Moscow, Russia...  Run-time engine [1.43] by Vadim Sytnikov
+			06.08.1997 */
 			"flop",
 			0,
 			AD_ENTRY1s("ffloped.exe", "t:2f90db8d9083b57c775375cf880dc777", 4609380),
@@ -256,7 +273,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000016
 	},
 	{
-		{
+		{ /* Copyright (C) 1998 by Gamos JSC, Moscow, Russia...  Run-time engine [1.51] by Vadim Sytnikov
+			03.12.1998 */
 			"flop",
 			0,
 			AD_ENTRY1s("fflope.exe", "t:7e098e0b565d97eb3d55eb3b6d45e413", 4633340),
@@ -269,7 +287,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000018
 	},
 	{
-		{
+		{ /* Copyright (C) 1998 by Gamos JSC, Moscow, Russia...  Run-time engine [1.51] by Vadim Sytnikov
+			03.12.1998 */
 			"flop",
 			0,
 			AD_ENTRY1s("ffloped.exe", "t:1be4097f6564e9240c58079addfd4600", 4613290),
@@ -282,7 +301,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000018
 	},
 	{
-		{
+		{ /* Copyright (C) 1997 by Gamos JSC, Moscow, Russia...  Run-time engine [1.42] by Vadim Sytnikov
+			31.07.1997 */
 			"flop",
 			0,
 			AD_ENTRY1s("fflope.exe", "t:8ffba2d788278457bb2f9661b7bdf4ec", 4629006),
@@ -295,7 +315,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000016
 	},
 	{
-		{
+		{ /* Copyright (C) 1997 by Gamos JSC, Moscow, Russia...  Run-time engine [1.43] by Vadim Sytnikov
+			06.08.1997 */
 			"flop",
 			0,
 			AD_ENTRY1s("fflopr.exe", "t:7e452d6f5fbfe32dc1ee3c53fba27f88", 4629153),
@@ -308,7 +329,7 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000016
 	},
 	{ // 1.13rus
-		{
+		{ /* Copyright (C) 1996 by Gamos JSC, Moscow, Russia...  Run-time engine [1.13] by Vadim Sytnikov */
 			"flop",
 			0,
 			AD_ENTRY1s("fflop.exe", "t:fd32bc85b8686504f3e63c8e00444c68", 6662497),
@@ -321,7 +342,7 @@ const GamosGameDescription gameDescriptions[] = {
 		0x8000000b
 	},
 	{ // 1.13eng
-		{
+		{ /* Copyright (C) 1996 by Gamos JSC, Moscow, Russia...  Run-time engine [1.13] by Vadim Sytnikov */
 			"flop",
 			0,
 			AD_ENTRY1s("fflop.exe", "t:3f7b90481959b408c9f7dface714cb75", 6691028),
@@ -334,7 +355,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x8000000b
 	},
 	{ // 1.29eng
-		{
+		{ /* Copyright (C) 1996 by Gamos JSC, Moscow, Russia...  Run-time engine [1.29] by Vadim Sytnikov
+			04.01.1997 */
 			"flop",
 			0,
 			AD_ENTRY1s("fflop.exe", "t:a1f917e41e24cbb9e2bda3cf2da5e8fb", 6860990),
@@ -347,7 +369,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000012
 	},
 	{
-		{
+		{ /* Copyright (C) 1997 by Gamos JSC, Moscow, Russia...  Run-time engine [1.43] by Vadim Sytnikov
+			06.08.1997 */
 			"netwalk",
 			0,
 			AD_ENTRY1s("it.exe", "t:76c0cce851177ed28e0d78ab989c6b63", 4125894),
@@ -360,7 +383,7 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000016
 	},
 	{
-		{
+		{ /* Copyright (C) 1996 by Gamos JSC, Moscow, Russia...  Run-time engine [1.13] by Vadim Sytnikov */
 			"netwalk",
 			0,
 			AD_ENTRY1s("it.exe", "t:455ad057b10542674c69f428311b6daf", 4744297),
@@ -373,7 +396,7 @@ const GamosGameDescription gameDescriptions[] = {
 		0x8000000b
 	},
 	{
-		{
+		{ /* Copyright (C) 1996 by Gamos JSC, Moscow, Russia...  Run-time engine [1.13] by Vadim Sytnikov */
 			"netwalk",
 			0,
 			AD_ENTRY1s("netwalk.exe", "t:f7e100541adf71f1cec35ecc914c8ca3", 5607140),
@@ -386,7 +409,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x8000000b
 	},
 	{
-		{
+		{ /* Copyright (C) 1997 by Gamos JSC, Moscow, Russia...  Run-time engine [1.43] by Vadim Sytnikov
+			06.08.1997 */
 			"netwalk",
 			0,
 			AD_ENTRY1s("netwalk.exe", "t:3512b1127b93c2ffe074e292f19a0ffb", 4981212),
@@ -399,7 +423,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000016
 	},
 	{
-		{
+		{ /* Copyright (C) 1997 by Gamos JSC, Moscow, Russia...  Run-time engine [1.43] by Vadim Sytnikov
+			06.08.1997 */
 			"netwalk",
 			0,
 			AD_ENTRY1s("netwalk.exe", "t:f1b1713c122a4d60a773046f6aa1e1d8", 4972704),
@@ -412,7 +437,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000016
 	},
 	{
-		{
+		{ /* Copyright (C) 1996 by Gamos JSC, Moscow, Russia...  Run-time engine [1.27] by Vadim Sytnikov
+			15.12.1996 */
 			"netwalk",
 			0,
 			AD_ENTRY1s("netwalk.exe", "t:ee6ec7c4d1f451d30038665e2edcc278", 5624675),
@@ -425,7 +451,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000012
 	},
 	{
-		{
+		{ /* Copyright (C) 1998 by Gamos JSC, Moscow, Russia...  Run-time engine [1.51] by Vadim Sytnikov
+			03.12.1998 */
 			"netwalk",
 			0,
 			AD_ENTRY1s("netwalk.exe", "t:140a884942bbc396ae0640645d8118aa", 4979981),
@@ -438,7 +465,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000018
 	},
 	{
-		{
+		{ /* Copyright (C) 1998 by Gamos JSC, Moscow, Russia...  Run-time engine [1.51] by Vadim Sytnikov
+			03.12.1998 */
 			"netwalk",
 			0,
 			AD_ENTRY1s("netwalk.exe", "t:4d78e86ef47ceb6c578081ba367ee8ea", 4984836),
@@ -451,7 +479,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000018
 	},
 	{
-		{
+		{ /* Copyright (C) 1997 by Gamos JSC, Moscow, Russia...  Run-time engine [1.47] by Vadim Sytnikov
+			04.09.1995 ??? */
 			"vitamin",
 			0,
 			AD_ENTRY1s("vitam_e.exe", "t:04cae8b641a7d79a1842c7ac1899b534", 429943),
@@ -464,7 +493,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000016
 	},
 	{
-		{
+		{ /* Copyright (C) 1998 by Gamos JSC, Moscow, Russia...  Run-time engine [1.51] by Vadim Sytnikov
+			03.12.1998 */
 			"vitamin",
 			0,
 			AD_ENTRY1s("vitam_e.exe", "t:3506cd8e1a32ec3e841a2043d9dabc5b", 1122512),
@@ -477,7 +507,8 @@ const GamosGameDescription gameDescriptions[] = {
 		0x80000018
 	},
 	{
-		{
+		{ /* Copyright (C) 1997 by Gamos JSC, Moscow, Russia...  Run-time engine [1.47] by Vadim Sytnikov
+			04.09.1995 ??? */
 			"vitamin",
 			0,
 			AD_ENTRY1s("vitam_r.exe", "t:427c0269c7327c7ca4f434e22c5cfce9", 459546),
