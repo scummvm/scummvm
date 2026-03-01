0936ed7a13bedd1772572f9b5cca2990fa9fd5b0
neuromancer <gustavo.grieco@gmail.com>
2026-03-01 14:25:47 +0100

    FREESCAPE: updated credits

--- engines/freescape/credits.pl
+++ engines/freescape/credits.pl
@@ -1,4 +1,5 @@
 begin_section("Freescape");
 	add_person("Chris Allen", "", "Sound engine programming");
+	add_person("Dhruv", "", "Render engine fixes");
 	add_person("Gustavo Grieco", "neuromancer", "");
 end_section();
