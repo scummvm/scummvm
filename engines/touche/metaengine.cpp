57246d7b7025b4fccb07d3333e3eadbcce36366e
Mariusz Górski <takashivip@gmail.com>
2026-03-02 19:41:59 +0100

    TOUCHE: Move hotspot toggle to game-shortcuts keymap
    The "h" keybinding was added to the engine keymap ("touche-default")
    which is never disabled. Moving it to "game-shortcuts" ensures it is
    correctly suppressed during text input (e.g. save state name entry),
    consistent with how Touche already disables that keymap in menu.cpp.

--- engines/touche/metaengine.cpp
+++ engines/touche/metaengine.cpp
@@ -135,7 +135,7 @@ Common::KeymapArray ToucheMetaEngine::initKeymaps(const char *target) const {
 	act = new Action(kStandardActionToggleHotspots, _("Show hotspots"));
 	act->setCustomEngineActionEvent(Common::kEngineActionHotspotToggle);
 	act->addDefaultInputMapping("h");
-	engineKeyMap->addAction(act);
+	gameKeyMap->addAction(act);
 
 	{
 		act = new Action("SKIPORQUIT", _("Skip sequence / open quit dialog"));
