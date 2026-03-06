e8d7414f5c7e93ad40a051d738f18b1efd1ad2d0
Simon Delamarre <simon.delamarre14@gmail.com>
2026-03-06 22:52:18 +0100

    GOB: Fix some edge case high color map computation for Adibou2/Adi4
    When start_color != 0 and useSpecialBlackWhiteValues = true, the special
    transparent value 0 will be copied at wrong indexes.

--- engines/gob/surface.cpp
+++ engines/gob/surface.cpp
@@ -1186,9 +1186,9 @@ void Surface::computeHighColorMap(uint32 *highColorMap, const byte *palette,
 
 		if (!useSpecialBlackWhiteValues)
 			highColorMap[indexDest] = format.RGBToColor(red, green, blue);
-		else if (i == 0) // Trick from the original engine to handle transparency with high color surfaces
+		else if (indexDest == 0) // Trick from the original engine to handle transparency with high color surfaces
 			highColorMap[indexDest] = 0; // Palette index 0 is always mapped to high color value 0, possibly interpreted as the special transparent color.
-		else if (i == 255)
+		else if (indexDest == 255)
 			highColorMap[indexDest] = format.RGBToColor(0xFF, 0xFF, 0xFF); // Palette index 255 is always mapped to white.
 		else if (red == 0 && green == 0 && blue == 0)
 			highColorMap[indexDest] = format.RGBToColor((1 << format.rLoss),
