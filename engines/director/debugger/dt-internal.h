db77068e26c5ead663ab37a0fadd5df2e6945524
ramyak-sharma <ramyaksharma1@gmail.com>
2026-03-07 08:53:08 +0530

    JANITORIAL: Add correct indentation

--- engines/director/debugger/dt-internal.h
+++ engines/director/debugger/dt-internal.h
@@ -118,9 +118,9 @@ enum ThemeID {
 };
 
 enum SearchMode {
-    kSearchAll = 0,
-    kSearchHandlerNames,
-    kSearchScriptBody,
+	kSearchAll = 0,
+	kSearchHandlerNames,
+	kSearchScriptBody,
 };
 
 struct DebuggerTheme {
