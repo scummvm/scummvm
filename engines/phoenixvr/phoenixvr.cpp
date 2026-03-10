b3c7d95765948928f21910feb635fa418f6f4a83
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2026-03-10 01:43:52 +0000

    PHOENIXVR: remove tick log

--- engines/phoenixvr/phoenixvr.cpp
+++ engines/phoenixvr/phoenixvr.cpp
@@ -1064,7 +1064,6 @@ Common::Error PhoenixVREngine::run() {
 			}
 		}
 		float dt = float(frameDuration) / 1000.0f;
-		debug("tick %g", dt);
 		tick(dt);
 
 		// Delay for a bit. All events loops should have a delay
