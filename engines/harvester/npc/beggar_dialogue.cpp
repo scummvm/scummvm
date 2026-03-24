d47caec136b74e642bbea9baa49d82c8a72fa2a0
Alex Bevilacqua <alex@alexbevi.com>
2026-03-23 20:18:08 -0400

    HARVESTER: Recover beggar dialogue flow

--- engines/harvester/npc/beggar_dialogue.cpp
+++ engines/harvester/npc/beggar_dialogue.cpp
@@ -29,17 +29,28 @@ namespace Harvester {
 
 namespace {
 
+static const char *const kBeggarNpc = "BEGGAR";
+static const char *const kPcSpeaker = "PC";
 static const char *const kBeggarInterruptFlag = "BEGGAR_INTERRUPT_CONVERSATION";
 static const char *const kBeggarDialog3ActionTag = "BEGGAR_DIALOG_3";
+static const int kBeggarInitialResponseLine = 0x0;
+static const int kBeggarFollowupResponseLine = 0x1;
 
 } // End of namespace
 
 bool BeggarDialogueHandler::matchesNpc(const Common::String &npcName) const {
-	return npcName.equalsIgnoreCase("BEGGAR");
+	return npcName.equalsIgnoreCase(kBeggarNpc);
 }
 
 Common::Error BeggarDialogueHandler::handleDialogue(DialogueRuntime &runtime,
 		const Common::String &, DialogueSharedState &) {
+	auto playBeggarLine = [&](int wavId, int headVariant = 0) -> Common::Error {
+		return runtime.playDialogueLineWithVariant(wavId, kBeggarNpc, headVariant);
+	};
+	auto playPcLine = [&](int wavId, int headVariant = 0) -> Common::Error {
+		return runtime.playDialogueLineWithVariant(wavId, kPcSpeaker, headVariant);
+	};
+
 	if (!runtime.startupScript().getFlagValue(kBeggarInterruptFlag))
 		return Common::kNoError;
 
@@ -49,7 +60,50 @@ Common::Error BeggarDialogueHandler::handleDialogue(DialogueRuntime &runtime,
 		runtime.queueDialogueInteractionIfNeeded(interaction);
 	}
 
-	return runtime.playDialogueLineWithVariant(0x1276, "BEGGAR", 1);
+	Common::Error lineError = playBeggarLine(0x1276, 1);
+	if (lineError.getCode() != Common::kNoError)
+		return lineError;
+
+	lineError = playPcLine(0x127a, 2);
+	if (lineError.getCode() != Common::kNoError)
+		return lineError;
+
+	lineError = playBeggarLine(0x127e);
+	if (lineError.getCode() != Common::kNoError)
+		return lineError;
+
+	int responseIndex = 0;
+	Common::Error responseError = runtime.runResponseMenu(
+		kBeggarInitialResponseLine, responseIndex);
+	if (responseError.getCode() != Common::kNoError)
+		return responseError;
+
+	if (responseIndex == 1) {
+		lineError = playBeggarLine(0x128d, 1);
+	} else if (responseIndex == 2) {
+		lineError = playBeggarLine(0x1291, 3);
+	} else {
+		return Common::kNoError;
+	}
+	if (lineError.getCode() != Common::kNoError)
+		return lineError;
+
+	lineError = playBeggarLine(0x1295);
+	if (lineError.getCode() != Common::kNoError)
+		return lineError;
+
+	responseIndex = 0;
+	responseError = runtime.runResponseMenu(
+		kBeggarFollowupResponseLine, responseIndex);
+	if (responseError.getCode() != Common::kNoError)
+		return responseError;
+
+	if (responseIndex == 1)
+		return playBeggarLine(0x12ab, 2);
+	if (responseIndex == 2)
+		return playBeggarLine(0x12a5, 2);
+
+	return Common::kNoError;
 }
 
 } // End of namespace Harvester
