53d3068f33a640bb99e669290d52d62f3b699a1e
Alex Bevilacqua <alex@alexbevi.com>
2026-03-23 21:26:20 -0400

    HARVESTER: Reimplement gladiator dialogue

--- engines/harvester/npc/gladiator_dialogue.cpp
+++ engines/harvester/npc/gladiator_dialogue.cpp
@@ -29,21 +29,101 @@ namespace Harvester {
 
 namespace {
 
+static const char *const kGladiatorNpc = "GLADIATOR";
 static const char *const kGladiatorInterruptFlag = "GLADIATOR_INTERRUPT_CONVERSATION";
+static const char *const kStartMercyTimerActionTag = "START_MERCY_TIMR";
+static const char *const kDialogueC110FstPath = "GRAPHIC/FST/C110.FST";
+static const int kGladiatorInitialResponseLine = 0xc3;
+static const int kGladiatorFollowupResponseLine = 0xc4;
 
 } // End of namespace
 
 bool GladiatorDialogueHandler::matchesNpc(const Common::String &npcName) const {
-	return npcName.equalsIgnoreCase("GLADIATOR");
+	return npcName.equalsIgnoreCase(kGladiatorNpc);
 }
 
 Common::Error GladiatorDialogueHandler::handleDialogue(DialogueRuntime &runtime,
 		const Common::String &, DialogueSharedState &) {
+	auto playGladiatorLine = [&](int wavId, int headVariant = 0) -> Common::Error {
+		return runtime.playDialogueLineWithVariant(wavId, kGladiatorNpc, headVariant);
+	};
+	auto queueGladiatorDeathOrMonsterfyTransition = [&]() {
+		StartupInteractionResult interaction;
+		if (runtime.startupScript().triggerRuntimeNpcDeathOrMonsterfy(kGladiatorNpc))
+			interaction.mutatedRuntimeState = true;
+		runtime.queueDialogueInteractionIfNeeded(interaction);
+	};
+	auto runGladiatorDeathBranch = [&](int leadWavId) -> Common::Error {
+		Common::Error lineError = playGladiatorLine(leadWavId, 2);
+		if (lineError.getCode() != Common::kNoError)
+			return lineError;
+
+		lineError = playGladiatorLine(0x6cd, 2);
+		if (lineError.getCode() != Common::kNoError)
+			return lineError;
+
+		queueGladiatorDeathOrMonsterfyTransition();
+		return Common::kNoError;
+	};
+
 	if (!runtime.startupScript().getFlagValue(kGladiatorInterruptFlag))
 		return Common::kNoError;
 
 	(void)runtime.startupScript().setRuntimeFlagValue(kGladiatorInterruptFlag, false);
-	return runtime.playDialogueLineWithVariant(0x69c, "GLADIATOR", 1);
+
+	Common::Error lineError = playGladiatorLine(0x69c, 1);
+	if (lineError.getCode() != Common::kNoError)
+		return lineError;
+
+	lineError = playGladiatorLine(0x6a3, 0);
+	if (lineError.getCode() != Common::kNoError)
+		return lineError;
+
+	int responseIndex = 0;
+	Common::Error responseError = runtime.runResponseMenu(
+		kGladiatorInitialResponseLine, responseIndex);
+	if (responseError.getCode() != Common::kNoError)
+		return responseError;
+
+	if (responseIndex == 1)
+		return runGladiatorDeathBranch(0x6af);
+	if (responseIndex != 2)
+		return Common::kNoError;
+
+	lineError = playGladiatorLine(0x6b3, 0);
+	if (lineError.getCode() != Common::kNoError)
+		return lineError;
+
+	lineError = runtime.playDialogueFst(kDialogueC110FstPath);
+	if (lineError.getCode() != Common::kNoError)
+		return lineError;
+
+	responseIndex = 0;
+	responseError = runtime.runResponseMenu(
+		kGladiatorFollowupResponseLine, responseIndex);
+	if (responseError.getCode() != Common::kNoError)
+		return responseError;
+
+	if (responseIndex == 1)
+		return runGladiatorDeathBranch(0x6c3);
+	if (responseIndex != 2)
+		return Common::kNoError;
+
+	lineError = playGladiatorLine(0x6c7, 1);
+	if (lineError.getCode() != Common::kNoError)
+		return lineError;
+
+	lineError = playGladiatorLine(0x6cd, 0);
+	if (lineError.getCode() != Common::kNoError)
+		return lineError;
+
+	StartupInteractionResult interaction;
+	if (runtime.startupScript().executeActionTag(kStartMercyTimerActionTag, interaction)) {
+		runtime.applyImmediateDialogueInteractionEffects(interaction);
+		runtime.queueDialogueInteractionIfNeeded(interaction);
+	}
+
+	return Common::kNoError;
 }
 
 } // End of namespace Harvester
