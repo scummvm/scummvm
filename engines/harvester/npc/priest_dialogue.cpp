97a271d9ae282662cc2eb68c38d03a21ae33a6df
Alex Bevilacqua <alex@alexbevi.com>
2026-03-24 04:13:15 -0400

    HARVESTER: Reimplement priest dialogue handler

--- engines/harvester/npc/priest_dialogue.cpp
+++ engines/harvester/npc/priest_dialogue.cpp
@@ -29,21 +29,131 @@ namespace Harvester {
 
 namespace {
 
+static const char *const kPriestNpc = "PRIEST";
 static const char *const kPcTalkedToPriestFlag = "PC_TALKED_TO_PRIEST";
+static const char *const kPriestFlag = "PRIEST_FLAG";
+static const char *const kFollowerMonsterfyActionTag = "FOLOWR_MNSTFY_A";
+static const char *const kReligiousExitsActionTag = "RELIG_EXITS_A";
+static const int kPriestInitialResponseLine = 0x27a;
+static const int kPriestSecondResponseLine = 0x27b;
+static const int kPriestThirdResponseLine = 0x27c;
+static const int kPriestFourthResponseLine = 0x27d;
+static const int kPriestFinalResponseLine = 0x27e;
 
 } // End of namespace
 
 bool PriestDialogueHandler::matchesNpc(const Common::String &npcName) const {
-	return npcName.equalsIgnoreCase("PRIEST");
+	return npcName.equalsIgnoreCase(kPriestNpc);
 }
 
 Common::Error PriestDialogueHandler::handleDialogue(DialogueRuntime &runtime,
 		const Common::String &, DialogueSharedState &) {
+	auto playPriestLine = [&](int wavId, int headVariant = 0) -> Common::Error {
+		return runtime.playDialogueLineWithVariant(wavId, kPriestNpc, headVariant);
+	};
+	auto executeDialogueActionTag = [&](const char *tag) {
+		StartupInteractionResult interaction;
+		if (runtime.startupScript().executeActionTag(tag, interaction)) {
+			runtime.applyImmediateDialogueInteractionEffects(interaction);
+			runtime.queueDialogueInteractionIfNeeded(interaction);
+		}
+	};
+	auto runFollowerMonsterfyBranch = [&]() -> Common::Error {
+		Common::Error lineError = playPriestLine(0xcc4, 2);
+		if (lineError.getCode() != Common::kNoError)
+			return lineError;
+
+		executeDialogueActionTag(kFollowerMonsterfyActionTag);
+		return Common::kNoError;
+	};
+
 	if (runtime.startupScript().getFlagValue(kPcTalkedToPriestFlag))
 		return Common::kNoError;
 
 	(void)runtime.startupScript().setRuntimeFlagValue(kPcTalkedToPriestFlag, true);
-	return runtime.playDialogueLine(0xcb2, "PRIEST");
+
+	Common::Error lineError = playPriestLine(0xcb2);
+	if (lineError.getCode() != Common::kNoError)
+		return lineError;
+
+	int responseIndex = 0;
+	Common::Error responseError = runtime.runResponseMenu(kPriestInitialResponseLine, responseIndex);
+	if (responseError.getCode() != Common::kNoError)
+		return responseError;
+
+	if (responseIndex == 2)
+		return runFollowerMonsterfyBranch();
+	if (responseIndex != 1)
+		return Common::kNoError;
+
+	lineError = playPriestLine(0xcbf, 1);
+	if (lineError.getCode() != Common::kNoError)
+		return lineError;
+
+	responseIndex = 0;
+	responseError = runtime.runResponseMenu(kPriestSecondResponseLine, responseIndex);
+	if (responseError.getCode() != Common::kNoError)
+		return responseError;
+
+	if (responseIndex == 2)
+		return runFollowerMonsterfyBranch();
+	if (responseIndex != 1)
+		return Common::kNoError;
+
+	lineError = playPriestLine(0xccf, 1);
+	if (lineError.getCode() != Common::kNoError)
+		return lineError;
+
+	responseIndex = 0;
+	responseError = runtime.runResponseMenu(kPriestThirdResponseLine, responseIndex);
+	if (responseError.getCode() != Common::kNoError)
+		return responseError;
+
+	if (responseIndex == 1)
+		return runFollowerMonsterfyBranch();
+	if (responseIndex != 2)
+		return Common::kNoError;
+
+	lineError = playPriestLine(0xce5, 1);
+	if (lineError.getCode() != Common::kNoError)
+		return lineError;
+
+	responseIndex = 0;
+	responseError = runtime.runResponseMenu(kPriestFourthResponseLine, responseIndex);
+	if (responseError.getCode() != Common::kNoError)
+		return responseError;
+
+	if (responseIndex == 2)
+		return runFollowerMonsterfyBranch();
+	if (responseIndex != 1)
+		return Common::kNoError;
+
+	lineError = playPriestLine(0xcf2, 1);
+	if (lineError.getCode() != Common::kNoError)
+		return lineError;
+
+	responseIndex = 0;
+	responseError = runtime.runResponseMenu(kPriestFinalResponseLine, responseIndex);
+	if (responseError.getCode() != Common::kNoError)
+		return responseError;
+
+	if (responseIndex == 4) {
+		lineError = playPriestLine(0xd0e, 1);
+		if (lineError.getCode() != Common::kNoError)
+			return lineError;
+
+		(void)runtime.startupScript().setRuntimeFlagValue(kPriestFlag, false);
+		executeDialogueActionTag(kReligiousExitsActionTag);
+		return Common::kNoError;
+	}
+
+	lineError = playPriestLine(0xd13, 3);
+	if (lineError.getCode() != Common::kNoError)
+		return lineError;
+
+	(void)runtime.startupScript().setRuntimeFlagValue(kPriestFlag, true);
+	executeDialogueActionTag(kFollowerMonsterfyActionTag);
+	return Common::kNoError;
 }
 
 } // End of namespace Harvester
