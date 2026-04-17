afe82fb96d143c009c7f871b392a3b11b70467db
Matthew Duggan <mgithub@guarana.org>
2026-04-17 16:00:23 +1000

    ACCESS: Add frame to video playback

--- engines/access/asurface.h
+++ engines/access/asurface.h
@@ -102,6 +102,8 @@ public:
 
 	virtual void drawBox();
 
+	virtual void drawBox(int x1, int y1, int x2, int y2, int color);
+
 	virtual void copyBuffer(Graphics::ManagedSurface *src);
 
 	void copyTo(BaseSurface *dest);
