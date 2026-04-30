4bef2e5483d6b289fc4f3732f60d809245e2a73a
neuromancer <gustavo.grieco@gmail.com>
2026-04-30 23:46:27 +0200

    SCUMM: RA2: Add missing header

---
+++ engines/scumm/smush/rebel/codec_ra2.h
@@ -0,0 +1,36 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef SCUMM_SMUSH_REBEL_CODEC_RA2_H
+#define SCUMM_SMUSH_REBEL_CODEC_RA2_H
+
+#include "common/scummsys.h"
+
+namespace Scumm {
+
+void smushDecodeRLEOpaque(byte *dst, const byte *src, int left, int top, int width, int height, int pitch);
+void smushDecodeLineUpdate(byte *dst, const byte *src, int left, int top, int width, int height, int pitch);
+void smushDecodeSkipRLE(byte *dst, const byte *src, int left, int top, int width, int height, int pitch, int dataSize);
+void smushDecodeRA2Bomp(byte *dst, const byte *src, int left, int top, int width, int height, int pitch, int dataSize);
+
+} // End of namespace Scumm
+
+#endif
