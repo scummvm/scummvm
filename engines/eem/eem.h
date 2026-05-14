23baebb9a800604e43f16a02f7fe5bf5935aa3c3
neuromancer <gustavo.grieco@gmail.com>
2026-05-14 12:26:25 +0200

    EEM: removed incorrect attribution in the headers

--- engines/eem/eem.h
+++ engines/eem/eem.h
@@ -17,7 +17,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Based on the original engine code by EA Kids / Storm Software (1994).
  *
  */
 
