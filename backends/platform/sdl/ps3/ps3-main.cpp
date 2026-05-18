b39bd7812317d80928d34dd4cc4c704eb0f2a946
Paweł Kołodziejski <aquadran@gmail.com>
2026-05-18 19:57:00 +0200

    PS3: Use SDK macro for stack size
    This reverts commit 692565fc06b6e19a50bedc5771cd49ca0ac0207e.
    
    Macro does not represent as amount but as bitfield.

--- backends/platform/sdl/ps3/ps3-main.cpp
+++ backends/platform/sdl/ps3/ps3-main.cpp
@@ -30,7 +30,7 @@
 #include <sys/process.h>
 
 // Set program stack size from default 64KB to 256KB
-SYS_PROCESS_PARAM(1000, SYS_PROCESS_SPAWN_STACK_SIZE_256K)
+SYS_PROCESS_PARAM(1000, 0x40000)
 
 int main(int argc, char *argv[]) {
 
