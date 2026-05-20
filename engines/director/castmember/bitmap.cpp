scummvm/scummvm

commit 81eef69c9448ffd9300e99c7b7c8309398f45662

DIRECTOR: Align BitmapCastMember pitch to 2 bytes in D3
Fixes crash decoding 4-bit image in amandastories-mac

engines/director/castmember/bitmap.cpp
@@ -101,11 +101,11 @@ BitmapCastMember::BitmapCastMember(Cast *cast, uint16 castId, Common::SeekableRe
 		}
 
 		_pitch = _initialRect.width();
-		if (_pitch % 16)
-			_pitch += 16 - (_initialRect.width() % 16);
 
 		_pitch *= _bitsPerPixel;
 		_pitch >>= 3;
+		if (_pitch % 2)
+			_pitch += 2 - (_pitch % 2);
 
 	} else if (version >= kFileVer400 && version < kFileVer600) {
 		_flags1 = flags1;

commit d335f3b022e3dcd3067a977b6d417482f47aa8d4

DIRECTOR: Fix regression for 1-bit bitmaps
Introduced in 81eef69c9448ffd9300e99c7b7c8309398f45662. Still required.

The easiest way to see is graphics in The Apartment

engines/director/castmember/bitmap.cpp
@@ -102,6 +102,11 @@ BitmapCastMember::BitmapCastMember(Cast *cast, uint16 castId, Common::SeekableRe
 
 		_pitch = _initialRect.width();
 
+		if (_bitsPerPixel == 1) {
+			if (_pitch % 16)
+				_pitch += 16 - (_initialRect.width() % 16);
+		}
+
 		_pitch *= _bitsPerPixel;
 		_pitch >>= 3;
 		if (_pitch % 2)