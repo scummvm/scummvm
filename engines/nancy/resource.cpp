scummvm/scummvm

commit 7eeff191221dbeac72373892fe221febea824b44

NANCY: Refactor more uses of friend class

engines/nancy/resource.cpp
@@ -293,22 +293,14 @@ void ResourceManager::list(const Common::String &treeName, Common::Array<Common:
 		if (!tree) {
 			return;
 		}
-		for (auto &i : tree->_fileMap) {
-			if (type == CifInfo::kResTypeAny || i._value.type == type) {
-				outList.push_back(i._key);
-			}
-		}
+		outList = tree->getPathsForType(type);
 	} else {
 		for (uint i = 0; i < _cifTreeNames.size(); ++i) {
 			// No provided tree name, check inside every loaded tree
 			Common::String upper = _cifTreeNames[i];
 			upper.toUppercase();
 			const CifTree *tree = (const CifTree *)SearchMan.getArchive(treePrefix + upper);
-			for (auto &it : tree->_fileMap) {
-				if (type == CifInfo::kResTypeAny || it._value.type == type) {
-					outList.push_back(it._key);
-				}
-			}
+			outList = tree->getPathsForType(type);
 		}
 	}
 }
@@ -459,11 +451,11 @@ bool ResourceManager::exportCifTree(const Common::String &treeName, const Common
 		file.addInfo(info);
 	}
 
-	uint16 dataOffset = headerSize + file._writeFileMap.size() * infoSize; // Initial offset after header/infos
-	for (uint i = 0; i < file._writeFileMap.size(); ++i) {
-		file._writeFileMap[i].dataOffset = dataOffset;
+	uint16 dataOffset = headerSize + file.writeFileMapSize() * infoSize; // Initial offset after header/infos
+	for (uint i = 0; i < file.writeFileMapSize(); ++i) {
+		file.setDataOffset(i, dataOffset);
 		for (uint j = 0; j < i; ++j) {
-			file._writeFileMap[i].dataOffset += resStreams[j]->size(); // Final offset, following raw data of previous files
+			file.setDataOffset(i, file.getDataOffset(i) + resStreams[j]->size()); // Final offset, following raw data of previous files
 		}
 	}
 

engines/nancy/action/conversation.cpp
@@ -723,23 +723,10 @@ public:
 	ConversationCelLoader(ConversationCel &owner) : _owner(owner) {}
 
 private:
-	bool loadInner() override;
+	bool loadInner() override { return _owner.load(); }
 
 	ConversationCel &_owner;
 };
-
-bool ConversationCelLoader::loadInner() {
-	for (uint i = _owner._curFrame; i < _owner._celNames[0].size(); ++i) {
-		for (uint j = 0; j < _owner._celRObjects.size(); ++j) {
-			if (!_owner._celCache.contains(_owner._celNames[j][i])) {
-				_owner.loadCel(_owner._celNames[j][i], _owner._treeNames[j]);
-				return false;
-			}
-		}
-	}
-
-	return true;
-}
 
 ConversationCel::~ConversationCel() {
 	// Make sure there isn't a single-frame gap between conversation scenes where
@@ -873,6 +860,19 @@ ConversationCel::Cel &ConversationCel::loadCel(const Common::Path &name, const C
 	Cel &newCel = _celCache.getOrCreateVal(name);
 	g_nancy->_resource->loadImage(name, newCel.surf, treeName, &newCel.src, &newCel.dest);
 	return _celCache[name];
+}
+
+bool ConversationCel::load() {
+	for (uint i = _curFrame; i < _celNames[0].size(); ++i) {
+		for (uint j = 0; j < _celRObjects.size(); ++j) {
+			if (!_celCache.contains(_celNames[j][i])) {
+				loadCel(_celNames[j][i], _treeNames[j]);
+				return false;
+			}
+		}
+	}
+
+	return true;
 }
 
 void ConversationSoundTerse::readData(Common::SeekableReadStream &stream) {

engines/nancy/action/conversation.h
@@ -161,7 +161,6 @@ class ConversationCelLoader;
 // Conversation with separate cels for the body and head of the character.
 // Cels are separate images bundled inside a .cal file
 class ConversationCel : public ConversationSound {
-	friend class ConversationCelLoader;
 public:
 	ConversationCel() {}
 	virtual ~ConversationCel();
@@ -171,6 +170,9 @@ public:
 	void updateGraphics() override;
 
 	void readData(Common::SeekableReadStream &stream) override;
+
+	bool load();
+	uint getCurFrame() const { return _curFrame; }
 
 protected:
 	Common::String getRecordTypeName() const override { return "ConversationCel"; }

engines/nancy/cif.cpp
@@ -343,6 +343,18 @@ bool CifTree::sync(Common::Serializer &ser) {
 	return true;
 }
 
+Common::Array<Common::Path> CifTree::getPathsForType(CifInfo::ResType type) const {
+	Common::Array<Common::Path> pathList;
+
+	for (auto &it : _fileMap) {
+		if (type == CifInfo::kResTypeAny || it._value.type == type) {
+			pathList.push_back(it._key);
+		}
+	}
+
+	return pathList;
+}
+
 bool PatchTree::hasFile(const Common::Path &path) const {
 	if (CifTree::hasFile(path)) {
 		// An association is just a Pair of two StringArrays

engines/nancy/cif.h
@@ -78,7 +78,6 @@ private:
 
 // Container type comprising of multiple CIF files. Contrary to its name it contains no tree structure.
 class CifTree : public Common::Archive {
-	friend class ResourceManager;
 public:
 	CifTree() : _stream(nullptr) {}
 	CifTree(Common::SeekableReadStream *stream, const Common::Path &name);
@@ -101,6 +100,11 @@ public:
 	bool sync(Common::Serializer &ser);
 
 	void addInfo(const CifInfo &info) { _writeFileMap.push_back(info); }
+	uint writeFileMapSize() const { return _writeFileMap.size(); }
+	uint32 getDataOffset(uint i) const { return _writeFileMap[i].dataOffset; }
+	void setDataOffset(uint i, uint32 offset) { _writeFileMap[i].dataOffset = offset; }
+
+	Common::Array<Common::Path> getPathsForType(CifInfo::ResType type = CifInfo::kResTypeAny) const;
 
 private:
 	Common::Path _name;

engines/nancy/video.cpp
@@ -42,9 +42,9 @@ private:
 };
 
 bool VideoCacheLoader::loadInner() {
-	AVFDecoder::CacheHint hint = _owner._cacheHint;
-	int frameID = _owner._curFrame;
-	int frameCount = _owner._frameCount;
+	AVFDecoder::CacheHint hint = _owner.getCacheHint();
+	int frameID = _owner.getCurFrame();
+	int frameCount = _owner.getFrameCount();
 
 	for (int i = 0; i < frameCount; ++i) {
 		if (frameID < 0) {
@@ -55,16 +55,16 @@ bool VideoCacheLoader::loadInner() {
 			frameID -= frameCount;
 		}
 
-		if (!_owner._frameCache[frameID].getPixels()) {
+		if (!_owner.getSurfaceForFrame(frameID)->getPixels()) {
 			_owner.decodeFrame(frameID);
 			return false;
 		}
 
 		// Select next frame based on hint and play direction
 		if (hint != AVFDecoder::kLoadBidirectional) {
-			frameID = _owner._reversed ? frameID - 1 : frameID + 1;
+			frameID = _owner.isReversed() ? frameID - 1 : frameID + 1;
 		} else {
-			frameID = _owner._curFrame + (i % 2 ? i >> 1 : -(i >> 1));
+			frameID = _owner.getCurFrame() + (i % 2 ? i >> 1 : -(i >> 1));
 		}
 	}
 

engines/nancy/video.h
@@ -54,7 +54,6 @@ public:
 private:
 	CacheHint _cacheHint;
 	class AVFVideoTrack : public FixedRateVideoTrack {
-	friend class VideoCacheLoader;
 	public:
 		AVFVideoTrack(Common::SeekableReadStream *stream, uint32 chunkFileFormat, CacheHint cacheHint);
 		virtual ~AVFVideoTrack();
@@ -71,7 +70,9 @@ private:
 		bool endOfTrack() const override;
 		const Graphics::Surface *decodeNextFrame() override;
 		const Graphics::Surface *decodeFrame(uint frameNr);
+		const Graphics::Surface *getSurfaceForFrame(uint frameNr) { return &_frameCache[frameNr]; }
 		void addFrameTime(const uint16 timeToAdd) { _frameTime += timeToAdd; }
+		CacheHint getCacheHint() const { return _cacheHint; }
 
 	protected:
 		Common::Rational getFrameRate() const override { return Common::Rational(1000, _frameTime); }

commit 95e82d6bbb7c60dcc12f85822e4643a260f96d9d

NANCY: Fix regression in debug command scan_ar_type
Fix loading consecutive lists from ResourceManager::list().
A regression from 7eeff19.

engines/nancy/resource.cpp
@@ -301,14 +301,16 @@ void ResourceManager::list(const Common::String &treeName, Common::Array<Common:
 		if (!tree) {
 			return;
 		}
-		outList = tree->getPathsForType(type);
+		Common::Array<Common::Path> result = tree->getPathsForType(type);
+		outList.insert_at(outList.size(), result);
 	} else {
 		for (uint i = 0; i < _cifTreeNames.size(); ++i) {
 			// No provided tree name, check inside every loaded tree
 			Common::String upper = _cifTreeNames[i];
 			upper.toUppercase();
 			const CifTree *tree = (const CifTree *)SearchMan.getArchive(treePrefix + upper);
-			outList = tree->getPathsForType(type);
+			Common::Array<Common::Path> result = tree->getPathsForType(type);
+			outList.insert_at(outList.size(), result);
 		}
 	}
 }