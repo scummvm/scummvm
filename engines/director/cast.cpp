scummvm/scummvm

commit 46ae24cdbbed5faf7ab5ba25ce30525160253530

DIRECTOR: Use castLibMapping start and end records
Without this, multiple internal castLibs will have incorrect start and
end records. This is especially a problem when the start index is
greater than zero, as the member numbers will be wrong.

Fixes showing emails on Mars in Mission to Planet X.

engines/director/cast.cpp
@@ -386,8 +386,20 @@ bool Cast::loadConfig() {
 	else
 		_movieRect = g_director->_fixStageRect;
 
-	_castArrayStart = stream->readUint16();
-	_castArrayEnd = stream->readUint16();
+	if (_isExternal || (g_director->getVersion() < 500)) {
+		// For D4 and below, and for external castlibs,
+		// the config chunk is the source of truth about
+		// where the members start and end.
+		_castArrayStart = stream->readUint16();
+		_castArrayEnd = stream->readUint16();
+	} else {
+		// castArrayStart and castArrayEnd are defined
+		// in the MCsL chunk read by Movie::loadCastLibMapping,
+		// the one in the config chunk is likely to be incorrect
+		// (e.g. multiple internal casts)
+		stream->readUint16();
+		stream->readUint16();
+	}
 
 	// D3 and below use this, override for D4 and over
 	// actual framerates are, on average: { 3.75, 4, 4.35, 4.65, 5, 5.5, 6, 6.6, 7.5, 8.5, 10, 12, 20, 30, 60 }

engines/director/movie.cpp
@@ -163,12 +163,12 @@ void Movie::loadCastLibMapping(Common::SeekableReadStreamEndian &stream) {
 		stream.readByte(); // null
 		if (pathSize > 1)
 			stream.readUint16();
-		stream.readUint16();
-		uint16 itemCount = stream.readUint16();
+		uint16 minMember = stream.readUint16();
+		uint16 maxMember = stream.readUint16();
 		stream.readUint16();
 		uint16 libResourceId = stream.readUint16();
 		uint16 libId = i + 1;
-		debugC(5, kDebugLoading, "Movie::loadCastLibMapping: name: %s, path: %s, itemCount: %d, libResourceId: %d, libId: %d", utf8ToPrintable(name).c_str(), utf8ToPrintable(path).c_str(), itemCount, libResourceId, libId);
+		debugC(5, kDebugLoading, "Movie::loadCastLibMapping: name: %s, path: %s, minMember: %d, maxMember: %d, libResourceId: %d, libId: %d", utf8ToPrintable(name).c_str(), utf8ToPrintable(path).c_str(), minMember, maxMember, libResourceId, libId);
 		Archive *castArchive = _movieArchive;
 		bool isExternal = !path.empty();
 		if (isExternal) {
@@ -189,6 +189,12 @@ void Movie::loadCastLibMapping(Common::SeekableReadStreamEndian &stream) {
 		}
 		_castNames[name] = libId;
 		cast->setArchive(castArchive);
+		// For multiple internal casts, inject the cast array start and end indexes,
+		// as the single pair in the config chunk won't be correct.
+		if (!isExternal) {
+			cast->_castArrayStart = minMember;
+			cast->_castArrayEnd = maxMember;
+		}
 	}
 	return;
 }

commit 042d55526379d5c499803e8f608591f4a952d6cf

DIRECTOR: Fix regressoin in VWCF checksum calculation
It was introduced in 46ae24cdbbed5faf7ab5ba25ce30525160253530,
where we skip reading _castArrayStart and _castArrayEnd,
which are later used for the checkusm calculations.

This is patch of a problem, and we could need to see how to compute
it in case we modify cast before saving

engines/director/cast.cpp
@@ -78,6 +78,7 @@ Cast::Cast(Movie *movie, uint16 castLibID, bool isShared, bool isExternal, uint1
 	_lingoArchive = new LingoArchive(this);
 
 	_castArrayStart = _castArrayEnd = 0;
+	_castArrayStartForChecksum = _castArrayEndForChecksum = 0;
 
 	_castIDoffset = 0;
 
@@ -403,15 +404,15 @@ bool Cast::loadConfig() {
 		// For D4 and below, and for external castlibs,
 		// the config chunk is the source of truth about
 		// where the members start and end.
-		_castArrayStart = stream->readUint16();
-		_castArrayEnd = stream->readUint16();
+		_castArrayStart = _castArrayStartForChecksum = stream->readUint16();
+		_castArrayEnd = _castArrayEndForChecksum = stream->readUint16();
 	} else {
 		// castArrayStart and castArrayEnd are defined
 		// in the MCsL chunk read by Movie::loadCastLibMapping,
 		// the one in the config chunk is likely to be incorrect
 		// (e.g. multiple internal casts)
-		stream->readUint16();
-		stream->readUint16();
+		_castArrayStartForChecksum = stream->readUint16();
+		_castArrayEndForChecksum = stream->readUint16();
 	}
 
 	// D3 and below use this, override for D4 and over
@@ -625,9 +626,9 @@ void Cast::saveConfig(Common::SeekableWriteStream *writeStream, uint32 offset) {
 
 	Movie::writeRect(writeStream, _checkRect);      // 4, 6, 8, 10
 
-	writeStream->writeUint16BE(_castArrayStart);    // 12
+	writeStream->writeUint16BE(_castArrayStartForChecksum);    // 12
 	// This will change
-	writeStream->writeUint16BE(_castArrayStart + _castArchive->getResourceIDList(MKTAG('C', 'A', 'S', 't')).size());      // 14
+	writeStream->writeUint16BE(_castArrayStartForChecksum + _castArchive->getResourceIDList(MKTAG('C', 'A', 'S', 't')).size());      // 14
 
 	writeStream->writeByte(_readRate);              // 16
 	writeStream->writeByte(_lightswitch);           // 17
@@ -1171,8 +1172,8 @@ uint32 Cast::computeChecksum() {
 	check *= _checkRect.left + 4;
 	check /= _checkRect.bottom + 5;
 	check *= _checkRect.right + 6;
-	check -= _castArrayStart + 7;
-	check *= _castArrayEnd + 8;
+	check -= _castArrayStartForChecksum + 7;
+	check *= _castArrayEndForChecksum + 8;
 	check -= (int8)_readRate + 9;
 	check -= _lightswitch + 10;
 

engines/director/cast.h
@@ -186,6 +186,9 @@ public:
 	LingoDec::ScriptContext *_lingodec = nullptr;
 	LingoDec::ChunkResolver *_chunkResolver = nullptr;
 
+	uint16 _castArrayStartForChecksum;
+	uint16 _castArrayEndForChecksum;
+
 	/* Config Data to be saved */
 	/*  0 */ uint16 _len;
 	/*  2 */ uint16 _fileVersion;