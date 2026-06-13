scummvm/scummvm

commit 24dda66e7783fad9d73b5f71dbfe73cea2e0b05b

NANCY: Disable global font kerning, as the engine has its own kerning

engines/nancy/font.cpp
@@ -51,7 +51,7 @@ void Font::read(Common::SeekableReadStream &stream) {
 	_color1CoordsOffset.y = stream.readUint32LE();
 
 	_spaceWidth = stream.readUint16LE();
-	_charSpace = stream.readSint16LE() - 1; // Account for the added pixel in readRect
+	_charSpace = stream.readSint16LE();
 
 	_uppercaseOffset					= stream.readUint16LE();
 	_lowercaseOffset					= stream.readUint16LE();

engines/nancy/font.h
@@ -46,7 +46,7 @@ public:
 	int getFontHeight() const override { return _fontHeight - 1; }
 	int getMaxCharWidth() const override { return _maxCharWidth; }
 	int getCharWidth(uint32 chr) const override;
-	int getKerningOffset(uint32 left, uint32 right) const override { return 1; }
+	int getKerningOffset(uint32 left, uint32 right) const override { return 0; }
 
 	void drawChar(Graphics::Surface *dst, uint32 chr, int x, int y, uint32 color) const override;
 

commit 1c5a9ed526efec93b1a9d837faa2cfecdd957652

NANCY: Fix regression with font height calculation in Nancy6+
A regression from 24dda66e7783fad9d73b5f71dbfe73cea2e0b05b.

Fix #16855

engines/nancy/font.cpp
@@ -147,7 +147,7 @@ void Font::read(Common::SeekableReadStream &stream) {
 	}
 
 	if (g_nancy->getGameType() >= kGameTypeNancy6) {
-		_fontHeight = getCharWidth('o') * 2 - 1;
+		_fontHeight = (getCharWidth('o') - 1) * 2;
 	}
 
 	_textboxData = GetEngineData(TBOX);