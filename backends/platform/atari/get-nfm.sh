a0a300c1bc48247dee298c86e8b7aaa63b7d8716
Paweł Góralski <20125592+n0kturnal@users.noreply.github.com>
2026-06-19 23:01:17 +0200

    AUDIO : Atari TOS hardware / software OPL2 / 3 (YM262 / YM3812)
    Helper script for downloading prebuild nFM library to Atari backend folder

---
+++ backends/platform/atari/get-nfm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -eux
+# -e: Exit immediately if a command exits with a non-zero status.
+# -u: Treat unset variables as an error when substituting.
+# -x: Display expanded script commands
+
+# downloads and unpacks in ./deps folder precompiled nFM library release for m68k Atari TOS / mintelf gcc stdlib
+NFM_VERSION=0.2.1
+
+curl -o ./backends/platform/atari/nfm.zip https://nokturnal.pl/downloads/atari/nokturnfm/nfmplay/builds/releases/0.2.1/ataritos-mintelf-gcc-stdlib-m68k-0.2.1-5c688e1a.zip
+rm -rf ./backends/platform/atari/deps/nfm
+
+unzip ./backends/platform/atari/nfm.zip "nFM_${NFM_VERSION}/*" -d ./backends/platform/atari/deps/
+mv ./backends/platform/atari/deps/nFM_${NFM_VERSION} ./backends/platform/atari/deps/nfm
+rm ./backends/platform/atari/nfm.zip
