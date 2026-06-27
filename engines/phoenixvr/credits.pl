b60f4cd19cb6d52f66ac7082fed3da5a863094b8
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2026-06-27 15:42:56 +0100

    PHOENIXVR: update credits

--- engines/phoenixvr/credits.pl
+++ engines/phoenixvr/credits.pl
@@ -1,4 +1,5 @@
 begin_section("PhoenixVR");
 	add_person("Vladimir Menshakov", "whoozle", "");
 	add_person("Hermann Noll", "Helco", "Help with 3D VR projections");
+	add_person("", "Scorp", "Dracula 1/2 and Messenger support, Engine improvements");
 end_section();
