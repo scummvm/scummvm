5876cedb9b0cfd4cef9d8ba61ce50d27f350f8c4
neuromancer <gustavo.grieco@gmail.com>
2026-07-01 22:10:44 +0200

    SCUMM: RA2: L2 and L11 starts with the player fully in cover

--- engines/scumm/insane/rebel2/runlevels.cpp
+++ engines/scumm/insane/rebel2/runlevels.cpp
@@ -268,10 +268,11 @@ void InsaneRebel2::resetLevelPhaseState(bool clearEnemies) {
 	if (clearEnemies)
 		_enemies.clear();
 
-	if (_selectedChapter == 1 || _selectedChapter == 10) {
-		_rebelAutopilot = 1;
-		_rebelDamageLevel = 5;
-	}
+	// Every phase starts with the player fully in cover. This only has an
+	// effect in the on-foot cover sections (handler 25, levels 2 and 11);
+	// the other levels ignore these values.
+	_rebelAutopilot = 1;
+	_rebelDamageLevel = 5;
 }
 
 void InsaneRebel2::clearEmbeddedHudFrames() {
