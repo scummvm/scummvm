7b33926daa9eddfdf0f2894c4b302ae5f0451eab
Priyanshu <10b.priyanshu@gmail.com>
2026-07-10 13:25:15 +0530

    SKY: Add options icon in iBASS

--- engines/sky/screen.cpp
+++ engines/sky/screen.cpp
@@ -290,6 +290,7 @@ void Screen::renderFinalFrame() {
 		update32BitScreen(_currentScreen, 0, 0, GAME_SCREEN_WIDTH, GAME_SCREEN_HEIGHT);
 		_paletteDirty = false;
 		setIcon(UI_ICON_INV, 0, GAME_SCREEN_HEIGHT - 35);
+		setIcon(UI_ICON_OPTIONS, 4, 4);
 		drawIbassIcon();
 		drawIbassInventory();
 		if (_screen32.getPixels()) {
