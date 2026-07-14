0cf82e357d6d9b51ce513daa40597c3b214b5819
Eugene Sandulenko <sev@scummvm.org>
2026-07-14 13:12:44 +0200

    PHOENIXVR: Added French retail release to detection

--- engines/phoenixvr/detection_tables.h
+++ engines/phoenixvr/detection_tables.h
@@ -249,6 +249,17 @@ const ADGameDescription gameDescriptions[] = {
 	},
 
 	{"dracula1",
+		"Retail version",
+		AD_ENTRY3s("Install/script.pak", "6998262fcce9cb5d3cc8a555f8ee024a", 265,
+				   "Install/InsertCD.pak", "f2a35e531a4ef5895e972e3c98330752", 499,
+				   "Interface.vr", "5d7d770a74d6035957c7deffc4865e12", 106762),
+		Common::FR_FRA,
+		Common::kPlatformWindows,
+		ADGF_DROPPLATFORM | ADGF_UNSTABLE,
+		GUIO1(GUIO_NONE)
+	},
+
+	{"dracula1",
 		nullptr,
 		AD_ENTRY2s("script.pak", "6998262fcce9cb5d3cc8a555f8ee024a", 265,
 				   "Interface.vr", "47099471f31f794be8a7e5a3c382bf62", 104304),
