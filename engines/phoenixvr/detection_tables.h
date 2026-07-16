07a1688b9ed89251e8fbd249f7f814b3727b00b9
Eugene Sandulenko <sev@scummvm.org>
2026-07-16 12:06:14 +0200

    PHOENXIVR: Added Polish drascula2 release detection

--- engines/phoenixvr/detection_tables.h
+++ engines/phoenixvr/detection_tables.h
@@ -413,6 +413,18 @@ const ADGameDescription gameDescriptions[] = {
 		"Retail version",
 		AD_ENTRY4s("Install/script.pak", "ff52d2000eddc5c438f3c8ef50fea858", 277,
 				   "Install/Compiler.dat", "ff30c7ea065af1e86182b428e1f2cc7b", 42650,
+				   "Install/InsertCD.vr", "be865f78d00c699e55e6e3bd2e83df26", 52983,
+				   "First.exe", "4b106f5fffb4b3c033edcdaa0893d616", 16384),
+		Common::PL_POL,
+		Common::kPlatformWindows,
+		ADGF_DROPPLATFORM | ADGF_TESTING,
+		GUIO1(GUIO_NONE)
+	},
+
+	{"dracula2",
+		"Retail version",
+		AD_ENTRY4s("Install/script.pak", "ff52d2000eddc5c438f3c8ef50fea858", 277,
+				   "Install/Compiler.dat", "ff30c7ea065af1e86182b428e1f2cc7b", 42650,
 				   "Install/InsertCD.vr", "7f54847d09c772d41c8726d5c5529a9a", 54005,
 				   "First.exe", "403417a28a11a08deb8443dc08a2213c", 31770),
 		Common::PT_PRT,
