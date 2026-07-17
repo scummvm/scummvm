eebfe2824ef84d628aa41b6d4f14261e9364c856
Donovan Watteau <contrib@dwatteau.fr>
2026-07-17 08:56:18 +0200

    SKY: Fix some -Wunused-but-set-variable warnings
    `scrPtr` can also be dropped, while there.
    
    Unused since commit b97b4dc390dd83fb3af3d66fe0bec554484a7c9a.

--- engines/sky/screen.cpp
+++ engines/sky/screen.cpp
@@ -723,29 +723,22 @@ void Screen::processSequence() {
 			}
 		} while (nrToDo == 0xFF);
 	} while (screenPos < (GAME_SCREEN_WIDTH * GAME_SCREEN_HEIGHT));
-	uint8 *gridPtr = _seqGrid; uint8 *scrPtr = _currentScreen; uint8 *rectPtr = NULL;
-	uint8 rectWid = 0, rectX = 0, rectY = 0;
+	uint8 *gridPtr = _seqGrid;
+	uint8 rectWid = 0;
 	for (uint8 cnty = 0; cnty < 12; cnty++) {
 		for (uint8 cntx = 0; cntx < 20; cntx++) {
 			if (*gridPtr) {
-				if (!rectWid) {
-					rectX = cntx;
-					rectY = cnty;
-					rectPtr = scrPtr;
-				}
 				rectWid++;
 			} else if (rectWid) {
 				renderFinalFrame();
 				rectWid = 0;
 			}
-			scrPtr += 16;
 			gridPtr++;
 		}
 		if (rectWid) {
 			renderFinalFrame();
 			rectWid = 0;
 		}
-		scrPtr += 15 * GAME_SCREEN_WIDTH;
 	}
 	_system->updateScreen();
 	_seqInfo.framesLeft--;
