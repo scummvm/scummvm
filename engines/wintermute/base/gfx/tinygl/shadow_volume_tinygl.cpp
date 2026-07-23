23debfd16c13d040c86f94ad48496921420738fa
Paweł Kołodziejski <aquadran@gmail.com>
2026-07-23 22:34:24 +0200

    WINTERMUTE: Sync TinyGL renderer for shadow volume

--- engines/wintermute/base/gfx/tinygl/shadow_volume_tinygl.cpp
+++ engines/wintermute/base/gfx/tinygl/shadow_volume_tinygl.cpp
@@ -49,6 +49,7 @@ bool ShadowVolumeTinyGL::render() {
 	tglDisable(TGL_TEXTURE_2D);
 	_game->_renderer3D->_lastTexture = nullptr;
 
+	tglColor4f(1.0f, 1.0f, 1.0f, 1.0f);
 	tglEnableClientState(TGL_VERTEX_ARRAY);
 	tglVertexPointer(3, TGL_FLOAT, 0, _vertices.getData());
 	tglDrawArrays(TGL_TRIANGLES, 0, _vertices.getSize());
