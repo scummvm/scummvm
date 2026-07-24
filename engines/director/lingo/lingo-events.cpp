03e99900a8ff2d088f781d3ef9b8a0a8e083b1df
Scott Percival <code@moral.net.au>
2026-07-25 01:11:59 +0800

    DIRECTOR: LINGO: Make menu callbacks run as the stage
    Fixes dropdown menu in Noir: A Shadowy Thriller.

--- engines/director/lingo/lingo-events.cpp
+++ engines/director/lingo/lingo-events.cpp
@@ -562,6 +562,7 @@ void Movie::queueEvent(Common::Queue<LingoEvent> &queue, LEvent event, int targe
 		case kEventKeyUp:
 		case kEventKeyDown:
 		case kEventTimeout:
+		case kEventMenuCallback:
 			break;
 
 		default:
