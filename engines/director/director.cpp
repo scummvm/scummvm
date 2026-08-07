4cee7bb03ffba9f22da836bc80cca4680d34bb8c
Scott Percival <code@moral.net.au>
2026-08-07 22:09:26 +0800

    DIRECTOR: Fix Window use-after-free bug

--- engines/director/director.cpp
+++ engines/director/director.cpp
@@ -237,10 +237,10 @@ bool DirectorEngine::isWindowRegistered(Window *window) const {
 void DirectorEngine::setCurrentWindow(Window *window) {
 	if (_currentWindow == window)
 		return;
+	window->incRefCount();
 	if (_currentWindow)
 		_currentWindow->decRefCount();
 	_currentWindow = window;
-	_currentWindow->incRefCount();
 }
 
 void DirectorEngine::setVersion(uint16 version) {
