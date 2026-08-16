2c6a22a7e5934610e279dd16e400ab54476a6059
Mohit Bankar <mohitbankar1212@gmail.com>
2026-08-16 18:32:21 +0530

    AVALANCHE: Create TitleScreen class

---
+++ engines/avalanche/titlescreen.cpp
@@ -0,0 +1,37 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "avalanche/avalanche.h"
+#include "avalanche/titlescreen.h"
+
+namespace Avalanche {
+
+TitleScreen::TitleScreen(AvalancheEngine *vm) : _vm(vm) {
+}
+
+TitleScreen::~TitleScreen() {
+}
+
+void TitleScreen::run() {
+	(void)_vm;
+}
+
+} // End of namespace Avalanche
