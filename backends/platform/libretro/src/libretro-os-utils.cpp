7fc17c3631d4118a74330d96e9a2647cd03b2b9d
Giovanni Cascione <ing.cascione@gmail.com>
2026-08-21 18:13:18 +0200

    LIBRETRO: Remove dead browser_lastpath branch in checkPathSetting

--- backends/platform/libretro/src/libretro-os-utils.cpp
+++ backends/platform/libretro/src/libretro-os-utils.cpp
@@ -170,9 +170,7 @@ bool OSystem_libretro::checkPathSetting(const char *setting, Common::String cons
 	Common::String setPath;
 	if (ConfMan.hasKey(setting))
 		setPath = Common::Path::fromConfig(ConfMan.get(setting)).toString();
-	if (!strcmp(setting, "browser_lastpath") && setPath == "/" && !defaultPath.empty() && defaultPath != "/")
-		ConfMan.removeKey(setting, Common::ConfigManager::kApplicationDomain);
-	else if (setPath.empty() || !(isDirectory ? LibRetroFilesystemNode(setPath).isDirectory() : LibRetroFilesystemNode(setPath).exists()))
+	if (setPath.empty() || !(isDirectory ? LibRetroFilesystemNode(setPath).isDirectory() : LibRetroFilesystemNode(setPath).exists()))
 		ConfMan.removeKey(setting, Common::ConfigManager::kApplicationDomain);
 
 	if (! ConfMan.hasKey(setting))
