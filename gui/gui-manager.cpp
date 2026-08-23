4429ba16b48c191d1c94e9045fa5ac445775c054
Le Philousophe <lephilousophe@users.noreply.github.com>
2026-08-23 19:12:19 +0200

    Revert "GUI: Prevent use-after-free in `_tooltip`"
    This reverts commit 80aca9635f1baedb97f7e058cfc47a4c545d8d89.
    
    This introduces a memory leak because the _tooltip variable is cleared
    when closeTopDialog is called.
    We could circumvent this by keeping the local variable as before but
    still clearing _tooltip in the end.
    I prefer that we properly understand the underlying bug instead.
    closeTopDialog is expected to be called before returning from runModal
    and deleting the tooltip.

--- gui/gui-manager.cpp
+++ gui/gui-manager.cpp
@@ -701,12 +701,12 @@ void GuiManager::runLoop() {
 						wdg->handleTooltipUpdate(_lastMousePosition.x + activeDialog->_x - wdg->getAbsX(), _lastMousePosition.y + activeDialog->_y - wdg->getAbsY());
 
 					if (wdg->hasTooltip()) {
-						_tooltip = new Tooltip();
-						_tooltip->setup(activeDialog, wdg, _lastMousePosition.x, _lastMousePosition.y);
+						Tooltip *tooltip = new Tooltip();
+						tooltip->setup(activeDialog, wdg, _lastMousePosition.x, _lastMousePosition.y);
+						_tooltip = tooltip;
 						_tooltip->runModal();
 						// _tooltip is reset in closeTopDialog
-						delete _tooltip;
-						_tooltip = nullptr;
+						delete tooltip;
 					}
 				}
 			}
