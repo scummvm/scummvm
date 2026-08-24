9358a2b0b8997c63b9e5b0318eea6d7d821a73aa
Martin Gerhardy <martin.gerhardy@gmail.com>
2026-08-24 21:11:09 +0200

    MACS2: fixed CID 1685486

--- engines/macs2/macs2.cpp
+++ engines/macs2/macs2.cpp
@@ -1672,18 +1672,19 @@ bool Macs2Engine::loadDeltaAnimResource(uint8 resourceIndex, uint16 executingObj
 		_fileStream->seek(oldPos, SEEK_SET);
 		return false;
 	}
+	const uint16 numFrames = frameCount;
 
 	// Scripts call addDeltaSfx before playDiskDelta; keep the pending SFX list.
 	Common::Array<DeltaSfxEvent> savedSfx = Common::move(_deltaAnim.sfxEvents);
 	clearDeltaAnim();
 	_deltaAnim.sfxEvents = Common::move(savedSfx);
 	_fileStream->read(_deltaAnim.palette, 0x300);
-	_deltaAnim.frames.resize(frameCount);
-	_deltaAnim.frameCount = frameCount;
+	_deltaAnim.frames.resize(numFrames);
+	_deltaAnim.frameCount = numFrames;
 	_deltaAnim.loaded = true;
 
 	const uint32 base = address + 4;
-	for (uint16 fi = 0; fi < frameCount; fi++) {
+	for (uint16 fi = 0; fi < numFrames; fi++) {
 		const uint32 absOff = relOffsets[fi] + base;
 		if (absOff >= (uint32)_fileStream->size())
 			continue;
