4c24d3fd651bf93ffefbd119bc1e116b9e2ba51c
neuromancer <gustavo.grieco@gmail.com>
2026-09-02 12:43:04 +0200

    HOLLYWOOD: normalized the ScummVM license header in all nine identified files

--- engines/hollywood/scenes/playable/scene6080.h
+++ engines/hollywood/scenes/playable/scene6080.h
@@ -2,7 +2,7 @@
  *
  * ScummVM is the legal property of its developers, whose names
  * are too numerous to list here. Please refer to the COPYRIGHT
- * file for details.
+ * file distributed with this source distribution.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -11,11 +11,12 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #ifndef HOLLYWOOD_SCENES_PLAYABLE_SCENE6080_H
--- engines/hollywood/scenes/scene_text_store.cpp
+++ engines/hollywood/scenes/scene_text_store.cpp
@@ -2,7 +2,7 @@
  *
  * ScummVM is the legal property of its developers, whose names
  * are too numerous to list here. Please refer to the COPYRIGHT
- * file distributed with this program.
+ * file distributed with this source distribution.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
