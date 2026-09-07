1ce6a9973b337059905b04d16871d64cd8031c55
Cameron Cawley <ccawley2011@gmail.com>
2026-09-07 17:09:17 +0100

    SURFACESDL: Fix setting the colour key for mouse cursors with SDL3

--- backends/graphics/surfacesdl/surfacesdl-graphics.h
+++ backends/graphics/surfacesdl/surfacesdl-graphics.h
@@ -204,7 +204,6 @@ protected:
 	virtual void SDL_UpdateRects(SDL_Surface *screen, int numrects, SDL_Rect *rects);
 	int SDL_SetColors(SDL_Surface *surface, SDL_Color *colors, int firstcolor, int ncolors);
 	int SDL_SetAlpha(SDL_Surface *surface, Uint32 flag, Uint8 alpha);
-	int SDL_SetColorKey(SDL_Surface *surface, Uint32 flag, Uint32 key);
 #endif
 
 	/** Unseen game screen */
