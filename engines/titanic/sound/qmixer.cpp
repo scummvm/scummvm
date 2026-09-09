1fdb6b3b0ce9e39367dae5940d9de10195d9bb59
Michael <michael_kuerbis@web.de>
2026-09-09 22:15:18 +0200

    TITANIC: use arrow dereferencing operator

--- engines/titanic/sound/qmixer.cpp
+++ engines/titanic/sound/qmixer.cpp
@@ -73,7 +73,7 @@ void QMixer::qsWaveMixFlushChannel(int iChannel, uint flags) {
 		Common::List<SoundEntry>::iterator i;
 		Common::List<SoundEntry> &sounds = _channels[iChannel]._sounds;
 		for (i = sounds.begin(); i != sounds.end(); ++i)
-			_mixer->stopHandle((*i)._soundHandle);
+			_mixer->stopHandle(i->_soundHandle);
 
 		sounds.clear();
 	}
