d0e22ea62555f0c33322fc0a502369ff87d8330c
Shreshtha Sartha <shreshthasartha@MacBookAir.Excitel>
2026-09-16 01:44:48 +0530

    Fix stale game list selection after filtering (#17040)

--- gui/widgets/groupedlist.cpp
+++ gui/widgets/groupedlist.cpp
@@ -584,6 +584,7 @@ void GroupedListWidget::setFilter(const Common::U32String &filter, bool redraw) 
 	_fluidScroller->setPosition(_scrollPos);
 	_selectedItem = -1;
 	_lastSelectionStartItem = -1;
+	clearSelection();
 
 	if (redraw) {
 		scrollBarRecalc();
